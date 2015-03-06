3a01e0adba7f25c9bdb1c09a407cbfcbf531028b
Louise Beh <louise.beh@gmail.com>
2015-03-06 16:31:56 +0000

    Added pin class & methods with successful tests

---
+++ src/Pin.js
@@ -0,0 +1,17 @@
+var Pin = function (){
+  this.standing = true;
+};
+
+  Pin.prototype.fall = function() {
+    this.standing = false;
+  };
+
+
+  Pin.prototype.qwerty = function() {
+    if (this.standing === false) {
+      return 'not standing';
+    } else {
+      return 'standing';
+    }
+  };
+

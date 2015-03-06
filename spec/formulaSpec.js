7e980b56807f8069922575bbe2a1ed347bd01365
Spike Nox <spike01@gmail.com>
2015-03-06 15:10:24 +0000

    Mrr

--- spec/formulaSpec.js
+++ spec/formulaSpec.js
@@ -4,7 +4,7 @@ describe('Formula', function() {
 
     it('selects a random formula', function() {
       spyOn(Math, 'random').and.returnValue(0.1)
-      expect(formula.random()).toEqual('profession noun')
+      expect(formula.random()).toEqual('rofession noun')
     });
 
     it('composes a pub name when given a formula', function() {

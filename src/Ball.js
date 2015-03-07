879a27792e13c815dd7bcc6691f95a2033e514b6
wardymate <chriswarddev@gmail.com>
2015-03-07 15:42:12 +0000

    Ball knows its score and whether it is a strike

---
+++ src/Ball.js
@@ -0,0 +1,14 @@
+var Ball = function() {
+  this.score = 0;
+  this.isRolled = false;
+  this.isStrike = false;
+};
+
+Ball.prototype.setScore = function(score) {
+  this.score = score;
+  this.isRolled = true;
+  if (score === 10) {this.isStrike = true;};
+};
+
+
+

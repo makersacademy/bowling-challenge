1c6d54b594162879641e7f3d827370028831885e
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-08 15:48:10 +0000

    three tests passing

--- js/bowling.js
+++ js/bowling.js
@@ -1,7 +1,16 @@
 var Bowling = function() {
   this.currentScore = 0;
+  this.gutterGame = 0
 };
 
 Bowling.prototype.currentScore = function() {
-  this.currentScore = 0;
+  this.currentScore;
+};
+
+Bowling.prototype.gutterGame = function() {
+  this.gutterGame;
+};
+
+Bowling.prototype.roll = function(pins){
+  this.currentScore += pins;
 };
--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,8 +5,17 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
-  it('has a total score of 0 when started', function() {
+  it('should have a total score of 0 when started', function() {
     expect(bowling.currentScore).toEqual(0);
+  });
+
+  it('should have a total score of 0 when a gutter game is bowled', function() {
+    expect(bowling.gutterGame).toEqual(0);
+  });
+
+  it('should have a total score of 20 when all ones are bowled', function() {
+    bowling.roll(20);
+    expect(bowling.currentScore).toEqual(20)
   });
 
 });

9a0abd9334b9411fc1deed838dce487052334c5a
Tom Coakes <tomcoakes@gmail.com>
2015-03-08 21:11:16 +0000

    The scorecard tracks the frame number

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,10 +1,25 @@
 describe("Bowling", function() {
 
-var scorecard = new Scorecard();
+  beforeEach(function() {
+    scorecard = new Scorecard();
+  });
 
   it("can add points to a total score", function() {
     scorecard.addPoint(4);
     expect(scorecard.totalScore).toEqual(4);
   });
+
+  it("doesn't move to the next frame after only one bowl has been added", function() {
+    scorecard.addPoint(4);
+    expect(scorecard.currentFrame).toEqual(1);
+  });
+
+  it("moves to the next frame after two bowls have been added", function() {
+    scorecard.addPoint(4);
+    scorecard.addPoint(4);
+    expect(scorecard.currentFrame).toEqual(2);
+  });
+
+
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,7 +1,21 @@
 var Scorecard = function() {
   this.totalScore = 0;
+  this.currentFrame = 1;
+  this.bowlNumber = 1;
 };
 
 Scorecard.prototype.addPoint = function(points) {
   this.totalScore += points;
+  this.switchBowlNumber();
+  this.updateFrame();
+};
+
+Scorecard.prototype.switchBowlNumber = function() {
+  (this.bowlNumber === 1) ? this.bowlNumber = 2 : this.bowlNumber = 1;
+};
+
+Scorecard.prototype.updateFrame = function() {
+  if(this.bowlNumber === 1) {
+    this.currentFrame += 1;
+  }
 };

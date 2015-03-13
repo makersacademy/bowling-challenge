331760591f4b96e4b07dacbcbd71cc4296d23af8
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-13 11:59:50 +0000

    Implemented Spy testing. All green.

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,24 +1,37 @@
 describe("Bowling Game", function() {
-  var game = new BowlingGame();
-  var frame1 = new BowlingFrame(3);
-  var frame2; //ToDo: How to spy again???
+  var game;
+  var frame;
+  var frame1;
+  var frame2;
 
+
+  var setupGame = function(){
+    // frames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10];
+    game = new BowlingGame();
+    game.hold(frames);
+  };
 
   describe("Taking and saving rolls", function() {
 
+    beforeEach(function() {
+      frame1 = jasmine.createSpyObj('frame1', ['rolls', 'bonus', 'saveRoll']);
+      frame2 = jasmine.createSpyObj('frame2', ['rolls', 'bonus', 'saveRoll']);
+      frames = [frame1, frame2];
+      setupGame();
+    });
+
+
     it("can take a roll", function() {
-      game.roll(3)
-      expect(game.currentFrame).toEqual(frame1)
+      game.roll(3);
+      expect(frame1.saveRoll).toHaveBeenCalledWith(3);
     });
 
     it("switches to next frame after second roll", function(){
-      //the last spec's 3 pins are still there.
-      game.roll(2)
-      game.roll(3)
-      expect(game.scoreCard).toEqual(frame2)
-      expect(game.currentFrame).toEqual(frame1)
+      game.roll(7);
+      game.roll(2);
+      expect(game.currentFrame).toBe(undefined)
+      game.roll(3);
+      expect(game.scoreCard).not.toEqual([])
     });
-
-
   });
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,11 +1,13 @@
 BowlingGame = function(){
   this.currentFrame;
   this.scoreCard = [];
+  this.freshFrames = [];
 };
 
 BowlingGame.prototype.roll = function(pins) {
   if(this.currentFrame == undefined){
-    this.currentFrame = new BowlingFrame(pins);
+    this.nextFrame();
+    this.currentFrame.saveRoll(pins);
   }else{
     this.currentFrame.saveRoll(pins);
     this.finishFrame();
@@ -16,4 +18,19 @@ BowlingGame.prototype.roll = function(pins) {
 BowlingGame.prototype.finishFrame = function() {
   this.scoreCard.push(this.currentFrame)
   this.currentFrame = undefined;
+};
+
+
+BowlingGame.prototype.hold = function (frame) {
+  if(Array.isArray(frame))
+    {for(i=0; i<frame.length; i++){this.freshFrames.push(frames[i])};
+  }else{
+  this.freshFrames.push(frame);
+  }
+
+};
+
+
+BowlingGame.prototype.nextFrame = function () {
+  this.currentFrame = this.freshFrames.shift();
 };

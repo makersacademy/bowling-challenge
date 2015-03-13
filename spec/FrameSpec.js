bb958b065e809c903de522915c8c1ac3adbf69de
Kate <katebeavis84@gmail.com>
2015-03-13 14:47:11 +0000

    Changed frame class to bowl, creating one class before refactoring

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,20 +1,38 @@
-describe ("Frame", function() {
+describe ("Bowl", function() {
 
-  var frame;
+  var roll1;
+  var bowl;
+  bowl = new Bowl();
 
-  beforeEach(function() {
-    frame = new Frame;
-});
 
-  it("should have a score of 0 when created", function() {
-    expect(frame.score).toEqual(0);
+
+
+  it("should have a frame 1 with a score of 0 when created", function() {
+    expect(bowl.frame1).toEqual(0);
   });
 
+  it("should have a frame 2 with a score of 0 when created", function() {
+    expect(bowl.frame2).toEqual(0);
+  });
 
+  it("should have 10 pins when created", function() {
+    expect(bowl.pins).toEqual(10);
+  });
 
+  it("should be able to move onto the next frame", function() {
+    bowl.nextFrame();
+    expect(bowl.frameNumber).toEqual(2);
+  });
 
+  it("should let rolls hit pins", function() {
+    bowl.pinHit();
+    expect(bowl.pins).toEqual(9);
+  });
 
-
+  xit("should only let the roll have a maximum of 10", function() {
+    roll1.roll;
+    expect(bowl.roll1(11)).toEqual(10)
+  });
 
 
 
--- src/Frame.js
+++ src/Frame.js
@@ -1,3 +1,14 @@
-var Frame = function() {
- this.score = 0;
+var Bowl = function() {
+ this.frame1 = 0;
+ this.frame2 = 0;
+ this.pins = 10;
+ this.frameNumber = 1;
+};
+
+Bowl.prototype.nextFrame = function() {
+  this.frameNumber++;
+};
+
+Bowl.prototype.pinHit = function() {
+  this.pins -= 1;
 };

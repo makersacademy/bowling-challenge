6a49d39f639bac0a224dd80ab12cfa846b28c261
Vanessa <virgitti@gmail.com>
2015-03-13 15:33:44 +0000

    added test to add a second try

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -9,9 +9,21 @@ describe("game", function() {
   });
 
   it('can have 3 pins down after the first try', function() {
-    game.hitPins(3);
+    game.hitPinsFirstTry(3);
     expect(game.fallenPins).toEqual(3);
   });
+
+  it('can try a second time if the first try was not a strike', function(){
+    game.hitPinsFirstTry(3);
+    game.hitPinsSecondTry(4);
+    game.sumFirstSecondTries();
+    expect(game.fallenPins).toEqual(7);
+  });
+
+  // it ('can move to the second frame if the first try is a strike', function (){
+  //   game.hitPins(10);
+  //   expect(game.frameNumber).toEqual(2);
+  // });
 
 
 
--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,32 @@
-var frame = 1;
+var frameNumber = 1;
 var totalScore = 0;
 var currentScore = 0;
 var bonus = 0;
 var maxPinsDown = 10;
+var firstTry = 0;
+var secondTry = 0;
+var sumTries = 0;
 
 var Game = function() {
   this.fallenPins = 0;
 };
 
-Game.prototype.hitPins = function(n) {
+Game.prototype.hitPinsFirstTry = function(n) {
   for(this.fallenPins = 0; this.fallenPins < n; this.fallenPins ++){
-    console.log(this.fallenPins + " pins have been knocked out at the " + n + " turn");
-    console.log(game.fallenPins);
   }
+  console.log(this.fallenPins + " have been knocked down on first try");
+  var firstTry = this.fallenPins;
 };
+
+Game.prototype.hitPinsSecondTry = function(n) {
+    for(this.fallenPins = 0; this.fallenPins < n; this.fallenPins ++){
+  }
+  console.log(this.fallenPins + " have been knocked down on the second try");
+  var secondTry = this.fallenPins;
+};
+
+Game.prototype.sumFirstSecondTries = function() {
+  firstTry + secondTry;
+  console.log(this.fallenPins + " total");
+};
+

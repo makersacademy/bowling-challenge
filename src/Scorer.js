34ec936512795168cd73d0721bf6aa4d3372bfd9
wardymate <chriswarddev@gmail.com>
2015-03-13 09:47:42 +0000

    scorer scores one spare correctly

--- src/Scorer.js
+++ src/Scorer.js
@@ -1,8 +1,20 @@
 var Scorer = function() {
-  this.total = 0
+  this.rolls = [];
 };
 
 Scorer.prototype.roll = function(score) {
-  // body...
+  this.rolls.push(score);
 };
 
+Scorer.prototype.total = function(first_argument) {
+  var numberOfRolls = this.rolls.length;
+  var runningTotal = 0;
+  for (var i = 0; i < numberOfRolls; i++) {
+    if (this.rolls[i]+this.rolls[i+1] ===10){
+      runningTotal = runningTotal + this.rolls[i+2];
+    };
+    runningTotal = runningTotal + this.rolls[i];
+  };
+  return runningTotal;
+
+};

1cc8b21bd6228a4f1151c7125d1596e00c4e7f77
Kate <katebeavis84@gmail.com>
2015-03-13 15:55:50 +0000

    Roll and frame have maximum numbers

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -2,40 +2,63 @@ describe ("Bowl", function() {
 
   var roll1;
   var bowl;
-  bowl = new Bowl();
-
-
-
-
-  it("should have a frame 1 with a score of 0 when created", function() {
-    expect(bowl.frame1).toEqual(0);
+  beforeEach(function() {
+  bowl = new Bowl;
   });
 
-  it("should have a frame 2 with a score of 0 when created", function() {
-    expect(bowl.frame2).toEqual(0);
+  describe ("Starting a game", function() {
+
+    it("should have a roll 1 with a score of 0 when created", function() {
+      expect(bowl.roll1).toEqual(0);
+    });
+
+    it("should have a roll 2 with a score of 0 when created", function() {
+      expect(bowl.roll2).toEqual(0);
+    });
+
+    it("should have 10 pins when created", function() {
+      expect(bowl.pins).toEqual(10);
+    });
+
+    it("should only have a maxiumum of 10 frames", function() {
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      bowl.nextFrame();
+      expect(bowl.frameNumber).toEqual(10);
+    });
+
   });
 
-  it("should have 10 pins when created", function() {
-    expect(bowl.pins).toEqual(10);
+  describe ("Playing the game", function() {
+
+    it("should be able to move onto the next frame", function() {
+      bowl.nextFrame();
+      expect(bowl.frameNumber).toEqual(2);
+    });
+
+    it("should let rolls hit pins", function() {
+      bowl.pinHit(roll1);
+      expect(bowl.pins).toEqual(9);
+    });
+
+    it("should increase the roll score when a roll hits a pin", function() {
+      bowl.roll(10);
+      expect(bowl.roll1).toEqual(10);
+    });
+
+    it("should only let the roll have a maximum of 10", function() {
+      bowl.roll(15);
+      expect(bowl.roll1).toEqual(10)
+    });
+
   });
-
-  it("should be able to move onto the next frame", function() {
-    bowl.nextFrame();
-    expect(bowl.frameNumber).toEqual(2);
-  });
-
-  it("should let rolls hit pins", function() {
-    bowl.pinHit();
-    expect(bowl.pins).toEqual(9);
-  });
-
-  xit("should only let the roll have a maximum of 10", function() {
-    roll1.roll;
-    expect(bowl.roll1(11)).toEqual(10)
-  });
-
-
-
-
 
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,14 +1,26 @@
 var Bowl = function() {
- this.frame1 = 0;
- this.frame2 = 0;
+ this.roll1 = 0;
+ this.roll2 = 0;
+ this.score = 0;
  this.pins = 10;
+ this.maximumHit = 10;
  this.frameNumber = 1;
+ this.maximumFrames = 10;
 };
 
 Bowl.prototype.nextFrame = function() {
   this.frameNumber++;
+  if (this.frameNumber > this.maximumFrames)
+    this.frameNumber = this.maximumFrames;
 };
 
-Bowl.prototype.pinHit = function() {
+Bowl.prototype.pinHit = function(roll1) {
   this.pins -= 1;
+};
+
+Bowl.prototype.roll = function(number) {
+  number = number;
+  this.roll1 = number;
+  if (this.roll1 > this.maximumHit)
+    this.roll1 = this.maximumHit;
 };

25f20d14f364d60f8baccc7a3fa44eaf0f35cbb8
wardymate <chriswarddev@gmail.com>
2015-03-14 10:49:07 +0000

    started html and css

--- src/Scorer.js
+++ src/Scorer.js
@@ -7,20 +7,22 @@ Scorer.prototype.roll = function(score) {
 };
 
 Scorer.prototype.total = function() {
+
   var runningTotal = 0;
   var rollNumber = 0;
   var frameNumber = 1
+
   for (var i = frameNumber; i < 11; i++) {
     if(this.rolls[rollNumber] === 10) {
       runningTotal += this.rolls[rollNumber] + this.rolls[rollNumber+1]+ this.rolls[rollNumber+2];
-      rollNumber = rollNumber+1;}
+      rollNumber +=1;}
     else if (this.rolls[rollNumber] + this.rolls[rollNumber+1] === 10) {
       runningTotal += this.rolls[rollNumber] + this.rolls[rollNumber+1]+ this.rolls[rollNumber+2];
-      rollNumber = rollNumber+2;
+      rollNumber +=2;
     }
     else {
       runningTotal += this.rolls[rollNumber] + this.rolls[rollNumber+1];
-      rollNumber = rollNumber+2;
+      rollNumber +=2;
     }
   };
   return runningTotal;

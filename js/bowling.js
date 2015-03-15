ef79e403bfd9535b62fb892f9dad1efb81f41032
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-15 13:13:44 +0000

    can roll a strike

--- js/bowling.js
+++ js/bowling.js
@@ -15,14 +15,42 @@ Bowling.prototype.roll = function(pins) {
 Bowling.prototype.currentScore = function() {
   var score = 0;
   var rollNumber = 0;
+  var bowl = this;
 
   for (var frameNumber = 0; frameNumber < 10; frameNumber++) {
-    if(this.rolls[rollNumber] + this.rolls[rollNumber + 1] == 10) {
-      score += this.rolls[rollNumber] + this.rolls[rollNumber + 1] + this.rolls[rollNumber + 2];
-    } else {
-      score += this.rolls[rollNumber] + this.rolls[rollNumber + 1];
+    if(rollStrike()) {
+      score += strikeScore();
+      rollNumber++;
     }
-    rollNumber += 2;
+    else if(rollSpare()) {
+      score += spareScore();
+      rollNumber += 2;
+    } else {
+      score += normalScore();
+      rollNumber += 2;
+    }
   }
+
   return score;
+
+  function rollStrike() {
+    return bowl.rolls[rollNumber] == 10;
+  }
+
+  function rollSpare() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1] == 10;
+  }
+
+  function strikeScore() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1] + bowl.rolls[rollNumber + 2];
+  }
+
+  function spareScore() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1] + bowl.rolls[rollNumber + 2];
+  }
+
+  function normalScore() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1];
+  }
+
 };

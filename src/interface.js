1f6b258f3f65210a99e499715159769096a3d80a
Vanessa <virgitti@gmail.com>
2015-03-15 22:43:12 +0000

    fixed adding score to html

--- src/interface.js
+++ src/interface.js
@@ -3,6 +3,9 @@ var game = new Game();
 $(document).ready(function (){
 
   $('#button-6').click(function(){
+    game.hitPinsFirstTry();
+    game.hitPinsSecondTry();
+    game.sumFirstSecondTries();
   $(".frame-1").html("6");
   });
 

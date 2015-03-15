b96cff35ecc0d811a759b9b0aaa4d0e748695ea8
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-15 10:56:29 +0000

    restarted to make it simple

--- js/bowling.js
+++ js/bowling.js
@@ -1,49 +1,10 @@
 var Bowling = function() {
-  this.pins = 10;
-  this.player = 1;
-  this.frameNumber = 1;
-  this.rollNumber = 1;
-  this.strike = 0;
-  this.spare = 0;
   this.currentScore = 0;
 };
 
-Bowling.prototype.pinsHit = function(number) {
-  if(number === 10)
-  {
-    this.strike += 1;
-    this.rollNumber + 1;
-    this.totalScore();
-    this.nextFrame();
+Bowling.prototype.roll = function(number, pins) {
+  var i;
+  for (i = 0; i < number; i++) {
+    this.roll(pins);
   }
-  else{this.pins = (this.pins -= number);
-    this.rollSpare();
-    this.nextFrame();
-  }
-};
-
-Bowling.prototype.rollSpare = function() {
-  if(this.pins === 0) {this.spare += 1;}
-};
-
-Bowling.prototype.nextFrame = function() {
-  if(this.rollNumber === 1) {this.rollNumber += 1;}
-  else if(this.rollNumber === 2)
-  {
-    this.rollNumber -= 1;
-    this.totalScore();
-    this.frameTotal();
-    this.pins = 10;
-  }
-};
-
-Bowling.prototype.frameTotal = function() {
-  if(this.frameNumber === 10) {this.frameNumber = 1; return console.log("Game over");}
-  else{this.frameNumber += 1;}
-};
-
-Bowling.prototype.totalScore = function() {
-  if(this.spare != 0) {(this.currentScore = (10 * this.spare) + this.currentScore);}
-  else if(this.strike != 0) {(this.currentScore = (10 * this.strike) + this.currentScore);}
-  else{(this.currentScore = (10 - this.pins) + this.currentScore);}
 };
--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,63 +5,9 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
-  it('starts with 10 pins standing', function() {
-    expect(bowling.pins).toEqual(10);
+  it('should be able to roll a gutter game', function() {
+    bowling.roll(20, 0);
+    expect(bowling.currentScore).toEqual(0);
   });
 
-  it('pins can be knocked down', function() {
-    bowling.pinsHit(2);
-    expect(bowling.pins).toEqual(8);
-  });
-
-  it('can have a strike', function() {
-    bowling.pinsHit(10);
-    expect(bowling.strike).toBe(1);
-  });
-
-  it('can have one player', function() {
-    expect(bowling.player).toEqual(1);
-  });
-
-  it('player can start a frame', function() {
-    expect(bowling.rollNumber).toBe(bowling.player);
-  });
-
-  it('player can have two rolls in one frame', function() {
-    expect(bowling.rollNumber).toEqual(1);
-    bowling.pinsHit(9);
-    expect(bowling.rollNumber).toEqual(2);
-  });
-
-  it('player can roll a spare', function() {
-    bowling.pinsHit(5);
-    bowling.pinsHit(5);
-    expect(bowling.spare).toEqual(1);
-  });
-
-  it('player can move to the next frame after roll 2', function() {
-    expect(bowling.rollNumber).toEqual(1);
-    bowling.pinsHit(9);
-    expect(bowling.rollNumber).toEqual(2);
-    bowling.pinsHit(1);
-    expect(bowling.frameNumber).toEqual(2);
-  });
-
-  it('player can move to the next frame when hitting a strike', function() {
-    expect(bowling.rollNumber).toEqual(1);
-    bowling.pinsHit(10);
-    expect(bowling.rollNumber).toEqual(2);
-  });
-
-  it('player can have a total score after each frame', function() {
-    bowling.pinsHit(2);
-    bowling.pinsHit(3);
-    expect(bowling.currentScore).toEqual(5);
-  });
-
-  // it('should add the score for a strike to the next frame score', function() {
-  //   bowling.pinsHit(10);
-  //   expect(bowling.strike).toBe(1);
-  //   expect(bowling.currentScore).toEqual(0);
-  // });
 });

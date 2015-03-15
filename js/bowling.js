7305137cc33eb7d52e56a7c61e1a6cb62318f5a3
Kevin Lanzon <kevinlanzon@gmail.com>
2015-03-15 14:31:59 +0000

    switched back to simple versin. Need to limit frames to 10

--- js/bowling.js
+++ js/bowling.js
@@ -1,80 +1,50 @@
 var Bowling = function() {
-  this.pins = 10;
-  this.roll1 = 0;
-  this.roll2 = 0;
-  this.player = 1;
-  this.frameNumber = 1;
-  this.rollNumber = 1;
-  this.strike = 0;
-  this.spare = 0;
-  this.currentScore = 0;
-  this.bonusStrike = 0;
-  this.bonusSpare = 0;
+  this.rolls = [];
+  this.frameNumber = 0;
+  this.score = 0;
 };
 
-Bowling.prototype.pinsHit = function(number) {
-  if(number === 10)
-  {
-    this.strike += 1;
-    this.rollNumber + 1;
-    this.nextFrame();
-  }
-  else{this.pins = (this.pins -= number);
-    this.rollSpare();
-    this.nextFrame();
+Bowling.prototype.gameRolls = function(pins, number) {
+  for( var i = 0; i < number; i += 1) {
+    this.roll(pins);
   }
 };
 
-Bowling.prototype.rollSpare = function() {
-
-  if(this.pins === 0)
-  {
-    this.spare += 1
-  }
-  else if (this.rollNumber === 1)
-  {
-    this.roll1 = (10 - this.pins)
-  }
-  else (this.rollNumber === 2)
-    this.roll2 = ((10 - this.pins) - this.roll1)
-  };
-
-Bowling.prototype.nextFrame = function() {
-  if(this.rollNumber === 1) {this.rollNumber += 1;}
-  else if(this.rollNumber === 2)
-  {
-    this.rollNumber -= 1;
-    this.totalScore();
-    this.frameTotal();
-    this.pins = 10;
-  }
+Bowling.prototype.roll = function(pins) {
+  this.rolls.push(pins);
 };
 
-Bowling.prototype.frameTotal = function() {
-  if(this.frameNumber === 10) {this.frameNumber = 1; return console.log("Game over");}
-  else{this.frameNumber += 1;}
-};
+Bowling.prototype.currentScore = function() {
+  var rollNumber = 0;
+  var bowl = this;
 
-Bowling.prototype.totalScore = function() {
-  if(this.strike != 0)
-  {
-    (this.bonusStrike = (10 * this.strike) + this.bonusSrike)
-    this.strike = 0;
-  }
-  else if(this.spare != 0)
-  {
-    (this.bonusSpare = (10 * this.spare) + this.bonusSpare)
-    this.spare = 0
-  }
-  else
-  {
-    if(this.bonusSpare > 0)
-    {
-      (this.currentScore = (10 - this.pins + (this.bonusSpare)) + (this.roll1) + this.currentScore)
+  for(this.frameNumber = 0; this.frameNumber < 10; this.frameNumber += 1) {
+    if(rollStrike()) {
+      this.score += strikeScore();
+      rollNumber += 1;
     }
-    else if(this.bonusStrike > 0)
-    {
-      (this.currentScore = (10 - this.pins + (this.bonusStrike)) + (this.roll1 + this.roll2) + this.currentScore)
-    };
-  };
+    else if(rollSpare()) {
+      this.score += spareScore();
+      rollNumber += 2;
+    } else {
+      this.score += normalScore();
+      rollNumber += 2;
+    }
+  }
+  return this.score;
+
+  function rollStrike() {
+    return bowl.rolls[rollNumber] == 10;}
+
+  function rollSpare() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1] == 10;}
+
+  function strikeScore() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1] + bowl.rolls[rollNumber + 2];}
+
+  function spareScore() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1] + bowl.rolls[rollNumber + 2];}
+
+  function normalScore() {
+    return bowl.rolls[rollNumber] + bowl.rolls[rollNumber + 1];}
 };

a7829ccc58b3a14ee0672f819d44e431166b1441
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-16 00:00:05 +0000

    frame 10 coming along

--- spec/FrameTenSpec.js
+++ spec/FrameTenSpec.js
@@ -5,7 +5,8 @@ describe ('FrameTen', function(){
 
   beforeEach(function() {
     frame = new FrameTen;
-    player = jasmine.createSpyObj('player', ['roll']);
+    player = new Player
+    player.newFrame()
   });
 
   it('has 10 pins on initialization', function(){
@@ -13,21 +14,49 @@ describe ('FrameTen', function(){
   });
 
   it('if roll one is a strike the roll one score is 10', function(){
-    player.roll = 10
-    frame.getRollOne(player.roll)
+    spyOn(player, "roll").and.returnValue(10);
+    roll = player.roll()
+    frame.getRollOne(roll, player)
     expect(frame.rollOneScore).toEqual(10)
   });
    
   it('if roll one is not a strike, it knows that it will have to add up the roll score', function(){
-    player.roll = 5
-    frame.getRollOne(player.roll)
+    spyOn(player, "roll").and.returnValue(5);
+    roll = player.roll()
+    frame.getRollOne(roll, player)
     expect(frame.rollOneScore).toEqual(5)
   }); 
 
-  it('if knows that if roll one was not a strike, then the pins remaining should be less than the roll', function(){
-    player.roll = 5
-    frame.getRollOne(player.roll)
+  it('knows that if roll one was not a strike, then the pins remaining should be less than the roll', function(){
+    spyOn(player, "roll").and.returnValue(5);
+    roll = player.roll()
+    frame.getRollOne(roll, player)
     expect(frame.pinsRemaining).toEqual(5)
+  });
+
+  it('knows that if roll one was not a strike, then it has to add up roll two scores', function(){
+    spyOn(player, "roll").and.returnValue(5);
+    roll = player.roll()
+    frame.getRollOne(roll, player);
+    frame.getRollTwo(roll, player);
+    expect(frame.rollOneScore).toEqual(5)
+    expect(frame.rollTwoScore).toEqual(5)
+  });
+
+  it('knows that if there was a spare on roll two, that roll three can commence', function(){
+    spyOn(player, "roll").and.returnValue(5);
+    roll = player.roll()
+    frame.getRollOne(roll, player);
+    frame.getRollTwo(roll, player);
+    expect(frame.isRollThree()).toEqual(true);  
+  });
+
+  it('knows that if there was NOT a spare on roll two, that roll three cannot commence', function(){
+    spyOn(player, "roll").and.returnValue(3);
+    roll = player.roll();
+    frame.getRollOne(roll, player);
+    frame.getRollTwo(roll, player);
+    expect(frame.isRollThree()).toEqual(false);  
   });
 
 });
--- src/FrameTen.js
+++ src/FrameTen.js
@@ -8,17 +8,40 @@ FrameTen = function() {
   this.rollThreeDone = false
 };
 
-FrameTen.prototype.getRollOne = function(roll) {
-  if(roll === 10) return this.rollOneScore = 10
-  if(roll !== 10) return this.rollOneScore = this.getScore(roll)
+FrameTen.prototype.getRollOne = function(roll, player) {
+  if(roll === 10) {
+    this.rollOneScore = 10
+    player.newFrame()
+  }else if(roll !== 10) {
+    this.rollOneScore = this.getScore(roll)   
+  }
+  this.rollOneDone = true;
 };
 
-FrameTen.prototype.getRollTwo = function(roll){
+FrameTen.prototype.getRollTwo = function(roll, player){
+  if(roll === 10) {
+    this.rollOneScore = 10
+    player.newFrame()
+  }else if(roll !== 10) {
+    this.rollTwoScore = this.getScore(roll)   
+  }; 
+  if(this.rollOneScore + this.rollTwoScore === 10){
+    player.newFrame;
+  }
+  this.rollTwoDone = true;
+};
 
+FrameTen.prototype.isRollThree = function(){
+  if(this.rollOneScore === 10 || this.rollOneScore + this.rollTwoScore === 10){
+    return true
+  }else{
+    return false
+  }
 };
 
 FrameTen.prototype.getRollThree = function(roll){
-
+  this.rollThreeScore = roll
+  this.rollThreeDone = true
 };
 
 FrameTen.prototype.getScore = function(roll){  

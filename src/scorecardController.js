25cb157b4403d74dd894e43c822677aee12c68f7
Phil Brockwell <nineteen40eight@gmail.com>
2015-03-16 10:40:59 +0000

    refactored controller

--- src/scorecardController.js
+++ src/scorecardController.js
@@ -10,9 +10,7 @@ $('document').ready(function() {
   });
 
   function updateButtons() {
-
     var limit = scorecard.frameRecord[scorecard.getCurrentFrame()].firstBowl;
-
     if (limit === null) {
       $('#buttons > li').show();
     } else {
@@ -20,23 +18,12 @@ $('document').ready(function() {
         $('#buttons > li').eq(i).hide();
       };
     }
-
-    console.log(limit)
-
   };
 
   function update() {
-    $('#frame1').text(scorecard.totals[0]);
-    $('#frame2').text(scorecard.totals[1]);
-    $('#frame3').text(scorecard.totals[2]);
-    $('#frame4').text(scorecard.totals[3]);
-    $('#frame5').text(scorecard.totals[4]);
-    $('#frame6').text(scorecard.totals[5]);
-    $('#frame7').text(scorecard.totals[6]);
-    $('#frame8').text(scorecard.totals[7]);
-    $('#frame9').text(scorecard.totals[8]);
-    $('#frame10').text(scorecard.totals[9]);
+    for (var i = 1; i <= 10; i++) {
+      $('#frame' + i).text(scorecard.totals[i - 1]);
+    };
     $('#total').text(scorecard.currentTotal());
   };
-
 });

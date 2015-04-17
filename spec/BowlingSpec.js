7f312772739e1ebdc783d82124c672c9a4120657
sanjsanj <sanjsanj@hotmail.com>
2015-04-17 22:25:17 +0100

    Can increment frame

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -6,13 +6,19 @@ describe('Bowling scoresheet', function(){
     bowling = new Bowling
   });
 
-  it('can retain the score from a roll', function(){
+  it('can retain the score from a single roll', function(){
     bowling.roll(5);
     expect(bowling.score()).toBe(5);
   });
 
   it('rejects an illegal score', function(){
     expect(function(){bowling.roll(11)}).toThrow('Illegal score');
+  });
+
+  it('can know it has incremented frame', function(){
+    bowling.roll(5);
+    bowling.roll(5);
+    expect(bowling.frame()).toBe(2);
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,10 +1,14 @@
 var Bowling = function(){
   this._score = 0;
+  this._frame = 1;
+  this._rollsThisFrame = 0;
 };
 
 Bowling.prototype.roll = function(rollScore){
   if (rollScore <= 10){
     this._score += rollScore;
+    this._rollsThisFrame ++;
+    this.rollRegistered();
   } else {
     throw 'Illegal score';
   };
@@ -12,4 +16,16 @@ Bowling.prototype.roll = function(rollScore){
 
 Bowling.prototype.score = function(){
   return this._score;
+};
+
+Bowling.prototype.frame = function(){
+  return this._frame;
+};
+
+Bowling.prototype.rollRegistered = function(){
+  if (this._rollsThisFrame < 2){
+    this._rollsThisFrame ++;
+  } else {
+    this._frame ++;
+  };
 };

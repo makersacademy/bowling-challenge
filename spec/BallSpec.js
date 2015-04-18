71abc66f984a984ca444ee059c04f8f81d40fdb8
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-18 16:47:49 +0100

    Ball isnt rolled when created

--- spec/BallSpec.js
+++ spec/BallSpec.js
@@ -10,4 +10,13 @@ describe('Ball', function() {
     expect(ball.score).toEqual(0);
   });
 
+  it("should be able to set it's score", function() {
+    ball.setScore(1);
+    expect(ball.score).toEqual(1);
+  });
+
+  it('should have a status of unrolled when created', function() {
+    expect(ball.isRolled).toBe(false);
+  });
+
 });
--- src/Ball.js
+++ src/Ball.js
@@ -1,3 +1,8 @@
 var Ball = function() {
-  this.score = 0;
+	this.score = 0;
+	this.isRolled = false;
+};
+
+Ball.prototype.setScore = function(score) {
+	this.score = score;
 };

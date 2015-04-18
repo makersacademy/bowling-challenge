0398edcc75a071d40f067647d0c6e3f899b3bdb8
sanjsanj <sanjsanj@hotmail.com>
2015-04-18 20:00:48 +0100

    Can't get hontas method to work

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,5 +1,17 @@
 describe('Bowling scoresheet', function(){
 
+  describe('rejects', function(){
+
+    var bowling;
+    beforeEach(function(){
+      bowling = new Bowling
+    });
+
+    it('an illegal score', function(){
+      expect(function(){bowling.roll(11)}).toThrow('Illegal score');
+    });
+  });
+
   describe('knows the cumulative score for', function(){
 
     var bowling;
@@ -13,14 +25,26 @@ describe('Bowling scoresheet', function(){
       };
     };
 
-    it('a typical game', function(){
-      helper(20, 4);
-      expect(bowling.cumulativeScore()).toBe(80);
+    // it('a single roll', function(){
+    //   helper(1, 5);
+    //   expect(bowling.cumulativeScore()).toBe(5);
+    // });
+
+    it('two non-spare/non-strike rolls', function(){
+      helper(1, 5);
+      helper(1, 2);
+      expect(bowling.cumulativeScore()).toBe(7);
     });
 
-    it('a gutter game', function(){
-      helper(20, 0);
-      expect(bowling.cumulativeScore()).toBe(0);
+    it('a strike and a spare', function(){
+      helper(1, 10);
+      helper(2, 5);
+      expect(bowling.cumulativeScore()).toBe(30);
+    });
+
+    it('an unremarkable game', function(){
+      helper(20, 4);
+      expect(bowling.cumulativeScore()).toBe(80);
     });
 
     it('a perfect game', function(){
@@ -28,34 +52,33 @@ describe('Bowling scoresheet', function(){
       expect(bowling.cumulativeScore()).toBe(300);
     });
 
-    // it('from a single roll', function(){
-    //   bowling.roll(5);
-    //   expect(bowling.cumulativeScore()).toBe(5);
-    // });
-
-    it('from two non-spare/non-strike rolls', function(){
-      bowling.roll(5);
-      bowling.roll(2);
-      expect(bowling.cumulativeScore()).toBe(7);
+    it('a gutter game', function(){
+      helper(20, 0);
+      expect(bowling.cumulativeScore()).toBe(0);
     });
 
-    it('from a strike and a spare', function(){
+    it('an example game', function(){
+      bowling.roll(1);
+      bowling.roll(4);
+      bowling.roll(4);
+      bowling.roll(5);
+      bowling.roll(6);
+      bowling.roll(4);
+      expect(bowling.cumulativeScore()).toBe(24);
+      bowling.roll(5);
+      bowling.roll(5);
       bowling.roll(10);
-      bowling.roll(5);
-      bowling.roll(5);
-      expect(bowling.cumulativeScore()).toBe(30);
-    });
-  });
-
-  describe('rejects', function(){
-
-    var bowling;
-    beforeEach(function(){
-      bowling = new Bowling
-    });
-
-    it('an illegal score', function(){
-      expect(function(){bowling.roll(11)}).toThrow('Illegal score');
+      bowling.roll(0);
+      bowling.roll(1);
+      bowling.roll(7);
+      bowling.roll(3);
+      bowling.roll(6);
+      bowling.roll(4);
+      bowling.roll(10);
+      bowling.roll(2);
+      bowling.roll(8);
+      bowling.roll(6);
+      expect(bowling.cumulativeScore()).toBe(133);
     });
   });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,44 +1,57 @@
 var Bowling = function(){
   this._rolls = [];
-  this._cumulativeScore = 0;
 };
 
 Bowling.prototype.roll = function(rollScore){
+  var self = this;
+
   if (rollScore > 10) throw 'Illegal score';
-  this._rolls.push(rollScore);
-  console.log(this._rolls);
+  self._rolls.push(rollScore);
 };
 
 Bowling.prototype.cumulativeScore = function(){
+  var self = this;
+  var score = 0;
   var frameStart = 0;
-  var rolls = this._rolls;
-  var score = this._cumulativeScore;
 
   function frameTotal(){
-    return rolls[frameStart] + rolls[frameStart + 1] || 0;
-  };
+    // console.log(self._rolls[frameStart] + self._rolls[frameStart + 1] || 0);
+    return self._rolls[frameStart] + self._rolls[frameStart + 1] || 0;
+  }
 
   function spareBonus() {
-    return rolls[frameStart + 2] || 0;
-  };
+    return self._rolls[frameStart + 2];
+  }
 
   function strikeBonus() {
-    return rolls[frameStart + 1] + rolls[frameStart + 2] || 0;
-  };
+    return self._rolls[frameStart + 1] + self._rolls[frameStart + 2];
+  }
+
+  function isStrike(){
+    return self._rolls[frameStart] === 10;
+  }
+
+  function isSpare(){
+    self._rolls[frameStart] + self._rolls[frameStart + 1] === 10;
+  }
 
   for (var i = 0; i < 10; i ++){
-    if (rolls[frameStart] === 10){
+    if (isStrike()){
+      // console.log('strike');
       score += 10 + strikeBonus();
       frameStart ++;
-    } else if (rolls[frameStart] + rolls[frameStart + 1] === 10){
+    } else if (isSpare()){
+      // console.log('spare');
       score += 10 + spareBonus();
-      frameStart ++;
+      frameStart += 2;
     } else {
+      // console.log(score);
       score += frameTotal();
       frameStart += 2;
     };
-  };
-  console.log('---' + score);
+    // console.log('score ' + score);
+    // console.log('framestart ' + frameStart);
+  }
 
   return score;
 };

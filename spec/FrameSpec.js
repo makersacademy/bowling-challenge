80c8baf58635fa3b1dd24be3dcb92bb7cb9b5432
paulwallis42 <paulwallis42@gmail.com>
2015-04-18 11:53:06 +0100

    working strike and spare function on frame 1

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -11,14 +11,28 @@ describe('Frame', function(){
   });
 
   it('frameOne can take scores', function(){
-    frame.frameOne(5, 5)
-    expect(frameOneInfo[0]).toEqual(5)
+    frame.frameOne(5, 5);
+    expect(frameOneInfo[0]).toEqual(5);
   });
 
   it('frameOne can also record a strike', function(){
-    frame.frameOne(10, 0)
-    expect(frameOneInfo[2]).toEqual('strike')
+    frame.frameOne(10, 0);
+    expect(frameOneInfo[2]).toEqual('strike');
   });
 
+  it('frameOne does not record a strike if the first score is not a 10', function(){
+    frame.frameOne(5, 5);
+    expect(frameOneInfo[2]).not.toEqual('strike');
+  });
+
+  it('frameOne records a strike if the first score is a 10', function(){
+    frame.frameOne(10, 0);
+    expect(frameOneInfo[2]).toEqual('strike');
+  });
+
+  it('frameOne can record a spare if the first score is not 10 but the total is 10', function(){
+    frame.frameOne(1, 9);
+    expect(frameOneInfo[2]).toEqual('spare');
+  });
 
 });
--- src/Frame.js
+++ src/Frame.js
@@ -4,8 +4,15 @@ var Frame = function(){
 
 Frame.prototype.frameOne = function (x, y) {
   frameOneInfo = [];    // Told not to do this but test fails with var in front?!
+
   frameOneInfo.push(x);
   frameOneInfo.push(y);
-  frameOneInfo.push('strike')
+
+  if ( x === 10 ) {
+  frameOneInfo.push('strike');
+} else if ( x != 10 && x + y === 10 ) {
+  frameOneInfo.push('spare')
+}
+
   return frameOneInfo;
 };

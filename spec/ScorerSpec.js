237e5c6739a09f9d8ff80426c9371f3a107d45f8
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-19 13:54:54 +0100

    no tests

--- spec/ScorerSpec.js
+++ spec/ScorerSpec.js
@@ -3,44 +3,6 @@ describe('Scorer', function() {
 	var scorer;
 
 	describe('start of a game', function() {
-		scorer = new Scorer();
-		var frame;
-		for (i = 0; i < 9; i++) {
-			frame = new NormalFrame();
-			scorer.loadFrame(frame);
-		};
-
-		it('should accept 9 normal frames', function() {
-			expect(scorer.framesCount()).toEqual(9);
-		});
-
-		it('should have a total score of 0 when game starts', function() {
-			expect(scorer.totalScore).toEqual(0);
-		});
-
-		it('should accept a ball', function() {
-			ball = new Ball();
-			ball.setScore(3)
-			scorer.bowl(ball);
-			expect(scorer.frameOneScore).toEqual(3);
-			expect(scorer.totalScore).toEqual(3);
-		});
-
-	});
-
-	describe('frames one to nine', function() {
-
-		describe('scoring a game with no strikes or spares', function() {
-
-			xit('accepts a ball', function() {
-				ballOne = new Ball();
-				ballOne.setScore(3);
-
-				Scorer()
-				expect(totalScore);
-			});
-
-		});
 
 	});
 
--- src/Scorer.js
+++ src/Scorer.js
@@ -1,17 +1,3 @@
 var Scorer = function() {
-	this.frames = [];
-	this.totalScore = 0;
-	this.frameOneScore = 0;
-};
 
-Scorer.prototype.loadFrame = function(inputFrame) {
-	this.frames.push(inputFrame);
-};
-
-Scorer.prototype.framesCount = function() {
-	return this.frames.length;
-};
-
-Scorer.prototype.bowl = function(ball) {
-	
 };

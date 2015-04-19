4b4f1ffec81d1b2e6555e3b1d5b5b6c4522b7fe7
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-19 14:16:32 +0100

    scorer calculates a spare correctly

--- spec/ScorerSpec.js
+++ spec/ScorerSpec.js
@@ -1,9 +1,32 @@
 describe('Scorer', function() {
 
-	var scorer;
+	var scorer
 
-	describe('start of a game', function() {
+	it('gives a score of 0 if no pins are hit', function() {
+		scorer = new Scorer();
+		for (var i = 0; i < 20; i++) {
+			scorer.roll(0)
+		};
+		expect(scorer.total()).toEqual(0);
+	});
 
+	it('gives a score of 20 if 1 pin is hit each roll', function() {
+		scorer = new Scorer();
+		for (var i = 0; i < 20; i++) {
+			scorer.roll(1)
+		};
+		expect(scorer.total()).toEqual(20);
+	});
+
+	it('gives a score of 12 if the second roll is a spare', function() {
+		scorer = new Scorer();
+		scorer.roll(4);
+		scorer.roll(6);
+		scorer.roll(1);
+		for (var i = 0; i < 17; i++) {
+			scorer.roll(0)
+		};
+		expect(scorer.total()).toEqual(12);
 	});
 
 });
--- src/Scorer.js
+++ src/Scorer.js
@@ -1,3 +1,19 @@
 var Scorer = function() {
+	this.rolls = [];
+};
 
+Scorer.prototype.roll = function(score) {
+	this.rolls.push(score)
+};
+
+Scorer.prototype.total = function() {
+	var numberOfRolls = this.rolls.length;
+	var runningTotal = 0;
+	for (var i = 0; i < numberOfRolls; i ++) {
+		if (this.rolls[i] + this.rolls[i+1] === 10) {
+			runningTotal = runningTotal + this.rolls[i + 2];
+		};
+		runningTotal = runningTotal + this.rolls[i];
+	};
+	return runningTotal;
 };

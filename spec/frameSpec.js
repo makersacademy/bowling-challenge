54d8a5865d78c00575724a491c06bcf65795ae19
Richard Lynch <rjlynch86@googlemail.com>
2015-04-24 17:10:26 +0100

    frame is over if 2 rolls

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,14 +2,48 @@ describe('Frame', function(){
 
   var frame;
 
-  it('returns the number of pins standing after 1 roll', function(){
-    frame = new Frame();
-    frame.roll(3);
-    expect(frame.pins).toEqual(7);
-    frame = new Frame();
-    frame.roll(6);
-    expect(frame.pins).toEqual(4);
+  describe('returns the number of pins standing', function(){
+    
+    it('after one roll', function(){
+      frame = new Frame();
+      frame.roll(3);
+      expect(frame.pins).toEqual(7);
+      frame = new Frame();
+      frame.roll(6);
+      expect(frame.pins).toEqual(4);
+    });
+
+    it('after two rolls', function(){
+      frame = new Frame();
+      frame.roll(4);
+      frame.roll(5);
+      expect(frame.pins).toEqual(1);
+      frame = new Frame();
+      frame.roll(3);
+      frame.roll(2);
+      expect(frame.pins).toEqual(5);
+    });
+
   });
+
+  describe('is over if there has been', function(){
+
+    it('two rolls', function(){
+      frame = new Frame();
+      expect(frame.is_over).toEqual(false);
+      frame.roll(3);
+      frame.roll(5);
+      expect(frame.is_over).toEqual(true);
+
+    });
+
+    it('a strike', function(){
+      
+    });
+
+  });
+
+
 
 
 });
--- src/frame.js
+++ src/frame.js
@@ -1,8 +1,17 @@
 var Frame = function(){
   this.pins = 10;
+  this.is_over = false;
+  this.rolls = [];
 };
 
 Frame.prototype.roll = function(pins_hit) {
   this.pins -= pins_hit
+  this.rolls.push(pins_hit);
+  this.check_if_over();
 };
 
+Frame.prototype.check_if_over = function() {
+  if (this.rolls.length == 2){
+    this.is_over = true;
+  }
+};

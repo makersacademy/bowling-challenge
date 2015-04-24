139b2eaef18419b2450ae5fb773c5827be670544
Phoebe Hugh <phoebehugh@gmail.com>
2015-04-24 18:24:11 +0100

    Starts again from server point of view

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -9,16 +9,4 @@ describe('Bowling Game', function() {
     expect(bowling.score).toEqual(0);
   });
 
-  it('has a score of 5 when the player his 5 pins', function() {
-    bowling.roll(5);
-    expect(bowling.score).toEqual(5);
-  });
-
-  it('has a score of 10 when the player hits 3 and 7', function() {
-    bowling.roll(3);
-    bowling.roll(7);
-    expect(bowling.score).toEqual(10);
-  });
-
-
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,13 +1,3 @@
 var Bowling = function () {
-  this.score = 0;
-  this.frameNumber = 1;
-};
 
-Bowling.prototype.roll = function(pins) {
-  this.score += pins;
-};
-
-Bowling.prototype.frameNumber = function(roll) {
-  this.frameNumber += 1;
-  // if 
 };

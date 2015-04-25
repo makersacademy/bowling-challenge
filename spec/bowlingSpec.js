5abafe252821c097df7f6bfbb19c05696a29201f
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-26 00:42:16 +0100

    records the score of a frame

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,4 +1,4 @@
-describe("Thermostat", function(){
+describe("Bowling Game", function(){
 
   beforeEach(function(){
     game = new Game();
@@ -7,5 +7,33 @@ describe("Thermostat", function(){
   it("will return the current frame", function(){
     expect(game.frame).toEqual(1);
   });
+
+  it("will return the current bowl", function(){
+    expect(game.ball).toEqual(1);
+  });
+
+  it("will record the knocked down pins of a ball", function(){
+    var knockedDown = 8
+    game.bowl(knockedDown)
+    expect(game.score).toEqual(knockedDown);
+  });
+
+  it("will record the knocked down pins of a frame", function(){
+    var knockedDownBall1 = 5
+    var knockedDownBall2 = 2
+    game.bowl(knockedDownBall1)
+    game.bowl(knockedDownBall2)
+    expect(game.score).toEqual(knockedDownBall1 + knockedDownBall2);
+  });
+
+  it("knows the next ball and frame to be played", function(){
+    var knockedDown = 2
+    game.bowl(knockedDown)
+    game.bowl(knockedDown)
+    expect(game.ball).toEqual(2);
+    expect(game.frame).toEqual(2);
+  });
+
+
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,9 @@
 var Game = function() {
-
+  this.frame = 1;
+  this.ball = 1;
+  this.score = 0;
 }
+
+Game.prototype.bowl = function(knockedDown) {
+  this.score += knockedDown
+};

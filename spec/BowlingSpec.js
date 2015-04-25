f51f2baaa2cb93d796032a0b55ab479aa5fa171d
icicleta <icicleta@gmail.com>
2015-04-25 13:04:20 +0100

    nicely refactored

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,62 +1,61 @@
-describe('Bowling', function(){
-
+describe('Bowling', function() {
   var bowling;
 
-  beforeEach(function(){
-    bowling = new Bowling;
-
+  beforeEach(function() {
+    bowling = new Bowling();
   });
 
-  describe('ball thrown', function(){
+  describe('number of frame', function() {
+    it('has 1 frame', function() {
+      expect(bowling.frameNumber).toBe(1);
+    });
+  });
 
-    describe('is one, ', function(){
-
-      it('pins knocked down are less or equal to 10', function(){
-        expect(bowling.pinsKnocked(11)).toBeLessThan(11);
+  describe('number of ball', function() {
+    describe('is One', function() {
+      it('then number of pins knocked down has to be less than 11', function() {
+        expect(bowling.pinsKnocked(10)).toBeLessThan(11);
       });
 
-      beforeEach(function(){
+      describe('and number of pins down is 10', function(){
+
+        it('then pins left are 0', function(){
+          spyOn(bowling, 'pinsKnocked').and.returnValue(10);
+          expect(bowling.ballOne()).toBe(0);
+        });
+
+        it('roll is Strike and frame is over', function(){
+          spyOn(bowling, 'ballOne').and.returnValue(0);
+          expect(bowling.gameFrame()).toEqual("Strike, frame is over");
+          expect(bowling.frameScore()).toEqual(10);
+        });
       });
 
-      it('and pins knocked down are 10, player makes Strike', function(){
-        spyOn(bowling, 'pinsKnocked').and.returnValue(10);
-        expect(bowling.rolling()).toEqual('Strike');
+      describe('and number of pins down is less than 10', function(){
+
+        it('the frames continues', function(){
+          spyOn(bowling, 'ballOne').and.returnValue(6);
+          expect(bowling.gameFrame()).toEqual("Frame continues");
+        });
+
+        it('then pins left are 10 minus pins knocked down', function(){
+          spyOn(bowling, 'pinsKnocked').and.returnValue(6);
+          expect(bowling.ballOne()).toBe(4);
+        });
       });
-
-      it('points in this frame are 10', function(){
-        spyOn(bowling, 'rolling').and.returnValue('Strike');
-        // bowling.rolling();
-        expect(bowling.frameScore()).toEqual(10);
-      });
-
-      it('Frame ends and next frame starts'),function(){
-        expect(bowling.bowFrame).toBe(2);
-      };
-
     });
 
-    describe('is two,', function(){
-      beforeEach(function(){
-        var pinsDown = spyOn(bowling, 'pinsKnocked').and.returnValue(5);
-        this.pins = bowling.pinsLeft - bowling.pinsKnocked();
+    describe('is 2', function() {
+      it('then number of pins knocked down has to be less than pins left in ball 1', function() {
+        spyOn(bowling, 'ballOne').and.returnValue(6);
+        pinsLeft = 10 - bowling.ballOne();
+        expect(bowling.pinsKnocked(pinsLeft)).toBeLessThan(5);
       });
 
-      // afterEach(function(){
-      //   bowling.pinsKnocked(this.pins).reset();
-      // });
-
-      it('pins knocked down are less or equal than pins left on first ball', function(){
-        expect(bowling.pinsKnocked(this.pins)).toBeLessThan(6);
-      });
-
-      it('and sum of pins down in ball 1 plus ball 2, makes 10', function(){
-        spyOn(bowling, 'pinsKnocked').and.returnValue(5)
-        expect(bowling.rolling()).toEqual('Spare');
-      });
-
-      it('frame stops and player pass to next frame', function(){
-        bowling.rolling();
-        expect(bowling.bowFrame).toBe(2);
+      it('and number of pins knocked down plus number of pins knocked by ball 1, is 10', function() {
+        spyOn(bowling, 'ballOne').and.returnValue(6);
+        spyOn(bowling, 'ballTwo').and.returnValue(0);
+        expect(bowling.gameFrame()).toEqual("Spare, frame is over");
       });
     });
   });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,69 +1,47 @@
-function Bowling(){
-  this.rollScore = 0;
-  this.score = 0;
-  this.bowFrame = 1;
-  // this.ball1Gets = 0;
-  // this.ball2Gets = 0;
-  this.ball = 0;
-  this.pinsLeft = 10;
-  this.alert = "Start Game";
+function Bowling() {
+  this.frameNumber = 1;
+  this.startPins = 10;
+}
+
+Bowling.prototype.gameFrame = function() {
+  if (this.ballOne() === 0){
+    this.message = "Strike, frame is over";
+  } else if (this.ballTwo() === 0){
+    this.message = "Spare, frame is over";
+  } else {
+    this.message ="Frame continues";
+  };
+  return this.message;
+};
+
+Bowling.prototype.ballOne = function() {
+  var pinsDown = this.pinsKnocked(this.startPins);
+  if (pinsDown === 10){
+  this.pinsLeft = 0;
+  } else{
+    this.pinsLeft = this.startPins - pinsDown;
+  };
+  return this.pinsLeft;
+};
+
+Bowling.prototype.ballTwo = function() {
+  var pinsDown = this.pinsKnocked(this.pinsLeft);
+  var totalPinsDown = this.pinsLeft + pinsDown;
+  console.log(totalPinsDown);
+  if (totalPinsDown === 10){
+     this.pinsLeft = 0;
+  };
+  return this.pinsLeft;
+  // this.ballTwo();
+};
+
+Bowling.prototype.frameScore = function() {
+  if (this.message === "Strike, frame is over") {
+    return 10;
+  };
 };
 
 Bowling.prototype.pinsKnocked = function(pins) {
   range = pins + 1;
   return Math.floor(Math.random() * range);
 };
-
-Bowling.prototype.rolling = function() {
-  this.ball ++;
-  var pins = this.pinsLeft;
-  this.ball1Gets = this.pinsKnocked(pins);
-  if (this.ball1Gets === 10){
-    this.bowFrame ++
-    this.alert = "Strike";
-    return this.alert;
-  };
-  if (this.ball1Gets < 10){
-    this.ball ++;
-    this.pinsLeft -= this.ball1Gets;
-    var pins = this.pinsLeft;
-    this.ball2Gets = this.pinsKnocked(pins);
-    this.bowFrame ++;
-    this.ballsGet = this.ball1Gets + this.ball2Gets;
-    if (this.ballsGet === 10){
-      this.alert = "Spare";
-    }
-    else{
-      this.alert = "Open Frame";
-    };
-    this.bowFrame ++;
-  };
-  this.ball = 0;
-  return this.alert;
-};
-
-Bowling.prototype.frameScore = function(alert, pinsDown) {
-  alert = this.alert;
-  pinsDown = this.ballsGet;
-  this.rollScore = pinsDown;
-  if (alert === "Strike" || alert === "Spare") {
-    this.rollScore = 10;
-    return this.rollScore;
-  };
-  if (alert === "Open Frame") {
-    this.rollScore = pinsDown;
-    return this.rollScore;
-  };
-};
-
-bowling = new Bowling;
-
-console.log(bowling);
-console.log(bowling.rolling());
-// console.log(bowling.totalScore());
-// console.log(bowling.rolling());
-// console.log(bowling.totalScore());
-// console.log(bowling.rolling());
-// console.log(bowling.totalScore());
-// console.log(bowling.rolling());
-console.log(bowling.frameScore());

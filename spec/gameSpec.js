20c895d3d8396ca642541046cce3b289238b27a1
Stacca <caronstace@gmail.com>
2015-04-26 19:04:11 +0100

    spare attempt

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,7 +9,7 @@ describe('Game', function () {
     	expect(game.frame_total).toEqual(10);
   	});
 
-  it('can all gutter balls', function (){
+  it('can roll all gutter balls', function (){
     var game = new Game();
     for (var i = 0; i < 20; i++) {
       game.roll(0);
@@ -25,4 +25,10 @@ describe('Game', function () {
   	expect(game.score()).toBe(20);
   });
 
-	});
+  // it('can roll a spare', function(){
+  //  game.roll(3);
+  //  game.roll(7);
+  //  game.roll(2);
+  //  expect(frame.rolls[0]).toEqual(12);
+  // });
+});

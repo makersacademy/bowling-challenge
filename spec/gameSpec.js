1d6a38bed71fcc9b5deecf7f793e811834a8326c
Stacca <caronstace@gmail.com>
2015-04-30 22:27:57 +0100

    added failing spare

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,26 +9,27 @@ describe('Game', function () {
     	expect(game.frame_total).toEqual(10);
   	});
 
-  it('can roll all gutter balls', function (){
-    var game = new Game();
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
+    it('can roll all gutter balls', function (){
+      var game = new Game();
+      for (var i = 0; i < 20; i++) {
+        game.roll(0);
     }
-    expect(game.score()).toBe(0);
-  });
+      expect(game.score()).toBe(0);
+    });
 
-  it('can roll all ones in all goes', function () {
-  	var game = new Game();
-  	for (var i = 0; i < 20; i++) {
-  		game.roll(1);
-  	}
-  	expect(game.score()).toBe(20);
-  });
+    it('can roll all ones in all goes', function () {
+  	 var game = new Game();
+  	 for (var i = 0; i < 20; i++) {
+  		  game.roll(1);
+  	 }
+  	 expect(game.score()).toBe(20);
+    });
 
-  // it('can roll a spare', function(){
-  //  game.roll(3);
-  //  game.roll(7);
-  //  game.roll(2);
-  //  expect(frame.rolls[0]).toEqual(12);
-  // });
-});
+    it('can check for a spare', function(){
+      var game = new Game();
+      game.roll(7);
+      game.roll(3);
+        expect(game.rolls[2]).toEqual(10);
+        expect(game.isSpare()).toBe(true)
+    });
+  });

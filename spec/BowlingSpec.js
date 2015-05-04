088c39082114672c577006faac7a44d7ce0790d9
Mark Pokorny <iridium193@gmail.com>
2015-05-04 11:44:01 +0100

    Added checks to ensure score can increase, but not beyond the starting number of skittles.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -10,12 +10,28 @@ describe('Bowling Scorecard', function() {
       it('is initally 0', function() {
         expect(frame.frameScore).toEqual(0);
       });
+
+      it('(potentially) increases as balls are bowled', function() {
+        frame.bowl();
+        expect(frame.frameScore >= 0).toBeTruthy();
+      });
+
+      it('does not exceed the initial number of skittles', function() {
+        numberOfSkittles = frame.skittlesRemaining;
+        frame.bowl();
+        expect(frame.frameScore <= numberOfSkittles).toBeTruthy();
+      });
     });
 
     describe('keeps track of the number of skittles remaining, which:', function() {
       it('is initally 10', function() {
         expect(frame.skittlesRemaining).toEqual(10);
       });
+    });
+
+    it('can bowl a ball and knock down some/all skittles (but no more than that!)', function() {
+      frame.bowl();
+      expect(frame.skittlesRemaining).toBeGreaterThan(-1);
     });
   });
 });

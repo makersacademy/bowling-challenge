b5f4a0ba8737ef5df08c0fd90a59bc4b7a374f3e
smarbaf <smarbaf@hotmail.com>
2015-05-29 17:10:35 +0100

    Limit Game to 20 Rolls

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,43 +2,27 @@ describe("Bowling Score Card", function(){
 
   xit("should record a score of 300 for a perfect game", function() {
     game = new Game();
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
-    game.roll(10);
+    for (i = 0; i < 12; i++) {
+        game.roll(10);
+    };
     expect(game.score).toEqual(300);
   });
 
   it("should record a score of zero for a gutter game", function() {
     game = new Game();
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
-    game.roll(0);
+    for (i = 0; i < 20; i++) {
+        game.roll(0);
+    };
     expect(game.score).toEqual(0);
   });
+
+it("limits the game to 20 rolls", function() {
+    game = new Game();
+    for (i = 0; i < 20; i++) {
+        game.roll(4);
+    };
+    expect(function(){game.roll(4)}).toThrow();
+  });
+
 });
+
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,12 @@
 function Game(){
   this.score = 0;
+  this.rollCount = 0;
 };
 
 Game.prototype.roll = function(pinsDown) {
   this.score += pinsDown;
+  if (this.rollCount >= 20)  {
+    throw "Game Over";
+  }
+  this.rollCount += 1;
 };

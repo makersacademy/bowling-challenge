7324806fd7dde4d7c6534bcf1fcc7b01962c24ba
Alex Handy <handy437@gmail.com>
2015-05-29 13:07:08 +0100

    Can score basic game and store rolls by frame in array

--- public/js/spec/ScorecardSpec.js
+++ public/js/spec/ScorecardSpec.js
@@ -9,4 +9,28 @@ describe('Scorecard', function(){
     expect(scorecard.totalScore).toEqual(0);
   });
 
+
+  it('has an array of individual scores when initialized', function() {
+    expect(scorecard.rolls).toEqual([]);
+  });
+
+  it('can store individual rolls in an array', function (){
+    scorecard.oneFrame(2,3);
+    expect(scorecard.rolls).toEqual([[2,3]]);
+  });
+
+  it('can track all 10 frames of 20 rolls with no spares or half strikes', function(){
+      for (var i = 0; i < 10; i++){
+        scorecard.oneFrame(2,3);
+      }
+      expect(scorecard.totalScore).toEqual(50);
+  });
+
+  it('can calculate the score of a game with no spares or half strikes', function(){
+      for (var i = 0; i < 10; i++){
+        scorecard.oneFrame(2,3);
+      }
+      expect(scorecard.rolls).toEqual([[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3],[2,3]])
+  });
+
 });
--- public/js/src/Scorecard.js
+++ public/js/src/Scorecard.js
@@ -1,5 +1,11 @@
 function Scorecard(){
   this.totalScore = 0;
+  this.rolls = [];
+};
+
+Scorecard.prototype.oneFrame = function(roll1, roll2) {
+  this.totalScore += roll1 + roll2;
+  (this.rolls).push([roll1, roll2]);
 };
 
 

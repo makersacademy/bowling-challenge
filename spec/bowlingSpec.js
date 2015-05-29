7a9584475d847367f4eaca4214feeea928f8cf95
Daryl Watson <darylwtsn@gmail.com>
2015-05-29 11:25:24 +0100

    More error handling for first frame

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -8,4 +8,32 @@ describe('In the first frame, player bowls twice', function() {
     player.calculate();
     expect(player.score).toEqual(5);
   });
+
+  it('and scores a 7 and a 1', function (){
+    player.firstBowl(7);
+    player.secondBowl(1);
+    player.calculate();
+    expect(player.score).toEqual(8);
+  });
+
+  it('and scores a spare', function () {
+    player.firstBowl(9);
+    player.secondBowl(1);
+    player.calculate();
+    expect(player.score).toEqual(10);
+  })
+
+  it('but cannot hit more than 10 pins in one bowl', function() {
+    expect( function(){ player.firstBowl(11); } ).toThrow(new Error("Cannot hit more than 10 pins"));
+  });
+
+  it('but cannot hit more than 10 pins over two bowls', function() {
+    player.firstBowl(3);
+    expect( function() { player.secondBowl(8); } ).toThrow(new Error("Cannot hit more than 10 pins"));
+  });
+
+  it('but cannot bowl twice if a strike is scored', function() {
+    player.firstBowl(10);
+    expect( function() { player.secondBowl(1); } ).toThrow(new Error("Cannot bowl twice on a strike"));
+  });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -5,11 +5,24 @@ function Player() {
 };
 
 Player.prototype.firstBowl = function(pins) {
-  this.firstHit = pins;
+  if(pins > 10) {
+    throw new Error('Cannot hit more than 10 pins');
+  }
+  else {
+    this.firstHit = pins;
+  }
 };
 
 Player.prototype.secondBowl = function(pins) {
+  if(this.firstHit == 10) {
+    throw new Error('Cannot bowl twice on a strike');
+  }
+  else if(pins + this.firstHit > 10) {
+    throw new Error('Cannot hit more than 10 pins');
+  }
+  else {
   this.secondHit = pins;
+  }
 };
 
 Player.prototype.calculate = function() {

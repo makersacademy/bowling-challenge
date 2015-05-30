e4f563bdfb691b2e990ed5cbf1e9586aa019ab86
Rocco <roccorighi@hotmail.com>
2015-05-30 20:06:30 +0100

    adds logic.not working

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -5,23 +5,40 @@ describe('Player', function(){
   it('can play a frame and see his points', function() {
     player.bowl1(7);
     player.bowl2(2);
-    expect(player.points).toEqual(9);
+    expect(player.scorecard[0]).toEqual(9);
   });
   it('can bowl a strike', function(){
     player.bowl1(10);
-    expect(player.points).toEqual(0);
+    expect(player.scorecard[0]).toEqual(0);
     expect(player.bonus).toEqual(10);
   })
   it('can bowl a spare', function(){
     player.bowl1(2);
     player.bowl2(8);
-    expect(player.points).toEqual(2);
-    expect(player.bonus).toEqual(8);
+    expect(player.scorecard[0]).toEqual(0);
+    expect(player.bonus).toEqual(10);
   });
 
   it('cannot bowl twice after a strike', function(){
     player.bowl1(10);
     expect(function(){player.bowl2(1);}).toThrow('Strike, wait for next frame!');
   });
-  it('can bowl a strike and a spare', function)
+  it('can bowl a strike and a spare', function(){
+    player.bowl1(10);
+    player.bowl1(8);
+    player.bowl2(2);
+    expect(player.scorecard[0]).toEqual(20);
+  });
+  it('can bowl 4 strikes in a row', function(){
+    for (var i = 0; i < 4; i++) {
+      player.bowl1(10);
+    };
+    expect(player.scorecard[1]).toEqual(60);
+  });
+   it('can bowl 6 strikes in a row', function(){
+    for (var i = 0; i < 6; i++) {
+      player.bowl1(10);
+    };
+    expect(player.scorecard[3]).toEqual(120);
+  });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,24 +1,58 @@
 function Player (){
+  this.scorecard = [];
   this.points = 0;
   this.bonus = 0;
   this.firstbowl = 0;
+  this.secondbowl = 0;
+  this.frames = 0;
+  this.strike = false;
+  this.spare = false;
 };
 
 Player.prototype.bowl1 = function(pins) {
+  this.firstbowl = pins;
+  this.frames ++;
   if (pins == 10) {
-    this.bonus =+ pins;
-    this.firstbowl = pins;
-  } else {
-    this.points += pins;
-    this.firstbowl = pins;
+    this.strike = true;
+    this.bonus += pins;
   };
+  this.tracker();
 };
 
 Player.prototype.bowl2 = function(pins) {
+  this.secondbowl = pins;
   if(this.firstbowl == 10) throw 'Strike, wait for next frame!'
   if(this.firstbowl + pins == 10) {
-    this.bonus = pins;
+    this.spare = true;
+    this.bonus = pins + this.firstbowl;
+  }
+  this.tracker();
+};
+
+Player.prototype.tracker = function() {
+  var position = (this.frames -1);
+  var total = (this.firstbowl + this.secondbowl + this.bonus);
+
+  if (this.strike) {
+    this.strike = false;
+    this.bonus += this.firstbowl + this.secondbowl;
+    if ((this.frames == 1) || (this.frames == 2)) {
+      this.scorecard[position] = 0;
+
+    } else {
+        this.scorecard[this.frames - 3] = this.bonus;
+      };
+
+  } else if (this.spare) {
+    this.spare = false;
+    if (this.frames == 1) {
+      this.bonus =+ this.firstbowl + this.secondbowl;
+      this.scorecard[position] = 0;
+    } else {
+      this.bonus =+ this.firstbowl + this.secondbowl;
+      this.scorecard[position] = this.bonus;
+    };
   } else {
-    this.points += pins
+    this.scorecard[position] = total;
   };
 };

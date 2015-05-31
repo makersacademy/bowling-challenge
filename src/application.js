010dd9ec8c1eb212343851c111d7af5bd0a8166d
Daryl Watson <darylwtsn@gmail.com>
2015-05-31 19:41:05 +0100

    Made it better

--- src/application.js
+++ src/application.js
@@ -1,18 +1,42 @@
 player = new Player;
-var frames = 1;
 
 $(document).ready( function () {
+  var x = 0;
+  var frames = 0;
   $('#button-group1 input').click(function () {
     var pins1 = $("form input[type=radio]:checked").val();
-    $("input:radio").removeAttr("checked");
     player.firstBowl(pins1);
-    $('#hits').html(pins1);
+    if(pins1 == 10) {
+      $('#hits' + x).val(pins1);
+      $('#hits' + (x + 1)).val("X");
+      var sum = 0;
+      for (i = 0; i < player.score.length; i++) {
+        $("#score" + i).val(player.score[i]);
+        sum += player.score[i];
+      }
+    x += 2;
+    $("#total").val(sum);
+    frames = player.score.length + 1;
+    $('#frames').html('Frame ' + frames);
+    }
+    else {
+      $('#hits' + x).val(pins1);
+      x ++;
+    }
+    $("input:radio").removeAttr("checked");
   });
     $('#button-group2 input').click(function () {
     var pins2 = $("form input[type=radio]:checked").val();
     player.secondBowl(pins2);
-    $('#hits').html(pins2);
-    $('#score1').html(player.score.toString());
+    $('#hits' + x).val(pins2);
+    $("input:radio").removeAttr("checked");
+    x ++;
+    var sum = 0;
+    for (i = 0; i < player.score.length; i++) {
+      $("#score" + i).val(player.score[i]);
+      sum += player.score[i];
+    }
+    $("#total").val(sum);
     frames = player.score.length + 1;
     $('#frames').html('Frame ' + frames);
   });

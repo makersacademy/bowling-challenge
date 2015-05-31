dc468674c4e998cd0644125cd22c6e84368e9e83
Anna <anna.klimas@gmail.com>
2015-05-31 14:08:47 +0100

    Board can record points for strike.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,10 +5,15 @@ beforeEach(function() {
     game = new Game();
 });
 
-describe('board can record',function(){
+describe('board can record points for',function(){
    it('a roll', function(){
     game.roll(1, 2);
     expect(game.frameScore()).toEqual(3);
+   });
+
+   it('a strike', function(){
+    game.strike();
+    expect(game.frameScore()).toEqual(10);
    });
 
     });
--- src/game.js
+++ src/game.js
@@ -1,13 +1,20 @@
 function Game(){
-  this.framePoints = 0;
+  this.rollPoints = 0;
+  this.strikePoints = 0;
+  this.stikePresent = false;
 };
 
 Game.prototype.roll = function(bowl1, bowl2) {
-  this.framePoints =  bowl1 +  bowl2;
+  this.rollPoints =  bowl1 +  bowl2;
 
   };
 
-  Game.prototype.frameScore = function() {
-    if(this.framePoints <10) return this.framePoints;
+  Game.prototype.strike = function() {
+    this.strikePoints = 10;
+    this.strikePresent = true;
+  };
 
+  Game.prototype.frameScore = function() {
+    if(this.rollPoints > 0) return this.rollPoints;
+    if(this.strikePresent == true) return this.strikePoints;
   };

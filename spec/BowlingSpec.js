2649a813bfc53cd3ad93e0aa2bf87d9c780f1ab1
andygout <andygout@hotmail.co.uk>
2015-05-31 01:03:20 +0100

    Working tests for 1 gutterball + 2 gutterballs

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -4,9 +4,20 @@ describe('Bowling', function () {
     game = new Game();
   });
 
-  describe('has a default', function() {
-    it('game scoresheet', function() {
-      expect(game.scoresheet).toContain('scoresheet')
+  describe('Play a game and', function() {
+    it('roll a gutterball', function() {
+      game.rollResult(0);
+      expect(game.scoresheet['Frame 1']['Roll 1']).toBe(0);
+    });
+
+    it('roll two gutterballs', function() {
+      var testResults = [0, 0];
+      var arrLength = testResults.length;
+      for(var i = 0; i < arrLength; i++) {
+        game.rollResult(testResults[i]);
+      };
+      expect(game.scoresheet['Frame 1']['Roll 1']).toBe(0);
+      expect(game.scoresheet['Frame 1']['Roll 2']).toBe(0);
     });
   });
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,3 +1,16 @@
 function Game () {
-  this.scoresheet = 'scoresheet';
+  this.scoresheet = [];
+  this.frameCount = 1;
+  this.rollCount = 1;
+};
+
+Game.prototype.rollResult = function(pinsKnocked) {
+  if(this.rollCount === 1) {
+    this.scoresheet['Frame ' + this.frameCount] = []
+    this.scoresheet['Frame ' + this.frameCount]['Roll ' + this.rollCount] = pinsKnocked;
+    this.rollCount = 2;
+  } else if(this.rollCount === 2) {
+    this.scoresheet['Frame ' + this.frameCount]['Roll ' + this.rollCount] = pinsKnocked;
+    this.rollCount = 1;
+  };
 };

9c47a5ed01278438dfc5254f7585fcbca9ff4760
Daryl Watson <darylwtsn@gmail.com>
2015-05-31 14:35:33 +0100

    Using buttons

--- src/application.js
+++ src/application.js
@@ -1,11 +1,18 @@
 player = new Player;
 
 $(document).ready( function () {
-  $('#submit').click(function () {
-    player.firstBowl($('#bowl1').val());
-    // var bowl1 = $('#bowl1').val();
-    player.secondBowl($('#bowl2').val());
-    // var bowl2 = $('#bowl2').val();
-    $('#score1').html(player.score);
+  $('#button-group input').click(function () {
+    var pins = $("form input[type=radio]:checked").val()
+    $("input:radio").removeAttr("checked");
+    // player.firstBowl(pins);
+
+
+
+    // player.secondBowl($('#bowl2').val());
+    $('#score1').html(pins);
+    // $('#score1').after().html('<input type="text" id="bowl1of2" placeholder="Did it!">');
+    // $('#score1').after().html('<input type="text" placeholder="And again!">');
   });
+
 });
+

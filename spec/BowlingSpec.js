cbcbc2eaeba2c4b798f692979dafaf495e74b0bd
andygout <andygout@hotmail.co.uk>
2015-05-31 09:59:02 +0100

    Added: Tests for additional frames

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -19,6 +19,25 @@ describe('Bowling', function () {
       expect(game.scoresheet['Frame 1']['Roll 1']).toBe(0);
       expect(game.scoresheet['Frame 1']['Roll 2']).toBe(0);
     });
+
+    it('move onto next frame after rolling two gutterballs', function() {
+      var testResults = [0,0];
+      var arrLength = testResults.length;
+      for(var i = 0; i < arrLength; i++) {
+        game.rollResult(testResults[i]);
+      };
+      expect(game.frameCount).toBe(2);
+    });
+
+    it('roll only gutterballs and receive final score of 0', function() {
+      var testResults = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+      var arrLength = testResults.length;
+      for(var i = 0; i < arrLength; i++) {
+        game.rollResult(testResults[i]);
+      };
+      expect(game.scoresheet['Frame 10']['Roll 2']).toBe(0);
+      expect(game.score).toBe(0);
+    });
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,15 +2,29 @@ function Game () {
   this.scoresheet = [];
   this.frameCount = 1;
   this.rollCount = 1;
+  this.score = 0;
 };
 
 Game.prototype.rollResult = function(pinsKnocked) {
-  if(this.rollCount === 1) {
-    this.scoresheet['Frame ' + this.frameCount] = []
-    this.scoresheet['Frame ' + this.frameCount]['Roll ' + this.rollCount] = pinsKnocked;
-    this.rollCount = 2;
-  } else if(this.rollCount === 2) {
-    this.scoresheet['Frame ' + this.frameCount]['Roll ' + this.rollCount] = pinsKnocked;
-    this.rollCount = 1;
+  if(this.frameCount <= 10) {
+    if(this.rollCount === 1) {
+      this.logRoll1(pinsKnocked);
+    } else if(this.rollCount === 2) {
+      this.logRoll2(pinsKnocked);
+    };
   };
+};
+
+Game.prototype.logRoll1 = function(pinsKnocked) {
+  this.scoresheet['Frame ' + this.frameCount] = []
+  this.scoresheet['Frame ' + this.frameCount]['Roll ' + this.rollCount] = pinsKnocked;
+  this.score += pinsKnocked;
+  this.rollCount = 2;
+};
+
+Game.prototype.logRoll2 = function(pinsKnocked) {
+  this.scoresheet['Frame ' + this.frameCount]['Roll ' + this.rollCount] = pinsKnocked;
+  this.score += pinsKnocked;
+  this.rollCount = 1;
+  this.frameCount += 1;
 };

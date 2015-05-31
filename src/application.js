47dbc21d900387e2de21c1128fe71e32be15c97b
Daryl Watson <darylwtsn@gmail.com>
2015-05-31 20:44:06 +0100

    Working except for tenth frame

--- src/application.js
+++ src/application.js
@@ -1,44 +1,38 @@
 player = new Player;
 
+function calculateTotalPoints() {
+  var sum = 0;
+  for (i = 0; i < player.score.length; i++) {
+    $("#score" + i).val(player.score[i]);
+    sum += player.score[i];
+  }
+  $("#total").val(sum);
+  frames = player.score.length + 1;
+  $('#frames').html('Frame ' + frames);
+}
+
 $(document).ready( function () {
   var x = 0;
   var frames = 0;
   $('#button-group1 input').click(function () {
     var pins1 = $("form input[type=radio]:checked").val();
     player.firstBowl(pins1);
+    $('#hits' + x).val(pins1);
     if(pins1 == 10) {
-      $('#hits' + x).val(pins1);
-      $('#hits' + (x + 1)).val("X");
-      var sum = 0;
-      for (i = 0; i < player.score.length; i++) {
-        $("#score" + i).val(player.score[i]);
-        sum += player.score[i];
-      }
-    x += 2;
-    $("#total").val(sum);
-    frames = player.score.length + 1;
-    $('#frames').html('Frame ' + frames);
+      x ++ ;
+      $('#hits' + x).val("X");
     }
-    else {
-      $('#hits' + x).val(pins1);
-      x ++;
-    }
+    x ++;
+    calculateTotalPoints();
     $("input:radio").removeAttr("checked");
   });
-    $('#button-group2 input').click(function () {
+  $('#button-group2 input').click(function () {
     var pins2 = $("form input[type=radio]:checked").val();
     player.secondBowl(pins2);
     $('#hits' + x).val(pins2);
     $("input:radio").removeAttr("checked");
     x ++;
-    var sum = 0;
-    for (i = 0; i < player.score.length; i++) {
-      $("#score" + i).val(player.score[i]);
-      sum += player.score[i];
-    }
-    $("#total").val(sum);
-    frames = player.score.length + 1;
-    $('#frames').html('Frame ' + frames);
+    calculateTotalPoints();
   });
 
 });

89cfa65f90ed1e24045436290927fe14d926dea2
Stefan Lissia <stefan.emailacct@gmail.com>
2015-05-31 18:15:45 +0100

    first 4 tests - first frame

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -5,4 +5,52 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
+
+  describe('in the first frame...', function() {
+
+      it('in the first frame can score a 5 and a 2', function() {
+        bowling.firstBowl(5);
+        bowling.secondBowl(2);
+        expect(bowling.totalScore).toEqual(7);
+      });
+
+      it('in the first frame can score a 8 and a 1', function() {
+        bowling.firstBowl(8);
+        bowling.secondBowl(1);
+        expect(bowling.totalScore).toEqual(9);
+      });
+
+      it('in the first frame can score a spare', function() {
+        bowling.firstBowl(9);
+        bowling.secondBowl(1);
+        expect(bowling.totalScore).toEqual(10);
+      });
+
+      it('in the first frame can score a strike', function() {
+        bowling.firstBowl(10);
+        expect(bowling.totalScore).toEqual(10);
+      });
+
+  }); //in the first frame
+
+
+  describe('in the second frame...', function() {
+
+
+
+
+
+
+
+  }); //in the second frame
+
+
+
+
+
+
+
+
+
+
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,3 +1,28 @@
 var Bowling = function() {
-  this.score = 0;
+  this.score1 = 0;
+  this.score2 = 0;
+  this.totalScore = 0;
+
+
+};
+
+Bowling.prototype.firstBowl = function(pins) {
+  this.score1 += pins;
+  console.log(this.score1);
+  this.score2 = 0;
+  this.calculateScore();
+
+
+};
+
+Bowling.prototype.secondBowl = function(pins) {
+  this.score2 += pins;
+  this.calculateScore();
+
+};
+
+
+Bowling.prototype.calculateScore = function(num1,num2) {
+  this.totalScore = (this.score1 + this.score2);
+
 };

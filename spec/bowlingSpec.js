85730a95e35d366aa9957e38570bb1bdf98e4307
Anna <anna.klimas@gmail.com>
2015-05-31 18:59:23 +0100

    Board can record total frame points for roll, spare and strike

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -21,6 +21,16 @@ describe('board can record points for',function(){
     expect(game.sparePoints).toEqual(10);
    });
  });
+
+
+ describe('board will NOT record points for',function(){
+   it('a roll > 10', function(){
+    game.roll(5, 6);
+    expect(game.rollPoints).toEqual(0);
+   });
+ });
+
+
  describe('board can record bonus points for', function(){
   it('a roll equal 10', function(){
     game.roll(1, 9);
@@ -37,7 +47,34 @@ describe('board can record points for',function(){
     game.spareBonus(2);
     expect(game.frameBonus).toEqual(2);
   });
+});
 
- });
+
+ describe('board will NOT record bonus points for', function(){
+    it('a roll equal to less than 10', function(){
+      game.roll(1, 2);
+      game.rollBonus(2);
+      expect(game.frameBonus).toEqual(0);
+    });
+  });
+
+
+ describe('board can record total frame points for', function(){
+   it('a roll', function(){
+    game.roll(5, 5);
+    game.rollBonus(5);
+    expect(game.frameScore()).toEqual(15);
+   });
+ it('a strike', function(){
+    game.strike();
+    game.strikeBonus(10, 10);
+    expect(game.frameScore()).toEqual(30);
+   });
+ it('a spare', function(){
+    game.spare();
+    game.spareBonus(10);
+    expect(game.frameScore()).toEqual(20);
+   });
+  });
 });
 
--- src/game.js
+++ src/game.js
@@ -7,9 +7,9 @@ function Game(){
   this.frameBonus = 0
 };
 
-Game.prototype.roll = function(bowl1, bowl2) {
-  this.rollPoints =  bowl1 +  bowl2;
-
+  Game.prototype.roll = function(bowl1, bowl2) {
+  if(bowl1 + bowl2 > 10) return "There is only 10 pins available for one roll";
+  if(bowl1 + bowl2 <= 10) return this.rollPoints =  bowl1 +  bowl2;
   };
 
   Game.prototype.strike = function() {
@@ -32,4 +32,11 @@ Game.prototype.roll = function(bowl1, bowl2) {
 
   Game.prototype.spareBonus = function(bowl1) {
     if(this.sparePresent == true) return this.frameBonus = bowl1;
+  };
+
+  Game.prototype.frameScore = function() {
+    if(this.rollPoints == 10) return (this.rollPoints + this.frameBonus);
+    if(this.strikePresent == true) return (this.strikePoints + this.frameBonus);
+    if(this.sparePresent == true) return (this.sparePoints + this.frameBonus);
+    if(this.rollPoints > 0) return this.rollPoints;
   };

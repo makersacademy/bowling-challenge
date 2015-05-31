37740b0f4e74a680d75d7cdaca9d5a3dc7b5b4f3
andygout <andygout@hotmail.co.uk>
2015-05-31 13:25:59 +0100

    Added: Strike bonus logic (for 2 consecutive strikes)

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,7 +7,7 @@ describe('Bowling', function () {
   describe('Play a game and', function() {
     it('roll a gutterball', function() {
       game.rollResult(0);
-      expect(game.scoresheet['Frame 1']['Roll 1']).toBe(0);
+      expect(game.scoresheet[1][1]).toBe(0);
     });
 
     it('roll two gutterballs', function() {
@@ -16,8 +16,8 @@ describe('Bowling', function () {
       for(var i = 0; i < arrLength; i++) {
         game.rollResult(testResults[i]);
       };
-      expect(game.scoresheet['Frame 1']['Roll 1']).toBe(0);
-      expect(game.scoresheet['Frame 1']['Roll 2']).toBe(0);
+      expect(game.scoresheet[1][1]).toBe(0);
+      expect(game.scoresheet[1][2]).toBe(0);
     });
 
     it('move onto next frame after rolling two gutterballs', function() {
@@ -38,23 +38,36 @@ describe('Bowling', function () {
       expect(game.score).toBe(0);
     });
 
-    it('roll a strike in one frame, then a 2 then 3, and receive bonus of 5 for former frame', function() {
+    it('roll a strike, then 2 & 3, and receive bonus of 5 for first frame (total: 20)', function() {
       var testResults = [10,2,3];
       var arrLength = testResults.length;
       for(var i = 0; i < arrLength; i++) {
         game.rollResult(testResults[i]);
       };
-      expect(game.scoresheet['Frame 1']['Bonus']).toBe(5);
+      expect(game.scoresheet[1]['Bonus']).toBe(5);
+      expect(game.score).toBe(20);
     });
 
-    it('roll a spare in one frame, then a 2 then 3, and receive bonus of 2 for former frame', function() {
+    it('roll a spare, then 2 & 3, and receive bonus of 2 for first frame (total: 17)', function() {
       var testResults = [5,5,2,3];
       var arrLength = testResults.length;
       for(var i = 0; i < arrLength; i++) {
         game.rollResult(testResults[i]);
       };
-      expect(game.scoresheet['Frame 1']['Bonus']).toBe(2);
+      expect(game.scoresheet[1]['Bonus']).toBe(2);
+      expect(game.score).toBe(17);
     });
+
+    it('roll a strike, then two more, and receive bonus of 20 for initial frame (total: 50)', function() {
+      var testResults = [10,10,10];
+      var arrLength = testResults.length;
+      for(var i = 0; i < arrLength; i++) {
+        game.rollResult(testResults[i]);
+      };
+      expect(game.scoresheet[1]['Bonus']).toBe(20);
+      expect(game.score).toBe(50);
+    });
+
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -9,27 +9,30 @@ Game.prototype.rollResult = function(pinsKnocked) {
   if(this.frameCount <= 10) {
     if(this.rollCount === 1) {
       this.logRoll1(pinsKnocked);
-    } else if(this.rollCount === 2) {
+    }
+    else if(this.rollCount === 2) {
       this.logRoll2(pinsKnocked);
     };
   };
 };
 
 Game.prototype.logRoll1 = function(pinsKnocked) {
-  this.scoresheet['Frame ' + this.frameCount] = [];
-  this.scoresheet['Frame ' + this.frameCount]['Bonus'] = null;
+  this.scoresheet[this.frameCount] = [];
+  this.scoresheet[this.frameCount]['Bonus'] = null;
   this.score += pinsKnocked;
-  this.scoresheet['Frame ' + this.frameCount]['Roll 1'] = pinsKnocked;
+  this.scoresheet[this.frameCount][this.rollCount] = pinsKnocked;
   this.spareBonus();
   if(pinsKnocked < 10) {
     this.rollCount = 2;
-  } else {
-    this.logRoll2(null)
+  }
+  else {
+    this.rollCount = 2;
+    this.logRoll2(null);
   }
 };
 
 Game.prototype.logRoll2 = function(pinsKnocked) {
-  this.scoresheet['Frame ' + this.frameCount]['Roll 2'] = pinsKnocked;
+  this.scoresheet[this.frameCount][this.rollCount] = pinsKnocked;
   this.score += pinsKnocked;
   this.strikeBonus();
   this.rollCount = 1;
@@ -37,21 +40,24 @@ Game.prototype.logRoll2 = function(pinsKnocked) {
 };
 
 Game.prototype.spareBonus = function() {
-  if(this.frameCount > 1) {
-    if((this.scoresheet['Frame ' + (this.frameCount - 1)]['Roll 1'] + this.scoresheet['Frame ' + (this.frameCount - 1)]['Roll 2'] === 10) && (this.scoresheet['Frame ' + (this.frameCount - 1)]['Roll 1'] < 10)) {
-      var bonus = this.scoresheet['Frame ' + this.frameCount]['Roll 1'];
-      this.scoresheet['Frame ' + (this.frameCount - 1)]['Bonus'] = bonus;
-      this.score += bonus;
-    };
+  var currFrame = this.frameCount;
+  if((this.frameCount > 1) && ((this.scoresheet[currFrame-1][1] + this.scoresheet[currFrame-1][2] === 10) && (this.scoresheet[currFrame-1][1] < 10))) {
+    var bonus = this.scoresheet[currFrame][1];
+    this.scoresheet[currFrame-1]['Bonus'] = bonus;
+    this.score += bonus;
   };
 };
 
 Game.prototype.strikeBonus = function() {
-  if(this.frameCount > 1) {
-    if(this.scoresheet['Frame ' + (this.frameCount - 1)]['Roll 1'] === 10) {
-      var bonus = this.scoresheet['Frame ' + this.frameCount]['Roll 1'] + this.scoresheet['Frame ' + this.frameCount]['Roll 2'];
-      this.scoresheet['Frame ' + (this.frameCount - 1)]['Bonus'] = bonus;
-      this.score += bonus;
-    };
+  var currFrame = this.frameCount;
+  if((this.frameCount > 2) && (this.scoresheet[currFrame-2][1] === 10) && (this.scoresheet[currFrame-1][1] === 10)) {
+    var bonus = this.scoresheet[currFrame-1][1] + this.scoresheet[currFrame][1];
+    this.scoresheet[currFrame-2]['Bonus'] = bonus;
+    this.score += bonus;
+  }
+  else if((this.frameCount > 1) && (this.scoresheet[currFrame-1][1] === 10) && (this.scoresheet[currFrame][1] < 10)) {
+    var bonus = this.scoresheet[currFrame][1] + this.scoresheet[currFrame][2];
+    this.scoresheet[currFrame-1]['Bonus'] = bonus;
+    this.score += bonus;
   };
 };

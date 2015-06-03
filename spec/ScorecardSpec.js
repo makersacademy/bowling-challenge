4611c06bfb3d271dae4af823dd0ebc5400c873fe
Tim <timrobertson0122@gmail.com>
2015-06-03 11:12:36 +0100

    added scorecard with total

---
+++ spec/ScorecardSpec.js
@@ -0,0 +1,20 @@
+describe("Scorecard", function() {
+  var scorecard;
+
+  beforeEach(function() {
+    scorecard = new Scorecard();
+  });
+
+  it("can take a frame", function(){
+    scorecard.addFrame({});
+    expect(scorecard.frames).toEqual([{}]);
+  });
+
+  it("can calculate a total of a series of frames", function(){
+    scorecard.addFrame({total : function() { return 3} });
+    scorecard.addFrame({total : function() { return 3} });
+    expect(scorecard.total()).toEqual(6);
+  })
+
+
+});
---
+++ src/Scorecard.js
@@ -0,0 +1,15 @@
+function Scorecard() {
+  this.frames = [];
+};
+
+Scorecard.prototype.addFrame = function(frame) {
+  this.frames.push(frame)
+};
+
+Scorecard.prototype.total = function() {
+  var runningTotal = 0;
+  for(var i = 0; i < this.frames.length; i ++) {
+    runningTotal += this.frames[i].total();
+  }
+  return runningTotal;
+};

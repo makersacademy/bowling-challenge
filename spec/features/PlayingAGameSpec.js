93d033422914525a59de193d4db2ed0ae669ee37
Tim <timrobertson0122@gmail.com>
2015-06-04 18:55:58 +0100

    trying to break into a features test

---
+++ spec/features/PlayingAGameSpec.js
@@ -0,0 +1,15 @@
+describe("Features", function() {
+
+  beforeEach(function() {
+    scorecard = new Scorecard();
+  });
+
+  describe("Play a game", function(){
+
+    it("can score 10 assorted frames", function(){
+      
+    })
+
+
+
+  });

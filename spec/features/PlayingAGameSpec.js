e5ae92b5bf8737366c98dd0b46ce04a7cf47ae9a
Tim <timrobertson0122@gmail.com>
2015-06-05 17:37:36 +0100

    failing test for all 5s game

--- spec/features/PlayingAGameSpec.js
+++ spec/features/PlayingAGameSpec.js
@@ -19,12 +19,38 @@ describe("Playing a game", function() {
   describe("Gutter game", function(){
 
     it("has a score of zero", function(){
-      for(var i = 0; i <= 10; i ++) {
+      for(var i = 0; i < 20; i ++) {
         var frame = new Frame;
         frame.registerGo(0);
         scorecard.addFrame(frame);
       };
       expect(scorecard.total()).toEqual(0);
+    });
+
+  });
+
+  describe("All 3s game", function(){
+
+    it("has a score of sixty", function(){
+      for(var i = 0; i < 20; i ++) {
+        var frame = new Frame;
+        frame.registerGo(3);
+        scorecard.addFrame(frame);
+      };
+      expect(scorecard.total()).toEqual(60);
+    });
+
+  });
+
+  describe("All 5s game", function(){
+
+    it("has a score of 150", function(){
+      for(var i = 0; i <= 20; i ++) {
+        var frame = new Frame;
+        frame.registerGo(5);
+        scorecard.addFrame(frame);
+      };
+      expect(scorecard.total()).toEqual(150);
     });
 
   });

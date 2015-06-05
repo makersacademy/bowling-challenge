d6000377731b362afe426e678c4629dc51a8902e
Rocco <roccorighi@hotmail.com>
2015-06-05 19:00:18 +0100

    adds basic logic and rewritten tests

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,14 +1,21 @@
 describe('Scorecard', function(){
+  var scorecard;
+  var frame;
+
 
   beforeEach(function() {
-    scorecard = new Scorecard();
-    frame = new Frame();
+    scorecard = new Scorecard;
+    frame = new Frame;
   });
 
-  it('can show the total for a frame', function() {
-    frame.bowl1(7);
-    frame.bowl2(2);
-    expect(scorecard.total).toEqual(9);
+  it('can show the total for a game with no spares or strikes', function() {
+    bowler(bowlFrame,3);
+    expect(scorecard.total).toEqual(21);
+  });
+
+  it('can show the total for a game with only strikes', function() {
+    bowler(bowlStrike,5);
+    expect(scorecard.total).toEqual(50);
   });
 
   it('cannot bowl twice after a strike', function(){
@@ -16,4 +23,21 @@ describe('Scorecard', function(){
     expect(function(){frame.bowl2(1);}).toThrow('Strike, wait for next frame!');
   });
 
+  function bowlFrame() {
+    frame = new Frame;
+    frame.bowl1(4);
+    frame.bowl2(3);
+    scorecard.addFrame(frame);
+  }
+
+  function bowlStrike() {
+    frame = new Frame;
+    frame.bowl1(10);
+    scorecard.addFrame(frame);
+  }
+  function bowler(func, times) {
+    for (var i = 0; i < times; i++) {
+      func();
+    };
+  };
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,22 +1,28 @@
 function Scorecard (){
-  this.total = 0;
-  this.frame = [];
+  this.bonus = 0;
+  this.total = + this.bonus;
+  this.strike = false;
+  this.frames = [];
 };
 
 
 Scorecard.prototype.addFrame = function(frame) {
-  this.frame.push(frame);
-  this.calcTots();
+  this.frames.push(frame);
+  this.calcTots(frame);
 };
 
-Scorecard.prototype.calcTots = function() {
-  var rounds = this.frame.length;
-  var that = this;
-
-  for (var i = 0; i < this.frame.length; i++) {
-    this.total += this.frame[i].total
+Scorecard.prototype.calcTots = function(frame) {
+  this.total += frame.total;
+  this.bonus = 0;
+  var rounds = this.frames.length;
+  for (var i = 0; i < rounds; i++) {
+    if (this.frames[i].strike && i > 2) {
+      if (this.frames[i-3].strike && this.frames[i-2].strike) {
+        this.bonus = this.frames[i-2].total + this.frames[i-1].go1;
+      }
+    this.total += this.frames[i].total
     }
-  }
+  };
 };
 
 

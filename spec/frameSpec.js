6f824783c509f30383a971057c1c32fb3e865790
Anna <anna.klimas@gmail.com>
2015-06-06 15:05:10 +0100

    Frame code and tests refactored.

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,15 +9,15 @@ it('can hold 10 pins', function(){
    expect(frame.startingPins).toEqual(10);
 });
 
-it('can record a bowl', function(){
+it('can record a roll', function(){
   frame.bowl(3);
-  expect(frame.outstandingPins()).toEqual(7);
+  expect(frame.bothRolls[0]).toEqual(3);
 });
 
-it('can record a roll (two bowls)', function(){
+it('can record two rolls', function(){
    frame.bowl(2);
    frame.bowl(3);
-   expect(frame.outstandingPins()).toEqual(5);
+   expect(frame.bothRolls).toEqual([2, 3]);
 });
 
 it('can record a strike', function(){
@@ -31,7 +31,13 @@ it('can record a spare', function(){
   expect(frame.frameSpare()).toEqual(true);
 });
 
-it('is over after 2 bowls', function(){
+it('recognises rolls [4, 6] as a spare', function(){
+  frame.bowl(4);
+  frame.bowl(6);
+  expect(frame.frameSpare()).toEqual(true);
+});
+
+it('is over after 2 rolls', function(){
   frame.bowl(2);
   frame.bowl(3);
   expect(frame.isOver()).toEqual(true);
@@ -42,25 +48,25 @@ it('is NOT over after one bowl if not a strike or a spare', function(){
   expect(frame.isOver()).toEqual(false);
 });
 
-it('is over when bowl is a strike', function(){
+it('is over when frame is a strike', function(){
   frame.bowl(10);
   expect(frame.isOver()).toEqual(true);
 });
 
-it('accepts only one roll', function(){
+it('accepts only two roll', function(){
   frame.bowl(2);
   frame.bowl(3);
   expect(frame.bowl(1)).toEqual("This frame is over");
 });
 
-it('can calculate a score after one bowl', function(){
+it('can calculate a score after one roll', function(){
   frame.bowl(5);
   frame.bowl(2);
   expect(frame.bothRolls[0]).toEqual(5);
 
 });
 
-it('can calculate total score frame score', function(){
+it('can calculate total frame score', function(){
   frame.bowl(2);
   frame.bowl(5);
   expect(frame.totalFrameScore()).toEqual(7);

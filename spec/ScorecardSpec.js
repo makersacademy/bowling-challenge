50c3d0b527950afac07744fdaf1db0d40c177b98
Dan Bolger <bowedcymbal@gmail.com>
2015-06-07 16:50:08 +0100

    adds total number of frames

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -10,4 +10,10 @@ describe("Scorecard", function() {
     expect(scorecard.frames).toEqual([{}]);
   });
 
+  it("can calculate a total over a number of frames", function(){
+    scorecard.recordFrame({total : function(){ return 4} })
+    // // this creates an object which has a 'total' function which returns 4 (and then 6 below)
+    scorecard.recordFrame({total : function(){ return 6} })
+    expect(scorecard.total()).toEqual(10);
+  })
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -5,3 +5,11 @@ function Scorecard() {
 Scorecard.prototype.recordFrame = function(frame) {
   this.frames.push(frame)
 };
+
+Scorecard.prototype.total = function() {
+  var runningTotal = 0
+  for(var i = 0; i < this.frames.length; i = (i + 1)) {
+    runningTotal += this.frames[i].total();
+  }
+  return runningTotal;
+};

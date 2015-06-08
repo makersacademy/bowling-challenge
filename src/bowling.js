97fe04ee8c655d3dfc160fd6d1c063620331f552
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-08 01:13:53 +0100

    multiple refactors to store frames, score summation failing

--- src/bowling.js
+++ src/bowling.js
@@ -2,23 +2,50 @@ function Scorecard(){
 // Simplest structure: array, each element is a frame
 // and each frame is itself an array of 2-3 integers: 1st, (2nd,) bonus
   this.shots = [];
+  this.currentFrame = [];
 };
 
+// Mark the result of a single bowl
+// Missing: bonus points, 3 bowls on 10th, 10 pin limit on frame
 Scorecard.prototype.bowl = function (pins) {
-  // Simple sanity check. Better version checks for max 10 in a frame
+  // Simple sanity check
   if(pins > 10) {
     throw new Error("You bowled more pins than were set out");
   };
+  // Add the pins to the currentFrame array
+  this.currentFrame.push(pins);
+  // If the currentFrame is over, move to the next one
+  if (this.currentFrame.length == 2 || pins == 10) {
+    this.nextFrame();
+  };
+};
 
-  this.shots.push(pins);
+//  Stores the current frame, resets variables
+Scorecard.prototype.nextFrame = function () {
+  this.shots.push(this.currentFrame);
+  this.currentFrame = [];
+};
+
+Scorecard.prototype.whichFrame = function () {
+  // Return 1 if the shots array is empty
+  if(this.shots.length == 0) {
+    return 1
+  };
+  // Otherwise return the last saved frame plus one
+  return this.shots.length + 1;
 };
 
 Scorecard.prototype.score = function () {
-  // Return score 0 if array is empty
-  if (this.shots.length == 0) {
-    return 0;
+  var score = 0;
+  // iterate through each stored score and summate them
+  if (this.shots.length > 0) {
+    score += this.shots.reduce((a, b) => a + b);
   };
-  // Otherwise iterate through each element and summate them
-  var score = this.shots.reduce((a, b) => a + b);
+
+  // Now add the current frame pins bowled
+  if (this.currentFrame.length > 0) {
+    score += this.currentFrame.reduce((a, b) => a + b);
+  };
+
   return score;
 };

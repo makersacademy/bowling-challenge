dcbfb3b0339ec542d5ff6091476d1e64d7ca3dbb
DanielJohnston <macademy@danieljohnston.co.uk>
2015-06-08 03:40:24 +0100

    tests passing on strikes and splits after heavy restructure

--- src/bowling.js
+++ src/bowling.js
@@ -1,17 +1,20 @@
 function Scorecard(){
-// Simplest structure: array, each element is a frame
-// and each frame is itself an array of 2-3 integers: 1st, (2nd,) bonus
+// New structure: each element is an array of frameNum, RollNum, pins
   this.shots = [];
-  this.currentFrame = [];
+  this.currentFrame = 1;
+  this.currentRoll = 1;
 };
 
 // Mark the result of a single bowl
 // Missing: bonus points, 3 bowls on 10th, 10 pin limit on frame
 Scorecard.prototype.bowl = function (pins) {
+
+  var shot = [this.currentFrame, this.currentRoll, pins];
+
   // Limit the current frame to 10 pins
   // Note this conflicts with 10th frame special
-  if (this.currentFrame.length == 1) {
-    if((pins + this.currentFrame[0]) > 10) {
+  if(this.currentRoll > 1) {
+    if(pins + this.shots[this.shots.length - 1][2] > 10) {
       throw new Error("You bowled more pins than were set out");
     };
   } else {
@@ -20,44 +23,60 @@ Scorecard.prototype.bowl = function (pins) {
     };
   };
 
-  // Add the pins to the currentFrame array
-  this.currentFrame.push(pins);
-  // If the currentFrame is over, move to the next one
-  if (this.currentFrame.length == 2 || pins == 10) {
-    this.nextFrame();
-  };
+  // Store the current shot
+  this.shots.push(shot);
+
+  // Move to the next roll or frame
+  this.nextRoll();
+
 };
 
-//  Stores the current frame, resets variables
-Scorecard.prototype.nextFrame = function () {
-  this.shots.push(this.currentFrame);
-  this.currentFrame = [];
+Scorecard.prototype.nextRoll = function () {
+  // Test for second roll in a frame - ignore 10th frame for now
+  if(this.currentRoll === 2) {
+    this.currentRoll = 1;
+    this.currentFrame ++;
+  // Test for a strike on the first roll - ignore 10th frame for now
+  } else if (this.shots[this.shots.length -1][2] === 10) {
+    this.currentRoll = 1;
+    this.currentFrame ++;
+  // Otherwise increment from first to second roll
+  } else {
+    this.currentRoll ++;
+  };
 };
 
 Scorecard.prototype.whichFrame = function () {
-  // Return 1 if the shots array is empty
-  if(this.shots.length == 0) {
-    return 1
-  };
-  // Otherwise return the last saved frame plus one
-  return this.shots.length + 1;
+  return this.currentFrame;
 };
 
+// This gives the total score, not broken down
 Scorecard.prototype.score = function () {
   var score = 0;
-  // iterate through each stored score and summate them
-  if (this.shots.length > 0) {
-    for (var i=0;i<this.shots.length;i++) {
-      for (var j=0;j<this.shots[i].length;j++) {
-        score += this.shots[i][j];
-      };
+
+  // prevent errors from empty array
+  if (this.shots.length === 0) {
+    return 0;
+  };
+
+  // iterate through each score and sum them up
+  for (var shot=0;shot<this.shots.length;shot++) {
+    score += this.shots[shot][2];
+
+    // add on strikes and spares if necessary
+    var bonusRolls = 0;
+    if(this.shots[shot][2] === 10 && this.shots[shot][1] === 1) {
+      bonusRolls = 2;
+    } else if (this.shots[shot][1] === 2 && this.shots[shot][2] +
+      this.shots[shot-1][2] === 10) {
+      bonusRolls =1;
     };
+    for (var i=1;i<=bonusRolls && shot+i < this.shots.length;i++) {
+      score += this.shots[shot+i][2];
+    };
+
   };
-  // Now add the current frame pins bowled
-  if (this.currentFrame.length > 0) {
-    score += this.currentFrame.reduce((a, b) => a + b);
-    // console.log(score);
-  };
+
   return score;
 };
 

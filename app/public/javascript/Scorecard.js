cdd40b18b2f6e4eaa090c5b243cd3cba4611cd48
Simon Legg <leggsimon@gmail.com>
2015-07-11 19:27:25 +0100

    Scorecard registers frames

--- app/public/javascript/Scorecard.js
+++ app/public/javascript/Scorecard.js
@@ -1,9 +1,14 @@
 var BowlingScorecard = function() {
   this.score = 0
+  this.frame = 0
   this.framerolls = [[],[],[],[],[],[],[],[],[],[]]
+
 };
 
 BowlingScorecard.prototype.roll = function (numberOfPinsHit) {
   this.score += numberOfPinsHit
-  this.framerolls[0].push(numberOfPinsHit)
+  if (this.framerolls[this.frame].length >= 2) {
+    this.frame ++
+  }
+  this.framerolls[this.frame].push(numberOfPinsHit)
 };
--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -12,8 +12,8 @@ describe ('BowlingScorecard', function() {
     expect(scorecard.framerolls.length).toEqual(10)
   });
 
-  describe('Player can', function() {
-    it('roll a ball', function() {
+  describe('Player', function() {
+    it('can roll a ball', function() {
       scorecard.roll(4)
       expect(scorecard.score).toEqual(4)
     });
@@ -23,11 +23,22 @@ describe ('BowlingScorecard', function() {
     // });
   });
 
-  describe('Rolls', function() {
-    it('are inputted into frames', function() {
+  describe('Roll', function() {
+    it('is inputted into frame', function() {
       scorecard.roll(3)
       scorecard.roll(4)
       expect(scorecard.framerolls[0]).toEqual([3, 4])
+    });
+  });
+
+  describe('Frame', function() {
+    it('can only have 2 rolls', function() {
+      scorecard.roll(3)
+      scorecard.roll(4)
+      scorecard.roll(6)
+      scorecard.roll(2)
+      expect(scorecard.framerolls[0]).toEqual([3, 4])
+      expect(scorecard.framerolls[1]).toEqual([6, 2])
     });
   });
 });

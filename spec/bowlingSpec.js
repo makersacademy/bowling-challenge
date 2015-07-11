ddb1054d0ee0d7daccf461dc6680fc0afc28afed
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-12 00:29:29 +0100

    Ensuring score card can logging the results of rolls in the correct frames

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,16 +5,43 @@ describe('BowlingScoreCard', function() {
     bowlingScoreCard = new BowlingScoreCard();
   });
 
-  it('has 10 frames', function() {
-    expect(bowlingScoreCard.allFrames.length).toEqual(10);
+  describe('setup', function() {
+
+    it('has 10 frames', function() {
+      expect(bowlingScoreCard.allFrames.length).toEqual(10);
+    });
+
+    it('has two rolls per frame', function() {
+      expect(bowlingScoreCard.allFrames[0].length).toEqual(2);
+    });
+
+    it('starts with a total score of 0', function() {
+      expect(bowlingScoreCard.totalScore).toEqual(0);
+    });
   });
 
-  it('has two rolls per frame', function() {
-    expect(bowlingScoreCard.roll.length).toEqual(2);
-  });
+  describe('logging results of rolls', function() {
 
-  it('starts with a total score of 0', function() {
-    expect(bowlingScoreCard.totalScore).toEqual(0);
+    it('can track rolls', function() {
+      expect(bowlingScoreCard.roll(6)).toEqual(6)
+    });
+
+    it('knows the current frame', function() {
+      bowlingScoreCard.roll(6);
+      bowlingScoreCard.roll(2);
+      expect(bowlingScoreCard.getCurrentFrame()).toEqual(1)
+    });
+
+    it('knows the current roll', function() {
+      bowlingScoreCard.roll(6);
+      expect(bowlingScoreCard.getCurrentRoll()).toEqual([6, null])
+    });
+
+    it('moves to the next frame when a stike has been hit', function() {
+      bowlingScoreCard.roll(10);
+      expect(bowlingScoreCard.getCurrentFrame()).toEqual(1)
+    });
+
   });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,5 +1,49 @@
 var BowlingScoreCard = function() {
-  this.roll = [null,null];
-  this.allFrames = Array.apply(null, new Array(10)).map(function(){return this.roll});
+  this.allFrames = [[null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null]]
   this.totalScore = 0;
+};
+
+BowlingScoreCard.prototype.showScore = function() {
+  return this.totalScore;
+};
+
+BowlingScoreCard.prototype.roll = function(pinsHit) {
+  var currentFrame = this.getCurrentFrame();
+  var currentRoll = this.getCurrentRoll();
+  if (pinsHit === 10) {
+    this.allFrames[currentFrame] = [10, '-'];
+  }
+  else if (currentRoll[0] === null) {
+    currentRoll[0] = pinsHit;
+    this.allFrames[currentFrame] = currentRoll;
+    // console.log('The current frame is '+ currentFrame + ' and the current roll is ' + currentRoll);
+  } else {
+    currentRoll[1] = pinsHit;
+    this.allFrames[currentFrame] = currentRoll;
+    // console.log('The current frame is '+ currentFrame + ' and the current roll is ' + currentRoll);
+  }
+  return pinsHit;
+};
+
+BowlingScoreCard.prototype.getCurrentFrame = function() {
+  for (index=0; index < this.allFrames.length; index++){
+    if (this.allFrames[index][1] === null) {
+      return index;
+      break;
+    }
+  }
+};
+
+BowlingScoreCard.prototype.getCurrentRoll = function() {
+  var currentFrame = this.getCurrentFrame();
+  return this.allFrames[currentFrame]
 };

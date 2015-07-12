d6fad90a51581435c796ec212e214b6634810629
Gerard Morera <grdmorera@gmail.com>
2015-07-12 21:19:29 +0100

    taking nice path

--- spec/BowlingCountSpec.js
+++ spec/BowlingCountSpec.js
@@ -4,39 +4,50 @@ describe('BowlingCounter', function () {
     bowlingCounter = new BowlingCounter(); 
   });
 
-  describe('The counting of one game move', function () {
-    it('throws 5 pins', function () {
-      bowlingCounter.ball(5)
+  xdescribe('The counting of one ball', function () {
+    it('gets the correct result', function () {
+      bowlingCounter.getPinsPerBalls(5)
 
-      expect(bowlingCounter.getScore()).toEqual(5)
-    });
-
-    it('throws strike' ,function() {
-      bowlingCounter.ball(10)
-
-      expect(bowlingCounter.getScore()).toEqual(10)
+      expect(bowlingCounter.score()).toEqual(5)
+      expect(bowlingCounter.frameSum).toEqual(5)  
     });
   });
 
-  describe('The counting of a two move game', function () {
-    describe('the first move', function () {
-      it('', function () {
-        
-      });
+  xdescribe('The counting of two balls', function () {
+    xit('gets the correct result', function () {
 
-      it('throws spare' ,function() {
-        
-      });
+      bowlingCounter.getPinsPerBalls(5,2)
 
+      expect(bowlingCounter.score()).toEqual(7)  
+      // expect(bowlingCounter.frameSum).toEqual(7)  
     });
-    describe('The second move', function () {
-      it('', function () {
-        
-      });
+  });
 
-      it('throws spare' ,function() {
-        
-      });
+ describe('The counting of three balls', function () {
+    it('gets the correct result with an strike', function () {
+      
+      bowlingCounter.getPinsPerBalls(10,5,2)
+
+      expect(bowlingCounter.score()).toEqual(24)  
+    });
+
+    it('gets the correct result with a spare' ,function() {
+      
+      bowlingCounter.getPinsPerBalls(3,7,2)
+
+      expect(bowlingCounter.score()).toEqual(14)  
+    });
+  });
+
+  describe('the counting of a normal game', function () {
+    it('gets the correct result', function () {
+      bowlingCounter.getPinsPerBalls(10,5,2,10,5,2,8,2,8,0,2,3,5,5,2,4,10)
+
+      expect(bowlingCounter.score()).toEqual(107)  
+    });
+
+    xit('' ,function() {
+      
     });
   });
 
--- src/BowlingCount.js
+++ src/BowlingCount.js
@@ -1,11 +1,95 @@
+
 function BowlingCounter () {
-  this.accumulatedScore
+  this.frameSum         = [];
+  this.arrayPinsPerBall = [];
+  this.arrayOfFrames    = [];
+  this.ballCounter      = 0;
+  this.accumulatedScore = 0;
 };
 
-BowlingCounter.prototype.ball = function (pins) {
-  this.accumulatedScore = pins
+BowlingCounter.prototype.getPinsPerBalls = function () {
+  var args = Array.prototype.slice.call(arguments);
+  this.arrayPinsPerBall = args;
+  
+  this.operator()
 };
 
-BowlingCounter.prototype.getScore = function () {
-  return this.accumulatedScore
+BowlingCounter.prototype.operator = function () {
+  while(this.arrayPinsPerBall.length > 0 ){
+     // resetFrameSum()
+    this.arrayOfFrames.push(this.makeFrameSum(this.getFrameValues()));
+  }
 };
+
+BowlingCounter.prototype.getFrameValues = function () {
+  var frame = []
+
+  if(this.strike()) {
+    frame = this.arrayPinsPerBall.splice(0,1);
+  }
+  else {
+    frame = this.arrayPinsPerBall.splice(0,2);
+  }
+  return frame;
+};
+
+BowlingCounter.prototype.makeFrameSum = function (value) {
+  var concatValue = []
+  this.increaseBallCounterByTwo();
+  
+  if(this.isPairOfBalls(value)) {
+    this.frameSum = this.reduceFrame(value);
+    this.spareDetector()
+  }
+  else {
+    concatValue = value.concat(this.arrayPinsPerBall.slice(0,2))
+    this.frameSum = this.reduceFrame(concatValue);
+  }
+  return this.frameSum;
+};
+
+BowlingCounter.prototype.reduceFrame = function (frame) {
+  return frame.reduce(function (a, b) {
+    return(a + b);
+  });
+};
+
+BowlingCounter.prototype.flattenArray = function (arr) {
+  return [].concat.apply([],arr);
+};
+
+BowlingCounter.prototype.score = function () {
+  return this.arrayOfFrames.reduce(function (a,b) {
+    return a + b;
+  });
+};
+
+BowlingCounter.prototype.increaseBallCounterByTwo = function () {
+  this.ballCounter += 2
+};
+
+
+BowlingCounter.prototype.resetFrameSum = function () {
+   this.frameSum.length = 0
+};
+
+BowlingCounter.prototype.spareDetector = function () {
+  if (this.spare()) {
+    concatValue = [10].concat(this.arrayPinsPerBall.slice(0,1))
+    this.frameSum = this.reduceFrame(concatValue);
+  }
+};
+
+BowlingCounter.prototype.spare = function () {
+  return this.frameSum === 10;
+};
+
+BowlingCounter.prototype.strike = function () {
+  return this.arrayPinsPerBall[0] === 10;
+};
+
+BowlingCounter.prototype.isPairOfBalls = function (value) {
+  return value.length === 2;
+};
+
+

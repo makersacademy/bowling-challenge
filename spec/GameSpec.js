fb6aa58bd2f5dadad978bd794eec42358d3f7bc7
Natalie Akam <natalie.akam@gmail.com>
2015-07-12 16:39:35 +0100

    Added logic for all rolls, not accounting for score augmentation by strikes, spares

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,47 +6,148 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it ("should with at a score of zero", function() {
+  it ("should start with at a score of zero", function() {
     expect(game.score).toBe(0);
   });
 
-  it ("should with ten pins", function() {
+  it ("should have ten pins", function() {
     expect(game.pins).toBe(10);
+  });
+
+  it ("should be on roll 1", function() {
+    expect(game.roll).toBe(1);
+  });
+
+  it ("should be on frame 1", function() {
+    expect(game.frame).toBe(1);
   });
 
   describe("first bowl of frame", function() {
     it("should bowl a number between 0 and 10", function() {
       game.bowl(10);
       expect(game.score).toBeLessThan(11);
-      expect(game.score).toBeGreaterThan(0);
+      expect(game.score).toBeGreaterThan(-1);
     });
 
     describe("0 is bowled", function() {
-      it("should leave 10 pins standing", function() {
+
+      beforeEach(function() {
         spyOn(Math, 'floor').and.returnValue(0);
         game.bowl(10);
+      });
+
+      it("should leave 10 pins standing", function() {
         expect(game.pins).toBe(10);
       });
 
       it("should have a score of 0", function() {
-        spyOn(Math, 'floor').and.returnValue(0);
-        game.bowl(10);
         expect(game.score).toBe(0);
+        expect(game.scorecard[0][0]).toBe(0);
+      });
+
+      it("should record score", function() {
+        expect(game.scorecard[0][0]).toBe(0);
+      });
+
+      it("should move to roll 2", function() {
+        expect(game.roll).toBe(2);
       });
     });
 
     describe("10 is bowled", function() {
-      it("should leave 0 pins standing", function() {
+      beforeEach(function() {
         spyOn(Math, 'floor').and.returnValue(10);
         game.bowl(10);
-        expect(game.pins).toBe(0);
       });
 
       it("should have a score of 10", function() {
-        spyOn(Math, 'floor').and.returnValue(10);
-        game.bowl(10);
         expect(game.score).toBe(10);
+      });
+
+      it("should record a strike", function() {
+        expect(game.scorecard[0][0]).toBe('X');
+      });
+
+      it("should move onto the next frame", function() {
+        expect(game.frame).toBe(2);
+      });
+
+      it("should keep the roll number as 1", function() {
+        expect(game.roll).toBe(1);
+      });
+
+      it("should put 10 pins back up", function() {
+        expect(game.pins).toBe(10);
       });
     });
   });
+
+  describe("second bowl of frame", function() {
+    it ("should start with at a score of less than 10", function() {
+      game.bowl(9);
+      expect(game.score).toBeLessThan(10);
+    });
+
+    it ("should have more than 0 pins", function() {
+      game.bowl(9);
+      expect(game.pins).toBeGreaterThan(0);
+    });
+
+    it ("should be on roll 2", function() {
+      game.bowl(9);
+      expect(game.roll).toBe(2);
+    });
+
+      describe("split", function() {
+        beforeEach(function() {
+          spyOn(Math, 'floor').and.returnValue(5);
+          game.bowl(5);
+          game.bowl(5);
+        });
+
+        it("should have a score of 10", function() {
+          expect(game.score).toBe(10);
+        });
+
+        it("should record a split", function() {
+          expect(game.scorecard[0][1]).toBe('/');
+        });
+
+        it("should move onto the next frame", function() {
+          expect(game.frame).toBe(2);
+        });
+
+        it("should change the roll number to 1", function() {
+          expect(game.roll).toBe(1);
+        });
+
+        it("should put 10 pins back up", function() {
+          expect(game.pins).toBe(10);
+        });        
+      });
+
+      describe("frame score 8 ", function() {
+        beforeEach(function() {
+          spyOn(Math, 'floor').and.returnValue(4);
+          game.bowl(10);
+          game.bowl(6);
+        });
+
+        it("should move onto the next frame", function() {
+          expect(game.frame).toBe(2);
+        });
+
+        it("should change the roll number to 1", function() {
+          expect(game.roll).toBe(1);
+        });
+
+        it("should put 10 pins back up", function() {
+          expect(game.pins).toBe(10);
+        });
+
+        it("should record the score", function() {
+          expect(game.scorecard[0][1]).toBe(4);
+        });
+      });
+    });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,50 @@
 function Game() {
   this.score = 0;
   this.pins = 10;
+  this.scorecard = [[]];
+  this.frame = 1;
+  this.roll = 1;
 }
 
-Game.prototype.bowl = function(max) {
-  turn = Math.floor(Math.random() * (max + 1));
-  this.score += turn;
-  this.pins -= turn;
+Game.prototype.bowl = function(pins) {
+  hit = Math.floor(Math.random() * (pins + 1));
+  this.score += hit;
+  this.pins -= hit;
+
+  if (this.pins == 0) {
+    if (hit == 10) {
+      this.strike();
+    }
+    else {
+      this.split();
+    }
+    this.frameReset();
+  }
+  else {
+    this.recordScore();
+  }
+};
+
+Game.prototype.strike = function() {
+  this.scorecard[this.frame - 1][0] = 'X';
+};
+
+Game.prototype.split = function() {
+  this.scorecard[this.frame - 1][1] = '/';
+};
+
+Game.prototype.recordScore = function() {
+  this.scorecard[this.frame - 1][this.roll - 1] = hit;
+  if (this.roll == 2) {
+  	this.frameReset();
+  }
+  else {
+    this.roll = 2;
+  }
+};
+
+Game.prototype.frameReset = function() {
+  this.frame += 1;
+  this.pins = 10;
+  this.roll = 1;
 };

644dac98a5f6644c9194286d05abe137e09ab398
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-12 23:03:01 +0100

    1 failing test for 10 frame max per game, add function for basic scoring for first frame

--- public/src/game.js
+++ public/src/game.js
@@ -1,10 +1,41 @@
-// var Thermostat = function() {
-// 	this.temperature =  20;
-// 	this.powerSaver = true;
-// 	this.maxTempSaver = 25;
-// 	this.maxTemp = 32;
-// };
-
 var Game = function() {
-  this.frameArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  this.frameCount = 1;
+  // this.throwCount = 0; // do you need this?
+  this.frameScore = [];
+  this.cumulativeScore = 0;
+};
+
+Game.prototype.throw1 = function(pinsDown) {
+  pins = parseInt(pinsDown)
+  this.score1 = pins;
+  if(pins == 10) {
+    this.score2 = 0;
+    this.calculateScore();
+    this.calculateTotalScore(); // maybe move this elsewhere later when you aren't just trying to pass test?
+  }
+};
+
+Game.prototype.throw2 = function(pinsDown) {
+  pins = parseInt(pinsDown)
+  this.score2 = pins;
+  this.calculateScore();
+  this.calculateTotalScore(); // maybe move this elsewhere later when you aren't just trying to pass test?
+};
+
+Game.prototype.calculateScore = function() {
+    // pushes score1 and score 2 to the frameScore array
+    this.frameScore.push(this.score1);
+    this.frameScore.push(this.score2);
+};
+
+Game.prototype.calculateTotalScore = function() {
+    total = this.score1 + this.score2;
+    // this.cumulativeScore.push(total);
+    this.cumulativeScore += total;
+    return this.cumulativeScore;
+};
+
+// add something to end game when 10 frames reached
+Game.prototype.nextFrame = function() {
+  this.frameCount += 1;
 };
--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,93 +1,96 @@
-// has 10 frames
+// 10 frames per game
+// * 2 throws in each frame (except the 10th where there is a possibility of 3 throws if a strike or spare was thrown in the 9th)
+// * 1 - 9 frames
+// * strike - if all 10 pins are knocked down on the 1st throw (throw1) - second throw is disabled
+// * spare - if 10 pins are knocked down on the 2nd throw (throw2)
+// * score if total pins knocked down in a frame is less than 10, the num of pins knocked down is throw1 + throw2 (returns a numeric score rather than a spare / or strike X)
+// * 1 - 9 frames
+// * 10th frame - if strike or spare thrown in previous go - 3 throws, if not - disable third throw
+//
+// # Scoring
+// * strike - add 10, plus the number of pins knocked down by the next two balls to the score of the previous frame
+// * spare - add 10, plus the number of pins knocked down by the next ball to the score of the previous frame
+// * regular - add the total of pins knocked down by the two throws to the previous frame. If no pins knocked down = gutter ball = zero points
+
 // count score for each frame (frameScore) and adds to totalScore
 // knows when all 10 frames are complete and it is the end of the game
 // knows when a perfact game score of 300 is achieved
 
 
-describe('Game scoring system', function() {
+
+describe('A game of bowling', function() {
 	var game;
 
 	beforeEach(function() {
 		game = new Game();
 	});
 
-	describe('number of frames', function() {
-		it('consists of 10 frames only', function(){
-			// something here to increase game number to attempt to increase frames above 10 and it to fail
-			expect(game.frameArr.length).toEqual(10)
+
+	it('consists of 10 frames', function(){
+		game.frameCount = 9;
+		game.nextFrame();
+		expect(game.frameCount).toEqual(10);
+	});
+
+	it('does not contain 11 frames', function(){
+		game.frameCount = 10;
+		game.nextFrame();
+		expect(game.frameCount).toEqual(10);
+	});
+
+	//test for max 2 throws per frame
+
+	//test for max 3 throws max in final 10th frame
+
+
+	describe('play for 1st frame - MVP - at least it\'ll be a quick game', function() {
+
+		it('calculates frame score array when throw is not spare or strike', function() {
+			game.throw1(1);
+			game.throw2(3);
+			expect(game.frameScore).toEqual([1,3])
 		});
-		it('does not contain more than 10 frames', function(){
-			expect(game.frameArr.length).not.toEqual(11)
+
+		it('calculates cumulative score when throw is not spare or strike', function() {
+			game.throw1(1);
+			game.throw2(3);
+			expect(game.cumulativeScore).toEqual(4)
 		});
+
+		// it('detects a spare', function() {
+		// 	game.throw1(5);
+		// 	game.throw2(5);
+		// 	expect(game.isSpare)toBe(true)
+		// });
+
+		// it('detects a strike', function() {
+		// 	game.throw1(5);
+		// 	game.throw2(5);
+		// 	expect(game.isStrike)toBe(true)
+		// });
+
+		// it('doesn\'t incorrectly detect a strike', function() {
+		// 	game.throw1(5);
+		// 	game.throw2(5);
+		// 	expect(game.isStrike)toBe(false)
+		// });
+
+		// it('detects a gutter frame', function() {
+		// 	game.throw1(0);
+		// 	game.throw2(0);
+		// 	expect(game.isGutter)toBe(true)
+		// });
+
 
 	});
 
-	// describe end of game function?
 
-	describe('number of rolls per frame', function() {
+// describe('scoring', function() {
+// it('knows what its max/perfect score is', function() {
+// 	expect(game.maxScore).toEqual(300);
+// });
+// });
 
-	});
-  //
-	// describe('temperature change', function(){
-	// 	it('can increase', function(){
-	// 		thermostat.increase();
-	// 		expect(thermostat.temperature).toEqual(21);
-	// 	});
-  //
-	// 	it('can decrease', function(){
-	// 		thermostat.decrease();
-	// 		expect(thermostat.temperature).toEqual(19);
-	// 	});
-	// });
-  //
-	// describe('has a minimum temp',function(){
-	// 	it('minimum is 10 degrees', function(){
-	// 		thermostat.temperature = 10;
-	// 		thermostat.decrease();
-	// 		expect(thermostat.temperature).toEqual(10);
-	// 	});
-	// });
-  //
-	// describe('power saving mode on', function() {
-	// 	it('max temperature is 25 degrees', function() {
-	// 		thermostat.temperature = 25;
-	// 		thermostat.increase();
-	// 		expect(thermostat.temperature).toEqual(25);
-	// 	});
-	// });
-  //
-	// describe('power saving mode off', function() {
-	// 	it('max temperature is 32 degrees', function() {
-	// 		thermostat.powerSavingOff();
-	// 		thermostat.temperature = 32;
-	// 		thermostat.increase();
-	// 		expect(thermostat.temperature).toEqual(32);
-	// 	});
-	// });
-  //
-	// describe('reset button', function(){
-	// 	it('resets it to twenty', function(){
-	// 		thermostat.temperature = 25;
-	// 		thermostat.resetTemp();
-	// 		expect(thermostat.temperature).toEqual(20);
-	// 	});
-	// });
-  //
-	// describe('energy usage', function(){
-	// 	it('is set to low if temperature is below 18 degrees', function(){
-	// 		thermostat.temperature = 17;
-	// 		expect(thermostat.energyUse()).toEqual('low');
-	// 	});
-  //
-	// 	it ('is set to medium if temperature is below 25', function(){
-	// 		thermostat.temperature = 24;
-	// 		expect(thermostat.energyUse()).toEqual('medium');
-	// 	});
-  //
-	// 	it ('is set to high if temperature is 25 degrees or above', function(){
-	// 		thermostat.temperature = 25;
-	// 		expect(thermostat.energyUse()).toEqual('high');
-	// 	})
-	// });
+
 
 });

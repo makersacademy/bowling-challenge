9b0fddfab13f8e04d260205c318d444437eda192
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-13 18:26:06 +0100

    setting up basic bowling rules

--- app/public/javascripts/Bowling.js
+++ app/public/javascripts/Bowling.js
@@ -1,10 +1,30 @@
 function Bowling() {
+  this.score = 0;
+  this.turns = 0;
+  this.bowlingFrame = [[null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null],
+                    [null, null]];
 
-  Bowling.prototype.strike = function (turn) {
-    if (turn === 10) {
-      return true;
+  Bowling.prototype.roll = function (pins) {
+    if(pins > 10 || pins < 0) {
+      throw new Error("invalid roll");
+    }
+    this.score += pins;
+    this.turns += 1;
+  };
+
+  Bowling.prototype.getCurrentFrame = function () {
+    if (this.turns % 2 !== 0) {
+      return (this.turns / 2) + 0.5;
     } else {
-      return false;
+      return this.turns / 2;
     }
   };
 
--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -5,5 +5,34 @@ describe('Bowling', function (){
     bowling = new Bowling();
   });
 
-  
+  it('starts at 0 points', function() {
+    expect(bowling.score).toBe(0);
+  });
+
+  it('starts at 0 frames', function() {
+    expect(bowling.turns).toBe(0);
+  });
+
+  it('increases the total score for a roll by the roll amount', function() {
+    bowling.roll(6);
+    expect(bowling.score).toBe(6);
+  });
+
+  it('adds a frame after the first roll', function() {
+    bowling.roll(6);
+    expect(bowling.turns).toBe(1);
+  });
+
+  it('only allows a maximum roll of a strike', function() {
+    expect( function(){ bowling.roll("11"); } ).toThrow(new Error("invalid roll"));
+  });
+
+  it('only allows a minimum roll of a gutter ball', function() {
+    expect( function(){ bowling.roll("-1"); } ).toThrow(new Error("invalid roll"));
+  });
+
+  it('only allows two turns in one frame to add up to 10', function() {
+    bowling.roll(5);
+    expect( function(){ bowling.roll("6"); } ).toThrow(new Error("invalid roll"));
+  });
 });

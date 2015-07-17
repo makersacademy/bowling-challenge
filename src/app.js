650cf6c809ed8c599301fb6b97ebc4f0e6d92a41
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-17 12:04:49 +0100

    Checking for cheats

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-$(document).ready(function() {
+var bowlingScoreCard = new ScoreCard();
 
-  var bowlingScoreCard = new ScoreCard();
+$(document).ready(function() {
 
   $('input[type="submit"]').click(function() {
     var pins = $('input[type="text"]').val()
-    if (isNaN(pins) || parseInt(pins) < 0 || parseInt(pins) > 10) { alert("You can only knock down 0-10 pins"); return}
-    bowlingScoreCard.roll(parseInt(pins))
+
+    /// Check if user has input a number
+    if (isNaN(pins)) { alert('Pins must be a number'); return }
+
+    /// Convert user input string to number
+    var pinsNum = parseInt(pins)
+
+    ///Check number is between 1 and 10
+    if ( pinsNum < 0 || pinsNum > 10) { alert("You can only knock down 0-10 pins"); return}
+
+    /// Check user is not trying to knock down more than 10 pins in first 9 frames
+    if ( checkForCheats(pinsNum) ) {return}
+
+    /// Add pins to the score card and show to user
+    bowlingScoreCard.roll(pinsNum)
     var api = bowlingScoreCard.api()
     var html = ''
     if (api.length > 0) {
@@ -41,6 +54,15 @@ $(document).ready(function() {
     $('td:contains("Strike!")').each(function() {
       $(this).css("color", "red").wrap('<strong></strong>')
     })
+  }
+
+  var checkForCheats = function(pins) {
+    if (bowlingScoreCard.currentFrame !== 9 || (bowlingScoreCard.currentFrame === 9 && bowlingScoreCard.frames[9].roll1 !== 10) ) {
+      if (bowlingScoreCard.frames[bowlingScoreCard.currentFrame].roll1 + pins > 10) {
+        alert('Stop cheating!')
+        return true
+      }
+    }
   }
 
 })

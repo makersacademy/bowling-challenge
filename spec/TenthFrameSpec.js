0e6b164bb8f9074b566c1769ce23ed23e57e9a58
Michael Arnott <arnottmj@aol.com>
2015-07-19 20:33:49 +0100

    complete although need to refactor tests with doubles

--- spec/TenthFrameSpec.js
+++ spec/TenthFrameSpec.js
@@ -76,7 +76,7 @@ describe('TenthFrame', function() {
       expect(frame.isInProgress()).toBe(false);
     });
 
-    it('after 3 rolls and there has been a strike', function() {
+    it('after 3 rolls and there has been a spare', function() {
       frame.roll(1);
       frame.roll(9);
       frame.roll(4);
@@ -84,6 +84,87 @@ describe('TenthFrame', function() {
     });
   });
 
-  // further tests ommitted as all tests need to be re-written with doubles.
+  describe('total score', function() {
+    it('is zero for a new frame', function() {
+      expect(frame.totalScore()).toEqual(0);
+    });
+
+    it('is 10 for a strike', function() {
+      frame.roll(10);
+      frame.roll(5);
+      frame.roll(5);
+      expect(frame.totalScore()).toEqual(10);
+    });
+
+    it('is only the first two rolls when there has been a spare', function() {
+      frame.roll(1);
+      frame.roll(9);
+      frame.roll(5);
+      expect(frame.totalScore()).toEqual(10);
+    });
+
+    it('is correct for an open frame', function() {
+      frame.roll(1);
+      frame.roll(8);
+      expect(frame.totalScore()).toEqual(9);
+    });
+  });
+
+  describe('bonus score', function() {
+    it('is zero for a new frame', function() {
+      expect(frame.totalBonus()).toEqual(0);
+    });
+
+    it('is zero for an open frame', function() {
+      frame.roll(5);
+      frame.roll(4);
+      expect(frame.totalBonus()).toEqual(0);
+    });
+
+    it('is correct when there has been a strike', function() {
+      frame.roll(10);
+      frame.roll(4);
+      frame.roll(4);
+      expect(frame.totalBonus()).toEqual(8);
+    });
+
+    it('is correct when there has been a spare', function() {
+      frame.roll(1);
+      frame.roll(9);
+      frame.roll(4);
+      expect(frame.totalBonus()).toEqual(4);
+    });
+  });
+
+  describe('knows', function() {
+    it('when there has been a strike', function() {
+      frame.roll(10);
+      expect(frame.isStrike()).toBe(true);
+    });
+
+    it('when there has not been a strike', function() {
+      frame.roll(9);
+      expect(frame.isStrike()).toBe(false);
+    });
+
+    it('when there has been a spare', function() {
+      frame.roll(3);
+      frame.roll(7);
+      expect(frame.isSpare()).toBe(true);
+    });
+
+    it('when there has not been a spare', function() {
+      frame.roll(3);
+      frame.roll(6);
+      expect(frame.isSpare()).toBe(false);
+    });
+
+    it('its total score plus any bonus', function() {
+      frame.roll(10);
+      frame.roll(7);
+      frame.roll(7);
+      expect(frame.totalScoreWithBonus()).toEqual(24);
+    });
+  });
 });
 

a12bd40fc0a9f505632df41778102fca1c71a4f5
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-20 20:03:41 +0100

    Refactoring

--- src/api.js
+++ src/api.js
@@ -22,78 +22,10 @@ API.prototype.call = function () {
 
         /// Bonus round
       } else if (i === 9) {
-        /// Hit a strike
-        if (this.bowlingScoreCard.frames[i].roll1 === 10){
-          info = {frame: i+1,
-                  rollNum: 1,
-                  roll: this.bowlingScoreCard.frames[i].roll1,
-                  bonus: "",
-                  score: "",
-                  comments: "Strike! Two bonus rolls"}
-          api.push(info)
-          info = {frame: i+1,
-                  rollNum: 2,
-                  roll: this.bowlingScoreCard.frames[i].roll2,
-                  bonus: "",
-                  score: "",
-                  comments: ""}
-          api.push(info)
-          info = {frame: i+1,
-                  rollNum: 3,
-                  roll: this.bowlingScoreCard.frames[i].bonusRoll,
-                  bonus: "",
-                  score: total,
-                  comments: ""}
-          api.push(info)
-        /// Hits a spare
-      } else if (this.bowlingScoreCard.frames[i].roll1 + this.bowlingScoreCard.frames[i].roll2 === 10){
-          info = {frame: i+1,
-                  rollNum: 1,
-                  roll: this.bowlingScoreCard.frames[i].roll1,
-                  bonus: "",
-                  score: "",
-                  comments: ""}
-          api.push(info)
-          info = {frame: i+1,
-                  rollNum: 2,
-                  roll: this.bowlingScoreCard.frames[i].roll2,
-                  bonus: "",
-                  score: "",
-                  comments: "Spare! One bonus roll"}
-          api.push(info)
-          info = {frame: i+1,
-                  rollNum: 3,
-                  roll: this.bowlingScoreCard.frames[i].bonusRoll,
-                  bonus: "",
-                  score: total,
-                  comments: ""}
-          api.push(info)
-        /// Nothing special
-        } else {
-          info = {frame: i+1,
-                  rollNum: 1,
-                  roll: this.bowlingScoreCard.frames[i].roll1,
-                  bonus: "",
-                  score: "",
-                  comments: ""}
-          api.push(info)
-          info = {frame: i+1,
-                  rollNum: 2,
-                  roll: this.bowlingScoreCard.frames[i].roll2,
-                  bonus: "",
-                  score: total,
-                  comments: ""}
-          api.push(info)
-        }
-        /// Hits a spare
+        this.bounsRoundLogic(i, api, total)
+        /// Hits a spare not on last frame
       } else if (this.bowlingScoreCard.frames[i].isSpare()){
-        info = {frame: i+1,
-                rollNum: 1,
-                roll: this.bowlingScoreCard.frames[i].roll1,
-                bonus: "",
-                score: "",
-                comments: ""}
-        api.push(info)
+        this.regularRoll1(i, api, total)
         info = {frame: i+1,
                 rollNum: 2,
                 roll: this.bowlingScoreCard.frames[i].roll2,
@@ -103,22 +35,78 @@ API.prototype.call = function () {
         api.push(info)
       /// Regular frame
       } else {
-        info = {frame: i+1,
-                rollNum: 1,
-                roll: this.bowlingScoreCard.frames[i].roll1,
-                bonus: "",
-                score: "",
-                comments: ""}
-        api.push(info)
-        info = {frame: i+1,
-                rollNum: 2,
-                roll: this.bowlingScoreCard.frames[i].roll2,
-                bonus: "",
-                score: total,
-                comments: ""}
-        api.push(info)
+        this.regularRoll1(i, api, total)
+        this.regularRoll2(i, api, total)
       }
 
   }
   return api
+};
+
+API.prototype.regularRoll1 = function (i, api, total) {
+  info = {frame: i+1,
+          rollNum: 1,
+          roll: this.bowlingScoreCard.frames[i].roll1,
+          bonus: "",
+          score: "",
+          comments: ""}
+  api.push(info)
+};
+
+API.prototype.regularRoll2 = function (i, api, total) {
+  info = {frame: i+1,
+          rollNum: 2,
+          roll: this.bowlingScoreCard.frames[i].roll2,
+          bonus: "",
+          score: total,
+          comments: ""}
+  api.push(info)
+};
+
+API.prototype.bounsRoundLogic = function (i, api, total) {
+  /// Hit a strike
+  if (this.bowlingScoreCard.frames[i].roll1 === 10){
+    info = {frame: i+1,
+            rollNum: 1,
+            roll: this.bowlingScoreCard.frames[i].roll1,
+            bonus: "",
+            score: "",
+            comments: "Strike! Two bonus rolls"}
+    api.push(info)
+    info = {frame: i+1,
+            rollNum: 2,
+            roll: this.bowlingScoreCard.frames[i].roll2,
+            bonus: "",
+            score: "",
+            comments: ""}
+    api.push(info)
+    info = {frame: i+1,
+            rollNum: 3,
+            roll: this.bowlingScoreCard.frames[i].bonusRoll,
+            bonus: "",
+            score: total,
+            comments: ""}
+    api.push(info)
+  /// Hits a spare
+} else if (this.bowlingScoreCard.frames[i].roll1 + this.bowlingScoreCard.frames[i].roll2 === 10){
+    this.regularRoll1(i, api, total)
+    info = {frame: i+1,
+            rollNum: 2,
+            roll: this.bowlingScoreCard.frames[i].roll2,
+            bonus: "",
+            score: "",
+            comments: "Spare! One bonus roll"}
+    api.push(info)
+    info = {frame: i+1,
+            rollNum: 3,
+            roll: this.bowlingScoreCard.frames[i].bonusRoll,
+            bonus: "",
+            score: total,
+            comments: ""}
+    api.push(info)
+  /// Nothing special
+  } else {
+    this.regularRoll1(i, api, total)
+    this.regularRoll2(i, api, total)
+  }
 };

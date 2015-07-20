aa93a74ae0166a7e9b1c3405b1a222bf8f12cecc
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-20 23:05:02 +0100

    incorrect scoring for strikes and bonuses

--- public/src/game.js
+++ public/src/game.js
@@ -21,8 +21,12 @@ Game.prototype.runningTotal = function() {
   for (var i = 0; i < this.frameList.length; i++) {
     total += this.frameList[i].calculateScore();
     // include some logic to calculate strikes, spares, bonuese etc.
-    // if (this.frameList[i].addBonus() === true) total += ..........
-    // if (this.frameList[i].isStrike() === true) total += .........
+    // if (this.frameList[i].addBonus() === true) {
+    //   total += ..........
+    // }
+    // if (this.frameList[i].isStrike() === true) {
+    //   total += ..........
+    // }
     trackThrow += this.frameList[i].throwCount;
   }
   return total;

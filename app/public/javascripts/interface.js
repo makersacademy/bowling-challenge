dfb375da3fd975ad10417fa5c9ddaa42322ac9ec
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-20 16:29:39 +0100

    some css work

--- app/public/javascripts/interface.js
+++ app/public/javascripts/interface.js
@@ -2,5 +2,40 @@ var bowling = new Bowling();
 
 $("#submit_0").click(function(){
   bowling.roll(0);
-  
+});
+
+$("#submit_1").click(function(){
+  bowling.roll(1);
+});
+
+$("#submit_2").click(function(){
+  bowling.roll(2);
+});
+
+$("#submit_3").click(function(){
+  bowling.roll(3);
+});
+
+$("#submit_4").click(function(){
+  bowling.roll(4);
+});
+
+$("#submit_5").click(function(){
+  bowling.roll(5);
+});
+
+$("#submit_6").click(function(){
+  bowling.roll(6);
+});
+
+$("#submit_7").click(function(){
+  bowling.roll(7);
+});
+
+$("#submit_8").click(function(){
+  bowling.roll(8);
+});
+
+$("#submit_9").click(function(){
+  bowling.roll(9);
 });

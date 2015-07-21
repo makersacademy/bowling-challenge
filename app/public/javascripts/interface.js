a6c662014dd1430b9887ee7df7f1b75e1e9251a6
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-21 14:35:50 +0100

    buttons updating score

--- app/public/javascripts/interface.js
+++ app/public/javascripts/interface.js
@@ -1,43 +1,41 @@
 var bowling = new Bowling();
 
-$('span.score').text(bowling.score);
-
-$("#submit_0").click(function(){
+$("#0").click(function(){
   bowling.roll(0);
 });
 
-$("#submit_1").click(function(){
+$("#1").click(function(){
   bowling.roll(1);
 });
 
-$("#submit_2").click(function(){
+$("#2").click(function(){
   bowling.roll(2);
 });
 
-$("#submit_3").click(function(){
+$("#3").click(function(){
   bowling.roll(3);
 });
 
-$("#submit_4").click(function(){
+$("#4").click(function(){
   bowling.roll(4);
 });
 
-$("#submit_5").click(function(){
+$("#5").click(function(){
   bowling.roll(5);
 });
 
-$("#submit_6").click(function(){
+$("#6").click(function(){
   bowling.roll(6);
 });
 
-$("#submit_7").click(function(){
+$("#7").click(function(){
   bowling.roll(7);
 });
 
-$("#submit_8").click(function(){
+$("#8").click(function(){
   bowling.roll(8);
 });
 
-$("#submit_9").click(function(){
+$("#9").click(function(){
   bowling.roll(9);
 });

8082814117d99924474fb23dc2ce07a9717c488c
Leon Wee <leonwee27@gmail.com>
2015-08-22 17:27:21 +0700

    Successfully used a spy

--- spec/rollSpec.js
+++ spec/rollSpec.js
@@ -5,10 +5,43 @@ describe('Roll', function() {
     roll = new Roll();
   });
 
-  describe('each roll should initialize with', function() {
-    it('potential number of hits of each roll', function() {
-      expect(roll.potentialHits).toEqual([0,1,2,3,4,5,6,7,8,9,10])
+  describe('Intialized properties has', function() {
+    it('potential number of hits', function() {
+      expect(roll.potentialHits).toEqual([0,1,2,3,4,5,6,7,8,9,10]);
+    });
+
+    it('a roll counter', function() {
+      expect(roll.counter).toEqual(0);
     });
   });
+
+  describe('Methods include', function() {
+    var roll, numberofPinsHit, fetchedNumber;
+
+    beforeEach(function() {
+      roll = {
+        setNumber: function(value) {
+          numberOfPinsHit = value
+        },
+        firstRound: function() {
+          return numerofPinsHit;
+        }
+      };
+
+      spyOn(roll, "firstRound").and.returnValue(6);
+
+      roll.setNumber(6);
+      fetchedNumber = roll.firstRound();
+    });
+
+    it('tracks that the spy was called', function() {
+      expect(roll.firstRound).toHaveBeenCalled();
+    });
+
+    // it('first round', function() {
+    //   expect(roll.firstRound()).toEqual(6)
+    // });
+  });
+
 
 });
--- src/roll.js
+++ src/roll.js
@@ -1,3 +1,4 @@
 var Roll = function Roll() {
   this.potentialHits = [0,1,2,3,4,5,6,7,8,9,10]
+  this.counter = 0
 };

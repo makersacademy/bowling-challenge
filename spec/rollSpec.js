4c93e4cb9f0ebd81bf40302238237fcc19a16a78
Leon Wee <leonwee27@gmail.com>
2015-08-22 19:37:30 +0700

    Deleted Random Idea

--- spec/rollSpec.js
+++ spec/rollSpec.js
@@ -13,35 +13,12 @@ describe('Roll', function() {
     it('a roll counter', function() {
       expect(roll.counter).toEqual(0);
     });
+
+    it('measures first roll score', function() {
+      expect(roll.firstRoundScore).toEqual(0);
+    });
   });
 
-  describe('Methods include', function() {
-    var roll, numberofPinsHit, fetchedNumber;
-
-    beforeEach(function() {
-      roll = {
-        setNumber: function(value) {
-          numberOfPinsHit = value
-        },
-        firstRound: function() {
-          return numerofPinsHit;
-        }
-      };
-
-      spyOn(roll, "firstRound").and.returnValue(6);
-
-      roll.setNumber(6);
-      fetchedNumber = roll.firstRound();
-    });
-
-    it('tracks that the spy was called', function() {
-      expect(roll.firstRound).toHaveBeenCalled();
-    });
-
-    // it('first round', function() {
-    //   expect(roll.firstRound()).toEqual(6)
-    // });
-  });
 
 
 });
--- src/roll.js
+++ src/roll.js
@@ -1,4 +1,6 @@
 var Roll = function Roll() {
   this.potentialHits = [0,1,2,3,4,5,6,7,8,9,10]
   this.counter = 0
+  this.firstRoundScore = 0
 };
+

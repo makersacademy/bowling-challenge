7643ffa6652e15b0af7715629e949f0fcca0a381
Leon Wee <leonwee27@gmail.com>
2015-08-23 15:09:02 +0700

    Added 3 functionality

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,9 +5,25 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
-  describe('Initialized stage', function() {
-    it('contains the number of pins', function() {
+  describe('Initialized stage contains', function() {
+    it('the number of pins', function() {
       expect(bowling.pins).toEqual(10);
     })
+
+    it('the first roll score', function() {
+      expect(bowling.firstRollScore).toEqual(0);
+    });
+  });
+
+  describe('Methods', function() {
+    it('counts the first roll score', function() {
+      bowling.firstRoll(6);
+      expect(bowling.firstRollScore).toEqual(6);
+    });
+
+    it('automatically updates the number of pins', function() {
+      bowling.firstRoll(6);
+      expect(bowling.pins).toEqual(4);
+    });
   });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,9 @@
 var Bowling = function Bowling() {
   this.pins = 10
+  this.firstRollScore = 0
+};
+
+Bowling.prototype.firstRoll = function(number) {
+  this.pins -= number
+  return this.firstRollScore = number
 };

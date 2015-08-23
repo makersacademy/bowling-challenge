a5effe5179fae1282237d9fbfb142f8ac6921a9b
Leon Wee <leonwee27@gmail.com>
2015-08-23 15:03:45 +0700

    Passed first test

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,43 +1,13 @@
-describe('Ball', function() {
-  var ball;
+describe('Bowling', function() {
+  var bowling;
 
   beforeEach(function() {
-    ball = new Ball();
+    bowling = new Bowling();
   });
 
-  describe('Intialized properties has', function() {
-    it('measures first round score', function() {
-      expect(ball.firstRollScore).toEqual(0);
-    });
-  });
-
-  describe('First roll score', function() {
-    it('takes an input number', function() {
-      expect(ball.firstRoll(6)).toEqual(6)
-    });
-  });
-});
-
-
-describe('Pin', function() {
-  var pin, ball;
-
-  beforeEach(function() {
-    pin = new Pin();
-    ball = new Ball();
-  });
-
-  describe('each new frame initializes with', function() {
-    it('10 pins to begin with', function() {
-      expect(pin.number).toEqual(10)
-    });
-  });
-
-  describe('each roll', function() {
-    it('automatically updates the pins', function() {
-      ball.firstRoll(6);
-      spyOn(pin, 'firstUpdate').and.returnValue(4)
-      expect(pin.number).toEqual(4);
-    });
+  describe('Initialized stage', function() {
+    it('contains the number of pins', function() {
+      expect(bowling.pins).toEqual(10);
+    })
   });
 });
---
+++ src/bowling.js
@@ -0,0 +1,3 @@
+var Bowling = function Bowling() {
+  this.pins = 10
+};

bae90673ce0256ae013781c22bad88519aeeebe4
Leon Wee <leonwee27@gmail.com>
2015-08-23 15:15:12 +0700

    Implemented first roll and second roll

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -13,10 +13,14 @@ describe('Bowling', function() {
     it('the first roll score', function() {
       expect(bowling.firstRollScore).toEqual(0);
     });
+
+    it('the second roll score', function() {
+      expect(bowling.secondRollScore).toEqual(0);
+    });
   });
 
   describe('Methods', function() {
-    it('counts the first roll score', function() {
+    it('displays the first roll score', function() {
       bowling.firstRoll(6);
       expect(bowling.firstRollScore).toEqual(6);
     });
@@ -24,6 +28,17 @@ describe('Bowling', function() {
     it('automatically updates the number of pins', function() {
       bowling.firstRoll(6);
       expect(bowling.pins).toEqual(4);
+    });
+
+    it('displays the second roll score', function() {
+      bowling.secondRoll(2);
+      expect(bowling.secondRollScore).toEqual(2);
+    });
+
+    it('also updates the number of pins', function() {
+      bowling.firstRoll(6);
+      bowling.secondRoll(2);
+      expect(bowling.pins).toEqual(2);
     });
   });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,9 +1,15 @@
 var Bowling = function Bowling() {
   this.pins = 10
   this.firstRollScore = 0
+  this.secondRollScore = 0
 };
 
 Bowling.prototype.firstRoll = function(number) {
   this.pins -= number
   return this.firstRollScore = number
+};
+
+Bowling.prototype.secondRoll = function(number) {
+  this.pins -= number
+  return this.secondRollScore = number
 };

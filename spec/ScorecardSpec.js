2e6b83480c660603d6c6f84a911ac717b4b396c8
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-23 14:13:14 +0100

    apparently my test is passing but i'm really not sure it should be

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,12 +1,13 @@
 describe('Scorecard', function() {
 
-  var Scorecard;
-  beforeEach(function() {
-    scorecard = new Scorecard();
-  });
+  // var Scorecard;
+  // beforeEach(function() {
+  //   scorecard = new Scorecard();
+  // });
 
   describe('it displays', function(){
     it("number from roll that it's been given", function() {
+      scorecard = new Scorecard();
       expect(scorecard.recordRoll(5)).toEqual(5);
     })
   });
---
+++ src/Scorecard.js
@@ -0,0 +1,8 @@
+var Scorecard = function() {
+};
+
+
+
+Scorecard.prototype.recordRoll = function(roll) {
+  return roll;
+};

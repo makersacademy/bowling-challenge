d2e6fae49bba71f9abebf71b212d4c3b61df94b2
RichardCharman <richardcharman@live.com>
2015-08-24 21:46:02 +0000

    Added tests for start of game, and the appropriate initialization

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,4 +1,41 @@
-describe('Bowling', function() {
-  var bowling;
+describe('Game', function() {
+  var game;
+  
+  beforeEach(function() {
+    game = new Game();
+  });
+  
+  describe('at start of a new game', function () {
+    it('play begins at frame 1', function() {
+      expect(game.frame).toEqual(1);   
+    });  
+
+    it('play begins at ball 1', function() {
+      expect(game.ball).toEqual(1);   
+    });  
+    
+    it('player has a frame score of 0', function() {
+      expect(game.score).toEqual(0);   
+    }); 
+    
+    it('player has a total score of 0', function() {
+      expect(game.score).toEqual(0);   
+    });  
+    
+    it('player has a maximum of 22 rolls available', function() {
+      expect(game.rolls).toEqual(22);   
+    });  
+    
+    it('all 10 pins are set up ready to play', function() {
+      expect(game.pinsInPlay).toContain(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);   
+    });     
+  });  
+  
+  describe('play', function () {
+    it('lets player roll a ball', function() {
+ 
+    });  
+   
+  }); 
   
 });
---
+++ src/Bowling.js
@@ -0,0 +1,8 @@
+var Game = function() {
+  this.score = 0;
+  this.frame = 1;
+  this.ball = 1;
+  this.rolls = 22;
+  frameScore = [];
+  this.pinsInPlay = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+};

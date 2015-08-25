918bd4523e5294d92c46c261134c3b7289630115
RichardCharman <richardcharman@live.com>
2015-08-25 22:16:29 +0000

    updated scoring system and tests. Can simulate a 10 frame game in HTML

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -14,16 +14,8 @@ describe('Game', function() {
       expect(game.frameBall).toEqual(1);   
     });  
     
-    it('player has a frame score of 0', function() {
-      expect(game.score).toEqual(0);   
-    }); 
-    
     it('player has a total score of 0', function() {
       expect(game.score).toEqual(0);   
-    });  
-    
-    it('player has a maximum of 22 rolls available', function() {
-      expect(game.rolls).toEqual(22);   
     });  
     
     it('all 10 pins are set up ready to play', function() {
@@ -33,25 +25,27 @@ describe('Game', function() {
   
   describe('when playing a game', function () {
     it('player can roll a ball', function() {
-      game.rollBall();
-      expect(game.rolls).toEqual(21);
+      game.play();
       expect(game.frameBall).toEqual(2);
-    });  
+    });     
    
-    it('knows how many pins have been knocked down', function() {
-      game.hitPin(2);
-      game.hitPin(4);
-      game.hitPin(7);
-      expect(game.pinsInPlay).toContain(1, 3, 5, 6, 8, 9, 10)
-      expect(game.knockedDown).toContain(2, 4, 7)
+    it('score records how many pins have been knocked down', function() {
+      game.play;
+      expect(game.rollBall()).toEqual(game.frameScore);
     });
  
-    it('can calculate a players score', function() {
-      game.hitPin(2);
-      game.hitPin(4);
-      game.hitPin(7);
-      expect(game.frameScore).toEqual(3)
+    it('can add points from roll to players total score', function() {
+      game.play;
+      game.play;
+      expect(game.totalScore()).toEqual(game.score);
     });
+    
+    it('logs score on scorecard', function() {
+      game.knockedDown = [];
+      game.rollBall();
+      game.updateScoreCard();
+      expect(game.knockedDown.pop()).toEqual(game.frameScore);
+    });    
     
     it('knows if player has rolled a gutter ball', function() {
       game.hitPin(2);
@@ -63,13 +57,10 @@ describe('Game', function() {
     }); 
     
     it('knows if player has rolled a spare', function() {
-      game.rollBall();
       game.hitPin(1);
       game.hitPin(2);
       game.hitPin(3);
-      expect(game.frameScore).toEqual(3)
-      expect(game.frameBall).toEqual(2)      
-      game.rollBall();
+      expect(game.frameScore).toEqual(3)    
       game.hitPin(4);
       game.hitPin(5);
       game.hitPin(6);
@@ -77,8 +68,7 @@ describe('Game', function() {
       game.hitPin(8);
       game.hitPin(9);
       game.hitPin(10);
-      expect(game.frameScore).toEqual(10)
-      expect(game.frameBall).toEqual(3)      
+      expect(game.frameScore).toEqual(10)   
     }); 
     
     it('knows if player has rolled a strike', function() {
@@ -96,9 +86,10 @@ describe('Game', function() {
     });  
     
     it('knows what frame is being played', function() {
-      game.rollBall();
-      game.rollBall();
-      expect(game.frame).toEqual(2);
-    });     
-  });   
+      game.play();
+      expect(game.play()).toEqual(10);
+    });  
+        
+  });  
+  
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,16 +2,31 @@ var Game = function() {
   this.score = 0;
   this.frame = 1;
   this.frameBall = 1;
-  this.rolls = 22;
   this.frameScore = 0;
   this.knockedDown = [];
+  this.scoreCard = [];  
   this.pinsInPlay = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 };
 
+Game.prototype.play = function() {
+  while(this.frame < 10) {
+    this.knockedDown = [];
+    this.frameBall = 0;
+    this.rollBall();
+    this.scoreCard.push(this.knockedDown);
+    this.frame += 1;
+  };
+  return this.frame;
+};
+
 Game.prototype.rollBall = function() {
-  this.rolls = this.rolls-1;
-  this.frameBall = this.frameBall+1;
-  this.frame = this.frame+0.5;
+  while(this.frameBall < 2) {
+    this.frameBall+=1;
+    this.frameScore = this.rollScore();
+    this.updateScoreCard();
+    this.totalScore();    
+  };
+  return this.frameScore
 };
 
 Game.prototype.hitPin = function(pin) {
@@ -19,5 +34,17 @@ Game.prototype.hitPin = function(pin) {
   this.knockedDown.push(pin);
   if(pin != 0) {
     this.frameScore = this.frameScore + 1    
-  } 
+  };
+};
+
+Game.prototype.rollScore = function() {
+  return Math.floor(Math.random()*11);
+};
+
+Game.prototype.updateScoreCard = function() {
+  return this.knockedDown.push(this.frameScore);
+};
+
+Game.prototype.totalScore = function() {
+  return this.score += this.frameScore
 };

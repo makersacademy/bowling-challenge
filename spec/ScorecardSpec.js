c338431ff63c325d0f0f89595c5a3cab53534522
Ajitsy <sanghera.p@gmail.com>
2015-08-28 14:49:42 +0100

    adds additional bonus points to a frame for a spare or strike

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -6,14 +6,10 @@ describe('Scorecard', function(){
     scorecard = new Scorecard();
   });
 
-  describe('can see scores', function(){
+  describe('can see scores but no totals', function(){
 
     it('running total starts at zero', function(){
       expect(scorecard.runningTotal).toBeNull();
-    });
-
-    it('frame total starts at zero', function(){
-      expect(scorecard.frameTotal).toBeNull();
     });
 
     it('cannot score more than 10 points for first roll', function(){
@@ -34,5 +30,26 @@ describe('Scorecard', function(){
       }
       expect(function() { scorecard.roll(5, 4) }).toThrow(new Error("You cannot play more than 10 frames!"));
     });
+  });
+
+  describe('can calculate frame total', function(){
+
+    it('shows the frame score', function(){
+      scorecard.roll(3,5);
+      expect(scorecard.frameTotal(1)).toBe(8);
+    });
+
+    it('adds additional points for bonuses for spares and shows score', function(){
+      scorecard.roll(3,7); // this is roll 1 - We get a spare!
+      scorecard.roll(5,4); // this is roll 2
+      expect(scorecard.frameTotal(1)).toBe(15);
+    });
+
+    it('adds additional points for bonuses for a strike and shows score', function(){
+      scorecard.roll(10,0); // this is roll 1 - We get a strike!
+      scorecard.roll(5,4);  // this is roll 2
+      expect(scorecard.frameTotal(1)).toBe(19);
+    });
+
   });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,19 +1,34 @@
 var Scorecard = function(){
 this.runningTotal = null;
-this.frameTotal = null;
 this.rolls = [];
-
+this.frameTotals = []
 };
 
 Scorecard.prototype.roll = function(roll1, roll2){
   if (this.rolls.length > 10){throw new Error("You cannot play more than 10 frames!");};
   if(roll1 > 10){throw new Error("can't score more than 10!");};
   if(roll2 > 10){throw new Error("can't score more than 10!");};
+  if((roll1+roll2) > 10) {throw new Error("You cannot score more than 10 from two rolls!");};
+  this.rolls.push([roll1,roll2]);
+};
+// f = frame , r = roll
+Scorecard.prototype.frameTotal = function(i){
+  var f1r1 = (this.rolls[i-1][0]);
+  var f1r2 = (this.rolls[i-1][1]);
+  if(this.rolls.length > 1) {
+    var f2r1 = (this.rolls[i][0]);
+    var f2r2 = (this.rolls[i][1]);
+  }
 
-  var roll_total = [roll1+roll2];
+  var total = (f1r1 + f1r2);
 
-  if(roll_total[0] > 10) {throw new Error("You cannot score more than 10 from two rolls!");};
-
-  this.rolls.push(roll_total);
+  // this is function when we get spare & strike
+  if((f1r1 || f1r2) === 10) {
+    return total += (f2r1 + f2r2);
+  } else if(total === 10) {
+      return total += f2r1;
+  } else {
+    return total;
+  }
 
 };

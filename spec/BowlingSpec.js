4c97c8856f7b8121ca82d4244f189f2a19341013
lewis <ljones140@gmail.com>
2015-08-29 21:30:06 +0100

    10 balls max per frame

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -6,11 +6,49 @@ describe('BowlingScore', function(){
 
   var bowlingScore
 
-  describe('score', function(){
+  it('starts at 0', function(){
+    expect(bowlingScore.score).toBe(0);
+  });
 
-    it('starts at 0', function(){
-      expect(bowlingScore.score).toBe(0);
+  it('starts at frame 1', function(){
+    expect(bowlingScore.currentFrame).toEqual([]);
+  });
+
+  it('starts with 10 pins', function(){
+    expect(bowlingScore.framePinCount).toBe(10);
+  });
+
+  it('starts at ball per frame one', function(){
+    expect(bowlingScore.currentBall).toBe(1);
+  });
+
+  describe('roll', function(){
+
+    it('increments currentBall ', function(){
+      bowlingScore.roll(1);
+      expect(bowlingScore.currentBall).toBe(2);
     });
+
+    it('adds frame to frame after 2 ball rolled', function(){
+      bowlingScore.roll(1);
+      bowlingScore.roll(1);
+      expect(bowlingScore.bowlingFrames).toEqual([[1,1]]);
+    });
+
+    it('reduces framePinCount', function(){
+      bowlingScore.roll(1);
+      expect(bowlingScore.framePinCount).toEqual(9);
+    });
+
+    it('reduces framePinCount', function(){
+      bowlingScore.roll(5);
+      expect(bowlingScore.framePinCount).toEqual(5);
+    });
+
+    it('does not allow > 10 pins per frame', function(){
+      expect(function() {bowlingScore.roll(11)} ).toThrow(new Error("only 10 pins per frame"));
+    });
+
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,3 +1,25 @@
 var BowlingScore = function(){
-  this.score = 0
+  this.score = 0;
+  this.currentFrame = [];
+  this.framePinCount = 10;
+  this.bowlingFrames = [];
+  this.currentBall = 1;
 };
+
+BowlingScore.prototype.roll = function(pinsHit) {
+  this.currentBall++;
+  this.checkValidRoll(pinsHit);
+  this.framePinCount = this.framePinCount - pinsHit;
+  this.currentFrame.push(pinsHit);
+  if (this.currentBall == 2) {
+    this.bowlingFrames.push(this.currentFrame);
+  };
+};
+
+BowlingScore.prototype.checkValidRoll = function(pinsHit) {
+  if (pinsHit > this.framePinCount) {
+    throw new Error("only 10 pins per frame");
+  }
+};
+
+

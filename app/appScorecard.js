57071934f4b4379a6a9be9061bfabd452f68f7cb
Ajitsy <sanghera.p@gmail.com>
2015-08-30 00:34:57 +0100

    basic HTML page created

---
+++ app/appScorecard.js
@@ -0,0 +1,12 @@
+$( document ).ready(function(){
+scorecard = new Scorecard();
+
+function ReplaceCellContent(find, replace){
+    $("#table_headers td:contains('" + find + "')").html(replace);
+};
+
+$('#0').click(function(){
+
+  });
+
+});

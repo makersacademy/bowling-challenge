16d58529f90d8392310ef025610b417b6242095f
Winifred Au <winifredau1990@gmail.com>
2015-08-30 19:57:30 -0400

    adding scorecard.js

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -21,26 +21,3 @@ ScoreCard.prototype.frameScore = function() {
 ScoreCard.prototype.addFramePoints = function(number) {
   return this.frameScore = this.frameScore + number
 };
-
-// function Player() {
-// }
-// Player.prototype.play = function(song) {
-//   this.currentlyPlayingSong = song;
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.pause = function() {
-//   this.isPlaying = false;
-// };
-//
-// Player.prototype.resume = function() {
-//   if (this.isPlaying) {
-//     throw new Error("song is already playing");
-//   }
-//
-//   this.isPlaying = true;
-// };
-//
-// Player.prototype.makeFavorite = function() {
-//   this.currentlyPlayingSong.persistFavoriteStatus(true);
-// };

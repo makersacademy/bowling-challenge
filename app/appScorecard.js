5037f31b79025ce5b3656a783f63a3ea0c48fee8
Ajitsy <sanghera.p@gmail.com>
2015-08-30 16:24:06 +0100

    bowling.html and jquery implemented

--- app/appScorecard.js
+++ app/appScorecard.js
@@ -1,12 +1,42 @@
 $( document ).ready(function(){
 scorecard = new Scorecard();
+var rowID = 1;
+var frameID = 0;
+var array = [];
 
-function ReplaceCellContent(find, replace){
-    $("#table_headers td:contains('" + find + "')").html(replace);
-};
+  $( ".pins" ).click(function() {
+    var rollNo = parseInt($( this ).text());
 
-$('#0').click(function(){
+    $('#' + rowID).text(rollNo);
+    array.push(rollNo);
 
+    var i = array.length;
+
+    if((i > 0) && ((i)%2 == 0) && (rowID < 19)){
+      roll2 = array.pop();
+      roll1 = array.pop();
+      scorecard.roll(roll1,roll2);
+      scorecard.runningTotal();
+      frameID += 1;
+    } else if((i > 0) && ((i)%2 != 0) && (rowID > 19)){
+      roll3 = array.pop();
+      roll2 = array.pop();
+      roll1 = array.pop();
+      scorecard.roll(roll1,roll2,roll3);
+      scorecard.runningTotal();
+      frameID += 1;
+    }
+
+    $('#f' + (frameID-2)).text(scorecard.frameTotals[(frameID-3)]);
+    $('#f' + (frameID-1)).text(scorecard.frameTotals[(frameID-2)]);
+    $('#f' + frameID).text(scorecard.frameTotals[(frameID-1)]);
+
+    // alert(scorecard.frameTotals);
+    rowID += 1;
+  });
+
+  $('#reset').click(function(){
+    window.location.reload();
   });
 
 });

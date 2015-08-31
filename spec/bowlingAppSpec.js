2e35142d77047c3466944c40dd3293f3ff850df8
Leon Wee <leonwee27@gmail.com>
2015-08-31 18:46:58 +0700

    Started with Jquery

--- spec/bowlingAppSpec.js
+++ spec/bowlingAppSpec.js
@@ -7,4 +7,11 @@ describe('Bowling Application', function() {
   it('displays welcome to bowling', function() {
     expect('h1').toContainText('Bowling')
   });
+
+ it('shows the scorecard correctly', function() {
+   expect($('.bowling_scores th').length).toEqual(11);
+   expect($('.bowling_scores tr:nth-child(2) td').length).toEqual(21);
+   expect($('.bowling_scores tr:nth-child(3) td').length).toEqual(11);
+ });
+
 });
--- src/bowlingApp.js
+++ src/bowlingApp.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
+  var bowling = new Bowling();
 
+
+  var buildSelector = function() {
+    for (var i = 0; i < 11; i++) {
+      $('.list').append($('<option>').val(i).html(i))
+    }
+  };
+
+  var buildGUI = function() {
+    var row;
+    for (var i = 0; i < 3; ++i){
+      row = $('<tr>');
+      $('.bowling_scores').append(row);
+      for (var j = 1; j < 12; ++j) {
+        if (i === 0) { row.append($('<th>').text('Frame ' + j).attr('colspan', 2)); }
+        if (i === 1) { row.append('<td>&nbsp;'); row.append('<td>&nbsp;'); }
+        if (i === 2) { row.append($('<td>&nbsp;').attr('colspan', 2)); }
+      }
+    }
+    $('.bowling_scores th:last').html('Total');
+    $('.bowling_scores tr:nth-child(2) td:last').remove();
+  };
+
+
+  buildGUI();
+  buildSelector();
 });

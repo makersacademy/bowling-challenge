489781498a38f88a17d2ce29452f32da54d31e90
Leon Wee <leonwee27@gmail.com>
2015-08-31 16:58:34 +0700

    Refactored completely

--- src/bowling.js
+++ src/bowling.js
@@ -11,29 +11,17 @@ var Bowling = function Bowling() {
 
 Bowling.prototype.firstRoll = function(number) {
   if (this._isInvalid(number)) throw new Error('That is an invalid number');
-  if (this._strikes(number)) this._processStrikes();
+  if (this._isAStrike(number)) this._processStrikes();
   if (this._sparesExist()) this._processSpares(number);
   this.firstRollScore = number
 };
 
 Bowling.prototype.secondRoll = function(number) {
   if (this._exceedsTenPins(number)) throw new Error('There are only 10 pins');
-  if (this.firstRollScore + number === 10) {
-    this.spare += 1
-  }
-  if (this.strike === 1) {
-    this.bonusPoints += (this.firstRollScore + number)
-    this.strike = 0
-  }
-  if (this.strike > 1) {
-    this._calculatesConsecutiveBonusPoints(number);
-    this.strike = 0
-  }
-  this.secondRollScore = number
-  if (this.frameNumber < 10) {
-    this._countsTotalScore();
-    this.frameNumber += 1
-  }
+  if (this._isASpare(number)) this.spare += 1;
+  if (this._hasJustOneStrike()) this._calculatesOneStrike(number);
+  if (this._hasConsecutiveStrikes()) this._calculatesAllStrikes(number);
+  this.frameNumber === 10 ? this.secondRollScore = number : this._processFrame(number);
 };
 
 Bowling.prototype.thirdRoll = function(number) {
@@ -41,13 +29,41 @@ Bowling.prototype.thirdRoll = function(number) {
   this._countsTotalScore();
 };
 
-Bowling.prototype._calculatesConsecutiveBonusPoints = function(number) {
+Bowling.prototype._calculatesBonuses = function(number) {
   this.bonusPoints = ((this.strike - 1) * 2 - 1) * 10 + this.firstRollScore * 2 + number
 };
 
 Bowling.prototype._countsTotalScore = function() {
   this.totalScore += (this.firstRollScore + this.secondRollScore + this.bonusPoints + this.thirdRollScore)
   this._reset();
+};
+
+Bowling.prototype._calculatesOneStrike = function(number) {
+  this.bonusPoints += (this.firstRollScore + number)
+  this.strike = 0
+};
+
+Bowling.prototype._calculatesAllStrikes = function(number) {
+  this._calculatesBonuses(number);
+  this.strike = 0
+};
+
+Bowling.prototype._processStrikes = function() {
+  this.strike += 1
+  this.frameNumber += 1
+  this.firstRollScore = 10
+  this._countsTotalScore();
+};
+
+Bowling.prototype._processSpares = function(number) {
+  this.bonusPoints += number
+  this.spare = 0
+};
+
+Bowling.prototype._processFrame = function(number) {
+  this.secondRollScore = number
+  this._countsTotalScore();
+  this.frameNumber += 1
 };
 
 Bowling.prototype._reset = function() {
@@ -65,22 +81,22 @@ Bowling.prototype._exceedsTenPins = function(number) {
   return (number + this.firstRollScore > 10) && (this.frameNumber < 10)
 };
 
-Bowling.prototype._strikes = function(number) {
+Bowling.prototype._isAStrike = function(number) {
   return number === 10 && this.frameNumber < 10
+};
+
+Bowling.prototype._isASpare = function(number) {
+  return this.firstRollScore + number === 10
 };
 
 Bowling.prototype._sparesExist = function() {
   return this.spare > 0
 };
 
-Bowling.prototype._processStrikes = function() {
-  this.strike += 1
-  this.frameNumber += 1
-  this.firstRollScore = 10
-  this._countsTotalScore();
+Bowling.prototype._hasJustOneStrike = function() {
+  return this.strike === 1
 };
 
-Bowling.prototype._processSpares = function(number) {
-  this.bonusPoints += number
-  this.spare = 0
+Bowling.prototype._hasConsecutiveStrikes = function() {
+  return this.strike > 1
 };

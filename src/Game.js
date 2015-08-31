9f2440e0fccdac5fbb3a01ece26207f575ad3d79
Aftermaths <fnzzhou@gmail.com>
2015-08-31 15:40:38 +0100

    backend complete

--- src/Game.js
+++ src/Game.js
@@ -43,7 +43,15 @@ Game.prototype.spareBonus = function(frame) {
 };
 
 Game.prototype.strikeBonus = function(frame) {
-  return this.frameScore(frame + 1);
+  if (this.isStrike(frame + 1)) {
+    if (this.isStrike(frame + 2)) {
+      return this.frameScore(frame + 1) + this.frameScore(frame + 2);
+    } else {
+      return this.frameScore(frame + 1) + this.allFrame[frame + 2][0];
+    };
+  } else {
+    return this.frameScore(frame + 1);
+  };
 };
 
 Game.prototype.score = function() {

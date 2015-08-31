7711fbc7bf9856343736ba4688ea81895cd1ef04
Sivan Patel <sivan.patel@googlemail.com>
2015-08-31 17:09:16 +0100

    All working apart from strikes and spares

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,111 @@
+describe('bowling game logic', function(){
+  var game;
+
+  beforeEach(function(){
+    game = new BowlingGame();
+  });
+
+  it('starts with score = 0', function() {
+    expect(game.score).toEqual(0);
+  });
+
+  it('has 10 frames', function() {
+    expect(game.allFrames.length).toEqual(10);
+  });
+
+  it('can roll a ball', function() {
+    game.roll(2);
+    expect(game.score).toEqual(2);
+  });
+
+  it('can only take rolls that are 10 or less', function() {
+    expect(function() { game.roll(11) } ).toThrow(new Error ("Illegal throw"));
+    expect(game.score).toEqual(0);
+  })
+
+  it('a roll in input into the frame scores', function() {
+    game.roll(5);
+    game.roll(4);
+    expect(game.allRolls[0]).toEqual([5,4]);
+  });
+
+  it('can only roll twice before moving onto another frame', function() {
+    game.roll(2);
+    game.roll(2);
+    game.roll(2);
+    game.roll(2);
+    expect(game.allRolls[0]).toEqual([2,2]);
+    expect(game.allRolls[1]).toEqual([2,2]);
+    expect(game.score).toEqual(8);
+  });
+
+  it('only rolls once if a strike is scored', function() {
+  game.roll(10);
+  game.roll(5);
+  expect(game.allFrames[0]).toEqual(10);
+  expect(game.allFrames[1]).toEqual(5);
+  });
+
+  it('can roll a strike', function() {
+    game.roll(10);
+    expect(game.allRolls[0]).toEqual([10,0]);
+    expect(game.allFrames[0]).toEqual(10);
+  })
+
+  it('can calculate the score for one frame', function() {
+    game.roll(2);
+    game.roll(6);
+    expect(game.score).toEqual(8);
+  });
+
+  it('can calculate the score for more than one frame', function() {
+    game.roll(2);
+    game.roll(6);
+    game.roll(2);
+    game.roll(6);
+    expect(game.allFrames[0]).toEqual(8);
+    expect(game.allFrames[1]).toEqual(8);
+    expect(game.score).toEqual(16);
+  });
+
+  it('can give bonus points for strikes', function() {
+    game.roll(10);
+    game.roll(3);
+    game.roll(2);
+    expect(game.allFrames[0]).toEqual(15);
+    expect(game.allFrames[1]).toEqual(5);
+    expect(game.score).toEqual(20);
+  });
+
+  it('can give bonus points for spares', function() {
+    game.roll(7);
+    game.roll(3);
+    game.roll(4);
+    expect(game.allFrames[0]).toEqual(14);
+    expect(game.allFrames[1]).toEqual(4);
+    expect(game.score).toEqual(18);
+  });
+
+
+});
+
+
+// it('applies bonus to a frame where a strike was scored', function() {
+//   scorecard.roll(10)
+//   scorecard.roll(3)
+//   scorecard.roll(4)
+//   expect(scorecard.score).toEqual(24)
+//   expect(scorecard.framescore[0]).toEqual(17)
+//   expect(scorecard.framescore[1]).toEqual(7)
+// });
+//
+// it('applies bonuses when 2 strikes have been scored', function() {
+//   scorecard.roll(10)
+//   scorecard.roll(10)
+//   scorecard.roll(3)
+//   scorecard.roll(4)
+//   expect(scorecard.score).toEqual(47)
+//   expect(scorecard.framescore[0]).toEqual(23)
+//   expect(scorecard.framescore[1]).toEqual(17)
+//   expect(scorecard.framescore[2]).toEqual(7)
+// });

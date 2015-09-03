08ec8e43bcc26c088e2b493696c53f4df179ed73
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-03 15:02:06 +0100

    scoring working for 1 frame, unable to get new frame working currently

--- jasmine_template/src/Frame.js
+++ jasmine_template/src/Frame.js
@@ -1,45 +1,67 @@
 var Frame = function() {
-  this.pins = 10;
-  this.totalTurns = 2;
+  this.totalPins = 10;
   this.rollOneScore = 0;
   this.rollTwoScore = 0;
+  this.strike = false;
+  this.totalRollsPerFrame = 2;
   this.totalFrameScore = 0;
   this.totalGameScore = 0;
-  this.strike = 10;
   this.frameNumber = 1;
+  this.totalFrames = 10;
 };
 
 Frame.prototype.newGame = function() {
-  return this.pins;
+  return this.totalPins;
 };
 
 Frame.prototype.rollOne = function(number) {
-  if (this.totalTurns <= 0 ) {
+  if (this.totalTurnsPerFrame <= 0 ) {
     if (this.strike === true) {
       return 'STRIKE!'}
   } else {
-    this.totalTurns --;
+    this.totalRollsPerFrame --;
     this.rollOneScore = number;
-    return this.pins -= number;
+    return this.totalPins -= number;
   }
 };
 
 Frame.prototype.rollTwo = function(number) {
-  if (this.totalTurns === 0) {
+  if (this.totalTurnsPerFrame === 0) {
     return 'No more throws available' }
   else {
-    this.totalTurns --;
+    this.totalRollsPerFrame --;
     this.rollTwoScore = number;
     this.totalFrameScore = this.rollOneScore + this.rollTwoScore;
-    this.frameNumber ++;
+    this.totalFrames;
+    return this.totalPins -= number
   };
 };
 
-Frame.prototype.totalScore = function() {
-  this.totalGameScore += totalFrameScore;
+Frame.prototype.playerTurns = function() {
+  if (this.totalTurnsPerFrame === 0)
+    return 'Frame completed'
 };
 
-Frame.prototype.playerTurns = function() {
-  if (this.totalTurns === 0)
-    return 'Frame completed'
+Frame.prototype.frameCount = function(number) {
+  this.totalFrames -= number;
+    return this.totalFrames;
+};
+
+Frame.prototype.fullStrike = function() {
+  this.strike = true;
+  this.totalPins = 0;
+  this.rollOne = 10;
+  this.rollOneScore = 10;
+  this.totalFrameScore = 10;
+    return "STRIKE!";
+};
+
+Frame.prototype.startNewFrame = function() {
+  this.totalPins = 10;
+  this.rollOneScore = 0;
+  this.rollTwoScore = 0;
+  this.totalRollsPerFrame = 2;
+  this.totalFrameScore = 0;
+  this.frameNumber ++;
+    return 'New Frame'
 };

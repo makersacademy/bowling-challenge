df59fb39216d818c6e777fc0a87e199778668722
Jongmin Kim <kjm141215@gmail.com>
2015-10-02 20:29:38 +0100

    Add jasmine and create bowling constructor function

---
+++ spec/BowlingSpec.js
@@ -0,0 +1,9 @@
+describe('Bowling', function() {
+
+  beforeEach(function() {
+    bowling = new Bowling();
+  });
+  it ('has 10 pins', function() {
+    expect(bowling.pins).toBe(10);
+  });
+});
---
+++ src/Bowling.js
@@ -0,0 +1,3 @@
+function Bowling() {
+  this.pins = 10;
+}

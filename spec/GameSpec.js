b60dd28806b624fc43c8c57a9693c1712dd04c60
Jongmin Kim <kjm141215@gmail.com>
2015-10-03 20:51:35 +0100

    Edit method so that it can add spare/strike bonus to total

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,4 +12,33 @@ describe ('Game', function() {
     game.countFallenPins(8);
     expect(game.total).toBe(8);
   });
+
+  it('has 10 frames', function() {
+    expect(game.frames.length).toBe(10);
+  });
+
+  it ('has 2 rolls in each frame', function() {
+    expect(game.frames[0].length).toBe(2);
+  });
+
+  // it ('counts scores in each frame', function() {
+  //   game.frames[4][0] = 7;
+  //   game.frames[4][1] = 2;
+  //   expect(game.countFrameScores(5)).toBe(9);
+  // })
+  // it('has bonus when you spare', function() {
+  //   game.frames[4][0] = 7;
+  //   game.frames[4][1] = 3;
+  //   game.frames[5][0] = 4;
+  //   expect(game.countFrameScores(5)).toBe(14);
+  // });
+
+  it('has more bonus when you strike', function() {
+    game.frames[4][0] = 10;
+    game.frames[4][1] = 0;
+    game.frames[5][0] = 4;
+    game.frames[5][1] = 4;
+    expect(game.countFrameScores(5)).toBe(18);
+  });
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,25 @@
 function Game() {
   this.total = 0;
+  var array = new Array();
+  for (var i=0; i<10; i++) {
+    array.push(new Array(2));
+  }
+  this.frames = array
 };
 
 Game.prototype.countFallenPins = function(pins) {
   this.total += pins
 };
+
+Game.prototype.countFrameScores = function(nth) {
+  var sum, spareBonus, strikeBonus;
+  spareBonus = this.frames[nth][0];
+  strikeBonus = spareBonus + this.frames[nth][1];
+  sum = this.frames[nth-1][0] + this.frames[nth-1][1];
+  if (this.frames[nth-1][0] === 10) {
+    sum += strikeBonus;
+  } else if (sum === 10) {
+    sum += spareBonus;
+  }
+  return sum;
+}

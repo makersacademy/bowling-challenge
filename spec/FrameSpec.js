df034b29f051d09e3d7d349c270dbeaf5033d424
Michael Roger <bodgeuk12@gmail.com>
2015-10-05 00:14:39 +0100

    Restarted due to idiotic first attempt

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -2,25 +2,68 @@ describe("Frame", function() {
 
   var frame;
 
-  beforeEach(function(){
+  beforeEach(function() {
     frame = new Frame();
   });
 
-  it("starts with a score of zero", function(){
-    expect(frame.score).toEqual(0);
+  it("starts with 10 pins", function() {
+    expect(frame.pinsRemaining).toEqual(10);
+  })
+
+  describe("#rollOne", function() {
+
+    it("knocks down pins and retains the value (stubbed to 6)", function(){
+      spyOn(Math, 'random').and.returnValue(0.6);
+      frame.rollOne();
+      expect(frame.rollOne).toEqual(6);
+    });
+
+    it("pins remaining is reduced after roll (roll stubbed to 6)", function(){
+      spyOn(Math, 'random').and.returnValue(0.6);
+      frame.rollOne();
+      expect(frame.pinsRemaining).toEqual(4);
+    });
+
+    it("doesn't allow repeated rolls", function(){
+      frame.rollOne();
+      console.log(frame)
+      expect(function() { frame.rollOne(); }).toThrowError();
+    });
+
   });
 
-  it("after a roll has a score between zero and 10 (stubbed to 5)", function(){
-    spyOn(Math, 'random').and.returnValue(0.5);
-    frame.roll();
-    expect(frame.score).toEqual(5);
-  });
 
-  it("the score accumulates for each successive roll (rolls stubbed to 6)", function(){
-    spyOn(Math, 'random').and.returnValue(0.6);
-    frame.roll();
-    frame.roll();
-    expect(frame.score).toEqual(12);
-  });
+
+  // it("score accumulates for each successive roll (rolls stubbed to 6)", function(){
+  //   spyOn(Math, 'random').and.returnValue(0.6);
+  //   frame.roll(1);
+  //   frame.roll(2);
+  //   expect(frame.score).toEqual(12);
+  // });
+
+  // describe("number of rolls allowed", function(){
+
+  //   it("restricted to 2 by default (rolls stubbed to 4)", function(){
+  //     spyOn(Math, 'random').and.returnValue(0.4);
+  //     frame.roll(1);
+  //     frame.roll(2);
+  //     expect(function(){ frame.roll(3); }).toThrowError("Frame is over")
+  //   });
+
+  //   it("restricted to 1 if a strike is rolled", function(){
+  //     spyOn(Math, 'random').and.returnValue(0.99);
+  //     frame.roll(1);
+  //     expect(function(){ frame.roll(2); }).toThrowError("Frame is over")
+  //   });
+
+  //   xit("is 3 in final frame if a strike is rolled first", function(){
+  //     frame.isLastFrame = true
+  //     spyOn(Math, 'random').and.returnValue(0.99);
+  //     frame.rollOne();
+  //     frame.rollTwo();
+  //     frame.rollThree();
+  //   });
+
+  // });
 
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,10 +1,31 @@
 function Frame() {
-  this.score = 0;
-}
-
-Frame.prototype.roll = function() {
-  this.score += Math.floor(Math.random()*11);
+  this.pinsRemaining = 10;
+  this.rollsTaken = 0;
+  // this.isLastFrame = false
+  // this.rollsAllowed = 2
 };
+
+Frame.prototype.rollOne = function() {
+  if (this.rollsTaken >= 1) {
+    throw Error("Already rolled")
+  }
+  this.rollOne = Math.floor(Math.random()*11)
+  this.pinsRemaining -= this.rollOne;
+};
+
+// Frame.prototype.roll = function(number) {
+//   if (this.rollsTaken >= number) {
+//     throw Error("Already rolled")
+//   } else if (this.rollsAllowed < number) {
+//     throw Error("Frame is over");
+//   }
+//   this.rollBall();
+//   this.rollsTaken++
+// };
+
+// Frame.prototype.rollBall = function() {
+//   this.score += Math.floor(Math.random()*11);
+// };
 
 
 

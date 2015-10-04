9b795ab4a45c57a06d8ebddd9c6de5b2fe76d8e5
Harry <harryandrew.dix@gmail.com>
2015-10-04 12:17:10 +0100

    failing spec - player bowls strike -1 frame

--- src/javascripts/bowling-app.js
+++ src/javascripts/bowling-app.js
@@ -1,4 +1,12 @@
 function Game() {
   this.score = 0
-  this.player_frames = 10
+  this.frames_remaining = 10
 };
+
+Game.prototype.play = function() {
+  return Math.floor(Math.random() * (0 + 11)) - 0;
+};
+
+// function getRandomInt(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }

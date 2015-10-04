4bd1b5b76b4d016a3ec4d8f9c798816588ddf40b
Jongmin Kim <kjm141215@gmail.com>
2015-10-04 16:34:59 +0100

    Create scoring table

--- application.js
+++ application.js
@@ -1,7 +1,9 @@
 $(function() {
-
-
-  $('#test').text('hihihi')
-
-
+  for (var i=1; i < 11; i++) {
+    $('#fallenPins').append('<div>' + i + '</div>')
+  }
+  $('tr:first-child').children().addClass('firstRow')
+  $('tr:last-child').children().addClass('secondRow')
+  $('tr:last-child').children().append('<div id="first"></div><div id="second"></div><div id="third"></div>')
+  $('.secondRow:last-child #second').append('<div id="tenth2"></div><div id="tenth3"></div>')
 })

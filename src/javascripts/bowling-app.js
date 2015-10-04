16e8bd5703fa390eb54a1c9854fe4e556d2abefe
Harry <harryandrew.dix@gmail.com>
2015-10-04 18:59:14 +0100

    passes game spec incrementing frames

--- src/javascripts/bowling-app.js
+++ src/javascripts/bowling-app.js
@@ -1,9 +1,12 @@
 function Game() {
   this.score = 0;
-  this.frameover = false;
-  this.rolls = []
+  this.frameOver = false;
+  this.rolls = [];
+  this.currentFrame = 1;
 };
 
 Game.prototype.roll = function(pins) {
   this.rolls.push(pins);
+  if (pins === 10 || this.frameOver === true) {this.currentFrame++};
+  this.frameOver = !this.frameOver
 };

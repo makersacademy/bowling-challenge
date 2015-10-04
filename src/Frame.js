564a5d161fcf5d305bf4e380d550b98b0997e9c8
Proskurina <yproskurina@yahoo.com>
2015-10-04 21:04:03 +0100

    Adds Frame, tests pass

---
+++ src/Frame.js
@@ -0,0 +1,42 @@
+Frame = function(){
+  this.pinsHitByFirstRoll = null
+  this.pinsHitBySecondRoll = null
+  this.pinsLeft = 10
+  this.zeroOption = 1
+};
+
+Frame.prototype.roll = function(){
+  if (this.pinsHitByFirstRoll == null) {
+    this.firstRollResult();
+  } else if ((this.pinsHitBySecondRoll == null) && !this.isStrike()){
+    this.secondRollResult();
+  } else {
+    return "No more rolls";
+  };
+};
+
+Frame.prototype.pinsHit = function(){
+  return Math.floor(Math.random()*(this.pinsLeft + this.zeroOption))
+};
+
+Frame.prototype.isStrike = function(){
+  return (this.pinsHitByFirstRoll == 10)
+};
+
+Frame.prototype.isSpare = function(){
+  return (this.totalHitInFrame() == 10)
+};
+
+Frame.prototype.totalHitInFrame = function(){
+return (this.pinsHitByFirstRoll + this.pinsHitBySecondRoll)
+};
+
+Frame.prototype.firstRollResult = function(){
+  this.pinsHitByFirstRoll = this.pinsHit();
+  this.pinsLeft -= this.pinsHitByFirstRoll;
+};
+
+Frame.prototype.secondRollResult = function(){
+  this.pinsHitBySecondRoll = this.pinsHit();
+  this.pinsLeft -= this.pinsHitBySecondRoll;
+};

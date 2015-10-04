4e45ddf799771e2c1e296b6d13365ccb307d6db0
Harry <harryandrew.dix@gmail.com>
2015-10-04 19:57:44 +0100

    converts frame tracking to hash system

--- src/javascripts/bowling-app.js
+++ src/javascripts/bowling-app.js
@@ -1,15 +1,14 @@
 function Game() {
   this.score = 0;
   this.frameOver = false;
-  this.rolls = [];
+  this.rolls = {1:[],
+                2:[],
+                3:[]};
   this.currentFrame = 1;
 };
 
 Game.prototype.roll = function(pins) {
-  this.rolls.push(pins);
-  if (pins === 10 || this.frameOver === true) {this.currentFrame++ ; this.frameOver = !this.frameOver}
+  this.rolls[this.currentFrame].push(pins);
+  if (pins === 10 || this.frameOver === true) {this.currentFrame++; this.frameOver = !this.frameOver}
   else this.frameOver = !this.frameOver;
 };
-
-
- // if (this.frameOver === false) { this.leftPins = 10 - pins; } else { this.leftPins = 10; }

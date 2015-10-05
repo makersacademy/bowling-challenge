1a8b9b83b32d732180923dee0a6cc2a9fd4aa7be
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-05 08:39:59 +0100

    delays scoring spares/strikes

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,18 +1,51 @@
 describe('Game', function() {
 
-  var game;
+  var game, frame, spare, strike;
   beforeEach(function() {
     game = new Game();
-    frame = {};
+    frame = { first: 4, second: 5,
+      isSpare: function() { return false; },
+      isStrike: function() { return false; }
+    };
+    spare = { first: 4, second: 6,
+      isSpare: function() { return true; },
+      isStrike: function() { return false; }
+    };
+    strike = { first: 10,
+      isSpare: function() { return false; },
+      isStrike: function() { return true; }
+    };
   });
 
-  it('starts with no frames', function() {
-    expect(game.frameList.length).toBe(0);
+  describe('starts with', function(){
+    it('an empty frame list', function() {
+      expect(game.frameList.length).toBe(0);
+    });
+    it('an empty frame score list', function() {
+      expect(game.frameScore.length).toBe(0);
+    });
+    it('a null game score', function() {
+      expect(game.score).toBe(null);
+    });
   });
 
-  it('can have frames appended', function() {
-    game.addFrame(frame);
-    expect(game.frameList.length).toBe(1);
+  describe('when frame is added', function(){
+    it('appends it to the list', function() {
+      game.addFrame(frame);
+      expect(game.frameList.length).toBe(1);
+    });
+    it('calculates score if not spare/strike', function() {
+      game.addFrame(frame);
+      expect(game.frameScore[0]).toBe(9);
+    });
+    it('delays scoring spares', function() {
+      game.addFrame(spare);
+      expect(game.frameScore[0]).toBe(null);
+    });
+    it('delays scoring strikes', function() {
+      game.addFrame(spare);
+      expect(game.frameScore[0]).toBe(null);
+    });
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,18 @@
 function Game() {
   this.frameList = new Array;
+  this.frameScore = new Array;
+  this.score = null;
 };
 
 Game.prototype.addFrame = function(frame) {
   this.frameList.push(frame);
+  this.frameScore.push(null);
+  this._calculateScores(this.frameList.length - 1);
+};
+
+Game.prototype._calculateScores = function(index) {
+  var frame = this.frameList[index];
+  if (!frame.isSpare() && !frame.isStrike()) {
+    this.frameScore[index] = frame.first + frame.second;
+  };
 };

1e0ed45a25aea0b52f8ef5051a1bf5474391c12c
Michael Roger <bodgeuk12@gmail.com>
2015-10-06 12:28:43 +0100

    Game has frame index for selecting current frame. Increases by 1 following second roll

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,10 +12,15 @@ describe("Game", function(){
     expect(game.frameArray.length).toEqual(10);
   });
 
+  it("starts with a frame index set to zero", function(){
+    expect(game.frameIndex).toEqual(0);
+  });
+
   describe("#bowl", function(){
 
     beforeEach(function(){
-      frame = jasmine.createSpyObj('frame', ['firstRoll'])
+      frame = jasmine.createSpyObj('frame', ['firstRoll']);
+      frame.rollsTaken = 0
       for (var i = 0; i < 10; i++) {
         game.frameArray[i] = frame;
       };
@@ -25,6 +30,50 @@ describe("Game", function(){
     it("selects the first frame and calls its #firstRoll function", function(){
       game.bowl();
       expect(game.frameArray[0].firstRoll).toHaveBeenCalled();
+    });
+
+    describe("strike NOT thrown on frist roll", function(){
+
+      beforeEach(function(){
+        firstFrame = jasmine.createSpyObj('firstFrame', ['secondRoll']);
+        firstFrame.firstRollScore = 6;
+        firstFrame.rollsTaken = 1;
+        firstFrame.pinsRemaining = 4;
+        firstFrame.isStrike = false;
+        firstFrame.totalScore = 6
+        game.frameArray[0] = firstFrame;
+      });
+
+      it("calls the first frame's #secondRoll function", function(){
+        game.bowl();
+        expect(game.frameArray[0].secondRoll).toHaveBeenCalled();
+      });
+
+      it("frame index increases by one following frame's second roll", function(){
+        game.bowl();
+        expect(game.frameIndex).toEqual(1);
+      });
+
+      xdescribe("spare NOT thrown", function(){
+
+        beforeEach(function(){
+          firstFrame.secondRollScore = 2;
+          firstFrame.rollsTaken = 2;
+          firstFrame.pinsRemaining = 2;
+          firstFrame.isSpare = false;
+          firstFrame.totalScore = 8
+          secondFrame = jasmine.createSpyObj('secondFrame', ['firstRoll']);
+          secondFrame.rollsTaken = 0;
+          game.frameArray[1] = secondFrame;
+        });
+
+        it("#bowl calls the next frames firstRoll function", function(){
+          game.bowl();
+          expect(game.frameArray[1].firstRoll).toHaveBeenCalled();
+        });
+
+      });
+
     });
 
 
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,9 @@
 function Game(frameConstructor) {
-  this.frameGenerator(frameConstructor);
+  this.frameArrayGenerator(frameConstructor);
+  this.frameIndex = 0;
 };
 
-Game.prototype.frameGenerator = function(frameConstructor) {
+Game.prototype.frameArrayGenerator = function(frameConstructor) {
   this.frameArray = [];
   for (var i = 0; i < 10; i++) {
     this.frameArray[i] = new frameConstructor;
@@ -10,5 +11,16 @@ Game.prototype.frameGenerator = function(frameConstructor) {
 };
 
 Game.prototype.bowl = function() {
-  this.frameArray[0].firstRoll();
+  if (this.currentFrame().rollsTaken === 0) {
+    this.currentFrame().firstRoll();
+  } else {
+    this.currentFrame().secondRoll();
+    this.frameIndex++;
+  };
+
+};
+
+Game.prototype.currentFrame = function() {
+  var i = this.frameIndex;
+  return this.frameArray[i];
 };

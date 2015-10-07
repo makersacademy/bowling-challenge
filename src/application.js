899ae48f73fcc91faf201147b75c46c7f700d16d
Dom Batten <dominic.batten@googlemail.com>
2015-10-07 16:04:50 +0100

    Frame score card fully functional. working on X and / for strike and spare

--- src/application.js
+++ src/application.js
@@ -8,10 +8,12 @@ game = new Game(Frame, LastFrame);
     game.bowl(pins);
     var currentFrame = game.currentFrame;
     $('#score_box_' + currentFrame + '_1').text(game.frames[currentFrame].firstRoll);
-    if(game.currentFrame > 0) {
-      $('#score_box_' + (currentFrame - 1) + '_2').text(game.frames[currentFrame -1].secondRoll);
-    };
-    for(i = 0; i < currentFrame; i++) {
+    // if(game.currentFrame > 0) {
+    //   $('#score_box_' + (currentFrame - 1) + '_2').text(game.frames[currentFrame -1].secondRoll);
+    // };
+    for(i = 0; i <= currentFrame; i++) {
+      $('#score_box_' + i + '_1').text(game.frames[i].firstRoll);
+      $('#score_box_' + i + '_2').text(game.frames[i].secondRoll);
       $('#frame_score_' + i).text(game.frameScores[i]);
     }
     if(game.isGameOver()){

9642bf25ea20b3aed2474fede847f4c1cd25c8b4
Michael Roger <bodgeuk12@gmail.com>
2015-10-07 17:21:36 +0100

    All feature tests passing and game appears to be working fully. Final test for perfect game to come next

--- spec/interface/BowlaramaSpec.js
+++ spec/interface/BowlaramaSpec.js
@@ -1,19 +1,69 @@
 describe("Bowlarama", function(){
 
   beforeEach(function(){
-    jasmine.getFixtures().fixturesPath = './UserInterface';
+    jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('bowlarama.html');
   })
 
   it("displays 10 frame score containers", function(){
-    expect($('.frame').length).toEqual(10);
+    expect($('.frame-table').length).toEqual(10);
   });
 
-  it("clcik bowl and first frame updated with result of first roll", function(){
-    spyOn(Math, 'random').and.returnValue(0.6)
-    $('button#bowl').click();
-    expect('.frame#0').toContainText('6');
+  it("click bowl and displays result of first roll", function(){
+    spyOn(Math, 'random').and.returnValue(0.6);
+    $('#bowl').click();
+    expect('#first-0').toContainText('6');
+  });
+
+  it("continues to display results of subsequent rolls", function(){
+    spyOn(Math, 'random').and.returnValue(0.5);
+    for (var i = 5; i > 0; i--) {
+      $('#bowl').click();
+    };
+
+    expect('#first-1').toContainText('5');
+    expect('#second-1').toContainText('3');
+    expect('#first-2').toContainText('5');
+  });
+
+  it("displays individual frame totals", function(){
+    spyOn(Math, 'random').and.returnValue(0.5);
+    for (var i = 5; i > 0; i--) {
+      $('#bowl').click();
+    };
+
+    expect('#total-0').toContainText('8');
+    expect('#total-1').toContainText('8');
+    expect('#total-2').toContainText('5');
   })
+
+  it("displays current game total", function(){
+    spyOn(Math, 'random').and.returnValue(0.5);
+    for (var i = 5; i > 0; i--) {
+      $('#bowl').click();
+    };
+
+    expect('#game-total').toContainText('21');
+  });
+
+  describe("Last Frame", function(){
+
+    beforeEach(function(){
+      spyOn(Math, 'random').and.returnValue(0.8);
+      for (var i = 18; i > 0; i--) {
+        $('#bowl').click();
+      };
+
+    });
+
+    it("displays third roll", function(){
+      $('#bowl').click();
+      $('#bowl').click();
+      $('#bowl').click();
+      expect('#third-9').toContainText('8')
+    });
+
+  });
 
 
 });
--- src/interface/Bowlarama.js
+++ src/interface/Bowlarama.js
@@ -2,5 +2,55 @@ var game = new Game(Frame);
 
 $('#bowl').click(function(){
   game.bowl();
-  $('.frame#0').html(game.frameArray[0].firstRollScore)
+  scoreRefresh();
 });
+
+
+scoreRefresh = function(){
+  game.frameArray.forEach(function(frame, index) {
+    $('#first-' + String(index)).text(frame.firstRollScore);
+    $('#second-' + String(index)).text(frame.secondRollScore);
+    $('#total-' + String(index)).text(frame.totalScore);
+    if (index === 9) $('#third-9').text(frame.thirdRollScore);
+  });
+
+  var gameTotal = game.totalAllFrames();
+  $('#game-total').text(gameTotal);
+}
+
+
+createFrameTable = function(){
+  var htmlString = tableHeadingGenerator() + tableDataGenerator()
+  $('#frame-display').html(htmlString);
+};
+
+tableHeadingGenerator = function(){
+  var htmlString = '<table><tr>'
+  for (var i = 1; i <= 10; i++) {
+    var heading = '<th>Frame ' + String(i) + '</th>'
+    htmlString += heading
+  };
+  htmlString += '<th>Total Score</th></tr>'
+  return htmlString;
+};
+
+tableDataGenerator = function(){
+  var htmlString = '<tr>'
+  for (var i = 0; i <= 9; i++) {
+    var forLastFrame = "";
+    if (i === 9) forLastFrame = "<td id='third-9'></td>";
+
+    var data = "<td><table class='frame-table'><tr><td id='first-" + String(i) + "'></td><td id='second-" + String(i) + "'></td>" +
+      forLastFrame + "</tr>" +
+      "<tr><td>Total:</td><td id='total-" + String(i) + "'></td></tr></table></td>"
+
+
+    htmlString += data
+  };
+
+  htmlString += "<td><table><td id='game-total'></td></tr></table>"
+  return htmlString;
+};
+
+createFrameTable();
+

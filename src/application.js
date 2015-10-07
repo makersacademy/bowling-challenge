afb292da58dc38e6da35efa40eb835bd2d170ddf
Dom Batten <dominic.batten@googlemail.com>
2015-10-07 15:47:10 +0100

    Frame scores showing correctly for strikes. Strikes still not showing up in first score box

--- src/application.js
+++ src/application.js
@@ -10,8 +10,10 @@ game = new Game(Frame, LastFrame);
     $('#score_box_' + currentFrame + '_1').text(game.frames[currentFrame].firstRoll);
     if(game.currentFrame > 0) {
       $('#score_box_' + (currentFrame - 1) + '_2').text(game.frames[currentFrame -1].secondRoll);
-      $('#frame_score_' + (currentFrame - 1)).text(game.frameScores[currentFrame - 1])
     };
+    for(i = 0; i < currentFrame; i++) {
+      $('#frame_score_' + i).text(game.frameScores[i]);
+    }
     if(game.isGameOver()){
       $('#score_box').hide(500);
       $('#score_btn').show(500);

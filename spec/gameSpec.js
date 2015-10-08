10ab19b5edae3cb0916df0ea56b0e143252ea41a
chweeks <chrisharryweeks@gmail.com>
2015-10-08 15:47:58 +0100

    game updates score of player

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,11 +5,25 @@ describe('Game', function (){
     game = new Game;
   });
 
-  it('initiated on the first frame', function(){
-    expect(game.frame).toBe(1)
+  describe('Initializing', function(){
+    it('initiated on the first frame', function(){
+      expect(game.frame).toBe(1)
+    });
+
+    it('initiated with a score of 0', function(){
+      expect(game.score).toBe(0)
+    });
   });
 
-  it('initiated with a score of 0', function(){
-    expect(game.score).toBe(0)
+  describe('#updateScore', function(){
+
+    player = {downedPins: function() {
+                return 8 }
+             };
+
+    it('updates score based on players downed pins', function(){
+      game.updateScore(player);
+      expect(game.score).toBe(8)
+    });
   });
 });
--- src/game.js
+++ src/game.js
@@ -2,3 +2,7 @@ function Game() {
   this.frame = 1;
   this.score = 0
 };
+
+Game.prototype.updateScore = function(player){
+  this.score += (player.downedPins())
+};

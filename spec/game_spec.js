957623471bdbadc78ead6399a3cb8a051adc9df2
nathanielgreen <ngreen2303@gmail.com>
2015-10-08 15:44:32 +0100

    Added tests and prototypes for scoring and returning current score for 1 frame

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -4,6 +4,10 @@ describe ('game', function(){
     game = new Game();
   });
 
+  it('has 10 pins to start each frame', function(){
+    expect(game.pins).toEqual(10);
+  });
+
   it('starts on frame 1', function(){
     expect(game.frame).toEqual(1);
   });
@@ -28,9 +32,20 @@ describe ('game', function(){
     expect(game.frame).toEqual(2);
   });
 
-  it('has 10 pins to start each frame', function(){
-    expect(game.pins).toEqual(10);
+  it('has a limit of 10 frames', function(){
+    game.frame = 10
+    expect(game.moveFrame()).toEqual('Game Over')
   });
 
+  it('can add a score on a roll', function(){
+    game.scoreRoll(1);
+    game.scoreRoll(2);
+    expect(game.scores[1]).toEqual([1, 2]);
+  });
 
+  it('can return the current score', function(){
+    game.scoreRoll(1);
+    game.scoreRoll(2);
+    expect(game.currentScore(1)).toEqual(3);
+  });
 });
--- src/game.js
+++ src/game.js
@@ -2,11 +2,17 @@ function Game(){
   this.frame = 1;
   this.pins = 10;
   this.roll = 1;
+  this.scores = {1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[]};
 };
 
 Game.prototype.moveFrame = function(){
-  this.frame += 1;
   this.roll = 1;
+  if (this.frame === 10) {
+    return 'Game Over'
+  }
+  else {
+    this.frame += 1;
+  }
 };
 
 Game.prototype.moveRoll = function(){
@@ -14,4 +20,17 @@ Game.prototype.moveRoll = function(){
   if (this.roll === 3) {
     this.moveFrame();
   }
+};
+
+Game.prototype.scoreRoll = function(score){
+  this.scores[this.frame].push(score);
+  this.moveRoll();
+};
+
+Game.prototype.currentScore = function(frame){
+  var total = 0;
+  for (var i=0; i<this.scores[frame].length; i++) {
+    total += this.scores[frame][i];
+  };
+  return total;
 };

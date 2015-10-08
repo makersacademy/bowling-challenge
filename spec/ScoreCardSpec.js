b12c61d47240238c054a3c0fdaff9f4faf29c4cf
Emily Worrall <worrall.emily@gmail.com>
2015-10-08 12:18:37 +0100

    Has maximum 10 frames, can store a score for each frame but not individual rolls yet

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -18,5 +18,23 @@ describe('ScoreCard', function() {
       expect(scorecard.currentFrame).toEqual(2);
     });
 
-    
+    it('has a maximum of 10 frames', function() {
+      scorecard.currentFrame = 10;
+      expect(scorecard.nextFrame()).toEqual("End of game");
+    });
+
+    it('begins game on roll 1', function () {
+      expect(scorecard.roll).toEqual(1);
+    });
+
+    it('can move to the next roll', function () {
+      scorecard.nextRoll();
+      expect(scorecard.roll).toEqual(2);
+    });
+
+    it('can store a score for each roll', function() {
+      scorecard.rollScore(5);
+      expect(scorecard.score[1]).toEqual([5]);
+    });
+
 });
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,8 +1,18 @@
 function ScoreCard() {
   this.pins = 10;
   this.currentFrame = 1;
-}
+  this.roll = 1;
+  this.score = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: []}
+};
 
 ScoreCard.prototype.nextFrame = function() {
-  this.currentFrame += 1;
+    return this.currentFrame < 10 ? this.currentFrame += 1 : "End of game";
+};
+
+ScoreCard.prototype.nextRoll = function() {
+  return this.roll === 1 ? this.roll = 2 : this.roll = 1;
+};
+
+ScoreCard.prototype.rollScore = function(score) {
+  this.score[this.currentFrame].push(score);
 };

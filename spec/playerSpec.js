5f92cc7c417670c547f16b537aa912ea8526a3fe
chweeks <chrisharryweeks@gmail.com>
2015-10-08 14:52:02 +0100

    player initiated on first turn

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -1,1 +1,13 @@
+describe("Player", function() {
+  var player;
 
+  beforeEach(function() {
+
+    player = new Player();
+  });
+
+  it('is initiated on his first throw of frame', function(){
+    expect(player.turn).toBe(1)
+  });
+
+});

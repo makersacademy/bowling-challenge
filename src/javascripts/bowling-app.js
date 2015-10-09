703dd1cf5198f1e35ec6ff0c3aed17147d13f3b7
Harry <harryandrew.dix@gmail.com>
2015-10-09 16:18:26 +0100

    untested bowling logic updated

--- src/javascripts/bowling-app.js
+++ src/javascripts/bowling-app.js
@@ -4,19 +4,70 @@ function Game() {
   this.rolls = {1:[], 2:[], 3:[], 4:[], 5:[],
                 6:[], 7:[], 8:[], 9:[],10:[]};
   this.currentFrame = 1;
-  this.bonusManager = null;
-  this.bonusCounter = 0
+  this.lastFrameStrike = false;
+  this.lastFrameSpare = false;
+  this.isStrike = false
 };
 
 Game.prototype.roll = function(pins) {
+  this.bonusDistributor(pins)
   this.bonusChecker(pins);
   this.rolls[this.currentFrame].push(pins);
-  this.bonusDistributor(pins);
-  this.frameChecker(pins);
-  this.scoreUpdate();
+  this.scoreUpdate(pins);
+  this.frameHandler(pins)
 };
 
-Game.prototype.frameChecker = function(pins) {
+// --------------------------------------------------
+
+Game.prototype.bonusChecker = function(pins) {
+  if (pins === 10) {
+    this.isStrike = true
+    this.lastFrameStrike = true
+  };
+  var total = 0;
+    $.each(this.rolls[this.currentFrame],function() {
+    total += this
+  });
+  if (total === 10) {
+    this.lastFrameSpare = true
+  };
+  this.isStrike = false
+};
+
+// --------------------------------------------------
+
+Game.prototype.bonusDistributor = function(pins) {
+  // push to last frame if last frame was strike and make lastlast true
+  if(this.lastFrameStrike === true) { this.addToLast(pins) ; this.lastLastFrameStrike = true };
+  if(this.lastFrameStrike === false && this.lastLastFrameStrike === true) { this.addToLastAgain(pins) ; this.lastLastFrameStrike = false };
+  if(this.lastFrameSpare === true) { this.addToLast(pins) ; this.lastFrameSpare = false }
+};
+
+// --------------------------------------------------
+
+Game.prototype.addToLast = function(pins) {
+  this.rolls[this.currentFrame - 1].push(pins)
+};
+
+Game.prototype.addToLastAgain = function(pins) {
+  if (this.isStrike === true) {this.rolls[this.currentFrame - 2].push(pins)};
+  this.rolls[this.currentFrame - 1].push(pins)
+};
+
+// --------------------------------------------------
+
+Game.prototype.scoreUpdate = function(pins) {
+  this.score += pins
+  var sum = 0;
+  for (var k in this.rolls) {
+  vals = this.rolls[k];
+    for (var i=0; i<vals.length; i++) {
+      sum += vals[i] || 0;
+    }
+  }
+};
+
+Game.prototype.frameHandler = function(pins) {
   if (pins === 10 || this.frameOver === true) {
     this.currentFrame++; this.frameOver = !this.frameOver
   } else {
@@ -24,45 +75,4 @@ Game.prototype.frameChecker = function(pins) {
   }
 };
 
-Game.prototype.bonusChecker = function(pins) {
-  if (pins === 10) {
-    this.nextTwoBonus;
-    this.score +=10
-  };
-  var total = 0;
-    $.each(this.rolls[this.currentFrame],function() {
-    total += this
-  });
-  if (total === 10) {
-    this.nextOneBonus;
-    this.score += pins
-  }
-  this.score += pins
-};
-
-Game.prototype.nextTwoBonus = function() {
-  this.bonusCounter = 2;
-  this.bonusManager = this.rolls[this.currentFrame]
-};
-
-Game.prototype.nextOneBonus = function() {
-  this.bonusCounter = 1;
-  this.bonusManager = this.rolls[this.currentFrame]
-};
-
-Game.prototype.bonusDistributor = function(pins) {
-  if (this.bonusCounter != 0) {
-    this.bonusManager += pins;
-    this.bonusCounter--;
-  };
-};
-
-Game.prototype.scoreUpdate = function() {
-  var sum = 0;
-  for (var k in this.rolls) {
-    vals = this.rolls[k];
-  for (var i=0; i<vals.length; i++) {
-    sum += vals[i] || 0;
-  }
-  }
-};
+// --------------------------------------------------

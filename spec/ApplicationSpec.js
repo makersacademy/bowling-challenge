ae711ceef8a67f872dd04636ea15addf7e7d8da4
chn.challenger <chn.challenger@gmail.com>
2015-10-09 07:39:36 +0100

    fixed a bug where a frame score of 0 followed by 10 was counted as a strike

---
+++ spec/ApplicationSpec.js
@@ -0,0 +1,96 @@
+describe('Application of Bowling Score Updater', function() {
+
+  var bowling;
+
+  beforeEach(function(){
+    // bowling = new Bowling();
+    // jasmine.getStyleFixtures().fixturesPath = './public';
+    // loadStyleFixtures('stylesheet.css');
+    // jasmine.getFixtures().fixturesPath = '.';
+    // loadFixtures('index.html');
+
+    jasmine.getFixtures().fixturesPath = '.';
+    loadFixtures('index.html');
+  });
+
+  it('a title message', function(){
+    expect('title').toContainText("Joe's Bowling Scoresheet")
+  });
+
+  it('buttons to enter pins scored', function(){
+    for (var i = 0; i <= 10; i ++){
+      expect('#btn' + i).toContain(i);
+    };
+  });
+
+});
+//
+// describe('Interface knows the cumulative score for', function(){
+//
+//   var bowling;
+//
+//   beforeEach(function(){
+//     bowling = new Bowling();
+//     jasmine.getStyleFixtures().fixturesPath = './public';
+//     loadStyleFixtures('stylesheet.css');
+//     jasmine.getFixtures().fixturesPath = '.';
+//     loadFixtures('home.html');
+//     $.holdReady(false);
+//   });
+//
+//   function helper(rolls, score){
+//     for (var i = 0; i < rolls; i ++){
+//       $("#btn" + score).click();
+//     };
+//   };
+//
+//   it('two non-spare/non-strike rolls', function(){
+//     $("#btn5").click();
+//     $("#btn2").click();
+//     expect("#f10score").toContainText('7');
+//   });
+//
+//   it('a strike and a spare', function(){
+//     helper(1, 10);
+//     helper(2, 5);
+//     expect("#f10score").toContainText('30');
+//   });
+//
+//   it('an unremarkable game', function(){
+//     helper(20, 4);
+//     expect("#f10score").toContainText('80');
+//   });
+//
+//   it('a perfect game', function(){
+//     helper(12, 10);
+//     expect("#f10score").toContainText('300');
+//   });
+//
+//   it('a gutter game', function(){
+//     helper(20, 0);
+//     expect("#f10score").toContainText('0');
+//   });
+//
+//   it('an example game', function(){
+//     $("#btn1").click();
+//     $("#btn4").click();
+//     $("#btn4").click();
+//     $("#btn5").click();
+//     $("#btn6").click();
+//     $("#btn4").click();
+//     $("#btn5").click();
+//     $("#btn5").click();
+//     $("#btn10").click();
+//     $("#btn0").click();
+//     $("#btn1").click();
+//     $("#btn7").click();
+//     $("#btn3").click();
+//     $("#btn6").click();
+//     $("#btn4").click();
+//     $("#btn10").click();
+//     $("#btn2").click();
+//     $("#btn8").click();
+//     $("#btn6").click();
+//     expect("#f10score").toContainText('133');
+//   });
+// });
--- src/Application.js
+++ src/Application.js
@@ -1,12 +1,6 @@
-//deal with invalid input
-//total at the end
-//round at the end does not work when there are 3 x 10s
-
 updater = new BowlingScoreUpdater();
 
 var htmlRoundNumber = 1;
-
-
 
 var htmlWriter = function(score){
 
@@ -14,27 +8,17 @@ var htmlWriter = function(score){
     return;
   };
 
-  updater.newRound(score);
-
-  if (score === 10){
+  if (score === 10 && updater.frameRoundsLeft === 0){
     if (htmlRoundNumber < 19) {htmlRoundNumber += 1};
     document.getElementById('round'+ htmlRoundNumber.toString()).innerHTML = 'X';
-  } else if (updater.currentFrameTotal === 10 && updater.frameRoundsLeft === 0) {
+  } else if (updater.currentFrameTotal + score === 10 && updater.frameRoundsLeft === 1) {
     document.getElementById('round'+ htmlRoundNumber.toString()).innerHTML = '/';
   } else {
     document.getElementById('round'+ htmlRoundNumber.toString()).innerHTML = score;
   };
   htmlRoundNumber += 1;
 
-  // if (score === 10){
-  //   htmlRoundNumber += 1;
-  //   document.getElementById('round'+ updater.roundNumber.toString()).innerHTML = 'X';
-  // } else if (updater.currentFrameTotal === 10 && updater.frameRoundsLeft === 0) {
-  //   document.getElementById('round'+ updater.roundNumber.toString()).innerHTML = '/';
-  // } else {
-  //   document.getElementById('round'+ updater.roundNumber.toString()).innerHTML = score;
-  // };
-  // htmlRoundNumber += 1;
+  updater.newRound(score);
 
   var currentFrameNumber = updater.currentFrameNumber;
   var prevFrameNumber = updater.currentFrameNumber - 1;
@@ -55,80 +39,18 @@ var htmlWriter = function(score){
       = updater.prevPrevFrameTotal;
   };
 
-  // document.getElementById('total_score').innerHTML = updater.GameTotal;
-
   var scoreTotal = 0;
-
   for(i = 1; i < 11; i++){
     scoreTotal += Number(document.getElementById('f' + i.toString()).innerHTML);
   };
-
   document.getElementById('total_score').innerHTML = scoreTotal;
 
 };
 
-
-
 document.addEventListener('DOMContentLoaded', function() {
-
-  // console.log(document.getElementById('f1').innerHTML);
-
   for(i = 0; i < 11; i++){
     document.getElementById('btn' + i.toString()).addEventListener("click", function() {
       htmlWriter(Number(this.id.replace('btn', '')));
     });
   };
 });
-
-
-
-
-
-//
-// document.addEventListener('DOMContentLoaded', function() {
-//
-//   document.getElementById("btn0").addEventListener("click", function() {
-//     alert("Button clicked, id "+this.id+", text"+this.innerHTML);
-//     htmlWriter(0);
-//   });
-//
-//   document.getElementById("btn1").addEventListener("click", function() {
-//     htmlWriter(1);
-//   });
-//
-//   document.getElementById("btn2").addEventListener("click", function() {
-//     htmlWriter(Number(this.id.replace('btn', '')));
-//   });
-//
-//   document.getElementById("btn3").addEventListener("click", function() {
-//     htmlWriter(3);
-//   });
-//
-//   document.getElementById("btn4").addEventListener("click", function() {
-//     htmlWriter(4);
-//   });
-//
-//   document.getElementById("btn5").addEventListener("click", function() {
-//     htmlWriter(5);
-//   });
-//
-//   document.getElementById("btn6").addEventListener("click", function() {
-//     htmlWriter(6);
-//   });
-//
-//   document.getElementById("btn7").addEventListener("click", function() {
-//     htmlWriter(7);
-//   });
-//
-//   document.getElementById("btn8").addEventListener("click", function() {
-//     htmlWriter(8);
-//   });
-//
-//   document.getElementById("btn9").addEventListener("click", function() {
-//     htmlWriter(9);
-//   });
-//
-//   document.getElementById("btn10").addEventListener("click", function() {
-//     htmlWriter(10);
-//   });
-// });

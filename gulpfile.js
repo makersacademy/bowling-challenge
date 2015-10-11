fa37822101a0974a892d21bffed54f7e769612fd
Harry <harryandrew.dix@gmail.com>
2015-10-12 00:46:45 +0100

    tweaks to gulpfile, imports module

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
- 
+var Game = require("./src/javascripts/bowling-app");
+
 gulp.task('default', function () {
     return gulp.src('lib/spec/bowlingSpec.js')
         .pipe(jasmine());

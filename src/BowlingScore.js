a2fea80592a69062f89fdf7472cc7521e9ed043b
chn.challenger <chn.challenger@gmail.com>
2015-10-11 23:49:50 +0100

    CI file movement

--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -1,22 +1,22 @@
-function BowlingScore() {
-  this.rawScores = [];
-  // this.frameScores = {f1:[],f2:[],f3:[],f4:[],f5:[],f6:[],f7:[],f8:[],f9:[],f10:[]};
-};
-
-BowlingScore.prototype.addNewRoundScore = function(score) {
-  this.rawScores.push(score);
-};
-
-BowlingScore.prototype.makeFrameScores = function() {
-  this.frameScores = [];
-  for(i = 0; i < this.rawScores.length; i+=1) {
-    var currentFrame = [this.rawScores[i]];
-    // currentFrame.push(this.rawScores[i]);
-    this.frameScores.push(currentFrame);
-    console.log("finish");
-  };
-
-};
+// function BowlingScore() {
+//   this.rawScores = [];
+//   // this.frameScores = {f1:[],f2:[],f3:[],f4:[],f5:[],f6:[],f7:[],f8:[],f9:[],f10:[]};
+// };
+//
+// BowlingScore.prototype.addNewRoundScore = function(score) {
+//   this.rawScores.push(score);
+// };
+//
+// BowlingScore.prototype.makeFrameScores = function() {
+//   this.frameScores = [];
+//   for(i = 0; i < this.rawScores.length; i+=1) {
+//     var currentFrame = [this.rawScores[i]];
+//     // currentFrame.push(this.rawScores[i]);
+//     this.frameScores.push(currentFrame);
+//     console.log("finish");
+//   };
+//
+// };
 //
 // [2,4,2,3,6,2]
 //

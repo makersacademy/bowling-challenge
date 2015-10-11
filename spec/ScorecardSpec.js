d4db87ab46cd418f9eb1fd07073148d1b28423e5
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-10-11 16:11:55 +0100

    Can update frameScore and calc totalScore

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -5,8 +5,22 @@ describe ('Scorecard', function(){
     scorecard = new Scorecard();
   });
 
-  it ('logs score of each roll', function(){
+  it ('can log roll score', function(){
     scorecard.logScore(1);
     expect(scorecard.rollScore[0]).toEqual(1);
+  });
+
+  it ('can update frame score, no strike or spare', function(){
+    scorecard.logScore(1);
+    scorecard.logScore(2);
+    scorecard.updateFrameScore();
+    expect(scorecard.frameScore[0]).toEqual(3);
+  });
+
+  it ('can calculate total score', function(){
+    scorecard.logScore(1);
+    scorecard.logScore(2);
+    scorecard.updateFrameScore();
+    expect(scorecard.calculateTotalScore()).toEqual(3);
   });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,7 +1,23 @@
 var Scorecard = function(){
   this.rollScore = [];
+  this.frameScore = [];
 };
 
 Scorecard.prototype.logScore = function(score){
   this.rollScore.push(score);
+};
+
+Scorecard.prototype.updateFrameScore = function(){
+  var numScore = this.rollScore.length;
+  var firstScore = this.rollScore.slice(numScore-1)[0];
+  var secondScore = this.rollScore.slice(numScore-2)[0];
+  this.frameScore.push(firstScore + secondScore);
+};
+
+Scorecard.prototype.calculateTotalScore = function(){
+  var totalScore = 0
+  this.frameScore.forEach(function(i){
+    totalScore += i;
+  });
+  return totalScore;
 };

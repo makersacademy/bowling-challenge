68b6d1c7218c155de736c13302cf290a84b61d91
philip <philip.clarke@redhood.co.uk>
2015-10-11 11:43:56 +0100

    reworked game logic

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,36 +1,163 @@
 describe('Game', function() {
-  var frameCount = 0;
-  var totalGameScore = 0;
+  // var rolls = [];
+  // var currentRoll = 0;
 
   beforeEach(function() {
     game = new Game();
-    frame = new Frame();
   });
 
   it('allows you to add a score to a game', function() {
-    game.score(5, 5);
-    expect(game.totalGameScore).toEqual(10);
+    game.roll(5);
+    expect(game._totalGameScore()).toEqual(5);
+
   });
 
-  it('keeps a track of the total score', function() {
-    game.score(5, 5);
-    frame = new Frame();
-    game.score(5, 5);
-    expect(game.totalGameScore).toEqual(20);
+  it('gives you the total current score for the game without strikes', function() {
+    game.roll(5);
+    game.roll(9);
+    expect(game._totalGameScore()).toEqual(14);
+
   });
 
-  it('counts the number of frames', function() {
-    game.score(5, 5);
-    frame = new Frame;
-    game.score(5, 5);
-    expect(game.frameCount).toEqual(2);
+  it('tells you the current roll', function() {
+    game.roll(5);
+    game.roll(9);
+    game.roll(5);
+    game.roll(9);
+    game.roll(5);
+    expect(game.currentGameRoll()).toEqual(5);
+
   });
 
-  it('adds the sum of the current frame as a bonus if the prevous frame was a strike', function() {
-    game.score(10, 0);
-    frame = new Frame;
-    game.score(1, 2);
-    expect(game.totalGameScore).toEqual(16);
+  it('tells you the current frame', function() {
+    game.roll(5);
+    game.roll(9);
+    game.roll(5);
+    game.roll(9);
+    game.roll(5);
+    expect(game.currentGameFrame()).toEqual(3);
+  });
+
+  it('adds the bonus for a strike at the end of the following frame', function() {
+    game.roll(10);
+    game.roll(0);
+    game.roll(8);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(28);
+  });
+
+  it('gives the score for a spare in the first frame when no other rolls have occured', function() {
+    game.roll(5);
+    game.roll(5);
+    expect(game._totalGameScore()).toEqual(10);
+  });
+
+  it('adds the bonus for a spare in first frame', function() {
+    game.roll(5);
+    game.roll(5);
+    game.roll(5);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(21);
+  });
+
+  it('adds the bonus for a strike followed by a spare', function() {
+    game.roll(10);
+    game.roll(0);
+    game.roll(5);
+    game.roll(5);
+    game.roll(5);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(41);
+  });
+
+  it('does not add a strike bonus if 10 is scored in the second roll of a frame', function() {
+    game.roll(0);
+    game.roll(0);
+    game.roll(0);
+    game.roll(10);
+    game.roll(1);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(13);
+  });
+
+  it('adds the bonus for two stikes in a row', function() {
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(1);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(44);
+  });
+
+  it('adds the bonus for three stikes in a row', function() {
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(1);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(74);
+  });
+
+  it('adds the bonus for a spare at the end of the following frame', function() {
+    game.roll(2);
+    game.roll(8);
+    game.roll(8);
+    game.roll(1);
+    expect(game._totalGameScore()).toEqual(27);
+  });
+
+  it('calculates the a game up to the last frame with perfect scores', function() {
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    expect(game._totalGameScore()).toEqual(280);
+  });
+
+  it('calculates the perfect game', function() {
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game.roll(10);
+    game.roll(0);
+    game._lastRoll(10);
+    game._lastRoll(10);
+    expect(game._grandTotal()).toEqual(300);
   });
 
 });

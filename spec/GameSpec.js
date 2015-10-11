cbb44521cf4c4a573366ff598c595460001c4c01
zsid <zhivko.siderov@gmail.com>
2015-10-11 11:16:44 +0100

    Strike Bonus function done

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -17,53 +17,96 @@ describe('Game', function() {
     });
 
     it('has score card', function() {
-      var scoreboard = {1: [], 2: [], 3: [], 4: [], 5: [],
+      var scorecard = {1: [], 2: [], 3: [], 4: [], 5: [],
                      6: [], 7: [], 8: [], 9: [], 10: []};
-    expect(game.scorecard).toEqual(scoreboard);
+    expect(game.scorecard).toEqual(scorecard);
     });
+
   });
+
 
   describe('should', function() {
 
+    beforeEach(function() {
+      game.throwBall(5);
+      game.throwBall(4);
+    });
+
     it('be able to throw a ball', function() {
-      expect(game.throwBall(5)).toEqual(5);
+      expect(game.throwBall(8)).toEqual(8);
     });
 
     it('record a throw', function() {
-      game.throwBall(5);
-      expect(game.scorecard[game.frame]).toEqual([5]);
+      expect(game.scorecard[1]).toEqual([5, 4]);
     });
 
     it('record two throws', function() {
-      game.throwBall(5);
-      game.throwBall(4);
       expect(game.scorecard[1]).toEqual([5, 4]);
     });
 
     it('change roll turn after a throw', function() {
-      game.throwBall(5);
+      game.throwBall(7);
       expect(game.rollTurn).toBe(2);
     });
 
     it('reset roll turn to 1 after two throws', function() {
-      game.throwBall(5);
-      game.throwBall(4);
       expect(game.rollTurn).toBe(1);
     });
 
     it('change frame after two throws', function() {
-      game.throwBall(5);
-      game.throwBall(4);
       expect(game.frame).toBe(2);
     });
 
     it('record a strike', function() {
       game.throwBall(10);
-      expect(game.scorecard[1]).toEqual([10])
+      expect(game.scorecard[2]).toEqual([10])
       expect(game.rollTurn).toBe(1);
-      expect(game.frame).toBe(2);
+      expect(game.frame).toBe(3);
+    });
+
+    it('give a strike bonus', function() {
+      game.throwBall(10);
+      game.throwBall(5);
+      game.throwBall(1);
     });
 
   });
+
+
+  describe('calculates', function() {
+    beforeEach(function() {
+      game.throwBall(5);
+      game.throwBall(4);
+    });
+
+    it('a frame\'s score', function() {
+      expect(game._frameScore(1)).toEqual(9);
+    });
+
+    it('total score after a completed roll', function() {
+      expect(game.totalScore()).toEqual(9);
+    });
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,7 @@ var Game = function() {
   this.frame = 1
   this.rollTurn = 1
   this.scorecard = {1: [], 2: [], 3: [], 4: [], 5: [],
-                 6: [], 7: [], 8: [], 9: [], 10: []}
+                    6: [], 7: [], 8: [], 9: [], 10: []}
 };
 
 Game.prototype.throwBall = function(pinsDown) {
@@ -37,4 +37,31 @@ Game.prototype._strike = function() {
   this.scorecard[this.frame].push(10)
   this._nextFrame();
   return 'X'
+};
+
+Game.prototype._isPreviousStrike = function() {
+  return this.scorecard[this.frame - 1][0] === 10
+};
+
+Game.prototype._strikeBonus = function() {
+  if (this._isPreviousStrike) {
+    this.scorecard[this.frame - 1].push(this._frameScore[this.frame])
+  }
+};
+
+
+
+
+Game.prototype._frameScore = function(frame) {
+  return this.scorecard[frame].reduce(function(sum, element) {
+    return sum += element });
+};
+
+//Calcs probalem <=, cannot see score half way through the roll.
+Game.prototype.totalScore = function() {
+  var totalScore = 0;
+  for (i = 1; i < this.frame; i++ ) {
+    totalScore += this._frameScore(i);
+  }
+  return totalScore;
 };

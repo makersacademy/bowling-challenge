c6633881f4a6eeeb263fc94742463a92923b7258
Harry <harryandrew.dix@gmail.com>
2015-10-12 01:34:45 +0100

    installs coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var jasmine = require('gulp-jasmine-phantom');
-var Game = require("./src/javascripts/bowling-app");
 
 gulp.task('default', function () {
     return gulp.src('lib/spec/bowlingSpec.js')

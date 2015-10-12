70a0fbf3b1689e2e17872415c3fab97fcce6769f
chn.challenger <chn.challenger@gmail.com>
2015-10-12 23:26:24 +0100

    started second method of calculating scores from scatch

--- spec/BowlingScoreSpec.js
+++ spec/BowlingScoreSpec.js
@@ -1,35 +1,43 @@
-// var game = new BowlingScore();
-//
-// describe("BowlingScore",function(){
-//
-//
-//
-//   describe("addNewRoundScore",function(){
-//
-//
-//     it("correctly push scores into rawScore array",function(){
-//
-//       game.addNewRoundScore(6);
-//       game.addNewRoundScore(3);
-//       expect(game.rawScores).toEqual([6,3]);
-//     });
-//
-//     it("correctly push scores into rawScore array",function(){
-//       game.addNewRoundScore(7);
-//       game.addNewRoundScore(2);
-//       game.addNewRoundScore(4);
-//       game.addNewRoundScore(5);
-//       expect(game.rawScores).toEqual([6,3,7,2,4,5]);
-//     });
-//   });
-//
-//   describe("frameScoresArray",function(){
-//     it("restructures rawScores into frameScoresArray",function(){
-//         game.makeFrameScores();
-//         expect(game.frameScores).toEqual([[6,3],[7,2],[4,5]]);
-//     });
-//
-//   });
-//
-//
-// });
+var game = new BowlingScore();
+
+describe("BowlingScore",function(){
+
+  describe("addNewRoundScore",function(){
+    it("correctly push scores into rawScore array with 2 rounds",function(){
+      game.addRoundToRawScores(1);
+      game.addRoundToRawScores(9);
+      expect(game.rawScores).toEqual([1,9]);
+    });
+
+    it("correctly push scores into rawScore array with 6 rounds",function(){
+      game.addRoundToRawScores(8);
+      game.addRoundToRawScores(2);
+      game.addRoundToRawScores(3);
+      game.addRoundToRawScores(1);
+      expect(game.rawScores).toEqual([1,9,8,2,3,1]);
+    });
+  });
+
+  describe("createFrameScores",function(){
+    it("creates FrameScores array of frame arrays",function(){
+      game.createFrameScores();
+      expect(game.frameScores).toEqual([[1,9],[8,2],[3,1]]);
+    });
+
+    it("add a frame with first round of 0 and second of 10",function(){
+      game.addRoundToRawScores(0);
+      game.addRoundToRawScores(10);
+      game.createFrameScores();
+      expect(game.frameScores).toEqual([[1,9],[8,2],[3,1],[0,10]]);
+    });
+
+    it("add a frame with first round of 10 and no second round",function(){
+      game.addRoundToRawScores(10);
+      game.addRoundToRawScores(2);
+      game.addRoundToRawScores(3);
+      game.createFrameScores();
+      expect(game.frameScores).toEqual([[1,9],[8,2],[3,1],[0,10],[10],[2,3]]);
+    });
+  });
+
+});
--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -1,34 +1,26 @@
-// function BowlingScore() {
-//   this.rawScores = [];
-//   this.frameScores = {f1:[],f2:[],f3:[],f4:[],f5:[],f6:[],f7:[],f8:[],f9:[],f10:[]};
-//   this.frameBonuses = {f1:0,f2:0,f3:0,f4:0,f5:0,f6:0,f7:0,f8:0,f9:0,f10:0};
-//   this.frameTotals = {f1:0,f2:0,f3:0,f4:0,f5:0,f6:0,f7:0,f8:0,f9:0,f10:0};
-// };
-//
-// //[2,3,4,5,6,7,7,7,2,3]
-//
-// BowlingScore.prototype.addNewRoundScore = function(score) {
-//   this.rawScores.push(score);
-// };
-//
-// BowlingScore.prototype.makeFrameScores = function() {
-//   this.frameScores = {};
-//   var j = 1;
-//   for(i = 0; i < this.rawScores.length; i+=1) {
-//     if (this.frameScores) {
-//
-//     } else {
-//
-//     };
-//
-//
-//     var currentFrame = [this.rawScores[i]];
-//     // currentFrame.push(this.rawScores[i]);
-//     this.frameScores.push(currentFrame);
-//     console.log("finish");
-//   };
-
+function BowlingScore() {
+  this.rawScores = [];
 };
+
+//[2,3,4,5,6,7,7,7,2,3]
+
+BowlingScore.prototype.addRoundToRawScores = function(score) {
+  this.rawScores.push(score);
+};
+
+BowlingScore.prototype.createFrameScores = function() {
+  this.frameScores = [];
+  if (this.frameScores.length === 0) {this.frameScores.push([this.rawScores[0]])};
+  for(i = 1; i < this.rawScores.length; i+=1) {
+    if ((this.frameScores[this.frameScores.length - 1]).length === 1 && this.frameScores[this.frameScores.length - 1][0] !== 10) {
+      (this.frameScores[this.frameScores.length - 1]).push(this.rawScores[i]);
+    } else {
+      this.frameScores.push([this.rawScores[i]]);
+    };
+  };
+};
+
+
 //
 // [2,4,2,3,6,2]
 //

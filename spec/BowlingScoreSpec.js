1a19900f221366c4373d51d4bfe74bcd13c81232
chn.challenger <chn.challenger@gmail.com>
2015-10-13 16:20:22 +0100

    edit

--- spec/BowlingScoreSpec.js
+++ spec/BowlingScoreSpec.js
@@ -1,15 +1,15 @@
 var game = new BowlingScore();
 
-describe("BowlingScore",function(){
+describe("BowlingScore",function() {
 
-  describe("addNewRoundScore",function(){
-    it("correctly push scores into rawScore array with 2 rounds",function(){
+  describe("addNewRoundScore",function() {
+    it("correctly push scores into rawScore array with 2 rounds",function() {
       game.addRoundToRawScores(1);
       game.addRoundToRawScores(9);
       expect(game.rawScores).toEqual([1,9]);
     });
 
-    it("correctly push scores into rawScore array with 6 rounds",function(){
+    it("correctly push scores into rawScore array with 6 rounds",function() {
       game.addRoundToRawScores(8);
       game.addRoundToRawScores(2);
       game.addRoundToRawScores(3);
@@ -18,20 +18,20 @@ describe("BowlingScore",function(){
     });
   });
 
-  describe("createFrameScores",function(){
-    it("creates FrameScores array of frame arrays",function(){
+  describe("createFrameScores",function() {
+    it("creates FrameScores array of frame arrays",function() {
       game.createFrameScores();
       expect(game.frameScores).toEqual([[1,9],[8,2],[3,1]]);
     });
 
-    it("add a frame with first round of 0 and second of 10",function(){
+    it("add a frame with first round of 0 and second of 10",function() {
       game.addRoundToRawScores(0);
       game.addRoundToRawScores(10);
       game.createFrameScores();
       expect(game.frameScores).toEqual([[1,9],[8,2],[3,1],[0,10]]);
     });
 
-    it("add a frame with first round of 10 and no second round",function(){
+    it("add a frame with first round of 10 and no second round",function() {
       game.addRoundToRawScores(10);
       game.addRoundToRawScores(2);
       game.addRoundToRawScores(3);
@@ -39,5 +39,72 @@ describe("BowlingScore",function(){
       expect(game.frameScores).toEqual([[1,9],[8,2],[3,1],[0,10],[10],[2,3]]);
     });
   });
+
+  describe("createFrameBonus",function() {
+    it("computes frame bonuses as an array of bonuses",function() {
+      game.createFrameBonus();
+      expect(game.frameBonus).toEqual([8,3,0,10,5,0]);
+    });
+
+    it("computes frame bonuses for upto 9th frame",function() {
+      game.addRoundToRawScores(10);
+      game.addRoundToRawScores(10);
+      game.addRoundToRawScores(10);
+      game.createFrameScores();
+      game.createFrameBonus();
+      expect(game.frameBonus).toEqual([8,3,0,10,5,0,20,10,0]);
+    });
+
+    it("computes frame bonuses also for 10th frame",function() {
+      game.addRoundToRawScores(10);
+      game.addRoundToRawScores(10);
+      game.addRoundToRawScores(10);
+      game.createFrameScores();
+      game.createFrameBonus();
+      expect(game.frameBonus).toEqual([8,3,0,10,5,0,20,20,20,20,10,0]);
+    });
+  });
+
+  describe("createFrameTotals",function() {
+    it("computes totals for each frame",function() {
+      game.createFrameTotals();
+      expect(game.frameTotals).toEqual([ 18, 13, 4, 20, 15, 5, 30, 30, 30, 30, 20, 10 ]);
+    });
+  });
+
+  describe("createGameTotal",function() {
+    it("computes total for the game so far",function() {
+      game.createGameTotal();
+      expect(game.gameTotal).toEqual(195);
+    });
+  });
+
+  // describe("newRound", function() {
+  //   var newGame = new BowlingScore();
+  //   newGame.newRound(9);
+  //   newGame.newRound(1);
+  //   newGame.newRound(2);
+  //   newGame.newRound(3);
+  //   newGame.newRound(10);
+  //   newGame.newRound(0);
+  //   newGame.newRound(10);
+  //   newGame.newRound(10);
+  //   newGame.newRound(3);
+  //   it("computes frame scores", function() {
+  //
+  //   });
+  //
+  //   it("computes frame bonuses", function() {
+  //
+  //   });
+  //
+  //   it("computes frame totals", function() {
+  //
+  //   });
+  //
+  //   it("computes game total", function() {
+  //
+  //   });
+  // });
 
 });
--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -2,8 +2,6 @@ function BowlingScore() {
   this.rawScores = [];
 };
 
-//[2,3,4,5,6,7,7,7,2,3]
-
 BowlingScore.prototype.addRoundToRawScores = function(score) {
   this.rawScores.push(score);
 };
@@ -20,6 +18,50 @@ BowlingScore.prototype.createFrameScores = function() {
   };
 };
 
+BowlingScore.prototype.createFrameBonus = function() {
+  this.frameBonus = [];
+  for(i = 0; i < this.frameScores.length; i+=1) {
+    if (this.frameScores[i+1] === undefined) {
+      var bonus_pool = [0,0];
+    } else if (this.frameScores[i+2] === undefined) {
+      var bonus_pool = [this.frameScores[i+1],0];
+      bonus_pool = [].concat.apply([], bonus_pool);
+    } else {
+      var bonus_pool = [].concat.apply([],[this.frameScores[i+1],this.frameScores[i+2]]);
+    };
+    if (this.frameScores[i][0] === 10) {
+      this.frameBonus.push(bonus_pool[0] + bonus_pool[1]);
+    } else if (this.frameScores[i][0] + this.frameScores[i][1] === 10) {
+      this.frameBonus.push(bonus_pool[0]);
+    } else {
+      this.frameBonus.push(0);
+    };
+  };
+};
+
+BowlingScore.prototype.createFrameTotals = function() {
+  this.frameTotals = [];
+  for(i = 0; i < this.frameScores.length; i+=1) {
+    var total = [].concat.apply([], [this.frameScores[i],this.frameBonus[i]]);
+    this.frameTotals.push(total.reduce(function(a, b) {return a + b;}));
+  };
+};
+
+BowlingScore.prototype.createGameTotal = function() {
+  this.gameTotal = 0;
+  if (this.frameTotals.length < 10) {var limit = this.frameTotals.length} else {var limit = 10};
+  for(i = 0; i < limit; i+=1) {
+    this.gameTotal += this.frameTotals[i];
+  };
+};
+
+BowlingScore.prototype.newRound = function(score) {
+  this.addRoundToRawScores(score);
+  this.createFrameScores();
+  this.createFrameBonus();
+  this.createFrameTotals();
+  this.createGameTotal();
+};
 
 //
 // [2,4,2,3,6,2]

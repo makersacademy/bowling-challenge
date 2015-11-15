d2124db8635116cb970c5a39b7b5adca28ff9f6d
Mari-Ann Meling <mariann013@gmail.com>
2015-11-15 14:54:32 +0000

    can roll the ball twice

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,13 +1,25 @@
 describe("Frame", function() {
 
   var pins;
+  var rolls;
 
   beforeEach( function() {
-  frame = new Frame();
-});
-
-  it("each frame should have 10 pins", function() {
-    expect(frame.pins).toEqual(10);
+  newFrame = new Frame();
   });
 
+
+    it("each frame should have 10 pins", function() {
+      expect(newFrame.pins).toEqual(10);
+    });
+
+    it("should let you roll the ball and reduce the number of rolls", function() {
+      newFrame.roll();
+      expect(newFrame.rolls).toEqual(1);
+    });
+
+    it("should let you roll the ball twice", function() {
+      newFrame.roll();
+      newFrame.roll();
+      expect(function(){ newFrame.roll(); }).toThrow("Can not roll ball, frame is over!");
+    });
 })
--- src/Frame.js
+++ src/Frame.js
@@ -1,4 +1,14 @@
 function Frame() {
 
   this.pins = 10;
+  this.rolls = 2;
+
+};
+
+Frame.prototype.roll = function () {
+  if (this.rolls > 0) {
+    this.rolls -= 1;
+  } else {
+    throw "Can not roll ball, frame is over!";
+  };
 }

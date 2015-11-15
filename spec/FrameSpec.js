f580d6b585d1a0ea1ec009a018feff1a8fe1244b
Mari-Ann Meling <mariann013@gmail.com>
2015-11-15 14:13:01 +0000

    each frame has 10 pins

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,12 +1,13 @@
-describe("Frame", function(){
+describe("Frame", function() {
 
   var pins;
 
   beforeEach( function() {
   frame = new Frame();
-)};
+});
 
   it("each frame should have 10 pins", function() {
     expect(frame.pins).toEqual(10);
   });
+
 })

0b84a690ef07f06276886cea24d57b7b57772e93
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-17 13:27:45 +0100

    gameclass

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,60 +1,50 @@
 $(document).ready(function() {
 	var game = new Game();
-	var pin = new Pin();
-	$('#frame1').text(game.rollsArray[0,1]);
-	$('#frame2').text(game.rollsArray[2,3]);
 
 	$('#zero').click(function() {
-		pin.pinsDown(game.rollBall(0));
+		game.rollBall(0);
 	});
 
 	$('#one').click(function() {
-		pin.pinsDown(game.rollBall(1));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(1);
 	});
 
 	$('#two').click(function() {
-		pin.pinsDown(game.rollBall(2));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(2);
 	});
 
 	$('#three').click(function() {
-		pin.pinsDown(game.rollBall(3));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(3);
 	});
 
 	$('#four').click(function() {
-		pin.pinsDown(game.rollBall(4));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(4);
 	});
 
 	$('#five').click(function() {
-		pin.pinsDown(game.rollBall(5));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(5);
 	});
 
 	$('#six').click(function() {
-		pin.pinsDown(game.rollBall(6));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(6);
 	});
 
   $('#seven').click(function() {
-		pin.pinsDown(game.rollBall(7));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(7);
 	});
 
 	$('#eigth').click(function() {
-		pin.pinsDown(game.rollBall(8));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(8);
 	});
 
 	$('#nine').click(function() {
-		pin.pinsDown(game.rollBall(9));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(9);
 	});
 
 	$('#ten').click(function() {
-		pin.pinsDown(game.rollBall(10));
-		$('#pins').text(game.rollsArray);
+		game.rollBall(10);
 	});
+
+	$('#frame1').text(game.rollsArray[0,1]);
+	$('#frame2').text(game.rollsArray[2,3]);
 });

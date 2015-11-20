a145aaaee727b79062c4d104a132fe5691a007e0
samover <samuel.overloop@gmail.com>
2015-11-20 15:07:46 +0000

    game registers a roll

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -3,5 +3,12 @@ describe('Game', function() {
 
   beforeEach(function() {
     game = new Game();
+    game.roll(3);
   })
+
+  describe('#roll', function() {
+    it('registers a roll', function() {
+      expect(game.gameRolls[0]).toEqual(3);
+    });
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,3 +1,7 @@
 function Game() {
-  
+  this.gameRolls = [];
+}
+
+Game.prototype.roll = function(roll) {
+  this.gameRolls.push(roll);
 }

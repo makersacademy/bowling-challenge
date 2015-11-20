229ab5e48c4a21de3e0832606de01bb6a8f6762a
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-20 11:47:41 +0100

    spare

--- src/game.js
+++ src/game.js
@@ -3,17 +3,24 @@ function Game() {
 	this.secondRoll = false;
 	this.currentScore = [];
 	this.halfStrike = false;
+	this.bonus = 0;
 };
 
 Game.prototype.rollBall = function(pinsDown) {
 	if (pinsDown === 10) {
-		this.rollsArray.push(pinsDown, 0);
+			this.rollsArray.push(pinsDown, 0);
+	} else if (this.halfStrike) {
+			this.currentScore.splice(-1, 1, parseInt(this.currentScore.slice(-1), 10) + pinsDown);
+			this.bonus += pinsDown;
+			this.rollsArray.push(pinsDown);
 	} else {
-		this.rollsArray.push(pinsDown);
+			this.rollsArray.push(pinsDown);
 	}
 	console.log(this.rollsArray);
-	this.spare();
 	console.log(this.spare());
+	console.log(this.currentScore);
+
+	this.spare();
 	this.isSecondRoll();
 	this.score();
 	return pinsDown;
@@ -28,16 +35,16 @@ Game.prototype.isSecondRoll = function() {
 };
 
 Game.prototype.score = function() {
-	
+
 	if (this.secondRoll) {
-		return this.currentScore.push(this.rollsArray.reduce(function(a,b){return a+b;}));
+		return this.currentScore.push(this.bonus + this.rollsArray.reduce(function(a,b){return a+b;}));
 	} else {
 		return this.currentScore;
 	}
 };
 
 Game.prototype.spare = function() {
-	if (this.rollsArray.length % 2 === 0 && this.rollsArray.slice(-2).reduce(function(a,b){return a+b;}) === 10) {
+	if (this.rollsArray.length % 2 === 0 && this.rollsArray.slice(-2).reduce(function(a,b){return a+b;}) === 10 && this.rollsArray.slice(-1) != 0) {
 		return this.halfStrike = true;
 	} else {
 		return this.halfStrike = false;

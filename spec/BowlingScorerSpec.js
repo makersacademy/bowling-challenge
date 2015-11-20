31e8a366a40e6ead0c7c9adbfdb9c00cda7b7786
Sam Joseph <tansaku@gmail.com>
2015-11-20 07:43:32 +0000

    puts basic scoring in place

--- spec/BowlingScorerSpec.js
+++ spec/BowlingScorerSpec.js
@@ -1,15 +1,37 @@
 describe('BowlingScorer',function(){
   var scorer;
+
   beforeEach(function(){
     scorer = new BowlingScorer();
   });
+
   it('starts with a score of zero',function(){
     expect(scorer.score()).toEqual(0);
   });
+
   it('can score a gutter game',function(){
     for(var i=0;i<10;i++){
       scorer.addFrame(0,0);
     }
     expect(scorer.score()).toEqual(0);
   });
+
+  it('can score a single frame with a single roll', function(){
+    scorer.addFrame(3,0);
+    expect(scorer.score()).toEqual(3);
+  });
+
+  it('can score a single frame with two rolls', function(){
+    scorer.addFrame(3,6);
+    expect(scorer.score()).toEqual(9);
+  });
+
+  it('can score multiple frames with multiple rolls', function(){
+    scorer.addFrame(3,6);
+    scorer.addFrame(2,4);
+    expect(scorer.score()).toEqual(15);
+  });
+
 });
+
+
--- src/BowlingScorer.js
+++ src/BowlingScorer.js
@@ -1,10 +1,11 @@
 function BowlingScorer() {
-
+  this._score = 0;
 }
 
 BowlingScorer.prototype.score = function() {
-  return 0;
+  return this._score;
 };
 
-BowlingScorer.prototype.addFrame = function() {
+BowlingScorer.prototype.addFrame = function(roll1, roll2) {
+  this._score += roll1 + roll2;
 };

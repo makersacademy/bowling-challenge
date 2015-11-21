93ed0254cd00264a6f82dca03f6610763c970d1c
Tom Barnes <ruralmyth@gmail.com>
2015-11-21 17:02:49 +0000

    hit one pin on each bowl works

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,15 +2,26 @@ describe("Game", function() {
   var game;
 
   beforeEach(function() {
-    game = new Game;
+    game = new Game();
   });
+
+  function bowlHelper(bowlTimes, pinsHitOnEachBowl) {
+    for (var i = 0; i < bowlTimes; i++) {
+      game.bowl(pinsHitOnEachBowl);
+    }
+  } 
 
   describe("No pins are hit - gutter game", function() {
     it("final score should be zero", function() {
-      for (var i = 0; i <= 20; i++) {
-        game.bowl();
-      }
+      bowlHelper(20,0);
       expect(game.finalScore()).toEqual(0);
+    });
+  });
+
+  describe("One pin hit on each bowl", function() {
+    it("final score should be 20", function() {
+      bowlHelper(20,1);
+      expect(game.finalScore()).toEqual(20);
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,18 @@
+"use strict";
+
 function Game() {
+  this.scores = [];
+  this.calcScore = 0;
 }
 
-Game.prototype.bowl = function() {
+Game.prototype.bowl = function(pins) {
+  this.scores.push(pins);
 };
 
 Game.prototype.finalScore = function() {
-  return 0
+  for (var i = 0; i <= (this.scores.length -1); i++) {
+    this.calcScore += this.scores[i];
+    console.log(i + ': ' + this.calcScore);
+  }
+  return this.calcScore;
 };

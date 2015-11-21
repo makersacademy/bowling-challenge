ba73cf2d3eeae2aac7f9584ff3c4a9f714e660da
samover <samuel.overloop@gmail.com>
2015-11-21 19:58:54 +0000

    add layout and jqueyr for scorecard

--- src/app.js
+++ src/app.js
@@ -6,21 +6,32 @@ $(document).ready(function() {
 
   game = new Game();
 
-  $( 'input#rollButton ' ).click( function() { 
+  $( 'input#rollButton ' ).click( function(event) { 
+    event.preventDefault();
     rollValue = document.getElementById( 'rollValue' ).value;
     game.roll( parseInt(rollValue) );
     updateScore();
-    event.preventDefault();
   });
 
   
   function updateScore() {
-    var rollResult = '<div id="frameNumber">1</div>' + 
-                     '<div id="roll1">' + game.gameRolls + '</div>' +
-                     '<div id="roll2"></div>' + 
-                     '<div id="frameScore">' + game.score() + '</div>';
-    $( '#frame1' ).html(rollResult);
-    $( '#listScore' ).text( 'Score: ' + game.score() );
 
+    game.frames.forEach( function( frame, index ) {   
+      var i = frame.frameIndex + 1,
+          roll1 = game.gameRolls[frame.rollIndex],
+          roll2 = ''; 
+
+      if(frame.isStrike()) { 
+        roll1 = 'X';
+      } else if( frame.isSpare() ) {
+        roll2 = '/'; 
+      } else if ( frame.isFinished()) {
+        roll2 = game.gameRolls[frame.rollIndex + 1];
+      }
+
+      $( '#frame' + i + ' .roll1' ).text( roll1 );
+      $( '#frame' + i + ' .roll2' ).text( roll2 );
+      $( '#frame' + i + ' .frameScore' ).text( game.score(i) );
+    });
   }
 });

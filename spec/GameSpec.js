ed0ef8c90bbe4ca514730a1bcf9c95047c4c8718
octavian <octavianrotari@gmail.com>
2015-11-22 17:29:57 +0000

    (WIP) refatoring bug the game should end at the 10th frame but it does not

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,7 +1,6 @@
 describe('Game',function(){
   beforeEach(function(){
     game = new Game();
-    strike = 10;
   })
 
   it('if user does strike the score is changed by 10',function(){
@@ -9,15 +8,10 @@ describe('Game',function(){
     expect(game.score).toEqual(30);
   })
 
-  it ('if strike changes the frame and it does not allow second roll',function(){
-    game.turn(10)
-    expect(game.frame).toEqual(1)
-  })
-
-  it('if spare the total score is not changing at the end of the frame',function(){
+  it('counts the frames',function(){
     game.turn(5);
-    game.turn(5);
-    expect(game.score).toEqual(0)
+    game.turn(3);
+    expect(game.frame).toEqual(1);
   })
 
   it('it ads the score of the frame to the total if no spare',function(){
@@ -26,8 +20,29 @@ describe('Game',function(){
     expect(game.score).toEqual(8);
   })
 
-  it('it prints the game is finish if frames equal 10',function(){
+  it('if spare the total score is not changing at the end of the frame',function(){
+    game.turn(5);
+    game.turn(5);
+    expect(game.score).toEqual(0);
+  })
+
+  it('if spare it ads the points of the first roll in the next fram to the frame before',function(){
+    game.turn(5);
+    game.turn(5);
+    game.turn(2);
+    expect(game.score).toEqual(12);
+  })
+
+  it('the score is 300 if a perfect game is played',function(){
+    for(var i=1; i < 11; i++){
+      game.turn(10)
+    };
+    expect(game.score).toEqual(300);
+  })
+
+  it('the game ends if 10 frames are played',function(){
     for(var i=0; i < 10; i++){
-    }
+      game.turn(10)
+    };
   })
 })
--- src/Game.js
+++ src/Game.js
@@ -5,50 +5,45 @@ function Game(){
   this.scoreFrame = 0;
   this.bowlTurn = 0;
   this.frame = 0;
-  this.turnPoints = 0;
+  this.turnScore = 0;
   this.sparePoints = 0;
   this.TOTALFRAME = 10;
 }
 
 Game.prototype.turn = function(points){
-  if (this.bowlTurn === 0){
+  if (this.frame >= this.TOTALFRAME) {
+    return;
+  } else if (this.bowlTurn === 0){
     this.firstRoll(points);
   } else {
     this.secondRoll(points);
   }
+    console.log('End of the game')
 }
 
 Game.prototype.firstRoll = function(points){
-  this.calcStrike(points);
-  this.calcBonus(points);
-  if (points < this.MAXPOINTS ){
-    if (this.sparePoints !== 0){
-      this.score += (this.sparePoints + points);
-      this.sparePoints = 0;
-      console.log(this.score);
-    }
-    this.turnPoints += points;
-    this.bowlTurn += 1
-  }
+  this.firstRollCalcStrike(points);
+  this.firstRollCalcNormalPoints(points);
 }
 
 Game.prototype.secondRoll = function(points){
-  this.turnPoints += points;
-  if (this.turnPoints === this.MAXPOINTS){
-    this.sparePoints = this.turnPoints;
-  } else if (this.turnPoints < this.MAXPOINTS ){
-    this.score += this.turnPoints;
-  }
-  this.turnPoints = 0;
-  this.frame += 1;
-  this.bowlTurn = 0;
+  this.turnScore += points;
+  this.secondRollSpare();
+  this.secondRollNormalPoints();
+  this.endFrame();
 }
 
-Game.prototype.calcStrike = function(points){
+Game.prototype.bonusRoll = function(points) {
+  this.bonusPoints += 10;
+  this.scoreFrame += this.bonusPoints;
+  this.bonusPoints = 0;
+}
+
+Game.prototype.firstRollCalcStrike = function(points){
   if ( points === this.MAXPOINTS ){
     this.strike();
-    this.firstRoll('bonus');
-    this.firstRoll('bonus');
+    this.bonusRoll(10);
+    this.bonusRoll(10);
     this.score += this.scoreFrame;
     this.scoreFrame = 0;
   }
@@ -60,19 +55,40 @@ Game.prototype.calcBonus = function(points){
   }
 }
 
+Game.prototype.firstRollCalcNormalPoints = function(points){
+  if (points < this.MAXPOINTS ){
+    this.firstRollCalcSpare(points);
+    this.turnScore += points;
+    this.bowlTurn += 1
+  }
+}
+
+Game.prototype.firstRollCalcSpare = function(points){
+  if (this.sparePoints !== 0){
+    this.score += (this.sparePoints + points);
+    this.sparePoints = 0;
+  }
+}
+
+Game.prototype.secondRollSpare = function(){
+  if (this.turnScore === this.MAXPOINTS){
+    this.sparePoints = this.turnScore;
+  }
+}
+
+Game.prototype.secondRollNormalPoints = function(){
+  if (this.turnScore < this.MAXPOINTS ){
+    this.score += this.turnScore;
+  }
+}
+
 Game.prototype.strike = function(){
   this.scoreFrame += this.MAXPOINTS;
   this.frame += 1;
 }
 
-Game.prototype.bonus = function(){
-  this.bonusPoints += 10;
-  this.scoreFrame += this.bonusPoints;
-  this.bonusPoints = 0;
-}
-
-Game.prototype.endTurn = function(){
-  this.turnPoints = 0;
+Game.prototype.endFrame = function(){
+  this.turnScore = 0;
   this.bowlTurn = 0;
   this.frame += 1;
 }

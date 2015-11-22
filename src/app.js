f363016c756a53d3642a28899c95053062456a6e
samover <samuel.overloop@gmail.com>
2015-11-22 15:26:58 +0000

    show / and X in last frame

--- src/app.js
+++ src/app.js
@@ -58,18 +58,38 @@ $(document).ready(function() {
       var i = frame.frameIndex + 1,
       roll1 = game.gameRolls[frame.rollIndex],
       roll2 = '',
+      roll3 = '', 
       identifier = '#game' + gameNumber + ' .frame' + i;
 
-      if(frame.isStrike()) { 
-        roll1 = 'X';
-      } else if( frame.isSpare() ) {
-        roll2 = '/'; 
-      } else if ( frame.isFinished()) {
-        roll2 = game.gameRolls[frame.rollIndex + 1];
-      }
+      if(frame.isLastFrame()) {
+        roll1 = roll1 === 10 ? 'X' : roll1;
+
+        switch(frame.turns) {
+          case 2:
+            roll2 = game.gameRolls[frame.rollIndex + 1];
+            roll2 = roll2 === 10 ? 'X' : roll2;
+            roll2 = frame.isSpare() ? '/' : roll2; 
+            break;
+          case 3:
+            roll2 = game.gameRolls[frame.rollIndex + 1];
+            roll2 = roll2 === 10 ? 'X' : roll2;
+            roll2 = roll1 + roll2 === 10 ? '/' : roll2; 
+            roll3 = game.gameRolls[frame.rollIndex + 2]; 
+            roll3 = roll3 === 10 ? 'X' : roll3;
+            break;
+        }
+      } else if(frame.isFinished()) {
+          if(frame.isStrike()) {
+            roll1 = '';
+            roll2 = 'X';
+          } else {
+            roll2 = frame.isSpare() ? '/' : game.gameRolls[frame.rollIndex + 1];
+          }
+        }
 
       $( identifier + ' .roll1' ).html('<p>' +  roll1 + '</p>');
       $( identifier + ' .roll2' ).html('<p>' +  roll2 + '</p>');
+      $( identifier + ' .roll3' ).html('<p>' +  roll3 + '</p>');
       $( identifier + ' .frameScore' ).html('<p>' +  game.score(i) + '</p>' );
     });
   }
@@ -80,63 +100,93 @@ $(document).ready(function() {
     "      <li class='player1'></li>" +
     "      <li class='frame1'>" +
     "        <div class='frameNumber'>1</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame2'>" +
     "        <div class='frameNumber'>2</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame3'>" +
     "        <div class='frameNumber'>3</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame4'>" +
     "        <div class='frameNumber'>4</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame5'>" +
     "        <div class='frameNumber'>5</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame6'>" +
     "        <div class='frameNumber'>6</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame7'>" +
     "        <div class='frameNumber'>7</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame8'>" +
     "        <div class='frameNumber'>8</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame9'>" +
     "        <div class='frameNumber'>9</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='emptyDiv'></div>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "      <li class='frame10'>" +
     "        <div class='frameNumber'>10</div>" +
+    "        <div class='rolls'>" +
+    "          <div class='roll1'></div>" +
+    "          <div class='roll2'></div>" +
+    "          <div class='roll3'></div>" +
+    "        </div>" +
     "        <div class='frameScore'></div>" +
-    "        <div class='roll1'></div>" +
-    "        <div class='roll2'></div>" +
     "      </li>" +
     "    </ul>"; 
 

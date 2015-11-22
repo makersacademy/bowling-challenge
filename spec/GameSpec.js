367185e84382773b169757d9d9e59aeda636f90d
Chuka Ebi <chuka.ebi@outlook.com>
2015-11-22 22:57:03 +0000

    need to fix spy for strike test suite

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -31,4 +31,35 @@ describe('Game',function(){
       expect(game._noOfPins()).toEqual(10);
     });
   });
+
+  describe('when a strike is rolled', function(){
+    beforeEach(function(){
+      spyOn(game,"getRandomInt").and.returnValue(10);
+    });
+    it("should reset the frame", function(){
+      game.roll();
+      expect(game._noOfPins()).toEqual(10);
+    });
+    it('should give bonus points on the next frame', function(){
+      game.roll;
+      game.getRandomInt.isSpy = false;
+      spyOn(game,"getRandomInt").and.returnValue(4);
+      for(i=0;i<=1;i++){ game.roll; }
+      expect(game.score).toEqual(28);
+    });
+  });
+
+
+  describe('when a game is finished', function(){
+    beforeEach(function(){
+      spyOn(game,"getRandomInt").and.returnValue(5);
+    });
+
+    it('prints a message and reports your final score', function(){
+      for(i=1; i<=19; i++){
+        game.roll();
+      }
+      expect(game.roll()).toEqual("Your final score is " +game.score+ ".");
+    });
+  });
 });

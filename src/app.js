ea9735faf776a418e1542267d37a6a1cf1a5b5c0
samover <samuel.overloop@gmail.com>
2015-11-22 01:55:52 +0000

    add cute buttons for roll

--- src/app.js
+++ src/app.js
@@ -6,21 +6,52 @@ $(document).ready(function() {
 
   game = new Game();
 
-  $( 'input#rollButton ' ).click( function() { 
-    rollValue = document.getElementById( 'rollValue' ).value;
+  drawButtons();
+
+  $( '.btn' ).click( function(event) {
+    console.log('button clicked');
+    event.preventDefault();
+    rollValue = $(this).attr('data-value');
+    console.log('rollValue= ' + rollValue)
     game.roll( parseInt(rollValue) );
     updateScore();
-    event.preventDefault();
-  });
+    drawButtons();
+  })
 
-  
+  function drawButtons() {
+    var i,
+        btn,
+        numButtons;
+
+    numButtons = 10 - game.currentFrame().pins;
+    $( '#buttonList' ).empty();
+    console.log(numButtons);
+    for( i = 0; i <= numButtons; i++ ) {
+      btn  = "<button id='btn" + i + "' type='button' class='btn' " + 
+        "data-value='" + i + "'>" + i + "</button>";
+      $( '#buttonList' ).append(btn);
+      console.log('drawing button ' + i);
+    }
+    console.log('drawing buttons done!;');
+  }
+
   function updateScore() {
-    var rollResult = '<div id="frameNumber">1</div>' + 
-                     '<div id="roll1">' + game.gameRolls + '</div>' +
-                     '<div id="roll2"></div>' + 
-                     '<div id="frameScore">' + game.score() + '</div>';
-    $( '#frame1' ).html(rollResult);
-    $( '#listScore' ).text( 'Score: ' + game.score() );
+    game.frames.forEach( function( frame, index ) {   
+      var i = frame.frameIndex + 1,
+      roll1 = game.gameRolls[frame.rollIndex],
+      roll2 = ''; 
 
+      if(frame.isStrike()) { 
+        roll1 = 'X';
+      } else if( frame.isSpare() ) {
+        roll2 = '/'; 
+      } else if ( frame.isFinished()) {
+        roll2 = game.gameRolls[frame.rollIndex + 1];
+      }
+
+      $( '#frame' + i + ' .roll1' ).html('<p>' +  roll1 + '</p>');
+      $( '#frame' + i + ' .roll2' ).html('<p>' +  roll2 + '</p>');
+      $( '#frame' + i + ' .frameScore' ).html('<p>' +  game.score(i) + '</p>' );
+    });
   }
 });

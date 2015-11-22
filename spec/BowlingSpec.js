993c2c01079242284104e3439101ff968e2e34fb
jamierose <jrose111284@gmail.com>
2015-11-22 21:42:46 +0000

    change a few tests

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -26,28 +26,39 @@ describe ('Bowling', function() {
         expect(bowling.totalFrame(1)).toEqual(10);
       })
 
-        it ('has more bonus when you strike', function() {
-          bowling.rolls(10, 0, 0);
-          bowling.rolls(3, 2, 1);
-          expect(bowling.strikeBonus(0)).toBe(15);
-        });
+      it ('can check if you have a spare', function() {
+        bowling.rolls(4, 6, 0);
+        expect(bowling.totalFrame(0)).toEqual(10);
+      })
 
-        it ('has more bonus when you spare', function() {
-          bowling.rolls(5, 5, 0);
-          bowling.rolls(2, 0, 1);
-          expect(bowling.spareBonus(0)).toEqual(12);
-        });
+      it ('bonus when you strike', function() {
+        bowling.rolls(10, 0, 0);
+        bowling.rolls(3, 2, 1);
+        expect(bowling.strikeBonus(0)).toBe(15);
+      });
+
+      it ('bonus when you spare', function() {
+        bowling.rolls(5, 5, 0);
+        bowling.rolls(2, 0, 1);
+        expect(bowling.spareBonus(0)).toEqual(12);
+      });
+
+      it ('bonus when strike on 10th frame', function() {
+        bowling.rolls(10, 0, 0);
+        bowling.rolls(6, 0, 1);
+        expect(bowling.strikeBonus(10)).toEqual(16);
+      });
   })
 
   describe ('#bowling total', function(){
-      it ('counts frames', function() {
+      it ('counts first few frames', function() {
         bowling.rolls(5, 3, 1);
         bowling.rolls(2, 3, 2);
         expect(bowling.addScores(2)).toEqual(13);
       })
   });
 
-  describe ('#max game and gutter ball', function(){
+  describe ('#max games and gutter ball', function(){
       it ('has max scores of 300', function() {
          bowling.rolls(10, 0, 1);
          bowling.rolls(10, 0, 2);
@@ -61,7 +72,7 @@ describe ('Bowling', function() {
          bowling.rolls(10, 0, 10);
          bowling.rolls(10, 0, 11);
          bowling.rolls(10, 0, 12);
-         expect(bowling.allFrameScores(12)).toEqual(120);
+         expect(bowling.addScores(12)).toEqual(120);
        })
 
        it ('has a gutter game', function() {
@@ -77,5 +88,19 @@ describe ('Bowling', function() {
           bowling.rolls(0, 0, 9);
           expect(bowling.totalFrame(9)).toEqual(0);
         })
+
+        it ('has a random game score', function() {
+           bowling.rolls(2, 4, 1);
+           bowling.rolls(2, 4, 2);
+           bowling.rolls(2, 4, 3);
+           bowling.rolls(2, 4, 4);
+           bowling.rolls(2, 4, 5);
+           bowling.rolls(2, 4, 6);
+           bowling.rolls(2, 4, 7);
+           bowling.rolls(2, 4, 8);
+           bowling.rolls(2, 4, 9);
+           bowling.rolls(2, 4, 10);
+           expect(bowling.addScores(10)).toEqual(60);
+         })
     });
 });

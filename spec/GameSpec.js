ae41fbd8bd9ec08e0867a2534b98232d14e11b56
Tom Barnes <ruralmyth@gmail.com>
2015-11-22 13:53:28 +0000

    perfect game scores correctly

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,22 +11,29 @@ describe("Game", function() {
     }
   } 
 
-  describe("No pins are hit - gutter game", function() {
-    it("final score should be zero", function() {
+  describe("No pins are hit - gutter game.", function() {
+    it("The final score should be zero", function() {
       bowlHelper(20,0);
       expect(game.finalScore()).toEqual(0);
     });
   });
 
-  describe("One pin hit on each bowl", function() {
-    it("final score should be 20", function() {
+  describe("One pin hit on each bowl.", function() {
+    it("The final score should be 20", function() {
       bowlHelper(20,1);
       expect(game.finalScore()).toEqual(20);
     });
   });
 
+  describe("All strikes - perfect game.", function() {
+    it("The final score should be 300", function() {
+      bowlHelper(12, 10);
+      expect(game.finalScore()).toEqual(300);
+    });
+  });
+
   describe("A spare.", function() {
-    it("a spare on first frame, then all 1's. Final score should be 29", function() {
+    it("A spare on first frame, then all 1's. Final score should be 29", function() {
       bowlHelper(2, 5);
       bowlHelper(18,1);
       expect(game.finalScore()).toEqual(29);
@@ -34,7 +41,7 @@ describe("Game", function() {
   });
 
   describe("A strike.", function() {
-    it("a strike on first frame, then all 1's. Final score should be 30", function() {
+    it("A strike on first frame, then all 1's. Final score should be 30", function() {
       bowlHelper(1, 10);
       bowlHelper(18, 1);
       expect(game.finalScore()).toEqual(30);

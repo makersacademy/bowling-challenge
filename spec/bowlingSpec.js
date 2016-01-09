0e5f411ad18a28c0c6b9868fcee97fe11a58e217
willibaur <williambautista@gmail.com>
2016-01-09 16:22:03 +0000

    Roll method added and remaining pins stored

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -4,6 +4,7 @@ describe ('Bowling', function() {
 
 beforeEach(function() {
   bowling = new Bowling();
+  spyOn(Math, 'random').and.returnValue(0.4);
 });
 
 describe('Sets up default settings', function() {
@@ -15,5 +16,14 @@ describe('Sets up default settings', function() {
   it('starts a frame with 10 pins', function() {
     bowling.startFrame(1);
     expect(bowling.getRemainingPins()).toBe(10);
+  });
+});
+
+describe('Playing one frame', function() {
+  it('returns number of pins knocked-down', function() {
+    bowling.startFrame(1);
+    bowling.rollBall();
+    expect(bowling.getKnockedPins()).toBe(4);
+    expect(bowling.getRemainingPins()).toBe(6);
   });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -2,6 +2,7 @@ function Bowling() {
   this.playerName    = 'Player1'
   this.remainingPins = 10;
   this.score         = {};
+  this.knockedPins   = 0;
 }
 
 Bowling.prototype.setPlayerName = function (name) {
@@ -12,10 +13,19 @@ Bowling.prototype.getPlayerName = function () {
   return this.playerName;
 };
 
-Bowling.prototype.getRemainingPins = function () {
-  return this.remainingPins;
-};
-
 Bowling.prototype.startFrame = function (number) {
   this.score[number] = [];
+};
+
+Bowling.prototype.rollBall = function () {
+  this.knockedPins = Math.floor(Math.random() * (this.remainingPins + 1));
+  this.remainingPins -= this.knockedPins;
+};
+
+Bowling.prototype.getKnockedPins = function () {
+  return this.knockedPins;
+};
+
+Bowling.prototype.getRemainingPins = function () {
+  return this.remainingPins;
 };

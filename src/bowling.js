4fd2dcd8c741c3dedee7e7134b890edd55caf914
willibaur <williambautista@gmail.com>
2016-01-09 15:32:50 +0000

    Default settings for a game added

--- src/bowling.js
+++ src/bowling.js
@@ -1,5 +1,7 @@
 function Bowling() {
-  this.playerName = 'Player1'
+  this.playerName    = 'Player1'
+  this.remainingPins = 10;
+  this.score         = {};
 }
 
 Bowling.prototype.setPlayerName = function (name) {
@@ -8,4 +10,12 @@ Bowling.prototype.setPlayerName = function (name) {
 
 Bowling.prototype.getPlayerName = function () {
   return this.playerName;
+};
+
+Bowling.prototype.getRemainingPins = function () {
+  return this.remainingPins;
+};
+
+Bowling.prototype.startFrame = function (number) {
+  this.score[number] = [];
 };

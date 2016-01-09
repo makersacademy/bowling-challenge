1df33b7e810967b314ff6ff4d913193c942cfc04
Jelgar <jonathanelgardiner@gmail.com>
2016-01-09 21:33:31 +0000

    isSpare scoring wip

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -1,5 +1,4 @@
 describe("Game", function() {
-  var game;
 
   beforeEach(function() {
     game = new Game();
@@ -11,5 +10,57 @@ describe("Game", function() {
       game.addFrame(frame);
       expect(game._frames).toContain(frame);
     });
+
+    it("should add the score from that frame to a total score", function() {
+      frame.bowl(7);
+      frame.bowl(1);
+      game.addFrame(frame);
+      expect(game._score).toEqual(8);
+    });
   });
 });
+
+describe("Frame", function() {
+  var game;
+
+  beforeEach(function() {
+    frame = new Frame()
+  });
+
+  describe("bowl", function() {
+    it("should deduct the correct amount of pins from frame", function() {
+      frame.bowl(7);
+      expect(frame._pins).toEqual(3);
+    });
+
+    it("should raise an error if bowl is used more than twice per frame", function() {
+      frame.bowl(7);
+      frame.bowl(1);
+      expect(function(){
+        frame.bowl(9);
+      }).toThrowError("You have used all your rolls in this frame");
+    });
+
+    it("should not add a score immediately if a spare is scored", function() {
+      frame.bowl(7)
+      frame.bowl(3)
+      expect(frame._frameScore).toEqual(7);
+    });
+
+    // it("should add the correct score if a spare has been scored in the previous frame")
+  });
+
+  describe("isSpare", function() {
+    it("should return true when there are 0 pins remaining after 2 rolls of a frame", function() {
+      frame.bowlSpare();
+      expect(frame.isSpare()).toEqual(true);
+    });
+  });
+});
+
+//Helper Methods//
+
+Frame.prototype.bowlSpare = function() {
+  this.bowl(7);
+  this.bowl(3);
+};
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,13 +1,38 @@
 function Game() {
-  this._frames = []
-}
-
-var game = new Game();
-
-function Frame() {
-
+  this._frames = [];
+  this._score = 0;
 }
 
 Game.prototype.addFrame = function(frame){
-  this._frames.push(frame)
+  this._frames.push(frame);
+  this._score += frame._frameScore;
 }
+
+function Frame() {
+  this._pins = 10
+  this._rolls = 0
+  this._frameScore = 0
+}
+
+Frame.prototype.bowl = function(pinsdowned){
+  if(this._rolls >= 2){
+    throw new TypeError("You have used all your rolls in this frame");
+  } else {
+    this._pins -= pinsdowned;
+    this._rolls += 1;
+  }
+  if(this.isSpare === true) {
+    this._bonus = "spare"
+  } else {
+    this._frameScore += pinsdowned
+
+  }
+};
+
+Frame.prototype.isSpare = function(){
+  if(this._pins === 0 && this._rolls === 2){
+    return true;
+  } else {
+    return false;
+  }
+};

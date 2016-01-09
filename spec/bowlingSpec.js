9c008b7c4df218643d84d75cdb1c05bf56863cda
Chris Wynne <chris.wynndow@gmail.com>
2016-01-09 16:20:38 +0000

    Adds getFrame and error throwing for end of game and scoring over 10 per frame

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -10,18 +10,55 @@ describe('Game', function() {
     expect(game.score).toEqual(0);
   });
 
+  it('raises an error if play continues after the 10th frame', function(){
+    var n
+    for (n=0; n<20; n++) {
+      game.knockDownPins(4);
+    }
+    expect(function(){
+      game.knockDownPins(9);
+    }).toThrowError('Game over: The 10th frame has finished')
+  });
+
   describe('#pinsKnockedDown', function() {
     it('adds the number of pins to the score', function() {
       game.knockDownPins(9);
       expect(game.score).toEqual(9);
     });
+
+    it('doesn\'t allow more than 10 pins per frame', function () {
+      game.knockDownPins(9);
+      expect(function() {
+        game.knockDownPins(2);
+      }).toThrowError('Not possible: Only 10 pins per frame')
+    })
   });
 
   describe('#getScore', function() {
     it('returns the current score', function() {
-      game.knockDownPins(9);
-      game.knockDownPins(8);
-      expect(game.getScore()).toEqual(17);
+      game.knockDownPins(4);
+      game.knockDownPins(5);
+      expect(game.getScore()).toEqual(9);
+    });
+  });
+
+  describe('#getFrame', function() {
+    it('initially returns frame 1', function () {
+      expect(game.getFrame()).toEqual(1);
+    });
+
+    it('returns the correct frame after 2 balls', function () {
+      game.knockDownPins(4);
+      game.knockDownPins(5);
+      expect(game.getFrame()).toEqual(2);
+    });
+
+    it('returns the correct frame after 15 balls', function() {
+      var n
+      for (n = 0; n <=14; n++) {
+        game.knockDownPins(4);
+      }
+      expect(game.getFrame()).toEqual(8)
     });
   });
 
--- src/bowling.js
+++ src/bowling.js
@@ -1,11 +1,25 @@
 function Game() {
   this.score = 0;
+  this.ballcount = 0;
+  this.previousBall;
 }
 
 Game.prototype.knockDownPins = function(number) {
+  if (this.getFrame() > 10) {
+    throw new Error('Game over: The 10th frame has finished')
+  }
+  if (((this.ballcount+1) % 2 === 0) && (this.previousBall + number > 10)) {
+    throw new Error('Not possible: Only 10 pins per frame')
+  }
+  this.ballcount += 1;
   this.score += number;
+  this.previousBall = number;
 };
 
 Game.prototype.getScore = function() {
   return this.score;
+};
+
+Game.prototype.getFrame = function() {
+  return Math.floor((this.ballcount / 2) + 1);
 };

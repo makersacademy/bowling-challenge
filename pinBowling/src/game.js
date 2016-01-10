31cac8f355f41f87636f4c4de42c7cb154702497
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-10 21:09:49 +0000

    progress on scoring

--- pinBowling/src/game.js
+++ pinBowling/src/game.js
@@ -10,15 +10,18 @@ function Game (pin, defaultScore) {
   this._bonusCollection = [];
 }
 
+// Need to unravel below function or start from scratch
+
 Game.prototype.pinsHit = function (number) {
   this.pin.pinsHit(number);
   this._increaseScore(number);
   if (this._bonusCollection.length !== 0) {
-  for (var bonus in this._bonusCollection) {bonus.reduceTimeLimit();}
+  for (var bonus in this._bonusCollection) {bonus.timeLimit--;}
   this._loopLimit =
     this._bonusCollection.reduce(function(previousValue, currentValue) {
     return previousValue.loopLimit + currentValue.loopLimit;
   });}
+  this._strikeEffectOnLoopLimit(number);
   // if (this._strikeBonusTime > 0){this._strikeBonusTime--;}
   // this._strikeEffectOnLoopLimit(number);
 };
@@ -31,8 +34,7 @@ Game.prototype._increaseScore = function (number) {
   for (var i = 0; i < this._loopLimit + 1; i++) {this.score += number;}
 };
 
-// Game.prototype._strikeEffectOnLoopLimit = function (number) {
-//   if (number >= this._initialPinsThere) {
-//     this._strikeBonusTime += 2;
-//     this._loopLimit += 1;}
-// };
+Game.prototype._strikeEffectOnLoopLimit = function (number) {
+  var strike = new Bonus (2);
+  if (number >= this._initialPinsThere) { (this._bonusCollection).push(strike);}
+};

989015982a47876cbfa58d0ddac202d70879b5cc
Chris Wynne <chris.wynndow@gmail.com>
2016-01-10 12:52:16 +0000

    Adds spechelper and refactors out new Game() from spec

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,21 +1,17 @@
 describe('Game', function() {
 
-  var game
-
   it('is defined', function() {
     expect(game = new Game()).toBeDefined();
   });
 
   describe('#bowlA', function() {
     it('is defined', function() {
-      game = new Game();
       expect(game.bowlA(5)).toBeDefined();
     });
   });
 
   describe('#getBallCount', function() {
     it ('can return the current number of balls bowled', function() {
-      game = new Game();
       game.bowlA(5);
       expect(game.getBallCount()).toEqual(1);
     });

d3beb16cb4d31d514d763790a8e198e7bd005f1d
Alan Gabbianelli <alan.gabbianelli@gmail.com>
2016-01-10 16:12:24 +0000

    Adds standing and knocked pins + accepts only valid value as knocked pins

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,12 +1,58 @@
-describe("Frame", function() {
+'use strict';
+
+describe('Frame', function() {
+  var frame;
 
   beforeEach(function() {
-    frame = new Frame()
+    frame = new Frame();
   });
 
-  describe("Initial pins", function() {
-    it("should be 10", function() {
+  describe('#standingPins', function() {
+    it('has an initial value of 10', function() {
       expect(frame.standingPins).toEqual(10);
     });
-  })
-})
+  });
+
+  describe('#knockedPins', function() {
+    it('has an initial value of 0', function() {
+      expect(frame.knockedPins).toEqual(0);
+    });
+  });
+
+  describe('#getStandingPins', function() {
+    it('returns the number of standing pins', function() {
+      expect(frame.getStandingPins()).toEqual(10);
+    });
+  });
+
+  describe('#getKnockedPins', function() {
+    it('returns the number of knocked pins', function() {
+      expect(frame.getKnockedPins()).toEqual(0);
+    });
+  });
+
+  describe('#isValidNumber', function() {
+    it('returns true only with positive integers between 0 and 10', function() {
+      expect(frame.isValidNumber(-1)).toEqual(false);
+      expect(frame.isValidNumber(11)).toEqual(false);
+      expect(frame.isValidNumber(3.2)).toEqual(false);
+      expect(frame.isValidNumber('one')).toEqual(false);
+      expect(frame.isValidNumber(3)).toEqual(true);
+    });
+  });
+
+  describe('#setKnockedPins', function() {
+    it('throws an error if passed an invalid value', function() {
+      var error = 'Only valid numbers: no cheating, please XD'
+      expect(function() { frame.setKnockedPins(-1) }).toThrowError(error);
+      expect(function() { frame.setKnockedPins(11) }).toThrowError(error);
+      expect(function() { frame.setKnockedPins(3.2) }).toThrowError(error);
+      expect(function() { frame.setKnockedPins('one') }).toThrowError(error);
+    });
+
+    it('sets the number of knocked pins', function() {
+      frame.setKnockedPins(4);
+      expect(frame.getKnockedPins()).toEqual(4)
+    });
+  });
+});
--- src/frame.js
+++ src/frame.js
@@ -1,3 +1,24 @@
+'use strict';
+
 function Frame() {
-  
+this.standingPins = 10
+this.knockedPins = 0
+}
+
+Frame.prototype.getStandingPins = function() {
+  return this.standingPins;
+}
+
+Frame.prototype.getKnockedPins = function() {
+  return this.knockedPins;
+}
+
+Frame.prototype.setKnockedPins = function(number) {
+  var error = "Only valid numbers: no cheating, please XD";
+  if (!this.isValidNumber(number)) { throw new Error (error) }
+  this.knockedPins = number;
+}
+
+Frame.prototype.isValidNumber = function(number) {
+  return Number.isInteger(number) && number >= 0 && number <= 10;
 }

7ad5a9ac0e2add7af67d363a206740a458cbde26
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-10 13:23:11 +0000

    enables round incrementation

--- pinBowling/src/frame.js
+++ pinBowling/src/frame.js
@@ -1,13 +1,23 @@
-function Frame (firstFrame) {
+function Frame (firstFrame, initialRound) {
   'use strict';
   this._firstFrame = firstFrame || 1;
+  this._initialRound = initialRound || 1;
   this._frame = this._firstFrame;
+  this._round = this._initialRound;
 }
 
 Frame.prototype.giveFrame = function () {
   return this._frame;
 };
 
-Frame.prototype.switchFrame = function () {
+Frame.prototype.upFrame = function () {
   this._frame++;
+};
+
+Frame.prototype.giveRound = function () {
+  return this._round;
+};
+
+Frame.prototype.upRound = function () {
+  this._round++;
 };

25e9c86b6f3aa668632b649d2d4a7610cb019a00
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-10 11:29:03 +0000

    improves feature tests

--- pinBowling/src/pin.js
+++ pinBowling/src/pin.js
@@ -1,7 +1,14 @@
-function Pin () {
+function Pin (initialPins) {
   'use strict'
+  this._initialPinsThere = initialPins || 10;
+  this._pinsThere = this._initialPinsThere
 }
 
 Pin.prototype.pinsHit = function (number) {
+  if (number > this._pinsThere) {throw "cannot exceed pin number"; }
   return number;
+};
+
+Pin.prototype.countPinsThere = function () {
+  return this._pinsThere;
 };

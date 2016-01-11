de02e341b9cc6eb2bb530322313d421ddd3fd945
Adam Lancaster <lancaster28@hotmail.com>
2016-01-11 00:55:01 +0000

    Bonus rolls when strike or spare on last go implemented. Tests need to be added. Still need ability to foul go, and jquery link up

--- src/game.js
+++ src/game.js
@@ -6,21 +6,17 @@ function Game(){
 	this.bowlingFrames = [];
 	this.score = 0;
 	this.scoresArray = [];	
-
+	this.count = 0;
 };
 
 
 Game.prototype.playFrame = function() {
 	let frame = new Frame;
 	if(this.scoresArray.length===8){
-		frame.firstRoll();
-		frame.secondRoll();
-		this.bowlingFrames.push(frame);
-		this._totalScore();
+		this._lastRoll();
 	} else if (this.bowlingFrames.length===2){
 	this._bonusCheck();
-	this.scoresArray.push(this.bowlingFrames[0].frameScore);
-	this.bowlingFrames.shift();
+	this._addToScoresArray();
 	this.playFrame();
 	} else {
 		frame.firstRoll();
@@ -40,10 +36,38 @@ Game.prototype._bonusCheck = function() {
 	}
 };
 
-Game.prototype._totalScore = function() {
+
+Game.prototype._addToScoresArray = function(){
 	this.scoresArray.push(this.bowlingFrames[0].frameScore);
 	this.bowlingFrames.shift();
-	this.scoresArray.push(this.bowlingFrames[0].frameScore);
-	this.bowlingFrames.shift();
-	this.score = this.scoresArray.reduce(function(a, b){ return a + b });
 };
+
+Game.prototype._lastRoll = function() {
+	let frame = new Frame;
+	if(this.count<3){
+		frame.firstRoll();
+		frame.secondRoll();
+		this.bowlingFrames.push(frame);
+		if(frame.didStrike){
+			this.count ++;
+			this._lastRoll();
+		} else if(frame.didSpare) {
+			this.count+=2
+			this._lastRoll();
+		} else {
+			this._totalScore();
+		}
+	} else {
+		this._totalScore();
+	};
+};
+
+
+Game.prototype._totalScore = function() {
+	let index;
+	this.score = this.scoresArray.reduce(function(a, b){ return a + b });
+	for (index = 0; index < this.bowlingFrames.length; ++index) {
+    this.score += this.bowlingFrames[index].frameScore
+	}
+};
+

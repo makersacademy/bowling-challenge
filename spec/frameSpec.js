c9d89a0f7c549e3d5a9e32b92865bb8580b926f4
willibaur <williambautista@gmail.com>
2016-01-14 22:39:05 +0000

    New solution, jshint config fixed and frame class added

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,38 +1,21 @@
+
 describe ('frame', function() {
   var frame;
-});
 
-beforeEach(function() {
-  frame = new Frame();
-});
-
-describe('Stores partial score', function() {
-  it('of knocked pins', function() {
-    frame.setKnockedPins(4);
-    frame.startFrame(1,1)
-    expect(frame.getTotal()).toBe(4);
+  beforeEach(function() {
+    frame = new Frame();
   });
-});
 
-describe('Playing one frame', function() {
-  describe('returns frame total', function() {
-    it('combines roll 1 with roll 2', function() {
-      frame.setKnockedPins(4);
-      frame.startFrame(1,1);
-      frame.setKnockedPins(3);
-      frame.startFrame(1,2);
-      expect(frame.getTotal()).toBe(7);
+  describe('#getFrameInfo', function() {
+    it('Allows to play a frame', function(){
+      expect(frame.getFrameInfo()).toEqual({rolls: [],
+                                            score: 0, bonus: null});
     });
   });
-});
 
-describe('frame reset', function() {
-  it('sets default values',function() {
-    frame.setKnockedPins(4);
-    frame.startFrame(1,1);
-    frame.setKnockedPins(3);
-    frame.startFrame(1,2);
-    frame.resetFrame();
-    expect(frame.getTotal()).toBe(0);
-  });
+
+  // describe('', function() {
+  //   it('', function(){
+  //   });
+  // });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,30 +1,9 @@
 function Frame() {
-  this.remainingPins = 10;
-  this.frameScore    = [0,0,0];
-  this.knockedPins   = 0;
+  this._rolls = [];
+  this._score = 0;
+  this._bonus = null;
 }
 
-
-Frame.prototype.startFrame = function (frameNumber, roll) {
-  this.frameScore[roll-1] = this.getKnockedPins();
-  this.frameScore[2] += this.getKnockedPins();
-};
-
-Frame.prototype.getKnockedPins = function () {
-  return this.knockedPins;
-};
-
-Frame.prototype.setKnockedPins = function (number) {
-  this.knockedPins = number;
-  this.remainingPins -= this.knockedPins;
-};
-
-Frame.prototype.getTotal = function () {
-  return this.frameScore[2];
-};
-
-Frame.prototype.resetFrame = function () {
-  this.remainingPins = 10;
-  this.frameScore    = [0,0,0];
-  this.knockedPins   = 0;
+Frame.prototype.getFrameInfo = function () {
+  return {rolls: this._rolls, score: this._score, bonus: this._bonus};
 };

24c5aba6cee153686277617ed9f4595b7540dafc
Chris Wynne <chris.wynndow@gmail.com>
2016-01-15 10:18:08 +0000

    Refactoring to reduced cyclomatic complexity

--- src/frame.js
+++ src/frame.js
@@ -4,18 +4,8 @@ function Frame() {
 }
 
 Frame.prototype.receivePins = function(pins) {
-  if (this.currentFrame.length < 2) {
     this.currentFrame.push(pins);
-    if (pins === 10) {
-      this.frames.push(this.currentFrame);
-      this.currentFrame = [];
-      return;
-    }
-    if (this.currentFrame.length === 2) {
-      this.frames.push(this.currentFrame);
-      this.currentFrame = [];
-    }
-  }
+    this._storeCurrentFrameIfCompleted(pins);
 }
 
 Frame.prototype.getFrameResults = function() {
@@ -24,20 +14,9 @@ Frame.prototype.getFrameResults = function() {
 
 Frame.prototype.getFrameScores = function() {
   var output = [];
-  var score;
   var arrayLength = this.frames.length;
   for (var i = 0; i < arrayLength; i++) {
-    var frame = this.frames[i];
-    var nextFrame = this.frames[i+1];
-    score = frame.reduce((a, b) => a + b, 0)
-    if (frame.length === 2 && score === 10) {
-      if (typeof nextFrame === 'undefined') {
-        this.currentFrame.length === 0 ? (score = 0) : (score += this.currentFrame[0])
-      } else if (typeof nextFrame === 'object') {
-        score += nextFrame[0];
-      }
-    }
-    output.push([score])
+    output.push([this._reduceFrameToScore(i)]);
   }
   return output;
 };
@@ -51,4 +30,30 @@ Frame.prototype.isTooManyPinsInOneFrame = function(numberOfPins) {
   } else {
     return (numberOfPins > 10)
   }
+}
+
+Frame.prototype._storeCurrentFrameIfCompleted = function(pins) {
+  if (pins === 10 || this.currentFrame.length === 2) {
+    this.frames.push(this.currentFrame);
+    this.currentFrame = [];
+  }
+};
+
+Frame.prototype._reduceFrameToScore = function(i) {
+  var frame = this.frames[i];
+  var nextFrame = this.frames[i+1];
+  var score = frame.reduce((a, b) => a + b, 0)
+  return this._addBonusOrSetScoreToZero(score, frame, nextFrame);
+};
+
+Frame.prototype._addBonusOrSetScoreToZero = function(score, frame, nextFrame) {
+  var bonus_score = score
+  if (frame.length === 2 && score === 10) {
+    if (typeof nextFrame === 'undefined') {
+      bonus_score = this.currentFrame.length === 0 ? (0) : (score += this.currentFrame[0])
+    } else if (typeof nextFrame === 'object') {
+      bonus_score += nextFrame[0];
+    }
+  }
+  return bonus_score;
 }

3034f01f1d27b9599646181e5023a41be2375dbb
willibaur <williambautista@gmail.com>
2016-01-15 00:11:11 +0000

    Frame class added, all info for a single frame provided

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -7,15 +7,50 @@ describe ('frame', function() {
   });
 
   describe('#getFrameInfo', function() {
-    it('Allows to play a frame', function(){
-      expect(frame.getFrameInfo()).toEqual({rolls: [],
-                                            score: 0, bonus: null});
+    it('Returns frame info template', function(){
+      expect(frame.getFrameInfo()).toEqual({rolls: [], score: 0, bonus: null});
     });
   });
 
+  describe('#roll', function() {
+    describe('Allows to play a frame', function() {
+      it('without bonus', function(){
+        frame.roll(6);
+        frame.roll(3);
+        expect(frame.getFrameInfo()).toEqual({rolls: [6, 3],
+                                              score: 9, bonus: null});
+      });
 
-  // describe('', function() {
-  //   it('', function(){
-  //   });
-  // });
+      describe('with bonus', function() {
+        describe('as a spare', function () {
+          it('with pins knocked on both rolls', function(){
+            frame.roll(5);
+            frame.roll(5);
+            expect(frame.getFrameInfo()).toEqual({rolls: [5, 5],
+                                                  score: 10, bonus: 'spare'});
+            });
+          it('with pins knocked only on second roll', function () {
+            frame.roll(0);
+            frame.roll(10);
+            expect(frame.getFrameInfo()).toEqual({rolls: [0, 10],
+                                                  score: 10, bonus: 'spare'});
+          });
+        });
+
+        describe('as a strike', function() {
+          it('with all pins knocked in the first round', function(){
+            frame.roll(10);
+            expect(frame.getFrameInfo()).toEqual({rolls: [10, 0],
+                                                  score: 10, bonus: 'strike'});
+          });
+        });
+      });
+    });
+  });
 });
+
+
+// describe('', function() {
+//   it('', function(){
+//   });
+// });
--- src/frame.js
+++ src/frame.js
@@ -7,3 +7,27 @@ function Frame() {
 Frame.prototype.getFrameInfo = function () {
   return {rolls: this._rolls, score: this._score, bonus: this._bonus};
 };
+
+Frame.prototype.roll = function (pinsKnocked) {
+  if (this._rolls.length === 0) {
+    this._storeRoll(pinsKnocked);
+    if (pinsKnocked === 10) {
+      this._storeRoll(0);
+      this._bonus = 'strike';
+    }
+  } else {
+    this._storeRoll(pinsKnocked);
+    this._isSpare();
+  }
+};
+
+Frame.prototype._storeRoll = function (pinsKnocked) {
+  this._rolls.push(pinsKnocked);
+  this._score += pinsKnocked;
+};
+
+Frame.prototype._isSpare = function () {
+  if (this._score === 10) {
+    this._bonus = 'spare';
+  }
+};

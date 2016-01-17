c6bf25be42497e011352b4d86c7b6fd5de1a9043
Chris Wynne <chris.wynndow@gmail.com>
2016-01-17 17:37:02 +0000

    Combines features tests

--- spec/features/checkingTheScoreSpec.js
+++ spec/features/checkingTheScoreSpec.js
@@ -1,4 +1,4 @@
-describe ('FEATURE TEST :Checking the score', function () {
+describe ('FEATURE TEST: Checking the score', function () {
 
   var game;
   beforeEach(function() {
@@ -71,5 +71,67 @@ describe ('FEATURE TEST :Checking the score', function () {
         expect(game.seeFrameScores()).toEqual([[25]])
       })
     });
+  });
+
+  describe('Final score', function() {
+    beforeEach(function() {
+      var n;
+      for (n=0; n<18; n++) {
+        game.bowlA(4)
+      }
+    });
+
+    it('is correct with a non bonus final frame', function() {
+      game.bowlA(4);
+      game.bowlA(4);
+      expect(game.totalScore()).toEqual(80);
+    });
+
+    it('is correct with a spare final frame', function() {
+      game.bowlA(4);
+      game.bowlA(6);
+      game.bowlA(5);
+      expect(game.totalScore()).toEqual(87);
+    });
+
+    it('is correct with a spare final frame with strike bonus', function() {
+      game.bowlA(4);
+      game.bowlA(6);
+      game.bowlA(10);
+      expect(game.totalScore()).toEqual(92);
+    });
+
+    it('is correct with a strike final frame', function() {
+      game.bowlA(10);
+      game.bowlA(4);
+      game.bowlA(4);
+      expect(game.totalScore()).toEqual(90);
+    });
+
+    it('is correct with a strike final with two strike bonus\'s', function() {
+      game.bowlA(10);
+      game.bowlA(10);
+      game.bowlA(10);
+      expect(game.totalScore()).toEqual(102);
+    });
+
+    it('is correct for a perfect game', function() {
+      var game = new Game();
+      var n;
+      for (n=0; n<12; n++) {
+        game.bowlA(10)
+      }
+      expect(game.totalScore()).toEqual(300);
+    });
+
+    it('is correct for a gutter game', function() {
+      var game = new Game();
+      var n;
+      for (n=0; n<20; n++) {
+        game.bowlA(0)
+      }
+      expect(game.totalScore()).toEqual(0);
+    });
+
   });
 });

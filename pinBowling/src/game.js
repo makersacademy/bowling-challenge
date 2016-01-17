0e4fc9cb9fd2402269b2487fd849e3d02d9f543c
Andrew Burnie <apburnie@hotmail.co.uk>
2016-01-17 17:36:33 +0000

    renames _reserve _strikeReserve

--- pinBowling/src/game.js
+++ pinBowling/src/game.js
@@ -5,7 +5,7 @@ function Game (pin, defaultScore) {
   this._DEFAULT_SCORE = defaultScore || 0;
   this.score = this._DEFAULT_SCORE;
   this._initialPinsThere = this.pin._initialPinsThere;
-  this.reserve = 0;
+  this._strikeReserve = 0;
 }
 
 Game.prototype.pinsHit = function (number) {
@@ -19,6 +19,6 @@ Game.prototype.getScore = function () {
 };
 
 Game.prototype._bonus = function (number) {
-  if (this.reserve >= 10) {this.score += number; this.reserve -= 10}
-  if (number === 10) {this.reserve += 10*2}
+  if (this._strikeReserve >= 10) {this.score += number; this._strikeReserve -= 10}
+  if (number === 10) {this._strikeReserve += 10*2}
 };

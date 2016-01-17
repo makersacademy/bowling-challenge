38b9dfa2d4fb7a86713d432e0e8d46abaef18bc6
Jamie Brown <jamiebrown201@hotmail.com>
2016-01-17 20:48:24 +0000

    implement all user stories

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,6 +1,5 @@
 var BowlingGame = function() {
   this.gameScores = [];
-  this.frameCounter = 0;
 };
 
 BowlingGame.prototype.scoreInput = function(pins) {
@@ -9,8 +8,40 @@ BowlingGame.prototype.scoreInput = function(pins) {
 
 BowlingGame.prototype.score = function() {
   var result = 0;
-  for( var i = 0; i < 20; i++) {
-    result += this.gameScores[i];
+  var frameIndex = 0;
+  var self = this;
+
+  function _frameScore() {
+    return self.gameScores[frameIndex] + self.gameScores[frameIndex + 1];
+  }
+
+  function _isSpare() {
+    return self.gameScores[frameIndex] + self.gameScores[frameIndex + 1] === 10;
+  }
+
+  function _spareBonus() {
+    return self.gameScores[frameIndex + 2];
+  }
+
+  function _isStrike() {
+    return self.gameScores[frameIndex] === 10;
+  }
+
+  function _strikeBonus() {
+    return self.gameScores[frameIndex + 1] + self.gameScores[frameIndex + 2];
+  }
+
+  for( var i = 0; i < 10; i++) {
+    if (_isStrike()) {
+      result += 10 + _strikeBonus();
+      frameIndex ++;
+    } else if (_isSpare()) {
+      result += _frameScore() + _spareBonus();
+      frameIndex += 2;
+    } else {
+      result += _frameScore();
+      frameIndex += 2;
+    }
   }
   return result
 }
--- test/spec/bowlingGameSpec.js
+++ test/spec/bowlingGameSpec.js
@@ -1,24 +1,48 @@
 describe('bowlingGame', function() {
 
   beforeEach(function() {
-      bowlingGame = new BowlingGame
-  });
-
-  describe('default', function() {
-    it('bowlingGame frame counter starts on frame 1', function() {
-      expect(bowlingGame.frameCounter).toBe(0);
-    });
+      bowlingGame = new BowlingGame;
   });
 
   it('can roll a gutter game', function() {
     customInput(20, 0);
-    expect(bowlingGame.score()).toBe(0)
+    expect(bowlingGame.score()).toBe(0);
   });
 
   it('can roll a one point game', function() {
     customInput(20, 1);
-    expect(bowlingGame.score()).toBe(20)
+    expect(bowlingGame.score()).toBe(20);
   });
+
+  it('can roll score a custom amount of scores', function() {
+    customInput(5, 2);
+    customInput(10, 4);
+    customInput(3, 3);
+    customInput(2, 1);
+    expect(bowlingGame.score()).toBe(61);
+  });
+
+  it('spare bonus is calculated into the score', function() {
+    customInput(2, 5);
+    customInput(2, 3);
+    customInput(16, 0);
+    expect(bowlingGame.score()).toBe(19);
+  });
+
+  it('strike bonus is calculated into the score', function() {
+    customInput(1, 10);
+    customInput(2, 3);
+    customInput(16, 0)
+    expect(bowlingGame.score()).toBe(22);
+  });
+
+  it('can play a perfect game (last frame can score 3 times)', function() {
+    customInput(12, 10);
+    expect(bowlingGame.score()).toBe(300);
+  });
+
+
+
 
 
 

46d85f7f61520443ede41d8340ea9964ee45036c
Daniela <daniela.galeote@gmail.com>
2016-01-18 07:32:32 +0000

    Introducing score of frame wip

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -28,25 +28,5 @@ describe('Game', function() {
       });
     });
 
-  //
-  // // demonstrates use of spies to intercept and test method calls
-  // it("tells the current song if the user has made it a favorite", function() {
-  //   spyOn(song, 'persistFavoriteStatus');
-  //
-  //   player.play(song);
-  //   player.makeFavorite();
-  //
-  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  // });
-  //
-  // //demonstrates use of expected exceptions
-  // describe("#resume", function() {
-  //   it("should throw an exception if song is already playing", function() {
-  //     player.play(song);
-  //
-  //     expect(function() {
-  //       player.resume();
-  //     }).toThrowError("song is already playing");
-  //   });
-  // });
+
 });

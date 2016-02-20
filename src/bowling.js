22c8c882afa713a3e9967e18cfbcbe95c3ddd436
rachelsmithcode <rachelcodesmith@outlook.com>
2016-02-20 18:30:04 +0000

    First commit

---
+++ src/bowling.js
@@ -0,0 +1,29 @@
+'use strict';
+
+function Scorecard(){
+    this.bowlingFrames = []
+    this.lastFrameStrike = false;
+    this.lastFrameSpare = false;
+  }
+
+  Scorecard.prototype.checkStrike = function(){
+    
+   };
+
+   Scorecard.prototype.checkSpare = function(){
+
+   };
+
+   Scorecard.prototype.ballOne = function(){
+    //  cannot enter a score higher than 10
+    // check if it's a strike
+   };
+
+   Scorecard.prototype.ballTwo = function(){
+    //  cannot enter a score higher than 10-ballOne
+    // check if it's a spare
+   };
+
+   Scorecard.prototype.calculateScore = function(){
+
+   };
---
+++ src/bowlingConsole.js
@@ -0,0 +1,77 @@
+$( document ).ready(function() {
+
+  // var thermostat = new Thermostat();
+  //
+  // $pscolour = function() {
+  //   // must be at top of document so as it loads in order
+  //   if (thermostat.displayColour === 'high') {
+  //   color =  'rgba(117,19,12,1)'; // transparant red
+  //   // where r is 232, g is 169, b is 60 and the transparancy (a) is at 50% (0.5)
+  // } else if (thermostat.displayColour === 'medium') {
+  //   color = 'rgba(232,169,60,1)'; //transparant orange
+  // } else if (thermostat.displayColour === 'low') {
+  //   color =  'rgba(55,94,72,1)'; // transparant green
+  //   } else {
+  //   color =  'rgba(0,0,0,0.5)'; // safty condition transparant black
+  //   }
+  //   $('body').css({
+  //     backgroundColor: color
+  //   // declare backgroundColor for changes but the constant things go in the CSS sheet
+  //   });
+  // };
+  //
+  // $pscolour();
+  // // declare the colour first so it starts with one
+  //
+  // $('#temp').text((thermostat.temperature)+'C');
+  //
+  // $('#increase-temp').click(function(){
+  //   thermostat.up();
+  //   $pscolour();
+  //   // declare colour change
+  //   $('#temp').text((thermostat.temperature)+'C');
+  // });
+  //
+  // $('#decrease-temp').click(function(){
+  //   thermostat.down();
+  //   $pscolour();
+  //   // declare colour change
+  //   $('#temp').text((thermostat.temperature)+'C');
+  // });
+  //
+  // $('#psm-on').click(function(){
+  //   thermostat.powerSavingModeOn();
+  //   $('#psm').text("On");
+  // });
+  //
+  // $('#test').click(function(){
+  //   thermostat.powerSavingModeSwitch();
+  //   status = thermostat.powerSavingMode;
+  //   console.log(status);
+  //
+  // });
+  //
+  // $('#toggle1').click(function(){
+  //   thermostat.powerSavingModeSwitch();
+  //   status = thermostat.powerSavingMode;
+  //   console.log(status);
+  //   $('#temp').text((thermostat.temperature)+'C');
+  //   $pscolour();
+  //
+  // });
+  //
+  // $('#psm-off').click(function(){
+  //   thermostat.powerSavingModeOff();
+  //   $('#psm').text("Off");
+  // });
+  //
+  //
+  // $('#reset-temp').click(function(){
+  //   thermostat.resetButton();
+  //   $('#temp').text((thermostat.temperature)+'C');
+  //   $pscolour();
+  // });
+
+
+
+});

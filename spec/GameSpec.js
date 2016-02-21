aad4c2e1bab337ec3fd1a73c526e44267d334530
Matt <mattpgough@gmail.com>
2016-02-21 15:51:17 +0000

    Restructured code to reflect SRP, added scores to an array.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,24 +7,38 @@ describe('Game', function() {
     game = new Game();
   });
 
-    it('has a starting score of 0', function(){
-      expect(game.getCurrentScore()).toEqual(0)
+    it('has a starting score of 0', function() {
+      expect(game.getScore()).toEqual(0)
     });
 
-    it('bowling returns a number', function(){
+    it('bowling returns a number', function() {
       expect(game.bowl()).toEqual(jasmine.any(Number));
     });
 
-    it('adds bowled score to total score', function() {
-      spyOn(game, "bowl").and.returnValue(5);
-      expect(game.totalScore()).toEqual(5);
+    it('adds score to total score', function() {
+      spyOn(game, "_number").and.returnValue(5);
+      game.bowl();
+      expect(game.getScore()).toEqual(5);
     });
 
     it('adds two scores together to total score', function() {
-      spyOn(game, "bowl").and.returnValue(5);
-      expect(game.totalScore()).toEqual(5);
-      game.bowl.and.returnValue(4);
-      expect(game.totalScore()).toEqual(9);
+      spyOn(game, "_number").and.returnValue(4);
+      game.bowl();
+      game.bowl();
+      expect(game.getScore()).toEqual(8);
     });
 
+    it('keeps track of the number of rounds', function() {
+      for (var i = 0; i < 8; i++) {
+       game.bowl();
+      }
+      expect(game.getRoundNumber()).toEqual(4);
+    });
+
+    it('adds scores to an array', function(){
+      spyOn(game, "_number").and.returnValue(4);
+      game.bowl();
+      game.bowl();
+      expect(game.getFullScores()).toEqual([4,4])
+    });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,22 +1,41 @@
 "use strict";
 
 function Game() {
-  this.STARTING_SCORE = 0;
-  this.score = this.STARTING_SCORE;
-};
+  this.score = 0;
+  this.roundNumber = 0;
+  this.fullScores = [];
+}
 
-Game.prototype.getCurrentScore = function(){
+Game.prototype.getScore = function(){
   return this.score;
 }
 
-Game.prototype.totalScore = function(){
-  return this.score += this.bowl();
+Game.prototype.bowl = function() {
+  this._addRoundNumber();
+  this._addToFullScores();
+  return this.score += this._number();
 }
 
-Game.prototype.bowl = function(){
-  return this._number();
+Game.prototype.getRoundNumber = function(){
+  return this.roundNumber;
 }
 
-Game.prototype._number = function() {
+Game.prototype.getRoundScore = function(){
+  return this.roundScore;
+}
+
+Game.prototype.getFullScores = function() {
+  return this.fullScores;
+}
+
+Game.prototype._number = function(){
   return Math.round(Math.random()*10);
+}
+
+Game.prototype._addRoundNumber = function(){
+  this.roundNumber += 0.5;
+}
+
+Game.prototype._addToFullScores = function(){
+  this.fullScores.push(this._number());
 }

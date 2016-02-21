5106e7061ff0fab6096a9d14445055225cff18f6
sarah crawley <sarah.crawley6@gmail.com>
2016-02-21 13:08:27 +0000

    gutter game added

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,14 +1,23 @@
-describe('BowlingGame', function() {
+describe('BowlingGame', function(){
 
-    var game = new BowlingGame();
+  var bowlingGame;
 
-  // beforeEach(function(){
-  //   var game = new BowlingGame();
-  // });
-
-  it("has a default value", function() {
-    expect(game.score).toEqual(0);
+  beforeEach(function(){
+    bowlingGame = new BowlingGame();
   });
 
+  it("has a default value of 0", function() {
+    expect(bowlingGame.score).toEqual(0);
+  });
+
+  describe("#gutter game", function() {
+
+    it("ends with a score of 0", function() {
+      for (var i=0; i <20; i++)
+      { bowlingGame.bowl(0); }
+      expect(bowlingGame.score).toEqual(0);
+    });
+
+  });
 
 });

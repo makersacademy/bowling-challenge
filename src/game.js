9b509ddf6df98a1f2e6c6c839185e82b8dc13c16
Reiss Johnson <reissjohnson@me.com>
2016-02-21 13:05:36 +0000

    slight refactoring userstory 1

--- src/game.js
+++ src/game.js
@@ -4,7 +4,11 @@ function Game(){
   this.score = 0;}
 
   Game.prototype.bowl = function (){
-    var result = Math.floor(Math.random() * 10) + 1; 
+    var result = this._randNum();
     this.score += result;
     return result;
+  };
+
+  Game.prototype._randNum = function (){
+    return Math.floor(Math.random() * 10) + 1; 
   };

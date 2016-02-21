dbb8a808eb7d36f101d98f473bb3c4f152caf185
sarah crawley <sarah.crawley6@gmail.com>
2016-02-21 16:20:09 +0000

    bowl and score outline functions added

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,23 +1,33 @@
-describe('BowlingGame', function(){
+describe("BowlingGame", function(){
 
   var bowlingGame;
 
-  beforeEach(function(){
+  beforeEach(function() {
     bowlingGame = new BowlingGame();
   });
 
-  it("has a default value of 0", function() {
-    expect(bowlingGame.score).toEqual(0);
-  });
 
-  describe("#gutter game", function() {
+  describe("#initialize new game", function() {
+    it("has a maximun of 10 frames", function() {
+      expect(bowlingGame.maxFrames).toEqual(10);
+    });
 
-    it("ends with a score of 0", function() {
-      for (var i=0; i <20; i++)
-      { bowlingGame.bowl(0); }
+    it("has a default score of 0", function() {
       expect(bowlingGame.score).toEqual(0);
     });
 
+    it("has 10 pins", function() {
+      expect(bowlingGame.pins).toEqual(10);
+    });
+  });
+
+  describe("#gutter game", function() {
+    it("ends with a score of 0", function() {
+      for (var i=0; i <20; i++) {
+        bowlingGame.bowl(0);
+      }
+      expect(bowlingGame.score()).toEqual(0);
+    });
   });
 
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,3 +1,12 @@
-function BowlingGame(){
+function BowlingGame() {
   this.score = 0;
+  this.maxFrames = 10
+  this.pins=10
 }
+
+BowlingGame.prototype.bowl = function(pins) {
+};
+
+BowlingGame.prototype.score = function() {
+  return 0;
+};

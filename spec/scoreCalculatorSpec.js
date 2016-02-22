a6f3ec3d6678dfcf89eba5e4877c2a04a09b31a2
Eilw <eirikwiig@gmail.com>
2016-02-22 15:59:18 +0000

    refractored scorecalculator

--- spec/scoreCalculatorSpec.js
+++ spec/scoreCalculatorSpec.js
@@ -14,20 +14,20 @@ describe("scoreCalculator",function(){
 
   describe("type",function(){
     it("finds a spare",function(){
-      expect(calculator.type(spareRoll)).toEqual('spare');
+      expect(calculator.isSpare(spareRoll)).toEqual(true);
     });
 
     it("finds a spare",function(){
       var spareRoll2 = [0,10,7,2];
-      expect(calculator.type(spareRoll2)).toEqual('spare');
+      expect(calculator.isSpare(spareRoll2)).toEqual(true);
     });
 
     it("does not find a spare",function(){
-      expect(calculator.type(normalRoll)).toEqual('normal');
+      expect(calculator.isSpare(normalRoll)).toEqual(false);
     });
 
     it("finds a strike",function(){
-      expect(calculator.type(strikeRoll)).toEqual('strike');
+      expect(calculator.isStrike(strikeRoll)).toEqual(true);
     });
   });
 
@@ -50,45 +50,47 @@ describe("scoreCalculator",function(){
     });
   });
 
-  describe("incrementalScore",function(){
+  describe("finalCalc",function(){
     it("calculates the right scores of a spare roll",function(){
-      expect(calculator.incrementalScore(halfSpareRoll)).toEqual([7,20,27,34,41]);
+      expect(calculator.finalCalc(halfSpareRoll)).toEqual([7,20,27,34,41]);
     });
 
     it("calculates the right scores of a strike roll",function(){
-      expect(calculator.incrementalScore(halfSingleStrikeRoll)).toEqual([7,16,33,40,47]);
+      expect(calculator.finalCalc(halfSingleStrikeRoll)).toEqual([7,16,33,40,47]);
     });
 
     it("doesnt return the value if bonus is in last frame",function(){
       var thirdFrameStrike = [0,5,0,5,10,0];
-      expect(calculator.incrementalScore(thirdFrameStrike)).toEqual([5,10,NaN]);
+      expect(calculator.finalCalc(thirdFrameStrike)).toEqual([5,10,NaN]);
     });
 
 
     it("calculates the right scores of a double strike roll",function(){
       var halfDoubleStrikeRoll = [2,5,4,5,10,0,10,0,6,1];
-      expect(calculator.incrementalScore(halfDoubleStrikeRoll)).toEqual([7,16,42,59,66]);
+      expect(calculator.finalCalc(halfDoubleStrikeRoll)).toEqual([7,16,42,59,66]);
     });
 
     it("calculates the right scores of a triple strike roll",function(){
       var halfDoubleStrikeRoll = [2,5,10,0,10,0,10,0,6,1];
-      expect(calculator.incrementalScore(halfDoubleStrikeRoll)).toEqual([7,37,63,80,87]);
-    });
-
-    it("works from the first roll",function(){
-      var firstFrame = [1];
-      expect(calculator.incrementalScore(firstFrame)).toEqual([1]);
+      expect(calculator.finalCalc(halfDoubleStrikeRoll)).toEqual([7,37,63,80,87]);
     });
 
     it("works from the first frame",function(){
       var firstFrame = [1,2];
-      expect(calculator.incrementalScore(firstFrame)).toEqual([3]);
+      expect(calculator.finalCalc(firstFrame)).toEqual([3]);
     });
 
     it("works for ten frames",function(){
       var tenFrames = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,];
-      expect(calculator.incrementalScore(tenFrames)).toEqual([3,6,9,12,15,18,21,24,27,30]);
+      expect(calculator.finalCalc(tenFrames)).toEqual([3,6,9,12,15,18,21,24,27,30]);
     });
+
+    it("one strike in last frame",function(){
+      var tenFramesOneStrike = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,10,5,4];
+      expect(calculator.finalCalc(tenFramesOneStrike)).toEqual([3,6,9,12,15,18,21,24,27,46]);
+    });
+
+
   });
 
   describe("tenFrameCalc",function(){
@@ -96,12 +98,7 @@ describe("scoreCalculator",function(){
       var tenthFrame = [10,5,4];
       expect(calculator.tenFrameCalc(tenthFrame)).toEqual(19);
     });
+
   });
 
-  describe("finalCalc",function(){
-    it("one strike in last frame",function(){
-      var tenFramesOneStrike = [1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,1,2,10,5,4];
-      expect(calculator.finalCalc(tenFramesOneStrike)).toEqual([3,6,9,12,15,18,21,24,27,46]);
-    });
-  });
 });
--- src/scoreCalculator.js
+++ src/scoreCalculator.js
@@ -1,22 +1,16 @@
 function ScoreCalculator(){
+  this.incrementalOverview = []
+  this.ALL_PINS_DOWN = 10;
+  this.nineFrameRollsComplete = 18;
+  this.nineFramesComplete = 9;
 }
 
-ScoreCalculator.prototype.type = function(rolls){
-  if(this.isStrike(rolls.slice(0,2))){
-    return 'strike'
-  }else if(this.isSpare(rolls.slice(0,2))){
-    return 'spare'
-  }else{
-    return 'normal'
-  }
-};
-
 ScoreCalculator.prototype.isSpare = function(twoRolls){
-  return (twoRolls[0]+twoRolls[1]===10 && twoRolls[0] !== 10);
+  return (twoRolls[0] + twoRolls[1] === this.ALL_PINS_DOWN && twoRolls[0] !== this.ALL_PINS_DOWN);
 }
 
 ScoreCalculator.prototype.isStrike = function(twoRolls){
-  return (twoRolls[0]===10);
+  return (twoRolls[0] === this.ALL_PINS_DOWN);
 }
 
 ScoreCalculator.prototype.spareBonus = function(rolls){
@@ -24,50 +18,49 @@ ScoreCalculator.prototype.spareBonus = function(rolls){
 }
 
 ScoreCalculator.prototype.strikeBonus = function(rolls){
-  if(rolls[0]===10 ){
-    return rolls[0]+rolls[2];
+  if(rolls[0] === this.ALL_PINS_DOWN){
+    return rolls[0] + rolls[2];
   }
   else {
-    return rolls[0]+rolls[1];
+    return rolls[0] + rolls[1];
   }
 }
 
 ScoreCalculator.prototype.calculateBonus = function(fullRoll){
-  if(this.type(fullRoll.slice(0,2)) === 'strike'){
+  if(this.isStrike(fullRoll.slice(0,2))){
     return this.strikeBonus(fullRoll.slice(2,fullRoll.length));
-  } else if (this.type(fullRoll.slice(0,2)) === 'spare'){
+  } else if (this.isSpare(fullRoll.slice(0,2))){
     return this.spareBonus(fullRoll.slice(2,fullRoll.length));
   } else{
     return 0;
   }
 };
 
-ScoreCalculator.prototype.incrementalScore = function(fullRoll){
-  if(fullRoll.length === 1){return fullRoll};
-  var incrementalOverview = []
+ScoreCalculator.prototype._incrementalScore = function(fullRoll){
   var incrementalValue = 0;
   for(var i = 0; i<(fullRoll.length); i += 2){
     var bonus = this.calculateBonus(fullRoll.slice(i,fullRoll.length));
     incrementalValue += bonus + fullRoll[i] + fullRoll[i+1];
-    incrementalOverview.push(incrementalValue);
+    this.incrementalOverview.push(incrementalValue);
   };
-  return incrementalOverview;
+  return this.incrementalOverview;
 };
 
 ScoreCalculator.prototype.tenFrameCalc = function(tenthFrame){
   if(this.isStrike(tenthFrame)){
-    return 10 + tenthFrame[1]+tenthFrame[2];
+    return this.ALL_PINS_DOWN + tenthFrame[1]+tenthFrame[2];
   } else{
     return this.calculateBonus(tenthFrame)+tenthFrame[0]+tenthFrame[1];
   }
 }
 
 ScoreCalculator.prototype.finalCalc = function(fullRoll){
-  if (fullRoll.length<18){
-    return this.incrementalScore(fullRoll);
+  if (fullRoll.length<this.nineFrameRollsComplete){
+    return this._incrementalScore(fullRoll);
   }
-  var result = this.incrementalScore(fullRoll).slice(0,9);
-  var tenFrame = result[result.length-1]+this.tenFrameCalc(fullRoll.slice(18,fullRoll.length));
-  result.push(tenFrame);
-  return result;
+  var frameResults = this._incrementalScore(fullRoll).slice(0,this.nineFramesComplete);
+  var tenthFrameRoll = fullRoll.slice(this.nineFrameRollsComplete,fullRoll.length)
+  var tenFrameValue = frameResults[frameResults.length-1] + this.tenFrameCalc(tenthFrameRoll);
+  frameResults.push(tenFrameValue);
+  return frameResults;
 }

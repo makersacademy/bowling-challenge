59f681e83ad3c43ec63ec121a7f442be75fb4aa6
Henry Gordon <hwgordon@Henrys-MBP.lan>
2016-02-24 15:04:20 +0000

    about to drastically chage for spares

--- jasmine/spec/FeatureSpec.js
+++ jasmine/spec/FeatureSpec.js
@@ -9,10 +9,29 @@ describe("Feature", function(){
 
   describe("score patterns:", function(){
 
+    it("normal, strike, normal", function(){
+      bowling.pinsHit(7);
+      bowling.pinsHit(2);
+      bowling.pinsHit(10);
+      bowling.pinsHit(5);
+      bowling.pinsHit(2);
+      expect(bowling.calculateScore()).toEqual(33);
+    });
+
     it("strike, strike", function(){
       bowling.pinsHit(10);
       bowling.pinsHit(10);
       expect(bowling.calculateScore()).toEqual(0);
+    });
+
+    it("strike, normal, strike, normal", function(){
+      bowling.pinsHit(10);
+      bowling.pinsHit(5);
+      bowling.pinsHit(2);
+      bowling.pinsHit(10);
+      bowling.pinsHit(5);
+      bowling.pinsHit(2);
+      expect(bowling.calculateScore()).toEqual(48);
     });
 
     it("strike, strike, normal", function(){
@@ -30,6 +49,44 @@ describe("Feature", function(){
       bowling.pinsHit(5);
       bowling.pinsHit(2);
       expect(bowling.calculateScore()).toEqual(79);
+    });
+
+    it("strike, strike, strike, strike, normal", function(){
+      bowling.pinsHit(10);
+      bowling.pinsHit(10);
+      bowling.pinsHit(10);
+      bowling.pinsHit(10);
+      bowling.pinsHit(5);
+      bowling.pinsHit(2);
+      expect(bowling.calculateScore()).toEqual(109);
+    });
+
+    it("normal, spare, normal", function(){
+      bowling.pinsHit(7);
+      bowling.pinsHit(2);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(2);
+      expect(bowling.calculateScore()).toEqual(31);
+    });
+
+    it("spare, spare", function(){
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      expect(bowling.calculateScore()).toEqual(0);
+    });
+
+    it("spare, spare, normal", function(){
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(5);
+      bowling.pinsHit(2);
+      expect(bowling.calculateScore()).toEqual(0);
     });
 
   });

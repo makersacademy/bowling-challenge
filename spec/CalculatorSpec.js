0dc6e81da5cfe6e9ddfdabfb08a8faedae590afc
Ina <inatsetsova@gmail.com>
2016-02-28 13:04:02 +0000

    spares calculate without 10th frame

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -11,25 +11,41 @@ describe("Game", function() {
     expect(game.calculateScore()).toEqual(0);
   });
 
-  it ("20 gutter balls return 0 points", function(){
-    for(var i = 0; i < 10; i ++){
-      game.addFrame(0,0);
-    }
-    expect(game.calculateScore()).toEqual(0);
-  });
+  describe("Scoring", function() {
 
-  it ("More than 10 frames returns an error", function(){
-    for(var i = 0; i < 10; i ++){
-      game.addFrame(0,0);
-    }
-    expect(function() {game.addFrame(0,0)}).toThrowError(TypeError, "Can't add over ten frames");
-  });
+    it ("20 gutter balls return 0 points", function(){
+      for(var i = 0; i < 10; i ++){
+        game.addFrame(0,0);
+      }
+      expect(game.calculateScore()).toEqual(0);
+    });
 
-  it ("calculates score correctly without strikes or spares", function(){
-    for(var i = 0; i < 5; i ++){
-      game.addFrame(1,6);
-      game.addFrame(4,3);
-    }
-    expect(game.calculateScore()).toEqual(70);
+    it ("calculates score correctly without strikes or spares", function(){
+      for(var i = 0; i < 5; i ++){
+        game.addFrame(1,6);
+        game.addFrame(4,3);
+      }
+      expect(game.calculateScore()).toEqual(70);
+    });
+
+    it ("calculates score correctly with spares", function(){
+      for(var i = 0; i < 5; i ++){
+        game.addFrame(4,6);
+        game.addFrame(4,4);
+      }
+      expect(game.calculateScore()).toEqual(110);
+    });
+    describe ("Tenth frame", function(){
+
+    });
+    describe ("Error handling", function(){
+      it ("More than 10 frames returns an error", function(){
+        for(var i = 0; i < 10; i ++){
+          game.addFrame(0,0);
+        }
+        expect(function() {game.addFrame(0,0)}).toThrowError(TypeError, "Can't add over ten frames");
+      });
+    });
+
   });
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -13,8 +13,16 @@ Game.prototype.calculateScore = function(){
   return this.frames.map(frameScore).reduce(sum, 0)
 }
 
-function frameScore(frame) {
-   return frame.reduce(sum);
+function frameScore(frame,frameNum, frames) {
+  if(frames[frameNum + 1] && frame.reduce(sum) === 10){
+    return frame.reduce(sum) + rollOne(frames,frameNum);
+  } else {
+    return frame.reduce(sum);
+  }
 }
 
 var sum = function(prev, curr) {return prev + curr};
+
+var rollOne = function(frames, frameNum) {
+  return frames[frameNum + 1][0]
+};

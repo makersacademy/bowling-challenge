eac9d25e5bee2eea743dd36f4fe572f1f5ea57ea
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-29 00:32:45 +0000

    Major refactor of code and tests, streamlined implementation

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -6,28 +6,33 @@ describe('Game', function() {
     frame = jasmine.createSpyObj('frame',
       ['addRoll', 'isRollsComplete', 'calcTotal', 'addBonus', 'isFrameClosed']);
     function frameKlass() { return frame; }
-
     game = new Game(frameKlass);
     });
 
   describe('initializing a new Game', function() {
     it('starts out empty', function() {
       expect(game.frames).toEqual([]);
-      expect(game.currFrameNum).toEqual(0);
     });
   });
 
   describe('#addRoll', function() {
-    it('adds a roll to the game and calls addroll on frame', function() {
+    it('adds a roll to the game and calls addRoll on frame', function() {
       game.addRoll(0)
       expect(frame.addRoll).toHaveBeenCalled();
     });
 
-    it('pushes to frames a frame that has rolls complete', function() {
+    it('pushes a frame that has rolls complete to frames', function() {
       frame.isRollsComplete.and.returnValue(true);
       game.addRoll(0)
-      game.addRoll(0)
       expect(game.frames).toContain(frame);
+    });
+  });
+
+  describe('#addRoll and maximum frame handling', function() {
+    it('frame.isFrameClosed is called on the final frame', function() {
+      frame.isRollsComplete.and.returnValue(true);
+      for (var i = 0; i < 11; i++) { game.addRoll(0); }
+      expect(frame.isFrameClosed).toHaveBeenCalled();
     });
 
     it('adds maximum frames to the game', function() {
@@ -35,12 +40,6 @@ describe('Game', function() {
       frame.isFrameClosed.and.returnValue(true);
       for (var i = 0; i<10; i++) { game.addRoll(0); }
       expect(function() { game.addRoll(0); }).toThrowError("Max frames");
-    });
-
-    it('adds extra balls on final frame with bonus', function() {
-      frame.isRollsComplete.and.returnValue(true);
-      for (var i = 0; i < 11; i++) { game.addRoll(); }
-      expect(frame.isFrameClosed).toHaveBeenCalled();
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -2,91 +2,45 @@
 
 function Game(frameKlass) {
   this.frames = [];
-  this.currFrameNum = 0;
   this.totalScore = [];
   this.frameKlass = frameKlass;
   this.currFrame = new this.frameKlass();
-
+  this.MAXFRAMES = 10;
 }
 
 Game.prototype.addRoll = function (pinsDown) {
-  this.currFrameNum = this.frames.length;
-  if (this.currFrameNum < 10 || this.lastFrameBonus()) {
+  if (this.frames.length < this.MAXFRAMES || this._lastFrameBonus()) {
     this.currFrame.addRoll(pinsDown);
   } else {
     throw new Error("Max frames");
   }
-  this.calcPrevBonuses(pinsDown);
+  this._calcPrevBonuses(pinsDown);
+  this._checkReloadNewFrame();
+};
+
+Game.prototype._calcPrevBonuses = function (pinsDown) {
+  for(var i = 0; i < this.frames.length; i++) {
+    if(this.frames[i].bonusBalls >= 1) { this.frames[i].addBonus(pinsDown) }
+  }
   this._updateTotalScore();
-  // this._updateFrameScores();
+};
+
+Game.prototype._updateTotalScore = function() {
+  for(var i = 0, prev = 0; i < this.frames.length; i++) {
+    this.totalScore[i] = this.frames[i].calcTotal() + prev;
+    prev = this.totalScore[i];
+  }
+}
+
+Game.prototype._checkReloadNewFrame = function () {
   if(this.currFrame.isRollsComplete()) {
     this.frames.push(this.currFrame)
     this._updateTotalScore();
-    // this._updateFrameScores();
     this.currFrame = new this.frameKlass();
   }
 };
 
-// Game.prototype._updateFrameScores = function() {
-  // this.ball1 = this.currFrame.rolls[0];
-  // this.ball2 = this.currFrame.rolls[1];
-  // this.calcPrevSpareBonus();
-  // this.calcPrevStrikeBonus();
-  // this.calcPrevDoubleStrikeBonus();
-  // this.calcPrevBonuses();
-  // this._updateTotalScore();
-// };
-
-Game.prototype._updateTotalScore = function() {
-  for(var i = 0; i < this.frames.length; i++) {
-    this.totalScore[i] = this.frames[i].calcTotal() + ( this.totalScore[i-1] || 0 );
-  }
-};
-
-Game.prototype.calcPrevBonuses = function (pinsDown) {
-  for(var i = 0; i < this.frames.length; i++) {
-    if(this.frames[i].bonusBalls >= 1) {
-      this.frames[i].addBonus(pinsDown)
-    }
-  }
-};
-
-// Game.prototype.calcPrevSpareBonus = function () {
-//   var prev = this.currFrameNum - 1;
-//   if(this.frames[prev] && this.frames[prev].isSpare()) {
-//     this.frames[prev].bonus[0] = this.ball1;
-//   }
-// };
-//
-// Game.prototype.calcPrevStrikeBonus = function () {
-//   var prev = this.currFrameNum - 1;
-//   if(prev >= 0 && this.frames[prev].isStrike() &&
-//     !this.currFrame.isStrike()) {
-//       this.frames[prev].bonus[0] = this.ball1;
-//       if(this.currFrame.isRollsComplete()) {
-//         this.frames[prev].bonus[1] = this.ball2;
-//       }
-//   }
-// };
-//
-// Game.prototype.calcPrevDoubleStrikeBonus = function () {
-//   var prev = this.currFrameNum - 1;
-//   var prevprev = this.currFrameNum - 2;
-//   if(prevprev >= 0 &&
-//     this.frames[prevprev].isStrike() &&
-//     this.frames[prev].isStrike()) {
-//       this.frames[prevprev].bonus[0] = 10;
-//       this.frames[prevprev].bonus[1] = this.ball1;
-//     }
-// };
-
-Game.prototype.lastFrameBonus = function () {
-  // var currFrameNum = this.currFrameNum;
-  // if (currFrameNum === 10 && this.frames[9].isStrike() &&
-  //   !this.frames[9].isFrameClosed()) { return true; }
-  // if (currFrameNum === 11 && this.frames[9].isStrike() &&
-  //   this.frames[10].isStrike() && !this.frames[10].isFrameClosed())   { return true; }
-  // if (this.frames[9].isSpare() && !this.frames[9].isFrameClosed()) { return true; }
-  if (!this.frames[9].isFrameClosed()) { return true; }
+Game.prototype._lastFrameBonus = function () {
+  if (!this.frames[this.MAXFRAMES-1].isFrameClosed()) { return true; }
   return false;
 };

d402e3b7a99bf4d0c43cb874f1c9df2d45456cc4
Mo Deerow <mo@Mos-MacBook-Pro.local>
2016-02-29 08:40:02 +0000

    all games

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,63 +4,40 @@ describe('Game', function(){
     game = new Game();
   });
 
-  it('returns the score', function(){
-    game.score(6);
-    game.score(2);
-    expect(game.total()).toEqual(8);
+  it('can roll a gutter game', function (){
+    rollMany(0,20);
+    expect(game.score()).toBe(0);
   });
 
-  it('records the total frames played', function(){
-    game.score(6);
-    game.score(2);
-    expect(game.log).toContain([6, 2]);
+  it('can roll all ones', function (){
+    rollMany(1, 20);
+    expect(game.score()).toBe(20);
   });
 
-  it('calcs strike bonus', function(){
-    game.score(10);
-    game.score(2);
-    game.score(3);
-    expect(game.strikeCalc()).toEqual(5);
+  it('can roll a spare', function(){
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(0, 17);
+    expect(game.score()).toBe(16);
   });
 
-  it('calcs spare bonus', function(){
-    game.score(2);
-    game.score(6);
-    game.score(5);
-    game.score(5);
-    game.score(1);
-    game.score(2);
-    expect(game.spareCalc()).toEqual(1);
+  it('can roll a strike', function(){
+    game.roll(10);
+    game.roll(4);
+    game.roll(3);
+    rollMany(0, 16);
+    expect(game.score()).toBe(24);
   });
 
-  it('checks if extra rolls given after a tenth frame strike', function(){
-    for(i = 0; i < 18; i++) {
-      game.score(4);
+  it('roll a perfect game', function(){
+    rollMany(10, 12);
+    expect(game.score()).toBe(300);
+  })
+
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
     }
-    game.score(10);
-    expect(game.extraRolls).toEqual(2)
-  });
-
-  it('checks if extra rolls given after a tenth frame spare', function(){
-    for(i = 0; i < 18; i++) {
-      game.score(4);
-    }
-    game.score(4);
-    game.score(6);
-    expect(game.extraRolls).toEqual(1)
-  });
-
-  it('checks error is displayed when game over after bonus', function(){
-    var error = function() {
-         throw new TypeError("start new game");
-       };
-
-    for(i = 0; i < 18; i++) {
-      game.score(4);
-    }
-    game.score(10);
-    game.score(4);
-    game.score(4);
-    expect(function(){game.score(4);}).toThrowError("start new game");
-  });
+  };
 });

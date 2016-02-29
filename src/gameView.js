bf0d663172651d5b1e10b307ab1d429511d73b8d
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-29 01:04:24 +0000

    Added error handling display, spare and strike display format

--- src/gameView.js
+++ src/gameView.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
 
-  var game = new Game();
+  var game = new Game(Frame);
   var rollCount = 0;
+  var prevpins;
 
   $('.button-roll').click(function() {
     var pins = parseInt($(this).attr('value'));
+    try {
       game.addRoll(pins);
+      console.log(game);
       updateRollDisplay(pins);
       updateTotalDisplay();
+      isGameOver();
+    } catch(err) {
+      $('#errors').html(err).fadeIn('fast').fadeOut('slow');
+    }
   });
 
   function updateTotalDisplay() {
@@ -17,9 +24,17 @@ $(document).ready(function() {
   }
 
   function updateRollDisplay(pins) {
+    if(rollCount % 2 === 1 && pins + prevpins === 10) { pins = '/'; }
     if(pins === 10) { pins = 'X'; }
     if(pins === 'X' && game.totalScore.length < 10) { rollCount++; }
     $('tr:eq(1) td:eq(' + rollCount + ')').html(pins);
     rollCount++;
+    prevpins = pins;
+  }
+
+  function isGameOver() {
+    if($('tr:eq(2) td:eq(9)').html()) {
+      $('#gameover').fadeIn('fast');
+    }
   }
 });

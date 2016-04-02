fc31abda7b60e1564b165b95cadcbb8beb08ea86
Pete Burch <peteburch223@gmail>
2016-04-02 17:08:40 +0100

    calculating bonuses...but clumsy

--- public/js/frame.js
+++ public/js/frame.js
@@ -3,15 +3,61 @@
 function Frame(rollDefinition=Roll){
   this.score = 0;
   this.rolls = [];
+  this.nextFrames = [];
   this.rollDefinition = rollDefinition;
+};
+
+
+Frame.prototype.getBonus = function(){
+  if(this.isStrike()){
+    if (!!this.nextFrames[0]){
+      if (this.nextFrames[0].isStrike()){
+        if (!!this.nextFrames[1]){
+          return this.nextFrames[0].getScore() + this.nextFrames[1].getRollScores()[0];
+        } else {
+          return this.nextFrames[0].getScore();
+        }
+      } else {
+        return this.nextFrames[0].getScore();
+      }
+    } else {
+      return 0;
+    }
+  } else if (this.isSpare()) {
+    if (!!this.nextFrames[0]){
+      return this.nextFrames[0].getRollScores()[0];
+    } else {
+      return 0;
+    }
+  } else {
+    return 0;
+  }
+};
+
+
+
+
+
+Frame.prototype.getRollScores = function(){
+  result=[];
+  for(i=0; i<this.rolls.length; i++){
+    result.push(this.rolls[i].numFelledPins());
+  }
+  return result;
+};
+
+
+Frame.prototype.getTotal = function(){
+  return this.getScore() + this.getBonus();
 };
 
 Frame.prototype.getScore = function(){
   return this.score;
 };
 
-Frame.prototype.getRolls = function(){
-  return this.rolls;
+
+Frame.prototype.pushNextFrame = function(frame){
+  this.nextFrames.push(frame);
 };
 
 Frame.prototype.roll = function(){
--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -3,138 +3,162 @@
 describe("Frame", function(){
 
   var frame;
-  var roll;
-  var firstRoll, secondRoll;
+  var firstRoll  = {
+    numStandingPins:  function() { return 5; },
+    numFelledPins:    function() { return 5; },
+    isSpare:          function() { return false; },
+    isStrike:         function() { return false; }
+  };
+
+  var crapRoll  = {
+    numStandingPins:  function() { return 10; },
+    numFelledPins:    function() { return 0; },
+    isSpare:          function() { return false; },
+    isStrike:         function() { return false; }
+  };
+
+  var spareRoll = {
+    numStandingPins:  function() { return 0; },
+    numFelledPins:    function() { return 10; },
+    isSpare:          function() { return true; },
+    isStrike:         function() { return false; }
+  };
+
+  var strikeRoll = {
+    numStandingPins:  function() { return 0; },
+    numFelledPins:    function() { return 10; },
+    isSpare:          function() { return false; },
+    isStrike:         function() { return true; }
+  };
+
+  var strikeFrame = {
+    getScore:         function() { return 10; },
+    isSpare:          function() { return false; },
+    isStrike:         function() { return true; },
+    getRollScores:    function() { return [10]; }
+  };
+
+  var spareFrame = {
+    getScore:         function() { return 10; },
+    isSpare:          function() { return true; },
+    isStrike:         function() { return false; },
+    getRollScores:    function() { return [5,5]; }
+  };
+
+  var averageFrame = {
+    getScore:         function() { return 5; },
+    isSpare:          function() { return false; },
+    isStrike:         function() { return false; },
+    getRollScores:    function() { return [3,2]; }
+  };
+
+  var crapFrame = {
+    getScore:         function() { return 0; },
+    isSpare:          function() { return false; },
+    isStrike:         function() { return false; },
+    getRollScores:    function() { return [0,0]; }
+  };
 
   describe("default", function(){
 
     beforeEach(function(){
-      roll  = {
-        numStandingPins: function() {
-          return 5;
-        },
-        numFelledPins: function() {
-          return 5;
-        },
-        isSpare: function() {
-          return false;
-        },
-        isStrike: function() {
-          return false;
-        }
-      };
 
-      spyOn(Roll, "createInstance").and.returnValue(roll);
+      spyOn(Roll, "createInstance").and.returnValue(firstRoll);
       frame = new Frame(Roll);
     });
 
     it("has a zero score", function(){
-      expect(frame.getScore()).toEqual(0);
-    });
-
-    it("has no rolls", function(){
-      expect(frame.getRolls()).toEqual([]);
+      expect(frame.getTotal()).toEqual(0);
     });
 
     it("can create rolls", function(){
-      expect(frame.createRoll()).toEqual(roll);
+      expect(frame.createRoll()).toEqual(firstRoll);
     });
   });
 
   describe("first roll is a strike", function(){
     beforeEach(function(){
-      firstRoll  = {
-        numStandingPins: function() {
-          return 0;
-        },
-        numFelledPins: function() {
-          return 10;
-        },
-        isSpare: function() {
-          return false;
-        },
-        isStrike: function() {
-          return true;
-        }
-      };
-
-      spyOn(Roll, "createInstance").and.returnValue(firstRoll);
+      spyOn(Roll, "createInstance").and.returnValue(strikeRoll);
       frame = new Frame(Roll);
       frame.roll();
     });
-
     it("#isStrike", function(){
       expect(frame.isStrike()).toEqual(true);
     });
-
     it("#isSpare", function(){
       expect(frame.isSpare()).toEqual(false);
     });
-
     it("#isComplete", function(){
       expect(frame.isComplete()).toEqual(true);
     });
-
     it("score = 10", function(){
-      expect(frame.getScore()).toEqual(10);
+      expect(frame.getTotal()).toEqual(10);
     });
   });
 
   describe("first roll is not a strike, second is a spare", function(){
-
     beforeEach(function(){
-      firstRoll  = {
-        numStandingPins: function() {
-          return 10;
-        },
-        numFelledPins: function() {
-          return 0;
-        },
-        isSpare: function() {
-          return false;
-        },
-        isStrike: function() {
-          return false;
-        },
-        id: 'roll 1'
-      };
-      secondRoll  = {
-        numStandingPins: function() {
-          return 0;
-        },
-        numFelledPins: function() {
-          return 10;
-        },
-        isSpare: function() {
-          return true;
-        },
-        isStrike: function() {
-          return false;
-        },
-        id: 'roll 2'
-    };
-
-      spyOn(Roll, "createInstance").and.returnValues(firstRoll, secondRoll);
-
+      spyOn(Roll, "createInstance").and.returnValues(crapRoll, spareRoll);
       frame = new Frame(Roll);
       frame.roll();
       frame.roll();
     });
-
     it("#isStrike", function(){
       expect(frame.isStrike()).toEqual(false);
     });
-
     it("#isSpare", function(){
       expect(frame.isSpare()).toEqual(true);
     });
-
     it("#isComplete", function(){
       expect(frame.isComplete()).toEqual(true);
     });
-
     it("score = 10", function(){
-      expect(frame.getScore()).toEqual(10);
+      expect(frame.getTotal()).toEqual(10);
+    });
+  });
+
+  describe("calculates bonuses from subsequent frames:", function(){
+    describe("strike", function(){
+      beforeEach(function(){
+        spyOn(Roll, "createInstance").and.returnValue(strikeRoll);
+        frame = new Frame(Roll);
+        frame.roll();
+      });
+
+      it(" followed by two strikes: score = 30", function(){
+        frame.pushNextFrame(strikeFrame);
+        frame.pushNextFrame(strikeFrame);
+        expect(frame.getTotal()).toEqual(30);
+      });
+
+      it(" followed by strike followed by non-strike: score = 23", function(){
+        frame.pushNextFrame(strikeFrame);
+        frame.pushNextFrame(averageFrame);
+        expect(frame.getTotal()).toEqual(23);
+      });
+
+      it(" followed by non-strike: score = 15", function(){
+        frame.pushNextFrame(averageFrame);
+        expect(frame.getTotal()).toEqual(15);
+      });
+    });
+
+    describe("spare", function(){
+      beforeEach(function(){
+        spyOn(Roll, "createInstance").and.returnValues(crapRoll, spareRoll);
+        frame = new Frame(Roll);
+        frame.roll();
+        frame.roll();
+      });
+      it("followed by strike: score = 20", function(){
+        frame.pushNextFrame(strikeFrame);
+        expect(frame.getTotal()).toEqual(20);
+      });
+
+      it("followed by non-strike: score = 13", function(){
+        frame.pushNextFrame(averageFrame);
+        expect(frame.getTotal()).toEqual(13);
+      });
     });
   });
 });

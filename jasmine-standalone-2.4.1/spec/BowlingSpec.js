10d1bcd9d65f7d9d8c55f804a6c19e88102df560
Patrick <p_donati@hotmail.com>
2016-04-02 18:15:03 +0100

    can kinda not hit a strike

--- jasmine-standalone-2.4.1/spec/BowlingSpec.js
+++ jasmine-standalone-2.4.1/spec/BowlingSpec.js
@@ -16,8 +16,8 @@ describe('Bowling', function (){
       expect(bowling.getCurrentFrame()).toEqual(1);
     });
 
-    xit('has a maximum frame', function(){
-
+    it('has a defualt bowl number', function(){
+      expect(bowling.getCurrentBowl()).toEqual(1);
     });
 
     it('begins with a score of zero', function(){
@@ -29,20 +29,44 @@ describe('Bowling', function (){
     beforeEach(function(){
       bowling.bowl(10);
     });
+
     it('records the pins hit when a user bowls', function(){
       expect(bowling.getStandingPins()).toEqual(0);
     });
+
     it('updates the frame when no more pins are standing', function(){
       expect(bowling.getCurrentFrame()).not.toEqual(1);
     });
+
     it('updates the score with the number of fallen pins', function(){
       expect(bowling.getCurrentScore()).toEqual(10);
     });
+
+    it('does not update the bowl number', function(){
+      expect(bowling.getCurrentBowl()).toEqual(1);
+    });
+
   });
 
   describe('When a user does not bowl a strike', function(){
     beforeEach(function(){
       bowling.bowl(5);
+    });
+
+    it('records the pins hit when a user bowls', function(){
+      expect(bowling.getStandingPins()).toEqual(5);
+    });
+
+    it('does not update the frame whilr pins are standing', function(){
+      expect(bowling.getCurrentFrame()).toEqual(1);
+    });
+
+    it('updates the score with the number of fallen pins', function(){
+      expect(bowling.getCurrentScore()).toEqual(5);
+    });
+
+    it('does updates the bowl number', function(){
+      expect(bowling.getCurrentBowl()).toEqual(2);
     });
   });
 });
--- jasmine-standalone-2.4.1/src/Bowling.js
+++ jasmine-standalone-2.4.1/src/Bowling.js
@@ -4,10 +4,12 @@ function Bowling(){
   var DEFAULT_STANDING_PINS = 10
   var DEFAULT_FRAME = 1
   var DEFAULT_SCORE = 0
+  var DEFAULT_BOWL = 1
 
   this._standingPins = DEFAULT_STANDING_PINS
   this._currentFrame = DEFAULT_FRAME
   this._currentScore = DEFAULT_SCORE
+  this._currentBowl = DEFAULT_BOWL
 }
 
 Bowling.prototype.getStandingPins = function (){
@@ -22,8 +24,19 @@ Bowling.prototype.getCurrentScore = function () {
   return this._currentScore
 };
 
+Bowling.prototype.getCurrentBowl = function (){
+  return this._currentBowl
+};
+
 Bowling.prototype.bowl = function (pins) {
-  this._standingPins -= pins
-  this._currentFrame += 1
-  this._currentScore += pins
+  if (pins === 10) {
+    this._standingPins -= pins
+    this._currentFrame += 1
+    this._currentScore += pins
+  } else {
+    this._standingPins -= pins
+    this._currentScore += pins
+    this._currentBowl += 1
+  }
+
 };

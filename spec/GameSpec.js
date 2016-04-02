b549b7f70d90784e21a02115c510f2e2b590905e
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-04-02 21:51:14 +0100

    First passing test

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,1 +1,15 @@
+'use strict';
 
+describe('Game', function(){
+
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  it('score starts at 0', function(){
+    expect(game.score).toEqual(0);
+  });
+
+});

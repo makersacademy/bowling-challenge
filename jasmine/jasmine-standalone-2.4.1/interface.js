8d241e1b196ecaa9965b1e95b661d2354548b741
phoebe <phoebe.greig@gmail.com>
2016-04-03 13:16:13 +0100

    added jquery

--- jasmine/jasmine-standalone-2.4.1/interface.js
+++ jasmine/jasmine-standalone-2.4.1/interface.js
@@ -1,24 +1,26 @@
-var bowling = new Bowling()
+
+
+var bowling = new Bowling();
 
 document.getElementById("totalScore").innerHTML= "Current Score: "+ bowling.currentScore
 
 function enterPinsHit(id) {
-    var pinsKnocked = prompt("Please enter how many pins you knocked down")
-    if(pinsKnocked=="" || pinsKnocked==null){
-    	enterPinsHit(id);
-    } else {
-	    document.getElementById(id).innerHTML = pinsKnocked;
-	    document.getElementById(id).setAttribute('value', pinsKnocked); 
+	cell = $('#'+id)
 
-	    bowling.updateScore(parseInt(pinsKnocked));
-	    
-		}
-
-		if(document.getElementById(id).value){
-			btn.style.visibility === "hidden";
-		}
-		
-
+  var pinsKnocked = prompt("Please enter how many pins you knocked down")
+  if(pinsKnocked=="" || pinsKnocked==null){
+    enterPinsHit(id);
+  } else {
+    document.getElementById(id).innerHTML = pinsKnocked;
+    document.getElementById(id).setAttribute('value', pinsKnocked); 
+    bowling.updateScore(parseInt(pinsKnocked));	   
+    $('#b'+id).hide(); 	
+  }
+	if(bowling.strike===1){
+		nextCell = $('#'+id).closest('td').next()[0].id
+		$('#b'+nextCell).hide();
+		console.log(nextCell);
+	}
 
 document.getElementById("totalScore").innerHTML= "Current Score: "+ bowling.currentScore
 

d960af9ab810d68adb66f7fdf7083243acc8233b
Shane Oston Stowe <shane@shaneoston.com>
2016-04-03 17:20:03 +0100

    added features 6-7

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,14 +10,14 @@ describe('Game', function() {
     game.throw(2);
     throws(19, 0);
     expect(game.score()).toBe(2);
-  })
+  });
 
   it('can score two throws which do not add up to a strike or spare', function() {
     game.throw(2);
     game.throw(7);
     throws(19, 0);
     expect(game.score()).toBe(9);
-  })
+  });
 
   it('can score a spare', function() {
     game.throw(2);
@@ -25,7 +25,7 @@ describe('Game', function() {
     game.throw(4);
     throws(17, 0);
     expect(game.score()).toBe(18);
-  })
+  });
 
   it('can score multiple spares', function() {
     game.throw(2);
@@ -36,7 +36,7 @@ describe('Game', function() {
     game.throw(6);
     throws(14, 0);
     expect(game.score()).toBe(37);
-  })
+  });
 
   it('can score a strike', function() {
     game.throw(10);
@@ -44,7 +44,28 @@ describe('Game', function() {
     game.throw(4);
     throws(17,0);
     expect(game.score()).toBe(24)
+  });
+
+  it('can score back to back strikes', function() {
+    game.throw(10);
+    game.throw(10);
+    throws(18, 0);
+    expect(game.score()).toBe(30);
+  });
+
+  it('can score a combination of spares and strikes', function() {
+    game.throw(7);
+    game.throw(3);
+    game.throw(10);
+    game.throw(0);
+    game.throw(10);
+    throws(14,0);
+    expect(game.score()).toBe(50);
   })
+
+  it('can score a full house of strikes', function() {
+    expect(game.score()).toBe(300);
+  });
 
   function throws(throws, pins) {
     for (i = 0; i < throws; i++) {
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,6 @@
 function Game() {
   this.rolls = [];
-};
+}
 
 Game.prototype.throw = function(pins) {
   if (pins === 10) {
@@ -9,21 +9,43 @@ Game.prototype.throw = function(pins) {
   } else {
     this.rolls.push(pins);
   };
-};
+}
 
 Game.prototype.score = function() {
+  var game = this;
+
   var result = 0;
   var rollTally = 0;
 
   for (var frame = 0; frame < 10; frame++) {
-    if ((this.rolls[rollTally] == 10) && (this.rolls[rollTally + 1] == 0)) {
-      result += this.rolls[rollTally] + this.rolls[rollTally + 1] + this.rolls[rollTally + 2] + this.rolls[rollTally + 3];
-    } else if (this.rolls[rollTally] + this.rolls[rollTally + 1] == 10) {
-      result += this.rolls[rollTally] + this.rolls[rollTally + 1] + this.rolls[rollTally + 2];
+    if (isStrike()) {
+      result += getStrikeScore();
+    } else if (isSpare()) {
+      result += getSpareScore();
     } else {
-      result += this.rolls[rollTally] + this.rolls[rollTally + 1];
+      result += getNormalScore();
     }
     rollTally += 2;
   }
   return result;
-};
+
+  function isSpare() {
+    return game.rolls[rollTally] + game.rolls[rollTally + 1] == 10
+  }
+
+  function isStrike() {
+    return (game.rolls[rollTally] == 10) && (game.rolls[rollTally + 1] == 0)
+  }
+
+  function getSpareScore() {
+    return game.rolls[rollTally] + game.rolls[rollTally + 1] + game.rolls[rollTally + 2];
+  }
+
+  function getStrikeScore() {
+    return game.rolls[rollTally] + game.rolls[rollTally + 1] + game.rolls[rollTally + 2] + game.rolls[rollTally + 3];
+  }
+
+  function getNormalScore() {
+    return game.rolls[rollTally] + game.rolls[rollTally + 1]
+  }
+}

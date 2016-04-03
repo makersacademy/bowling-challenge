55338df1a68ca74756624744b3be76f9518b99f2
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-03 17:19:07 +0100

    added spare and strike frame options to frame

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -2,19 +2,41 @@ describe("Frame", function() {
   var frame;
 
   beforeEach(function() {
-    frame = new Frame;
+    frame = new Frame();
+    frame.rollBall(4);
   });
 
   describe('rolling the ball', function() {
     it('sets first roll value', function() {
-      frame.rollBall(4);
       expect(frame.getFirstRoll()).toEqual(4);
-    })
+    });
     it('sets second roll value', function() {
-      frame.rollBall(4);
       frame.rollBall(5);
       expect(frame.getSecondRoll()).toEqual(5);
-    })
+    });
+    it('stops after 2 rolls', function() {
+      frame.rollBall(5);
+      frame.rollBall(3);
+      expect(frame.getSecondRoll()).toEqual(5);
+    });
+  });
+
+  describe('special frames', function() {
+    beforeEach(function() {
+      frame = new Frame();
+    });
+    it('can have a spare, when sum of rolls is all pins', function() {
+      frame.rollBall(6);
+      frame.rollBall(4);
+      expect(frame.isSpare()).toBeTruthy();
+      expect(frame.isStrike()).toBeFalsy();
+    });
+
+    it('can have a strike, when first roll is all pins', function() {
+      frame.rollBall(10);
+      expect(frame.isSpare()).toBeFalsy();
+      expect(frame.isStrike()).toBeTruthy();
+    });
   });
 
 });
--- src/Frame.js
+++ src/Frame.js
@@ -2,14 +2,28 @@ function Frame() {
   this._roll = 0;
   this._firstRoll = 0;
   this._secondRoll = 0;
+  this.ALL_PINS = 10;
 }
 
 Frame.prototype.rollBall = function(numberOfPins) {
-  if (this.isFirstRoll) {
+  if (this.isFirstRoll()) {
     this.setFirstRoll(numberOfPins);
     this.nextRoll();
+  } else if (this.isSecondRoll()) {
+    this.setSecondRoll(numberOfPins);
+    this.nextRoll();
+  } else {
+    return;
   }
 };
+
+Frame.prototype.isFirstRoll = function() {
+  return this._roll === 0;
+}
+
+Frame.prototype.isSecondRoll = function() {
+  return this._roll === 1;
+}
 
 Frame.prototype.setFirstRoll = function(numberOfPins) {
   this._firstRoll += numberOfPins;
@@ -19,10 +33,25 @@ Frame.prototype.getFirstRoll = function() {
   return this._firstRoll;
 };
 
-Frame.prototype.isFirstRoll = function() {
-  return this._roll === 0;
+Frame.prototype.setSecondRoll = function(numberOfPins) {
+  this._secondRoll += numberOfPins;
+}
+
+Frame.prototype.getSecondRoll = function() {
+  return this._secondRoll;
 }
 
 Frame.prototype.nextRoll = function() {
   this._roll += 1;
+}
+
+Frame.prototype.isSpare = function() {
+  if (this.isStrike()) {
+    return false;
+  }
+  return this.getFirstRoll() + this.getSecondRoll() === this.ALL_PINS;
+}
+
+Frame.prototype.isStrike = function() {
+  return this.getFirstRoll() === this.ALL_PINS;
 }

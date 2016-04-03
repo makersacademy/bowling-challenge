4417df1354c005fc30d8b7cfe12e834984d999d3
Jojo <joseph.sharratt@gmail.com>
2016-04-03 23:40:42 +0100

    Testing inSpare

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,9 +1,14 @@
 describe("Frame", function(){
 
   var frame;
+  var spareFrame;
+  var strikeFrame;
 
   beforeEach(function(){
-    frame = new Frame(10,0);
+    frame = new Frame(5, 2)
+    spareFrame = new Frame(7,3);
+    strikeFrame = new Frame(10,0);
+
   });
 
   it("receives the result of two rolls", function(){
@@ -14,4 +19,24 @@ describe("Frame", function(){
   it("expect total pins knocked down to be no more than 10", function(){
     expect(frame.roll1 + frame.roll2).toBeLessThan(11);
   });
+
+
+  it("tallies a score that is below 10",function(){
+    expect(frame.tally()).toEqual(7)
+    expect(frame.scorePending).toBe(false)
+    expect(frame.bonus).toEqual("none")
+  });
+
+  it("recognises when a spare has been rolled", function(){
+    expect(spareFrame.tally()).toEqual(10)
+    expect(spareFrame.scorePending).toBe(true)
+    expect(spareFrame.bonus).toEqual("spare")
+  });
+
+  it("recognises when a strike has been rolled", function(){
+    expect(strikeFrame.tally()).toEqual(10)
+    expect(strikeFrame.scorePending).toBe(true)
+    expect(strikeFrame.bonus).toEqual("strike")
+  });
+
 });
---
+++ user_stories.md
@@ -0,0 +1,32 @@
+```
+
+As a bowler
+So that I can begin a game
+I would like to create a scoresheet with my name
+
+
+As a bowler
+So that I can keep track of each frame
+I would like the result of each frame I play to be saved
+
+
+As a bowler
+So that I can know how well I’m doing
+I would like my total score to be updated each frame
+
+
+As a bowler
+So that I can be rewarded for high scoring frames
+I would like to receive appropriate bonuses for strikes and spares
+
+
+As a bowler
+So that I maximize my score
+I would like a spare in the tenth frame to trigger an additional roll
+
+
+As a bowler
+So that I maximize my score
+I would like a strike in the tenth frame to trigger two additional rolls
+
+```

0d3db9d1a79efe91a599e4c3ec5b77eed2dc9f47
Annemarie Kohler <annemarie.k89@gmail.com>
2016-04-10 21:09:42 +0100

    User can enter number of knocked down pins. This number is then displayed.

--- public/scripts/interface.js
+++ public/scripts/interface.js
@@ -1,3 +1,10 @@
 $( document ).ready(function() {
 
-}
+  $( "#submitPins" ).click(function() {
+    var numberOfPins = $( "#enterPins" ).val();
+    $( "#score" ).append( numberOfPins );
+    $( "#enterPins" ).val("");
+    // $( "input[name*='']" ).val( "has man in it!" );
+  });
+
+});

144efd5b1a170c974d1333ac11cb136ac795cf92
Murilo Dal Ri <murilodalri@gmail.com>
2016-04-10 15:55:46 +0100

    restarted

--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,38 @@
 function Game(){
-  this.currentFrame = new Frame();
-  this.framesPlayed = 0;
-  this._frames = [];
-  this.score = 0;
-  this.bonusShots = 0;
+  this.rolls = [];
+  this.currentRoll = 0;
 }
 
-Game.prototype.nextFrame = function () {
-  // checkLastFrame();
-  // checkFrameOver();
-  // checkSpare();
-  // checkStrike();
-  // addScore();
-  this.framesPlayed ++;
-  // addFrame();
+Game.prototype.roll = function(pins){
+  this.rolls[this.currentRoll++] = pins;
 };
 
-Game.prototype.addScore = function () {
-  this.score += this.currentFrame.score
+Game.prototype.score = function(){
+  var index = 0;
+  var score = 0;
+  for (var frame = 0; frame < 10; frame++){
+    if(this.isStrike(index)){
+      score += 10 + this.strikeBonus(index);
+      index++;
+    } else if(this.isSpare(index)){
+      score += 10 + this.spareBonus(index);
+      index += 2;
+    }
+  }
+}
+
+Game.prototype.isStrike = function (index) {
+  return this.rolls[index] === 10;
+};
+
+Game.prototype.isSpare = function (index) {
+  return this.rolls[index] + this.rolls[index++] === 10;
+};
+
+Game.prototype.strikeBonus = function (index) {
+  return this.rolls[index++] + this.rolls[index+2];
+};
+
+Game.prototype.spareBonus = function (index) {
+  return this.rolls[index+2]
 };

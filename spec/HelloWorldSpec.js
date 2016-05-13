e65d426fb252b49176257b9b3e63b3b71f656ec9
Omar Alvarez <omajul2@hotmail.com>
2016-05-13 10:46:55 +0200

    Update project tree

---
+++ spec/HelloWorldSpec.js
@@ -0,0 +1,31 @@
+describe("Hello World", function(){
+  var x = 4;
+  var y = 5;
+
+  // Making my own matcher
+  beforeEach(function(){
+    jasmine.addMatchers({
+      toBeDivisibleByTwo: function(){
+        return (this.actual % 2) === 0;
+      }
+    }); 
+  });
+
+  // Equal matcher
+  it("says hello", function(){
+    expect(helloWorld()).toEqual("Hello World!");    
+  }); 
+  
+  // Test if the string contains the word
+  it("says hello", function(){
+    expect(helloWorld()).toContain("World!");    
+  });
+  
+  it("test inequality", function(){
+    expect(x).not.toEqual(y);        
+  });
+  
+  xit("is divisible by two", function(){
+    expect(4).toBeDivisibleByTwo();
+  });
+}); 

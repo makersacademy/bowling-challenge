df83d054447e834a36a707e8ef522c1e0fcaacbf
Max Provin <maxprovin@outlook.com>
2016-05-14 22:44:32 +0100

    Frame logs spares and strikes

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -6,15 +6,34 @@ describe("Frame", function() {
     frame = new Frame()
   })
 
-  it("should log the score of a roll", function() {
-    frame.rollOne(5)
-    expect(frame.score()).toEqual(5)
+  describe('roll count', function() {
+    it('equals 1 after one roll', function() {
+      frame.rollOne(5)
+      expect(frame.rollNumber()).toEqual(1)
+    })
+
+    it("equals 2 after two rolls", function() {
+      frame.rollOne(5)
+      frame.rollTwo(1)
+      expect(frame.rollNumber()).toEqual(2)
+    })
   })
 
   it("should sum the score of the first and second roll", function() {
     frame.rollOne(4)
     frame.rollTwo(3)
     expect(frame.score()).toEqual(7)
+  })
+
+  it('player gets a spare if score of 10 after 2 rolls', function() {
+    frame.rollOne(5)
+    frame.rollTwo(5)
+    expect(frame.score()).toEqual('/')
+  })
+
+  it('player gets a strike if score of 10 on first roll', function() {
+    frame.rollOne(10)
+    expect(frame.score()).toEqual('X')
   })
 
 })
--- src/Frame.js
+++ src/Frame.js
@@ -1,15 +1,24 @@
 function Frame() {
   this._score = 0
+  this._rollNum = 0
 }
 
 Frame.prototype.rollOne = function(score) {
   this._score = score
+  this._rollNum = 1
 }
 
 Frame.prototype.rollTwo = function(score) {
   this._score += score
+  this._rollNum = 2
 };
 
 Frame.prototype.score = function() {
+  if(this._score === 10 && this._rollNum === 1) { return 'X' }
+  if(this._score === 10 ) { return '/' }
   return this._score
 }
+
+Frame.prototype.rollNumber = function() {
+  return this._rollNum
+};

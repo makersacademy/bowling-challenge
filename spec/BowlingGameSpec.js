974afe1396c4d315549c4f44b36e8f4f0f051ca4
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-14 01:17:32 +0100

    WIP thinking about program structure

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -3,27 +3,41 @@ describe("BowlingGame",function() {
   "use strict"
   var game;
 
+  beforeEach(function(){
+    game = new BowlingGame();
+
+  });
+
   describe('on new game, ',function(){
     it('score is 0',function() {
-      game = new BowlingGame();
-      expect(game.score()).toEqual(0);
+      expect(game.total_score()).toEqual(0);
     });
   });
 
-  describe('roll function, ',function(){
-    it('with 0 rolls, score is zero',function(){
-      game = new BowlingGame();
-      for (var i=0; i<20; i++) {
-        game.roll(0)
-      }
-      expect(game.score()).toEqual(0);
+  describe('rolls,  ',function(){
+    beforeEach(function(){
+      BowlingGame.prototype.rollMany = function(times, pins) {
+        for (var i=0; i<times; i++) {
+          game.roll(pins);
+        }
+      };
+    });
+    it('gutter game, score is zero',function(){
+      game.rollMany(20,0);
+      expect(game.total_score()).toEqual(0);
     });
 
     it('with rolls of 1, score is 20',function(){
-      for (var i=0; i<20; i++) {
-        game.roll(1);
-      }
-      expect(game.score()).toEqual(20);
+      game.rollMany(20,1);
+      expect(game.total_score()).toEqual(20);
+    });
+
+    it('F1R1:1, F1R2:9, (spare), F2R1: 5, F2R2: 0 gives score 20', function(){
+      game.roll(1);
+      game.roll(9);
+      game.roll(5);
+      game.rollMany(17,0);
+      expect(game.total_score()).toEqual(20);
     });
 
   });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,13 +1,37 @@
 function BowlingGame() {
 
-  var _score = 0;
+  var _total_score = 0;
+  var _rolls = [];
+  var ROLLS_Length = 21;
+  var FRAMES_Length = 10;
+  var currentRoll = 0;
 
-  BowlingGame.prototype.score = function(){
-    return _score;
+  BowlingGame.prototype.begin = function() {
+    for(var i=0;i<ROLLS_Length;i++) {
+      _rolls[i] = 0;
+    }
+  };
+
+  BowlingGame.prototype.total_score = function(){
+    return _total_score;
   };
 
   BowlingGame.prototype.roll = function(pins) {
-    _score += pins;
+    setRoll[currentRoll++] = pins;
+    if (isStrike(pins)) {
+      currentRoll++;
+    }
+    for (var i=0;i<FRAMES_Length;i++) {
+      var a;
+    }
+    _total_score += pins;
   };
 
+  function setRoll(index, pins) {
+    _rolls[index] = pins;
+  }
+
+  function isStrike(frame, pins) {
+    return pins === 10;
+  }
 }

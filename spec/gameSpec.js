00f2dec23d66bacae581f19fc1e668b2a41d9109
Amy Nicholson <missamynicholson@gmail.com>
2016-05-14 10:16:57 +0100

    First roll

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,9 +7,52 @@ describe("Feature test:", function() {
 
   describe("score at start", function() {
     it("player has zero points when game starts", function() {
-      expect(game.currentScore()).toEqual(0)
+      expect(game.cumulativeScore()).toEqual(0)
+    })
+
+    it("current frame number should be 1 at start", function() {
+      expect(game.frameNumber()).toEqual(1)
+    })
+
+    it("return true for being first roll in a frame", function() {
+      expect(game.isFirstRoll()).toBeTruthy()
     })
   })
 
+  describe("first roll", function() {
+    describe("when knock down three pins", function() {
+      it("adds 3 to score", function() {
+        game.roll(3)
+        expect(game.cumulativeScore()).toEqual(3)
+      })
+
+      it("return false for being first roll in a frame", function() {
+        game.roll(3)
+        expect(game.isFirstRoll()).toBeFalsy()
+      })
+    })
+
+    describe("when knock down 10 pins", function() {
+      it("add 10 to score", function() {
+        game.roll(10)
+        expect(game.cumulativeScore()).toEqual(10)
+      })
+
+      it("return true for being first roll in a frame", function() {
+        game.roll(10)
+        expect(game.isFirstRoll()).toBeTruthy()
+      })
+
+      it("return 2 for frame number", function() {
+        game.roll(10)
+        expect(game.frameNumber()).toEqual(2)
+      })
+
+      it("returns 2 bonus rolls if get a strike", function() {
+        game.roll(10)
+        expect(game.bonusRolls()).toEqual(2)
+      })
+    })
+  })
 
 })
--- src/game.js
+++ src/game.js
@@ -1,7 +1,36 @@
 function Game () {
-  this._score = 0
+  this._cumulativeScore = 0
+  this._frameNumber = 1
+  this._isFirstRoll = true
+  this._bonusRolls = 0
 }
 
-Game.prototype.currentScore = function() {
-  return this._score
+Game.prototype.cumulativeScore = function() {
+  return this._cumulativeScore
+}
+
+Game.prototype.frameNumber = function() {
+  return this._frameNumber
+}
+
+Game.prototype.bonusRolls = function() {
+  return this._bonusRolls
+}
+
+Game.prototype.isFirstRoll = function() {
+  return this._isFirstRoll
+}
+
+Game.prototype.roll = function(pinsDown) {
+  if (this.isAStrike(pinsDown)) {
+    this._bonusRolls = 2
+    this._frameNumber += 1
+  } else {
+    this._isFirstRoll = !this._isFirstRoll
+  }
+  this._cumulativeScore += pinsDown
+}
+
+Game.prototype.isAStrike = function(pinsDown) {
+  return (this.isFirstRoll() && pinsDown === 10)
 }

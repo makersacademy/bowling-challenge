81addf3b6805449e379de0afb37fbf4343a6e5f4
Max Provin <maxprovin@outlook.com>
2016-05-15 20:28:24 +0100

    dear god what the hell am I making...

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,14 @@
 function Game() {
-  this._frames = {'1':{'pins':0,'roll':0}, '2':{'pins':0,'roll':0},
-                  '3':{'pins':0,'roll':0}, '4':{'pins':0,'roll':0},
-                  '5':{'pins':0,'roll':0}, '6':{'pins':0,'roll':0},
-                  '7':{'pins':0,'roll':0}, '8':{'pins':0,'roll':0},
-                  '9':{'pins':0,'roll':0}, '10':{'pins':0,'roll':0}}
+  this._frames = {'1':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '2':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '3':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '4':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '5':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '6':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '7':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '8':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '9':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}},
+                  '10':{'r1':{'pins':0,'roll':0},'r2':{'pins':0,'roll':0}}}
   this._frameRollNumber = 0
   this._currentFrame = 1
   this._totalRolls = 0
@@ -19,13 +24,13 @@ Game.prototype.totalRolls = function() {
 
 Game.prototype.roll = function(score) {
   this.updateRollCounts()
-  this.updateFrameStats(score)
   if(this._frameRollNumber === 3) { this._frameRollNumber = 1}
+  this.updateFrameStats(score)
   if(this._frameRollNumber === 2 || score === 10) { this._currentFrame ++ }
 }
 
 Game.prototype.frameScore = function(frameNumber) {
-  return this._frames[frameNumber]['pins']
+  return (this._frames[frameNumber]['r1']['pins'] + this._frames[frameNumber]['r2']['pins'])
 }
 
 Game.prototype.currentFrame = function() {
@@ -43,14 +48,16 @@ Game.prototype.totalScore = function() {
   return score
 }
 
+Game.prototype.updateFrameStats = function(score) {
+  if(this._frameRollNumber === 1) {this._frames[this._currentFrame]['r1']['pins'] = score;
+      this._frames[this._currentFrame]['r1']['roll'] += this._totalRolls}
+  if(this._frameRollNumber === 2) {this._frames[this._currentFrame]['r2']['pins'] = score;
+      this._frames[this._currentFrame]['r2']['roll'] += this._totalRolls}
+}
+
 Game.prototype.updateRollCounts = function() {
   this._frameRollNumber ++
   this._totalRolls ++
-}
-
-Game.prototype.updateFrameStats = function(score) {
-  this._frames[this._currentFrame]['pins'] += score
-  this._frames[this._currentFrame]['rolls'] += this._totalRolls
 }
 
 

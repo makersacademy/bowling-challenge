c2611b55aac9d412a1b0c8aca0619dac7863d188
Ben Stein <bumstain@gmail.com>
2016-05-15 14:08:25 +1000

    Frames can be added to a game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,10 +1,13 @@
 'use strict';
 
 describe("Game", function(){
-var game;
+  var game;
+  var frame = jasmine.createSpyObj('frame', ['isEmpty', 'true', 'false'])
 
   beforeEach(function(){
     game = new Game();
+    frame.isEmpty.and.returnValue(true);
+    game.addFrames(frame);
   });
 
   it('opens with a starting score of zero', function() {
@@ -13,6 +16,10 @@ var game;
 
   it('opens with empty frames', function() {
     expect(new Game()._frames).toEqual([]);
+  });
+
+  it('has a maximum of ten frames', function() {
+    expect(game._frames.length).toEqual(10);
   });
 
  
--- src/game.js
+++ src/game.js
@@ -2,3 +2,9 @@ function Game(){
   this._score = 0;
   this._frames = [];
 }
+
+Game.prototype.addFrames = function(frame){
+  for(var f = 0; f < 10; f++) {
+    this._frames.push(frame);
+  }
+}

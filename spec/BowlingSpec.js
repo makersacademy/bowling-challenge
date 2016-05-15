54ffe591058966ed09600923d5785efb29b0e237
Matthew Ward <iammatthewward@gmail.com>
2016-05-15 17:18:06 +0100

    Basic scoring complete

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -12,7 +12,17 @@ describe('Bowling', function(){
 
   describe('Score', function(){
     it('Score begins at 0', function(){
-      expect(bowling.score).toEqual(0);
+      expect(bowling._score).toEqual(0);
+    });
+    it('Score is kept in an object', function(){
+      bowling.takeTurn(1);
+      bowling.takeTurn(2);
+      expect(bowling.scoreByFrame.frame1).toEqual([1,2]);
+    });
+    it('Running total score is kept', function(){
+      bowling.takeTurn(1);
+      bowling.takeTurn(2);
+      expect(bowling.totalScore()).toEqual(3);
     });
   });
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -3,5 +3,54 @@
 }());
 
 function Bowling() {
-  this.score = 0;
+  this._score = 0;
+  this._currentFrameScore = [];
+  this._currentFrame = 1;
+  this._currentBall = 1;
+  this.scoreByFrame = {};
 }
+
+Bowling.prototype.takeTurn = function(pins) {
+  if (this._isFirstBall()) {
+    this._logFirstBall(pins);
+  } else {
+    this._logSecondBall(pins);
+  }
+}
+
+Bowling.prototype.totalScore = function() {
+  return this._score;
+};
+
+//PRIVATE
+
+Bowling.prototype._logFirstBall = function(pins) {
+  this._currentFrameScore.push(pins);
+  this._currentBall = 2;
+}
+
+Bowling.prototype._logSecondBall = function(pins) {
+  this._currentFrameScore.push(pins);
+  this._updateFrameScores();
+  this._endTurn();
+}
+
+Bowling.prototype._updateFrameScores = function() {
+  this.scoreByFrame["frame" + this._currentFrame] = this._currentFrameScore;
+  this._updateTotalScore();
+};
+
+Bowling.prototype._updateTotalScore = function() {
+  for (var i = 0; i < this._currentFrameScore.length; i ++) {
+    this._score += this._currentFrameScore[i];
+  }
+};
+
+Bowling.prototype._endTurn = function() {
+  this._currentFrameScore = [];
+  this._currentBall = 1;
+};
+
+Bowling.prototype._isFirstBall = function() {
+  return (this._currentBall === 1);
+};

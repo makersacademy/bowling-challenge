005ae3e4301d85960e145d0c44b7c165040d512c
Max Provin <maxprovin@outlook.com>
2016-05-15 19:19:34 +0100

    modified scoring mechanic slightly

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -48,6 +48,31 @@ describe('Game', function() {
     expect(game.totalRolls()).toEqual(9)
   })
 
+  it('Total score is the sum of all frame scores', function() {
+    for (var i = 0; i < 10; i++) {
+      game.roll(1)
+    }
+    expect(game.totalScore()).toEqual(10)
+  })
+
+  describe('spare', function() {
+    it('next roll gets added to spare frames score', function() {
+      game.roll(5)
+      game.roll(5)
+      game.roll(4)
+      game.roll(3)
+      expect(game.frameScore(1)).toEqual(14)
+    })
+  })
+
+    describe('strike', function() {
+    it('next 2 rolls gets added to strike frames score', function() {
+      game.roll(10)
+      game.roll(5)
+      game.roll(4)
+      expect(game.frameScore(1)).toEqual(19)
+    })
+  })
 
 })
 
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 function Game() {
-  this._frames = {'1':0, '2':0, '3':0, '4':0, '5':0,
-                  '6':0, '7':0, '8':0, '9':0, '10':0}
+  this._frames = {'1':{'r1':0,'r2':0}, '2':{'r1':0,'r2':0}, '3':{'r1':0,'r2':0},
+                  '4':{'r1':0,'r2':0}, '5':{'r1':0,'r2':0}, '6':{'r1':0,'r2':0},
+                  '7':{'r1':0,'r2':0}, '8':{'r1':0,'r2':0}, '9':{'r1':0,'r2':0},
+                  '10':{'r1':0,'r2':0}}
   this._frameRollNumber = 0
   this._currentFrame = 1
   this._totalRolls = 0
@@ -15,21 +17,40 @@ Game.prototype.totalRolls = function() {
 }
 
 Game.prototype.roll = function(score) {
-  this._frames[this._currentFrame] += score
-  this._frameRollNumber ++
-  this._totalRolls ++
+  this.updateRollCounts()
   if(this._frameRollNumber === 3) { this._frameRollNumber = 1}
+  this.updateFrameScore(score)
   if(this._frameRollNumber === 2 || score === 10) { this._currentFrame ++ }
 }
 
 Game.prototype.frameScore = function(frameNumber) {
-  return this._frames[frameNumber]
+  return (this._frames[frameNumber]['r1'] + this._frames[frameNumber]['r2'])
 }
 
 Game.prototype.currentFrame = function() {
   return this._currentFrame
 }
 
+Game.prototype.totalScore = function() {
+  self = this
+  var score = 0
+  var frame = 0
+  for (var i = 0; i < 10; i++) {
+    frame ++
+    score += self.frameScore(frame)
+  }
+  return score
+}
+
+Game.prototype.updateFrameScore = function(score) {
+  if(this._frameRollNumber === 1) {this._frames[this._currentFrame]['r1'] = score}
+  if(this._frameRollNumber === 2) {this._frames[this._currentFrame]['r2'] = score}
+}
+
+Game.prototype.updateRollCounts = function() {
+  this._frameRollNumber ++
+  this._totalRolls ++
+}
 
 
 

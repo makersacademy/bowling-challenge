b6217b4f30750e96d84b697454f83897aa0e7668
Mara Wanot <m.d.wanot@hotmail.com>
2016-05-17 21:06:31 +0100

    basic set up, bowls and pins constant

---
+++ src/bowling.js
@@ -0,0 +1,3 @@
+function Bowling() {
+  this.ROUNDS = 10;
+}
---
+++ src/round.js
@@ -0,0 +1,7 @@
+function Round() {
+  this.MAX_BOWLS = 2;
+  this.MAX_PINS = 10;
+
+  this.bowl = this.MAX_BOWLS;
+  this.pins = this.MAX_PINS;
+}

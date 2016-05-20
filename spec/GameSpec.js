bddc0bf82dd8239f58bffd10991b94527b4d1cd7
Lucy Clarke <l.l.c.clarke@gmail.com>
2016-05-20 15:48:38 +0100

    more tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,13 +3,35 @@ describe('Game', function(){
 
     beforeEach(function(){
       game = new Game();
+      round = new Round();
     });
 
     describe('when a game starts', function(){
       it('should have 10 rounds', function(){
-        expect(game.ROUNDS).toEqual(10);
+        expect(game.rounds).toEqual(10);
       });
 
+      it('should call a new round', function(){
+
+
+      });
+
+      it('should remove a round when a new round is called', function(){
+        game.newRound();
+        expect(game.rounds).toEqual(9);
+
+      });
+
+      it('should save pins from round', function(){
+        spyOn(Math, 'random').and.returnValue(.1);
+        round.roll;
+        expect(game.score).toEqual(1);
+
+      });
+
+
     });
+
+
 
 });

c0bb6ea7986b9e163f6dd16fb75b8ad64c514c74
Lexi Tran <hazukitran@gmail.com>
2016-05-21 19:23:03 +0700

    Setting up calculate scores

--- bowling-game/spec/gameSpec.js
+++ bowling-game/spec/gameSpec.js
@@ -11,9 +11,22 @@ describe("Game", function(){
 		expect(game.currentFrameNumber()).toEqual(1);
 		game.addScore(10);
 		expect(game.currentFrameNumber()).toEqual(2);
-
 		game.addScore(3);
 		game.addScore(4);
 		expect(game.currentFrameNumber()).toEqual(3);
+	});
+
+	it('calculates the score in each frame', function(){
+		game.addScore(4);
+		game.addScore(5);
+		expect(game.calculateScores()).toEqual(9);
+		game.addScore(6);
+		game.addScore(4);
+		game.addScore(5);
+		expect(game.calculateScores()).toEqual(24);
+		game.addScore(10);
+		game.addScore(4);
+		game.addScore(5);
+		expect(game.calculateScores()).toEqual(43);
 	});
 });
--- bowling-game/src/game.js
+++ bowling-game/src/game.js
@@ -1,10 +1,35 @@
+"use strict";
+
 function Game(){
-	this.frame = [];
+	this.frames = [];
+	this.currentFrame = 1;
+	this.countScore = 0;
 
-	for (i = 0; i < 10; i++) {
-		this.frame.push(new Frame());
+	for (var i = 0; i < 10; i++) {
+		this.frames.push(new Frame());
 	}
-
-	this.frame[0].score
 };
 
+Game.prototype.currentFrameNumber = function(){
+	return this.currentFrame;
+}
+
+Game.prototype.addScore = function(number){
+	this.frames[this.currentFrame - 1].addScore(number);
+
+	if (this.frames[this.currentFrame - 1].isComplete()) {
+ 		this.currentFrame += 1;
+	}
+}
+
+Game.prototype.calculateScores = function(){
+	for (var i = 0; i < 10; i++) {
+		if (this.frames[i].isStrike()) {
+			console.log(this.frames[i].score() + 10 + this.frames[i + 1].score());
+		} else if (this.frames[i].isSpare()) {
+			console.log(this.frames[i].score() + 10 + this.frames[i + 1].rolls[0]);
+		} else {
+			console.log(this.frames[i].score());
+		}
+	}
+}

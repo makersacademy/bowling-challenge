00efad62781d1352599f19a245aedeb8bdb920a3
letianw <tmac4ever0824@gmail.com>
2016-05-21 22:51:43 +1000

    hello

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,9 +1,26 @@
+Array.prototype.last = function(){
+  return this[this.length-1];
+}
+
 function Bowling(){
   this._frames = [];
 }
 
-Bowling.prototype.roll = function(){
-  if (this.isFinished) {
-    //do something
+Bowling.prototype.roll = function(pins){
+  if (this._frames.length === 0 || this._frames.last.isFinished()) {
+    this._frames.push(new Frame());
+    this._frames.last.recordFirstRoll(pins);
+  } else {
+    this._frames.last.recordSecondRoll(pins);
   }
+}
+
+Bowling.prototype.gameScore = function(){
+  var sum = 0
+  this._frames.forEach(function(frame){
+    if (frame.isAllBonusAdded()){
+      sum += frame.calculateTotal();
+    }
+  });
+  return sum;
 }

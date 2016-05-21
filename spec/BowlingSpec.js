ecfd6d0e848700ed24b5b8ac3e01d51221160a64
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-21 10:37:34 +0100

    can roll a spare

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -8,14 +8,28 @@ describe("Bowling", function () {
     bowling = new Bowling();
   });
 
-  it("should return a score after the first bowl", function () {
-    bowling.firstBowl()
-    expect(bowling.score).toEqual(5);
+  it("can roll a gutter game", function () {
+    rollMany(0,20)
+    expect(bowling.score()).toEqual(0)
   });
 
   it("should respond to the random score generator", function () {
     spyOn(Math, "random").and.returnValue(0.5)
     expect(bowling.randomScore()).toEqual(5)
   });
+
+  it("can roll a spare", function () {
+    bowling.roll(4)
+    bowling.roll(6)
+    bowling.roll(2)
+    rollMany(0,17)
+    expect(bowling.score()).toEqual(14)
+  });
+
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      bowling.roll(pins)
+    }
+  }
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,14 +1,51 @@
 'use strict';
 
 function Bowling() {
-  this.score = 0
   this.STARTING_PINS = 10
+  this.rolls = [];
 }
 
-Bowling.prototype.firstBowl =  function() {
-  this.score += 5
+Bowling.prototype.roll = function(pins) {
+  this.rolls.push(pins)
+};
+
+Bowling.prototype.score =  function() {
+  var result = 0
+  var rollIndex = 0
+  var bowling = this
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++){
+    if (isSpare()){
+      result = this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2]
+    } else {
+result += this.rolls[rollIndex] + this.rolls[rollIndex + 1]
+}
+rollIndex += 2
+}
+return result
+
+function isSpare() {
+  return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] == 10
+}
 }
 
 Bowling.prototype.randomScore = function() {
   return Math.floor((Math.random()) * 11);
 }
+
+
+Bowling.prototype.strikeScore = function() {
+
+}
+
+// for (var frameIndex = 0; frameIndex < 10; frameIndex++)
+
+Bowling.prototype.spareScore = function() {
+
+}
+
+var rollMany = function (pins, rolls) {
+  for (var i = 0; i < rolls; i++) {
+
+  }
+}
+

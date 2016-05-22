aa0f78befd79a621f77c6dec13c592f5acd307a9
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-22 22:19:49 +0100

    Frame tracks: pins knocked down, strike status, spare status

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -3,12 +3,49 @@
 describe('Frame', function(){
   var frame;
 
-  describe('tracks 2 rolls', function(){
+  beforeEach(function(){
+    frame = new Frame();
+  });
+
+  describe('tracks rolls', function(){
     it('tracks 2 rolls', function(){
-      frame = new Frame;
       frame.roll(5);
-      frame.roll(8);
-      expect(frame.getRolls()).toEqual([5,8]);
+      frame.roll(2);
+      expect(frame.getRolls()).toEqual([5,2]);
+    });
+
+    it('prevents more than 2 rolls', function(){
+      frame.roll(5);
+      frame.roll(2);
+      expect(function() {
+        frame.roll(1);
+      }).toThrowError('Exceeded maximum rolls');
+
     });
   });
+  describe('records number of pins', function() {
+
+    it('tracks a strike', function(){
+      frame.roll(10);
+      expect(frame.isStrike()).toEqual(true);
+    });
+
+    it('tracks a spare', function(){
+      frame.roll(4);
+      frame.roll(6);
+      expect(frame.isSpare()).toEqual(true);
+    });
+
+    it('returns the number of pins knocked down', function(){
+      frame.roll(2);
+      frame.roll(7);
+      expect(frame.getPins()).toEqual(9);
+    });
+
+    // it('tracks max of 10 pins', function(){
+    //
+    // });
+
+  });
+
 });
--- src/frame.js
+++ src/frame.js
@@ -1,13 +1,29 @@
 'use strict';
 
 function Frame(){
-  this.rolls = []
+  this.rolls = [];
+  this.totalPins = 10;
 }
 
 Frame.prototype.roll = function(noOfPins) {
+  if(this.rolls.length >= 2){
+    throw new Error('Exceeded maximum rolls');
+  };
   this.rolls.push(noOfPins);
 };
 
 Frame.prototype.getRolls = function() {
   return this.rolls;
+};
+
+Frame.prototype.getPins = function() {
+  return this.getRolls().reduce((previousValue,currentValue) => previousValue + currentValue);
+};
+
+Frame.prototype.isStrike = function() {
+  return this.rolls[0] === this.totalPins;
+};
+
+Frame.prototype.isSpare = function() {
+  return (this.rolls[0] != this.totalPins) && (this.getPins() === this.totalPins);
 };

f41983ac4d508a13b00a95de61eab210e95e8640
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-22 20:35:56 +0100

    new branch

---
+++ src/Bowling.js
@@ -0,0 +1,75 @@
+"use strict"
+
+function Bowling() {
+  this.rolls = [];
+  this.frameIndex = 0
+  this.gameOver = false
+}
+
+Bowling.prototype.roll = function(pins) {
+  this.rolls.push(pins)
+}
+
+Bowling.prototype.endGame = function() {
+  if (this.rolls.length = 20) {
+    this.gameOver = true
+  }
+  }
+
+
+Bowling.prototype.score =  function() {
+  var result = 0
+  var rollIndex = 0
+  var bowling = this
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++){
+    if (isStrike()) {
+      result += getStrikeScore()
+      rollIndex++
+    } else if (isSpare()){
+      result += getSpareScore()
+      rollIndex += 2
+    } else {
+      result += getNormalScore()
+      rollIndex += 2
+  }
+}
+  return result
+
+
+  function isStrike() {
+    return bowling.rolls[rollIndex] === 10
+  }
+
+  function getStrikeScore() {
+   return bowling.rolls[rollIndex]+
+   bowling.rolls[rollIndex + 1]+
+   bowling.rolls[rollIndex + 2]
+ }
+
+ function isSpare() {
+  return bowling.rolls[rollIndex] +
+  bowling.rolls[rollIndex + 1] === 10
+}
+
+function getSpareScore() {
+  return bowling.rolls[rollIndex]+
+  bowling.rolls[rollIndex + 1] +
+  bowling.rolls[rollIndex + 2]
+}
+
+function getNormalScore() {
+  return bowling.rolls[rollIndex] +
+  bowling.rolls[rollIndex + 1]
+}
+
+
+var rollMany = function (pins, rolls) {
+  for (var i = 0; i < rolls; i++) {
+  }
+}
+}
+
+
+
+
+

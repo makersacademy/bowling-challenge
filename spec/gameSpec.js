0d777468d242876a3484a0fddbc94f6c8d3abc2a
Apoorva Saxena <apsaxena24@gmail.com>
2016-06-25 15:33:14 +0100

    Created frame logic

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,7 +4,32 @@ describe("Game", function(){
   beforeEach(function(){
     game = new Game;
   });
+  describe("game start",function(){
   it("has a starting score of 0", function(){
     expect(game.gameScore()).toEqual(0);
+  });
+  it("starting frame is 1", function(){
+    expect(game.currentFrame()).toEqual(1);
+  });
+});
+  it("should allow the user to play",function(){
+    spyOn(Math, 'random').and.returnValue(.6);
+    expect(game.play()).toEqual(6);
+  });
+  it ('should allow the user to get duck', function(){
+    spyOn(Math, 'random').and.returnValue(0);
+    expect(game.play()).toEqual(0);
+  });
+  it('should have 2 rolls in each frame', function(){
+    game.play()
+    expect(game.currentFrame()).toEqual(1);
+  });
+  it('should not allow the user to play the eleventh frame', function(){
+    for(var i; i<21; i++)
+    {
+      game.increaseFrame();
+    }
+    expect(game.currentFrame()).toEqual(1);
+
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,6 +1,28 @@
 function Game() {
+  this.frame = 1;
+  this.score = 0;
 }
 
 Game.prototype.gameScore = function(value) {
-  return 0;
+  return this.score;
+};
+
+Game.prototype.currentFrame = function() {
+  return Math.floor(this.frame);
+};
+
+Game.prototype.play = function(){
+  var pinsStruck = Math.floor(Math.random() * (10 + 1));
+  return pinsStruck;
+};
+
+Game.prototype.increaseFrame = function(){
+  if(this.frame < 10)
+  {
+    this.frame += 0.5;
+  }
+  else {
+    this.frame = 1;
+    return this.frame;
+  }
 };

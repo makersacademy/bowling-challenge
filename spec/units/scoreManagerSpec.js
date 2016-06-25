62ba77d1307301e84e79de58a59fefd51d86f78e
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-25 18:50:23 +0100

    Implement scoreManager Model

--- spec/units/scoreManagerSpec.js
+++ spec/units/scoreManagerSpec.js
@@ -5,12 +5,17 @@ describe('ScoreManager', function() {
   var scoreManager;
   var frame;
 
-  beforeEach(function() {
-    scoreManager = new ScoreManager;
-    frame = jasmine.createSpy('frame');
-  });
+  describe('at the start of the game, with all rolls valid', function(){
 
-  describe('at the start of the game', function(){
+    beforeEach(function() {
+      frame = {
+        isRollValid: function(pins) { return true; },
+        isNewFrame: function() {},
+        isStrike: function() { return false; },
+        isSpare: function() { return false; }
+      };
+      scoreManager = new ScoreManager(frame);
+    });
 
     it('.currentFrame() returns 0', function() {
       expect(scoreManager.currentFrame()).toEqual(0);
@@ -31,6 +36,99 @@ describe('ScoreManager', function() {
 
     it('.isGameFinished() returns false', function() {
       expect(scoreManager.isGameFinished()).toEqual(false)
+    });
+  });
+
+
+  describe('at the start of the game, with all rolls invalid', function(){
+
+    beforeEach(function() {
+      frame = {
+        isRollValid: function(pins) { return false; } 
+      };
+      scoreManager = new ScoreManager(frame);
+    });
+
+    it('an invalid roll raises an error', function() {
+      expect(function() { scoreManager.roll(5); }).toThrow('Cannot roll: not enough pins');
+    });
+  });
+
+
+  describe('after 2 valid rolls, no bonus', function(){
+
+    beforeEach(function() {
+      frame = {
+        isRollValid: function(pins) { return true; },
+        isNewFrame: function() { return false; },
+        isSpare: function() { return false; },
+        isStrike: function() { return false; }
+      };
+      scoreManager = new ScoreManager(frame);
+      scoreManager.roll(3);
+      frame.isNewFrame = function() { return true; };
+      scoreManager.roll(4);
+    });
+
+    it('.currentFrame returns 1', function() {
+      expect(scoreManager.currentFrame()).toEqual(1);
+    });
+
+    it('.getScore returns the sum of the two rolls', function() {
+      expect(scoreManager.getScore()).toEqual(7);
+    });
+  });
+
+  describe('after 3 valid rolls, first frame was spare', function(){
+
+    beforeEach(function() {
+      frame = {
+        isRollValid: function(pins) { return true; },
+        isNewFrame: function() { return true; },
+        isSpare: function() { return false; },
+        isStrike: function() { return false; } 
+      };
+      scoreManager = new ScoreManager(frame);
+      scoreManager.roll(6);
+      frame.isNewFrame = function() { return false; };
+      scoreManager.roll(4);
+      frame.isNewFrame = function() { return true; };
+      frame.isStrike = function() { return false; };
+      frame.isSpare = function() { return true; };
+      scoreManager.roll(1);
+    });
+
+    it('.currentFrame returns 2', function() {
+      expect(scoreManager.currentFrame()).toEqual(2);
+    });
+
+    it('.getScore returns the sum of the three rolls plus third roll bonus', function() {
+      expect(scoreManager.getScore()).toEqual(12);
+    });
+  });
+
+  describe('after 3 valid rolls, first frame was strike', function(){
+
+    beforeEach(function() {
+      frame = {
+        isRollValid: function(pins) { return true; },
+        isNewFrame: function() { return true; },
+        isSpare: function() { return false; }, 
+        isStrike: function() { return false; } 
+      };
+      scoreManager = new ScoreManager(frame);
+      scoreManager.roll(10);
+      frame.isNewFrame = function() { return true; };
+      frame.isStrike = function() { return true; };
+      scoreManager.roll(4);
+      frame.isNewFrame = function() { return false; };
+      frame.isSpare = function() { return false; };
+      frame.isStrike = function() { return true; };
+      scoreManager.roll(1);
+    });
+
+    it('.getScore returns the sum of the three rolls plus second/third roll bonus', function() {
+      expect(scoreManager.getScore()).toEqual(20);
     });
   });
 });
--- src/scoreManager.js
+++ src/scoreManager.js
@@ -1,14 +1,18 @@
+//Understands how to keep track of the score in a game of 10-pin bowling
+
 'use strict';
 
-function ScoreManager() {
+function ScoreManager(frameModel = new Frame) {
+  this._frameModel = frameModel
   this._score = 0;
   this._gameFinished = false;
+  this._currentFrame = 0;
 };
 
 ScoreManager.prototype = {
   
   currentFrame: function() {
-    return 0;
+    return this._currentFrame;
   },
 
   getScore: function() {
@@ -16,14 +20,30 @@ ScoreManager.prototype = {
   },
 
   roll: function(pins) {
-    this._score += pins;
+    if (this.isRollValid(pins)) {
+      this._score += pins;
+      this.checkBonusPoints(pins);
+      if (this._frameModel.isNewFrame()) {
+        this._currentFrame += 1;
+      }
+    } else {
+      throw('Cannot roll: not enough pins');
+    }
   },
 
+  //This is wrapping a method from a different object so that the interface deal with 
+  //just the scoreManager rather than scoreManager + frame
   isRollValid: function(pins) {
-    return true;
+    return this._frameModel.isRollValid(pins);
   },
 
   isGameFinished: function() {
     return this._gameFinished;
+  },
+
+  checkBonusPoints: function(pins) {
+    if (this._frameModel.isSpare() || this._frameModel.isStrike()) {
+      this._score += pins;
+    }
   }
 };

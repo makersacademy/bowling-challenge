5d0428d5a85c8cce88a3acd0315b9a1980052057
riyapabari <riya.pabari@gmail.com>
2016-06-26 20:43:19 +0100

    All logic of game & specs finished, tests passing

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+var Game = function() {
+	this.frames = [];
+};
+
+Game.prototype.roll = function(rolls) {
+	var frame = new Frame(rolls);
+	this.frames.push(frame);
+};
+
+Game.prototype.score = function() {
+	return this.frames.reduce(function(score, frame, index, frames) {
+		return score + frame.total(frames[index + 1], frames[index + 2]);
+	}, 0);
+};

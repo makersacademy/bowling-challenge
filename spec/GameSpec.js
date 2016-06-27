dee2f24c1369120bbff1c6be765c2434709ae238
paulkenrick <pkenrick@gmail.com>
2016-06-27 09:01:32 +0100

    uncommenting tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,45 +12,45 @@ describe('Game', function(){
     expect(game.frames.length).toEqual(10);
   });
 
-  // it('#roll adds the pins knocked down to the current frame (when no strike or spare)', function(){
-  //   game.roll(2);
-  //   game.roll(3);
-  //   game.roll(4);
-  //   game.roll(1);
-  //   expect(game.frames[0]).toEqual([2, 3]);
-  //   expect(game.frames[1]).toEqual([4, 1]);
-  // });
+  it('#roll adds the pins knocked down to the current frame (when no strike or spare)', function(){
+    game.roll(2);
+    game.roll(3);
+    game.roll(4);
+    game.roll(1);
+    expect(game.frames[0]).toEqual([2, 3]);
+    expect(game.frames[1]).toEqual([4, 1]);
+  });
 
-  // it('#roll adds the pins knocked down to the current frame (when spare)', function(){
-  //   game.roll(7);
-  //   game.roll(3);
-  //   game.roll(4);
-  //   expect(game.frames[0]).toEqual([7, 3]);
-  //   expect(game.frames[1]).toEqual([4]);
-  // });
+  it('#roll adds the pins knocked down to the current frame (when spare)', function(){
+    game.roll(7);
+    game.roll(3);
+    game.roll(4);
+    expect(game.frames[0]).toEqual([7, 3]);
+    expect(game.frames[1]).toEqual([4]);
+  });
 
-  // it('#score determines the score of the game so far (when no strikes or spares)', function(){
-  //   game.roll(2);
-  //   game.roll(3);
-  //   game.roll(6);
-  //   game.roll(1);
-  //   expect(game.score).toEqual(12);
-  // });
+  it('#score determines the score of the game so far (when no strikes or spares)', function(){
+    game.roll(2);
+    game.roll(3);
+    game.roll(6);
+    game.roll(1);
+    expect(game.score).toEqual(12);
+  });
 
-  // it('#score determines the score of the game so far (when spare)', function(){
-  //   game.roll(7);
-  //   game.roll(3);
-  //   game.roll(6);
-  //   game.roll(1);
-  //   expect(game.score).toEqual(23);
-  // });
+  it('#score determines the score of the game so far (when spare)', function(){
+    game.roll(7);
+    game.roll(3);
+    game.roll(6);
+    game.roll(1);
+    expect(game.score).toEqual(23);
+  });
 
-  //  it('#score determines the score of the game so far (when strike)', function(){
-  //   game.roll(10);
-  //   game.roll(6);
-  //   game.roll(1);
-  //   expect(game.score).toEqual(24);
-  // });
+   it('#score determines the score of the game so far (when strike)', function(){
+    game.roll(10);
+    game.roll(6);
+    game.roll(1);
+    expect(game.score).toEqual(24);
+  });
 
   it('#score determines the score of the game so far (when strike)', function(){
     game.roll(7);

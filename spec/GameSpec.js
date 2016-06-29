271c8920570fb969528b2213bf35fb88a237c369
Carlos <chdezmar@gmail.com>
2016-06-29 19:17:38 +0100

    add spare, strike, perfect game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -17,6 +17,28 @@ describe('Bowling Game', function(){
 		expect(game.score()).toEqual(20);
 	})
 
+	it('can roll a spare', function(){
+		game.roll(5);
+		game.roll(5);
+		game.roll(3);
+		rollMany(0, 17);
+		expect(game.score()).toBe(16);
+	});
+
+	it('can roll a strike', function(){
+		game.roll(10);
+		game.roll(4);
+		game.roll(3);
+		rollMany(0, 16);
+		expect(game.score()).toBe(24);
+	});
+
+	it('can roll a perfect game', function(){
+		rollMany(10, 12);
+		expect(game.score()).toBe(300);
+	})
+
+
 	var rollMany = function(pins, rolls) {
 		for (var i = 0; i < rolls; i++) {
 			game.roll(pins);
--- src/Game.js
+++ src/Game.js
@@ -11,9 +11,43 @@ Game.prototype = {
 	},
 	score: function(){
 		var result = 0;
-		for(var i = 0; i < 20; i++){
-			result += this._rolls[i];
+		var rollIndex = 0;
+		var game = this;
+
+		for(var frameIndex = 0; frameIndex < 10; frameIndex++){
+			if (isStrike()) {
+				result += getStrikeScore();
+				rollIndex++;
+			} else if (isSpare()) {
+				result += getSpareScore();
+				rollIndex += 2;
+			} else {
+				result += getNormalScore();
+				rollIndex += 2;
+			}
 		}
+
 		return result;
+
+		function isSpare() {
+			return game._rolls[rollIndex] + game._rolls[rollIndex + 1] === 10;
+		}
+
+		function isStrike() {
+			return game._rolls[rollIndex] === 10;
+		}
+
+		function getSpareScore() {
+			return game._rolls[rollIndex] + game._rolls[rollIndex + 1] + game._rolls[rollIndex + 2];
+		}
+
+		function getStrikeScore() {
+			return game._rolls[rollIndex] + game._rolls[rollIndex + 1] + game._rolls[rollIndex + 2];
+		}
+
+		function getNormalScore() {
+			return game._rolls[rollIndex] + game._rolls[rollIndex + 1];
+		}
+
 	}
 };

cd10117dcd98b38833d6128542ddcd1d38b37051
Carlos <chdezmar@gmail.com>
2016-06-29 18:12:20 +0100

    refactor, added rollMany function

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,14 +2,25 @@
 
 describe('Bowling Game', function(){
 
+
 	it('starts a new game', function(){
 		expect(game).toEqual(new Game());
 	});
 
 	it('can roll a gutter game', function() {
-		for(var i = 1; i < 20; i++){
-			game.roll(0);
-		}
-		expect(game.score()).toBe(0);
+		rollMany(0,20);
+		expect(game.score()).toEqual(0);
 	});
+
+	it('can roll all ones', function() {
+		rollMany(1,20);
+		expect(game.score()).toEqual(20);
+	})
+
+	var rollMany = function(pins, rolls) {
+		for (var i = 0; i < rolls; i++) {
+			game.roll(pins);
+		}
+	};
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,19 @@
 'use strict';
 
 function Game(){
+	this._rolls = [];
 
 }
 
 Game.prototype = {
-roll: function (pins) {
-
-},
-score: function(){
-	return 0;
-}
-
+	roll: function(pins) {
+		this._rolls.push(pins);
+	},
+	score: function(){
+		var result = 0;
+		for(var i = 0; i < 20; i++){
+			result += this._rolls[i];
+		}
+		return result;
+	}
 };

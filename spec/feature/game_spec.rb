35f8b40a357b0fd77ccb582deea6aa20ee2cab0f
kennbarr <kenneth@Kenneths-iMac.local>
2016-07-03 11:45:03 +0100

    hound tweaks

--- spec/feature/game_spec.rb
+++ spec/feature/game_spec.rb
@@ -2,25 +2,25 @@
 feature "Bowling score calculator" do
 
   scenario "Gutter game bowled", js: true do
-    visit '/'
-    20.times { click_button 'score0' }
-    expect(page).to have_content 'final score: 0'
+    visit "/"
+    20.times { click_button "score0" }
+    expect(page).to have_content "final score: 0"
 
   end
 
   scenario "Perfect game bowled", js: true do
-    visit '/'
-    12.times { click_button 'score10' }
-    expect(page).to have_content 'final score: 300'
+    visit "/"
+    12.times { click_button "score10" }
+    expect(page).to have_content "final score: 300"
   end
 
   scenario "So-so game bowled", js: true do
-    visit '/'
+    visit "/"
     10.times do
-      click_button 'score3'
-      click_button 'score5'
+      click_button "score3"
+      click_button "score5"
     end
-    expect(page).to have_content 'final score: 80'
+    expect(page).to have_content "final score: 80"
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,17 @@
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
+require "capybara"
+require "capybara/rspec"
+require "rspec"
 
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), "..", "app.rb")
 
 Capybara.app = Game
 
 RSpec.configure do |config|
-
   config.expect_with :rspec do |expectations|
-
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-
   config.mock_with :rspec do |mocks|
-
     mocks.verify_partial_doubles = true
   end
-
 end

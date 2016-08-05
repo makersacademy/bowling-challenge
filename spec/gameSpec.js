f6ec0a1e3d4caca1d644d471dfe4d4e2ab6e1742
albiebabie <albiebabie@gmail.com>
2016-08-05 18:44:57 +0100

    game starts with 10 pins

---
+++ spec/gameSpec.js
@@ -0,0 +1,25 @@
+// Tests should be organized under appropriate describe blocks, with descriptive
+// titles and should be DRY. They are unDry if they are repeating themselves in
+// the test in a way that could be extracted). Can you understand exactly how to
+// use the solution solely by reading the tests?
+
+// Many solutions rely on a 'virtuous consumer' - i.e. they do not validate inputs
+// or check for out of range values etc. In the bowling challenge this includes
+// checking for odd corner cases such as the gutter game and the perfect game, and
+// odd combinations of strikes and spares. But also defending against non-numeric
+// or meaningless values being passed in to the engine.
+
+'use strict';
+
+describe('Game', function(){
+
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  it('starts with 10 pins', function(){
+    expect(game.pins).toEqual(10);
+  });
+});

a55303c4e75c82af08b8ff8d0dadccd303ee13c5
Adam <adamjbrowner@gmail.com>
2016-08-05 16:57:22 +0100

    Adds a score of 10 on a strike

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,6 +6,26 @@ describe("Player", function() {
   });
 
   it("Should start with a blank score", function() {
-    expect(game._score).toEqual(0);
+    expect(game._currentScore).toEqual(0);
+  });
+
+  it("Should start on frame 1", function() {
+    expect(game._frame).toEqual(1)
+  })
+
+  it("Should accept score inputs", function() {
+    game.inputScore(4)
+    expect(game._currentScore).toEqual(4);
+  });
+
+  it("Should advance to the next frame after two normal score inputs", function() {
+    game.inputScore(3)
+    game.inputScore(4)
+    expect(game._frame).toEqual(2)
+  });
+
+  it("Should add a score of 10 on a strike", function() {
+    game.inputScore("strike")
+    expect(game._currentScore).toEqual(10)
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,21 @@
 Game = function() {
-  this._score = 0
+  this._currentScore = 0
+  this._frame = 1
+  this._frameBowlCount = 0
 }
+
+Game.prototype.inputScore = function(number) {
+  if (number === "strike") {
+    this._currentScore += 10;
+  } else {
+  this._currentScore += number;
+  this._frameBowlCount += 1 };
+  if (this._frameBowlCount === 2) {
+    this.nextFrame()
+  };
+};
+
+Game.prototype.nextFrame = function () {
+  this._frame += 1;
+  this._frameBowlCount = 0;
+};

15efa858168a0fd3c9ac59981398ec097ae2e69c
Adam <adamjbrowner@gmail.com>
2016-08-06 21:52:59 +0100

    refactor

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -26,38 +26,38 @@ describe("Game", function() {
   });
 
   it("Recognises when a strike has been bowled", function() {
-    game.inputScore(10)
+    game.inputScore(10);
     expect(game._strikeBowlsToCount).toEqual(2);
   });
 
   it("Advances frame number after a strike", function() {
-    game.inputScore(10)
+    game.inputScore(10);
     expect(game._frame).toEqual(2);
   });
 
   it("Adds the next two bowls after a strike as a bonus", function() {
-    game.inputScore(10)
+    game.inputScore(10);
     game.inputScore(5);
     game.inputScore(5);
     expect(game._score).toEqual(30);
-  })
+  });
 
   it("Takes into account subsequent strikes", function() {
-    game.inputScore(10)
-    game.inputScore(10);
+    game.multipleRolls(10, 2);
     game.inputScore(4);
     game.inputScore(2);
     expect(game._score).toEqual(46);
   });
 
   it("Adds a spare bonus after a spare", function() {
-    game.inputScore(5)
-    game.inputScore(5)
-    game.inputScore(3)
-    expect(game._score).toEqual(16)
-  })
+    game.inputScore(5);
+    game.inputScore(5);
+    game.inputScore(3);
+    expect(game._score).toEqual(16);
+  });
+
   it("Returns gutter game on a final score of 0", function() {
-    game.multipleRolls(0, 20)
-    expect(game._gutterGame).toEqual(true)
-  })
+    game.multipleRolls(0, 20);
+    expect(game._gutterGame).toEqual(true);
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,57 +1,90 @@
 Game = function() {
   this._score = 0;
-  this._frame = 1
-  this._frameScore = 0
-  this._frameBowlNumber = 1
-  this._strikeBowlsToCount = 0
-  this._strikeBonus = 0
-}
+  this._frame = 1;
+  this._frameScore = 0;
+  this._frameBowlNumber = 1;
+  this._strikeBowlsToCount = 0;
+  this._strikeBonus = 0;
+};
 
 Game.prototype.inputScore = function(score) {
-  this._frameScore += score
+  this._frameScore += score;
+  this.spareBonus(score);
+  this.isSpare(score);
+  this.strikeBonus(score);
+  this.strikeBowlsLeft();
+  this.isStrike(score);
+  this.nextFrame();
+  this._score += score;
+  this.gutterGame();
+};
+
+Game.prototype.spareBonus = function(score) {
   if (this._spareBowlsToCount > 0) {
-    this._score += score
-    this._spareBowlsToCount = 0
-  }
+    this._score += score;
+    this._spareBowlsToCount = 0;
+  };
+};
+
+Game.prototype.isSpare = function(score) {
   if (this._frameScore === 10 & score !== 10) {
-    this._spareBowlsToCount = 1
-  }
+    this._spareBowlsToCount = 1;
+  };
+};
+
+Game.prototype.strikeBonus = function(score) {
   if (this._strikeBowlsToCount === 3) {
-    this._strikeBonus += (score * 2)
+    this._strikeBonus += (score * 2);
   }
   if (this._strikeBowlsToCount !== 0 && this._strikeBowlsToCount !== 3) {
-    this._strikeBonus += score
-  }
+    this._strikeBonus += score;
+  };
+};
+
+Game.prototype.strikeBowlsLeft = function() {
   if (this._strikeBowlsToCount !== 0) {
     if (this._strikeBowlsToCount === 3) {
-      this._strikeBowlsToCount -= 2
+      this._strikeBowlsToCount -= 2;
     } else {
-      this._strikeBowlsToCount --
+      this._strikeBowlsToCount --;
     }
-    if (this._strikeBowlsToCount === 0) {
-      this._score += this._strikeBonus
-      this._strikeBonus = 0
-    }
-  }
+    this.addStrikeBonus();
+  };
+};
+
+Game.prototype.addStrikeBonus = function() {
+  if (this._strikeBowlsToCount === 0) {
+    this._score += this._strikeBonus;
+    this._strikeBonus = 0;
+  };
+};
+
+Game.prototype.isStrike = function(score) {
   if (score === 10) {
     this._strikeBowlsToCount += 2;
     this._frame ++;
   } else {
     this._frameBowlNumber ++;
   };
+};
+
+
+Game.prototype.nextFrame = function() {
   if (this._frameBowlNumber > 2) {
     this._frame ++;
     this._frameScore = 0;
-    this._frameBowlNumber = 1
+    this._frameBowlNumber = 1;
   };
-  this._score += score
+};
+
+Game.prototype.gutterGame = function() {
   if (this._score === 0 & this._frame === 11) {
-    this._gutterGame = true
-  }
+    this._gutterGame = true;
+  };
 };
 
 Game.prototype.multipleRolls = function(x, y) {
   for (var i = 0; i < y; i++) {
-    this.inputScore(x)
-  }
-}
+    this.inputScore(x);
+  };
+};

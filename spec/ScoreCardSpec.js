97c47e64f3033002c8b40210cfc44c615a0c575e
Sophie Gill <sophie-gill@hotmail.com>
2016-08-06 13:29:27 +0100

    adds frames to game

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -6,26 +6,57 @@ describe('ScoreCard', function() {
     scoreCard = new ScoreCard();
   });
 
+  describe('frames', function() {
+    it('has a default of 1 frames', function() {
+      expect(scoreCard._frames).toEqual(1);
+    });
+    it('has a maximum of 10 frames', function() {
+      for(i = 1; i < 11; i++) {
+        scoreCard.newFrame()
+      }
+      expect(function() { scoreCard.newFrame(); }).toThrowError("There cannot be more than 10 frames in a game")
+    });
+  });
+
   describe('total', function() {
     it('is zero by default', function() {
       expect(scoreCard.total).toEqual(0);
     });
   });
 
-  describe('roll', function() {
-    it('accepts an integer score', function() {
-      expect(function() { scoreCard.roll("not a number"); }).toThrowError("Please enter a number between 0 and 10");
+  // describe('roll', function() {
+  //   it('accepts an integer score', function() {
+  //     expect(function() { scoreCard.roll("not a number"); }).toThrowError("Please enter a number between 0 and 10");
+  //   });
+  //   it('does not accept numbers lower than 0', function() {
+  //     expect(function() { scoreCard.roll(-1); }).toThrowError("Please enter a number between 0 and 10");
+  //   });
+  //   it('does not accept numbers higher than 10', function() {
+  //     expect(function() { scoreCard.roll(11); }).toThrowError("Please enter a number between 0 and 10");
+  //   });
+  // });
+
+  describe('game', function() {
+    it('is empty by default', function() {
+      expect(scoreCard.game).toEqual({});
     });
-    it('does not accept numbers lower than 0', function() {
-      expect(function() { scoreCard.roll(-1); }).toThrowError("Please enter a number between 0 and 10");
+  });
+
+
+  describe('frame', function() {
+    it('is added to the game', function() {
+      scoreCard.newFrame()
+      expect(scoreCard.game).toEqual({frame1: 0});
     });
-    it('does not accept numbers higher than 10', function() {
-      expect(function() { scoreCard.roll(11); }).toThrowError("Please enter a number between 0 and 10");
-    });
-    it('adds the result to the total', function() {
-      scoreCard.roll(5);
-      expect(scoreCard.total).toEqual(5);
-    });
+    it('can add multiple frames to the game', function() {
+      scoreCard.newFrame()
+      scoreCard.newFrame()
+      expect(scoreCard.game).toEqual({frame1: 0, frame2: 0})
+    })
+    it('increases the frame counter by 1', function() {
+      scoreCard.newFrame();
+      expect(scoreCard._frames).toEqual(2);
+    })
   });
 
 });
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,14 +1,25 @@
 function ScoreCard() {
+  this._frames = 1;
   this.total = 0;
+  this.game = {};
+
 }
 
-ScoreCard.prototype.roll = function(number) {
-  if(this.integerCheck(number)) {
-    throw new Error("Please enter a number between 0 and 10");
+ScoreCard.prototype.newFrame = function() {
+  if (this._frames > 10) {
+    throw new Error("There cannot be more than 10 frames in a game")
   }
-  this.total += number;
-};
-
-ScoreCard.prototype.integerCheck = function(number) {
-  return (isNaN(number) || number < 0 || number > 10);
+  this.game["frame" + this._frames] = 0;
+  this._frames += 1;
 }
+
+// ScoreCard.prototype.roll = function(number) {
+//   if(this.integerCheck(number)) {
+//     throw new Error("Please enter a number between 0 and 10");
+//   }
+//   this.total += number;
+// };
+//
+// ScoreCard.prototype.integerCheck = function(number) {
+//   return (isNaN(number) || number < 0 || number > 10);
+// }

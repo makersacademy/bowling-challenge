7c8758f111964a372e76e1fccca5bf378ac5c47c
Adam <adamjbrowner@gmail.com>
2016-08-06 18:24:20 +0100

    multiple strikes now work, I think

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,4 +5,49 @@ describe("Game", function() {
     game = new Game();
   });
 
-  
+  it("Starts with a blank score", function() {
+    expect(game._score).toEqual(0);
+  });
+
+  it("Starts on frame 1", function() {
+    expect(game._frame).toEqual(1);
+  });
+
+  it("Add's the score to the total score", function() {
+    game.inputScore(4);
+    game.inputScore(3);
+    expect(game._score).toEqual(7);
+  });
+
+  it("Advances the frame after two normal bowls", function() {
+    game.inputScore(4);
+    game.inputScore(3);
+    expect(game._frame).toEqual(2);
+  });
+
+  it("Recognises when a strike has been bowled", function() {
+    game.inputScore(10)
+    expect(game._strikeBowlsToCount).toEqual(2);
+  });
+
+  it("Advances frame number after a strike", function() {
+    game.inputScore(10)
+    expect(game._frame).toEqual(2);
+  });
+
+  it("Adds the next two bowls after a strike as a bonus", function() {
+    game.inputScore(10)
+    game.inputScore(5);
+    game.inputScore(5);
+    expect(game._score).toEqual(30);
+  })
+
+  it("Takes into account subsequent strikes", function() {
+    game.inputScore(10)
+    game.inputScore(10);
+    game.inputScore(4);
+    game.inputScore(2);
+    game.inputScore(0)
+    expect(game._score).toEqual(46);
+  });
+});
--- src/Game.js
+++ src/Game.js
@@ -1,1 +1,33 @@
-Game = function() {}
+Game = function() {
+  this._score = 0;
+  this._frame = 1
+  this._frameBowlNumber = 0
+  this._strikeBowlsToCount = 0
+  this._strikeBonus = 0
+}
+
+Game.prototype.inputScore = function(score) {
+  if (this._strikeBowlsToCount === 3) {
+    this._strikeBonus += (score * 2)
+  }
+  if (this._strikeBowlsToCount !== 0 && this._strikeBowlsToCount !== 3) {
+    this._strikeBonus += score
+  }
+  if (this._strikeBowlsToCount !== 0) {
+    this._strikeBowlsToCount --
+    if (this._strikeBowlsToCount === 0) {
+      this._score += this._strikeBonus
+      this._strikeBonus = 0
+    }
+  }
+  if (score === 10) {
+    this._strikeBowlsToCount += 2;
+    this._frame ++;
+  } else {
+    this._frameBowlNumber ++;
+  };
+  if (this._frameBowlNumber === 2) {
+    this._frame ++;
+  };
+  this._score += score
+};

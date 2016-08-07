437c81cc1a0836627d73340c2350e8b78f7a3617
nikklein <objcoding@gmail.com>
2016-08-07 17:39:14 +0100

    Add calculate score for a spare

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,36 @@
-function Game() {
-  this.frames = [];
 
+function Game() {
+  this.rolls = [];
 }
 
-Game.prototype.play = function(pins) {
-  this.frames.push(pins);
+Game.prototype.roll = function(pins) {
+  this.rolls.push(pins);
 };
 
 Game.prototype.score = function() {
-  return this.frames.reduce((prev, curr) => prev + curr );
+   var total = 0;
+   var rollIndex = 0;
+   var these = this;
+
+  for(var framesCount = 0;framesCount < 10; framesCount++) {
+    if (isSpare()) {
+      total += getSpareScore();
+    } else {
+      total += getStandardScore();
+    }
+    rollIndex += 2;
+  }
+
+  return total;
+
+function isSpare() {
+ return these.rolls[rollIndex] + these.rolls[rollIndex + 1] === 10;
+}
+function getSpareScore() {
+ return these.rolls[rollIndex] + these.rolls[rollIndex + 1] + these.rolls[rollIndex + 2];
+}
+function getStandardScore() {
+ return these.rolls[rollIndex] + these.rolls[rollIndex + 1];
+}
+
 };

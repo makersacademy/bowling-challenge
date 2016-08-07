efa113ed34e576d3ecd21c0887391c710b4e736d
j-luong <jason.qh.luong@outlook.com>
2016-08-08 00:08:00 +0100

    added methods to calculate bonus scores

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -5,13 +5,57 @@ describe('Scorecard', function(){
     scorecard = new Scorecard;
   })
 
+  var addScore = function(score, frameTries){
+    for (var i = 0; i < frameTries; i++){
+      scorecard.addScore(score);
+    }
+  }
+
   describe('initial state', function(){
     it('frame starts at 1', function(){
       expect(scorecard.currentFrame).toEqual(1);
     })
 
     it('score starts at 0', function(){
-      expect(scorecard.frameScore[scorecard.currentFrame]).toEqual(0);
+      expect(scorecard.score[scorecard.currentFrame]).toEqual(0);
+    })
+  })
+
+  describe('scoring', function(){
+    it('increases score of current frame', function(){
+      addScore(5,1);
+      expect(scorecard.score[scorecard.currentFrame]).toEqual(5);
+    })
+
+    it('calculates score of multiple frames', function(){
+      addScore(4,5);
+      expect(scorecard.score[1]).toEqual(8);
+      expect(scorecard.score[2]).toEqual(8);
+      expect(scorecard.score[3]).toEqual(4);
+    })
+  })
+
+  describe('frameTries', function(){
+    it('frametries increases after first try', function(){
+      addScore(5,1);
+      expect(scorecard.frameTries).toEqual(2);
+    })
+
+    it('currentFrame does not change after 1 frame try', function(){
+      addScore(5,1);
+      expect(scorecard.currentFrame).toEqual(1);
+    })
+
+    it('changes frame after two tries', function(){
+      addScore(4,2);
+      expect(scorecard.currentFrame).toEqual(2);
+    })
+  })
+
+  describe('bonus points', function(){
+    it('adds correct bonus points for a spare', function(){
+      addScore(5,3); //bowl three 5's in a row
+      expect(scorecard.score[1]).toEqual(15);
     })
   })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,20 +1,45 @@
 var Scorecard = function(){
-  this.frameScore = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
-                     6: 0, 7: 0, 8: 0, 9: 0, 10: 0};
-  this.frameTries = 1;
+  // this.score = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0,
+  //               6: 0, 7: 0, 8: 0, 9: 0, 10: 0};
+  this.score = {1: [0,0], 2: [0,0], 3: [0,0], 4: [0,0], 5: [0,0],
+                6: [0,0], 7: [0,0], 8: [0,0], 9: [0,0], 10: [0,0]};
+  this.frameTries = 0;
   this.currentFrame = 1;
 }
 
 Scorecard.prototype = {
-  calculateScore: function(score){
-    this.frameScore[this.currentFrame] += score;
-    this.frameTries++;
+  addScore: function(score){
+    if (score === 10){
+      this.score[this.currentFrame] = [10,0];
+      this.nextFrame();
+    }
+    else {
+      this.score[this.currentFrame][this.frameTries] += score;
+      this.frameTries++;
+      this.calculateFrame();
+    }
+  },
+
+  calculateScore: function(frame){
+    this.score[frame].reduce((a, b) => a + b, 0);
   },
 
   calculateFrame: function(){
-    if (this.frameTries === 2){
-      this.currentFrame++
-      this.frameTries = 1;
+    if (this.frameTries > 1){
+      this.nextFrame();
     }
+  },
+
+  calculateStrike: function(){
+    this.score[this.currentFrame - 2].concat(this.score[this.currentFrame - 1]);
+  },
+
+  calculateSpare: function(){
+    this.score[this.currentFrame -2].concat(this.score[this.currentFrame - 1][0]);
+  },
+
+  nextFrame: function(){
+    this.currentFrame++
+    this.frameTries = 0;
   }
 }

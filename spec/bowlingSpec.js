223b9cec46a3050e81e5a6548e008f52c5af8e32
Arunas Skirius <arukomp@gmail.com>
2016-08-07 22:40:03 +0100

    Redesigned and refactored a lot of code, moved majority of the logic into a separate Scorecard class

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,10 +5,12 @@ describe("Bowling", function() {
 
   beforeEach(function() {
     bowling = new Bowling();
-  });
-
-  it ("keeps score", function() {
-    expect(bowling.getScore()).toEqual(0);
+    bowling.scorecard = {
+      getPinsLeft: function() {
+        return 10;
+      },
+      receiveScore: function(num) {}
+    };
   });
 
   describe ("Knocking down pins (playing)", function() {
@@ -26,90 +28,6 @@ describe("Bowling", function() {
       expect(result).toBeGreaterThan(-1);
       expect(result).toBeLessThan(2);
     });
-    it ("second roll, moves on to the next frame and puts 10 pins again", function() {
-      bowling._currentRoll = 2;
-      bowling.play();
-      expect(bowling.getCurrentFrameNumber()).toEqual(2);
-      expect(bowling._pinsLeft).toEqual(10);
-    });
-    it ("second roll on frame 10, strike moves on to the third roll", function() {
-      bowling._currentFrame = 10;
-      bowling._currentRoll = 2;
-      bowling._roll = function() { return 10; };
-      bowling.play();
-      expect(bowling._currentRoll).toEqual(3);
-    });
-  });
-
-  describe ("Frame", function() {
-    it ("can get the current frame scores", function() {
-      var scores = {1: null, 2: null};
-      expect(bowling.getCurrentFrame()).toEqual(scores);
-    });
-  });
-
-  describe ("Frame number", function() {
-    it ("can get the current frame number", function() {
-      expect(bowling.getCurrentFrameNumber()).toEqual(1); 
-    });
-    it ("increases the frame number after a couple of plays", function() {
-      bowling._roll = function() { return 1; };
-      bowling.play();
-      expect(bowling.getCurrentFrameNumber()).toEqual(1);
-      bowling.play();
-      expect(bowling.getCurrentFrameNumber()).toEqual(2);
-    });
-    it ("jumps straight to the next frame number after a strike", function() {
-      bowling._roll = function() { return 10; };
-      bowling.play();
-      expect(bowling.getCurrentFrameNumber()).toEqual(2);
-    });
-    it ("unable to play after reaching Frame 11 (doesn't exist)", function() {
-      bowling._currentFrame = 10;
-      bowling._currentRoll = 3;
-      bowling.play();
-      expect(bowling.play()).toEqual(false);
-    });
-  });
-
-  describe ("Keeping Score", function() {
-
-    beforeEach(function() {
-      bowling._roll = function() { return 5; };
-    });
-    it ("adds the points to the score", function() {
-      var points = bowling.play();
-      expect(bowling.getScore()).toEqual(points);
-    });
-    it ("records the score inside the frame object", function() {
-      var frame = bowling.getCurrentFrameNumber();
-      var points = bowling.play();
-      expect(bowling.getFrame(frame)[1]).toEqual(points);
-    });
-
-    describe("Bonus Points", function() {
-      it ("correctly adds bonus points", function() {
-        bowling._hasBonusPoints = 1;
-        var result = bowling.play();
-        expect(bowling.getScore()).toEqual(result * 2);
-      });
-      it ("reduces the amount of bonus point rolls", function() {
-        bowling._hasBonusPoints = 2;
-        bowling.play();
-        expect(bowling._hasBonusPoints).toEqual(1);
-      });
-      it ("gives one spare bonus if player knocked all pins on second roll", function() {
-        bowling.play();
-        bowling.play();
-        expect(bowling._hasBonusPoints).toEqual(1);
-      });
-      it ("gives two spare bonuses if player striked on first roll", function() {
-        bowling._roll = function() { return 10; };
-        bowling.play();
-        expect(bowling._hasBonusPoints).toEqual(2);
-      });
-    });
-
   });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,93 +1,19 @@
 'use strict';
 
 function Bowling() {
-  this._score = 0;
-  this._frames = {};
-  this._currentFrame = 1;
-  this._currentRoll = 1;
-  this._pinsLeft = 10;
-  this._hasBonusPoints = 0;
+  this.scorecard = new Scorecard();
 }
 
 Bowling.prototype = {
 
-  getScore: function() {
-    return this._score;
-  },
-
   play: function() {
-    if (this._currentFrame === 11) {
-      return false;
-    }
-    var roll = this._processRoll();
-    this._resetPins();
-    return roll;
+    var pinsDown = this._roll(this.scorecard.getPinsLeft());
+    this.scorecard.receiveScore(pinsDown);
+    return pinsDown;
   },
 
-  getCurrentFrameNumber: function() {
-    return this._currentFrame;
-  },
-
-  getFrame: function(frame) {
-    if (frame > 10 || frame < 1) {
-      return;
-    }
-    if (!this._frames[frame]) {
-      return this._frames[frame] = {
-        1: null,
-        2: null
-      };
-    }
-    return this._frames[frame];
-  },
-
-  getCurrentFrame: function() {
-    return this.getFrame(this._currentFrame);
-  },
-
-
-  // Private methods
-  _roll: function() {
-    return Math.floor(Math.random() * (this._pinsLeft + 1));
-  },
-
-  _addScore: function(score) {
-    this._pinsLeft -= score;
-    this.getCurrentFrame()[this._currentRoll] = score;
-    this._score += score;
-    if (this._hasBonusPoints > 0) {
-      this._score += score;
-      this._hasBonusPoints--;
-    }
-  },
-
-  _resetPins: function() {
-    if (this._currentFrame === 10 && this._pinsLeft === 0 && this._currentRoll < 3) {
-      this._currentRoll++;
-      this._pinsLeft = 10;
-      return;
-    }
-    if (this._pinsLeft === 0 || this._currentRoll >= 2) {
-      this._currentFrame++;
-      this._currentRoll = 1;
-      this._pinsLeft = 10;
-      return;
-    }
-    this._currentRoll++;
-  },
-
-  _processRoll: function() {
-
-    var roll = this._roll();
-    this._addScore(roll);
-    if (this._pinsLeft === 0 && this._currentRoll === 1) {
-      this._hasBonusPoints = 2;
-    }
-    if (this._pinsLeft === 0 && this._currentRoll === 2) {
-      this._hasBonusPoints = 1;
-    }
-    return roll;
-
+  _roll: function(max) {
+    return Math.floor(Math.random() * (1 + max));
   },
 
 }

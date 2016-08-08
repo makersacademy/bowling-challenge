09918d8597ed9f5d0e371700c9dc5590a8bb4462
acsauk <acsauk@gmail.com>
2016-08-08 21:07:32 +0100

    completes Uncle Bobs first step - first time

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -1,102 +1,13 @@
 'use strict';
 
-describe('Scoring', function() {
+describe('BowlingGame', function() {
 
-  var scoring;
-
-  beforeEach(function() {
-    scoring = new Scoring();
-  });
-
-  describe('gutter game', function() {
-
-    it('returns true when game was a gutter game/0 total score', function() {
-      expect(scoring.isGutterGame(0)).toBe(true);
-    });
-
-    it('returns false when game was not a gutter game/> 0 total score',function() {
-      expect(scoring.isGutterGame(1)).toBe(false);
-    });
-
-  });
-
-  describe('one frame', function(){
-
-    it('starts each frame with an empty array', function(){
-      expect(scoring._currentFrame).toEqual([])
-    });
-
-    it('returns the score for one frame', function() {
-      expect(scoring.frameScore(8, 1)).toEqual(9);
-    });
-
-  });
-
-  describe('multiple frames', function() {
-
-    it('adds the frame score to totalScore array', function() {
-      var frameScore = 2;
-      scoring.saveFrameScore(frameScore);
-      expect(scoring._gameScore).toContain(frameScore);
-    });
-
-    it('returns the score for multiple frames', function() {
-      scoring.saveFrameScore(1);
-      scoring.saveFrameScore(9);
-      expect(scoring.getTotalScore()).toEqual(10);
-    });
-
-  });
-
-  describe('roll', function(){
-
-    it('saves the amount of pins knocked down into _currentFrame', function() {
-      spyOn(Math, 'random').and.returnValue(0.2);
-      var rollResult = scoring.roll();
-      expect(scoring._currentFrame).toContain(rollResult);
-    });
-
-    it('ensures roll cannot return a number more than pins left in current frame', function() {
-      spyOn(Math, 'random').and.returnValue(1);
-      scoring.roll();
-      expect(scoring.roll()).toEqual(0);
-    });
-
-    it('returns the pins left in current frame', function() {
-      expect(scoring.pinsLeft(1)).toEqual(9);
-    });
-
-    it('resets _currentFrame to an empty array when frame is over', function() {
-      spyOn(Math, 'random').and.returnValue(0.4);
-      scoring.roll();
-      scoring.roll();
-      expect(scoring._currentFrame.length).toEqual(0);
-    });
-
-    it('limits the game to 10 frames', function() {
-      spyOn(Math, 'random').and.returnValue(1);
-      for(var count = 0; count < 10; count++) {
-        scoring.roll();
-      };
-      expect(scoring.roll()).toEqual("10 frames have been played - the game is now over.");
-    });
-
-  });
-
-  describe('random', function() {
-
-    it('returns a random amount of pins knocked down', function(){
-      spyOn(Math, 'random').and.returnValue(0.2);
-      expect(scoring.random()).toEqual(2);
-    });
-
+  it('handle gutter game', function() {
+    var game = new BowlingGame();
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(0);
   });
 
 });
-
-// Gutter game
-// One frame
-// Multiple frames
-// Spares
-// Strikes
-// Final Frame
--- src/scoring.js
+++ src/scoring.js
@@ -1,59 +1,16 @@
 'use strict';
 
-var Scoring = function(){
-  this._gameScore = [];
-  this._currentFrame = [];
+var BowlingGame = function() {
+  this.rolls = [];
+  this.currentRoll = 0;
 };
 
-Scoring.prototype.isGutterGame = function(score) {
-  var result = score == 0 ? true : false;
-  return result;
+BowlingGame.prototype.roll = function(pins) {
+  this.rolls[this.currentRoll++] = pins;
 };
 
-Scoring.prototype.frameScore = function(rollOneScore, rollTwoScore) {
-  var result = rollOneScore + rollTwoScore;
-  return result;
-};
+BowlingGame.prototype.score = function() {
+  var score = 0;
 
-Scoring.prototype.saveFrameScore = function(frameScore) {
-  this._gameScore.push(frameScore);
-  this._currentFrame = [];
-};
-
-Scoring.prototype.getTotalScore = function() {
-  var summedScore = this._gameScore.reduce(function(a, b) {
-    return a + b;
-  }, 0)
-  return summedScore;
-};
-
-Scoring.prototype.roll = function() {
-  if(this._gameScore.length == 10) {
-    return "10 frames have been played - the game is now over.";
-  };
-
-  var rollResult = this.random();
-
-  if(rollResult == 10 || this._currentFrame.length > 1) {
-    this._currentFrame.push(rollResult);
-    var score = this.frameScore(this._currentFrame[0], this._currentFrame[1]);
-    this.saveFrameScore(score);
-  } else {
-    this._currentFrame.push(rollResult);
-    return rollResult;
-  }
-};
-
-Scoring.prototype.pinsLeft = function(pins=this._currentFrame[0]) {
-  if (pins != 10) {
-    return 10 - pins;
-  } else {
-    return 0;
-  }
-};
-
-Scoring.prototype.random = function(min=0, high=10) {
-  var min = Math.ceil(0);
-  var max = Math.floor(high);
-  return Math.floor(Math.random() * (max - min)) + min;
+  return score;
 };

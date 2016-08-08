471c906baa9676d01fe13bd3980973413f7779f5
acsauk <acsauk@gmail.com>
2016-08-08 21:17:02 +0100

    completes uncle Bob's first test - second attempt

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -1,1 +1,12 @@
 'use strict';
+
+describe('BowlingGame', function() {
+
+  it('handle gutter game', function() {
+    var game = new BowlingGame();
+    for (var i=0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(0);
+  });
+});

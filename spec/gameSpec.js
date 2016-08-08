8f0b7a526245711573f1a79e56ea2e89f6e79eb8
Titus Iudean <iudeant@gmail.com>
2016-08-08 10:54:16 +0300

    Restructures game to acommodate spares and strikes

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,13 +1,35 @@
 describe('Bowling Game', function() {
   var game;
+  var bowls;
+  var pin;
+
   beforeEach(function () {
     game = new Game();
   });
-  
-  it('calculates the score when pins are hit one at a time', function () {
-    for (var i = 0; i < 20; i++) {
-      game.bowl(1);
+
+  function bowlings(pin, bowls) {
+    for(var i = 0; i < bowls; i++) {
+      game.bowl(pin);
     }
+  }
+
+  it('calculates the score when pins are hit one at a time', function () {
+    bowls = 20;
+    pin = 1;
+    bowlings(pin, bowls);
     expect(game.calculateScore()).toBe(20);
   });
+
+  // it('calculates the score for a spare', function () {
+  //   var aSpare = 2;
+  //   var extraBowls = 16;
+  //   var extraPins = 0;
+  //   for (var i = 0; i < aSpare; i++) {
+  //     game.bowl(5);
+  //   }
+  //   game.bowl(6);
+  //   game.bowl(7);
+  //   bowlings(extraPins, extraBowls);
+  //   expect(game.calculateScore()).toBe(29);
+  // });
 });
--- src/game.js
+++ src/game.js
@@ -1,16 +1,27 @@
 function Game() {
-  this.bowlings = [];
+  this.executedBowl = 0;
+  this.successfullBowls = [];
 }
 
 Game.prototype.bowl = function (pinsHit) {
-  this.bowlings.push(pinsHit);
+  this.successfullBowls.push(pinsHit);
+  //this.executedBowls++;
 };
 
 Game.prototype.calculateScore = function () {
-  var totalBowlings = this.bowlings;
-  var total = 0;
-  for (var i = 0; i < totalBowlings.length; i++) {
-    total += totalBowlings[i];
+  var totalBowlings = this.successfullBowls;
+  var attempt = this.executedBowl;
+  var score = 0;
+
+  for (var frame = 0; frame < 10; frame++) {
+    // if (totalBowlings[attempt] + totalBowlings[attempt + 1]) === 10 {
+    //   score = score + totalBowlings[attempt] + totalBowlings[attempt + 1] + totalBowlings[attempt + 2];
+    //   attempt += 2;
+    // }
+    // else {
+      score = score + totalBowlings[attempt] + totalBowlings[attempt + 1]
+      attempt += 2;
+    // }
   }
-  return total;
+  return score;
 };

27266017cd57708207120deea62974a2c3d49287
Nomi Vos <nvoswork@gmail.com>
2016-08-09 18:18:15 -0500

    refactor out beginning basic test

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,9 +1,5 @@
 describe('Bowling Game', function() {
 
-  it('can create a game', function() {
-    var game = new BowlingGame();
-  });
-
   it('can roll a gutter game', function() {
     var game = new BowlingGame();
     for (var i = 0; i < 20; i++) {

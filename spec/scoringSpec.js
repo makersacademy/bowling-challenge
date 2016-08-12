fe6c8a5047e663bf2c5cf3fb77b2a37ca4d0cb29
acsauk <acsauk@gmail.com>
2016-08-12 22:50:38 +0100

    completes uncle Bob's second test - first attempt

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -9,4 +9,12 @@ describe('BowlingGame', function() {
     }
     expect(game.score()).toEqual(0);
   });
+
+  it('should handle all ones', function() {
+    var game = new BowlingGame();
+    for(var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20);
+  });
 });

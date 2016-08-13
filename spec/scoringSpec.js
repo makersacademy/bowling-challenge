50a15901ba2f07bf02331f06ad1dcaa96c593c38
acsauk <acsauk@gmail.com>
2016-08-13 16:00:36 +0100

    wipes uncle Bob's solution ready to start fifth attempt

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -1,32 +1,20 @@
 'use strict';
 
 describe('BowlingGame', function() {
-var game;
-
-beforeEach(function() {
-  game = new BowlingGame();
-});
-
-function manyRoll(n, pins) {
-  for(var i = 0; i < n; i++) {
-    game.roll(pins);
-  }
-}
 
   it('can handle a gutter game', function() {
-    manyRoll(20,0);
+    var game = new BowlingGame();
+    for(var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
     expect(game.score()).toEqual(0);
   });
 
   it('can handle a game of all ones', function() {
-    manyRoll(20,1);
+    var game = new BowlingGame();
+    for(var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
     expect(game.score()).toEqual(20);
-  });
-
-  it('can handle one spare', function() {
-    manyRoll(2,5);
-    game.roll(3);
-    manyRoll(17,0)
-    expect(game.score()).toEqual(16);
   });
 });

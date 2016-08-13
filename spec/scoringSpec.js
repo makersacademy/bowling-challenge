41fbfb0a238674e8ed593ec32159f8e42f63141a
acsauk <acsauk@gmail.com>
2016-08-13 12:54:32 +0100

    wipes uncle Bob's second test solution ready to start fifth attempt

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -10,12 +10,4 @@ describe('BowlingGame', function() {
     expect(game.score()).toEqual(0);
   });
 
-  it('can handle a game of all one\'s', function() {
-    var game = new BowlingGame();
-    for(var i = 0; i < 20; i++) {
-      game.roll(1);
-    }
-    expect(game.score()).toEqual(20);
-  });
-
 });
--- src/scoring.js
+++ src/scoring.js
@@ -11,17 +11,6 @@ BowlingGame.prototype.roll = function(pins) {
 
 BowlingGame.prototype.score = function() {
   var score = 0;
-  var frameIndex = 0;
-  var self = this;
-
-  function sumOfBallsInFrame() {
-    return self.rolls[frameIndex] + self.rolls[frameIndex + 1]
-  }
-
-  for (var frame = 0; frame < 10; frame++) {
-    score += sumOfBallsInFrame();
-    frameIndex += 2;
-  }
 
   return score;
 };

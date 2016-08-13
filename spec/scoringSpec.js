5fc6006eb460ee63e8b0048cd6cced2088808bef
acsauk <acsauk@gmail.com>
2016-08-13 16:16:09 +0100

    completes uncle Bob's third test fifth attempt

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -1,20 +1,33 @@
 'use strict';
 
 describe('BowlingGame', function() {
+var game;
+
+beforeEach(function() {
+  game = new BowlingGame();
+});
+
+function manyRolls(n, pins) {
+  for(var i = 0; i < n; i++) {
+    game.roll(pins);
+  }
+}
 
   it('can handle a gutter game', function() {
-    var game = new BowlingGame();
-    for(var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    manyRolls(20,0);
     expect(game.score()).toEqual(0);
   });
 
   it('can handle a game of all ones', function() {
-    var game = new BowlingGame();
-    for(var i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+    manyRolls(20,1);
     expect(game.score()).toEqual(20);
   });
+
+  it('can handle one spare', function() {
+    manyRolls(2,5);
+    game.roll(3);
+    manyRolls(17,0);
+    expect(game.score()).toEqual(16);
+  });
+
 });

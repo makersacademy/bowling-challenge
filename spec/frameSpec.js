821d74a3da752187af3bac7c70771888794dfa3f
JulieLev <jkgee1@yahoo.co.uk>
2016-08-13 13:24:54 +0100

    simple roll and strike

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -21,5 +21,21 @@ describe('Frame', function() {
       expect(frame.getPinCount()).toEqual(10);
     });
 
+    it('has a bonus score of zero', function() {
+      expect(frame.getBonusScore()).toEqual(0);
+    });
   });
+
+  describe('Roll score', function() {
+    it('can add the pins knocked down to the roll score', function() {
+      frame.roll(2);
+      expect(frame.rollScore).toEqual(2);
+    });
+
+    it('knows when the roll is a strike', function() {
+      frame.roll(frame.DEFAULT_PIN_COUNT);
+      expect(frame.isStrike()).toBeTruthy();
+    });
+  });
+
 });
--- src/Frame.js
+++ src/Frame.js
@@ -2,19 +2,41 @@
 
 function Frame() {
   this.frameScore = 0;
-  this.rollCounter = 0;
-  this.pinCount = 10;
+  this.DEFAULT_PIN_COUNT = 10;
+  this.pinCount = this.DEFAULT_PIN_COUNT;
+  this.bonusScore = 0;
+  this.rolls = [];
 };
-
 
 Frame.prototype.getFrameScore = function() {
   return this.frameScore;
 };
 
 Frame.prototype.getRollCounter = function() {
+  this.rollCounter = this.rolls.length;
   return this.rollCounter;
 };
 
 Frame.prototype.getPinCount = function() {
   return this.pinCount;
+};
+
+Frame.prototype.getBonusScore = function() {
+  return this.bonusScore;
+};
+
+Frame.prototype.roll = function(numberOfPins) {
+  this.rollScore = numberOfPins;
+  this.pinCount -= numberOfPins;
+  this.addRollScore();
+};
+
+Frame.prototype.addRollScore = function() {
+  this.rolls.push(this.rollScore);
+};
+
+Frame.prototype.isStrike = function() {
+  if ((this.getRollCounter() === 1) && (this.getPinCount() === 0)) {
+    return true;
+  };
 };

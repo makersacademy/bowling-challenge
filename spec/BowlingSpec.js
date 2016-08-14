fb8212144168c567392df51a2421f3bb6127afad
Bart <kluczynskib@gmail.com>
2016-08-14 12:27:31 +0200

    start from the beginning

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -5,32 +5,6 @@ describe ('bowling game', function(){
     bowling = new Bowling();
   });
 
-  it('starts with zero points', function(){
-    expect(bowling.getScore()).toEqual(0);
-  });
-
-  it('stores the available pin', function(){
-    expect(bowling.getPins()).toEqual(10);
-  });
-
-  it('stores all the rolls', function(){
-    for (var i = 0; i < 21; i++){
-    bowling.roll = function() { return 5; };
-    bowling.updateRolls();
-    }
-    expect(bowling.addScore()).toEqual(150);
-  });
-
-  it('accurately calculates the spare', function(){
-    for (var i = 0; i < 21; i++){
-    bowling.roll = function() { return 5; };
-    bowling.updateRolls();
-
-  }
-    expect(bowling.addScore()).toEqual(150);
-  });
-
-
 
 
 });

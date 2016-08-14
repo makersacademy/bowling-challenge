98bac8882bdad30cbfd7a682104f52e75d25c929
Robert Summers <rsummers02@gmail.com>
2016-08-14 10:23:46 +0100

    calculats a perfect game as well as incomplete games

--- README.md
+++ README.md
@@ -1,13 +1,30 @@
 # Bowling Scoresheet
 
+This little javscript app calculates the score of a 10 pin bowling game.  It can calculate the score after each frame of bowls is entered.  It requires the player to be honest as it does not check for 'valid' frame results.
+
 ## Instructions
-* to do...
+* clone this repo
+* open SpecRunner.html in your browser
+* open the browser console.
+* add your results for each frame with the frameBowls(bowl1, bowl2 (optional bowl3 for 10th frame)) function.
+* check your current score after each frame with the score() function
+
+```
+> game = new Game()
+< Game {_score: Array[10], _frame: 1}
+> game.frameBowls(10,0)
+< undefined
+> game.frameBowls(5,5)
+< undefined
+> game.score()
+< 30
+```
 
 ## User Stories
 ```
 As a bowler,
 So I can keep track of my game,
-I'd like to enter my result after each bowl.
+I'd like to enter my result after each frame.
 
 As a bowler,
 So that I know how well I did,
@@ -21,13 +38,4 @@ As a bowler,
 So that I know how well I'm doing,
 I'd like to see my total score for the game.
 
-As a bowler,
-To entertain me,
-I'd like to see a cool little animation when I score a Strike.
 ```
-
-
-## Domain Model
-- Frame > validates input for the frame, stores the results
-- Game > receives score, knows the frame
-- Score > calculates score by frame, and total score
--- js/bowlingGame.js
+++ js/bowlingGame.js
@@ -1,27 +1,69 @@
 'use strict';
 
 function Game() {
-  this._score = [];
+  this._score = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0,0]];
+  this._frame = 1;
 }
 
-Game.prototype.frameResult = function() {
-  return this._score;
-};
-
-Game.prototype.frameBowls = function(bowl1, bowl2) {
-  this._score.push([bowl1, bowl2]);
+Game.prototype.frameBowls = function(bowl1, bowl2, bowl3) {
+  this._score[this._frame-1][0] = bowl1;
+  this._score[this._frame-1][1] = bowl2;
+  if (this._frame === 10) {
+    this._score[this._frame-1][2] = bowl3;
+  };
+  this._frame += 1;
 };
 
 Game.prototype.score = function() {
-  var calcTheScore = this._score.reduce(makeFlat, []);
-  return calcTheScore.reduce(add, 0);
+  var total = 0;
+  var frameResults = this._score
+  for (var frame = 0; frame < 10; frame++) {
+    if (frame < 8) {
+      // bonus score for a strike
+      if (isStrike(frameResults[frame])) {
+        if (isStrike(frameResults[frame+1])) {
+          total += frameResults[frame+1][0] + frameResults[frame+2][0];
+        } else {
+          total += frameResults[frame+1].reduce(add,0);
+        };
+      // bonus score for a spare
+    } else if (isSpare(frameResults[frame])) {
+        total += frameResults[frame+1][0];
+      };
+      // adding the result of the frame pins knocked down
+      total += frameResults[frame].reduce(add,0);
+    };
+    // for frame 9
+    if (frame === 8) {
+      if (isStrike(frameResults[frame])) {
+        total += frameResults[frame+1][0] + frameResults[frame+1][1];
+      } else if (isSpare(frameResults[frame])) {
+        total += frameResults[frame+1][0];
+      };
+      total += frameResults[frame].reduce(add,0);
+    };
+    // for frame 10
+    if (frame === 9) {
+      total += frameResults[frame].reduce(add,0);
+    };
+
+  };
+  return total;
 };
 
+// function to determine rules for the score function
+function isStrike(frame) {
+  if (frame[0] === 10) {
+    return true
+  }
+  return false
+};
 
-// guard conditions on frame score input
-
-function validateScore(bowl1, bowl2) {
-  
+function isSpare(frame) {
+  if (frame[0] + frame[1] === 10) {
+    return true
+  }
+  return false
 };
 
 // functions to use with reduce

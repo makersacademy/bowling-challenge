a15ae368bc772878ae16fd865f6dcfdb997e5a91
Bart <kluczynskib@gmail.com>
2016-08-14 14:34:06 +0200

    the game adds the score, resets the pins, shows the current frame and the current roll

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,9 +1,33 @@
+'use strict';
+
 describe ('bowling game', function(){
   var bowling;
 
   beforeEach(function(){
     bowling = new Bowling();
   });
+
+  it('starts with a score', function(){
+    expect(bowling.getScore()).toEqual(0);
+  });
+
+  it('lets the player to roll', function(){
+    expect(bowling.getRoll()).not.toEqual(0);
+  });
+
+  it('lets the player to roll two times in one frame',function(){
+
+    bowling.roll = function() { return 5; };
+    bowling.getRoll();
+    bowling.getRoll();
+    expect(bowling.getCurrentFrame()).toEqual(2)
+    expect(bowling.getScore()).toEqual(15)
+  });
+
+  it ('shows the pins you start the game with', function(){
+    expect(bowling.getPins()).toEqual(10);
+  });
+
 
 
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,3 +1,39 @@
-function Bowling(){
+'use strict';
 
+function Bowling(){
+this.score = 0;
+this.pins = 10;
+this.currentFrame = 1;
+this.gameRoll = 0;
+
+};
+
+Bowling.prototype.getScore = function() {
+  return this.score;
+};
+
+Bowling.prototype.roll = function(){
+return Math.floor(Math.random() * (this.pins + 1));
+};
+
+Bowling.prototype.getRoll = function() {
+  var roll = this.roll();
+  this.score += roll;
+  this.pins -= roll;
+  this.gameRoll += 1;
+  if (this.gameRoll === 2){
+    this.currentFrame += 1
+    this.gameRoll = 1
+    this.pins = 10
+  };
+
+  return roll;
+};
+
+Bowling.prototype.getPins = function(){
+  return this.pins;
+};
+
+Bowling.prototype.getCurrentFrame = function(){
+  return this.currentFrame;
 };

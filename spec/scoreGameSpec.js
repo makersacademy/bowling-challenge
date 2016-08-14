1d29f294b05f29752f0e20c73639c23af9360297
noby <n.fujioka@abacusmaths.info>
2016-08-14 23:18:35 +0100

    broken while fixing strikes.

--- spec/scoreGameSpec.js
+++ spec/scoreGameSpec.js
@@ -5,85 +5,153 @@ describe('ScoreGame:', function(){
     var scoreGame = new ScoreGame();
     it('A score(points) should be added to a frame when he/she rolls a ball.', function(){
       scoreGame.getScore();
-      expect(scoreGame.game[0][0][0]).toBeGreaterThan(-1);
-      expect(scoreGame.game[0][0][0]).toBeLessThan(11);
+      expect(scoreGame.game[1][0][0]).toBeGreaterThan(-1);
+      expect(scoreGame.game[1][0][0]).toBeLessThan(11);
     });
-    it('If user scores 7 points, 7 points should be added to the frame.', function(){
+    it('If player scores 7 points, 7 points should be added to the frame.', function(){
       scoreGame.resetGame();
       scoreGame.getScore(7);
-      expect(scoreGame.game[0][0][0]).toEqual(7);
+      expect(scoreGame.game[1][0][0]).toEqual(7);
     });
   });
-  describe('Frame:', function(){
+
+  describe('Frames:', function(){
     var scoreGame = new ScoreGame();
-    it('User should be able to roll 2nd ball (get another score) if the first score is less than 10 in the frame.', function(){
+    it('player should be able to roll 2nd ball (get another score) if the first score is less than 10 in the frame.', function(){
       scoreGame.getScore(6);
       scoreGame.getScore(2);
-      expect(scoreGame.game[0][0][0]).toEqual(6);
-      expect(scoreGame.game[0][0][1]).toEqual(2);
+      expect(scoreGame.game[1][0][0]).toEqual(6);
+      expect(scoreGame.game[1][0][1]).toEqual(2);
     });
-    it('There are only 10 pins that a user can score within a frame.', function(){
+    it('There are only 10 pins that a player can score within a frame.', function(){
       scoreGame.resetGame();
       scoreGame.getScore(7);
       scoreGame.getScore();
 
-      expect(scoreGame.game[0][0][0]+scoreGame.game[0][0][1]).toBeLessThan(11);
+      expect(scoreGame.game[1][0][0]+scoreGame.game[1][0][1]).toBeLessThan(11);
     });
-    it('After user rolls 2 balls, which total less than 10 points in the current frame, the total should be immediately recored within the frame ', function(){
+    it('After player rolls 2 balls, which total less than 10 points in the current frame, the total should be immediately recored within the frame ', function(){
       scoreGame.resetGame();
       scoreGame.getScore(3);
       scoreGame.getScore(5);
-      expect(scoreGame.game[0][1]).toEqual(8);
+      expect(scoreGame.game[1][1]).toEqual(8);
     });
-    it('User rolls in the next frame after he rolls max 2 balls in the current frame.', function(){
+    it('player rolls in the next frame after he rolls max 2 balls in the current frame.', function(){
       scoreGame.resetGame();
       scoreGame.getScore(2);
       scoreGame.getScore(4);
       scoreGame.getScore(8);
-      expect(scoreGame.game[1][0][0]).toEqual(8);
+      expect(scoreGame.game[2][0][0]).toEqual(8);
+    });
+    it('Each frame should display a cumulative total of its and previous frames after its scoring is complete.', function(){
+      scoreGame.resetGame();
+      scoreGame.getScore(1);
+      scoreGame.getScore(2);
+      expect(scoreGame.game[1][1]).toEqual(3);
+      scoreGame.getScore(3);
+      scoreGame.getScore(4);
+      expect(scoreGame.game[2][1]).toEqual(10);
+      scoreGame.getScore(5);
+      scoreGame.getScore(2);
+      expect(scoreGame.game[3][1]).toEqual(17);
     });
   });
 
   describe("Spare", function(){
     var scoreGame = new ScoreGame();
-    it("If user's two tries in a frame total 10 points, he/she is awarded 'spare'.", function(){
+    it("If player's two tries in a frame total 10 points, he/she is awarded 'spare'.", function(){
       scoreGame.getScore(4);
       scoreGame.getScore(6);
       expect(scoreGame.bonus).toEqual('spare');
     });
-    it("After user gets his first score in the subsequent frame, 10 points and this score as bonus points are added to the total in the previous frame.", function(){
+    it("After player gets his first score in the subsequent frame, 10 points and this score as bonus points are added to the total in the previous frame.", function(){
       scoreGame.getScore(5);
-      expect(scoreGame.game[0][1]).toEqual(10+scoreGame.game[1][0][0]);
+      expect(scoreGame.game[1][1]).toEqual(10+scoreGame.game[2][0][0]);
     });
   });
 
-  describe("Strike", function(){
+  describe("Strike:", function(){
     var scoreGame = new ScoreGame();
-    it("If user knocks down all 10 pins (10 scores) at his/her first try in a frame, he/she is awarded 'strike'.", function(){
+    it("If player knocks down all 10 pins (10 scores) at his/her first try in a frame, he/she is awarded 'strike'.", function(){
       scoreGame.getScore(10);
       expect(scoreGame.bonus).toEqual('strike');
     });
-    it("After a strike in a frame, there is no more pin left in the frame, so user rolls in the next frame.", function(){
+    it("After a strike in the current frame, there is no more pin left in the frame, so player rolls in the next frame.", function(){
+      scoreGame.getScore(4);
+      expect(scoreGame.game[2][0][0]).toEqual(4);
+    });
+    it("After player finishes all his/her tries in the next frame, 10 points and all the points from these tries are added to the total in the frame he/she got strike.", function(){
+      scoreGame.getScore(5);
+      expect(scoreGame.game[2][0][1]).toEqual(5);
+      expect(scoreGame.game[1][1]).toEqual(19);
+    });
+    it("If player got a strike in the previous frame and gets another strike in the current frame, 20 points plus the score from the next try are added to the total in the frame player got the strick.", function(){
       scoreGame.resetGame();
       scoreGame.getScore(10);
+      scoreGame.getScore(10);
       scoreGame.getScore(3);
-      expect(scoreGame.game[1][0][0]).toEqual(3);
+      expect(scoreGame.game[1][0][0]).toEqual(10);
+      expect(scoreGame.game[2][0][0]).toEqual(10);
+      expect(scoreGame.game[3][0][0]).toEqual(3);
+      expect(scoreGame.game[1][1]).toEqual(23);
     });
   });
-    // describe('Spare', function(){
-    //   var scoreGame = new ScoreGame();
-    //   it("User is awarded 'Spare' if he/she knocks down all the remaining pins (total of 10 pins) at his 2nd throw within the frame.", function(){
-    //     scoreGame.getScore(2);
-    //     scoreGame.getScore(8);
-    //
-    //   });
-    // });
 
-    // it('User gets 2nd ball (get another score) within the same frame if the first score is 10 in the frame.', function(){
-    //   scoreGame.getScore(10);
-    //   scoreGame.getScore();
-    //   expect(scoreGame.getScore()).toEqual("Spare!");
-    // });
-  // });
+  describe("10th Frame: ", function(){
+    var scoreGame = new ScoreGame();
+    it('Player can roll only twice if he/she does not get a spare or strike at the 10th frame.', function(){
+      scoreGame.frameNth = 10;
+      scoreGame.getScore(3);
+      scoreGame.getScore(4);
+      console.log(scoreGame.game[10][0][0]);
+      console.log(scoreGame.game[10][0][1]);
+      console.log(scoreGame.game[10][1]);
+      expect(scoreGame.game[10][0][0]).toEqual(3);
+      expect(scoreGame.game[10][0][1]).toEqual(4);
+      expect(scoreGame.getScore(5)).toEqual("Your game is finished.");
+    });
+    it('allows player to throw additional ball(try) if he/she knocks down all 10 pins with the first two tries (spare) in the 10th frame.', function(){
+      scoreGame.resetGame();
+      scoreGame.frameNth = 10;
+      scoreGame.getScore(4);
+      scoreGame.getScore(6);
+      scoreGame.getScore(7);
+      console.log(scoreGame.game[10][0][0]);
+      console.log(scoreGame.game[10][0][1]);
+      console.log(scoreGame.game[10][0][2]);
+      console.log(scoreGame.game[10][1]);
+      expect(scoreGame.game[10][0][0]).toEqual(4);
+      expect(scoreGame.game[10][0][1]).toEqual(6);
+      expect(scoreGame.game[10][0][2]).toEqual(7);
+      expect(scoreGame.game[10][1]).toEqual(17);
+    });
+    it('allows player to get strikes three times if he/she knocks down all 10 pins with each try in the 10th frame.', function(){
+      scoreGame.resetGame();
+      scoreGame.frameNth = 10;
+      scoreGame.getScore(10);
+      scoreGame.getScore(10);
+      scoreGame.getScore(10);
+      console.log(scoreGame.game[10][0][0]);
+      console.log(scoreGame.game[10][0][1]);
+      console.log(scoreGame.game[10][0][2]);
+      console.log(scoreGame.game[10][1]);
+      expect(scoreGame.game[10][0][0]).toEqual(10);
+      expect(scoreGame.game[10][0][1]).toEqual(10);
+      expect(scoreGame.game[10][0][2]).toEqual(10);
+      expect(scoreGame.game[10][1]).toEqual(30);
+    });
+  });
+
+  describe("Perfect Game:", function(){
+    var scoreGame = new ScoreGame();
+    it("Player receives 300 points when he/she rolls 12 strikes in the game.", function(){
+      for (var i=1;i<=12;i++) {
+        console.log("i is:", i);
+        scoreGame.getScore(10);
+      }
+      expect(scoreGame.game[10][1]).toEqual(300);
+
+    });
+  });
 
 });
--- src/scoreGame.js
+++ src/scoreGame.js
@@ -3,70 +3,170 @@ function ScoreGame() {
 }
 
 ScoreGame.prototype.getScore = function(score) {
-
+  if (this.gameOn === false) {
+    return "Your game is finished.";
+  }
   switch (this.tryNth) {
     case 1:
-      if (typeof score === 'undefined') {
+      if (this.isUndefined(score)) {
       score = Math.floor(Math.random()*11);
       }
-      this.game[this.frameNth-1][0][this.tryNth-1] = score;
+      this.game[this.frameNth][0][this.tryNth-1] = score;
+      console.log("try1:", this.game[this.frameNth][0][this.tryNth-1]);
+      console.log(
+       "1 Frame:", this.frameNth, "try:", this.tryNth,
+       "score:", this.game[this.frameNth][0][this.tryNth-1], "Bonus:", this.bonus
+      );
+
       if (this.bonus === "spare") {
-        this.game[(this.frameNth-1)-1][1] = 10 + score;
-        console.log("previous frameTotal:",this.game[(this.frameNth-1)-1][1]);
+        this.totalScore += (10 + score);
+        this.game[this.frameNth-1][1] = this.totalScore;
+        console.log("previous frameTotal:",this.game[this.frameNth-1][1]);
         this.bonus = "";
       }
 
+      if (this.bonus === "strike") {
+        this.strikeSum.push(score);
+        if (this.strikeSum.length === 2) {
+          this.totalScore += 10 + this.strikeSum.reduce(function(a,b){return a+b});;
+          this.game[this.strikeFrame][1] = this.totalScore;
+          console.log("previous frameTotal:",this.game[this.strikeFrame][1]);
+          this.bonus = "";
+        }
+      }
+
       if (score === 10) {
-        this.bonus = "strike";
-        console.log(
-         "Frame:", this.frameNth, "try:", this.tryNth,
-         "score:", score, "Bonus:", this.bonus
-        );
-        this.frameNth += 1;
-        this.tryNth = 1;
+        console.log("STRIKE!");
+        this.frameNth === 10 ? this.bonus = "TFStrike" : this.bonus = "strike";
+        this.strikeFrame = this.frameNth;
+
+        if (this.frameNth <10){
+          this.moveToNextFrame();
+        } else if(this.frameNth === 10){
+          console.log("moving on to 2nd try. Score for 1st try is:",   this.game[this.frameNth][0][this.tryNth-1] );
+          console.log("total score is:", this.totalScore);
+          this.moveToNextTry();
+        }
       } else {
         console.log(
          "Frame:", this.frameNth, "try:", this.tryNth,
          "score:", score, "Bonus:", this.bonus
         );
-        this.tryNth +=1;
+        this.moveToNextTry();
       }
       break;
-    case 2:
-      if (typeof score === 'undefined') {
-        score = Math.floor(Math.random()*(11-this.game[this.frameNth-1][0][(this.tryNth-1)-1]));
-      }
-      this.game[this.frameNth-1][0][this.tryNth-1] = score;
 
-      var try1 = this.game[this.frameNth-1][0][(this.tryNth-1)-1];
-      var try2 = this.game[this.frameNth-1][0][this.tryNth-1];
-      if ((try1+try2)===10) {
+    case 2:
+      var try1 = this.game[this.frameNth][0][(this.tryNth-1)-1];
+
+      if (this.isUndefined(score) && this.bonus !== "TFStrike") {
+        score = Math.floor(Math.random()*(11-try1));
+      } else if (this.isUndefined(score) && this.bonus === "TFStrike"){
+        score = Math.floor(Math.random()*11);
+      }
+      this.game[this.frameNth][0][this.tryNth-1] = score;
+      var try2 = this.game[this.frameNth][0][this.tryNth-1];
+      console.log("try2:", try2);
+
+
+      if (this.bonus === "strike") {
+        this.strikeSum.push(score);
+        if (this.strikeSum.length === 2) {
+          this.totalScore += 10 + this.strikeSum.reduce(function(a,b){return a+b});;
+          this.game[this.strikeFrame][1] = this.totalScore;
+          console.log("previous frameTotal:",this.game[this.strikeFrame][1]);
+          this.bonus = "";
+        }
+      }
+
+      if (try1 < 10 && (try1+try2)===10) {
         this.bonus = "spare";
         console.log(
          "Frame:", this.frameNth, "try:", this.tryNth,
          "score:", score, "Bonus:", this.bonus
         );
-      } else {
-        this.game[this.frameNth-1][1] = try1 + try2;
+      } else if (this.frameNth < 10){
+        console.log("Hey can you see me?");
+        this.totalScore += try1 + try2;
+        this.game[this.frameNth][1] = this.totalScore;
         console.log(
          "Frame:", this.frameNth, "try:", this.tryNth,
          "score:", score, "Bonus:", this.bonus
         );
       }
-      this.frameNth += 1;
-      this.tryNth = 1;
+      if (this.frameNth <10){
+        this.moveToNextFrame();
+      } else if(this.bonus === "spare" || this.bonus === "TFStrike"){
+        this.bonus = "";
+        console.log("moving on to 3rd try. Score for 2nd try is:",   this.game[this.frameNth][0][this.tryNth-1] );
+        console.log("total score is:", this.totalScore);
+        this.moveToNextTry();
+      } else {
+        console.log("You finished the game. 10th frame with 2 tries");
+        this.gameOn = false;
+      }
+      break;
+
+    case 3:
+      if (this.isUndefined(score)) {
+      score = Math.floor(Math.random()*11);
+      }
+      this.game[this.frameNth][0][this.tryNth-1] = score;
+      console.log("try3:", this.game[this.frameNth][0][this.tryNth-1]);
+      var try1 = this.game[this.frameNth][0][(this.tryNth-1)-2];
+      var try2 = this.game[this.frameNth][0][(this.tryNth-1)-1];
+      var try3 = this.game[this.frameNth][0][this.tryNth-1];
+      this.totalScore += try1 + try2 + try3;
+      this.game[this.frameNth][1] = this.totalScore;
+      console.log("try 1 score:", try1, "try 2 score:", try2, "try 3 score:", try3);
+      console.log("totalScore is:", this.totalScore);
+      this.bonus = "";
+      console.log("You finished the game. 10th frame with 3 tries");
+      this.gameOn = false;
   }
 
 };
 
 ScoreGame.prototype.resetGame = function(){
   this.totalScore = 0;
-  this.game =[ [[0,0],0], [[0,0],0], [[0,0],0], [[0,0],0], [[0,0],0],
+  this.game =[ [[0,0],0], [[0,0],0], [[0,0],0], [[0,0],0], [[0,0],0], [[0,0],0],
               [[0,0],0], [[0,0],0], [[0,0],0], [[0,0],0], [[0,0,0],0]];
   this.frameNth = 1;
   this.tryNth = 1;
   this.bonus = "";
+  this.strikeFrame = 0;
+  this.strikeSum = [];
+  this.gameOn = true;
 }
+
+ScoreGame.prototype.isUndefined = function(score){
+  return typeof score === 'undefined';
+}
+
+ScoreGame.prototype.moveToNextTry = function() {
+  this.tryNth +=1;
+}
+
+ScoreGame.prototype.moveToNextFrame = function(){
+  this.frameNth += 1;
+  this.tryNth = 1;
+}
+
+ScoreGame.prototype.strikesCheck = function(this.strikeFrame, score){
+
+  if (this.bonus === "strike") {
+    this.strikeSum.push(score);
+    if (this.strikeSum.length === 2) {
+      this.totalScore += 10 + this.strikeSum.reduce(function(a,b){return a+b});;
+      this.game[this.strikeFrame][1] = this.totalScore;
+      console.log("previous frameTotal:",this.game[this.strikeFrame][1]);
+      this.bonus = "";
+    }
+  }
+
+
+}
+
 // ScoreGame.prototype.calcBonus = function(score) {
 //   return score+10;
 // };

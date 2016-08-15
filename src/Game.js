256ffd3c4a0dc8073d2dfd8861d0098ccea2477a
6eff <6eff@mail.ru>
2016-08-15 13:42:47 +0300

    refactors game.js

--- src/Game.js
+++ src/Game.js
@@ -1,44 +1,60 @@
 'use strict';
 
-function Game(){
-  this.FRAMES = 10;
-  this.currentFrame = [];
-  this.currentScore = 0;
-  this.MAX_SCORE = 300;
-  this.frame_pins = 10;
+function Game() {
+	this.FRAMES = 10;
+	this.currentFrame = [];
+	this.currentScore = 0;
+	this.MAX_SCORE = 300;
+	this.frame_pins = 10;
+	this.isStrike = false;
+	this.isSpare = false;
 }
 
-Game.prototype.getCurrentScore = function () {
-  return this.currentScore;
+Game.prototype.getCurrentScore = function() {
+	return this.currentScore;
 };
 
-Game.prototype.frame = function (roll_1, roll_2, frame_number){
-  if(roll_1 === this.frame_pins){
-    return this.currentFrame[frame_number] = [roll_1];
-  }
-  if(roll_1 < this.frame_pins && roll_2 < this.frame_pins) {
-    return this.currentFrame[frame_number] = [roll_1,roll_2];
-  }
-  throw "illegal move"
+Game.prototype.frame = function(roll_1, roll_2, frame_number) {
+	if (roll_1 === this.frame_pins) {
+		this.isStrike = true;
+		return this.currentFrame[frame_number] = [roll_1];
+	}
+	if (roll_1 < this.frame_pins && roll_2 < this.frame_pins) {
+    if (roll_1 + roll_2 === this.frame_pins) {
+  		this.isSpare = true;
+  		return this.currentFrame[frame_number] = [roll_1, roll_2];
+  	}
+		return this.currentFrame[frame_number] = [roll_1, roll_2];
+	}
+
+	throw "illegal move"
 };
 
-Game.prototype.frameScore = function (frame_number) {
-  return this.currentFrame[frame_number].reduce(function(a, b){
-    return a + b;
-  });
+Game.prototype.frameScore = function(frame_number) {
+	return this.currentFrame[frame_number].reduce(function(a, b) {
+		return a + b;
+	});
 };
 
-Game.prototype.totalScore = function (frame_number) {
-  for (var i = 0; i < frame_number; i++) {
-    this.currentScore += this.frameScore(i+1);
-  }
-  return this.currentScore;
+Game.prototype.totalScore = function(frame_number) {
+	for (var i = 0; i < frame_number; i++) {
+		this.currentScore += (this.frameScore(i + 1) + this.frameStrike(i) + this.frameSpare(i));
+	}
+	return this.currentScore;
 };
 
-Game.prototype.frameStrike = function () {
- return this.frameScore(1) + this.frameScore(2);
+Game.prototype.frameStrike = function() {
+	if (this.isStrike) {
+		return this.frameScore(1) + this.frameScore(2);
+    return this.isStrike = false;
+	}
+  return null;
 };
 
-Game.prototype.frameSpare = function () {
-  return this.frameScore(1) + this.currentFrame[2][1];
+Game.prototype.frameSpare = function() {
+	if (this.isSpare) {
+		return this.frameScore(1) + this.currentFrame[2][1];
+    return this.isSpare = false;
+	}
+  return null;
 };

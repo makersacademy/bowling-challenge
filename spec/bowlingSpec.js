fd82ff0d209061e4f8b80e4309504973c585867a
MrJons <shad_stick@hotmail.co.uk>
2016-09-18 17:35:42 +0100

    frame and roll logic, score recording and current roll functionality implemented

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,3 +1,5 @@
+/*jshint -W117 */
+
 describe("feature test", function(){ "use strict";
 
   var bowling;
@@ -19,10 +21,37 @@ describe("feature test", function(){ "use strict";
     expect(bowling._score).toEqual(0);
   });
 
-  it("bowl method calls dependant functions", function(){
-    spyOn(bowling, "pinsKnockdown");
-    bowling.bowl()
-    expect(bowling.pinsKnockdown).toHaveBeenCalled();
+  it("pinsKnockdown method generates number between 0 and 10 ", function(){
+    spyOn(bowling, 'pinsKnockdown').and.returnValue(3);
+    expect(bowling.pinsKnockdown()).toEqual(3);
   });
+
+  it("alternates roll count", function(){
+    bowling.rollAlternate();
+    expect(bowling._roll).toEqual(2);
+    bowling.rollAlternate();
+    expect(bowling._roll).toEqual(1);
+  });
+
+  it("increments frame count", function(){
+    bowling._standingPins = 0
+    bowling.frameIncrement();
+    expect(bowling._frame).toEqual(2);
+  });
+
+  it("manages frame and roll logic", function(){
+    bowling._standingPins = 0
+    bowling.frameAndRoll();
+    expect(bowling._frame).toEqual(2);
+    expect(bowling._roll).toEqual(1);
+  });
+
+  it("records roll score in correct variable",function(){
+    bowling.frameAndRoll();
+    spyOn(bowling, 'pinsKnockdown').and.returnValue(3);
+    bowling.rollScoreRecord();
+    expect(bowling._rollScore1).toEqual(0)
+    expect(bowling._rollScore2).toEqual(3)
+  })
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,22 +1,59 @@
-
+// check tests. next is frame reset.
 
 var Bowling = function Bowling(){"use strict";
   this._frame = 1;
   this._roll = 1;
   this._score = 0;
-  this._rollScore = 0;
+  this._rollScore1 = 0;
+  this._rollScore2 = 0;
   this._frameScore = 0;
-  this.standingPins = 10;
+  this._currentKnockdown = 0;
+  this._standingPins = 10;
 };
 
 Bowling.prototype = {
   bowl: function () {
-    this._rollScore = this.pinsKnockdown();
-    this.standingPins -= this.pinsKnockdown();
+    this.rollScoreRecord();
+    this.remainingPins();
+    this.frameAndRoll();
   },
 };
- 
 
+
+Bowling.prototype.rollScoreRecord = function(){
+  this._currentKnockdown = this.pinsKnockdown();
+  if(this._roll === 1){
+    this._rollScore1 = this._currentKnockdown
+  } else {
+    this._rollScore2 = this._currentKnockdown
+  }
+}
+
+// Random pin knockdown assignment
 Bowling.prototype.pinsKnockdown = function(){
-  return Math.floor(Math.random() * 10);
+  return Math.floor(Math.random() * (this._standingPins + 1));
+}
+
+Bowling.prototype.remainingPins = function(){
+  this._standingPins -= this._currentKnockdown
+}
+
+// below 3 methods manage frame and roll count logic
+Bowling.prototype.frameAndRoll = function(){
+  this.frameIncrement();
+  this.rollAlternate();
+}
+
+Bowling.prototype.frameIncrement = function(){
+  if(this._roll === 2 || this._standingPins === 0){
+    this._frame ++
+  }
+}
+
+Bowling.prototype.rollAlternate = function(){
+  if(this._roll === 1 && this._standingPins > 0){
+    this._roll = 2;
+  } else {
+    this._roll = 1;
+  }
 }

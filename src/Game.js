455ef13d70ffa0744c965d9480280069c27be896
lsewilson <laurawilson@hotmail.co.uk>
2016-09-18 18:51:33 +0100

    calculates scores when more than one strike in a row

--- src/Game.js
+++ src/Game.js
@@ -8,29 +8,42 @@ Game.prototype.bowl = function (rolls) {
 
 Game.prototype.score = function () {
   return this.frames.reduce(function(total, frame, i, frames){
-      return total + frame.total(frames[i + 1]);
+      return total + frame.total(frames[i + 1],frames[i+2]);
     }, 0);
 };
 
-////////////////////////////////////
+/////////////////////////////////////////////
 
 
 function Frame(rolls) {
   this.rolls = rolls;
+  this.noOfPins = 10;
 }
 
-Frame.prototype.total = function (next) {
+Frame.prototype.total = function (next_first, next_second) {
   var score = this.rolls.reduce(function(sum,roll){
     return sum + roll ;
   },0);
 
-  if (next === undefined || score !== 10 ) {
-    return score;
-  } else if (this.rolls[0] === 10){
-    var strikeBonus = next.rolls[0] + next.rolls[1];
+  if (this.isStrike()) {
+    if (next_first.isStrike()) {
+      var doubleBonus = next_first.rolls[0] + next_second.rolls[0];
+      return score + doubleBonus;
+    }
+    var strikeBonus = next_first.rolls[0] + next_first.rolls[1];
     return score + strikeBonus;
-  } else {
-    var spareBonus = next.rolls[0];
+  } else if (this.isSpare()) {
+    var spareBonus = next_first.rolls[0];
     return score + spareBonus;
+  } else {
+    return score;
   }
+};
+
+Frame.prototype.isSpare = function () {
+  return this.rolls[0] + this.rolls[1] === this.noOfPins
+};
+
+Frame.prototype.isStrike = function () {
+  return this.rolls[0] === this.noOfPins;
 };

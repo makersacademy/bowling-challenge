485aa2b10c3d1d31a35d270f2bdbaaf60e67e594
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-18 20:33:32 +0200

    basic function, bonus score calculation logic added

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,9 +1,63 @@
+"use strict";
+
 describe("Bowling", function() {
 
   var bowling;
 
   beforeEach(function() {
-    bowling = new Bowling();
+    bowling = new Bowling("Bob");
+  });
+
+  describe("stores player name", function() {
+    it("it stores the player's name at start", function() {
+      expect(bowling.playerName).toEqual("Bob");
+    })
+  });
+
+  describe("play game", function() {
+    it("rolls the ball twice in a frame with less than 10 pins down ", function() {
+      spyOn(Math, "floor").and.returnValue(4);
+      bowling.roll();
+      bowling.roll();
+      expect(bowling.score).toEqual(8);
+    });
+
+    it("it increases the frameNumber after completing a frame", function() {
+      bowling.roll();
+      bowling.roll();
+      expect(bowling.frameNumber).toEqual(2);
+    });
+
+    it("finishes the frame if the first roll is a strike", function() {
+      spyOn(Math, "floor").and.returnValue(10);
+      bowling.roll();
+      expect(bowling.frameNumber).toEqual(2);
+    });
+  });
+
+  describe("handle bonus scores", function() {
+    it("adds bonus scores in case of a strike", function() {
+      spyOn(Math, "floor").and.returnValue(10);
+      bowling.roll();
+      bowling.roll();
+      expect(bowling.score).toEqual(30);
+    });
+
+    it("add bonus scores in case of spares, in 2 frames", function() {
+      spyOn(Math, "floor").and.returnValue(5);
+      for (var i = 0; i < 4; i++) {
+        bowling.roll();
+      };
+      expect(bowling.score).toEqual(25)
+    });
+
+    it("add bonus scores in case of spares, in 6 frames", function() {
+      spyOn(Math, "floor").and.returnValue(5);
+      for (var i = 0; i < 12; i++) {
+        bowling.roll();
+      }
+      expect(bowling.score).toEqual(85);
+    });
   });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,65 @@
-function Bowling() {
+'use strict';
 
+function Bowling(name) {
+  this.game = [];
+  this.gameScores = [];
+  this.playerName = name;
+  this.score = 0;
+  this.frameNumber = 1;
+  this.currentFrame = [];
 }
+
+Bowling.prototype = {
+  isFirstRoll: function() {
+    return this.currentFrame.length === 0;
+  },
+  isSecondRoll: function() {
+    return this.currentFrame.length === 1
+  },
+  roll: function() {
+    if (this.isFirstRoll()) {
+      var pinsDown = Math.floor(Math.random() * 11);
+      // console.log("pinsDown1: " + pinsDown);
+      this.currentFrame.push(pinsDown);
+      this.score += pinsDown;
+      if (pinsDown === 10) {
+        this.currentFrame.push(0);
+        // console.log("currentFrame: " + this.currentFrame);
+        this.game.push(this.currentFrame);
+        // console.log("game: " + this.game);
+        // console.log("score: " + this.score);
+        this.calculateBonusScore();
+        this.frameNumber++;
+        this.currentFrame = [];
+      };
+    }
+    else if (this.isSecondRoll()){
+      var availablePins = 10 - this.currentFrame[0];
+      var pinsDown = Math.floor(Math.random() * (availablePins + 1));
+      this.score += pinsDown;
+      this.currentFrame.push(pinsDown);
+      this.game.push(this.currentFrame);
+      // console.log("Score2: " + this.score);
+      this.calculateBonusScore();
+      this.currentFrame = [];
+      this.frameNumber++
+    }
+  },
+  calculateBonusScore: function() {
+    // console.log("Game2: " + this.game);
+    if (this.frameNumber != 1 && this.game[this.game.length - 1][0] === 10) {
+      var bonusScore = this.currentFrame[0] + this.currentFrame[1];
+      // console.log("bonusScore: " + bonusScore);
+      this.gameScores[this.gameScores.length - 1] += bonusScore;
+      this.score += bonusScore;
+    }
+    else if (this.frameNumber != 1 && ((this.game[this.game.length - 1][0] + this.game[this.game.length - 1][1]) === 10)) {
+      var bonusScore = this.currentFrame[0];
+      // console.log("bonusScore2: " + bonusScore);
+      this.gameScores[this.gameScores.length - 1] += bonusScore;
+      this.score += bonusScore;
+    }
+  }
+
+
+};

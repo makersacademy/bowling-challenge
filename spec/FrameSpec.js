3d3fe47fb72912bf0a11042c231194b1a90d5565
lsewilson <laurawilson@hotmail.co.uk>
2016-09-18 17:55:10 +0100

    calculates strike bonus in frame class

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -9,6 +9,13 @@ describe("Frame", function(){
   it("calculates the total for a spare", function() {
     var frame1 = new Frame([6,4]);
     var frame2 = new Frame([1,1]);
-    expect(frame1.total(frame2)).toEqual(11)
+    expect(frame1.total(frame2)).toEqual(11);
   })
+
+  it("calculates the total for a strike", function(){
+    var frame1 = new Frame([10,0]);
+    var frame2 = new Frame([6,2]);
+    expect(frame1.total(frame2)).toEqual(18);
+  })
+
 })

90f45c3c940826baa3a59a95987132737f977193
johnnydee8 <j.dworzynski@me.com>
2016-09-24 11:25:46 +0100

    passing test for strike

--- spec/ScoreSpec.js
+++ spec/ScoreSpec.js
@@ -5,48 +5,57 @@ describe("Score", function() {
     score = new Score();
   });
 
-  it("should score frames as arrays inside an array", function() {
-    score.addRoll(1);
-    console.log('Added ' + 1)
-    score.addRoll(4);
-    score.addRoll(7);
-    score.addRoll(1);
-    expect(score.results).toEqual([[1,4],[7,1]]);
-  });
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
+  describe("storing frames", function() {
+    it("should score frames as arrays inside an array", function() {
+      score.addRoll(1);
+      score.addRoll(4);
+      score.addRoll(7);
+      score.addRoll(1);
+      expect(score.results).toEqual([[1,4],[7,1]]);
     });
 
-    xit("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
+    it("should close frame on strike", function(){
+      score.addRoll(10);
+      score.addRoll(7);
+      score.addRoll(1);
+      expect(score.results).toEqual([[10],[7,1]])
+    })
 
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
+    
+
+
+    // beforeEach(function() {
+    //   player.play(song);
+    //   player.pause();
+    // });
+    //
+    // xit("should indicate that the song is currently paused", function() {
+    //   expect(player.isPlaying).toBeFalsy();
+    //
+    //   // demonstrates use of 'not' with a custom matcher
+    //   expect(player).not.toBePlaying(song);
+    // });
 
   });
 
   // demonstrates use of spies to intercept and test method calls
-  xit("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
+  // xit("tells the current song if the user has made it a favorite", function() {
+  //   spyOn(song, 'persistFavoriteStatus');
+  //
+  //   player.play(song);
+  //   player.makeFavorite();
+  //
+  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  // });
 
   //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    xit("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
-  });
+  // describe("#resume", function() {
+  //   xit("should throw an exception if song is already playing", function() {
+  //     player.play(song);
+  //
+  //     expect(function() {
+  //       player.resume();
+  //     }).toThrowError("song is already playing");
+  //   });
+  // });
 });
--- src/Score.js
+++ src/Score.js
@@ -5,17 +5,25 @@ function Score() {
 }
 
 Score.prototype.addRoll = function(roll) {
-  if (this.frameFull(this.results[this.results.length - 1])) {
+  if (this.isFrameFull(this.results[this.results.length - 1])) {
     this.results.push([roll]);
   } else {
-    this.results[this.results.length -1].push(roll);
+    this.results[this.results.length - 1].push(roll);
   }
 };
 
-Score.prototype.frameFull = function (currentFrame) {
-    if (currentFrame.length === 2){
+Score.prototype.isFrameFull = function (currentFrame) {
+    if (currentFrame.length === 2 || this.isStrike(currentFrame)){
       return true
     } else {
       return false
     }
+};
+
+Score.prototype.isStrike = function (currentFrame) {
+  if (currentFrame[currentFrame.length - 1] === 10) {
+    return true
+  } else {
+    return false
+  }
 };

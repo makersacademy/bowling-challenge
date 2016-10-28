2f1555835cd73aae3c4c5e5b6d9805d9163aea55
Chetan Sanghani <crsanghani@gmail.com>
2016-10-29 00:35:44 +0100

    first test written for starting score

---
+++ spec/bowlingScoreCardSpec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe ('Scorecard', function() {
+
+  var Scorecard = require('../src/bowlingScoreCard');
+  var scorecard;
+
+  beforeEach(function() {
+    scorecard = new Scorecard;
+  });
+
+  describe('start conditions', function(){
+
+    it('it starts with a total score of 0', function(){
+      expect(scorecard.total).toEqual(0)
+    });
+  });
+});

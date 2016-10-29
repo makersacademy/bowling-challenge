ff54712a4edb29be4438b6f4671705665e7be817
Malinna Leach <malinnab@yahoo.com>
2016-10-29 17:16:40 +0100

    refactored some of the logic

--- src/Game.js
+++ src/Game.js
@@ -18,59 +18,70 @@ Game.prototype.roll = function() {
   this.resetStatus(hits);
   if (!this.gameOver) {
     this.resetPins(hits);
-    this.resetMode(hits);
+    this.resetMode(hits, this.scoreMode);
     this.resetRolls(hits);
     this.lastRollScore = hits;
-  };
+  }
 };
 
 Game.prototype.score = function(hits) {
-  if (this.scoreMode == "normal") {
+  if (this.scoreMode === "normal") {
       this.scoreTotal += hits;
-  } else if (this.scoreMode == "doubleStrike") {
+  } else if (this.scoreMode === "doubleStrike") {
     this.scoreTotal += hits *3;
   } else {
       this.scoreTotal += hits * 2;
-  };
+  }
 };
 
 Game.prototype.resetStatus = function(hits) {
-  if (this.currentFrame == 10 && this.currentRoll == 2 && (hits + this.lastRollScore) < 10) {
+  if (this.currentFrame === 10 && this.currentRoll === 2 && (hits + this.lastRollScore) < 10) {
     this.gameOver = true;
-  } else if (this.currentFrame == 10 && this.currentRoll == 3) {
+  } else if (this.currentFrame === 10 && this.currentRoll === 3) {
     this.gameOver = true;
-  };
+  }
 };
 
 Game.prototype.resetPins = function(hits) {
-  if (this.currentRoll == 1 && hits < 10) {
+  if (this.currentRoll === 1 && hits < 10) {
     this.pins = 10 - hits;
   } else {
     this.pins = 10;
-  };
+  }
 };
 
-Game.prototype.resetMode = function(hits) {
-  if ((this.scoreMode == "strike" && this.lastRollScore < 10) || (this.currentFrame == 10 && this.currentRoll == 2)) {
-    this.scoreMode = "normal";
-  } else if (hits == 10 && this.lastRollScore < 10) {
+
+Game.prototype.resetMode = function(hits, mode) {
+  if (this.currentFrame === 10) {
+    this.resetFrame10(hits, mode);
+  } else if (hits === 10 && this.lastRollScore < 10) {
     this.scoreMode = "strike";
-  } else if(this.scoreMode == "doubleStrike" && (this.lastRollScore < 10 || (this.currentFrame == 10 && this.currentRoll == 1))) {
-    this.scoreMode = "strike";
-  } else if (hits == 10 && this.lastRollScore == 10) {
+  } else if (hits === 10 && this.lastRollScore === 10) {
     this.scoreMode = "doubleStrike";
-  } else if (this.currentRoll == 2 && this.lastRollScore + hits == 10) {
+  } else if (mode === "strike" && this.lastRollScore < 10) {
+    this.scoreMode = "normal";
+  } else if (mode === "doubleStrike" && this.lastRollScore < 10) {
+    this.scoreMode = "strike"
+  } else if (this.currentRoll === 2 && this.lastRollScore + hits === 10) {
     this.scoreMode = "spare";
-  } else if (this.scoreMode == "spare") {
+  } else if (mode === "spare") {
+    this.scoreMode = "normal";
+  }
+};
+
+Game.prototype.resetFrame10 = function(hits, mode) {
+  if (hits === 10 && this.lastRollScore === 10 && this.currentRoll === 1) {
+        this.scoreMode = "strike"
+  }  else {
     this.scoreMode = "normal";
   }
 };
 
 Game.prototype.resetRolls = function(hits) {
-    if((this.currentRoll == 2 || hits == 10) && this.currentFrame != 10 ){
+    if((this.currentRoll === 2 || hits === 10) && this.currentFrame !== 10 ){
       this.currentFrame += 1;
       this.currentRoll = 1;
     } else {
       this.currentRoll += 1
-  };
+  }
 };

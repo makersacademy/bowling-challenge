6198aeca81300470de2cbf54ed5cd48d0e7038f5
Darrenf <darrenf54321@gmail.com>
2016-10-29 14:43:24 +0100

    first six tests passing, html and css file added (with relevant disclosure in Readme file

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,20 +1,41 @@
 'use strict';
 
-describe('Game', function() {
+describe("Game", function() {
   var game;
 
   beforeEach(function() {
     game = new Game();
   });
 
-  describe('start of game', function(){
-
-    it('starts a game with 10 pins', function() {
-      expect(game.pins).toEqual(10);
-    });
-
-    it('starts a game with 10 frames', function() {
-      expect(game.frames).toEqual(10);
-    });
+  it("starts a game with a 10 frames maximum", function() {
+    expect(game.MAX_FRAMES).toEqual(10);
   });
+
+  it("starts a game with 10 pins", function() {
+    expect(game.gamePins).toEqual(10);
+  });
+
+  it("starts the game with a score of 0", function() {
+    expect(game.getCurrentScore()).toEqual(0);
+  });
+
+  it ("ends the game after 10 frames", function() {
+    game.currentFrame;
+    for (var i = 0; i < 10; i++) {
+      game.playFrame();
+    }
+    expect(function(){ game.playFrame(); }).toThrowError("Game Over");
+  });
+
+  it('confirms each play function plays a frame', function(){
+    game.playFrame();
+    expect(game.currentFrame).toEqual(1);
+  });
+
+  it("updates the score", function () {
+    game.updateScore(4);
+    expect(game.getCurrentScore()).toEqual(4);
+  });
+
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,31 @@
+
 'use strict';
 
 function Game() {
+  this.MAX_FRAMES = 10;
+  this.gamePins = 10;
+  this.currentFrame = 0;
+  this.gamePoints = 0;
+  this.framePoints = 0;
+  this.frames = [];
 
-  this.pins = 10;
-  this.frames = 10;
 }
 
+Game.prototype.getCurrentFrame = function() {
+  return this.currentFrame;
+}
 
-// Game.prototype.play = function(num) {
-//   if (this.pins  === 0); {
-//   return this.isGameOver
-// }
+Game.prototype.playFrame = function() {
+  if(this.currentFrame + 1 > this.MAX_FRAMES) {
+    throw new Error("Game Over");
+  }
+  this.currentFrame += 1;
+}
+
+Game.prototype.getCurrentScore = function () {
+  return this.framePoints;
+}
+
+Game.prototype.updateScore = function (number) {
+  this.framePoints += number;
+}

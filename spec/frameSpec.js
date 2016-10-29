363aaa10449f284af2aa55121dcc99b902e35383
Laura Weston <laura.weston@outlook.com>
2016-10-29 17:45:13 +0100

    Frame calculates strikes and spares

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,28 +1,45 @@
 describe('Frame', function() {
     var Frame = require('../lib/Frame.js');
-    var frame = new Frame();
+    var frame;
+
+    beforeEach(function() {
+        frame = new Frame();
+    });
 
     describe('initialization', function() {
-        it('score for each bowl is undefined', function() {
-            expect(frame.firstBowl).toEqual(undefined);
-            expect(frame.secondBowl).toEqual(undefined);
-        });
         it('total frame score starts at zero', function() {
-            expect(frame.totalScore).toEqual(0);
+            expect(frame.totalScore()).toEqual(0);
         });
     })
     
     describe('keeping score', function() {
         it('takes a score for first bowl', function() {
             frame.bowl(5);
-            expect(frame.firstBowl).toEqual(5);
-            expect(frame.totalScore).toEqual(5);
+            expect(frame.totalScore()).toEqual(5);
         });
         it('takes a score for second bowl', function() {
+            frame.bowl(5);
             frame.bowl(3);
-            expect(frame.secondBowl).toEqual(3);
-            expect(frame.totalScore).toEqual(8);
+            expect(frame.totalScore()).toEqual(8);
         });
     });
-    
+
+    describe('strikes and spares', function() {
+        it('reports it is not a strike at start of game', function() {
+            expect(frame.isAStrike()).toBe(false);
+        });
+        it('reports it is not a spare at start of game', function() {
+            expect(frame.isASpare()).toBe(false);
+        });
+        it('reports it is a strike when first bowl scores 10', function() {
+            frame.bowl(10);
+            expect(frame.isAStrike()).toBe(true);
+            expect(frame.isASpare()).toBe(false);
+        });
+        it('reports it is a spare when total score after two bowls is 10', function() {
+            frame.bowl(5);
+            frame.bowl(5);
+            expect(frame.isASpare()).toBe(true);
+        });
+    });
 });

55e7fc50035423d7d1f093bf4771ffca98bf76db
Tomy8s <tomy8s@hotmail.com>
2016-10-29 17:10:39 +0100

    Slider returns num of pins down to console

---
+++ app/bowling.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#bowl").click(function() {
+    // console.log(Math.abs($("#throwAngle").val()));
+    console.log(10 - Math.abs($("#throwAngle").val()));
+  });
+});

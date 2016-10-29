bb148238c3b5ca1b98dab4fac48873ee78854549
Tomy8s <tomy8s@hotmail.com>
2016-10-29 17:24:46 +0100

    calculates total and inserts into correct cell

--- app/bowling.js
+++ app/bowling.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var frame = 1;
     var ball = 1;
+    var total = 0;
   $("#bowl").click(function() {
     // Calculate number of pins down
     // console.log(Math.abs($("#throwAngle").val()));
@@ -8,8 +9,9 @@ $(document).ready(function() {
     console.log(pinsDown);
 
     // Insert result into table
-
     $("#frame"+ frame +" #ball"+ ball).text(pinsDown);
+    total += pinsDown;
+    $("#total").text(total);
     ball++;
     if(ball === 3) {
       frame++;

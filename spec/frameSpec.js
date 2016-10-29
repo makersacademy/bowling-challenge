e92eefca7ae59d87edaaf8402920f642a0c4780e
Laura Weston <laura.weston@outlook.com>
2016-10-29 14:13:19 +0100

    Add describes to frameSpec

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,19 +2,25 @@ describe('Frame', function() {
     var Frame = require('../lib/Frame.js');
     var frame = new Frame();
 
-    it('starts with each bowl score undefined', function() {
-        expect(frame.firstBowl).toEqual(undefined);
-        expect(frame.secondBowl).toEqual(undefined);
+    describe('initialization', function() {
+        it('score for each bowl is undefined', function() {
+            expect(frame.firstBowl).toEqual(undefined);
+            expect(frame.secondBowl).toEqual(undefined);
+        });
+        it('total frame score starts at zero', function() {
+            expect(frame.totalScore()).toEqual(0);
+        });
+    })
+    
+    describe('keeping score', function() {
+        it('takes a score for first bowl', function() {
+            frame.bowl(5);
+            expect(frame.firstBowl).toEqual(5);
+        });
+        it('takes a score for second bowl', function() {
+            frame.bowl(3);
+            expect(frame.secondBowl).toEqual(3);
+        });
     });
-    it('starts with total frame score of zero', function() {
-        expect(frame.totalScore()).toEqual(0);
-    });
-    it('takes a score for first bowl', function() {
-        frame.bowl(5);
-        expect(frame.firstBowl).toEqual(5);
-    });
-    it('takes a score for second bowl', function() {
-        frame.bowl(3);
-        expect(frame.secondBowl).toEqual(3);
-    });
+    
 });

3ecaac3706d8a3e7206b9774807e5296c5788fc4
Laura Weston <laura.weston@outlook.com>
2016-10-29 14:08:20 +0100

    Frame takes scores for first and second bowls

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,11 +2,19 @@ describe('Frame', function() {
     var Frame = require('../lib/Frame.js');
     var frame = new Frame();
 
-    it('starts with zero score for the two bowls', function() {
-        expect(frame._firstBowl).toEqual(0);
-        expect(frame._secondBowl).toEqual(0);
+    it('starts with each bowl score undefined', function() {
+        expect(frame.firstBowl).toEqual(undefined);
+        expect(frame.secondBowl).toEqual(undefined);
     });
     it('starts with total frame score of zero', function() {
         expect(frame.totalScore()).toEqual(0);
+    });
+    it('takes a score for first bowl', function() {
+        frame.bowl(5);
+        expect(frame.firstBowl).toEqual(5);
+    });
+    it('takes a score for second bowl', function() {
+        frame.bowl(3);
+        expect(frame.secondBowl).toEqual(3);
     });
 });

ff552fcdf6b90e3183b25733932aa81d0dcd694d
Laura Weston <laura.weston@outlook.com>
2016-10-29 13:58:59 +0100

    Frame starts with total score of zero

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,8 +2,11 @@ describe('Frame', function() {
     var Frame = require('../lib/Frame.js');
     var frame = new Frame();
 
-    it('has two bowls', function() {
+    it('starts with zero score for the two bowls', function() {
         expect(frame._firstBowl).toEqual(0);
         expect(frame._secondBowl).toEqual(0);
+    });
+    it('starts with total frame score of zero', function() {
+        expect(frame.totalScore()).toEqual(0);
     });
 });

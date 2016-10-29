5ff67daf20247d438a79cacc98e4bf499aba8aa6
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-29 18:32:11 +0100

    first commit

--- Jasmine/spec/WeatherSpec.js
+++
@@ -1,8 +0,0 @@
-describe('Weather', function () {
-  var weather = new Weather ();
-
-  it ('should define if the weather is stormy', function () {
-    spyOn(weather, 'isStormy').and.returnValue(true)
-    expect(weather.isStormy()).toEqual(true)
-  });
-});
--- Jasmine/src/Weather.js
+++
@@ -1,7 +0,0 @@
-function Weather () {
-
-};
-
-Weather.prototype.isStormy = function () {
-  return Math.random >= 0.75;
-};

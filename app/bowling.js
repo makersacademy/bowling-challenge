7d0c445fe3b73ed36f87dc627c6e7e5699675cb8
Tomy8s <tomy8s@hotmail.com>
2016-10-29 17:20:24 +0100

    Inserts pinsDown (<=10) into correct cell

--- app/bowling.js
+++ app/bowling.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+    var frame = 1;
+    var ball = 1;
   $("#bowl").click(function() {
+    // Calculate number of pins down
     // console.log(Math.abs($("#throwAngle").val()));
-    console.log(10 - Math.abs($("#throwAngle").val()));
+    var pinsDown =  10 - Math.abs($("#throwAngle").val());
+    console.log(pinsDown);
+
+    // Insert result into table
+
+    $("#frame"+ frame +" #ball"+ ball).text(pinsDown);
+    ball++;
+    if(ball === 3) {
+      frame++;
+      ball = 1;
+    }
   });
 });

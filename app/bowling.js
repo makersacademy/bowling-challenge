c6338f077c2eb18d04e56b4791194365e59800e2
Tomy8s <tomy8s@hotmail.com>
2016-10-29 19:14:44 +0100

    added spare bonus functionality

--- app/bowling.js
+++ app/bowling.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
   var ball = 1;
   var total = 0;
   var points = 0;
-  // Randomise throwAngle
+
+  // Randomise default throwAngle i.e. can't set slider to '10' once and keep 
+  // clicking to add 10 every time!
   var randomAngle = function() {
     var angle = Math.floor(Math.random() * 21) - 10;
     $("#throwAngle").val(angle);
@@ -15,14 +17,12 @@ $(document).ready(function() {
     if(frame <= 10) {
       // Calculate number of pins down
       var pinsDown =  10 - Math.abs($("#throwAngle").val());
-      // console.log(pinsDown);
 
       //Calculate correct value for 2nd bowl
       if(ball === 1) {
         points = pinsDown;
       } else {
         console.log(pinsDown + "last roll " + points);
-        // previousBowl = parseInt($("#frame"+ frame +" #ball"+ (ball - 1)).text());
         points = (points + pinsDown > 10) ? (10 - points) : pinsDown;
       }
 
@@ -32,17 +32,24 @@ $(document).ready(function() {
       $("#total").text(total);
       $("#frame" + frame + " #ball3").text(total);
 
-      // Add bonus if strike
-      if($("#frame" + (frame - 1) + " #ball1").text() == 10) {
+      // Add bonus if strike ELSE if spare
+      var previousBall1 = parseInt($("#frame" + (frame - 1) + " #ball1").text());
+      var previousBall2 = parseInt($("#frame" + (frame - 1) + " #ball2").text());
+
+      if(previousBall1 == 10) {
         total += points;
-        if(ball === 2) {
-          var previousPoints = parseInt($("#frame"+ frame +" #ball"+ (ball - 1)).text());
-          $("#frame" + (frame - 1) + " #ball3").text(total - points - previousPoints);
-        } else {
+        if(ball === 1) {
           $("#frame" + (frame - 1) + " #ball3").text(total - points);
+          var previousPoints = points;
+        } else {
+          $("#frame" + (frame - 1) + " #ball3").text(total - points - previousPoints);
         }
         $("#frame" + frame + " #ball3").text(total);
-      }
+        } else if ((previousBall1 + previousBall2 === 10) && ball === 1) {
+          total += points;
+          $("#frame" + (frame - 1) + " #ball3").text(total - points);
+        }
+
 
       // Prepare for next entry
       if((ball === 2) || (ball === 1 && pinsDown === 10)) {

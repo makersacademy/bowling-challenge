e0e6835bde8aa1b87bd2a2bbded198dc635090dc
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-29 17:55:11 +0100

    adds refactoring to make Score into object constructor, rather than object literal. Allows for multiple scores to be kept.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,11 +1,13 @@
 describe('bowling: ', function() {
 
   var player1;
+  var score1;
 
   beforeEach(function() {
     player1 = new Player();
-    roll1 = player1.roll();
-    // roll2 = player1.roll();
+    score1 = new Score();
+    roll1 = player1.roll(score1);
+
   });
 
   it('the player knocks down 0-10 pins when I roll',function() {
@@ -16,13 +18,13 @@ describe('bowling: ', function() {
   describe('scoring: ', function() {
 
     it('stores the most recent roll', function() {
-      expect(score._mostRecentRoll).toEqual(roll1);
+      expect(score1._mostRecentRoll).toEqual(roll1);
     });
 
     it('moves the "current roll" to "previous roll" when the ball is rolled again', function() {
-      roll2 = player1.roll();
-      expect(score._previousRoll).toEqual(roll1);
-      expect(score._mostRecentRoll).toEqual(roll2);
+      roll2 = player1.roll(score1);
+      expect(score1._previousRoll).toEqual(roll1);
+      expect(score1._mostRecentRoll).toEqual(roll2);
     });
 
   });

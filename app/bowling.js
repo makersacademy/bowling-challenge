6ebb7fce5459165fb321afcf22f1ff3b7f9194d1
Tomy8s <tomy8s@hotmail.com>
2016-10-29 17:41:10 +0100

    caps max per frame to 10

--- app/bowling.js
+++ app/bowling.js
@@ -4,9 +4,15 @@ $(document).ready(function() {
     var total = 0;
   $("#bowl").click(function() {
     // Calculate number of pins down
-    // console.log(Math.abs($("#throwAngle").val()));
     var pinsDown =  10 - Math.abs($("#throwAngle").val());
-    console.log(pinsDown);
+    // console.log(pinsDown);
+
+    //Calculate correct value for 2nd bowl
+    if(ball > 1) {
+      console.log(pinsDown);
+      previousBowl = parseInt($("#frame"+ frame +" #ball"+ (ball - 1)).text());
+      pinsDown = (previousBowl + pinsDown > 10) ? (10 - previousBowl) : pinsDown;
+    }
 
     // Insert result into table
     $("#frame"+ frame +" #ball"+ ball).text(pinsDown);

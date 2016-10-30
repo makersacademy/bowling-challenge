8c496fcb0737decc6c9bf008cf9c69dbe02abcc1
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-30 18:17:59 +0000

    create method pinsLeft that subtracts from first roll

--- jasmine-standalone-2.5.2/spec/frameSpec.js
+++ jasmine-standalone-2.5.2/spec/frameSpec.js
@@ -1,15 +1,25 @@
-describe('Frame', function(){
-   it('begins each frame with a score of zero', function(){
-     var frame = new Frame();
-     expect(frame.score1).toBe(0);
+'use strict';
+describe('Frame', function () {
+    var frame;
+
+    beforeEach(function () {
+      frame = new Frame();
+    });
+
+     it('begins each frame with a score of zero', function () {
+       expect(frame.score1).toBe(0);
+     });
+
+     it('is able to return a score out of 10 on the first roll', function() {
+       frame.roll1();
+       expect(frame.score1).toBeGreaterThan(0);
+       expect(frame.score1).toBeLessThan(11);
+     });
+
+      it('is able to asign the remaining pins to roll2', function () {
+        spyOn(frame, 'score1').and.returnValue(4);
+        frame.roll1();
+        frame.pinsLeft();
+        expect(frame.totalPins).toBeLessThan(10);
+      });
    });
-
-   it('is able to return a score out of 10 on the first roll', function(){
-     var frame = new Frame();
-     frame.roll1();
-     expect(frame.score1).toBeGreaterThan(0);
-     expect(frame.score1).toBeLessThan(11);
-
-    //  expect(frame.score1).toBeWithInRange(0,10);
-  });
-});
--- jasmine-standalone-2.5.2/src/frame.js
+++ jasmine-standalone-2.5.2/src/frame.js
@@ -1,8 +1,56 @@
+'use strict';
 function Frame() {
-    'use strict';
-    this.score1=0
-};
+    this.score1 = 0;
+    this.totalPins;
+  };
 
-Frame.prototype.roll1 = function() {
+  Frame.prototype.roll1 = function() {
   return this.score1 = Math.floor((Math.random() * 10) + 1);
-};
+  };
+
+  Frame.prototype.pinsLeft = function() {
+    this.totalPins = 10 - this.score1;
+    return this.totalPins;
+  };
+// };
+
+// Bowling.prototype.firstRoll = function () {
+//   this.pinsKnocked = Math.round(Math.random() * (10 - 0));
+//   this.pinsLeft = 10 - this.pinsKnocked;
+//   this.changeRollNum();
+//   this.changeFrameNum();
+// };
+
+
+
+// function Person(name, family) {
+//     this.name = name;
+//     this.family = family;
+//
+//     var records = [{type: "in", amount: 0}];
+//
+//     this.addTransaction = function(trans) {
+//         if(trans.hasOwnProperty("type") && trans.hasOwnProperty("amount")) {
+//            records.push(trans);
+//         }
+//     }
+//
+//     this.balance = function() {
+//        var total = 0;
+//
+//        records.forEach(function(record) {
+//            if(record.type === "in") {
+//              total += record.amount;
+//            }
+//            else {
+//              total -= record.amount;
+//            }
+//        });
+//
+// Person.prototype.getFull = function() {
+//     return this.name + " " + this.family;
+// };
+//
+// Person.prototype.getProfile = function() {
+//      return this.getFull() + ", total balance: " + this.balance();
+// };

1bd6e64282cc3c0a12c5c916b71d66f9e6cbb656
Stephen Gregory <stejgregory@gmail.com>
2016-10-30 17:01:00 +0000

    Jasmine and Folders setup

---
+++ src/bowlingScorecard.js
@@ -0,0 +1,11 @@
+// 'use strict';
+//
+// function Thermostat() {
+//   this.DEFAULT_TEMPERATURE = 20;
+//   this.temperature = this.DEFAULT_TEMPERATURE;
+//   this.MINIMUM_TEMPERATURE = 10;
+//   this.powerSavingModeOn = true;
+//   this.MAX_LIMIT_PSM_ON = 25;
+//   this.MAX_LIMIT_PSM_OFF = 32;
+//   this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+// }

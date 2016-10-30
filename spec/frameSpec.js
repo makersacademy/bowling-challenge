a1529eb6f4a6041dd34bb574a9104babca478ebe
Laura Weston <laura.weston@outlook.com>
2016-10-30 16:55:15 +0000

    Refactor Frame completion test cases

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,75 +1,89 @@
 var Frame = require('../lib/Frame.js');
 
-describe('Frame', function() {
+describe('Frame', function () {
     var frame;
 
-    beforeEach(function() {
+    beforeEach(function () {
         frame = new Frame();
     });
 
-    describe('initialization', function() {
-        it('total frame score starts at zero', function() {
+    describe('initialization', function () {
+        it('total frame score starts at zero', function () {
             expect(frame.totalScore()).toEqual(0);
         });
     })
-    
-    describe('keeping score', function() {
-        it('takes a score for first bowl', function() {
+
+    describe('keeping score', function () {
+        it('takes a score for first bowl', function () {
             frame.bowl(5);
             expect(frame.totalScore()).toEqual(5);
         });
-        it('takes a score for second bowl', function() {
+        it('takes a score for second bowl', function () {
             frame.bowl(5);
             frame.bowl(3);
             expect(frame.totalScore()).toEqual(8);
         });
     });
 
-    describe('strikes and spares', function() {
-        it('reports it is not a strike at start of game', function() {
+    describe('strikes and spares', function () {
+        it('reports it is not a strike at start of game', function () {
             expect(frame.isAStrike()).toBe(false);
         });
-        it('reports it is not a spare at start of game', function() {
+        it('reports it is not a spare at start of game', function () {
             expect(frame.isASpare()).toBe(false);
         });
-        it('reports it is a strike when first bowl scores 10', function() {
+        it('reports it is a strike when first bowl scores 10', function () {
             frame.bowl(10);
             expect(frame.isAStrike()).toBe(true);
             expect(frame.isASpare()).toBe(false);
         });
-        it('reports it is a spare when total score after two bowls is 10', function() {
+        it('reports it is a spare when total score after two bowls is 10', function () {
             frame.bowl(5);
             frame.bowl(5);
             expect(frame.isASpare()).toBe(true);
         });
-        it('reports it is a spare when first bowl scores 0 and second bowl scores 10', function() {
+        it('reports it is a spare when first bowl scores 0 and second bowl scores 10', function () {
             frame.bowl(0);
             frame.bowl(10);
             expect(frame.isASpare()).toBe(true);
         });
     });
 
-    describe('completion', function() {
-        it('reports the frame is not completed at start', function() {
-            expect(frame.isComplete()).toBe(false);
+    describe('completion', function () {
+        var testCases = [
+            {
+                bowls: [],
+                expectIsComplete: false
+            },
+            {
+                bowls: [5],
+                expectIsComplete: false
+            },
+            {
+                bowls: [5, 4],
+                expectIsComplete: true
+            },
+            {
+                bowls: [10],
+                expectIsComplete: true
+            },
+            {
+                bowls: [0, 10],
+                expectIsComplete: true
+            },
+            {
+                bowls: [0, 0],
+                expectIsComplete: true
+            }
+        ];
+
+        testCases.forEach(function (testCase) {
+            it('reports whether the frame is completed', function () {
+                testCase.bowls.forEach(function (score) {
+                    frame.bowl(score);
+                });
+                expect(frame.isComplete()).toBe(testCase.expectIsComplete);
+            });
         });
-        it('reports the frame is not completed after first bowl if score is less than 10', function() {
-            frame.bowl(5);
-            expect(frame.isComplete()).toBe(false);
-        });
-        it('reports the frame is complete if first bowl is a strike', function() {
-            frame.bowl(10);
-            expect(frame.isComplete()).toBe(true);
-        });
-        it('reports the frame is complete after two bowls', function() {
-            frame.bowl(0);
-            frame.bowl(10);
-            expect(frame.isComplete()).toBe(true);
-        })
-        it('reports the frame is complete after two gutter bowls', function() {
-            frame.bowl(0);
-            frame.bowl(0);
-            expect(frame.isComplete()).toBe(true);
-        })
     });
 });

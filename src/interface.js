ab0b2b24413decfb01aea27977417644cd1bc0a5
glynester <glynester@hotmail.com>
2016-10-30 15:49:19 +0000

    Can pass value of a button to the rollBall function.

--- src/interface.js
+++ src/interface.js
@@ -5,21 +5,19 @@ $(document).ready(function(){
   // Function to detect button clicked.
 
 
-  function rollBall(pinsDown){
-    alert("rollBall called!");
-  }
-
-  $(".bowling-calc-buttons-container").click(function(){
-    // alert(this['name']);
-  });
+  // function rollBall(pinsDown){
+  //   alert("rollBall called!");
+  // }
 
   $("[type=button]").click(function(){
-    alert(this['value']);
+    // alert(this['value']);
+    rollBall(this['value']);
   });
 
   // $(".bowling-calc-buttons-container").click(function(){
-  $('[name=b-3]').click(function(){
-
+  // $('[name=b-3]').click(function(){
+  function rollBall(pinsDown){
+    // alert(pinsDown);
     roll = new Roll();
     roll.addRoll(6);
     roll.addRoll(2);
@@ -50,7 +48,7 @@ $(document).ready(function(){
         // })
         $('#edit-game-result').val(game.totalScore);
       }
-    })
+    }
 
   //edit-frame1-res
   //edit-frame1-1, edit-frame1-2, edit-frame2-1

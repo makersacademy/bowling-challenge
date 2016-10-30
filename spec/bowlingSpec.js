a9d7c50554b05338054283f9de88aed3415e66b1
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-30 20:31:22 +0000

    adds mostly working code before refactoring

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -15,7 +15,7 @@ describe('bowling: ', function() {
     expect(roll1).toBeLessThan(11);
   });
 
-  describe('scoring: ', function() {
+  describe('frame management: ', function() {
 
     it('stores the most recent roll', function() {
       expect(score1._mostRecentRoll).toEqual(roll2);
@@ -27,25 +27,30 @@ describe('bowling: ', function() {
     });
 
     it('stores the frame when the frame is finished (not strike)', function() {
+      // console.log('roll 1: ' + roll1);
+      // console.log('roll 2: ' + roll2);
+      // console.log('all frames: ' + score1._allFrames)
+      // console.log('current frame: ' + score1._currentFrame)
       expect(score1._allFrames[0][0]).toEqual(roll1);
       expect(score1._allFrames[0][1]).toEqual(roll2);
     });
 
-    it('moves the current frame to the frame history when the frame is finished', function() {
-      console.log('roll 1: ' + roll1);
-      console.log('roll 2: ' + roll2);
-
+    it('moves the current frame to the frame history when the frame is finished (non-strike)', function() {
       roll3 = player1.roll(score1);
       roll4 = player1.roll(score1);
-      //
-      console.log('roll 3: ' + roll3);
-      console.log('roll 4: ' + roll4);
-      console.log('all frames: ' + score1._allFrames)
-      console.log('current frame: ' + score1._currentFrame)
-      console.log('roll count: ' + rollCount);
       expect(score1._allFrames[0][0]).toEqual(roll1);
       expect(score1._allFrames[0][1]).toEqual(roll2);
     });
+
+// YOU WERE ADDING STRIKE FUNCTIONALITY TO CODE.
+// YOU NEED TO SUBTRACT ROLL1 PINS FOR ROLL2!
+
+
+    // it('stores strikes in a frame by themselves', function() {
+    //
+    // });
+
+
 
   });
 
@@ -55,3 +60,8 @@ describe('bowling: ', function() {
 //   spyOn(score1, 'mostRecentRoll').and.returnValue(10);
 //   expect(XXX).toEqual(10);
 // });
+
+//
+
+
+// console.log('roll count: ' + rollCount);

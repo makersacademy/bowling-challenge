35895410d61193a82fbe846f906270de564e9722
crispinandrews <crispin.andrews@gmail.com>
2016-10-30 14:15:47 +0000

    Bowl count added, endframe method runs automatically after second bowl, score  can be calculated via both frame totals and individual bowl scores

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,27 +2,26 @@
 
 describe('Game', function(){
   var game;
-  // var frame;
 
   beforeEach(function(){
     game = new Game();
-    // frame = new Frame();
   });
 
   it('has an initial score of 0', function(){
     expect(game.frameScore).toEqual(0)
   });
 
-  it('can add a bowl score to the frame', function(){
+  it('can add a bowl score to the current frame', function(){
     game.bowl(5);
     game.bowl(1);
     expect(game.frameScore).toEqual(6)
   });
 
-  it('can store each bowls score the frame array', function(){
+  it('can store each bowls score in the frame array', function(){
     game.bowl(5);
     game.bowl(1);
-    expect(game.currentFrame).toEqual([5,1])
+    expect(game.currentFrame).toEqual([])
+    expect(game.allFrames).toEqual([[5,1]])
   });
 
   it('reduces the bowl count by 1 after each bowl is taken', function(){
@@ -33,11 +32,43 @@ describe('Game', function(){
   it('holds the games frames', function(){
     game.bowl(1)
     game.bowl(2)
-    game.endFrame(game.currentFrame)
     game.bowl(5)
-    game.bowl(6)
-    game.endFrame(game.currentFrame)
-    expect(game.allFrames).toEqual([[1,2],[5,6]])
+    game.bowl(4)
+    expect(game.allFrames).toEqual([[1,2],[5,4]])
     expect(game.currentFrame).toEqual([])
+  });
+
+  it('holds the games frame by frame total scores', function(){
+    game.bowl(1)
+    game.bowl(2)
+    game.bowl(5)
+    game.bowl(4)
+    expect(game.frameByFrameScore).toEqual([3,9])
+  });
+
+  it('counts the frame number', function(){
+    game.bowl(1)
+    game.bowl(2)
+    game.bowl(5)
+    game.bowl(4)
+    expect(game.frameCount).toEqual(2)
+  });
+
+  it('calculates the score total frame scores', function(){
+    game.bowl(1)
+    game.bowl(2)
+    game.bowl(5)
+    game.bowl(4)
+    game.calculateScore(game.frameByFrameScore)
+    expect(game.total).toEqual(12)
+  });
+
+  it('calculates the score from the nested fram score arrays', function(){
+    game.bowl(1)
+    game.bowl(2)
+    game.bowl(5)
+    game.bowl(4)
+    game.calcScore(game.allFrames)
+    expect(game.totalScore).toEqual(12)
   });
 });
--- src/frame.js
+++
@@ -1,9 +0,0 @@
-// 'use strict';
-//
-// function Frame() {
-//   this.allFrames = []
-// };
-//
-// Frame.prototype.save = function(frame) {
-//   this.allFrames.push(frame)
-// };

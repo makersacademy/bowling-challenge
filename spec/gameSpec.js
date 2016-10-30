160e0100f100aebf4faaec39ee64fcf2e8364656
Laura Weston <laura.weston@outlook.com>
2016-10-30 10:50:49 +0000

    Game keeps score for the first bowl

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,10 +4,17 @@ describe('Game', function() {
     var game;
     beforeEach(function() {
         game = new Game();
+        game.start();
     });
     
     it('contains 10 frames', function() {
-        game.start();
         expect(game.frames.length).toEqual(10);
+    });
+    it('has a score of zero at start', function() {
+        expect(game.score()).toEqual(0);
+    });
+    it('takes score for first bowl', function() {
+        game.bowl(5);
+        expect(game.score()).toEqual(5);
     });
 });

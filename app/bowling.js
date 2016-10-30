239cb0db7607baf257407499e3d0af34e9996bc5
Tomy8s <tomy8s@hotmail.com>
2016-10-30 11:41:17 +0000

    ends game after10th frame

--- app/bowling.js
+++ app/bowling.js
@@ -19,33 +19,34 @@ $("document").ready(function() {
     var cellOne = $("#frame" + frame + " #ball1");
     var cellTwo = $("#frame" + frame + " #ball2");
     var cellFrameTot = $("#frame" + frame + " #ball3");
-    console.log(ball);
-    if(ball === 1) {
-      firstGo = slider;
-      cellOne.text(firstGo);
-      if(firstGo === 10) {
-        second = 0;
-        cellFrameTot.text(firstGo);
-        game['frame' + frame] = [{'frameTotal': firstGo}, firstGo, ];
-        game.total += firstGo;
+    if(frame < 11) {
+      if(ball === 1) {
+        firstGo = slider;
+        cellOne.text(firstGo);
+        if(firstGo === 10) {
+          second = 0;
+          cellFrameTot.text(firstGo);
+          game['frame' + frame] = [{'frameTotal': firstGo}, firstGo, ];
+          game.total += firstGo;
+          console.log(game);
+          frame++;
+          randomAngle();
+        } else {
+          randomAngle();
+          ball++;
+        }
+      } else if(ball === 2 ) {
+        secondGo = slider - firstGo < 1 ? 0 : slider - firstGo;
+        frameTotal = firstGo + secondGo;
+        cellTwo.text(secondGo);
+        cellFrameTot.text(frameTotal);
+        game['frame' + frame] = [{'frameTotal':frameTotal}, firstGo, secondGo];
+        game.total += frameTotal;
         console.log(game);
+        ball = 1;
         frame++;
         randomAngle();
-      } else {
-        randomAngle();
-        ball++;
       }
-    } else if(ball === 2 ) {
-      secondGo = slider - firstGo < 1 ? 0 : slider - firstGo;
-      frameTotal = firstGo + secondGo;
-      cellTwo.text(secondGo);
-      cellFrameTot.text(frameTotal);
-      game['frame' + frame] = [{'frameTotal':frameTotal}, firstGo, secondGo];
-      game.total += frameTotal;
-      console.log(game);
-      ball = 1;
-      frame++;
-      randomAngle();
     }
   });
 });

6e4d14f526f7bb17353b7ae1e4a7390c9cc51fe7
Malinna Leach <malinnab@yahoo.com>
2016-10-30 12:14:44 +0000

    some animation working

--- src/Interface.js
+++ src/Interface.js
@@ -2,11 +2,20 @@
 $( document ).ready(function () {
   var game = new Game();
 
+  $(".title").hide();
+
   function update() {
     $(".frame").text(game.currentFrame);
     $(".roll").text(game.currentRoll);
     $(".score").text(game.scoreTotal);
-    $(".hits").text(game.lastRollScore)
+    $(".hits").text(game.lastRollScore);
+    if (game.pins < 10) {
+      for (var i = 1; i <= (10 - game.pins); i++) {
+        $('#'+i.toString()).hide("slow");
+      }
+    } else {
+      $(":hidden").show("slow");
+    }
   };
 
   update();

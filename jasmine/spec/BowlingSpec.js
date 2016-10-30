f3552c503358018f59995af7622480881e492ae7
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-30 10:10:40 +0000

    Added tests, but need to work on calculation section as appears to be failing tests

--- jasmine/spec/BowlingSpec.js
+++ jasmine/spec/BowlingSpec.js
@@ -1,20 +1,41 @@
 describe("Bowling game", function() {
 
-var game;
+  var game;
 
   beforeEach(function() {
-  game = new BowlingGame();
-});
-
-  it('should be able to start a new game', function() {
-    var game = new BowlingGame();
+    game = new BowlingGame();
   });
 
-  it('should be able to score a frame where no pins are hit', function () {
-    for (var i = 0; i < 2; i++) {
-      game.roll(0)
-    }
+  it('should be able to score a game where no pins are hit', function () {
+    rollMany(0, 20)
     expect(game.score()).toEqual(0);
   });
+
+  it('should be able to score when player only hits single pins', function () {
+    rollMany(1, 20)
+    expect(game.score()).toEqual(20);
+  });
+
+  it('should be able to score a spare', function () {
+    game.roll(6)
+    game.roll(4)
+    game.roll(5)
+    rollMany(0, 17)
+    expect(game.score()).toEqual(20);
+  });
+
+  it('should be able to score a strike', function () {
+    game.roll(10);
+    game.roll(4);
+    game.roll(4);
+    rollMany(0, 16);
+    expect(game.score()).toEqual(25)
+  });
+
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  };
 
 });
--- jasmine/src/bowling.js
+++ jasmine/src/bowling.js
@@ -1,11 +1,28 @@
-function BowlingGame() {
-this.rolls = [];
+var BowlingGame = function() {
+  this.rolls = [];
 }
 
 BowlingGame.prototype.roll = function (pins) {
   this.rolls.push(pins);
-};
+}
 
 BowlingGame.prototype.score = function () {
-  return 0;
+  var currentScore = 0;
+  var rollIndex = 0;
+
+  for (var i = 0; i < 10; i++) {
+    if (this.rolls[rollIndex] === 10) {
+      currentScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+      rollIndex++;
+    }
+    else if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10) {
+      currentScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+      rollIndex += 2;
+    }
+    else {
+      currentScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+      rollIndex += 2;
+    }
+    return currentScore;
+  }
 };

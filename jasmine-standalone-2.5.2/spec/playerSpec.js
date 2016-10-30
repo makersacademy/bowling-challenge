8f7e08662bbfcc7fb802d3005b5dac868688a5f5
Katy McCann <katy.alice.mccann@gmail.com>
2016-10-30 10:40:17 +0000

    Player is instanstiated with a property 'frame' that has a value of 1

---
+++ jasmine-standalone-2.5.2/spec/playerSpec.js
@@ -0,0 +1,8 @@
+describe('Player', function () {
+   describe('Player', function () {
+      it('starts at frame 1', function () {
+        var player = new Player();
+        expect(player.frame).toBe(1);
+      });
+    });
+  });
---
+++ jasmine-standalone-2.5.2/src/player.js
@@ -0,0 +1,4 @@
+function Player() {
+    'use strict';
+    this.frame = 1;
+};

8dac3e5cefc2b28abdd9a0374e4513f01656c94f
Dionysis <Dionysis.k22@gmail.com>
2016-10-30 17:20:28 +0000

    frame score for a single frame jasmine success

--- jasmine/src/Frame.js
+++ jasmine/src/Frame.js
@@ -1,3 +1,27 @@
-var Frame = function([roll1, roll2]) {
-  this.rolls = [roll1, roll2]
+var Frame = function(rolls) {
+  this.rolls = rolls;
+};
+
+Frame.prototype.totalPoints = function(frame, nextFrame) {
+  return this._rollScore() + this._bonusScore(frame, nextFrame);
+};
+
+Frame.prototype._rollScore = function() {
+  return this.rolls.reduce(function(score, roll) {
+    return (score + roll);
+  });
+};
+
+
+Frame.prototype._bonusScore = function(frame, nextFrame) {
+  if (undefined === frame) {
+    return 0;
+  }
+  if (this._isStrike()) {
+    return frame._strikeBonus(nextFrame);
+  }
+  if (this._isSpare()) {
+    return frame._spareBonus();
+  }
+  return 0;
 };

1c330a59deccc91362afeedab2bea5390701423e
Tadas Majeris <tadas.majeris@gmail.com>
2016-10-31 08:03:32 +0000

    Basic game logic

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,12 +1,41 @@
 describe("Game", function(){
   var game;
+  var frame;
 
   beforeEach(function(){
     game = new Game();
+    frame = jasmine.createSpyObj('frame', ['isComplete', 'addRoll']);
   });
 
   it('has default score of 0', function(){
     expect(game.score()).toEqual(0);
+  });
+
+  it('has a maximum of ten frames', function(){
+    for(var i=0; i<(game.MAX_FRAMES+5); i++){
+      game.addFrame(frame);
+    }
+    expect(game.frames().length).toEqual(game.MAX_FRAMES);
+  });
+
+  describe('currentFrame', function(){
+    it('is null at initialization', function(){
+      expect(game.currentFrame()).toBe(null);
+    });
+
+    it('knows the current frame', function(){
+      game.addFrame(frame);
+      expect(game.currentFrame()).toBe(frame);
+      expect(game.frames()).toContain(frame);
+    });
+  });
+
+  describe('roll', function(){
+    it('creates a new frame and adds the roll', function(){
+      game.roll(5);
+      expect(game.frames().length).toEqual(1);
+      expect(game.score()).toEqual(5);
+    });
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,36 @@
 function Game() {
-  this._score = 0;
+  this._frames = [];
+  this._currentFrame = null;
+  this.MAX_FRAMES = 10;
 };
 
 Game.prototype.score = function() {
+  this._score = 0;
+  for(var i=0; i<this.frames().length; i++) {
+    this._score += this.frames()[i].total();
+  }
   return this._score;
 };
+
+Game.prototype.addFrame = function(frame) {
+  if(this.frames().length < this.MAX_FRAMES) {
+    this._frames.push(frame);
+    this._currentFrame = frame;
+  }
+};
+
+Game.prototype.currentFrame = function() {
+  return this._currentFrame;
+};
+
+Game.prototype.frames = function() {
+  return this._frames.slice(0);
+};
+
+Game.prototype.roll = function(n) {
+  if(!this.currentFrame() || this.currentFrame().isComplete()) {
+    frame = new Frame();
+    this.addFrame(frame);
+  }
+  this.currentFrame().addRoll(n);
+}

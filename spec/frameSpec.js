524f081f044eb6757be282f117bb266ba7d36e2c
ManuCiao <mn.sabatino@gmail.com>
2016-10-31 12:43:25 +0100

    Rolling twice-game over

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -10,4 +10,25 @@ describe("Frame", function() {
     expect(frame.defaultPins()).toEqual(10);
   });
 
+  it("returns one successful roll", function() {
+    frame.oneGame(5);
+    expect(frame.defaultPins()).toEqual(5);
+  });
+
+  it("returns twice rolls", function(){
+    frame.oneGame(3);
+    frame.oneGame(5);
+    expect(frame.defaultPins()).toEqual(2);
+  });
+
+  it("returns game over", function(){
+    frame.oneGame(3);
+    frame.oneGame(7);
+    expect(frame.isGameOver()).toEqual(true);
+  });
+
+  it("returns the game is not over after rolling twice", function(){
+    expect(frame.isGameOver()).toEqual(false);
+  });
+
 });
--- src/frame.js
+++ src/frame.js
@@ -1,11 +1,17 @@
 function Frame(){
+ this.roll = 2;
  this.pinCount = 10;
 }
 
 Frame.prototype.oneGame = function (pinCount) {
+  this.roll --;
   this.pinCount -= pinCount;
 };
 
 Frame.prototype.defaultPins = function () {
-  return 10;
+  return this.pinCount;
+};
+
+Frame.prototype.isGameOver = function () {
+  return (this.roll === 0);
 };

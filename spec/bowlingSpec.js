866cad25d488871ea5d3c619f28edb2ea8133f24
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-31 09:17:48 +0000

    fixed weird bug where test passed if it was only test running

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -26,30 +26,42 @@ describe('bowling: ', function() {
       expect(score1._mostRecentRoll).toEqual(roll2);
     });
 
-    it('stores the frame when the frame is finished (not strike)', function() {
-      // console.log('roll 1: ' + roll1);
-      // console.log('roll 2: ' + roll2);
-      // console.log('all frames: ' + score1._allFrames)
-      // console.log('current frame: ' + score1._currentFrame)
-      expect(score1._allFrames[0][0]).toEqual(roll1);
-      expect(score1._allFrames[0][1]).toEqual(roll2);
+    it('stores the frame when the frame is finished', function() {
+      console.log('roll 1: ' + roll1);
+      console.log('roll 2: ' + roll2);
+      console.log('all frames: ' + score1._allFrames)
+      console.log('current frame: ' + score1._currentFrame)
+      if (roll1 < 10 ) { //|| roll1 == 0
+
+        expect(score1._allFrames[0][0]).toEqual(roll1);
+
+        expect(score1._allFrames[0][1]).toEqual(roll2);
+      } else if (roll1 == 10) {
+        expect(score1._allFrames[0][0]).toEqual(roll1);
+        expect(score1._currentFrame[0]).toEqual(roll2);
+      };
+
     });
 
-    it('moves the current frame to the frame history when the frame is finished (non-strike)', function() {
-      roll3 = player1.roll(score1);
-      roll4 = player1.roll(score1);
-      expect(score1._allFrames[0][0]).toEqual(roll1);
-      expect(score1._allFrames[0][1]).toEqual(roll2);
-    });
 
-// YOU WERE ADDING STRIKE FUNCTIONALITY TO CODE.
-// YOU NEED TO SUBTRACT ROLL1 PINS FOR ROLL2!
-
-
-    // it('stores strikes in a frame by themselves', function() {
-    //
+    // this test isn't working because it doesn't accurately assess how strikes work.
+    // it('moves the current frame to the frame history when the frame is finished (non-strike)', function() {
+    //   roll3 = player1.roll(score1);
+    //   roll4 = player1.roll(score1);
+    //   if (roll1 != 10) {
+    //     expect(score1._allFrames[0][0]).toEqual(roll1);
+    //     expect(score1._allFrames[0][1]).toEqual(roll2);
+    //   } else if (roll1 == 10) {
+    //     expect(score1._allFrames[0][0]).toEqual(roll1);
+    //   };
     // });
 
+    // it('keeps a track of which frame is currently being played', function() {
+    //   roll3 = player1.roll(score1);
+    //   roll4 = player1.roll(score1);
+    //   expect(score1.frameCount()).toEqual(3);
+    // });
+    //
 
 
   });
@@ -61,7 +73,10 @@ describe('bowling: ', function() {
 //   expect(XXX).toEqual(10);
 // });
 
-//
+// console.log('roll 1: ' + roll1);
+// console.log('roll 2: ' + roll2);
+// console.log('all frames: ' + score1._allFrames)
+// console.log('current frame: ' + score1._currentFrame)
 
 
 // console.log('roll count: ' + rollCount);

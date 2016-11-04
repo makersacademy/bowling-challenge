4b78f947e56d1b2b3272535090684ca55d2976c0
glynester <glynester@hotmail.com>
2016-11-04 09:28:56 +0000

    Wrote 2nd test to check that game total updtes but not yet working.

--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -3,4 +3,17 @@ feature 'Playing 10 pin bowling' do
     visit '/'
     expect(page).to have_content('Ten Pin Bowling Score Sheet')
   end
+
+  scenario 'Playing a game' do
+      visit '/'
+      click_button("X")
+      # expect('edit-game-result').to eq(10)
+      # expect(find_field('edit-game-result').value).to eq '10'
+      # page.find_by_id('red')
+      # expect(page).to have_field('edit-game-result', with: '10')
+      page.find_by_id('edit-game-result')
+
+      # Finds the element but still need to check value.
+  end
+
 end

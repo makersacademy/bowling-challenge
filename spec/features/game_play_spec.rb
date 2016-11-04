bf690b3d5c46e47a72531e37a374019038d33463
glynester <glynester@hotmail.com>
2016-11-04 09:13:12 +0000

    Set up capybara and first test.

--- spec/features/game_play_spec.rb
+++ spec/features/game_play_spec.rb
@@ -1,4 +1,6 @@
-describe "Game play" do
-  
-
+feature 'Playing 10 pin bowling' do
+  scenario "Viewing the game page" do
+    visit '/'
+    expect(page).to have_content('Ten Pin Bowling Score Sheet')
+  end
 end

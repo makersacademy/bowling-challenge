bd3a175e30d1be2aaa36297dd1ede12d3bd9255e
andy jenkins <andygjenkins@gmail.com>
2016-11-04 21:47:33 +0000

    Add calculate rolls to game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,10 +12,27 @@ describe('Game:', function(){
       expect(game.frames.length).toEqual(0);
     });
   });
+
   describe('it calculates the score', function(){
     it('initializes at 0', function(){
       expect(game.score).toEqual(0);
     });
   });
-  
+
+  describe('Bowl', function(){
+    it('identifies the first roll', function(){
+      var fakeGame = new Game();
+      spyOn(fakeGame, "calcFirstRoll");
+      fakeGame.bowl(5);
+      expect(fakeGame.calcFirstRoll).toHaveBeenCalled();
+    });
+    
+    it('identifies the second roll', function(){
+      var fakeGame = new Game();
+      spyOn(fakeGame, "calcSecondRoll");
+      fakeGame.bowl(5);
+      fakeGame.bowl(5);
+      expect(fakeGame.calcSecondRoll).toHaveBeenCalled();
+    });
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,24 @@
 'use strict';
 
 function Game(){
-  this.frames = []
-  this.score = 0
+  this.frames = [];
+  this.score = 0;
+  this.isFirstRoll = true;
+};
+
+
+Game.prototype.bowl = function (pins) {
+  if (this.isFirstRoll === true) {
+    this.calcFirstRoll(pins);
+  }
+  this.calcSecondRoll(pins);
+};
+
+Game.prototype.calcFirstRoll = function (pins) {
+  return pins
+  this.isFirstRoll = false;
+};
+
+Game.prototype.calcSecondRoll = function (pins) {
+  return pins
 };

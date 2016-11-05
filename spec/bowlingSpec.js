907d7d397a97c133c6a30d6387135ba4f5969464
Rory Aitchison <rory.aitchison@gmail.com>
2016-11-05 10:51:07 +0000

    deleted all tests after refactoring random rolls and player class out of the code. About to start rewriting the tests.

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,96 +1,131 @@
 describe('bowling: ', function() {
 
-  var player1;
   var score1;
 
-  describe('spy section', function() {
-
-
     beforeEach(function() {
-      player1 = new Player();
       score1 = new Score();
-      roll1 = player1.roll(score1);
-      // roll2 = player1.roll(score1);
     });
 
-    it('stores the current frame in allFrames when a strike is rolled', function() {
-      spyOn(score1, 'knockedPins').and.returnValue(10);
-      expect(score1._allFrames[0]).toEqual([10]);
-    });
-  });
+    it('', function() {
 
-  describe('frame management: ', function() {
-
-    beforeEach(function() {
-      player1 = new Player();
-      score1 = new Score();
-      roll1 = player1.roll(score1);
-      roll2 = player1.roll(score1);
     });
 
-    it('stores the most recent roll', function() {
-      expect(score1._mostRecentRoll).toEqual(roll2);
-    });
-
-    it('moves the "current roll" to "previous roll" when the ball is rolled again', function() {
-      expect(score1._previousRoll).toEqual(roll1);
-      expect(score1._mostRecentRoll).toEqual(roll2);
-    });
-
-
-    // it('stores the frame when the frame is finished', function() {
-    //   // spyOn(player, 'roll').andReturn()
-    //
-    //
-    //     if (roll1 + roll2 < 10 && roll1 < 10) { //|| roll1 == 0
-    //
-    //       expect(score1._allFrames[0][0]).toEqual(roll1);
-    //
-    //       expect(score1._allFrames[0][1]).toEqual(roll2);
-    //     } else if (roll1 == 10) {
-    //       expect(score1._allFrames[0][0]).toEqual(roll1);
-    //       expect(score1._currentFrame[0]).toEqual(roll2);
-    //     };
-    //   });
+});
 
 
 
-      // console.log('roll 1: ' + roll1);
-      // console.log('roll 2: ' + roll2);
-      // console.log('all frames: ' + score1._allFrames)
-      // console.log('current frame: ' + score1._currentFrame)
-
-    // it('moves the current frame to the frame history when the frame is finished (non-strike)', function() {
-    //   roll3 = player1.roll(score1);
-    //   roll4 = player1.roll(score1);
-    //     if (roll1 < 10 ) { //|| roll1 == 0
-    //
-    //       expect(score1._allFrames[0][0]).toEqual(roll1);
-    //
-    //       expect(score1._allFrames[0][1]).toEqual(roll2);
-    //     } else if (roll1 == 10) {
-    //       expect(score1._allFrames[0][0]).toEqual(roll1);
-    //       expect(score1._currentFrame[0]).toEqual(roll2);
-    //     };
-    //   });
-    //
-    // it('keeps a track of which frame is currently being played', function() {
-    //   roll3 = player1.roll(score1);
-    //   roll4 = player1.roll(score1);
-    //   expect(score1.frameCount()).toEqual(3);
-    // });
-
-  });
 
 
 
-    //
-    // it('', function() {
-    //
-    // });
 
 
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // describe('frame management: ', function() {
+  //
+  //   beforeEach(function() {
+  //     player1 = new Player();
+  //     score1 = new Score();
+  //     roll1 = player1.roll(score1);
+  //     roll2 = player1.roll(score1);
+  //   });
+  //
+  //   it('stores the most recent roll', function() {
+  //     expect(score1._mostRecentRoll).toEqual(roll2);
+  //   });
+  //
+  //   it('moves the "current roll" to "previous roll" when the ball is rolled again', function() {
+  //     expect(score1._previousRoll).toEqual(roll1);
+  //     expect(score1._mostRecentRoll).toEqual(roll2);
+  //   });
+  //
+  //
+  //   // it('stores the frame when the frame is finished', function() {
+  //   //   // spyOn(player, 'roll').andReturn()
+  //   //
+  //   //
+  //   //     if (roll1 + roll2 < 10 && roll1 < 10) { //|| roll1 == 0
+  //   //
+  //   //       expect(score1._allFrames[0][0]).toEqual(roll1);
+  //   //
+  //   //       expect(score1._allFrames[0][1]).toEqual(roll2);
+  //   //     } else if (roll1 == 10) {
+  //   //       expect(score1._allFrames[0][0]).toEqual(roll1);
+  //   //       expect(score1._currentFrame[0]).toEqual(roll2);
+  //   //     };
+  //   //   });
+  //
+  //
+  //
+  //     // console.log('roll 1: ' + roll1);
+  //     // console.log('roll 2: ' + roll2);
+  //     // console.log('all frames: ' + score1._allFrames)
+  //     // console.log('current frame: ' + score1._currentFrame)
+  //
+  //   // it('moves the current frame to the frame history when the frame is finished (non-strike)', function() {
+  //   //   roll3 = player1.roll(score1);
+  //   //   roll4 = player1.roll(score1);
+  //   //     if (roll1 < 10 ) { //|| roll1 == 0
+  //   //
+  //   //       expect(score1._allFrames[0][0]).toEqual(roll1);
+  //   //
+  //   //       expect(score1._allFrames[0][1]).toEqual(roll2);
+  //   //     } else if (roll1 == 10) {
+  //   //       expect(score1._allFrames[0][0]).toEqual(roll1);
+  //   //       expect(score1._currentFrame[0]).toEqual(roll2);
+  //   //     };
+  //   //   });
+  //   //
+  //   // it('keeps a track of which frame is currently being played', function() {
+  //   //   roll3 = player1.roll(score1);
+  //   //   roll4 = player1.roll(score1);
+  //   //   expect(score1.frameCount()).toEqual(3);
+  //   // });
+  //
+  // });
+  //
+  //
+  //
+  //   //
+  //   // it('', function() {
+  //   //
+  //   // });
+  //
+
+
 
 
 

6a30b4dc7e733b0ead4ad90ebcd1ebe7148a3809
Ed Powderham <edpowderham@outlook.com>
2016-11-05 22:15:11 +0000

    Added basic front-end styling

--- public/js/Interface.js
+++ public/js/Interface.js
@@ -1,27 +1,34 @@
 $( document ).ready(function () {
   var bowling = new Bowling();
 
-  var updateScore = function() {
-    $("#total").text(bowling.calculateTotalScore())
-    for(i=1; i <= bowling._turnLog.length; i++){
-      console.log(bowling._turnLog[0])
-      if(bowling._turnLog[0].isTurnOver() === true){
-        $("#tb-total-"+i).text(bowling._turnLog[i-1].totalScore())
-      }
-    }
-  }
-
   var updateTurn = function() {
     for(i=1; i <= bowling._turnLog.length; i++){
       $("#tb-turn-"+i).text(bowling._turnLog[i-1].displayValue())
     }
+    $("#tb-total-"+bowling._turnLog.length).text(bowling.calculateTotalScore())
+  }
+
+  function resetPins () {
+    $("#bowl-pins").val('')
   }
 
   $("#bowl-input").click(function (){
     var pins = parseInt($("#bowl-pins").val());
+    if (isNaN(pins) || pins > 10) {
+      alert('Only numbers 0-10 can be entered')
+      return
+    }
     bowling.recordThrow(pins);
     console.log(pins);
-    updateScore();
     updateTurn();
+    resetPins()
+  });
+
+  $("#bowl-random").click(function (){
+    var pins = Math.floor((Math.random()*10) +1)
+    $("#bowl-pins").val(pins)
+    bowling.recordThrow(pins);
+    updateTurn();
+    resetPins()
   });
 });

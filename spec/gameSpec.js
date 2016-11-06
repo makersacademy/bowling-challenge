c76d03b044293b8034015899db94b03e78ed64e3
Laura Weston <laura.weston@outlook.com>
2016-11-06 23:14:54 +0000

    WIP

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -42,61 +42,72 @@ describe('Game', function () {
                 expectedScore: 60
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 expectedScore: 0
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5],
                 expectedScore: 10
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10],
                 expectedScore: 10
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,10],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10],
                 expectedScore: 30
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,0],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0],
                 expectedScore: 10
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,1],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 1],
                 expectedScore: 11
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,5,10],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 10],
                 expectedScore: 20
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,0,0],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0],
                 expectedScore: 10
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,1,1],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 1, 1],
                 expectedScore: 12
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,4,6],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 4, 6],
                 expectedScore: 20
             },
             {
-                bowls: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,10,10],
+                bowls: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10],
                 expectedScore: 30
             },
             {
-                bowls: [10,10,10,10,10,10,10,10,10,10,10,10],
+                bowls: [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
                 expectedScore: 300
             }
         ];
         testCases.forEach(function (testCase) {
-            it('keeps the correct score for bowls', function () {
+            it('keeps the correct score for bowls ' + testCase.bowls.toString(), function () {
                 testCase.bowls.forEach(function (score) {
                     game.bowl(score);
                 });
                 expect(game.score()).toEqual(testCase.expectedScore);
             });
+        });
+    });
+
+    describe('game over', function () {
+        it('cannot bowl after game has ended', function () {
+            var bowlAfterGameOver = function () {
+                for (var i = 0; i < 22; i++) {
+                    game.bowl(0);
+                };
+            }
+            expect(bowlAfterGameOver).toThrowError("Game is over");
         });
     });
 });

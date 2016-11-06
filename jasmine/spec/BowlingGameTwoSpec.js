a6b0c2ac7e80f8d6d88d0dd416395bdb5c042371
Dionysis <Dionysis.k22@gmail.com>
2016-11-06 14:21:27 +0000

    testing for a gutter game, requiring roll and score functions

--- jasmine/spec/BowlingGameTwoSpec.js
+++ jasmine/spec/BowlingGameTwoSpec.js
@@ -1,8 +1,19 @@
 describe('Bowling game Two', function() {
 
-  it('can start a game', function() {
-    var game = new BowlingGameTwo()
+  var gameTwo;
+
+  beforeEach(function() {
+    var gameTwo = new BowlingGameTwo()
   });
 
+  it('can start a game', function() {
+  });
+
+  it('rolling a gutter game', function() {
+    for (var i=0; i<20; i++) {
+      gameTwo.roll(0)
+    }
+    expect(gameTwo.score()).toEq(0)
+  });
 
 });

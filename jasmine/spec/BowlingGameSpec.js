f02f94eed23a3357c5e318495809714eb2c357c5
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-06 17:07:34 +0000

    Spare game

--- jasmine/spec/BowlingGameSpec.js
+++ jasmine/spec/BowlingGameSpec.js
@@ -1,15 +1,31 @@
-describe('The Bowling Game', function () {
+describe('BowlingGame', function () {
+  var bowlingGame;
 
-  it('can create a game', function () {
-    var game = new BowlingGame();
+  beforeEach(function() {
+    bowlingGame = new BowlingGame()
   });
 
-  it('can roll', function () {
-    var game = new BowlingGame();
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
+  it('can roll gutter game', function () {
+    rollMany(0, 20);
+    expect(bowlingGame.score()).toBe(0);
+  });
+
+  it('can roll all ones', function () {
+    rollMany(1, 20);
+    expect(bowlingGame.score()).toBe(20);
+  });
+
+  it('can roll a spare', function () {
+    bowlingGame.roll(6);
+    bowlingGame.roll(4);
+    bowlingGame.roll(4);
+    rollMany(0, 17);
+    expect(bowlingGame.score()).toBe(18)
+  });
+
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      bowlingGame.roll(pins);
     }
-    expect(game.score()).toBe(0);
-  });
-
-})
+  };
+});
--- jasmine/src/BowlingGame.js
+++ jasmine/src/BowlingGame.js
@@ -1,11 +1,22 @@
-var BowlingGame = function () {
-
+function BowlingGame() {
+this.rolls = [];
 };
 
 BowlingGame.prototype.roll = function (pins) {
-
+  this.rolls.push(pins);
 };
 
 BowlingGame.prototype.score = function () {
-  return 0;
-}
+  var result = 0;
+  var rollIndex = 0;
+
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
+    if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10) {
+      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    } else {
+      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+    rollIndex += 2;
+  }
+  return result;
+};

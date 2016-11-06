80a357823397a0f759ff73d6fe673005b6b443b1
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-11-06 11:19:30 +0000

    Added basic bowl functionality

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,4 +10,13 @@ describe('Game', function() {
     expect(game.getTotalScore()).toEqual(0);
   });
 
+  it('starts on frame zero', function() {
+    expect(game.frame).toEqual(0);
+  });
+
+  it('adds a single bowl to the frame score', function() {
+    game.bowl(6);
+    expect(game.frameScore).toContain(6);
+  });
+
 });
--- src/game.js
+++ src/game.js
@@ -1,7 +1,13 @@
 function Game() {
   this.totalScore = 0
+  this.frame = 0
+  this.frameScore = []
 }
 
 Game.prototype.getTotalScore = function() {
   return this.totalScore;
+};
+
+Game.prototype.bowl = function(pins) {
+  this.frameScore += pins
 };

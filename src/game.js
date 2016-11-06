5e7ef3bbd46dd098c0fa2115f3d18d754e6c1553
Stevie Winston-Gore <slwinstongore@gmail.com>
2016-11-06 19:54:50 +0000

    renaming and removed method

--- src/game.js
+++ src/game.js
@@ -12,9 +12,7 @@ Game.prototype.recordRoll = function(pins) {
     throw new Error('Game over, please start a new game')
   }
   this.currentFrame.bowl(pins);
-  if (this.currentFrame.isEndFrame()) {
-    this.nextFrame();
-  }
+  if (this.currentFrame.isEndFrame()) { this.nextFrame(); }
 };
 
 Game.prototype.nextFrame = function() {
@@ -30,8 +28,4 @@ Game.prototype.addScore = function() {
 
 Game.prototype.isOver = function() {
   return this.allFrames.length === 10
-};
-
-Game.prototype.gameOver = function() {
-  console.log('Game Over');
 };

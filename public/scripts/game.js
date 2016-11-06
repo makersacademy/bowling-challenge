0e9bebe42d47e64879e344b66a35972d805d03f1
Rosie <r.gollancz@gmail.com>
2016-11-06 17:15:01 +0000

    Adds passing tests

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,28 +1,101 @@
-function BowlingGame() {
-  this.TOTAL_TURNS = 10;
-  this._turnsPlayed = [];
-  this._currentTurn
-  this._overallScore = 0;
+function Game() {
+  this.TOTAL_FRAMES = 10
+  this.MAX_FRAME_SCORE = 10
+  this.frameScores = []
+  this.tenthFrameScores = []
+  this.framesPlayed = 0
+}
+
+Game.prototype.newTurn = function(pinsOne,pinsTwo,pinsThree=0) {
+  if (this.framesPlayed >= this.TOTAL_FRAMES) throw "Game over!";
+    if (this.framesPlayed === 9) {
+      this.tenthFrameScores.push(pinsOne,pinsTwo,pinsThree);
+    } else {
+      this.frameScores.push([pinsOne,pinsTwo]);
+    }
+    this.framesPlayed ++;
 };
 
-BowlingGame.prototype.newRoll = function(pins) {
-  if (this._currentTurn.status === "complete") {
-    saveTurn();
-      newTurn(pins);
+Game.prototype.isStrike = function(n) {
+  return this.frameRollOne(n) === this.maxFrameScore();
+};
+
+Game.prototype.maxFrameScore = function() {
+  return this.MAX_FRAME_SCORE;
+}
+
+Game.prototype.isSpare = function(n) {
+  var rollOne = this.frameRollOne(n);
+  var rollTwo = this.frameRollTwo(n);
+  return (rollOne + rollTwo) === this.maxFrameScore();
+};
+
+Game.prototype.strikeScore = function(n) {
+  if (this.nextFrameRollOne(n) + this.nextFrameRollTwo(n) === this.maxFrameScore()) {
+    return 20 + this.thirdFrameRollOne(n);
   } else {
-    this._currentTurn.play(pins);
+    return 10 + (this.nextFrameRollOne(n) + this.nextFrameRollTwo(n));
   };
 };
 
-BowlingGame.prototype.newTurn(pins) = function () {
-  this._currentTurn = new Turn(pins)
+Game.prototype.spareScore = function(n) {
+  return 10 + this.nextFrameRollOne(n);
 };
 
-BowlingGame.prototype.saveTurn = function () {
-  this._TurnsPlayed.push(this._currentTurn);
-  updateScore();
+Game.prototype.frameTotal = function(n) {
+  return this.frameRollOne(n) + this.frameRollTwo(n);
 };
 
-BowlingGame.prototype.updateScore = function () {
- // no idea
+Game.prototype.frameRollOne = function(n) {
+  var frame = this.frameScores[n];
+  return frame[0];
+};
+
+Game.prototype.frameRollTwo = function(n) {
+  var frame = this.frameScores[n];
+  return frame[1];
+};
+
+Game.prototype.nextFrameRollOne = function(n) {
+  var nextFrame = this.frameScores[n+1];
+  return nextFrame[0];
+};
+
+Game.prototype.nextFrameRollTwo = function(n) {
+  var nextFrame = this.frameScores[n+1];
+  return nextFrame[1];
+};
+
+Game.prototype.thirdFrameRollOne = function(n) {
+  var frameThree = this.frameScores[n+2];
+  return frameThree[0];
+};
+
+Game.prototype.finalFrameScore = function () {
+  var lastFrameScore = this.tenthFrameScores.reduce(function(sum, score) {
+    return sum + score;
+  }, 0);
+  return lastFrameScore
+};
+
+Game.prototype.mostRecentFrameScore = function () {
+  var lastFrameScore = this.frameScores[this.framesPlayed-1].reduce(function(sum, score) {
+    return sum + score;
+  }, 0);
+  return lastFrameScore
+};
+
+Game.prototype.score = function(n) {
+  var score = 0
+  for (n = 0; n < this.framesPlayed-1; n++) {
+    if (this.isStrike(n)) {
+      score += this.strikeScore(n);
+    } else if (this.isSpare(n)) {
+      score += this.spareScore(n);
+    } else {
+      score += this.frameTotal(n);
+    };
+  };
+  if (this.tenthFrameScores === []) return score + this.finalFrameScore();
+  if (this.tenthFrameScores != []) return score + this.mostRecentFrameScore();
 };

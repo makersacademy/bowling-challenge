2c518d8cbd5d9fcd22ee6282f97833ba0571f6ac
Dionysis <Dionysis.k22@gmail.com>
2016-11-06 14:52:42 +0000

    roll and score functions

--- jasmine/spec/BowlingGameTwoSpec.js
+++ jasmine/spec/BowlingGameTwoSpec.js
@@ -10,4 +10,12 @@ describe('Bowling game Two', function() {
     expect(gameTwo.score()).toBe(0);
   });
 
+  it('roll 1 and 1 pin, for 10 frames', function() {
+    var gameTwo = new BowlingGameTwo()
+    for (var i=1; i<21; i++) {
+      gameTwo.roll(1);
+    }
+    expect(gameTwo.score()).toBe(20);
+  });
+
 });
--- jasmine/src/BowlingGameTwo.js
+++ jasmine/src/BowlingGameTwo.js
@@ -1,11 +1,15 @@
 function BowlingGameTwo() {
-
+  this.rolls = [];
 };
 
 BowlingGameTwo.prototype.roll = function (pins) {
-
+  this.rolls.push(pins);
 };
 
 BowlingGameTwo.prototype.score = function () {
-  return 0;
+  var result = 0;
+  for (var i=0; i<20; i++) {
+    result += this.rolls[i];
+  }
+  return result;
 };

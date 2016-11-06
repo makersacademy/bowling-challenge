62a5379b0e55472aabbc1486b33b4bd74456f171
Julio Gonzalez <julio.gteran@gmail.com>
2016-11-06 16:01:03 +0000

    Bowling Game

---
+++ jasmine/spec/BowlingGameSpec.js
@@ -0,0 +1,15 @@
+describe('The Bowling Game', function () {
+
+  it('can create a game', function () {
+    var game = new BowlingGame();
+  });
+
+  it('can roll', function () {
+    var game = new BowlingGame();
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+
+})
---
+++ jasmine/src/BowlingGame.js
@@ -0,0 +1,11 @@
+var BowlingGame = function () {
+
+};
+
+BowlingGame.prototype.roll = function (pins) {
+
+};
+
+BowlingGame.prototype.score = function () {
+  return 0;
+}

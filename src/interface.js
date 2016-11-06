84b043918f359252e30d81c5363d7c646043c78b
hanrattyjen <hanratty.jen@gmail.com>
2016-11-06 18:24:48 +0000

    Added newTurn function which resets count number on browser

--- src/interface.js
+++ src/interface.js
@@ -1,36 +1,53 @@
 $(document).ready(function() {
 
   var result = 0;
-  var pinsDown = Math.floor((Math.random() * 10) + 1);
   var game = new Game();
+  var gameTracker = [];
+  var count = 20;
 
   $('#points').text(result);
+  $('#strike').hide();
+  $('.score').hide();
 
-  $('#score-tally').text(result);
-
-  $('#game-start').click(function() {
-    firstTurn = game.roll(pinsDown);
-    $('#points').text(firstTurn);
-    $('#score-tally').text(game.score(firstTurn));
+  // take turn
+  $('#take-turn').click(function() {
+    newTurn();
+    $('#turn-tally').text(count);
   });
 
-  $('take-turn').click(function() {
+  function newTurn() {
+    $('#strike').hide();
+    if (count === 0) {
+      $('#take-turn').hide();
+      $('#turn-tally').hide();
+      $('.score').show();
+      finalResult = game.score();
+      $('#final-result').text(finalResult);
+    }
+    var pinsDown = Math.floor((Math.random() * 10) + 1);
     turn = game.roll(pinsDown);
-    game.score(turn);
-    $('#points').text(result);
-  });
+    gameTracker.push(turn);
+    count -= 1;
+    if (turn === 10) {
+      $('#strike').show();
+      count -= 1;
+    }
+    $('#points').text(turn);
+    $('#score-card').text(gameTracker);
+  }
 
+
+
+  function hideStartButton(button) {
+      //set input text value to be the current label text
+      var currentLabelText = button.find("Game start").text();
+      button.find("input[type=text]").val(currentLabelText);
+
+      // hide things
+      button.find("Game start").hide();
+
+      //show things
+      button.find("Take turn").show();
+  }
 
 });
-
-function hideStartButton(button) {
-    //set input text value to be the current label text
-    var currentLabelText = button.find("Game start").text();
-    button.find("input[type=text]").val(currentLabelText);
-
-    // hide things
-    button.find("Game start").hide();
-
-    //show things
-    button.find("Take turn").show();
-}

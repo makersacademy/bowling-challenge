854327205bb311ca73f929a3de0b4fa107437905
James <bakerje@mac.com>
2016-11-07 09:15:32 +0000

    Added Spare Score and test

--- src/bowling.js
+++ src/bowling.js
@@ -10,8 +10,29 @@ BowlingGame.prototype.roll = function (pins) {
 
 BowlingGame.prototype.score = function () {
   var result = 0;
-  for (var i = 0; i < 20; i++) {
-    result += this.rolls[i];
+  var rollIndex = 0;
+  var game = this;
+
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
+    if (isSpare()) {
+      result += getSpareScore();
+    } else {
+      result += getNormalScore();
+    }
+    rollIndex += 2;
   }
 return result;
+
+function isSpare () {
+  return game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10;
+}
+
+function getSpareScore () {
+ return game.rolls[rollIndex] + game.rolls[rollIndex +1] + game.rolls[rollIndex + 2];
+}
+
+function getNormalScore () {
+ return game.rolls[rollIndex] + game.rolls[rollIndex +1];
+}
+
 };

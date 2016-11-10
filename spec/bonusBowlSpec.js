de538324477e0add734c4c3c5899c14637bcb42d
Laura Weston <laura.weston@outlook.com>
2016-11-10 21:57:54 +0000

    Create BonusBowl constructor and prototype with tests

---
+++ spec/bonusBowlSpec.js
@@ -0,0 +1,51 @@
+var BonusBowl = require('../lib/BonusBowl.js');
+
+describe('BonusBowl', function () {
+    var bonusBowl;
+
+    beforeEach(function () {
+        bonusBowl = new BonusBowl();
+    });
+
+    describe('initialization', function () {
+        it('has a total score starting at zero', function () {
+            expect(bonusBowl.totalScore()).toEqual(0);
+        });
+    });
+
+    describe('keeping score', function () {
+        it('takes a given score for a bonus bowl', function () {
+            bonusBowl.bowl(5);
+            expect(bonusBowl.totalScore()).toEqual(5);
+        });
+        it('cannot take more than one bowl score', function () {
+            var bowlMoreThanOnce = function () {
+                bonusBowl.bowl(5);
+                bonusBowl.bowl(3);
+            };
+            expect(bowlMoreThanOnce).toThrowError("Bonus bowl already complete");
+        });
+    });
+
+    describe('invalid scores', function () {
+        var invalidScores = [11, -1, "10", 1.5, true, {}, [10], undefined]
+        invalidScores.forEach(function (score) {
+            it('only accepts integer scores between 0 and 10 inclusive', function () {
+                var invalidScore = function () {
+                    return bonusBowl.bowl(score);
+                };
+                expect(invalidScore).toThrowError("Score must be an integer 0-10");
+            });
+        });
+    });
+
+    describe('completion', function () {
+        it('reports bonus bowl is not complete before bowling', function () {
+            expect(bonusBowl.isComplete()).toBe(false);
+        });
+        it('reports bonus bowl is complete after bowling', function () {
+            bonusBowl.bowl(5);
+            expect(bonusBowl.isComplete()).toBe(true);
+        });
+    });
+});

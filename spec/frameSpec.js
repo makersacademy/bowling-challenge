7ca87d59ea3985dc3de97638ae914c7b097784fe
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-10 19:16:50 +0000

    Adds ability to indicate when a frame is over

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -6,23 +6,39 @@ describe("Frame", function(){
     frame = new Frame();
   })
 
-  describe("Points", function(){
-    it("can be a maximum of 10", function(){
+  describe("Creation", function(){
+    it("sets a maximum of 10 points", function(){
       expect(frame.MAX_POINTS).toEqual(10);
     })
-    it("can be calculated per roll", function(){
-      expect(frame.roll()).toBeGreaterThan(-1);
-      expect(frame.roll()).not.toBeGreaterThan(10);
+    it("sets a maximum value of 2 rolls", function(){
+      expect(frame.MAX_ROLLS).toEqual(2);
+    })
+    it("sets an initial roll count of 0", function(){
+      expect(frame.rollCount).toEqual(0);
+    })
+    it("sets an initial score of 0", function(){
+      expect(frame.score).toEqual(0);
     })
   })
 
-  describe("Rolls", function(){
-    it("sets a maximum value of 2", function(){
-      expect(frame.MAX_ROLLS).toEqual(2);
+  describe("Scores", function(){
+    it("can be randomized per roll", function(){
+      expect(frame.calculateRollScore()).toBeGreaterThan(-1);
+      expect(frame.calculateRollScore()).not.toBeGreaterThan(10);
     })
-    it("count starts off at 0 with each frame", function(){
-      expect(frame.rollCount).toEqual(0);
+    it("can be added per roll", function(){
+      spyOn(frame, "calculateRollScore").and.returnValue(4);
+      frame.roll();
+      expect(frame.score).toEqual(4);
     })
+    it("cannot exceed 10", function(){
+      spyOn(frame, "calculateRollScore").and.returnValue(10);
+      frame.roll();
+      expect(function(){frame.roll();}).toThrow("Frame is over");
+    })
+  })
+
+  describe("Roll", function(){
     it("count increases by 1 when a roll is taken", function(){
       frame.roll();
       expect(frame.rollCount).toEqual(1);
@@ -32,6 +48,30 @@ describe("Frame", function(){
       expect(function(){frame.roll();}).toThrow("Frame is over");
     })
   })
+
+  describe("End", function(){
+    it("can indicate a frame is over when 10 points has been reached", function(){
+      spyOn(frame, "calculateRollScore").and.returnValue(10);
+      frame.roll();
+      expect(frame.hasEnded()).toEqual(true);
+    })
+    it("can indicate a frame is over when 2 rolls have been taken", function(){
+      spyOn(frame, "calculateRollScore").and.returnValue(4);
+      for(var i = 0; i<2; i++){ frame.roll(); }
+      expect(frame.hasEnded()).toEqual(true);
+    })
+    it("can indicate a frame is not over when 1 roll has been taken", function(){
+      spyOn(frame, "calculateRollScore").and.returnValue(6);
+      frame.roll();
+      expect(frame.hasEnded()).toEqual(false);
+    })
+    it("can indicate a frame is not over when 6 points has been reached in 1 roll", function(){
+      spyOn(frame, "calculateRollScore").and.returnValue(6);
+      frame.roll();
+      expect(frame.hasEnded()).toEqual(false);
+    })
+  })
+
 
 
 
--- src/frame.js
+++ src/frame.js
@@ -2,13 +2,30 @@ var Frame = function(){
   this.MAX_POINTS = 10;
   this.MAX_ROLLS = 2;
   this.rollCount = 0;
+  this.score = 0;
 };
 
 Frame.prototype.roll = function(){
-  if (this.rollCount === this.MAX_ROLLS) {
+  if (this.hasEnded()) {
     throw "Frame is over"
   } else {
-    this.rollCount += 1;
-    return Math.floor(Math.random() * 10);
+    this.updateRollCount();
+    this.updateScore();
   }
+}
+
+Frame.prototype.updateRollCount = function(){
+  this.rollCount += 1;
+}
+
+Frame.prototype.updateScore = function(){
+  this.score += this.calculateRollScore();
+}
+
+Frame.prototype.calculateRollScore = function(){
+  return Math.floor(Math.random() * 10);
+}
+
+Frame.prototype.hasEnded = function(){
+  return (this.rollCount === this.MAX_ROLLS) || (this.score === this.MAX_POINTS)
 }

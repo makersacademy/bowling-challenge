7b0698dba38d08fbc3a2cd19c19ea630c3d8fd40
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-10 18:48:47 +0000

    Add ability to count the number of rolls

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -10,11 +10,26 @@ describe("Frame", function(){
     it("can be a maximum of 10", function(){
       expect(frame.MAX_POINTS).toEqual(10);
     })
+    it("can be calculated per roll", function(){
+      expect(frame.roll()).toBeGreaterThan(-1);
+      expect(frame.roll()).not.toBeGreaterThan(10);
+    })
   })
 
   describe("Rolls", function(){
-    it("can be a maximum of 2", function(){
+    it("sets a maximum value of 2", function(){
       expect(frame.MAX_ROLLS).toEqual(2);
+    })
+    it("count starts off at 0 with each frame", function(){
+      expect(frame.rollCount).toEqual(0);
+    })
+    it("count increases by 1 when a roll is taken", function(){
+      frame.roll();
+      expect(frame.rollCount).toEqual(1);
+    })
+    it("count cannot exceed 2", function(){
+      for(var i = 0; i<2; i++){ frame.roll(); }
+      expect(function(){frame.roll();}).toThrow("Frame is over");
     })
   })
 
--- src/frame.js
+++ src/frame.js
@@ -1,4 +1,14 @@
 var Frame = function(){
   this.MAX_POINTS = 10;
   this.MAX_ROLLS = 2;
+  this.rollCount = 0;
 };
+
+Frame.prototype.roll = function(){
+  if (this.rollCount === this.MAX_ROLLS) {
+    throw "Frame is over"
+  } else {
+    this.rollCount += 1;
+    return Math.floor(Math.random() * 10);
+  }
+}

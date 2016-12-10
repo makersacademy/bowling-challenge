b86f4879fdcb33b0c25bd48dac1324e8341e113b
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-10 15:38:55 +0000

    added start function

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,8 +6,27 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("should return an empty array when score is initialized", function(){
-    expect(game.score).toEqual([]);
-  });
+    it("should initialize with a constant of pins", function(){
+      expect(game.pins).toEqual([0,1,2,3,4,5,6,7,8,9,10])
+    });
+
+    it("should return an empty array when score is initialized", function(){
+      expect(game.score).toEqual([]);
+    });
+
+    it("should initialize with a frame count of zero", function(){
+      expect(game.frameCount).toEqual(0);
+    });
+
+    it("should initialize with no pins set up (rack = false)", function(){
+      expect(game.rack).toEqual(false);
+    });
+
+    it("should load pins", function(){
+      game.start()
+      expect(game.rack).toEqual(true);
+    });
+
+
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,14 @@
 'use strict';
 
 function Game() {
+ this.pins = [0,1,2,3,4,5,6,7,8,9,10]
  this.score = [];
-
+ this.frameCount = 0;
+ this.rack = false;
 }
+
+Game.prototype.start = function() {
+  var pinsReady = this.pins;
+  this.rack = true;
+  return this.rack;
+};

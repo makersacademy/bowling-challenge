a5af77ae766684396b08c36a213dc98016960773
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-10 19:29:37 +0000

    Set initial conditions for Game constructor function

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,6 +10,15 @@ describe("Game", function(){
     it("has a maximum of 10 frames", function(){
       expect(game.MAX_FRAMES).toEqual(10);
     })
+    it("has an initial frame count of 1", function(){
+      expect(game.frameCount).toEqual(1);
+    })
+    it("has an initial score of 0", function(){
+      expect(game.score).toEqual(0);
+    })
+    it("has an initial empty points array", function(){
+      expect(game.points).toEqual([]);
+    })
   })
 
 
--- src/game.js
+++ src/game.js
@@ -1,3 +1,6 @@
 var Game = function(){
   this.MAX_FRAMES = 10;
+  this.frameCount = 1;
+  this.score = 0;
+  this.points = [];
 };

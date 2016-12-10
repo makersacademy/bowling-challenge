d5548bb02f0b0530f8921641dda1627145ac131c
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-10 17:35:43 +0000

    single responsibility methods validate throws

--- jasmine/src/bowlingCalculator.js
+++ jasmine/src/bowlingCalculator.js
@@ -13,10 +13,18 @@ BowlingCalculator.prototype.throwBall = function(pins) {
 };
 
 BowlingCalculator.prototype.validateThrowBall = function(number){
+  this.isNegativeNumber(number);
+  this.isNotANumber(number);
+};
+
+BowlingCalculator.prototype.isNegativeNumber = function(number){
   if (number < 0) {
     throw new Error("Invalid input: negative number.");
-  }
+  };
+};
+
+BowlingCalculator.prototype.isNotANumber = function(number){
   if (isNaN(number)) {
     throw new Error("Invalid input: not a number.")
-  }
+  };
 };

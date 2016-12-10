19e2bebe2a94c480c85831950f67203530a976ed
TudorTacal <tudortacal@gmail.com>
2016-12-10 18:46:15 +0000

    Started to implement the frames feature. Created a new Frame object. Injected it into a game method. WIP. Trying to figure the best way to make this work.

---
+++ src/frame.js
@@ -0,0 +1,7 @@
+var Frame = function () {
+  this.rolls = 0;
+};
+
+Frame.prototype.roll = function (){
+  
+};
--- src/game.js
+++ src/game.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 var Game = function () {
   this.frames = [];
   this.PINS_NUMBER = 10;
@@ -8,37 +10,38 @@ var Game = function () {
 Game.prototype.hit = function () {
   this._knockedDownPins();
   this._calculateScore();
-
-}
+  this.changeFrames();
+};
 
 Game.prototype._reset = function () {
   this.pins = 10;
   return this.pins;
-}
+};
 
-Game.prototype.frames = function () {
+Game.prototype.changeFrames = function (frame = new Frame()) {
 
-}
+  this.frames.push(frame);
+};
 
 Game.prototype.displayScore = function () {
   return this.score;
-}
+};
 
 Game.prototype.remainingPinsNumber = function () {
   return this.pins;
-}
+};
 
 
 Game.prototype._knockedDownPins = function () {
   this.pins = this.PINS_NUMBER - this._randomNumber();
   return this.pins;
-}
+};
 
 Game.prototype._calculateScore = function () {
   this.score += this._knockedDownPins();
-}
+};
 
 
 Game.prototype._randomNumber = function () {
   return Math.floor((Math.random() * 11));
-}
+};

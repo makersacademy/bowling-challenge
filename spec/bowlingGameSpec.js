51355d8ddd861deac44e19adca4d70b123086bf4
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-12-10 19:16:36 +0000

    resetting the game once maximum number of frames is reached

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -1,11 +1,42 @@
 describe("Bowling Game", function() {
   var game;
-  game = new BowlingGame();
-  it("should start the game on 0 points", function() {
-    expect(game.currentTotal).toEqual(0)
+  beforeEach(function() {
+    game = new BowlingGame();
   })
 
-  it("should start on the first frame", function() {
-    expect(game.currentFrame).toEqual(1)
+  describe("a new game", function() {
+
+    it("should start the game on 0 points", function() {
+      expect(game.currentTotal).toEqual(0);
+    })
+
+    it("should start on the first frame", function() {
+      expect(game.currentFrame).toEqual(1);
+    })
+
   })
+
+  describe("playing a game", function() {
+    it("moves to the next frame after two balls are played", function() {
+      game.moveToNextFrame()
+      expect(game.currentFrame).toEqual(2)
+    })
+  })
+
+  describe("end of the game", function() {
+    it("at the end of the game the frame number is set back to zero", function() {
+      game.moveToNextFrame()
+      game.moveToNextFrame()
+      game.resetGame()
+      expect(game.currentFrame).toEqual(1)
+    })
+
+    it("the game has a maximum of ten frames", function() {
+      for(count = 0; count < 10; count++ ) {
+        game.moveToNextFrame()
+      }
+      expect(function() { game.moveToNextFrame()}).toThrow(new Error("Game over!"))
+    })
+  })
+
 })
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,4 +1,15 @@
 function BowlingGame() {
-  this.currentTotal = 0
+  this.currentTotal = 0;
+  this.currentFrame = 1;
+}
+
+BowlingGame.prototype.moveToNextFrame = function() {
+  if (this.currentFrame > 10) {
+    throw new Error("Game over!")
+  }
+  this.currentFrame++;
+}
+
+BowlingGame.prototype.resetGame = function() {
   this.currentFrame = 1
 }

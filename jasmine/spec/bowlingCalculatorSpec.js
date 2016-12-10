5341ab957630dbeb01fdd7689472d630218eb127
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-10 16:44:14 +0000

    can add to total score

--- jasmine/spec/bowlingCalculatorSpec.js
+++ jasmine/spec/bowlingCalculatorSpec.js
@@ -8,8 +8,13 @@ describe('bowlingCalculator', function(){
     bowlingcalculator = new BowlingCalculator();
   });
 
-  it("initializes with a total score set to zero", function(){
+  it("should initialize with a total score set to zero", function(){
     expect(bowlingcalculator.totalScore).toEqual(0);
+  });
+
+  it("should be able to add to the total score", function(){
+    bowlingcalculator.addToScore(3)
+    expect(bowlingcalculator.totalScore).toEqual(3);
   });
 
 });
--- jasmine/src/bowlingCalculator.js
+++ jasmine/src/bowlingCalculator.js
@@ -1,3 +1,7 @@
 var BowlingCalculator = function () {
   this.totalScore = 0;
 };
+
+BowlingCalculator.prototype.addToScore = function(number) {
+  this.totalScore += number
+};

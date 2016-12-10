3cf8a23ca9dc18f376d9978e0307fed98f7c52fe
Louisa <louisaspicer@gmail.com>
2016-12-10 23:46:01 +0000

    Frame function created in order to pass first BowlingGame prototype test

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -27,10 +27,17 @@ describe("BowlingGame", function() {
 
   describe("rolling a ball", function() {
 
-    it("should create new frame on first roll", function() {
+    it("should add new frame on first roll", function() {
       game.roll(2);
-      expect(game.framesInPlay).toEqual([frame]);
+      expect(game.framesInPlay.length).toEqual(1);
     });
+
+    it("should add a second frame after two balls have been thrown", function() {
+      game.roll(2);
+      game.roll(5);
+      expect(game.framesInPlay.length).toEqual(2);
+    });
+    
   });
 
 
---
+++ spec/FrameSpec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe("Frame", function() {
+  var frame;
+
+  beforeEach(function() {
+    var knockedPins = 5
+    frame = new Frame(knockedPins)
+  });
+
+
+
+
+
+
+
+});

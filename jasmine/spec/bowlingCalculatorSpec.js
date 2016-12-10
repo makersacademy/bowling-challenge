69af0fe06a512851b0e5cc9c9b2fe4fe67c681eb
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-10 17:19:28 +0000

    refactor: validate input per ball not when scoring total

--- jasmine/spec/bowlingCalculatorSpec.js
+++ jasmine/spec/bowlingCalculatorSpec.js
@@ -8,7 +8,7 @@ describe('bowlingCalculator', function(){
     bowlingcalculator = new BowlingCalculator();
   });
 
-  describe("#new", function(){
+  describe("#new()", function(){
 
     it("should initialize with a total score set to zero", function(){
       expect(bowlingcalculator.totalScore).toEqual(0);
@@ -20,25 +20,33 @@ describe('bowlingCalculator', function(){
 
   });
 
-  it("should be able to add to the total score", function(){
-    bowlingcalculator.addToScore(3)
-    expect(bowlingcalculator.totalScore).toEqual(3);
+  describe("#addToScore(number)", function(){
+
+    it("should be able to add to the total score", function(){
+      bowlingcalculator.addToScore(3)
+      expect(bowlingcalculator.totalScore).toEqual(3);
+    });
+
   });
 
-  it("should store balls you throw separately", function(){
-    bowlingcalculator.throwBall(5);
-    bowlingcalculator.throwBall(3);
-    expect(bowlingcalculator.balls).toContain(5, 3);
+  describe("#throwBall(pins)", function(){
+
+    it("should store balls you throw separately", function(){
+      bowlingcalculator.throwBall(5);
+      bowlingcalculator.throwBall(3);
+      expect(bowlingcalculator.balls).toContain(5, 3);
+    });
+
   });
 
   describe("edge cases", function() {
 
-    it("should not accept negative numbers for scores", function(){
-      expect(function(){bowlingcalculator.addToScore(-5)}).toThrowError("Invalid input: negative number.")
+    it("should not accept negative numbers for balls thrown", function(){
+      expect(function(){bowlingcalculator.throwBall(-5)}).toThrowError("Invalid input: negative number.")
     });
 
-    it("should not accept anything except numbers for scores", function(){
-      expect(function(){bowlingcalculator.addToScore("not a number")}).toThrowError("Invalid input: not a number.")
+    it("should not accept anything except numbers for balls thrown", function(){
+      expect(function(){bowlingcalculator.throwBall("not a number")}).toThrowError("Invalid input: not a number.")
     });
 
   });
--- jasmine/src/bowlingCalculator.js
+++ jasmine/src/bowlingCalculator.js
@@ -4,15 +4,19 @@ var BowlingCalculator = function () {
 };
 
 BowlingCalculator.prototype.addToScore = function(number) {
+  this.totalScore += number
+};
+
+BowlingCalculator.prototype.throwBall = function(pins) {
+  this.validateThrowBall(pins);
+  this.balls.push(pins);
+};
+
+BowlingCalculator.prototype.validateThrowBall = function(number){
   if (number < 0) {
     throw new Error("Invalid input: negative number.");
   }
   if (isNaN(number)) {
     throw new Error("Invalid input: not a number.")
   }
-  this.totalScore += number
-};
-
-BowlingCalculator.prototype.throwBall = function(pins) {
-  this.balls.push(pins);
 };

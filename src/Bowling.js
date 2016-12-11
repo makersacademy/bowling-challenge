f6c8387455285ac08b01cbd1e05d9bf9fc4dd449
George Seeger <georgeseeger@hotmail.com>
2016-12-11 12:18:33 +0000

    Formatting bowling.js

--- src/Bowling.js
+++ src/Bowling.js
@@ -10,7 +10,7 @@ Bowling.prototype.spares = function(){
       this.bonus += this.game[i+2];
     }
   }
-}
+};
 
 Bowling.prototype.strikes = function(){
   for(var i = 0; i < 20; i+= 2){
@@ -18,23 +18,22 @@ Bowling.prototype.strikes = function(){
       this.bonus += this.nextTwoRollsSum(i);
     }
   }
-}
+};
 
 Bowling.prototype.scoreSum = function(){
-  if (this.game[18] === 10){this.game[19] = 0}
-  return this.game.slice(0,20).reduce(function(a, b){ return a + b }, 0)
-
-}
+  if (this.game[18] === 10){ this.game[19] = 0; }
+  return this.game.slice(0,20).reduce(function(a, b){ return a + b; }, 0);
+};
 
 Bowling.prototype.score = function(){
   this.spares();
   this.strikes();
   return this.bonus + this.scoreSum();
-}
+};
 
 Bowling.prototype.frameSum = function(index){
   return this.game[index] + this.game[index+1];
-}
+};
 
 Bowling.prototype.nextTwoRollsSum = function(index){
   var i = index + 2;
@@ -43,10 +42,10 @@ Bowling.prototype.nextTwoRollsSum = function(index){
   } else if(this._isStrike(i)){
     return this.game[i] + this.game[i+2];
   } else {
-  return this.game[i] + this.game[i+1]
+  return this.game[i] + this.game[i+1];
   }
-}
+};
 
 Bowling.prototype._isStrike = function(index){
   return (this.game[index] === 10 && this.game[index+1] === 0);
-}
+};

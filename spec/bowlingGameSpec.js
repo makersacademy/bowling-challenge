079e20c8e470f4b174fffd021306eba21b4cd603
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-11 00:27:27 +0000

    added next frame functionality

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -15,13 +15,27 @@ describe("bowlingGame", function() {
 
   describe("#roll", function(){
     it("should allow input of a number of pins and store it in the current roll score", function(){
-      expect(game.roll(7)).toEqual(7)
+      game.roll(7);
+      expect(game._rollScore).toEqual(7);
     });
 
     it("should move on to the next frame when a player gets a strike", function(){
-      game.roll(10)
+      game.roll(10);
       expect(game._frame).toEqual(2)
     });
+
+    it("should move on to the next frame when a player rolls twice", function(){
+      game.roll(4);
+      game.roll(3);
+      expect(game._frame).toEqual(2)
+    });
+
+    // it("should double the score in the next frame if a player rolls a strike the previous frame", function(){
+    //   game.roll(10);
+    //   game.roll(5);
+    //   game.roll(1);
+    //   expect(game.score).toEqual(22)
+    // });
   });
 
 
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,18 +2,35 @@
 
 function BowlingGame(){
   this.score = 0;
-  this.rollScore = 0;
+  this._rollScore = 0;
+  this._numberOfRolls = 0;
   this._frame = 1;
+  this.strike = false;
 };
 
+const RESET = 0
+
 BowlingGame.prototype.roll = function(number){
-  this.rollScore += number;
+  this._RollCalculator(number);
+  this._nextFrame();
   this._isStrike(number);
-  return this.rollScore;
 };
 
 BowlingGame.prototype._isStrike = function(number){
   if(number === 10){
     this._frame += 1;
+  };
+};
+
+BowlingGame.prototype._RollCalculator = function(number){
+  this._rollScore += number;
+  this._numberOfRolls += 1;
+};
+
+BowlingGame.prototype._nextFrame = function(){
+  if(this._numberOfRolls === 2){
+    this._frame += 1;
+    this._numberOfRolls = RESET;
+    this._rollScore = RESET;
   };
 };

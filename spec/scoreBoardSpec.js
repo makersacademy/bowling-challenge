cf8ade0237bb7f04772ed2eb6ecbb98d471ff5ea
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-11 09:41:02 +0000

    Added a frame count

--- spec/scoreBoardSpec.js
+++ spec/scoreBoardSpec.js
@@ -9,6 +9,10 @@ describe("Scoreboard", function(){
     expect(scoreBoard.currentScore).toEqual([]);
   });
 
+  it("should initialize with a frame count of zero", function(){
+    expect(scoreBoard.frameCount).toEqual(0);
+  });
+
   it("should set the players first roll score", function(){
     scoreBoard.firstRoll(3);
     expect(scoreBoard.currentScore).toContain(3);
@@ -17,6 +21,17 @@ describe("Scoreboard", function(){
   it("should set the players second roll score", function(){
     scoreBoard.secondRoll(4);
     expect(scoreBoard.currentScore).toContain(4);
+  });
+
+  it("should increase the frame count by 1 for each roll set", function(){
+    scoreBoard.secondRoll(4);
+    scoreBoard.currentFrame();
+    expect(scoreBoard.frameCount).toEqual(1);
+  });
+
+  it("should calculate the total score for the set", function(){
+    scoreBoard.secondRoll(4);
+    expect(scoreBoard.calculateScore()).toEqual(7);
   });
 
 
--- src/scoreBoard.js
+++ src/scoreBoard.js
@@ -1,5 +1,6 @@
 function Scoreboard (){
   this.currentScore = [];
+  this.frameCount = 0;
 };
 
   Scoreboard.prototype.firstRoll = function(scoreOne){
@@ -10,4 +11,9 @@ function Scoreboard (){
   Scoreboard.prototype.secondRoll = function(scoreTwo){
     this.currentScore.push(scoreTwo);
     return this.currentScore;
-  }
+  };
+
+  Scoreboard.prototype.currentFrame = function() {
+    this.frameCount = this.currentScore.length;
+    return this.frameCount
+  };

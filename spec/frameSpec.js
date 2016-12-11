a646a2faafbb87cbbb42096ce4282f63d700c4b7
ajbater <amanda22@hotmail.co.uk>
2016-12-11 13:28:16 +0000

    when a new frame is created it has the number of pins knocked down on the first and second roll

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -6,21 +6,21 @@ describe('Frame', function() {
   var game;
 
   beforeEach(function() {
-    frame = new Frame();
-    game = jasmine.createSpyObj('game', ['takeTurn']);
+    frame = new Frame(0, 0);
   });
 
   it('knows that the maximum score is 10', function() {
     expect(frame.MAXIMUM_PINS).toEqual(10);
   });
 
-  it('when player takes their turn the take turn method is called in game', function() {
-    game.takeTurn(0, 0);
-    expect(game.takeTurn).toHaveBeenCalledWith(0, 0);
-  });
+  describe('when a game is in play', function() {
 
-  // it('receives the number of pins knocked down in the second roll from game', function() {
-  //   game.takeTurn(0, 0);
-  //   expect(frame._secondRoll).toEqual(0);
-  // });
+    it('receives the number of pins knocked down in the first roll from game', function() {
+      expect(frame._firstRoll).toEqual(0);
+    });
+
+    it('receives the number of pins knocked down in the second roll from game', function() {
+      expect(frame._secondRoll).toEqual(0);
+    });
+  });
 });

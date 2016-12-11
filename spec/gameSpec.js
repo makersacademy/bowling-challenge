a7e554d3c432dc4f8a53f69ccc3f5b39dbde087b
ajbater <amanda22@hotmail.co.uk>
2016-12-11 12:22:49 +0000

    changed a method name

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -16,12 +16,12 @@ describe('Game', function() {
       expect(game._frames).toEqual([]);
     });
 
-    it('is expected to respond to roll', function() {
-      expect('roll' in game).toEqual(true);
+    it('is expected to respond to takeTurn', function() {
+      expect('takeTurn' in game).toEqual(true);
     });
 
     it('when roll is called it creates a new frame', function() {
-      game.roll();
+      game.takeTurn();
       expect(game._frames.length).toEqual(1);
     });
 
--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ function Game() {
   this._frames = [];
 }
 
-Game.prototype.roll = function(firstRoll, secondRoll) {
+Game.prototype.takeTurn = function(firstRoll, secondRoll) {
   frame = new Frame(firstRoll, secondRoll);
   this._frames.push(frame);
 };

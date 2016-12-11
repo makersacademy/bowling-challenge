3d5bce37eead025af1aa669919af98d4c8c4fc75
Asuka Ochi <asukaochi@gmail.com>
2016-12-11 00:41:01 +0000

    Add basic functions

--- spec/CaluclaterTenPinsBowlingSpec.js
+++ spec/CaluclaterTenPinsBowlingSpec.js
@@ -2,8 +2,8 @@
 
 describe ('Caluclater for Ten pins bowling', function(){
 
-  var caluclater
-  var pins
+  var caluclater;
+  var pins;
 
   beforeEach(function(){
     caluclater = new CaluclaterTenPinsBowling();
@@ -13,23 +13,75 @@ describe ('Caluclater for Ten pins bowling', function(){
 
   describe ('as a default', function(){
 
-    it('should have scores', function(){
-      expect( caluclater.scores ).toEqual([]);
+    it('should set gameScores a empty hash', function(){
+      expect( caluclater.gameScores ).toEqual({});
     });
 
-    it('should have roll', function(){
+    it('should set frameScores a empty array', function(){
+      expect( caluclater.frameScores ).toEqual([]);
+    });
+
+    it('should set roll "1"', function(){
       expect( caluclater.roll ).toBe(1);
     });
 
-    it('should have frame', function(){
-      expect( caluclater.roll ).toBe(1);
+    it('should set frame "1"', function(){
+      expect( caluclater.frame ).toBe(1);
+    });
+
+    it('should set strike flag "false"', function(){
+      expect( caluclater.strike ).toBe(false);
+    });
+
+    it('should set constant about number of Strike pins', function(){
+      expect( caluclater.STRIKE_PINS ).toBe(10)
     });
 
   });
 
-  it ('should pass how many pins', function(){
-    expect( caluclater.passScore(pins) ).toEqual(5);
+  it ('should store Scores how many pins the player knocks down', function(){
+    caluclater.passScore(pins);
+    expect( caluclater.frameScores ).toEqual([5]);
+    caluclater.passScore(pins);
+    expect( caluclater.frameScores ).toEqual([5,5]);
+    expect( caluclater.gameScores ).toEqual({1:[5,5]});
   });
 
+  it ('should store Scores when Strike', function(){
+    caluclater.passStrike();
+    expect( caluclater.gameScores ).toEqual({1:[10,"-"]});
+  });
+
+
+  it ('should change 1 to 2 when change to next roll', function(){
+    caluclater.changeRoll();
+    expect( caluclater.roll ).toBe(2);
+  });
+
+  it ('should change 2 to 1 when change to next roll', function(){
+    caluclater.changeRoll();
+    caluclater.changeRoll();
+    expect( caluclater.roll ).toBe(1);
+  });
+
+  it ('should change 1 to 2 when moved the next frame', function(){
+    caluclater.increaseFrame();
+    expect( caluclater.frame ).toBe(2);
+  });
+
+  it ('should clear frameScores', function(){
+    caluclater.clearFrameScores();
+    expect( caluclater.frameScores ).toEqual([]);
+  });
+
+  it ('should set strike "true"', function(){
+    caluclater.setStrike();
+    expect( caluclater.strike ).toBe(true);
+  });
+
+  it ('should check strike status', function(){
+    caluclater.setStrike();
+    expect( caluclater.strike ).toBe(true);
+  });
 
 });
--- src/CaluclaterTenPinsBowling.js
+++ src/CaluclaterTenPinsBowling.js
@@ -1,12 +1,40 @@
 'use strict';
 
 function CaluclaterTenPinsBowling() {
-  this.scores = []
-  this.roll = 1
-  this.frame = 1
+  this.gameScores  = {};
+  this.frameScores = [];
+  this.roll  = 1;
+  this.frame = 1;
+  this.strike = false;
+  this.STRIKE_PINS = 10;
 };
 
 
 CaluclaterTenPinsBowling.prototype.passScore = function(pins){
-  return pins
+  if( this.frameScores.length === 0 ){
+    this.frameScores[0] = pins
+  } else {
+    this.frameScores[1] = pins
+    this.gameScores[ this.frame ] = this.frameScores
+  }
+};
+
+CaluclaterTenPinsBowling.prototype.passStrike = function(){
+  this.gameScores[ this.frame ] = [ this.STRIKE_PINS, "-" ];
+};
+
+CaluclaterTenPinsBowling.prototype.changeRoll = function(){
+  this.roll === 1 ? this.roll = 2 : this.roll = 1;
+};
+
+CaluclaterTenPinsBowling.prototype.increaseFrame = function(){
+  this.frame ++;
+};
+
+CaluclaterTenPinsBowling.prototype.clearFrameScores = function(){
+  this.frameScores = [];
+};
+
+CaluclaterTenPinsBowling.prototype.setStrike = function(){
+  this.strike = true;
 };

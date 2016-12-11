ad538d47f550e84523ab21173ca4d988e3e6f1a0
Pea M. Tyczynska <CrystalPea@github.com>
2016-12-11 21:32:39 +0000

    Code refactored for Single Responsibility Principle

--- src/Game.js
+++ src/Game.js
@@ -6,17 +6,23 @@ function Game() {
 };
 Game.prototype.recordRoll = function(roll) {
   this.rolls.push(roll);
-  if (this.rolls.length === 2) {
+  if (this.isFrameCompleted()) {
     this.recordFrame();
   }
+};
+Game.prototype.isFrameCompleted = function() {
+  return this.rolls.length === 2;
 };
 
 Game.prototype.recordFrame = function() {
   this.framez.push(this.rolls);
   this.rolls = [];
-  if (this.framez.length === 10) {
+  if (this.isGameCompleted()) {
     this.calculateResult();
   }
+};
+Game.prototype.isGameCompleted = function () {
+  return this.framez.length === 10
 };
 
 Game.prototype.calculateResult = function() {

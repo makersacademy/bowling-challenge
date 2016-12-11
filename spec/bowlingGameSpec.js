6ca858c88e253ea429645f74bca112613bfb73b3
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-11 05:13:06 +0000

    massively refactored

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -17,14 +17,26 @@
 
     it("should store the score in scoresheet after finishing a frame", function(){
       game.rolls(10);
+      console.log(game.scoreSheet)
       expect(game.scoreSheet).toEqual([10])
     });
 
     it("should score double points in the next frame if a strike is obtained", function(){
       game.rolls(10);
       game.rolls(4,1);
+      console.log(game.scoreSheet)
       expect(game.scoreSheet).toEqual([10,10])
     });
+
+    it("should score 120 with 5 strikes in a row", function(){
+      for (var i = 0; i < 6; i++) {
+       game.rolls(10)
+     }
+     game.rolls(4,1)
+     expect(game.scoreTotal).toEqual(130)
+
+    });
+
   });
 
 
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -4,26 +4,26 @@ function BowlingGame(){
   this.scoreTotal = 0;
   this._rollOne = 0;
   this._rollTwo = 0;
-  this._frameRolls = 0;
   this.frame = 1;
   this.scoreSheet = []
   this.bonus = null
+  this.previousStrikeHeld = 0
 
 };
 
 const RESET = 0
 
 BowlingGame.prototype.rolls = function(number1,number2){
-  number2 = number2 || 0
-  this._RollCalculator(number1,number2);
+  var number2 = number2 || 0
+  console.log(number2)
+  this._rollCalculator(number1,number2);
   this._isStrike(number1);
   this._nextFrame();
 };
 
-BowlingGame.prototype._RollCalculator = function(number1,number2){
+BowlingGame.prototype._rollCalculator = function(number1,number2){
   this._rollOne = number1;
   this._rollTwo = number2;
-  this._frameRolls += 1;
 };
 
 
@@ -36,20 +36,38 @@ BowlingGame.prototype._isStrike = function(number1){
 
 
 BowlingGame.prototype._nextFrame = function(){
-  if(this._frameRolls === 2 || this._strike === true){
     this.frame += 1;
-    this._scoringFrame()
-    this.bonus = 'strike'
+    this._scoringFrame();
+    this.bonus = null
+    this.bonusAvailable()
+    this._frameRolls = 0
+    this._strike = false;
 
+};
+
+BowlingGame.prototype.bonusAvailable = function(){
+  if (this._strike === true){
+   this.bonus = 'strike'
   };
 };
 
 BowlingGame.prototype._scoringFrame = function(){
-     var rollScore = this._rollOne + this._rollTwo
+     var rollScore = this._rollOne + this._rollTwo + this.previousStrikeHeld;
+
+     if (this.bonus === 'strike' && this._strike === true){
+       return this.previousStrikeHeld += 10;
+     };
      if (this.bonus === 'strike'){
        rollScore *= 2;
        this.bonus = null;
-     }
+     };
 
-     this.scoreSheet.push(rollScore)
+     this.previousStrikeHeld = 0;
+     this.scoreSheet.push(rollScore);
+     this.scoreTotal += this.tallyScore()
+};
+
+BowlingGame.prototype.tallyScore = function(){
+  var sum = this.scoreSheet.reduce(function(a,b){return a + b;},0);
+  return sum
 };

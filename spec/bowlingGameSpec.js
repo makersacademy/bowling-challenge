e12dfedcc460bcc8764fe3a00117edcdafb88987
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-11 03:29:03 +0000

    massively refactored

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -1,11 +1,11 @@
-'use strict';
+  'use strict';
 
-describe("bowlingGame", function() {
-  var game;
+  describe("bowlingGame", function() {
+    var game;
 
-  beforeEach(function(){
-    game = new BowlingGame;
-  });
+    beforeEach(function(){
+      game = new BowlingGame;
+    });
 
   describe("Initialize", function(){
     it("Should initialize with score property set to zero", function(){
@@ -14,14 +14,24 @@ describe("bowlingGame", function() {
   });
 
   describe("#roll", function(){
-    it("should allow input of a number of pins and store it in the current roll score", function(){
-      game.roll(7);
-      expect(game._rollScore).toEqual(7);
+
+    it("should store the score in scoresheet after finishing a frame", function(){
+      game.rolls(10);
+      expect(game.scoreSheet).toEqual([10])
+    });
+
+    it("should score double points in the next frame if a strike is obtained", function(){
+      game.rolls(10);
+      game.rolls(4,1);
+      expect(game.scoreSheet).toEqual([10,10])
     });
   });
 
-  it("should store the score in scoresheet after finishing a frame", function(){
-    game.roll(10);
-    expect(game.scoreSheet).toEqual([10])
-  });
+
+
+
+
+
+
+
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,30 +2,35 @@
 
 function BowlingGame(){
   this.scoreTotal = 0;
-  this._rollScore = 0;
+  this._rollOne = 0;
+  this._rollTwo = 0;
   this._frameRolls = 0;
   this.frame = 1;
   this.scoreSheet = []
+  this.bonus = null
 
 };
 
 const RESET = 0
 
-BowlingGame.prototype.roll = function(number){
-  this._RollCalculator(number);
-  this._isStrike(number);
+BowlingGame.prototype.rolls = function(number1,number2){
+  number2 = number2 || 0
+  this._RollCalculator(number1,number2);
+  this._isStrike(number1);
   this._nextFrame();
 };
 
-BowlingGame.prototype._RollCalculator = function(number){
-  this._rollScore += number;
+BowlingGame.prototype._RollCalculator = function(number1,number2){
+  this._rollOne = number1;
+  this._rollTwo = number2;
   this._frameRolls += 1;
 };
 
 
-BowlingGame.prototype._isStrike = function(number){
-  if(number === 10){
+BowlingGame.prototype._isStrike = function(number1){
+  if(number1 === 10){
     this._strike = true;
+
   };
 };
 
@@ -34,10 +39,17 @@ BowlingGame.prototype._nextFrame = function(){
   if(this._frameRolls === 2 || this._strike === true){
     this.frame += 1;
     this._scoringFrame()
+    this.bonus = 'strike'
 
   };
 };
 
 BowlingGame.prototype._scoringFrame = function(){
-     this.scoreSheet.push(this._rollScore)
+     var rollScore = this._rollOne + this._rollTwo
+     if (this.bonus === 'strike'){
+       rollScore *= 2;
+       this.bonus = null;
+     }
+
+     this.scoreSheet.push(rollScore)
 };

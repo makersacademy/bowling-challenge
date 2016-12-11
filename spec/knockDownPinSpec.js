3f43c3ed3eb3010ee8824015e76e063d9c8b29ec
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-12-11 11:48:29 +0000

    moving through attempts

--- spec/knockDownPinSpec.js
+++ spec/knockDownPinSpec.js
@@ -6,8 +6,35 @@ describe("Knocking pins down", function() {
 
   describe("attempt", function() {
     it("returns a whole number between one and ten", function() {
-      attempt = pins.attempt()
+      attempt = pins.attemptBall()
       expect(attempt >= 0 && attempt <= 10).toEqual(true)
     })
+
+    it("Keeps track of the number of attempts", function() {
+      expect(pins.attempts).toBeDefined()
+    })
+
+    it("Starts off with no attempts", function() {
+      expect(pins.attempts).toEqual(0)
+    })
+
+    it("Moves to the next attempt", function() {
+      pins.attemptBall();
+      expect(pins.attempts).toEqual(1)
+    })
+
+    it("Raises an error when the maximum number of attempts is reached", function() {
+      for (count=0; count<2; count++) {
+        pins.attemptBall();
+      }
+      expect(function() {pins.attemptBall()}).toThrow(new Error("Maximum attempts reached"))
+    })
+
+    it("Resets the attempts back to zero", function() {
+      pins.attemptBall();
+      pins.resetAttempts();
+      expect(pins.attempts).toEqual(0)
+    })
+
   })
 })
--- src/knockDownPin.js
+++ src/knockDownPin.js
@@ -1,7 +1,20 @@
 function KnockDownPin() {
-
+  this.attempts = 0;
 }
 
-KnockDownPin.prototype.attempt = function() {
-  return 3;
+KnockDownPin.prototype.attemptBall = function() {
+  this.nextAttempt();
+  if (this.attempts > 2) {
+    throw new Error("Maximum attempts reached")
+  } else {
+    return 3;
+  }
+}
+
+KnockDownPin.prototype.nextAttempt = function() {
+  this.attempts++;
+}
+
+KnockDownPin.prototype.resetAttempts = function() {
+  this.attempts = 0;
 }

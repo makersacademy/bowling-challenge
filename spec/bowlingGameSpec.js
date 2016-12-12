525e0e2d7ac775ad93fe5f3ca2e77b66fd26df2c
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-12 00:43:53 +0000

    initializes with score set to zero

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -8,34 +8,14 @@
     });
 
   describe("Initialize", function(){
-    it("Should initialize with score property set to zero", function(){
-      expect(game.scoreTotal).toEqual(0);
-    });
+     it("Should initialize with score set to zero", function(){
+       expect(game.score).toEqual(0)
+     });
   });
 
   describe("#roll", function(){
 
-    it("should store the score in scoresheet after finishing a frame", function(){
-      game.rolls(10);
-      console.log(game.scoreSheet)
-      expect(game.scoreSheet).toEqual([10])
-    });
 
-    it("should score double points in the next frame if a strike is obtained", function(){
-      game.rolls(10);
-      game.rolls(4,1);
-      console.log(game.scoreSheet)
-      expect(game.scoreSheet).toEqual([10,10])
-    });
-
-    it("should score 120 with 5 strikes in a row", function(){
-      for (var i = 0; i < 6; i++) {
-       game.rolls(10)
-     }
-     game.rolls(4,1)
-     expect(game.scoreTotal).toEqual(130)
-
-    });
 
   });
 

9cbd15ce3c8f67d7ef99e9c8f3d2f66041d7d420
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-20 14:26:08 +0000

    fix spare last turn function

--- jasmine/spec/bowlingCalculatorSpec.js
+++ jasmine/spec/bowlingCalculatorSpec.js
@@ -166,14 +166,14 @@ describe('BowlingCalculator', function(){
 
   describe("#_isSpareLastTurn()", function(){
 
-    xit("should return true if you got a spare last turn", function(){
+    it("should return true if you got a spare last turn", function(){
       bowlingcalculator.throwBall(5);
       bowlingcalculator.throwBall(5);
       bowlingcalculator.endTurn();
       expect(bowlingcalculator._isSpareLastTurn()).toBeTruthy();
     });
 
-    xit("should return false if you didn't get a spare last turn", function(){
+    it("should return false if you didn't get a spare last turn", function(){
       bowlingcalculator.throwBall(5);
       bowlingcalculator.throwBall(1);
       bowlingcalculator.endTurn();

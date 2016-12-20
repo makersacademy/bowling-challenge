4fc72e9047452a03a2ca44be0b7c9ac077731b68
George Eaton <george_eaton@hotmail.co.uk>
2016-12-20 16:42:39 +0000

    adds 10 frames for tests

--- spec/BowlingScorerSpec.js
+++ spec/BowlingScorerSpec.js
@@ -2,12 +2,28 @@ describe("bowlingScorer", function() {
   var scorer;
   var frame1;
   var frame2;
+  var frame3;
+  var frame4;
+  var frame5;
+  var frame6;
+  var frame7;
+  var frame8;
+  var frame9;
+  var frame10;
 });
 
 beforeEach(function() {
   scorer = new bowlingScorer();
   frame1 = jasmine.createSpy('frame1', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
   frame2 = jasmine.createSpy('frame2', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame3 = jasmine.createSpy('frame3', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame4 = jasmine.createSpy('frame4', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame5 = jasmine.createSpy('frame5', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame6 = jasmine.createSpy('frame6', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame7 = jasmine.createSpy('frame7', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame8 = jasmine.createSpy('frame8', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame9 = jasmine.createSpy('frame9', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
+  frame10 = jasmine.createSpy('frame10', ['rollOne', 'rollTwo', '_isStrike', 'frameBonus']);
 });
 
 it("should start with a total score of zero", function() {
@@ -50,6 +66,7 @@ describe("calculating bonuses", function() {
     scorer.addFrame(frame2);
     expect(scorer.frames[0].frameBonus).toEqual(0);
   });
+
 });
   describe("addBase()", function() {
     it("should add all scores to the baseTotal", function() {

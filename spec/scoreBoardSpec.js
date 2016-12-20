ad75bff78ebe0e1171303fb957e01dc57717222a
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-20 13:04:15 +0000

    Added a spare strike function

--- spec/scoreBoardSpec.js
+++ spec/scoreBoardSpec.js
@@ -2,10 +2,11 @@
 
 describe("Scoreboard", function(){
   var scoreboard;
+  var game;
 
   beforeEach(function(){
     scoreboard = new Scoreboard();
-
+    game = jasmine.createSpy('game');
   });
 
   it("should intialize with an empty scores array", function(){
@@ -21,62 +22,128 @@ describe("Scoreboard", function(){
   });
 
   it("should return a calculation of the current score per throw set", function(){
-    var currentScore = [3,4]
-    scoreboard.calculateScore(currentScore);
+    game.currentScore = [3,4]
+    scoreboard.calculateScore(game.currentScore);
     expect(scoreboard.scores).toContain(7);
   });
 
   it("should return an array of all individual scores obtained", function(){
-    var currentScore = [3,4]
-    scoreboard.calculateScore(currentScore);
+    game.currentScore = [3,4]
+    scoreboard.calculateScore(game.currentScore);
     expect(scoreboard.allScores).toEqual([[3,4]]);
   });
 
   it("should return a calculation of the current score per throw set", function(){
-    var currentScore = ["X"]
-    scoreboard.calculateScore(currentScore);
+    game.currentScore = ["X"]
+    scoreboard.calculateScore(game.currentScore);
     expect(scoreboard.scores).toContain('0X');
   });
 
+  it("should return the relevant function according to the score - 2 strikes", function(){
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    expect(scoreboard.scores).toContain(20);
+  });
+
   it("should return the relevant function according to the score - strike", function(){
-  var currentScore = ["X"];
-  scoreboard.calculateScore(currentScore);
-  var currentScore = [3,3];
-  scoreboard.calculateScore(currentScore);
-  scoreboard.bonusPoints();
-  expect(scoreboard.scores).toContain(16);
+    game.currentScore = ["X"]
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal()
+    game.currentScore = [3,3]
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal()
+    expect(scoreboard.scores).toContain(16);
   });
 
   it("should return the relevant function according to the score - spare", function(){
-   var currentScore = [5,5]
-   scoreboard.calculateScore(currentScore);
-   var currentScore = [3,6]
-   scoreboard.calculateScore(currentScore);
-   scoreboard.bonusPoints();
-   expect(scoreboard.scores).toContain(13);
+    game.currentScore = [5,5]
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal()
+    game.currentScore = [3,6]
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal()
+    expect(scoreboard.scores).toContain(13);
   });
 
-  it("should return the scores if no stikes or spares were scored", function(){
-    var currentScore = [5,2];
-    scoreboard.calculateScore(currentScore);
-    var currentScore = [3,6];
-    scoreboard.calculateScore(currentScore);
-    scoreboard.bonusPoints();
-    expect(scoreboard.scores).toEqual([7,9]);
+  it("should return the relevant function according to the score - spare", function(){
+    game.currentScore = [5,5];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ['X'];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    expect(scoreboard.scores).toContain(20);
+  });
+
+  it("should return the total score if no stikes or spares were scored", function(){
+    game.currentScore = [5,2];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = [3,6];
+    scoreboard.calculateScore(game.currentScore);
+    expect(scoreboard.currentTotal()).toEqual(16);
   });
 
   it("should calculate current total", function() {
-    var currentScore = [5,5]
-    scoreboard.calculateScore(currentScore);
-    var currentScore = [3,6]
-    scoreboard.calculateScore(currentScore);
+    game.currentScore = [5,5]
+    scoreboard.calculateScore(game.currentScore);
     scoreboard.bonusPoints();
+    game.currentScore = [3,6]
+    scoreboard.calculateScore(game.currentScore);
     expect(scoreboard.currentTotal()).toEqual(22)
   });
 
-  it("should return perfect score if score equals 300", function(){
-    scoreboard.total = 300;
+  it("should return perfect score if total score is 300", function(){
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
     scoreboard.currentTotal();
-    expect(scoreboard.perfectGame()).toEqual('Perfect Game!')
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    scoreboard.currentTotal();
+    game.currentScore = ["X"];
+    scoreboard.calculateScore(game.currentScore);
+    expect(scoreboard.currentTotal()).toEqual('Perfect Game!')
   });
+
+  it("should return gutter game if total score is zero", function() {
+    expect(scoreboard.currentTotal()).toEqual('Gutter Game!')
+  });
+
+  it("should count the frames completed", function() {
+    scoreboard.allScores = ([[1,3]]);
+    scoreboard.increaseFrame();
+    expect(scoreboard.frameCount).toEqual(1)
+  });
+
 });
--- src/scoreBoard.js
+++ src/scoreBoard.js
@@ -12,41 +12,73 @@ function Scoreboard() {
     return this.scores;
   };
 
+  Scoreboard.prototype.currentTotal = function() {
+    this.bonusPoints();
+    var total = this.scores.reduce(function(a,b){return a + b },0);
+      if (total === '2200X') {
+        return this.perfectGame();
+      } else if (total === 0) {
+        return this.gutterGame();
+      } else {
+        return total;
+      };
+  };
+
+  Scoreboard.prototype.increaseFrame = function() {
+    this.frameCount = this.allScores.length;
+    return this.frameCount
+  };
+
   Scoreboard.prototype.bonusPoints = function() {
-  if (this.scores[this.scores.length-2] === '0X') {
-    return this.aStrike();
+    if (this.scores[this.scores.length-2] === '0X' && this.scores[this.scores.length-1] === '0X'){
+      return this.multiStrike();
+  } else if (this.scores[this.scores.length-2] === '0X') {
+      return this.aStrike();
+  }  else if (this.scores[this.scores.length-2] === 10 && this.scores[this.scores.length-1] === '0X' ) {
+        return this.strikeSpare();
   } else if (this.scores[this.scores.length-2] === 10) {
-    return this.spare();
+      return this.spare();
   } else {
-    return this.scores;
-  };
-};
-
-Scoreboard.prototype.aStrike = function(){
-  var strike = this.scores.indexOf('0X');
-    if (~strike) {
-      this.scores[strike] = this.MAXSCORE + this.scores.slice(-1)[0];
       return this.scores;
-    }
+  };
   };
 
-Scoreboard.prototype.spare = function(currentScore) {
-  var spare = this.scores.indexOf(10);
-    if (~spare) {
-      this.scores[spare] = this.MAXSCORE + this.allScores[this.allScores.length-1][0];
-      return this.scores;
-    }
-};
-
-Scoreboard.prototype.currentTotal = function() {
-  this.total = this.scores.reduce(function(a,b){return a + b },0);
-    if (this.total === 300) {
-      return this.perfectGame();
-    } else {
-      return this.total;
+  Scoreboard.prototype.multiStrike = function(){
+    var strike = this.scores.indexOf('0X');
+      if (~strike) {
+        this.scores[strike] = this.MAXSCORE + this.MAXSCORE;
+        return this.scores;
+      }
     };
-};
 
-Scoreboard.prototype.perfectGame = function() {
-  return 'Perfect Game!'
-};
+  Scoreboard.prototype.aStrike = function(){
+    var strike = this.scores.indexOf('0X');
+      if (~strike) {
+        this.scores[strike] = this.MAXSCORE + this.scores.slice(-1)[0];
+        return this.scores;
+      }
+    };
+
+  Scoreboard.prototype.spare = function(currentScore) {
+    var spare = this.scores.indexOf(10);
+      if (~spare) {
+        this.scores[spare] = this.MAXSCORE + this.allScores[this.allScores.length-1][0];
+        return this.scores;
+      }
+  };
+
+  Scoreboard.prototype.strikeSpare = function(currentScore) {
+    var spare = this.scores.indexOf(10);
+      if (~spare) {
+        this.scores[spare] = this.MAXSCORE + this.MAXSCORE;
+        return this.scores;
+      }
+  };
+
+  Scoreboard.prototype.perfectGame = function() {
+    return 'Perfect Game!'
+  };
+
+  Scoreboard.prototype.gutterGame = function() {
+    return 'Gutter Game!'
+  };

ab53060f19f8a10257c8307f66a1ea498c1141b8
Courtney Osborn <Courtney.lee.osborn@gmail.com>
2016-12-20 13:05:03 +0000

    Added running total function

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,9 +2,11 @@
 
 describe("Game", function(){
   var game;
+  var scoreboard;
 
   beforeEach(function(){
     game = new Game();
+    scoreboard = new Scoreboard();
   });
 
   it("should initialize with a constant of pin setup", function(){
@@ -27,8 +29,12 @@ describe("Game", function(){
     expect(game.score).toEqual([]);
   });
 
-  it("should initilize with a sweep function returning false", function(){
+  it("should initialize with a sweep function returning false", function(){
     expect(game.sweep).toBe(false);
+  });
+
+  it("should initialize with a new Scoreboard", function() {
+    expect(game.scoreboard).toEqual(new Scoreboard())
   });
 
   it("should re-rack the pins between each round if frame count is less than ", function(){
@@ -43,13 +49,11 @@ describe("Game", function(){
   });
 
   it("should return amount of pins knocked down in first throw- excluding a strike", function(){
-    game.rackUp();
     spyOn(game, 'firstRoll').and.returnValue(4);
     expect(game.firstRoll()).toEqual(4);
   });
 
   it("should return X when 10 pins are knocked down in first throw", function(){
-    game.rackUp();
     spyOn(game, "firstRoll").and.returnValue(10);
     expect(game.firstRoll()).toEqual(10);
   });
@@ -57,19 +61,17 @@ describe("Game", function(){
   it("should return the left over pins", function(){
     game.rackUp();
     game.currentScore = [9];
-    game.pinSweep();
+    game.firstRoll();
     expect(game.setUpPins).toEqual([0,1]);
   });
 
-  it("should return the true when remaing pins are returned", function(){
-    game.rackUp();
+  it("should return the true when remaining pins are returned", function(){
     game.currentScore = ['X'];
-    game.pinSweep();
-    expect(game.sweep).toBe(false);
+    game.firstRoll();
+    expect(game.sweep).toBe(true);
   });
 
   it("should return amount of pins knocked down in second throw", function(){
-    game.rackUp();
     game.currentScore = [4];
     game.pinSweep();
     spyOn(game, 'secondRoll').and.returnValue(3);
@@ -79,6 +81,28 @@ describe("Game", function(){
   it("should return the frame count to zero", function(){
     game.newGame();
     expect(game.frameCount).toEqual(0);
+  });
+
+  it("should calculate the current score", function() {
+    game.currentScore = [3, 4];
+    expect(game.currentRoundScore()).toContain(7);
+  });
+
+  it("should increase frame Count when there are two scores in current score", function() {
+    game.currentScore = [3, 4];
+    game.currentRoundScore();
+    game.increaseFrameCount();
+    expect(game.frameCount).toEqual(1);
+  });
+
+  it("should return the current total", function() {
+    game.currentScore = [5,2];
+    game.currentRoundScore();
+    game.runningTotal();
+    game.currentScore = [3,6];
+    game.currentRoundScore();
+    game.runningTotal();
+    expect(game.runningTotal()).toEqual(16);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -7,38 +7,50 @@ function Game() {
   this.currentScore = [];
   this.score = [];
   this.sweep = false;
-  this.scoreboard = new Scoreboard;
+  this.scoreboard = new Scoreboard();
 };
 
-Game.prototype.rackUp = function(){
-  if (this.frameCount < 10) {
-    return this.setUpPins = this.PINS
-  } else {
-    throw new Error("Game Over! Please start a new game");
+  Game.prototype.rackUp = function(){
+    if (this.frameCount < 10) {
+      return this.setUpPins = this.PINS
+    } else {
+      throw new Error("Game Over! Please start a new game");
+    };
   };
-};
 
-Game.prototype.firstRoll = function(){
-  var score = Math.floor(Math.random() * this.setUpPins.length);
-  if (score === 10) {
-    return this.currentScore.push('X');
-  } else {
-    return this.currentScore.push(score);
+  Game.prototype.firstRoll = function(){
+    var score = Math.floor(Math.random() * this.setUpPins.length);
+    if (score === 10) {
+      return this.currentScore.push('X');
+    } else {
+      this.pinSweep();
+      return this.currentScore.push(score);
+    };
   };
-};
 
-Game.prototype.pinSweep = function(){
-  if (this.currentScore != 'X') {
-   this.setUpPins.splice(-(this.currentScore));
-   return this.sweep = true;
+  Game.prototype.pinSweep = function(){
+     this.setUpPins.splice(-(this.currentScore));
+     return this.sweep = true;
   };
-};
 
-Game.prototype.secondRoll = function(){
-  var score = Math.floor(Math.random() * this.setUpPins.length);
-    return this.currentScore.push(score);
-};
+  Game.prototype.secondRoll = function(){
+    var score = Math.floor(Math.random() * this.setUpPins.length);
+      return this.currentScore.push(score);
+  };
 
-Game.prototype.newGame = function(){
-  this.frameCount = 0;
-};
+  Game.prototype.currentRoundScore = function() {
+    return this.scoreboard.calculateScore(this.currentScore);
+  };
+
+  Game.prototype.runningTotal = function() {
+    return this.scoreboard.currentTotal();
+  };
+
+  Game.prototype.increaseFrameCount = function() {
+    this.frameCount = this.scoreboard.increaseFrame();
+    return this.frameCount;
+  };
+
+  Game.prototype.newGame = function(){
+    this.frameCount = 0;
+  };

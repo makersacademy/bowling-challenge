b7298bfac4c5befc05e79774bc69c3645f9eaa25
pflarcology <rhrg500@gmail.com>
2016-12-21 15:26:35 +0000

    stars frame at 1

--- jasmine-standalone-2.5.2/spec/bowlingSpec.js
+++ jasmine-standalone-2.5.2/spec/bowlingSpec.js
@@ -9,4 +9,8 @@ describe("Bowling", function() {
     // var bowling = new Bowling();
     expect(bowling.score).toEqual(0);
   });
+
+  it("should start at 1", function() {
+    expect(bowling.frame).toEqual(1);
+  })
 });
--- jasmine-standalone-2.5.2/src/bowling.js
+++ jasmine-standalone-2.5.2/src/bowling.js
@@ -1,3 +1,4 @@
 function Bowling () {
   this.score = 0;
+  this.frame = 1;
 }

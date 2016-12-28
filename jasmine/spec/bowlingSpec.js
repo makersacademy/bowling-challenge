66e4f46c940bee911c41a401684cd20d819b5861
Rob Adams <robert.andrew.adams@gmail.com>
2016-12-28 23:40:18 +0000

    frame object created

--- jasmine/spec/bowlingSpec.js
+++ jasmine/spec/bowlingSpec.js
@@ -8,4 +8,13 @@ describe('Bowling', function() {
     it("expects a score sheet to exist", function() {
       expect(game.scoreSheet).toBeDefined()
     });
+
+    it("expects a frame to exist",function(){
+      expect(game.currentFrame).toBeDefined()
+    });
+
+    it("expects a frame to have a number of bowls",function (){
+      game.newGame()
+      expect(game.currentFrame.bowlsRemaining).toEqual(2)
+    });
 });
--- jasmine/src/bowling.js
+++ jasmine/src/bowling.js
@@ -1,3 +1,12 @@
-var Bowling = function() {
-  this.scoreSheet = []
+function Bowling() {
+  this.scoreSheet = [];
+  this.currentFrame = {}
+
+  Bowling.prototype.newGame = function() {
+    this.currentFrame = new Frame()
+  };
+};
+
+function Frame() {
+  this.bowlsRemaining = 2
 };

624b3ea231a19dede53f25754d50fc41fe76ccb2
Rob Adams <robert.andrew.adams@gmail.com>
2016-12-29 10:56:43 +0000

    basic score saving

--- jasmine/spec/bowlingSpec.js
+++ jasmine/spec/bowlingSpec.js
@@ -1,20 +1,29 @@
 describe('Bowling', function() {
-  var bowling;
+    var bowling;
 
     beforeEach(function() {
         game = new Bowling();
     });
 
     it("expects a score sheet to exist", function() {
-      expect(game.scoreSheet).toBeDefined()
+        expect(game.scoreSheet).toBeDefined()
     });
 
-    it("expects a frame to exist",function(){
-      expect(game.currentFrame).toBeDefined()
+    it("expects a frame to exist", function() {
+        expect(game.currentFrame).toBeDefined()
     });
 
-    it("expects a frame to have a number of bowls",function (){
-      game.newGame()
-      expect(game.currentFrame.bowlsRemaining).toEqual(2)
+    it("expects a frame to have a number of bowls", function() {
+        game.newGame()
+        expect(game.currentFrame.bowlsRemaining).toEqual(2)
     });
+
+    it("expects a score to be recorded in a frame", function() {
+        game.newGame();
+        game.bowlScore(3)
+        game.bowlScore(6);
+        expect(game.currentFrame.score()).toEqual(9)
+    });
+
+
 });
--- jasmine/src/bowling.js
+++ jasmine/src/bowling.js
@@ -1,12 +1,26 @@
 function Bowling() {
-  this.scoreSheet = [];
-  this.currentFrame = {}
+    this.scoreSheet = [];
+    this.currentFrame = {}
 
-  Bowling.prototype.newGame = function() {
-    this.currentFrame = new Frame()
-  };
+    Bowling.prototype.newGame = function() {
+        this.currentFrame = new Frame()
+    };
+
+    Bowling.prototype.bowlScore = function(score) {
+        this.currentFrame.saveFrameScore(score)
+    };
 };
 
 function Frame() {
-  this.bowlsRemaining = 2
+    this.bowls = []
+
+    Frame.prototype.saveFrameScore = function(score) {
+        var entry = {
+            Throw: this.bowlsRemaining,
+            Score: score
+        }
+        this.bowls.push(entry)
+    }
+
+  
 };

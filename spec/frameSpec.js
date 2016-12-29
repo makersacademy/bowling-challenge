6c43278b422e67c654e7c3ccb677644fc47763c3
Dan Pelensky <dan@pelensky.com>
2016-12-29 13:48:52 +0000

    allows the ball to be rolled twice

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -5,13 +5,21 @@ describe("Frame", function() {
     frame = new Frame();
   });
 
-  describe('should start the frame with', function() {
+  describe('The frame should start with', function() {
     it("0 rolls", function(){
-      expect(frame.rollNumber).toEqual(0);
+      expect(frame.rollsComplete).toEqual(0);
+    });
+
+    it("0 for the first roll", function(){
+      expect(frame.firstRollPins).toEqual(0);
+    });
+
+    it("0 for the second roll", function(){
+      expect(frame.secondRollPins).toEqual(0);
     });
 
     it("0 pins knocked down", function(){
-      expect(frame.pinsDown).toEqual(0);
+      expect(frame.totalPinsDown).toEqual(0);
     });
 
     it("a maximum of 10 pins per frame", function(){
@@ -21,6 +29,32 @@ describe("Frame", function() {
     it("a maximum of two rolls per frame", function(){
       expect(frame.MAXROLLS).toEqual(2);
     });
-
   });
+
+  describe("Frame rules", function(){
+    it("roll number should increase by 1", function(){
+      frame.increaseRollNumber();
+      expect(frame.rollsComplete).toEqual(1);
+    });
+
+    it("roll numbers should not be more than the max", function(){
+      frame.increaseRollNumber();
+      frame.increaseRollNumber();
+      expect(function () {frame.increaseRollNumber()}).toThrow(`${frame.MAXROLLS} rolls maximum.`)
+    });
+  });
+
+  describe("Rolls", function(){
+    it("should keep the score of the first roll", function(){
+      frame.rollFirstBall(8);
+      expect(frame.firstRollPins).toEqual(8);
+    });
+
+    it("shoukd keep the score of the second roll", function(){
+      frame.rollSecondBall(1);
+      expect(frame.secondRollPins).toEqual(1);
+    });
+  });
+
+
 });
--- src/frame.js
+++ src/frame.js
@@ -1,7 +1,25 @@
 // manages a single frame
 function Frame() {
-  this.rollNumber = 0;
-  this.pinsDown = 0;
+  this.rollsComplete = 0;
+  this.firstRollPins = 0;
+  this.secondRollPins = 0;
+  this.totalPinsDown = 0;
   this.MAXPINS = 10;
   this.MAXROLLS = 2;
+}
+
+Frame.prototype.increaseRollNumber = function() {
+  if(this.rollsComplete >= this.MAXROLLS) {
+    throw `${this.MAXROLLS} rolls maximum.`
+  } else {
+  this.rollsComplete += 1;
+}
+}
+
+Frame.prototype.rollFirstBall = function(pins){
+  this.firstRollPins = pins;
+}
+
+Frame.prototype.rollSecondBall = function(pins){
+  this.secondRollPins = pins;
 }

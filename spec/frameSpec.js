90736b2d967fbe1006a1050a92c5a244844b60f8
Dan Pelensky <dan@pelensky.com>
2016-12-29 12:27:48 +0000

    first passing test

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -4,3 +4,11 @@ describe("Frame", function() {
   beforeEach(function() {
     frame = new Frame();
   });
+
+describe('should start the frame with', function() {
+  it("0 rolls", function(){
+    expect(frame.rollNumber).toEqual(0);
+  });
+});
+
+});
--- src/frame.js
+++ src/frame.js
@@ -1,1 +1,4 @@
 // manages a single frame
+function Frame() {
+  this.rollNumber = 0; 
+}

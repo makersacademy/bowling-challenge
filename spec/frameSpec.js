2f6b267ae2709c3315789dfac53af6662d9e12e2
unicornelia <ez.kornelia@gmail.com>
2017-01-01 16:22:39 +0000

    Adding score and bonus calculation to frame function

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,65 +9,80 @@ describe('Frame', function() {
 
   it('accepts a score for a roll', function() {
     frame.roll(4);
-    expect(frame.score()).toEqual(4);
+    expect(frame.finalScore()).toEqual(4);
   });
 
   it('accepts two rolls and calculates the sum score of them', function() {
     frame.roll(3);
     frame.roll(2);
-    expect(frame.score()).toEqual(5);
+    expect(frame.finalScore()).toEqual(5);
   });
 
-describe('strike', function() {
-  it('returns true for strike if the score of the first roll is 10', function() {
-    frame.roll(10);
-    expect(frame.isStrike()).toBe(true);
+  describe('strike', function() {
+    it('returns true for strike if the score of the first roll is 10', function() {
+      frame.roll(10);
+      expect(frame.isStrike()).toBe(true);
+    });
+
+    it('returns false for strike if the score is less than 10 in the first roll', function() {
+      frame.roll(9);
+      expect(frame.isStrike()).toBe(false);
+    });
   });
 
-  it('returns false for strike if the score is less than 10 in the first roll', function() {
-    frame.roll(9);
-    expect(frame.isStrike()).toBe(false);
-  });
-});
+  describe('spare', function() {
+    it('return true for spare if the score of the first and second roll is 10', function() {
+      frame.roll(2);
+      frame.roll(8);
+      expect(frame.isSpare()).toBe(true);
+    });
 
-describe('spare', function() {
-  it('return true for spare if the score of the first and second roll is 10', function() {
-    frame.roll(2);
-    frame.roll(8);
-    expect(frame.isSpare()).toBe(true);
-  });
+    it('returns false for spare if the score of the first and second roll is less than 10', function() {
+      frame.roll(1);
+      frame.roll(7);
+      expect(frame.isSpare()).toBe(false);
+    });
 
-  it('returns false for spare if the score of the first and second roll is less than 10', function() {
-    frame.roll(1);
-    frame.roll(7);
-    expect(frame.isSpare()).toBe(false);
-  });
-
-  it('returns false for spare if the score of the first roll is 10', function() {
-    frame.roll(10);
-    expect(frame.isSpare()).toBe(false);
-  });
-});
-
-describe('frame complete', function() {
-  it('is true when there are two rolls', function () {
-    frame.roll(1);
-    frame.roll(3);
-    expect(frame.isFinished()).toBe(true);
+    it('returns false for spare if the score of the first roll is 10', function() {
+      frame.roll(10);
+      expect(frame.isSpare()).toBe(false);
+    });
   });
 
-  it('is true when the frame is a strike', function() {
-    frame.roll(10);
-    expect(frame.isFinished()).toBe(true);
+  describe('frame complete', function() {
+    it('is true when there are two rolls', function () {
+      frame.roll(1);
+      frame.roll(3);
+      expect(frame.isFinished()).toBe(true);
+    });
+
+    it('is true when the frame is a strike', function() {
+      frame.roll(10);
+      expect(frame.isFinished()).toBe(true);
+    });
+
+    it('is false if there is one roll and not a strike', function() {
+      frame.roll(9);
+      expect(frame.isFinished()).toBe(false);
+    });
   });
 
-  it('is false if there is one roll and not a strike', function() {
-    frame.roll(9);
-    expect(frame.isFinished()).toBe(false);
+  describe('bonus', function() {
+    it('calculates the bonus for a strike based on the next 2 roll', function() {
+      frame.roll(10);
+      frame.bonus([1, 3]);
+      expect(frame.finalScore()).toBe(14);
+    });
+
+    it('calculates the bonus for a spare based on the next 1 roll', function() {
+      frame.roll(5);
+      frame.roll(5);
+      frame.bonus([1]);
+      expect(frame.finalScore()).toBe(11);
+    });
+
+    // it('stores the bonuses in a separate array', function() {
+    //   frame.bonus();
+    // });
   });
-});
-
-
-// describe('bonus', function() {
-// });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,25 +1,40 @@
 var Frame = function() {
-  this._rolls = []
+  this._rolls = [];
+  this._bonus = 0;
 };
 
 Frame.prototype.roll = function(value) {
   this._rolls.push(value);
 };
 
-Frame.prototype.score = function() {
+Frame.prototype.internalScore = function() {
   return this._rolls.reduce(function(score, nextRoll) {
     return score + nextRoll;
   }, 0);
 };
 
 Frame.prototype.isStrike = function() {
-  return this._rolls.length === 1 && this.score() === 10;
+  return this._rolls.length === 1 && this.internalScore() === 10;
 };
 
 Frame.prototype.isSpare = function() {
-  return this._rolls.length === 2 && this.score() === 10;
+  return this._rolls.length === 2 && this.internalScore() === 10;
 };
 
 Frame.prototype.isFinished = function() {
   return this._rolls.length === 2 || this.isStrike();
+};
+
+Frame.prototype.bonus = function(bonusValue) {
+  if (this.isStrike()) {
+    this._bonus = bonusValue.reduce(function(sum, bonus) {
+      return sum + bonus;
+    }, 0);
+  } else if (this.isSpare()) {
+    this._bonus = bonusValue[0];
+  }
+};
+
+Frame.prototype.finalScore = function() {
+  return this.internalScore() + this._bonus;
 };

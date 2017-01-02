606e7f07e03c37d6f8fe815c07390c29fe8cba2d
keomony khun <keomony@gmail.com>
2017-01-02 16:28:32 +0000

    refactors spec

--- spec/frontend/unit/BowlingScoreSpec.js
+++ spec/frontend/unit/BowlingScoreSpec.js
@@ -3,7 +3,7 @@
 describe("Bowling Game: ",function(){
   var game;
 
-  this.rollMulti = function(game,nRolls,pins) {
+  var rollMulti = function(game,nRolls,pins) {
     for(var i = 0; i < nRolls; i++){
       game.roll(pins);
     }
@@ -22,19 +22,25 @@ describe("Bowling Game: ",function(){
     expect(game.rolls[0]).toEqual(4);
   });
 
-  it("should be gutter game when game is ended the total score is 0",function(){
-    this.rollMulti(game,20,0);
+  it("should be gutter game the total score is 0",function(){
+    rollMulti(game,20,0);
     expect(game.getTotal()).toEqual(0);
   });
 
   xit("should be perfect game when there are 12 strikes",function(){
-    this.rollMulti(game,12,10);
+    rollMulti(game,12,10);
     expect(game.getTotal()).toEqual(300);
   });
 
-  it("should return the total score when the game is ended(no strike nor spare)",function(){
-    this.rollMulti(game,20,4);
-    expect(game.getTotal()).toEqual(80);
+  describe("no strike nor spare", function(){
+    
+    it("should return the total score", function(){
+      rollMulti(game,18,0);
+      game.roll(4);
+      game.roll(5);
+      expect(game.getTotal()).toEqual(9);
+    });
+
   });
 
 });

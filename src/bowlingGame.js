c4b705104fe4f30aafb1c2f3cca949d0abedf186
malin patel <malin.patel@icloud.com>
2017-01-02 17:35:06 +0000

    Refactored score function

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -9,9 +9,10 @@ BowlingGame.prototype.roll = function(pins) {
 BowlingGame.prototype.score = function() {
   var result = 0;
   var rollIndex = 0;
+  var game = this;
 
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10) {
+    if (isSpare()) {
       result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
     } else {
         result += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
@@ -20,4 +21,8 @@ BowlingGame.prototype.score = function() {
   };
 
   return result;
+
+  function isSpare(){
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] === 10;
+  }
 };

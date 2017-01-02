aa87e750003a0e952d8666ee1cc7140acc3c4cf9
keomony khun <keomony@gmail.com>
2017-01-02 14:56:50 +0000

    refactors by changing describe and text in spec

--- spec/frontend/unit/BowlingScoreSpec.js
+++ spec/frontend/unit/BowlingScoreSpec.js
@@ -1,17 +1,17 @@
 "use strict";
 
-describe("Bowling Score Calculation: ",function(){
+describe("Bowling Game: ",function(){
   var game;
 
   beforeEach(function(){
     game = new Game();
   });
 
-  it("Score should be 0 when the game starts",function(){
+  it("should have score 0 when the game starts",function(){
     expect(game.getTotal()).toEqual(0);
   });
 
-  it("Should store the number of pins knocked down by each roll",function(){
+  it("should store the number of pins knocked down by each roll",function(){
     game.roll(4);
     expect(game.rolls[0]).toEqual(4);
   });

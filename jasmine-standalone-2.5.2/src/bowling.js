599cbee8797fa7fc50cebd74873aafb9ff3cb973
pflarcology <rhrg500@gmail.com>
2017-01-02 17:48:00 +0000

    further refactoring

--- jasmine-standalone-2.5.2/src/bowling.js
+++ jasmine-standalone-2.5.2/src/bowling.js
@@ -14,76 +14,118 @@ function Bowling () {
   this.unscored_half_strike = false;
 }
 
-Bowling.prototype.enter_score = function(number) {
+Bowling.prototype.enterScore = function(number) {
   if (this.go === 1) {
     this.firstGo(number)}
   else {
-    this.secondGo(number)}
+    this.secondGo(number)
+  }
 };
 
 Bowling.prototype.firstGo = function(number) {
   if (number === MAXIMUM_SCORE && this.unscored_strikes < 2) {
-    if (this.unscored_half_strike === true) {
-      this.score += MAXIMUM_SCORE + MAXIMUM_SCORE
-    }
-    this.frame += 1;
-    this.unscored_strikes += 1;
-    this.running_total.push("X");
+    this.firstGoStrikeManyStrikes(number);
   }
   else if (number === MAXIMUM_SCORE) {
-    this.frame += 1;
-    this.score += (MAXIMUM_SCORE + MAXIMUM_SCORE + MAXIMUM_SCORE);
+    this.firstGoStrike(number);
   }
   else {
-    if (this.unscored_strikes > 1) {
-      this.go += 1;
-      this.first_go = number;
-      this.score += (MAXIMUM_SCORE + MAXIMUM_SCORE + number);
-      this.unscored_strikes -= 1;
-    }
-    else if (this.unscored_strikes === 1) {
-      this.go += 1;
-      this.first_go = number;
-      this.running_total.push(number);
-    }
-    else if (this.unscored_half_strike === true) {
-      this.go += 1;
-      this.first_go = number;
-      this.score += MAXIMUM_SCORE + number;
-      this.unscored_half_strike = false;
-    }
-    else {
-    this.go += 1;
-    this.running_total.push(number);
-    this.first_go = number;
-}};
+    this.firstGoNotStrike(number);
+    };
+}
+
+Bowling.prototype.firstGoStrikeManyStrikes = function(number) {
+  if (this.unscored_half_strike === true) {
+    this.score += MAXIMUM_SCORE + MAXIMUM_SCORE
+  };
+  this.frame += 1;
+  this.unscored_strikes += 1;
+  this.running_total.push("X");
+}
+
+Bowling.prototype.firstGoStrike = function(number) {
+  this.frame += 1;
+  this.score += (MAXIMUM_SCORE + MAXIMUM_SCORE + MAXIMUM_SCORE);
+}
+
+Bowling.prototype.firstGoNotStrike = function(number) {
+  if (this.unscored_strikes > 1) {
+    this.firstGoNotStrikeUnscoredStrikes(number);
+  }
+  else if (this.unscored_strikes === 1) {
+    this.firstGoNotStrikeUnscoredStrike(number);
+  }
+  else if (this.unscored_half_strike === true) {
+    this.firstGoNotStrikeUnscoredHalfStrike(number)
+  }
+  else {
+  this.firstGoNotStrikeNormal(number);
+  }
+}
+
+Bowling.prototype.firstGoNotStrikeUnscoredStrikes = function(number) {
+  this.go += 1;
+  this.first_go = number;
+  this.score += (MAXIMUM_SCORE + MAXIMUM_SCORE + number);
+  this.unscored_strikes -= 1;
+}
+
+Bowling.prototype.firstGoNotStrikeUnscoredStrike = function(number) {
+  this.go += 1;
+  this.first_go = number;
+  this.running_total.push(number);
+}
+
+Bowling.prototype.firstGoNotStrikeUnscoredHalfStrike = function(number) {
+  this.go += 1;
+  this.first_go = number;
+  this.score += MAXIMUM_SCORE + number;
+  this.unscored_half_strike = false;
+}
+
+Bowling.prototype.firstGoNotStrikeNormal = function(number) {
+  this.go += 1;
+  this.running_total.push(number);
+  this.first_go = number;
 }
 
 Bowling.prototype.secondGo = function(number) {
   if (this.unscored_strikes > 0) {
-    this.go = 1;
-    this.second_go = number;
-    this.unscored_strikes = 0;
-    if (this.first_go + this.second_go === 10) {
-    this.score += MAXIMUM_SCORE + this.first_go + this.second_go ;
-    this.unscored_half_strike = true;
-    }
-    else {
-      this.score += (MAXIMUM_SCORE + 2 * (this.first_go + this.second_go));
-    }
+    this.secondGoUnscoredStrikes(number);
   }
   else if (this.first_go + number === 10) {
-    this.go = 1;
-    this.second_go = number;
-    this.unscored_half_strike = true;
+    this.secondGoHalfStrike(number)
   }
   else {
+    this.secondGoNormal(number)
+  }
+}
+
+Bowling.prototype.secondGoUnscoredStrikes = function(number) {
+  this.go = 1;
+  this.second_go = number;
+  this.unscored_strikes = 0;
+  if (this.first_go + this.second_go === 10) {
+  this.score += MAXIMUM_SCORE + this.first_go + this.second_go ;
+  this.unscored_half_strike = true;
+  }
+  else {
+    this.score += (MAXIMUM_SCORE + 2 * (this.first_go + this.second_go));
+  }
+}
+
+Bowling.prototype.secondGoHalfStrike = function(number) {
+  this.go = 1;
+  this.second_go = number;
+  this.unscored_half_strike = true;
+}
+
+Bowling.prototype.secondGoNormal = function(number) {
   this.go = 1;
   this.second_go = number;
   this.frame += 1;
   this.score += this.first_go + number;
   this.running_total.push(number);
-}}
 }
 
 Bowling.prototype.showScore = function() {

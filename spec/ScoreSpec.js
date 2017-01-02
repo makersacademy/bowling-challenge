34d06e25cdb6baa3f0843047866d37a686d957d4
Tom Fuller <tomf95@hotmail.co.uk>
2017-01-02 10:58:41 +0000

    strikes work, needs refactoring

--- spec/ScoreSpec.js
+++ spec/ScoreSpec.js
@@ -4,7 +4,7 @@ describe('Bowling Score', function() {
   var player;
 
   beforeEach(function() {
-    player = jasmine.createSpyObj('player', ['bowl', 'secondBowl'])
+    player = jasmine.createSpyObj('player', ['bowl', 'secondBowl']);
     score = new Score(player);
   });
 
@@ -23,9 +23,34 @@ describe('Bowling Score', function() {
   it('should give an array of 10, / if a strike is scored', function() {
     player.bowl.and.returnValue(10)
     score.round()
-    expect(score._scores[0]).toEqual([10, '/'])
+    expect(score._scores[0]).toEqual(['/'])
   });
 
+  it('should give an array of first score and / when spare is scored', function() {
+    player.bowl.and.returnValue(6)
+    player.secondBowl.and.returnValue(4)
+    score.round()
+    expect(score._scores[0]).toEqual([6, '/']);
+  });
 
+  it('should calculate a total score for the game', function() {
+    player.bowl.and.returnValue(5);
+    player.secondBowl.and.returnValue(4);
+    score.round();
+    score.round();
+    score.round();
+    score.totalCalculator();
+    expect(score._total).toEqual(27);
+  });
+
+  it('should calculate the score for a strike', function() {
+    player.bowl.and.returnValue(10);
+    score.round();
+    player.bowl.and.returnValue(5);
+    player.secondBowl.and.returnValue(4);
+    score.round();
+    score.totalCalculator();
+    expect(score._total).toEqual(28);
+  });
 
 });
--- src/Score.js
+++ src/Score.js
@@ -1,20 +1,50 @@
 var Score = function(player) {
   this.player = player ? player : new Player()
   this._scores = [];
+  this._total = 0;
 };
 
 Score.prototype.round = function() {
   this._round = [];
   bowl = this.player.bowl()
-  this._round.push(bowl);
   if(this.isStrike(bowl))
     { this._round.push('/');
       this._scores.push(this._round); }
   else
-  { this._round.push(this.player.secondBowl());
-  this._scores.push(this._round); }
+  { this._round.push(bowl);
+    secondBowl = this.player.secondBowl();
+    if(this.isSpare(bowl, secondBowl))
+      {
+        this._round.push('/')
+        this._scores.push(this._round); }
+    else {
+      this._round.push(secondBowl);
+      this._scores.push(this._round); }}
 };
 
-Score.prototype.isStrike = function() {
+Score.prototype.isStrike = function(bowl) {
   return(bowl === 10)
+};
+
+Score.prototype.isSpare = function(bowl, secondBowl) {
+  return(bowl + secondBowl === 10)
+};
+
+Score.prototype.totalCalculator = function() {
+    total = []
+    for (var index = 0; index < this._scores.length; index++) {
+    if(this._scores[index].includes("/")) {
+      next = index + 1;
+      if(this._scores[next].includes("/")) {
+        total.push(30); }
+      else {
+        sum = 10 + this._scores[next][0] + this._scores[next][1];
+        total.push(sum)}
+       }
+    else
+      {sum = this._scores[index][0] + this._scores[index][1];
+      total.push(sum); }
+    };
+    this._total = total.reduce(add, 0);
+    function add(a,b) { return a + b; }
 };

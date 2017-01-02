850e963fb3d64b7a61cea1ca1ae6ed1fa41ecfbe
keomony khun <keomony@gmail.com>
2017-01-02 14:53:56 +0000

    stores number of pins of each roll

--- public/js/BowlingScore.js
+++ public/js/BowlingScore.js
@@ -1,5 +1,10 @@
 var Game = function(){
   this.score = 0;
+  this.rolls = [];
+
+  this.roll = function(pins){
+    this.rolls.push(pins);
+  }
 
   this.getTotal = function(){
     if(this.score !== 0 ){
--- spec/frontend/unit/BowlingScoreSpec.js
+++ spec/frontend/unit/BowlingScoreSpec.js
@@ -2,13 +2,18 @@
 
 describe("Bowling Score Calculation: ",function(){
   var game;
-  
+
   beforeEach(function(){
     game = new Game();
   });
 
   it("Score should be 0 when the game starts",function(){
     expect(game.getTotal()).toEqual(0);
+  });
+
+  it("Should store the number of pins knocked down by each roll",function(){
+    game.roll(4);
+    expect(game.rolls[0]).toEqual(4);
   });
 
 });

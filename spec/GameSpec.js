9d64815adcdca31f8bf311c1cfa25c7580f766ff
Kat Hicks <kmhicks92@gmail.com>
2017-02-04 20:03:14 +0000

    Added bonus point functionality for spares

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -9,6 +9,7 @@ describe("Game", function() {
   it('should return 0 in a gutter game', function(){
     for (var i = 0; i < game._TOTAL_FRAMES; i++) {
       game.roll(0);
+      game.roll(0);
     }
     expect(game.getScore()).toEqual(0);
   });
@@ -19,6 +20,21 @@ describe("Game", function() {
       game.roll(1);
     }
     expect(game.getScore()).toEqual(20);
+  });
+
+  it('should add a correct bonus score if rolls a spare', function(){
+    game.roll(5);
+    game.roll(5); // spare as all pints knocked over with two rolls
+
+    game.roll(3);
+    game.roll(2); // bonus due is three as the score of the next roll
+
+    for (var i = 0; i < (game._TOTAL_FRAMES - 2); i++) {
+      game.roll(0);
+      game.roll(0);
+    }
+
+    expect(game.getScore()).toEqual(10 + 5 + 3);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,21 +1,78 @@
 function Game() {
   this._score = 0;
+  this._rolls = [];
+  this._frame = [];
 
   this._TOTAL_FRAMES = 10;
 }
 
-// PUBLIC
+// ------------ PUBLIC ------------
+
+// Accessing properties
 
 Game.prototype.getScore = function() {
   return this._score;
 };
 
-Game.prototype.roll = function(pins) {
-  this._setScore(pins);
+Game.prototype.getRolls = function() {
+  return this._rolls;
 };
 
-// PRIVATE
+Game.prototype.getFrame = function() {
+  return this._frame;
+};
+
+// Object actions
+
+Game.prototype.roll = function(pins) {
+  this._addRoll(pins);
+  this._setScore(pins);
+
+  if (this.getFrame().length === 2) {
+    this._addFrame(this.getFrame());
+    this._newFrame();
+  }
+};
+
+// ----------- PRIVATE ------------
+
+// Altering properties
 
 Game.prototype._setScore = function(points) {
+  if (this._checkSpare() && this._checkNewFrame() ) {
+    this._score += points;
+  }
   this._score += points;
+};
+
+Game.prototype._addFrame = function(frame) {
+  this._rolls.push(frame);
+};
+
+Game.prototype._addRoll = function(roll) {
+  this._frame.push(roll);
+};
+
+Game.prototype._newFrame = function() {
+  this._frame = [];
+};
+
+// Internal actions
+
+Game.prototype._getLastFrame = function() {
+  return this.getRolls().slice(-1)[0]
+}
+
+Game.prototype._sumLastFrame = function() {
+  if (typeof this._getLastFrame() !== 'undefined') {
+    return ((this._getLastFrame()[0]) + (this._getLastFrame()[1]));
+  }
+};
+
+Game.prototype._checkSpare = function() {
+  return (this._sumLastFrame() === 10);
+};
+
+Game.prototype._checkNewFrame = function() {
+  return (this.getFrame().length === 1);
 };

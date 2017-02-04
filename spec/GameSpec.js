e8e4bc9de8aeaae88aba0697b6839fec0aa661a9
Kat Hicks <kmhicks92@gmail.com>
2017-02-04 17:37:33 +0000

    Added first unit test for a gutter game and passed the test

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -4,4 +4,12 @@ describe("Game", function() {
   beforeEach(function() {
     game = new Game();
   });
+
+  it('should return 0 in a gutter game', function(){
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.getScore()).toEqual(0);
+  });
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,2 +1,19 @@
 function Game() {
+  this._score = 0;
 }
+
+// PUBLIC
+
+Game.prototype.getScore = function() {
+  return this._score;
+};
+
+Game.prototype.roll = function(pins) {
+  this._setScore(pins);
+};
+
+// PRIVATE
+
+Game.prototype._setScore = function(points) {
+  this._score += points;
+};

8faeef90dd298d8fc3ff8e99a651f439844f6aea
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-02-05 15:28:40 +0000

    refactor isSpare to private

--- app/public/js/src/Game.js
+++ app/public/js/src/Game.js
@@ -9,14 +9,19 @@ Game.prototype.roll = function(pins) {
 
 Game.prototype.getScore = function() {
   var score = 0;
-  var i = 0;
+  var frameIndex = 0;
   for (frame = 0; frame < 10; frame++)
-    if (this.rolls[i] + this.rolls[i + 1] === 10) {
-      score += 10 + this.rolls[i + 2];
-      i += 2;
+    if (this._isSpare(frameIndex)) {
+      score += 10 + this.rolls[frameIndex + 2];
+      frameIndex += 2;
     } else {
-      score += this.rolls[i] + this.rolls[i+1];
-      i += 2;
+      score += this.rolls[frameIndex] + this.rolls[frameIndex+1];
+      frameIndex += 2;
     }
   return score;
+}
+
+
+Game.prototype._isSpare = function(frameIndex) {
+  return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10
 }

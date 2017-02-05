f682636a14fd241b992db6df5f7fb7683066503e
Kat Hicks <kmhicks92@gmail.com>
2017-02-05 01:30:03 +0000

    Added next test for a strike

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,7 +6,7 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it('should return 0 in a gutter game', function(){
+  it('should give the correct score of 0 for a gutter game', function(){
     for (var i = 0; i < game._TOTAL_FRAMES; i++) {
       game.roll(0);
       game.roll(0);
@@ -14,7 +14,7 @@ describe("Game", function() {
     expect(game.getScore()).toEqual(0);
   });
 
-  it('should return 20 if every roll is a 1', function(){
+  it('should give the correct score of 20 for all rolls of 1', function(){
     for (var i = 0; i < game._TOTAL_FRAMES; i++) {
       game.roll(1);
       game.roll(1);
@@ -22,7 +22,7 @@ describe("Game", function() {
     expect(game.getScore()).toEqual(20);
   });
 
-  it('should add a correct bonus score if rolls a spare', function(){
+  it('should add the correct bonus score for a spare', function(){
     game.roll(5);
     game.roll(5); // spare as all pints knocked over with two rolls
 
@@ -35,6 +35,20 @@ describe("Game", function() {
     }
 
     expect(game.getScore()).toEqual(10 + 5 + 3);
+  });
+
+  it('should add the correct bonus score for a strike', function(){
+    game.roll(10); // strike as knocked all ten pins in first roll
+
+    game.roll(3);
+    game.roll(2);
+
+    for (var i = 0; i < (game._TOTAL_FRAMES - 2); i++) {
+      game.roll(0);
+      game.roll(0);
+    }
+
+    expect(game.getScore()).toEqual(24);
   });
 
 });

38ba55d00491cf361b19d768fbbfbac1f66a8e0c
Ross Benzie <rossbenzie@Ross-MacBook-Pro.local>
2017-02-05 16:38:09 +0000

    minor updates all test passing

--- jasmine-2.5.2/src/Bowling.js
+++ jasmine-2.5.2/src/Bowling.js
@@ -3,104 +3,103 @@
 function Bowling() {
   this._frame = 1;
   this._roll = 1;
-  this._pins = 10;
   this._hits = 0;
   this._bonus = 0;
-  this._extraNext = 0;
-  this._extra2ndNext = 0;
+  this._strikeOrSpare = "";
   this._runningTotal = 0;
-  this._bonuses = 0; // to do deprecate
-};
-Bowling.prototype.getRunningTotal = function() {return this._runningTotal;};
-Bowling.prototype.getHits = function() {return this._hits;};
-Bowling.prototype.getFrame = function() {return this._frame;};
-Bowling.prototype.getPins = function() {return this._pins;};
-Bowling.prototype.getRoll = function() {return this._roll;};
-Bowling.prototype.getBonuses = function() {return this._bonuses;};
-Bowling.prototype.getBonus = function() {return this._bonus;};
-Bowling.prototype.getExtraNext = function() {return this._extraNext;};
-Bowling.prototype.getExtra2ndNext = function() {return this._extra2ndNext;};
 
-// Need these:
-
-// getScore() - needs splitting as said elsewhere
-// getBonus()   - get bonuses does something else
-// getStrikeOrSpare()
-// getRunningTotal() √
-
-Bowling.prototype.knockDownPins = function(hits) {
-  if (hits > this.getPins()) {
-    hits = this.getPins();
-  };
-  this._pins -= hits;
-  this._hits = hits;
-};
-Bowling.prototype.updateRunningTotal = function() {
-  if ((this.getBonuses() > 0) && (this.getFrame() <= 10 )) {
-    this._hits *= 2;
-    this._bonuses--;
-  }
-  this._runningTotal += this.getHits();
-};
-
-Bowling.prototype.recordBonuses = function() {
-  if (this.isAStrike()) {
-    this.addBonuses(2);
-  } else if (this.isASpare()) {
-    this.addBonuses(1);
-  };
-};
-
-Bowling.prototype.updateRollAndFrame = function() {
-  if (this.getRoll() == 2 || this.isAStrike()) {
-    this.newFrame();
-  } else {
-    this.secondRoll();
-  };
-};
-
-Bowling.prototype.addBonuses = function(bonuses) {
-  this._bonuses += bonuses;
-}
-
-Bowling.prototype.isAStrike = function() {
-  return (this.getPins() == 0 && this.getRoll() == 1);
-};
-
-Bowling.prototype.isASpare = function() {
-  return (this.getPins() == 0 && this.getRoll() == 2);
-};
-
-Bowling.prototype.newFrame = function() {
-  this._frame++;
   this._pins = 10;
-  this._roll = 1;
-  this._hits = 0;
+  this._extra = 0;
+  this._extraNext = 0;
 };
 
-Bowling.prototype.secondRoll = function() {
-  this._roll = 2;
-  this._hits = 0;
-};
+Bowling.prototype.getFrame = function() {return this._frame;};
+Bowling.prototype.getRoll = function() {return this._roll;};
+Bowling.prototype.getHits = function() {return this._hits;};
+Bowling.prototype.getBonus = function() {return this._bonus;};
+Bowling.prototype.getStrikeOrSpare = function() {return this._strikeOrSpare;}
+Bowling.prototype.getRunningTotal = function() {return this._runningTotal;};
 
-Bowling.prototype.isGameOver = function() {
-  return (this.getFrame() == 11 && this.getBonuses() == 0);
+Bowling.prototype.getPins = function() {return this._pins;}
+Bowling.prototype.getExtra = function() {return this._extra;}
+
+Bowling.prototype.addRow = function(hits) { // hits = user input
+  this.setHits(hits);
+  this.updateBonus();
+  if (this.noPinsLeft()) { this.updateStrikeOrSpare();};
+  this.updateRunningTotal();
+  this.writeRow();
+  this.endOfRowProcessing();
 }
-
-
-
-// Bowling.prototype.updateRow = function() {
-//   // NOT TESTED // if not game over:
-  // knockDownPins();
-  // updateRunningTotal();
-  // recordBonuses();
-  // updateRollAndFrame();
-// }
-// make _hits into separate values to be less error prone:
-// _pinsDown _bonusScore
-// then _runningTotal = _runningTotal + _pinsDown + _bonusScore
-
-// to do
-// SRP and classes
-
-// write html and rethink for output
+Bowling.prototype.setHits = function(hits) {
+  if (hits > this.getPins()) {
+    this._hits = this.getPins();
+  } else {
+    this._hits = hits;
+  }
+};
+Bowling.prototype.updateBonus = function() {
+  this._bonus = this.getHits() * this.getExtra();
+}
+Bowling.prototype.noPinsLeft = function() {
+  return this.getPins() - this.getHits() == 0;
+}
+Bowling.prototype.updateStrikeOrSpare = function() {
+  if (this.getRoll() == 1) {
+    this._strikeOrSpare = "Strike";
+  } else {
+    this._strikeOrSpare = "Spare";
+  }
+}
+Bowling.prototype.updateRunningTotal = function() {
+  this._runningTotal += this.getHits() + this.getBonus();
+};
+Bowling.prototype.writeRow = function() {
+  console.log( "Frame: " + this.getFrame() +
+  "	  Roll: " + this.getRoll() + "	 Hits: " + this.getHits() +
+  "  " + this.getStrikeOrSpare() + "  Bonus: " + this. getBonus() +
+  "		Total: " + this.getRunningTotal());
+}
+Bowling.prototype.isNewFrame = function() {
+  return (this.getRoll() == 2 || this.getStrikeOrSpare() == "Strike")
+}
+Bowling.prototype.endOfRowProcessing = function() {
+  if (this.isNewFrame()) {
+    this.setUpNewFrame();
+  } else {
+    this.setUpNewRow();
+  }
+  this.updateExtras();
+}
+Bowling.prototype.setUpNewFrame = function() {
+  this._frame++;
+  this._roll = 1;
+  this._pins = 10;
+  this.resetScoring();
+};
+Bowling.prototype.setUpNewRoll = function() {
+  this._roll = 2;
+  this._pins -= this.getHits();
+  this.resetScoring();
+};
+Bowling.prototype.resetScoring = function() {
+  this._hits = 0;
+  this._bonus = 0;
+  this._strikeOrSpare = ""; //
+}
+Bowling.prototype.updateExtras = function() {
+  this.removeSpentExtras();
+  this.addNewExtras();
+};
+Bowling.prototype.removeSpentExtras = function() {
+  this._extra = this._extraNext;
+  this._extraNext--;
+}
+Bowling.prototype.addNewExtras = function() {
+  if (this.getStrikeOrSpare() == "Strike") {
+    this._extra++;
+    this._extraNext++;
+  } else if (this.getStrikeOrSpare() == "Spare") {
+    this._extra++;
+  }
+}

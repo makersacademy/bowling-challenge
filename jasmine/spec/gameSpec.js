c01fc6648d76493862d0f60f9b367a4378cbb320
Shereen Finney <shereenfinney@gmail.com>
2017-02-05 16:09:23 +0000

    Created a gameSpec.js

---
+++ jasmine/spec/gameSpec.js
@@ -0,0 +1,15 @@
+// Count and sum the scores of a bowling game for one player (in JavaScript).
+// A bowling game consists of 10 frames in which the player tries to knock down
+// the 10 pins. In every frame the player can roll one or two times.
+// The actual number depends on strikes and spares. The score of a frame is the
+// number of knocked down pins plus bonuses for strikes and spares.
+// After every frame the 10 pins are reset.
+
+describe('Game', function() {
+  var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+
+
+});

d1c9536f010a3ae0403c8aaac550dafd3e904f1d
Shereen Finney <shereenfinney@gmail.com>
2017-02-05 16:45:15 +0000

    Testing that a frame contains 2 rolls

--- jasmine/spec/frameSpec.js
+++ jasmine/spec/frameSpec.js
@@ -1,12 +1,26 @@
-describe('Frame', function() {
+// Count and sum the scores of a bowling game for one player (in JavaScript).
+// A bowling game consists of 10 frames in which the player tries to knock down
+// the 10 pins. In every frame the player can roll one or two times.
+// The actual number depends on strikes and spares. The score of a frame is the
+// number of knocked down pins plus bonuses for strikes and spares.
+// After every frame the 10 pins are reset.
 
+describe('Frame', function() {
+  var frame;
+  beforeEach(function() {
+    frame = new Frame();
+  });
 
   it("exists", function(){
-    var frame;
-    frame = new Frame();
     expect(frame).toBeDefined();
   });
 
+  it("contains roll1 and roll2", function(){
+    var roll1;
+    var roll2
+    expect(frame.roll1).toBeDefined();
+    expect(frame.roll2).toBeDefined();
+  });
 
 
 });

6dbdf58181ad4166c2c411508942419a073763b9
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-05 20:16:33 +0000

    Less readable code. But shorter altogether.

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -10,42 +10,20 @@ BowlingGame.prototype.roll = function (pins) {
 BowlingGame.prototype.score = function () {
   var result = 0;
   var rollIndex = 0;
-  var game = this;
 
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (isStrike()) {
-      result += getStrikeScore();
+    if (this.rolls[rollIndex] === 10) {
+      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
       rollIndex++
-    } else if (isSpare()) {
-      result += getSpareScore();
+    } else if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10) {
+      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
       rollIndex += 2;
     } else {
-      result += getNormalScore();
+      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
       rollIndex += 2;
     }
   }
 
   return result;
-
-  function isStrike() {
-    return game.rolls[rollIndex] === 10;
-  }
-
-  function isSpare() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] === 10;
-  }
-
-  function getSpareScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
-  }
-
-  function getStrikeScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
-  }
-
-  function getNormalScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
-  }
-
 
 };

35f2ae3668116f676b1ccc846f9755d8777ef37c
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-02-05 22:24:12 +0000

    Re-did whole game. Everything in the game class. Need to refactor the scoring method

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,45 +1,113 @@
 describe("Bowling Game", function(){
+  var game
 
   beforeEach(function(){
     game = new Game();
   });
 
-  describe("New Games", function(){
-    it("Game begins with no frames", function(){
-      expect(game._gameFrames).toEqual([]);
+  describe("New games", function(){
+    it("Begins with an scorecard", function(){
+      expect(game._rolls).toEqual([]);
     });
 
-    it("Frame number begins as 0", function(){
-    expect(game._frameNumber).toEqual(0);
-   });
-  });
-
-  describe("Playing", function(){
-    it("Can play a frame", function(){
-      game.newRoll(1,4)
-      expect(game._gameFrames.length).toEqual(1)
-    });
-
-    it("Can play 10 frames", function(){
-      for(var i=0; i < 10; i++){
-        game.newRoll(1,4)
-      }
-      expect(game._gameFrames.length).toEqual(10)
-    });
-
-    it("On 11th frame an error is thrown saying game ended", function(){
-      for(var i=0; i < 10; i++){
-        game.newRoll(1,4)
-      }
-      expect(function(){ game.newRoll(3,4)}).toThrow("Your game has ended")
+    it("Can reset scorecard to begin a new game", function(){
+      for( var i = 0; i < 20; i++){
+        game.roll(3)
+      };
+      game.resetGame()
+      expect(game._rolls).toEqual([]);
     });
   });
 
-  describe("Viewing frame scores", function(){
-    it("Can view individual frame rolls", function(){
-      game.newRoll(1,4)
-      game.newRoll(2,3)
-      expect(game.showFrame(1)).toEqual([2,3])
+  describe("Playing the game", function(){
+    it("Inserts score from each game into rolls array", function(){
+      game.roll(4)
+      expect(game._rolls.length).toEqual(1);
+    });
+
+    it("Current Roll counter increments after each roll", function(){
+      game.roll(4)
+      expect(game._currentRoll).toEqual(1);
+    });
+  });
+
+  describe("A normal game", function(){
+    it("Calculates total score for a normal game with no strikes", function(){
+        game.roll(3);
+        game.roll(6);
+        for( var i = 0; i < 10; i++){
+          game.roll(2)
+        };
+        for( var i = 0; i < 8; i++){
+            game.roll(7)
+          };
+        game.calculateFrameScores();
+        expect(game._totalScore).toEqual(85);
+    });
+
+    it("Calculates total score for a normal game with a mix of results", function(){
+      game.roll(10);
+      game.roll(5);
+      game.roll(5);
+      for( var i = 0; i < 16; i++){
+        game.roll(3)
+      };
+      game.calculateFrameScores();
+      expect(game._totalScore).toEqual(81);
+    });
+  });
+
+  describe("Strikes", function(){
+    it("Calculates bonus scores for a perfect game", function(){
+      for( var i = 0; i < 12; i++){
+        game.roll(10)
+      };
+      game.calculateFrameScores()
+      expect(game._totalScore).toEqual(300);
+    });
+
+    it("Calculates total score for a normal game with a strike at the end", function(){
+      game.roll(10);
+      for( var i = 0; i < 16; i++){
+        game.roll(3)
+      };
+      game.roll(10);
+      game.roll(3);
+      game.calculateFrameScores();
+      expect(game._totalScore).toEqual(77);
+    });
+  });
+
+  describe("Spare", function(){
+    it("Calculates bonus scores for a game of spares", function(){
+      for( var i = 0; i < 21; i++){
+        game.roll(5)
+      };
+      game.calculateFrameScores()
+      expect(game._totalScore).toEqual(150);
+    });
+
+    it("Calculates total score for a normal game with a spare at the end", function(){
+      game.roll(10);
+      for( var i = 0; i < 16; i++){
+        game.roll(3)
+      };
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      game.calculateFrameScores();
+      expect(game._totalScore).toEqual(77);
+    });
+  });
+
+
+  describe("Gutter Game", function(){
+    it("Scores no points in entire game", function(){
+      for( var i = 0; i < 20; i++){
+        game.roll(0)
+      };
+      game.calculateFrameScores()
+      expect(game._totalScore).toEqual(0);
     });
   });
 
--- src/Game.js
+++ src/Game.js
@@ -1,25 +1,55 @@
 "use strict";
 
 const MAX_FRAMES = 10
+const MAX_ROLLS = 20
+const MAX_PINS = 10
 
 function Game(){
-  this._gameFrames = [];
-  this._frameNumber = 0;
+  this._rolls = [];
+  this._currentRoll = 0;
+  this._totalScore = 0;
 }
 
-Game.prototype.newRoll = function(rolls){
-  if (this._frameNumber === MAX_FRAMES) {throw ("Your game has ended");
-  } else {
-    var frame
-    frame = new Frame(rolls);
-    this._gameFrames.push(frame);
-    this._frameNumber ++;
-  }
+Game.prototype.roll = function(pins){
+  this._rolls.push(pins);
+  this._currentRoll ++;
 };
 
-Game.prototype.showFrame = function(number){
-  var array = []
-  array.push(this._gameFrames[number][0])
-  array.push(this._gameFrames[number][1])
-  return array
+Game.prototype.resetGame = function(){
+  this._rolls = [];
+};
+
+Game.prototype.calculateFrameScores = function(){
+  var rollIndex = 0;
+
+  for(var frameIndex = 0; frameIndex < MAX_FRAMES; frameIndex ++){
+    if (frameIndex === 9 && this.isStrike(rollIndex)) {
+      if (this._rolls[rollIndex+1] === 10) {
+        this._totalScore += 10 + this._rolls[rollIndex+1] + this._rolls[rollIndex+2];
+      } else {
+      this._totalScore += 10 + this._rolls[rollIndex+1];
+    }}
+    else if (this.isStrike(rollIndex)) {
+      this._totalScore += 10 + this._rolls[rollIndex+1] + this._rolls[rollIndex+2];
+      rollIndex += 1;
+    }
+    else if (this.isSpare(rollIndex)) {
+      this._totalScore += 10 + this._rolls[rollIndex+2];
+      rollIndex += 2;
+    }
+    else {
+      this._totalScore += this._rolls[rollIndex] + this._rolls[rollIndex+1];
+      rollIndex += 2;
+    }
+    };
+  return this._totalScore;
+
+};
+
+Game.prototype.isStrike = function(rollIndex){
+  return this._rolls[rollIndex] === 10
+};
+
+Game.prototype.isSpare = function(rollIndex){
+  return this._rolls[rollIndex] + this._rolls[rollIndex+1] === 10
 };

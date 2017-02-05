8a0c40276c9c6c84835b4e89228d99dc66421532
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-02-05 15:15:46 +0000

    checks a spare returns correct result

--- app/public/js/spec/GameSpec.js
+++ app/public/js/spec/GameSpec.js
@@ -1,17 +1,41 @@
 describe("Game", function() {
-
   var game;
 
   beforeEach(function() {
     game = new Game();
   });
 
+  function rollMany(n, pins) {
+    for (var i = 0; i < n; i++)
+      game.roll(pins);
+  }
+
   describe("Gutter game", function() {
-    it('should return 0', function(){
-      for (var i = 0; i < game.frames; i++) {
-        game.rollBall(0);
-      }
+    it('should return 0', function() {
+      // var n = 20;
+      // var pins = 0;
+      rollMany(20, 0);
       expect(game.getScore()).toEqual(0);
     });
   });
+
+  describe("All rolls of the game", function() {
+    it('should return 20 points', function() {
+      rollMany(20, 1);
+      expect(game.getScore()).toEqual(20);
+    });
+  });
+
+  describe("Spare", function() {
+    it('should return the correct number of points', function() {
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      rollMany(17, 0);
+      expect(game.getScore()).toEqual(16);
+    });
+  });
+
+
+
 });
--- app/public/js/src/Game.js
+++ app/public/js/src/Game.js
@@ -1,16 +1,22 @@
 function Game() {
-  this.score  = 0;
-  this.frames = 10;
+  this.rolls = []
+  this._currentRoll = 0;
 }
 
-Game.prototype.rollBall = function(pins) {
-  this._countScore(pins);
-};
+Game.prototype.roll = function(pins) {
+  return this.rolls[this._currentRoll++] = pins;
+}
 
 Game.prototype.getScore = function() {
-  return this.score;
-};
-
-Game.prototype._countScore = function(points) {
-  this.score += points;
-};
+  var score = 0;
+  var i = 0;
+  for (frame = 0; frame < 10; frame++)
+    if (this.rolls[i] + this.rolls[i + 1] === 10) {
+      score += 10 + this.rolls[i + 2];
+      i += 2;
+    } else {
+      score += this.rolls[i] + this.rolls[i+1];
+      i += 2;
+    }
+  return score;
+}

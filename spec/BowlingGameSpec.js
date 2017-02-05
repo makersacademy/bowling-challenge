e74f557effc4175fea48cf095245080ceb5cc96a
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-05 19:08:34 +0000

    Can roll a gutter game & refactor code.

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,8 +1,12 @@
 describe('The Bowling Game', function() {
 
-  it('can create a game', function() {
+  it('can roll a gutter game', function() {
     var game;
     game = new BowlingGame();
+    for (var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(0);
   });
 
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,3 +1,12 @@
 function BowlingGame (){
 
 }
+
+BowlingGame.prototype.roll = function (pins) {
+
+};
+
+
+BowlingGame.prototype.score = function () {
+  return 0;
+};

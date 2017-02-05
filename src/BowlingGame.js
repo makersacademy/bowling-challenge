5e59cbb3379ecc0f9517c9700f66e65e2a4cf97d
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-05 19:38:22 +0000

    Refactor code.

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -10,14 +10,30 @@ BowlingGame.prototype.roll = function (pins) {
 BowlingGame.prototype.score = function () {
   var result = 0;
   var rollIndex = 0;
+  var game = this;
 
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10) {
-      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    if (isSpare()) {
+      result += getSpareScore();
     } else {
-      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+      result += getNormalScore();
     }
     rollIndex += 2;
   }
+
   return result;
+
+  function isSpare() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] === 10;
+  }
+
+  function getSpareScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  }
+
+  function getNormalScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
+  }
+
+
 };

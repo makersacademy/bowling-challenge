87a51dab52ccdb3d734da11c85622febd6efb318
Kat Hicks <kmhicks92@gmail.com>
2017-02-05 20:30:18 +0000

    Started the extraction of frame and roll objects

---
+++ src/Frame.js
@@ -0,0 +1,33 @@
+function Frame() {
+  this._frame = [];
+}
+
+Frame.prototype.getFrame = function() {
+  return this._frame;
+};
+
+Frame.prototype.addRoll = function(roll) {
+  this._frame.push(roll);
+};
+
+Frame.prototype.sumRolls = function() {
+  return (this._frame[0] + this._frame[1]);
+}
+
+Frame.prototype.isNew = function() {
+  return (this.getFrame().length === 1);
+};
+
+Frame.prototype.isComplete = function() {
+  return (this.getFrame().length === 2);
+};
+
+Frame.prototype.isStrike = function() {
+  return (this._frame[0] === 10);
+};
+
+Frame.prototype.isSpare = function() {
+  if (!this._frame.isStrike()) {
+    return (this._frame.sumRolls() === 10);
+  }
+};
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,9 @@
 function Game() {
-  this._score  = 0;
   this._rolls  = [];
 
-  this._frame  = [];
+  this._score  = 0;
+
+  this._frame  = new Frame();
   this._frames = [];
 
   this._TOTAL_FRAMES = 10;
@@ -21,25 +22,29 @@ Game.prototype.getRolls = function() {
 };
 
 Game.prototype.getFrame = function() {
-  return this._frame;
+  return this._frame.getFrame();
 };
 
 Game.prototype.getFrames = function() {
   return this._frames;
 };
 
+Game.prototype.getLength = function() {
+  return this.getFrames().length;
+}
+
 // Object actions
 
 Game.prototype.roll = function(pins) {
   this._validateRoll(pins);
 
-  this._addToFrame(pins);
+  this._frame.addRoll(pins);
   this._addToRolls(pins);
 
   this._setScore(pins);
 
-  if (this._checkStrike(this._frame)) { this._addToFrame(0); }
-  if (this.getFrame().length === 2)   { this._closeFrame(this.getFrame()); }
+  if (this._frame.isStrike()) { this._frame.addRoll(0); }
+  if (this._frame.isComplete()) { this._closeFrame(this.getFrame()); }
 
   try {
     this._checkGameOver();
@@ -59,17 +64,13 @@ Game.prototype._setScore = function(points) {
   this._score += points;
 };
 
-Game.prototype._addToFrame = function(roll) {
-  this._frame.push(roll);
-};
-
 Game.prototype._addToRolls = function(roll) {
   this._rolls.push(roll);
 };
 
 Game.prototype._closeFrame = function(frame) {
   this._frames.push(frame);
-  this._frame = [];
+  this._frame = new Frame();
 };
 
 // Internal actions
@@ -94,10 +95,10 @@ Game.prototype._scoreStrike = function(points) {
   if (this.getRolls().length === 1 && this.getRolls().slice(-1)[0] === 10) {
     return 'undefined';
   }
-  if (this.getFrames().length < 10 && this.getRolls().length > 1 && this.getRolls().slice(-2)[0] === 10) {
+  if (this.getLength() < 10 && this.getRolls().length > 1 && this.getRolls().slice(-2)[0] === 10) {
     this._score += points;
   }
-  if (this.getFrames().length < 11 && this.getRolls().length > 2 && this.getRolls().slice(-3)[0] === 10) {
+  if (this.getLength() < 11 && this.getRolls().length > 2 && this.getRolls().slice(-3)[0] === 10) {
     this._score += points;
   }
 }
@@ -107,7 +108,7 @@ Game.prototype._sumFrame = function(frame) {
 };
 
 Game.prototype._checkNewFrame = function() {
-  return (this.getFrame().length === 1);
+  return (this._frame.isNew());
 };
 
 Game.prototype._getLastFrame = function() {
@@ -124,13 +125,13 @@ Game.prototype._validateRoll = function(roll) {
 };
 
 Game.prototype._checkGameOver = function() {
-  if (this._frames.length >= 10 && !this._checkBonusFrames()) {
+  if (this.getLength() >= 10 && !this._checkBonusFrames()) {
     throw Error('Game over!');
   }
-  if (this._frames.length >= 11 && this._checkSpare(this._getLastFrame())) {
+  if (this.getLength() >= 11 && this._checkSpare(this._getLastFrame())) {
     throw Error('Game over!');
   }
-  if (this._frames.length >= 12 && this._checkStrike(this._getLastFrame())) {
+  if (this.getLength() >= 12 && this._checkStrike(this._getLastFrame())) {
     throw Error('Game over!');
   }
 };

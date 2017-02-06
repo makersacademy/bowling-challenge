7c5b5e086700c4c297e532237b20db93b1b49db0
Kat Hicks <kmhicks92@gmail.com>
2017-02-06 20:05:30 +0000

    Extracted score object

--- src/Game.js
+++ src/Game.js
@@ -1,20 +1,15 @@
 function Game() {
-  this._rolls  = [];
-
-  this._score  = 0;
-
-  this._frame  = new Frame();
+  this._score  = new Score();
   this._frames = [];
+
+  this._rolls  = [];
+  this._frame  = new Frame();
 
   this._TOTAL_FRAMES = 10;
 }
 
-// ------------ PUBLIC ------------
-
-// Accessing properties
-
 Game.prototype.getScore = function() {
-  return this._score;
+  return this._score._total;
 };
 
 Game.prototype.getRolls = function() {
@@ -37,60 +32,29 @@ Game.prototype.lastFrame = function() {
   return this.getFrames().slice(-1)[0]
 };
 
-// Object actions
-
-Game.prototype.roll = function(pins) {
-  this._frame.isWrong(pins);
-
-  this._frame.addRoll(pins);
-  this._addToRolls(pins);
-
-  this._setScore(pins);
-
-  if (this._frame.isStrike()) { this._frame.addRoll(0); }
-  if (this._frame.isComplete()) { this._close(this._frame); }
-  this.isOver();
-};
-
-// ----------- PRIVATE ------------
-
-// Altering properties
-
-Game.prototype._setScore = function(points) {
-  this._scoreStrike(points);
-  this._scoreSpare(points);
-  this._score += points;
-};
-
-Game.prototype._addToRolls = function(roll) {
-  this._rolls.push(roll);
-};
-
-Game.prototype._close = function(frame) {
+Game.prototype.closeFrame = function(frame) {
   this._frames.push(frame);
   this._frame = new Frame();
 };
 
-// Internal actions
+Game.prototype.roll = function(pins) {
+  this._frame.isWrong(pins);
+  this._frame.addRoll(pins);
+  this._score.setScore(pins, this.lastFrame(), this.getFrame(), this.getLength());
 
-Game.prototype._scoreSpare = function(points) {
-  if (typeof this.lastFrame() !== 'undefined') {
-    if (this.lastFrame().isSpare() && this._frame.isNew()) {
-      this._score += points;
-    }
-  }
+  if (this._frame.isStrike()) { this._frame.addRoll(0); }
+  if (this._frame.isComplete()) { this.closeFrame(this._frame); }
+
+  this.isOver();
+};
+
+Game.prototype.isOver = function() {
+  try { this.isComplete(); }
+  catch(err) { var errorMessage = err; }
 }
 
-Game.prototype._scoreStrike = function(points) {
-  if (this.getRolls().length === 1 && this.getRolls().slice(-1)[0] === 10) {
-    return 'undefined';
-  }
-  if (this.getLength() < 10 && this.getRolls().length > 1 && this.getRolls().slice(-2)[0] === 10) {
-    this._score += points;
-  }
-  if (this.getLength() < 11 && this.getRolls().length > 2 && this.getRolls().slice(-3)[0] === 10) {
-    this._score += points;
-  }
+Game.prototype.extraFrame = function() {
+  return (this.lastFrame().isStrike() || this.lastFrame().isSpare());
 }
 
 Game.prototype.isComplete = function() {
@@ -104,12 +68,3 @@ Game.prototype.isComplete = function() {
     throw Error('Game over!');
   }
 };
-
-Game.prototype.isOver = function() {
-  try { this.isComplete(); }
-  catch(err) { var errorMessage = err; }
-}
-
-Game.prototype.extraFrame = function() {
-  return (this.lastFrame().isStrike() || this.lastFrame().isSpare());
-}

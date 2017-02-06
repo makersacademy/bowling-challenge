03f700bc86d198d5a832a9dd6c313e73c43d95f7
Justinio14 <jbutwell@hotmail.com>
2017-02-06 22:56:37 +0000

    WIP scoring system modification

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,11 +4,12 @@ describe('Game', function() {
 
   var game;
   var bowl;
-  var lastFrame;
+  var bonus;
 
   beforeEach(function() {
     game = new Game();
     bowl = jasmine.createSpyObj('bowl', ['._currentGo']);
+    bonus = jasmine.createSpyObj('bonus', ['._strike', '._spare']);
   });
 
   it('a player can roll two balls for their turn', function() {
@@ -17,7 +18,8 @@ describe('Game', function() {
 
       it('player gets a strike', function() {
       game.bowl(10,0);
-      expect(game.strike()).toEqual(game.PINS);
+      game.playerScore();
+      expect(game._strike).toBe(true);
       });
 
   it('keeps a record of the score', function() {
@@ -26,13 +28,20 @@ describe('Game', function() {
 
     it('player gets a spare', function() {
       game.bowl(8,2);
-      expect(game.spare()).toEqual(game.PINS);
+      game.playerScore();
+      expect(game._spare).toBe(true);
     });
 
     it('player fails to knock over all pins', function() {
       game.bowl(8,1);
       expect(game.open()).not.toEqual(game.PINS);
     });
+
+    // it('calculates to bonus for a strike', function() {
+    //   game.bowl(10,0);
+    //   expect(game.strike()).toBe(true);
+    //   expect(game.bonus()).not.toBeNull();
+    // });
 
     // it('calls final frame function when 9 games have been playes', function() {
     //   game._frame.length = 8;
--- src/Game.js
+++ src/Game.js
@@ -4,46 +4,83 @@ function Game() {
   this._frame = [];
   this._currentGo = [];
   this._currentScore = 0;
+  this._nextGo = [];
+  this._nextScore = 0;
   this.frameScore = 0;
   this.PINS = 10;
   this.BONUS = 10;
+  this._strike = false;
+  this._spare = false;
 
 }
-
-  Game.prototype.bowl = function(ball1, ball2) {
-    if (this._frame.length < 8) {
-      this._currentGo = [ball1, ball2];
-      this._frame.push([ball1, ball2]);
-    } else {
-      console.log('last frame');
+  Game.prototype.frameNumber = function () {
+    if (this._frame.length > 8) {
       this.lastFrame();
     };
   };
 
-  Game.prototype.strike = function() {
-    if (this._currentGo[0] == 10) {
-      return this.BONUS;
-    };
+  Game.prototype.bowl = function(ball1, ball2) {
+      this.frameNumber();
+
+      this._currentGo = [ball1, ball2];
+      this._frame.push([ball1, ball2]);
+      // this._frame.push([ball1, ball2]);
   };
 
-  Game.prototype.spare = function() {
-    if (this._currentGo[0] + this._currentGo[1] == 10) {
-      return this.BONUS;
-    };
+  Game.prototype.currentScore = function() {
+    this._currentScore = this._currentGo.reduce((a,b) => a + b, 0);
   };
+
+  Game.prototype.strike = function() {
+    this.currentScore();
+        if (this._currentGo[0] == 10){
+           console.log((this.BONUS * 2) + this._currentScore);
+           console.log(this._currentScore);
+           this._spare = false;
+        } else if (this._currentScore == 10) {
+          console.log(this.BONUS + this._currentScore);
+          this._strike = false;
+          this._spare = true;
+        }
+      };
+
+  Game.prototype.spare = function() {
+    this.currentScore();
+      if (this._nextGo[0] == 10){
+            console.log(this.BONUS + this._currentScore);
+            console.log(this._currentScore);
+            this._spare = false;
+            this._strike = true;
+          } else if (this._currentScore == 10) {
+             console.log(this.BONUS + this._currentGo[0]);
+             this._strike = false;
+           } else {
+             console.log(this.BONUS + this._currentGo);
+               this._spare = false
+           }
+         };
 
   Game.prototype.open = function() {
     console.log(this._currentScore);
     return this._currentScore;
   };
 
-  Game.prototype.playerScore = function() {
-    this._currentScore = this._currentGo.reduce((a,b,c) => a + b, 0);
-    this.frameScore += this._currentScore;
-    console.log(this.frameScore);
-  };
-
   Game.prototype.lastFrame = function(ball1, ball2, ball3) {
     this._currentGo = [ball1, ball2, ball3];
     this._frame.push([ball1, ball2, ball3]);
   };
+
+  Game.prototype.playerScore = function() {
+          this.currentScore();
+      if (this._currentGo[0] == 10) {
+          this._strike = true;
+          this.strike();
+      } else if (this._currentScore == 10) {
+          this._spare = true;
+          this.spare();
+      } else {
+        this.open();
+      }
+      this._currentGo = [];
+      this._currentScore = 0;
+    };

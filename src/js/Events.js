c3e7469f27c69b64effaad824fefe02d1239d89e
Kat Hicks <kmhicks92@gmail.com>
2017-02-08 23:15:35 +0000

    Increased font size of scores and made for score to update dynamically

--- src/js/Events.js
+++ src/js/Events.js
@@ -2,8 +2,17 @@ $( document ).ready(function() {
   var game = new Game();
 
   $('table').on('change','select', function() {
-    game.roll(this.value.to_i);
-    $("#round-1").text("Score");
+    game.roll(parseInt(this.value));
+    $("#round-1").text(game._score._scoreboard[0]);
+    $("#round-2").text(game._score._scoreboard[1]);
+    $("#round-3").text(game._score._scoreboard[2]);
+    $("#round-4").text(game._score._scoreboard[3]);
+    $("#round-5").text(game._score._scoreboard[4]);
+    $("#round-6").text(game._score._scoreboard[5]);
+    $("#round-7").text(game._score._scoreboard[6]);
+    $("#round-8").text(game._score._scoreboard[7]);
+    $("#round-9").text(game._score._scoreboard[8]);
+    $("#round-10").text(game._score._scoreboard[9]);
   });
 
 });

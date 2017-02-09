74bd8c5d89d51e7bc0794115d54e0741926d9632
Justinio14 <jbutwell@hotmail.com>
2017-02-09 09:26:07 +0000

    scoring logic completed - modification required when bonus is followed by an open score

--- src/Game.js
+++ src/Game.js
@@ -5,8 +5,6 @@ function Game() {
   this._currentGo = [];
   this._currentScore = 0;
   this._bonusAward = false;
-  this._strikeBonus = false;
-  this._spareBonus = false;
   this.frameScore = [];
   this.PINS = 10;
   this.BONUS = 10;
@@ -25,17 +23,8 @@ function Game() {
       this.frameNumber();
       this._currentGo = [ball1, ball2];
       this._frame.push([ball1, ball2]);
+      this.currentScore();
       this.playerScore();
-      console.log(this._strike);
-      console.log(this._spare);
-      // this.frameScore.push(this._tally += this.score);
-      this.bonusAdjust();
-      // this.bonusApply();
-      console.log(this._strike);
-      console.log(this._spare);
-      // this.bonusAdjust();
-      // this.frameScore.shift(this.score);
-
   };
 
   Game.prototype.currentScore = function() {
@@ -51,7 +40,6 @@ function Game() {
   };
 
   Game.prototype.strike = function() {
-    this.currentScore();
         if (this._currentGo[0] == 10){
            this.score = this.BONUS * 2 + this._currentScore;
         } else {
@@ -61,14 +49,12 @@ function Game() {
       };
 
   Game.prototype.spare = function() {
-              this.currentScore();
               this.score = this.BONUS + this._currentGo[0];
               this.accumulator();
         };
 
   Game.prototype.open = function() {
     this.score = this._currentScore;
-    this.accumulator();
     this._strike = false;
     this._spare = false;
   };
@@ -80,9 +66,11 @@ function Game() {
 
   Game.prototype.bonusApply = function() {
     if (this._currentGo[0] == 10) {
+      console.log(this._currentGo[0]);
       this._strike = true;
       this._spare = false;
     } else if (this._currentScore == 10) {
+      console.log(this._currentScore);
       this._spare = true;
       this._strike = false;
     } else {
@@ -95,24 +83,22 @@ function Game() {
  Game.prototype.bonusAdjust = function() {
    if (this._currentGo[0] == 10 && this._strike == false) {
     this.bonusApply();
-  // } else if (this._currentGo[0] == 10 && this._spare == false) {
-  //   this.bonusApply();
+  } else if (this._currentScore == 10 && this._currentGo[0] != 10 && this._spare == false) {
+     this.bonusApply();
   }
   else {
     this.bonusApply();
-    console.log(this._currentScore);
     this.frameScore.push(this._tally += this.score);
-  }
+  };
  };
 
   Game.prototype.playerScore = function() {
-          this.currentScore();
         if (this._strike == true && this._spare != true) {
           this.strike();
       } else if (this._spare == true && this._strike != true) {
           this.spare();
       } else if (this._currentScore < 10 && this._spare == false && this._strike == false) {
          this.open();
-      }
-
+      };
+      this.bonusAdjust();
     };

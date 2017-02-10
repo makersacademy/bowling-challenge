c4222dfa50ee025ef428f62ba23c8fa54e5e2dde
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-02-10 17:01:14 +0000

    Scores saved in frame

--- jasmine-standalone-2.5.2/spec/bowlingSpec.js
+++ jasmine-standalone-2.5.2/spec/bowlingSpec.js
@@ -1,11 +1,22 @@
 describe("First bowl", function(){
 
+
+  var scorecard;
+
   beforeEach(function(){
-    scorecard = new scorecard();
+    scorecard = new Scorecard();
   });
 
-  it("can select a number of pins for the first bowl", function(){
-    expect(scorecard.frame(5)).toEqual(5);
+  it("can store a number of pins for the first bowl", function(){
+    scorecard.bowlOne(5);
+    expect(scorecard.firstBowl).toEqual(5);
+  });
+
+  it("saves the result of a frame", function(){
+    scorecard.bowlOne(3);
+    scorecard.bowlTwo(4);
+    scorecard.finishFrame();
+    expect(scorecard.currentFrame).toEqual( [ [3, 4] ] );
   });
 
 });
--- jasmine-standalone-2.5.2/src/Bowling.js
+++ jasmine-standalone-2.5.2/src/Bowling.js
@@ -1,7 +1,18 @@
-function scorecard() {
-
+function Scorecard() {
+  this.playerScores = [];
+  this.currentFrame = [];
+  this.firstBowl = 0;
+  this.secondBowl = 0;
 };
 
-scorecard.prototype.frame = function(number){
-  return(5);
+Scorecard.prototype.bowlOne = function(number){
+  this.firstBowl = number;
+};
+
+Scorecard.prototype.bowlTwo = function(number){
+  this.secondBowl = number;
+};
+
+Scorecard.prototype.finishFrame = function(){
+  this.currentFrame.push( [(this.firstBowl), (this.secondBowl)] );
 };

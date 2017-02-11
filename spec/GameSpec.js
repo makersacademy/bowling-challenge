afbe50645a572e5b936e45273cbf165792e430a4
Matthew Perry <thesedatedprince@gmail.com>
2017-02-11 17:34:24 +0000

    finished basic scoring system

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,8 +10,53 @@ describe("Game", function(){
   it("should track the framescore", function(){
     spyOn(player, 'pinsKnocked').and.returnValue(3);
     player.throwBall();
+    expect(game._frameScore).toEqual(3);
+  });
+
+  it("should update the roll tracker on each throw", function(){
     player.throwBall();
-    expect(game._frameScore).toEqual(6);
+    expect(game._rollTracker).toEqual(2);
+  });
+
+  it("should update the frame number after 2 normal throws", function(){
+    spyOn(player, 'pinsKnocked').and.returnValue(3);
+    player.throwBall();
+    player.throwBall();
+    expect(game._frame).toEqual(2);
+  });
+
+  it("should update the frame to frame 3 after 4 normal throws", function(){
+    spyOn(player, 'pinsKnocked').and.returnValue(3);
+    player.throwBall();
+    player.throwBall();
+    player.throwBall();
+    player.throwBall();
+    expect(game._frame).toEqual(3);
+  });
+
+  it("should reset the roll tracker when frame changes", function(){
+    spyOn(player, 'pinsKnocked').and.returnValue(3);
+    player.throwBall();
+    player.throwBall();
+    expect(game._rollTracker).toEqual(1);
+  });
+
+  it("should update the total score at the end of each frame", function(){
+    spyOn(player, 'pinsKnocked').and.returnValue(3);
+    player.throwBall();
+    player.throwBall();
+    expect(game._totalScore).toEqual(6);
+  });
+
+  it("should NOT update the total score until the frame is over", function(){
+    player.throwBall();
+    expect(game._totalScore).toEqual(0);
+  });
+
+  it("should reset the frame score after each frame", function(){
+    player.throwBall();
+    player.throwBall();
+    expect(game._frameScore).toEqual(0);
   })
 
 })
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,29 @@
 function Game(){
   this._frameScore = 0;
+  this._rollTracker = 1;
+  this._frame = 1;
+  this._totalScore = 0;
 }
 
 Game.prototype.updateFrameScore = function(pins){
   this._frameScore += pins;
+  this.updateRollTracker();
+}
+
+Game.prototype.updateRollTracker = function(){
+  this._rollTracker += 1;
+  this.updateFrame();
+}
+
+Game.prototype.updateFrame = function(){
+  if (this._rollTracker > 2){
+    this.updateTotalScore();
+    this._frameScore = 0
+    this._frame += 1;
+    this._rollTracker = 1;
+  }
+}
+
+Game.prototype.updateTotalScore = function(){
+  this._totalScore += this._frameScore;
 }

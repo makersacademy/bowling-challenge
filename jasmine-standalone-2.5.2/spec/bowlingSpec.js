fd9f5322090ba3c1269d76f70d3879dcabe2b5de
Gurminder Bhachu <gurminderbhachu@hotmail.com>
2017-02-11 17:08:06 +0000

    Function to alternate bowls added

--- jasmine-standalone-2.5.2/spec/bowlingSpec.js
+++ jasmine-standalone-2.5.2/spec/bowlingSpec.js
@@ -1,108 +1,96 @@
-describe("Gameplay", function() {
+describe("Gameplay", function(){
 
-    var scorecard;
+  var scorecard;
 
-    beforeEach(function() {
-        scorecard = new Scorecard();
-    });
+  beforeEach(function(){
+    scorecard = new Scorecard();
+  });
 
-    it("can store a number of pins for the first bowl", function() {
-        scorecard.bowlOne(5);
-        expect(scorecard.firstBowl).toEqual(5);
-    });
+  it("can store a number of pins for the first bowl", function(){
+    scorecard.bowlOne(5);
+    expect(scorecard.firstBowl).toEqual(5);
+  });
 
-    it("saves the result of a frame", function() {
-        scorecard.bowlOne(3);
-        scorecard.bowlTwo(4);
-        expect(scorecard.lastFrame).toEqual([3, 4]);
-    });
+  it("saves the result of a frame", function(){
+    scorecard.bowlOne(3);
+    scorecard.bowlTwo(4);
+    expect(scorecard.lastFrame).toEqual( [3, 4] );
+  });
 
-    it("calculates a total for the frame", function() {
-        scorecard.bowlOne(3);
-        scorecard.bowlTwo(4);
-        expect(scorecard.playerScores).toEqual([7]);
-    });
+  it("calculates a total for the frame", function(){
+    scorecard.bowlOne(3);
+    scorecard.bowlTwo(4);
+    expect(scorecard.playerScores).toEqual([7]);
+  });
 
-    it("saves multiple frames and an accumalative total score", function() {
-        scorecard.bowlOne(3);
-        scorecard.bowlTwo(4);
-        scorecard.bowlOne(5);
-        scorecard.bowlTwo(1);
-        expect(scorecard.allFrames).toEqual([
-            [3, 4],
-            [5, 1]
-        ]);
-        expect(scorecard.playerScores).toEqual([7, 13]);
-    });
+  it("saves multiple frames and an accumalative total score", function(){
+    scorecard.bowlOne(3);
+    scorecard.bowlTwo(4);
+    scorecard.bowlOne(5);
+    scorecard.bowlTwo(1);
+    expect(scorecard.allFrames).toEqual( [ [3,4] , [5,1] ] );
+    expect(scorecard.playerScores).toEqual( [7, 13] );
+  });
 
-    it("registers a strike if all pins are knocked over", function() {
-        scorecard.bowlOne(10);
-        expect(scorecard.lastFrame).toEqual(['X']);
-    });
+  it("registers a strike if all pins are knocked over", function(){
+    scorecard.bowlOne(10);
+    expect(scorecard.lastFrame).toEqual( ['X'] );
+  });
 
-    it("registers a spare if the frame adds up to 10", function() {
-        scorecard.bowlOne(5);
-        scorecard.bowlTwo(5);
-        expect(scorecard.lastFrame).toEqual([5, '/']);
-    });
+  it("registers a spare if the frame adds up to 10", function(){
+    scorecard.bowlOne(5);
+    scorecard.bowlTwo(5);
+    expect(scorecard.lastFrame).toEqual( [5, '/'] );
+  });
+
+  it("alternates appropriately between bowlOne and bowlTwo", function(){
+    scorecard.bowl(5);
+    scorecard.bowl(3);
+    scorecard.bowl(10);
+    scorecard.bowl(2);
+    scorecard.bowl(4);
+    expect(scorecard.allFrames).toEqual( [ [5, 3], ['X'], [2, 4] ] );
+    expect(scorecard.playerScores).toEqual( [8, 24, 30] );
+  });
 
 });
 
-describe("Strikes and spares", function() {
+describe("Strikes and spares", function(){
 
-    var scorecard;
+  var scorecard;
 
-    beforeEach(function() {
-        scorecard = new Scorecard();
-    });
+  beforeEach(function(){
+    scorecard = new Scorecard();
+  });
 
-    it("adds bonus points from the next two rolls after a strike", function() {
-        scorecard.bowlOne(10);
-        scorecard.bowlOne(5);
-        scorecard.bowlTwo(2);
-        expect(scorecard.allFrames).toEqual([
-            ['X'],
-            [5, 2]
-        ]);
-        expect(scorecard.playerScores).toEqual([17, 24]);
-    });
+  it("adds bonus points from the next two rolls after a strike", function(){
+    scorecard.bowlOne(10);
+    scorecard.bowlOne(5);
+    scorecard.bowlTwo(2);
+    expect(scorecard.allFrames).toEqual( [ ['X'], [5, 2] ] );
+    expect(scorecard.playerScores).toEqual( [17, 24] );
+  });
 
-    it("adds bonus points from the next roll after a spare", function() {
-        scorecard.bowlOne(9);
-        scorecard.bowlTwo(1);
-        scorecard.bowlOne(6);
-        scorecard.bowlTwo(2);
-        expect(scorecard.allFrames).toEqual([
-            [9, '/'],
-            [6, 2]
-        ]);
-        expect(scorecard.playerScores).toEqual([16, 24]);
-    });
+  it("adds bonus points from the next roll after a spare", function(){
+    scorecard.bowlOne(9);
+    scorecard.bowlTwo(1);
+    scorecard.bowlOne(6);
+    scorecard.bowlTwo(2);
+    expect(scorecard.allFrames).toEqual( [ [9, '/'], [6, 2] ] );
+    expect(scorecard.playerScores).toEqual( [16, 24] );
+  });
 
-    it("correctly scores a spare following a strike", function() {
-        scorecard.bowlOne(10);
-        scorecard.bowlOne(7);
-        scorecard.bowlTwo(3);
-        scorecard.bowlOne(9);
-        scorecard.bowlTwo(0);
-        scorecard.bowlOne(10);
-        scorecard.bowlOne(0);
-        scorecard.bowlTwo(8);
-        expect(scorecard.allFrames).toEqual([
-            ['X'],
-            [7, '/'],
-            [9, 0],
-            ['X'],
-            [0, 8]
-        ]);
-        expect(scorecard.playerScores).toEqual([20, 39, 48, 66, 74]);
-    });
-
-    it('I can bowl a perfect game', function() {
-        for (var i = 1; i = 15; i++) {
-            scorecard.bowlOne(10);
-        }
-        expect(scorecard.playerScores).toEqual(300);
-    });
+  it("correctly scores a spare following a strike", function(){
+    scorecard.bowlOne(10);
+    scorecard.bowlOne(7);
+    scorecard.bowlTwo(3);
+    scorecard.bowlOne(9);
+    scorecard.bowlTwo(0);
+    scorecard.bowlOne(10);
+    scorecard.bowlOne(0);
+    scorecard.bowlTwo(8);
+    expect(scorecard.allFrames).toEqual( [ ['X'], [7, '/'], [9, 0], ['X'], [0, 8] ] );
+    expect(scorecard.playerScores).toEqual( [20, 39, 48, 66, 74] );
+  });
 
 });

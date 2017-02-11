79a84477e27c11bdb1f21a08114c014728c3d731
Matthew Perry <thesedatedprince@gmail.com>
2017-02-11 16:07:55 +0000

    Player can now throw a ball

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,58 +1,12 @@
-describe("Player", function() {
-  var player;
-  var song;
+describe("Player", function(){
+    var player;
 
-  beforeEach(function() {
-    player = new Player();
-    song = new Song();
+    beforeEach(function(){
+      player = new Player();
   });
 
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
+    it("should be able to throw a ball", function(){
+      player.throwBall();
+      expect(player._framescore).not.toEqual(0);
   });
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
-    });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-  });
-
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrowError("song is already playing");
-    });
-  });
-});
+})
--- src/Player.js
+++ src/Player.js
@@ -1,22 +1,7 @@
-function Player() {
+function Player(){
+  this._frameScore = 0
 }
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
-  }
-
-  this.isPlaying = true;
-};
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
+Player.prototype.throwBall = function(){
+  this._frameScore += 6
+}

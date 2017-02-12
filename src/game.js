b9d8e14262aef4f53d451f0469d2d47026fc5e2b
Shereen Finney <shereenfinney@gmail.com>
2017-02-12 17:30:53 +0000

    Added final playframe methods to game.js

--- src/game.js
+++ src/game.js
@@ -52,6 +52,42 @@ Game.prototype.playFrame3 = function(roll1, roll2) {
   }
 };
 
+Game.prototype.playFrame4 = function(roll1, roll2) {
+  this._frame4.setRoll1(roll1);
+  this._frame4.setRoll2(roll2);
+  this.getBonus(this._frame3, roll1); //prev frame may get a bonus
+  if (this._frame2.getStrikeType() === "X"){
+    this.getBonus(this._frame2, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame5 = function(roll1, roll2) {
+  this._frame5.setRoll1(roll1);
+  this._frame5.setRoll2(roll2);
+  this.getBonus(this._frame4, roll1); //prev frame may get a bonus
+  if (this._frame3.getStrikeType() === "X"){
+    this.getBonus(this._frame3, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame6 = function(roll1, roll2) {
+  this._frame6.setRoll1(roll1);
+  this._frame6.setRoll2(roll2);
+  this.getBonus(this._frame5, roll1); //prev frame may get a bonus
+  if (this._frame4.getStrikeType() === "X"){
+    this.getBonus(this._frame4, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame7 = function(roll1, roll2) {
+  this._frame7.setRoll1(roll1);
+  this._frame7.setRoll2(roll2);
+  this.getBonus(this._frame6, roll1); //prev frame may get a bonus
+  if (this._frame5.getStrikeType() === "X"){
+    this.getBonus(this._frame5, roll1);//prev prev frame gets a bonus
+  }
+};
+
 Game.prototype.playFrame8 = function(roll1, roll2) {
   this._frame8.setRoll1(roll1);
   this._frame8.setRoll2(roll2);

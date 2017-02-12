e8f4c75d791bcc1693f5dc7cf2a6eaf8d71b7fa3
Enrico Graziani <mrenrich84@gmail.com>
2017-02-12 16:47:43 +0000

    USER STORY 2 As a player So I can play my game I want my scoreboard to be created with 10 frames

--- jasmine/spec/features/bowlingScoreboardSpec.js
+++ jasmine/spec/features/bowlingScoreboardSpec.js
@@ -5,6 +5,7 @@ describe("BowlingScoreboard", function() {
     bowlingScoreboard = new BowlingScoreboard();
   });
 
+  // USER STORY 1
   // As a player
   // So I can play my game
   // I want my scoreboard to be created with 10 frames
@@ -12,4 +13,14 @@ describe("BowlingScoreboard", function() {
     expect(bowlingScoreboard.frames.length).toEqual(10);
   });
 
+
+  // USER STORY 2
+  // As a player
+  // So I can play my game
+  // I want my scoreboard to be created with 10 frames
+  it("has 10 pins on each new frame ", function() {
+    for (i = 0; i < 10; i++) {
+      expect(bowlingScoreboard.frames[i].pins).toEqual(10);
+    };
+  });
 });
--- jasmine/spec/units/framesSpec.js
+++ jasmine/spec/units/framesSpec.js
@@ -9,5 +9,7 @@ describe("Frame", function() {
     expect(function(){new Frame()}).not.toThrow();
   });
 
-
+  it("has 10 pins on new frame ", function() {
+    expect(frame.pins).toEqual(10);
+  });
 });

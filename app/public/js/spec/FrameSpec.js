10df2dda4b98064de9bcceec481969ae2d41808b
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-02-12 15:12:53 +0000

    tests for spare and strike

--- app/public/js/spec/FrameSpec.js
+++ app/public/js/spec/FrameSpec.js
@@ -20,4 +20,42 @@ describe('Frame', function() {
      expect(frame.sumRolls()).toEqual(5);
    });
   });
+
+  describe("Gutter game", function() {
+    it('should return 0', function() {
+      frame.addRolls(0)
+      frame.addRolls(0)
+      expect(frame.sumRolls()).toEqual(0);
+    });
+  });
+
+  describe("Strike", function() {
+
+    it('evaluates to true when first roll score is 10'), function() {
+      frame.addRolls(10);
+      expect(frame.isStrike()).toBe(true);
+    });
+
+    it('should return the correct number of points', function() {
+      frame.addRolls(10);
+      frame.addRolls(3);
+      frame.addRolls(4);
+      expect(frame.sumRolls()).toEqual(24);
+    });
+  });
+
+  describe("Spare", function() {
+    it('evaluates to true when the sume of both rolls is 10'), function() {
+      frame.addRolls(5);
+      frame.addRolls(5);
+      expect(frame.isSpare()).toBe(true);
+    });
+
+    it('should return the correct number of points', function() {
+      frame.addRolls(5);
+      frame.addRolls(5);
+      frame.addRolls(3);
+      expect(frame.sumRolls()).toEqual(16);
+    });
+  });
 });
--- app/public/js/src/Frame.js
+++ app/public/js/src/Frame.js
@@ -1,6 +1,7 @@
 function Frame(){
   this._rolls = [];
   this._total = 0;
+  this._bonus = null;
 };
 
 Frame.prototype.getRolls = function () {
@@ -17,4 +18,16 @@ Frame.prototype.addRolls = function(roll) {
 
 Frame.prototype.sumRolls = function() {
   return this._rolls.reduce(function(score, nextRoll) { return score + nextRoll; }, this._total);
+};
+
+Frame.prototype.totalScore = function() {
+  return this.sumRolls() + this._bonus;
+};
+
+Frame.prototype.isStrike = function() {
+  return this._rolls.length === 1 && this.sumRolls() === 10;
+};
+
+Frame.prototype.isSpare = function() {
+  return this._rolls.length === 2 && this.sumRolls() === 10;
 };

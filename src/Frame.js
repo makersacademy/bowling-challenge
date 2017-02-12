a646a16f2756f95244a31171faf24771425957f6
Varvara Shinkarenko <shinkarenko.varvara@gmail.com>
2017-02-12 23:40:24 +0000

    Strike Bonus counted in total score of the game

--- src/Frame.js
+++ src/Frame.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function Frame(){
-  this._score = 0
-  this._roll_1 = 0
-  this._roll_2 = 0
+  this._score = 0;
+  this._roll_1 = 0;
+  this._roll_2 = 0;
+  this.MAX = 10;
 }
 
 Frame.prototype.calcScore = function () {
@@ -11,14 +12,14 @@ Frame.prototype.calcScore = function () {
 };
 
 Frame.prototype.rollBallOne = function () {
-  return this._roll_1 = getRandomInt(0, 10);
-   if(this._roll_1 === 10) {
-      this._score = 10;
+   if(this.isStrike()) {
+      this._score = this.MAX;
    }
+  return this._roll_1 = getRandomInt(0, 10);
 };
 
 Frame.prototype.rollBallTwo = function () {
-  if (this._roll_1 < 10 ){
+  if (this._roll_1 < this.MAX ){
     var pins_left = 10 - this._roll_1
     this._roll_2 =  getRandomInt(0, pins_left);
   } else { throw new Error('You had a strike - no pins left to knock');
@@ -32,8 +33,10 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Frame.prototype.isStrike = function () {
-//   return this._roll_1 === 10
-//
-//   }
-// };
+Frame.prototype.isStrike = function () {
+  return this._roll_1 === 10
+};
+
+Frame.prototype.isSpare = function () {
+  return this._roll_2 + this._roll_1 === 10
+};
--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,12 @@
 'use strict';
 
-function Game(){
+function Game() {
   this._frames = [];
   this.MAX_FRAMES = 10;
   this._total = 0;
 }
 
-Game.prototype.totalsOfFrames = function () {
+Game.prototype.totalsOfFrames = function() {
   return this._frames;
 };
 
@@ -24,23 +24,51 @@ Game.prototype.addFrame = function(frame){
   }
 };
 
-Game.prototype.calcTotal = function () {
-  var game_played = this;
-  game_played._total = 0;
-  this._frames.forEach(function(frame) {
-    game_played._total += frame.calcScore();
-  });
-  return game_played._total;
-};
-// Game.prototype.strikeBonus = function(index) {
-//   var bonus = this._frames[index+1]._score;
-//   var frames = this._frames;
-//   if (this.isAStrike(index+1)) {
-//     bonus = isAStrike(index+2) ? 20 : 10 + this.frames[index+2].firstBowl;
-//   }
-//   return bonus;
-// }
+Game.prototype.strikeBonus = function(index) {
 
-Game.prototype.isAStrike = function(index){
-  return this._frames[index]._score === 'X';
+  var bonus = this._frames[index+1].calcScore();
+
+  // var frames = this._frames;
+  // if (frame.isStrike() {
+  //   bonus = isStrike(index+2) ? 20 : 10 + frames[index+2].rollBallOne();
+  // }
+  return bonus;
+};
+
+Game.prototype.spareBonus = function(index) {
+   var bonus = this._frames[index+1].rollBallOne();
+   return bonus;
+};
+
+Game.prototype.calcTotal = function() {
+  var game = this;
+
+  var current_score = 0;
+  //console.log(current_score);
+
+  this._total = 0;
+  // debugger;
+  this._frames.forEach(function(frame, index) {
+    current_score = frame.calcScore();
+    if (frame.isStrike()) {
+
+       console.log(current_score);
+      // console.log(game.strikeBonus(index));
+      // // var score = frame.calcScore()
+      debugger;
+      current_score += game.strikeBonus(index);
+      console.log(current_score);
+    }
+  //   else if (frame.isSpare()) {
+  //     // console.log(current_score);
+  //
+  //     current_score += game.spareBonus(index);
+  //     debugger;
+  //      console.log(current_score);
+  //   }
+    game._total += current_score;
+  });
+
+  console.log(game._total)
+  return game._total
 };

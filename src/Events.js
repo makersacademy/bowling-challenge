2b3ea2fd7c7884634b2d412f048bc9b31c11999a
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-02-12 19:25:54 +0000

    completed round jquery

--- src/Events.js
+++ src/Events.js
@@ -39,6 +39,132 @@ $(document).ready(function(){
       $("#round-1").text(game.frameScore(1));
       $("#total-score").text(game.totalScore());
     }
+
+    if ($(this).attr('id') === 'roll-2.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-2").text(roll2_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-2.2') {
+      game.roll(parseInt(roll2_1.value),parseInt(roll2_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-2").text(game.frameScore(2));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-3.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-3").text(roll3_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-3.2') {
+      game.roll(parseInt(roll3_1.value),parseInt(roll3_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-3").text(game.frameScore(3));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-4.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-4").text(roll4_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-4.2') {
+      game.roll(parseInt(roll4_1.value),parseInt(roll4_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-4").text(game.frameScore(4));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-5.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-5").text(roll5_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-5.2') {
+      game.roll(parseInt(roll5_1.value),parseInt(roll5_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-5").text(game.frameScore(5));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-6.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-6").text(roll6_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-6.2') {
+      game.roll(parseInt(roll6_1.value),parseInt(roll6_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-6").text(game.frameScore(6));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-7.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-7").text(roll7_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-7.2') {
+      game.roll(parseInt(roll7_1.value),parseInt(roll7_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-7").text(game.frameScore(7));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-8.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-8").text(roll8_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-8.2') {
+      game.roll(parseInt(roll8_1.value),parseInt(roll8_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-8").text(game.frameScore(8));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-9.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-9").text(roll9_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-9.2') {
+      game.roll(parseInt(roll9_1.value),parseInt(roll9_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-9").text(game.frameScore(9));
+      $("#total-score").text(game.totalScore());
+    }
+
+    if ($(this).attr('id') === 'roll-10.1') {
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-10").text(roll10_1.value);
+    }
+
+    if ($(this).attr('id') === 'roll-10.2') {
+      game.roll(parseInt(roll10_1.value),parseInt(roll10_2.value));
+      $(this).hide();
+      $(this).parent().html(this.value);
+      $("#round-10").text(game.frameScore(10));
+      $("#total-score").text(game.totalScore());
+    }
   });
 
 });

01b2c345bf8c9480334f7b3152e7e4e70e60bf03
Shereen Finney <shereenfinney@gmail.com>
2017-02-17 16:46:43 +0000

    Added remaining playFrame tests, and added the get final bonus test

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -52,6 +52,27 @@ describe('Game', function() {
   it("has a playFrame3 method", function(){
     expect(game.playFrame3).toBeDefined();
   });
+  it("has a playFrame4 method", function(){
+    expect(game.playFrame4).toBeDefined();
+  });
+  it("has a playFrame5 method", function(){
+    expect(game.playFrame5).toBeDefined();
+  });
+  it("has a playFrame6 method", function(){
+    expect(game.playFrame6).toBeDefined();
+  });
+  it("has a playFrame7 method", function(){
+    expect(game.playFrame7).toBeDefined();
+  });
+  it("has a playFrame8 method", function(){
+    expect(game.playFrame8).toBeDefined();
+  });
+  it("has a playFrame9 method", function(){
+    expect(game.playFrame9).toBeDefined();
+  });
+  it("has a getFinalBonus method", function(){
+    expect(game.getFinalBonus).toBeDefined();
+  });
 
   // 3. Detail of each Frame method
 
@@ -103,12 +124,12 @@ describe('Game', function() {
       expect(game._frame2._roll1).toEqual(1);
       expect(game._frame2._roll2).toEqual(2);
     });
-    it ('will add a bonus to the previous frame received a strike (X)', function() {
+    it ('will add a bonus to frame 1 if frame 1 received a strike (X)', function() {
       game.playFrame1(10,0);
       game.playFrame2(1,2);
       expect(game._frame1._frameScore).toEqual(20);
     });
-    it ('will add a bonus to the previous frame received a spare (/)', function() {
+    it ('will add a bonus to frame 1 if frame 1 received a spare (/)', function() {
       game.playFrame1(5,5);
       game.playFrame2(4,5);
       expect(game._frame1._frameScore).toEqual(14);
@@ -126,17 +147,17 @@ describe('Game', function() {
       expect(game._frame3._roll1).toEqual(1);
       expect(game._frame3._roll2).toEqual(2);
     });
-    it ('will add a bonus to the previous frame if it received a strike (X)', function() {
+    it ('will add a bonus to frame 2 if frame 2 received a strike (X)', function() {
       game.playFrame2(10,0);
       game.playFrame3(1,2);
       expect(game._frame2._frameScore).toEqual(20);
     });
-    it ('will add a bonus to the previous frame if it received a spare (/)', function() {
+    it ('will add a bonus to frame 2 if frame 2 received a spare (/)', function() {
       game.playFrame2(5,5);
       game.playFrame3(4,5);
       expect(game._frame2._frameScore).toEqual(14);
     });
-    it ('will add a bonus to the previous previous frame, if it received a strike (X)', function() {
+    it ('will add a bonus to frame 1 if frame 1 received a strike (X)', function() {
       game.playFrame1(10,0);
       game.playFrame2(10,0);
       game.playFrame3(1,2);
@@ -145,13 +166,208 @@ describe('Game', function() {
     });
   });
 
+  describe('#frame4', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
 
+    it ('sets the pins knocked down in the 4th frame', function() {
+      game.playFrame4(1,2);
+      expect(game._frame4._roll1).toEqual(1);
+      expect(game._frame4._roll2).toEqual(2);
+    });
+    it ('will add a bonus to frame 3 if frame 3 received a strike (X)', function() {
+      game.playFrame3(10,0);
+      game.playFrame4(1,2);
+      expect(game._frame3._frameScore).toEqual(20);
+    });
+    it ('will add a bonus to frame 3 if frame 3 received a spare (/)', function() {
+      game.playFrame3(5,5);
+      game.playFrame4(4,5);
+      expect(game._frame3._frameScore).toEqual(14);
+    });
+    it ('will add a bonus to frame 2 if frame 2 received a strike (X)', function() {
+      game.playFrame2(10,0);
+      game.playFrame3(10,0);
+      game.playFrame4(1,2);
+      expect(game._frame3._frameScore).toEqual(20);
+      expect(game._frame2._frameScore).toEqual(30);
+    });
+  });
 
-  // it ('finishes if a strike is made on the 1st roll', function() {
-  //   spyOn(frame1, 'getStrikeType').and.returnValue("strike");
-  //   expect(frame1._roll2).toEqual(0);
-  // });
+  describe('#frame5', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
 
+    it ('sets the pins knocked down in the 5th frame', function() {
+      game.playFrame5(1,2);
+      expect(game._frame5._roll1).toEqual(1);
+      expect(game._frame5._roll2).toEqual(2);
+    });
+    it ('will add a bonus to frame 4 if frame 4 received a strike (X)', function() {
+      game.playFrame4(10,0);
+      game.playFrame5(1,2);
+      expect(game._frame4._frameScore).toEqual(20);
+    });
+    it ('will add a bonus to frame 4 if frame 4 received a spare (/)', function() {
+      game.playFrame4(5,5);
+      game.playFrame5(4,5);
+      expect(game._frame4._frameScore).toEqual(14);
+    });
+    it ('will add a bonus to frame 3 if frame 3 received a strike (X)', function() {
+      game.playFrame3(10,0);
+      game.playFrame4(10,0);
+      game.playFrame5(1,2);
+      expect(game._frame4._frameScore).toEqual(20);
+      expect(game._frame3._frameScore).toEqual(30);
+    });
+  });
+
+  describe('#frame6', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+
+    it ('sets the pins knocked down in the 6th frame', function() {
+      game.playFrame6(1,2);
+      expect(game._frame6._roll1).toEqual(1);
+      expect(game._frame6._roll2).toEqual(2);
+    });
+    it ('will add a bonus to frame 5 if frame 5 received a strike (X)', function() {
+      game.playFrame5(10,0);
+      game.playFrame6(1,2);
+      expect(game._frame5._frameScore).toEqual(20);
+    });
+    it ('will add a bonus to frame 5 if frame 5 received a spare (/)', function() {
+      game.playFrame5(5,5);
+      game.playFrame6(4,5);
+      expect(game._frame5._frameScore).toEqual(14);
+    });
+    it ('will add a bonus to frame 4 if frame 4 received a strike (X)', function() {
+      game.playFrame4(10,0);
+      game.playFrame5(10,0);
+      game.playFrame6(1,2);
+      expect(game._frame5._frameScore).toEqual(20);
+      expect(game._frame4._frameScore).toEqual(30);
+    });
+  });
+
+  describe('#frame7', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+
+    it ('sets the pins knocked down in the 7th frame', function() {
+      game.playFrame7(1,2);
+      expect(game._frame7._roll1).toEqual(1);
+      expect(game._frame7._roll2).toEqual(2);
+    });
+    it ('will add a bonus to frame 6 if frame 6 received a strike (X)', function() {
+      game.playFrame6(10,0);
+      game.playFrame7(1,2);
+      expect(game._frame6._frameScore).toEqual(20);
+    });
+    it ('will add a bonus to frame 6 if frame 6 received a spare (/)', function() {
+      game.playFrame6(5,5);
+      game.playFrame7(4,5);
+      expect(game._frame6._frameScore).toEqual(14);
+    });
+    it ('will add a bonus to frame 5 if frame 5 received a strike (X)', function() {
+      game.playFrame5(10,0);
+      game.playFrame6(10,0);
+      game.playFrame7(1,2);
+      expect(game._frame6._frameScore).toEqual(20);
+      expect(game._frame5._frameScore).toEqual(30);
+    });
+  });
+
+  describe('#frame8', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+
+    it ('sets the pins knocked down in the 8th frame', function() {
+      game.playFrame8(1,2);
+      expect(game._frame8._roll1).toEqual(1);
+      expect(game._frame8._roll2).toEqual(2);
+    });
+    it ('will add a bonus to frame 7 if frame 7 received a strike (X)', function() {
+      game.playFrame7(10,0);
+      game.playFrame8(1,2);
+      expect(game._frame7._frameScore).toEqual(20);
+    });
+    it ('will add a bonus to frame 7 if frame 7 received a spare (/)', function() {
+      game.playFrame7(5,5);
+      game.playFrame8(4,5);
+      expect(game._frame7._frameScore).toEqual(14);
+    });
+    it ('will add a bonus to frame 6 if frame 6 received a strike (X)', function() {
+      game.playFrame6(10,0);
+      game.playFrame7(10,0);
+      game.playFrame8(1,2);
+      expect(game._frame7._frameScore).toEqual(20);
+      expect(game._frame6._frameScore).toEqual(30);
+    });
+  });
+
+  describe('#frame9', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+
+    it ('sets the pins knocked down in the 9th frame', function() {
+      game.playFrame9(1,2);
+      expect(game._frame9._roll1).toEqual(1);
+      expect(game._frame9._roll2).toEqual(2);
+    });
+    it ('will add a bonus to frame 8 if frame 8 received a strike (X)', function() {
+      game.playFrame8(10,0);
+      game.playFrame9(1,2);
+      expect(game._frame8._frameScore).toEqual(20);
+    });
+    it ('will add a bonus to frame 8 if frame 8 received a spare (/)', function() {
+      game.playFrame8(5,5);
+      game.playFrame9(4,5);
+      expect(game._frame8._frameScore).toEqual(14);
+    });
+    it ('will add a bonus to frame 7 if frame 7 received a strike (X)', function() {
+      game.playFrame7(10,0);
+      game.playFrame8(10,0);
+      game.playFrame9(1,2);
+      expect(game._frame8._frameScore).toEqual(20);
+      expect(game._frame7._frameScore).toEqual(30);
+    });
+  });
+
+  describe('#getFinalBonus', function() {
+    var game;
+    beforeEach(function() {
+      game = new Game();
+    });
+    it ('adds a bonus to the previous frame if the previous frame had a strike (X)', function() {
+      game._frame1.setRoll1(10);
+      game._frame1.setRoll2(0);
+      game.getFinalBonus(game._frame1, 10, 0)
+      expect(game._frame1._strikeType).toEqual("X");
+      expect(game._frame1._frameScore).toEqual(20);
+    });
+    it ('adds a bonus to the previous frame if the previous frame had a spare (X)', function() {
+      game._frame1.setRoll1(5);
+      game._frame1.setRoll2(5);
+      game._frame2.setRoll1(5);
+      game._frame2.setRoll2(5);
+      game.getFinalBonus(game._frame1, 5, 5)
+      expect(game._frame1._strikeType).toEqual("/");
+      expect(game._frame1._frameScore).toEqual(15);
+    });
+  });
 
 
 
--- src/game.js
+++ src/game.js
@@ -41,61 +41,72 @@ Game.prototype.playFrame3 = function(roll1, roll2) {
     this.getBonus(this._frame1, roll1);//prev prev frame gets a bonus
   }
 };
-//
-// Game.prototype.playFrame4 = function(roll1, roll2) {
-//   this._frame4.setRoll1(roll1);
-//   this._frame4.setRoll2(roll2);
-//   this.getBonus(this._frame3, roll1); //prev frame may get a bonus
-//   if (this._frame2.getStrikeType() === "X"){
-//     this.getBonus(this._frame2, roll1);//prev prev frame gets a bonus
-//   }
-// };
-//
-// Game.prototype.playFrame5 = function(roll1, roll2) {
-//   this._frame5.setRoll1(roll1);
-//   this._frame5.setRoll2(roll2);
-//   this.getBonus(this._frame4, roll1); //prev frame may get a bonus
-//   if (this._frame3.getStrikeType() === "X"){
-//     this.getBonus(this._frame3, roll1);//prev prev frame gets a bonus
-//   }
-// };
-//
-// Game.prototype.playFrame6 = function(roll1, roll2) {
-//   this._frame6.setRoll1(roll1);
-//   this._frame6.setRoll2(roll2);
-//   this.getBonus(this._frame5, roll1); //prev frame may get a bonus
-//   if (this._frame4.getStrikeType() === "X"){
-//     this.getBonus(this._frame4, roll1);//prev prev frame gets a bonus
-//   }
-// };
-//
-// Game.prototype.playFrame7 = function(roll1, roll2) {
-//   this._frame7.setRoll1(roll1);
-//   this._frame7.setRoll2(roll2);
-//   this.getBonus(this._frame6, roll1); //prev frame may get a bonus
-//   if (this._frame5.getStrikeType() === "X"){
-//     this.getBonus(this._frame5, roll1);//prev prev frame gets a bonus
-//   }
-// };
-//
-// Game.prototype.playFrame8 = function(roll1, roll2) {
-//   this._frame8.setRoll1(roll1);
-//   this._frame8.setRoll2(roll2);
-//   this.getBonus(this._frame7, roll1); //prev frame may get a bonus
-//   if (this._frame6.getStrikeType() === "X"){
-//     this.getBonus(this._frame6, roll1);//prev prev frame gets a bonus
-//   }
-// };
-//
-// Game.prototype.playFrame9 = function(roll1, roll2) {
-//   this._frame9.setRoll1(roll1);
-//   this._frame9.setRoll2(roll2);
-//   this.getBonus(this._frame8, roll1); //prev frame may get a bonus
-//   if (this._frame7.getStrikeType() === "X"){
-//     this.getBonus(this._frame7, roll1);//prev prev frame gets a bonus
-//   }
-// };
-//
+
+Game.prototype.playFrame4 = function(roll1, roll2) {
+  this._frame4.setRoll1(roll1);
+  this._frame4.setRoll2(roll2);
+  this.getBonus(this._frame3, roll1); //prev frame may get a bonus
+  if (this._frame2.getStrikeType() === "X"){
+    this.getBonus(this._frame2, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame5 = function(roll1, roll2) {
+  this._frame5.setRoll1(roll1);
+  this._frame5.setRoll2(roll2);
+  this.getBonus(this._frame4, roll1); //prev frame may get a bonus
+  if (this._frame3.getStrikeType() === "X"){
+    this.getBonus(this._frame3, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame6 = function(roll1, roll2) {
+  this._frame6.setRoll1(roll1);
+  this._frame6.setRoll2(roll2);
+  this.getBonus(this._frame5, roll1); //prev frame may get a bonus
+  if (this._frame4.getStrikeType() === "X"){
+    this.getBonus(this._frame4, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame7 = function(roll1, roll2) {
+  this._frame7.setRoll1(roll1);
+  this._frame7.setRoll2(roll2);
+  this.getBonus(this._frame6, roll1); //prev frame may get a bonus
+  if (this._frame5.getStrikeType() === "X"){
+    this.getBonus(this._frame5, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame8 = function(roll1, roll2) {
+  this._frame8.setRoll1(roll1);
+  this._frame8.setRoll2(roll2);
+  this.getBonus(this._frame7, roll1); //prev frame may get a bonus
+  if (this._frame6.getStrikeType() === "X"){
+    this.getBonus(this._frame6, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.playFrame9 = function(roll1, roll2) {
+  this._frame9.setRoll1(roll1);
+  this._frame9.setRoll2(roll2);
+  this.getBonus(this._frame8, roll1); //prev frame may get a bonus
+  if (this._frame7.getStrikeType() === "X"){
+    this.getBonus(this._frame7, roll1);//prev prev frame gets a bonus
+  }
+};
+
+Game.prototype.getFinalBonus = function(previousFrame, roll1, roll2) {
+  var rolls = roll1 + roll2;
+  if (previousFrame.getStrikeType() === "X") {
+    previousFrame.addBonus(rolls);
+  }
+  else if (previousFrame.getStrikeType() === "/") {
+    previousFrame.addBonus(roll1);
+  } else {
+  }
+};
+
 // Game.prototype.playFrame10 = function(roll1, roll2) {
 //   this._frame10.setRoll1(roll1);
 //   this._frame10.setRoll2(roll2);
@@ -136,15 +147,3 @@ Game.prototype.playFrame3 = function(roll1, roll2) {
 //   return this._gameTotal;
 // };
 //
-
-//
-// Game.prototype.getFinalBonus = function(previousFrame, roll1, roll2) {
-//   var rolls = roll1 + roll2;
-//   if (previousFrame.getStrikeType() === "X") {
-//     previousFrame.addBonus(rolls);
-//   }
-//   else if (previousFrame.getStrikeType() === "/") {
-//     previousFrame.addBonus(roll1);
-//   } else {
-//   }
-// };

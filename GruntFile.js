c7e3cb9b43eb33def62b16d6345149af2f949619
Rob Brentnall <rob.brentnall@gmail.com>
2017-03-11 14:21:00 +0000

    added coverage

--- GruntFile.js
+++ GruntFile.js
@@ -1,12 +1,25 @@
 module.exports = function (grunt) {
   grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
-      jasmine: {
-        src: ['src/*.js'],
-        options: {
-          specs: ['spec/*Spec.js'],
-          vendor: []
-        }
+      jasmine : {
+          coverage : {
+              src : [
+              'src/*.js' ],
+              options : {
+                  specs: ['spec/*Spec.js'],
+                  template : require('grunt-template-jasmine-istanbul'),
+                  templateOptions : {
+                      coverage : 'coverage/coverage.json',
+                      report : 'coverage',
+                      thresholds : {
+                          lines : 5,
+                          statements : 5,
+                          branches : 1,
+                          functions : 1
+                      }
+                  }
+              }
+          }
       }
     });
   grunt.loadNpmTasks('grunt-contrib-jasmine');

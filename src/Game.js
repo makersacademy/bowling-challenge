5302bfd21c1eead0d505e37ad09dc32e0ceb8efb
Ashwini <ashie.oh@gmail.com>
2017-03-18 21:55:14 +0000

    Changes last frame to have 3 rolls

--- src/Game.js
+++ src/Game.js
@@ -4,7 +4,7 @@
     this.frames = [new Frame(), new Frame(), new Frame(),
                   new Frame(), new Frame(),
                   new Frame(), new Frame(), new Frame(),
-                  new Frame(), new Frame()];
+                  new Frame(), new Frame(3)];
   }
 
   Game.prototype.getFrames = function(){
@@ -16,25 +16,27 @@
     var bonus = 0;
 
     for (var i = 0; i < 10; i++){
-      console.log(this.frames[i])
+      //console.log(i);
+      var thisFrame = this.frames[i];
+      var nextFrame = this.frames[i+1] || thisFrame;
 
-      if(this.frames[i].isDone()){
+      if(thisFrame.isDone()){
         var frameScore = this.frames[i].calculateFrameScore();
-        totalScore += frameScore
-
-        if (this.frames[i].hasStrike()){
-          bonus = this.frames[i+1].calculateFrameScore();
+        totalScore += frameScore;
+        if (thisFrame.hasStrike()){
+          bonus = (nextFrame === thisFrame? thisFrame.getRolls()[2].getPinsKnocked() : nextFrame.calculateFrameScore());
           totalScore += bonus;
+          //console.log(bonus);
         }
 
-        if (this.frames[i].isSpare() && !(this.frames[i].hasStrike())){
-          bonus = this.frames[i+1].getRolls()[0].getPinsKnocked();
+        if (thisFrame.isSpare() && !(thisFrame.hasStrike())){
+          bonus = (nextFrame === thisFrame ? thisFrame.getRolls()[2].getPinsKnocked() : nextFrame.getRolls()[0].getPinsKnocked());
           totalScore += bonus;
+          //console.log(bonus);
         }
 
-        console.log(totalScore);
       }// end if
-
+      //console.log(totalScore);
     } //end for
     return totalScore;
   };

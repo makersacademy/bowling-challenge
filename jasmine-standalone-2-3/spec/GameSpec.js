d1b8d1a72c8219468d4c6ceafc9ffc2b815f5c82
Bernard Malhame <bernard.malhame@gmail.com>
2017-03-18 14:13:31 +0000

    adds test for series of non-strike/spare games

--- jasmine-standalone-2-3/spec/GameSpec.js
+++ jasmine-standalone-2-3/spec/GameSpec.js
@@ -6,11 +6,20 @@ describe("Game", function(){
     game = new Game();
   })
 
-  it("can roll a gutter game", function(){
-    for (i = 0; i < 10; i++) {
-      game.bowl([0,0]);
+  function setUpFrames(frame) {
+    for( i = 0; i < 10; i++) {
+      game.bowl(frame);
     }
+  }
+
+  it("can roll a gutter game", function(){
+    setUpFrames([0,0]);
     expect(game.score()).toEqual(0);
+  })
+
+  it("can roll a game of all 1s", function(){
+    setUpFrames([1,1]);
+    expect(game.score()).toEqual(20);
   })
 
 })

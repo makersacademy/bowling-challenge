8c1822462eeea17155271df62d89ac64c4efb6a7
Tahir Raza <taraza@tahirraza.com>
2017-03-18 10:31:47 +0000

    Create a Gutter game

---
+++ spec/bowlingGameSpec.js
@@ -0,0 +1,23 @@
+'use strict';
+describe('The Bowling Game', function(){
+  var game;
+  beforeEach(function(){
+    game = new BowlingGame();
+  });
+
+  it('start thegame', function(){
+    game = new BowlingGame();
+  });
+
+  it('can roll a gutter game', function(){
+    for(var i =0;i<20;i++){
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+
+
+
+
+
+}); //  The Bowling Game describe
---
+++ src/bowlingGame.js
@@ -0,0 +1,11 @@
+'use strict';
+function BowlingGame(){
+
+};
+
+BowlingGame.prototype.roll = function(pins){
+
+}
+BowlingGame.prototype.score = function(){
+  return 0;
+}

eb8e62dca0313f5429b23a068ef9a174b456d68b
Albert <ayanit1@googlemail.com>
2017-03-18 16:10:02 +0000

    Refactor previous commit

--- jasmine/spec/GameSpec.js
+++ jasmine/spec/GameSpec.js
@@ -11,18 +11,14 @@ describe ("Game", function() {
   describe ("Upon instantiating", function() {
 
     it("is defined", function() {
-
       expect(game).toBeDefined();
-
     });
 
     // As a game,
     // so that the game is played properly,
     // A game has a current score set as 0 initially.
     it("has default current score of 0", function() {
-
       expect(game.currentScore).toEqual(0);
-
     });
 
   });

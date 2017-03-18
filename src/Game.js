899f8e41e999921d338b050361b299b2113e04c6
Christos <chris.paraskeva10@gmail.com>
2017-03-18 21:14:53 +0000

    some refactoring and shortening of functions

--- src/Game.js
+++ src/Game.js
@@ -11,21 +11,37 @@ Game.prototype.rollBall = function(score) {
   this.latestRoll = score;
   // this.currentScore = Math.floor(Math.random() * 11);
   if (this.latestRoll === 10 && this.currentTurn === 0) {
-    this.currentFrame++;
-    this.currentTurn = 0;
-    this.totalScores.push(this.latestRoll)
+    this.strikeRoll();
   }
   else if (this.latestRoll < 10 && this.currentTurn === 0) {
-    this.standingPins = this.standingPins - this.latestRoll;
-    this.currentTurn++;
-    this.latestScore.push(this.latestRoll)
+    this.firstRoll();
   }
   else if (this.latestRoll <= 10 && this.currentTurn === 1) {
-    this.currentFrame++;
-    this.currentTurn = 0;
-    this.standingPins = 10;
-    this.latestScore.push(this.latestRoll);
-    this.totalScores.push(this.latestScore);
-    this.latestScore = [];
+    this.secondRoll();
   }
+};
+
+Game.prototype.firstRoll = function () {
+  this.standingPins = this.standingPins - this.latestRoll;
+  this.currentTurn++;
+  this.latestScore.push(this.latestRoll)
+};
+
+Game.prototype.secondRoll = function() {
+  this.currentFrame++;
+  this.currentTurn = 0;
+  this.standingPins = 10;
+  this._addBothRollsToScore();
+};
+
+Game.prototype.strikeRoll = function () {
+  this.currentFrame++;
+  this.currentTurn = 0;
+  this.totalScores.push(this.latestRoll)
+};
+
+Game.prototype._addBothRollsToScore = function () {
+  this.latestScore.push(this.latestRoll);
+  this.totalScores.push(this.latestScore);
+  this.latestScore = [];
 };

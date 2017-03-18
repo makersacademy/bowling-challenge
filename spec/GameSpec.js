2aa2cb2f1b431426be7d5d2094bd0f7359220fb9
Christos <chris.paraskeva10@gmail.com>
2017-03-18 20:02:54 +0000

    test passing for rolling less than 10 and the correct total of remaining pins standing

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -13,17 +13,38 @@ describe('Game', function(){
     });
   });
 
-  describe('After a player takes their turn', function(){
+  describe('When taking a turn and scoring less than 10', function(){
+    it('The player will get another turn for the frame, with the remaining pins standing', function() {
+      game.rollBall(6);
+      spyOn(Math, 'floor').and.returnValue(10);
+
+      // game.standingPins = 3
+      // game.currentScore = 7
+      expect(game.standingPins).toEqual(10 - game.currentScore)
+    });
+  });
+
+
+  // describe('When taking a turn', function(){
+  //   it('The player will get another turn for the frame if they fail to get a strike', function() {
+  //     game.rollBall();
+  //     expect(game.currentFrame).toEqual(1)
+  //   });
+  // });
+
+  describe('After a player completes their turn', function(){
     it('The current frame will increment up by 1', function() {
-      game.rollBall();
+      game.rollBall(10);
+      // spyOn(Math, 'floor').and.returnValue(10);
       expect(game.currentFrame).toEqual(2)
     });
   });
 
   describe('When I roll the ball', function() {
     it('up to 10 pins can be knocked down', function (){
-      game.rollBall();
-      expect(game.frameScore >= 0 && game.frameScore <= 10).toBeTruthy();
+      score = Math.floor(Math.random() * 11);
+      game.rollBall(score);
+      expect(game.currentScore >= 0 && game.currentScore <= 10).toBeTruthy();
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 function Game() {
   this.currentFrame = 1
-  this.frameScore;
+  this.currentScore;
+  this.standingPins = 0
+  this.currentTurn = 0
 };
 
-Game.prototype.rollBall = function() {
-  this.frameScore = Math.floor(Math.random() * 11);
-  this.currentFrame++;
+Game.prototype.rollBall = function(score) {
+  this.currentScore = score
+  // this.currentScore = Math.floor(Math.random() * 11);
+  if (this.currentScore === 10 && this.currentTurn === 0) {
+    this.currentFrame++;
+    this.currentTurn = 0;
+  }
+  else if (this.currentScore < 10 && this.currentTurn === 0) {
+    this.standingPins = (10 - this.currentScore)
+    this.currentTurn++;
+  }
+  else if (this.currentScore <= 10 && this.currentTurn === 1) {
+    this.currentFrame++;
+  }
 };
+
+// Game.prototype.rollScore() = function(){
+//     this.frameScore = Math.floor(Math.random() * 11);
+// };

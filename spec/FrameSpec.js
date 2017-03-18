3e04ce6f7a67e6a412c2eae162665d29b122b7c3
Ashwini <ashie.oh@gmail.com>
2017-03-18 15:46:37 +0000

    Adds logic to calculate frame total

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -15,7 +15,7 @@ describe('Frame', function(){
 
   });
 
-  describe('getFrameScore', function(){
+  describe('#getFrameScore', function(){
 
     it('should be zero to begin with', function(){
       expect(frame.getFrameScore()).toEqual(0);
@@ -23,13 +23,29 @@ describe('Frame', function(){
 
   });
 
-  describe('getRolls', function(){
+  describe('#getRolls', function(){
 
     it('should contain atleast two instances of Roll', function(){
       expect(frame.getRolls().length).toEqual(2);
       expect(frame.getRolls().every(roll => roll instanceof Roll)).toBe(true);
     });
 
+  });
+
+  describe('#play', function(){
+
+    it("sets current roll's knocked pins", function(){
+        expect(function(){frame.play(4)}).not.toThrow();
+    });
+
+  });
+
+  describe('#calculateFrameScore', function(){
+    it('calculates score for a frame', function(){
+      frame.play(4);
+      frame.play(1);
+      expect(frame.calculateFrameScore()).toEqual(5);
+    });
   });
 
 });
--- src/Frame.js
+++ src/Frame.js
@@ -3,6 +3,7 @@
 function Frame (){
   this.frameScore = 0;
   this.rolls = [new Roll(), new Roll()];
+  this.isDone = false;
 }
 
 Frame.prototype.getFrameScore = function(){
@@ -11,4 +12,24 @@ Frame.prototype.getFrameScore = function(){
 
 Frame.prototype.getRolls = function(){
   return this.rolls
+};
+
+Frame.prototype.play = function(pinsKnocked){
+  this._currentRoll().setPinsKnocked(pinsKnocked);
+};
+
+Frame.prototype._currentRoll = function(){
+  var currentRoll = this.getRolls().find(function(roll){
+    return roll.isSet === false
+  });
+  this.isDone = !!(currentRoll); 
+  return currentRoll;
+};
+
+Frame.prototype.calculateFrameScore = function(){
+  var rollScores = this.getRolls().map(function(roll){
+    return roll.getPinsKnocked();
+  });
+  var sum = rollScores.reduce(function(acc, val){ return acc + val},0);
+  return sum;
 };

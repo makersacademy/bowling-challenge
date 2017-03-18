768908096da5e568514094ff5bde7e05643c279f
Christos <chris.paraskeva10@gmail.com>
2017-03-18 20:23:26 +0000

    logic for correct frames and scoring working

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,6 +1,5 @@
 describe('Game', function(){
   var game;
-  // var player;
 
   beforeEach(function() {
     game = new Game();
@@ -8,7 +7,6 @@ describe('Game', function(){
 
   describe('When starting a new game', function() {
     it('The current frame is set to 1', function (){
-      // game.startNewGame();
       expect(game.currentFrame).toEqual(1)
     });
   });
@@ -16,26 +14,13 @@ describe('Game', function(){
   describe('When taking a turn and scoring less than 10', function(){
     it('The player will get another turn for the frame, with the remaining pins standing', function() {
       game.rollBall(6);
-      spyOn(Math, 'floor').and.returnValue(10);
-
-      // game.standingPins = 3
-      // game.currentScore = 7
-      expect(game.standingPins).toEqual(10 - game.currentScore)
+      expect(game.standingPins).toEqual(10 - game.latestScore)
     });
   });
-
-
-  // describe('When taking a turn', function(){
-  //   it('The player will get another turn for the frame if they fail to get a strike', function() {
-  //     game.rollBall();
-  //     expect(game.currentFrame).toEqual(1)
-  //   });
-  // });
 
   describe('After a player completes their turn', function(){
     it('The current frame will increment up by 1', function() {
       game.rollBall(10);
-      // spyOn(Math, 'floor').and.returnValue(10);
       expect(game.currentFrame).toEqual(2)
     });
   });
@@ -44,7 +29,7 @@ describe('Game', function(){
     it('up to 10 pins can be knocked down', function (){
       score = Math.floor(Math.random() * 11);
       game.rollBall(score);
-      expect(game.currentScore >= 0 && game.currentScore <= 10).toBeTruthy();
+      expect(game.latestScore >= 0 && game.latestScore <= 10).toBeTruthy();
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,26 +1,24 @@
 function Game() {
   this.currentFrame = 1
-  this.currentScore;
-  this.standingPins = 0
-  this.currentTurn = 0
+  this.latestScore;
+  this.standingPins = 10;
+  this.currentTurn = 0;
 };
 
 Game.prototype.rollBall = function(score) {
-  this.currentScore = score
+  this.latestScore = score;
   // this.currentScore = Math.floor(Math.random() * 11);
-  if (this.currentScore === 10 && this.currentTurn === 0) {
+  if (this.latestScore === 10 && this.currentTurn === 0) {
     this.currentFrame++;
     this.currentTurn = 0;
   }
-  else if (this.currentScore < 10 && this.currentTurn === 0) {
-    this.standingPins = (10 - this.currentScore)
+  else if (this.latestScore < 10 && this.currentTurn === 0) {
+    this.standingPins = this.standingPins - this.latestScore;
     this.currentTurn++;
   }
-  else if (this.currentScore <= 10 && this.currentTurn === 1) {
+  else if (this.latestScore <= 10 && this.currentTurn === 1) {
     this.currentFrame++;
+    this.currentTurn = 0;
+    this.standingPins = 10;
   }
 };
-
-// Game.prototype.rollScore() = function(){
-//     this.frameScore = Math.floor(Math.random() * 11);
-// };

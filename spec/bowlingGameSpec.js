e082ebf12a1ca2de81277af4ccab571ad2058da2
Tahir Raza <taraza@tahirraza.com>
2017-03-18 10:42:49 +0000

    Can roll all ones

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -4,16 +4,19 @@ describe('The Bowling Game', function(){
   beforeEach(function(){
     game = new BowlingGame();
   });
-
-  it('start thegame', function(){
-    game = new BowlingGame();
-  });
-
+  
   it('can roll a gutter game', function(){
     for(var i =0;i<20;i++){
       game.roll(0);
     }
     expect(game.score()).toBe(0);
+  });
+
+  it('can roll all ones', function(){
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
   });
 
 
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,11 +1,15 @@
 'use strict';
 function BowlingGame(){
-
+  this.rolls = [];
+  this.gameResult = 0;
 };
 
 BowlingGame.prototype.roll = function(pins){
-
+  this.rolls.push(pins);
 }
 BowlingGame.prototype.score = function(){
-  return 0;
+  for(var i=0; i<20;i++){
+    this.gameResult += this.rolls[i];
+  }
+  return this.gameResult;
 }

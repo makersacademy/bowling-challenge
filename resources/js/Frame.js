e41c0ed011efaa1018383db8d7c9fadf0c545c76
Albert <ayanit1@googlemail.com>
2017-03-18 20:03:38 +0000

    Fix Frame conditional isSpare and isStrike functions

--- resources/js/Frame.js
+++ resources/js/Frame.js
@@ -1,18 +1,17 @@
 'use strict';
 
-function Frame(score1, score2)
+function Frame(score1, score2=0)
 {
   this.score1 = score1
   this.score2 = score2
-
   if (this.score1 === 10) {
-    this.isStrike = true
+    this.isStrike = true;
   } else {
-    this.isStrike = false
+    this.isStrike = false;
   };
-  if (this.score1 != 0 && this.score1 + this.score2 === 10) {
-    this.isSpare = true
+  if (this.score1 !== 10 && this.score1 + this.score2 === 10) {
+    this.isSpare = true;
   } else {
-    this.isSpare = false
-  }
-};
+    this.isSpare = false;
+  };
+}

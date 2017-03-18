9c4ba7a7577ccb56b7e9ce75394d27f51897e4c8
Christos <chris.paraskeva10@gmail.com>
2017-03-18 20:51:58 +0000

    can keep track of previous scores

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -14,7 +14,7 @@ describe('Game', function(){
   describe('When taking a turn and scoring less than 10', function(){
     it('The player will get another turn for the frame, with the remaining pins standing', function() {
       game.rollBall(6);
-      expect(game.standingPins).toEqual(10 - game.latestScore)
+      expect(game.standingPins).toEqual(10 - game.latestRoll)
     });
   });
 
@@ -23,13 +23,20 @@ describe('Game', function(){
       game.rollBall(10);
       expect(game.currentFrame).toEqual(2)
     });
+
+    it('The score will be recorded', function() {
+      game.rollBall(4);
+      game.rollBall(5);
+      game.rollBall(10);
+      expect(game.totalScores).toEqual([[4,5], 10])
+    });
   });
 
   describe('When I roll the ball', function() {
     it('up to 10 pins can be knocked down', function (){
       score = Math.floor(Math.random() * 11);
       game.rollBall(score);
-      expect(game.latestScore >= 0 && game.latestScore <= 10).toBeTruthy();
+      expect(game.latestRoll >= 0 && game.latestRoll <= 10).toBeTruthy();
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,24 +1,31 @@
 function Game() {
   this.currentFrame = 1
-  this.latestScore;
+  this.latestRoll;
   this.standingPins = 10;
   this.currentTurn = 0;
+  this.totalScores = [];
+  this.latestScore = [];
 };
 
 Game.prototype.rollBall = function(score) {
-  this.latestScore = score;
+  this.latestRoll = score;
   // this.currentScore = Math.floor(Math.random() * 11);
-  if (this.latestScore === 10 && this.currentTurn === 0) {
+  if (this.latestRoll === 10 && this.currentTurn === 0) {
     this.currentFrame++;
     this.currentTurn = 0;
+    this.totalScores.push(this.latestRoll)
   }
-  else if (this.latestScore < 10 && this.currentTurn === 0) {
-    this.standingPins = this.standingPins - this.latestScore;
+  else if (this.latestRoll < 10 && this.currentTurn === 0) {
+    this.standingPins = this.standingPins - this.latestRoll;
     this.currentTurn++;
+    this.latestScore.push(this.latestRoll)
   }
-  else if (this.latestScore <= 10 && this.currentTurn === 1) {
+  else if (this.latestRoll <= 10 && this.currentTurn === 1) {
     this.currentFrame++;
     this.currentTurn = 0;
     this.standingPins = 10;
+    this.latestScore.push(this.latestRoll);
+    this.totalScores.push(this.latestScore);
+    this.latestScore = [];
   }
 };

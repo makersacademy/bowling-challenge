e802489ba16430f74bc8b89a784117e301e1999a
Nicholas Leacock <nicholas@swissmail.com>
2017-03-19 08:36:13 +0000

    Added scoreTotal method and test.

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -6,19 +6,36 @@ describe('Frame', function() {
     expect(frame.roll()).toBeLessThan(11);
   });
 
+  it('adds present frame hits to overall score', function(){
+    expect(frame.scoreTotal()).toEqual(frame.rollResult())
+  });
+
   describe("Strike", function() {
 
+    beforeEach(function(){
+      spyOn(frame, 'rollResult').and.returnValue(10)
+    });
+
     it('is registered on the first roll when all ten pins have been knocked down.', function(){
-      spyOn(frame, 'roll').and.returnValue(10)
       spyOn(frame, 'checkWhichRoll').and.returnValue(1)
       expect(frame.strike()).toBe(true)
     });
 
     it('is not registered on the second roll when all ten pins have been knocked down.', function(){
-      spyOn(frame, 'roll').and.returnValue(10)
       spyOn(frame, 'checkWhichRoll').and.returnValue(2)
       expect(frame.strike()).toBe(false)
     });
   });
+
+  describe("Spare", function() {
+
+    it('is registered on the second roll when all ten pins have been knocked down.', function(){
+      spyOn(frame, 'rollResult').and.returnValue(10)
+      spyOn(frame, 'checkWhichRoll').and.returnValue(2)
+      expect(frame.spare()).toBe(true)
+    });
+
+  });
+
 
 });
--- src/frame.js
+++ src/frame.js
@@ -2,7 +2,8 @@ function Frame(){
   this.total = [];
 }
 
-var rollCounter = 0;
+var rollCounter = 0, x;
+
 while (rollCounter <= 1) {
   rollCounter += 1;
 
@@ -11,12 +12,30 @@ while (rollCounter <= 1) {
   };
 
   Frame.prototype.roll = function(){
-    return Math.floor((Math.random() * 11) + 0);
+    x = Math.floor((Math.random() * 11) + 0);
+    return x;
+  };
+
+  Frame.prototype.rollResult = function(){
+    return x;
+  }
+
+  Frame.prototype.scoreTotal = function(){
+    this.total.push(x);
+    this.total.reduce(function(a, b){
+      c = a + b;
+    }, 0);
+      this.spare();
+      this.strike();
+      return c;
   };
 
   Frame.prototype.strike = function(){
-    return ((this.roll() == 10) && (this.checkWhichRoll() == 1)) ? true : false
+    return ((this.rollResult() == 10) && (this.checkWhichRoll() == 1)) ? true : false
   };
 
-  
+  Frame.prototype.spare = function(){
+  return ((this.rollResult() == 10) && (this.checkWhichRoll() == 2)) ? true : false
+  };
+
 }// while loop!

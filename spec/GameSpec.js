1245d4e71af69b2bd34f037c280106543fe9fb3e
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-19 17:42:17 +0000

    Add score calculator

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,18 +10,14 @@ describe("Game", function() {
 
   describe("score", function() {
 
-    it("is defined", function() {
-      expect(game.score).toBeDefined();
-    });
-
     it("is 0 when game starts", function () {
-      expect(game.score).toEqual(0);
+      expect(game.score()).toEqual(0);
     });
 
     it("remembers score after every roll", function() {
       game.roll(1);
       game.roll(5);
-      expect(game.score).toEqual(6);
+      expect(game.score()).toEqual(6);
     });
 
     it("returns a current frame", function() {
@@ -38,6 +34,13 @@ describe("Game", function() {
       game.roll(5);
       game.roll(3);
       expect(game.completedFrames).toEqual([[1, 3], [5, 3]]);
+    });
+
+    it("calculates score", function() {
+      var game = new Game;
+      game.roll(1);
+      game.roll(3);
+      expect(game.score()).toEqual(4);
     });
 
   });
--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,10 @@
 var Game = function() {
-  this.score = 0;
   this.currentFrame = [];
   this.completedFrames = [];
 };
 
 Game.prototype.roll = function(pinsKnockedDown) {
   this.newFrame(pinsKnockedDown);
-  this.score += pinsKnockedDown;
 };
 
 Game.prototype.newFrame = function(pinsKnockedDown) {
@@ -18,4 +16,15 @@ Game.prototype.newFrame = function(pinsKnockedDown) {
     (this.completedFrames).push(this.currentFrame);
     this.currentFrame = [];
   };
+};
+
+Game.prototype.score = function() {
+  var total = 0;
+  for (var i = 0; i < this.completedFrames.length; i++) {
+    var frame = this.completedFrames[i];
+    for (var j = 0; j < frame.length; j++) {
+      total += frame[j];
+    }
+  }
+  return total;
 };

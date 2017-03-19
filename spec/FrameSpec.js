336de1a2ba6db7ae709a468d1ffb30f5bf8e763e
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-19 15:19:49 +0000

    Rewrite Frame with strike and spare checks

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -7,11 +7,51 @@ describe ("Frame", function() {
     frame = new Frame();
   });
 
-  describe("Scoring", function() {
+  describe("First Bowl", function() {
 
-    it("is saved into the frame", function() {
-      frame.inputScore(6, 2)
-      expect(frame.totalFrame).toEqual([[6, 2]]);
+    it("first bowl is saved into the frame", function() {
+      frame.inputFirstBowl(6)
+      expect(frame.totalFrame).toEqual([6]);
+    });
+
+    it("wont allow score to be over 10", function() {
+      expect(function(){frame.inputFirstBowl(11);}).toThrowError("Score cannot be over 10");
+    });
+
+    it("checks if strike", function() {
+      expect(frame.isStrike(10)).toEqual(true);
+      expect(frame.isStrike(5)).toEqual(false);
+    });
+  });
+
+  describe("Second Bowl", function() {
+
+    it("second bowl is saved into the frame", function() {
+      frame.inputSecondBowl(2)
+      expect(frame.totalFrame).toEqual([2]);
+    });
+
+    it("wont allow score to be over 10", function() {
+      expect(function(){frame.inputSecondBowl(11);}).toThrowError("Score cannot be over 10");
+    });
+
+    it("puts both rolls into the frame", function() {
+      frame.inputFirstBowl(7)
+      frame.inputSecondBowl(1)
+      expect(frame.totalFrame).toEqual([7,1]);
+    });
+
+    it("check if IS spare", function() {
+      // spyOn(this.totalFrame).andReturn([5,5])
+      frame.inputFirstBowl(5)
+      frame.inputSecondBowl(5)
+      expect(frame.isSpare()).toEqual(true);
+    });
+
+    it("check if NOT spare", function() {
+      frame.inputFirstBowl(1)
+      frame.inputSecondBowl(5)
+      expect(frame.isSpare()).toEqual(false);
     });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -2,8 +2,28 @@
 
 function Frame(){
   this.totalFrame = [];
+}
+
+Frame.prototype.inputFirstBowl = function(pins) {
+  this.checkNumberOfPins(pins);
+  return this.totalFrame.push(pins);
 };
 
-Frame.prototype.inputScore = function (bowl1, bowl2 = 0) {
-  return this.totalFrame.push([bowl1, bowl2]);
+Frame.prototype.inputSecondBowl = function(pins) {
+  this.checkNumberOfPins(pins);
+  return this.totalFrame.push(pins);
+};
+
+Frame.prototype.checkNumberOfPins = function(pins) {
+  if (pins > 10) { throw new Error ("Score cannot be over 10"); }
+};
+
+Frame.prototype.isStrike = function(pins) {
+  return pins === 10 ? true : false;
+};
+
+Frame.prototype.isSpare = function() {
+  return this.totalFrame.reduce(function (first, second){
+    return first + second === 10 ? true : false;
+  });
 };

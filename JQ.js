5d18b2ff74431548ca6dd33489a8b99d15c35cf2
Nicholas Leacock <nicholas@swissmail.com>
2017-03-20 20:07:08 +0000

    Changed name of js file JQ to jq

--- JQ.js
+++ JQ.js
@@ -1,26 +1,23 @@
 $( document ).ready(function() {
   var frame = new Frame();
-  var x = 1;
+  var x = 1, m;
 
     $ ("#remaining-pins").text(frame.pinsStanding());
 
-      $ ("#which-roll").text(x);
+    $ ("#which-roll").text(x);
 
-
-    // if (x == 2){
-    //   $("#next-frame-prompt").text(frame.framePrompt());
-    // }
+    $ ("#current-score").text(0);
 
 
     $ ("#roll").click(function(){
-
-      x == 2 ? frame.nextFrame() : x++
 
       $ ("#current-score").text(frame.roll());
 
       $ ("#remaining-pins").text(frame.score());
 
-       $ ("#which-roll").text(x);
+      $ ("#which-roll").text(x + 1);
+
+      x == 2 ? frame.nextFrame() : x++
 
     });
 

7e9e45be3e31fda78fd7e78c2a13c3e4377b362f
Tahir Raza <taraza@tahirraza.com>
2017-03-22 20:57:46 +0000

    User Interface - created 2d array to hold my frame logic - updated interface.js to accomodate the clicking of buttons

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -3,16 +3,51 @@ function BowlingGame(){
   this.rolls = [];
   this.gameResult = 0;
   this.rollIndex = 0;
+  this.rollIndex1 = 0;
+  this.frameIndex = 0;
+  this.frame = [];
+  for(var i= 0;i<10;i++){
+    this.frame[i] = [];
+      for(var j=0;j<2;j++){
+        this.frame[i][j]=0;
+      }
+  }
 };
 
 BowlingGame.prototype.roll = function(pins){
   this.rolls.push(pins);
+  console.log('roll:' + this.rollIndex1);
+  console.log('frame:' + this.frameIndex);
+  this.frame[this.frameIndex][this.rollIndex1] = pins;
+  console.log(this.frame[this.frameIndex][this.rollIndex1]);
+
+  this.registerFrame();
+
+
 }
+  BowlingGame.prototype.f = function(){
+    return this.frame;
+  }
+
+  BowlingGame.prototype.registerFrame = function(){
+    if(this.rollIndex1<=1){
+       this.rollIndex1++;
+    }
+    if(this.rollIndex1 > 1){
+     this.rollIndex1 = 0;
+     this.frameIndex++;
+    }
+
+    if(this.frameIndex>9){
+      this.frameIndex = 0;
+    }
+  }
+
+
 BowlingGame.prototype.score = function(){
   for(var frameIndex=0; frameIndex<10;frameIndex++){
     if (this.isStrike()) {
       this.getStrikeScore();
-      this.rollIndex++;
     } else if  (this.isSpare()){
       this.getSpareScore();
       this.rollIndex+=2;
--- src/interFace.js
+++ src/interFace.js
@@ -1,75 +1,141 @@
 'use strict';
 var bowling = new BowlingGame();
 $(document).ready(function(){
+  var rollIndex = 1;
+  var frameCount = 1;
+  var rollCount = 1;
 
   var disableButtons = function(n){
-
-    for(var i = 10; i>(10-n);i--){
-      $('#pin'+i).attr('disabled', true);
-      $('#pin'+i).css('color', 'white');
-      $('#pin'+i).css('background-color', 'black');
+      for(var i = 10; i>(10-n);i--){
+        $('#pin'+i).attr('disabled', true);
+        $('#pin'+i).css('color', 'white');
+        $('#pin'+i).css('background-color', 'black');
+      }
   }
-}
+  var enableButtons = function(){
+    for(var i = 0; i<=10;i++){
+      $('#pin'+i).attr('disabled', false);
+      $('#pin'+i).css('color', 'black');
+      $('#pin'+i).css('background-color', 'lightgrey');
+    }
+  }
+  var count = function(n){
+    if (n<10){
+      if (rollCount < 2) {
+        enableDissable(n);
+        rollCount++
+      } else {
+        enableDissable(n);
+        rollCount = 1;
+      }
+    } else {
+      rollCount = 1;
+      enableButtons();
+    }
+    console.log('count:'+rollCount);
+  }
+  var enableDissable = function(n){
+    if ( rollCount === 1) {
+      console.log("Disabling Buttons");
+      disableButtons(n);
+    } else {
+      console.log('Enabling Buttons');
+      enableButtons();
+    }
+  }
 
   $('#new-game').click(function(){
     location.reload();
   });
+
   $('#pin0').click(function(){
+    $("#roll"+rollIndex+"-score").html('0');
+    rollIndex++
+    // enableDissable(0);
+    count(0);
     bowling.roll(0);
-    disableButtons(0);
-
 
   });
+
   $('#pin1').click(function(){
+    $("#roll"+rollIndex+"-score").html('1');
+    rollIndex++;
+    // enableDissable(1);
+    count(1);
     bowling.roll(1);
-    disableButtons(1);
-    // $('#pin1').text('one')
-    //$('#pin1').attr('disabled', true);
   });
+
   $('#pin2').click(function(){
+    $("#roll"+rollIndex+"-score").html('2');
+    rollIndex++;
     bowling.roll(2);
-    disableButtons(2);
-
+    count(2);
   });
+
   $('#pin3').click(function(){
+    $("#roll"+rollIndex+"-score").html('3');
+    rollIndex++;
+    count(3);
     bowling.roll(3);
-    disableButtons(3);
-
   });
+
   $('#pin4').click(function(){
+    $("#roll"+rollIndex+"-score").html('4');
+    rollIndex++;
+    count(4);
     bowling.roll(4);
-    disableButtons(4);
-
   });
+
   $('#pin5').click(function(){
+    $("#roll"+rollIndex+"-score").html('5');
+    rollIndex++;
+    count(5);
     bowling.roll(5);
-    disableButtons(5);
-
   });
+
   $('#pin6').click(function(){
+    $("#roll"+rollIndex+"-score").html('6');
+    rollIndex++;
+    count(6);
     bowling.roll(6);
-    disableButtons(6);
-
+    // disableButtons(6);
   });
+
   $('#pin7').click(function(){
+    $("#roll"+rollIndex+"-score").html('7');
+    rollIndex++;
+    count(7);
     bowling.roll(7);
-    disableButtons(7);
-
+    // disableButtons(7);
   });
+
   $('#pin8').click(function(){
+    $("#roll"+rollIndex+"-score").html('8');
+    rollIndex++;
+    count(8);
     bowling.roll(8);
-    disableButtons(8);
-
+    // disableButtons(8);
   });
+
   $('#pin9').click(function(){
+    $("#roll"+rollIndex+"-score").html('9');
+    rollIndex++;
+    count(9);
     bowling.roll(9);
-    disableButtons(9);
-
+    // disableButtons(9);
   });
-  $('#pin10').click(function(){
-    bowling.roll(10);
-    disableButtons(10);
 
+  $('#pin10').click(function(){
+    $("#roll"+rollIndex+"-score").html('10');
+    if (rollCount == 1){
+      rollIndex++;
+      rollIndex++;
+    } else {
+      rollIndex++;
+    }
+    count(10);
+    bowling.roll(10);
+    // disableButtons(10);
   });
 
 });   //  Interface ready function

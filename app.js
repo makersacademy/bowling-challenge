76f59777ecd32d3aab3d18e4bfbb29f7812e5f41
John Chang <johnchang9094@gmail.com>
2017-03-23 23:20:48 +0000

    Updated function names

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-var events = require('events');
+var http = require('http');
 
-var util = require('util');
-
-var Person = function(name) {
-  this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-var james = new Person("james");
-var mary = new Person("mary");
-var john = new Person("john");
-var people = [james, mary, john];
-
-people.forEach(function(person) {
-  person.on('speak', function(mssg) {
-    console.log(person.name + ' said: ' + mssg);
+var server = http.createServer(function(req, res) {
+  console.log("Request was made: " + req.url);
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
   });
+  res.end('Hey, heres some plain text');
 });
 
-james.emit('speak', 'Hey Man');
-john.emit('speak', 'Im pretty good');
+server.listen(3000, '127.0.0.1');
+console.log('You are live on port 3000');

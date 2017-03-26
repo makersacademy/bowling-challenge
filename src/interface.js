78aa92a3af7ee02c2aa904b1ca5dec8bf97377e0
Sophie <sophie.k.mclean@btinternet.com>
2017-03-26 22:41:29 +0100

    partially set up table

--- src/interface.js
+++ src/interface.js
@@ -1,15 +1,22 @@
 $ (document).ready(function(){
   var bowling = new Bowling ();
 
-  // $('#pins').html(bowling.pinsKnockedOver());
-
+   
   $('#roll').click(function(){
     bowling.pinsKnockedOver();
-    bowling.check();
-    bowling.settingRollsPerGame();
-    $('#pins').html(bowling.score());
+     bowling.check();
+     bowling.spare();
+     bowling.settingRollsPerGame();
+    $('#score').html(bowling.score());
+    $('#pins').html(bowling.pins)
+    $('#secondThrow').html(bowling.secondThrow)
+    $('#thirdThrow').html(bowling.thirdThrow)
   });
 
+  // $('#roll').click(function(){
+  //   bowling.pinsKnockedOver();
+  //   $('#pins').html(bowling.pinsKnockedOver());
+  // });
 
   $('#score').html(bowling.score());
 

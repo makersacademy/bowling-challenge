3a9c4019d0248a0f1d996201cc2adc225b72e35a
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-26 19:10:39 +0100

    Refactor condition to isSpare function

--- src/Game.js
+++ src/Game.js
@@ -4,16 +4,20 @@ var Game = function() {
 };
 
 Game.prototype.roll = function(pinsKnockedDown) {
-
   this.rolls[this.currentRoll] = pinsKnockedDown;
   this.currentRoll++;
 };
 
+Game.prototype.isSpare = function(frameNumber) {
+  return this.rolls[frameNumber] + this.rolls[frameNumber + 1] == 10
+}
+
 Game.prototype.score = function() {
+
   var total = 0;
   var frameNumber = 0;
   for (var frame = 0; frame < 10; frame++) {
-    if (this.rolls[frameNumber] + this.rolls[frameNumber + 1] == 10) {
+    if (this.isSpare(frameNumber)) {
       total += 10 + this.rolls[frameNumber + 2];
       frameNumber += 2;
     }

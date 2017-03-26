e4fb19958b91e9032f83575161121814e9f007ca
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-26 17:13:42 +0100

    Add gutter game score

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,40 +8,60 @@ describe("Game", function() {
     expect(game).toBeDefined();
   });
 
-  describe("score", function() {
+  describe("score", function () {
 
-    it("is 0 when game starts", function () {
-      expect(game.score()).toEqual(0);
+    it("scores gutter game", function() {
+      for (var i = 0; i < 20; i++) {
+        game.roll(0);
+      }
+      expect(game.score).toEqual(0);
     });
 
-    it("remembers score after every roll", function() {
-      game.roll(1);
-      game.roll(5);
-      expect(game.score()).toEqual(6);
+    it("scores every roll", function() {
+      for (var i = 0; i < 20; i++) {
+        game.roll(1);
+      }
+      expect(game.score).toEqual(20);
     });
-
-    it("returns a current frame", function() {
-      var game = new Game;
-      game.roll(1);
-      game.roll(3);
-      expect(game.completedFrames).toEqual([[1, 3]]);
-    });
-
-    it("saves completed frames", function() {
-      var game = new Game;
-      game.roll(1);
-      game.roll(3);
-      game.roll(5);
-      game.roll(3);
-      expect(game.completedFrames).toEqual([[1, 3], [5, 3]]);
-    });
-
-    it("saves frame if it's strike after first roll", function() {
-      var game = new Game;
-      game.roll(10);
-      expect(game.completedFrames).toEqual([[10, 0]]);
-    })
 
   });
 
 });
+
+//   describe("score", function() {
+//
+//     it("is 0 when game starts", function () {
+//       expect(game.score()).toEqual(0);
+//     });
+//
+//     it("remembers score after every roll", function() {
+//       game.roll(1);
+//       game.roll(5);
+//       expect(game.score()).toEqual(6);
+//     });
+//
+//     it("returns a current frame", function() {
+//       var game = new Game;
+//       game.roll(1);
+//       game.roll(3);
+//       expect(game.completedFrames).toEqual([[1, 3]]);
+//     });
+//
+//     it("saves completed frames", function() {
+//       var game = new Game;
+//       game.roll(1);
+//       game.roll(3);
+//       game.roll(5);
+//       game.roll(3);
+//       expect(game.completedFrames).toEqual([[1, 3], [5, 3]]);
+//     });
+//
+//     it("saves frame if it's strike after first roll", function() {
+//       var game = new Game;
+//       game.roll(10);
+//       expect(game.completedFrames).toEqual([[10, 0]]);
+//     })
+//
+//   });
+//
+// });
--- src/Game.js
+++ src/Game.js
@@ -1,35 +1,30 @@
 var Game = function() {
-  this.currentFrame = [];
-  this.completedFrames = [];
+  this.score = 0;
+  // this.completedFrames = [];
 };
 
 Game.prototype.roll = function(pinsKnockedDown) {
-  if (pinsKnockedDown == 10) {
-    this.completedFrames.push([10, 0]);
-  }
-  else {
-    this.newFrame(pinsKnockedDown);
-  };
+  this.score += pinsKnockedDown;
 };
 
-Game.prototype.newFrame = function(pinsKnockedDown) {
-  if (this.currentFrame.length == 0) {
-    this.currentFrame[0] = pinsKnockedDown;
-  }
-  else {
-    this.currentFrame[1] = pinsKnockedDown;
-    (this.completedFrames).push(this.currentFrame);
-    this.currentFrame = [];
-  };
-};
-
-Game.prototype.score = function() {
-  var total = 0;
-  for (var i = 0; i < this.completedFrames.length; i++) {
-    var frame = this.completedFrames[i];
-    for (var j = 0; j < frame.length; j++) {
-      total += frame[j];
-    }
-  }
-  return total;
-};
+// Game.prototype.newFrame = function(pinsKnockedDown) {
+//   if (this.currentFrame.length == 0) {
+//     this.currentFrame[0] = pinsKnockedDown;
+//   }
+//   else {
+//     this.currentFrame[1] = pinsKnockedDown;
+//     (this.completedFrames).push(this.currentFrame);
+//     this.currentFrame = [];
+//   };
+// };
+//
+// Game.prototype.score = function() {
+//   var total = 0;
+//   for (var i = 0; i < this.completedFrames.length; i++) {
+//     var frame = this.completedFrames[i];
+//     for (var j = 0; j < frame.length; j++) {
+//       total += frame[j];
+//     }
+//   }
+//   return total;
+// };

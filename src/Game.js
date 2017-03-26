3b7e0f3d46eb1474f61a4b330c50ccc90aebda34
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-26 18:36:12 +0100

    Add spares to score function

--- src/Game.js
+++ src/Game.js
@@ -14,8 +14,14 @@ Game.prototype.score = function() {
   var total = 0;
   var i = 0;
   for (var frame = 0; frame < 10; frame++) {
+    if (this.rolls[i] + this.rolls[i + 1] == 10) {
+      total += 10 + this.rolls[i + 2];
+      i += 2;
+    }
+    else {
     total += this.rolls[i] + this.rolls[i + 1];
     i += 2;
+    }
   }
   return total;
 };

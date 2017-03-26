c2b0c21c15a0efc68d5defb3be50ff08c8438f25
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-26 18:40:19 +0100

    Change i to frameNumber

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,6 @@
 var Game = function() {
   this.rolls = [];
   this.currentRoll = 0;
-  // this.completedFrames = [];
 };
 
 Game.prototype.roll = function(pinsKnockedDown) {
@@ -12,39 +11,16 @@ Game.prototype.roll = function(pinsKnockedDown) {
 
 Game.prototype.score = function() {
   var total = 0;
-  var i = 0;
+  var frameNumber = 0;
   for (var frame = 0; frame < 10; frame++) {
-    if (this.rolls[i] + this.rolls[i + 1] == 10) {
-      total += 10 + this.rolls[i + 2];
-      i += 2;
+    if (this.rolls[frameNumber] + this.rolls[frameNumber + 1] == 10) {
+      total += 10 + this.rolls[frameNumber + 2];
+      frameNumber += 2;
     }
     else {
-    total += this.rolls[i] + this.rolls[i + 1];
-    i += 2;
+    total += this.rolls[frameNumber] + this.rolls[frameNumber + 1];
+    frameNumber += 2;
     }
   }
   return total;
 };
-
-
-// Game.prototype.newFrame = function(pinsKnockedDown) {
-//   if (this.currentFrame.length == 0) {
-//     this.currentFrame[0] = pinsKnockedDown;
-//   }
-//   else {
-//     this.currentFrame[1] = pinsKnockedDown;
-//     (this.completedFrames).push(this.currentFrame);
-//     this.currentFrame = [];
-//   };
-// };
-//
-// Game.prototype.score = function() {
-//   var total = 0;
-//   for (var i = 0; i < this.completedFrames.length; i++) {
-//     var frame = this.completedFrames[i];
-//     for (var j = 0; j < frame.length; j++) {
-//       total += frame[j];
-//     }
-//   }
-//   return total;
-// };

11790a2d40a170ee546e7eb5142a0735b4304a27
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-03-26 21:40:29 +0100

    Interface WiP. Implement 1st + 2nd ball btns

--- src/interface.js
+++ src/interface.js
@@ -1,16 +1,41 @@
-var game = new Game();
-var frame = new Frame();
-
 $(document).ready(function() {
+  var game = new Game();
+  var frame = new Frame();
 
-$("#bowl-btn").click(function() {
-  var pins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  var index = Math.floor(Math.random() * 10) + 1;
-  var removedPins = pins.splice(0, index)
-  console.log("bowled over:", removedPins);
-  console.log("still standing:", pins);
-  $('#bowled-pins').html(removedPins.length);
-  $('#remaining-pins').html(pins.length);
-});
+  $("#bowl-second-ball-btn").hide();
+
+  $("#frame-number").html(game.checkAllScores().length + 1);
+  $("#max-frames").html(game.checkGameLength());
+
+  $("#bowl-first-ball-btn").click(function() {
+    var pins = 10;
+    var bowledPins = Math.floor(Math.random() * pins) + 1;
+    var remainingPins = pins - bowledPins;
+    console.log("bowled over:", bowledPins);
+    console.log("still standing:", remainingPins);
+    console.log(game._frames);
+    frame.bowlFirstBall(bowledPins);
+    $("#bowl-first-ball-btn").hide();
+    $("#bowl-second-ball-btn").show();
+    $('#bowled-pins').html(bowledPins);
+    $('#remaining-pins').html(remainingPins);
+  });
+
+  $("#bowl-second-ball-btn").click(function() {
+    var remainingPins = 10 - frame.firstBall();
+    console.log("still standing ready to be hit:", remainingPins);
+    var bowledPins = Math.floor(Math.random() * remainingPins) + 1;
+    var missedPins = remainingPins - bowledPins;
+    console.log("bowled over:", bowledPins);
+    console.log("missed pins:", missedPins);
+    frame.bowlSecondBall(bowledPins);
+    $("#frame-number").html(game.checkAllScores().length + 2);
+    game.addFrame(frame);
+    frame = new Frame();
+    $("#bowl-first-ball-btn").show();
+    $("#bowl-second-ball-btn").hide();
+    $('#bowled-pins').html(bowledPins);
+    $('#remaining-pins').html(missedPins);
+  });
 
 });

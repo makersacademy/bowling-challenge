3f40bdd0a6d98f45ab04aaf4761325d1194a3398
Nicholas Leacock <nicholas@swissmail.com>
2017-03-27 10:12:58 +0100

    Altered tests

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -18,37 +18,6 @@ describe('Frame', function() {
   });
 
 
-  xdescribe('A strike', function() {
-    x = 0;
-    beforeEach(function() {
-      spyOn(frame, 'roll').and.returnValue(10)
-    });
-
-    it('is registered on the first roll when all ten pins have been knocked down.', function() {
-      spyOn(frame, 'checkWhichRoll').and.returnValue(2)
-      expect(frame.applyPoints('strike')).toEqual("STRIKE!");
-    });
-
-    it('is not registered on the second roll when all ten pins have been knocked down.', function() {
-      spyOn(frame, 'checkWhichRoll').and.returnValue(3)
-      expect(frame.applyPoints('spare')).not.toEqual("STRIKE!");
-    });
-  });
-
-  xdescribe('A spare', function() {
-
-    it('is registered on the second roll when all ten pins have been knocked down.', function() {
-      spyOn(frame, 'checkWhichRoll').and.returnValue(3)
-      frame.calculate(10)
-      expect(frame.applyPoints('spare')).toEqual("SPARE!");
-    });
-
-    it('is not registered on the first roll when all ten pins have been knocked down.', function() {
-      spyOn(frame, 'checkWhichRoll').and.returnValue(2)
-      spyOn(frame, 'roll').and.returnValue(10)
-      expect(frame.applyPoints('strike')).not.toEqual("SPARE!");
-    });
-
-  });
+  
 
 });

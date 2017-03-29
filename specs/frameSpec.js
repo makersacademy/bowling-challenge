283dfb691a672e62f8987976429990d01758b8e5
joemaidman <joemaidman@gmail.com>
2017-03-29 20:17:07 +0100

    Restructure project  directory (add app folder)

--- specs/frameSpec.js
+++ specs/frameSpec.js
@@ -300,6 +300,7 @@ describe('Frame', function() {
       });
 
     });
+    
   });
 
 });
--- specs/printerSpec.js
+++ specs/printerSpec.js
@@ -103,6 +103,9 @@ describe('Printer', function() {
         });
 
       });
+
     });
+
   });
+  
 });

f24e5c46e27c16fd1f7598b937b460c9853551ce
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-14 21:28:55 +0100

    add method for dealing with a bakfast

--- src/Game.js
+++ src/Game.js
@@ -24,6 +24,9 @@ Game.prototype._storeFrame = function() {
   if(this._frames.length > 0) {
     this._addBonus(this._frames[this._frames.length - 1], this._currentFrame)
   }
+  if(this._frames.length > 1 && this._isBakfast()) {
+    this._addBonus(this._frames[this._frames.length - 2], this._currentFrame)
+  }
   this._frames.push(this._currentFrame);
   this._currentFrame = new Frame();
 }
@@ -35,4 +38,11 @@ Game.prototype._addBonus = function(lastFrame, thisFrame){
   if(lastFrame.isStrike()) {
     lastFrame.addBonus(thisFrame.points())
   }
+}
+
+Game.prototype._isBakfast = function() {
+  if(this._frames[this._frames.length - 1].isStrike() && this._frames[this._frames.length - 2].isStrike()) {
+    return true
+  }
+  return false
 }

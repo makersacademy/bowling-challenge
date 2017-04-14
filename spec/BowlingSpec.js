54fc35e093369d3b6ce6ee3131f1f8802ebd05ca
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-14 20:51:29 +0100

    add frame complete function

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -15,11 +15,41 @@ describe('Bowling', function() {
 
     it('starts at frame 1', function() {
       expect(bowling.getCurrentFrame()).toEqual(1);
+    });
+
+    it('starts at roll 1', function() {
+      expect(bowling.getCurrentRoll()).toEqual(1);
+    });
+
+    it('frame complete is false', function() {
+      expect(bowling.isFrameComplete()).toBeFalsy();
     })
   });
 
-  it('increases score with add()', function() {
-    bowling.add(1);
-    expect(bowling.getCurrentScore()).toEqual(1);
+  describe('score', function() {
+    it('increases score with add()', function() {
+      bowling.add(1);
+      expect(bowling.getCurrentScore()).toEqual(1);
+    });
+  });
+
+  describe('bowl', function() {
+    it('increments roll by 1', function() {
+      bowling.bowl(1);
+      expect(bowling.getCurrentRoll()).toEqual(2);
+    });
+
+    it('returns number of pins scored', function() {
+      bowling.bowl(5);
+      expect(bowling.getFrameScore()).toEqual(5);
+    });
+  });
+
+  describe('frame complete', function() {
+    it('is true after 2 bowls', function() {
+      bowling.bowl(4);
+      bowling.bowl(3);
+      expect(bowling.isFrameComplete()).toBeTruthy();
+    });
   });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -3,6 +3,8 @@
 function Bowling() {
   this.score = 0;
   this.frame = 1;
+  this.roll = 1;
+  this.frameScore = 0;
 }
 
 Bowling.prototype.getCurrentScore = function () {
@@ -13,6 +15,28 @@ Bowling.prototype.getCurrentFrame = function () {
   return this.frame;
 };
 
+Bowling.prototype.getCurrentRoll = function () {
+  return this.roll;
+};
+
 Bowling.prototype.add = function (number) {
   this.score += number;
+};
+
+Bowling.prototype.bowl = function (pins) {
+  this.roll += 1;
+  this.frameScore += pins;
+};
+
+Bowling.prototype.getFrameScore = function () {
+  return this.frameScore;
+};
+
+Bowling.prototype.isFrameComplete = function () {
+  if(this.roll === 1){
+    return false;
+  }
+  else {
+    return true;
+  }
 };

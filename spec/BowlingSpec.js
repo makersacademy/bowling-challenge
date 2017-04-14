cf3519d2587624c15547ac45a14a4e0ab2fd5953
Colin Turner <colinturner34@gmail.com>
2017-04-14 20:52:01 +0100

    Bowling increments Roll # up by 1 and Frame is complete after 2 bowls

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,11 +7,17 @@ describe ('Bowling', function(){
     bowling = new Bowling();
   });
 
-  describe('score', function(){
+  describe('intialize', function(){
     it('starts at zero', function(){
       expect(bowling.getCurrentScore()).toEqual(0);
     });
 
+    it('is false', function(){
+      expect(bowling.isFrameComplete()).toBeFalsy();
+    });
+  });
+
+  describe('add', function(){
     it('can have be added to', function(){
       bowling.add(1);
       expect(bowling.getCurrentScore()).toEqual(1);
@@ -27,6 +33,26 @@ describe ('Bowling', function(){
   describe('roll', function(){
     it('begins on Roll #1', function(){
       expect(bowling.getCurrentRoll()).toEqual(1);
+    });
+  });
+
+  describe('bowl', function(){
+    it('increments roll by 1', function(){
+      bowling.bowl(6);
+      expect(bowling.getCurrentRoll()).toEqual(2);
+    });
+
+    it('returns the number of pins knocked down on a roll', function(){
+      bowling.bowl(4);
+      expect(bowling.getFrameScore()).toEqual(4);
+    });
+  });
+
+  describe('frame complete', function(){
+    it('isFrameComplete is true after 2 bowls', function(){
+      bowling.bowl(3);
+      bowling.bowl(4);
+      expect(bowling.isFrameComplete()).toBeTruthy();
     });
   });
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -4,6 +4,7 @@ function Bowling() {
   this.score = 0;
   this.frame = 1;
   this.roll = 1;
+  this.frameScore = 0;
 };
 
 Bowling.prototype.getCurrentScore = function () {
@@ -20,4 +21,21 @@ Bowling.prototype.getCurrentFrame = function () {
 
 Bowling.prototype.getCurrentRoll = function () {
   return this.roll;
+};
+
+Bowling.prototype.bowl = function (pins) {
+  this.roll += 1;
+  this.frameScore += pins;
+};
+
+Bowling.prototype.getFrameScore = function () {
+  return this.frameScore;
+};
+
+Bowling.prototype.isFrameComplete = function () {
+  if (this.roll === 1) {
+    return false;
+  } else {
+    return true;
+  }
 };

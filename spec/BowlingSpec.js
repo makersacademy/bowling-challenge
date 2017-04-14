e82d68273004c0704e1f46f7d3c6ff77acdc853b
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-14 19:58:36 +0100

    current frame starts at 0

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -10,7 +10,16 @@ describe('Bowling', function() {
 
   describe('initially', function() {
     it('starts at 0 points', function() {
-      expect(bowling.score).toEqual(0);
+      expect(bowling.getCurrentScore()).toEqual(0);
     });
+
+    it('starts at frame 1', function() {
+      expect(bowling.getCurrentFrame()).toEqual(1);
+    })
+  });
+
+  it('increases score with add()', function() {
+    bowling.add(1);
+    expect(bowling.getCurrentScore()).toEqual(1);
   });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,4 +2,17 @@
 
 function Bowling() {
   this.score = 0;
+  this.frame = 1;
 }
+
+Bowling.prototype.getCurrentScore = function () {
+  return this.score;
+};
+
+Bowling.prototype.getCurrentFrame = function () {
+  return this.frame;
+};
+
+Bowling.prototype.add = function (number) {
+  this.score += number;
+};

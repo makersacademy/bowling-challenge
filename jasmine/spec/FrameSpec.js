fc3e58e4e6f6720f633ac876ab59f099f010f3aa
Edward Ward <edwardwardward@gmail.com>
2017-04-15 21:25:36 +0100

    add complete frame logic"

--- jasmine/spec/FrameSpec.js
+++ jasmine/spec/FrameSpec.js
@@ -12,4 +12,42 @@ describe("Frame", function() {
   it('starts with 2 bowls', function() {
     expect(frame.bowls).toBe(2)
   });
+
+  describe('bowling', function(){
+    it('bowls the first ball(5)', function(){
+      frame.bowl_one(5)
+      expect(frame.pins).toBe(5)
+    })
+    it('bowls the first ball(10)', function(){
+      frame.bowl_one(10)
+      expect(frame.pins).toBe(0)
+    })
+    it('bowls the second ball(5)', function(){
+      frame.bowl_two(5)
+      expect(frame.pins).toBe(5)
+    })
+    it('bowls two balls(3 & 5)', function(){
+      frame.bowl_one(3)
+      frame.bowl_two(5)
+      expect(frame.pins).toBe(2)
+    })
+    it('returns 0 pins if bowl_one knocks 10 pins down', function() {
+      frame.complete(10,4)
+      expect(frame.pins).toBe(0)
+    })
+    it('returns a strike if bowl_one knocks 10 pins down', function() {
+      expect(frame.complete(10,4)).toBe('strike')
+    });
+    it('returns a spare if complete bowl knocks 10 pins down', function() {
+      expect(frame.complete(6,4)).toBe('spare')
+    });
+    it('returns 0 pins if complete bowl knocks 10 pins down', function() {
+      frame.complete(1,9)
+      expect(frame.pins).toBe(0)
+    });
+    it('returns 2 if complete bowl knocks 8 pins down', function() {
+      frame.complete(3,5)
+      expect(frame.pins).toBe(2)
+    });
+  });
 });
--- jasmine/src/Frame.js
+++ jasmine/src/Frame.js
@@ -2,3 +2,25 @@ function Frame() {
   this.pins = 10
   this.bowls = 2
 }
+
+Frame.prototype.bowl_one = function(pins) {
+  this.pins -= pins
+}
+
+Frame.prototype.bowl_two = function(pins) {
+  this.pins -= pins
+}
+
+Frame.prototype.complete = function(one,two) {
+  if(one === 10) {
+    this.bowl_one(one)
+    return 'strike' }
+    else if(one + two === 10) {
+      this.bowl_one(one)
+      this.bowl_two(two)
+      return 'spare' }
+    else {
+      this.bowl_one(one)
+      this.bowl_two(two)
+    }
+}

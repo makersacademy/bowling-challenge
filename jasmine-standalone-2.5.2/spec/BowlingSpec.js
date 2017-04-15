9c52b563ba040ce3fab0327ad87092404a03a35c
Jean-Jacques Calbayrac <calbayracjj@gmail.com>
2017-04-15 19:39:59 +0100

    Created firstThrow, secondThrow, isStrike, frameScore functions, and tested them

--- jasmine-standalone-2.5.2/spec/BowlingSpec.js
+++ jasmine-standalone-2.5.2/spec/BowlingSpec.js
@@ -6,8 +6,38 @@ describe('Bowling', function() {
   bowling = new Bowling;
 
   it('User can input one roll', function(){
-    bowling.throw(3);
-    expect(bowling.currentScore).toEqual(3);
+    bowling.firstThrow(3);
+    expect(bowling.frame.first).toEqual(3);
   });
+
+  it('User can make a second roll and both score accumulate for a frame', function(){
+    bowling.firstThrow(3);
+    bowling.secondThrow(6);
+    // debugger;
+    expect(bowling.frameScore()).toEqual(9);
+  });
+
+  it('User can make a Strike', function(){
+    bowling.firstThrow(10);
+    bowling.isStrike();
+    // debugger;
+    expect(bowling.strike).toEqual(true);
+  });
+
+  it("Check Strike is not set if 10 pins are scored on 2 throws", function(){
+    bowling.firstThrow(5);
+    bowling.secondThrow(5);
+    bowling.isStrike();
+    // debugger;
+    expect(bowling.strike).toEqual(false);
+  });
+
+  it("User can make a Spare", function(){
+    bowling.firstThrow(5);
+    bowling.secondThrow(5);
+    bowling.isSpare();
+    expect(bowling.spare).toEqual(true);
+  });
+
 
 });
--- jasmine-standalone-2.5.2/src/Bowling.js
+++ jasmine-standalone-2.5.2/src/Bowling.js
@@ -1,9 +1,32 @@
 'use strict';
 
 function Bowling(){
+  this.pins = 10
   this.currentScore = 0
+  this.frame = {first: 0, second: 0}
+  this.strike = false
 };
 
-Bowling.prototype.throw = function(score){
-  this.currentScore += score
+Bowling.prototype.firstThrow = function(score){
+  this.frame = {first: 0, second: 0}
+  this.frame.first += score
+  this.pins -= score
+};
+
+Bowling.prototype.secondThrow = function(score){
+  this.pins -= score
+  this.frame.second += score
+  this.currentScore += (this.frame.first + this.frame.second)
+};
+
+Bowling.prototype.frameScore = function () {
+  return this.frame.first + this.frame.second
+};
+
+Bowling.prototype.isStrike = function(){
+  if(this.frame.first === 10) {
+    this.strike = true
+  } else {
+    this.strike = false
+  }
 };

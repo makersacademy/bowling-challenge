4237e65cc8031a492eb3f87a77b53c2354abb8f4
Freddy Fallon <freddyfallon@gmail.com>
2017-04-15 13:10:04 +0100

    Add Javascript formatting

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,9 @@
 function Game() {
   this.framesLeft = this.DEFAULTFRAMESLEFT;
   this.frames = [];
-  this.current_frame = 1;
+  this.currentFrame = 1;
   this._frame = new Frame();
-};
+}
 
 Game.prototype.DEFAULTFRAMESLEFT = 10;
 
@@ -11,7 +11,7 @@ Game.prototype.play = function() {
   if(this._frame.rollsLeft === 2) {
     this._frame.play();
   }
-  else if(this._frame.rollsLeft == 1){
+  else if(this._frame.rollsLeft === 1){
     this._frame.play();
     this.frames.push(this._frame.score);
     this.framesLeft -= 1;

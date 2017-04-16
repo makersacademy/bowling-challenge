4d484666f9cdaacf528259b8072d79f1eb914f29
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-16 21:19:48 +0100

    second set of passing test cases for bowl

--- spec/bowlSpec.js
+++ spec/bowlSpec.js
@@ -8,40 +8,43 @@ describe("Bowl", function () {
 
   describe('throw', function(){
     it('should return 6 with input 11', function(){
-      expect(bowl.throw(11, bowl.hitPinsRound1)).toEqual(6);
+      expect(bowl.throw(11)).toEqual(5);
     });
     it('should return 7 with input -13', function(){
-      expect(bowl.throw(-13, bowl.hitPinsRound1)).toEqual(7);
+      expect(bowl.throw(-13)).toEqual(6);
     });
     it('should return 0 with input 20', function(){
-      expect(bowl.throw(20, bowl.hitPinsRound1)).toEqual(0);
+      expect(bowl.throw(20)).toEqual(0);
+    });
+    it('should return 0 with input 0', function(){
+      expect(bowl.throw(3)).toEqual(2);
     });
   });
 
   describe('knockDown', function() {
     it('should push pins to the hit pins array', function() {
       spyOn(Math, 'random').and.returnValue(0.5);
-      bowl.knockDown(1, bowl.hitPinsRound1);
-      expect(bowl.hitPinsRound1).toEqual([2, 3]);
+      bowl.knockDown(0);
+      expect(bowl.pins[1]).toEqual('X');
     });
   });
 
   describe('domino', function() {
     it('should push pins to array and return score', function() {
       spyOn(Math, 'random').and.returnValue(0.5);
-      bowl.throw(0, bowl.hitPinsRound1);
-      bowl.domino(bowl.hitPinsRound1);
-      expect(bowl.score(bowl.hitPinsRound1)).toEqual(10);
+      bowl.throw(0);
+      bowl.domino();
+      expect(bowl.score()).toEqual(10);
     });
   });
 
   describe('runninng throw twice', function() {
     it('should be able to return score after two throws', function() {
-      bowl.throw(0, bowl.hitPinsRound1);
+      bowl.throw(0);
       spyOn(Math, 'random').and.returnValue(0.5);
-      bowl.throw(5, bowl.hitPinsRound2);
-      bowl.domino(bowl.hitPinsRound2);
-      expect(bowl.score(bowl.hitPinsRound2)).toEqual(6);
+      bowl.throw(5);
+      bowl.domino();
+      expect(bowl.score()).toEqual(10);
     });
   });
 
--- src/bowl.js
+++ src/bowl.js
@@ -1,36 +1,40 @@
 function Bowl() {
-  this.hitPinsRound1 = []
-  this.hitPinsRound2 = []
-  this.pinAngle = {1: 0, 2: -5, 3: 5, 4: -10, 6: 10, 7: -15, 10: 15}
-  this.pinDomino = {1: [2, 3], 2: [4, 5], 3: [5, 6], 4: [7, 8], 5: [8, 9], 6: [9, 10]}
+  this.pins = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  this.pinAngle = {0: 0, 1: -5, 2: 5, 3: -10, 5: 10, 6: -15, 9: 15}
+  this.pinDomino = {0: [1, 2], 1: [3, 4], 2: [4, 5], 3: [6, 7], 4: [7, 8], 5: [8, 9]}
 }
 
-Bowl.prototype.throw = function(angle, pinRound) {
+Bowl.prototype.throw = function(angle) {
   if (angle < -15 || angle > 15) return 0;
-  for(i = 1; i <= 10; i++) {
-    if ([5, 8, 9].includes(i) || this.hitPinsRound1.includes(i) || this.hitPinsRound2.includes(i)) continue;
-    if (Math.abs(this.pinAngle[i] - angle) <= 2.5) {
-      pinRound.push(i);
+  for(i = 0; i <= 10; i++) {
+    if ([4, 7, 8].includes(i)) continue;
+    if (Math.abs((this.pinAngle[i]) - angle) <= 2.5) {
+      this.pins[i] = 'X';
       return i;
     };
   };
 };
 
-Bowl.prototype.pushPins = function(behindPins, pinRound, i) {
-  if ((Math.random() > 0.2) && !pinRound.includes(behindPins[i])) pinRound.push(behindPins[i]);
+Bowl.prototype.pushPins = function(behindPins, i) {
+  if (Math.random() > 0.2) this.pins[behindPins[i]] = 'X';
 }
 
-Bowl.prototype.knockDown = function(pin, pinRound) {
-  this.pushPins(this.pinDomino[pin], pinRound, 0)
-  this.pushPins(this.pinDomino[pin], pinRound, 1)
+Bowl.prototype.knockDown = function(pin) {
+  this.pushPins(this.pinDomino[pin], 0)
+  this.pushPins(this.pinDomino[pin], 1)
 }
 
-Bowl.prototype.domino = function(pinRound) {
-  for(i = 0; i <= 6; i++) {
-    if (pinRound.includes(i)) this.knockDown(i, pinRound);
+Bowl.prototype.domino = function() {
+  for(i = 0; i < 6; i++) {
+    if (this.pins[i] == 'X')
+    this.knockDown(i);
   }
 }
 
-Bowl.prototype.score = function(pinRound) {
-  return pinRound.length;
+Bowl.prototype.score = function() {
+  score = 0
+  for(i = 0; i < 10; i++) {
+    if (this.pins[i] == 'X') score++
+  }
+  return score;
 }

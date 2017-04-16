b487508d2f671867190add99758e5fb5185ead0c
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-16 17:06:59 +0100

    passing set of test cases for two turn logic

--- spec/bowlSpec.js
+++ spec/bowlSpec.js
@@ -7,8 +7,42 @@ describe("Bowl", function () {
   });
 
   describe('throw', function(){
-    it('should return a value between 1 and 10', function(){
-      expect(bowl.throw(11)).toEqual(6);
+    it('should return 6 with input 11', function(){
+      expect(bowl.throw(11, bowl.hitPinsRound1)).toEqual(6);
+    });
+    it('should return 7 with input -13', function(){
+      expect(bowl.throw(-13, bowl.hitPinsRound1)).toEqual(7);
+    });
+    it('should return 0 with input 20', function(){
+      expect(bowl.throw(20, bowl.hitPinsRound1)).toEqual(0);
     });
   });
+
+  describe('knockDown', function() {
+    it('should push pins to the hit pins array', function() {
+      spyOn(Math, 'random').and.returnValue(0.5);
+      bowl.knockDown(1, bowl.hitPinsRound1);
+      expect(bowl.hitPinsRound1).toEqual([2, 3]);
+    });
+  });
+
+  describe('domino', function() {
+    it('should push pins to array and return score', function() {
+      spyOn(Math, 'random').and.returnValue(0.5);
+      bowl.throw(0, bowl.hitPinsRound1);
+      bowl.domino(bowl.hitPinsRound1);
+      expect(bowl.score(bowl.hitPinsRound1)).toEqual(10);
+    });
+  });
+
+  describe('runninng throw twice', function() {
+    it('should be able to return score after two throws', function() {
+      bowl.throw(0, bowl.hitPinsRound1);
+      spyOn(Math, 'random').and.returnValue(0.5);
+      bowl.throw(5, bowl.hitPinsRound2);
+      bowl.domino(bowl.hitPinsRound2);
+      expect(bowl.score(bowl.hitPinsRound2)).toEqual(6);
+    });
+  });
+
 });
--- src/bowl.js
+++ src/bowl.js
@@ -1,15 +1,36 @@
 function Bowl() {
-  this.hitPins = []
-  this.pins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  this.hitPinsRound1 = []
+  this.hitPinsRound2 = []
   this.pinAngle = {1: 0, 2: -5, 3: 5, 4: -10, 6: 10, 7: -15, 10: 15}
-  this.pinKnockOn = {1: [2, 3], 2: [4, 5], 3: [5, 6], 4: [7, 8], 5: [8, 9], 6: [9, 10]}
+  this.pinDomino = {1: [2, 3], 2: [4, 5], 3: [5, 6], 4: [7, 8], 5: [8, 9], 6: [9, 10]}
 }
 
-Bowl.prototype.throw = function(angle) {
-  for (i = 1, i <= 10, i++) {
-    if ([5, 8, 9].contains(i)) continue;
-    if ((pinAngle[i] - angle) <= 2.5)
+Bowl.prototype.throw = function(angle, pinRound) {
+  if (angle < -15 || angle > 15) return 0;
+  for(i = 1; i <= 10; i++) {
+    if ([5, 8, 9].includes(i) || this.hitPinsRound1.includes(i) || this.hitPinsRound2.includes(i)) continue;
+    if (Math.abs(this.pinAngle[i] - angle) <= 2.5) {
+      pinRound.push(i);
       return i;
     };
   };
 };
+
+Bowl.prototype.pushPins = function(behindPins, pinRound, i) {
+  if ((Math.random() > 0.2) && !pinRound.includes(behindPins[i])) pinRound.push(behindPins[i]);
+}
+
+Bowl.prototype.knockDown = function(pin, pinRound) {
+  this.pushPins(this.pinDomino[pin], pinRound, 0)
+  this.pushPins(this.pinDomino[pin], pinRound, 1)
+}
+
+Bowl.prototype.domino = function(pinRound) {
+  for(i = 0; i <= 6; i++) {
+    if (pinRound.includes(i)) this.knockDown(i, pinRound);
+  }
+}
+
+Bowl.prototype.score = function(pinRound) {
+  return pinRound.length;
+}

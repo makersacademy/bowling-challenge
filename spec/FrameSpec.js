48206c2cd536f4e7060536bea6337fba60994480
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-17 14:33:34 +0100

    Added method to show if frame is complete

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -7,4 +7,12 @@ describe("Frame", function(){
   it("initializes with 10 pins remaining", function() {
     expect(frame.pinsremaining).toEqual(10);
   });
+
+  describe("bowl", function(){
+    it("knocks down a random number of pins", function() {
+      spyOn(Math, 'floor').and.returnValue(5);
+      frame.bowl();
+      expect(frame.pinsremaining).toEqual(5)
+    });
+  });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,3 +1,12 @@
 function Frame() {
   this.pinsremaining = 10
+  this.currentFrame = []
+}
+
+Frame.prototype.bowl = function() {
+  this.pinsremaining -= Math.floor(Math.random() * 11);
+}
+
+Frame.prototype.isComplete = function() {
+  if this.pinsremaining === 0 || this.currentFrame.length === 2
 }

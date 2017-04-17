62c0630292d19f927a6b657e925ca1854a3955ca
Colin Turner <colinturner34@gmail.com>
2017-04-17 17:59:07 +0100

    Simplify method names

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -9,7 +9,7 @@ describe ('Bowling', function(){
 
   describe('intialize', function(){
     it('score starts at zero', function(){
-      expect(bowling.getCurrentScore()).toEqual(0);
+      expect(bowling.getTotal()).toEqual(0);
     });
 
     it('isFrameComplete returns false', function(){
@@ -17,29 +17,22 @@ describe ('Bowling', function(){
     });
   });
 
-  describe('add', function(){
-    it('score can have be added to', function(){
-      bowling.add(1);
-      expect(bowling.getCurrentScore()).toEqual(1);
-    });
-  });
-
   describe('frame', function(){
     it('begins on Frame #1', function(){
-      expect(bowling.getCurrentFrame()).toEqual(1);
+      expect(bowling.getFrame()).toEqual(1);
     });
   });
 
   describe('roll', function(){
     it('begins on Roll #1', function(){
-      expect(bowling.getCurrentRoll()).toEqual(1);
+      expect(bowling.getRoll()).toEqual(1);
     });
   });
 
   describe('bowl', function(){
     it('increments roll by 1', function(){
       bowling.bowl(6);
-      expect(bowling.getCurrentRoll()).toEqual(2);
+      expect(bowling.getRoll()).toEqual(2);
     });
 
     it('getFrameScore returns the number of pins knocked down on a roll', function(){
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,35 +1,31 @@
 'use strict';
 
 function Bowling() {
-  this.score = 0;
   this.frame = 1;
   this.roll = 1;
   this.frameScore = 0;
+  this.total = 0;
 };
 
-Bowling.prototype.getCurrentScore = function () {
-  return this.score;
-};
-
-Bowling.prototype.add = function (pins) {
-  this.score += pins;
-};
-
-Bowling.prototype.getCurrentFrame = function () {
+Bowling.prototype.getFrame = function () {
   return this.frame;
 };
 
-Bowling.prototype.getCurrentRoll = function () {
+Bowling.prototype.getRoll = function () {
   return this.roll;
+};
+
+Bowling.prototype.getFrameScore = function () {
+  return this.frameScore;
+};
+
+Bowling.prototype.getTotal = function () {
+  return this.total;
 };
 
 Bowling.prototype.bowl = function (pins) {
   this.roll += 1;
   this.frameScore += pins;
-};
-
-Bowling.prototype.getFrameScore = function () {
-  return this.frameScore;
 };
 
 Bowling.prototype.isFrameComplete = function () {

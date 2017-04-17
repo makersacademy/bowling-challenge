5a6b6e0ef3c04d69ed92e0c970dbff0c75ff9b25
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-17 11:06:22 +0100

    add bowl method

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -9,4 +9,25 @@ describe("Frame", function() {
   it("initializes with ten pins", function() {
     expect(frame._pins).toEqual(10);
   });
-  })
+
+  it("knocks down random number of pins", function() {
+    spyOn(Math, 'floor').and.returnValue(4);
+    frame.bowl();
+    expect(frame._pins).toEqual(6);
+  });
+
+  it("stores number of pins knocked down in first bowl", function() {
+    spyOn(Math, 'floor').and.returnValue(4);
+    frame.bowl();
+    expect(frame._bowls).toEqual([4]);
+  });
+
+  it("stores number of pins knocked down in second bowl", function() {
+    spyOn(Math, 'floor').and.returnValue(4);
+    frame.bowl();
+    frame.bowl();
+    expect(frame._bowls).toEqual([4, 4]);
+  });
+
+  
+  });
--- src/Frame.js
+++ src/Frame.js
@@ -2,4 +2,11 @@
 
 function Frame() {
   this._pins = 10;
-}
+  this._bowls = []
+};
+
+Frame.prototype.bowl = function() {
+  var fallenPins = Math.floor((Math.random() * 11));
+  this._pins = this._pins - (fallenPins);
+  this._bowls.push(fallenPins);
+};

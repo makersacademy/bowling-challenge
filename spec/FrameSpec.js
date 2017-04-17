8c7e217bc364146366f038e65418fff763717c40
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-17 12:59:00 +0100

    refactored update frame method

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -7,27 +7,31 @@ describe("Frame", function() {
   });
 
   it("initializes with ten pins", function() {
-    expect(frame._pins).toEqual(10);
+    expect(frame.remainingPins).toEqual(10);
   });
 
   it("knocks down random number of pins", function() {
     spyOn(Math, 'floor').and.returnValue(4);
     frame.bowl();
-    expect(frame._pins).toEqual(6);
+    expect(frame.remainingPins).toEqual(6);
   });
 
   it("stores number of pins knocked down in first bowl", function() {
     spyOn(Math, 'floor').and.returnValue(4);
     frame.bowl();
-    expect(frame._bowls).toEqual([4]);
+    expect(frame.currentFrame).toEqual([4]);
   });
 
   it("stores number of pins knocked down in second bowl", function() {
     spyOn(Math, 'floor').and.returnValue(4);
     frame.bowl();
     frame.bowl();
-    expect(frame._bowls).toEqual([4, 4]);
+    expect(frame.currentFrame).toEqual([4, 4]);
   });
 
-  
+  it("completes the frame if first bowl knocks down ten pins", function() {
+    spyOn(Math, 'floor').and.returnValue(10);
+    frame.bowl();
+    expect(frame.currentFrame).toEqual([10, 0]);
+    });
   });
--- src/Frame.js
+++ src/Frame.js
@@ -1,12 +1,24 @@
 "use strict";
 
 function Frame() {
-  this._pins = 10;
-  this._bowls = []
+  this.remainingPins = 10;
+  this.currentFrame = []
 };
 
 Frame.prototype.bowl = function() {
   var fallenPins = Math.floor((Math.random() * 11));
-  this._pins = this._pins - (fallenPins);
-  this._bowls.push(fallenPins);
+  this.remainingPins = this.remainingPins - (fallenPins);
+  this.updateFrame();
+};
+
+Frame.prototype.updateFrame = function() {
+  if(this.currentFrame[0] === undefined && this.remainingPins !== 0) {
+    this.currentFrame.push(10-this.remainingPins)}
+  else if(
+    this.currentFrame[0] === undefined && this.remainingPins === 0) {
+    this.currentFrame.push(10, 0)}
+  else {
+    var previousBowl = this.currentFrame[0];
+    this.currentFrame.push(10-(this.remainingPins + previousBowl))
+  }
 };

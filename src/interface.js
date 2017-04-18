5723c91a4932b05febc963c55a92785392bc6c00
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-18 21:13:05 +0100

    added function to update score

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
   var game = new Game();
 
+
   $(document).ready(function() {
     $('#play').click(function() {
       game.play();
     });
   });
+
+  function updatescore() {
+  $('#scores').table(game.scores);
+}
 
 
 

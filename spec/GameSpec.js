c5dbe57eb4407f103821e389f87f227c14aacc4e
Vivien Tang <vivientang@Viviens-MacBook-Pro.local>
2017-04-22 18:32:55 +0100

    add update score function

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,31 +3,76 @@
 describe('Game', function() {
   var game;
   var frame;
+  var strike;
   beforeEach(function() {
     game = new Game();
-    frame = new Frame();
+    frame = [4,5];
+    strike = [10,0];
   });
 
-  it('has a starting score of 0', function() {
-    expect(game.score).toEqual(0);
+  it('starts off with an empty scoresheet', function() {
+    expect(game.scoresheet).toEqual([[]]);
   });
 
-  it('starts off with no frames', function() {
-    expect(game.frames).toEqual([]);
+  it('has a turn counter', function() {
+    expect(game.turn).toEqual(0);
   });
 
-  it('adds frames to the game', function() {
+  it('adds one frame to the game', function() {
     game.addFrames(frame);
-    expect(game.frames).toEqual([frame]);
+    expect(game.scoresheet).toEqual([[frame]]);
   });
 
-  it('updates game score', function() {
-    spyOn(Math, 'floor').and.returnValue(4)
+  it('adds the score of the turn to the game', function() {
     game.addFrames(frame);
-    game.frames[0].bowl();
-    game.frames[0].bowl();
-    game.updateScore();
-    expect(game.score).toEqual(8);
+    game.updateScore(frame);
+    expect(game.scoresheet).toEqual([[frame,9]]);
   });
 
+  it('increases the turn counter by one', function() {
+    game.nextTurn();
+    expect(game.turn).toEqual(1);
+  });
+
+  it('updates score accordingly when player bowls a strike', function() {
+    game.addFrames(strike);
+    game.updateScore(strike);
+    game.nextTurn();
+    game.addFrames(frame);
+    game.updateScore(frame);
+    expect(game.scoresheet).toEqual([[strike, 19],[frame,9]]);
+  });
+
+
+  // it('calculates frame score', function() {
+  //   spyOn(Math, 'floor').and.returnValue(4)
+  //   game.addFrames(frame);
+  //   game.currentPlayingFrame.bowl();
+  //   game.currentPlayingFrame.bowl();
+  //   game.calculateFrameScore()
+  // })
+
+  // it('updates game score', function() {
+  //   spyOn(Math, 'floor').and.returnValue(4)
+  //   game.addFrames(frame);
+  //   game.frames[0].bowl();
+  //   game.frames[0].bowl();
+  //   game.updateScore();
+  //   expect(game.score).toEqual(8);
+  // });
+
+  // it('returns true if frame is a strike', function() {
+  //   spyOn(Math, 'floor').and.returnValue(10);
+  //   game.addFrames(frame);
+  //   game.frames[0].bowl();
+  //   expect(game._strike()).toBe(true);
+  // });
+  //
+  // it('adds bonus if frame is a strike', function() {
+  //   spyOn(Math, 'floor').and.returnValue(5);
+  //   game.addFrames([10, 0]);
+  //   game.frames[0].bowl();
+  //   game.addFrames(frame);
+  //   expect(game.score).toEqual
+  // })
 });
--- src/Game.js
+++ src/Game.js
@@ -3,16 +3,64 @@
 'use strict';
 
 function Game() {
-  this.score = 0
-  this.frames = []
+  // this.frames = []
+  this.turn = 0
+  this.scoresheet = [[]]
 }
 
-Game.prototype.addFrames = function(frame) {
-  this.frames.push(frame);
+Game.prototype.viewScoresheet = function() {
+  return this.scoresheet;
 };
 
-Game.prototype.updateScore = function() {
-  for (var i = 0; i<this.frames.length; i++) {
-    this.score = this.score + (this.frames[i].currentFrame[0] + this.frames[i].currentFrame[1]);
-  };
+Game.prototype.addFrames = function(frame) {
+  this.scoresheet[this.turn].push(frame);
 };
+
+Game.prototype.updateScore = function(frame) {
+  var sum = frame[0] + frame[1];
+  this.scoresheet[this.turn].push(sum);
+};
+
+Game.prototype.nextTurn = function() {
+  this.turn += 1;
+};
+
+
+// Game.prototype.currentPlayingFrame = function() {
+//   this.frames[this.frames.length-1];
+// };
+
+// Game.prototype.currentTurn = function() {
+//   return(this.frames[this.frames.length-1]);
+// };
+//
+// Game.prototype.currentPlayingFrame = function() {
+//   return(this.frames[this.frames.length-1][0]);
+// };
+//
+// Game.prototype.currentActualFrame = function() {
+//   return(this.frames[this.frames.length-1][0][0]);
+// };
+//
+//
+// Game.prototype.currentFirstBonus = function() {
+//   return(this.frames[this.frames.length-1][1]);
+// };
+//
+// Game.prototype.currentSecondBonus = function() {
+//   return(this.frames[this.frames.length-1][2]);
+// };
+
+
+
+// Game.prototype.updateScore = function() {
+//   for (var i = 0; i<this.frames.length; i++) {
+//     this.score = this.score + (this.frames[i].currentFrame[0] + this.frames[i].currentFrame[1]);
+//   };
+// };
+//
+// Game.prototype._strike = function() {
+//   if(this.frames[0].currentFrame[0] === 10){
+//     return true;
+//   } else {return false;}
+// };

fe067ea3217151269049fb407c299b87feaac973
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-23 11:08:09 +0100

    Interface set up, refactor constants, refactor src

--- interface.js
+++ interface.js
@@ -1,24 +1,15 @@
-// $(document).ready(function() {
-//   var bowlingGame = new BowlingGame();
+$(document).ready(function() {
+  var bowlingGame = new BowlingGame();
 
-// // function addScore(score) {
-// //   $('.frame > p') //select all the elements with class .frames
-// //     .eq(bowlingGame.frames.length - 1) //limit selection only to element 5
-// //     .text(score); //set text to value of pins
-// // }
+  $('.button-a').on('click', function() {
+    var currentValue = $(".button-a").text();
+    var pin = parseInt(parseFloat(currentValue));
+    bowlingGame.firstShot(pin);
+  });
 
+  $('.buttonb').on('click', function() {
+    // $('.firstShot').text('1');
+    bowlingGame.secondShot(2);
+  });
 
-//   // $(".one").click(function(arg) {
-//   //   bowlingGame.firstShot(arg);
-//   // });
-//   $('#one').click(function(){
-//     var passThis = $('input[name="one"]').val();
-//     bowlingGame.firstShot(passThis = 1);
-//   });
-
-//   $('#two').click(function(){
-//     var passThis = $('input[name="one"]').val();
-//     bowlingGame.secondShot(passThis = 2);
-//   });
-
-// });
+});
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,89 +2,86 @@
    'use strict';
 }());
 
+const STRIKE_VALUE = 10;
+const SPARE_VALUE = 10;
+
 function BowlingGame() {
   this.frames = [];
   this.total = 0;
   this.spare = false;
   this.strike = false;
-  this.shots = 0;
-  // this.secondScore = 0;
-  // this.strikeValue = 10;
-  // this.spareValue = 10;
+  this.firstScore = 0;
+  this.secondScore = 0;
+  this.strikeValue = 10;
+
 }
 
-BowlingGame.prototype.addShot = function(pins) {
-  this.shots += pins;
+BowlingGame.prototype.firstShot = function(pins) {
+  this.firstScore += pins; //10
   this.total += pins;
-  // this.nextScore += pins;
-  // if(this.spare === true) {
-  //   this.addScoresWithSpare();
-  // }
-  // this.ifFirstShotIsTen();
-
-  // $('<div>')
-  //   .text(pins)
-  //   .appendTo('body')
+  if (this.spare) {
+    this.addScoresWithSpare();
+  }
+  this.handleStrikes(); //10
 }
 
-// BowlingGame.prototype.secondShot = function(pins) {
-//   this.secondScore += pins;
-//   this.total += pins;
-//   this.ifFirstAndSecondAreTen();
-//   this.checkTypeOfScore();
-//   this.resetScores();
+BowlingGame.prototype.secondShot = function(pins) {
+  this.secondScore += pins;
+  this.total += pins;
+  this.handleSpares();
+  this.checkTypeOfScore();
+  this.resetScores();
+}
+
+BowlingGame.prototype.checkTypeOfScore = function(test) {
+  if(this.strike) {
+      this.addFrame(STRIKE_VALUE + this.firstScore + this.secondScore);
+      this.addFrame(this.total);
+      this.strike = !this.strike;
+    } else if(this.spare) {
+        this.resetScores();
+    } else {
+      if(this.frames.length > 0) {
+        this.addFrame(this.total);
+      } else {
+        this.addFrame(this.firstScore + this.secondScore);
+      }
+    }
+}
+
+BowlingGame.prototype.addFrame = function(score) {
+  this.frames.push({ score: score });
+
+  // //assuming the frames are already on the html
+  // $('.frame > p') //select all the elements with class .frames
+  //   .eq(this.frames.length - 1) //limit selection only to element 5
+  //   .text(score); //set text to value of pins
+
+}
 
 
-    // $('<div>')
-    // .text(pins)
-    // .appendTo('body')
-// }
-// BowlingGame.prototype.checkTypeOfScore = function(test) {
-//   if(this.strike === true) {
-//       this.addFrame(this.strikeValue + this.firstScore + this.secondScore);
-//       this.addFrame(this.total + this.firstScore + this.secondScore);
-//       this.strike = !this.strike;
-//     } else if(this.spare === true) {
-//         this.resetScores();
-//     } else {
-//       if(this.frames.length > 0) {
-//         this.addFrame(this.total + this.firstScore + this.secondScore);
-//       } else {
-//         this.addFrame(this.firstScore + this.secondScore);
-//       }
-//     }
-// }
+BowlingGame.prototype.addScoresWithSpare = function() {
+  if(this.spare) {
+      this.addFrame(SPARE_VALUE + this.firstScore);
+      this.addFrame(this.total);
+      this.spare = !this.spare;
+  }
+}
 
-// BowlingGame.prototype.addFrame = function(score) {
-//   this.frames.push({ score: score });
+BowlingGame.prototype.resetScores = function() {
+  this.firstScore = 0;
+  this.secondScore = 0;
+}
 
-//   //assuming the frames are already on the html
+BowlingGame.prototype.handleStrikes = function() {
+  if(this.firstScore === 10) {
+    this.strike = !this.strike;
+    this.resetScores();
+  }
+}
 
-// }
-
-
-// BowlingGame.prototype.addScoresWithSpare = function() {
-//   if(this.spare === true) {
-//       this.addFrame(this.spareValue + this.firstScore);
-//       this.addFrame(this.total + this.firstScore + this.secondScore);
-//       this.spare = !this.spare;
-//   }
-// }
-
-// BowlingGame.prototype.resetScores = function() {
-//   this.firstScore = 0;
-//   this.secondScore = 0;
-// }
-
-// BowlingGame.prototype.ifFirstShotIsTen = function() {
-//   if(this.firstScore === 10) {
-//     this.strike = !this.strike;
-//     this.resetScores();
-//   }
-// }
-
-// BowlingGame.prototype.ifFirstAndSecondAreTen = function() {
-//   if(this.firstScore + this.secondScore === 10) {
-//     this.spare = !this.spare;
-//   }
-// }
+BowlingGame.prototype.handleSpares = function() {
+  if(this.firstScore + this.secondScore === 10) {
+    this.spare = !this.spare;
+  }
+}

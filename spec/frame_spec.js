57db54807fa201bd55f3b4803a7a3052453673d1
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-23 09:49:52 +0100

    Add tests scores with strikes

--- spec/frame_spec.js
+++ spec/frame_spec.js
@@ -17,4 +17,29 @@ describe("A frame of bowling", function(){
     var next_frame = new Frame([3,6]);
     expect(frame.total(next_frame)).toEqual(19);
   });
+
+  it("calculates a total for two strikes in a row", function(){
+    var frame = new Frame([10]);
+    var next = new Frame([10]);
+    var next_to_next_frame = new Frame([3, 4]);
+    expect(frame.total(next, next_to_next_frame)).toEqual(23);
+  });
+
+  it("calculates three strikes in a row", function(){
+    var frame = new Frame([10]);
+    var next = new Frame([10]);
+    var next_to_next_frame = new Frame([10]);
+    expect(frame.total(next, next_to_next_frame)).toEqual(30);
+  });
+
+  it ("calculates a strike in the final frame", function(){
+    var frame = new Frame([10,10,10]);
+    expect(frame.total()).toEqual(30);
+  });
+
+  it ("calculates a strike in the one before final frame", function(){
+    var frame = new Frame([10]);
+    var next = new Frame([10,10,10]);
+    expect(frame.total(next)).toEqual(30);
+  });
 });

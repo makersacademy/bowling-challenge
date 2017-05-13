33738689790b6be42cc4c7fd755f61a056b5fe99
Paula <pmuldoon@umich.edu>
2017-05-13 15:30:35 +0100

    added test for roll1

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,12 +1,13 @@
 describe("Player", function() {
   var player;
+  var frames = 9;
 
   beforeEach(function() {
     player = new Player();
   });
 
-  it('should be able to bowl and score 0 points', function () {
-    player.bowl();
-    expect(player.score).toEqual(0);
+  it('should be able to bowl the first roll and score 1 point', function () {
+    player.roll1();
+    expect(player.score).toEqual(1);
   });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,6 +1,11 @@
 function Player() {
   this.score = 0
+  this.frames = 9
 }
 Player.prototype.bowl = function() {
-   this.score = 0
+   this.roll1();
+};
+
+Player.prototype.roll1 = function(){
+  this.score = 1
 };

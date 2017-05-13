8bc9e8810d2e81ae4399ac1f46032ed2d069901f
Timothy Robinson <timmyrobby@gmail.com>
2017-05-13 15:22:04 +0100

    Adds Jasmine testing framework

---
+++ spec/ScoreSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe("Score", function() {
+  var score;
+
+  beforeEach(function() {
+    score = new Score();
+  });
+
+  it('is defined', function() {
+    expect(score).toBeDefined();
+  });
+});

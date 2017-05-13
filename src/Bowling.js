2afca02b0a4633a08fea835416f8e8783c0bbd0f
Paula <pmuldoon@umich.edu>
2017-05-13 18:39:03 +0100

    added if statement to finish game

--- src/Bowling.js
+++ src/Bowling.js
@@ -5,31 +5,32 @@ function Player() {
   this.frames = 10;
   this.roll1Score = 0;
   this.roll2Score = 0;
-  this.frameScore = 0
+  this.frameScore = 0;
 }
 
-Player.prototype.bowl = function() {
-   this.roll1(1);
-   this.roll2(2);
-   this.frames -= 1;
-   this.finish();
+Player.prototype.bowl = function () {
+  this.roll1(1);
+  this.roll2(2);
+  this.frames -= 1;
+  if (this.frames === 5) {
+    this.finish();
+  }
 };
 
 Player.prototype.roll1 = function (number) {
-  this.roll1Score = number
+  this.roll1Score = number;
 };
 
 Player.prototype.roll2 = function (number) {
-  this.roll2Score = number
+  this.roll2Score = number;
   this.frameScore  = this.roll2Score + this.roll1Score;
-  this.score += this.frameScore
+  this.score += this.frameScore;
 };
 
 Player.prototype.spare = function (number) {
   return 12;
+};
 
-}
-
-Player.prototype.finish = function() {
+Player.prototype.finish = function () {
   return true;
 };

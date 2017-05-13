3e6ee384a51ef49b8357b7d1f5098ccdc843c57f
JayWebDevCom <jaiye.s@gmail.com>
2017-05-13 20:09:49 +0100

    first commit

---
+++ spec/bowling_spec.js
@@ -0,0 +1,15 @@
+describe("Thermostat", function() {
+
+  beforeEach(function() {
+
+  });
+
+  describe("intialization", function() {
+
+    it("is not decided yet", function(){
+      expect(true).toBe(true);
+    });
+
+  });
+
+});

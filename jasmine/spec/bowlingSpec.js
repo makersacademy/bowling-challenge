5380d4906fde40b0a79ee7ac5768e679ad6ea2e2
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-14 13:35:45 +0100

    restructures bowling functions and refactors code

--- jasmine/spec/bowlingSpec.js
+++ jasmine/spec/bowlingSpec.js
@@ -2,41 +2,25 @@ describe('Bowling', function(){
 
   beforeEach(function(){
     bowling = new Bowling()
-    array = [0,1,2,3,4,5,6,7,8,9,10];
     spyOn(Math,'random').and.returnValue(0.5);
   })
 
   describe('randomBowl', function(){
     it('produces a number from 0 to 10', function(){
-      expect(array).toContain(_randomBowl())
+      expect(_randomBowl()).toEqual(5)
     });
   });
 
-  // describe('Bowling', function(){
-  //   it('it is initiated with four variables with a value of zero', function(){
-  //
-  //     expect(bowling.firstBowl).toEqual(0)
-  //     expect(bowling.secondBowl).toEqual(0)
-  //     expect(bowling.extraBowl).toEqual(0)
-  //     expect(bowling.strikeBowl).toEqual(0)
-  //   });
-  // });
+  describe('Bowling', function(){
+    it('it is initiated with a an array of bowls', function(){
+      expect(bowling.bowlCount).toEqual([])
+    });
+  });
 
   describe('#frame', function(){
-    it('sets firstBowl to a random number', function(){
+    it('inputs a bowl into the array', function(){
       bowling.frame()
-      expect(array).toContain(bowling.firstBowl)
-    });
-
-    it('sets the secondBowl to random number', function(){
-      bowling.frame()
-      expect(array).toContain(bowling.secondBowl)
+      expect(bowling.bowlCount).toContain(5)
     });
   });
-
-  // describe('#frameScoreCheck', function(){
-  //   it('checks the status of a frame', function(){
-  //     expect(_frameScoreCheck()).toEqual('spare')
-  //   });
-  // });
 });
--- jasmine/src/bowling.js
+++ jasmine/src/bowling.js
@@ -1,22 +1,11 @@
 function Bowling(){
-  this.firstBowl
-  this.secondBowl
-  this.extraBowl
-  this.strikeBowl
-};
+  this.bowlCount = []
+ };
 
 Bowling.prototype.frame = function () {
-  this.firstBowl = _randomBowl()
-  this.secondBowl = _randomBowl()
+  this.bowlCount.push(_randomBowl())
 };
 
-// function _frameScoreCheck() {
-//   if(this.firstBowl === 10) {
-//     return 'strike'
-//   } else if (this.firstBowl + this.secondBowl === 10 ) {
-//     return 'spare'
-//   };
-// };
 function _randomBowl() {
   return Math.floor(Math.random()*10)+0
 };

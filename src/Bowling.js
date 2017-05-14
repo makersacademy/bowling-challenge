9d1e7cfeee5c92f28d19ad1f37ee9ed51fe0186c
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-14 21:02:36 +0100

    Refactor to rename spareArray to currentArray

--- src/Bowling.js
+++ src/Bowling.js
@@ -9,14 +9,11 @@ function Bowling () {
   this._bonusCounter = 0;
   this._isStrike = false;
   this._isSpare = true;
-  this._spareArray = [];
+  this._currentFrame = [];
   this._isFirstBowlOfFrame = true;
 }
 Bowling.prototype.setFrames = function (value) {
   this._frames.push(value);
-};
-Bowling.prototype.getBonusCounter = function () {
-  return this._bonusCounter;
 };
 Bowling.prototype.setBonusCounter = function (value) {
   this._bonusCounter += value;
@@ -30,8 +27,8 @@ Bowling.prototype.setIsStrike = function (value) {
 Bowling.prototype.setIsSpare = function (value) {
   this._isSpare = value;
 };
-Bowling.prototype.setSpareArray = function (value) {
-  this._spareArray = value;
+Bowling.prototype.resetCurrentFrame = function (value) {
+  this._currentFrame = value;
 };
 Bowling.prototype.toggleFirstBowlOfGame = function ( value ) {
   this._isFirstBowlOfFrame = this._isFirstBowlOfFrame ? false : true;
@@ -51,12 +48,17 @@ Bowling.prototype.randomNumberOfPins = function () {
 };
 Bowling.prototype.finalScore = function () {
   this.frameTotal = 0;
-  for(var i in this._frames) { this.frameTotal += this._frames[i]; }
+
+  for( var i in this._frames )
+  { this.frameTotal += this._frames[i]; }
+
   this.frameTotal += this._bonusPoints;
-  if (  this.frameTotal !== 0 )
-    return 'Final score is! ' + (this.frameTotal);
-  else
+
+  if ( this.frameTotal === 0 )
     return 'Gutter game! Better luck next time...';
+  else
+    return 'Final score is! ' + (this.frameTotal);
+
 };
 Bowling.prototype.bowl = function (number) {
   if(this._frameCounter === this.FRAME_COUNT_LIMIT) {
@@ -64,10 +66,12 @@ Bowling.prototype.bowl = function (number) {
   }
 
   this.setIsStrike(false);
+
   var pins = number || this.randomNumberOfPins();
+
   this.toggleFirstBowlOfGame();
 
-  if (this.getBonusCounter() >= 1) {
+  if (this._bonusCounter >= 1) {
     this.setBonusPoints(pins);
     this.setBonusCounter(-1);
   }
@@ -88,16 +92,17 @@ Bowling.prototype.strike = function () {
   this.calculateFrameCount();
 };
 Bowling.prototype.spareChecker = function (pins) {
-  this._spareArray.push(pins);
-  if (this._spareArray.length === 1) {
+  this._currentFrame.push(pins);
+  if (this._currentFrame.length === 1) {
     this.setIsSpare(false);
-  } else if ((this._spareArray.length === 2) && (this._spareArray[0] + this._spareArray[1] !== this.STRIKE)) {
+  } else if ((this._currentFrame.length === 2) &&
+    (this._currentFrame[0] + this._currentFrame[1] !== this.STRIKE)) {
     this.setIsSpare(false);
-    this.setSpareArray([]);
+    this.resetCurrentFrame([]);
   } else {
     console.log('Spare! /');
     this.setIsSpare(true);
     this.setBonusCounter(1);
-    this.setSpareArray([]);
+    this.resetCurrentFrame([]);
   }
 };

dd88854c3babdbf876765e10e4b912cec1d040ba
Alex Law <achlawuk@gmail.com>
2017-05-14 19:48:13 +0100

    Refactorised spare

--- src/Bowling.js
+++ src/Bowling.js
@@ -9,14 +9,27 @@ Bowling.prototype.roll = function(pins) {
 Bowling.prototype.score = function() {
   var result = 0;
   var rollIndex = 0;
+  var game = this;
 
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10) {
-      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
-    } else {
-      result += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    if (isSpare()) {
+      result += getSpareScore();
+      } else {
+      result += getNormalScore();
     }
     rollIndex += 2;
   }
   return result;
+
+  function isSpare() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10;
+  }
+
+  function getSpareScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  }
+
+  function getNormalScore() {
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
+  }
 };

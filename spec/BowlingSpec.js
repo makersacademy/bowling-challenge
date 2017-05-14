94cff2d5db150cf678c05f8493e13a26e301501f
Alex Law <achlawuk@gmail.com>
2017-05-14 17:28:36 +0100

    Refactored tests

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,19 +1,25 @@
 describe('Bowling', function() {
 
+  var game;
+
+  beforeEach(function() {
+    game = new Bowling();
+  });
+
   it('can roll a gutter game', function() {
-    var game = new Bowling();
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollMany(0, 20);
     expect(game.score()).toBe(0);
   });
 
   it('can roll a game of ones', function() {
-    var game = new Bowling();
-    for (var i = 0; i < 20; i++) {
-      game.roll(1)
-  	}
+    rollMany(1,20);
   	expect(game.score()).toBe(20);
   });
+
+  var rollMany = function(pins, rolls) {
+    for (var i =0; i < rolls; i++){
+      game.roll(pins);
+    }
+  };
 
 });

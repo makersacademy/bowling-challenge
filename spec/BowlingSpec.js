d08952049b2f452f1ee20bb56ceaea66528dc2b1
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-14 11:00:41 +0100

    Add _frameCounter property

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -6,9 +6,12 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
-  it('has an array storing results', function() {
+  it('has an array storing results, empty by default', function() {
     expect(bowling.getFrames()).toEqual([]);
   });
 
+  it('has a frame counter, 0 by default', function() {
+    expect(bowling.getFrameCounter()).toEqual(0);
+  });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,8 +2,13 @@
 
 function Bowling () {
   this._frames = [];
+  this._framecounter = (this.getFrames()) / 2;
 }
 
 Bowling.prototype.getFrames = function () {
   return this._frames;
+};
+
+Bowling.prototype.getFrameCounter = function () {
+  return this._framecounter;
 };

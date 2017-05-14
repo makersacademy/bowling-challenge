f1e41ce912ea7058d9482109e66695dc3f050ef2
Alex Law <achlawuk@gmail.com>
2017-05-14 17:56:32 +0100

    Add failing test roll a spare

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -16,6 +16,14 @@ describe('Bowling', function() {
   	expect(game.score()).toBe(20);
   });
 
+  it('can roll a spare', function() {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(0, 17);
+    expect(game.score()).toBe(16);
+  });
+
   var rollMany = function(pins, rolls) {
     for (var i =0; i < rolls; i++){
       game.roll(pins);

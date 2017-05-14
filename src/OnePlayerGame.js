3f0ce2ea8241015d932491695538aaa1bdbda87c
Alex Chalk <alexchalk17@gmail.com>
2017-05-14 17:55:16 -0400

    Refactor OnePlayerGame.js

--- src/OnePlayerGame.js
+++ src/OnePlayerGame.js
@@ -3,8 +3,6 @@ var OnePlayerGameFile = (function() {
   var Frame = FrameFile.Frame;
   var OnePlayerGame = function() {
     this.frame1 = new Frame(this);
-    this.frame1.activate();
-    this.frame1.setPriorScore(0);
     this.frame2 = new Frame(this);
     this.frame3 = new Frame(this);
     this.frame4 = new Frame(this);
@@ -14,6 +12,8 @@ var OnePlayerGameFile = (function() {
     this.frame8 = new Frame(this);
     this.frame9 = new Frame(this);
     this.frame10 = new Frame(this, "tenth frame");
+    this.frame1.activate();
+    this.frame1.setPriorScore(0);
     this.frames = [this.frame1, this.frame2, this.frame3, this.frame4, this.frame5, this.frame6, this.frame7, this.frame8, this.frame9, this.frame10];
     this.score = 0;
   };
@@ -23,6 +23,7 @@ var OnePlayerGameFile = (function() {
       return frame.isActive();
     });
   };
+
   OnePlayerGame.prototype.setScore = function(points) {
     this.score = points;
   };
@@ -32,19 +33,13 @@ var OnePlayerGameFile = (function() {
 
   OnePlayerGame.prototype.passOnScore = function(score, previousFrame) {
     this.setScore(score);
-    if (previousFrame === this.frame10) {
-      return;
-    }
+    if (previousFrame === this.frame10) { return; }
     var nextFrameIndex = this.frames.indexOf(previousFrame) + 1;
     this.frames[nextFrameIndex].setPriorScore(score);
   };
 
   OnePlayerGame.prototype.nextInactiveFrameIndex = function() {
-    var highestActiveFrame;
-    var activeFrames = this.frames.filter(function(frame) {
-      return frame.isActive();
-    });
-    highestActiveFrame = activeFrames[activeFrames.length -1];
+    var highestActiveFrame = this.activeFrames()[this.activeFrames().length - 1];
     return this.frames.indexOf(highestActiveFrame) + 1;
   };
 
@@ -54,13 +49,8 @@ var OnePlayerGameFile = (function() {
     });
   };
 
-  OnePlayerGame.prototype.updateBoxes = function() {
-  };
-
   OnePlayerGame.prototype.activateNextFrame = function() {
-    if (this.frame10.isActive()) {
-      return;
-    }
+    if (this.frame10.isActive()) { return; }
     this.frames[this.nextInactiveFrameIndex()].activate();
   };
     

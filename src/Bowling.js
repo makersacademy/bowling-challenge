3c0a2196860aefcc8df803253d4fd1bdc1fba9bd
Peter Barcsak <peter.barcsak@gmail.com>
2017-05-14 20:41:48 +0100

    Refactoring

--- src/Bowling.js
+++ src/Bowling.js
@@ -3,8 +3,7 @@ function Bowling() {
 };
 
 Bowling.prototype.roll = function(pins) {
-  if ( pins == 10 ) { this.rolls.push(pins, 0) }
-  else this.rolls.push(pins);
+  ( pins == 10 ) ? this.rolls.push(pins, 0) : this.rolls.push(pins);
 }
 
 Bowling.prototype.frames = function() {
@@ -18,38 +17,27 @@ Bowling.prototype.score = function() {
   var game = this;
 
   for ( var frameIndex = 0; frameIndex < this.frames(); frameIndex++) {
-    // console.log(rollIndex)
-    if (strike(rollIndex)) {
-      totalScore += frameScore(rollIndex, 4)
-      console.log(totalScore)
-    } else if (spare(rollIndex)) {
-      console.log(totalScore)
-      totalScore += frameScore(rollIndex, 3)
-    } else {
-      console.log(totalScore)
-      totalScore += frameScore(rollIndex)
-    }
+    var bonus = strike() || spare() || 0
+    totalScore += frameScore(bonus)
     rollIndex += 2;
   }
   return totalScore;
 
-  function spare(rollIndex) {
-    // console.log(rollIndex)
-    return (game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10)
+  function spare() {
+
+    if (game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10) return 1
   }
 
-  function strike(rollIndex) {
-    // console.log(rollIndex)
-    return (game.rolls[rollIndex] == 10)
+  function strike() {
+    if (game.rolls[rollIndex] == 10) return 2
   }
 
-  function frameScore(rollIndex, n = 2) {
+  function frameScore(bonus) {
+    rollsPlusBonus = Math.min(2 + bonus, game.rolls.length - rollIndex)
     var score = 0;
-    for ( i = 0; i < n; i++) {
-      console.log(score)
+    for ( i = 0; i < rollsPlusBonus; i++) {
         score += game.rolls[rollIndex + i];
     }
     return score
   }
-
 };

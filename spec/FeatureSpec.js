362fccf7460911d57bfd2e7f54d0c65631d7fa16
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-14 11:26:45 +0100

    Bowling game can calulate frame counts
    Add FeatureSpec and initial feature test

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -8,6 +8,9 @@ describe('Features', function() {
 
   it('Player can bowl a ball', function(){
     expect(bowling.bowl(5)).toEqual(5);
+    expect(bowling.bowl(3)).toEqual(3);
+    expect(bowling.getFrames()).toEqual([5, 3]);
+    expect(bowling.getFrameCounter()).toEqual(1);
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,22 +2,28 @@
 
 function Bowling () {
   this._frames = [];
-  this._framecounter = (this.getFrames()) / 2;
+  this._frameCounter = this.calculateFrameCount();
 }
-
 Bowling.prototype.getFrames = function () {
   return this._frames;
 };
-
+Bowling.prototype.setFrames = function (value) {
+  return ( this._frames.push(value) );
+};
 Bowling.prototype.getFrameCounter = function () {
-  return this._framecounter;
+  return this._frameCounter;
 };
-
+Bowling.prototype.calculateFrameCount = function () {
+  var numberOfBowls = this._frames.length;
+  var frameCount = this._frameCounter = ( numberOfBowls / 2 );
+  return frameCount;
+};
 Bowling.prototype.bowl = function (number) {
-  var pins = this.randomNumberOfPins();
-  return number || pins;
+  var pins =  number || this.randomNumberOfPins();
+  this.setFrames(pins);
+  this.calculateFrameCount();
+  return pins;
 };
-
 Bowling.prototype.randomNumberOfPins = function () {
   return Math.floor( Math.random () * ( 10 - 1 + 1)) + 1;
 };

f6e8778e047559b074c6ea6277d27fb57d74ae57
Paula <pmuldoon@umich.edu>
2017-05-14 10:46:53 +0100

    added console.log messages to show score

--- src/Bowling.js
+++ src/Bowling.js
@@ -20,6 +20,15 @@ Player.prototype.bowl = function (number) {
 
 Player.prototype.roll1 = function (number) {
   this.roll1Score = number;
+  if (this.roll1Score === 0) {
+    console.log('Oops, you didn\'t score anything.  Try again on the second roll!');
+  }
+
+  else if (this.roll1Score === 1) {
+    console.log('You knocked over 1 pin...kind of pathetic but better than 0.');
+  }
+
+  else
   console.log('Congrats, you knocked over ' + this.roll1Score + ' pins!');
 };
 
@@ -27,6 +36,15 @@ Player.prototype.roll2 = function (number) {
   this.roll2Score = number;
   this.frameScore  = this.roll2Score + this.roll1Score;
   this.score += this.frameScore;
+  if (this.roll2Score === 0) {
+    console.log('Oops, you didn\'t score anything.  Better luck next frame!');
+  }
+
+  else if (this.roll2Score === 1) {
+    console.log('You knocked over 1 pin...kind of pathetic but better than 0.');
+  }
+
+  else
   console.log('Congrats, you knocked over ' + this.roll2Score + ' pins!');
   if (this.frameScore === 10) {
     this.spare(number);
@@ -36,12 +54,15 @@ Player.prototype.roll2 = function (number) {
 Player.prototype.spare = function (number) {
   this.number = number;
   this.spareRoll1Score = number * 2;
-  console.log('Congrats, you knocked over ' + this.spareRoll1Score + ' pins!');
+  console.log('Congrats, you knocked over ' + this.number + ' pins!');
   this.spareRoll2Score = number;
-  console.log('Congrats, you knocked over ' + this.spareRoll1Score + ' pins!');
+  console.log('Congrats, you knocked over ' + this.spareRoll2Score + ' pins!');
   this.spareFrameScore = this.spareRoll2Score + this.spareRoll1Score;
   this.score += this.spareFrameScore;
   this.frames -= 1;
+  console.log('Score on this frame: ' + this.spareFrameScore + '.');
+  console.log('Total score: ' + this.score + '.');
+  console.log('Frames remaining: ' + this.frames + '.');
 };
 
 Player.prototype.finish = function () {

f82d0a5f9f95d0b50f6135f82103c7d55ef2773a
Peter Barcsak <peter.barcsak@gmail.com>
2017-05-14 16:49:48 +0100

    Basic functionality

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,10 +1,23 @@
 describe('Bowling', function() {
 
-  var bowling = new Bowling();
+  var game
 
-  describe('#score', function() {
-    it('returns the starting score', function() {
-      expect(bowling.score).toEqual(0);
+  beforeEach (function() {
+    game = new Bowling();
+  });
+
+  describe('#roll', function() {
+    it('adds the knocked down pins to the rolls',function() {
+      game.roll(5)
+      expect(game.rolls).toContain(5);
     });
   });
+
+  describe('#score', function() {
+    it('returns the total score',function() {
+      game.roll(5)
+      expect(game.score()).toEqual(5);
+    });
+  });
+
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,3 +1,15 @@
 function Bowling() {
-  this.score = 0
+  this.rolls = [];
 };
+
+Bowling.prototype.roll = function(pins) {
+  this.rolls.push(pins);
+}
+
+Bowling.prototype.score = function() {
+  var totalScore = 0;
+  for ( var i = 0; i < this.rolls.length; i++) {
+    totalScore += this.rolls[i];
+  }
+  return totalScore;
+}

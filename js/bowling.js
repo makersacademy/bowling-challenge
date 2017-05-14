34075b885e72b35f6e9bc3dbf44fcc843d9df05b
JayWebDevCom <jaiye.s@gmail.com>
2017-05-14 11:07:10 +0100

    add more passing jasmine cl tests

--- js/bowling.js
+++ js/bowling.js
@@ -1,5 +1,27 @@
 var Bowling = function () {
+  this._rolls = []
+}
 
+Bowling.prototype.roll = function (pinsDown) {
+  this._rolls.push(pinsDown);
+}
+
+Bowling.prototype.scoreIs = function () {
+  var score = 0;
+  var rollIndex = 0;
+
+  for (var frameIndex = 0; frameIndex < 10; frameIndex ++) {
+
+    if (this._rolls[rollIndex] + this._rolls[rollIndex + 1] == 10) {
+      score += this._rolls[rollIndex] + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2]
+    } else {
+      score += this._rolls[rollIndex] + this._rolls[rollIndex + 1]
+    }
+
+    rollIndex += 2
+  }
+
+  return score;
 }
 
 module.exports = Bowling;
--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,17 +1,50 @@
 describe('Bowling', function () {
 
   var Bowling = require('../js/bowling');
-  var bowling;
+  var bowlingGame;
 
   beforeEach(function () {
-    bowling = new Bowling ();
+    bowlingGame = new Bowling ();
   })
 
   describe('Games', function () {
     it('can be created', function () {
-      var name = bowling.constructor.name
+      var name = bowlingGame .constructor.name
       expect(name).toBe('Bowling')
     })
   })
+
+  describe('Games', function () {
+    it('can be all 0 scoring frames i.e. gutter rolls', function () {
+      multiRoll(0, 20)
+      expect(bowlingGame.scoreIs()).toEqual(0)
+    })
+    it('can be all 1\'s', function () {
+      multiRoll(1, 20)
+      expect(bowlingGame.scoreIs()).toEqual(20)
+    })
+  })
+
+  describe('Spares', function () {
+    it('can be rolled as 5, 5', function () {
+      bowlingGame.roll(5)
+      bowlingGame.roll(5)
+      multiRoll(0, 18)
+      expect(bowlingGame.scoreIs()).toEqual(10)
+    })
+    it('can be rolled as 3, 7', function () {
+      bowlingGame.roll(3)
+      bowlingGame.roll(7)
+      bowlingGame.roll(5)
+      multiRoll(0, 17)
+      expect(bowlingGame.scoreIs()).toEqual(20)
+    })
+  })
+
+  var multiRoll = function (pinsDown, rolls) {
+    for (var i=0; i<rolls; i++) {
+      bowlingGame.roll(pinsDown);
+    }
+  }
 
 })

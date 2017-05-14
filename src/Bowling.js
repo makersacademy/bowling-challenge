1a5912872412b74560513cbb99131c95a9ec07b9
Alex Law <achlawuk@gmail.com>
2017-05-14 17:19:09 +0100

    Test roll a game of ones now passing

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,11 +1,15 @@
 function Bowling() {
-
+  this.rolls = [];
 };
 
 Bowling.prototype.roll = function(pins) {
-  
+  this.rolls.push(pins);
 };
 
 Bowling.prototype.score = function() {
-  return 0;
+  var result = 0;
+  for (var i = 0; i < 20; i++) {
+    result += this.rolls[i];
+  }
+  return result;
 };

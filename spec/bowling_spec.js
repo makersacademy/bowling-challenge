380cf2e02be62425001f978fab805c3b7931cfd6
Unai Motriko <unaimotriko@gmail.com>
2017-05-14 14:15:36 +0100

    It sets pins to 10

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -4,14 +4,21 @@ describe('Bowling', function() {
     theDude = new Bowling();
   });
 
-  describe('it knows which frame it is, ', function() {
+  describe('it knows which frame it is,', function() {
     it('returns 1 for first game.', function() {
       expect(theDude._currentFrame).toEqual(1);
     });
   });
-  describe('it knows which throw it is, ', function() {
+
+  describe('it knows which throw it is,', function() {
     it('returns \'first\' at the start of game.', function() {
       expect(theDude._currentThrow).toEqual('first');
     });
   });
+  describe('it knows how many pins there are,', function() {
+    it('returns 10 at the beggining of game', function() {
+      expect(theDude._pins).toEqual(10);
+    });
+  });
+
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,4 +1,6 @@
 function Bowling() {
   this._currentFrame = 1;
   this._currentThrow = 'first';
+  this._pins = 10;
+
 };

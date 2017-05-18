f31434ce86071039ea72d052a2dd5ac8cf60d8e9
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-18 19:38:23 +0100

    adds frameCounter

--- bowling/spec/BowlingSpec.js
+++ bowling/spec/BowlingSpec.js
@@ -5,13 +5,21 @@ describe('Bowling', function() {
     expect(bowling.score).toEqual(0);
   })
 
+  it('begins a game with a frame counter of 0', function() {
+    expect(bowling.frameCounter).toEqual(0);
+  })
+
   describe('#takeAGo', function() {
     it('returns STRIKE if the randomiser returns 10', function() {
       expect(bowling.takeAGo(10)).toEqual('STRIKE');
-    });
+    })
 
     it('returns GUTTER BALL if the randomiser returns 0', function() {
       expect(bowling.takeAGo(0)).toEqual('GUTTER BALL');
-    });
-  });
-});
+    })
+
+    it('else returns score integer', function() {
+      expect(bowling.takeAGo(2)).toEqual(2);
+    })
+  })
+})
--- bowling/src/Bowling.js
+++ bowling/src/Bowling.js
@@ -1,8 +1,10 @@
 function Bowling() {
   this.score = 0;
+  this.frameCounter = 0;
 }
 
 Bowling.prototype.takeAGo = function(score = (new RandomScore()).createScore()) {
   if ( score == 10 ) { return 'STRIKE' };
   if ( score == 0 ) { return 'GUTTER BALL' };
+  return score;
 }

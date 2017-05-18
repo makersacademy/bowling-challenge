94bb694deae447cecfea219a2195d7103fcc83f6
danielemanuel <daniel.costea@vkernel.ro>
2017-05-18 06:33:04 +0100

    Refactor code

--- spec/Game_spec.js
+++ spec/Game_spec.js
@@ -1,55 +1,17 @@
-'use strict'
+describe("feature test", function(){ "use strict";
 
-describe('Game', function() {
   var game;
 
-  beforeEach(function() {
-    game = new Game;
+  beforeEach(function(){
+    game = new game();
   });
 
-  describe('#start_game', function() {
+  describe("bowl functionality", function(){
 
-    it('starts the game with score 0', function() {
-      expect(game.gameScore()).toEqual(0);
+    it("pinsKnockdown method generates number between 0 and 10 ", function(){
+      spyOn(game, 'pinsKnockdown').and.returnValue(3);
+      expect(game.pinsKnockdown()).toEqual(3);
     });
-  });
-
-    it('starts the game with frame 1', function() {
-      expect(game.currentFrame()).toEqual(1);
-    });
-
-    it('allows the user the option to play', function() {
-      expect(game.gameScore()).toEqual(0);
-    });
-
-    it('should have 2 rolls in each frame', function() {
-      game.play();
-      expect(game.currentFrame()).toEqual(1);
-    });
-
-    it('allows the user to get duck', function() {
-      spyOn(Math, 'random').and.returnValue(0);
-      expect(game.gameScore()).toEqual(0);
-    });
-
-    it('should not allow the user to play the eleventh frame', function(){
-    for(var i; i<21; i++)
-    {
-      game.increaseFrame();
-    }
-    expect(game.currentFrame()).toEqual(1);
-  });
-
-  it('increases the game score with 4 if 4 pins fall on rolling', function() {
-    spyOn(Math,'random').and.returnValue(4);
-    game.play();
-    expect(game.gameScore()).toEqual(4);
-  });
-
-  it('shows score equal to 6', function() {
-    spyOn(Math, 'random').and.returnValue(6);
-    game.play();
-    expect(game.gameScore()).toEqual(6);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,67 +1,12 @@
-function Game() {
-  this.score = 0;
-  this.frame = 1;
-  this.hitPins = 0;
-  this.remainedPins = 10;
-  this.countRoll = 1;
-  this.MAXIMUM_PINS = 10;
-  this.MAXIMUM_FRAME = 10;
-}
 
-Game.prototype.play = function() {
-  this.hitPins = Math.floor(Math.random() * (this.remainedPins + 1));
-  this.updateRoll();
-  this.increaseGameScore();
-  this.countRoll;
-  this.score;
-};
-
-Game.prototype.increaseGameScore = function() {
-  if(this.hitPins === this.MAXIMUM_PINS)
-  {
-    this.score += this.MAXIMUM_PINS;
-    this.remainedPins = this.MAXIMUM_PINS;
-  } else
-  {
-    this.score += this.hitPins;
-    this.remainedPins = this.MAXIMUM_PINS - this.hitPins;
-  }
-    return this.score;
-};
-
-Game.prototype.gameScore = function(value) {
-  return this.score;
-};
-
-Game.prototype.currentFrame = function() {
-  return Math.floor(this.frame);
-};
-
-Game.prototype.hitPins = function() {
-  return this.hitPins;
-};
-
-Game.prototype.updateRoll = function() {
-  if(this.frame < 10)
-  {
-    if(this.countRoll === 1)
-    {
-      this.countRoll = 2;
-    } else
-    {
-      this.countRoll =1;
-      this.frame++;
-    }
-  } else
-  {
-    this.frame =1;
-  }
-};
-
-Game.prototype.getRollNumber = function() {
-  return this.countRoll;
-};
-
-Game.prototype.pinsKnockDown = function() {
-  return this.hitPins;
+function Game() {"use strict";
+  this._frame = 1;
+  this._roll = 1;
+  this._rollScore1 = 0;
+  this._rollScore2 = 0;
+  this._totalScore = 0;
+  this._currentKnockdown = 0;
+  this._standingPins = 10;
+  this._sX = "";
+  this._maxRounds = 10;
 };

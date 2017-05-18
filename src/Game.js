5011d9586bc1c5c9b78210f6333fa0ce971dcd4e
danielemanuel <daniel.costea@vkernel.ro>
2017-05-18 06:38:26 +0100

    Game has the ability to generate random pins

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,27 @@ function Game() {"use strict";
   this._rollScore2 = 0;
   this._totalScore = 0;
   this._currentKnockdown = 0;
-  this._standingPins = 10;
-  this._sX = "";
   this._maxRounds = 10;
 };
+
+Game.prototype.bowl = function() {
+  this.rollScoreMethod();
+};
+
+Game.prototype.rollScoreMethod = function(){
+  this._currentKnockdown = this.pinsKnockdown();
+  if(this._roll === 1){
+    this._rollScore1 = this._currentKnockdown
+  } else {
+    this._rollScore2 = this._currentKnockdown
+  }
+  this.remainingPins();
+}
+
+Game.prototype.pinsKnockdown = function(){
+  return Math.floor(Math.random() * (this._standingPins + 1));
+}
+
+Game.prototype.remainingPins = function(){
+  this._standingPins -= this._currentKnockdown
+}

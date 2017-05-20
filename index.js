bf06e44cfa467b695af5f55651aa08c5d366a43e
Paula <pmuldoon@umich.edu>
2017-05-20 14:52:18 +0100

    added random number to bowl()

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
 
-  $('#roll_ball').click(function (clickEvent) {
-    player.bowl(3);
+  $('#roll_frame').click(function (clickEvent) {
+    player.bowl(Math.floor((Math.random() * 11) + 1));
     displayScore();
   });
 });
 
 function displayScore() {
+  $('#roll1').text(player.roll1Score);
+  $('#roll2').text(player.roll2Score);
   $('#frame_score').text(player.frameScore);
   $('#total_score').text(player.score);
   $('#frames_left').text(player.frames);

7c7dc8467ebdfee207805d0b1b20ff688586ae31
hendrik <g9a2hvk9@me.com>
2017-05-20 12:36:23 +0100

    Adds score multiplyer functionality and passes tests

--- jasmine-standalone/spec/FrameSpec.js
+++ jasmine-standalone/spec/FrameSpec.js
@@ -1,76 +1,122 @@
 describe("Frame", function() {
-  var testFrame;
+  var testFrame1;
+  var testFrame2;
 
   beforeEach(function() {
-    testFrame = new Frame();
+    testFrame1 = new Frame();
+    testFrame2 = new Frame();
   });
 
 
   // Set up tests. Relate only to intital set up of the Frame function.
 
-  it("initiates with resultA and resultB both being 0", function(){
-    expect(testFrame._resultA).toEqual(0);
-    expect(testFrame._resultB).toEqual(0);
+  it("initiates with the results hash setting the scores of both rolls equal to null", function(){
+    expect(testFrame1._result["A"]).toBe(null);
+    expect(testFrame1._result["B"]).toBe(null);
   });
 
-  it("initiates with a frameResult equal to the cumulative result of A and B - namely 0", function(){
-    expect(testFrame._frameResult).toEqual(testFrame._resultA + testFrame._resultB);
-    expect(testFrame._frameResult).toEqual(0);
+  // it("initiates with a frameResult equal to the cumulative result of A and B - namely 0", function(){
+  //   expect(testFrame1._frameResult).toEqual(testFrame1._resultA + testFrame1._resultB);
+  //   expect(testFrame1._frameResult).toEqual(0);
+  // });
+
+  it("initiates with a frameScore equal to null", function(){
+    expect(testFrame1._frameScore).toBe(null);
   });
 
-  it("initiates with a frameScore equal to 0", function(){
-    expect(testFrame._frameScore).toEqual(0);
-  });
-
-  it("initiates with a _possibleResults array containing the possible results from 0 to 10", function(){
-    expect(testFrame._possibleResults).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  it("initiates with a _pins attribute that is equal to 10", function(){
+    expect(testFrame1._pins).toEqual(10)
   });
 
 
   // Functionality tests. Relate to use and update of game relevant data in a frame.
 
-  it("allows me to record the result from rollA in resultA", function(){
-    testFrame.rollA(5);
-    expect(testFrame._resultA).toEqual(5);
+  it("allows me to record the result from rollA in key 'A' of the _result", function(){
+    testFrame1.rollA(5);
+    expect(testFrame1._result["A"]).toEqual(5);
   });
 
-  it("allows me to record the result from rollB in resultB", function(){
-    testFrame.rollB(5);
-    expect(testFrame._resultB).toEqual(5);
+  it("allows me to record the result from rollB in key 'B of the the _result'", function(){
+    testFrame1.rollB(5);
+    expect(testFrame1._result["B"]).toEqual(5);
   });
 
-  it("updates the _possibleResults array to mirror the possible scores in the next role", function(){
-    testFrame.rollA(5);
-    expect(testFrame._possibleResults).toEqual([0, 1, 2, 3, 4, 5]);
+  it("updates the _possibleResults array to mirror the possible scores in the next roll", function(){
+    testFrame1.rollA(5);
+    expect(testFrame1._pins).toEqual(5);
   });
 
 
   // Result testing. Relate to recording and updating initial results from full first frame.
 
-  it("allows me to record the cumulative results from rollA and rollB in _frameResult", function(){
-    testFrame.rollA(5);
-    testFrame.rollB(5);
-    expect(testFrame._frameResult).toEqual(10);
+  it("allows me to record the cumulative results from rollA and rollB in _frameScore", function(){
+    testFrame1.rollA(3);
+    testFrame1.rollB(3);
+    expect(testFrame1._frameScore).toEqual(6);
+  });
+
+  it("does not set a frameScore if a player rolls a strike", function(){
+    testFrame1.rollA(10);
+    expect(testFrame1._frameScore).toBe(null);
+  });
+
+  it("sets the score multiplyer equal to 1 when frame is a strike", function(){
+    testFrame1.rollA(10);
+    expect(testFrame1._multiplyer).toEqual(1);
+  });
+
+  it("does not set a frameScore if a player rolls a spare", function(){
+    testFrame1.rollA(5);
+    testFrame1.rollB(5);
+    expect(testFrame1._frameScore).toBe(null);
+  });
+
+  it("sets the score multiplyer equal to 0 when frame is a spare", function(){
+    testFrame1.rollA(5);
+    testFrame1.rollB(5);
+    expect(testFrame1._multiplyer).toEqual(0);
+  });
+
+  
+
+  // Results testing for score dependency. Makes sure that later frames influence the score of previous frames, based on the multiplyer.
+
+  it("sets the previous frameScore dependent on rollA and rollB when previous frame is a strike", function(){
+    testFrame1.rollA(10);
+    testFrame2.rollA(5);
+    testFrame2.rollB(3);
+    testFrame2.setPreviousScore(testFrame1);
+    expect(testFrame1._frameScore).toEqual(18);
+  });
+
+  it("sets the previous frameScore dependent on rollA when previous frame is a spare", function(){
+    testFrame1.rollA(5);
+    testFrame1
+
+    .rollB(5);
+    testFrame2.rollA(5);
+    testFrame2.rollB(3);
+    testFrame2.setPreviousScore(testFrame1);
+    expect(testFrame1._frameScore).toEqual(15);
   });
 
 
   // Edge case testing. Relates to errors and guard clauses within Frame function.
 
   it("throws an error when I try to topple less than 0 pins on rollA", function(){
-    expect(function(){testFrame.rollA(-1)}).toThrowError("Invalid number of toppledPins");
+    expect(function(){testFrame1.rollA(-1)}).toThrowError("Invalid number of toppledPins");
   });
 
   it("throws an error when I try to topple more than 10 pins on rollA", function(){
-    expect(function(){testFrame.rollA(11)}).toThrowError("Invalid number of toppledPins");
+    expect(function(){testFrame1.rollA(11)}).toThrowError("Invalid number of toppledPins");
   });
 
   it("throws an error when I try to topple less than 0 pins on rollB", function(){
-    testFrame.rollA(5);
-    expect(function(){testFrame.rollB(-1)}).toThrowError("Invalid number of toppledPins");
+    expect(function(){testFrame1.rollB(-1)}).toThrowError("Invalid number of toppledPins");
   });
 
   it("throws an error when I try to topple more than the remaining pins on rollB", function(){
-    testFrame.rollA(5);
-    expect(function(){testFrame.rollB(6)}).toThrowError("Invalid number of toppledPins");
+    testFrame1._pins = 5
+    expect(function(){testFrame1.rollB(6)}).toThrowError("Invalid number of toppledPins");
   });
 });
--- jasmine-standalone/src/Frame.js
+++ jasmine-standalone/src/Frame.js
@@ -1,40 +1,48 @@
 function Frame() {
-  this._resultA = 0;
-  this._resultB = 0;
-  this._frameResult = 0;
-  this._frameScore = 0;
-  this._possibleResults = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  this._result = {"A": null, "B": null};
+  this._frameScore = null;
+  this._pins = 10;
+  this._multiplyer = null;
 };
 
 // Roll functions below. Not DRY code, but most functional solution for now.
 // !!Pay close attention to updating possible outcomes at the end of rollA
 
 Frame.prototype.rollA = function(toppledPins){
-  if(toppledPins < 0 || toppledPins > 10){throw new TypeError("Invalid number of toppledPins")}; // Guard clause against invalid toppledPins inputs
-  this._resultA = toppledPins;
-  this._updateFrameResult();
-  this._updatePossible();
+  if(toppledPins < 0 || toppledPins > this._pins){throw new TypeError("Invalid number of toppledPins")}; // Guard clause against invalid toppledPins inputs
+  this._result["A"] = toppledPins;
+  this._pins = 10 - this._result["A"];
+  if(this._pins == 0){this._multiplyer = 1};
+  this.setCurrentScore();
 };
 
 Frame.prototype.rollB = function(toppledPins){
-  if(toppledPins < 0 || toppledPins > Math.max(...this._possibleResults)){throw new TypeError("Invalid number of toppledPins")}; // Guard clause against invalid toppledPins inputs
-  this._resultB = toppledPins;
-  this._updateFrameResult();
+  if(toppledPins < 0 || toppledPins > this._pins){throw new TypeError("Invalid number of toppledPins")}; // Guard clause against invalid toppledPins inputs
+  this._result["B"] = toppledPins;
+  this.setCurrentScore();
+};
+
+Frame.prototype.setCurrentScore = function(){
+  if (this._result["A"] == 10){
+    this._multiplyer = 1;
+  } else if ((this._result["A"] + this._result["B"]) == 10){
+    this._multiplyer = 0;
+  } else if (this._result["B"]){
+    this._frameScore = (this._result["A"] + this._result["B"]);
+  };
+};
+//
+Frame.prototype.setPreviousScore = function(previousRoll){
+  if (previousRoll._multiplyer == 1){
+    previousRoll._frameScore = ((this._result["A"] + this._result["B"]) + (previousRoll._result["A"] + previousRoll._result["B"]));
+  } else if (previousRoll._multiplyer == 0){
+    previousRoll._frameScore = (this._result["A"] + previousRoll._result["A"] + previousRoll._result["B"]);
+  } else {
+    previousRoll._frameScore = previousRoll._frameScore;
+  };
 };
 
 // The functions below act only to preserve functionality of the Frame function
 // and should not be called in isolation
-
-Frame.prototype._updatePossible = function(){
-  this._possibleResults = [];
-  var remaining = 10 - this._resultA;
-  for (i=0; i<= remaining; i++){
-    this._possibleResults.push(i)
-  };
-};
-
-Frame.prototype._updateFrameResult = function(){
-  this._frameResult = (this._resultA + this._resultB);
-};
 
 // Guard clause functions. Guard against invalid inputs and erroneous outcomes.

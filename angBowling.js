02641038ac48dba78b09b125a12cc2ed82055b37
Peter Barcsak <peter.barcsak@gmail.com>
2017-05-21 22:43:09 +0100

    Alerts

--- angBowling.js
+++ angBowling.js
@@ -17,15 +17,22 @@ app.controller('myCtrl', function($scope) {
       rollIndex = 0;
 
           for ( i = 1; i <= 10; i++) {
-          array.push([i,rollIndex, rollIndex + 1])
+            array.push([i,rollIndex, rollIndex + 1])
           rollIndex += 2;
         }
         return array
       };
 
+
     $scope.pins = pins();
     $scope.numberOfRolls = frameStructure();
     $scope.rolls = game.rolls;
-    $scope.roll = game.roll;
+    $scope.roll = function(n) {
+      if ( game.rolls.length < 21 && game.rolls.length % 2 != 0 && game.rolls[game.rolls.length - 1] + n > 10 ) { window.alert("The score of a frame can't be more than 10") }
+      if ( game.rolls.length >= 22 ) { window.alert("The game finished") }
+      if ( game.rolls[18] != 10 && game.rolls.length >= 20 ) { window.alert("The game finished") }
+      else { game.roll(n) };
+    };
     $scope.score = game.score;
+
 });

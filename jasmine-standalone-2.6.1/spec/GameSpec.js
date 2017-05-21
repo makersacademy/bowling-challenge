4278e557f42380eb9438238357499e05fd8b66e6
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-21 15:59:11 +0100

    Refacotors tests by adding rollMany function

--- jasmine-standalone-2.6.1/spec/GameSpec.js
+++ jasmine-standalone-2.6.1/spec/GameSpec.js
@@ -5,19 +5,21 @@ describe('Game', function(){
     game = new Game();
   });
 
-describe('roll', function(){
+  describe('roll', function(){
     it('can roll a gutter game', function(){
-      for (var i = 0; i < 20; i++) {
-        game.roll(0);
-      }
+      rollMany(0, 20);
       expect(game.score()).toBe(0);
     });
 
     it('can roll a game of ones', function (){
-      for (var i = 0; i < 20; i++) {
-        game.roll(1);
-      }
+      rollMany(1, 20);
       expect(game.score()).toBe(20);
     });
+
+    var rollMany = function (pins, rolls) {
+      for (var i = 0; i < rolls; i++) {
+        game.roll(pins);
+      }
+    };
   });
 });

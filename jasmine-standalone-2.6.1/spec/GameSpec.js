db4a65338fafc60eaf5387d2714437bc7cef2546
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-21 14:50:47 +0100

    Removes previous tests and adds tests for gutter game and game of ones

--- jasmine-standalone-2.6.1/spec/GameSpec.js
+++ jasmine-standalone-2.6.1/spec/GameSpec.js
@@ -5,24 +5,19 @@ describe('Game', function(){
     game = new Game();
   });
 
-  describe('start of game conditions', function(){
-    it('starts on 0 points', function(){
-      expect(game.viewScore()).toEqual(0);
-    });
-    it('starts on roll 0', function(){
-      expect(game.viewRoll()).toEqual(0);
-    });
-    it('starts on frame 1', function(){
-      expect(game.viewFrame()).toEqual(1);
-    });
-  });
-
-  describe('moving to frame 2', function(){
-    it('moves the frame 2 after roll 2', function(){
-      for(var i=0; i<3; i++){
-        game.rollBall();
+describe('roll', function(){
+    it('can roll a gutter game', function(){
+      for (var i = 0; i < 20; i++) {
+        game.roll(0);
       }
-      expect(game.viewFrame()).toEqual(2);
+      expect(game.score()).toBe(0);
+    });
+
+    it('can roll a game of ones', function (){
+      for (var i = 0; i < 20; i++) {
+        game.roll(1);
+      }
+      expect(game.score()).toBe(20);
     });
   });
-  });
+});
--- jasmine-standalone-2.6.1/src/Game.js
+++ jasmine-standalone-2.6.1/src/Game.js
@@ -1,33 +1,15 @@
 function Game() {
-   this.score = STARTING_SCORE;
-   this.roll = STARTING_ROLL;
-   this.frame = STARTING_FRAME;
- }
+  this.rolls = [];
+}
 
- const STARTING_SCORE = 0;
- const STARTING_ROLL = 0;
- const STARTING_FRAME = 1;
+Game.prototype.roll = function (pins) {
+  this.rolls.push(pins);
+};
 
- Game.prototype.viewScore = function () {
-   return this.score;
- };
-
- Game.prototype.viewRoll = function () {
-   return this.roll;
- };
-
- Game.prototype.viewFrame = function () {
-   return this.frame;
- };
-
- Game.prototype.rollBall = function () {
-   this.roll += 1;
-   this.newFrame();
- };
-
- Game.prototype.newFrame = function () {
-   if (this.roll === 2){
-     this.frame += 1;
-     this.roll = STARTING_ROLL;
-   }
- };
+Game.prototype.score = function () {
+  var result = 0;
+  for (var i = 0; i < 20; i++) {
+    result += this.rolls[i];
+  }
+  return result;
+};

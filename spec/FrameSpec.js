fad02b4fd20d93e8e26a1d75c2378e3c42c7253a
milesillsley <milesillsley@hotmail.co.uk>
2017-05-21 01:15:33 +0100

    Frame: is a strike if 10 is scored on the first throw, is a spare if 10 is scored by the first two throws

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -19,5 +19,14 @@
       frame.score(4);
       expect(frame.isOver()).toBe(true);
     });
+    it('is a strike if 10 is scored on the first throw', function() {
+      frame.score(10);
+      expect(frame.isStrike()).toBe(true);
+    });
+    it('is a spare if 10 is scored by the first two throws', function() {
+      frame.score(5);
+      frame.score(5);
+      expect(frame.isSpare()).toBe(true);
+    });
   });
 }());
--- src/Frame.js
+++ src/Frame.js
@@ -5,3 +5,7 @@ Frame.prototype.score = function(pinsHit) { this._score.push(pinsHit); };
 Frame.prototype.getFirstScore = function() { return this._score[0]; };
 Frame.prototype.getSecondScore = function() { return this._score[1]; };
 Frame.prototype.isOver = function() { return this._score.length === 2; };
+Frame.prototype.isStrike = function() { return this._score[0] === 10; };
+Frame.prototype.isSpare = function() {
+  return (this._score[0] + this._score[1]) === 10;
+};

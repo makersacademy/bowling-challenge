e631dd5719087202be0c537977822fcb4f493c7f
Anthony Crisp <anthony_crisp@hotmail.com>
2017-05-21 21:08:00 +0100

    can score a complete frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,6 +9,35 @@ describe("Frame", function() {
     it("has a first roll score of 1", function() {
       expect(frame.firstRollScore()).toEqual(1)
     })
+
+    it("has a second roll score of null", function() {
+      expect(frame._secondRoll).toEqual(null)
+    })
+
+    it("is a incomplete frame", function() {
+      expect(frame.isComplete()).toBeFalsy()
+    })
+
+    it("has a first and second roll score of 1", function() {
+      expect(frame.firstAndSecondRollScore()).toEqual(1)
+    })
+  })
+
+  describe("when second roll of frame", function() {
+    it("has a second roll score of 1", function() {
+      frame.addSecondRoll(1)
+      expect(frame._secondRoll).toEqual(1)
+    })
+
+    it("is a complete frame", function() {
+      frame.addSecondRoll(1)
+      expect(frame.isComplete()).toBeTruthy()
+    })
+
+    it("has a first and second roll score of 2", function() {
+      frame.addSecondRoll(1)
+      expect(frame.firstAndSecondRollScore()).toEqual(2)
+    })
   })
 
 })
--- src/frame.js
+++ src/frame.js
@@ -1,7 +1,20 @@
 function Frame(pins) {
   this._firstRoll = pins
+  this._secondRoll = null
 }
 
 Frame.prototype.firstRollScore = function() {
   return this._firstRoll
+}
+
+Frame.prototype.addSecondRoll = function(pinsDown) {
+  this._secondRoll = pinsDown
+}
+
+Frame.prototype.isComplete = function() {
+  return (this._secondRoll !== null)
+}
+
+Frame.prototype.firstAndSecondRollScore = function() {
+  return this._firstRoll + this._secondRoll
 }

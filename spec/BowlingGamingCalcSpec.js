fd3209dc35b0c53e95c4460bbc08d26db416b179
Adam Erdem Er <adamerdemer@gmail.com>
2017-05-22 05:45:42 +0100

    Finished README

--- spec/BowlingGamingCalcSpec.js
+++ spec/BowlingGamingCalcSpec.js
@@ -1,9 +1,48 @@
 describe('Bowling Game Calcualtor', function() {
 
-  it('creates game', function() {
-    var game = BowlingGame();
+  var game;
+
+  beforeEach(function() {
+    game = new BowlingGame();
   });
 
+    it('rolls gutter game', function() {
+      rollMany(0, 20);
+      expect(game.score()).toBe(0);
+    });
+
+    it('rolls 1 pin, 20 times', function() {
+      rollMany(1, 20);
+      expect(game.score()).toBe(20);
+    });
+
+    it('rolls spare', function () {
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      rollMany(0, 17);
+      expect(game.score()).toBe(16);
+    });
+
+    it('rolls a strike', function() {
+      game.roll(10);
+      game.roll(4);
+      game.roll(3);
+      rollMany(0, 16);
+      expect(game.score()).toBe(24);
+    });
+
+    it('roll perfect game', function(){
+      rollMany(10, 12);
+      expect(game.score()).toBe(300);
+    });
+
+    var rollMany = function (pins, rolls) {
+      for (var i = 0; i < 20; i++) {
+        game.roll(pins);
+      }
+
+    };
 
 
 });

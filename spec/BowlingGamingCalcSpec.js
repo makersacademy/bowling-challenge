2484b79a4d5b6c27cdac9e7e253b860cfbb6dfad
Adam Erdem Er <adamerdemer@gmail.com>
2017-05-22 04:09:00 +0100

    Started again, new setup

---
+++ spec/BowlingGamingCalcSpec.js
@@ -0,0 +1,9 @@
+describe('Bowling Game Calcualtor', function() {
+
+  it('creates game', function() {
+    var game = BowlingGame();
+  });
+
+
+
+});
---
+++ src/BowlingGame.js
@@ -0,0 +1,3 @@
+var BowlingGame = function () {
+
+};

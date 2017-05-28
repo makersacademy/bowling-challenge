731d589f7091e173d39e45f07a12005a4fa89fb0
Unai Motriko <unaimotriko@gmail.com>
2017-05-28 21:42:58 +0100

    It refactors tests to adapt to new encapsulation

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -1,51 +1,53 @@
 describe('Bowling, when starting a new game:', function() {
-  var theDude;
+  var bowling;
   var game;
   var score;
+  var spare;
   beforeEach(function() {
     game = new Game;
     game.newGame();
     score = new Score();
-    theDude = new Bowling(game, score);
+    spare = new Spare(game,score,bowling);
+    bowling = new Bowling(game,score,spare);
   });
 
   describe('it knows which frame it is,', function() {
     it('returns 1 for first game.', function() {
-      expect(theDude.game.currentFrame).toEqual(1);
+      expect(bowling.game.currentFrame).toEqual(1);
     });
   });
 
   describe('it knows which throw it is,', function() {
     it('returns \'first\' at the start of game.', function() {
-      expect(theDude.game.currentThrow).toEqual('first');
+      expect(bowling.game.currentThrow).toEqual('first');
     });
   });
   describe('it knows how many pins there are,', function() {
     it('returns 10 at the beggining of game', function() {
-      expect(theDude._pins).toEqual(10);
+      expect(bowling._pins).toEqual(10);
     });
   });
 
   describe('it knows the current frames\' points,', function() {
     it('returns null.', function() {
-      expect(theDude.score.framePoints).toEqual(0);
+      expect(bowling.score.framePoints).toEqual(0);
     });
   });
 
   describe('it knows the current throws\' points,', function() {
     it('returns null.', function() {
-      expect(theDude._firstThrow).toEqual(null);
+      expect(bowling.firstThrow).toEqual(null);
     });
   });
 
   describe('it knows if the current round is a Strike round', function() {
     it('returns false', function() {
-      expect(theDude._strikeRound).toEqual(false);
+      expect(bowling._strikeRound).toEqual(false);
     })
   })
   describe('it knows if the current round is a Spare round', function() {
     it('returns false', function() {
-      expect(theDude._spareRound).toEqual(false)
+      expect(bowling.spare.spareRound).toEqual(false)
     });
     });
 });

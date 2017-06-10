cce3d5ac14fd37c2acb77fde6e0cf7cf98b2353a
RSijelmass <R.Sijelmass@gmail.com>
2017-06-10 10:34:20 +0100

    Create this.frame - two rolls never exceed 10 - two rolls stored in an array - create rollTwo

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,8 +7,13 @@ describe('Bowling', function() {
 	
 	describe('a frame', function() {
 		it('displays a number between 0 and 10 with every roll', function() {
-			expect(bowling.roll()).toBeGreaterThan(-1);
-			expect(bowling.roll()).toBeLessThan(11);
+			expect(bowling.rollOne()).toBeGreaterThan(-1);
+			expect(bowling.rollOne()).toBeLessThan(11);
+		});
+		it('two rolls in a frame never exceed 10', function() {
+			bowling.rollOne();
+			bowling.rollTwo();
+			expect(bowling.frame[0] + bowling.frame[1]).toBeLessThan(11);
 		});
 	});
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,11 +1,18 @@
 function Bowling() {
+		this.frame = [];
 
-		this.roll = function() {
-			return (Math.random() * 10);
+		this.rollOne = function() {
+			this.frame[0] = Math.round((Math.random() * 10));
+			return this.frame;
 		};
-
+		
+		this.rollTwo = function() {
+			this.frame[1] = Math.round((Math.random()) * (10 - this.frame[0]));
+			return this.frame;
+		};
 };
 
 bowling = new Bowling();
 
-console.log(bowling.roll());
+console.log(bowling.rollOne());
+console.log(bowling.rollTwo());

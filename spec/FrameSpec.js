e4501b99c53bbaff7a0b00f388efbca9ff688c35
Kynosaur <kye.bracey@gmail.com>
2017-06-10 17:00:34 +0100

    Adds test expecting error when trying to bowl three times in one frame

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -10,7 +10,7 @@ describe('Frame', function() {
   describe('bowls', function() {
 
     it('starts out as an array containing null, null', function() {
-      expect(frame.bowls()).toEqual([null, null])
+      expect(frame.bowls()).toEqual([null, null]);
     });
 
   });
@@ -18,14 +18,20 @@ describe('Frame', function() {
   describe('bowl', function() {
 
     it('Updates the first bowl', function() {
-      frame.bowl(5)
-      expect(frame.bowls()).toEqual([5, null])
+      frame.bowl(5);
+      expect(frame.bowls()).toEqual([5, null]);
     })
 
     it('Updates the second bowl', function() {
-      frame.bowl(5)
-      frame.bowl(3)
-      expect(frame.bowls()).toEqual([5, 3])
+      frame.bowl(5);
+      frame.bowl(3);
+      expect(frame.bowls()).toEqual([5, 3]);
     })
+
+    it('Does not allow player to bowl more than twice in one frame', function() {
+      frame.bowl(5);
+      frame.bowl(3);
+      expect(function(){ frame.bowl(1); }).toThrow(new Error('Cannot bowl - this frame is complete'));
+    });
   });
 });

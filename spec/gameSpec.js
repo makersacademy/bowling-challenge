8988d28c4c80208b2106b6106b683a5ba6e07036
“rogrenke” <roland@rolandgrenke.com>
2017-06-10 21:47:21 +0100

    Adds #calculateTotalScore in Game object which calculates the total score of a game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -11,17 +11,26 @@ describe("Game", function () {
     role2.addPoints(5);
     frame1.add(role1);
     frame1.add(role2);
+    frame1.calculateScore();
     frame2 = new Frame();
     role3 = new Role();
     role4 = new Role();
-    role3.addPoints(3);
-    role4.addPoints(5);
-    frame2.add(role1);
-    frame2.add(role2);
+    role3.addPoints(4);
+    role4.addPoints(1);
+    frame2.add(role3);
+    frame2.add(role4);
+    frame2.calculateScore();
   });
 
   it("has functionality to add frames", function() {
     game.add(frame1);
     expect(game.frames[0]).toEqual(frame1);
+  });
+
+  it("has functionality to calculate total score", function() {
+    game.add(frame1);
+    game.add(frame2);
+    console.log(game);
+    expect(game.calculateTotalScore()).toEqual(13);
   });
 });
--- src/game.js
+++ src/game.js
@@ -5,3 +5,11 @@ function Game () {
 Game.prototype.add = function (frame) {
   this.frames.push(frame);
 };
+
+Game.prototype.calculateTotalScore = function () {
+  var currentTotalScore = 0
+  for (var i = 0; i < this.frames.length; i++) {
+    currentTotalScore += this.frames[i].score;
+  };
+  return currentTotalScore
+};

f25fdb94d8b4683bc4de7bfabdba6d92cfcc0211
Sam Pritchard <sampritchard@msn.com>
2017-06-10 14:25:20 +0100

    created a updateFrame method which updates when 2 rolls. Passed tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -14,5 +14,16 @@ it ('starts with the score 0', function() {
 it ('can increase the score depending on the roll', function() {
   game.roll(5)
   expect(game.score).toEqual(5);
-})
+});
+
+it ('the score is classed as a X if a 10 is rolled', function() {
+  game.roll(10)
+  expect(game.score).toEqual('X');
+});
+
+it ('starts the second frame after 2 bowls', function() {
+  game.roll(5)
+  game.roll(4)
+  expect(game.frame).toEqual(2);
+});
 });
--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,28 @@
 
 var Game = function() {
   this.score = 0;
+  this.frame = 1;
+  this.rollsThisFrame = 0;
 }
 
 Game.prototype.roll = function (pins) {
+  if (pins === 10) {
+    this.score = 'X';
+  } else {
   this.score += pins;
+  this.rollsThisFrame++;
+  this.updateFrame();
+};
+};
+
+Game.prototype.frame = function () {
+  return this.frame;
+};
+
+Game.prototype.updateFrame = function () {
+  if (this.rollsThisFrame < 2 ) {
+    this.rollsThisFrame++;
+  } else {
+    this.frame++;
+  };
 };

9ed619d9d300d4a976795a79f2aa607edecb44bc
Sam Pritchard <sampritchard@msn.com>
2017-06-10 14:47:49 +0100

    test

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,7 +11,6 @@ it ('starts with the score 0', function() {
   expect(game.score).toEqual(0);
 });
 
-
 it ('can increase the score depending on the roll', function() {
   game.roll(5)
   expect(game.score).toEqual(5);

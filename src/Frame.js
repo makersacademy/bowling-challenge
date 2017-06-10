9e98e0e7419f27e956489aa3653da04418874d31
Kynosaur <kye.bracey@gmail.com>
2017-06-10 17:38:37 +0100

    Adds Frame.score property which knows the total score of both bowls

--- src/Frame.js
+++ src/Frame.js
@@ -4,6 +4,7 @@ var Frame = function() {
   this._bowls = [null, null];
   this._currentBowl = 0
   this._remainingPins = 10
+  this._score = 0
 }
 
 Frame.prototype.bowls = function() {
@@ -14,10 +15,15 @@ Frame.prototype.bowl = function(pinsAmount) {
   if(this._remainingPins < pinsAmount) throw new Error('Error - cannot knock down more pins than are here! (' + this._remainingPins + ')')
   if(this._currentBowl === 2 || this._remainingPins === 0) throw new Error('Cannot bowl - this frame is complete')
   this._bowls[this._currentBowl] = pinsAmount;
-  this._remainingPins -= pinsAmount
+  this._remainingPins -= pinsAmount;
+  this._score += pinsAmount;
   this._currentBowl ++
 }
 
 Frame.prototype.isComplete = function() {
   return this._currentBowl === 2 ? true : false;
+}
+
+Frame.prototype.score = function() {
+  return this._score;
 }

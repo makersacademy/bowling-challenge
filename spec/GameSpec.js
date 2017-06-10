f83c69ebbdea7358a0e1a4c6b7e4a63b050c6592
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-10 16:49:47 +0100

    Adds tests for 20 rolls which have no strikes & no spares

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -17,4 +17,15 @@ describe('Game', function() {
     for (var roll = 0; roll < 20; roll++) { game.rolls_current_pins(0); };
     expect(game.score()).toEqual(0);
   });
+
+  it("rolls a game with no strikes or spares", function() {
+    for (var roll = 0; roll < 20; roll++) { game.rolls_current_pins(1); };
+    expect(game.stored_pins).toEqual([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+  });
+
+  it("returns a score of 20 for a game where 1 is rolled each time", function(){
+    for (var roll = 0; roll < 20; roll++) { game.rolls_current_pins(1); };
+    expect(game.score()).toEqual(20);
+  });
+
 });

7abb8336db8da66ffcee062cdd0d87024c0dcee3
Spencer <spencer.bartonfisher@me.com>
2017-06-11 17:34:20 +0100

    first roll test added

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -5,9 +5,16 @@ describe ('bowling', function() {
     bowling = new Bowling;
   });
 
-  describe('shows a starting total', function() {
-    it('shows a default starting total of 0', function() {
-      expect(bowling.total).toEqual(0);
+  describe('shows a starting score', function() {
+    it('shows a default starting score of 0', function() {
+      expect(bowling.score).toEqual(0);
     });
   });
+
+  describe('returns a players roll score', function() {
+    it('shows the players score from their roll', function() {
+      expect(bowling.firstRoll()).toBeLessThan(10);
+    });
+  });
+
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,5 +1,12 @@
 var Bowling = function() {
   var STARTING_TOTAL = 0;
+  var ROLL_ONE = Math.floor(Math.random() * 11);
 
-  this.total = STARTING_TOTAL;
+  this.score = STARTING_TOTAL;
+
+
+  Bowling.prototype.firstRoll() = function() {
+    this.score = ROLL_ONE;
+  };
+
 };

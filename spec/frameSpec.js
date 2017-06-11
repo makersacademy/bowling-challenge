4456719cad2e49968069592f37c4804a7b3d9f77
bannastre <bannastre@gmail.com>
2017-06-11 13:32:49 +0100

    Add bowl function that returns a score

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -11,6 +11,20 @@ describe('Frame', function(){
     it('starts with a score of zero', function(){
       expect(frame.score).toEqual(0);
     })
+
+    it('increments the score cumulatively', function(){
+      frame.bowl();
+      expect(frame.score).toEqual(10);
+      frame.bowl();
+      expect(frame.score).toEqual(20);
+    })
+  });
+
+  describe('bowling', function(){
+    it('bowls a ball', function(){
+      expect(frame.bowl()).toMatch("You bowled a ball");
+      expect(frame.score).toEqual(10);
+    });
   });
 
 });
--- src/frame.js
+++ src/frame.js
@@ -3,3 +3,8 @@
 function Frame() {
   this.score = 0
 }
+
+Frame.prototype.bowl = function(){
+  this.score += 10;
+  return 'You bowled a ball'
+}

5de523ed5d52880b83dde196d8e3eae8c19753dd
Kynosaur <kye.bracey@gmail.com>
2017-06-11 15:48:10 +0100

    Updates Game test suite to expect automatic frame adding. ie broke everything, yay :)

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -9,8 +9,9 @@ describe('Game', function() {
 
   describe('frames', function() {
 
-    it('Starts out as an empty array', function() {
-      expect(game.frames()).toEqual([])
+    it('Starts out containing one frame', function() {
+      var frames = game.frames();
+      expect(frames.length).toEqual(1)
     });
   });
 
@@ -19,25 +20,31 @@ describe('Game', function() {
     it('Adds a new frame to the array', function() {
       game.addNewFrame();
       var frames = game.frames();
-      expect(frames.length).toEqual(1);
+      expect(frames.length).toEqual(2);
     });
   });
+
+  describe('_currentFrame', function() {
+
+    it('Knows the index of the current frame', function() {
+      expect(game._currentFrame()).toEqual(0)
+      spyOn(Math, 'random').and.returnValue(0.4);
+      for(var i = 0; i < 2; i++) { game.bowl(); };
+      expect(game._currentFrame()).toEqual(1);
+    })
+  })
 
   describe('currentScore', function() {
 
     it('(after one frame) knows the current score', function() {
-      game.addNewFrame();
       spyOn(Math, 'random').and.returnValue(0.4);
       for(var i = 0; i < 2; i++) { game.bowl(); };
       expect(game.currentScore()).toEqual(8);
     });
 
     it('(after two frames, no bonuses) knows the current score', function() {
-      game.addNewFrame();
       spyOn(Math, 'random').and.returnValue(0.3);
-      for(var i = 0; i < 2; i++) { game.bowl(); };
-      game.addNewFrame();
-      for(var i = 0; i < 2; i++) { game.bowl(); };
+      for(var i = 0; i < 4; i++) { game.bowl(); };
       expect(game.currentScore()).toEqual(12);
     })
   });

4242ce299b522d915ac19112e045abb32c1b5e83
Spencer <spencer.bartonfisher@me.com>
2017-06-17 11:24:23 +0100

    started again from previous commits as was unable to get framscore working, bowling game created and player is able to roll a gutter game and a game with no strike

--- src/bowling.js
+++ src/bowling.js
@@ -1,48 +1,37 @@
 var Bowling = function() {
-  var STARTING_TOTAL = 0;
-  var ROLL_ONE = Math.floor(Math.random() * 11);
-  var ROLL_TWO = Math.floor(Math.random() * 11);
-  var X = 10
-
-  this.score = STARTING_TOTAL;
-  var firstRoll;
-  var secondRoll;
-  // var _common;
-
-  Bowling.prototype.firstRoll = function() {
-    var firstRoll = ROLL_ONE;
-    // _common = firstRoll1;
-    return firstRoll
-  };
-
-  Bowling.prototype.secondRoll = function() {
-    var firstRoll = this.firstRoll()
-    var secondRoll = Math.floor(Math.random() * (11-firstRoll));
-    // _common = secondRoll;
-    return secondRoll;
-  };
-
-  Bowling.prototype.frameScore = function () {
-    var frameScores = this.firstRoll() + this.secondRoll()
-    return frameScores;
-  };
-
-  Bowling.prototype.strikeScore = function() {
-    strike = X;
-    return strike;
-  };
-
-  // Bowling.prototype.score1 = function(){
-  //   this.score = ROLL_ONE
-  // if (this.score === 1) {
-  //   this.score
-  //     this.score = this.score + Math.floor(Math.random() * 10);
-  //     return this.score;
-  // } else if(this.score === 2) {
-  //   this.score
-  //     this.score = this.score + Math.floor(Math.random() * 9);
-  //     return this.score;
-  // };
-  // }
-
+  this.rolls = [];
 };
+
+  Bowling.prototype.roll = function(pins) {
+    this.rolls.push(pins);
+  };
+
+  Bowling.prototype.score = function() {
+    var bowlingResult = 0;
+    for (var i = 0; i < 20; i++){
+      bowlingResult += this.rolls[i];
+    }
+    return bowlingResult;
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  

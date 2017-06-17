3146fa0fd0a94784b1bd71c2e1d6adb6b0e71f72
Kynosaur <kye.bracey@gmail.com>
2017-06-17 17:35:11 +0100

    Adds feature test for full game without 10th frame bonus

--- spec/FeaturesSpec.js
+++ spec/FeaturesSpec.js
@@ -14,3 +14,35 @@ describe('Gutter game', function() {
     expect(function() { game.bowl(0) }).toThrow(new Error('Cannot bowl - game is complete'));
   });
 });
+
+describe('Game with no 10th frame bonus', function() {
+  var game;
+
+  beforeEach(function() {
+    game = new Game;
+  });
+
+  it('Calculates all scores and bonuses correctly, knows it is finished', function() {
+    game.bowl(1);
+    game.bowl(4);
+    game.bowl(4);
+    game.bowl(5);
+    game.bowl(6);
+    game.bowl(4);
+    game.bowl(5);
+    game.bowl(5);
+    game.bowl(10);
+    game.bowl(0);
+    game.bowl(1);
+    game.bowl(7);
+    game.bowl(3);
+    game.bowl(6);
+    game.bowl(4);
+    game.bowl(10);
+    game.bowl(2);
+    game.bowl(7);
+    game.calculateScore();
+    expect(game.currentScore()).toEqual(125);
+    expect(function() { game.bowl(0) }).toThrow(new Error('Cannot bowl - game is complete'));
+  });
+});

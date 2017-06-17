5caf2fb9496a305e3080d39806d6f116ad3db418
Kynosaur <kye.bracey@gmail.com>
2017-06-17 17:47:29 +0100

    Adds feature test for full game with 10th frame bonus

--- spec/FeaturesSpec.js
+++ spec/FeaturesSpec.js
@@ -46,3 +46,36 @@ describe('Game with no 10th frame bonus', function() {
     expect(function() { game.bowl(0) }).toThrow(new Error('Cannot bowl - game is complete'));
   });
 });
+
+describe('Game with 10th frame bonus', function() {
+  var game;
+
+  beforeEach(function() {
+    game = new Game;
+  });
+
+  it('Calculates all scores and bonuses correctly, knows it is finished', function() {
+    game.bowl(1);
+    game.bowl(4);
+    game.bowl(4);
+    game.bowl(5);
+    game.bowl(6);
+    game.bowl(4);
+    game.bowl(5);
+    game.bowl(5);
+    game.bowl(10);
+    game.bowl(0);
+    game.bowl(1);
+    game.bowl(7);
+    game.bowl(3);
+    game.bowl(6);
+    game.bowl(4);
+    game.bowl(10);
+    game.bowl(2);
+    game.bowl(8);
+    game.bowl(6);
+    game.calculateScore();
+    expect(game.currentScore()).toEqual(133);
+    expect(function() { game.bowl(0) }).toThrow(new Error('Cannot bowl - game is complete'));
+  });
+});

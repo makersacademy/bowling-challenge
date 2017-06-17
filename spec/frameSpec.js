f7d1a55414def2298577846b2121934d3d7424c9
bannastre <bannastre@gmail.com>
2017-06-17 22:25:07 +0100

    player can knock down (only) the remaining pins

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -8,6 +8,11 @@ describe('Frame', function(){
   });
 
   it('can knock down pins', function(){
-    expect(frame.bowl()).toEqual(10);
+    expect(frame.bowl()).toBeLessThan(10);
+  });
+
+  it('can knock down the remaining pins', function(){
+    spyOn(frame, 'randomPins').and.returnValue(6)
+    expect(frame.bowl()).toBeLessThan(5);
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,9 +1,13 @@
 // Frame is responsible for knocking down up to 10 pins
 
 var Frame = function() {
-
+  this.remainingPins = 10
 }
 
-Frame.prototype.bowl = function(){
-  return 10;
+Frame.prototype.bowl = function() {
+  return this.remainingPins = 10 - this.randomPins();
+}
+
+Frame.prototype.randomPins = function() {
+  return Math.floor((Math.random() * 10) + 1);
 }

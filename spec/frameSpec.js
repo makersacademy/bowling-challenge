4abcb1d75a1078af0400b594d9b1194a84c4ea5f
Panteha <panteha.ahmadi@gmail.com>
2017-06-17 17:48:40 +0100

    Added Maximum knocked down pins in each frame is 10

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -7,17 +7,24 @@ describe ('Frame', function() {
 
   describe('Frame size', function() {
     it('can have at most two rolls', function() {
-      frame.add_roll(3);
-      frame.add_roll(6);
+      frame.add_roll(1);
+      frame.add_roll(2);
       expect(function(){frame.add_roll(4)}).toThrow(new Error('Each frame can have at most two rolls'));
     });
   });
 
   describe('Frame score', function(){
-    it('calculates the score for one single frame', function(){
+    it('calculates the score for one single frame with no bonus', function(){
       frame.add_roll(1);
       frame.add_roll(4);
       expect(frame.score_no_bonus()).toEqual(5)
+    });
+  });
+
+  describe('Total sum of pins', function(){
+    it('Maximum knocked down pins in each frame is 10', function(){
+      frame.add_roll(6);
+      expect(function(){frame.add_roll(8)}).toThrow(new Error('Maximum knocked down pins in each frame is 10'));
     });
   });
 
--- src/frame.js
+++ src/frame.js
@@ -2,11 +2,17 @@ var Frame = function() {
   this.rolls = [];
 
  Frame.prototype.add_roll = function(knocked_down_pins){
+   var max_knocked_down_pins = knocked_down_pins;
+   this.rolls.forEach(function (roll, index) {
+     max_knocked_down_pins += roll;
+   });
+   if(max_knocked_down_pins > 10){
+     throw new Error('Maximum knocked down pins in each frame is 10');
+   }
    if(this.rolls.length >= 2){
      throw new Error('Each frame can have at most two rolls');
-   } else {
+   }
    this.rolls.push(knocked_down_pins);
-  }
  }
 
  Frame.prototype.score_no_bonus = function(){
@@ -16,7 +22,6 @@ var Frame = function() {
    });
    return score;
  }
-
 
 
 

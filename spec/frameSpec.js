60cebede326891baaf06722b56ff8a0ba6e7bdd9
Panteha <panteha.ahmadi@gmail.com>
2017-06-17 17:11:23 +0100

    Added calculate score for a frame with no bonus

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -5,11 +5,19 @@ describe ('Frame', function() {
     frame = new Frame;
   });
 
-  describe('frame size', function() {
+  describe('Frame size', function() {
     it('can have at most two rolls', function() {
       frame.add_roll(3);
       frame.add_roll(6);
       expect(function(){frame.add_roll(4)}).toThrow(new Error('Each frame can have at most two rolls'));
+    });
+  });
+
+  describe('Frame score', function(){
+    it('calculates the score for one single frame', function(){
+      frame.add_roll(1);
+      frame.add_roll(4);
+      expect(frame.score_no_bonus()).toEqual(5)
     });
   });
 
--- src/frame.js
+++ src/frame.js
@@ -1,12 +1,20 @@
 var Frame = function() {
-  this.roll_count = 0
+  this.rolls = [];
 
  Frame.prototype.add_roll = function(knocked_down_pins){
-   if(this.roll_count >= 2){
+   if(this.rolls.length >= 2){
      throw new Error('Each frame can have at most two rolls');
-   } else{
-   this.roll_count += 1
+   } else {
+   this.rolls.push(knocked_down_pins);
+  }
  }
+
+ Frame.prototype.score_no_bonus = function(){
+   var score = 0;
+   this.rolls.forEach(function (roll, index) {
+     score += roll;
+   });
+   return score;
  }
 
 

0977f202d3454ee6817b534abb1d78078f8a6702
Kavita <kkavita92@gmail.com>
2017-06-17 16:51:35 +0100

    Minor changes to tests

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -11,32 +11,46 @@ describe("Game", function() {
 
   it('can handle gutter game', function() {
     playGame(0,0,10);
-    expect(game.score()).toEqual(0);
+    expect(game.score(10)).toEqual(0);
   });
 
   it('can handle normal frames', function() {
     playGame(2,3,10);
-    expect(game.score()).toEqual(50);
+    expect(game.score(10)).toEqual(50);
   });
 
   it('can handle a spare', function() {
-    game.addFrame(new Frame(4,6));
-    game.addFrame(new Frame(8,1));
+    game.addFrame(4,6);
+    game.addFrame(8,1);
     playGame(0,0,8);
-    expect(game.score()).toEqual(27);
+    expect(game.score(10)).toEqual(27);
   });
 
   it('can handle a strike', function() {
-    game.addFrame(new Frame(10));
-    game.addFrame(new Frame(4,3));
+    game.addFrame(10);
+    game.addFrame(4,3);
     playGame(0,0,8);
-    expect(game.score()).toEqual(24);
+    expect(game.score(10)).toEqual(24);
+  });
+
+  it('can handle 2 strikes in a row', function () {
+    game.addFrame(10);
+    game.addFrame(10);
+    game.addFrame(4,0);
+    console.log(game.frames[2][0]);
+    expect(game.score(3)).toEqual(42);
+  });
+
+  it('can calculate score before game finishes', function() {
+    game.addFrame(10);
+    game.addFrame(4,3);
+    expect(game.score(2)).toEqual(24);
   });
 
 
-  function playGame(firstRoll, secondRoll, n) {
-    for(var i = 0;i < n; i++) {
-      game.addFrame(new Frame(firstRoll, secondRoll));
+  function playGame(firstRoll, secondRoll, frames) {
+    for(var i = 0;i < frames; i++) {
+      game.addFrame(firstRoll, secondRoll);
     };
   };
 
--- src/game.js
+++ src/game.js
@@ -2,28 +2,47 @@ var Game = function () {
   this.frames = [];
 };
 
-Game.prototype.addFrame = function(frame) {
+Game.prototype.addFrame = function(firstRoll, secondRoll) {
+  frame = new Frame(firstRoll, secondRoll);
   this.frames.push(frame.rolls);
 };
 
-Game.prototype.score = function() {
-  var score = 0;
-  for(var i = 0; i < 10; i++) {
-    if(this.isStrike(this.frames[i])) {
-      score += (10 + this.frames[i+1][0] + this.frames[i+1][1]);
-    } else if(this.isSpare(this.frames[i])) {
-      score += 10 + this.frames[i+1][0];
-    } else {
-      score += (this.frames[i][0] + this.frames[i][1]);
-    };
-  };
-  return score;
+Game.prototype.frameTotal = function(frame) {
+  return frame[0] + frame[1];
 };
 
 Game.prototype.isSpare = function(frame) {
-  return frame[0] + frame[1]  === 10;
+  return this.frameTotal(frame) === 10;
 };
 
 Game.prototype.isStrike = function(frame) {
   return frame[0] === 10;
+};
+
+Game.prototype.strikeBonus = function(index) {
+  if (this.frames[index + 1][0] === 10) { 
+    return this.frameTotal(this.frames[index + 1]) + this.frames[index + 2][0];
+  } else {
+  return this.frameTotal(this.frames[index + 1]);
+  };
+};
+
+Game.prototype.spareBonus = function(index) {
+  return this.frames[index + 1][0];
+};
+
+Game.prototype.score = function(framesPlayed) {
+  var score = 0;
+  var frames = this.frames;
+
+  for(var i = 0; i < framesPlayed; i++) {
+    if(this.isStrike(frames[i])) {
+      score += 10 + this.strikeBonus(i);
+    } else if(this.isSpare(frames[i])) {
+      score += 10 + this.spareBonus(i);
+    } else {
+      score += this.frameTotal(frames[i]);
+    };
+  };
+  return score;
 };

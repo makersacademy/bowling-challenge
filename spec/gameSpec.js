59e8282d38515208ecb292e502d5b08b8a7aa925
Mário Ribeiro <mail@marioribeiro.com>
2017-06-18 21:59:12 +0100

    Add Games totalScore and framesPlayed

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,4 +10,25 @@ describe('Game', function(){
     game.roll([4,4])
     expect(game._frames[0].rolls).toEqual([4,4])
   })
+
+  it('calculates the total game score', function(){
+    game.roll([4,4])
+    game.roll([4,1])
+    game.roll([10,0])
+    game.roll([9,0])
+    game.roll([5,3])
+    game.roll([4,4])
+    game.roll([4,1])
+    game.roll([10,0])
+    game.roll([9,0])
+    game.roll([5,3])
+    expect(game.totalScore()).toEqual(80);
+  })
+
+  it('keeps track of the number of frames player', function(){
+    game.roll([4,4])
+    game.roll([4,1])
+    game.roll([10,0])
+    expect(game.framesPlayed()).toEqual(3);
+  })
 })
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,22 @@
 function Game(){
   this._frames = []
+  this.score = 0;
+  this._MAX_FRAMES = 10
 }
 
 Game.prototype.roll = function(rolls) {
   frame = new Frame(rolls)
+  frame.calculateScore()
   this._frames.push(frame)
+}
+
+Game.prototype.totalScore = function() {
+  for (i = 0; i < this._frames.length; i++) {
+    this.score += this._frames[i]._score
+  }
+  return this.score
+}
+
+Game.prototype.framesPlayed = function(){
+  return this._frames.length
 }

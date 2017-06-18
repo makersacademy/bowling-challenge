926b56e32707f88dbcea443f5bbaf19cbb8af4cb
Kynosaur <kye.bracey@gmail.com>
2017-06-18 11:21:11 +0100

    Adds TenthFrame.isComplete() function

--- src/TenthFrame.js
+++ src/TenthFrame.js
@@ -3,9 +3,19 @@
 var TenthFrame = function() {
   this._bowls = [null, null, null]
   this._currentBowl = 0
+  this._remainingPins = 10
+  this._score = 0
 }
 
 TenthFrame.prototype.bowl = function(pinsAmount) {
   this._bowls[this._currentBowl] = pinsAmount;
   this._currentBowl ++;
+  this._score += pinsAmount;
+  this._remainingPins -= pinsAmount;
+}
+
+TenthFrame.prototype.isComplete = function() {
+  if(this._currentBowl < 2) return false;
+  if(this._currentBowl < 3 && this._score === (10 || 20)) return false;
+  return true;
 }

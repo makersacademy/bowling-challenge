eed80a794a74a8c482e3422a4d7e1c698324aa53
Aaron <aaronrod@gmail.com>
2017-06-18 21:05:45 +0100

    Adds Css and html to scorecard -Adds framescore function to connect to the css -All tests pass

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,7 +1,34 @@
+// $(document).ready(function(){
+//   game = new Game();
+//   var firstRoll;
+//   var runScore;
+//   pinHit = function(pins){
+//     if(isRollValid(pins) == false){
+//       return;
+//     }
+//     isGameOver();
+//     game.roll(pins);
+//     frameScore();
+//     totalScore();
+//   };
+  
 var BowlingGame = function(){
   this.rolls = [];
 };
 
+frameScore = function() {
+    frame1.innerHTML = game.frameScore['1'];
+    frame2.innerHTML = game.frameScore['2'];
+    frame3.innerHTML = game.frameScore['3'];
+    frame4.innerHTML = game.frameScore['4']
+    frame5.innerHTML = game.frameScore['5']
+    frame6.innerHTML = game.frameScore['6']
+    frame7.innerHTML = game.frameScore['7']
+    frame8.innerHTML = game.frameScore['8']
+    frame9.innerHTML = game.frameScore['9']
+    frame10.innerHTML = game.frameScore['10']
+  };
+  
 BowlingGame.prototype.roll = function(pins) {
   this.rolls.push(pins);
 };

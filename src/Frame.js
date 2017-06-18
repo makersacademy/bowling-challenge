7353ad871dfdc6e54dea9c587aaed4a3c059ce1d
Mário Ribeiro <mail@marioribeiro.com>
2017-06-19 00:07:46 +0100

    Remove MAX_SCORE (unused)

--- src/Frame.js
+++ src/Frame.js
@@ -1,16 +1,15 @@
 function Frame(rolls){
   this._score = 0
-  this.MAX_SCORE = 10
   this.rolls = rolls
 }
 
 Frame.prototype.firstRoll = function() {
   return this.rolls[0]
-};
+}
 
 Frame.prototype.secondRoll = function() {
   return this.rolls[1]
-};
+}
 
 Frame.prototype.calculateScore = function() {
   this._score += this.firstRoll() + this.secondRoll()

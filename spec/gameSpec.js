509760fcb7514c19b27eb4fbccd313d1d04a5313
Dom <dom.mcdonnell@gmail.com>
2017-06-18 19:07:06 +0100

    Game now starts with a score of 0

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,4 +5,9 @@ describe('Game', function(){
   it('can start a new game', function(){
     var game = new Game();
   });
+
+  it('starts a new game with a score of 0', function(){
+    var game = new Game;
+    expect(game.score).toEqual(0);
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 'use strict'
 
 var Game = function(){
-
+  this.score = 0;
 }

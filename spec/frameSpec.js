f839e114f8a0ce5a0e120f06f9fdaadfa6ce639a
Dom <dom.mcdonnell@gmail.com>
2017-06-18 21:10:31 +0100

    -Frame stores result in an array.

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,23 +1,38 @@
 'use strict'
 
 describe('Frame', function(){
-  var frame = new Frame;
+  var frame;
+
+  beforeEach(function(){
+    frame = new Frame();
+  });
 
   it('starts a frame with a roll count of 0', function(){
     expect(frame.rollCount).toEqual(0);
   });
   it('can roll and knock down a random number of pins less than 10', function(){
     frame.roll();
-    expect(frame.score).toBeLessThan(10);
+    expect(frame.score).toBeLessThan(11);
   });
   it('can count the number of rolls', function(){
+    frame.roll();
     frame.roll();
     expect(frame.rollCount).toEqual(2);
   });
   it('throws an error if three rolls are taken', function(){
     frame.roll();
+    frame.roll();
+    frame.roll();
     expect(function(){
       frame.roll();
     }).toThrow("Only two rolls allowed!");
+  });
+  it('starts with a result of an empty array', function(){
+    expect(frame.result).toEqual([])
+  });
+  it('can return the score after each frame', function(){
+    frame.roll();
+    frame.roll();
+    expect(frame.result.length).not.toBe(null);
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -3,10 +3,13 @@
 var Frame = function(){
   this.rollCount = 0;
   this.score = 0;
+  this.pinsRemaining = 10;
+  this.result = [];
 }
 
 Frame.prototype.roll = function(){
-  if (this.rollCount > 2) throw ('Only two rolls allowed!');
-  this.score = Math.floor(Math.random() * 11);
+  if (this.rollCount >= 3) throw ('Only two rolls allowed!');
+  this.score = Math.floor(Math.random() * (this.pinsRemaining)+1);
   this.rollCount ++;
+  this.result.push(this.score);
 }

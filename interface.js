18c94c054fa7335cf9cded9d017bbf4198439aff
Panteha <panteha.ahmadi@gmail.com>
2017-06-18 21:42:32 +0100

    Ran eslint

--- interface.js
+++ interface.js
@@ -1,28 +1,25 @@
-  var game = new Game();
-$(document).ready(function(){
-
-
-  for(var i=0; i<10; i++){
-    frame = new Frame(i==9);
-    game.add_frame(frame);
+var game = new Game()
+$(document).ready(function () {
+  for (var i = 0; i < 10; i++) {
+    frame = new Frame(i === 9)
+    game.add_frame(frame)
   }
 
-  $('#calculate_score').click(function(){
-    $("#final_score").val(game.finalScore());
-  });
+  $('#calculate_score').click(function () {
+    $('#final_score').val(game.finalScore())
+  })
 
-  function createUpdate(i, j) {
-    $( "#frame_"+(i+1)+"_"+j+"_pins" ).change(function() {
-      var frame = game.frames[i];
-      frame.add_roll(parseInt($("#frame_"+(i+1)+"_"+j+"_pins" ).val()));
-      $("#frame_"+(i+1)+"_"+j+"_score").text(frame.score_no_bonus());
-  });
+  function createUpdate (i, j) {
+    $('#frame_' + (i + 1) + '_' + j + '_pins').change(function () {
+      var frame = game.frames[i]
+      frame.add_roll(parseInt($('#frame_' + (i + 1) + '_' + j + '_pins').val()))
+      $('#frame_' + (i + 1) + '_' + j + '_score').text(frame.score_no_bonus())
+    })
   }
 
-  for(var i=0; i< 10; i++ ){
-    createUpdate(i,1);
-    createUpdate(i,2);
-}
-createUpdate(9,3);
-
-});
+  for (var i = 0; i < 10; i++) {
+    createUpdate(i, 1)
+    createUpdate(i, 2)
+  }
+  createUpdate(9, 3)
+})

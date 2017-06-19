66f1705c030e8c7d11680f443c0545c0e345ca14
jade <jadealvares@hotmail.co.uk>
2017-06-19 11:14:50 +0100

    implemented new guard clause to not bowl more than 10 frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,19 +8,17 @@ describe("Game", function(){
   });
 
   it("should add all the frames into an array names game", function(){
+    game.addFrame()
     game.frames[0].getFirstBowl(4);
     game.frames[0].getSecondBowl(3);
     game.addFrame()
     game.frames[1].getFirstBowl(2);
     game.frames[1].getSecondBowl(6);
-    game.addFrame()
-    expect(game.frames.length).toEqual(3)
+    expect(game.frames.length).toEqual(2)
   });
 
   it("should not have more than 10 frames in", function() {
-    for(var i = 0; i < 9; i++) {
-      game.addFrame()
-    }
-    expect(function(){game.addFrame()}).toThrow("You can only have 10 frames")
+    for(var i = 0; i < 10; i++) {
+      game.addFrame()} expect(function(){game.addFrame();}).toThrow("You can only have 10 frames")
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,18 @@
 function Game() {
-  this.frames = [new Frame()]
+  this.frames = [];
+  // [new Frame()]
 }
 
 Game.prototype.addFrame = function() {
-  if(this.frames.length > 11) throw ("You can only have 10 frames")
-  this.frames.push(new Frame())
+  // console.log(game.frames)
+  if(this.frames.length >= 10) throw ("You can only have 10 frames");
+  // console.log(game.frames)
+  this.frames.push(new Frame());
 };
+
+// Game.prototype.checksIfStrike = function() {
+//   for(var i = 0; i < 10; i++) {
+//     if(game.frames[i].isStrike)
+//     // check each bowl in each frame if 10, then add [i+1] to 10
+//   }
+// }

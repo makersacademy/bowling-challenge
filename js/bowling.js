6a6b02636a23f3e7bd8f50e7858e58766a9b6e1d
JayWebDevCom <jaiye.s@gmail.com>
2017-07-03 13:19:38 +0100

    refactor

--- js/bowling.js
+++ js/bowling.js
@@ -1,234 +1,227 @@
-var Bowling = function () {
-  this._score = 0
-  this._frames = []
-  for (var i=0; i<10; i++){
-    this._frames.push(new Frame)
+var Bowling = function() {
+  this._score = 0;
+  this._frames = [];
+  for (var i = 0; i < 10; i++) {
+    this._frames.push(new Frame());
   }
-}
-
-Bowling.prototype.score = function(){
-  return this._score
-}
-
-Bowling.prototype.framesToPlay = function(){
-  return this._frames.filter(function(frame){
-    return frame._finished === false
-  })
-}
-
-Bowling.prototype.frameIndex = function(frame){
-  return this._frames.indexOf(frame)
-}
-
-
-Bowling.prototype.updateRollScoreSheet = function(frameIndex, rollIndex, pinsDown){
-  var element = 'frame-' + (frameIndex + 1) + '-roll-' + (rollIndex + 1)
-  document.getElementById(element).innerHTML = pinsDown
-}
-
-Bowling.prototype.updateFrameScoreSheet = function () {
-  this._frames.forEach(function(frame, index, array){
-    var frameIndex = index
-    var element = 'frame-' + ( parseInt(frameIndex) + 1)
-    document.getElementById(element).innerHTML = frame._score
-  })
-}
-
-Bowling.prototype.updateTotalScore = function () {
-  var totalScore = 0
-  this._frames.forEach(function(frame){
-    totalScore += frame._score
-  })
-  document.getElementById('total-score').innerHTML = totalScore
-  this._score = totalScore
 };
 
-Bowling.prototype.hideButtons = function (rollIndex, pinsDown) {
-  if (rollIndex == 0 && pinsDown != 10 ){
-    var upto = 10 - pinsDown
-    for( i = upto+1; i<=10; i++    ){
-      document.getElementById('num-' + i).style.display = "none";
+Bowling.prototype.score = function() {
+  return this._score;
+};
+
+Bowling.prototype.framesToPlay = function() {
+  return this._frames.filter(function(frame) {
+    return frame._finished === false;
+  });
+};
+
+Bowling.prototype.frameIndex = function(frame) {
+  return this._frames.indexOf(frame);
+};
+
+Bowling.prototype.updateRollScoreSheet = function(
+  frameIndex,
+  rollIndex,
+  pinsDown
+) {
+  var element = "frame-" + (frameIndex + 1) + "-roll-" + (rollIndex + 1);
+  document.getElementById(element).innerHTML = pinsDown;
+};
+
+Bowling.prototype.updateFrameScoreSheet = function() {
+  this._frames.forEach(function(frame, index, array) {
+    var frameIndex = index;
+    var element = "frame-" + (parseInt(frameIndex) + 1);
+    document.getElementById(element).innerHTML = frame._score;
+  });
+};
+
+Bowling.prototype.updateTotalScore = function() {
+  var totalScore = 0;
+  this._frames.forEach(function(frame) {
+    totalScore += frame._score;
+  });
+  document.getElementById("total-score").innerHTML = totalScore;
+  this._score = totalScore;
+};
+
+Bowling.prototype.hideButtons = function(rollIndex, pinsDown) {
+  if (rollIndex == 0 && pinsDown != 10) {
+    var upto = 10 - pinsDown;
+    for (i = upto + 1; i <= 10; i++) {
+      document.getElementById("num-" + i).style.display = "none";
     }
   }
 };
 
-Bowling.prototype.showButtons = function () {
-  for(var i=0; i<=10; i++ ){
-    document.getElementById('num-' + i).style.display = "block";
+Bowling.prototype.showButtons = function() {
+  for (var i = 0; i <= 10; i++) {
+    document.getElementById("num-" + i).style.display = "block";
   }
 };
 
-Bowling.prototype.addBonusesTenthFrame = function (array) {
-  array.forEach(function(frame, index, array){
-    if ( frame.isSpare() && frame._updated == false) {
+Bowling.prototype.addBonusesTenthFrame = function(array) {
+  array.forEach(function(frame, index, array) {
+    if (frame.isSpare() && frame._updated == false) {
       if (index < 9) {
-      var nextFrame = array[index + 1]
-      if (nextFrame._finished == true ) {
-        frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
-        frame._updated = true
-      }
-      }
-    }
-
-    if ( frame.isStrike() && frame._updated == false) {
-
-      if ( index == 7 ) { // 8th frame
-        var nextFrame = array[index + 1]
-        var twoFramesAfter = array[index + 2]
-        if (nextFrame._finished == true ) {
-          if ( nextFrame.isStrike() ) {
-            frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
-            frame._score += parseInt(twoFramesAfter._rolls[0]._pinsDown)
-          } else {
-            frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
-            frame._score += parseInt(nextFrame._rolls[1]._pinsDown)
-          }
-          frame._updated = true
-        }
-      } else if (index == 8) {  // 9th frame
-        var nextFrame = array[index + 1]
+        var nextFrame = array[index + 1];
         if (nextFrame._finished == true) {
-          frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
-          frame._score += parseInt(nextFrame._rolls[1]._pinsDown)
-          frame._updated = true
+          frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
+          frame._updated = true;
         }
       }
     }
 
-  }) // close forEach
+    if (frame.isStrike() && frame._updated == false) {
+      if (index == 7) {
+        // 8th frame
+        var nextFrame = array[index + 1];
+        var twoFramesAfter = array[index + 2];
+        if (nextFrame._finished == true) {
+          if (nextFrame.isStrike()) {
+            frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
+            frame._score += parseInt(twoFramesAfter._rolls[0]._pinsDown);
+          } else {
+            frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
+            frame._score += parseInt(nextFrame._rolls[1]._pinsDown);
+          }
+          frame._updated = true;
+        }
+      } else if (index == 8) {
+        // 9th frame
+        var nextFrame = array[index + 1];
+        if (nextFrame._finished == true) {
+          frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
+          frame._score += parseInt(nextFrame._rolls[1]._pinsDown);
+          frame._updated = true;
+        }
+      }
+    }
+  }); // close forEach
+}; // close function
 
-} // close function
-
-Bowling.prototype.addBonusesFrameNinthFrameOrLess = function (array) {
-  array.forEach(function(frame, index, array){
-    if ( frame.isStrike() && frame._updated == false) {
-      var nextFrame = array[index + 1]
-      var twoFramesAfter = array[index + 2]
-      if (nextFrame._finished == true ) {
-        if ( nextFrame.isStrike() ) {
-          frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
-          frame._score += parseInt(twoFramesAfter._rolls[0]._pinsDown)
+Bowling.prototype.addBonusesFrameNinthFrameOrLess = function(array) {
+  array.forEach(function(frame, index, array) {
+    if (frame.isStrike() && frame._updated == false) {
+      var nextFrame = array[index + 1];
+      var twoFramesAfter = array[index + 2];
+      if (nextFrame._finished == true) {
+        if (nextFrame.isStrike()) {
+          frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
+          frame._score += parseInt(twoFramesAfter._rolls[0]._pinsDown);
         } else {
-          frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
-          frame._score += parseInt(nextFrame._rolls[1]._pinsDown)
+          frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
+          frame._score += parseInt(nextFrame._rolls[1]._pinsDown);
         }
         // console.log('just updated a strike frame')
-        frame._updated = true
+        frame._updated = true;
       }
-    } else if ( frame.isSpare() && frame._updated == false) {
-      var nextFrame = array[index + 1]
-      if (nextFrame._finished == true ) {
-        frame._score += parseInt(nextFrame._rolls[0]._pinsDown)
+    } else if (frame.isSpare() && frame._updated == false) {
+      var nextFrame = array[index + 1];
+      if (nextFrame._finished == true) {
+        frame._score += parseInt(nextFrame._rolls[0]._pinsDown);
         // console.log('just updated a spare frame')
-        frame._updated = true
+        frame._updated = true;
       }
     }
-  }) // close forEach
+  }); // close forEach
+};
+
+function allRollsOver(roll, index, array) {
+  return roll._finished === true;
 }
 
+Bowling.prototype.roll = function(pinsDown) {
+  this.showButtons();
 
-function allRollsOver(roll, index, array){
-  return roll._finished === true
-}
+  var frameToPlay = this.framesToPlay()[0];
 
-Bowling.prototype.roll = function (pinsDown) {
-
-  this.showButtons()
-
-  var frameToPlay = this.framesToPlay()[0]
-
-  if (typeof frameToPlay === 'undefined') {
-    alert("Game should be over, no more frames!")
+  if (typeof frameToPlay === "undefined") {
+    alert("Game should be over, no more frames!");
   }
 
-  var frameIndex = this.frameIndex(frameToPlay)
+  var frameIndex = this.frameIndex(frameToPlay);
 
-  if ( frameIndex <= 8 ) {
+  if (frameIndex <= 8) {
+    var roll = frameToPlay.returnRolls()[0];
 
-    var roll = frameToPlay.returnRolls()[0]
+    var rollIndex = frameToPlay.rollIndex(roll);
 
-    var rollIndex = frameToPlay.rollIndex(roll)
+    roll.knockPinsDown(pinsDown);
 
-    roll.knockPinsDown(pinsDown)
+    frameToPlay._score += parseInt(roll._pinsDown);
 
-    frameToPlay._score += parseInt(roll._pinsDown)
+    this.hideButtons(rollIndex, pinsDown);
 
-    this.hideButtons(rollIndex, pinsDown)
-
-    if ( frameToPlay.isStrike() ){
+    if (frameToPlay.isStrike()) {
       // console.log('a strike frame')
-      frameToPlay._isStrike = true
-      var nextRoll = frameToPlay.returnRolls()[rollIndex + 1]
-      nextRoll._finished = true
+      frameToPlay._isStrike = true;
+      var nextRoll = frameToPlay.returnRolls()[rollIndex + 1];
+      nextRoll._finished = true;
     }
 
-    if (frameIndex <= 9 ) {
-
+    if (frameIndex <= 9) {
       // update strike bonus
       // does not update until next frame is complete
-      this.addBonusesFrameNinthFrameOrLess(this._frames)
-
-
-    } else if ( frameIndex == 9 ) { // close if (frameIndex <= 9 )
+      this.addBonusesFrameNinthFrameOrLess(this._frames);
+    } else if (frameIndex == 9) {
+      // close if (frameIndex <= 9 )
       // update strike bonus
       // does not update until next frame is complete
-      if ( frame.isStrike() && frame._updated == false) {
-        var nextFrame = this._frames[frameIndex + 1]
-        if (nextFrame._finished == true ) {
+      if (frame.isStrike() && frame._updated == false) {
+        var nextFrame = this._frames[frameIndex + 1];
+        if (nextFrame._finished == true) {
           // console.log('just updated a strike frame this is frame 9')
-          frame._updated = true
+          frame._updated = true;
         }
       }
     }
 
-    roll._finished = true
+    roll._finished = true;
 
-    this.updateRollScoreSheet(frameIndex, rollIndex, pinsDown)
+    this.updateRollScoreSheet(frameIndex, rollIndex, pinsDown);
+  } else if (frameIndex == 9) {
+    //tenth frame
 
-  } else if (frameIndex == 9) { //tenth frame
+    var frameToPlay = this.framesToPlay()[0];
+    var frameIndex = this.frameIndex(frameToPlay);
+    var roll = frameToPlay.returnRolls()[0];
+    var rollIndex = frameToPlay.rollIndex(roll);
 
-    var frameToPlay = this.framesToPlay()[0]
-    var frameIndex = this.frameIndex(frameToPlay)
-    var roll = frameToPlay.returnRolls()[0]
-    var rollIndex = frameToPlay.rollIndex(roll)
+    roll.knockPinsDown(pinsDown);
 
-    roll.knockPinsDown(pinsDown)
+    this.hideButtons(rollIndex, pinsDown);
 
-    this.hideButtons(rollIndex, pinsDown)
+    frameToPlay._score += parseInt(roll._pinsDown);
 
-    frameToPlay._score += parseInt(roll._pinsDown)
+    this.updateRollScoreSheet(frameIndex, rollIndex, pinsDown);
 
-    this.updateRollScoreSheet(frameIndex, rollIndex, pinsDown)
-
-    if ( frameToPlay.isStrike() ){
-      if ( frameToPlay._rolls.length == 2 && frameToPlay.isStrike()){
-        frameToPlay._rolls.push(new Roll)
+    if (frameToPlay.isStrike()) {
+      if (frameToPlay._rolls.length == 2 && frameToPlay.isStrike()) {
+        frameToPlay._rolls.push(new Roll());
       }
-      frameToPlay._isStrike = true
-      var nextRoll = frameToPlay.returnRolls()[rollIndex + 1]
+      frameToPlay._isStrike = true;
+      var nextRoll = frameToPlay.returnRolls()[rollIndex + 1];
     }
 
-    roll._finished = true
+    roll._finished = true;
 
-
-    if (  frameToPlay._rolls.every(allRollsOver)  ){
-      this.updateFrameScoreSheet(frameIndex, frameToPlay)
-      frameToPlay._finished = true
+    if (frameToPlay._rolls.every(allRollsOver)) {
+      this.updateFrameScoreSheet(frameIndex, frameToPlay);
+      frameToPlay._finished = true;
     }
 
     // adding bonuses
     this.addBonusesTenthFrame(this._frames);
-
   } // close 10th frame
 
-
-  if (  frameToPlay._rolls.every(allRollsOver)  ){
-    this.updateFrameScoreSheet(frameIndex, frameToPlay)
-    frameToPlay._finished = true
+  if (frameToPlay._rolls.every(allRollsOver)) {
+    this.updateFrameScoreSheet(frameIndex, frameToPlay);
+    frameToPlay._finished = true;
   }
 
-  this.updateTotalScore()
-
+  this.updateTotalScore();
 };
 
 // module.exports = Bowling;

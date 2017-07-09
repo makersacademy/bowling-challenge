7c5c9de39add210b1bbf55f509bb5e304b38c6b1
Simon Hempenstall <simonjhemp@hotmail.com>
2017-07-09 17:01:03 +0100

    Add new frame methods to check for spare or strike

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -10,7 +10,48 @@ describe("Frame", function () {
       expect(frame.firstRoll).toEqual(0);
     });
     it("second roll score starts at 0", function () {
-      expect(frame.firstRoll).toEqual(0);
+      expect(frame.secondRoll).toEqual(0);
+    });
+    it("spare is false by default", function () {
+      expect(frame.spare).toEqual(false);
+    });
+    it("strike is false by default", function () {
+      expect(frame.strike).toEqual(false);
+    });
+    it("rollsMade is 0 by default", function () {
+      expect(frame.rollsMade).toEqual(0);
     });
   });
+
+  describe("#roll", function () {
+    beforeEach(function () {
+      frame.roll(5);
+    });
+
+    it("records first roll score", function () {
+      expect(frame.firstRoll).toEqual(5);
+    });
+    it("records second roll score", function () {
+      frame.roll(4);
+      expect(frame.secondRoll).toEqual(4);
+    });
+    it("raises error if too many pins entered", function () {
+      expect(function() {frame.roll(20)}).toThrow("Too many pins, please start a new frame")
+    });
+    it("raises error if more than two rolls are attempted", function () {
+      frame.roll(4);
+      expect(function() {frame.roll(2)}).toThrow("Only two rolls allowed per frame")
+    });
+
+    it("changes spare to true when all pins are down on secondRoll", function () {
+      frame.roll(5)
+      expect(frame.spare).toEqual(true);
+    });
+  });
+
+  it("changes strike to true when 10 is scored on first roll", function () {
+    frame.roll(10);
+    expect(frame.strike).toEqual(true);
+  });
+
 });
--- src/frame.js
+++ src/frame.js
@@ -1,12 +1,46 @@
 function Frame() {
-  this.firstRoll = 0
-  this.secondRoll = 0
+  this.firstRoll = 0;
+  this.secondRoll = 0;
+  this.spare = false;
+  this.strike = false;
+  this.rollsMade = 0;
 }
 
-Frame.prototype.rollOne = function(number) {
-  this.firstRoll += number;
-};
+Frame.prototype.roll = function(numberOfPins) {
+  if (this.rollsMade === 0) {
+    this.firstRoll += numberOfPins;
+    this.checks();
+  }
+  else {
+    this.secondRoll += numberOfPins;
+    this.checks();
+  }
+}
 
-Frame.prototype.rollTwo = function(number) {
-  this.secondRoll += number;
-};
+Frame.prototype.spareOrStrike = function () {
+  if (this.firstRoll === 10) {
+    this.strike = true;
+  } else if((this.firstRoll + this.secondRoll) === 10 ) {
+    this.spare = true;
+  }
+}
+
+Frame.prototype.rollNumber = function () {
+  if (this.rollsMade < 2) {
+    this.rollsMade += 1;
+  } else {
+    throw "Only two rolls allowed per frame";
+  }
+}
+
+Frame.prototype.tooManyPins = function () {
+  if ((this.firstRoll + this.secondRoll) > 10) {
+    throw "Too many pins, please start a new frame"
+  }
+}
+
+Frame.prototype.checks = function () {
+  this.rollNumber();
+  this.spareOrStrike();
+  this.tooManyPins();
+}

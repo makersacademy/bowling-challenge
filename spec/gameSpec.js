411e9bcb5c0af85d12703e9b467345e550ac0447
Verity <vhonebon@gmail.com>
2017-07-10 09:30:37 +0100

    refactored game code

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -14,6 +14,10 @@ describe("Game", function() {
     expect(game.frames).toEqual([]);
   });
 
+  it("starts with a score of 0", function() {
+    expect(game.score).toEqual(0);
+  });
+
   describe("addFrame", function() {
     it("can add a frame to the frames array", function() {
       game.addFrame(frame);
@@ -23,13 +27,34 @@ describe("Game", function() {
 
   describe("totalGameScore", function() {
     it("can give you a total score for the game", function() {
-      frame.score = 6
-      frame2.score = 7
+      frame.score = 6;
+      frame2.score = 7;
       game.addFrame(frame);
       game.addFrame(frame2);
-      expect(game.totalGameScore()).toEqual(13);
+      game.totalGameScore();
+      expect(game.score).toEqual(13);
     });
+  });
 
+  describe("spareBonus", function() {
+    it("calculates bonus points resulting from spares", function() {
+      frame.spare = true;
+      frame2.turn = [4,3];
+      game.addFrame(frame);
+      game.addFrame(frame2);
+      game.spareBonus();
+      expect(game.score).toEqual(4);
+    });
+  });
 
+  describe("strikeBonus", function() {
+    it("calculates bonus points resulting from stikes", function() {
+      frame.strike = true;
+      frame2.score = 7;
+      game.addFrame(frame);
+      game.addFrame(frame2);
+      game.strikeBonus();
+      expect(game.score).toEqual(7);
+    });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 function Game() {
   this.frames = [];
+  this.score = 0;
 }
 
 Game.prototype.addFrame = function(frame) {
@@ -7,9 +8,29 @@ Game.prototype.addFrame = function(frame) {
 }
 
 Game.prototype.totalGameScore = function() {
-  var gameScore = 0
+  var gameScore = 0;
   this.frames.forEach(function(element) {
     gameScore = gameScore + element.score;
   });
-  return gameScore
+  this.score = this.score + gameScore
+}
+
+Game.prototype.spareBonus = function() {
+  indices = this.frames.reduce((a, e, i) => (e.spare === true) ? a.concat(i) : a, [])
+  var arrayOfArrays = this.frames;
+  var spareScore = 0;
+  indices.forEach(function(element) {
+    spareScore = spareScore + arrayOfArrays[element + 1].turn[0];
+  });
+  this.score = this.score + spareScore
+}
+
+Game.prototype.strikeBonus = function() {
+  indices = this.frames.reduce((a, e, i) => (e.strike === true) ? a.concat(i) : a, [])
+  var arrayOfArrays = this.frames;
+  var strikeScore = 0;
+  indices.forEach(function(element) {
+    strikeScore = strikeScore + arrayOfArrays[element + 1].score;
+  });
+  this.score = this.score + strikeScore
 }

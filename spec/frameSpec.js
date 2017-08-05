07463be3aad3ddd3587ee044c5874b5d100ef0e6
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-05 16:02:57 +0100

    Added second roll to frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -15,6 +15,11 @@ describe('Frame', function() {
     it('has zero pins recorded for first roll', function() {
       expect(frame.getFirstRoll()).toEqual(0);
     });
+
+    it('has zero pins recorded for second roll', function() {
+      expect(frame.getSecondRoll()).toEqual(0);
+    });
+
   });
 
   describe('setFirstRoll', function() {
@@ -25,6 +30,19 @@ describe('Frame', function() {
 
     it('cannot set first roll to more than 10 pins', function() {
       expect(function() { frame.setFirstRoll(11); }).toThrowError('Pins cannot exceed a maximum of 10');
+    });
+  });
+
+  describe('setSecondRoll', function() {
+    it('can set second roll', function() {
+      frame.setFirstRoll(6);
+      frame.setSecondRoll(3);
+      expect(frame.getSecondRoll()).toEqual(3);
+    });
+
+    it('cannot set second roll if the total is more than 10 pins', function() {
+      frame.setFirstRoll(6);
+      expect(function() { frame.setSecondRoll(5); }).toThrowError('Pins cannot exceed a maximum of 10');
     });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -3,6 +3,7 @@
 var Frame = function(id) {
   this._id = id;
   this._firstRoll = 0;
+  this._secondRoll = 0;
 };
 
 Frame.prototype.getID = function() {
@@ -14,8 +15,17 @@ Frame.prototype.setFirstRoll = function(pins) {
   this._firstRoll = pins;
 };
 
+Frame.prototype.setSecondRoll = function(pins) {
+  this._checkPinsExceeded(pins);
+  this._secondRoll = pins;
+};
+
 Frame.prototype.getFirstRoll = function() {
   return this._firstRoll;
+};
+
+Frame.prototype.getSecondRoll = function() {
+  return this._secondRoll;
 };
 
 Frame.prototype._checkPinsExceeded = function(pins) {

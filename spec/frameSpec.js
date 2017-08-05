12ab46bed487105282ffbe58f1420a323a5cbcce
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-05 15:43:39 +0100

    Added firstRoll to frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -11,5 +11,20 @@ describe('Frame', function() {
     it('has an ID', function() {
       expect(frame.getID()).toEqual(1);
     });
+
+    it('has zero pins recorded for first roll', function() {
+      expect(frame.getFirstRoll()).toEqual(0);
+    });
+  });
+
+  describe('setFirstRoll', function() {
+    it('can set first roll', function() {
+      frame.setFirstRoll(6);
+      expect(frame.getFirstRoll()).toEqual(6);
+    });
+
+    it('cannot set first roll to more than 10 pins', function() {
+      expect(function() { frame.setFirstRoll(11); }).toThrowError('Pins cannot exceed a maximum of 10');
+    });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -2,8 +2,22 @@
 
 var Frame = function(id) {
   this._id = id;
+  this._firstRoll = 0;
 };
 
 Frame.prototype.getID = function() {
   return this._id;
+};
+
+Frame.prototype.setFirstRoll = function(pins) {
+  this._checkPinsExceeded(pins);
+  this._firstRoll = pins;
+};
+
+Frame.prototype.getFirstRoll = function() {
+  return this._firstRoll;
+};
+
+Frame.prototype._checkPinsExceeded = function(pins) {
+  if (this._firstRoll + pins > 10 ) throw new Error('Pins cannot exceed a maximum of 10');
 };

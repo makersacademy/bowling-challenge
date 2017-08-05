9ec1f0c2981c8a1b1f4057009e58722ed5e925b4
BDCraven <bencraven@hotmail.com>
2017-08-05 16:34:05 +0100

    Add passing tests: it can score a strike

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -36,4 +36,14 @@ describe('Bowling Game', function() {
     expect(game.score()).toEqual(14);
   });
 
+  it('can score a strike', function() {
+    game.roll(10);
+    game.roll(2);
+    game.roll(3);
+    for (var i = 0; i < 16; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(20);
+  });
+
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -10,11 +10,15 @@ bowlingGame.prototype.score = function() {
   var result = 0;
 
   for (var scoreIndex = 0; scoreIndex < this.scores.length; scoreIndex++) {
-    result += (this.scores[scoreIndex] + this.scores[scoreIndex + 1]);
-    if (this.scores[scoreIndex] + this.scores[scoreIndex + 1] === 10) {
-      result += this.scores[scoreIndex + 2];
+    if (this.scores[scoreIndex] === 10) {
+      result += (this.scores[scoreIndex] + this.scores[scoreIndex + 1] + this.scores[scoreIndex + 2]);
+    } else {
+      result += (this.scores[scoreIndex] + this.scores[scoreIndex + 1]);
+      if (this.scores[scoreIndex] + this.scores[scoreIndex + 1] === 10) {
+        result += this.scores[scoreIndex + 2];
+      }
+      scoreIndex += 1;
     }
-    scoreIndex += 1;
   }
   return result;
 };

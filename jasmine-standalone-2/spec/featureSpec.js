39e32cf6762485e0e9e9a96ccad93acdc87361a9
EOSullivanBerlin <esully666@gmail.com>
2017-08-05 17:43:56 +0200

    Added the ablity to add up all the scores for a game up to scoring nine every time

--- jasmine-standalone-2/spec/featureSpec.js
+++ jasmine-standalone-2/spec/featureSpec.js
@@ -1,7 +1,59 @@
 describe('Bowling Game', function() {
-  var game = new BowlingGame;
-  it('allows the player to roll a ball and get a random score between 0 - 10', function(){
-    expect(game.roll()).toBeLessThan(11);
-    expect(game.roll()).toBeGreaterThan(-1);
+  var game;
+  beforeEach(function() {
+    game = new BowlingGame;
   });
+
+  it('allows the player to roll a ball and get a random score between 0 - 10', function(){
+    game.roll(2)
+    expect(game.score()).toBeLessThan(11);
+    expect(game.score()).toBeGreaterThan(-1);
+  });
+
+  it('it counts the number of rolls', function(){
+    game.roll(1);
+    game.roll(1);
+    expect(game.rollCount()).toEqual(2);
+  });
+  describe('#score', function() {
+
+    it('returns strike if I score ten', function() {
+      spyOn(Math, 'floor').and.returnValue(10);
+      game.roll(10);
+      expect(game.score()).toEqual('strike');
+    });
+
+    it('returns spare if the score 10 in with two rolls', function() {
+      spyOn(Math, 'floor').and.returnValue(5);
+      game.roll(5);
+      game.roll(5);
+      expect(game.score()).toEqual('spare');
+    });
+  });
+ describe('All the different games', function() {
+
+  it('the player can have a gutter game', function() {
+      // spyOn(Math, 'floor').and.returnValue(0);
+      for (var i = 0; i < 20; i++) {
+  			game.roll(0);
+  		};
+
+      expect(game.result()).toEqual(0)
+    });
+
+    // function rollMany (n, pins) {
+    // for (var i = 0; i < n; i++) {
+    //   game.roll(pins)
+    // }
+
+  it('the player can score all ones', function() {
+      // spyOn(Math, 'floor').and.returnValue(2);
+      for (var i = 0; i < 20; i++) {
+  			game.roll(1);
+  		};
+      expect(game.result()).toEqual(20)
+    });
+
+  });
+
 });
--- jasmine-standalone-2/src/bowlingGame.js
+++ jasmine-standalone-2/src/bowlingGame.js
@@ -1,7 +1,39 @@
 var BowlingGame = function() {
   this._possibleScore = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  this._rollCount = 0;
+  this._score = [];
 };
 
-BowlingGame.prototype.roll = function() {
-return this._possibleScore[Math.floor(Math.random() * this._possibleScore.length)];
+// BowlingGame.prototype.roll = function() {
+//   this._rollCount += 1;
+//   console.log(Math.floor(Math.random() * this._possibleScore.length));
+//   this._score.push(this._possibleScore[Math.floor(Math.random() * this._possibleScore.length)]);
+//   return this._score[0]
+// };
+
+BowlingGame.prototype.roll = function(roll) {
+  this._rollCount += 1;
+  this._score.push(roll);
+  console.log(this._score)
+};
+
+
+BowlingGame.prototype.rollCount = function() {
+  return this._rollCount;
+};
+
+BowlingGame.prototype.score = function() {
+  if (this._score[0] === 10 && this._rollCount == 1){
+    return 'strike';
+  } else if ((this._score[0] + this._score[1]) === 10 && this._rollCount == 2){
+    return 'spare';
+  }
+    return this._score[0];
+  };
+
+BowlingGame.prototype.result = function() {
+  var sum = this._score.reduce(function(pv, cv) { return pv + cv; }, 0);
+  // console.log(sum);
+  return sum;s
+  console.log(this._score)
 };

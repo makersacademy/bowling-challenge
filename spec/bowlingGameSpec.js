f7271280a2e575523621f566dc2fb617b5659385
BDCraven <bencraven@hotmail.com>
2017-08-06 08:53:37 +0100

    Refactoring

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -9,20 +9,16 @@ describe('Bowling Game', function() {
   });
 
   it('has no score by default', function() {
-    expect(game.score()).toEqual(0);
+    expect(game.score()).toEqual(NaN);
   });
 
   it('can score a gutter game', function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollMany(20, 0);
     expect(game.score()).toEqual(0);
   });
 
   it('can score 20 rolls which knock down one pin per roll', function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+    rollMany(20, 1);
     expect(game.score()).toEqual(20);
   });
 
@@ -30,9 +26,7 @@ describe('Bowling Game', function() {
     game.roll(5);
     game.roll(5);
     game.roll(2);
-    for (var i = 0; i < 17; i++) {
-      game.roll(0);
-    }
+    rollMany(17,0);
     expect(game.score()).toEqual(14);
   });
 
@@ -40,10 +34,19 @@ describe('Bowling Game', function() {
     game.roll(10);
     game.roll(2);
     game.roll(3);
-    for (var i = 0; i < 16; i++) {
-      game.roll(0);
-    }
+    rollMany(16, 0);
     expect(game.score()).toEqual(20);
   });
+
+  it('can score a perfect game', function() {
+    rollMany(12, 10);
+    expect(game.score()).toEqual(300);
+  });
+
+  function rollMany(rolls, score) {
+    for (var i = 0; i < rolls; i++) {
+    game.roll(score);
+    }
+  }
 
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -8,17 +8,40 @@ bowlingGame.prototype.roll = function(pins) {
 
 bowlingGame.prototype.score = function() {
   var result = 0;
+  var scoreIndex = 0;
+  var game = this;
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
 
-  for (var scoreIndex = 0; scoreIndex < this.scores.length; scoreIndex++) {
-    if (this.scores[scoreIndex] === 10) {
-      result += (this.scores[scoreIndex] + this.scores[scoreIndex + 1] + this.scores[scoreIndex + 2]);
-    } else {
-      result += (this.scores[scoreIndex] + this.scores[scoreIndex + 1]);
-      if (this.scores[scoreIndex] + this.scores[scoreIndex + 1] === 10) {
-        result += this.scores[scoreIndex + 2];
-      }
+    if (isStrike()) {
+      result += strikeScore();
       scoreIndex += 1;
+    } else {
+      result += standardScore();
+      if (isSpare()) {
+          result += spareScore();
+      }
+      scoreIndex += 2;
     }
   }
   return result;
+
+  function isStrike() {
+    return game.scores[scoreIndex] === 10;
+  }
+
+  function strikeScore() {
+    return (game.scores[scoreIndex] + game.scores[scoreIndex + 1] + game.scores[scoreIndex + 2]);
+  }
+
+  function isSpare() {
+    return (game.scores[scoreIndex] + game.scores[scoreIndex + 1] === 10);
+  }
+
+  function spareScore () {
+    return game.scores[scoreIndex + 2];
+  }
+
+  function standardScore() {
+    return (game.scores[scoreIndex] + game.scores[scoreIndex + 1]);
+  }
 };

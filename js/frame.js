77d0c34d59c3d14cdb03bbbca88f116bb207366e
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-08-06 15:47:27 +0100

    Add frame object with tests

--- js/frame.js
+++ js/frame.js
@@ -1,4 +1,33 @@
 'use strict';
 
 function Frame() {
+  this.MAX_PINS = 10;
+  this.complete = false;
+  this.rollsCount = 0;
+  this.pinsKnocked = 0;
+};
+
+Frame.prototype.isComplete = function() {
+  return this.complete;
+};
+
+Frame.prototype.getPinsKnocked = function() {
+  return this.pinsKnocked;
+};
+
+Frame.prototype.roll = function(pins) {
+  this.rollsCount += 1;
+  this._increaseScoreBy(pins);
+  this._CompleteFrameIfRequired();
+};
+
+
+// Private implementation
+
+Frame.prototype._increaseScoreBy = function(n) {
+  this.pinsKnocked += n;
+};
+
+Frame.prototype._CompleteFrameIfRequired = function() {
+  if(this.getPinsKnocked() === 10 || this.rollsCount === 2) { this.complete = true };
 };
--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,5 +1,48 @@
 'use strict';
 
 describe ('Frame', function() {
-  var frame = new Frame();
+  var frame;
+
+  beforeEach(function(){
+    frame = new Frame();
+  });
+
+  describe('Should allow rolls and return the number of pins knocked', function() {
+    it('Receives single roll and returns correct number of pins knocked', function(){
+      var pins = Math.floor(Math.random() * 11);
+      frame.roll(pins);
+      expect(frame.getPinsKnocked()).toEqual(pins);
+    });
+
+    it('Receives two rolls and returns correct number of pins knocked', function(){
+      var pins1 = Math.floor(Math.random() * 10);
+      frame.roll(pins1);
+      var pins2 = Math.floor(Math.random() * 11 - pins1);
+      frame.roll(pins2);
+      expect(frame.getPinsKnocked()).toEqual(pins1 + pins2);
+    });
+  });
+
+  describe('Should handle completion', function(){
+    it('Not complete at the beginning', function(){
+      expect(frame.isComplete()).toEqual(false);
+    });
+
+    it('Not complete after a non-strike roll', function(){
+      var pins = Math.floor(Math.random() * 10);
+      frame.roll(pins);
+      expect(frame.isComplete()).toEqual(false);
+    });
+
+    it('Complete after two rolls', function(){
+      frame.roll(3);
+      frame.roll(1);
+      expect(frame.isComplete()).toEqual(true);
+    });
+
+    it('Complete after strike', function(){
+      frame.roll(10);
+      expect(frame.isComplete()).toEqual(true);
+    });
+  });
 });

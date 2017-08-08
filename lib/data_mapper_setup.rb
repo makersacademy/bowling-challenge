a34c62cf7651d3067717b06a66fc836a36fbfe51
Ryan Davidson <daviryan@aol.com>
2017-08-08 07:28:13 +0100

    Trial php app for single page html app

--- lib/data_mapper_setup.rb
+++
@@ -1,6 +0,0 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_development")
-# DataMapper.auto_migrate!
-DataMapper.finalize

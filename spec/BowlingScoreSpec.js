5fa524fbb937636d85adafaf01c73968a122f9ed
Costas Kiteou <ckiteou22@gmail.com>
2017-08-12 04:50:57 +0100

    Add functionality to know whether its the first or secoond bowl in a frame

--- spec/BowlingScoreSpec.js
+++ spec/BowlingScoreSpec.js
@@ -13,12 +13,23 @@ describe("BowlingScore", function() {
 
     describe("#enterBowlingScore", function() {
         it("can increase the score when score is entered", function() {
-            bowlingscore.enterBowlingScore(1)
-            expect(bowlingscore.showTotalScore()).toEqual(1)
+            bowlingscore.enterBowlingScore(1);
+            expect(bowlingscore.showTotalScore()).toEqual(1);
         });
 
-        it("cannot enter a score higher than 10", function() {
+        it("cannot enter a score higher than 10 pins on first bowl", function() {
             expect(function() { bowlingscore.enterBowlingScore(11); }).toThrow("Maximum score is 10")
+        });
+    });
+
+    describe("#isThisFirstBowl", function() {
+        it("knows if it is the first bowl in a frame", function() {
+          expect(bowlingscore.isThisFirstBowl()).toEqual(true);
+        });
+
+        it("knows if it is the second bowl in a frame", function() {
+          bowlingscore.enterBowlingScore(1);
+          expect(bowlingscore.isThisFirstBowl()).toEqual(false);
         });
     });
 });
--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -3,13 +3,24 @@ function BowlingScore() {
   this.totalScore = this.DEFAULT_TOTAL_SCORE;
   this.MAX_SCORE = 10;
   this.maxScore = this.MAX_SCORE;
+  this.isFirstBowl = true;
 }
+
+BowlingScore.prototype.enterBowlingScore = function (score) {
+  if (this.isFirstBowl) {
+    if (score > this.maxScore) throw "Maximum score is 10"
+    this.totalScore += score;
+    this.isFirstBowl = false;
+  } else {
+    this.totalScore += score;
+    this.isFirstBowl = true;
+  }
+};
 
 BowlingScore.prototype.showTotalScore = function () {
   return this.totalScore;
 };
 
-BowlingScore.prototype.enterBowlingScore = function (score) {
-  if (score > this.maxScore) { throw "Maximum score is 10" }
-  this.totalScore += score;
+BowlingScore.prototype.isThisFirstBowl = function () {
+  return this.isFirstBowl;
 };

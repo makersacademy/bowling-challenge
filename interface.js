113d2c87475aa4ecac745f69c7a27e7d762fa227
BDCraven <bencraven@hotmail.com>
2017-08-13 14:47:23 +0100

    Score of 10 now displays in the correct cells

--- interface.js
+++ interface.js
@@ -1,20 +1,18 @@
 $(document).ready(function() {
   var game = new bowlingGame();
-  // var count = 0;
   var roll = 1;
 
   $('#Btn0, #Btn1, #Btn2, #Btn3, #Btn4, #Btn5, #Btn6, #Btn7, #Btn8, #Btn9, #Btn10').click(function() {
-    // if (count == 0){
-    //   game.roll(parseInt($(this).val()));
-    //   $('#roll1').text($(this).val());
-    //
-    // }else{
-      game.roll(parseInt($(this).val()));
-      $('#roll' + roll).text($(this).val());
-    // }
+    var buttonValue = parseInt($(this).val());
+
+    game.roll(buttonValue);
+    $('#roll' + roll).text(buttonValue);
     $('#score11').text(game.score());
+    if (buttonValue == 10 && roll < 19) {
+      roll += 2;
+    }else{
     roll += 1;
-    // count += 1;
+    }
   });
 
   $('#btn11').click(function() {

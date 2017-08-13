194368512b152c0fc519191bc0445054f826f3b1
Sapphire Mason-Brown <saphmb@gmail.com>
2017-08-13 18:26:23 +0100

    Calculations for frame scores

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,1 +1,30 @@
 'use strict';
+
+describe ('Frame', function() {
+
+  var frame;
+
+  beforeEach(function() {
+    frame = new Frame();
+  });
+
+  describe('when it is initialized', function() {
+    it('should have a frame score of 0', function () {
+      expect(frame.getFrameScore()).toEqual(0);
+    });
+  });
+
+	describe('the frame score', function() {
+
+
+
+		it('should increase by the roll score', function () {
+			frame.setFirstRoll(4);
+			frame.setSecondRoll(3);
+			console.log(this.firstRoll)
+			console.log(this.secondRoll)
+			expect(frame.getFrameScore()).toEqual(7);
+		});
+	});
+
+});
--- src/Frame.js
+++ src/Frame.js
@@ -1,1 +1,28 @@
 "use strict";
+
+function Frame () {
+	this.frameScore = 0;
+	this.bonus = 0;
+	this.rolls = [];
+	this.firstRoll = 0;
+	this.secondRoll = 0;
+};
+
+Frame.prototype.getFrameScore = function() {
+	this.calculateFrameScore();
+	return this.frameScore;
+};
+
+Frame.prototype.setFirstRoll = function(score) {
+	this.firstRoll = score;
+	this.rolls.push(this.firstRoll);
+};
+
+Frame.prototype.setSecondRoll = function(score) {
+	this.secondRoll += score;
+	this.rolls.push(this.secondRoll);
+};
+
+Frame.prototype.calculateFrameScore = function(score) {
+	this.frameScore = this.rolls.reduce(function (a, b) { return a + b; }, 0);
+};

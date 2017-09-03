807eb05b624aa5164508dc4d66a6acf4e1440503
Jenny Bacon <jenniferbacon01@gmail.com>
2017-09-03 20:03:32 +0100

    trying travis again3

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+jasmine: {
+  js: {
+    src: jsFiles,
+    options: {
+      specs: 'spec/*Spec.js',
+      // template: "SpecRunner.html",
+      // templateOptions: {
+      //   application_id: process.env.ALGOLIA_APPLICATION_ID,
+      //   api_key: process.env.ALGOLIA_API_KEY
+      // }
+    }
+  }
+},

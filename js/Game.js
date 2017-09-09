6945ed472d717c90c5b8f44d873e0a40b696a4ad
rolandosorbelli <rolando.sorbelli@gmail.com>
2017-09-09 10:42:24 +0100

    Refactored some code and added the spare calculation

--- js/Game.js
+++ js/Game.js
@@ -1,15 +1,41 @@
 function Game(){
-	this.frame = [];
-	this.result = 0;
+	this.turns = []; // <--- Array of pins of all the turns that complete a game. ~
 };
 
 Game.prototype.roll = function(pins){
-	this.frame.push(pins);
-};
+	this.turns.push(pins); // <--- Pushes the pins into the array of turns. ~
+}
 
 Game.prototype.score = function(){
-	for (var i = 0; i < this.frame.length; i++) {
-		this.result += this.frame[i];
+	var result = 0;
+	var turnNumber = 0;
+	var frame = this;
+
+	function spare(){
+		return frame.turns[turnNumber] + frame.turns[turnNumber + 1] == 10;
+		// <--- I removed "this" because it was referring to a different Object. So I gave it a variable called "frame". ~
 	}
-	return this.result;
+
+	function spareScore(){
+		return frame.turns[turnNumber] + frame.turns[turnNumber + 1] + frame.turns[turnNumber + 2];
+	} // <--- It calculates the number of pins of the first 3 turns. ~
+
+	function standardScore(){
+		return frame.turns[turnNumber] + frame.turns[turnNumber + 1];
+	} // <--- It calculates the number of pins of the first 2 turns. ~
+
+	function nextFrame(){
+		turnNumber += 2;
+	} // <--- It goes to the next frame (adds 2 index numbers in the array). ~
+
+	for (var frameNum = 0; frameNum < 10; frameNum++) {
+
+		if (spare()) {
+			result += spareScore();
+		} else {
+			result += standardScore();
+		}
+		nextFrame();
+	};
+	return result; // <--- Calculation of the score. ~
 };
--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,25 +1,42 @@
 describe("Game", function() {
+
   var game;
 
   beforeEach(function() {
     game = new Game();
   });
 
-  it("should start a new game", function(){
-    expect(game.score()).toEqual(0);
-  });
-
   it("should return the number of the pins that have been knocked down", function(){
     game.roll(5);
     game.roll(2);
+    for (var i = 0; i < 18; i++) {
+      game.roll(0);
+    };
     expect(game.score()).toEqual(7);
-  })
+  });
 
   it("scores a gutter game", function(){
     for (var i = 0; i < 20; i++) {
       game.roll(0);
-    }
+    };
     expect(game.score()).toEqual(0);
+  });
+
+  it("rolls 3 every turns", function(){
+    for (var i = 0; i < 20; i++) {
+      game.roll(3);
+    };
+    expect(game.score()).toEqual(60);
+  });
+
+  it("rolls a spare", function(){
+    game.roll(3);
+    game.roll(7);
+    game.roll(2);
+    for (var i = 0; i < 17; i++) {
+      game.roll(1);
+    };
+    expect(game.score()).toEqual(31);
   })
-  
+
 });

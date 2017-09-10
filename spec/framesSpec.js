056f16af94a76a0ed8c626d29da078a0c60157bc
Nick Otter <nicholas.a.otter@gmail.com>
2017-09-10 20:47:31 +0100

    Added gutter game functionality

--- spec/framesSpec.js
+++ spec/framesSpec.js
@@ -11,15 +11,46 @@ describe('Frames Class', function() {
       frames = new Frames();
     });
 
-    it('frames starts empty', function() {
-      expect(frames.getAllFrameScores()).toEqual([]);
+    it('frame scores starts empty', function() {
+      expect(frames.getFrameScores()).toEqual([]);
+    });
+
+    it('frame totals starts empty', function() {
+      expect(frames.getFrameTotals()).toEqual([]);
+    });
+
+    it('it current roll starts at 0', function() {
+      expect(frames.getCurrentRoll()).toEqual(0);
     });
 
     it('score per frame is recorded', function() {
       // mock
       game.roll(5);
-      expect(game.frames.getAllFrameScores()).toEqual([5]);
+      expect(game.frames.getFrameScores()).toEqual([5]);
     });
+
+    it('frame totals are recorded', function() {
+      game.roll(5);
+      game.roll(5);
+      expect(game.frames.getFrameTotals()).toEqual([10]);
+    });
+
+    it('frame totals aren\'t recorded before frame complete', function() {
+      game.roll(5);
+      expect(game.frames.getFrameTotals()).toEqual([0]);
+    });
+
+  describe('.resetTest', function() {
+    it('frame scores reset after a gutter game', function() {
+      var i = 0;
+
+      for (i = 0; i < 21; i++) {
+            game.roll(0);
+                               }
+       game.frames.resetTest;
+     expect(game.frames.getFrameScores()).toEqual([]);
+    });
+  });
 
   });
 });
--- src/frames.js
+++ src/frames.js
@@ -6,17 +6,53 @@
 
 function Frames() {
   this.DEFAULT_FRAME_SCORES = [];
+  this.DEFAULT_FRAME_TOTALS = [];
+  this.DEFAULT_SPARE = false;
+  this.DEFAULT_CURRENT_ROLL = 0;
+  this.DEFAULT_TOTAL_SCORE = 0;
   this.STRIKE = 10;
   this.GUTTER = 0;
-  this.DEFAULT_SPARE = false;
 
+  this.currentRoll = this.DEFAULT_CURRENT_ROLL;
   this.spare = this.DEFAULT_SPARE;
   this.frameScores = this.DEFAULT_FRAME_SCORES;
+  this.frameTotals = this.DEFAULT_FRAME_TOTALS;
+  this.totalScore = this.DEFAULT_TOTAL_SCORE;
 };
 
-Frames.prototype.getAllFrameScores = function() {
-  return this.frameScores;
-};
+  Frames.prototype.getFrameScores = function() {
+    return this.frameScores;
+  };
 
-Frames.prototype.isASpare = function() {
-};
+  Frames.prototype.getFrameTotals = function() {
+    return this.frameTotals;
+  };
+
+  Frames.prototype.getCurrentRoll = function() {
+    return this.currentRoll;
+  };
+
+  Frames.protoype.updateFrameTotals = function() {
+  };
+
+  Frames.prototype.isASpare = function() {
+  };
+
+  Frames.prototype.isAStrike = function() {
+  };
+
+  Frames.prototype.isAGutterGame = function() {
+    this.totalScore === 0 && (this.frameScores.length) === 21;
+  };
+
+  Frames.prototype.resetTest = function() {
+    if (isAGutterGame) {
+       reset;
+    }
+  };
+
+  Frames.prototype.reset = function() {
+    this.frameScores = this.DEFAULT_FRAME_SCORES;
+    this.frameTotals = this.DEFAULT_FRAME_TOTALS;
+    this.totalScore = this.DEFAULT_TOTAL_SCORE;
+  };

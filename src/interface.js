078b40a3ce4aaea13c7c5a5683879aa1a3e9d3cf
Nick Otter <nicholas.a.otter@gmail.com>
2017-09-11 01:19:20 +0100

    Added UI, jQuery init

---
+++ src/interface.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var game = new Game();
+
+$(document).ready(function(){
+
+  $('#0').on("click", function(){
+    game.roll(0);
+    game.calculateTotalScore;
+  })
+
+  $('#1').on("click", function(){
+    game.roll(1);
+    game.calculateTotalScore;
+  })
+
+  $('#2').on("click", function(){
+    game.roll(2);
+    game.calculateTotalScore;
+  })
+
+  $('#3').on("click", function(){
+    game.roll(3);
+    game.calculateTotalScore;
+  })
+
+  $('#4').on("click", function(){
+    game.roll(4);
+    game.calculateTotalScore;
+  })
+
+  $('#5').on("click", function(){
+    game.roll(5);
+    game.calculateTotalScore;
+  })
+
+  $('#6').on("click", function(){
+    game.roll(6);
+    game.calculateTotalScore;
+  })
+
+  $('#7').on("click", function(){
+    game.roll(7);
+    game.calculateTotalScore;
+  })
+
+  $('#8').on("click", function(){
+    game.roll(8);
+    game.calculateTotalScore;
+  })
+
+  $('#9').on("click", function(){
+    game.roll(9);
+    game.calculateTotalScore;
+  })
+
+  $('#10').on("click", function(){
+    game.roll(10);
+    game.calculateTotalScore;
+  })
+
+};

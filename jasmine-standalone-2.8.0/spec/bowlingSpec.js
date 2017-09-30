1e5390e1db5639e47d1f6a4f145cf22abd60f289
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-30 13:25:17 +0100

    Files, folders and first test

---
+++ jasmine-standalone-2.8.0/spec/bowlingSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe ('Bowling scorecard does the following', function() {
+
+  var bowling;
+
+  beforeEach(function(){
+    bowling = new Bowling();
+  });
+
+  it('starts with default score of 0', function() {
+    expect(bowling.score()).toEqual(0);
+  });
+
+});

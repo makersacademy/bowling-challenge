da9a439be026ce1155296751a05815202f5486bd
Luan van Pletsen <luanvanpletsen@msn.com>
2017-09-30 16:59:22 +0100

    3 rolls error

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -6,4 +6,12 @@ describe('Frame', function() {
       expect(frame.score()).toBe(0);
     });
   });
+
+  describe('#roll', function() {
+    it('more than 2 rolls throws an error', function() {
+      frame.roll(1);
+      frame.roll(1);
+      expect(function() {frame.roll();}).toThrowError('Illegal roll, frame complete');
+    });
+  });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,7 +1,16 @@
 function Frame() {
-
+  this.rolls = [];
 }
 
 Frame.prototype.score = function () {
   return 0;
+};
+
+Frame.prototype.roll = function (pins) {
+  if (this.rolls.length === 2) {
+    throw new Error('Illegal roll, frame complete');
+  } else {
+    this.rolls.push(pins);
+  }
+
 };

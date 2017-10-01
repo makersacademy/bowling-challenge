6e068e9a18203cbcc72d79336183f73b7f1def49
James Hughes <hugjimbo77@gmail.com>
2017-10-01 21:10:10 +0100

    added functionality for gutter game & all ones

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -2,8 +2,24 @@
 
 describe ("Scorecard", function() {
 
-  it("start a new scorecard", function () {
+  it("New Scorecard", function () {
     var scoreCard = new ScoreCard();
+  });
+
+  it("Gutter Game", function() {
+    var scoreCard = new ScoreCard();
+    for (var i = 0; i < 20; i++) {
+      scoreCard.roll(0);
+    }
+    expect(scoreCard.score()).toBe(0);
+  });
+
+  it("Roll all ones", function() {
+    var scoreCard = new ScoreCard();
+    for (var i = 0; i < 20; i++) {
+      scoreCard.roll(1);
+    }
+    expect(scoreCard.score()).toBe(20);
   });
 
 });
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,5 +1,17 @@
 'use strict';
 
 var ScoreCard = function() {
+  this.rolls = [];                          //rolls defined as an empty array
+};
 
+ScoreCard.prototype.roll = function(pins) {
+  this.rolls.push(pins);                     //roll function, push on the array
+};
+
+ScoreCard.prototype.score = function() {     //iterate through the rolls array
+  var result = 0;
+  for (var i = 0; i < 20; i++) {
+    result += this.rolls[i];
+  }
+  return result;
 };

7c671ec5b23ce5fa08d58bb51d3c9bd50c2c3c46
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-01 13:39:31 +0100

    Add Game totalScore and framesPlayed

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,7 +8,28 @@ describe('Game', function(){
   });
 
   it('rolls and registers frame', function(){
-    game.roll([4,4]);
-    expect(game._frames[0].rolls).toEqual([4,4]);
+    game.roll([3,4]);
+    expect(game._frames[0].rolls).toEqual([3,4]);
+  });
+
+  it('calculates the total game score', function(){
+    game.roll([3,4]);
+    game.roll([5,1]);
+    game.roll([10,0]);
+    game.roll([9,1]);
+    game.roll([5,3]);
+    game.roll([6,6]);
+    game.roll([10,0]);
+    game.roll([10,0]);
+    game.roll([9,0]);
+    game.roll([5,3]);
+    expect(game.totalScore()).toEqual(90);
+  });
+
+  it("keeps track of player's number of frames", function(){
+    game.roll([3,4]);
+    game.roll([5,1]);
+    game.roll([10,0]);
+    expect(game.framesPlayed()).toEqual(3);
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,23 @@
 
 function Game(){
   this._frames = [];
+  this.score = 0;
+  this._MAX_FRAMES = 10
 }
 
 Game.prototype.roll = function(rolls) {
   var frame = new Frame(rolls);
+  frame.calculateScore();
   this._frames.push(frame);
+};
+
+Game.prototype.totalScore = function() {
+  for (var i = 0; i < this._frames.length; i++) {
+    this.score += this._frames[i]._score;
+  }
+  return this.score;
+};
+
+Game.prototype.framesPlayed = function(){
+  return this._frames.length;
 };

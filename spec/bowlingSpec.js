753ac60beea70baca4a1a31ea2781209f4a5c2f2
Thomas Ikimi <themerc01@gmail.com>
2017-10-01 14:58:11 +0100

    Strike and Spare checks

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -3,33 +3,54 @@ describe('Bowling', function(){
 
   beforeEach(function(){
     game = new Game();
+    game.addFrame(1);
   });
 
   describe('Start new game', function(){
 
     it('starts a new frame', function(){
-      console.log(game.frames)
-      game.addFrame(1);
-      console.log(game.frames)
+      // game.addFrame(1);
       expect(game.currentFrame()).toEqual({frame: 1, round: 1, score: null})
     });
   });
 
-  describe('Score a strike', function(){
+  describe('Score a STRIKE', function(){
 
     it('in round 1', function(){
-      game.addFrame(1);
-      game.roundScore(1, 10);
-      expect(game.currentFrame()).toEqual({frame: 1, round: 1, score: 10})
+      // game.addFrame(1);
+      game.roundScore(10);
+      expect(game.currentFrame()).toEqual({frame: 1, round: 1, score: 10});
+      expect(game.isStrike()).toEqual(true);
     });
   });
 
   describe('Score a hit', function(){
 
     it('knock down 1 pin round 1', function(){
-      game.addFrame(1);
-      game.roundScore(1, 1);
+      // game.addFrame(1);
+      game.roundScore(1);
       expect(game.currentFrame()).toEqual({frame: 1, round: 1, score: 1})
+    });
+  });
+
+  describe('Check round changes', function(){
+
+    it('to 2 after first round', function(){
+      // game.addFrame(1);
+      game.roundScore(1);
+      game.roundScore(1);
+      expect(game.currentFrame().round).toEqual(2);
+    });
+  });
+
+  describe('Check if score', function(){
+
+    it('is a SPARE', function(){
+      // game.addFrame(1);
+      game.roundScore(5);
+      game.roundScore(5);
+      expect(game.currentFrame()).toEqual({frame: 1, round: 2, score: 10});
+      expect(game.isSpare()).toEqual(true);
     });
   });
 
--- src/bowling.js
+++ src/bowling.js
@@ -1,9 +1,9 @@
 var Game = function(){
   this.frames = [];
+  this.currentRound = 1
 };
 
 Game.prototype.addFrame = function(frameNumber){
-  // var frame =
   this.frames.push({frame: frameNumber, round: 1, score: null})
 }
 
@@ -11,7 +11,33 @@ Game.prototype.currentFrame = function(){
   return this.frames[(this.frames.length - 1)];
 };
 
-Game.prototype.roundScore = function(thisRound, thisScore){
-  this.currentFrame().round = thisRound
-  this.currentFrame().score = thisScore
+Game.prototype.roundScore = function(thisScore){
+  this.checkRound();
+  this.currentFrame().round = this.currentRound;
+  this.currentFrame().score += thisScore;
+  this.isStrike();
+  this.isSpare();
+};
+
+Game.prototype.checkRound = function(){
+    if (this.currentFrame().score === null){
+      this.currentRound = 1}
+      else{
+      this.currentRound = 2};
+};
+
+Game.prototype.isStrike = function(){
+  if (this.currentFrame().score === 10){
+    return true}
+  else{
+    false
+  };
+};
+
+Game.prototype.isSpare = function(){
+  if ((this.currentFrame().score === 10) && (this.currentFrame().round === 2)){
+    return true}
+  else{
+    false
+  };
 };

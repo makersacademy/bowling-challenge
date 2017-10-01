9dd137907a7ab95bda06443afca357b6e9e32d8e
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-01 14:46:56 +0100

    The score is updated after each roll. A roll is only accepted if the game is active.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -37,4 +37,37 @@ describe('Game', function(){
     game.roll([3,4]);
     expect(game.framesLeft()).toEqual(9);
   });
+
+  it('is active if there are still frames to be played', function(){
+    game.roll([3,4]);
+    expect(game.isActive()).toEqual(true);
+  });
+
+  it('is not active if all the frames were played', function(){
+    game.roll([3,4]);
+    game.roll([5,1]);
+    game.roll([10,0]);
+    game.roll([9,1]);
+    game.roll([5,3]);
+    game.roll([6,6]);
+    game.roll([10,0]);
+    game.roll([10,0]);
+    game.roll([9,0]);
+    game.roll([5,3]);
+    expect(game.isActive()).toEqual(false);
+  });
+
+  it('blocks roll when game is finished', function(){
+    game.roll([3,4]);
+    game.roll([5,1]);
+    game.roll([10,0]);
+    game.roll([9,1]);
+    game.roll([5,3]);
+    game.roll([6,6]);
+    game.roll([10,0]);
+    game.roll([10,0]);
+    game.roll([9,0]);
+    game.roll([5,3]);
+    expect(function(){ game.roll();}).toThrowError('Game is finished');
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -4,15 +4,22 @@ function Game(){
   this._frames = [];
   this.score = 0;
   this._MAX_FRAMES = 10
+  this.active = true
 }
 
 Game.prototype.roll = function(rolls) {
-  var frame = new Frame(rolls);
-  frame.calculateScore();
-  this._frames.push(frame);
+  if (this.isActive()) {
+    var frame = new Frame(rolls);
+    frame.calculateScore();
+    this._frames.push(frame);
+    this.update()
+  } else {
+    throw new Error('Game is finished')
+  }
 };
 
 Game.prototype.totalScore = function() {
+  this.score = 0
   for (var i = 0; i < this._frames.length; i++) {
     this.score += this._frames[i]._score;
   }
@@ -25,4 +32,15 @@ Game.prototype.framesPlayed = function(){
 
 Game.prototype.framesLeft = function(){
   return this._MAX_FRAMES - this.framesPlayed();
+};
+
+Game.prototype.update = function(){
+  this.totalScore();
+  if (this.framesLeft() === 0) {
+    this.active = false;
+  }
+};
+
+Game.prototype.isActive = function() {
+  return this.active;
 };

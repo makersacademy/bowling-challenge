7d292067924428e799e85cb180ff822afee266bb
James Hughes <hugjimbo77@gmail.com>
2017-10-01 21:42:12 +0100

    Refactored ScoreCardSpec file with higher scope variable

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -2,23 +2,25 @@
 
 describe ("Scorecard", function() {
 
-  it("New Scorecard", function () {
-    var scoreCard = new ScoreCard();
-  });
+  var scoreCard;                                //high scope scoreCard variable
+
+  beforeEach(function() {
+    scoreCard = new ScoreCard();
+  })
+
+  var multipleRolls = function(pins,rolls) {
+    for (var i = 0; i < rolls; i++) {
+      scoreCard.roll(pins);
+    }
+  };
 
   it("Gutter Game", function() {
-    var scoreCard = new ScoreCard();
-    for (var i = 0; i < 20; i++) {
-      scoreCard.roll(0);
-    }
+    multipleRolls(0,20);                        //roll zero pins twenty times
     expect(scoreCard.score()).toBe(0);
   });
 
   it("Roll all ones", function() {
-    var scoreCard = new ScoreCard();
-    for (var i = 0; i < 20; i++) {
-      scoreCard.roll(1);
-    }
+    multipleRolls(1,20);                        //roll 1 pin twenty times
     expect(scoreCard.score()).toBe(20);
   });
 

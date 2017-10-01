93b15495a1a699c52f7d6d273adb106c15faa63d
Joe Ingram <josephcingram@hotmail.co.uk>
2017-10-01 15:46:07 +0100

    add indiv roll methods and new current score test

--- jasmine-standalone-2.8.0/spec/bowlingSpec.js
+++ jasmine-standalone-2.8.0/spec/bowlingSpec.js
@@ -12,26 +12,71 @@ describe ('Bowling scorecard does the following', function() {
     expect(bowling.currentScore()).toEqual(0);
   });
 
-  it('allows user to roll ball and to score points', function(){
-    bowling.roll(3);
+  it('allows user to roll ball and to score 1 point', function(){
+    bowling.roll1();
+    expect(bowling.currentScore()).toEqual(1);
+  });
+
+  it('allows user to roll ball and to score 2 points', function(){
+    bowling.roll2();
+    expect(bowling.currentScore()).toEqual(2);
+  });
+
+  it('allows user to roll ball and to score 3 points', function(){
+    bowling.roll3();
     expect(bowling.currentScore()).toEqual(3);
   });
 
+  it('allows user to roll ball and to score 4 points', function(){
+    bowling.roll4();
+    expect(bowling.currentScore()).toEqual(4);
+  });
+
+  it('allows user to roll ball and to score 5 points', function(){
+    bowling.roll5();
+    expect(bowling.currentScore()).toEqual(5);
+  });
+
+  it('allows user to roll ball and to score 6 points', function(){
+    bowling.roll6();
+    expect(bowling.currentScore()).toEqual(6);
+  });
+
+  it('allows user to roll ball and to score 7 points', function(){
+    bowling.roll7();
+    expect(bowling.currentScore()).toEqual(7);
+  });
+
+  it('allows user to roll ball and to score 6 points', function(){
+    bowling.roll8();
+    expect(bowling.currentScore()).toEqual(8);
+  });
+
+  it('allows user to roll ball and to score 9 points', function(){
+    bowling.roll9();
+    expect(bowling.currentScore()).toEqual(9);
+  });
+
+  it('allows user to roll ball and to score 10 points', function(){
+    bowling.rollStrike();
+    expect(bowling.currentScore()).toEqual(10);
+  });
+
   it('pushes rolls to array of scores', function() {
-    bowling.roll(4);
-    bowling.roll(6);
+    bowling.roll4();
+    bowling.roll6();
     expect(bowling.allRolls()).toEqual([4,6]);
   });
 
   it('skips second roll if user rolls a strike', function() {
-    bowling.roll(10);
-    expect(bowling.allRolls()).toEqual([10,"X"]);
+    bowling.rollStrike();
+    expect(bowling.allRolls()).toEqual([10,null]);
   });
 
   it('calculates score if player roles a strike', function() {
-    bowling.roll(10);
-    bowling.roll(7);
-    bowling.roll(1);
+    bowling.rollStrike();
+    bowling.roll7();
+    bowling.roll1();
     expect(bowling.currentScore()).toEqual(26);
   })
 });
--- jasmine-standalone-2.8.0/src/bowling.js
+++ jasmine-standalone-2.8.0/src/bowling.js
@@ -5,17 +5,57 @@ function Bowling(){
 }
 
 Bowling.prototype.currentScore = function(){
-  return this.score;
+  return this.allScores.reduce(function (a, b) {return a + b}, 0);
 };
 
-Bowling.prototype.roll = function(number) {
-  this.score += number;
-  this.allScores.push(number);
-  if (number === 10) {
-    this.allScores.push("X");
-  }
+Bowling.prototype.roll1 = function() {
+  this.allScores.push(1);
 };
+
+Bowling.prototype.roll2 = function() {
+  this.allScores.push(2);
+};
+
+Bowling.prototype.roll3 = function() {
+  this.allScores.push(3);
+};
+
+Bowling.prototype.roll4 = function() {
+  this.allScores.push(4);
+};
+
+Bowling.prototype.roll5 = function() {
+  this.allScores.push(5);
+};
+
+Bowling.prototype.roll6 = function() {
+  this.allScores.push(6);
+};
+
+Bowling.prototype.roll7 = function() {
+  this.allScores.push(7);
+};
+
+Bowling.prototype.roll8 = function() {
+  this.allScores.push(8);
+};
+
+Bowling.prototype.roll9 = function() {
+  this.allScores.push(9);
+};
+
+Bowling.prototype.rollStrike = function() {
+    this.allScores.push(10,null);
+};
+
+
 
 Bowling.prototype.allRolls = function() {
   return this.allScores;
 };
+
+Bowling.prototype.frameStore = function() {
+  if (this.allScores.count === 2){
+
+  }
+}

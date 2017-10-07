4de5382d50a0ab4665620c413da89ea80ca585f6
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-07 14:49:41 +0100

    Can roll a gutter game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,4 +6,12 @@ describe('Game', function(){
     var game = new Game();
   });
 
+  it('can roll gutter game', function(){
+    var game = new Game();
+    for (var i = 0; i < 20; i++){
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,13 @@
 'use strict';
 
 var Game = function(){
-  
+
+};
+
+Game.prototype.roll = function(pins){
+
+};
+
+Game.prototype.score = function(){
+  return 0;
 };

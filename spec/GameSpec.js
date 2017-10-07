ae71aa82205430188053c7f963a3f519074f0041
Zoe Hopkins <zoe.hopkins@gmail.com>
2017-10-07 16:16:30 +0100

    Can roll a perfect game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -34,6 +34,11 @@ describe('Game', function(){
     expect(game.score()).toBe(24);
   });
 
+  it('can roll perfect game', function(){
+    rollMany(10, 12);
+    expect(game.score()).toBe(300);
+  });
+
   var rollMany = function(pins, rolls){
     for (var i = 0; i < rolls; i++){
       game.roll(pins);

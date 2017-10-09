8a0212c43f63237cfa1eac457dfd1eef19f00278
etiennemustow <etienne.o.mustow@gmail.com>
2017-10-09 02:35:08 +0100

    Can roll all ones test passed. Spec test refactored

--- spec/bowling-challengeSpec.js
+++ spec/bowling-challengeSpec.js
@@ -1,11 +1,25 @@
 describe('Bowling Challenge', function() {
-  
+
+  var game;
+
+  beforeEach(function() {
+    game = new BowlingGame();
+  })
+
     it('can roll a gutter game', function() {
-    var game = new BowlingGame();
-    for (var i = 0; i < 20; i++){
-      game.roll(0)
-    }
-    expect(game.score()).toBe(0)
+      rollMany(0, 20);
+      expect(game.score()).toBe(0)
     })
+
+    it('can roll all ones', function () {
+      rollMany(1, 20);
+      expect(game.score()).toBe(20);
+    })
+
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins)
+    }
+  }
 
 })

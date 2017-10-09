d3b469de371ec5b0cd9ca8910fc402f6cd474d36
etiennemustow <etienne.o.mustow@gmail.com>
2017-10-09 03:01:26 +0100

    Can roll a spare test passed

--- spec/bowling-challengeSpec.js
+++ spec/bowling-challengeSpec.js
@@ -16,6 +16,17 @@ describe('Bowling Challenge', function() {
       expect(game.score()).toBe(20);
     })
 
+    it('can roll a spare', function () {
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      rollMany(0, 17);
+      expect(game.score()).toBe(16);
+    })
+
+
+
+
   var rollMany = function (pins, rolls) {
     for (var i = 0; i < rolls; i++) {
       game.roll(pins)

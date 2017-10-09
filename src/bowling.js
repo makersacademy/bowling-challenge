ef2e64efb398df8a9b3cb647e8276e6dab4babd2
Marcus Rands <mrands@hotmail.co.uk>
2017-10-09 09:52:21 +0100

    Minor style changes

--- src/bowling.js
+++ src/bowling.js
@@ -4,18 +4,20 @@ this.bowlCount = []
 this.rollCount = 0
 this.isStrike = false
 this.isSpare = false
+this.currentFrame = []
 }
 
 Game.prototype.bowl = function(pins) {
-if(this.bowlCount.length < 21 && pins < 11) {
-this.bowlCount.push(pins);
-this.confirmIsStrike();
-this.confirmIsSpare();
-this.updateRollCount();
-} else if(this.bowlCount.length < 21 && pins > 10){
-  throw "You can't enter a score greater than 10."
-} else {
-  throw "The game is complete.";
+  if(this.bowlCount.length < 21 && pins < 11) {
+    this.bowlCount.push(pins);
+    this.confirmIsStrike();
+    this.confirmIsSpare();
+    this.updateRollCount();
+    this.updateScore(pins);
+  } else if(this.bowlCount.length < 21 && pins > 10){
+      throw "You can't enter a score greater than 10."
+  } else {
+      throw "The game is complete.";
   }
 };
 
@@ -38,3 +40,12 @@ Game.prototype.confirmIsSpare = function() {
   if(this.bowlCount.slice(-1)[0] + this.bowlCount.slice(-2)[0] === 10) {
     this.isSpare = true
   }
+
+Game.prototype.updateScore = function(pins) {
+  if(this.isStrike) {
+    this.currentFrame.push(pins)
+  } else if(this.isSpare) {
+    this.currentFrame.push(pins)
+  }else {}
+
+}

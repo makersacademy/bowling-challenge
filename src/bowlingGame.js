5c10583168be8c8de0d952df24c75c05038f2bdf
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-10-27 17:21:21 +0100

    removed roll number property

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,27 +1,28 @@
 function BowlingGame() {
   this._frameNumber = 1
-  this._rollNumber = 1
+  // this._rollNumber = 1
 }
 
 BowlingGame.prototype.nextRound = function() {
   if(this._frameNumber >= 11) throw Error("Game Over!")
   this._frameNumber += 1;
+  // this._rollNumber = 1
 };
 
 BowlingGame.prototype.firstRoll = function(score) {
   if (score === 10 && this._frameNumber === 10) {
+    // this.secondRoll();
     this._rollNumber +=1
   } else if (score === 10) {
     this._frameNumber += 1
   }
   this._frameNumber
-  this._rollNumber += 1
 };
 
 BowlingGame.prototype.secondRoll = function(score) {
-  this._rollNumber = 2
+  // this._rollNumber = 2
   if (score === 10 && this._frameNumber === 10) {
-    this._rollNumber += 1
+    // this._rollNumber += 1
     this._frameNumber -=1
   } else if (score !== 10 && this._frameNumber === 10) {
     this.isOver();

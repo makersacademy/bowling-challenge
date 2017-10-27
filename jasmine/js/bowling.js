e17db20cf9d3c6de4af6642e9adc78938e2b25c5
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-27 20:43:56 +0100

    added strike logic

--- jasmine/js/bowling.js
+++ jasmine/js/bowling.js
@@ -2,26 +2,39 @@ function BowlingGame() {
   'use strict';
   this._totalScore = 0;
   this._scoreCard = [];
-  this._currentFrame = 0;
+  this._currentFrame = 1;
 }
 
 BowlingGame.prototype.score = function() {
   return this._totalScore;
 };
 
-BowlingGame.prototype.roll = function(r1, r2) {
-  if (this._currentFrame === 20) {
+BowlingGame.prototype.roll = function(r1, r2 = 0) {
+  if (this._currentFrame === 21) {
     throw new Error('game has finished!');
   } else {
     this._scoreCard.push({
       rollOne: r1,
       rollTwo: r2
     });
-    this._currentFrame += 1;
-    this._totalScore += (r1 + r2);
+    if (this._currentFrame === 1) {
+      this._totalScore += (r1 + r2);
+      this._currentFrame += 1;
+    } else if (this._scoreCard[this._currentFrame - 2].rollOne === 10) {
+      this._totalScore += (r1 * 2 + r2 * 2);
+      this._currentFrame += 1;
+    } else {
+      this._totalScore += (r1 + r2);
+      this._currentFrame += 1;
+    }
   }
 };
-
 BowlingGame.prototype.showScoreCard = function() {
   return this._scoreCard;
+};
+
+BowlingGame.prototype.bonusPoints = function() {
+  return this._scoreCard[this._currentFrame - 2].rollOne +
+    this._scoreCard[this._currentFrame - 2].rollTne;
+
 };
--- jasmine/spec/bowlingSpec.js
+++ jasmine/spec/bowlingSpec.js
@@ -5,19 +5,56 @@ describe("BowlingGame", function() {
   beforeEach(function() {
     game = new BowlingGame();
   });
-
   it('score starts as 0', function() {
     expect(game.score()).toEqual(0);
   });
-
   it('starts with an empty scorecard', function() {
     expect(game.showScoreCard()).toEqual([]);
   });
-
-  it('keeps score', function() {
+  it('keeps score card', function() {
     game.roll(4, 5);
     expect(game.showScoreCard()[0].rollOne).toEqual(4);
     expect(game.showScoreCard()[0].rollTwo).toEqual(5);
   });
+  it('starts at frame 1', function() {
+    expect(game._currentFrame).toEqual(1);
+  });
+  it('keeps normal scoring', function() {
+    game.roll(4, 5);
+    expect(game.score()).toEqual(9);
+  });
+  it('has 20 frames only', function() {
+    for (var i = 1; i <= 20; i++) {
+      game.roll(6, 4);
+    }
+    expect(function() {
+      game.roll(1, 3);
+    }).toThrowError('game has finished!');
+  });
+
+
+  describe("Strike", function() {
+    it('ends the frame; roll 2 scores 0', function() {
+      game.roll(10);
+      expect(game.score()).toEqual(10);
+      expect(game.showScoreCard()[0].rollTwo).toEqual(0);
+    });
+    it('awards bonus points', function() {
+      console.log(game._currentFrame);
+      game.roll(10);
+      console.log(game._currentFrame);
+      expect(game.score()).toEqual(10);
+      console.log(game._scoreCard[game._currentFrame - 2].rollOne === 10);
+      console.log(game._currentFrame);
+      game.roll(4, 5);
+      console.log(game._currentFrame);
+      console.log(game._scoreCard[game._currentFrame - 2].rollOne === 10);
+      expect(game.score()).toEqual(28);
+    });
+  });
+  // describe("Spare", function(){
+  //
+  // });
+
 
 });

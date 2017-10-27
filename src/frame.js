64c78ebd70bfeec38a4f030a06bd32d032971c78
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-27 12:08:26 +0100

    Added Bonus score constant

--- src/frame.js
+++ src/frame.js
@@ -5,19 +5,22 @@ function frame() {
   this._isASpare = false;
 }
 
+BONUS_SCORE = 10;
+
 frame.prototype.firstRollScore = function(firstScore) {
   this._firstScore = firstScore;
-  if(this._firstScore === 10) {
+  if(this._firstScore === BONUS_SCORE) {
     this._isAStrike = true;
   }
+  // should write a predicate method for the above line at some point
   return this._firstScore;
 };
 
 frame.prototype.secondRollScore = function(secondScore) {
-  if(this._firstScore === 10) return this._secondScore;
-  // should write a predicate method for the above line at some point
+  if(this._isAStrike) return this._secondScore;
+  // should write a predicate method for the below if statement at some point
   this._secondScore = secondScore;
-  if(this._firstScore + this._secondScore === 10) {
+  if(this._firstScore + this._secondScore === BONUS_SCORE) {
     this._isASpare = true;
   }
   return this._secondScore;

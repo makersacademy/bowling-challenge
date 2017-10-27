0bcf80e748a53fbc8c41aafca28d1bf2ace61acf
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-27 19:05:19 +0100

    added keeping total score

--- jasmine/js/bowling.js
+++ jasmine/js/bowling.js
@@ -2,6 +2,7 @@ function BowlingGame() {
   'use strict';
   this._totalScore = 0;
   this._scoreCard = [];
+  this._currentFrame = 0;
 }
 
 BowlingGame.prototype.score = function() {
@@ -9,10 +10,16 @@ BowlingGame.prototype.score = function() {
 };
 
 BowlingGame.prototype.roll = function(r1, r2) {
-  this._scoreCard.push({
-    rollOne: r1,
-    rollTwo: r2
-  });
+  if (this._currentFrame === 20) {
+    throw new Error('game has finished!');
+  } else {
+    this._scoreCard.push({
+      rollOne: r1,
+      rollTwo: r2
+    });
+    this._currentFrame += 1;
+    this._totalScore += (r1 + r2);
+  }
 };
 
 BowlingGame.prototype.showScoreCard = function() {

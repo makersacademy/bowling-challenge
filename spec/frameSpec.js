57915e86b5e0751283c50fbf1dbb5fa31f7d3cd2
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-27 11:47:27 +0100

    Create a strike property for a frame and tested

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -11,9 +11,11 @@ describe('frame', function() {
       expect(testframe.firstRollScore(5)).toEqual(5);
       expect(testframe.secondRollScore(3)).toEqual(3);
     });
-    it('only has one roll score if the first roll score is 10', function() {
+    it('only has one roll score if the first roll score a strike', function() {
       testframe.firstRollScore(10);
       expect(testframe.secondRollScore(2)).toEqual(null);
+      // it seems a bit funny that you give the secondRollScore
+      // an argument in this case. Maybe try rearrange that
     });
   });
   describe('Total Score', function() {
@@ -21,6 +23,12 @@ describe('frame', function() {
       testframe.firstRollScore(5);
       testframe.secondRollScore(3);
       expect(testframe.totalScore()).toEqual(8);
+    });
+  });
+  describe('Strike', function() {
+    it('is a strike if the first roll score is 10', function() {
+      testframe.firstRollScore(10);
+      expect(testframe._isAStrike).toEqual(true);
     });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,16 +1,25 @@
 function frame() {
   this._firstScore = null;
   this._secondScore = null;
+  this._isAStrike = false;
+  this._isASpare = false;
 }
 
 frame.prototype.firstRollScore = function(firstScore) {
   this._firstScore = firstScore;
+  if(this._firstScore === 10) {
+    this._isAStrike = true;
+  }
   return this._firstScore;
 };
 
 frame.prototype.secondRollScore = function(secondScore) {
   if(this._firstScore === 10) return this._secondScore;
+  // should write a predicate method for the above line at some point
   this._secondScore = secondScore;
+  if(this._firstScore + this._secondScore === 10) {
+    this._isASpare = true;
+  }
   return this._secondScore;
 };
 

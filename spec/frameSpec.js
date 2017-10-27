7b7cbd61a1a78b9646bba2356e6b502507985d8b
Alexis Carlier <alexis.carlier0@gmail.com>
2017-10-27 11:02:40 +0100

    Added total score function to frame and tested it

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -11,5 +11,16 @@ describe('frame', function() {
       expect(testframe.firstRollScore(5)).toEqual(5);
       expect(testframe.secondRollScore(3)).toEqual(3);
     });
+    // it('only has one roll score if the first roll score is 10', function() {
+    //   testframe.firstRollScore(10);
+    //   expect(testframe.secondRollScore()).toEqual(null);
+    // });
+  });
+  describe('Total Score', function() {
+    it('has a total score equal to the sum of the roll scores by default', function() {
+      testframe.firstRollScore(5);
+      testframe.secondRollScore(3);
+      expect(testframe.totalScore()).toEqual(8);
+    });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,10 +1,20 @@
 function frame() {
+  this._firstScore = 0;
+  this._secondScore = 0;
 }
 
 frame.prototype.firstRollScore = function(firstScore) {
-  return firstScore;
+  this._firstScore = firstScore;
+  return this._firstScore;
 };
 
 frame.prototype.secondRollScore = function(secondScore) {
-  return secondScore;
+  this._secondScore = secondScore;
+  return this._secondScore;
+};
+
+// frame.prototype.rollScores = function()
+
+frame.prototype.totalScore = function() {
+  return this._firstScore + this._secondScore;
 };

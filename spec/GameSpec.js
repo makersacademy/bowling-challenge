7272142b4edb4e694be3f503b7063246b67256c2
Jon Sanders <jonsanders101@aol.com>
2017-10-28 21:39:12 +0100

    Add functionality to play metthod: updates scorecard with remaining pins and score

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,4 +12,26 @@ describe("Game", function () {
     expect(game.currentRoll).toEqual(1)
   })
 
+  describe("play", function () {
+
+    it("updates scorecard with number of pins hit", function () {
+      spyOn(game,'bowl').and.returnValue(3)
+      game.play()
+      expect(game.scorecard[1][1]['hitPins']).toEqual(3)
+    })
+
+    it("updates scorecard with number of remaining pins in current frame", function () {
+      spyOn(game,'bowl').and.returnValue(3)
+      game.play()
+      expect(game.scorecard[1]['remainingPins']).toEqual(7)
+    })
+
+    it("updates scorecard with score for the current frame", function () {
+      spyOn(game,'bowl').and.returnValue(3)
+      game.play()
+      expect(game.scorecard[1]['frameScore']).toEqual(3)
+    })
+
+  })
+
 })
--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,86 @@
 var Game = function () {
   this.currentFrame = 1
   this.currentRoll = 1
+  this.scorecard = {
+    1:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    2:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    3:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    4:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    5:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    6:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    7:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    8:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    9:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        },
+    10:  {
+          1: {hitPins: null, bonus: []},
+          2: {hitPins: null, bonus: []},
+          3: {hitPins: null, bonus: []},
+          remainingPins: 10,
+          frameScore: 0
+        }
+  }
+}
+
+Game.prototype.play = function () {
+  var hitPins = this.bowl()
+  this._updateScorecard(hitPins)
+}
+
+Game.prototype.bowl = function () {
+  return (Math.floor ((Math.random() * this._getRemainingPins() + 1)))
+}
+
+Game.prototype._getRemainingPins = function () {
+  return (this.scorecard[this.currentFrame]['remainingPins'])
+}
+
+Game.prototype._updateScorecard = function (hitPins) {
+  this.scorecard[this.currentFrame][this.currentRoll]['hitPins'] = hitPins
+  this.scorecard[this.currentFrame]['remainingPins'] -= hitPins
+  this.scorecard[this.currentFrame]['frameScore'] += hitPins
 }

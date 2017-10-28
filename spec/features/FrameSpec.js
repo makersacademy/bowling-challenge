221fb99850cd94e12bae52def4bf21787e9b941c
Ainsley <ab.chang@me.com>
2017-10-28 14:58:52 +0100

    created Game, Frame & set up Jasmine

---
+++ spec/features/FrameSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('Frame', function () {
+
+  var frame;
+
+  beforeEach(function () {
+    frame = new Frame();
+  })
+
+
+});
---
+++ spec/features/GameSpec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+describe('Game', function () {
+
+  var game;
+
+  beforeEach(function () {
+    game = new Game();
+  })
+  
+  // it('creates a new game object', function () {
+  //   expect(new Game()).toEqual(Game);    
+  // });
+
+});

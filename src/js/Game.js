695abb00b27f60cd0d66e0a6d3c79d2ebc0cdca0
Ainsley <ab.chang@me.com>
2017-10-28 21:46:56 +0100

    refactored Game prototype

--- src/js/Game.js
+++ src/js/Game.js
@@ -6,62 +6,64 @@ function Game() {
   this._pendingFrames = [];
 };
 
-Game.prototype.currentFrame = function () {
-  return this._frames.length + 1;
-}
+Game.prototype = {
+  
+  currentFrame: function () {
+    return this._frames.length + 1;
+  },
 
-Game.prototype.pinsRemaining = function () {
-  return this._frame.pinsRemaining();
-};
+  pinsRemaining: function () {
+    return this._frame.pinsRemaining();
+  },
 
-Game.prototype.bowl = function (pins) {
-  if (this.isComplete() && !this.isAllowExtraBowl()) throw new Error('Game has been completed');
-  this._addBonuses(pins);
-  if (!this.isComplete()) {
-    this._frame.bowl(pins);
-    this._completeBowl();
-  };
-};
-
-Game.prototype.currentRound = function () {
-  return this._frame.bowlRound();
-};
-
-Game.prototype._completeBowl = function () {
-
-  if (this._frame.isComplete()) {
-    this._frames.push(this._frame);
-    if (!this.isComplete()) this._frame = new Frame();      
-  };  
-
-};
-
-Game.prototype.score = function () {
-  return this._frames.map(this.frameScore ,this).reduce(this._sum, 0);
-};
-
-Game.prototype.frameScore = function (frame) {
-  return frame.bowls().reduce(this._sum, 0);
-};
-
-Game.prototype._sum = function (a, b) {
-  return a + b;
-};
-
-Game.prototype._addBonuses = function (pins) {
-
-  this._frames.forEach(function (frame) {
-    if (frame.isPendingBonus()) {
-      frame.addBonus(pins);
+  bowl: function (pins) {
+    if (this.isComplete() && !this.isAllowExtraBowl()) throw new Error('Game has been completed');
+    this._addBonuses(pins);
+    if (!this.isComplete()) {
+      this._frame.bowl(pins);
+      this._completeBowl();
     };
-  });
+  },
 
-};
+  currentRound: function () {
+    return this._frame.bowlRound();
+  },
 
-Game.prototype.isAllowExtraBowl = function () {
-  return this._frames[this._frames.length - 1].isPendingBonus();
-}
+  score: function () {
+    return this._frames.map(this.frameScore, this).reduce(this._sum, 0);
+  },
 
-Game.prototype.isComplete = function () {
-  return this._frames.length >= 10;
+  frameScore: function (frame) {
+    return frame.bowls().reduce(this._sum, 0);
+  },
+
+  isAllowExtraBowl: function () {
+    return this._frames[this._frames.length - 1].isPendingBonus();
+  },
+
+  isComplete: function () {
+    return this._frames.length >= 10;
+  },
+
+  _completeBowl: function () {
+    if (this._frame.isComplete()) {
+      this._frames.push(this._frame);
+      if (!this.isComplete()) this._frame = new Frame();
+    };
+  },
+
+  _sum: function (a, b) {
+    return a + b;
+  },
+
+  _addBonuses: function (pins) {
+
+    this._frames.forEach(function (frame) {
+      if (frame.isPendingBonus()) {
+        frame.addBonus(pins);
+      };
+    });
+
+  }
+
 };

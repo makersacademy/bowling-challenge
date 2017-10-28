b84a9515aea6139608e793ae91525f1bbaf5e17f
Chris <cdunham1989@gmail.com>
2017-10-28 13:21:22 +0100

    frame number now updates as the game progresses, unit test passed

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -7,23 +7,34 @@ describe('Game', function() {
     game = new Game();
   });
 
-  it('initializes the game score as 0', function() {
-    expect(game.getCurrentScore()).toEqual(0);
+  describe('initialization', function() {
+    it('initializes the game score as 0', function() {
+      expect(game.getCurrentScore()).toEqual(0);
+    });
+
+    it('initializes the frame count as 1', function() {
+      expect(game.getCurrentFrame()).toEqual(1);
+    });
   });
 
-  it('initializes the frame count as 1', function() {
-    expect(game.getCurrentFrame()).toEqual(1);
-  });
+  describe('point scoring', function() {
+    it('increases score when points are won', function() {
+      game.scoreUpdate(7)
+      expect(game.getCurrentScore()).toEqual(7);
+    });
 
-  it('increases score when points are won', function() {
-    game.scoreUpdate(7)
-    expect(game.getCurrentScore()).toEqual(7);
-  });
+    it('only increases score when a valid number of points are won', function() {
+      game.scoreUpdate(12);
+      game.scoreUpdate(-12);
+      expect(game.getCurrentScore()).toEqual(0);
+    });
 
-  it('only increases score when a valid number of points are added', function() {
-    game.scoreUpdate(12);
-    game.scoreUpdate(-12);
-    expect(game.getCurrentScore()).toEqual(0);
-  });
+    it('increases the frame number after two goes', function () {
+      game.scoreUpdate(7);
+      game.scoreUpdate(2);
+      expect(game.getCurrentScore()).toEqual(9);
+      expect(game.getCurrentFrame()).toEqual(2);
+    });
 
+  });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -4,6 +4,7 @@ function Game() {
   this.MAX_FRAMES = 10;
   this._score = 0;
   this.frame = 1;
+  this._attempts = 0;
 }
 
 Game.prototype.getCurrentScore = function() {
@@ -17,6 +18,16 @@ Game.prototype.getCurrentFrame = function() {
 Game.prototype.scoreUpdate = function(pins) {
   if (pins <11 && pins >= 0 ) {
     this._score += pins;
+    this._attempts += 1;
+    this.frameUpdate();
+  }
+  return;
+};
+
+Game.prototype.frameUpdate = function() {
+  if ( this._attempts === 2 ) {
+    this.frame += 1;
+    this._attempts = 0;
   }
   return;
 };

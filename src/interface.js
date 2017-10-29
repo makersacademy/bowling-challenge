0e6f5a559bb11428609393f7a8064fddfae61d62
Jon Sanders <jonsanders101@aol.com>
2017-10-29 16:26:29 +0000

    Add button selector for pins

--- src/interface.js
+++ src/interface.js
@@ -4,10 +4,23 @@ $( document ).ready(function() {
 
   var game = new Game()
 
-  $('.bowl').click(function () {
+  $('.random-bowl').click(function () {
     var frame = game.currentFrame
     var roll = game.currentRoll
     game.play()
+    $('.f'+frame+'r'+roll).html(game.scorecard[frame][roll]['hitPins'])
+    for(var i = 1; i <= frame; i++) {
+      $('.f'+i+'s').html(game.scorecard[i]['frameScore'])
+    }
+    $('.current-score').html(game.currentScore)
+    $('.final-score').html(game.finalScore)
+  })
+
+  $('[class*="-pin-select"]').click(function () {
+    var hitPins = (Number($(this).val()))
+    var frame = game.currentFrame
+    var roll = game.currentRoll
+    game.play(hitPins)
     $('.f'+frame+'r'+roll).html(game.scorecard[frame][roll]['hitPins'])
     for(var i = 1; i <= frame; i++) {
       $('.f'+i+'s').html(game.scorecard[i]['frameScore'])

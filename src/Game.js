89c9041b8b6c7886c8b9eaaec9f74d26d239b117
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-10-29 18:19:28 +0000

    Adds constants for STRIKE and SPARE

--- src/Game.js
+++ src/Game.js
@@ -11,43 +11,36 @@ Game.prototype.roll = function(roll) {
 };
 
 Game.prototype.score = function(){
+  const STRIKE = 10;
+  const SPARE = 10;
   var result = 0;
   var rollIndex = 0;
+  var game = this;
   for(var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if(this._rolls[rollIndex] == 10) {
-      result += this._rolls[rollIndex] + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+    if(isStrike()) {
+      getStrikeScore();
       rollIndex += 1;
-    }else if(this._rolls[rollIndex] + this._rolls[rollIndex + 1] == 10){
-      result += this._rolls[rollIndex] + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+    }else if(isSpare()){
+      getSpareScore();
       rollIndex += 2;
     }else{
-      result += this._rolls[rollIndex] + this._rolls[rollIndex + 1];
+      result += game._rolls[rollIndex] + game._rolls[rollIndex + 1];
       rollIndex += 2;
     };
   }
   return result;
-};
-/* function Player() {
-}
-Player.prototype.play = function(song) {
-  this.currentlyPlayingSong = song;
-  this.isPlaying = true;
-};
 
-Player.prototype.pause = function() {
-  this.isPlaying = false;
-};
-
-Player.prototype.resume = function() {
-  if (this.isPlaying) {
-    throw new Error("song is already playing");
+  function isStrike(){
+    return game._rolls[rollIndex] == 10;
+  }
+  function getStrikeScore(){
+    result += STRIKE + game._rolls[rollIndex + 1] + game._rolls[rollIndex + 2];
+  }
+  function isSpare(){
+    return game._rolls[rollIndex] + game._rolls[rollIndex + 1] == 10;
+  }
+  function getSpareScore(){
+    result += SPARE + game._rolls[rollIndex + 2];
   }
 
-  this.isPlaying = true;
 };
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
-};
-
-*/

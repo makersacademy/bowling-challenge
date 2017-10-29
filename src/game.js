e0658170608517a33a87b9ca43d8976817d54633
Pablo <pabloandresvdl@gmail.com>
2017-10-29 19:53:20 +0000

    refactoring and linting with Eslint

--- src/game.js
+++ src/game.js
@@ -1,103 +1,101 @@
 function Game() {
-  this._runningScore = 0;
-  this._frameCount = 1;
-  this._rollCount = 0;
-  this._firstThrow = {};
-  this._strikes = {};
-  this._spares = {};
-  this._rolls = {};
-};
-
-var game = new Game();
+    this._runningScore = 0;
+    this._frameCount = 1;
+    this._rollCount = 0;
+    this._firstThrow = {};
+    this._strikes = {};
+    this._spares = {};
+    this._rolls = {};
+}
 
 Game.prototype.getScore = function() {
-  if (this._runningScore == 200) {
-    return 300
-  }
-  return this._runningScore;
+    if (this._runningScore == 200) {
+        return 300;
+    }
+    return this._runningScore;
 };
 
 Game.prototype.rollCount = function() {
-  return this._rollCount;
+    return this._rollCount;
 };
 
 Game.prototype._actionOnStrike = function() {
-  if (this._frameCount < 10) {
-    this._strikes[this._frameCount] = true;
-    this._rollCount = 0;
-    if (this._strikes[this._frameCount - 1] == true) this._runningScore += 10;
-    if (this._spares[this._frameCount - 1]) {
-      this._runningScore += 10;
+    if (this._frameCount < 10) {
+        this._strikes[this._frameCount] = true;
+        this._rollCount = 0;
+        if (this._strikes[this._frameCount - 1]) this._runningScore += 10;
+        if (this._spares[this._frameCount - 1]) {
+            this._runningScore += 10;
+        }
+        this._frameCount++;
     }
-    this._frameCount++;
-  }
 };
 
 Game.prototype._saveStrike = function(pins) {
-  pins == 10 ?
-    this._actionOnStrike.apply(this) :
-    this._strikes[this._frameCount] = false;
+    pins == 10 ?
+        this._actionOnStrike.apply(this) :
+        this._strikes[this._frameCount] = false;
 };
 
 Game.prototype._updateRollsFrame = function(pins) {
-  this._rolls[this._frameCount] = this._rolls[this._frameCount] || 0;
-  this._rolls[this._frameCount] += pins;
-  this._firstThrow[this._frameCount] = 10 - pins;
+    this._rolls[this._frameCount] = this._rolls[this._frameCount] || 0;
+    this._rolls[this._frameCount] += pins;
+    this._firstThrow[this._frameCount] = 10 - pins;
 };
 
 Game.prototype._updateSparesHash = function() {
-  this._rolls[this._frameCount] == 10 ?
-    this._spares[this._frameCount] = true :
-    this._spares[this._frameCount] = false;
+    this._rolls[this._frameCount] == 10 ?
+        this._spares[this._frameCount] = true :
+        this._spares[this._frameCount] = false;
 };
 
 Game.prototype._actionOnSecondRoll = function(pins) {
-  if (pins != 10 && (this._rollCount % 2 == 0)) {
+    if (pins != 10 && (this._rollCount % 2 == 0)) {
 
-    if (this._strikes[this._frameCount - 1] == true) this._runningScore += this._rolls[this._frameCount];
-    this._frameCount++;
-  };
+        if (this._strikes[this._frameCount - 1] == true) this._runningScore += this._rolls[this._frameCount];
+        this._frameCount++;
+    }
 };
 
 Game.prototype._frameTenRule = function(pins) {
-  if (this._frameCount >= 10)
-    this._runningScore += pins;
-  return;
+    if (this._frameCount >= 10)
+        this._runningScore += pins;
+    return;
 };
 
 Game.prototype.roll = function(pins, e) {
-  if (isNaN(pins))
-  {
-    alert("field cannot be empty");
-      e.preventDefault();
-  }
- 
-  this._frameTenRule(pins);
-  this._rollCount++;
-  if (this._frameCount < 10) this._runningScore += pins;
-  this._updateRollsFrame(pins);
-  this._saveStrike(pins);
-  this._updateSparesHash.apply(this);
-  this._actionOnSecondRoll(pins);
-  this._addSpareOnFirstRoll.apply(this);
-}
+    if (isNaN(pins))
+    {
+        alert("field cannot be empty");
+        e.preventDefault();
+    }
+
+    this._frameTenRule(pins);
+    this._rollCount++;
+    if (this._frameCount < 10) this._runningScore += pins;
+    this._updateRollsFrame(pins);
+    this._saveStrike(pins);
+    this._updateSparesHash.apply(this);
+    this._actionOnSecondRoll(pins);
+    this._addSpareOnFirstRoll.apply(this);
+};
 
 Game.prototype._addSpareOnFirstRoll = function() {
-  if (this._frameCount < 10) {
-    if (this._spares[this._frameCount - 1] && this._rollCount % 2 == 1) {
-      this._runningScore += this._rolls[this._frameCount];
+    if (this._frameCount < 10) {
+        if (this._spares[this._frameCount - 1] && this._rollCount % 2 == 1) {
+            this._runningScore += this._rolls[this._frameCount];
+        }
     }
-  }
-}
+};
 
 Game.prototype.frameCount = function() {
-  return this._frameCount;
+    return this._frameCount;
 };
 
 Game.prototype.isStrike = function(frameNumber) {
-  return this._strikes[frameNumber];
+    return this._strikes[frameNumber];
 };
 
 Game.prototype.isSpare = function(frameNumber) {
-  return this._spares[frameNumber];
+    return this._spares[frameNumber];
 };
--- src/interface.js
+++ src/interface.js
@@ -1,154 +1,154 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-var game = new Game();
-
-
-$('.submitName').click(function(){
-  var namePlayer = $('.nameSubmitted').val();
-  $('#namePlayerForm').text(namePlayer + " is in the game!")
-  if (namePlayer.length > 0 ) $(".formPlayerInput").hide();
-});
+  var game = new Game();
 
 
- $('#go1').click(function(){
-   game.roll(parseInt($('#I1').val()));
-   var newValue = game._runningScore;
-   $('#input-I1').text(newValue);
- });
-
- $('#go2').click(function(){
-   game.roll(parseInt($('#I2').val()));
-   var newValue2 = game._runningScore;
-   $('#input-I2').text(newValue2);
- });
+  $(".submitName").click(function() {
+    var namePlayer = $(".nameSubmitted").val();
+    $("#namePlayerForm").text(namePlayer + " is in the game!");
+    if (namePlayer.length > 0) $(".formPlayerInput").hide();
+  });
 
 
-  $('#go3').click(function(){
-    game.roll(parseInt($('#II1').val()));
+  $("#go1").click(function() {
+    game.roll(parseInt($("#I1").val()));
+    var newValue = game._runningScore;
+    $("#input-I1").text(newValue);
+  });
+
+  $("#go2").click(function() {
+    game.roll(parseInt($("#I2").val()));
+    var newValue2 = game._runningScore;
+    $("#input-I2").text(newValue2);
+  });
+
+
+  $("#go3").click(function() {
+    game.roll(parseInt($("#II1").val()));
     var newValue3 = game._runningScore;
-    $('#input-II1').text(newValue3);
+    $("#input-II1").text(newValue3);
   });
 
-  $('#go4').click(function(){
-    game.roll(parseInt($('#II2').val()));
+  $("#go4").click(function() {
+    game.roll(parseInt($("#II2").val()));
     var newValue4 = game._runningScore;
-    $('#input-II2').text(newValue4);
+    $("#input-II2").text(newValue4);
   });
 
 
-  $('#go5').click(function(){
-    game.roll(parseInt($('#III1').val()));
+  $("#go5").click(function() {
+    game.roll(parseInt($("#III1").val()));
     var newValue5 = game._runningScore;
-    $('#input-III1').text(newValue5);
+    $("#input-III1").text(newValue5);
   });
 
-  $('#go6').click(function(){
-    game.roll(parseInt($('#III2').val()));
+  $("#go6").click(function() {
+    game.roll(parseInt($("#III2").val()));
     var newValue6 = game._runningScore;
-    $('#input-III2').text(newValue6);
+    $("#input-III2").text(newValue6);
   });
 
-  $('#go7').click(function(){
-    game.roll(parseInt($('#IV1').val()));
+  $("#go7").click(function() {
+    game.roll(parseInt($("#IV1").val()));
     var newValue7 = game._runningScore;
-    $('#input-IV1').text(newValue7);
+    $("#input-IV1").text(newValue7);
   });
 
-  $('#go8').click(function(){
-    game.roll(parseInt($('#IV2').val()));
+  $("#go8").click(function() {
+    game.roll(parseInt($("#IV2").val()));
     var newValue8 = game._runningScore;
-    $('#input-IV2').text(newValue8);
+    $("#input-IV2").text(newValue8);
   });
 
-  $('#go9').click(function(){
-    game.roll(parseInt($('#V1').val()));
+  $("#go9").click(function() {
+    game.roll(parseInt($("#V1").val()));
     var newValue9 = game._runningScore;
-    $('#input-V1').text(newValue9);
+    $("#input-V1").text(newValue9);
   });
 
-  $('#go10').click(function(){
-    game.roll(parseInt($('#V2').val()));
+  $("#go10").click(function() {
+    game.roll(parseInt($("#V2").val()));
     var newValue10 = game._runningScore;
-    $('#input-V2').text(newValue10);
+    $("#input-V2").text(newValue10);
   });
 
 
 
-  $('#go11').click(function(){
-    game.roll(parseInt($('#VI1').val()));
+  $("#go11").click(function() {
+    game.roll(parseInt($("#VI1").val()));
     var newValue11 = game._runningScore;
-    $('#input-VI1').text(newValue11);
+    $("#input-VI1").text(newValue11);
   });
 
-  $('#go12').click(function(){
-    game.roll(parseInt($('#VI2').val()));
+  $("#go12").click(function() {
+    game.roll(parseInt($("#VI2").val()));
     var newValue12 = game._runningScore;
-    $('#input-VI2').text(newValue12);
+    $("#input-VI2").text(newValue12);
   });
 
 
-   $('#go13').click(function(){
-     game.roll(parseInt($('#VII1').val()));
-     var newValue13 = game._runningScore;
-     $('#input-VII1').text(newValue13);
-   });
+  $("#go13").click(function() {
+    game.roll(parseInt($("#VII1").val()));
+    var newValue13 = game._runningScore;
+    $("#input-VII1").text(newValue13);
+  });
 
-   $('#go14').click(function(){
-     game.roll(parseInt($('#VII2').val()));
-     var newValue14 = game._runningScore;
-     $('#input-VII2').text(newValue14);
-   });
-
-
-   $('#go15').click(function(){
-     game.roll(parseInt($('#VIII1').val()));
-     var newValue15 = game._runningScore;
-     $('#input-VIII1').text(newValue15);
-   });
-
-   $('#go16').click(function(){
-     game.roll(parseInt($('#VIII2').val()));
-     var newValue16 = game._runningScore;
-     $('#input-VIII2').text(newValue16);
-   });
-
-   $('#go17').click(function(){
-     game.roll(parseInt($('#IX1').val()));
-     var newValue17 = game._runningScore;
-     $('#input-IX1').text(newValue17);
-   });
-
-   $('#go18').click(function(){
-     game.roll(parseInt($('#IX2').val()));
-     var newValue18 = game._runningScore;
-     $('#input-IX2').text(newValue18);
-   });
-
-   $('#go19').click(function(){
-     game.roll(parseInt($('#X1').val()));
-     var newValue19 = game._runningScore;
-     $('#input-X1').text(newValue19);
-   });
+  $("#go14").click(function() {
+    game.roll(parseInt($("#VII2").val()));
+    var newValue14 = game._runningScore;
+    $("#input-VII2").text(newValue14);
+  });
 
 
-   $('#go20').click(function(){
-     game.roll(parseInt($('#X2').val()));
-     var newValue20 = game._runningScore;
-     $('#input-X2').text(newValue20);
-   });
+  $("#go15").click(function() {
+    game.roll(parseInt($("#VIII1").val()));
+    var newValue15 = game._runningScore;
+    $("#input-VIII1").text(newValue15);
+  });
 
-   $('#go20').click(function(){
-     game.roll(parseInt($('#X2').val()));
-     var newValue20 = game._runningScore;
-     $('#input-X2').text(newValue20);
-   });
+  $("#go16").click(function() {
+    game.roll(parseInt($("#VIII2").val()));
+    var newValue16 = game._runningScore;
+    $("#input-VIII2").text(newValue16);
+  });
 
-   $('#go21').click(function(){
-     game.roll(parseInt($('#X3').val()));
-     var newValue20 = game._runningScore;
-     $('#input-X3').text(newValue20);
-     alert(newValue20 + " is your final Score!!")
-   });
+  $("#go17").click(function() {
+    game.roll(parseInt($("#IX1").val()));
+    var newValue17 = game._runningScore;
+    $("#input-IX1").text(newValue17);
+  });
+
+  $("#go18").click(function() {
+    game.roll(parseInt($("#IX2").val()));
+    var newValue18 = game._runningScore;
+    $("#input-IX2").text(newValue18);
+  });
+
+  $("#go19").click(function() {
+    game.roll(parseInt($("#X1").val()));
+    var newValue19 = game._runningScore;
+    $("#input-X1").text(newValue19);
+  });
+
+
+  $("#go20").click(function() {
+    game.roll(parseInt($("#X2").val()));
+    var newValue20 = game._runningScore;
+    $("#input-X2").text(newValue20);
+  });
+
+  $("#go20").click(function() {
+    game.roll(parseInt($("#X2").val()));
+    var newValue20 = game._runningScore;
+    $("#input-X2").text(newValue20);
+  });
+
+  $("#go21").click(function() {
+    game.roll(parseInt($("#X3").val()));
+    var newValue20 = game._runningScore;
+    $("#input-X3").text(newValue20);
+    alert(newValue20 + " is your final Score!!");
+  });
 
 
 });

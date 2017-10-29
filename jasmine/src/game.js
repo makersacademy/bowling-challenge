8c3e75ce0a937ff7132b8410afcc0f0320b63158
Canace Wong <canacewongtoday@hotmail.com>
2017-10-29 23:12:26 +0000

    added score spec

--- jasmine/src/game.js
+++ jasmine/src/game.js
@@ -8,12 +8,51 @@ BowlingGame.prototype.game = function() {
 };
 
 BowlingGame.prototype.addToFrame = function(pins) {
-    this._frame.push(pins);
-  if (this.IsAStrike(this._frame)) {
-    this.addToGame(this._frame);}
-  else if (this._frame.length === 2)
-  this.addToGame(this._frame);
+
+  if (this.isFinalFrame())
+     {this.addToFinalFrame(pins);}
+   else
+    {this._frame.push(pins);
+    if (this.IsAStrike(this._frame)) {
+      this.addToGame(this._frame);}
+    else if (this._frame.length === 2)
+      this.addToGame(this._frame);
+  }};
+
+BowlingGame.prototype.isFinalFrame = function() {
+  return this._game.length >= 9;
 };
+
+BowlingGame.prototype.addToFinalFrame = function(pins) {
+   this._frame.push(pins);
+
+   if ((this._frame[0] + this._frame[1]) < 10 )  {
+     this.addToGame(this._frame);
+   }
+
+   else if (this.IsDoubleStrike(this._frame)) {
+              if (this._frame.length === 3) {this.addToGame(this._frame);}
+  }
+
+   else if((this._frame[0] === 10) && (this._frame[1] < 10)) {
+        if((this._frame[1] + this._frame[2]) > 10) {
+         throw new Error ('There is only ' + (10 - this._frame[1]) + ' pins left');
+       }  else if (this._frame.length === 3) {this.addToGame(this._frame);} }
+
+
+
+   else if((this._frame[0] < 10) && ((this._frame[0] + this._frame[1]) > 10)) {
+        throw new Error ('There is only ' + (10 - this._frame[0]) + ' pins left');
+    }
+
+    else if (this._frame.length ===3) {this.addToGame(this._frame);}
+
+  };
+
+BowlingGame.prototype.IsDoubleStrike = function(frame) {
+  return ((this._frame[0] === 10) && (this._frame[1] === 10));
+};
+
 
 
 BowlingGame.prototype.frame = function() {
@@ -21,13 +60,17 @@ BowlingGame.prototype.frame = function() {
 };
 
 BowlingGame.prototype.addToGame = function(frame) {
+  if ((frame[0] + frame[1]) > 10) {
+      throw new Error ('There is only ' + (10 - frame[0]) + ' pins left');
+      }
   this._game.push(frame);
+  this._frame = [];
 };
 
 BowlingGame.prototype.IsAStrike = function(frame) {
-  return this._frame[0] === 10;
+  return frame[0] === 10;
 };
 
 BowlingGame.prototype.IsASpare = function(frame) {
-  return this._frame[0] + this._frame[1] === 10;
+  return frame[0] + frame[1] === 10;
 };

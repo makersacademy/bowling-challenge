3034c33d0db7ab01d77bfcd6a7b1e49753c12519
Pablo <pabloandresvdl@gmail.com>
2017-10-29 12:19:19 +0000

    add table and form in the index.html file

---
+++ src/interface.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+var game = new Game();
+
+$("#update-form-button").click(function(){
+      $("#name-player-form").text("Hello world!");
+  });
+
+
+});
+
+
+// //Get
+// var bla = $('#txt_name').val();
+//
+// //Set
+// $('#txt_name').val(bla);

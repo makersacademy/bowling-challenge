559a4b673c9a6e11da1e28074bfc8b06b3d37da9
Canace Wong <canacewongtoday@hotmail.com>
2017-10-29 10:47:21 +0000

    add frames to game

--- jasmine/spec/gameSpec.js
+++ jasmine/spec/gameSpec.js
@@ -15,5 +15,27 @@ describe('BowlingGame', function() {
     expect(bowlingGame.frame()).toEqual([8]);
   });
 
+  it('should decide if a roll is a strike', function(){
+    bowlingGame.addToFrame(10);
+    var frame = bowlingGame.frame();
+    expect(bowlingGame.IsAStrike(frame)).toBe(true);
+  });
+
+  it('should decide if two rolls is a spare', function(){
+    bowlingGame.addToFrame(8);
+    bowlingGame.addToFrame(2);
+    var frame = bowlingGame.frame();
+    expect(bowlingGame.IsASpare(frame)).toBe(true);
+  });
+
+
+  it('should add a frame with valid scores to game record', function(){
+    bowlingGame.addToFrame(2);
+    bowlingGame.addToFrame(8);
+    var frame = bowlingGame.frame();
+    expect(bowlingGame.game()).toEqual([[2, 8]]);
+  });
+
+
 
 });
--- jasmine/src/game.js
+++ jasmine/src/game.js
@@ -8,9 +8,26 @@ BowlingGame.prototype.game = function() {
 };
 
 BowlingGame.prototype.addToFrame = function(pins) {
-  this._frame.push(pins);
+    this._frame.push(pins);
+  if (this.IsAStrike(this._frame)) {
+    this.addToGame(this._frame);}
+  else if (this._frame.length === 2)
+  this.addToGame(this._frame);
 };
+
 
 BowlingGame.prototype.frame = function() {
   return this._frame;
+};
+
+BowlingGame.prototype.addToGame = function(frame) {
+  this._game.push(frame);
+};
+
+BowlingGame.prototype.IsAStrike = function(frame) {
+  return this._frame[0] === 10;
+};
+
+BowlingGame.prototype.IsASpare = function(frame) {
+  return this._frame[0] + this._frame[1] === 10;
 };

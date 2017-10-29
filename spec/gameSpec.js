18a214993aa62fd6ea8be1eb71348ae22cb1943c
David Halewood <davidnhalewood@hotmail.com>
2017-10-29 09:10:54 +0000

    can score a gutter game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -8,4 +8,26 @@ describe('Game', function () {
   it('starts with a score of zero', function() {
     expect(game.getScore()).toEqual(0)
   })
+
+  it('returns the score of the current game', function () {
+    game.bowl(1);
+    expect(game.getScore()).toEqual(1)
+  })
+
+  it('records a completed frame', function () {
+    game.bowl(2);
+    game.bowl(3);
+    expect(game._frames.length).toEqual(1)
+  })
+
+  it('can score a gutter game', function () {
+    gutterGame();
+    expect(game.getScore()).toEqual(0)
+  })
+
+  var gutterGame = function () {
+    for (var i = 0; i < 20; i++) {
+      game.bowl(0)
+    }
+  }
 });
--- src/game.js
+++ src/game.js
@@ -1,7 +1,29 @@
 function Game() {
   this._score = 0;
+  this._frames = []
 };
 
 Game.prototype.getScore = function () {
   return this._score;
+}
+
+Game.prototype.bowl = function (pins) {
+  if (!this._frame) {
+    this._firstBowl(pins)
+  } else if (this._frame) {
+    this._secondBowl(pins)
+  }
+}
+
+Game.prototype._firstBowl = function (pins) {
+  this._frame = new Frame
+  this._frame.firstRoll(pins)
+  this._score += pins
+}
+
+Game.prototype._secondBowl = function (pins) {
+  this._frame.secondRoll(pins)
+  this._frame = null
+  this._score += pins
+  this._frames.push(this._frame)
 }

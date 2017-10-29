b397cc9e48ad2d4cd7a112c704e8e2d34fb85132
David Halewood <davidnhalewood@hotmail.com>
2017-10-29 12:40:21 +0000

    refactoring frame object to include default scores

--- src/frame.js
+++ src/frame.js
@@ -2,20 +2,23 @@
 
 var Frame = function () {
   this._pinsRemaining = 10
-  this._pinsKnockedDown = []
+  this._pinsKnockedDown = [0, 0]
   this.roll = new Roll()
+  this._frameFinished = false
 }
 
 Frame.prototype.firstRoll = function (pins) {
-  this._pinsKnockedDown.push(this.roll.takeFirstTurn(pins))
+  this._pinsKnockedDown[0] = (this.roll.takeFirstTurn(pins))
   this._pinsRemaining -= pins
-  if (this.isStrike()) {
-    this._pinsKnockedDown.push(0)
+  if (this.isAStrike()) {
+    this._pinsKnockedDown[1] = 0
+    this._frameFinished = true
   }
 }
 
 Frame.prototype.secondRoll = function (pins) {
-  this._pinsKnockedDown.push(this.roll.takeSecondTurn(pins, this._pinsRemaining))
+  this._pinsKnockedDown[1] = (this.roll.takeSecondTurn(pins, this._pinsRemaining))
+  this._frameFinished = true
 }
 
 Frame.prototype.returnScore = function () {
@@ -25,9 +28,9 @@ Frame.prototype.returnScore = function () {
 }
 
 Frame.prototype.isFinished = function () {
-  return (this._pinsKnockedDown.length === 2)
+  return this._frameFinished
 }
 
-Frame.prototype.isStrike = function () {
-  return this._pinsRemaining === 0
+Frame.prototype.isAStrike = function () {
+  return this._pinsKnockedDown[0] === 10
 }

9b8ed70d5231bf437f78873140357a827d2f6d95
David Halewood <davidnhalewood@hotmail.com>
2017-10-29 07:41:23 +0000

    adding turn functionality to roll object

--- spec/rollSpec.js
+++ spec/rollSpec.js
@@ -1,8 +1,32 @@
 describe('Roll', function () {
-  
-  var roll = new Roll;
 
-  it('returns the number of pins knocked down each turn', function() {
-    expect(roll.takeTurn(4)).toEqual(4)
-  })
+  var roll; 
+  
+  beforeEach(function () {
+    roll = new Roll();
+  });
+
+  describe('the first turn', function () {
+    it('returns the number of pins knocked down for the first turn turn', function () {
+      expect(roll.takeFirstTurn(4)).toEqual(4)
+    });
+
+    it('doesn\'t allow more pins than exist to be knocked down', function () {
+      expect(function () {
+        roll.takeFirstTurn(11);
+      }).toThrow(new Error('Sorry please select from 0 to 10 pins!'))
+    });
+  });
+
+  describe('the second turn', function () {
+    it('returns the number of pins knocked down for the second turn', function () {
+      expect(roll.takeSecondTurn(4, 4)).toEqual(4)
+    });
+
+    it('doesn\'t allow more pins than exist to be knocked down', function () {
+      expect(function () {
+        roll.takeSecondTurn(8, 3);
+      }).toThrow(new Error('Sorry please select from 3 to 10 pins!'))
+    });
+  });
 })
--- src/roll.js
+++ src/roll.js
@@ -1,9 +1,19 @@
 'use strict';
 
-function Roll () {
+function Roll() {
 
 }
 
-Roll.prototype.takeTurn = function (pins) {
+Roll.prototype.takeFirstTurn = function (pins) {
+  if (pins > 10) {
+    throw new Error('Sorry please select from 0 to 10 pins!')
+  }
+  return pins
+}
+
+Roll.prototype.takeSecondTurn = function (pins, previousRoll) {
+  if (pins + previousRoll > 10) {
+    throw new Error('Sorry please select from ' + previousRoll + ' to 10 pins!')
+  }
   return pins
 }

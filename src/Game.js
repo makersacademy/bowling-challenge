c88ccd47688116d12706148ad99cefed0179284a
Tom Spencer <tomspencer_uk@yahoo.co.uk>
2017-10-29 14:39:44 +0000

    adds logic for roll gutter game

---
+++ src/Game.js
@@ -0,0 +1,45 @@
+function Game() {
+  this._rolls = []
+}
+
+Game.prototype.rolls = function() {
+  return this._rolls;
+};
+
+Game.prototype.roll = function(roll) {
+  this._rolls.push(roll);
+};
+
+Game.prototype.score = function(){
+  var result = 0;
+  var rollIndex = 0;
+  for(var frameIndex = 0; frameIndex < 10; frameIndex++) {
+    result = this._rolls[rollIndex] + this._rolls[rollIndex + 1];
+    rollIndex += 2;
+  }
+  return result;
+};
+/* function Player() {
+}
+Player.prototype.play = function(song) {
+  this.currentlyPlayingSong = song;
+  this.isPlaying = true;
+};
+
+Player.prototype.pause = function() {
+  this.isPlaying = false;
+};
+
+Player.prototype.resume = function() {
+  if (this.isPlaying) {
+    throw new Error("song is already playing");
+  }
+
+  this.isPlaying = true;
+};
+
+Player.prototype.makeFavorite = function() {
+  this.currentlyPlayingSong.persistFavoriteStatus(true);
+};
+
+*/

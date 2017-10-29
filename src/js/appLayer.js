f5e1004ba7be11a47a4be8fffb832b76be5540e7
Ainsley <ab.chang@me.com>
2017-10-29 17:58:11 +0000

    more styling completed

--- src/js/appLayer.js
+++ src/js/appLayer.js
@@ -30,7 +30,7 @@ function updateSelection() {
   $('#pin-selection').empty();
   if (isGameEnded()) return;
   for (var i = 1; i <= game.pinsRemaining(); i++) {
-    $('#pin-selection').append('<button class="pins" value="' + i + '">' + i + '</button>');
+    $('#pin-selection').append('<button class="pins" value="' + i + '">' + i + '</button>');  
   };
 };
 

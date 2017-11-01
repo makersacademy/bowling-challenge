ff6f81a7f980c21d6c1a7f48bef24468f537ca85
Alexis Carlier <alexis.carlier0@gmail.com>
2017-11-01 12:43:38 +0000

    Deleted comments in frame class

--- src/frame.js
+++ src/frame.js
@@ -2,7 +2,6 @@ function frame() {
   this._firstScore = null;
   this._secondScore = null;
   this._thirdScore = null;
-  // this._fourthScore = null;
 }
 
 BONUS_SCORE = 10;
@@ -22,12 +21,6 @@ frame.prototype.thirdRollScore = function(thirdScore) {
   return this._thirdScore;
 };
 
-// frame.prototype.fourthRollScore = function(fourthScore) {
-//   this._fourthScore = fourthScore;
-//   return this._fourthScore;
-// };
-
-
 frame.prototype.isAStrike = function() {
   return (this._firstScore === BONUS_SCORE);
 };
@@ -40,23 +33,3 @@ frame.prototype.isASpare = function() {
 frame.prototype.preBonusScore = function() {
   return this._firstScore + this._secondScore + this._thirdScore;
 };
-
-// frame.prototype.totalScore = function() {
-//   if(this.isRollAStrike()) {
-//     return this.strikeDefaultScore();
-//   }
-//   else if(this.isRollASpare()) {
-//     return this.spareDefaultScore();
-//   }
-//   return this._firstScore + this._secondScore;
-// };
-//
-// frame.prototype.strikeDefaultScore = function() {
-//   return (this._firstScore + this._secondScore + this._nextFrameFirstScore + this._nextFrameSecondScore);
-// };
-//
-// frame.prototype.spareDefaultScore = function() {
-//   return (this._firstScore + this._secondScore + this._nextFrameFirstScore);
-// };
-
-// predicate methods; would make them private in Ruby. Anything analogous here?

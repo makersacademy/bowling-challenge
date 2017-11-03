a3149cc4a2f4558f9b97813e2ea8ab232d63d6ae
Tim Jones <timjones10@hotmail.com>
2017-11-03 12:06:55 +0000

    FrameTotal function and frameCounter property added

--- spec/bowlingScorecardSpec.js
+++ spec/bowlingScorecardSpec.js
@@ -12,5 +12,18 @@ describe("Bowling Game", function() {
   it('Adds the results of two rolls to a new_frame array', function(){
   game.newframe(1,4);
   expect(game.frames()).toContain([1,4]);
+  console.log(game.frames());
   });
+
+  it ('Calculates the total for a frame where the score is less than 10', function()
+  {
+  game.newframe(1,4);
+  expect(game.frameTotal()).toEqual(5);
+  });
+
+  it ('Has a framecounter which counts the number of frames', function(){
+  game.newframe(1,4);
+  expect(game.frameCounter()).toEqual(1);
+  });
+
 });
--- src/bowlingScorecard.js
+++ src/bowlingScorecard.js
@@ -1,12 +1,24 @@
 
 function Game() {
   this._frames = [];
+  this._frameCounter = 0;
 }
 
 Game.prototype.frames = function() {
   return this._frames;
 };
 
+Game.prototype.frameCounter = function() {
+  return this._frameCounter;
+};
+
 Game.prototype.newframe = function(roll1, roll2) {
+this._frameCounter += 1;
 this._frames.push([roll1,roll2]);
+};
+
+Game.prototype.frameTotal = function() {
+  var frame = this._frames[0];
+  var total = frame[0] + frame[1];
+  return total;
 };

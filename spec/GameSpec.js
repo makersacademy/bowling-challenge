b21001292ee6233cda4346961eef3a8c425ff0e3
Aram Simonian <aram.contacts@gmail.com>
2017-11-04 21:19:07 +0000

    Reorganised game tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,32 +7,34 @@ describe("Game", function() {
 
   });
 
-  it("should have frames collection of ten items", function() {
-    expect(game.frames.length).toEqual(10);
-  });
-
-  it("should have a first frame that has no previous frame", function() {
-    expect(game.frames[0].previous).toEqual(null);
-  });
-
-  it("should have a first frame that has second frame as next", function() {
-    expect(game.frames[0].next).toEqual(game.frames[1]);
-  });
-
-  it("should have a second frame that has first frame as previous", function() {
-    expect(game.frames[1].previous).toEqual(game.frames[0]);
-  });
-
-  it("should have a second frame that has third frame as next", function() {
-    expect(game.frames[1].next).toEqual(game.frames[2]);
-  });
-
-  it("should have a last frame that has no next frame", function() {
-    expect(game.frames[game.frames.length-1].next).toEqual(null);
-  });
-
   it("should have currentFrame set to its first frame on initialisation", function() {
     expect(game.currentFrame).toEqual(game.frames[0]);
+  })
+
+  describe("frames collection", function() {
+    it("should comprise ten items", function() {
+      expect(game.frames.length).toEqual(10);
+    });
+
+    it("should have a first frame that has no previous frame", function() {
+      expect(game.frames[0].previous).toEqual(null);
+    });
+
+    it("should have a first frame that has second frame as next", function() {
+      expect(game.frames[0].next).toEqual(game.frames[1]);
+    });
+
+    it("should have a second frame that has first frame as previous", function() {
+      expect(game.frames[1].previous).toEqual(game.frames[0]);
+    });
+
+    it("should have a second frame that has third frame as next", function() {
+      expect(game.frames[1].next).toEqual(game.frames[2]);
+    });
+
+    it("should have a last frame that has no next frame", function() {
+      expect(game.frames[game.frames.length-1].next).toEqual(null);
+    });
   })
 
 });

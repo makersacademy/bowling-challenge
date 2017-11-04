629cacf2c6967c65e7c272f1966c6009aa607ba6
Jon Sanders <jonsanders101@aol.com>
2017-11-04 15:00:49 +0000

    Refactored scorecard'

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,14 +1,7 @@
 var Scorecard = function () {
-    this.frames = {
-    1: new Frame(),
-    2: new Frame(),
-    3: new Frame(),
-    4: new Frame(),
-    5: new Frame(),
-    6: new Frame(),
-    7: new Frame(),
-    8: new Frame(),
-    9: new Frame(),
-    10: new FinalFrame()
-  }
+    this.frames = {}
+      for (var i = 1; i < 10; i++) {
+        this.frames[i] = new Frame()
+      }
+    this.frames[10] = new FinalFrame()
 }

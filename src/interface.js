121cc28019c3ba6cfdb2530dda469e0aa028b64a
Jon Sanders <jonsanders101@aol.com>
2017-11-04 14:47:53 +0000

    Update jQuery interface'

--- src/interface.js
+++ src/interface.js
@@ -8,9 +8,9 @@ $( document ).ready(function() {
     var frame = game.currentFrame
     var roll = game.currentRoll
     game.play()
-    $('.f'+frame+'r'+roll).html(game.scorecard[frame][roll]['hitPins'])
+    $('.f'+frame+'r'+roll).html(game.scorecard.frames[frame].frame[roll]['hitPins'])
     for(var i = 1; i <= frame; i++) {
-      $('.f'+i+'s').html(game.scorecard[i]['frameScore'])
+      $('.f'+i+'s').html(game.scorecard.frames[i].frame['frameScore'])
     }
     $('.current-score').html(game.currentScore)
     $('.final-score').html(game.finalScore)
@@ -21,9 +21,9 @@ $( document ).ready(function() {
     var frame = game.currentFrame
     var roll = game.currentRoll
     game.play(hitPins)
-    $('.f'+frame+'r'+roll).html(game.scorecard[frame][roll]['hitPins'])
+    $('.f'+frame+'r'+roll).html(game.scorecard.frames[frame].frame[roll]['hitPins'])
     for(var i = 1; i <= frame; i++) {
-      $('.f'+i+'s').html(game.scorecard[i]['frameScore'])
+      $('.f'+i+'s').html(game.scorecard.frames[i].frame['frameScore'])
     }
     $('.current-score').html(game.currentScore)
     $('.final-score').html(game.finalScore)

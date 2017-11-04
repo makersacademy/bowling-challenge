be0672c2b309616d04b290f1249e55b59ae74c1c
Tim Jones <timjones10@hotmail.com>
2017-11-04 21:48:53 +0000

    Added a FrameNo and a isFirst Frame function to game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,6 +10,15 @@ beforeEach(function(){
   expect(game.frames.length).toEqual(0);
 });
 
-  it('is initialised with a frame object set to frame', function() {
+ it('is initialised with a frame object set to frame', function() {
     expect(game.frame()).toEqual(jasmine.objectContaining({}));
   });
+
+  it ('has a FrameNo function to return the current FrameNo', function() {
+    expect(game.FrameNo()).toEqual(1);
+  });
+
+
+ it ('has a function to check if frame is the first frame', function() {
+   expect (game.isFirstFrame()).toBe(true);
+ });
--- src/game.js
+++ src/game.js
@@ -13,3 +13,13 @@ Game.prototype.frames = function() {
 Game.prototype.frame = function() {
   return this._frame;
 };
+
+//functions to aid with scoring
+
+Game.prototype.FrameNo = function() {
+  return this.frames.length + 1;
+};
+
+Game.prototype.isFirstFrame = function() {
+  if (this.frames.length == 0) return true;
+};

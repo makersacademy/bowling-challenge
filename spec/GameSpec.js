7f7d66f562db50fa7c1f4b84f84b01baf78f179e
Aram Simonian <aram.contacts@gmail.com>
2017-11-04 21:00:40 +0000

    Added tests to check for frames collection being linked up Game now populates with frames and links items up

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,8 +7,28 @@ describe("Game", function() {
 
   });
 
-  it("should have ten frames", function() {
+  it("should have frames collection of ten items", function() {
     expect(game.frames.length).toEqual(10);
+  });
+
+  it("should have a first frame that has no previous frame", function() {
+    expect(game.frames[0].previous).toEqual(null);
+  });
+
+  it("should have a first frame that has second frame as next", function() {
+    expect(game.frames[0].next).toEqual(game.frames[1]);
+  });
+
+  it("should have a second frame that has first frame as previous", function() {
+    expect(game.frames[1].previous).toEqual(game.frames[0]);
+  });
+
+  it("should have a second frame that has third frame as next", function() {
+    expect(game.frames[1].next).toEqual(game.frames[2]);
+  });
+
+  it("should have a last frame that has no next frame", function() {
+    expect(game.frames[game.frames.length-1].next).toEqual(null);
   });
 
 });
--- src/game.js
+++ src/game.js
@@ -1,13 +1,31 @@
 'use strict';
 
 function Game() {
-  this.frames = Array(10);
+  this.frames = [];
+  this.init();
+}
 
+
+Game.prototype.init = function() {
+  this.setupFrames()
+}
+
+Game.prototype.setupFrames = function() {
+  var i;
+  for(i=0; i < 10; i++) {
+    var currentFrame = new Frame;
+    this.frames.push(currentFrame);
   }
+  this.setupFrameHierarchy()
+}
 
-// Game.prototype.getFrames() = function() {
-//   for(i=0; i < 10; i++) {
-//     var currentFrame = new Frame;
-//     this.frames.push(currentFrame);
-//   }
-// }
+Game.prototype.setupFrameHierarchy = function() {
+  var i;
+  for(i=0; i < this.frames.length - 1; i++) {
+    if(i > 0)
+      this.frames[i].previous = this.frames[i-1]
+
+    if(i < this.frames.length - 1)
+      this.frames[i].next = this.frames[i+1]
+  }
+}

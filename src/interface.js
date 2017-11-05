c2b43f4408113abe45ae2608d0837c2c67766e83
Ed Lowther <ed.lowther[at]gmail.com>
2017-11-05 12:14:52 +0000

    Refactored interface.js

--- src/interface.js
+++ src/interface.js
@@ -1,51 +1,78 @@
 $( document ).ready(function() {
-  $('form').submit(false);
+
   var game = new Game();
-  var rollValueTextInput = $('#rollValue');
-  var enterRollValueButton = $('#enterRollValue');
-  var totalScoreDisplay = $('#totalScore');
-  rollValueTextInput.focus();
-  enterRollValueButton.on('click', function() {
-    var rollValue = parseInt(rollValueTextInput.val());
+
+  var rollValueInput = $('#rollValue');
+  var submitButton = $('#enterRollValue');
+
+  $('form').submit(false);
+  rollValueInput.focus();
+
+  submitButton.on('click', function() {
+    updateRollScores();
+    printRunningTotals(game._frames);
+    resetInputField();
+  })
+
+  var updateRollScores = function() {
+    var rollValue = parseInt(rollValueInput.val());
     game.addRoll(rollValue);
+
     var frame = game._lastFrame();
     var frameIndex = game.numberOfFrames() - 1;
+
     if (frameIndex < 9) {
-      if (frame.isOngoing()) {
-        $('.rollOne:eq(' + frameIndex + ') p').text(rollValue)
-      }
-      else {
-        if (frame.isStrike()) {
-          $('.rollTwo:eq(' + frameIndex + ') p').text('X');
-        } else if (frame.isSpare()) {
-          $('.rollTwo:eq(' + frameIndex + ') p').text('/');
-        } else {
-          $('.rollTwo:eq(' + frameIndex + ') p').text(rollValue);
-        }
-      }
+      printRegularFrameScores(frame, frameIndex, rollValue);
     }
     else {
-      if (frame.secondRoll === null) {
-        $('.rollOneFrameTen p').text(String(rollValue).replace(10, 'X'));
-      }
-      else if (frame.thirdRoll === null) {
-        if (frame.isSpare()) {
-          $('.rollTwoFrameTen p').text('/');
-        }
-        else {
-          $('.rollTwoFrameTen p').text(String(rollValue).replace(10, 'X'));
-        }
-      }
-      else if (frame.firstRoll + frame.secondRoll >= 10) {
-        $('.rollThreeFrameTen p').text(String(rollValue).replace(10, 'X'));
+      printFrameTenScores(frame, frameIndex, rollValue);
+    }
+  }
+
+  var printRegularFrameScores = function(frame, frameIndex, rollValue) {
+    if (frame.isOngoing()) {
+      $('.rollOne:eq(' + frameIndex + ') p').text(subStrikesAndGutterballs(rollValue))
+    }
+    else {
+      if (frame.isSpare()) {
+        $('.rollTwo:eq(' + frameIndex + ') p').text('/');
+      } else {
+        $('.rollTwo:eq(' + frameIndex + ') p').text(subStrikesAndGutterballs(rollValue));
       }
     }
-    game._frames.forEach(function(frame, frameIndex) {
+  }
+
+  var printFrameTenScores = function(frame, frameIndex, rollValue) {
+    if (frame.secondRoll === null) {
+      $('.rollOneFrameTen p').text(subStrikesAndGutterballs(rollValue));
+    }
+    else if (frame.thirdRoll === null) {
+      if (frame.isSpare()) {
+        $('.rollTwoFrameTen p').text('/');
+      }
+      else {
+        $('.rollTwoFrameTen p').text(subStrikesAndGutterballs(rollValue));
+      }
+    }
+    else if (frame.firstRoll + frame.secondRoll >= 10) {
+      $('.rollThreeFrameTen p').text(subStrikesAndGutterballs(rollValue));
+    }
+  }
+
+  var printRunningTotals = function(frames) {
+    frames.forEach(function(frame, frameIndex) {
       if (frame.bonusRollsRequired() === 0 && !frame.isOngoing()) {
         $('.runningTotal:eq(' + frameIndex + ') p').text(frame.runningTotal())
       }
     });
-    rollValueTextInput.val('');
-    rollValueTextInput.focus();
-  })
+  }
+
+  var subStrikesAndGutterballs = function(rollValue) {
+    return String(rollValue).replace('10', 'X').replace('0', '-');
+  }
+
+  var resetInputField = function() {
+    rollValueInput.val('');
+    rollValueInput.focus();
+  }
 });

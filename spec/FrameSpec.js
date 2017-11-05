abb8464d27d94ca7626dcad3499aa0f223458374
Aram Simonian <aram.contacts@gmail.com>
2017-11-05 18:27:54 +0000

    Added and passed a test for strike score calculation. Added getStrikeBonus and getSpareBonus and modified score method

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -22,12 +22,22 @@ describe("Frame", function() {
     expect(frame.score()).toEqual(8);
   })
 
-  it("should, if a spare, have a bonus of rollOne from the next frame", function() {
+  it("should, if a spare, have a bonus of the next roll", function() {
     frame.rollOne = 3;
     frame.rollTwo = 7;
     nextFrame = new Frame();
     frame.next = nextFrame;
     nextFrame.rollOne = 4
     expect(frame.score()).toEqual(14);
+  })
+
+  it("should, if a strike, have a bonus of the next two rolls", function() {
+    frame.rollOne = 10;
+    frame.rollTwo = null;
+    nextFrame = new Frame();
+    frame.next = nextFrame;
+    nextFrame.rollOne = 4
+    nextFrame.rollTwo = 4
+    expect(frame.score()).toEqual(18);
   })
 })
--- src/frame.js
+++ src/frame.js
@@ -5,7 +5,6 @@ function Frame() {
   this.previous = null;
   this.rollOne = null;
   this.rollTwo = null;
-
 }
 
 Frame.prototype.isStrike = function() {
@@ -16,17 +15,28 @@ Frame.prototype.isSpare = function() {
   return (this.rollOne + this.rollTwo == 10 && !!this.rollTwo);
 }
 
+Frame.prototype.getStrikeBonus = function() {
+  if(this.isStrike()){
+    return this.rollOne + this.next.rollOne;
+  }
+  else {
+    return this.rollOne + this.rollTwo;
+  }
+}
+
+Frame.prototype.getSpareBonus = function() {
+  return this.rollOne;
+}
+
 Frame.prototype.score = function() {
   var bonus;
   if(this.isStrike()) {
-
-  }
-  else if (this.isSpare()) {
-    bonus = this.next.rollOne;
-  }
-  else {
+    bonus = this.next.getStrikeBonus();
+  } else if (this.isSpare()) {
+    bonus = this.next.getSpareBonus();
+  }  else {
     bonus = 0;
   }
-  return (this.rollOne + this.rollTwo + bonus);
 
+  return (this.rollOne + this.rollTwo + bonus);
 }

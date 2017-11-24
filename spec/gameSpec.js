3cb152ce3f2d4527ca5085f2eabb5b193775e704
Eustaquio122 <tiagofsousa@gmail.com>
2017-11-24 16:58:50 +0000

    Removes old files

--- spec/gameSpec.js
+++
@@ -1,9 +0,0 @@
-    describe('totalScore', function() {
-       it('returns accumulatedScore from last element in score.frames', function() {
-            expect(score.total()).toBe(5);
-        }); 
-    });
-
-Score.prototype.total = function() {
-    return last(this.frames).accumulatedScore;
-}

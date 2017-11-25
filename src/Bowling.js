c3050c13b4fd94ab8dd03ca55ddb20c769f42311
Allan Wazacz <cazwazacz@gmail.com>
2017-11-25 20:35:58 +0000

    DRY's up and extracts logic into private methods

--- src/Bowling.js
+++ src/Bowling.js
@@ -3,35 +3,37 @@ function Bowling() {
   this.frames = [];
 };
 
-Bowling.prototype.bowl = function (roll) {
-  var previousFrame = this.frames.slice(-1)[0];
-  var frameTotal = roll.reduce(function(a, b) { return a + b; }, 0);
+var previousFrame;
+var previousPreviousFrame;
+var frameTotal;
+var previousTotal;
+
+Bowling.prototype.bowl = function(roll) {
+  previousFrame = this.frames.slice(-1)[0];
+  frameTotal = roll.reduce(function(a, b) {return a + b;}, 0);
   if (this.frames.length === 9) {
-    var previousTotal = previousFrame.reduce(function(a, b) { return a + b; }, 0);
-    var frameSliceTotal = roll.slice(0,2).reduce(function(a, b) { return a + b; }, 0);
-    if (previousTotal == 10) {
-      if (previousFrame[0] == 10) {
-        this.total += frameSliceTotal;
-      } else {
-        this.total += roll[0];
-      }
-    }
+    frameSliceTotal = roll.slice(0,2).reduce(function(a, b) { return a + b; }, 0);
+    this._calculate(roll, frameSliceTotal);
   } else if (previousFrame) {
-    var previousTotal = previousFrame.reduce(function(a, b) { return a + b; }, 0);
-    if (previousTotal == 10) {
-      if (previousFrame[0] == 10) {
-        this.total += frameTotal;
-      } else {
-        this.total += roll[0];
-      }
-    }
+    this._calculate(roll, frameTotal);
   }
   if (this.frames.length >= 2) {
-    var previousPreviousFrame = this.frames.slice(-2)[0];
-    if (previousPreviousFrame[0] === 10 && previousFrame[1] == 0) {
-      this.total += roll[0];
-    }
+    this._checkTwoFramesBack(roll);
   }
   this.frames.push(roll);
   this.total += frameTotal;
 };
+
+Bowling.prototype._checkTwoFramesBack = function(roll) {
+  previousPreviousFrame = this.frames.slice(-2)[0];
+  if (previousPreviousFrame[0] === 10 && previousFrame[1] == 0) {
+    this.total += roll[0];
+  }
+}
+
+Bowling.prototype._calculate = function(roll, frameTotal) {
+  previousTotal = previousFrame.reduce(function(a, b) { return a + b; }, 0);
+  if (previousTotal == 10) {
+    previousFrame[0] == 10 ? this.total += frameTotal : this.total += roll[0];
+  }
+}

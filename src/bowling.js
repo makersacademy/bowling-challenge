f22a11fda8f522eb7cda2b8f701dc0f518606583
Tabish Raza <tabishraza@gmail.com>
2017-11-25 16:30:54 +0000

    logic for playing upto 12 frames

--- src/bowling.js
+++ src/bowling.js
@@ -1,8 +1,12 @@
+const MAXFRAME = 12;
+
 function Bowling(){
   this._total = 0;
   this._maxScore = 300;
   this._counter = 0;
-  // this._currentScore = 0;
+  this._currentScore = 0;
+  this._frame = 0;
+  this._bonus = 0;
 }
 
 Bowling.prototype.total = function(){
@@ -20,27 +24,40 @@ Bowling.prototype.addScore = function(score){
   return this._total += score;
 }
 
-Bowling.prototype.frame = function(){
+Bowling.prototype.frame = function(score){
   if (this._counter < 2){
-//     // addScore(score)
-//     score += this._currentScore
+    this._currentScore += score;
     this._counter += 1;
   } else {
+    this._frame += 1;
     this._counter = 0;
   }
-//   this._total += this._currentScore
-//   this._currentScore = 0
-// }
-//
-// if this._counter==0 && score == 10{
-//   this._total += score
-//   this.counter = 0;
-// } else if {this._counter==1{
-//     if(this._currentScore + score == 10){
-//       SPARE
-//       counter = 0
-//     }
 }
 
 
-// }
+Bowling.prototype.game = function(score){
+  if(this._frame <= MAXFRAME) {
+    if(this._counter === 0 && score === 10){
+      // strike
+      this._total += score;
+      this._counter = 0;
+      this._frame += 1;
+    } else if(this._counter === 1 && (this._currentscore + score === 10) ){
+      //spare
+      this._total += this._currentScore;
+      this._counter += 1;
+      this._frame += 1;
+    } else if(this._counter == 0){
+      //normal first roll
+      this._currentScore += score;
+      this._counter += 1;
+    } else if(this._counter === 1){
+      //normal second roll
+      this._currentScore += score;
+      this._total += this._currentScore;
+      this._counter = 0;
+      this._frame += 1;
+    }
+  }
+  return 'Cannot play more than 12 frames'
+}

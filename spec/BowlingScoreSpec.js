ac9704ab1dfbb80b4b641250d96348564b7c6484
majdeddine <majda-jambouri@hotmail.fr>
2017-11-25 22:14:59 +0000

    print current score

--- spec/BowlingScoreSpec.js
+++ spec/BowlingScoreSpec.js
@@ -7,14 +7,23 @@ describe("BowlingScore", function(){
   })
 
   it("should accept a roll", function(){
-    bowlingScore.roll(2)
-    expect(bowlingScore.pinsLeft()).toEqual(8)
+    bowlingScore.roll(2);
+    expect(bowlingScore.pinsLeft()).toEqual(8);
   })
 
   describe("frames", function(){
     it("should print the current frame",function(){
-      bowlingScore.roll(3)
+      bowlingScore.roll(3);
       expect(bowlingScore.currentFrame()).toEqual(1);
     })
+  })
+
+  describe("score", function(){
+    it("should print the current score",function(){
+      bowlingScore.roll(7);
+      bowlingScore.roll(2);
+      expect(bowlingScore.score()).toEqual(9);
+    })
+
   })
 })
--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -1,13 +1,15 @@
 
 function BowlingScore(){
   this._pinsLeft = 10;
-  this._currentFrame = 0
+  this._currentFrame = 0;
+  this._score = 0;
 
 }
 
 BowlingScore.prototype.roll = function(pins){
   this._pinsLeft = 10 - pins;
   this._currentFrame +=1;
+  this._score += pins;
 }
 
 BowlingScore.prototype.pinsLeft = function(){
@@ -16,4 +18,8 @@ BowlingScore.prototype.pinsLeft = function(){
 
 BowlingScore.prototype.currentFrame = function(){
   return this._currentFrame;
+}
+
+BowlingScore.prototype.score= function(){
+  return this._score
 }

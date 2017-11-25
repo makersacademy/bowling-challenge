260ab8886793a2acfa0ddc7d991e7eb60560b825
majdeddine <majda-jambouri@hotmail.fr>
2017-11-25 21:59:47 +0000

    print current frame

--- spec/BowlingScoreSpec.js
+++ spec/BowlingScoreSpec.js
@@ -1,9 +1,20 @@
 describe("BowlingScore", function(){
 
+  var bowlingScore;
+
+  beforeEach(function(){
+    bowlingScore = new BowlingScore();
+  })
+
   it("should accept a roll", function(){
-    var bowlingScore = new BowlingScore();
     bowlingScore.roll(2)
     expect(bowlingScore.pinsLeft()).toEqual(8)
   })
 
+  describe("frames", function(){
+    it("should print the current frame",function(){
+      bowlingScore.roll(3)
+      expect(bowlingScore.currentFrame()).toEqual(1);
+    })
+  })
 })
--- src/BowlingScore.js
+++ src/BowlingScore.js
@@ -1,13 +1,19 @@
 
 function BowlingScore(){
   this._pinsLeft = 10;
+  this._currentFrame = 0
 
 }
 
 BowlingScore.prototype.roll = function(pins){
-  this._pinsLeft = 10 - pins
+  this._pinsLeft = 10 - pins;
+  this._currentFrame +=1;
 }
 
 BowlingScore.prototype.pinsLeft = function(){
-  return this._pinsLeft
+  return this._pinsLeft;
+}
+
+BowlingScore.prototype.currentFrame = function(){
+  return this._currentFrame;
 }

f76990ca58441a9f32932630920de9f4f4adb7ca
Joe Cowton <joecowton@gmail.com>
2017-11-26 09:57:54 +0000

    added public folder

--- public/interface.js
+++ public/interface.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
+
   var game = new Game();
 
   $("#newGame").on('click', function() {
     $("#newGame").html(game.newGame());
-    // $("#temperature").html(thermostat.temperature);
-    // $("#energyUsage").html(thermostat.energyUsage());
   })
 
-};
+});

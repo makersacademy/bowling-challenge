3b4a81542278996afc40c75e878c7a258cb4e7e7
Peter Johnstone <peter.wdj@gmail.com>
2017-11-26 20:34:32 +0000

    Display user scores sequentially in html table

--- jQuery.js
+++ jQuery.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
 
+  var frame = new Frame();
+  var bowlCounter = 1;
+
+  $("#submit-score").click(function() {
+    var score = $("#bowl-score").val();
+    frame.setBowlOneScore(score);
+    $('td:nth-of-type('+(bowlCounter)+')').html(frame.getBowlOneScore());
+    i++;
+  });
 });

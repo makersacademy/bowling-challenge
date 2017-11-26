dde0a496e5c4d048b25dc25a3ffb7c0d1608ab36
antonio <antoniobelmardacosta@gmail.com>
2017-11-26 23:06:01 +0000

    Add method to add frames to scoresheet and two new related properties

--- public/src/ScoreSheet.js
+++ public/src/ScoreSheet.js
@@ -1,7 +1,34 @@
 function ScoreSheet() {
   this._count = 0
+  this._MAX_FRAMES = 10
+  this._frames = []
 };
 
 ScoreSheet.prototype.getCount = function() {
   return this._count;
+};
+
+ScoreSheet.prototype.incrementCount = function() {
+  this._count ++;
+};
+
+ScoreSheet.prototype.getMaxFrames = function() {
+  return this._MAX_FRAMES;
+};
+
+ScoreSheet.prototype.getFrames = function() {
+  return this._frames;
+};
+
+ScoreSheet.prototype.updateFrames = function(frame) {
+  this._frames.push(frame)
+};
+
+
+ScoreSheet.prototype.addFrame = function(frame, constructor = 0) {
+  if (this.getCount() >= this.getMaxFrames()) {
+    throw("This scoresheet already has " + this._MAX_FRAMES + " frames.")
+  }
+  this.updateFrames(frame)
+  this.incrementCount();
 };
--- spec/ScoreSheetSpec.js
+++ spec/ScoreSheetSpec.js
@@ -4,6 +4,7 @@ describe("ScoreSheet", function() {
 
   beforeEach(function() {
     scoresheet = new ScoreSheet();
+    frame = jasmine.createSpy("frame")
   });
 
   describe("#new", function() {
@@ -13,6 +14,28 @@ describe("ScoreSheet", function() {
     it("includes a property count",function() {
       expect(scoresheet.getCount()).toEqual(0);
     });
+    it("includes a property max frames",function() {
+      expect(scoresheet.getMaxFrames()).toEqual(10);
+    });
+    it("includes a property frames which is an empty array",function() {
+      expect(scoresheet.getFrames()).toEqual([]);
+    });
   });
 
+  describe ("#addFrame", function() {
+    it("increments count property", function() {
+      scoresheet.addFrame(frame);
+      expect(scoresheet.getCount()).toEqual(1);
+    });
+    it("throws error if count > 10", function() {
+      for(i=0; i<11; i++) {
+        scoresheet.incrementCount();
+      };
+      expect(function() {scoresheet.addFrame()}).toThrow("This scoresheet already has 10 frames.");
+    });
+    it("adds frame passed as argument to frames array", function() {
+      scoresheet.addFrame(frame);
+      expect(scoresheet.getFrames()).toContain(frame)
+    });
+  });
 });

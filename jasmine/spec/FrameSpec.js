8703d355e2acf6f39a75d9bad9cc12e3b2ec28c6
Tom Moir <trmoir9@gmail.com>
2017-11-26 13:37:46 +0000

    added logic to frame so for Strikes and Spares

--- jasmine/spec/FrameSpec.js
+++ jasmine/spec/FrameSpec.js
@@ -1,24 +1,51 @@
 describe('Frame', function() {
   beforeEach(function() {
-    frame = new Frame;
-    roll = new Roll;
+    frame       = new Frame;
+    firstRoll   = new Roll;
+    secondRoll  = new Roll;
   })
 
-  describe('#addRollToFrame', function() {
+  describe('#addToFrame', function() {
     it('adds a Roll to the Frame', function() {
-      frame.addRollToFrame(roll);
-      expect(frame.rollTally).toContain(roll);
+      firstRoll.pinfall = 4;
+      frame.addToFrame(firstRoll);
+      expect(frame.rollTally[0]).toEqual(4);
     })
   })
 
   describe('#totalPoints', function() {
     it('keeps track of the total points', function() {
-      roll.pinfall = 4;
-      frame.addRollToFrame(roll);
-      roll2 = new Roll;
-      roll2.pinfall = 2;
-      frame.addRollToFrame(roll2);
-      expect(frame.totalPoints).toEqual(6);
+      firstRoll.pinfall = 4;
+      frame.addToFrame(firstRoll);
+      secondRoll.pinfall = 2;
+      frame.addToFrame(secondRoll);
+      expect(frame.totalPoints()).toEqual(6);
+    })
+  })
+
+  describe('#isStrike', function() {
+    it('starts as false', function() {
+      expect(frame.isStrike()).toBe(false);
+    })
+
+    it('turns true when the first firstRoll has a pinfall of 10', function() {
+      firstRoll.pinfall = 10;
+      frame.addToFrame(firstRoll);
+      expect(frame.isStrike()).toBe(true);
+    })
+  })
+
+  describe('#isSpare', function() {
+    it('starts as false', function() {
+      expect(frame.isSpare()).toBe(false);
+    })
+
+    it('turns true when the after the second firstRolle and if the total is 10', function() {
+      firstRoll.pinfall = 7;
+      frame.addToFrame(firstRoll);
+      secondRoll.pinfall = 3;
+      frame.addToFrame(secondRoll)
+      expect(frame.isSpare()).toBe(true);
     })
   })
 })
--- jasmine/src/Frame.js
+++ jasmine/src/Frame.js
@@ -1,11 +1,25 @@
 function Frame() {
   this.rollTally = [];
-  this.totalPoints = 0;
 }
 
 Frame.prototype = {
-  addRollToFrame: function(roll) {
-    this.rollTally.push(roll);
-    this.totalPoints += roll.pinfall;
+  addToFrame: function(roll) {
+    this.rollTally.push(roll.pinfall);
+  },
+
+  totalPoints: function() {
+    var total = 0;
+    this.rollTally.forEach(function(pinfall) {
+      total += pinfall;
+    });
+    return total;
+  },
+
+  isStrike: function() {
+    return this.rollTally[0] === 10;
+  },
+
+  isSpare: function() {
+    return (this.isStrike() === false) && (this.totalPoints() === 10);
   }
 }

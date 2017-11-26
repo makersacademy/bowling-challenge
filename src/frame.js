12f94a2b8e5963dde4efcfa2d2c713ab98d3b525
Peter Johnstone <peter.wdj@gmail.com>
2017-11-26 15:28:37 +0000

    Minor changes

--- src/frame.js
+++ src/frame.js
@@ -12,20 +12,20 @@ Frame.prototype.getBowlOneScore = function() {
   return this.bowlOne;
 };
 
-Frame.prototype.setBowlTwoScore = function (score) {
+Frame.prototype.setBowlTwoScore = function(score) {
   this.bowlTwo = score;
 };
 
-Frame.prototype.getBowlTwoScore = function () {
+Frame.prototype.getBowlTwoScore = function() {
   return this.bowlTwo;
 };
 
-Frame.prototype.setScore = function () {
+Frame.prototype.setScore = function() {
   this.score = []
   this.score.push(this.getBowlOneScore());
   this.score.push(this.getBowlTwoScore());
 };
 
-Frame.prototype.getScore = function () {
+Frame.prototype.getScore = function() {
   return this.score;
 };

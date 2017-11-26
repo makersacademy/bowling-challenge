6e72e6ab7ef853ec9b12f27f84d491eabcd95a6b
antonio <antoniobelmardacosta@gmail.com>
2017-11-26 19:02:10 +0000

    Isolate FrameSpec tests

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,15 +1,16 @@
 describe("Frame", function() {
 
   beforeEach(function() {
-    frame = new Frame([3,5]);
+    rolls = jasmine.createSpy("rolls")
+    frame = new Frame(rolls);
   });
 
   describe("#new", function() {
-    it("creates a new scoresheet object", function() {
+    it("creates a new frame object", function() {
       expect(frame).toEqual(jasmine.any(Frame));
     });
     it("includes a property rolls",function() {
-      expect(frame.getRolls()).toEqual([3,5]);
+      expect(frame.getRolls()).toEqual(rolls);
     })
   });
 

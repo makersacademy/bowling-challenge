6d2cbe79f09223b234e4a3df9907749bb30ec698
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-26 09:55:44 +0000

    Added bonus

--- src/Game.js
+++ src/Game.js
@@ -13,7 +13,6 @@ Game.prototype.updateRolls = function (rollsList) {
     throw new Error('cannot have more than 21 rolls in a game')
   }
   this._rolls = rollsList;
-
 };
 
 Game.prototype.score = function () {
@@ -23,16 +22,16 @@ Game.prototype.score = function () {
   for (frameNr = 1; frameNr <= 10; frameNr ++) {
     var frame;
     frame = new Frame(frameNr,[],0)
-    if(game.isStrike(game._rolls[rollsIndex])) {
+    if(game.isStrike(rollsIndex)) {
       frame._rolls.push(game._rolls[rollsIndex]);
-      frame._intScore =frame._intScore + game._rolls[rollsIndex];
+      frame._intScore =frame._intScore + 10 + game.bonus(rollsIndex);
       rollsIndex = rollsIndex +1;
     }
 
     else if(game.isSpare(rollsIndex)) {
       frame._rolls.push(game._rolls[rollsIndex]);
       frame._rolls.push(game._rolls[(rollsIndex+1)]);
-      frame._intScore = frame._intScore + game._rolls[rollsIndex]+game._rolls[rollsIndex+1];
+      frame._intScore = frame._intScore + 10 + game.bonus(rollsIndex);
       rollsIndex = rollsIndex +2;
     }
 
@@ -47,14 +46,22 @@ Game.prototype.score = function () {
     console.log(frame);
    };
 return sum;
-
 };
 
-Game.prototype.isStrike = function (roll) {
-  return roll === 10;
-
+Game.prototype.isStrike = function(rollsIndex) {
+  return game._rolls[rollsIndex] === 10;
 };
 
 Game.prototype.isSpare = function(rollsIndex) {
   return (game._rolls[rollsIndex] + game._rolls[rollsIndex+1]) === 10;
-}
+};
+
+Game.prototype.bonus = function(rollsIndex) {
+  if(game.isStrike(rollsIndex)) {
+    return game._rolls[(rollsIndex+1)] + game._rolls[(rollsIndex+2)];
+  }
+
+  if(game.isSpare(rollsIndex)) {
+    return game._rolls[(rollsIndex+2)]
+  }
+};

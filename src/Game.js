b4358d91c3fb4cdde171f8444769bcff6e51379f
gabrielabud <gabriela.budeanu@gmail.com>
2017-11-26 02:06:38 +0000

    is Strike & is Spare implementation

--- src/Game.js
+++ src/Game.js
@@ -20,16 +20,29 @@ Game.prototype.score = function () {
   var sum = 0;
   var frameNr = 1;
   var rollsIndex = 0;
-  var allRolls = [];
   for (frameNr = 1; frameNr <= 10; frameNr ++) {
     var frame;
     frame = new Frame(frameNr,[],0)
-    allRolls = game._rolls;
-    if(game.isStrike(allRolls[rollsIndex])) {
-      frame._rolls.push(allRolls[rollsIndex]);
-      frame._intScore =frame._intScore + allRolls[rollsIndex];
+    if(game.isStrike(game._rolls[rollsIndex])) {
+      frame._rolls.push(game._rolls[rollsIndex]);
+      frame._intScore =frame._intScore + game._rolls[rollsIndex];
       rollsIndex = rollsIndex +1;
     }
+
+    else if(game.isSpare(rollsIndex)) {
+      frame._rolls.push(game._rolls[rollsIndex]);
+      frame._rolls.push(game._rolls[(rollsIndex+1)]);
+      frame._intScore = frame._intScore + game._rolls[rollsIndex]+game._rolls[rollsIndex+1];
+      rollsIndex = rollsIndex +2;
+    }
+
+    else {
+      frame._rolls.push(game._rolls[rollsIndex]);
+      frame._rolls.push(game._rolls[(rollsIndex+1)]);
+      frame._intScore = frame._intScore + game._rolls[rollsIndex]+game._rolls[rollsIndex+1];
+      rollsIndex = rollsIndex +2;
+    }
+
     sum = sum + frame._intScore;
     console.log(frame);
    };
@@ -41,3 +54,7 @@ Game.prototype.isStrike = function (roll) {
   return roll === 10;
 
 };
+
+Game.prototype.isSpare = function(rollsIndex) {
+  return (game._rolls[rollsIndex] + game._rolls[rollsIndex+1]) === 10;
+}

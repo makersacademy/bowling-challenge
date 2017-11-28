99608c72a21c6074c6b1ac8e3a18615f88d552ca
antonio <antoniobelmardacosta@gmail.com>
2017-11-28 22:12:39 +0000

    Add method to check for a strike in a frame

--- public/src/Frame.js
+++ public/src/Frame.js
@@ -1,9 +1,14 @@
 function Frame(roll1, roll2) {
   this._rolls = [roll1, roll2]
+  this._MAX_PINS = 10
 };
 
 Frame.prototype.getRolls = function() {
   return this._rolls;
+};
+
+Frame.prototype.getMaxPins = function() {
+  return this._MAX_PINS;
 };
 
 Frame.prototype.scoreFrame = function() {
@@ -12,6 +17,10 @@ Frame.prototype.scoreFrame = function() {
     baseScore += this.getRollKnockedPins(i);
   }
   return baseScore;
+};
+
+Frame.prototype.checkStrike = function() {
+  return this.getRollKnockedPins(0) == this.getMaxPins();
 };
 
 Frame.prototype.getRollKnockedPins = function(index) {
--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -19,6 +19,9 @@ describe("Frame", function() {
     it("includes a property rolls",function() {
       expect(frame.getRolls()).toEqual([roll1, roll2]);
     });
+    it("includes a property max pins",function() {
+      expect(frame.getMaxPins()).toEqual(10);
+    });
   });
 
   describe("#scoreFrame", function() {
@@ -35,6 +38,27 @@ describe("Frame", function() {
       };
       frame2 = new Frame(roll4, roll5);
       expect(frame2.scoreFrame()).toEqual(5);
+    });
+  });
+
+  describe("#checkStrike", function(){
+    it("expects it to be true if first roll knocks 10 pins", function() {
+      var roll6 = {
+        getKnockedPins: function() {
+          return 10
+        },
+      };
+      frame3 = new Frame(roll6, roll1)
+      expect(frame3.checkStrike()).toEqual(true)
+    });
+    it("expects it to be true if first roll knocks 10 pins", function() {
+      var roll6 = {
+        getKnockedPins: function() {
+          return 8
+        },
+      };
+      frame3 = new Frame(roll6, roll1)
+      expect(frame3.checkStrike()).toEqual(false)
     });
   });
 

f18d742ab888801e253b13743dfbaa48a8ae8dec
thatdania@gmail.com <thatdania@gmail.com>
2017-11-28 23:14:06 +0000

    Started from the other end

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -12,32 +12,12 @@ describe('Bowling', function(){
     });
   });
 
-  describe('Scoring a strike',function(){
-    it('in round 1', function(){
-      game.roll(10);
-      expect(game.rounds[0]).toEqual({frame: 1, round: 2, score: 10});
+  describe('Adds score to the frame', function(){
+    it('rolls a five in the first round, changes the round', function(){
+      game.roll(5);
+      expect(game.currentGame()).toEqual({frame: 1, round: 2, score: 5})
     });
   });
 
-  describe('Scoring a strike changes to new frame',function(){
-    it('in round 1', function(){
-      game.roll(10);
-      expect(game.rounds[1]).toEqual({frame: 2, round: 1, score: 0});
-    });
-  });
 
-  // describe('Round updates when score is calculated',function(){
-  //   it('round 1 to round 2', function(){
-  //     game.roll(5);
-  //     expect(game.rounds[0]).toEqual({frame: 1, round: 2, score: 5});
-  //   });
-  // });
-  //
-  // describe('After two rounds, a new frame should start',function(){
-  //   it('after 2 rounds', function(){
-  //     game.roll(5);
-  //     game.roll(4);
-  //     expect(game.rounds[0]).toEqual({frame: 2, round: 1, score: 0});
-  //   });
-  // });
 });

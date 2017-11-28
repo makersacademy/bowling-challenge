9c53d26434bbc81ea31442a37949130b34abc2a1
thatdania@gmail.com <thatdania@gmail.com>
2017-11-28 21:37:45 +0000

    Added strike tests and value

--- src/Game.js
+++ src/Game.js
@@ -11,13 +11,40 @@ Game.prototype.addFrame = function(){
   this.frame += 1
   this.currentround = 1
   this.rounds.push({frame: this.frame, round: this.currentround, score: this.currentpoints})
-}
+};
 
 Game.prototype.roll = function(points){
   this.currentpoints += points;
-  this.currentGame().score = points
-}
+  this.currentGame().score += points
+  this.addRound()
+  this.checkFrame()
+  console.log(this.rounds)
+};
 
 Game.prototype.currentGame = function(){
   return this.rounds[this.frame - 1];
-}
+};
+
+Game.prototype.addRound = function(){
+  if(this.currentGame().round < 2){
+  this.currentGame().round += 1}
+  else {
+    return false
+  }
+};
+
+Game.prototype.checkFrame = function(){
+if (this.currentGame().round >= 2){
+  this.newFrame()
+  this.rounds.push({frame: this.frame, round: this.currentround, score: this.currentpoints})
+  }
+else {
+  this.currentGame().frame + 1
+  }
+};
+
+Game.prototype.newFrame = function(){
+  this.frame += 1
+  this.currentround = 1
+  this.currentpoints = 0
+};

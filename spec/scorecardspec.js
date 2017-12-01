3679da29686dac0294a970d13de83a4fa468fb2b
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-12-01 17:55:25 +0000

    needs bonus scoring tweaking

--- spec/scorecardspec.js
+++ spec/scorecardspec.js
@@ -1,3 +1,5 @@
+
+
 describe("ScoreCard", function(){
 
   beforeEach(function(){
@@ -14,6 +16,49 @@ describe("ScoreCard", function(){
   describe("bonus check", function(){
     it("checks what bonus is needed for frame", function(){
       expect(scorecard.frameInPlay.bonusCheck()).toEqual('no-bonus')
+    });
+  });
+
+  describe("finish frame", function(){
+    it("adds the frame in play to the frames array", function(){
+      scorecard.finishFrame()
+      expect(scorecard.frames.length).toEqual(1);
+    });
+  });
+
+  describe("adding bonus points SPARE", function(){
+    it("it adds roll 1 points to previous frame if spare bonus applies", function(){
+      scorecard.frameInPlay.roll1.addPointsRoll(5)
+      scorecard.frameInPlay.roll2.addPointsRoll(5)
+      scorecard.frameInPlay.addPointsFrame()
+      scorecard.finishFrame()
+      scorecard.newFrame()
+      //debugger
+      oldFrame = scorecard.frames.pop()
+      console.log(oldFrame)
+      scorecard.frameInPlay.roll1.addPointsRoll(4)
+      scorecard.bonusScore()
+      console.log(bonusFrame)
+      expect(oldFrame.points).toEqual(14);
+    });
+  });
+
+  describe("enter bonus mode", function(){
+    it("enters spare, strike or no bonus mode for new frame based on previous frame", function(){
+      scorecard.frameInPlay.roll1.addPointsRoll(5)
+      scorecard.frameInPlay.roll2.addPointsRoll(5)
+      scorecard.frameInPlay.addPointsFrame()
+      scorecard.frameInPlay.rollCheck()
+      scorecard.frameInPlay.bonusCheck()
+      scorecard.setBonusMode()
+      expect(scorecard.bonusMode).toEqual('spare')
+    });
+  });
+
+  describe("roll score return", function(){
+    it("returns the score of the roll", function(){
+      scorecard.frameInPlay.roll1.addPointsRoll(5)
+      expect(scorecard.roll1Score()).toEqual(5)
     })
   })
 });
@@ -64,6 +109,21 @@ describe("Frame", function(){
       expect(frame.roll1Check()).toEqual(false)
     });
   });
+
+  describe("roll check", function(){
+    it("adds the correct number of rolls to counter in frame", function(){
+      frame.roll1.addPointsRoll(5)
+      frame.rollCheck()
+      expect(frame.rollCount).toEqual(1)
+    });
+  });
+
+  describe("roll check 1", function(){
+    it("returns the value of closed for roll 1", function(){
+      frame.roll1.addPointsRoll(5)
+      expect(frame.roll1Check()).toEqual(true)
+    })
+  })
 });
 
 
@@ -84,6 +144,6 @@ describe("Roll", function(){
     it("closes roll", function(){
       roll.closeRoll()
       expect(roll.closed).toEqual(true)
-    })
-  })
+    });
+  });
 });

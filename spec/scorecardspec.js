5aa986e9351b8065244d18822f0b7e7def2c6b05
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-12-01 14:16:37 +0000

    friday start

--- spec/scorecardspec.js
+++ spec/scorecardspec.js
@@ -5,11 +5,17 @@ describe("ScoreCard", function(){
   });
 
   describe("#addpoints", function(){
-    it("adds the number of points passed as argument", function(){
-      scorecard.addPointsScore(8)
-      expect(scorecard.points).toEqual(8);
+    it("adds the number of points", function(){
+      scorecard.addPointsScore()
+      expect(scorecard.points).toEqual(0);
     });
   });
+
+  describe("bonus check", function(){
+    it("checks what bonus is needed for frame", function(){
+      expect(scorecard.frameInPlay.bonusCheck()).toEqual('no-bonus')
+    })
+  })
 });
 
 describe("Frame", function(){
@@ -18,53 +24,48 @@ describe("Frame", function(){
     frame = new Frame()
   });
 
-  describe("add points", function(){
-    it("adds the number of points passed as an argument", function(){
-      frame1 = new Frame()
-      frame1.addPointsFrame(5)
-      expect(frame1.points).toEqual(5)
-      expect(frame1.rollCount).toEqual(1)
-    });
-  });
-
-  describe("strike", function(){
-    xit("sets the frame counter to 2", function(){
-      frame.strikeHit()
-      expect(frame.rollCount).toEqual(2)
-    });
-  });
 
   describe("strike check", function(){
-    it("returns true with 10 points", function(){
-      frame2 = new Frame()
-      frame2.addPointsFrame(10)
-      expect(frame2.strikeCheck()).toEqual(true)
-    });
-  });
-
-  describe("roll strike", function(){
-    it("returns strike with 10 points", function(){
-      frame3 = new Frame()
-      frame3.addPointsFrame(10)
-      expect(frame3.rollCount).toEqual(2)
-    });
-  });
-
-  describe("end frame", function(){
-    it("returns true at the end of two rolls", function(){
-      frame.addPointsFrame(10)
+    xit("returns true with 10 points", function(){
+      frame.roll1.points = 10
+      frame.addPointsFrame()
       expect(frame.endFrameCheck()).toEqual(true)
     });
   });
 
-  describe("end frame", function(){
-    it("returns true at the end of two rolls", function(){
-      frame.addPointsFrame(10)
-      expect(frame.addPointsFrame).toEqual("initiate new roll")
+  describe(" FIRST roll strike", function(){
+    xit("returns strike with 10 points", function(){
+      frame.roll1.points = 10
+      frame.addPointsFrame()
+      expect(frame.rollCount).toEqual(2)
     });
   });
 
+  describe("end frame", function(){
+    it(" SECOND returns true at the end of two rolls", function(){
+      frame.roll1.closeRoll()
+      frame.roll2.closeRoll()
+      frame.rollCheck()
+      expect(frame.endFrameCheck()).toEqual(true)
+    });
+  });
+
+  describe("frame points", function(){
+    it("returns the points sum of roll 1 and roll2", function(){
+      frame.roll1.points = 5
+      frame.roll2.points = 6
+      frame.addPointsFrame()
+      expect(frame.points).toEqual(11)
+    });
+  });
+
+  describe("roll check", function(){
+    it("returns the closed value of rolls", function(){
+      expect(frame.roll1Check()).toEqual(false)
+    });
+  });
 });
+
 
 describe("Roll", function(){
 
@@ -78,4 +79,11 @@ describe("Roll", function(){
       expect(roll.points).toEqual(5)
     });
   });
+
+  describe("roll close", function(){
+    it("closes roll", function(){
+      roll.closeRoll()
+      expect(roll.closed).toEqual(true)
+    })
+  })
 });

4257a25751ea58fdd50b2551f3e416e19bc24032
Jamie LeMercier <jamielemercier@gmail.com>
2017-12-01 20:29:24 +0000

    adds game object with 10 frames. Each frame has 2 rolls

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,6 +1,16 @@
-describe('Bowling Game', function() {
-    it('has 10 frames', function() {
-        var game = new Game();
-        expect(game.frames.length).toEqual(10);
+describe('Bowling Game', function () {
+    var game;
+    beforeEach(function () {
+        game = new Game();
+    });
+    describe('Game', function () {
+        it('has 10 frames', function () {
+            expect(game.frames.length).toEqual(10);
+        });
+    });
+    describe('Frame', function () {
+        it('has 2 rolls', function () {
+            expect(game.frames[0]).toEqual([0, 0]);
+        });
     });
 });
--- src/game.js
+++ src/game.js
@@ -1,3 +1,8 @@
 function Game() {
-    this.frames = [1,2,3,4,5,6,7,8,9,10]
+    this.frames = [];
+
+    for (i = 0; i < 10; i++) {
+        this.frames.push([0, 0]);
+        console.log(this.frames);
+    }
 }

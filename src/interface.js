d066a9ccc657d48478f924138f072f3aebdbd100
Eustaquio122 <tiagofsousa@gmail.com>
2017-12-02 12:06:26 +0000

    Removes outdated error messages

--- src/interface.js
+++ src/interface.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
 
     $("#submit_score").on("click", function() {
         event.preventDefault();
-        clearError();
         game = new Game();
         submission = []
         parseSubmission();
@@ -78,10 +77,6 @@ $(document).ready(function(){
     function showResults() {
         $("#output").attr("class", "result");
         $("#output_messages").attr("class", "results_message");
-        if (!game.frames) {
-            errorMessage();
-            return;
-        }
         for (let i = 1; i < 11; i ++) {
             $("#f" + i).text(game.frames[i - 1].accumulatedScore);
         }
@@ -92,58 +87,4 @@ $(document).ready(function(){
         return value > -1 || value < 11;
     }
 
-    function errorMessage () {
-        $('#error_message').text("Please refrain from inputing wrong inputs");
-    }
-
-    function clearError() {
-        $('#error_message').text("");
-    }
-
 });
-
-//     $('#down').on('click', function() {
-//         thermostat.down();
-//         updateInfo();
-//     });
-
-//     $('#reset').on('click', function() {
-//         thermostat.reset();
-//         updateInfo();
-//     });
-
-//     $('#power_saving').on('click', function() {
-//         thermostat.pressPowerSaving();
-//         updateInfo();
-//         $('#power_saving').attr('class', ('power_' + thermostat.powerSaving()));
-//     });
-
-//     $('#select-location').submit(function(event) {
-//         event.preventDefault();
-//         var location = $('#current-location').val();
-//         getWeather(location);
-//     });
-
-// //helpers
-//     function updateTemperature() {
-//         $('#temperature').text(thermostat.temperature());
-//     };
-
-//     function updateEnergyUsage() {
-//         $('#energy_usage').text(thermostat.energyUsage());
-//     };
-
-//     function updateInfo() {
-//         updateTemperature();
-//         updateEnergyUsage();
-//     };
-
-//     function getWeather(location) {
-//         var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
-//         var api_key = '&appid=27016742dff44ef54a22846755b2386d';
-//         var conversion = '&units=metric';
-//         $.get(url + location + api_key + conversion, function(data) {
-//             $('#outside-temperature').text(data.main.temp);
-//         });
-//     };
-// });

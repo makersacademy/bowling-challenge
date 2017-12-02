9c551d31e28bbb450fd9ba51c4f14d01b7b99a31
Lewis <lewisyoul@gmail.com>
2017-12-02 12:22:05 +0000

    restart challege with different approach

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,176 +1,64 @@
-describe("Game", function(){
-
-  function Roll4Then4(){
-    game.addRoll(4)
-    game.addRoll(4)
-  }
-
-  function RollSpare(){
-    game.addRoll(5)
-    game.addRoll(5)
-  }
-
-  function RollStrike(){
-    game.addRoll(10)
-  }
+describe('Game', function(){
 
   beforeEach(function(){
-    bonus = jasmine.createSpyObj('new Bonus()', ['setSpareTrue', 'setSpareFalse', 'setStrike', 'isSpare', 'isStrike']);
-    game = new Game(new Bonus());
+    game = new Game();
   });
 
-  describe("#getCurrentScore", function(){
-    it("returns a current score of 0", function(){
-      expect(game.getCurrentScore()).toEqual(0)
-    });
-
-    it("has a score of 40 after ten frames", function(){
-      for (var i = 0; i < 10; i++){
-        game.addRoll(4)
-      }
-      expect(game.getCurrentScore()).toEqual(40)
-    });
-  });
-
-  describe("#frames", function(){
-    it("is an object", function(){
-      expect(game.frames).toEqual(jasmine.any(Object))
-    });
-
-    it("returns an instance of Frame as the first element", function(){
-      expect(game.frames[1]).toEqual(jasmine.any(Frame))
-    })
-  });
-
-  describe("#getCurrentFrame", function(){
-    it("returns a current frame of 1", function(){
-      expect(game.getCurrentFrame()).toEqual(1)
-    });
-
-    it("doesn't increase the frame after only one roll (below 10)", function(){
-      game.addRoll(4)
-      expect(game.getCurrentFrame()).toEqual(1)
-    });
-
-    it("moves on to the next frame if the first roll is 10", function(){
-      game.addRoll(10)
-      expect(game.getCurrentFrame()).toEqual(2)
-    })
-
-    it("rolling twice (below 5) increases the current frame by 1", function(){
-      Roll4Then4();
-      expect(game.getCurrentFrame()).toEqual(2)
-    });
-
-    it("has a maximum of ten frames", function(){
+  describe('#getCurrentScore', function(){
+    it('returns a current score of zero', function(){
       for (var i = 0; i < 20; i++){
-        game.addRoll(4)
+        game.addPins(1)
       }
-      expect(game.getCurrentFrame()).toEqual(10)
+      expect(game.getCurrentScore()).toEqual(20)
     });
   });
 
-  describe("#isRollOne", function(){
-    it("is set to be true by default", function(){
-      expect(game.isRollOne()).toEqual(true)
-    });
-
-    it("is set to false after taking one roll", function(){
-      game.addRoll(4)
-      expect(game.isRollOne()).toEqual(false)
-    });
-
-    it("is set to true after making two rolls (below 5)", function(){
-      Roll4Then4();
-      expect(game.isRollOne()).toEqual(true)
-    })
-
-    it("is set to false after making three rolls (below 5)", function(){
-      Roll4Then4()
-      game.addRoll(4)
-      expect(game.isRollOne()).toEqual(false)
-    })
-  });
-
-  describe("#addRoll", function(){
-    it("adds a roll of 4 to the current score", function(){
-      game.addRoll(4)
-      expect(game.getCurrentScore()).toEqual(4)
-    });
-
-    it("throws an error if the number of pins hit is > 10", function(){
-      expect(function() { game.addRoll(11) }).toThrow("You can't knock down more than 10 pins")
-    })
-
-    it("throws an error if score for roll 2 would take total for frame over 10", function(){
-      game.addRoll(4)
-      expect(function(){ game.addRoll(7) }).toThrow("Your rolls can't sum over 10")
-    });
-  });
-
-  describe("#addFrame", function(){
-    it("adds one frame to the frame total", function(){
-      game.addFrame()
+  describe('#moveToNextFrame', function(){
+    it('moves the frame on by one', function(){
+      game.moveToNextFrame()
       expect(game.getCurrentFrame()).toEqual(2)
     });
 
-    it("doesn't add one frame when the number of frames is 10", function(){
-      for (var i = 0; i <15; i++){
-        game.addFrame()
-      }
-      expect(game.getCurrentFrame()).toEqual(10)
+    it('moves to the next frame after two normal rolls', function(){
+      game.addPins(1)
+      game.addPins(1)
+      expect(game.getCurrentFrame()).toEqual(2)
     });
   });
 
-  describe("#isSpareBonus", function(){
-    it("is set to false by default", function(){
-      game.bonus.isSpare = jasmine.createSpy('spare == false').and.returnValue(false)
-      expect(game.isSpareBonus()).toEqual(false)
-    });
-
-    it("is set to true if all pins are knocked down on the second roll", function(){
-      game.bonus.isSpare = jasmine.createSpy('spare == true').and.returnValue(true)
-      RollSpare()
-      expect(game.isSpareBonus()).toEqual(true)
-    });
-
-    it("resets to false after completing bonus roll", function(){
-      game.bonus.isSpare = jasmine.createSpy('spare == true').and.returnValue(true)
-      RollSpare()
-      game.bonus.isSpare = jasmine.createSpy('spare == false').and.returnValue(false)
-      game.addRoll(4)
-      expect(game.isSpareBonus()).toEqual(false)
-    });
-
-    it("resets to false after completing a bonus roll that is a strike", function(){
-      game.bonus.isSpare = jasmine.createSpy('spare == true').and.returnValue(true)
-      RollSpare()
-      game.bonus.isSpare = jasmine.createSpy('spare == false').and.returnValue(false)
-      RollStrike()
-      expect(game.isSpareBonus()).toEqual(false)
+  describe('#addPins', function(){
+    it('adds the number of pins knocked down to the current frame', function(){
+      game.addPins(1)
+      expect(game.scores[0]).toEqual([1])
     });
   });
 
-  describe("#addSpareToBonus", function(){
-    it("adds the value of the first roll after a spare to the previous frame's bonus slot", function(){
-      RollSpare()
-      game.addRoll(3)
-      expect(game.getCurrentScore()).toEqual(16)
+  describe('#getCurrentTurn', function(){
+    it('returns 1 if it is the first turn in a frame', function(){
+      expect(game.getCurrentTurn()).toEqual(1)
+    });
+
+    it('returns 2 if it is the second turn in a frame', function(){
+      game.addPins(1)
+      expect(game.getCurrentTurn()).toEqual(2)
+    });
+
+    it('returns 1 after two normal rolls', function(){
+      game.addPins(1)
+      game.addPins(1)
+      expect(game.getCurrentTurn()).toEqual(1)
     });
   });
 
-  describe("#isStrikeBonus", function(){
-    it("is true if 10 is rolled on first roll", function(){
-      RollStrike()
-      expect(game.isStrikeBonus()).toEqual(true)
+  describe('#addSpare', function(){
+    it('adds the value of the next roll following a spare to the previous frame', function(){
+      game.addPins(1)
+      game.addPins(9)
+      game.addPins(3)
+      expect(game.scores[0][2]).toEqual(3)
+      console.log(game.scores)
     });
   });
-  //DO ONCE SPARE BONUS IMPLEMENTED
-  // describe("#addStrikeBonus", function(){
-  //   it("adds the roll to previous Frame's bonus if strikeBonus is true", function(){
-  //     game.addRoll(10)
-  //     game.addRoll(4)
-  //
-  //   });
-  // });
+
+
 });
--- src/Game.js
+++ src/Game.js
@@ -1,125 +1,69 @@
-// NEXT - Implement the addition of spare bonus to previous frame's bonus
-
-function Game(bonus){
-  this.rollOne = true
-  this.bonus = bonus
+function Game(){
   this.currentFrame = 1
-  this.frames = {
-    1: new Frame(),
-    2: new Frame(),
-    3: new Frame(),
-    4: new Frame(),
-    5: new Frame(),
-    6: new Frame(),
-    7: new Frame(),
-    8: new Frame(),
-    9: new Frame(),
-    10: new Frame(),
-  }
+  this.scores = [[],[],[],[],[],[],[],[],[],[]]
+  this.currentTurn = 1
 }
 
 // GETTERS
 
-Game.prototype.getCurrentScore = function () {
-  var score = 0
-  for (var key in this.frames) {
-    if (this.frames.hasOwnProperty(key)) {
-      score += this.frames[key].sumAllRolls()
-    }
-  }
-  return score;
+
+Game.prototype.getCurrentScore = function(){
+  return this._sumGame()
 }
 
-Game.prototype.getCurrentFrame = function () {
+Game.prototype.getCurrentFrame = function(){
   return this.currentFrame
 }
 
-// ADD SCORE
+Game.prototype.getCurrentTurn = function(){
+  return this.currentTurn
+}
 
-Game.prototype.addRoll = function (roll) {
-  if (roll > 10) {
-    throw("You can't knock down more than 10 pins")
-  }
-    if (this.currentFrame <= 10) {
-      if (this.isRollOne()) {
-        this._saveRollOne(roll)
-      } else {
-        this._saveRollTwo(roll)
-      }
+// ADD PINS
+
+Game.prototype.addPins = function(pins){
+  if (this.currentFrame > 1) {
+    if (this.scores[this.currentFrame - 2][0] + this.scores[this.currentFrame - 2][1] === 10 && this.currentTurn === 1) {
+      this.scores[this.currentFrame - 2].push(pins)
     }
-    // This shouldnt be here as is setting strike bonus regardless of whether 10 was rolled on first or second go
-    // if (roll == 10) {
-    //   this.setStrikeBonus()
-    // }
-};
-
-
-// BOOLEAN
-
-Game.prototype.isRollOne = function(){
-  return this.rollOne
+  }
+  this.scores[this.currentFrame - 1].push(pins)
+  this._setCurrentTurn()
 }
 
-Game.prototype.isStrikeBonus = function(){
-  return this.bonus.isStrike()
+// MOVE TO NEXT FRAME
+
+Game.prototype.moveToNextFrame = function(){
+  this.currentFrame += 1
 }
 
-Game.prototype.isSpareBonus = function () {
-  return this.bonus.isSpare()
-};
+Game.prototype._setCurrentTurn = function(){
+  if (this.getCurrentTurn() === 1) {
+    this.currentTurn = 2
+  } else {
+    this.currentTurn = 1
+    this.moveToNextFrame()
+  }
+}
 
 // PRIVATE
 
-Game.prototype.setSpareBonusTrue = function(){
-  this.bonus.setSpareTrue()
+Game.prototype._sumFrames = function(){
+  var frameTotals = []
+  this.scores.forEach(function(frame){
+    var score = 0
+    frame.forEach(function(bowl){
+      score += bowl
+    });
+    frameTotals.push(score);
+  });
+  return frameTotals
 }
 
-Game.prototype.setSpareBonusFalse = function(){
-  this.bonus.setSpareFalse()
-}
-
-Game.prototype.setStrikeBonusTrue = function(){
-  this.bonus.setStrikeTrue()
-}
-
-Game.prototype.setStrikeBonusFalse = function(){
-  this.bonus.setStrikeFalse()
-}
-
-Game.prototype.addFrame = function() {
-  if (this.currentFrame < 10){
-    this.currentFrame += 1
-  }
-}
-
-Game.prototype._setRollOneFalse = function(){
-  this.rollOne = false
-}
-
-Game.prototype._setRollOneTrue = function(){
-  this.rollOne = true
-}
-
-Game.prototype._saveRollOne = function(roll){
-  this.frames[this.currentFrame].setRollOne(roll)
-  this.setSpareBonusFalse()
-  if (roll == 10) {
-    this.setStrikeBonusTrue()
-    // think it should be here - LINE 48
-    this.addFrame()
-  } else {
-    this._setRollOneFalse()
-  }
-}
-
-Game.prototype._saveRollTwo = function(roll){
-  if (roll + this.frames[this.currentFrame].getRollOne() > 10) {
-    throw "Your rolls can't sum over 10"
-  }
-  if (roll + this.frames[this.currentFrame].getRollOne() == 10) {
-    this.setSpareBonusTrue()
-  }
-  this.frames[this.currentFrame].setRollTwo(roll)
-  this.addFrame()
-  this._setRollOneTrue()
+Game.prototype._sumGame = function(){
+  var total = 0
+  this._sumFrames().forEach(function(frame){
+    total += frame
+  });
+  return total
 }

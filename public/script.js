fbd631323908cd6fca06131e4a9e59edd58781c2
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-03 00:48:28 +0000

    Add jquery

--- public/script.js
+++ public/script.js
@@ -1,2 +1,31 @@
+var game = new Game();
+
 $(document).ready( function(){
+  var $inputs = $("input");
+  var $totals = $(".total");
+
+  $inputs.on("keyup blur", function(e){
+    var code = e.which;
+    var scores = [];
+
+    if(code==9 || code==13 || e.type=="blur") {
+      $inputs.each(function(){
+        var roll = parseInt($(this).val());
+        if(roll)
+        {
+          scores.push(roll);
+        }
+      });
+
+      game.updateRolls(scores);
+      game.score();
+
+      var total = 0;
+
+      $totals.each(function(index){
+        total += game._frames[index]._intScore
+        $(this).html(total);
+      })
+    }
+  });
 });

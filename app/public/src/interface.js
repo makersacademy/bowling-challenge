06a0705ebc32fea2ddd904fed79e13e4561e9b64
antonio <antoniobelmardacosta@gmail.com>
2017-12-03 23:24:32 +0000

    Create interface js file and update layout file with scripts

---
+++ app/public/src/interface.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+
+  $("#score-button").click(function() {
+    if($("#f1r1").val() !== "") {
+      var f1r1 = new Roll($("#f1r1").val())
+    };
+    if($("#f1r2").val() !== "") {
+      var f1r2 = new Roll($("#f1r2").val())
+    };
+    if($("#f2r1").val() !== "") {
+      var f2r1 = new Roll($("#f2r1").val())
+    };
+    if($("#f2r2").val() !== "") {
+      var f2r2 = new Roll($("#f2r2").val())
+    };
+    if($("#f3r1").val() !== "") {
+      var f3r1 = new Roll($("#f3r1").val())
+    };
+    if($("#f3r2").val() !== "") {
+      var f3r2 = new Roll($("#f3r2").val())
+    };
+    if($("#f4r1").val() !== "") {
+      var f4r1 = new Roll($("#f4r1").val())
+    };
+    if($("#f4r2").val() !== "") {
+      var f4r2 = new Roll($("#f4r2").val())
+    };
+    if($("#f5r1").val() !== "") {
+      var f5r1 = new Roll($("#f5r1").val())
+    };
+    if($("#f5r2").val() !== "") {
+      var f5r2 = new Roll($("#f5r2").val())
+    };
+    if($("#f6r1").val() !== "") {
+      var f4r1 = new Roll($("#f6r1").val())
+    };
+    if($("#f6r2").val() !== "") {
+      var f6r2 = new Roll($("#f6r2").val())
+    };
+    if($("#f7r1").val() !== "") {
+      var f4r1 = new Roll($("#f7r1").val())
+    };
+    if($("#f7r2").val() !== "") {
+      var f7r2 = new Roll($("#f7r2").val())
+    };
+    if($("#f8r1").val() !== "") {
+      var f8r1 = new Roll($("#f8r1").val())
+    };
+    if($("#f8r2").val() !== "") {
+      var f8r2 = new Roll($("#f8r2").val())
+    };
+    if($("#f9r1").val() !== "") {
+      var f9r1 = new Roll($("#f9r1").val())
+    };
+    if($("#f9r2").val() !== "") {
+      var f9r2 = new Roll($("#f9r2").val())
+    };
+    if($("#f10r1").val() !== "") {
+      var f10r1 = new Roll($("#f10r1").val())
+    };
+    if($("#f10r2").val() !== "") {
+      var f10r2 = new Roll($("#f10r2").val())
+    };
+    if($("#f11r1").val() !== "") {
+      var f11r1 = new Roll($("#f11r1").val())
+    };
+    if($("#f11r2").val() !== "") {
+      var f11r2 = new Roll($("#f11r2").val())
+    };
+    if($("#f12r1").val() !== "") {
+      var f12r1 = new Roll($("#f12r1").val())
+    };
+    if($("#f12r2").val() !== "") {
+      var f12r2 = new Roll($("#f12r2").val())
+    };
+  })
+});

d67df30e647e0782704b31dd9006fb6439082e04
Peter Johnstone <peter.wdj@gmail.com>
2017-12-03 14:19:26 +0000

    Add functionality to update spare score

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,9 +1,52 @@
 describe("Scorecard", function() {
-  var scorecard = new Scorecard();
+  var scorecard;
+  var frame;
+
+  beforeEach(function() {
+    scorecard = new Scorecard();
+    frame = new Frame();
+  });
 
   describe("frameResults", function() {
     it("Should be set to an empty array by default", function() {
       expect(scorecard.frameResults).toEqual([])
     });
+  });
+
+  describe("addFrame", function() {
+    it("Should add a frame function into the frameResults array", function() {
+      scorecard.addFrame(frame);
+      expect(scorecard.frameResults).toEqual([frame])
+    });
+  });
+
+  describe("frameScores", function() {
+    it("Should be an empty array by default", function() {
+      expect(scorecard.frameScores).toEqual([])
+    });
+
+    it("Calling updateScores should set it to [7]", function() {
+      frame.setBowlOneScore(5);
+      frame.setBowlTwoScore(2);
+      frame.setFrameScore();
+      scorecard.addFrame(frame);
+      scorecard.updateScores();
+      expect(scorecard.frameScores).toEqual([7])
+    });
+
+    it("Scoring a spare should add the score of the next bowl onto the score", function() {
+      frame.setBowlOneScore(7);
+      frame.setBowlTwoScore(3);
+      frame.setFrameScore();
+      scorecard.addFrame(frame);
+      scorecard.updateScores();
+      var frame1 = new Frame();
+      frame1.setBowlOneScore(6);
+      frame1.setBowlTwoScore(2);
+      frame1.setFrameScore();
+      scorecard.addFrame(frame1);
+      scorecard.updateScores();
+      expect(scorecard.frameScores[0]).toEqual(16)
+    })
   });
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,3 +1,25 @@
 function Scorecard() {
   this.frameResults = [];
+  this.frameScores = []
+};
+
+Scorecard.prototype.addFrame = function (frame) {
+  this.frameResults.push(frame);
+};
+
+Scorecard.prototype.updateScores = function () {
+  var frame = this.frameResults[this.frameResults.length - 1];
+  var score = frame.score.reduce(function (total, amount) {
+    return total + amount;
+  });
+  if (frame.spare === false && frame.strike === false) {
+    if (this.frameResults.length > 1 && this.frameResults[this.frameResults.length - 2].spare === true) {
+      var spareScore = 10 + frame.getBowlOneScore();
+      this.frameScores.push(spareScore);
+      this.frameScores.push(score);
+    }
+    this.frameScores.push(score);
+  } else {
+    return;
+  }
 };

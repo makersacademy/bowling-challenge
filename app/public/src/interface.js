d88e1ad5c2068e44bd6a3f47bfe1fac3aaa2dc01
antonio <antoniobelmardacosta@gmail.com>
2017-12-04 10:11:55 +0000

    Update interface

--- app/public/src/interface.js
+++ app/public/src/interface.js
@@ -1,35 +1,64 @@
 $(document).ready(function() {
 
   $("#score-button").click(function() {
+    var f1r1;
+    var f1r2;
+    var f2r1;
+    var f2r2;
+    var f3r1;
+    var f3r2;
+    var f4r1;
+    var f4r2;
+    var f5r1;
+    var f5r2;
+    var f6r1;
+    var f6r2;
+    var f7r1;
+    var f7r2;
+    var f8r1;
+    var f8r2;
+    var f9r1;
+    var f9r2;
+    var f10r1;
+    var f10r2;
+    var f11r1;
+    var f11r2;
+    var f12r1;
+    var f12r2;
     if($("#f1r1").val() !== "") {
       var f1r1 = new Roll($("#f1r1").val())
     };
     if($("#f1r2").val() !== "") {
       var f1r2 = new Roll($("#f1r2").val())
+      var f1 = new Frame (f1r1, f1r2)
     };
     if($("#f2r1").val() !== "") {
       var f2r1 = new Roll($("#f2r1").val())
     };
     if($("#f2r2").val() !== "") {
       var f2r2 = new Roll($("#f2r2").val())
+      var f2 = new Frame (f2r1, f2r2)
     };
     if($("#f3r1").val() !== "") {
       var f3r1 = new Roll($("#f3r1").val())
     };
     if($("#f3r2").val() !== "") {
       var f3r2 = new Roll($("#f3r2").val())
+      var f3 = new Frame (f3r1, f3r2)
     };
     if($("#f4r1").val() !== "") {
       var f4r1 = new Roll($("#f4r1").val())
     };
     if($("#f4r2").val() !== "") {
       var f4r2 = new Roll($("#f4r2").val())
+      var f4 = new Frame (f4r1, f4r2)
     };
     if($("#f5r1").val() !== "") {
       var f5r1 = new Roll($("#f5r1").val())
     };
     if($("#f5r2").val() !== "") {
       var f5r2 = new Roll($("#f5r2").val())
+      var f5 
     };
     if($("#f6r1").val() !== "") {
       var f4r1 = new Roll($("#f6r1").val())

7dc94528e8abe9ec52ac28334046cf78094c4fa8
James Lizamore <jwlizamore@gmail.com>
2017-12-25 21:03:30 +0000

    Added more test cases for strike bonus

--- spec/scoreSpec.js
+++ spec/scoreSpec.js
@@ -11,6 +11,7 @@ describe("Score", function(){
       total: function(){return 7},
       isStrike: function(){return false},
       isSpare: function(){return false},
+      getRoll: function(i){return 4},
     };
     spareFrame = {roll1: 5, roll2: 5, total: function(){return 10}};
     strikeFrame = {roll1: 10,
@@ -29,8 +30,20 @@ describe("Score", function(){
     expect(score.getScore()).toEqual(70);
   })
 
-  it("Calculates strikebonus when a given frame is a strike", function(){
+  it("Calculates strikebonus as 20 when 3 strikes in a row", function(){
     for(var i = 0; i < 3; i++){ score.addFrame(strikeFrame); };
     expect(score.getStrikeBonus(0)).toEqual(20);
-  })
+  });
+
+  it("Calculates strikebonus as 14 when 2 strikes and a 4-3", function(){
+    for(var i = 0; i < 2; i++){ score.addFrame(strikeFrame); };
+    score.addFrame(nonSpareFrame)
+    expect(score.getStrikeBonus(0)).toEqual(14);
+  });
+
+  it("Calculates strikebonus as 7 when 1 strike and a 4-3", function(){
+    score.addFrame(strikeFrame);
+    score.addFrame(nonSpareFrame)
+    expect(score.getStrikeBonus(0)).toEqual(7);
+  });
 })

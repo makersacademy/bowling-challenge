31192f757ddbd3f15fd8dd476ca7e1c6b9ec086a
James Lizamore <jwlizamore@gmail.com>
2017-12-25 17:21:23 +0000

    Added isStrike predicate

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -5,8 +5,19 @@ describe("Frame", function(){
     frame = new Frame();
   });
 
-  it("Should record rolls", function(){
+  it("Should set and record rolls", function(){
     frame.setRoll(1, 5);
     expect(frame.getRoll(1)).toEqual(5);
+  })
+
+  it("Should return true when a strike", function(){
+    frame.setRoll(1, 10);
+    expect(frame.isStrike()).toBe(true);
+  })
+
+  it("Should return true when a spare", function(){
+    frame.setRoll(1,5);
+    frame.setRoll(2,5);
+    expect(frame.isSpare()).toBe(true);
   })
 })
--- src/frame.js
+++ src/frame.js
@@ -7,3 +7,11 @@ Frame.prototype.setRoll = function(roll, score){
 Frame.prototype.getRoll = function(roll){
   return this["roll"+roll.toString()]
 }
+
+Frame.prototype.isSpare = function(){
+  return ((this.getRoll(1) + this.getRoll(2)) === 10) && (!this.isStrike());
+}
+
+Frame.prototype.isStrike = function(){
+  return this.getRoll(1) === 10
+}

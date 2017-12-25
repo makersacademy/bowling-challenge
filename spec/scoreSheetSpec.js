ae510d7f12b7469a5f8e5bf0378523a1cb04bf92
Lan Pham <lanpham312@gmail.com>
2017-12-25 18:32:53 +0100

    adding score of 2 consecutive frames to scoresheet and refactoring test

--- spec/scoreSheetSpec.js
+++ spec/scoreSheetSpec.js
@@ -6,10 +6,21 @@ describe("ScoreSheet", function(){
   });
 
   it("should add the result of a frame to the scoresheet", function(){
-    frame = new Frame();
-    frame.appendNumberPinsDown(5)
-    frame.appendNumberPinsDown(3)
-    scoresheet.addNewFrame(frame.roll)
-    expect(scoresheet.frames[0]).toEqual([5,3]);
+    frameRoll(5,0)
+    expect(scoresheet.frames[0]).toEqual([5,0]);
   });
+
+  it("should log the result of the 2nd frame to the scoresheet", function(){
+    frameRoll(0,5)
+    frameRoll(4,3)
+    expect(scoresheet.frames[1]).toEqual([4,3]);
+    expect(scoresheet.frames).toEqual([[0,5], [4,3]]);
+  });
+
+  var frameRoll = function(value1, value2){
+    frame = new Frame();
+    frame.appendNumberPinsDown(value1)
+    frame.appendNumberPinsDown(value2)
+    scoresheet.addNewFrame(frame.roll)
+  };
 });

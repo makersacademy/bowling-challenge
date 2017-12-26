62241ee884001bb0eb0835e3eed78fb1a4303917
Keran <kerbraich@gmail.com>
2017-12-26 19:41:11 +0000

    add and pass test for initializing frame with 0 rolls

---
+++ Jasmine/spec/FrameSpec.js
@@ -0,0 +1,11 @@
+describe('Frame', function(){
+  var frame;
+
+  beforeEach(function(){
+    frame = new Frame();
+  });
+
+  it('initializes with 0 rolls', function(){
+    expect(frame.rolls).toEqual(0)
+  });
+});
---
+++ Jasmine/src/Frame.js
@@ -0,0 +1,6 @@
+// calculates score of each frame
+const MINIMUM_ROLLS = 0;
+
+function Frame(){
+  this.rolls = MINIMUM_ROLLS;
+};

ca2af999bc4b8f12c02c2349c2688c491c1bcc3a
Calum-W <cal7907@hotmail.co.uk>
2017-12-29 14:25:40 +0000

    Further refactors the Frame functions with a setRollScore

--- jasmine-standalone-2.8.0/src/Frame.js
+++ jasmine-standalone-2.8.0/src/Frame.js
@@ -13,18 +13,22 @@ function Frame(final = false) {
 
 Frame.prototype.bowl = function(rollScore) {
   this.score += rollScore;
+  this.setRollScore(rollScore)
   this.checkScoreType(rollScore);
   this.manageRoll(rollScore);
+}
+
+Frame.prototype.setRollScore = function(rollScore) {
+  if (this.rollNumber == 1) {this.rollOneScore = rollScore};
+  if (this.rollNumber == 2) {this.rollTwoScore = rollScore};
 }
 
 Frame.prototype.checkScoreType = function(rollScore) {
   if (rollScore === 10 && this.rollNumber == 1) {
     this.isStrike = true;
-    this.rollOneScore = 10;
   }
   else if (this.rollNumber === 2 && this.score === 10) {
     this.isSpare = true
-    this.rollTwoScore = rollScore
   }
 }
 
@@ -32,16 +36,12 @@ Frame.prototype.manageRoll = function(rollScore) {
   this.endFrame(rollScore)
   if (this.isFrameOver == false) {
     this.rollNumber += 1;
-    this.rollOneScore = rollScore;
    }
 }
 
 Frame.prototype.endFrame = function(rollScore) {
-  if (!this.isFinalFrame && this.rollNumber === 2) {
+  if (!this.isFinalFrame && (this.rollNumber === 2 || this.isStrike)) {
     this.isFrameOver = true;
-    this.rollTwoScore = rollScore;
-  } else if (!this.isFinalFrame && this.isStrike) {
-    this.isFrameOver = true
   } else if (this.isFinalFrame) {
     this.finalFrame(rollScore)
   }
@@ -51,11 +51,8 @@ Frame.prototype.finalFrame = function(rollScore) {
   if (this.rollNumber == 3) {
     this.isFrameOver = true;
   } else if (this.rollNumber == 2 && this.score < 10) {
-    this.rollTwoScore = rollScore
     this.isFrameOver = true;
   } else {
-    if (this.rollNumber == 1) {this.rollOneScore = rollScore};
-    if (this.rollNumber == 2) {this.rollTwoScore = rollScore};
     this.rollNumber += 1;
    }
 }

b7ae5af7599d5468bc405a6e7798dc109c778551
Samuel Johnson <samjohnson@samuels-mbp.lan>
2017-12-29 16:31:24 +0000

    Added and passed tests for bowling 2 balls in a frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,12 +1,27 @@
 describe("Frame", function() {
   var frame
 
-  beforeEach(function(){
+  beforeEach(function() {
   frame = new Frame;
   });
 
-  it ('initiliazes new frames with a score of 0', function() {
+  it('initiliazes new frames with a score of 0', function() {
     expect(frame.frameScore).toEqual(0)
+  });
+
+  it('initiliazes new frames with 10 pins', function() {
+    expect(frame.pinCount).toEqual(10)
+  });
+
+  it('bowls the first attempt reducing the pin count', function() {
+    frame.roll1(6)
+    expect(frame.pinCount).toEqual(4)
+  })
+
+  it('bowls the second attempt further reducing the pin count', function() {
+    frame.roll1(3)
+    frame.roll2(4)
+    expect(frame.pinCount).toEqual(3)
   })
 
 })
--- src/Frame.js
+++ src/Frame.js
@@ -1,3 +1,12 @@
 function Frame() {
   this.frameScore = 0
-}
+  this.pinCount = 10
+};
+
+Frame.prototype.roll1 = function(pinsHit) {
+  this.pinCount -= pinsHit
+};
+
+Frame.prototype.roll2 = function(pinsHit) {
+  this.pinCount -= pinsHit
+};

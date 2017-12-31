2cd2f34afd47eebe918c73e268dc3393b755d1b0
Tim Williamson <timwilliamson1337@gmail.com>
2017-12-31 12:42:47 +0000

    Basic test framework set up

---
+++ jasmine-standalone-2/spec/GameSpec.js
@@ -0,0 +1,81 @@
+describe("Game", function() {
+  console.log("Here")
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  describe("#roll", function() {
+    it("should increase the score", function() {
+      game.roll(3);
+      expect(game.score()).toEqual(3);
+      game.roll(6);
+      expect(game.score()).toEqual(9);
+    });
+    
+  });
+
+  describe("#score", function() {
+
+    it("should add up the score of a whole game", function() {
+      for(i = 0; i<= 12; i++) {
+      game.roll(10);
+      };
+      expect(game.score()).toEqual(300);
+    });
+  });
+
+});
+
+
+
+  // it("should be able to play a Song", function() {
+  //   player.play(song);
+  //   expect(player.currentlyPlayingSong).toEqual(song);
+
+  //   //demonstrates use of custom matcher
+  //   expect(player).toBePlaying(song);
+  // });
+
+  // describe("when song has been paused", function() {
+  //   beforeEach(function() {
+  //     player.play(song);
+  //     player.pause();
+  //   });
+
+  //   it("should indicate that the song is currently paused", function() {
+  //     expect(player.isPlaying).toBeFalsy();
+
+  //     // demonstrates use of 'not' with a custom matcher
+  //     expect(player).not.toBePlaying(song);
+  //   });
+
+  //   it("should be possible to resume", function() {
+  //     player.resume();
+  //     expect(player.isPlaying).toBeTruthy();
+  //     expect(player.currentlyPlayingSong).toEqual(song);
+  //   });
+  // });
+
+  // // demonstrates use of spies to intercept and test method calls
+  // it("tells the current song if the user has made it a favorite", function() {
+  //   spyOn(song, 'persistFavoriteStatus');
+
+  //   player.play(song);
+  //   player.makeFavorite();
+
+  //   expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+  // });
+
+  // //demonstrates use of expected exceptions
+  // describe("#resume", function() {
+  //   it("should throw an exception if song is already playing", function() {
+  //     player.play(song);
+
+  //     expect(function() {
+  //       player.resume();
+  //     }).toThrowError("song is already playing");
+  //   });
+  // });
+
---
+++ jasmine-standalone-2/src/Game.js
@@ -0,0 +1,11 @@
+function Game(){
+	this._score = 0
+};
+
+Game.prototype.roll = function(number){
+
+};
+
+Game.prototype.score = function(){
+	return this._score;
+};

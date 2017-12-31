284790a80b98bb255412bb6a7e550a52f1df3a9f
Valentina <valentina.rmo@gmail.com>
2017-12-31 14:18:33 +0000

    Add Strike and Spare logic

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,8 +1,11 @@
 describe("Frame", function() {
   var frame;
+  var frame2
+  var game;
 
   beforeEach(function(){
     frame = new Frame();
+    game = new Game();
   });
 
   it("shows the number of down pins after the first roll", function(){
@@ -10,5 +13,22 @@ describe("Frame", function() {
     frame.roll1(downPins);
     expect(frame.scoreRoll1).toEqual(3);
   });
+
+  it("checks if the result of the first roll is a Strike", function(){
+    frame.roll1(10);
+    expect(frame.isStrike).toBeTruthy();
+  })
+
+  it("shows the number of down pins after the second roll", function(){
+    var downPins = 7;
+    frame.roll2(downPins);
+    expect(frame.scoreRoll2).toEqual(7);
+  });
+
+  it("checks if the sum of the result of the first roll and second roll is a Spare", function(){
+    frame.roll1(3);
+    frame.roll2(7);
+    expect(frame.isSpare).toBeTruthy();
+  })
 
 })
--- src/Frame.js
+++ src/Frame.js
@@ -1,7 +1,24 @@
 function Frame(){
   this.MAX_PINS = 10;
+
 }
 
 Frame.prototype.roll1 = function(downPins){
   this.scoreRoll1 = downPins;
+  if(this.scoreRoll1 === 10){this.frameResult = "Strike"}
+
+};
+
+Frame.prototype.isStrike = function(){
+  if(this.frameResult === "Strike"){return true}
+};
+
+Frame.prototype.roll2 = function(downPins){
+  this.scoreRoll2 = downPins;
+  if(this.soreRoll1 + this.scoreRoll2 === 10){{this.frameResult = "Spare"}}
+
+Frame.prototype.isSpare = function(){
+    if(this.frameResult === "Spare"){return true}
+}
+
 };

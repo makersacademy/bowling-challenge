0388e8cd12bae6410c1d9a8b9e2f208a8b5ce1a1
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-01 23:09:17 +0000

    pin array can be manipulated and reset

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,7 +5,18 @@ describe("Game", function() {
     game = new Game()
   })
 
-  it("each round starts with 10 pins", function() {
+  it("each game starts with 10 pins", function() {
+    expect(game.pins.length).toEqual(10)
+  })
+
+  it(" 'knocks pins down' by removing entries from the pins array", function() {
+    game.knockDown(4)
+    expect(game.pins.length).toEqual(6)
+  })
+
+  it("resets pins array", function() {
+    game.knockDown(4)
+    game.resetPins()
     expect(game.pins.length).toEqual(10)
   })
 })
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,13 @@
 
 function Game() {
+  this.resetPins()
+}
+
+Game.prototype.knockDown = function(num) {
+  for(i=0;i<num;i++) { this.pins.pop() }
+}
+
+Game.prototype.resetPins = function() {
   this.pins = []
   for(i=0;i<10;i++) {  this.pins.push(1) }
 }

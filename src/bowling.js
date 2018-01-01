518bb75bc3b50ea73c3b08d063ca16cbc2ef2f8c
George Whiting <george_whiting@hotmail.com>
2018-01-01 22:53:33 +0000

    refactors most methods

--- src/bowling.js
+++ src/bowling.js
@@ -14,8 +14,8 @@ Game.prototype.nextRound = function() {
     throw new Error("A game is only 10 rounds long");
   } else {
     this.roundNumber ++ ;
-    this.throwNumber = 1;
-    this.remainingPins = 10;
+    this.resetThrows();
+    this.resetPins();
   }
 };
 
@@ -23,7 +23,79 @@ Game.prototype.bowl = function(pins) {
   if (pins > this.remainingPins) {
     throw new Error("You can't bowl higher than the remaining pins");
   }
-  this.bonusCalc();
+  this.multiplierCalc();
+  this.calculateRemainingBonus();
+  if (this.roundNumber === 10) {
+    this.tenthRound(pins);
+  } else {
+    this.firstNineRounds(pins);
+  }
+};
+
+Game.prototype.strikeBonus = function() {
+  this.bonusCalc(2);
+};
+
+Game.prototype.spareBonus = function() {
+  this.bonusCalc(1);
+};
+
+Game.prototype.bonusCalc = function(bonus) {
+  if (this.bonus[0] <= 0) {
+    this.bonus[0] = bonus;
+  } else {
+    this.bonus[1] = bonus;
+  }
+};
+
+Game.prototype.multiplierCalc = function() {
+  this.multiplier = 1;
+  for(var i = 0; i < 2; i++) {
+    if(this.bonus[i] > 0) {
+      this.multiplier ++;
+    }
+  };
+};
+
+Game.prototype.tenthRound = function(pins) {
+  if (this.throwNumber >= 4) {
+    throw new Error("Game is over");
+  }
+  if (this.throwNumber >= 3 && this.bonusThrows === 0 ){
+    throw new Error("Game is over");
+  }
+  if (this.bonusThrows > 0) {
+    this.bonusThrows -- ;
+  } else if (pins === 10) {
+    this.setBonusThrows(2);
+  } else if (pins === this.remainingPins) {
+    this.setBonusThrows(1);
+    this.resetPins();
+  } else {
+    this.knockPinsOver(pins);
+  }
+  this.incrementThrows();
+  this.addToScore(pins);
+};
+
+Game.prototype.firstNineRounds = function(pins) {
+  if (pins === 10) {
+    this.nextRound();
+    this.strikeBonus();
+  } else if (pins === this.remainingPins) {
+    this.nextRound();
+    this.spareBonus();
+  } else {
+    this.knockPinsOver(pins);
+    this.incrementThrows();
+    if (this.throwNumber >= 3) {
+      this.nextRound();
+    }
+  }
+  this.addToScore(pins);
+};
+
+Game.prototype.calculateRemainingBonus = function() {
   for(var i = 0; i < 2; i++) {
     if (this.bonus[i] > 0) {
       this.bonus[i] -- ;
@@ -31,65 +103,28 @@ Game.prototype.bowl = function(pins) {
       this.bonus[i] = 0;
     }
   };
-  if (this.roundNumber === 10) {
-    if (this.throwNumber >= 4) {
-      throw new Error("Game is over");
-    }
-    if (this.throwNumber >= 3 && this.bonusThrows === 0 ){
-      throw new Error("Game is over");
-    }
-    if (this.bonusThrows > 0) {
-      this.bonusThrows -- ;
-    } else if (pins === 10) {
-      this.bonusThrows = 2;
-    } else if (pins === this.remainingPins) {
-      this.bonusThrows = 1;
-      this.remainingPins = 10;
-    } else {
-      this.remainingPins = 10 - pins;
-    }
-    this.throwNumber ++ ;
-    this.score += (pins*this.multiplier);
-  } else {
-
-    if (pins === 10) {
-      this.nextRound();
-      this.strikeBonus();
-    } else if (pins === this.remainingPins) {
-      this.nextRound();
-      this.spareBonus();
-    } else {
-      this.remainingPins = 10 - pins;
-      this.throwNumber ++ ;
-      if (this.throwNumber >= 3) {
-        this.nextRound();
-      }
-    }
-    this.score += (pins*this.multiplier);
-  }
 };
 
-Game.prototype.strikeBonus = function() {
-  if (this.bonus[0] <= 0) {
-    this.bonus[0] = 2;
-  } else {
-    this.bonus[1] = 2;
-  }
+Game.prototype.resetPins = function() {
+  this.remainingPins = 10;
 };
 
-Game.prototype.spareBonus = function() {
-  if (this.bonus[0] <= 0) {
-    this.bonus[0] = 1;
-  } else {
-    this.bonus[1] = 1;
-  }
+Game.prototype.resetThrows = function() {
+  this.throwNumber = 1;
 };
 
-Game.prototype.bonusCalc = function() {
-  this.multiplier = 1;
-  for(var i = 0; i < 2; i++) {
-    if(this.bonus[i] > 0) {
-      this.multiplier ++;
-    }
-  };
+Game.prototype.knockPinsOver = function(pins) {
+  this.remainingPins = 10 - pins;
+};
+
+Game.prototype.incrementThrows = function() {
+  this.throwNumber ++ ;
+};
+
+Game.prototype.setBonusThrows = function(throws) {
+  this.bonusThrows = throws;
+};
+
+Game.prototype.addToScore = function(pins) {
+  this.score += (pins*this.multiplier);
 };

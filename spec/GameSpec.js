8b31e7fa9daa61ca21b6438af09a3ba065fb8a4c
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-01 23:58:43 +0000

    game now records strikes and spares

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -20,10 +20,16 @@ describe("Game", function() {
     expect(game.pins).toEqual(10)
   })
 
-  // it("increments strike count when scoring a strike", function() {
-  //   game.knockDown(10)
-  //   expect(game.strikes).toEqual(1)
-  // })
+  it("increments strike count when scoring a strike", function() {
+    game.knockDown(10)
+    expect(game.strikes).toEqual(1)
+  })
+
+  it("increments spare count when scoring a spare", function() {
+    game.knockDown(1)
+    game.knockDown(9)
+    expect(game.spares).toEqual(1)
+  })
 
   it("plays rounds (2 bowls per round)", function() {
     game.knockDown(4)
--- src/Game.js
+++ src/Game.js
@@ -3,21 +3,28 @@ function Game() {
   this.resetPins()
   this.round = 1
   this.bowlCount = 0
+  this.strikes = 0
+  this.spares = 0
 }
 
 Game.prototype.knockDown = function(num) {
   this.pins -= num
   this.bowlCount++
   if(this.isNewRound()) { this.updateRound() }
+  if(this.pins === 0) { this.isStrikeOrSpare() }
 }
 
 Game.prototype.resetPins = function() {
   this.pins = 10
 }
 
-// Game.prototype.isStrike = function() {
-//
-// }
+Game.prototype.isStrikeOrSpare = function() {
+  if(this.bowlCount === 1) {
+    this.strikes++
+  } else {
+    this.spares++
+  }
+}
 
 Game.prototype.updateRound = function() {
   this.round++

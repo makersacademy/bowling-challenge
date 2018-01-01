f875b50bae9ded8e0bb1258028d1f23cf6968736
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2018-01-01 21:21:13 +0000

    Refactoring to separate frame score and total score

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -14,31 +14,31 @@ describe("Bowling Game", function() {
 
 	it("should be able to roll a gutter game", function(){
 		manyRolls(0, 20);
-		expect(game.score()).toEqual(0);
+		expect(game.getFinalScore()).toEqual(0);
 	});
 
   it("should be able to roll a normal game (no strikes/spares)", function(){
     manyRolls(3, 15);
     manyRolls(3,5);
-    expect(game.score()).toEqual(60);
+    expect(game.getFinalScore()).toEqual(60);
   });
 
   it("should be able to roll a strike", function(){
     game.roll(10);
     manyRolls(1,18);
-    expect(game.score()).toEqual(30);
+    expect(game.getFinalScore()).toEqual(30);
   });
 
   it("should be able to roll the perfect game", function(){
     manyRolls(10,12);
-    expect(game.score()).toEqual(300);
+    expect(game.getFinalScore()).toEqual(300);
   });
 
   it("should be able to roll a spare", function() {
    game.roll(8);
    game.roll(2);
    manyRolls(1, 18);
-   expect(game.score()).toEqual(29);
+   expect(game.getFinalScore()).toEqual(29);
  });
 
 
--- src/bowling.js
+++ src/bowling.js
@@ -10,20 +10,42 @@ Game.prototype.roll = function (number) {
 	this._rolls.push(number);
 };
 
-Game.prototype.score = function(){
+Game.prototype.getFinalScore = function() {
+  return this.score(9);
+}
+
+Game.prototype.score = function(frame){
 	var result = 0;
 	var rollIndex = 0;
-	for(var frameIndex = 0; frameIndex < 10; frameIndex++) {
-		if(this._rolls[rollIndex] == 10) {
-			result += this._rolls[rollIndex] + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+
+	for(var frameIndex = 0; frameIndex <= frame; frameIndex++) {
+		if(this.isStrike(rollIndex)) {
+			result += this.getStrikeScore(rollIndex);
 			rollIndex += 1;
-		}else if(this._rolls[rollIndex] + this._rolls[rollIndex + 1] == 10){
-			result += this._rolls[rollIndex] + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+		}else if(this.isSpare(rollIndex)){
+			result += this.getSpareScore(rollIndex);
 			rollIndex += 2;
 		}else{
 			result += this._rolls[rollIndex] + this._rolls[rollIndex + 1];
 			rollIndex += 2;
-		}
+		};
 	}
 	return result;
 };
+
+
+Game.prototype.isStrike = function(rollIndex){
+  return this._rolls[rollIndex] == 10;
+}
+
+Game.prototype.getStrikeScore = function(rollIndex){
+  return 10 + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+}
+
+Game.prototype.isSpare = function(rollIndex){
+  return this._rolls[rollIndex] + this._rolls[rollIndex + 1] == 10;
+}
+
+Game.prototype.getSpareScore = function(rollIndex){
+  return 10 + this._rolls[rollIndex + 2];
+}

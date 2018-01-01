10a0b573379f21e43bf352fab4f29bae9b367260
Léa R <lea.rosamond@gmail.com>
2018-01-01 23:01:45 +0000

    Test for rolling only ones during the whole set of frames (20 rolls)

--- Jasmine/jasmine-standalone-2.8.0/spec/gameSpec.js
+++ Jasmine/jasmine-standalone-2.8.0/spec/gameSpec.js
@@ -6,12 +6,18 @@ describe("Game", function(){
     game = new Game();
   });
 
-  it("Roll the ball in the gutter", function(){
+  it("Rolls the ball in the gutter", function(){
     for (var i = 0; i < 20; i++){
       game.roll(0)
     }
     expect(game.score()).toEqual(0)
+  });
 
+  it("Rolls only ones during the whole set of frames", function() {
+    for (var i = 0; i < 20; i++){
+      game.roll(1)
+    }
+    expect(game.score()).toEqual(20)
   });
 
   // it("plays and add points to score", function(){

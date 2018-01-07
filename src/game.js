3e520e8276e6edb01b9b37c3fa98429473a9365d
umairb1 <umairbashir95@hotmail.com>
2018-01-07 23:54:40 +0000

    Added functionality for final frame strike

--- src/game.js
+++ src/game.js
@@ -1,5 +1,5 @@
 function Game() {
-  this.frames = Array.apply(null, Array(10)).map(function(){return new Frame()});
+  this.frames = Array.apply(null, Array(10)).map(function () { return new Frame() });
   this.currentFrame;
   this.index = 0;
   this.getCurrentFrame();
@@ -27,10 +27,7 @@ Game.prototype.getScore = function (score) {
   } else if (this.currentFrame.turnTwo === null) {
     this.currentFrame.turnTwo = score;
   }
-  if (this.isLastFrame() && this.isStrike()) {
-    this.currentFrame.turnTwo = score;
-    this.currentFrame.turnThree = score;
-  } else if (this.isLastFrame() && this.isSpear()) {
+  if (this.isLastFrame() && (this.isStrike() || this.isSpear())) {
     this.currentFrame.turnTwo = score;
     this.currentFrame.turnThree = score;
   }
@@ -39,7 +36,7 @@ Game.prototype.getScore = function (score) {
 Game.prototype.countScore = function (frame) {
   frame.score = (frame.turnOne + frame.turnTwo + frame.turnThree);
 
-  if (this.index > 1 && this.selectFrame(this.index -2).turnOne === 10) {
+  if (this.index > 1 && this.selectFrame(this.index - 2).turnOne === 10) {
     this.perfectGame(frame);
   } else if (this.index > 0) {
     this.countSpearScore(frame);
@@ -48,24 +45,25 @@ Game.prototype.countScore = function (frame) {
 };
 
 Game.prototype.countSpearScore = function (frame) {
-  if (this.selectFrame(this.index -1).score === 10 &&
-    this.selectFrame(this.index -1).turnTwo !== null) {
-    this.selectFrame(this.index -1).score += (frame.turnOne + frame.turnTwo)
+  if (this.selectFrame(this.index - 1).score === 10 &&
+    this.selectFrame(this.index - 1).turnTwo !== null) {
+    this.selectFrame(this.index - 1).score += (frame.turnOne + frame.turnTwo)
   }
 }
 
 Game.prototype.countStrikeScore = function (frame) {
-  if (this.selectFrame(this.index -1).turnOne === 10 &&
+  if (this.selectFrame(this.index - 1).turnOne === 10 &&
     frame.turnTwo !== null) {
-    this.selectFrame(this.index -1).score += frame.score
+    this.selectFrame(this.index - 1).score += frame.score
   }
 }
 
+
 Game.prototype.perfectGame = function (frame) {
-  if (this.selectFrame(this.index -2).turnOne === 10 &&
-    this.selectFrame(this.index -1).turnOne === 10) {
-    this.selectFrame(this.index -2).score +=
-      this.selectFrame(this.index -1).score + frame.score
+  if (this.selectFrame(this.index - 2).turnOne === 10 &&
+    this.selectFrame(this.index - 1).turnOne === 10) {
+    this.selectFrame(this.index - 2).score +=
+      this.selectFrame(this.index - 1).score + frame.score
   }
 }
 
@@ -74,13 +72,12 @@ Game.prototype.isLastFrame = function () {
 }
 
 Game.prototype.isStrike = function () {
-  return this.selectFrame(this.index -1).turnOne === 10;
+  return this.selectFrame(this.index - 1).turnOne === 10;
 }
 
 Game.prototype.isSpear = function () {
   return this.currentFrame.turnOne + this.currentFrame.turnTwo === 10;
 }
-
 
 
 Game.prototype.getCurrentFrame = function () {

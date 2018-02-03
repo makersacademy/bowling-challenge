a5fb77ff1843d5874093b9025944c8018ac7ce61
Leigh-ann <leighann.gant@gmail.com>
2018-02-03 22:00:34 +0000

    files created - frame tested and implemented

---
+++ spec/frameSpec.js
@@ -0,0 +1,57 @@
+describe('Frame',function() {
+  var frame
+
+  beforeEach(function() {
+    frame = new Frame();
+  });
+
+  it("initally has zero for first roll",function(){
+    expect(frame.firstRoll).toEqual(0)
+  })
+
+  it("initally has zero for second roll",function(){
+    expect(frame.firstRoll).toEqual(0)
+  })
+
+  describe('setFirstRoll', function(){
+    it('sets firstRoll to equal amount of pins knocked down',function(){
+      frame.setFirstRoll(4)
+      expect(frame.firstRoll).toEqual(4)
+    })
+  })
+  describe('setSecondRoll',function(){
+    it('sets secondRoll to equal amount of pins knocked down',function(){
+      frame.setSecondRoll(6)
+      expect(frame.secondRoll).toEqual(6)
+    })
+  })
+  describe('isStrike',function(){
+    it('returns true if firstRoll is equal to 10',function(){
+      frame.setFirstRoll(MAX_SCORE)
+      expect(frame.isStrike()).toEqual(true)
+    })
+    it('returns false if firstRoll is not equal to 10', function(){
+      frame.setFirstRoll(5)
+      expect(frame.isStrike()).toEqual(false)
+    })
+  })
+  describe('isSpare',function(){
+    it('returns true if firstRoll and secondRoll total is equal to 10',function(){
+      frame.setFirstRoll(5)
+      frame.setSecondRoll(5)
+      expect(frame.isSpare()).toEqual(true)
+    })
+    it('returns false if firstRoll and secondRoll total are not equal to 10',function(){
+      frame.setFirstRoll(4)
+      frame.setSecondRoll(5)
+      expect(frame.isSpare()).toEqual(false)
+    })
+  })
+  describe('frameScore', function(){
+    it('returns total of two rolls', function(){
+      frame.setFirstRoll(4)
+      frame.setSecondRoll(5)
+      expect(frame.score()).toEqual(9)
+    })
+  })
+});
---
+++ src/Frame.js
@@ -0,0 +1,26 @@
+const MAX_SCORE = 10;
+
+function Frame (){
+  this.firstRoll = 0
+  this.secondRoll = 0
+}
+
+Frame.prototype.setFirstRoll = function(pins){
+  this.firstRoll = pins
+}
+
+Frame.prototype.setSecondRoll = function(pins){
+  this.secondRoll = pins
+}
+
+Frame.prototype.isStrike = function(){
+  return this.firstRoll === MAX_SCORE
+}
+
+Frame.prototype.isSpare = function(){
+  return this.score() === MAX_SCORE
+}
+
+Frame.prototype.score = function(){
+  return this.firstRoll + this.secondRoll
+}

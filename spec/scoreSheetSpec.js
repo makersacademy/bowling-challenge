cd1d6743b6c408fca5685694dc61a610f672f835
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-03 20:49:53 +0000

    changed logic around for spare and strike to protect against edge casing

--- spec/scoreSheetSpec.js
+++ spec/scoreSheetSpec.js
@@ -8,16 +8,20 @@ describe('Bowling Scoresheet', function () {
 
   describe('Scoresheet begins with correct values', function () {
 
+    it('Pins droppped should be 0', function () {
+      expect(scoresheet.pinsDropped).toEqual(0);
+    });
+
+    it('Total rolls should be 0', function () {
+      expect(scoresheet.rollCounter).toEqual(0);
+    });
+
     it('Array roll should be empty', function () {
       expect(scoresheet.rollArray).toEqual([]);
     });
 
     it('Array score should be empty', function () {
-      expect(scoresheet.scoreArray).toEqual([]);
-    });
-
-    it('Total rolls should be 0', function () {
-      expect(scoresheet.rollCounter).toEqual(0);
+      expect(scoresheet.scoreArray).toEqual([[]]);
     });
 
     it('Frame counter should be 1', function () {
@@ -25,42 +29,73 @@ describe('Bowling Scoresheet', function () {
     });
   });
 
-  describe('Scoresheet accept score', function () {
-    it('Score Given function to have been called', function () {
-      spyOn(scoresheet, 'pinsDropped').and.returnValue(10);
-      expect(scoresheet.pinsDropped(10)).toEqual(10);
-    });
-  });
+  // describe('Scoresheet accept score', function () {
+  //   it('Score Given function to have been called', function () {
+  //     spyOn(scoresheet, 'pinsDropped').and.returnValue(10);
+  //     expect(scoresheet.pinsDropped(10)).toEqual(10);
+  //   });
+  // });
 
 
   describe('Strike , Spare or None', function () {
 
     it('Player scores strike on roll 1', function () {
-      scoresheet.pinsDropped(10);
-      console.log(scoresheet.scoreArray);
-      expect(scoresheet.isStrike()).toBeTruthy();
+      scoresheet.calculatingScore(10);
+
+      expect(function functionName() {scoresheet.isStrike();} ).toBeTruthy();
     });
 
-    it('Player scores a spare on roll 2, 5 pins per roll', function () {
-      scoresheet.pinsDropped(5);
-      scoresheet.pinsDropped(5);
-      console.log(scoresheet.scoreArray);
-      expect(scoresheet.isSpare()).toBeTruthy();
+    it('Player scores a spare on roll 2; 5 pins per roll', function () {
+      scoresheet.calculatingScore(5);
+      scoresheet.calculatingScore(5);
+
+      expect(function functionName() {scoresheet.isSpare();} ).toBeTruthy();
     });
 
-    it('Player scores a spare on roll 2, 1 pin and 9 pins in two rolls', function () {
-      scoresheet.pinsDropped(1);
-      scoresheet.pinsDropped(9);
-      console.log(scoresheet.scoreArray);
-      expect(scoresheet.isSpare()).toBeTruthy();
+    it('Player scores a spare on roll 2; 0 pins and 10 pins on second roll', function () {
+      scoresheet.calculatingScore(0);
+      scoresheet.calculatingScore(10);
+
+      expect(function functionName() {scoresheet.isSpare();} ).toBeTruthy();
     });
 
-    it("Player doesn't score either a spare or strike", function () {
-      scoresheet.pinsDropped(4);
-      scoresheet.pinsDropped(3);
-      console.log(scoresheet.scoreArray);
-      expect(scoresheet.scoreArray).toEqual([4,3]);
+    it('Player scores neither a spare or strike; 3 pins per roll', function () {
+      scoresheet.calculatingScore(3);
+      scoresheet.calculatingScore(3);
+
+      expect(scoresheet.totalScore()).toEqual(6);
     });
+
+
+    //
+    // it("Player doesn't score either a spare or strike", function () {
+    //   scoresheet.pinsDropped(4);
+    //   scoresheet.pinsDropped(3);
+    //   console.log(scoresheet.scoreArray);
+    //   expect(scoresheet.scoreArray).toEqual([4,3]);
+    // });
   });
+
+  // describe('Total Score so far', function () {
+  //   it('Should print provide total score after one frame', function () {
+  //     scoresheet.pinsDropped(4);
+  //     scoresheet.pinsDropped(5);
+  //
+  //     console.log(scoresheet.scoreArray);
+  //     expect(scoresheet.scoreCalculated).toEqual(9);
+  //   });
+  //
+  //   it('Should print total score after each frame', function () {
+  //     scoresheet.pinsDropped(10);
+  //
+  //     scoresheet.pinsDropped(1);
+  //     scoresheet.pinsDropped(9);
+  //
+  //     scoresheet.pinsDropped(5);
+  //     scoresheet.pinsDropped(1);
+  //     console.log(scoresheet.scoreArray);
+  //     //expect(scoresheet.scoreCalculated).toEqual(41);
+  //   });
+  // });
 
 });
--- src/scoresheet.js
+++ src/scoresheet.js
@@ -1,46 +1,89 @@
 function Scoresheet() {
 
-  this.rollArray = [];
-  this.scoreArray = [];
   this.rollCounter = 0;
+  this.pinsDropped = 0;
+  this.scoreArray = [[]];
+
+  this.rollArray = [];
   this.frameCounter = 1;
   this.scoreAfterFrame = 0;
+
 }
 
-
-Scoresheet.prototype.pinsDropped = function (numberOfPins) {
-
-  this.rollCounter += 1;
-  this.rollArray.push(numberOfPins);
-  this.scoreArray[this.rollCounter - 1] = numberOfPins;
-  this.scoreCalculated(numberOfPins);
+Scoresheet.prototype.totalScore = function () {
+  var total = 0;
+  for (var i = 0; i < this.scoreArray.length; i++) {
+    for (var j = 0; j < 2; j++) {
+       total += this.scoreArray[i][j];
+     }
+   }
+  return total;
 };
 
-Scoresheet.prototype.scoreCalculated = function (numberOfPins) {
-  this.scoreAfterFrame += numberOfPins;
+// Scoresheet.prototype.frameScore = function () {
+//
+//
+//   return framescore;
+// };
+
+
+Scoresheet.prototype.calculatingScore = function (pinsDropped) {
+  this.pinsDropped = pinsDropped;
+
+
+  if (this.isStrike()) {
+    this.scoreArray[this.frameCounter - 1][0] = 'X';
+    this.scoreArray[this.frameCounter - 1][1] = '';
+    this.rollCounter += 2;
+
+  } else if (this.isSpare()) {
+    // console.log('where is this line');
+    this.scoreArray[this.frameCounter-1][this.rollCounter] = '/';
+
+  }else {
+    this.scoreArray[this.frameCounter-1][this.rollCounter] = this.pinsDropped;
+  }
+
+  this.rollCounter += 1;
+
+  if (this.rollCounter === 2) {
+    this.rollCounter = 0;
+    this.frameCounter += 1;
+  }
+
 };
 
 
 Scoresheet.prototype.isStrike = function () {
-  if (this.rollCounter === 1 && this.rollArray[0] === 10) {
-    this.scoreArray.push([10,'X']);
-    this.rollArray = [];
-    this.rollCounter = 0;
+  // console.log('inside strike t');
+  if (this.rollCounter === 0 && this.pinsDropped === 10) {
     return true;
   }
-  return false;
 };
-
 
 Scoresheet.prototype.isSpare = function () {
-  if (this.rollCounter === 2 && (this.rollArray[0] + this.rollArray[1]) === 10) {
-    this.scoreArray.push([this.rollArray[0],'/']);
-    this.rollArray = [];
-    this.rollCounter = 0;
+  // console.log('inside spare ddd');
+  if (this.rollCounter === 1 && (this.scoreArray[this.frameCounter-1][0] + this.pinsDropped) === 10) {
+    // console.log('inside spare hello');
     return true;
   }
-  return false;
 };
+
+
+//
+// Scoresheet.prototype.scoreCalculated = function (numberOfPins) {
+//   //this.scoreAfterFrame += numberOfPins;
+//
+//   if (this.isStrike()) {
+//
+//     var frameScore = [];
+//     frameScore[0] =
+//
+//     return true;
+//   } else if(this.isSpare()){
+//     return true
+//   }
+// };
 
 
 

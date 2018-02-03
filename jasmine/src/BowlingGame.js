0f8d151bc10e1d0882cf3dea9d1f72c9b99d931c
Alexy <alexandra.fina@yahoo.com>
2018-02-03 19:06:55 +0000

    Make test score pass

--- jasmine/src/BowlingGame.js
+++ jasmine/src/BowlingGame.js
@@ -3,3 +3,8 @@ var BowlingGame = function (){};
 BowlingGame.prototype.roll = function () {
   return true;
 };
+
+BowlingGame.prototype.score = function () {
+  var score = 0;
+  return score + 5;
+};

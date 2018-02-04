2fc9eb34bda532e4808cb191339de6c32ca470b2
alextwilson <github@alextwilson.co.uk>
2018-02-04 18:27:15 +0000

    Rejects invalid pin values for non-final frames

--- jasmine/spec/frameSpec.js
+++ jasmine/spec/frameSpec.js
@@ -1,12 +1,14 @@
 describe('Frame', function() {
 
   var frame;
+  var error;
 
   beforeEach(function() {
     frame = new Frame();
+    error = new Error('Unable to add bowl, not a valid value');
   });
 
-  describe('add bowl', function() {
+  describe('addBowl', function() {
     it('sets the first bowl value to the score', function() {
       frame.addBowl(4);
       expect(frame.firstBowl).toEqual(4);
@@ -14,8 +16,23 @@ describe('Frame', function() {
 
     it('sets the second bowl value to the score', function() {
       frame.addBowl(4);
-      frame.addBowl(5)
+      frame.addBowl(5);
       expect(frame.secondBowl).toEqual(5);
+    });
+  });
+
+  describe('checkValid', function() {
+    it('rejects an initial score over 10', function() {
+      expect(function () { frame._checkValid(11); } ).toThrow(error);
+    });
+
+    it('rejects an initial score less than 0', function() {
+      expect(function () { frame._checkValid(-1); } ).toThrow(error);
+    });
+
+    it('rejects an second score if it would take the total above 10', function() {
+      frame.addBowl(5)
+      expect(function () { frame._checkValid(6); } ).toThrow(error);
     });
   });
 });
--- jasmine/src/frame.js
+++ jasmine/src/frame.js
@@ -6,9 +6,16 @@ function Frame() {
 };
 
 Frame.prototype.addBowl = function(score) {
+  this._checkValid(score)
   if (this.firstBowl === null) {
     this.firstBowl = score;
   } else if (this.secondBowl === null) {
     this.secondBowl = score;
   };
+};
+
+Frame.prototype._checkValid = function(score) {
+  var error = new Error('Unable to add bowl, not a valid value')
+  if (score > 10 || score < 0) throw error;
+  if (this.firstBowl != null && this.firstBowl + score > 10) throw error;
 };

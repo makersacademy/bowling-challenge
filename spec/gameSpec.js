fe32b0ba3f2d403e1da4f3feb653215117f1afcb
Ed <ea.litten@gmail.com>
2018-02-04 19:00:08 +0000

    Isolate game tests with spies

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,32 +1,77 @@
-describe('Game', function() {
+describe('Game', function () {
 
   var game;
 
-  beforeEach(function() {
+  beforeEach(function () {
     game = new Game
-    spyframe = jasmine.createSpyObj('spyframe',['score'])
+    frame = jasmine.createSpyObj('frame', {
+      // rolls [3,4]
+      'score': 7,
+      'isStrike': false,
+      'isSpare': false,
+      'firstRoll': 3
+    })
+    spareFrame = jasmine.createSpyObj('spareFrame', {
+      // rolls [3,7]
+      'score': 10,
+      'isStrike': false,
+      'isSpare': true,
+      'firstRoll': 3
+    })
+    strikeFrame = jasmine.createSpyObj('strikeFrame', {
+      // rolls [10,-]
+      'score': 10,
+      'isStrike': true,
+      'isSpare': false,
+      'firstRoll': 10
+    })
   });
 
-  it('starts with an empty set of frames', function(){
+  it('starts with an empty set of frames', function () {
     expect(game.getFrames()).toEqual([]);
   });
 
-  it('can add a frame', function(){
-    game.addFrame(spyframe)
-    expect(game.getFrames()).toContain(spyframe)
+  it('can add a frame', function () {
+    game.addFrame(frame)
+    expect(game.getFrames()).toContain(frame)
   });
 
-  it('a 10-frame game is scored correctly', function(){
+  it('a 10-frame game is scored correctly', function () {
     for (let i = 0; i < 10; i++) {
-      game.addFrame(new Frame([3,4]))
+      game.addFrame(frame)
     }
     expect(game.score()).toEqual(70);
   });
 
-  it('a spare has bonus points added correctly', function(){
-    game.addFrame(new Frame([3,7]))
-    game.addFrame(new Frame([3,4]))
-    expect(game.score()).toEqual(20);
-    // should equal 10 + 3 + 7
+  describe('special frame followed by a normal frame', function () {
+    it('a spare has bonus points added correctly', function () {
+      game.addFrame(spareFrame) // 10 + 3 bonus
+      game.addFrame(frame) // 7
+      expect(game.score()).toEqual(20);
+    });
+
+    it('a strike has bonus points added correctly', function () {
+      game.addFrame(strikeFrame)
+      game.addFrame(frame)
+      expect(game.score()).toEqual(24);
+    });
   });
+
+  describe('special frame followed by a special frame', function() {
+    it('a spare has bonus points added correctly', function(){
+      game.addFrame(strikeFrame) // 10 + 10 bonus
+      game.addFrame(spareFrame) // 10 + 3 bonus
+      game.addFrame(frame) // 7
+      expect(game.score()).toEqual(40); 
+    });
+
+    it('a strike has bonus points added correctly', function(){
+      game.addFrame(spareFrame) // 10 + 10 bonus
+      game.addFrame(strikeFrame) // 10 + 7 bonus
+      game.addFrame(frame) // 7
+      expect(game.score()).toEqual(44); 
+    });
+  });
+
+
 });

4ff046844759c7b3610b6f0f8f855185c40e65b7
Tom Grand <thomas.a.grand@gmail.com>
2018-02-04 18:46:09 +0000

    refactor frame to add rulebook

--- src/frame.js
+++ src/frame.js
@@ -8,14 +8,13 @@ class Frame {
   constructor(strikeRound = STRIKE_ROUND, spareRound = STANDARD_ROUND) {
     this.rounds = [];
     this.rules = {
-      strike: [strikeRound, STRIKE_SCORE_ROUND],
+      strike: [strikeRound, STRIKE_SCORE_ROUND], // note, this will become a class in the future
       spare: [spareRound, STRIKE_SCORE_ROUND],
-      normal: [STANDARD_ROUND, STANDARD_ROUND]
+      normal: [STANDARD_ROUND, STANDARD_ROUND],
     };
   }
 
   score() {
-    console.log(this.rounds)
     const validRolls = this.rounds.slice(0, this.rules[this.result()][1]);
     return this.add(validRolls);
   }
@@ -25,18 +24,18 @@ class Frame {
   }
 
   isFinished() {
-    return this.rounds.length >= this.rules[this.result()][0]
+    return this.rounds.length >= this.rules[this.result()][0];
   }
 
   // need to make private
-   result() {
+  result() {
     if (this.firstRoll() === STRIKE) { return 'strike'; }
-    if (this.firstRoll() + this.secondRoll() === STRIKE ) { return 'spare'; }
+    if (this.firstRoll() + this.secondRoll() === STRIKE) { return 'spare'; }
     return 'normal';
   }
 
   add(results) {
-    return results.reduce((a, b) => a + b, 0)
+    return results.reduce((a, b) => a + b, 0);
   }
 
 

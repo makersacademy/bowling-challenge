df325ada621d7a68732c4359badbe3fbfcca8f3e
Leigh-ann <leighann.gant@gmail.com>
2018-02-04 00:59:48 +0000

    Framelog count and end frame implemented

--- spec/frameLogSpec.js
+++ spec/frameLogSpec.js
@@ -3,10 +3,10 @@ describe('FrameLog',function(){
   var frame
 
   beforeEach(function(){
-    frame =  jasmine.createSpyObj('frame',['firstRoll','createFrame'])
+    frame =  jasmine.createSpyObj('frame',['firstRoll','createFrame','setSecondRoll'])
     framelog = new FrameLog(frame)
   })
-  
+
   it('has no frames by default',function(){
     expect(framelog.frames).toEqual([])
   })
@@ -28,6 +28,28 @@ describe('FrameLog',function(){
       frame.createFrame.and.returnValue(frame);
       framelog.startFrame(frame)
       expect(framelog.currentFrame).toEqual(frame)
+    })
+  })
+  describe('endFrame',function(){
+
+    beforeEach(function(){
+      frame.createFrame.and.returnValue(frame)
+      framelog.startFrame(frame)
+      framelog.endFrame()
+    })
+
+    it('adds second roll score to frame',function(){
+      expect(frame.setSecondRoll).toHaveBeenCalled()
+    })
+    it('clears currentFrame',function(){
+      expect(framelog.currentFrame).toEqual(null)
+    })
+  })
+  describe('frameCount',function(){
+    it('returns number of frames stored',function(){
+      framelog.startFrame(frame)
+      framelog.startFrame(frame)
+      expect(framelog.frameCount()).toEqual(2)
     })
   })
 })
--- src/FrameLog.js
+++ src/FrameLog.js
@@ -4,11 +4,20 @@ function FrameLog (frameClass) {
   this.currentFrame = null
 }
 
-FrameLog.prototype._addFrame = function(frame){
-  this.frames.push(frame)
-}
-
 FrameLog.prototype.startFrame = function(firstRollScore){
   this.currentFrame = this.frameClass.createFrame(firstRollScore)
   this._addFrame(this.currentFrame)
+}
+
+FrameLog.prototype.endFrame = function(secondRollScore){
+  this.currentFrame.setSecondRoll(secondRollScore)
+  this.currentFrame = null
+}
+
+FrameLog.prototype.frameCount = function(){
+  return this.frames.length
+}
+
+FrameLog.prototype._addFrame = function(frame){
+  this.frames.push(frame)
 }

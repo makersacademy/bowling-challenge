2d1a2beb6feead3ea9499e41a5b66ca559db9846
philb56 <phil.bugden@outlook.com>
2018-02-04 10:56:57 +0000

    Add more frame functionality

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,3 +1,8 @@
+// console.log("start");
+// console.log(frame._score)
+// console.log(frame._ballsRemaining)
+// console.log(frame._bonusBalls)
+// console.log("end");
 describe("Frame", function() {
   var frame;
 
@@ -29,34 +34,122 @@ describe("Frame", function() {
     expect(lastFrame.lastFrame()).toEqual(true);
   });
 
-  it("should be a strike if 10 or X with 2 balls left", function() {
-    expect(frame.isStrike("X")).toEqual(true);
-    expect(frame.isStrike(10)).toEqual(true);
-  });
-
-  it("shouldn't be a strike if <2 balls left", function() {
-    frame.play();
-    expect(frame.isStrike(10)).toEqual(false);
-  });
-
-  it("should be a spare if / with 1 ball left", function() {
-    frame.play();
-    expect(frame.isSpare("/")).toEqual(true);
-  });
-
-  it("should be a spare if score is 10 with 1 ball left", function() {
-    frame.play(7);
-    expect(frame.isSpare(3)).toEqual(true);
-  });
 
   it("should set bonus balls to two if a strike - X", function() {
     frame.play("X");
     expect(frame.bonusBalls()).toEqual(2);
+    expect(frame.score()).toEqual(10);
+    expect(frame.isComplete()).toEqual(true);
   });
 
   it("should set bonus balls to two if a strike - 10", function() {
-    frame.play("X");
+    frame.play(10);
     expect(frame.bonusBalls()).toEqual(2);
+    expect(frame.score()).toEqual(10);
+    expect(frame.isComplete()).toEqual(true);
   });
 
+  it("should set bonus balls to one if a spare - /", function() {
+    frame.play("-");
+    frame.play("/");
+    expect(frame.bonusBalls()).toEqual(1);
+    expect(frame.score()).toEqual(10);
+    expect(frame.isComplete()).toEqual(true);
+  });
+
+  it("should set bonus balls to one if a spare - 10", function() {
+    frame.play("-");
+    frame.play(10);
+    expect(frame.bonusBalls()).toEqual(1);
+    expect(frame.score()).toEqual(10);
+    expect(frame.isComplete()).toEqual(true);
+  });
+
+  it("should set bonus balls to one if a spare 3 & 7", function() {
+    frame.play(3);
+    frame.play(7);
+    expect(frame.bonusBalls()).toEqual(1);
+    expect(frame.score()).toEqual(10);
+    expect(frame.isComplete()).toEqual(true);
+  });
+
+  it("shouldn't set bonus balls if not a strike or spare 3&6", function() {
+    frame.play(3);
+    frame.play(6);
+    expect(frame.bonusBalls()).toEqual(0);
+    expect(frame.score()).toEqual(9);
+    expect(frame.isComplete()).toEqual(true);
+  });
+
+
+  it("should set balls remaining to two on last frame if a strike", function() {
+    lastFrame.play(10);
+    expect(lastFrame.ballsRemaining()).toEqual(2);
+  });
+
+  it("should set balls remaining to one on last frame if a spare", function() {
+    lastFrame.play(3);
+    lastFrame.play(7);
+    expect(lastFrame.ballsRemaining()).toEqual(1);
+  });
+
+  describe("Last frame not a strike or spare", function() {
+
+    beforeEach(function() {
+      lastFrame = new Frame(true);
+      lastFrame.play(3);
+      lastFrame.play(6);
+    });
+
+    it("shouldn't set bonus balls on the last frame if not a strike or spare", function() {
+      expect(lastFrame.bonusBalls()).toEqual(0);
+    });
+
+    it("shouldn't set balls remaining on the last frame if not a strike or spare", function() {
+      expect(lastFrame.ballsRemaining()).toEqual(0);
+    });
+
+    it("should set score to 9 on last frame with 3 & 6 pins", function() {
+      expect(lastFrame.score()).toEqual(9);
+    });
+
+    it("should set frame complete after two balls on last frame if score <10", function() {
+      expect(lastFrame.isComplete()).toEqual(true);
+    });
+
+  });
+  describe("Ensure correct pins added for each ball", function() {
+
+    beforeEach(function() {
+      frame = new Frame();
+    });
+
+    it("should set ball1 to X after strike", function() {
+      frame.play("X");
+      expect(frame._ball1).toEqual("X");
+    });
+    it("should set ball1 to - after first play", function() {
+      frame.play("-");
+      expect(frame._ball1).toEqual("-");
+    });
+    it("should set ball1 to 7 after first play", function() {
+      frame.play(7);
+      expect(frame._ball1).toEqual(7);
+    });
+    it("should set ball2 to / after spare", function() {
+      frame.play("-");
+      frame.play("/");
+      expect(frame._ball2).toEqual("/");
+    });
+    it("should set ball2 to 7 after spare 3&7", function() {
+      frame.play(3);
+      frame.play(7);
+      expect(frame._ball2).toEqual(7);
+    });
+    it("should set ball2 to 6 after second play", function() {
+      frame.play(3);
+      frame.play(6);
+      expect(frame._ball2).toEqual(6);
+    });
+  });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,8 +1,23 @@
-function Frame(lastFrame) {
+function Frame(lastFrame = false) {
   this._ballsRemaining = 2
   this._lastFrame = lastFrame
+  this._ball1 = 0
+  this._ball2 = 0
+  this._ball3 = 0
   this._bonusBalls = 0
   this._score = 0
+  this._ballInPlay = 0
+};
+
+Frame.prototype.bonusBalls = function() {
+  return this._bonusBalls;
+};
+
+Frame.prototype.lastFrame = function() {
+  return this._lastFrame;
+};
+Frame.prototype.score = function(pins) {
+  return this._score
 };
 
 Frame.prototype.isComplete = function() {
@@ -13,44 +28,63 @@ Frame.prototype.ballsRemaining = function() {
   return this._ballsRemaining;
 };
 
-Frame.prototype.isStrike = function(pins) {
-  return ((pins === "X" || pins === 10)
-          && this._ballsRemaining === 2
-          ? true: false );
-};
-
-Frame.prototype.isSpare = function(pins) {
-  return (this._score === 10
-          && this._ballsRemaining === 1
-          ? true: false );
-};
-
 Frame.prototype._decode = function(pins) {
-  if (pins = "X") {
-    return 10;
-  } else if (pins = "/") {
-    return 10 - this._score
-  } else if (pins = "-") {
-    return 0
-  } else {
-    return pins
-  };
+  switch (pins) {
+      case "X": return 10; break;
+      case "/": return 10; break;
+      case "-": return 0; break;
+      default: return parseInt(pins)
+  }
 };
 
-Frame.prototype.score = function(pins) {
-  this._score = this._decode(pins);
+Frame.prototype._addBallScore = function(pins) {
+  if (this._ballInPlay === 1) {this._ball1 = pins};
+  if (this._ballInPlay === 2) {this._ball2 = pins};
+  if (this._ballInPlay === 3) {this._ball3 = pins};
 };
 
+Frame.prototype.addScore = function(pins) {
+  this._addBallScore(pins);
+  this._score += this._decode(pins);
+};
+
+Frame.prototype.isStrike = function() {
+  return (this._score === 10
+          && this._ballInPlay === 1
+          ? true: false );
+};
+
+Frame.prototype.isSpare = function() {
+  return (this._score === 10
+          && this._ballInPlay === 2
+          ? true: false );
+};
+
+Frame.prototype._scoreType = function() {
+  if ( this.isStrike() ) {return "Strike"};
+  if ( this.isSpare() ) {return "Spare"};
+  return "default";
+}
+Frame.prototype._validate = function(pins = "-") {
+}
 Frame.prototype.play = function(pins = "-") {
-  this.score(pins);
-  if ( this.isStrike(pins) ){ this._bonusBalls = 2 };
-  this._ballsRemaining -= 1;
-};
-
-Frame.prototype.bonusBalls = function() {
-  return this._bonusBalls;
-};
-
-Frame.prototype.lastFrame = function() {
-  return this._lastFrame;
+  this._validate();
+  this._ballInPlay += 1;
+  this.addScore(pins);
+  switch (this._scoreType()) {
+    case "Strike":
+  // console.log("Strike");
+      this._bonusBalls = 2;
+      this._ballsRemaining = ( this.lastFrame() ? 2 : 0);
+      break;
+    case "Spare":
+// console.log("Spare");
+      this._bonusBalls = 1;
+      this._ballsRemaining = ( this.lastFrame() ? 1 : 0);
+      break;
+    default:
+      this._bonusBalls = 0;
+      this._ballsRemaining -= 1;
+// console.log("default");
+  }
 };

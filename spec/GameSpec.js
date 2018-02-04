fdd3edf8a82072e621b6a8ff841d3646bb1dcbb3
Ed Goold <teddy_goold@hotmail.com>
2018-02-04 18:31:52 +0000

    game class can calculate score and record frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,20 +1,47 @@
-'use strict';
+"use strict";
 
 
 describe("Game", function() {
-  var game;
+	var game;
+	var frame;
 
-  beforeEach(function() {
-    game =  new Game();
-  });
+	beforeEach(function() {
+		game =  new Game();
+		frame = jasmine.createSpy("frame");
+	});
 
-  describe("When a game starts", function() {
-    it("should start with a score of 0", function() {
-      expect(game.calculateScore()).toEqual(0);
-    });
+	describe("When a game starts", function() {
+		it("should start with a score of 0", function() {
+			expect(game.calculateScore()).toEqual(0);
+		});
 
-    it("should start with a tally of 0 frames", function() {
-      expect(game.frametally).toEqual(0);
-    });
-  });
+		it("should start with a tally of 0 frames", function() {
+			expect(game.frametally).toEqual(0);
+		});
+	});
+
+	describe("When a frame is recorded", function() {
+
+		it("should increase the tally of frames recorded when a frame is submitted", function() {
+			game.recordFrame(frame);
+			expect(game.frametally).toEqual(1);
+		});
+
+		it("should append the frame to the game's record of all frames when a frame is submitted",  function() {
+			game.recordFrame(frame);
+			expect(game.submittedframes[game.submittedframes.length - 1]).toEqual(frame);
+		});
+
+		it("should calculate the score when a frame is submitted", function(){
+			game.frametally = 8;
+			game.score = 100;
+			expect(game.recordFrame(frame)).toEqual("Frame " + game.frametally + " of 10 complete. Your current score is 100");
+		});
+
+		it("should calculate the final score when the frame tally reaches 10", function() {
+			game.frametally = 9;
+			game.score = "100";
+			expect(game.recordFrame(frame)).toEqual("Game is now concluded. Your final score is: 100");
+		});
+	});
 });
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,21 @@
-'use strict';
+"use strict";
 
 function Game() {
-  this.score = 0;
-  this.frametally = 0;
+	this.score = 0;
+	this.frametally = 0;
+	this.submittedframes = [];
 }
 
 Game.prototype.calculateScore = function() {
-  return this.score;
+	return this.score;
+};
+
+Game.prototype.recordFrame = function(frame) {
+	this.submittedframes.push(frame);
+	this.frametally++;
+	if (this.frametally == 10) {
+		return "Game is now concluded. Your final score is: " + this.calculateScore();
+	} else {
+		return "Frame " + this.frametally + " of 10 complete. Your current score is " + this.calculateScore();
+	}
 };

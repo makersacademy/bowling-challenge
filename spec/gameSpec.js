9838ca142c413755ae1c9d3e308519df1ba5f70a
Alexander Sergides <alexse5121@gmail.com>
2018-02-04 18:37:39 +0000

    throws an error when number of rolls more than 21

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -17,9 +17,15 @@ describe('game', function() {
     expect(game.score).toEqual(1)
   });
 
-//   it('increases in score with increaseScore()', function() {
-//     score.increaseScore();
-//     score.currentScore() = 1;
-//     expect(score.currentScore()).toEqual(1);
-//   });
+  it('calculates correctly the score', function() {
+    game.updateRolls([3,0,10,10,7,3,5,3,10,8,0,6,0,3,7,10,10,10]);
+    game.score();
+    expect(game.score()).toEqual(155);
+  });
+
+  it('throws an error when the nr of rolls > 21', function(){
+    expect(function(){ game.updateRolls([3 ,0, 10, 10, 7, 3, 5, 3, 10, 8, 0, 6, 0, 3, 7, 10, 10, 10, 10, 8, 2, 5 ,3]); }).toThrowError('cannot have more than 21 rolls in a game');
+  });
+
+
 });
--- src/game.js
+++ src/game.js
@@ -7,3 +7,10 @@ function Game() {
 Game.prototype.point_increase = function() {
   this.score += 1;
 };
+
+Game.prototype.updateRolls = function (rollsList) {
+  if(rollsList.length > 21){
+    throw new Error('cannot have more than 21 rolls in a game')
+  }
+  game._rolls = rollsList;
+};

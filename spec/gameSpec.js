98993dff2e79c935fda5b6c64376f72a1ad30cc0
Tom Grand <thomas.a.grand@gmail.com>
2018-02-04 18:46:35 +0000

    add feature test

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,14 +2,21 @@
 
 describe('Game', () => {
   let game;
+  let frame;
 
   beforeEach(() => {
-    game = new Game();
+    frame = jasmine.createSpyObj('Frame', {'new': true, });
+    function Frame() { return frame; }
+    game = new Game(Frame);
   });
 
   describe('Initialise', () => {
-    it('initialises with 10 frames'){
-       game.
-    }
+    it('initialises with 10 frames', () => {
+      expect(game.board.length).toEqual(10)
+    });
+
+    it('it stores frames in the board', () => {
+      expect(game.board).toContain(frame)
+    });
   });
-})
+});

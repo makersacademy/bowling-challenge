0383cc62433dce3acbfe070f8d71cc286fc75031
Tom Grand <thomas.a.grand@gmail.com>
2018-02-04 18:46:21 +0000

    add results and score method for game

---
+++ src/game.js
@@ -0,0 +1,26 @@
+const GAME_LENGTH = 10;
+const FINAL_GAME_STRIKE = 3;
+
+class Game {
+  constructor(Frame) {
+    this.board = [];
+    for (let i = 0; i < GAME_LENGTH - 1; i += 1) {
+      this.board.push(new Frame());
+    }
+    this.board.push(new Frame(FINAL_GAME_STRIKE, FINAL_GAME_STRIKE));
+  }
+
+  results(rolls) {
+    let start = 0
+    let board = this.board
+    rolls.forEach(function(roll) {
+    board[start].roll(roll);
+    if (board[start].isFinished()) { start += 1; console.log(roll)}
+
+    });
+  }
+
+  score() {
+   return this.board.reduce((a, b) => a + b.score() , 0)
+  }
+}

21be92cd04ac567d398302fe06c51c86870e02ba
Tom Grand <thomas.a.grand@gmail.com>
2018-02-08 19:53:29 +0000

    tidy up play method

--- src/game.js
+++ src/game.js
@@ -2,22 +2,23 @@ const GAME_LENGTH = 10;
 const FINAL_GAME_STRIKE = 3;
 
 class Game {
-  constructor(Frame) {
+  constructor(frame) {
     this.board = [];
+    this.Frames = frame
     for (let i = 0; i < GAME_LENGTH - 1; i += 1) {
-      this.board.push(new Frame());
+      this.board.push(new this.Frames());
     }
-    this.board.push(new Frame(FINAL_GAME_STRIKE, FINAL_GAME_STRIKE));
+    this.board.push(new this.Frames(FINAL_GAME_STRIKE, FINAL_GAME_STRIKE));
   }
 
   play(rolls) {
-    let index = 0
-    const board = this.board
-    rolls.forEach(function(roll) {
-    board[index].roll(roll);
-    if (index > 0) {board[index-1].roll(roll); }
-    if (index > 1) { board[index-2].roll(roll); }
-    if (board[index].isFinished()) { index += 1;} //next round when finished
+    let index = 0;
+    const board = this.board;
+    rolls.forEach((roll) => {
+      board[index].roll(roll);
+      if (index > 0) { board[index - 1].roll(roll); }
+      if (index > 1) { board[index - 2].roll(roll); }
+      if (board[index].isFinished()) { index += 1; }
     });
   }
 

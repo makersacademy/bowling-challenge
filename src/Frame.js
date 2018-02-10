06c3d8fe9ea6143f4357b611eb652f790ac6177c
domvernon <3686359+domvernon@users.noreply.github.com>
2018-02-10 22:28:09 +0000

    refactors frame class

--- src/Frame.js
+++ src/Frame.js
@@ -6,31 +6,47 @@ class Frame {
   }
 
   bowl(pins) {
-    if (this.cannotBowl()) {
-      throw Error(`Cannot have more than ${this.maxFrameLength}`);
-    } else {
+    try {
       this.recordTurn();
       this.recordRoll(pins);
-      if (this.rolls[1] + this.rolls[2] === 10) {
-        this.wasSpare = true;
-      } else if (this.rolls[1] === 10) {
-        this.wasStrike = true;
-      }
+      this.recordSpecial();
+    } catch (error) {
+      throw error;
     }
   }
 
   // Start of helper methods
 
-  cannotBowl() {
-    return this.bowlAttempts >= this.maxFrameLength;
+  recordTurn() {
+    if (this.exceedsTurns()) {
+      throw Error(`Cannot have more than ${this.maxFrameLength}`);
+    } else {
+      this.bowlAttempts += 1;
+    }
   }
 
-  recordTurn() {
-    this.bowlAttempts += 1;
+  exceedsTurns() {
+    return this.bowlAttempts >= this.maxFrameLength;
   }
 
   recordRoll(pins) {
     this.rolls[this.bowlAttempts] = pins;
+  }
+
+  recordSpecial() {
+    if (this.spare()) {
+      this.wasSpare = true;
+    } else if (this.strike()) {
+      this.wasStrike = true;
+    }
+  }
+
+  spare() {
+    return this.rolls[1] + this.rolls[2] === 10;
+  }
+
+  strike() {
+    return this.rolls[1] === 10;
   }
 }
 

7383c8fb6e9748d847e8ce0e0132abaed13255cd
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-11 18:42:25 +0000

    game now takes responsibility for knowing the current frame

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,27 +11,37 @@ describe("Game", function() {
       expect(game.frames.length).toEqual(10);
     });
 
-    // it("knows to enter into first frame", function() {
-    //   game.roll(1,1,3);
-    //   expect(game.frames[0].bowls[0]).toEqual(3);
-    // });
+    it("knows it is the first frame", function() {
+      expect(game.currentFrame).toEqual(1);
+    });
+
+    it("knows it is the second frame", function() {
+      game.roll(7);
+      game.roll(3);
+      expect(game.currentFrame).toEqual(2);
+    });
+
+    it("knows it is the first frame", function() {
+      game.roll(3);
+      expect(game.frames[0].bowls[0]).toEqual(3);
+    });
 
     it("knows it is the first roll", function() {
-      game.roll(1,7);
-      game.roll(1,3);
+      game.roll(7);
+      game.roll(3);
       expect(game.frames[1].isRoll).toEqual(1);
     });
 
     it("has a running total", function() {
-      game.roll(1,3);
-      game.roll(1,4);
+      game.roll(3);
+      game.roll(4);
       game.calculateTotal();
       expect(game.runningTotal).toEqual(7)
     });
 
     it("cannot add a roll if it takes the total over 10", function() {
-      game.roll(1,3);
-      expect( function() { game.roll(1,9) } ).toThrow('not enough pins!');
+      game.roll(3);
+      expect( function() { game.roll(9) } ).toThrow('not enough pins!');
     });
   });
 
@@ -42,13 +52,13 @@ describe("Game", function() {
     });
 
     it("enters a first bowl into a frame", function() {
-      game.roll(1,3);
+      game.roll(3);
       expect(game.frames[0].bowls).toEqual([3])
     });
 
     it("enters a second bowl into a frame", function() {
-      game.roll(1,3);
-      game.roll(1,4);
+      game.roll(3);
+      game.roll(4);
       expect(game.frames[0].bowls).toEqual([3,4])
     });
   });
@@ -60,18 +70,18 @@ describe("Game", function() {
     });
 
     it("adds the next roll to the previous frame score", function() {
-      game.roll(1,7);
-      game.roll(1,3);
-      game.roll(2,1);
-      game.roll(2,3);
+      game.roll(7);
+      game.roll(3);
+      game.roll(1);
+      game.roll(3);
       expect(game.frames[0].total).toEqual(11)
     });
 
     it("has the correct running total", function() {
-      game.roll(1,7);
-      game.roll(1,3);
-      game.roll(2,1);
-      game.roll(2,3);
+      game.roll(7);
+      game.roll(3);
+      game.roll(1);
+      game.roll(3);
       game.calculateTotal();
       expect(game.runningTotal).toEqual(15)
     });
@@ -84,26 +94,26 @@ describe("Game", function() {
     });
 
     it("adds the next two rolls to the previous frame score", function() {
-      game.roll(1,10);
-      game.roll(2,1);
-      game.roll(2,3);
+      game.roll(10);
+      game.roll(1);
+      game.roll(3);
       expect(game.frames[0].total).toEqual(14)
     });
 
     it("adds the next two rolls if the first of them is strike", function() {
-      game.roll(1,10);
-      game.roll(2,10);
-      game.roll(3,3);
-      game.roll(3,5);
+      game.roll(10);
+      game.roll(10);
+      game.roll(3);
+      game.roll(5);
       expect(game.frames[0].total).toEqual(23)
       expect(game.frames[1].total).toEqual(18)
     });
 
     it("has the correct running total after 2 strikes", function() {
-      game.roll(1,10);
-      game.roll(2,10);
-      game.roll(3,3);
-      game.roll(3,5);
+      game.roll(10);
+      game.roll(10);
+      game.roll(3);
+      game.roll(5);
       game.calculateTotal();
       expect(game.runningTotal).toEqual(49)
     });
@@ -133,8 +143,9 @@ describe("Game", function() {
     it("allows 3rd roll", function() {
       frame1 = new Frame();
       game.frames.push(frame1);
-      game.roll(10,2);
-      game.roll(10,8);
+      game.currentFrame = 10
+      game.roll(2);
+      game.roll(8);
       expect(game.frames[9].isRoll).toEqual(3)
     });
 
@@ -142,7 +153,8 @@ describe("Game", function() {
       frame1 = new Frame();
       frame1.bowls = [5,5]
       game.frames.push(frame1);
-      game.roll(10,5)
+      game.currentFrame = 10
+      game.roll(5)
       game.frames[9].sumTotal();
       game.calculateTotal();
       expect(game.runningTotal).toEqual(78)
@@ -153,15 +165,17 @@ describe("Game", function() {
       frame1.bowls = [10,8]
       frame1.isRoll = 3
       game.frames.push(frame1);
-      expect( function() { game.roll(10,8); } ).toThrow('not enough pins!');
+      game.currentFrame = 10
+      expect( function() { game.roll(8); } ).toThrow('not enough pins on 10th frame!');
     });
 
     it("counts 2 extra rolls if 10th frame is strike", function() {
-      frame1 = new Frame();
-      frame1.bowls = [10]
+      frame1 = new Frame()
       game.frames.push(frame1);
-      game.roll(10,10);
-      game.roll(10,4);
+      game.currentFrame = 10
+      game.roll(10);
+      game.roll(10);
+      game.roll(4);
       game.frames[9].sumTotal();
       game.calculateTotal();
       expect(game.runningTotal).toEqual(87)
@@ -181,8 +195,8 @@ describe("Game", function() {
           game.frames.push(frame);
           game.frames[i].sumTotal();
         }
-        game.roll(10,10);
-        game.roll(10,10);
+        game.roll(10);
+        game.roll(10);
         game.frames[9].sumTotal();
         game.calculateTotal();
         expect(game.runningTotal).toEqual(300)
@@ -197,7 +211,7 @@ describe("Game", function() {
           game.frames.push(frame);
           game.frames[i].sumTotal();
         }
-        game.roll(10,0);
+        game.roll(0);
         game.frames[9].sumTotal();
         game.calculateTotal();
         expect(game.runningTotal).toEqual(0)
@@ -206,25 +220,25 @@ describe("Game", function() {
 
     it("gives the correct total for the example README game", function() {
       game.start(10);
-      game.roll(1,1);
-      game.roll(1,4);
-      game.roll(2,4);
-      game.roll(2,5);
-      game.roll(3,6);
-      game.roll(3,4);
-      game.roll(4,5);
-      game.roll(4,5);
-      game.roll(5,10);
-      game.roll(6,0);
-      game.roll(6,1);
-      game.roll(7,7);
-      game.roll(7,3);
-      game.roll(8,6);
-      game.roll(8,4);
-      game.roll(9,10);
-      game.roll(10,2);
-      game.roll(10,8);
-      game.roll(10,6);
+      game.roll(1);
+      game.roll(4);
+      game.roll(4);
+      game.roll(5);
+      game.roll(6);
+      game.roll(4);
+      game.roll(5);
+      game.roll(5);
+      game.roll(10);
+      game.roll(0);
+      game.roll(1);
+      game.roll(7);
+      game.roll(3);
+      game.roll(6);
+      game.roll(4);
+      game.roll(10);
+      game.roll(2);
+      game.roll(8);
+      game.roll(6);
       game.calculateTotal();
       expect(game.runningTotal).toEqual(133)
     });
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 function Game() {
   this.frames = []
+  this.currentFrame = 1
   // this.currentFrame = this.frames[frame-1]
 };
 
@@ -7,20 +8,22 @@ Game.prototype.start = function(length) {
   for(var i=0; i < length; i++){
     this.frames.push(new Frame());
   }
-    // this.frames.push(new FinalFrame());
 };
 
-Game.prototype.roll = function(frame, pins) {
-  if (this.frames[frame-1].isRoll===2 && frame<10 && (10-this.frames[frame-1].bowls[0])<pins) {
+Game.prototype.roll = function(pins) {
+  if (this.frames[this.currentFrame-1].isRoll===2 && this.currentFrame<10 && (10-this.frames[this.currentFrame-1].bowls[0])<pins) {
     throw 'not enough pins!'
   }
-  if (this.frames[frame-1].isRoll===3 && this.frames[frame-1].bowls[1]!=10 && (10-this.frames[frame-1].bowls[0])<pins) {
-    throw 'not enough pins!'
+  if (this.frames[this.currentFrame-1].isRoll===3 && this.frames[this.currentFrame-1].bowls[1]!=10 && (10-this.frames[this.currentFrame-1].bowls[0])<pins) {
+    throw 'not enough pins on 10th frame!'
   };
-  // this.checksPins(frame,pins)
-  (this.frames[frame-1]).enterRoll(pins)
-  this.bonusPoints(frame,pins)
-  this.frames[frame-1].changeRoll();
+  (this.frames[this.currentFrame-1]).enterRoll(pins)
+  this.bonusPoints(pins)
+  if (this.frames[this.currentFrame-1].bowls[0]===10 && this.currentFrame !=10 || this.frames[this.currentFrame-1].isRoll===2 && this.currentFrame !=10) {
+    this.currentFrame += 1
+  } else {
+    this.frames[this.currentFrame-1].changeRoll()
+  }
 };
 
 Game.prototype.calculateTotal = function() {
@@ -32,34 +35,24 @@ Game.prototype.calculateTotal = function() {
   }
 };
 
-Game.prototype.checksPins = function(frame,pins) {
-  if (this.frames[frame-1].isRoll===2 && frame<10 && (10-this.frames[frame-1].bowls[0])<pins) {
-    throw 'not enough pins!'
-  }
-  if (this.frames[frame-1].isRoll===3 && this.frames[frame-1].bowls[1]!=10 && (10-this.frames[frame-1].bowls[0])<pins) {
-    throw 'not enough pins!'
-  }
-};
-
-
-Game.prototype.bonusPoints = function(frame,pins) {
-  if (frame != 1 && this.frames[frame-2].isStrike === true) {
-    this.twoStrikesPoints(frame,pins)
-    this.oneStrikePoints(frame,pins)
+Game.prototype.bonusPoints = function(pins) {
+  if (this.currentFrame != 1 && this.frames[this.currentFrame-2].isStrike === true) {
+    this.twoStrikesPoints(pins)
+    this.oneStrikePoints(pins)
   } else
-  if (frame != 1 && this.frames[frame-2].isSpare === true && this.frames[frame-1].isRoll===1) {
-    this.frames[frame-2].total += pins
+  if (this.currentFrame != 1 && this.frames[this.currentFrame-2].isSpare === true && this.frames[this.currentFrame-1].isRoll===1) {
+    this.frames[this.currentFrame-2].total += pins
   }
 };
 
-Game.prototype.oneStrikePoints = function(frame,pins) {
-  if (this.frames[frame-1].isRoll!=3) {
-    this.frames[frame-2].total += pins
+Game.prototype.oneStrikePoints = function(pins) {
+  if (this.frames[this.currentFrame-1].isRoll!=3) {
+    this.frames[this.currentFrame-2].total += pins
   }
 };
 
-Game.prototype.twoStrikesPoints = function(frame,pins) {
-  if (frame != 2 && this.frames[frame-3].isStrike === true && this.frames[frame-1].isRoll===1) {
-    this.frames[frame-3].total += pins
+Game.prototype.twoStrikesPoints = function(pins) {
+  if (this.currentFrame != 2 && this.frames[this.currentFrame-3].isStrike === true && this.frames[this.currentFrame-1].isRoll===1) {
+    this.frames[this.currentFrame-3].total += pins
   }
 };

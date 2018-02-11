040e2a3aedcf38db6642078933a03a41273c627a
Tom Grand <thomas.a.grand@gmail.com>
2018-02-11 21:52:31 +0000

    add some style

--- src/format.js
+++ src/format.js
@@ -2,32 +2,40 @@ document.addEventListener('DOMContentLoaded', () => {
   const STRIKE = 10;
   const STRIKE_SYMBOL = 'X';
   const SPARE_SYMBOL = '/';
-  const game = new Game(Frame);
+  let isParty = true;
+  let game = new Game(Frame);
 
-  const setTotal = () => {
+  let setTotal = () => {
     document.getElementById('currentScore').innerHTML = game.score();
   };
 
-  const individualScores = () => {
+  let individualScores = () => {
     let collumnID = 1;
-    const scores = game.runningScores();
+    let scores = game.runningScores();
     scores.forEach((score) => {
       document.getElementById(`scoreFrame${collumnID}`).innerHTML = `${score}`;
       collumnID += 1;
     });
   };
 
+  let colourGen = () => {
+      let a = Math.random() * 255;
+      let b = Math.random() * 255;
+      let c = Math.random() * 255;
+   return "rgb(" + a + ", " + b + ", " + c + ")"
+}
+
   // this logic should be in model not in the formatting page...
 
-  const strikeConverter = rolls => rolls.map(roll => (roll === STRIKE ? STRIKE_SYMBOL : roll));
+  let strikeConverter = rolls => rolls.map(roll => (roll === STRIKE ? STRIKE_SYMBOL : roll));
 
-  const spareConverter = (rolls) => {
+  let spareConverter = (rolls) => {
     if (rolls[0] + rolls[1] === STRIKE) { rolls[1] = SPARE_SYMBOL; }
     if (rolls[1] + rolls[2] === STRIKE) { rolls[2] = SPARE_SYMBOL; }
     return rolls;
   };
 
-  const rollPrinter = game => game.map((frame) => {
+  let rollPrinter = game => game.map((frame) => {
     let view = strikeConverter(frame);
     view = spareConverter(view);
     return view.join('');
@@ -35,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // this logic is to be refactored out
 
-  const showRolls = () => {
+  let showRolls = () => {
     let collumnID = 1;
     const frames = rollPrinter(game.view());
     frames.forEach((frame) => {
@@ -44,9 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  const roll = (value) => { game.play(value); setTotal(); individualScores(); showRolls(); };
+  let roll = (value) => { game.play(value); setTotal(); individualScores(); showRolls(); };
 
-  const rollButton = (pins) => {
+  let rollButton = (pins) => {
     const btn = document.createElement('button');
     btn.id = pins;
     btn.onclick = () => { roll(pins); };
@@ -54,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
     return btn;
   };
 
-  const showScoreButtons = (max = STRIKE) => {
+  let showScoreButtons = (max = STRIKE) => {
     for (let i = 0; i <= max; i += 1) {
       const btn = rollButton(i);
       document.getElementById('changeScores').appendChild(btn);
@@ -62,16 +70,16 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
 
-  const createFieldWithID = (id) => {
+  let createFieldWithID = (id) => {
     const field = document.createElement('td');
     field.id = id;
     return field;
   };
 
-  const createScoreCard = () => {
+  let createScoreCard = () => {
     for (let i = 1; i <= STRIKE; i += 1) {
       let title = document.createElement('td');
-      title.innerHTML = `frame ${i}`;
+      title.innerHTML = `Frame ${i} `;
       let scoreBox = createFieldWithID(`scoreFrame${i}`);
       let frameRolls = createFieldWithID(`views${i}`);
       document.getElementById('scorecardLabels').appendChild(title);

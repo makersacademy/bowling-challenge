989280e728fca4962c2738d28ea2a35cda32cf80
Tom Grand <thomas.a.grand@gmail.com>
2018-02-11 14:52:03 +0000

    add unit tests for game score

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,50 +1,87 @@
 /* eslint-env jasmine */
 const Game = require('../src/game');
 
-describe('Game', () => {
+describe('Initialise game', () => {
   let game;
   let frame;
 
   beforeEach(() => {
     frame = jasmine.createSpyObj('Frame', { score: 10 });
     function Frame() { return frame; }
-    spyOn(Game.prototype, 'setUpBoard').and.returnValue([frame]);
     game = new Game(Frame);
   });
 
-  describe('Initialise', () => {
-    it('initialises with 10 frames', () => {
-      expect(game.board.length).toEqual(10);
-    });
+  it('initialises with 10 frames', () => {
+    expect(game.board.length).toEqual(10);
+  });
 
-    it('it stores frames in the board', () => {
-      expect(game.board).toContain(frame);
-    });
+  it('it stores frames in the board', () => {
+    expect(game.board).toContain(frame);
+  });
+
+  // it('sets the final frames max length to three', () => {
+  //   expect(game.board).toContain(frame);
+  // });  Need to test this
+});
+
+describe('Game', () => {
+  let game;
+  let frame;
+  let frameTwo;
+  let frameThree;
+  let boardMock;
+
+  beforeEach(() => {
+    boardMock = [];
+    frame = jasmine.createSpyObj('Frame', { score: 10 });
+    frameTwo = jasmine.createSpyObj('Frame', { score: 9 });
+    frameThree = jasmine.createSpyObj('Frame', { score: 0 });
+    for (let i = 0; i < 10; i += 1) { boardMock.push(frame); }
   });
 
   describe('Score', () => {
+    it('checks the score of a frame when score is called', () => {
+      spyOn(Game.prototype, 'setUpBoard').and.returnValue(boardMock);
+      game = new Game();
+      game.score();
+
+      expect(frame.score).toHaveBeenCalledWith();
+    });
+
     it('should return a score of 100 for 10 frames with 10', () => {
+      spyOn(Game.prototype, 'setUpBoard').and.returnValue(boardMock);
+      game = new Game();
+
       expect(game.score()).toEqual(100);
     });
 
-    it('checks the score of a frame when score is called', () => {
-      game.score();
-
-      expect(frame.score).toHaveBeenCalledWith();
-    });
-
     it('when it has three frames, it gives the total score of those three', () => {
-      game.score();
+      boardMock = [];
+      boardMock.push(frame, frameTwo, frameThree);
+      spyOn(Game.prototype, 'setUpBoard').and.returnValue(boardMock);
+      game = new Game();
 
-      expect(frame.score).toHaveBeenCalledWith();
+      expect(game.score()).toEqual(19);
     });
 
     it('should return a score of 300 for 10 frames with 30 score', () => {
+      boardMock = [];
       frame = jasmine.createSpyObj('Frame', { score: 30 });
-      function Frame() { return frame; }
-      game = new Game(Frame);
+      for (let i = 0; i < 10; i += 1) { boardMock.push(frame); }
+      spyOn(Game.prototype, 'setUpBoard').and.returnValue(boardMock);
+      game = new Game();
 
       expect(game.score()).toEqual(300);
+    });
+
+    it('should return a score of 19 despite the fact some frames have no score', () => {
+      boardMock = [];
+      frameThree = jasmine.createSpyObj('Frame', { score: 0 });
+      boardMock.push(frame, frameTwo, frameThree);
+      spyOn(Game.prototype, 'setUpBoard').and.returnValue(boardMock);
+      game = new Game();
+
+      expect(game.score()).toEqual(19);
     });
   });
 });

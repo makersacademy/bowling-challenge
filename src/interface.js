c2720fe5ef5377c354863bd131b366f1f325c600
Leigh-ann <leighann.gant@gmail.com>
2018-02-11 11:32:38 +0000

    Interface and index refactored - removed repeating code

--- src/interface.js
+++ src/interface.js
@@ -4,20 +4,18 @@ $(document).ready(function(){
 
   $("#new-game").click(function(event){
     game =  new FrameLog()
-    $("#totalScore").text(0)
-    for(var i = 0; i < FRAME_LIMIT; i ++){
-      $(`#f${i+1}`).text(0)
-      $(`#f${i+1}rolls`).text('')
-    };
+    clearScorescard()
+    getMessage()
   })
 
-  function updateScores(){
-    isGameComplete()
+  function updateScores(rollNumber){
     $("#totalScore").text(game.calculateScore())
       for(var i = 0; i < game.frameCount(); i ++){
         $(`#f${i+1}`).text(game.frames[i].score())
       }
       updateRolls()
+      getMessage(rollNumber)
+      isGameComplete()
     }
 
     function updateRolls(){
@@ -27,65 +25,52 @@ $(document).ready(function(){
     }
 
     function isGameComplete(){
-      if (game.isFramelogComplete()) $('#complete').text("Frame Score Card Complete")
+      if (game.isFramelogComplete()){
+        $('#complete').text("Frame Score Card Complete")
+        $('#message').text("")
+      } else {
+        $('#complete').text("")
+      }
     }
 
-  $("#0-pin").click(function(event){
-    $('#message').text("Never a good time to clean the gutters.")
-    game.addRoll(0)
-    updateScores()
+    function clearScorescard(){
+      $("#totalScore").text(0)
+      for(var i = 0; i < FRAME_LIMIT; i ++){
+        $(`#f${i+1}`).text(0)
+        $(`#f${i+1}rolls`).text('')
+      };
+      isGameComplete()
+    }
+    function getMessage(rollNumber){
+      switch (rollNumber){
+        case 0:
+          $('#message').text("Never a good time to clean the gutters.");
+          break;
+        case 1:
+          $('#message').text("Knocking them down, one pin at a time");
+          break;
+        case 2:
+          $('#message').text("Your mum called. You left your game at home");
+          break;
+        case 5:
+          $('#message').text('Spare me');
+          break;
+        case 9:
+          $('#message').text("Bowlers always have time to spare.");
+          break;
+        case 10:
+          var messages = ["You're right up my alley", "KingPin", "That’s how I roll", "I hear the bowling thunder", "Strike it up with the gang."]
+          $('#message').text(messages[Math.floor(Math.random() * messages.length)])
+          break;
+        default:
+            $('#message').text("")
+      }
+    }
 
-  })
-  $("#1-pin").click(function(event){
-    $('#message').text("Knocking them down, one pin at a time")
-    game.addRoll(1)
-    updateScores()
+  $(".pin").click(function(event) {
+    var rollNumber = parseInt(event.currentTarget.innerHTML)
+    game.addRoll(rollNumber)
+    updateScores(rollNumber)
   })
 
-  $("#2-pin").click(function(event){
-    $('#message').text("Your mum called. You left your game at home")
-    game.addRoll(2)
-    updateScores()
-  })
-  $("#3-pin").click(function(event){
-    $('#message').text("")
-    game.addRoll(3)
-    updateScores()
-  })
-  $("#4-pin").click(function(event){
-    $('#message').text("")
-    game.addRoll(4)
-    updateScores()
-  })
-  $("#5-pin").click(function(event){
-    $('#message').text("Bowlers always have time to spare.")
-    game.addRoll(5)
-    updateScores()
-  })
-  $("#6-pin").click(function(event){
-    $('#message').text("")
-    game.addRoll(6)
-    updateScores()
-  })
-  $("#7-pin").click(function(event){
-    $('#message').text("")
-    game.addRoll(7)
-    updateScores()
-  })
-  $("#8-pin").click(function(event){
-    $('#message').text("")
-    game.addRoll(8)
-    updateScores()
-  })
-  $("#9-pin").click(function(event){
-    $('#message').text('Spare me')
-    game.addRoll(9)
-    updateScores()
-  })
-  $("#10-pin").click(function(event){
-    var messages = ["You're right up my alley", "KingPin", "That’s how I roll", "I hear the bowling thunder", "Strike it up with the gang."]
-    $('#message').text(messages[Math.floor(Math.random() * messages.length)])
-    game.addRoll(10)
-    updateScores()
-  })
 })

2fe44a2c061ceb1c7cb5da562132443f13479353
Tom Grand <thomas.a.grand@gmail.com>
2018-02-11 19:22:46 +0000

    tidy up format.js

--- src/format.js
+++ src/format.js
@@ -10,34 +10,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const individualScores = () => {
     let collumnID = 1;
-    let scores = game.runningScores()
+    const scores = game.runningScores();
     scores.forEach((score) => {
       document.getElementById(`scoreFrame${collumnID}`).innerHTML = `${score}`;
       collumnID += 1;
     });
   };
 
-// this logic should be in model not in the formatting page...
+  // this logic should be in model not in the formatting page...
 
-  const strikeConverter = (rolls) => {
-    return rolls.map((roll) => (roll === STRIKE) ? STRIKE_SYMBOL : roll);
-  }
+  const strikeConverter = rolls => rolls.map(roll => (roll === STRIKE ? STRIKE_SYMBOL : roll));
 
   const spareConverter = (rolls) => {
     if (rolls[0] + rolls[1] === STRIKE) { rolls[1] = SPARE_SYMBOL; }
     if (rolls[1] + rolls[2] === STRIKE) { rolls[2] = SPARE_SYMBOL; }
-    return rolls
-  }
+    return rolls;
+  };
 
-  const rollPrinter = (game) =>  {
-    return game.map((frame) => {
-      let view = strikeConverter(frame);
-      view = spareConverter(view);
-      return view.join('')
-    })
-  }
+  const rollPrinter = game => game.map((frame) => {
+    let view = strikeConverter(frame);
+    view = spareConverter(view);
+    return view.join('');
+  });
 
-// this logic is to be refactored out
+  // this logic is to be refactored out
 
   const showRolls = () => {
     let collumnID = 1;
@@ -51,34 +47,33 @@ document.addEventListener('DOMContentLoaded', () => {
   const roll = (value) => { game.play(value); setTotal(); individualScores(); showRolls(); };
 
   const rollButton = (pins) => {
-    let btn = document.createElement('button');
+    const btn = document.createElement('button');
     btn.id = pins;
     btn.onclick = () => { roll(pins); };
     btn.innerHTML = pins;
-    return btn
-  }
+    return btn;
+  };
 
   const showScoreButtons = (max = STRIKE) => {
     for (let i = 0; i <= max; i += 1) {
-      let btn = rollButton(i);
+      const btn = rollButton(i);
       document.getElementById('changeScores').appendChild(btn);
     }
   };
 
 
-const createFieldWithID = (id) => {
+  const createFieldWithID = (id) => {
     const field = document.createElement('td');
     field.id = id;
     return field;
-}
+  };
 
-// wow look how long this method is...
   const createScoreCard = () => {
     for (let i = 1; i <= STRIKE; i += 1) {
-      const title = document.createElement('td');
-      const scoreBox = createFieldWithID(`scoreFrame${i}`);
-      const frameRolls = createFieldWithID(`views${i}`);
+      let title = document.createElement('td');
       title.innerHTML = `frame ${i}`;
+      let scoreBox = createFieldWithID(`scoreFrame${i}`);
+      let frameRolls = createFieldWithID(`views${i}`);
       document.getElementById('scorecardLabels').appendChild(title);
       document.getElementById('scoreCardScores').appendChild(scoreBox);
       document.getElementById('frameResults').appendChild(frameRolls);

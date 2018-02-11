e47f97dd39742a8075208ef1958daf4b2d2e36a6
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-11 14:53:27 +0000

    tested green for various combinations of spares and strikes. At this stage logic is working fine.

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,38 +7,94 @@ describe('game', function () {
   });
 
   describe('Strike or Spare', function () {
-    it('Should return value of the score', function () {
-      expect(game.playerRolls(10)).toEqual(10);
+    it('Should increment frame counter by 1', function () {
+      game.pinsDroppedEachRoll(3);
+      expect(game.frameCounter).toEqual(1);
     });
 
     it('Should return true for a Strike', function () {
-      game.playerRolls(10);
+      expect(game.isStrike(10)).toBeTruthy();
+    });
 
-      expect(function functionName() {game.isStrike();} ).toBeTruthy();
+    it('Score array should contain 10 and 0 for frame one - after strike', function () {
+      game.pinsDroppedEachRoll(10);
+
+      expect(game.scoreArray).toEqual([10,0]);
     });
 
     it('Should return true if a Spare', function () {
-      game.playerRolls(4);
-      game.playerRolls(6);
+      game.pinsDroppedEachRoll(4);
 
-      expect(function functionName() {game.isSpare();} ).toBeTruthy();
+      expect(game.isSpare(6)).toBeTruthy();
     });
+
+    it('Neither spare or strike with 4 and 5 rolls', function () {
+      game.pinsDroppedEachRoll(4);
+
+      expect(game.isSpare(5)).toBeFalsy();
+    });
+
+    it('Score array should contain 4 and 5 rolls', function () {
+      game.pinsDroppedEachRoll(4);
+      game.pinsDroppedEachRoll(5);
+
+      expect(game.scoreArray).toEqual([4,5]);
+    });
+
   });
 
-  describe('Should give correct score for first frame', function () {
-    it('Return total score for 4 rolls', function () {
-      game.playerRolls(10);
-      game.playerRolls(10);
-      game.playerRolls(7);
-      game.playerRolls(2);
-      game.playerRolls(5);
-      game.playerRolls(5);
+  describe('Total score after each frame', function () {
+    it('Return total score for 3 frames: 4 and 5 rolls', function () {
+      game.startGame(5);
+      game.startGame(4);
 
+      game.startGame(5);
+      game.startGame(4);
 
-      console.log(game.finalScore);
-      console.log(game.scoreArray);
+      game.startGame(5);
+      game.startGame(4);
 
-      expect(game.scorePerFrame(1)).toEqual(28);
+      expect(game.finalScore).toEqual(27);
+    });
+
+    it('Return total score for 2 frames: strike followed by 4 and 5 rolls', function () {
+      game.startGame(10);
+      game.startGame(4);
+      game.startGame(5);
+
+      expect(game.finalScore).toEqual(28);
+    });
+
+    it('Return total score for 3 frames: 2 strikes followed by 4 and 4 rolls', function () {
+      game.startGame(10);
+      game.startGame(10);
+      game.startGame(4);
+      game.startGame(4);
+
+      expect(game.finalScore).toEqual(50);
+    });
+
+    it('Return total score for 3 strikes followed by: 3,4 and 5,2 rolls', function () {
+      game.startGame(10);
+      game.startGame(10);
+      game.startGame(10);
+      game.startGame(3);
+      game.startGame(4);
+      game.startGame(5);
+      game.startGame(2);
+      expect(game.finalScore).toEqual(84);
+    });
+
+    it('Return total score for spare (0,10) and a strike followed by: 3,4 and 5,2 rolls', function () {
+      game.startGame(0);
+      game.startGame(10);
+      game.startGame(10);
+      game.startGame(5);
+      game.startGame(2);
+      game.startGame(4);
+      game.startGame(1);
+      
+      expect(game.finalScore).toEqual(49);
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -1,80 +1,133 @@
 function Game() {
-  this.rollCounter = 0;
-  this.frameCounter = 1;
-  this.scoreArray = [];
-  this.finalScore = [];
-  this.pinsDropped = 0;
+  var self = this;
+
+  self.scoreArray = [];
+  self.screenArray = [];
+  self.frameCounter = 0;
+  self.finalScore = 0;
 }
 
-Game.prototype.playerRolls = function (rollscore) {
-  this.rollCounter += 1;
-  this.pinsDropped += rollscore;
-  this.isStrike();
-  this.isSpare();
+Game.prototype.startGame = function (pinsdropped) {
+  var self = this;
 
-  if (this.rollCounter % 2 === 0) {
-    this.frameCounter ++;
-    this.scoreArray.push(this.pinsDropped);
-    this.pinsDropped = 0;
-    this.addingScore();
+  self.pinsDroppedEachRoll(pinsdropped);
+
+  for (var i = 1; i < 11; i++) {
+    self.scoreTotal(i);
   }
-
-  //console.log(this.scoreArray);
-  return rollscore;
 };
 
-Game.prototype.isStrike = function () {
+Game.prototype.scoreTotal = function (frame) {
+  var self = this;
+  self.finalScore = 0;
 
-  if (this.pinsDropped === 10 && this.rollCounter % 2 !== 0) {
-    this.rollCounter += 1;
+    for (var r = 0; r < self.scoreArray.length; r+= 2) {
+      console.log('loop 2');
+      if (self.screenArray[r + 1] === 'X') {
+        self.finalScore += (10 + self.strikeBonus(r));
+
+      } else if (self.screenArray[r + 1] === '/') {
+        self.finalScore += (10 + self.spareBonus(r));
+
+      } else {
+        self.finalScore += self.frameTotal(r);
+      }
+    }
+
+  console.log(self.finalScore);
+  return self.finalScore;
+};
+
+Game.prototype.pinsDroppedEachRoll = function (pinsdropped) {
+  var self = this;
+
+
+  if (self.isStrike(pinsdropped)) {
+
+    self.scoreArray.push(10);
+    self.scoreArray.push(0);
+    self.frameCounter += 1;
+
+  } else if (self.isSpare(pinsdropped)) {
+
+    self.scoreArray.push(pinsdropped);
+
+  } else {
+
+    self.scoreArray.push(pinsdropped);
+    self.displayResult(pinsdropped);
+  }
+
+  self.frameCounter += 1;
+
+  console.log('score array');
+  console.log(self.scoreArray);
+};
+
+
+Game.prototype.isStrike = function (pinsdropped) {
+  var self = this;
+
+  if ( pinsdropped === 10 && self.frameCounter % 2 === 0 ) {
+    console.log('---------------->   STRIKE');
+
+    self.displayResult(0);
+    self.displayResult('X');
     return true;
   }
 };
 
-Game.prototype.isSpare = function () {
-  return this.playerRolls + this.scoreArray[this.scoreArray.length - 1] === 10;
-};
+Game.prototype.strikeBonus = function (index) {
+  var self = this;
 
-Game.prototype.tenthFrame = function () {
-  return this.frameCounter > 9;
-};
+  if (self.screenArray[index + 3] === 'X' && self.screenArray[index + 5] === 'X') {
 
-Game.prototype.addingScore = function () {
-console.log('addingScore');
-for (var i = 0; i < this.scoreArray.length - 1; i++) {
-  console.log('addingScore');
+    return 20;
 
-  if (isStrike()) {
-    console.log('if strike');
+  } else if (self.screenArray[index + 3] === 'X') {
 
-    if (this.scoreArray[i + 1] === 10 && this.scoreArray[i + 2] === 10) {
-      this.finalScore[i] = 30;
-
-    } else if (this.scoreArray[i + 1] === 10 && this.scoreArray[i + 2] < 10){
-      console.log('if spare');
-
-      this.finalScore[i] = 20 + this.scoreArray[i + 2];
-
-    }else {
-      console.log('if neither');
-
-      this.finalScore[i] = 10 + this.scoreArray[i + 1];
-    }
-
-  } else if (isSpare()) {
-
-    if (this.scoreArray[i + 1] <= 10 ) {
-
-      this.finalScore[i] = 10 + this.scoreArray[i + 1];
-    }
+    return (10 + self.scoreArray[index + 4]) || 0;
 
   } else {
-    this.finalScore[i] = this.scoreArray[i];
-  }
-}
 
-Game.prototype.scorePerFrame = function (frame) {
-  frameNo = frame - 1;
-  return this.finalScore[frameNo];
+    return self.scoreArray[index + 2] + self.scoreArray[index + 3] || 0;
+  }
+
 };
+
+Game.prototype.isSpare = function (pinsdropped) {
+  var self = this;
+
+  if (self.frameCounter % 2 !== 0) {
+
+    if (pinsdropped + self.scoreArray[self.frameCounter - 1] === 10) {
+      console.log('---------------->   SPARE');
+
+      self.displayResult('/');
+      return true;
+    }
+  }
+};
+
+Game.prototype.spareBonus = function (index) {
+  return this.scoreArray[index + 2] || 0;
+};
+
+Game.prototype.displayResult = function (displayresult) {
+  var self = this;
+
+  self.screenArray.push(displayresult);
+
+  console.log('screen array');
+  console.log(self.screenArray);
+};
+
+
+Game.prototype.tenthFrame = function () {
+  return this.frameCounter > 18;
+};
+
+
+Game.prototype.frameTotal = function (index) {
+  return this.scoreArray[index] + this.scoreArray[index + 1] || 0;
 };

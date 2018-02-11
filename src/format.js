931d328c4e512753028ef4e7f93710ece065feb5
Tom Grand <thomas.a.grand@gmail.com>
2018-02-11 17:57:38 +0000

    refactor format.js

--- src/format.js
+++ src/format.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
+// this logic should be in model not in the formatting page...
+
   const strikeConverter = (rolls) => {
     return rolls.map((roll) => (roll === STRIKE) ? STRIKE_SYMBOL : roll);
   }
@@ -35,6 +37,8 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
+// this logic is to be refactored out
+
   const showRolls = () => {
     let collumnID = 1;
     const frames = rollPrinter(game.view());
@@ -46,31 +50,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const roll = (value) => { game.play(value); setTotal(); individualScores(); showRolls(); };
 
-  const createButtons = (max = STRIKE) => {
+  const rollButton = (pins) => {
+    let btn = document.createElement('button');
+    btn.id = pins;
+    btn.onclick = () => { roll(pins); };
+    btn.innerHTML = pins;
+    return btn
+  }
+
+  const showScoreButtons = (max = STRIKE) => {
     for (let i = 0; i <= max; i += 1) {
-      const btn = document.createElement('button');
-      btn.id = i;
-      btn.onclick = () => { roll(i); };
-      const t = document.createTextNode(i);
-      btn.appendChild(t);
+      let btn = rollButton(i);
       document.getElementById('changeScores').appendChild(btn);
     }
   };
 
 
+const createFieldWithID = (id) => {
+    const field = document.createElement('td');
+    field.id = id;
+    return field;
+}
+
 // wow look how long this method is...
   const createScoreCard = () => {
     for (let i = 1; i <= STRIKE; i += 1) {
       const title = document.createElement('td');
-      const frame = document.createTextNode(`frame ${i}`);
-      const scoreBox = document.createElement('td');
-      const frameRolls = document.createElement('td');
-      scoreBox.id = `scoreFrame${i}`;
-      frameRolls.id = `views${i}`;
-      const filler = 'waiting for score';
-      title.appendChild(frame);
-      scoreBox.innerHTML = filler;
-      frameRolls.innerHTML = 'hi';
+      const scoreBox = createFieldWithID(`scoreFrame${i}`);
+      const frameRolls = createFieldWithID(`views${i}`);
+      title.innerHTML = `frame ${i}`;
       document.getElementById('scorecardLabels').appendChild(title);
       document.getElementById('scoreCardScores').appendChild(scoreBox);
       document.getElementById('frameResults').appendChild(frameRolls);
@@ -79,7 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
   setTotal();
-  createButtons();
+  showScoreButtons();
   createScoreCard();
   individualScores();
   showRolls();

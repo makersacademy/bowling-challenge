7ed05766a2f1b0ec0ab4dd9e5ed14c78fe7719d3
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-02-12 11:38:01 +0000

    hide pin buttons

--- src/GameJQuery.js
+++ src/GameJQuery.js
@@ -14,102 +14,128 @@ $( document ).ready(function() {
   //   };
 
   $( "#b0" ).click(function() {
-      var arrayFrame = game.currentFrame-1
       var frame = game.currentFrame
+      var arrayFrame = frame-1
       var roll = (game.frames[arrayFrame].isRoll)
       game.roll(0)
-      $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      console.log(game.frames[arrayFrame].bowls[roll-1])
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
       displaysScore(roll,frame)
+      if (roll === 1) {
+        for (var i = 10; i > (10-firstRoll); i--)
+        $("#b" + i).hide();
+      }
     });
 
   $( "#b1" ).click(function() {
-      var arrayFrame = game.currentFrame-1
       var frame = game.currentFrame
+      var arrayFrame = frame-1
       var roll = (game.frames[arrayFrame].isRoll)
       game.roll(1)
-      $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
       displaysScore(roll,frame)
+      if (roll === 1) {
+        for (var i = 10; i > (10-firstRoll); i--)
+        $("#b" + i).hide();
+      }
     });
 
   $( "#b2" ).click(function() {
-      var arrayFrame = game.currentFrame-1
       var frame = game.currentFrame
+      var arrayFrame = frame-1
       var roll = (game.frames[arrayFrame].isRoll)
       game.roll(2)
-      $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-      displaysScore(roll,frame)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
     });
 
   $( "#b3" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(3)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
-  });
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(3)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
+    });
 
   $( "#b4" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(4)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
-  });
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(4)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
+    });
 
   $( "#b5" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(5)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
-  });
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(5)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
+    });
 
   $( "#b6" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(6)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
-  });
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(6)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)    });
 
   $( "#b7" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(7)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(7)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
   });
 
   $( "#b8" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(8)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(8)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
   });
 
   $( "#b9" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(9)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    displaysScore(roll,frame)
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(9)
+      var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+        displaysScore(roll,frame)
+        hidePins(roll,firstRoll)
   });
 
   $( "#b10" ).click(function() {
-    var arrayFrame = game.currentFrame-1
-    var frame = game.currentFrame
-    var roll = (game.frames[arrayFrame].isRoll)
-    game.roll(10)
-    $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
-    afterFrame(frame);
+      var frame = game.currentFrame
+      var arrayFrame = frame-1
+      var roll = (game.frames[arrayFrame].isRoll)
+      game.roll(10)
+      $( "#r" + (arrayFrame + 1) + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+      afterFrame(frame);
   });
 
       //   $('#roll-form').submit(function(evt) {
@@ -131,17 +157,33 @@ $( document ).ready(function() {
         };
       };
 
-      function setVariables() {
-        var arrayFrame = game.currentFrame-1
-        var frame = game.currentFrame
-        var roll = (game.frames[arrayFrame].isRoll)
-      };
+      // function fillsInBowl() {
+      //   // var frame = game.currentFrame
+      //   // var arrayFrame = frame-1
+      //   // var roll = (game.frames[arrayFrame].isRoll)
+      //   // var firstRoll = game.frames[arrayFrame].bowls[roll-1]
+      //   console.log(game.frames[arrayFrame].bowls[roll-1])
+      //   $( "#r" + frame + roll ).text(game.frames[arrayFrame].bowls[roll-1]);
+      //   displaysScore(roll,frame)
+      //   hidePins(roll,firstRoll)
+      // };
 
       function displaysScore(roll,frame) {
         if (roll === 2) {
           afterFrame(frame);
         } else if (frame === 10 && roll === 3) {
           afterFrame(frame);
+        }
+      };
+
+      function hidePins(roll,firstRoll) {
+        if (roll === 1) {
+          for (var i = 10; i > (10-firstRoll); i--)
+          $("#b" + i).hide();
+        }
+        if (roll != 1) {
+          for (var i = 10; i >= 0; i--)
+          $("#b" + i).show();
         }
       };
 });

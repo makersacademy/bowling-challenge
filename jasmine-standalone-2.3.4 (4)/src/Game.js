d4f44b24354ccc14ada328bac0a505d9007d7dc4
kotpsot <zygmunt.justyna@gmail.com>
2018-02-24 15:33:54 +0000

    fix adding frames - all good :)

--- jasmine-standalone-2.3.4 (4)/src/Game.js
+++ jasmine-standalone-2.3.4 (4)/src/Game.js
@@ -5,10 +5,12 @@ function Game() {
 }
 
 Game.prototype._addFrame = function() {
-  if (this.frames.length === 0 || this.frames.length === 1 ) {
+  if (this.frames.length === 20) {
+      this.frames.push(10);
+  } else if (this.frames.length === 0 || this.frames.length === 1 ) {
     this.frames.push(1);
   } else if (this.frames[this.frames.length-1] !== this.frames[this.frames.length-2]) {
-    this.frames.push(this.frames.length-1);
+    this.frames.push(this.frames[this.frames.length-1]);
   } else if (this.frames[this.frames.length-1] === this.frames[this.frames.length-2]) {
     this.frames.push(this.frames[this.frames.length-1]+1);
   }

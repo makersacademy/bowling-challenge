c992a02e3ccae763490f716f0f630857cff56071
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-24 16:08:52 +0000

    Can calculate strike score based on 2 strikes in a row and 1 in a row

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,19 +7,53 @@ describe('Game', function(){
     game.allFrames = [[4,5],[5,4]];
     game2 = new Game();
     game2.allFrames = [[4,5],[5,4],[4,6],[3,5]];
+    game3 = new Game();
+    game3.allFrames = [[5,5],[5,4],[4,6],[3,5]];
+    game4 = new Game();
+    game4.allFrames = [[5,3],[10,0],[4,3],[3,5]];
+    game5 = new Game();
+    game5.allFrames = [[10,0],[10,0],[4,3],[3,5]];
   });
 
   describe('#calculateBaseScore', function(){
     it('can add up the basic score of a 2, 2 roll frames', function() {
       game.calculateBaseScore();
-      expect(game.currentBaseScore).toEqual(18);
+      expect(game.baseScore).toEqual(18);
     });
   });
 
   describe('#calculateSpareScore', function() {
     it('can calculate the bonus score of spares over 4 frames', function() {
       game2.calculateSpareScore();
-      expect(game2.currentSpareScore).toEqual(3);
+      expect(game2.spareScore).toEqual(3);
+    });
+
+    it('can calculate the bonus score of spares over 4 frames', function() {
+      game3.calculateSpareScore();
+      expect(game3.spareScore).toEqual(8);
+    });
+  });
+
+  describe('#calculateTotalScore', function(){
+    it('can add up the basic and spare score of game', function() {
+      game3.calculateTotalScore();
+      expect(game3.totalScore).toEqual(45);
+    });
+  });
+
+  describe('#calculateStrikeScore', function(){
+    it('can calculate a strike score where next roll is not another strike', function() {
+      game4.calculateStrikeScore();
+      expect(game4.strikeScore).toEqual(7);
+    });
+
+    it('can add up the strike score when next roll is also a strike', function() {
+      console.log(game5.strikeScore);
+      console.log(game5);
+      game5.calculateStrikeScore();
+      console.log(game5);
+      console.log(game5.strikeScore);
+      expect(game5.strikeScore).toEqual(21);
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -1,20 +1,21 @@
 var Game = function() {
   this.allFrames =[];
-  this.currentBaseScore = 0;
-  this.currentSpareScore = 0;
+  this.baseScore = 0;
+  this.spareScore = 0;
+  this.totalScore = 0;
+  this.strikeScore = 0;
 };
 
-console.log(this.currentScore);
-
-
 Game.prototype.calculateTotalScore = function() {
-  calculateBaseScore();
+  this.calculateBaseScore();
+  this.calculateSpareScore();
+  this.totalScore = this.baseScore + this.spareScore;
 };
 
 Game.prototype.calculateBaseScore = function() {
   for( var frame of this.allFrames) {
     for( var roll of frame) {
-      this.currentBaseScore += roll;
+      this.baseScore += roll;
     }
   }
 };
@@ -23,13 +24,35 @@ Game.prototype.calculateSpareScore = function() {
   for( var frameIndex = 0, len = this.allFrames.length; frameIndex < len; frameIndex++) {
     var currentFrame = this.allFrames[frameIndex];
     var currentFrameScore = this.frameScore(currentFrame);
-    var firstRollScore = currentFrame[0];
+    var firstRollScore = this.firstRollScore(currentFrame);
       if ((firstRollScore < 10) && (currentFrameScore === 10 )) {
-        this.currentSpareScore += this.allFrames[frameIndex+1][0];
+        this.spareScore += this.allFrames[frameIndex+1][0];
+      }
+  }
+};
+
+Game.prototype.calculateStrikeScore = function() {
+  for( var frameIndex = 0, len = this.allFrames.length; frameIndex < len; frameIndex++) {
+    var currentFrame = this.allFrames[frameIndex];
+    var nextFrame = this.allFrames[frameIndex + 1];
+    var firstRollScore = this.firstRollScore(currentFrame);
+      if (firstRollScore === 10) {
+        var nextRollScore = this.firstRollScore(nextFrame);
+        if (nextRollScore === 10) {
+          var frameAfterNext = this.allFrames[frameIndex + 2];
+          this.strikeScore += (nextRollScore + (this.firstRollScore(frameAfterNext)));
+        }
+        else {
+          this.strikeScore += (this.frameScore(nextFrame));
+        }
       }
   }
 };
 
 Game.prototype.frameScore = function(frame) {
   return frame[0]+frame[1];
+};
+
+Game.prototype.firstRollScore = function(frame) {
+  return frame[0];
 };

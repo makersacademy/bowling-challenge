2531a515c790c11b897a84d137716f7947ceb01a
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-24 15:06:17 +0000

    Can calculated spare score of a simple frame

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,13 +4,22 @@ describe('Game', function(){
 
   beforeEach(function(){
     game = new Game();
-    game.allFrames = [[4,5],[5,6]];
+    game.allFrames = [[4,5],[5,4]];
+    game2 = new Game();
+    game2.allFrames = [[4,5],[5,4],[4,6],[3,5]];
   });
 
-  describe('#calculateScore', function(){
+  describe('#calculateBaseScore', function(){
     it('can add up the basic score of a 2, 2 roll frames', function() {
-      game.calculateScore();
-      expect(game.currentScore).toEqual(20);
+      game.calculateBaseScore();
+      expect(game.currentBaseScore).toEqual(18);
+    });
+  });
+
+  describe('#calculateSpareScore', function() {
+    it('can calculate the bonus score of spares over 4 frames', function() {
+      game2.calculateSpareScore();
+      expect(game2.currentSpareScore).toEqual(3);
     });
   });
 
--- src/game.js
+++ src/game.js
@@ -1,16 +1,35 @@
 var Game = function() {
   this.allFrames =[];
-  this.currentScore = 0;
+  this.currentBaseScore = 0;
+  this.currentSpareScore = 0;
 };
 
 console.log(this.currentScore);
 
 
-Game.prototype.calculateScore = function() {
-  score2 = 0;
+Game.prototype.calculateTotalScore = function() {
+  calculateBaseScore();
+};
+
+Game.prototype.calculateBaseScore = function() {
   for( var frame of this.allFrames) {
     for( var roll of frame) {
-      this.currentScore += roll;
+      this.currentBaseScore += roll;
     }
   }
+};
+
+Game.prototype.calculateSpareScore = function() {
+  for( var frameIndex = 0, len = this.allFrames.length; frameIndex < len; frameIndex++) {
+    var currentFrame = this.allFrames[frameIndex];
+    var currentFrameScore = this.frameScore(currentFrame);
+    var firstRollScore = currentFrame[0];
+      if ((firstRollScore < 10) && (currentFrameScore === 10 )) {
+        this.currentSpareScore += this.allFrames[frameIndex+1][0];
+      }
+  }
+};
+
+Game.prototype.frameScore = function(frame) {
+  return frame[0]+frame[1];
 };

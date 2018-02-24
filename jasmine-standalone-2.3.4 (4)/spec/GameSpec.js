7d6e23326ba2500306da80494d46b66846e18538
kotpsot <zygmunt.justyna@gmail.com>
2018-02-24 13:25:21 +0000

    refactor tests - add before

--- jasmine-standalone-2.3.4 (4)/spec/GameSpec.js
+++ jasmine-standalone-2.3.4 (4)/spec/GameSpec.js
@@ -5,49 +5,44 @@ describe("Game", function() {
   beforeEach(function() {
     game = new Game();
     player = new Player();
-  });
-
-  it("has an empty array of rolls at the beginning", function() {
-    expect(game.scores).toEqual([]);
-  });
-
-  it("has empty array of frames at the beginning", function() {
-    expect(game.frames).toEqual([]);
-  });
-
-  it("adds rolls to rolls", function() {
     player.newGame(game);
-    player.roll(5);
-    player.roll(6);
-    expect(game.scores).toEqual([5, 6]);
   });
 
-  it("adds frames to frames", function() {
-    player.newGame(game);
-    player.roll(5);
-    player.roll(6);
-    player.roll(7);
-    player.roll(7);
-    player.roll(7);
-    expect(game.frames).toEqual([1, 1, 2, 2, 3]);
+  describe("beginning of the game", function() {
+
+    it("has an empty array of rolls at the beginning", function() {
+      expect(game.scores).toEqual([]);
+    });
+
+    it("has empty array of frames at the beginning", function() {
+      expect(game.frames).toEqual([]);
+    });
   });
 
-  it("adds hashes to hashes", function() {
-    player.newGame(game);
-    player.roll(5);
-    player.roll(6);
-    player.roll(7);
-    expect(game.pairs).toEqual([{1:5}, {1:6}, {2:7}])
-  });
+  describe("many-rolls game", function() {
 
-  it("returns last frame", function() {
-    player.newGame(game);
-    player.roll(5);
-    player.roll(6);
-    player.roll(7);
-    player.roll(7);
-    player.roll(7);
-    expect(game._currentFrame()).toEqual(3);
+    beforeEach(function() {
+      player.roll(5);
+      player.roll(6);
+      player.roll(7);
+      player.roll(8);
+    });
+
+    it("adds rolls to rolls", function() {
+      expect(game.scores).toEqual([5, 6, 7, 8]);
+    });
+
+    it("adds frames to frames", function() {
+      expect(game.frames).toEqual([1, 1, 2, 2]);
+    });
+
+    it("adds hashes to hashes", function() {
+      expect(game.pairs).toEqual([{1:5}, {1:6}, {2:7}, {2:8}])
+    });
+
+    it("returns last frame", function() {
+      expect(game._currentFrame()).toEqual(2);
+    });
   });
 
 });

16529865e24b992c74cb06e50d20312efee700db
Kaari Strack <kaari.strack@gmail.com>
2018-02-24 18:34:52 +0000

    added helper methods for spare and normal scores

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -11,13 +11,27 @@ BowlingGame.prototype.roll = function(pins) {
 BowlingGame.prototype.score = function() {
   var result = 0;
   var rollIndex = 0;
+  var game = this;
+
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (this.rollArray[rollIndex] + this.rollArray[rollIndex + 1] == 10) {
-      result += this.rollArray[rollIndex] + this.rollArray[rollIndex + 1] + this.rollArray[rollIndex + 2];
+    if (isSpare()) {
+      result += getSpareScore();
     } else {
-    result += this.rollArray[rollIndex] + this.rollArray[rollIndex + 1];
+      result += getNormalScore();
     }
     rollIndex += 2;
   }
   return result;
+
+  function isSpare() {
+    return game.rollArray[rollIndex] + game.rollArray[rollIndex + 1] === 10;
+  }
+
+  function getSpareScore() {
+    return game.rollArray[rollIndex] + game.rollArray[rollIndex + 1] + game.rollArray[rollIndex + 2];
+  }
+
+  function getNormalScore() {
+    return game.rollArray[rollIndex] + game.rollArray[rollIndex + 1];
+  }
 };

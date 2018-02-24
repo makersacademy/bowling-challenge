6ad8d004da5d6b67c6a725f19e0eec39f65dcc1a
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-24 23:59:56 +0000

    now can store all rolls of current frame, also checking if roll is legit

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -15,5 +15,19 @@ describe('Frame', () => {
     it('it does a roll and the returned value is the number of pins knocked', () => {
       expect(frame.roll(6)).toEqual(6);
     });
+
+    it('it throws an error if player tries to roll more than twice', () => {
+      frame.roll(4);
+      frame.roll(3);
+
+      expect(() => { frame.roll(6); }).toThrow('You already rolled twice. Start next frame!');
+    });
+
+    it('it checks if both rolls are stored for the current frame', () => {
+      frame.roll(4);
+      frame.roll(3);
+
+      expect(frame.rolls).toEqual([{ roll: 4 }, { roll: 3 }]);
+    });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -4,8 +4,22 @@
 
 function Frame() {
   this.pins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  this.rollCounter = 0;
+  this.rolls = [];
 }
 
 Frame.prototype.roll = function (pinsKnocked) {
+  this._isRollLegit();
+
+  this.rolls.push({ roll: parseInt(`${pinsKnocked}`) });
+
   return pinsKnocked;
+};
+
+Frame.prototype._isRollLegit = function () {
+  this.rollCounter += 1;
+
+  if (this.rollCounter >= 3) {
+    throw 'You already rolled twice. Start next frame!';
+  }
 };

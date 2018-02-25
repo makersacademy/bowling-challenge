f0fde0a2564f1c460118c5781f7db17835272265
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-25 16:43:05 +0000

    now calculating total points scored. No bonuses yet

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,6 +5,8 @@ describe('Game', () => {
   beforeEach(() => {
     game = new Game();
     frame = jasmine.createSpyObj('frame', ['rolls']);
+
+    frame.rolls.and.callFake(() => ([{ roll: 3 }, { roll: 3 }]));
   });
 
   describe('#initialize', () => {
@@ -17,13 +19,19 @@ describe('Game', () => {
     // This is not the whole object... just the rolls of the object
     // This is like passing frame.rolls though, but 'expected' behaviour
     it('it stores Frame rolls in the Game instance', () => {
-      frame.rolls.and.callFake(() => {
-        return ([{ roll: 3 }, { roll: 3 }]);
-      });
-
       game.addFrame(frame.rolls());
 
       expect(game.frames).toEqual([[{ roll: 3 }, { roll: 3 }]]);
+    });
+  });
+
+  describe('#finalScore', () => {
+    it('it calculates the total scores of the game', () => {
+      for (let i = 0; i < 10; i++) {
+        game.addFrame(frame.rolls());
+      }
+
+      expect(game.finalScore(game.frames)).toEqual(60);
     });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,16 @@
 function Game() {
   this.frames = [];
+  this.totalScore = 0;
 }
 
 Game.prototype.addFrame = function addFrame(frame) {
   this.frames.push(frame);
+};
+
+Game.prototype.finalScore = function finalScore(frames) {
+  frames.forEach((frame) => {
+    this.totalScore += frame[0].roll + frame[1].roll;
+  });
+
+  return this.totalScore;
 };

0181f012853e55133d84cdacdeb37be825350b61
Serena Hathi <hathi.serena@gmail.com>
2018-02-25 14:12:36 +0000

    Calculates spare score correctly

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,35 +5,41 @@ describe("game", function() {
   });  
 
   describe("before a new game", function() {
-    it("creates an array of a new empty score card", function() {
+    it("creates an empty array to hold scores", function() {
       expect(game.scoreCard).toEqual([]);
     });
 
-   it("creates an empty array to store frames", function(){
-    expect(game.frame).toEqual([]);
+   it("has a current score of 0", function(){
+    expect(game.result).toEqual(0);
    });
   });
 
-  describe("rolling", function(){
-    it("allows player to roll and saves score in array", function(){
-      game.roll(3);
-      expect(game.scoreCard).toEqual([3]);
-    });
+  describe("calculates score for", function(){
 
-    it("can roll a gutter game", function() {
-      for (var i = 0; i < 20; i++) {
-        game.roll(0);
-      }
+    it( "a gutter game", function() {
+        rollMany(0, 20);
       expect(game.score()).toEqual(0); 
     });
   
-    it("can roll a game of all ones", function(){
-      for (var i = 0; i < 20; i++) {
-        game.roll(1);
-      }
+    it("a game of all ones", function(){
+        rollMany(1, 20);
       expect(game.score()).toEqual(20); 
     });
+
+    it("a spare", function(){
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      rollMany(0, 17);
+      expect(game.score()).toEqual(16);
+    });
+
   });
+
+// helper method
+  var rollMany = function(pins, rolls) {
+    for (var i = 0; i < 20; i++) {
+      game.roll(pins);
+    }
+  };
 });
-
-
--- src/game.js
+++ src/game.js
@@ -1,21 +1,29 @@
 var Game = function() {
   this.scoreCard = [];
   this.frame = [];
-  this.currentScore = 0
+  this.result = 0;
 };
 
 Game.prototype = {
 
   roll: function(pins) {
     this.scoreCard.push(pins); 
-  },
+  }, 
 
   score: function() {
-    var result = 0;
-    for (var i = 0; i < 20; i++){
-      result += this.scoreCard[i];
+    var rollNumber = 0;
+    // Iterate through 10 frames and add one to frame number
+    for (var frameNumber = 0; frameNumber < 10; frameNumber++){
+      // If 10 points scored in roll 1 + roll 2 (a spare), the score is going to be 10 + the result of the first roll of the next frame
+      if (this.scoreCard[rollNumber] + this.scoreCard[rollNumber +1] === 10) {
+        this.result += this.scoreCard[rollNumber] + this.scoreCard[rollNumber +1] + this.scoreCard[rollNumber +2];
+      // If the result of roll 1 + 2 is less than 10, then simply add the two together
+      } else {
+      this.result += this.scoreCard[rollNumber] + this.scoreCard[rollNumber +1];
     }
-    return result;
+    rollNumber += 2;
   }
+  return this.result;  
+}
 };
 

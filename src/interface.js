9e4d3e8497854c6ba4386d08aecdd6ee3b9725b6
Lewis Watson <lewwwiswatson@gmail.com>
2018-02-25 11:34:36 +0000

    Add full game functionaltiy to bowling.html

--- src/interface.js
+++ src/interface.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
   var bowling = new Bowling();
 
-  
-};
+  $('#score').click(function(){
+    var gameArr = $('#full-game').val().replace(/\s/,'').split(',').map(Number);
+    // console.log(bowling.score(gameArr));
+    $('#full-game').val('');
+    $('#final-score').text('Total score for that game: ' + bowling.score(gameArr));
+  });
+});

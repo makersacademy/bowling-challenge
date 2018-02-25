e22ac01a0f0afaf7ab3bda84e8fca714bd43a7ee
jose <jennymarin1989@hotmail.com>
2018-02-25 12:39:58 +0000

    Adding points and current roll in initialization of Bowlinggame

--- jasmine-standalone-3.0.0 (2)/src/Bowling.js
+++ jasmine-standalone-3.0.0 (2)/src/Bowling.js
@@ -1,11 +1,22 @@
-var Bowling = function (){};
+'use strict'
 
 
-Bowling.prototype.roll = function(pins){
-
+var Bowling = function (){
+  this.rolls = [];
+  this.current = 0;
+  this.points = 0;
 };
 
-Bowling.prototype.score = function(){
 
-  return 0;
-};
+Bowling.prototype = {
+  roll: function(pins){
+    this.rolls[this.current++] = pins;
+  },
+
+   score: function(){
+     for (var i = 0; i < this.rolls.length; i ++){
+       this.points += this.rolls[i];
+     }
+     return this.points;
+   },
+}

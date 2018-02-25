25e2b4fe4f51a114cba5f0ba1779ed622aa95ec6
Lewis Watson <lewwwiswatson@gmail.com>
2018-02-25 09:15:34 +0000

    refactor total functions into a single sum function

--- src/bowling.js
+++ src/bowling.js
@@ -6,47 +6,43 @@ var Bowling = function() {
 };
 
 Bowling.prototype = {
-  score: function(balls) {
-    this.createAllFramesArray(balls);
-    return this.calculateTotal();
+  score: function(scores) {
+    this.createAllFramesArray(scores);
+    return this.total(this.allFrames);
   },
 
-  createAllFramesArray: function(balls) {
+  createAllFramesArray: function(scores) {
     while (this.allFrames.length < 10) {
-      if (balls[0] === 10) {
-        this.calculateStrike(balls);
-      } else if (balls[0] + balls[1] === 10){
-        this.calculateSpare(balls);
+      if (scores[0] === 10) {
+        this.getStrike(scores);
+      } else if (scores[0] + scores[1] === 10){
+        this.getSpare(scores);
       } else {
-        this.frame = balls.splice(0,2);
+        this.frame = scores.splice(0,2);
       }
       this.addFrameToAllFramesArray();
     }
   },
 
-  calculateStrike: function(balls) {
-    this.frame = balls.splice(0, 1);
-    this.frame.push(balls[0], balls[1]);
+  getStrike: function(scores) {
+    this.frame = scores.splice(0, 1);
+    this.frame.push(scores[0], scores[1]);
   },
 
-  calculateSpare: function(balls) {
-    this.frame = balls.splice(0,2);
-    this.frame.push(balls[0]);
+  getSpare: function(scores) {
+    this.frame = scores.splice(0,2);
+    this.frame.push(scores[0]);
   },
 
   addFrameToAllFramesArray: function() {
-    this.allFrames.push(this.frame);
+    this.allFrames.push(this.total(this.frame));
     this.frame = 0;
   },
 
-  calculateTotal: function() {
-    var total = 0;
-    this.allFrames.forEach(function(frame) {
-      frame = frame.reduce(function (acc, curr) {
-        return acc + curr;
-      });
-      total += frame;
+  total: function(arr) {
+    arr = arr.reduce(function (acc, curr) {
+      return acc + curr;
     });
-    return total;
-  }
+    return arr;
+  },
 }

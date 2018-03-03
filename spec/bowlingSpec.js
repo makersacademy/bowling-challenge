2da698b2b04c06cfb97dea5059885b4a2933b3a7
reena <reena.sanjay.sharma@gmail.com>
2018-03-03 20:45:37 +0000

    added some more tests

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -26,15 +26,6 @@ describe ('Bowling', function() {
         expect(bowling.gameScore()).toBe(28);
     });
 
-    it("throws an error if pins passed are out of range", function() {
-        var error = "Pins should be between 0 and 10 (both inclusive); got 11 pins";
-        expect(function() { bowling.roll(11); }).toThrow(Error(error));
-    });
-
-    it("throws an error if pin passed is not a valid pin", function() {
-        expect(function() { bowling.roll("&"); }).toThrow(Error("Not a valid pin"));
-    });
-
     it("returns 30 for consecutive 2 strikes", function() {
         var testPins = [10, 10];
         testPins.forEach(element => {
@@ -58,12 +49,6 @@ describe ('Bowling', function() {
         expect(bowling.gameScore()).toEqual(0);
     });
 
-    it("gutter game when player never hits a pin",function() {
-        for (var i = 0; i < 20;i++) {
-            bowling.roll(0);
-        }
-        expect(bowling.gameScore()).toEqual(0);
-    });
 
     it("should give correct score for a no-strike and no-spare game", function() {
         for (var i = 0; i < 20; i++) {
--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,9 +1,14 @@
 describe ("Frame", function() {
-    
-    var frame; 
+
+    var frame;
 
     beforeEach(function() {
         frame = new Frame();
+    });
+
+    it ("should return 10 if it isStrike", function() {
+        frame.addRoll(10);
+        expect(frame.toString()).toEqual("[10]");
     });
 
     it ("framescore returns 7 if roll 1 is 4 and roll 2 is 3", function() {
@@ -12,9 +17,19 @@ describe ("Frame", function() {
         expect(frame.frameScore()).toEqual(7);
     });
 
+    it ("should return isDone as true if frame is complete", function() {
+        frame.addRoll(4);
+        frame.addRoll(3);
+        expect(frame.isDone()).toEqual(true);
+    });
+
     it ("should return isStrike as true if first roll is 10", function() {
         frame.addRoll(10);
         expect(frame.isStrike()).toEqual(true);
+    });
+
+    it ("should return 10 if it isStrike", function() {
+        frame.addRoll(10);
         expect(frame.rollValues()).toEqual([10]);
     });
 
@@ -30,6 +45,12 @@ describe ("Frame", function() {
         expect(frame.rollValues()).toEqual([7,3]);
     });
 
+    it ("should return 10 as sum of(7,3) if it isSpare", function() {
+        frame.addRoll(7);
+        frame.addRoll(3);
+        expect(frame.rollValues()).toEqual([7,3]);
+    });
+
     it ("should return isSpare as false if sum of roll 1 & 2 of a frame is not 10", function() {
         frame.addRoll(6);
         frame.addRoll(3);
@@ -41,5 +62,14 @@ describe ("Frame", function() {
         frame.addRoll(2);
         expect(frame.rollValues()).toEqual([7,2]);
     });
-    
+
+    it("throws an error if pins passed are out of range", function() {
+        var error = "Pins should be between 0 and 10 (both inclusive); got 11 pins";
+        expect(function() { frame._validateRoll(11); }).toThrow(Error(error));
+    });
+
+    it("throws an error if pin passed is not a valid pin", function() {
+        expect(function() { frame._validateRoll("&"); }).toThrow(Error("Not a valid pin"));
+    });
+
 });

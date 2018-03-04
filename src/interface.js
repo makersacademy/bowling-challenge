3213debe08e6f600c24a0ac691ffbe8d99111ed4
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-04 15:39:25 +0000

    Frame scores outputting correctly. total score column added

--- src/interface.js
+++ src/interface.js
@@ -8,15 +8,16 @@ $(document).ready(function (){
 
   {$('.roll_scored').text('You rolled a: ' + pins);}
 
-  rollScores(game);
+  printScores(game);
   });
 });
 
-var rollScores = function(game) {
+var printScores = function(game) {
   for( var frameIndex = 0, len = game.allFrames.length; frameIndex < len; frameIndex++) {
     var currentFrame = game.allFrames[frameIndex];
     for( var rollNumber = 1, rollLen = (currentFrame.length + 1); rollNumber < rollLen; rollNumber++) {
       {$('#frame' + (frameIndex + 1) +'_roll'+ rollNumber).text(game.allFrames[frameIndex][rollNumber - 1]);}
     }
+    {$('#marker' + (frameIndex)).text(game.scoreCalculator.frameScores[frameIndex]);}
   }
 };

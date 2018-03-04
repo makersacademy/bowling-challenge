d25407a1dd9292b482280e62d38324320baf1053
marcusfgardiner <marcusfgardiner@gmail.com>
2018-03-04 15:12:48 +0000

    Correctly outputting roll scores into the scorecard:

--- src/interface.js
+++ src/interface.js
@@ -11,32 +11,8 @@ $(document).ready(function (){
   for( var frameIndex = 0, len = game.allFrames.length; frameIndex < len; frameIndex++) {
     var currentFrame = game.allFrames[frameIndex];
     for( var rollNumber = 1, rollLen = (currentFrame.length + 1); rollNumber < rollLen; rollNumber++) {
-      {$('#frame' + (frameIndex + 1) +'_roll'+ rollNumber).text(game.rollCount);}
+      {$('#frame' + (frameIndex + 1) +'_roll'+ rollNumber).text(game.allFrames[frameIndex][rollNumber - 1]);}
     }
   }
-
-  // if (game.rollCount === 2)
-  //   {$('#frame1_roll'+ '1').text(game.rollCount);}
   });
-
-//   $( '#temperature-up' ).click(function() {
-//   thermostat.upTempOne();
-//   $('#12345').text("Current temperature is: " + thermostat.temperature);
-// });
-
 });
-
-
-
-// $('header').text("Bowling game is ready");
-
-//   roll_score.visible = false;
-  // var show = false;
-// show = true;
-// roll_score.visible = true;
-//   // $('.roll_score').attr('class', 'visible_form');
-// // showFunction();
-// showFunction = function() {
-// if (show === true) {$('.roll_score').show();}
-// else {$('.roll_score').hide();}
-// };

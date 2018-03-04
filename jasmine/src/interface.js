f52e22bd45fc26613bb54bf218bb7ca7660c9f3b
jose <jennymarin1989@hotmail.com>
2018-03-04 17:47:04 +0000

    10 frames table with button to throw pins - failing some functionality with jquery

--- jasmine/src/interface.js
+++ jasmine/src/interface.js
@@ -3,38 +3,32 @@ window.onload = function() {
 };
 
 $(document).ready( function() {
-  // alert("WELCOME TO PIN BOWLING GAME!!");
   var bowling = new Bowling ();
+  var afteroll = function(){
+    for (var i = 1; i <= 21; i ++){
+      $('.roll' + i).text(bowling.roll());
+    };
+  };
 
-$('.rollpin').submit(function(e){
-  e.preventDefault();
-  var value = $('.enterpin').val();
-  console.log(value);
+  $('.pin1').click( function (){
+    var roll_button1 = $(this).val();
+    console.log(roll_button1);
+    bowling.roll(roll_button1);
+    afteroll();
 
-  $('.roll1').text(value);
-  $('.roll2').text(value);
+    $('.roll1').text(roll_button1);
 
+  })
 
+  $('.pin2').click( function (){
+    var roll_button2 = $(this).val();
+    console.log(roll_button2);
+    bowling.roll(roll_button2);
+    afteroll();
+    console.log(bowling.rolls);
 
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $('.roll2').text(roll_button2);
+  })
 
 
 

f57de0fd9474d3bdc1cf50ff12bb1aaf5aee597b
Terry Allitt <tallitt88@gmail.com>
2018-03-04 10:32:21 +0000

    add stylingsheet and jquery file

---
+++ src/jquery.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  const game = new Game();
+
+  $('#bowl').click(() => {
+    const pins = parseInt($('#pinScore').val());
+    updateBowlScore(pins);
+  });
+
+  function updateBowlScore(pins) {
+    $();
+  }
+});

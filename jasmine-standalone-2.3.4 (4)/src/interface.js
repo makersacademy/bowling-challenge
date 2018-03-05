5f528a74246bd47830a1eeb88185f40fb84087af
kotpsot <zygmunt.justyna@gmail.com>
2018-03-05 18:54:45 +0000

    fix typo in alert

--- jasmine-standalone-2.3.4 (4)/src/interface.js
+++ jasmine-standalone-2.3.4 (4)/src/interface.js
@@ -55,7 +55,7 @@ $( document ).ready(function() {
   }
 
   $('.about').click(function() {
-    alert("This app allows you to calculate a score of a classic 10-pins bowling game based upon your input.\n\n   * Click --New game-- to start counting;\n   * Click --Count final score-- to get the final result.\n\nThe basic score (ie. the number of pins knocked down) will be refreshed automatically each time you enter the number.\n\nMind the fact that the final score (basic score + bonuses for strikes and spares) will be available once the game is finished.\n\n ************************\n\nCopyright\n   * The whole code was written by me.\n   * The illustration in the header was been released on a CC licence by Vexels\n\n************************\n\nErrors\nIf you find any errors on this website, please do not hesitate to contact me on Github - contact details in the footer.")
+    alert("This app allows you to calculate a score of a classic 10-pins bowling game based upon your input.\n\n   * Click --New game-- to start counting;\n   * Click --Count final score-- to get the final result.\n\nThe basic score (ie. the number of pins knocked down) will be refreshed automatically each time you enter the number.\n\nMind the fact that the final score (basic score + bonuses for strikes and spares) will be available once the game is finished.\n\n ************************\n\nCopyright\n   * The whole code was written by @Kotauror.\n   * The illustration in the header has been released on a CC licence by Vexels\n\n************************\n\nErrors\nIf you find any errors on this website, please do not hesitate to contact me on Github - contact details in the footer.")
   });
 
   $('.new_game').click(function() {

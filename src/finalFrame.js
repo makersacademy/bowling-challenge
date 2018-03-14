911261d1b86feeb804cc6a5a15e0b90fd7f77e3e
Tom Grand <thomas.a.grand@gmail.com>
2018-03-14 09:22:34 +0000

    add final frame class

---
+++ src/finalFrame.js
@@ -0,0 +1,57 @@
+const STRIKE = 10;
+const STANDARD_ROUND = 2;
+const STRIKE_ROUND = 1;
+const STRIKE_SCORE_ROUND = 3;
+
+class Frame {
+  constructor(strikeRound = STRIKE_ROUND, spareRound = STANDARD_ROUND) {
+    this.rules = {
+      strike: { length: strikeRound, bonus: true },
+      spare: { length: spareRound, bonus: true },
+      normal: { length: STANDARD_ROUND, bonus: false },
+    };
+    this.rounds = [];
+    this.bonus = [];
+    this.frame = this.rules['normal'];
+  }
+
+  score() {
+    return this.rollsForScores().reduce((a, b) => a + b, 0);
+  }
+
+  roll(value) {
+    if (this.rounds.length < this.frame.length) { this.rounds.push(value); }
+    else if(this.bonus.length < STRIKE_SCORE_ROUND - this.frame.length) { this.bonus.push(value) }
+    this.resultType();
+  }
+
+  isFinished() {
+    return this.rounds.length === this.frame.length;
+  }
+
+  view() {
+    return this.rounds
+  }
+
+  // need to make private
+  resultType() {
+    let type = 'normal';
+    if (this.firstRoll() === STRIKE) { type = 'strike'; }
+    if (this.firstRoll() + this.secondRoll() === STRIKE) { type = 'spare'; }
+    this.frame = this.rules[type];
+  }
+
+  rollsForScores() {
+    return (this.frame.bonus) ? [...this.rounds, ...this.bonus] : this.rounds;
+  }
+
+  firstRoll() {
+    return this.rounds[0];
+  }
+
+  secondRoll() {
+    return this.rounds[1];
+  }
+}
+
+module.exports = Frame;

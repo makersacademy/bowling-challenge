395e790bc140b6e094b58c5037b74942eb2e622d
Raefe <raefe.nj@gmail.com>
2018-03-24 17:01:13 +0000

    changed scoring methods, and added test for 'gutter game'

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -12,5 +12,11 @@ describe('Scorecard', function(){
   it('adds the score', function(){
     scorecard.add(5)
     expect(scorecard.score()).toBe(5)
+  });
+  it('returns nil for a gutter game', function(){
+    for(var i = 0; i < 20; i++){
+      scorecard.add(0)
+    }
+    expect(scorecard.score()).toBe(0)
   })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,13 +1,18 @@
 'use strict';
 
 function Scorecard() {
-  this._score = 0;
+  this._score = [0];
 }
 
 Scorecard.prototype.score = function(){
-  return this._score
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return this._score.reduce(reducer)
 }
 
-Scorecard.prototype.add = function(number){
-  this._score += number
+Scorecard.prototype.add = function(frame_score){
+  this._score.push(frame_score)
+}
+
+Scorecard.prototype.frame = function(roll){
+  this._frame = 0
 }

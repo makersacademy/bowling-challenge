b229631c557f0a72e3757eb86dd03fec89441487
danielcampos <ddregalo@gmail.com>
2018-03-24 15:36:42 +0000

    Implement roll function in Game objet - test passed

--- spec/javascripts/game_spec.js
+++ spec/javascripts/game_spec.js
@@ -7,10 +7,32 @@ describe('Game', function() {
   });
 
   it('starts a game at frame 1', function() {
-    expect(game.frame).toEqual(1);
+    expect(game.currentFrame).toEqual(1);
   });
 
   it('starts a game at roll 1', function() {
     expect(game.currentRoll).toEqual(1);
+  });
+
+  it('starts each frame with 10 pins', function() {
+    expect(game.framePins).toEqual(10);
+  });
+
+  describe('roll', function() {
+    it('should decrese the framePins by number of pins', function() {
+      game.roll(3);
+      expect(game.framePins).toEqual(7);
+    });
+
+    it('should increase currentRoll by 1', function() {
+      game.roll(3);
+      expect(game.currentRoll).toEqual(2);
+    });
+
+    it('should increase currentFrame by 1 after second roll', function() {
+      game.roll(3);
+      game.roll(4);
+      expect(game.currentFrame).toEqual(2);
+    });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,4 +1,18 @@
 function Game() {
-  this.frame = 1
+  this.currentFrame = 1
   this.currentRoll = 1
+  this.framePins = 10
+  // var score = new Score();
+};
+
+Game.prototype.roll = function(pins) {
+  this.currentRoll ++
+  this.framePins -= pins
+  this._calcFrame();
+};
+
+Game.prototype._calcFrame = function () {
+  if (this.currentRoll == 2) {
+    this.currentFrame ++
+  };
 };

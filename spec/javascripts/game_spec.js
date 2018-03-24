f3cd6154984532e27158aab237e53dad166f2ea7
danielcampos <ddregalo@gmail.com>
2018-03-24 14:35:49 +0000

    Setup roll prperty in Game function, starts at roll 1 - test passed

--- spec/javascripts/game_spec.js
+++ spec/javascripts/game_spec.js
@@ -9,4 +9,8 @@ describe('Game', function() {
   it('starts a game at frame 1', function() {
     expect(game.frame).toEqual(1);
   });
+
+  it('starts a game at roll 1', function() {
+    expect(game.currentRoll).toEqual(1);
+  });
 });

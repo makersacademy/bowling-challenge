5faf13e6193df379304dd0bea6beebd522e7afc8
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-24 15:35:53 +0000

    added game, frame and round features and tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -5,7 +5,58 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("should have a inital score of 0", function() {
+  it("should have an inital score of 0", function() {
     expect(game._score).toEqual(0);
+  });
+});
+
+
+
+
+describe("Frame", function() {
+  var frame;
+  var round;
+
+  beforeEach(function() {
+    frame = new Frame();
+    // TODO How can I fake this and it's tests?
+    round = new Round();
+  });
+
+  it("should have an inital frame score of 0", function() {
+    expect(frame._score).toEqual(0);
+  });
+
+  it("should NOT be a last round frame unless specified", function() {
+    expect(frame._isLastRoundFrame).toEqual(false)
+
+    frame = new Frame(true);
+    expect(frame._isLastRoundFrame).toEqual(true);
+  });
+
+  it("should have two rounds if NOT a last round frame", function() {
+    expect(frame._rounds).toEqual([round, round])
+  });
+
+  it ("should have three rounds if frame is specified as a last round frame", function() {
+    frame = new Frame(true);
+
+    expect(frame._rounds).toEqual([round, round, round]);
+  });
+});
+
+
+
+
+
+describe("Round", function() {
+  var round;
+
+  beforeEach(function() {
+    round = new Round();
+  });
+
+  it("should have an inital score of 0", function() {
+    expect(round._score).toEqual(0);
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,13 @@
 function Game() {
   this._score = 0;
 };
+
+function Frame(lastRoundFrame = false, round = new Round()) {
+  this._score = 0;
+  this._isLastRoundFrame = lastRoundFrame;
+  this._rounds = lastRoundFrame ? [round, round, round] : [round, round];
+};
+
+function Round() {
+  this._score = 0;
+};

efd7774c8360ee5a54986a2f4fda1caba29e3515
danielcampos <ddregalo@gmail.com>
2018-03-24 14:29:52 +0000

    Add Game function with frame propoerty and set up test with jasmine

---
+++ spec/javascripts/game_spec.js
@@ -0,0 +1,12 @@
+describe('Game', function() {
+
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it('starts a game at frame 1', function() {
+    expect(game.frame).toEqual(1);
+  });
+});

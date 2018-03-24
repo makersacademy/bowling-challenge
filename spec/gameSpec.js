05057cf6fde8d5c25f53af93f66713c524d74444
antcin <ciniglioantonio@gmail.com>
2018-03-24 10:40:04 +0000

    Refactor: add helper to test

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,19 +9,21 @@ describe('Game', function(){
 
   describe('Gutter game', function(){
     it('rolls gutter game when players misses all pins', function(){
-      for (var i = 0; i < 20; i++){
-        game.roll(0)
-      }
+      rollMany(0, 20);
       expect(game.score()).toBe(0);
     });
   });
 
   describe('Player hits one pin every roll', function(){
     it('returns a score of 20 when player hits one pin per roll', function(){
-      for (var i = 0; i < 20; i++){
-        game.roll(1);
-      }
+      rollMany(1, 20);
       expect(game.score()).toBe(20);
     });
   });
+
+  var rollMany = function(pins, rolls) {
+    for (var i = 0; i < 20; i++){
+      game.roll(pins);
+    }
+  };
 });

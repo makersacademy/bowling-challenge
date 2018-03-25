8011713552e988a8537863877b470675da846366
Matt Burstein <j.matthew.burstein@gmail.com>
2018-03-25 18:28:50 +0100

    Rename some objects to improve clarity

--- images/How function spies work in JavaScript – CodeKraft_files/rate.php
+++
@@ -1,6 +0,0 @@
-PDRTJS_3529016_post_5447.avg_rating = 5;
-PDRTJS_3529016_post_5447.votes = 3;		
-PDRTJS_settings_3529016_post_5447= {			"type" : "stars",			"size" : "sml",			"star_color" : "yellow",			"custom_star" : "",			"font_size" : "",			"font_line_height" : "16px",			"font_color" : "",			"font_align" : "left",			"font_position" : "right",			"font_family" : "",			"font_bold" : "normal",			"font_italic" : "normal",			"text_vote" : "Vote",			"text_votes" : "Votes",			"text_rate_this" : "Rate This",			"text_1_star" : "Very Poor",			"text_2_star" : "Poor",			"text_3_star" : "Average",			"text_4_star" : "Good",			"text_5_star" : "Excellent",			"text_thank_you" : "Thank You",			"text_rate_up" : "Rate Up",			"text_rate_down" : "Rate Down"		};
-PDRTJS_3529016_post_5447.init();		
-PDRTJS_3529016_post_5447.token='d8cd235176c4dbbf2f24fc3e76de889c';
-/*3529016,_post_5447,wp-post-5447,3107316550,5-3*/

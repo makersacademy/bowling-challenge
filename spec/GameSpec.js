5c404761cd2a0e1587fe18524e5f3ad5caaafb2a
Somayeh <mail.somayeh@gmail.com>
2018-03-25 20:23:44 +0100

    check the set of rolls added to score, Gutter game and game of score one for all rolls

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,6 +8,14 @@ describe('Game', function() {
     game = new Game();
   });
 
+  it('start the game with current roll 0', function() {
+    expect(game.currentRoll).toEqual(0);
+  });
+
+  it('start the game with frame score 0', function() {
+    expect(game.frameScore).toEqual(0);
+  });
+
   it('total frame score when rolled twice in each frame', function() {
     game.roll(3, 5);
     expect(game.frameScore).toBe(8);
@@ -27,6 +35,32 @@ describe('Game', function() {
     game.roll(2, 4);
     game.roll(3, 1);
     expect(game.currentRoll).toEqual(4);
+  });
+
+  it('start the game with empty list of scores', function() {
+    expect(game.scoreArray).toEqual([]);
+  });
+
+  it('check the set of rolls are added to score array', function() {
+    game.roll(2, 6);
+    game.score();
+    expect(game.scoreArray.length).toEqual(2);
+  });
+
+  it('player can roll a Gutter game', function() {
+    for(var i = 0; i < 10 ; i++) {
+      game.roll(0, 0);
+      game.score();
+    }
+    expect(game.totalScore).toEqual(0);
+  });
+
+  it('player can score one for all rolls', function() {
+    for (var i = 0; i < 10 ; i++) {
+      game.roll(1, 1);
+      game.score();
+    }
+    expect(game.totalScore).toEqual(20);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -4,6 +4,7 @@ function Game() {
   this.totalScore = 0;
   this.frameScore = 0;
   this.currentRoll = 0;
+  this.scoreArray = [];
   this.roll1 = 0;
   this.roll2 = 0;
 
@@ -16,7 +17,8 @@ Game.prototype.roll = function(roll1, roll2) {
 }
 
 Game.prototype.score = function() {
-  this.totalScore += this.frameScore
+  this.totalScore += this.frameScore;
+  this.scoreArray.push(this.roll1, this.roll2);
 }
 
 Game.prototype.finalScore = function() {

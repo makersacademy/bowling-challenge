7398a8e2265b0f136de58d704cb1836e83e064cb
Vivian Allen <mrvivianallen@gmail.com>
2018-03-25 17:04:28 +0100

    game class knowing game is over after 10 frames, red

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -14,5 +14,11 @@ describe('Game', function(){
       for (var i = 1; i <= 5; i++) { game.addFrame(mockFrame) };
       expect(game.score()).toEqual(25);
     });
+    it('knows when the game is over', function(){
+      var mockFrame = 'frame';
+      var game = new Game;
+      for (var i = 1; i <= 10; i++) { game.addFrame(mockFrame) };
+      expect(game.isOver()).toEqual(true);
+    });
   });
 });

72f88888a919fa35b47b28de166070e693a18154
Vivian Allen <mrvivianallen@gmail.com>
2018-03-25 12:37:35 +0100

    frame class normal scoring only allows two rolls, green

--- src/frame.js
+++ src/frame.js
@@ -8,6 +8,9 @@ function Frame () {
 };
 
 Frame.prototype.roll = function(number) {
+  if (this._n != 10 && this._rolls.length === 2) {
+    return 'Only two rolls per frame until frame 10!';
+  }
   this._rolls.push(number);
 }
 

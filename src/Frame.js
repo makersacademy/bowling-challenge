99cb4b16235ee9fdcbf1b65c2795ec75d45b60d5
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-25 19:42:46 +0100

    Completed frame function and tests

--- src/Frame.js
+++ src/Frame.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const MAX_ROUNDS = 2;
+const MAX_PIN_COUNT = 10
 
 function Frame() {
   this._pins = [1,2,3,4,5,6,7,8,9,10];
   this._roundsPlayed = 0;
+  this._score = 0;
+  this._bonusFrame = false;
 };
 
 Frame.prototype.pinsKnockedOver = function(pinCount) {
+  if (pinCount > this._pins.length) {
+    throw new Error('Number of pins knocked over exceeds number of pins in frame');
+  };
   this._pins.splice(-pinCount, pinCount);
 };
 
@@ -19,10 +25,21 @@ Frame.prototype.isStrikeOnFirstRound = function() {
 };
 
 Frame.prototype.play = function(pinCount) {
-  if (this._roundsPlayed === 2) {
+  if (this._roundsPlayed === MAX_ROUNDS) {
     throw new Error('Max number of rounds played');
   } else {
-    this._roundsPlayed += 1;
     this.pinsKnockedOver(pinCount);
+    this.isBonusFrame();
+    this._roundsPlayed += 1;
+  };
+};
+
+Frame.prototype.score = function() {
+  return this._score = MAX_PIN_COUNT - this._pins.length
+};
+
+Frame.prototype.isBonusFrame = function() {
+  if (this._pins.length === 0) {
+    return this._bonusFrame = true;
   }
-}
+};

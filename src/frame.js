ade2324fa69d7770af434b8aaf5a7455de5f2970
Vivian Allen <mrvivianallen@gmail.com>
2018-03-25 12:50:49 +0100

    frame class spares logging feature, green

--- src/frame.js
+++ src/frame.js
@@ -8,12 +8,26 @@ function Frame () {
 };
 
 Frame.prototype.roll = function(number) {
-  if (this._n != 10 && this._rolls.length === 2) {
-    return 'Only two rolls per frame until frame 10!';
-  }
+  if (this.illegalRoll()) { return this.illegalRoll() }
   this._rolls.push(number);
+  this.specials();
 }
 
 Frame.prototype.score = function() {
   return this._rolls.reduce(function (sum, x) { return sum + x }, 0);
+}
+
+Frame.prototype.isSpare = function() {
+  return this._isSpare;
+}
+
+Frame.prototype.illegalRoll = function() {
+  if (this._n != 10 && this._rolls.length === 2) {
+    return 'Only two rolls per frame until frame 10!';
+  }
+  return false
+}
+
+Frame.prototype.specials = function() {
+  if (this._rolls.length === 2 && this.score() === 10) { this._isSpare = true };
 }

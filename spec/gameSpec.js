e4bb977c864c1ae724df02a45945be53f65eaedf
Vivian Allen <mrvivianallen@gmail.com>
2018-03-25 21:41:57 +0100

    frame numbering, red->green

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,11 +4,13 @@ describe('Game', function(){
   describe('a game with no specials', function(){
     it('initalises with a frame in play', function(){
       function MockFrame() {}
+      MockFrame.prototype.nSet = function(n) {};
       var game = new Game(MockFrame);
       expect(game.frames()[0] instanceof MockFrame).toEqual(true);
     });
     it('adds scores to the newest frame when playing', function(){
       function MockFrame() { this._score }
+      MockFrame.prototype.nSet = function(n) {};
       MockFrame.prototype.roll = function(number) {this._score = number};
       MockFrame.prototype.score = function() {return this._score};
       var game = new Game(MockFrame);
@@ -17,6 +19,7 @@ describe('Game', function(){
     });
     it('starts a new frame when the current one returns a response string', function(){
       function MockFrame () { this.i = 1; this.responses = [1, 2, 'a string'] }
+      MockFrame.prototype.nSet = function(n) {};
       MockFrame.prototype.roll = function(number) { this.i++; return this.responses[this.i-1]}
       var game = new Game(MockFrame);
       game.roll(1)
@@ -26,11 +29,26 @@ describe('Game', function(){
     });
     it('tallies the score', function(){
       function MockFrame() {}
+      MockFrame.prototype.nSet = function(n) {};
       MockFrame.prototype.roll = function() { };
       MockFrame.prototype.score = function() { return 5 };
       var game = new Game(MockFrame);
       game.roll(5);
       expect(game.score()).toEqual(5);
+    });
+    it('numbers frames', function(){
+        function MockFrame () {
+          this.i = 1; this.responses = [1, 2, 'a string'];
+          this._n;
+         }
+      MockFrame.prototype.nSet = function(n) {};
+      MockFrame.prototype.roll = function(number) { this.i++; return this.responses[this.i-1]}
+      MockFrame.prototype.nSet = function(n) { this._n = n };
+      MockFrame.prototype.n = function() { return this._n };
+      var game = new Game(MockFrame);
+      game.roll(1)
+      game.roll(1)
+      expect(game.frames()[1].n()).toEqual(2);
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,26 +1,29 @@
 'use strict';
 
 function Game (frameConstructor) {
-  this._frameConstructor = frameConstructor
-  this._frames = [new this._frameConstructor]
+  this._frameConstructor = frameConstructor;
+  this._frames = [new this._frameConstructor];
+  this._nFrames = 1;
+  this._frames[0].nSet(1);
 };
 
 //getters
 Game.prototype.frames = function(){ return this._frames };
-
-//setters
-
-//more complex stuff
 Game.prototype.score = function() {
   return this._frames.reduce(function(sum, frame) {
     return sum + frame.score()
   }, 0);
 }
+
+//setters
 Game.prototype.roll = function(number) {
-  if (typeof this._frames[this._frames.length-1].roll(number) === "string") {
+  if (typeof this._frames.slice(-1)[0].roll(number) === "string") {
     this._frames.push(new this._frameConstructor);
+    this._nFrames++;
+    this._frames.slice(-1)[0].nSet(this._nFrames);
   }
 }
+
 
 // checkers
 Game.prototype.isOver = function() { return this.frames().length >= 10 }

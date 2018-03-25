296954019217a5fff0f0a3b867fdb91f9b4e71b5
paosch <paola_schiaffino@yahoo.co.uk>
2018-03-25 21:13:17 +0100

    CalculateScore function needs fixing

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,19 +1,40 @@
-'use strict';
-
 describe('Bowling', function(){
   var bowling;
 
   beforeEach(function() {
     bowling = new Bowling();
+
   });
 
+
 it('zero points at the beginning of the game', function(){
-  expect(bowling.getPointsPerRoll()).toEqual([]);
+  expect(bowling.pointsPerRoll).toEqual([]);
 });
 
 it('gutter game', function(){
-  bowling.roll(0);
-  expect(bowling.getPointsPerRoll()).toEqual([0]);
+  for (var i = 0; i < 20; i ++) {
+    bowling.roll(0);
+  }
+  expect(bowling.pointsPerRoll).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  // expect(bowling.calculateScore()).toEqual([0]);
+
+  })
+
+
+it('player can score a strike', function(){
+  bowling.roll(10);
+  bowling.roll(3);
+  bowling.roll(5);
+  expect(bowling.pointsPerRoll).toEqual([10, 3, 5]);
+  // expect(bowling.calculateScore()).toEqual(18);
+
 })
+
+
+
+
+
+
+
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,16 +1,36 @@
-'use strict';
-
 function Bowling(){
   this.pointsPerRoll = []
 
 }
 
-Bowling.prototype.getPointsPerRoll = function(){
-  return this.pointsPerRoll;
+Bowling.prototype.roll = function(knockedPins){
+  this.pointsPerRoll.push(knockedPins);
+
 };
 
+Bowling.prototype.calculateScore = function(){
+  var points = 0;
+  var index = 0;
 
-Bowling.prototype.roll = function(pins){
-  this.pointsPerRoll.push(pins);
+  for (var i = 0; i < 10; i = i + 1){
+    if (this.pointsPerRoll[index] === 10) {
+      points += this.strikeBonus();
+      index ++;
+    } else {
+      points += this.scoreNoBonus();
+      index += 2;
+    }
+  }
+return points;
+
+
+
+Bowling.prototype.scoreNoBonus = function(){
+  return this.pointsPerRoll[index] + this.pointsPerRoll[index + 1];
+}
+
+Bowling.prototype.strikeBonus = function(){
+  return this.pointsPerRoll[index] + this.pointsPerRoll[index + 1] + this.pointsPerRoll[index + 2];
+}
 
 };

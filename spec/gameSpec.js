f150817deba5e678fec5a16145e88a8012d25063
Vivian Allen <mrvivianallen@gmail.com>
2018-03-25 20:48:43 +0100

    game class frame addition process, red

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -8,25 +8,29 @@ describe('Game', function(){
       expect(game.frames()[0] instanceof MockFrame).toEqual(true);
     });
     it('adds scores to the newest frame when playing', function(){
-      function MockFrame() {this._score}
-      MockFrame.prototype.roll = function(number) {this._score = number}
-      MockFrame.prototype.score = function() {return this._score}
+      function MockFrame() { this._score }
+      MockFrame.prototype.roll = function(number) {this._score = number};
+      MockFrame.prototype.score = function() {return this._score};
       var game = new Game(MockFrame);
       game.roll(5);
       expect(game.frames()[0].score()).toEqual(5);
     });
-    // it('tallies the score', function(){
-    //   function MockFrame() {}
-    //   MockFrame.prototype.score = function() { return 5 }
-    //   var game = new Game(MockFrame);
-    //   for (var i = 1; i <= 5; i++) { game.play() };
-    //   expect(game.score()).toEqual(25);
-    // });
-    // it('knows when the game is over', function(){
-    //   function MockFrame() {}
-    //   var game = new Game(MockFrame);
-    //   for (var i = 1; i <= 10; i++) { game.play() };
-    //   expect(game.isOver()).toEqual(true);
-    // });
+    it('starts a new frame when the current one returns a response string', function(){
+      function MockFrame () { this.i = 1; this.responses = [1, 2, 'a string'] }
+      MockFrame.prototype.roll = function(number) { this.i++; return this.responses[this.i-1]}
+      var game = new Game(MockFrame);
+      game.roll(1)
+      game.roll(1)
+      game.roll(1)
+      expect(game.frames().length).toEqual(2)
+    });
+    it('tallies the score', function(){
+      function MockFrame() {}
+      MockFrame.prototype.roll = function() { };
+      MockFrame.prototype.score = function() { return 5 };
+      var game = new Game(MockFrame);
+      game.roll(5);
+      expect(game.score()).toEqual(5);
+    });
   });
 });

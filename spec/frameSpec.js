cc33890bd0099a508bd7843c37d82b61f8ae7e41
Vivian Allen <mrvivianallen@gmail.com>
2018-03-25 12:33:21 +0100

    frame class normal scoring, green

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,17 +2,15 @@
 
 describe('Frame', function() {
 
-  beforeEach(function(){
-    var frame = new Frame();
-  });
-
   describe('normal scoring behaviour', function() {
     it('tallies a normal score', function() {
+      var frame = new Frame();
       frame.roll(2);
       frame.roll(4);
       expect(frame.score()).toEqual(6);
     });
     it('allows only two rolls in normal circumstances', function(){
+        var frame = new Frame();
       frame.roll(2);
       frame.roll(3);
       expect(frame.roll(2)).toEqual('Only two rolls per frame until frame 10!');
@@ -21,17 +19,20 @@ describe('Frame', function() {
 
   describe('spares', function() {
     it('logs a spare', function(){
+      var frame = new Frame();
       frame.roll(5);
       frame.roll(5);
       expect(frame.isSpare()).toEqual(true);
     });
     it('allows a single bonus rolls to be added if a spare is logged', function(){
+      var frame = new Frame();
       frame.roll(5);
       frame.roll(5);
       frame.bonus(5);
       expect(frame.score()).toEqual(15);
     });
     it('does not allow a bonus roll to be added is a spare is not logged', function(){
+      var frame = new Frame();
       frame.roll(5);
       expect(frame.bonus(2)).toEqual('Bonus rolls not permitted unless a strike or spare is logged!');
     });
@@ -39,20 +40,24 @@ describe('Frame', function() {
 
   describe('strikes', function(){
     it('logs a strike', function(){
+      var frame = new Frame();
       frame.roll(10);
       expect(frame.isStrike()).toEqual(true);
     });
     it('will not accept a second roll if a strike is logged', function(){
+      var frame = new Frame();
       frame.roll(10);
       expect(frame.roll(2)).toEqual('No second roll after strike!');
     });
     it('allows multiple bonus rolls to be added if a strike is logged', function(){
+      var frame = new Frame();
       frame.roll(10);
       frame.bonus(5);
       frame.bonus(5);
       expect(frame.score()).toEqual(20);
     });
     it('does not allow bonus rolls to be added is a strike is not logged', function(){
+      var frame = new Frame();
       frame.roll(5);
       expect(frame.bonus(2)).toEqual('Bonus rolls not permitted unless a strike or spare is logged!');
     });
@@ -60,6 +65,7 @@ describe('Frame', function() {
 
   describe('frame numbering', function(){
     it('allows a frame number to be set and returned', function(){
+      var frame = new Frame();
       frame.nSet(5);
       expect(frame.n()).toEqual(5);
     });
@@ -67,6 +73,7 @@ describe('Frame', function() {
 
   describe('frame 10', function(){
     it('allows three rolls if a the first roll is a strike', function(){
+      var frame = new Frame();
       frame.nSet(10);
       frame.roll(10);
       frame.roll(10);
--- src/frame.js
+++ src/frame.js
@@ -5,4 +5,12 @@ function Frame () {
   this._isSpare = false;
   this._isStrike = false;
   this._n
+};
+
+Frame.prototype.roll = function(number) {
+  this._rolls.push(number);
+}
+
+Frame.prototype.score = function() {
+  return this._rolls.reduce(function (sum, x) { return sum + x }, 0);
 }

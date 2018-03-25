242f323a3ab335c2143492e73165957fd53cab8b
Somayeh <mail.somayeh@gmail.com>
2018-03-25 22:36:43 +0100

    checking for roll of all ones

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,18 @@
 'use strict';
 
 function Game() {
+  this.rolls = [];
 
 }
 
-Game.prototype.roll = function() {
-
+Game.prototype.roll = function(pins) {
+  this.rolls.push(pins);
 };
 
 Game.prototype.score = function() {
-  return 0;
+  var result = 0;
+  for (var i = 0; i < 20 ; i++) {
+    result += this.rolls[i];
+  }
+  return result;
 };

17d7fe1595a67c787226d751f6821f3525fbcd0f
Vivian Allen <mrvivianallen@gmail.com>
2018-03-26 09:31:35 +0100

    game finishing, not functional

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,6 +5,7 @@ describe('Game', function(){
     it('initalises with a frame in play', function(){
       function MockFrame() {}
       MockFrame.prototype.nSet = function(n) {};
+        MockFrame.prototype.isFinished = function() { return false }
       var game = new Game(MockFrame);
       expect(game.frames()[0] instanceof MockFrame).toEqual(true);
     });
@@ -12,8 +13,37 @@ describe('Game', function(){
       function MockFrame() { this._n = 0 }
       MockFrame.prototype.nSet = function(n) { this._n = n };
       MockFrame.prototype.n = function() { return this._n };
+      MockFrame.prototype.isFinished = function() { return false }
       var game = new Game(MockFrame);
       expect(game.frames()[0].n()).toEqual(1);
+    });
+    it('adds a new frame when previous is finished', function(){
+      function MockFrame() {
+        this._n = 0;
+        this._responses = [true, false]
+      }
+      MockFrame.prototype.nSet = function(n) { this._n = n };
+      MockFrame.prototype.isFinished = function() { return this._responses[this._n-1] }
+      MockFrame.prototype.roll = function(score) {}
+      var game = new Game(MockFrame);
+      game.roll(2)
+      expect(game.frames()[1] instanceof MockFrame).toEqual(true);
+    });
+    it('knows when it is finished', function(){
+      function MockFrame() {
+        this._n = 0;
+        this._i = 0
+        this._responses = [true, false]
+      }
+      MockFrame.prototype.nSet = function(n) { this._n = n };
+      MockFrame.prototype.isFinished = function() {
+        this._i++;
+        return this._responses[this._i-1];
+       }
+      MockFrame.prototype.roll = function(score) {}
+      var game = new Game(MockFrame);
+      for (var i = 1; i <= 10; i++) { game.roll(2) };
+      expect(game.isFinished()).toEqual(true);
     });
   });
 });
--- src/game.js
+++ src/game.js
@@ -9,3 +9,18 @@ function Game (frameConstructor) {
 
 //getters
 Game.prototype.frames = function(){ return this._frames };
+
+//setters
+Game.prototype.roll = function(score) {
+  if (this._frames.slice(-1)[0].isFinished()) {
+    this._frames.push(new this._frameConstructor)
+    this._nFrames++;
+    this._frames.slice(-1)[0].nSet(this._nFrames);
+  };
+  this._frames.forEach (function (frame) { frame.roll(score) });
+}
+
+//checkers
+Game.prototype.isFinished = function(){
+  return (this._nFrames == 10 && this._frames.slice(-1)[0].isFinished())
+};

ebb09a4fedbb7ff5c30995063b63a013d593741d
julesnuggy <julesnuggy@gmail.com>
2018-03-26 19:49:42 +0100

    Created calc_bonus function

--- public/src/Scorecard.js
+++ public/src/Scorecard.js
@@ -1,23 +1,36 @@
 function Scorecard(){
   this._roll1 = this._roll2 = 0;
-  this._score = {}; //{frame: [roll1, roll2, score, bonus]}
-  this._strike = false;
-  this._spare = false;
+  this._score = {}; //{frame: [roll1, roll2, score, type, bonus]}
+  this._type = "";
 };
 
 Scorecard.prototype.calculate = function(frame, roll1, roll2) {
   score = roll1 + roll2;
   if (score > 10) {
     throw "Total exceeds 10 - check your inputs"
+  }
 
-  } else if (score === 10 && roll1 === 10) {
-    this._strike = true;
-    score_arr = [roll1, roll2, score];
-    this._score[frame] = score_arr
+  if (score === 10 && roll1 === 10) {
+    this._type = "strike"
 
   } else if (score === 10 && roll1 !== 10) {
-    this._spare = true;
-    score_arr = [roll1, roll2, score];
-    this._score[frame] = score_arr
+    this._type = "spare"
+  } else {
+    this._type = "normal"
+  }
+
+  score_arr = [roll1, roll2, score, this._type];
+  this._score[frame] = score_arr
+};
+
+Scorecard.prototype.calc_bonus = function(frame) {
+  if (this._score[frame][3] === "strike") {
+    this._score[frame].push(this._score[frame + 1][0] + this._score[frame + 1][1]);
+    console.log("strike")
+  } else if (this._score[frame][3] === "spare") {
+    this._score[frame].push(this._score[frame + 1][0]);
+    console.log("spare")
+  } else {
+    this._score[frame].push(0);
   };
 };
--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -13,18 +13,18 @@ describe("Scorecard", function() {
 
   describe("#calculate", function() {
     it("adds two numbers together to update _score", function() {
-      scorecard.calculate(1,5,5);
-      expect(scorecard._score).toEqual({1: [5,5,10]});
+      scorecard.calculate(1,2,5);
+      expect(scorecard._score).toEqual({1: [2,5,7,"normal"]});
     });
 
     it("knows when it's a strike", function() {
       scorecard.calculate(1,10,0);
-      expect(scorecard._strike).toBe(true);
+      expect(scorecard._score[1][3]).toEqual("strike");
     });
 
     it("knows when it's a spare", function() {
       scorecard.calculate(1,5,5);
-      expect(scorecard._spare).toBe(true);
+      expect(scorecard._score[1][3]).toEqual("spare");
     });
 
     it("throws an error when the two numbers sum to exceed 10", function(){
@@ -39,15 +39,24 @@ describe("Scorecard", function() {
     });
 
     it("adds a bonus of 0 for frames which score less than 10", function() {
- 
+      scorecard.calculate(1,2,2);
+      scorecard.calculate(2,3,3);
+      scorecard.calc_bonus(1)
+      expect(scorecard._score[1]).toEqual([2,2,4,"normal",0]);
     });
 
     it("adds a bonus of the next roll for spares", function() {
-
+      scorecard.calculate(1,5,5);
+      scorecard.calculate(2,3,5);
+      scorecard.calc_bonus(1)
+      expect(scorecard._score[1]).toEqual([5,5,10,"spare",3]);
     });
 
     it("adds a bonus of the next two rolls for strikes", function() {
-
+      scorecard.calculate(1,10,0);
+      scorecard.calculate(2,3,5);
+      scorecard.calc_bonus(1)
+      expect(scorecard._score[1]).toEqual([10,0,10,"strike",8]);
     });
   });
 });

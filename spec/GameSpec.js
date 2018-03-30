1d57bff87daefcb801a9fc923245e0de3275bb22
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-30 17:47:39 +0100

    Green Phase for 1st User Story. Initialized Game with an empty scorecard array.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,45 +1,18 @@
-// describe("Game", function() {
-//   var game;
-//
-//   beforeEach(function() {
-//     game = new Game();
-//   });
-//
-//   it("should be able to play a Song", function() {
-//     player.play(song);
-//     expect(player.currentlyPlayingSong).toEqual(song);
-//
-//     //demonstrates use of custom matcher
-//     expect(player).toBePlaying(song);
-//   });
-//
-//   describe("when song has been paused", function() {
-//     beforeEach(function() {
-//       player.play(song);
-//       player.pause();
-//     });
-//
-//     it("should indicate that the song is currently paused", function() {
-//       expect(player.isPlaying).toBeFalsy();
-//
-//   // demonstrates use of spies to intercept and test method calls
-//   it("tells the current song if the user has made it a favorite", function() {
-//     spyOn(song, 'persistFavoriteStatus');
-//
-//     player.play(song);
-//     player.makeFavorite();
-//
-//     expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-//   });
-//
-//   //demonstrates use of expected exceptions
-//   describe("#resume", function() {
-//     it("should throw an exception if song is already playing", function() {
-//       player.play(song);
-//
-//       expect(function() {
-//         player.resume();
-//       }).toThrowError("song is already playing");
-//     });
-//   });
-// });
+'use strict'
+
+describe('Game', function() {
+  var game;
+
+
+  describe('When a game starts.', function() {
+    beforeEach(function() {
+      game = new Game();
+    });
+
+    it('Contains an empty scorecard.', function(){
+     expect(game._scorecard).toEqual([]);
+     });
+
+  });
+
+});

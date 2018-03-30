f4fc14f0bf4fd2a8a7419ced5d7b475ca03b3fe3
Vivian Allen <mrvivianallen@gmail.com>
2018-03-30 10:56:34 +0100

    integration tests done

--- spec/gameFrameIntegration.js
+++ spec/gameFrameIntegration.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe ('game and frame integrated' , function(){
+describe ('Game and Frame integration' , function(){
   var game;
   beforeEach(function(){
     game = new Game(Frame)
@@ -16,8 +16,19 @@ describe ('game and frame integrated' , function(){
     });
   });
 
+  describe ('a gutter game', function(){
+    it ('knows the score is zero', function(){
+      for (var i = 1; i <= 20; i++) { game.roll(0) };
+      expect(game.score()).toEqual(0);
+    });
+    it ('knows it is finished', function(){
+      for (var i = 1; i <= 20; i++) { game.roll(0) };
+      expect(game.isFinished()).toEqual(true);
+    });
+  });
+
   describe('a game with no specials', function(){
-    it ('knows the score', function(){
+    it ('knows the score is 80', function(){
       for (var i = 1; i <= 20; i++) { game.roll(4) };
       expect(game.score()).toEqual(80);
     });
@@ -27,13 +38,15 @@ describe ('game and frame integrated' , function(){
     });
   })
 
-  describe ('a gutter game', function(){
-    it ('knows the score', function(){
-      for (var i = 1; i <= 20; i++) { game.roll(0) };
-      expect(game.score()).toEqual(0);
+  describe ('a 163 point game with specials', function(){
+    it ('knows the score is 163', function(){
+      var gamerolls = [10, 8, 1, 10, 10, 7, 3, 5, 3, 0, 6, 10, 7, 3, 10, 8, 1]
+      gamerolls.forEach(function(roll) { game.roll(roll) });
+      expect(game.score()).toEqual(163);
     });
     it ('knows it is finished', function(){
-      for (var i = 1; i <= 20; i++) { game.roll(0) };
+      var gamerolls = [10, 8, 1, 10, 10, 7, 3, 5, 3, 0, 6, 10, 7, 3, 10, 8, 1]
+      gamerolls.forEach(function(roll) { game.roll(roll) });
       expect(game.isFinished()).toEqual(true);
     });
   });

f126433d68efea5a7e03e49ce3cecf6aeeb26c5c
Raefe <raefe.nj@gmail.com>
2018-03-30 15:54:08 +0100

    working logic for game, passing all tests

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,11 +5,38 @@ describe('Game:', function(){
 
   beforeEach(function(){
     game = new Game
-  })
+  });
 
   it('adds a roll to the array of scores', function(){
     game.roll(3)
-    console.log(game.score())
     expect(game.score()).toEqual([3])
+  });
+  it('can deal with a multitude of scores', function(){
+    game.roll(10)
+    game.roll(4)
+    game.roll(5)
+    game.roll(7)
+    game.roll(3)
+    for(var i = 0; i < 14; i++){
+      game.roll(3)
+    }
+    expect(game.score()).toEqual(
+      [
+        10, 4, 5, 7,
+        3, 3, 3, 3, 3,
+        3, 3, 3, 3, 3,
+        3, 3, 3, 3, 3,
+      ])
+  });
+  describe('Tenth frame :-', function(){
+    it('gives two bonus rolls for a strike', function(){
+      for(var i = 0; i < 18; i++){
+        game.roll(1)
+      }
+      game.roll(10)
+      game.roll(3)
+      game.roll(3)
+      expect(game._playing).toBe(false)
+    })
   })
 })
--- src/game.js
+++ src/game.js
@@ -2,12 +2,57 @@
 
 function Game(){
   this._score = []
+  this._frameNumber = 1
+  this._rollsThisFrame = 0
+  this._playing = true
 }
 
 Game.prototype.roll = function(pinsKnockedDown){
-  this._score.push(pinsKnockedDown)
+  if (this._frameNumber == 10){
+    this.rollTenthFrame(pinsKnockedDown)}
+  else {
+    this.rollNormalFrame(pinsKnockedDown)}
+}
+
+Game.prototype.rollNormalFrame = function(pinsKnockedDown){
+  if (pinsKnockedDown == 10) {
+    this._score.push(pinsKnockedDown)
+    this.resetPins()
+  }
+  else if (this._rollsThisFrame == 1){
+    this._score.push(pinsKnockedDown)
+    this.resetPins()
+  }
+  else{
+    this._score.push(pinsKnockedDown)
+    this._rollsThisFrame += 1}
+}
+
+Game.prototype.rollTenthFrame = function(pinsKnockedDown){
+  if (this._rollsThisFrame == 0) {
+    this._score.push(pinsKnockedDown)
+    this._rollsThisFrame += 1
+  }
+  else if ((this._rollsThisFrame == 1) && (this._score.slice(-1) + pinsKnockedDown >= 10)){
+    this._score.push(pinsKnockedDown)
+    this._rollsThisFrame += 1
+  }
+  else if ((this._rollsThisFrame == 1) && (this._score.slice(-1) + pinsKnockedDown <= 9)){
+    this._score.push(pinsKnockedDown)
+    this._playing = false
+  }
+  else if (this._rollsThisFrame == 2) {
+    this._score.push(pinsKnockedDown)
+    this._playing = false
+  }
+
 }
 
 Game.prototype.score = function(){
   return this._score
+}
+
+Game.prototype.resetPins = function(){
+  this._frameNumber += 1
+  this._rollsThisFrame = 0
 }

5874293705f852b547c9646196684f6b346970a7
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-30 18:43:01 +0100

    Green Phase for 2nd User Story. Prototyped functions .roll and .tot to record my score and display the current total score.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,15 +3,30 @@
 describe('Game', function() {
   var game;
 
+  beforeEach(function() {
+    game = new Game();
+  });
 
   describe('When a game starts:', function() {
-    beforeEach(function() {
-      game = new Game();
-    });
-
     it('Contains an empty scorecard.', function(){
      expect(game._scorecard).toEqual([]);
      });
+  });
+
+  describe('While playing the game:', function() {
+    it('My scorecard records my score.', function() {
+      game.roll(1)
+      game.roll(3)
+      game.roll(9)
+      expect(game._scorecard).toEqual([1,3,9]);
+    });
+
+    it('Displays my current tot score.', function() {
+      game.roll(1)
+      game.roll(3)
+      game.roll(9)
+      expect(game.tot()).toEqual(13);
+    });
 
   });
 
--- src/Game.js
+++ src/Game.js
@@ -2,4 +2,16 @@
 
 function Game() {
   this._scorecard = [];
-}
+};
+
+Game.prototype.roll = function(pinsDown) {
+  this._scorecard.push(pinsDown);
+};
+
+Game.prototype.tot = function() {
+  var total = 0;
+  for(var idx = 0; idx < this._scorecard.length; ++idx ) {
+      total += this._scorecard[idx];
+  };
+  return total;
+};

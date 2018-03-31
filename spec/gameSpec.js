f01bdd91b8e86c5f8588212e431bf82794edaa55
Gia1987 <giacomo.n27@gmail.com>
2018-03-31 14:44:45 +0100

    Add function strike. full tested

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,29 +7,33 @@ describe('Game', function(){
     game = new Game();
   });
 
-  describe('Gutter game', function(){
-    it('rolls gutter game when players misses all pins', function(){
-      rollMany(0,20);
-      expect(game.score()).toBe(0);
-    });
+  it('rolls gutter game when players misses all pins', function(){
+    rollMany(0,20);
+    expect(game.score()).toBe(0);
   });
 
-  describe('player hits one pin every rolls', function(){
-    it('returns a score of 20 when player hits one pin per roll', function(){
-      rollMany(1,20);
-      expect(game.score()).toBe(20);
-    });
+  it('returns a score of 20 when player hits one pin per roll', function(){
+    rollMany(1,20);
+    expect(game.score()).toBe(20);
   });
 
-  describe('can roll a spare', function(){
-    it('can roll a spare', function(){
-      game.roll(5);
-      game.roll(5);
-      game.roll(3);
-      rollMany(0,17);
-      expect(game.score()).toBe(16);
-    });
+
+  it('can roll a spare', function(){
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(0,17);
+    expect(game.score()).toBe(16);
   });
+
+  it('can roll a strike', function(){
+    game.roll(10);
+    game.roll(4);
+    game.roll(3);
+    rollMany(0,16);
+    expect(game.score()).toBe(24);
+  });
+
   var rollMany = function(pins, rolls){
     for(var i = 0; i < rolls; i++){
       game.roll(pins);
--- src/game.js
+++ src/game.js
@@ -11,29 +11,40 @@ Game.prototype.roll = function(pins) {
 };
 
 Game.prototype.score = function() {
-  var result = 0
+  var result = 0;
   var rollIndex = 0;
   var game = this;
   for(var frameIndex = 0; frameIndex < 10; frameIndex++){
-    if(isSpare()){
+    if(isStrike()) {
+      result = result + getStrikeScore()
+      rollIndex++;
+    } else if (isSpare()) {
       result = result + getSpareScore();
+      rollIndex = rollIndex + 2;
     } else {
-
     result = result + getNormalScore();
-    }
     rollIndex = rollIndex + 2;
+    }
   }
   return result
+
+  function isStrike(){
+    return game.rolls[rollIndex] == 10;
+  }
 
   function isSpare(){
     return game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10;
   }
 
   function getSpareScore(){
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2]
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+  }
+
+  function getStrikeScore(){
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
   }
 
   function getNormalScore(){
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1]
+    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
   }
 };

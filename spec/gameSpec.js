5d900be32fcb17e10d55aeeaec6aa6567775776f
Gia1987 <giacomo.n27@gmail.com>
2018-03-31 12:39:58 +0100

    implement score function and tested ok

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -20,6 +20,16 @@ describe('Game', function(){
       expect(game.score()).toBe(20);
     });
   });
+
+  describe('can roll a spare', function(){
+    it('can roll a spare', function(){
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      rollMany(0,17);
+      expect(game.score()).toBe(16);
+    });
+  });
   var rollMany = function(pins, rolls){
     for(var i = 0; i < rolls; i++){
       game.roll(pins);
--- src/game.js
+++ src/game.js
@@ -12,8 +12,15 @@ Game.prototype.roll = function(pins) {
 
 Game.prototype.score = function() {
   var result = 0
-  for(var i = 0; i < 20; i++){
-    result = result + this.rolls[i];
+  var rollIndex = 0;
+  for(var frameIndex = 0; frameIndex < 10; frameIndex++){
+    if(this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10){
+      result = result + this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    } else {
+
+    result = result + this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+    rollIndex = rollIndex + 2;
   }
   return result
 };

6e06c69cc3cc6cb4347f352476bf7306aabdf73a
Reenakoudi <kreena92@gmail.com>
2018-04-01 00:31:22 +0100

    Game starts with 10 empty frames and returns current frame implemented

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -4,7 +4,15 @@ describe('Game', function(){
   var game;
 
   beforeEach(function(){
-    game = new Game();
+    game = new Game(Frame);
   });
 
+  it('Game starts with 10 empty frame', function(){
+    game.createEmptyFrames();
+    expect(game.getFrames().length).toEqual(10);
+  });
+
+  it('Current frame is 1', function(){
+    expect(game.getCurrentFrame()).toEqual(1);
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,1 +1,20 @@
 'use strict'
+
+function Game(frameClass){
+  this.frameClass = frameClass;
+  this.frames = [];
+  this.currentFrame = 1;
+};
+
+Game.prototype.createEmptyFrames = function(){
+  for(var i = 1; i <= 10; i++)
+  this.frames.push(new this.frameClass(i));
+};
+
+Game.prototype.getFrames = function(){
+  return this.frames;
+};
+
+Game.prototype.getCurrentFrame = function(){
+  return this.currentFrame;
+};

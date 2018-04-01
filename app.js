2971b70f73ce59fc19e16fae4f0e6944a1090806
Vivian Allen <mrvivianallen@gmail.com>
2018-04-01 18:34:07 +0100

    this probably doenst want to be a node app, does it

--- app.js
+++ app.js
@@ -2,13 +2,27 @@
 var express = require('express');
 var app = express();
 var ejs = require('ejs');
+var Frame = require('./src/frame');
+var Game = require('./src/game');
+var bodyParser = require('body-parser');
+var game = new Game (Frame);
 
 // === setup === //
 app.set('view engine', 'ejs')
 
 // === routes === //
-app.get('/', function(request,response){
-  response.render('app');
+app.get('/', function(req, res){
+  res.redirect('/bowling');
+});
+
+app.get('/bowling', function(req,res){
+  res.render('bowling', {game: game});
+});
+
+app.post('/bowling/startNew', function(req,res){
+  var game = new Game(Frame);
+    console.log(game);
+  res.redirect('/bowling');
 });
 
 app.listen(4567, function(){

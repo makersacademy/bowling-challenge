f20b976efc1822d95df237876edf728c17d299bb
Alfie Darko <mrhazzy@gmail.com>
2018-04-02 19:15:09 +0100

    passing green test that counts score if frame has spare

--- test/scorecard_spec.js
+++ test/scorecard_spec.js
@@ -11,22 +11,43 @@ describe("Scorecard", function() {
 		expect(game.frame).to.be.a("object");
 	});
 
-	it("can reference the current frame rolls within Scorecard function", function() {
-		game = new Scorecard();
+	describe(".frame.rolls", function() {
+		it("can reference the current frame rolls within Scorecard function", function() {
+			game = new Scorecard();
 
-		game.frame.logRoll(5);
-		game.frame.logRoll(3);
+			game.frame.logRoll(5);
+			game.frame.logRoll(3);
 
-		expect(game.frame.rolls).to.be.a("array");
-		expect(game.frame.rolls).to.include(5, 3);
+			expect(game.frame.rolls).to.be.a("array");
+			expect(game.frame.rolls).to.include(5, 3);
+		});
 	});
 
-	it("counts score after 2 rolls when not a spare or strike", function() {
-		game = new Scorecard();
+	describe(".addToScorecard()", function() {
+		it("counts score after 2 rolls when not a spare or strike", function() {
+			game = new Scorecard();
 
-		game.frame.logRoll(5);
-		game.frame.logRoll(3);
-		game.addToScorecard();
-		expect(game.score).to.include(8);
+			game.frame.logRoll(5);
+			game.frame.logRoll(3);
+			game.addToScorecard();
+			expect(game.score).to.include(8);
+		});
+	});
+
+	describe(".addToScorecard()", function() {
+		it("counts score after 3 rolls when a spare", function() {
+			game = new Scorecard();
+
+			game.frame.logRoll(8);
+			game.frame.logRoll(2);
+			game.frame.logRoll(7);
+			game.addToScorecard();
+
+			expect(game.score).to.include(17);
+		});
+	});
+
+	describe("", function() {
+		it("", function() {});
 	});
 });

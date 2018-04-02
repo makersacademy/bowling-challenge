4ac43250b86ae1754cbb780401b367e6e56a08cd
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-02 11:15:18 +0000

    Refactor index

--- index.js
+++ index.js
@@ -3,57 +3,57 @@ $(document).ready(function(){
 
   $('#zero').click(function() {
     game.roll(0);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#one').click(function() {
     game.roll(1);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#two').click(function() {
     game.roll(2);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#three').click(function() {
     game.roll(3);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#four').click(function() {
     game.roll(4);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#five').click(function() {
     game.roll(5);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#six').click(function() {
     game.roll(6);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#seven').click(function() {
     game.roll(7);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#eight').click(function() {
     game.roll(8);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#nine').click(function() {
     game.roll(9);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
   $('#ten').click(function() {
     game.roll(10);
-    $('#frameTotal').text(game.frameTotal());
+    $('#frameScore').text(game.frameScore());
   });
 
 });

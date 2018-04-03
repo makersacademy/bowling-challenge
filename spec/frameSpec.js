fbd4277dbbed5b82b66c0b09fb0fbae3edfccc96
Reenakoudi <kreena92@gmail.com>
2018-04-03 13:52:59 +0100

    Set secondRoll to zero and tested

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -11,11 +11,11 @@ it('has an ID', function(){
   expect(frame.getID()).toEqual(1);
 });
 
-it('set firstRoll to zero', function(){
+it('Set firstRoll to zero', function(){
   expect(frame.getFirstRoll()).toEqual(0);
 });
 
-it('set firstRoll', function(){
+it('Set firstRoll', function(){
   frame.setFirstRoll(6);
   expect(frame.getFirstRoll()).toEqual(6);
 });
@@ -23,5 +23,9 @@ it('set firstRoll', function(){
 it('Throw error if firstRoll set to more than 10 pins', function(){
   frame.setFirstRoll(11);
   expect(function(){frame.checkPinsExceeded(11)}).toThrowError('A roll cannot exceed maximum of 10 pins');
+});
+
+it('Set second roll to zero', function(){
+  expect(frame.getSecondRoll()).toEqual(0);
 });
 });
--- src/frame.js
+++ src/frame.js
@@ -3,6 +3,7 @@
 function Frame(id){
   this.id = id;
   this.firstRoll = 0;
+  this.secondRoll = 0;
 }
 
 Frame.prototype.getID = function(){
@@ -19,4 +20,8 @@ Frame.prototype.getFirstRoll = function(){
 
 Frame.prototype.checkPinsExceeded = function(pins){
   if(this.firstRoll + pins > 10) {throw Error('A roll cannot exceed maximum of 10 pins')};
+};
+
+Frame.prototype.getSecondRoll = function(){
+  return this.secondRoll;
 };

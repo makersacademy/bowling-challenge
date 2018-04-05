095d2dd7341d01a716900beab45fcc8296bfb58f
Edward <ea.litten@gmail.com>
2018-04-05 12:34:57 +0100

    Make linter happy

--- src/frameView.js
+++ src/frameView.js
@@ -3,17 +3,17 @@ function FrameView() { }
 FrameView.prototype = {
   initialise: function() {
     this._element = $(this.frameViewElement).html(
-      '<div class='frame-view'>' +
-        '<div class='frame-number'></div>' +
-        '<div class='frame-roll1'></div>' +
-        '<div class='frame-roll2'></div>' +
-      '</div>'
+      "<div class='frame-view'>" +
+        "<div class='frame-number'></div>" +
+        "<div class='frame-roll1'></div>" +
+        "<div class='frame-roll2'></div>" +
+      "</div>"
     );
   },
 
   appendTo: function(parent) {
     $(parent).append(this._element);
-  }
+  },
 
   recordRolls: function(frame) {
     $(element).find('.frame-roll1').text(getFirstRoll(frame));

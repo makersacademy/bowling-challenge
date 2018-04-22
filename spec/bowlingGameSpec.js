a6c4a9844ad7f8ccd3400e6a76307dafe1660b83
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-22 09:52:37 +0100

    second test, should handle all ones

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -1,11 +1,25 @@
 describe('BowlingGame', function() {
 
-  it('should be able to give score for Frame with 0 score', function() {
-    var game = new BowlingGame
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+  var game;
+
+  beforeEach(function() {
+    game = new BowlingGame();
+  });
+
+  it('can roll gutter game', function() {
+     rollMany(0, 20);
     expect(game.score()).toEqual(0);
   });
+
+  it("should handle rolling all ones", function() {
+     rollMany(1, 20);
+     expect(game.score()).toEqual(20);
+  });
+
+  var rollMany = function(pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins)
+    }
+  }
 
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,16 +2,18 @@
 
 function BowlingGame() {
   this.rolls = [];
-  this.currentRoll = 0
 };
 
 BowlingGame.prototype.roll = function(pins) {
-  this.rolls[this.currentRoll++] = pins;
+  this.rolls.push(pins);
 
 };
 
 BowlingGame.prototype.score = function() {
 	var score = 0;
 
+  for (var i = 0; i < 20; i++) {
+    score += this.rolls[i] ;
+  }
 	return score;
 };

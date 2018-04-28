7ffe861a4ad22451886d0f5e3add3d63f9539f6d
Jordan Brooks <jorbrooks92@gmail.com>
2018-04-28 14:54:53 +0100

    Frame.setRoll function with test

--- bowling-app/spec/FrameSpec.js
+++ bowling-app/spec/FrameSpec.js
@@ -1,15 +1,22 @@
 describe('Frame', function(){
   let frame;
 
-  beforeEach(function(){
+  beforeEach(function() {
     frame = new Frame();
   })
   
-  describe('score', function(){
+  describe('score', function() {
     it('returns the sum of rolls 1 and 2', function() {
       frame.rollOne = 5;
       frame.rollTwo = 4;
       expect(frame.score()).toEqual(9);
-    })
+    });
+  })
+
+  describe('setRoll', function() {
+     it('sets the first roll to specified number', function() {
+       frame.setRoll(1, 10);
+       expect(frame.rollOne).toEqual(10);
+     });
   })
 })
--- bowling-app/src/Frame.js
+++ bowling-app/src/Frame.js
@@ -5,3 +5,8 @@ function Frame() {
 Frame.prototype.score = function() {
   return this.rollOne + this.rollTwo;
 }
+
+Frame.prototype.setRoll = function(num, score) {
+  var roll = num === 1 ?  'rollOne' : 'rollTwo';
+  this[roll] = score;
+}

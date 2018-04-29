14839f7eab642332e7b5a75442b2e757dda84916
Jordan Brooks <jorbrooks92@gmail.com>
2018-04-29 16:45:29 +0100

    passing all feature tests

--- bowling-app/spec/featureSpec.js
+++ bowling-app/spec/featureSpec.js
@@ -12,7 +12,7 @@ describe("Game", function() {
     }
   }
 
-  describe('roll', function() {
+  describe('play', function() {
     it('scores a game of all gutter balls', function() {
       autoRoll(0, 20);
       expect(game.score()).toEqual(0);
@@ -24,6 +24,26 @@ describe("Game", function() {
     it('scores a game of spares', function() {
       autoRoll(5, 21);
       expect(game.score()).toEqual(150);
+    });
+    it('scores a game of numbers', function() {
+      autoRoll(5, 4);
+      autoRoll(3, 4);
+      autoRoll(5, 4);
+      autoRoll(3, 4);
+      autoRoll(10, 3);
+      autoRoll(1, 1);
+      expect(game.score()).toEqual(131);
+    });
+    it('scores another game of numbers', function() {
+      autoRoll(7, 1);
+      autoRoll(2, 3);
+      autoRoll(5, 2);
+      autoRoll(10, 4);
+      autoRoll(5, 3);
+      autoRoll(1, 1);
+      autoRoll(5, 2);
+      autoRoll(10, 1);
+      expect(game.score()).toEqual(179);
     });
   });
 });

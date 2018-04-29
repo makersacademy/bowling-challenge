468530a6ba77b4e56654a81a3ea3a0db3b61f7aa
Matthew <matt_burgoyne@live.co.uk>
2018-04-29 10:29:14 +0100

    current frame is displayed and score for first roll is displayed

--- jasmine-standalone-3.1.0/src/bowlingController.js
+++ jasmine-standalone-3.1.0/src/bowlingController.js
@@ -1,1 +1,17 @@
 var bowl = new Bowling();
+
+function updateScore() {
+  frame_score = bowl.currentFrameScore;
+  document.getElementById('current_frame_score').value = frame_score;
+
+}
+
+function countScore (score) {
+  score = parseInt(score);
+  bowl.countScore(score);
+}
+
+function updateFrame() {
+  current_frame = bowl.frameCount;
+  document.getElementById('frame').value = current_frame;
+}

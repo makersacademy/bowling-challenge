c15c0ffb100dc658d91742d3eb7433587f1b432b
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-19 16:25:39 +0100

    Starts new frame after a strike

--- src/bowling.js
+++ src/bowling.js
@@ -1,5 +1,6 @@
 function Bowling() {
   this.pins = 10
+  this.current_frame = 1
   this.current_roll = 1
   this.frame_score = 0
   this.score_card = []
@@ -11,11 +12,17 @@ Bowling.prototype.roll = function(num) {
   } else {
   this.pins -= num;
   this.frame_score += num
-  this.current_roll = 2
+  };
+
+  if (this.current_roll == 2 || this.pins == 0) {
+    this.end_frame();
+  } else {
+  this.current_roll = 2;
   };
 };
 
 Bowling.prototype.end_frame = function() {
   this.current_roll = 1
+  this.current_frame += 1
   this.score_card.push(this.frame_score)
 };

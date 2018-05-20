7e11237b1bfaee681b505bbf5aa97d1fa04e5e7c
Mark Channer <markchanner@gmail.com>
2018-05-20 15:02:08 +0100

    Extracts logic for checking for a spare from getScore method into _isSpare method

--- src/bowling-game.js
+++ src/bowling-game.js
@@ -10,15 +10,19 @@ BowlingGame.prototype.roll = function(pins) {
 
 BowlingGame.prototype.getScore = function() {
   var score = 0;
-  var i = 0;
+  var frameIndex = 0;
   for (var frame = 0; frame < 10; frame++) {
-    if (this.rolls[i] + this.rolls[i + 1] === 10) {
-      score += 10 + this.rolls[i + 2];
-      i += 2;
+    if (this._isSpare(frameIndex)) {
+      score += 10 + this.rolls[frameIndex + 2];
+      frameIndex += 2;
     } else {
-      score += this.rolls[i] + this.rolls[i + 1];
-      i += 2;
+      score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+      frameIndex += 2;
     }
   }
   return score;
+};
+
+BowlingGame.prototype._isSpare = function(frameIndex) {
+  return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
 };

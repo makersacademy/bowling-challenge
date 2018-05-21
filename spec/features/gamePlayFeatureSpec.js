25cb8d577aea5ddebd7fe094ffdd2ce5242fc47a
George Sykes <georgesykes86@gmail.com>
2018-05-21 07:17:19 +0100

    Adds another feature test

--- spec/features/gamePlayFeatureSpec.js
+++ spec/features/gamePlayFeatureSpec.js
@@ -23,10 +23,8 @@ describe("Game scenarios", function(){
 
     it("calculates the correct score", function(){
       var bowls = [10,7,3,10,6,4,10,5,5,10,0,10,10,6,4,10]
-
       for(var i = 0; i < 16; i++){
         game.bowl(bowls.shift());
-        console.log(game)
       }
       expect(game.isOver()).toBe(true);
       expect(game.frames.length).toEqual(10);
@@ -36,6 +34,27 @@ describe("Game scenarios", function(){
         expect(frame.score).toEqual(20);
       });
     });
+
+  });
+
+  describe("random game", function(){
+    it("calculates the correct score", function(){
+      var bowls = [5,3,8,2,10,6,1,0,10,5,4,3,7,10,6,3,10,10,8]
+      var scores = [8,20,17,7,15,9,20,19,9,28]
+      for(var i = 0; i < 19; i++){
+        game.bowl(bowls.shift());
+      }
+      expect(game.isOver()).toBe(true);
+      expect(game.frames.length).toEqual(10);
+      expect(game.currentScore()).toEqual(152);
+      game.frames.forEach(function(frame){
+        expect(frame.isComplete).toBe(true);
+        expect(frame.score).toEqual(scores.shift());
+      });
+    });
+  });
+
+  describe("halfway through a game", function(){
 
   });
 

c1538c0bd877055c0721106dbd6bcb58d8030a96
Asia Antczak <aasiaantczak@gmail.com>
2018-05-26 13:51:55 +0100

    check if frame is spare

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -21,7 +21,6 @@ describe("Game", function() {
     it("scores a game where 1 pin is knocked in each roll", function() {
       rollMany(20, 1)
       expect(game.score()).toEqual(20)
-      })
     });
 
     it("scores a game with one spare", function() {
@@ -31,7 +30,14 @@ describe("Game", function() {
       rollMany(17, 0);
       expect(game.score()).toEqual(16)
     });
+  });
 
-
+  describe("isSpare", function() {
+    it("returns true if frame is a spare", function() {
+      game.roll(4);
+      game.roll(6);
+      expect(game.isSpare(0)).toBe(true);
+    });
+  });
 
 });
--- src/game.js
+++ src/game.js
@@ -11,7 +11,7 @@ Game.prototype.roll = function(pins) {
 Game.prototype.score = function() {
   var frameIndex = 0
   for (var frame = 0; frame < 10; frame++) {
-    if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10) {
+    if (this.isSpare(frameIndex)) {
       this.scores += 10 + this.rolls[frameIndex + 2];
     } else {
       this.scores += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
@@ -19,4 +19,8 @@ Game.prototype.score = function() {
     frameIndex += 2;
   }
   return this.scores;
+}
+
+Game.prototype.isSpare = function(frameIndex) {
+  return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
 }

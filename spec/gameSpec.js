160c99b73b15cd9861a8b7e42d9234446e11f81c
Asia Antczak <aasiaantczak@gmail.com>
2018-05-26 12:13:06 +0100

    scores a gutter game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,20 +1,19 @@
 describe("Game", function() {
   var game;
-  const max_pins = 10;
-
 
   beforeEach(function() {
     game = new Game();
 
   });
 
-  it("adds knocked pins to frame", function() {
-    game.roll(6);
-    expect(game.frame).toEqual([6]);
-  });
+  describe("score", function() {
+    it("scores gutter game", function() {
+      for (var i = 0; i < 20; i++) {
+        game.roll(0);
+      }
+      expect(game.score).toEqual(0);
+    });
 
-  it("can knock max of 10 pins in one roll", function() {
-    expect(function() { game.roll(11) }).toThrow("There is max of 10 pins to knock");
-  });
+
 
 });
--- src/game.js
+++ src/game.js
@@ -1,13 +1,7 @@
-const max_pins = 10;
-
 function Game() {
-  this.frame = []
+  this.score = 0
 }
 
 Game.prototype.roll = function(pins) {
-  if (pins > 10) {
-    throw "There is max of 10 pins to knock";
-  } else {
-    this.frame.push(pins);
-  }
-};
+  this.score = 0
+}

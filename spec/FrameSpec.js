4e1bf56b1197496b5d9eead4a72ce44a8fb6cfcf
Revilo1882 <oliverrharris@gmail.com>
2018-05-27 16:43:05 +0100

    ends the frame after 2 bowls or a strike

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -7,9 +7,40 @@ describe ("Frame", function() {
   });
 
   describe('bowl',function(){
-    it('adds to the scores after a bowl', function(){
+    it('adds to the scores after a bowl', function() {
       frame.bowl(4);
       expect(frame.scores.length).toEqual(1);
+    });
+
+    it('reduce the nuber of pins by the score', function() {
+      frame.bowl(4);
+      expect(frame.pins).toEqual(6);
+    })
+
+    it('throws an error if score is too high', function() {
+      expect( function(){ frame.bowl(11); } ).toThrow("please enter a valid score");
+    });
+
+    it('ends the turn after a stike', function() {
+      frame.bowl(10);
+      expect(frame.finish).toBeTruthy;
+    });
+
+    it('ends the turn after two bowls', function() {
+      frame.bowl(4);
+      frame.bowl(4);
+      expect(frame.finish).toBeTruthy;
+    });
+
+    it('does not end the turn after one bowl', function() {
+      frame.bowl(4);
+      expect(frame.finish).not.toBeFalsy;
+    });
+
+    it('throws an error if the turn is over', function() {
+      frame.bowl(2);
+      frame.bowl(2);
+      expect( function(){ frame.bowl(4); } ).toThrow("frame has finished, start a new frame");
     });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,8 +1,31 @@
 function Frame() {
   this.scores = [];
   this.pins = 10;
+  this.finish = false
 }
 
 Frame.prototype.bowl = function(score) {
+  this._frameOver();
+  this._validScore(score);
   this.scores.push(score);
+  this.pins -= score;
+  this._endTurn();
+};
+
+
+Frame.prototype._validScore = function(score) {
+  if (score > this.pins) {
+    throw "please enter a valid score"
+  };
+};
+
+Frame.prototype._endTurn = function() {
+  if (this.pins === 0 || this.scores.length === 2) {
+  this.finish = !this.finish};
+};
+
+Frame.prototype._frameOver = function() {
+  if (this.finish === true) {
+    throw "frame has finished, start a new frame"
+  };
 };

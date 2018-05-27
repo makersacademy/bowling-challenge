b72613d89075b36447deb084d80395a482a86aac
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-27 12:04:18 +0100

    Refactor Bowling logic

--- src/bowling.js
+++ src/bowling.js
@@ -7,100 +7,47 @@ function Bowling() {
   this.game_scores = []
 }
 
-// Knocking down pins in a frame
 Bowling.prototype.knock_pins = function(num) {
-
-  //If the current frame is the 10th frame, abide by a different set of rules
   if (this.current_frame === 10) {
     this.frame_ten(num);
   } else {
-    // If the number of pins to knock is more than there are, raise error
-    if (this.pins - num < 0 || num > 10) {
-      throw("You cannot knock over more pins than there are standing")
-    } else {
-      // Otherwise, knock over that many pins, and update the frame score
-      this.pins -= num;
-      this.frame_score.push(num)
-    };
-  // If on any frame except the first
-    if (this.current_frame > 1) {
+    this.pins_knocked(num);
+    this.calculate_scoring(num);
 
-
-  //STRIKE SCORING
-
-    //If on the second roll of the current frame, and the last frame score was 10
-      if (this.current_roll === 2 && (this.score_card[this.score_card.length - 1]).length === 1 ) {
-        //Add to the previous frame score, the two scores of the current frame
-        this.score_card[this.score_card.length - 1].push(this.frame_score[0]);
-        this.score_card[this.score_card.length - 1].push(num);
-      };
-      //If on the first roll of the current frame, and the prevous frame score was 10, and the current roll is 10
-      if ((this.current_roll === 1) && ((this.score_card[this.score_card.length - 1]).length === 1) && (num === 10)) {
-        //Add the current score of 10 to the previous frame score
-        this.score_card[this.score_card.length - 1].push(num);
-      };
-
-
-  //SPARE SCORING
-
-    //If on the first roll of the current frame, and the previous frame score was a spare
-      if (this.current_roll === 1 && (this.score_card[this.score_card.length - 1]).length === 2 && (this.score_card[this.score_card.length - 1]).reduce(function(acc, val) { return acc + val; }) == 10) {
-        //Add to the previous frame score, the score of this first roll
-        this.score_card[this.score_card.length - 1].push(num);
-      };
-    };
-
-
-  //CONDITIONAL FOR GETTING A STRIKE, FOLLOWED BY A STRIKE, FOLLOWED BY FURTHER SCORING
-
-    //If on any frame beyond the second
-    if (this.current_frame > 2) {
-      //If on the first roll of the current frame, and the score of the frame before the last is currently a strike plus a single score
-      if ((this.current_roll === 1) && ((this.score_card[this.score_card.length - 2]).length === 2) && ((this.score_card[this.score_card.length - 2]).includes(10))) {
-        //Add to the frame score before last, the score of this first roll
-        this.score_card[this.score_card.length - 2].push(num);
-      };
-    };
-
-  //If on the second roll of the frame, or all the pins have been knocked over
     if (this.current_roll === 2 || this.pins === 0) {
-      //End the frame
       this.end_frame();
-      //Otherwise, set the current roll to be the second of the frame
     } else {
       this.current_roll = 2;
     };
   };
 };
 
-//End frame only occurs if completed 2 rolls in a frame, or all pins have been knocked over, i.e a strike has occured
-Bowling.prototype.end_frame = function() {
-  //If the current frame is 10, and all pins have been knocked, or two rolls have been taken
-  if (this.current_frame === 10) {
-    this.score_card.push(this.frame_score)
-    //Tally the total score and add to the game_scores array
-    this.score();
-    //Reset all game variables
-    this.reset_game();
-    //Otherwise, reset the appropriate variables, and start a new frame
+Bowling.prototype.pins_knocked = function(num) {
+  if (this.pins - num < 0 || num > 10) {
+    throw("You cannot knock over more pins than there are standing")
   } else {
-    this.pins = 10
-    this.current_frame += 1
-    this.current_roll = 1
-    this.score_card.push(this.frame_score)
-    this.frame_score = []
+    this.pins -= num;
+    this.frame_score.push(num)
   };
 };
 
-//If frame 10 has been completed, reset all game variables
+Bowling.prototype.end_frame = function() {
+  if (this.current_frame === 10) {
+    this.score_card.push(this.frame_score)
+    this.score();
+    this.reset_game();
+  } else {
+    this.new_frame();
+  };
+};
+
 Bowling.prototype.reset_game = function() {
   this.pins = 10
   this.current_frame = 1
   this.current_roll = 1
   this.frame_score = []
   this.score_card = []
-}
-
+};
 
 Bowling.prototype.score = function() {
   var total = 0
@@ -112,47 +59,33 @@ Bowling.prototype.score = function() {
   this.game_scores.push(total)
 };
 
+Bowling.prototype.new_frame = function() {
+  this.pins = 10
+  this.current_frame += 1
+  this.current_roll = 1
+  this.score_card.push(this.frame_score)
+  this.frame_score = []
+};
 
+Bowling.prototype.previous_frame = function() {
+  return (this.score_card[this.score_card.length - 1])
+}
 
+Bowling.prototype.frame_before_last = function() {
+  return (this.score_card[this.score_card.length - 2])
+}
 
+Bowling.prototype.previous_frame_was_a_spare = function() {
+  return (this.previous_frame().length === 2 && this.previous_frame().reduce(function(acc, val) { return acc + val; }) == 10)
+}
 
+Bowling.prototype.previous_frame_was_a_strike = function() {
+  return (this.previous_frame().length === 1)
+}
 
  Bowling.prototype.frame_ten = function(num) {
-   //If the number of pins to knock is more than there are, raise error
-   if (this.pins - num < 0 || num > 10) {
-     throw("You cannot knock over more pins than there are standing")
-     // Otherwise, knock over that many pins, and update the frame score
-   } else {
-     this.pins -= num;
-     this.frame_score.push(num)
-   };
-
-  //follows same scoring rules in terms of past frames
-
-  //SPARE SCORING
-
-  //If on the first roll of the current frame, and the previous frame score was a spare
-    if (this.current_roll === 1 && (this.score_card[this.score_card.length - 1]).length === 2 && (this.score_card[this.score_card.length - 1]).reduce(function(acc, val) { return acc + val; }) == 10) {
-      //Add to the previous frame score, the score of this first roll
-      this.score_card[this.score_card.length - 1].push(num);
-    };
-
-    //STRIKE SCORING
-
-    //If on the second roll of the current frame, and the last frame score was 10
-    if (this.current_roll === 2 && (this.score_card[this.score_card.length - 1]).length === 1 ) {
-      //Add to the previous frame score, the two scores of the current frame
-      this.score_card[this.score_card.length - 1].push(this.frame_score[0]);
-      this.score_card[this.score_card.length - 1].push(num);
-    };
-
-    //If on the first roll of the current frame, and the score of the frame before the last is currently a strike plus a single score
-    if ((this.current_roll === 1) && ((this.score_card[this.score_card.length - 2]).length === 2) && ((this.score_card[this.score_card.length - 2]).includes(10))) {
-      //Add to the frame score before last, the score of this first roll
-      this.score_card[this.score_card.length - 2].push(num);
-    };
-
-
+   this.pins_knocked(num);
+   this.frame_10_scoring(num)
     //If the third roll has been taken, or two rolls have been taken and the second roll has not knocked all pins
     if (this.current_roll === 3 || (this.current_roll === 2 && this.pins > 0)) {
       this.end_frame();
@@ -169,3 +102,53 @@ Bowling.prototype.score = function() {
       this.current_roll = 2;
     };
   };
+
+
+
+
+//METHODS THAT ARE TOO LONG
+
+
+
+Bowling.prototype.frame_10_scoring = function(num) {
+  //SPARE SCORING
+  if (this.current_roll === 1 && this.previous_frame_was_a_spare()) {
+    this.previous_frame().push(num);
+  };
+    //STRIKE SCORING
+  if (this.current_roll === 2 && this.previous_frame_was_a_strike()) {
+    this.previous_frame().push(this.frame_score[0]);
+    this.previous_frame().push(num);
+  };
+    //If on the first roll of the current frame, and the score of the frame before the last is currently a strike plus a single score
+  if ((this.current_roll === 1) && (this.frame_before_last().length === 2) && (this.frame_before_last().includes(10))) {
+    //Add to the frame score before last, the score of this first roll
+    this.frame_before_last().push(num);
+  };
+};
+
+Bowling.prototype.calculate_scoring = function(num) {
+  if (this.current_frame > 1) {
+//STRIKE SCORING
+    if (this.current_roll === 2 && this.previous_frame_was_a_strike()) {
+      this.previous_frame().push(this.frame_score[0]);
+      this.previous_frame().push(num);
+    };
+    //If on the first roll of the current frame, and the prevous frame score was 10, and the current roll is 10
+    if ((this.current_roll === 1) && (this.previous_frame_was_a_strike()) && (num === 10)) {
+      this.previous_frame().push(num);
+    };
+//SPARE SCORING
+    if (this.current_roll === 1 && this.previous_frame_was_a_spare()) {
+      this.previous_frame().push(num);
+    };
+  };
+//CONDITIONAL FOR GETTING A STRIKE, FOLLOWED BY A STRIKE, FOLLOWED BY FURTHER SCORING
+  //If on any frame beyond the second
+  if (this.current_frame > 2) {
+    //If on the first roll of the current frame, and the score of the frame before the last is currently a strike plus a single score
+    if ((this.current_roll === 1) && (this.frame_before_last().length === 2) && (this.frame_before_last().includes(10))) {
+      this.frame_before_last().push(num);
+    };
+  };
+};

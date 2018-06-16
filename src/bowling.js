bf2b3d5bdeef6fc4393b7828dea351419ca1ab6d
anguspettifer <angus.pettifer@gmail.com>
2018-06-16 11:19:56 +0100

    refactor

--- src/bowling.js
+++ src/bowling.js
@@ -5,11 +5,14 @@ function Bowling(){
 };
 
 Bowling.prototype.bowl = function () {
-  var pinsKnockedDown;
-  pinsKnockedDown = Math.floor(Math.random() * 11);
+  var pinsKnockedDown = this._randomNum1to10();
   this.score += pinsKnockedDown;
 };
 
 Bowling.prototype.score = function () {
   return this.score;
+};
+
+Bowling.prototype._randomNum1to10 = function () {
+  return Math.floor(Math.random() * 11);
 };

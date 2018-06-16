edffda37a3b67fdfed3b712f3699257564dc6b30
Simone <simonecaplin@gmail.com>
2018-06-16 14:24:53 +0100

    Current frame starts on 1 and can be updated

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,9 +1,25 @@
-describe("BowlingGame", function() {
-  var BowlingGame = require('../lib/BowlingGame.js');
+'use strict';
+var BowlingGame = require('../lib/BowlingGame.js');
+
+describe('BowlingGame', function() {
 
   var bowlingGame;
 
   beforeEach(function() {
     bowlingGame = new BowlingGame();
+  });
+
+  it('starts on the first frame', function() {
+    expect(bowlingGame.getCurrentFrame()).toEqual(1);
+  });
+
+  it('can move to the next frame', function(){
+    bowlingGame.nextFrame();
+    expect(bowlingGame.getCurrentFrame()).toEqual(2);
+  });
+
+  it('can return the number of the previous frame', function() {
+    bowlingGame.nextFrame();
+    expect(bowlingGame.getPreviousFrame()).toEqual(1);
   });
 });

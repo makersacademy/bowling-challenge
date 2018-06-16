2e410d9f80a5aa6478237c061edf016d7050c6c0
Simone <simonecaplin@gmail.com>
2018-06-16 13:17:09 +0100

    Set up Jasmine on the command line

---
+++ spec/BowlingGameSpec.js
@@ -0,0 +1,9 @@
+describe("BowlingGame", function() {
+  var BowlingGame = require('../lib/BowlingGame.js');
+
+  var bowlingGame;
+
+  beforeEach(function() {
+    bowlingGame = new BowlingGame();
+  });
+});

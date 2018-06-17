fb360614a9d3af56bd1b71a10aa9b46ca0990a20
luciagore <lucy.gore@hotmail.co.uk>
2018-06-17 09:16:54 +0100

    can increase the frame number and know there are 2 games in each frame

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -19,8 +19,8 @@ describe('Bowling', function(){
 	it('adds the scores together in the scorecard', function() {
 		bowling.roll(2);
 		bowling.roll(5);
-		expect(bowling._score).toEqual(7);
-		expect(function(){bowling.roll(4);}).toThrowError("Not possible!")
+		expect(bowling.currentTotalScore()).toEqual(7);
+		expect(function(){bowling.roll(11);}).toThrowError("Not possible!")
 	});
 
 	it('adds each individual score to a _cumalativescore array', function() {
@@ -29,6 +29,16 @@ describe('Bowling', function(){
 		bowling.roll(3);
 		bowling.roll(2);
 		expect(bowling._cumalativescore).toEqual([1, 2, 3, 2]);
+	});
+
+	it('can know there are two rounds in a frame and 10 frames in a game', function() {
+		bowling.roll(1);
+		bowling.roll(8);
+		bowling.roll(1);
+		bowling.roll(3);
+		bowling.roll(5);
+		expect(bowling.framescore()).toEqual(5);
+		expect(bowling.currentFrame()).toEqual(3);
 	});
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,6 +2,7 @@ function Bowling(){
 	this._pins = 10;
 	this._score = 0;
 	this._cumalativescore = [];
+	this._frame = 1;
 }
 
 Bowling.prototype.pins = function () {
@@ -17,14 +18,27 @@ Bowling.prototype.currentTotalScore = function () {
 };
 
 Bowling.prototype.roll = function (number) {
+	if(this._cumalativescore.length%2 == 0 && this._cumalativescore.length > 0) {
+		this.newFrame()
+	}
 	if(number > this._pins) {
 		throw new Error ("Not possible!");
 	}
-	this._pins -= number;
 	this._score += number;
+	this._pins -= number;
 	this._cumalativescore.push(number)
 };
 
-Bowling.prototype.scores = function () {
+Bowling.prototype.newFrame = function () {
+	this._frame += 1
+	this._pins = 10
+	this._score = 0
+};
+
+Bowling.prototype.framescore = function () {
 	return this._score;
+};
+
+Bowling.prototype.currentFrame = function () {
+	return this._frame
 };

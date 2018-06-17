a338f826ac082eb69173cd0702368772948e4770
luciagore <lucy.gore@hotmail.co.uk>
2018-06-17 08:01:33 +0100

    can add the score cumulatively and throws error if you add impossible score

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -8,13 +8,28 @@ describe('Bowling', function(){
 	});
 
 	it('starts a new game with 10 pins', function() {
-		expect(bowling.pins()).toEqual(10)
+		expect(bowling.pins()).toEqual(10);
 	});
 
 	it('user rolls a ball and knocks down pins', function() {
-		bowling.roll(4)
-		expect(bowling.pins()).toEqual(6)
+		bowling.roll(4);
+		expect(bowling.pins()).toEqual(6);
 	})
+
+	it('adds pins knocked down to a scorecard', function() {
+		bowling.roll(3);
+		expect(bowling._score).toEqual(3);
+	});
+
+	it('adds the scores together in the scorecard', function() {
+		bowling.roll(2);
+		bowling.roll(5);
+		expect(bowling._score).toEqual(7);
+	});
+
+	it('does not allow you to add a score more than there are available pins', function () {
+		expect(function(){bowling.roll(11);}).toThrowError("Not possible!")
+	});
 
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,12 +1,29 @@
 function Bowling(){
-	this._pins = 10
-
+	this._pins = 10;
+	this._score = 0;
+	this._cumalativescore = [];
 }
 
 Bowling.prototype.pins = function () {
-	return this._pins
+	return this._pins;
+};
+
+Bowling.prototype.currentTotalScore = function () {
+	var sum = this._cumalativescore.reduce(add, 0);
+	function add(a, b) {
+			return a + b;
+	}
+	return sum;
 };
 
 Bowling.prototype.roll = function (number) {
+	if(number > this._pins) {
+		throw new Error ("Not possible!");
+	}
 	this._pins -= number;
+	this._score += number;
+};
+
+Bowling.prototype.scores = function () {
+	return this._score;
 };

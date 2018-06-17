0fd574cbf2c9aae3fef3b60028016cea0e41b882
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-17 01:10:47 +0100

    Add scorecard total for all ten frames
    * Basic functionality with spies but needs refactoring

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -3,13 +3,55 @@
 describe('Scorecard', function() {
 
   var scorecard;
+  var frameOne;
+  var frameTwo;
+  var frameThree;
+  var frameFour;
+  var frameFive;
+  var frameSix;
+  var frameSeven;
+  var frameEight;
+  var frameNine;
+  var frameTen;
 
   beforeEach(function() {
-    scorecard = new Scorecard;
+    frameOne = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameTwo = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameThree = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameFour = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameFive = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameSix = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameSeven = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameEight = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameNine = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    frameTen = jasmine.createSpyObj('frame', ['getCurrentFrameScore']);
+    scorecard = new Scorecard(frameOne, frameTwo, frameThree, frameFour, frameFive, frameSix, frameSeven, frameEight, frameNine, frameTen);
   });
 
   it('has a default total score of zero', function() {
     expect(scorecard.totalScore).toEqual(0);
+  });
+
+  describe('with a top score', function() {
+
+    beforeEach(function() {
+      frameOne.getCurrentFrameScore.and.returnValue(30);
+      frameTwo.getCurrentFrameScore.and.returnValue(30);
+      frameThree.getCurrentFrameScore.and.returnValue(30);
+      frameFour.getCurrentFrameScore.and.returnValue(30);
+      frameFive.getCurrentFrameScore.and.returnValue(30);
+      frameSix.getCurrentFrameScore.and.returnValue(30);
+      frameSeven.getCurrentFrameScore.and.returnValue(30);
+      frameEight.getCurrentFrameScore.and.returnValue(30);
+      frameNine.getCurrentFrameScore.and.returnValue(30);
+      frameTen .getCurrentFrameScore.and.returnValue(30);
+    });
+
+    it('can calculate the total socre of ten frames', function() {
+      scorecard.calculateScore();
+      expect(scorecard.totalScore).toEqual(300);
+    });
+
   });
 
 });
--- src/scoreCard.js
+++ src/scoreCard.js
@@ -1,6 +1,24 @@
 'use strict';
 
-function Scorecard () {
+function Scorecard (frameOne, frameTwo, frameThree, frameFour, frameFive, frameSix, frameSeven, frameEight, frameNine, frameTen) {
   this.DEFAULT_SCORE = 0;
   this.totalScore = this.DEFAULT_SCORE;
+  this.MINIMUM_SCORE = 0;
+  this.MAXIMUM_SCORE = 300;
+  this.frameOne = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameTwo = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameThree = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameFour = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameFive = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameSix = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameSeven = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameEight = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameNine = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+  this.frameTen = typeof frameOne !== 'undefined' ? frameOne : new Frame();
+};
+
+Scorecard.prototype.calculateScore = function() {
+  var sum = (this.frameOne.getCurrentFrameScore() + this.frameTwo.getCurrentFrameScore() + this.frameThree.getCurrentFrameScore() + this.frameFour.getCurrentFrameScore() + this.frameFive.getCurrentFrameScore() + this.frameSix.getCurrentFrameScore() + this.frameSeven.getCurrentFrameScore() + this.frameEight.getCurrentFrameScore() + this.frameNine.getCurrentFrameScore() + this.frameTen.getCurrentFrameScore())
+  this.totalScore += sum;
+  return this.totalScore;
 };

b6b2d2ff71024e88ce901b5e273746f863191515
Robert Faldo <robertfaldo@gmail.com>
2018-06-23 17:57:48 +0100

    Add part of 2nd frame logic
    Add logic for when second frame is score is under 10 and the first
    frame is also under 10

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -3,7 +3,7 @@
 function Scorecard(){
   this._currentScore = 0;
   this._scoreCard = [];
-  this._currentFrame = 0;
+  this._currentFrame = 1;
 }
 
 Scorecard.prototype.showScore = function(){
@@ -13,28 +13,49 @@ Scorecard.prototype.showScore = function(){
 Scorecard.prototype.addFrameScore = function(firstBall, secondBall){
   var thisFrameScore = firstBall + secondBall;
 
-  if(thisFrameScore !== 10) {
-    this._currentScore += thisFrameScore;
+  if (this._currentFrame === 1) {
+    if(thisFrameScore !== 10) {
+      this._currentScore += thisFrameScore;
+    }
   }
-  if(this._currentFrame > 0 && this.previousFrameFirstPin() === 10){
-    this._currentScore += 10 + thisFrameScore
+
+  if(this._currentFrame === 2) {
+    if(thisFrameScore < 10){
+      if(this._isPreviousFrameSpare()) {
+        this._currentScore += 10 + firstBall + thisFrameScore;
+      }
+      if(this._isPreviousFrameStrike()) {
+        this._currentScore += 10 + thisFrameScore + thisFrameScore;
+      }
+      if(this.previousFrameScore() < 10) {
+        this._currentScore += thisFrameScore;
+      }
+    }
   }
-  // if the last frame total score was 10
-  else if (this._currentFrame > 0 && this.previousFrameScore() === 10){
-    this._currentScore += 10 + firstBall
-  }
+
   this._scoreCard.push([firstBall, secondBall]);
   this._currentFrame += 1;
 };
 
+
 Scorecard.prototype.previousFrameScore = function() {
   return this._scoreCard[this._scoreCard.length -1][0] +
-    this._scoreCard[this._scoreCard.length -1][0];
+    this._scoreCard[this._scoreCard.length -1][1];
 }
 
 Scorecard.prototype.previousFrameFirstPin = function() {
   return this._scoreCard[this._scoreCard.length -1][0];
 }
+
+Scorecard.prototype._isPreviousFrameSpare = function(){
+  return this._scoreCard[this._scoreCard.length -1][0] !== 10 &&
+    this.previousFrameScore() === 10;
+}
+
+Scorecard.prototype._isPreviousFrameStrike = function(){
+  return this._scoreCard[this._scoreCard.length -1][0] === 10
+}
+
 
 // If current score is not a strike
   // if previous score is strike then check if the score before is a strike

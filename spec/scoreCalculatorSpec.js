ae2d2d6a10fdb212e74344e7f5157c96356d1d7a
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-23 14:31:33 +0100

    Calculate total score of array with bonuses

--- spec/scoreCalculatorSpec.js
+++ spec/scoreCalculatorSpec.js
@@ -3,7 +3,6 @@
 describe('Score Calculator', function() {
 
   var scoreCalculator;
-  var scoreArgs;
 
   beforeEach(function() {
     scoreCalculator = new ScoreCalculator();
@@ -18,10 +17,19 @@ describe('Score Calculator', function() {
   });
 
   it('accepts a list of scores and stores them in the scores array', function() {
-    var scores = ([[10, 1], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]])
-    console.log(scores.length);
+    var scores = ([[10, 0], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]])
     scoreCalculator.addScore(scores);
-    expect(scoreCalculator.scores).toContain([10, 1], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]);
+    expect(scoreCalculator.scores).toEqual([[10, 0], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]]);
   });
+
+  it('calculates a total score for the game', function() {
+    var scores = ([[1, 4], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 4], [10, 0], [2, 8], [6, 0]])
+    scoreCalculator.addScore(scores);
+    scoreCalculator.calculateScore();
+    console.log(scoreCalculator.totalScore);
+    expect(scoreCalculator.totalScore).toEqual(133);
+  });
+
+  // sort the iteration, it's only going through the first four numbers, it's not looping. So I do need to have a second loop.
 
 });
--- src/scoreCalculator.js
+++ src/scoreCalculator.js
@@ -13,5 +13,47 @@ ScoreCalculator.prototype.getScore = function() {
 ScoreCalculator.prototype.addScore = function(scores) {
   for(var i = 0; i < (scores.length); i++) {
     this.scores.push(scores[i]);
-  }
+  };
 };
+
+console.log(1)
+ScoreCalculator.prototype.calculateScore = function() {
+  var i = 0;
+  var j = 1;
+  var k = 2;
+  var scores = this.scores
+  var length = scores.length;
+  console.log('score is ', this.totalScore);
+  console.log(length);
+  console.log(2)
+  for(; i < 10; (i++, j++, k++)) {
+    var a = (scores[i] === undefined) ? 0 : scores[i][0];
+    var b = (scores[i] === undefined) ? 0 : scores[i][1];
+    var c = (scores[j] === undefined) ? 0 : scores[j][0];
+    var d = (scores[j] === undefined) ? 0 : scores[j][1];
+    var e = (scores[k] === undefined) ? 0 : scores[k][0];
+    console.log(3)
+    console.log('this is a: ', a);
+    console.log('this is b: ', b);
+    console.log('this is c: ', c);
+    console.log('this is d: ', d);
+    console.log('this is e: ', e);
+    if (a === 10 && c === 10) {
+      console.log(4)
+      this.totalScore += (a + c + e);
+    } else if (a === 10) {
+      console.log(5)
+      this.totalScore += (a + c + d);
+    } else if (a + b === 10) {
+      console.log(6)
+      this.totalScore += (a + b + c);
+    } else {
+      console.log(7)
+      this.totalScore += (a + b);
+    }
+    console.log(8)
+    console.log('score is ', this.totalScore);
+  }
+  console.log(9)
+};
+console.log(10)

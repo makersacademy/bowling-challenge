0eb4965c9163aa86047de9c1e2c1a16a7b6dc004
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-23 16:39:08 +0100

    Further reformating of testing documentation

--- spec/scoreDisplaySpec.js
+++ spec/scoreDisplaySpec.js
@@ -9,30 +9,37 @@ describe('Score Display', function() {
     expect(scoreDisplay.getTotalScore()).toEqual(133);
   });
 
-  it('identifies a gutter game', function() {
-    scoreDisplay = new ScoreDisplay(0);
-    expect(scoreDisplay.isGutterGame()).toEqual(true);
-  });
-
-  it('identifies when the score is not a gutter game', function() {
-    scoreDisplay = new ScoreDisplay(132);
-    expect(scoreDisplay.isGutterGame()).toEqual(false);
-  });
-
-  it('identifies a perfect scoring game', function() {
-    scoreDisplay = new ScoreDisplay(300);
-    expect(scoreDisplay.isPerfectGame()).toEqual(true);
-  });
-
-  it('identifies when the score is not a perfect game', function() {
-    scoreDisplay = new ScoreDisplay(149);
-    expect(scoreDisplay.isPerfectGame()).toEqual(false);
-  });
-
   it('resets the to zero', function() {
     scoreDisplay = new ScoreDisplay(125);
     scoreDisplay.reset();
     expect(scoreDisplay.totalScore).toEqual(0);
   });
+
+  describe('Identifies a score as', function() {
+
+    it('a gutter game', function() {
+      scoreDisplay = new ScoreDisplay(0);
+      expect(scoreDisplay.isGutterGame()).toEqual(true);
+    });
+
+    it('not a gutter game', function() {
+      scoreDisplay = new ScoreDisplay(132);
+      expect(scoreDisplay.isGutterGame()).toEqual(false);
+    });
+
+    it('a perfect scoring game', function() {
+      scoreDisplay = new ScoreDisplay(300);
+      expect(scoreDisplay.isPerfectGame()).toEqual(true);
+    });
+
+    it('not a perfect scoring game', function() {
+      scoreDisplay = new ScoreDisplay(149);
+      expect(scoreDisplay.isPerfectGame()).toEqual(false);
+    });
+
+  });
+
+
+
 
 });

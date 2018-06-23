99bd7aefb2f29ebd73faed53fa95a7492914afad
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-23 16:00:10 +0100

    Add identification of gutter and perfect games
    * To be used in interface to highlight top scoring and lowest scoring
    games via the front end

--- spec/scoreDisplaySpec.js
+++ spec/scoreDisplaySpec.js
@@ -9,4 +9,24 @@ describe('Score Display', function() {
     expect(scoreDisplay.getTotalScore()).toEqual(133);
   });
 
+  it('identifies a gutter game', function() {
+    scoreDisplay = new ScoreDisplay(0);
+    expect(scoreDisplay.isGutterGame()).toEqual(true);
+  });
+
+  it('identifies when the score is not a gutter game', function() {
+    scoreDisplay = new ScoreDisplay(132);
+    expect(scoreDisplay.isGutterGame()).toEqual(false);
+  });
+
+  it('identifies a perfect scoring game', function() {
+    scoreDisplay = new ScoreDisplay(300);
+    expect(scoreDisplay.isPerfectGame()).toEqual(true);
+  });
+
+  it('identifies when the score is not a perfect game', function() {
+    scoreDisplay = new ScoreDisplay(149);
+    expect(scoreDisplay.isPerfectGame()).toEqual(false);
+  });
+
 });
--- src/scoreDisplay.js
+++ src/scoreDisplay.js
@@ -1,10 +1,19 @@
 'use strict';
 
 function ScoreDisplay(score) {
-  this.DEFAULT_SCORE = 0;
   this.totalScore = score;
+  this.GUTTER_GAME = 0;
+  this.PERFECT_GAME = 300;
 };
 
 ScoreDisplay.prototype.getTotalScore = function() {
   return this.totalScore;
 };
+
+ScoreDisplay.prototype.isGutterGame = function() {
+  return this.totalScore === this.GUTTER_GAME;
+};
+
+ScoreDisplay.prototype.isPerfectGame = function() {
+  return this.totalScore === this.PERFECT_GAME;
+}

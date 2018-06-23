80150aafa43feb5a26da342dcbb84c2c2c15efe1
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-23 15:31:15 +0100

    Create score display constructor function
    * Communicates score details with the interface

--- spec/scoreDisplaySpec.js
+++ spec/scoreDisplaySpec.js
@@ -1,1 +1,12 @@
 'use strict';
+
+describe('Score Display', function() {
+
+  var scoreDisplay;
+
+  it('returns the total score', function() {
+    scoreDisplay = new ScoreDisplay(133);
+    expect(scoreDisplay.getTotalScore()).toEqual(133);
+  });
+
+});
--- src/scoreDisplay.js
+++ src/scoreDisplay.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function ScoreDisplay(score) {
+  this.DEFAULT_SCORE = 0;
+  this.totalScore = score;
+};
+
+ScoreDisplay.prototype.getTotalScore = function() {
+  return this.totalScore;
+};

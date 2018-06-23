e85b6ad7f533a45b32f1f47e3d9abfedec58ca9f
luciagore <lucy.gore@hotmail.co.uk>
2018-06-23 15:57:04 +0100

    removing irrelevant tests and simplifying structure

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,43 +7,28 @@ describe('Bowling', function(){
 		bowling = new Bowling();
 	});
 
-	it('starts a new frame with 10 pins', function() {
-		expect(bowling.currentpins()).toEqual(10);
-	});
-
-	it('user rolls a ball and knocks down pins', function() {
-		bowling.roll(4);
-		expect(bowling.currentpins()).toEqual(6);
-	})
-
 	it('adds the scores together in the scorecard', function() {
 		rollMultiple(4, 20)
 		expect(bowling.score()).toEqual(80);
 	});
 
-	it('does not allow player to input invalid score', function() {
-		expect(function(){bowling.roll(11);}).toThrowError("Not possible!")
-	});
-
-	it('adds each individual score to a _cumalativescore array', function() {
+	it('adds each individual score to a cumalativescore array', function() {
 		rollMultiple(2, 4)
 		expect(bowling.cumalativescore).toEqual([2, 2, 2, 2]);
-	});
-
-	it('can know there are two rounds in a frame', function() {
-		rollMultiple(4, 5)
-		expect(bowling.currentFrame()).toEqual(3);
-	});
-
-	it('ends the game when 10 frames have been played', function() {
-		rollMultiple(1, 20)
-		expect(function(){bowling.roll(1);}).toThrowError("Game finished: Your final score is ");
 	});
 
 	it('awards a spare bonus if the player knocks down all 10 pins in one frame', function() {
 		rollMultiple(5, 3)
 		rollMultiple(0, 17)
 		expect(bowling.score()).toEqual(20);
+	});
+
+	it('awards a strike bonus if the player knocks down all 10 pins in first turn of frame', function() {
+		bowling.roll(10);
+		rollMultiple(4, 2);
+		rollMultiple(0, 16)
+		expect(bowling.score()).toEqual(26)
+
 	});
 
 	var rollMultiple = function (number, rolls) {
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,53 +1,46 @@
 function Bowling(){
-	this.pins = 10;
-	this.framescore = 0;
 	this.cumalativescore = [];
-	this.frame = 1;
 }
-
-Bowling.prototype.currentpins = function () {
-	return this.pins;
-};
 
 Bowling.prototype.score = function () {
 	var sum = 0;
 	var roll = 0;
+	var bowlinggame = this;
 	for (var frame = 0; frame < 10; frame++) {
-		if (this.cumalativescore[roll] + this.cumalativescore[roll + 1] == 10) {
-			sum += this.cumalativescore[roll] + this.cumalativescore[roll + 1] + this.cumalativescore[roll + 2];
+		if (strike()) {
+			sum += scoreifstrike();
+			roll++;
+		} else if (spare()) {
+			sum += scoreifspare();
+			roll += 2;
 		} else {
-			sum += (this.cumalativescore[roll] + this.cumalativescore[roll + 1]);
+			sum += scoreifNotspare();
+			roll += 2;
 		}
-		roll += 2;
 	};
 	return sum;
+
+	function spare() {
+		return bowlinggame.cumalativescore[roll] + bowlinggame.cumalativescore[roll + 1] == 10;
+	}
+
+	function scoreifspare() {
+		return bowlinggame.cumalativescore[roll] + bowlinggame.cumalativescore[roll + 1] + bowlinggame.cumalativescore[roll + 2];
+	}
+
+	function scoreifNotspare() {
+		return bowlinggame.cumalativescore[roll] + bowlinggame.cumalativescore[roll + 1];
+	}
+
+	function strike () {
+		return bowlinggame.cumalativescore[roll] == 10;
+	}
+
+	function scoreifstrike() {
+		return bowlinggame.cumalativescore[roll] + bowlinggame.cumalativescore[roll + 1] + bowlinggame.cumalativescore[roll + 2];
+	}
 };
 
 Bowling.prototype.roll = function (number) {
-	if(this.cumalativescore.length%2 == 0 && this.cumalativescore.length > 0) {
-		this.newFrame();
-	};
-	if(number > this.pins) {
-		throw new Error ("Not possible!");
-	};
-	this.pins -= number;
-	this.framescore += number;
 	this.cumalativescore.push(number);
-};
-
-Bowling.prototype.newFrame = function () {
-	if(this.currentFrame() >= 10) {
-		throw new Error ("Game finished: Your final score is ");
-	}
-	this.frame += 1;
-	this.pins = 10;
-	this.framescore = 0;
-};
-
-Bowling.prototype.framescore = function () {
-	return this.framescore;
-};
-
-Bowling.prototype.currentFrame = function () {
-	return this.frame;
 };

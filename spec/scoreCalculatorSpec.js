8b2e892f686a8f4a7ad8e2eb3d15370361b9aa66
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-24 16:53:32 +0100

    Fix inconsistent Jasmine test code style

--- spec/scoreCalculatorSpec.js
+++ spec/scoreCalculatorSpec.js
@@ -1,70 +1,70 @@
-'use strict';
+"use strict";
 
-describe('Score Calculator', function() {
+describe("Score Calculator", function() {
 
-  var scoreCalculator;
+	var scoreCalculator;
 
-  beforeEach(function() {
-    scoreCalculator = new ScoreCalculator();
-  });
+	beforeEach(function() {
+		scoreCalculator = new ScoreCalculator();
+	});
 
-  it('is intialised with a container for scores', function() {
-    expect(scoreCalculator.scores).toEqual([]);
-  });
+	it("is intialised with a container for scores", function() {
+		expect(scoreCalculator.scores).toEqual([]);
+	});
 
-  it('has a default score of zero', function() {
-    expect(scoreCalculator.getScore()).toEqual(0);
-  });
+	it("has a default score of zero", function() {
+		expect(scoreCalculator.getScore()).toEqual(0);
+	});
 
-  it('clears the scores', function() {
-    var scores = ([[8, 2], [9, 0], [4, 4], [7, 2], [9, 0], [10, 0], [10, 0], [8, 0], [3, 5], [9, 1], [7, 0]])
-    scoreCalculator.addScore(scores);
-    scoreCalculator.clearScore();
-    expect(scoreCalculator.scores).toEqual([]);
-  });
+	it("clears the scores", function() {
+		var scores = ([[8, 2], [9, 0], [4, 4], [7, 2], [9, 0], [10, 0], [10, 0], [8, 0], [3, 5], [9, 1], [7, 0]]);
+		scoreCalculator.addScore(scores);
+		scoreCalculator.clearScore();
+		expect(scoreCalculator.scores).toEqual([]);
+	});
 
-  it('accepts a list of scores and stores them in the scores array', function() {
-    var scores = ([[10, 0], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]])
-    scoreCalculator.addScore(scores);
-    expect(scoreCalculator.scores).toEqual([[10, 0], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]]);
-  });
+	it("accepts a list of scores and stores them in the scores array", function() {
+		var scores = ([[10, 0], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]]);
+		scoreCalculator.addScore(scores);
+		expect(scoreCalculator.scores).toEqual([[10, 0], [2, 6], [4, 4], [2, 6], [1, 6], [2, 1], [2, 6], [4, 4], [1, 6], [3, 6], [0, 0]]);
+	});
 
-  describe('Calculates the total score', function() {
+	describe("Calculates the total score", function() {
 
-    it('for a varied game', function() {
-      var scores = ([[1, 4], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 4], [10, 0], [2, 8], [6, 0]])
-      scoreCalculator.addScore(scores);
-      scoreCalculator.calculateScore();
-      expect(scoreCalculator.totalScore).toEqual(133);
-    });
+		it("for a varied game", function() {
+			var scores = ([[1, 4], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 4], [10, 0], [2, 8], [6, 0]]);
+			scoreCalculator.addScore(scores);
+			scoreCalculator.calculateScore();
+			expect(scoreCalculator.totalScore).toEqual(133);
+		});
 
-    it('for a perfect game', function() {
-      var scores = ([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10]])
-      scoreCalculator.addScore(scores);
-      scoreCalculator.calculateScore();
-      expect(scoreCalculator.totalScore).toEqual(300);
-    });
+		it("for a perfect game", function() {
+			var scores = ([[10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 0], [10, 10]]);
+			scoreCalculator.addScore(scores);
+			scoreCalculator.calculateScore();
+			expect(scoreCalculator.totalScore).toEqual(300);
+		});
 
-    it('for a game with a range of spares', function() {
-      var scores = ([[8, 0], [7, 0], [5, 3], [9, 1], [9, 1], [10, 0], [8, 0], [5, 1], [3, 7], [9, 0], [0, 0]])
-      scoreCalculator.addScore(scores);
-      scoreCalculator.calculateScore();
-      expect(scoreCalculator.totalScore).toEqual(122);
-    });
+		it("for a game with a range of spares", function() {
+			var scores = ([[8, 0], [7, 0], [5, 3], [9, 1], [9, 1], [10, 0], [8, 0], [5, 1], [3, 7], [9, 0], [0, 0]]);
+			scoreCalculator.addScore(scores);
+			scoreCalculator.calculateScore();
+			expect(scoreCalculator.totalScore).toEqual(122);
+		});
 
-    it('for a game with a mix of spares and strikes', function() {
-      var scores = ([[8, 2], [9, 0], [4, 4], [7, 2], [9, 0], [10, 0], [10, 0], [8, 0], [3, 5], [9, 1], [7, 0]])
-      scoreCalculator.addScore(scores);
-      scoreCalculator.calculateScore();
-      expect(scoreCalculator.totalScore).toEqual(133);
-    });
+		it("for a game with a mix of spares and strikes", function() {
+			var scores = ([[8, 2], [9, 0], [4, 4], [7, 2], [9, 0], [10, 0], [10, 0], [8, 0], [3, 5], [9, 1], [7, 0]]);
+			scoreCalculator.addScore(scores);
+			scoreCalculator.calculateScore();
+			expect(scoreCalculator.totalScore).toEqual(133);
+		});
 
-    it('for a gutter game', function() {
-      var scores = ([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]])
-      scoreCalculator.addScore(scores);
-      scoreCalculator.calculateScore();
-      expect(scoreCalculator.totalScore).toEqual(0);
-    });
-  });
+		it("for a gutter game", function() {
+			var scores = ([[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]);
+			scoreCalculator.addScore(scores);
+			scoreCalculator.calculateScore();
+			expect(scoreCalculator.totalScore).toEqual(0);
+		});
+	});
 
 });
--- spec/scoreDisplaySpec.js
+++ spec/scoreDisplaySpec.js
@@ -1,43 +1,43 @@
-'use strict';
+"use strict";
 
-describe('Score Display', function() {
+describe("Score Display", function() {
 
-  var scoreDisplay;
+	var scoreDisplay;
 
-  it('returns the total score', function() {
-    scoreDisplay = new ScoreDisplay(133);
-    expect(scoreDisplay.getTotalScore()).toEqual(133);
-  });
+	it("returns the total score", function() {
+		scoreDisplay = new ScoreDisplay(133);
+		expect(scoreDisplay.getTotalScore()).toEqual(133);
+	});
 
-  it('resets the score to zero', function() {
-    scoreDisplay = new ScoreDisplay(125);
-    scoreDisplay.reset();
-    expect(scoreDisplay.totalScore).toEqual(0);
-  });
+	it("resets the score to zero", function() {
+		scoreDisplay = new ScoreDisplay(125);
+		scoreDisplay.reset();
+		expect(scoreDisplay.totalScore).toEqual(0);
+	});
 
-  describe('Identifies a score as', function() {
+	describe("Identifies a score as", function() {
 
-    it('a gutter game', function() {
-      scoreDisplay = new ScoreDisplay(0);
-      expect(scoreDisplay.isGutterGame()).toEqual(true);
-    });
+		it("a gutter game", function() {
+			scoreDisplay = new ScoreDisplay(0);
+			expect(scoreDisplay.isGutterGame()).toEqual(true);
+		});
 
-    it('not a gutter game', function() {
-      scoreDisplay = new ScoreDisplay(132);
-      expect(scoreDisplay.isGutterGame()).toEqual(false);
-    });
+		it("not a gutter game", function() {
+			scoreDisplay = new ScoreDisplay(132);
+			expect(scoreDisplay.isGutterGame()).toEqual(false);
+		});
 
-    it('a perfect scoring game', function() {
-      scoreDisplay = new ScoreDisplay(300);
-      expect(scoreDisplay.isPerfectGame()).toEqual(true);
-    });
+		it("a perfect scoring game", function() {
+			scoreDisplay = new ScoreDisplay(300);
+			expect(scoreDisplay.isPerfectGame()).toEqual(true);
+		});
 
-    it('not a perfect scoring game', function() {
-      scoreDisplay = new ScoreDisplay(149);
-      expect(scoreDisplay.isPerfectGame()).toEqual(false);
-    });
+		it("not a perfect scoring game", function() {
+			scoreDisplay = new ScoreDisplay(149);
+			expect(scoreDisplay.isPerfectGame()).toEqual(false);
+		});
 
-  });
+	});
 
 
 

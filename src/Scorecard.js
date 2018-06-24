57ca2606dbeb0f9324cf928581cb8f38a644f19d
Robert Faldo <robertfaldo@gmail.com>
2018-06-24 17:45:06 +0100

    Refactor updateFrameScore
    All the big if statements have been extracted

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -45,57 +45,16 @@ Scorecard.prototype._updateSecondFrameScore = function(firstBall, secondBall, th
   }
 }
 
+
 Scorecard.prototype._updateFrameScore = function(firstBall, secondBall, thisFrameScore){
-  // when current frame is under 10
   if(thisFrameScore < 10) {
-    // when previous frame is under 10
-    if(this._previousFrameScore() < 10) {
-      this._addCurrentFrameScore(thisFrameScore);
-    }
-    // when previous frame is a strike
-    if(this._isPreviousFrameStrike()) {
-      if(this._previousFrameScore(2) < 10) {
-        // HEREEEE
-        this._currentScore += 10 + thisFrameScore * 2;
-      }
-      if(this._isPreviousFrameStrike(2)) {
-        this._currentScore += 30 + firstBall + thisFrameScore * 2;
-      }
-      if(this._isPreviousFrameSpare(2)) {
-        this._currentScore += 10 + thisFrameScore * 2;
-      }
-    }
-    // when previous frame is a spare
-    if(this._isPreviousFrameSpare()){
-      this._currentScore += 10 + firstBall + thisFrameScore;
-    }
+    this._updateScoreForFrameScoreUnder10(firstBall, secondBall, thisFrameScore);
   }
-  // when current frame is a spare
   if(firstBall !== 10 && thisFrameScore === 10) {
-    if(this._isPreviousFrameSpare()){
-      this._currentScore += 10 + firstBall;
-    }
-    // when the previous frame is a strike
-    if(this._isPreviousFrameStrike()){
-      if(this._previousFrameScore(2) < 10){
-        this._currentScore += 20;
-      }
-      if(this._isPreviousFrameStrike(2)){
-        this._currentScore += 40 + firstBall;
-      }
-      if(this._isPreviousFrameSpare(2)){
-        this._currentScore += 20;
-      }
-    }
+    this._updateScoreForFrameSpare(firstBall, secondBall, thisFrameScore);
   }
-  // when current frame is a strike
   if(firstBall === 10){
-    if(this._isPreviousFrameSpare()){
-      this._currentScore += 20;
-    }
-    if(this._isPreviousFrameStrike() && this._isPreviousFrameStrike(2)) {
-      this._currentScore += 30;
-    }
+    this._updateScoreForFrameStrike(firstBall, secondBall, thisFrameScore);
   }
 }
 
@@ -152,6 +111,57 @@ Scorecard.prototype._updateScoreForSecondFrameSpare = function(firstBall){
     this._currentScore += 10 + firstBall;
   }
 }
+
+Scorecard.prototype._updateScoreForFrameScoreUnder10 = function(firstBall, secondBall, thisFrameScore){
+  if(this._previousFrameScore() < 10) {
+    this._addCurrentFrameScore(thisFrameScore);
+  }
+  // when previous frame is a strike
+  if(this._isPreviousFrameStrike()) {
+    if(this._previousFrameScore(2) < 10) {
+      // HEREEEE
+      this._currentScore += 10 + thisFrameScore * 2;
+    }
+    if(this._isPreviousFrameStrike(2)) {
+      this._currentScore += 30 + firstBall + thisFrameScore * 2;
+    }
+    if(this._isPreviousFrameSpare(2)) {
+      this._currentScore += 10 + thisFrameScore * 2;
+    }
+  }
+  // when previous frame is a spare
+  if(this._isPreviousFrameSpare()){
+    this._currentScore += 10 + firstBall + thisFrameScore;
+  }
+}
+
+Scorecard.prototype._updateScoreForFrameSpare = function(firstBall, secondBall, thisFrameScore){
+  if(this._isPreviousFrameSpare()){
+    this._currentScore += 10 + firstBall;
+  }
+  // when the previous frame is a strike
+  if(this._isPreviousFrameStrike()){
+    if(this._previousFrameScore(2) < 10){
+      this._currentScore += 20;
+    }
+    if(this._isPreviousFrameStrike(2)){
+      this._currentScore += 40 + firstBall;
+    }
+    if(this._isPreviousFrameSpare(2)){
+      this._currentScore += 20;
+    }
+  }
+}
+
+Scorecard.prototype._updateScoreForFrameStrike = function(firstBall, secondBall, thisFrameScore){
+  if(this._isPreviousFrameSpare()){
+    this._currentScore += 20;
+  }
+  if(this._isPreviousFrameStrike() && this._isPreviousFrameStrike(2)) {
+    this._currentScore += 30;
+  }
+}
+
 
 Scorecard.prototype._endFrame = function(firstBall, secondBall){
   this._currentFrame += 1;

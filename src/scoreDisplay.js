4f5de7574fb12d5237bf7a52992326f27e0c85f4
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-24 16:50:01 +0100

    Fix inconsistent code style
    * Install ESLint
    * Fix missing variable definitions
    * Fix inconsistent quotes

--- src/scoreDisplay.js
+++ src/scoreDisplay.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
 function ScoreDisplay(score) {
-  this.totalScore = score;
-  this.GUTTER_GAME = 0;
-  this.PERFECT_GAME = 300;
-};
+	this.totalScore = score;
+	this.GUTTER_GAME = 0;
+	this.PERFECT_GAME = 300;
+}
 
 ScoreDisplay.prototype.getTotalScore = function() {
-  return this.totalScore;
+	return this.totalScore;
 };
 
 ScoreDisplay.prototype.isGutterGame = function() {
-  return this.totalScore === this.GUTTER_GAME;
+	return this.totalScore === this.GUTTER_GAME;
 };
 
 ScoreDisplay.prototype.isPerfectGame = function() {
-  return this.totalScore === this.PERFECT_GAME;
+	return this.totalScore === this.PERFECT_GAME;
 };
 
 ScoreDisplay.prototype.reset = function() {
-  this.totalScore = 0;
+	this.totalScore = 0;
 };

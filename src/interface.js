51547e49e3d57d70928c43fd3930f5ef27488871
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-24 18:58:44 +0100

    Refactor extraction of scores in the interface

--- src/interface.js
+++ src/interface.js
@@ -8,25 +8,23 @@ $(document).ready( function() {
 		}
 	});
 
+	function extractScores() {
+		var frames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "bonus"]
+		var extractedFrameScores = []
+		frames.forEach(function(i) {
+			extractedFrameScores.push([parseInt($("#frame-" + i + "-roll-one").val(), 10), parseInt($("#frame-" + i + "-roll-two").val(), 10)]);
+		});
+		return extractedFrameScores;
+	};
+
 	$("#submit").click ( function(event) {
 		event.preventDefault();
-		var frameOne = [parseInt($("#frame-one-roll-one").val(), 10), parseInt($("#frame-one-roll-two").val(), 10)];
-		var frameTwo = [parseInt($("#frame-two-roll-one").val(), 10), parseInt($("#frame-two-roll-two").val(), 10)];
-		var frameThree = [parseInt($("#frame-three-roll-one").val(), 10), parseInt($("#frame-three-roll-two").val(), 10)];
-		var frameFour = [parseInt($("#frame-four-roll-one").val(), 10), parseInt($("#frame-four-roll-two").val(), 10)];
-		var frameFive = [parseInt($("#frame-five-roll-one").val(), 10), parseInt($("#frame-five-roll-two").val(), 10)];
-		var frameSix = [parseInt($("#frame-six-roll-one").val(), 10), parseInt($("#frame-six-roll-two").val(), 10)];
-		var frameSeven = [parseInt($("#frame-seven-roll-one").val(), 10), parseInt($("#frame-seven-roll-two").val(), 10)];
-		var frameEight = [parseInt($("#frame-eight-roll-one").val(), 10), parseInt($("#frame-eight-roll-two").val(), 10)];
-		var frameNine = [parseInt($("#frame-nine-roll-one").val(), 10), parseInt($("#frame-nine-roll-two").val(), 10)];
-		var frameTen = [parseInt($("#frame-ten-roll-one").val(), 10), parseInt($("#frame-ten-roll-two").val(), 10)];
-		var bonus = [parseInt($("#bonus-roll-one").val(), 10), parseInt($("#bonus-roll-two").val(), 10)];
-		var scores = [frameOne, frameTwo, frameThree, frameFour, frameFive, frameSix, frameSeven, frameEight, frameNine, frameTen, bonus];
+		var scores = extractScores();
 		scoreCalculator.addScore(scores);
 		scoreCalculator.calculateScore();
 		var gameScore = scoreCalculator.getScore();
 		var scoreDisplay = new ScoreDisplay(gameScore);
 		$("#total-score").text(scoreDisplay.getTotalScore());
 	});
-
+	
 });

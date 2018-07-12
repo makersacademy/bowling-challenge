6fe32250e0481e976e9b26499dcd7fe96bb150cf
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-12 14:14:54 +0100

    Change frameScore column to pinsKnocked column

--- interface.js
+++ interface.js
@@ -5,16 +5,19 @@ $(document).ready(function() {
     var current_frame = document.getElementById('current_frame');
     var current_roll = document.getElementById('current_roll');
     var pinsStanding = document.getElementById('pins_standing');
+
     var table = document.getElementById('bowling_table');
     var rollRow = table.insertRow(-1);
     var frameColumn = rollRow.insertCell(0);
     var rollColumn = rollRow.insertCell(1);
-    var frameScoreColumn = rollRow.insertCell(2);
+    var pinsKnockedColumn = rollRow.insertCell(2);
     var totalScoreColumn = rollRow.insertCell(3);
     //Before bowling the ball
 
     frameColumn.innerHTML = `${game.current_frame}`;
     rollColumn.innerHTML = `${game.current_roll}`;
+    pinsKnockedColumn.innerHTML = `${score}`;
+
 
     //After rolling
     game.knock_pins(score)
@@ -22,9 +25,6 @@ $(document).ready(function() {
     current_frame.innerHTML = `${game.current_frame}`;
     current_roll.innerHTML = `${game.current_roll}`;
     pinsStanding.innerHTML = `${game.pins}`;
-    
-    var totalFrameScore = game.frame_score.reduce((a, b) => a + b, 0);
-    frameScoreColumn.innerHTML = `${totalFrameScore}`;
 
     var totalScore = (game.score_card.concat.apply([], game.score_card)).reduce((a, b) => a + b, 0);
     totalScoreColumn.innerHTML = `${totalScore}`;

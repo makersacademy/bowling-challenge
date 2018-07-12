59d031632ee5c64a58a89342214e6f6cc1075ef5
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-12 14:30:33 +0100

    ScoreSelector updates after rolling

--- interface.js
+++ interface.js
@@ -5,21 +5,17 @@ $(document).ready(function() {
     var current_frame = document.getElementById('current_frame');
     var current_roll = document.getElementById('current_roll');
     var pinsStanding = document.getElementById('pins_standing');
-
     var table = document.getElementById('bowling_table');
     var rollRow = table.insertRow(-1);
     var frameColumn = rollRow.insertCell(0);
     var rollColumn = rollRow.insertCell(1);
     var pinsKnockedColumn = rollRow.insertCell(2);
     var totalScoreColumn = rollRow.insertCell(3);
-    //Before bowling the ball
 
     frameColumn.innerHTML = `${game.current_frame}`;
     rollColumn.innerHTML = `${game.current_roll}`;
     pinsKnockedColumn.innerHTML = `${score}`;
 
-
-    //After rolling
     game.knock_pins(score)
 
     current_frame.innerHTML = `${game.current_frame}`;
@@ -30,9 +26,21 @@ $(document).ready(function() {
     totalScoreColumn.innerHTML = `${totalScore}`;
   };
 
+  var updateScoreSelector = function(score) {
+    scoreSelect.options.length = 0;
+    var rollLimit = document.getElementById('pins_standing').innerHTML
+    for (let i = 0; i <= rollLimit; i++) {
+      var option = document.createElement('option');
+      option.text = `${i}`;
+      option.value = `${i}`;
+      scoreSelect.add(option, scoreSelect[i]);
+    };
+  };
+
   $('#roll').on('click', function() {
     var scoreSelect = document.getElementById('scoreSelect');
     var score = parseInt(scoreSelect.value, 10);
     updateTable(score);
+    updateScoreSelector(score);
   });
 });

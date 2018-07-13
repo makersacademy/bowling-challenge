ef3bb1eea4ae1f1e176b8bde95f106bc97167e5c
Jay Khan <jkhan992@gmail.com>
2018-07-13 22:02:47 +0100

    Scaffolding and setup

---
+++ jasmine/jasmine-standalone-3.1.0/spec/GameSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe("Bowling Game", function() {
+  var game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  it('A Game contains Frames', function() {
+    expect(game.frames).toBeTruthy();
+  });
+});

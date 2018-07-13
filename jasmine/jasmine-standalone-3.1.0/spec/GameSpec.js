bfa803399572d579865e0303070c13541cd791e2
Jay Khan <jkhan992@gmail.com>
2018-07-13 22:49:53 +0100

    Feature: A User can Roll 2 times per Frame

--- jasmine/jasmine-standalone-3.1.0/spec/GameSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/GameSpec.js
@@ -10,4 +10,19 @@ describe("Bowling Game", function() {
   it('A Game contains Frames', function() {
     expect(game.getFrames()).toBeTruthy();
   });
+
+  it('A User can Roll 2 times per Frame', function() {
+    game.roll();
+    game.roll();
+    game.roll();
+    expect(game.getFrames()).toEqual(['X']);
+  });
+
+  // it('A User can Roll 20 times per Game', function() {
+  //   game.roll();
+  //   game.roll();
+  //   game.roll();
+  //   expect(game.getFrames()).toEqual(['X'], ['X']);
+  // });
+
 });
--- jasmine/jasmine-standalone-3.1.0/src/Game.js
+++ jasmine/jasmine-standalone-3.1.0/src/Game.js
@@ -2,8 +2,19 @@
 
 function Game() {
   this.frames = [];
+  this.rolls = 0
 }
 
 Game.prototype.getFrames = function() {
-    return this.frames
+  return this.frames
+};
+
+Game.prototype.roll = function() {
+ this.rolls += 1;
+
+  if (this.rolls === 2) {
+    this.frames.push('X');
+    this.rolls = 0;
+  }
+  return this.rolls;
 };

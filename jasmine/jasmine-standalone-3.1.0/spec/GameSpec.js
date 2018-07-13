cede36998fd965b429adcbd2774170a4d8ecfc56
Jay Khan <jkhan992@gmail.com>
2018-07-13 23:32:27 +0100

    Feature: A User can Roll 2 times and score 5 points. Introduced addFrame method to tests

--- jasmine/jasmine-standalone-3.1.0/spec/GameSpec.js
+++ jasmine/jasmine-standalone-3.1.0/spec/GameSpec.js
@@ -12,17 +12,44 @@ describe("Bowling Game", function() {
   });
 
   it('A User can Roll 2 times per Frame', function() {
-    game.roll();
-    game.roll();
-    game.roll();
+    game.roll(1);
+    game.roll(1);
+    game.roll(1);
     expect(game.getFrames()).toEqual(['X']);
   });
 
-  // it('A User can Roll 20 times per Game', function() {
+  // it('A User can Roll 3 times in Frame 10', function() {
+  //   game.frames = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
   //   game.roll();
   //   game.roll();
   //   game.roll();
-  //   expect(game.getFrames()).toEqual(['X'], ['X']);
+  //   expect(game.getFrames()).toEqual(['X']);
   // });
+
+  it('A User can Roll 20 times per Game', function() {
+    game.frames = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+    game.roll(1);
+    game.roll(1);
+    game.roll(1);
+    expect(game.frames.length).toEqual(10);
+  });
+
+  it('A User can Roll 20 times per Game', function() {
+
+    // This for loop adds 9 Frames to a Game
+    for (var i = 0; i < 10; i++) {
+      game.addFrame('X');
+    }
+    game.roll(1);
+    game.roll(1);
+    game.roll(1);
+    expect(game.frames.length).toEqual(10);
+  });
+
+  it('A User can Roll 2 times and score 5 points', function() {
+    game.roll(1);
+    game.roll(4);
+    expect(game.getPoints()).toEqual(5);
+  });
 
 });
--- jasmine/jasmine-standalone-3.1.0/src/Game.js
+++ jasmine/jasmine-standalone-3.1.0/src/Game.js
@@ -2,22 +2,33 @@
 
 function Game() {
   this.MAX_ROLLS = 2;
-  
+
   this.frames = [];
-  this.rolls = 0
+  this.rolls = 0;
+  this.score = 0;
+}
+
+Game.prototype.addFrame = function(frame){
+  this.frames.push(frame);
 }
 
 Game.prototype.getFrames = function() {
   return this.frames
 };
 
-Game.prototype.roll = function() {
+Game.prototype.roll = function(pins) {
+  this.score += pins
   this.rolls += 1;
 
   // 2 rolls = a completed Frame
-  if (this.rolls === this.MAX_ROLLS) {
+  if (this.frames.length <= 9 && this.rolls === this.MAX_ROLLS) {
     this.frames.push('X');
     this.rolls = 0;
   }
   return this.rolls;
+};
+
+
+Game.prototype.getPoints = function() {
+  return this.score
 };

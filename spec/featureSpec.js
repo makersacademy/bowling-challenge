189ee4ae5d85539f4858ca139ec66464bbb140a7
Cornelis Heuperman <cheuperman@gmail.com>
2018-07-14 13:21:41 +0100

    Set up testing framework and outline project intentions in README

---
+++ spec/featureSpec.js
@@ -0,0 +1,23 @@
+// As a bowler
+// So that I can know how I did
+// I want to see my total score
+//
+// As a bowler
+// So that good rolls are rewarded
+// I want to receive a bonus that equals my next roll when I roll a spare
+//
+// As a bowler
+// So that great rolls beget great rewards
+// I want to receive a bonus that equals my next two rolls when I roll a strike
+//
+// As a bowler
+// So that I get the bonus I deserve
+// I want to keep rolling for bonus points after the 10th frame
+//
+// As a bowler
+// So that I can know how I am doing
+// I want to see my score after every frame
+//
+// As a bowler
+// So that I can use the app more easily
+// I want a nice interactive interface

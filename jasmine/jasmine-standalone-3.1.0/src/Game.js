bb0986d7e7092cc7496c7e940cd8e3ad4b250b68
Jay Khan <jkhan992@gmail.com>
2018-07-14 13:09:55 +0100

    Refactor: reqorded property name

--- jasmine/jasmine-standalone-3.1.0/src/Game.js
+++ jasmine/jasmine-standalone-3.1.0/src/Game.js
@@ -6,7 +6,7 @@ function Game() {
   this._frames = [];
   this._rolls = 0;
   // this property records total pins hit in a Frame
-  this._pinsHit = 0;
+  this._totalPinsHitFrame = 0;
   this._calculate = new CalculateScore();
 }
 
@@ -21,15 +21,15 @@ Game.prototype.getFrames = function() {
 Game.prototype.roll = function(kockedDownPins) {
   if (this._rolls === 0) {
     this._calculate.storeRollScore(kockedDownPins);
-    this._pinsHit += kockedDownPins;
+    this._totalPinsHitFrame += kockedDownPins;
     this._rolls += 1;
     return "roll again"
   } else {
     this._rolls += 1; // 2 rolls
     this._calculate.storeRollScore(kockedDownPins);
-    this._pinsHit += kockedDownPins;
-    this._calculate.calculateScore(this._pinsHit);
-    this._pinsHit = 0;
+    this._totalPinsHitFrame += kockedDownPins;
+    this._calculate.calculateScore(this._totalPinsHitFrame);
+    this._totalPinsHitFrame = 0;
   }
 
   // TODO this logic is not in the corretc place
@@ -40,19 +40,15 @@ Game.prototype.roll = function(kockedDownPins) {
   // 2 rolls = a completed Frame
   if (this._frames.length <= 9 && this._rolls === this.MAX_ROLLS) {
     this._frames.push('X');
-
-
   }
   return this._rolls;
 };
 
 Game.prototype.score = function() {
-  // return this._score
   return this._calculate.getScore();
 };
 
 Game.prototype.getBonus = function() {
-  // return this._score
    return this._calculate.bonus(this._calculate._rollScore.length);
 };
 

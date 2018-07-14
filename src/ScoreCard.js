100b1486d9d5e602e6f0fbdff2caab6fa521da88
nabil far <nabil.far.contact@gmail.com>
2018-07-14 20:34:00 +0100

    a user can add a score to the second roll

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,13 +1,25 @@
 function ScoreCard (){
-  this.frame = []
-};
+  this.frame = [];
+  }
+
 
 
 
 ScoreCard.prototype.roll_1 = function(num) {
-  this.frame.push(num)
+  this.frame.push(num);
 };
 
-ScoreCard.prototype.getCurrentFrame = function() {
-  return this.frame[0]
+
+ScoreCard.prototype.getRoll_1 = function() {
+  return this.frame[0];
+};
+
+
+ScoreCard.prototype.roll_2 = function(num) {
+  this.frame.push(num);
+};
+
+ScoreCard.prototype.getRoll_2 = function() {
+  return this.frame[1];
+
 };

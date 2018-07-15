7210fe437e1beed652de4a2170b0d316e9d382f4
Elishka Flint <elishkaf@gmail.com>
2018-07-15 11:09:20 +0100

    TEST Add test for scorecard calculating a running total

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -1,20 +1,28 @@
 describe ('Scorecard', function() {
 
   var scorecard;
+  var frame1;
+  var frame2;
 
   beforeEach(function() {
     scorecard = new Scorecard();
+    frame1 = { number: 1, rolls: [], scores: [1,2] };
+    frame2 = { number: 2, rolls: [], scores: [3,4] };
   });
 
   it ('initializes with an empty frames array', function() {
-    console.log(scorecard)
     expect(scorecard.getFrames()).toEqual ([]);
   });
 
   it ('stores frames', function() {
-    var frame = new Frame(1);
-    scorecard.addFrame(frame);
-    expect(scorecard.getFrames()).toContain (frame);
+    scorecard.addFrame(frame1);
+    expect(scorecard.getFrames()).toContain (frame1);
+  });
+
+  it ('calculates a running total of all frames', function() {
+    scorecard.addFrame(frame1);
+    scorecard.addFrame(frame2);
+    expect(scorecard.totalScore()).toEqual(10);
   });
 
 });

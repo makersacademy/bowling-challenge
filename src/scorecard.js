240e224a8138bf79e05470219c8ba291152d1d20
Elishka Flint <elishkaf@gmail.com>
2018-07-15 12:24:51 +0100

    MODEL Set up scorecard with 10 empty frames

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,5 +1,16 @@
 function Scorecard() {
-  this.frames = [];
+  this.frames = [
+    frame1 =  new Frame(),
+    frame2 =  new Frame(),
+    frame3 =  new Frame(),
+    frame4 =  new Frame(),
+    frame5 =  new Frame(),
+    frame6 =  new Frame(),
+    frame7 =  new Frame(),
+    frame8 =  new Frame(),
+    frame9 =  new Frame(),
+    frame10 = new Frame(),
+  ];
 }
 
 Scorecard.prototype.getFrames = function() {

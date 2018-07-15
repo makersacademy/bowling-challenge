a3d0f567d9280a1d225e5a93dafb00d5818a5f95
nabil far <nabil.far.contact@gmail.com>
2018-07-15 22:41:00 +0100

    some small refactoring

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -8,7 +8,15 @@ describe('ScoreCard', function(){
     scorecard = new ScoreCard();
   });
 
-  it('adds a score to the first roll at the first frame', function(){
+  it('scoreboard starts with an empty array', function(){
+    expect(scorecard.getScoreBoard()).toEqual([])
+  });
+
+  it('should start with the first frame', function(){
+    expect(scorecard.getFrameNumber()).toEqual(1)
+  });
+
+  it('adds a score to the first roll', function(){
       scorecard.roll_1(5);
     expect(scorecard.getRoll_1()).toEqual(5);
   });
@@ -18,27 +26,15 @@ describe('ScoreCard', function(){
     expect(scorecard.checkForStrike()).toEqual("strike")
   });
 
-  it('should start with the first frame', function(){
-    expect(scorecard.getCurrentFrame()).toEqual(1)
-  });
+// --------
 
-  it('adds the current frame to the board', function(){
-    scorecard.roll_1(5);
-    scorecard.roll_2(3);
-    scorecard.addToFrame();
-    expect(scorecard.getBoard()).toEqual([[5,3],]);
+  it('should go to the next frame when hit a strike', function(){
+      scorecard.roll_1(10);
+      expect(scorecard.getNextFrame()).toEqual(2);
   });
 
 
-  it('should go to the next frame', function(){
-    expect(scorecard.getNextFrame()).toEqual(2);
-  });
-
-
-  it('scoreboard starts with an empty object', function(){
-    expect(scorecard.getScoreBoard()).toEqual([])
-  });
-
+// --------
 
 
   it('adds a score to the second roll at the first frame', function(){
@@ -47,13 +43,21 @@ describe('ScoreCard', function(){
     expect(scorecard.getRoll_2()).toEqual(3);
   });
 
-
-
   it('should know if the second roll is a spare', function(){
     scorecard.roll_1(5);
     scorecard.roll_2(5);
     expect(scorecard.checkForSpare()).toEqual("spare")
   });
+
+
+
+  it('adds the current frame to the board', function(){
+    scorecard.roll_1(5);
+    scorecard.roll_2(3);
+    scorecard.addFrameToBoard();
+    expect(scorecard.getBoard()).toEqual([[5,3],]);
+  });
+
 
   it('should know the total points of a frame', function(){
     scorecard.roll_1(3);
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,41 +1,65 @@
 'use strict';
 
 function ScoreCard (){
+  this.board = [];
   this.currenFrame = 1;
+
   this.frame = [];
   this.total = 0;
-  this.board = [];
+
 };
 
-
-  ScoreCard.prototype.roll_1 = function(value) {
-    this.frame.push(value);
-    this.checkForStrike();
-
-  };
-
-  ScoreCard.prototype.checkForStrike = function(){
-    if(this.frame[0] === 10) {
-        return "strike";
-  };
-};
-
-
-ScoreCard.prototype.getCurrentFrame = function() {
-  return this.currenFrame
-};
-
-
-ScoreCard.prototype.getNextFrame = function() {
-  return this.currenFrame += 1
-};
 
 ScoreCard.prototype.getScoreBoard = function() {
     return this.board
 };
 
+ScoreCard.prototype.getFrameNumber = function() {
+  return this.currenFrame
+};
 
-ScoreCard.prototype.addToFrame = function() {
+ScoreCard.prototype.roll_1 = function(value) {
+  this.frame.push(value);
+};
+
+ScoreCard.prototype.getRoll_1 = function() {
+  return this.frame[0];
+};
+
+ScoreCard.prototype.checkForStrike = function(){
+  if(this.frame[0] === 10) {
+    return "strike";
+  };
+};
+
+// --------
+
+ScoreCard.prototype.getNextFrame = function() {
+  return this.currenFrame += 1
+};
+
+
+// --------
+
+
+
+ScoreCard.prototype.roll_2 = function(value) {
+  this.frame.push(value);
+};
+
+ScoreCard.prototype.getRoll_2 = function() {
+  return this.frame[1];
+};
+
+
+ScoreCard.prototype.checkForSpare = function(){
+  if((this.frame[0] + this.frame[1]) === 10) {
+    return "spare";
+  };
+};
+
+
+ScoreCard.prototype.addFrameToBoard = function() {
     this.board.push(this.frame);
 };
 
@@ -44,27 +68,6 @@ ScoreCard.prototype.getBoard = function() {
 };
 
 
-  ScoreCard.prototype.getRoll_1 = function() {
-    return this.frame[0];
-  };
-
-  ScoreCard.prototype.roll_2 = function(value) {
-    this.frame.push(value);
-  };
-
-  ScoreCard.prototype.getRoll_2 = function() {
-    return this.frame[1];
-  };
-
-
-
-  ScoreCard.prototype.checkForSpare = function(){
-    if((this.frame[0] + this.frame[1]) === 10) {
-        return "spare";
-  };
-};
-
 ScoreCard.prototype.getTotalFrame = function(){
   return this.total += this.frame[0] + this.frame[1]
-
 };

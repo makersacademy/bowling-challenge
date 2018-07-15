95b96ec335b854a9f6ab9a183dd6c829e4a65425
Elishka Flint <elishkaf@gmail.com>
2018-07-15 14:55:18 +0100

    REFACTOR Add more describe blocks to scorecard tests

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -8,38 +8,44 @@ describe ('Scorecard', function() {
     scorecard = new Scorecard();
   });
 
-  it ('initializes with an array of 10 frames', function() {
-    expect(scorecard.frames.length).toEqual (10);
+  describe('.initialize', function() {
+    it ('initializes with an array of 10 frames', function() {
+      expect(scorecard.frames.length).toEqual (10);
+    });
   });
 
-  it ('calculates a running total of all frame scores', function() {
-    scorecard.addScore(1, 3)
-    scorecard.addScore(2, 4)
-    expect(scorecard.totalScore()).toEqual (7);
+  describe('.addScore', function() {
+    it ('adds a score to the correct frame', function() {
+      var frame = 1;
+      var score = 5
+      scorecard.addScore(frame, score)
+      expect(scorecard.frames[(frame - 1)].totalScore()).toEqual (5);
+    });
   });
 
-  it ('adds a score to the correct frame', function() {
-    var frame = 1;
-    var score = 5
-    scorecard.addScore(frame, score)
-    expect(scorecard.frames[(frame - 1)].totalScore()).toEqual (5);
+  describe('.totalScore', function() {
+    it ('calculates a running total of all frame scores', function() {
+      scorecard.addScore(1, 3)
+      scorecard.addScore(2, 4)
+      expect(scorecard.totalScore()).toEqual (7);
+    });
   });
 
-  it ('checks the score type - ordinary number', function() {
-    scorecard.addScore(1, 3)
-    expect(scorecard.checkScoreType(1, 3)).toEqual('Ordinary Roll')
-  })
-
-  it ('checks the score type - spare', function() {
-    scorecard.addScore(1, 3)
-    scorecard.addScore(1, 7)
-    expect(scorecard.checkScoreType(1, 3)).toEqual('Spare')
-  })
-
-  it ('checks the score type - strike', function() {
-    scorecard.addScore(1, 10)
-    console.log(scorecard)
-    expect(scorecard.checkScoreType(1, 10)).toEqual('Strike')
-  })
+  describe('.checkScoreType', function() {
+    it ('checks the score type - ordinary number', function() {
+      scorecard.addScore(1, 3)
+      expect(scorecard.checkScoreType(1, 3)).toEqual('Ordinary Roll')
+    });
+    it ('checks the score type - spare', function() {
+      scorecard.addScore(1, 3)
+      scorecard.addScore(1, 7)
+      expect(scorecard.checkScoreType(1, 3)).toEqual('Spare')
+    });
+    it ('checks the score type - strike', function() {
+      scorecard.addScore(1, 10)
+      console.log(scorecard)
+      expect(scorecard.checkScoreType(1, 10)).toEqual('Strike')
+    });
+  });
 
 });

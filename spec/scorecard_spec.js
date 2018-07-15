e24394b51b1cc061347089844140900403924062
Elishka Flint <elishkaf@gmail.com>
2018-07-15 14:35:54 +0100

    TEST Add test for checking the type of roll

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -25,4 +25,20 @@ describe ('Scorecard', function() {
     expect(scorecard.frames[(frame - 1)].totalScore()).toEqual (5);
   });
 
+  it ('checks the score type - ordinary number', function() {
+    scorecard.addScore(1, 3)
+    expect(scorecard.checkScoreType(1, 3)).toEqual('Ordinary Roll')
+  })
+
+  it ('checks the score type - spare', function() {
+    scorecard.addScore(1, 3)
+    scorecard.addScore(1, 7)
+    expect(scorecard.checkScoreType(1, 3)).toEqual('Spare')
+  })
+
+  it ('checks the score type - strike', function() {
+    scorecard.addScore(1, 10)
+    expect(scorecard.checkScoreType(1, 3)).toEqual('Strike')
+  })
+
 });

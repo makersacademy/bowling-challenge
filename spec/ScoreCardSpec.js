7dc6a55effa95f2f1ff521316ae7126c561ec5fc
nabil far <nabil.far.contact@gmail.com>
2018-07-22 18:56:25 +0100

    knows when previous frame is a spare

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -8,13 +8,95 @@ describe('ScoreCard', function(){
     scorecard = new ScoreCard();
   });
 
-  it('scoreboard starts with an empty array', function(){
-    expect(scorecard.getScoreBoard()).toEqual([])
-  });
 
   it('should start with the first frame', function(){
     expect(scorecard.getFrameNumber()).toEqual(1)
   });
+
+
+  it('should move to the next frame', function(){
+    scorecard.nextFrame();
+    expect(scorecard.getFrameNumber()).toEqual(2);
+  });
+
+
+  it('should get the previous frame', function(){
+    scorecard.nextFrame();
+    expect(scorecard.getPreviousFrame()).toEqual(1);
+  });
+
+
+  it('starts on the first roll', function(){
+    expect(scorecard.getRollNumber()).toEqual(1);
+  });
+
+
+  it('should change the roll number', function(){
+    scorecard.changeRollNumber();
+    expect(scorecard.getRollNumber()).toEqual(2);
+  });
+
+
+
+it('scoreboard start at an empty object', function(){
+  expect(scorecard.getBoard()).toEqual({});
+});
+
+
+it('should add a score to the first roll and added to the scoreboard', function(){
+  scorecard.addRoll(5);
+  expect(scorecard.getBoard()).toEqual({1: [5, undefined]});
+});
+
+it('should add a 4 to the second roll and added to the scoreboard', function(){
+  scorecard.board = {1: [3, undefined]};
+  scorecard.changeRollNumber();
+  scorecard.addRoll(4);
+  expect(scorecard.getBoard()).toEqual({1: [3, 4]});
+});
+
+
+
+it('should change the roll number when add first roll',  function(){
+  scorecard.addRoll(5);
+  expect(scorecard.getRollNumber()).toEqual(2);
+});
+
+it('should change the frameNumber and previousFrame when adding a second roll', function(){
+  scorecard.board = {1: [3, undefined]};
+  scorecard.changeRollNumber();
+  scorecard.addRoll(3);
+  expect(scorecard.getPreviousFrame()).toEqual(1);
+  expect(scorecard.getFrameNumber()).toEqual(2);
+});
+
+
+it('should go to the next frame if strike', function(){
+  scorecard.addRoll(10);
+  expect(scorecard.getFrameNumber()).toEqual(2);
+  expect(scorecard.getBoard()).toEqual({1: [10, undefined]});
+});
+
+
+
+it('should know if a previous frame is a spare', function(){
+  scorecard.addRoll(5);
+  scorecard.addRoll(5);
+  expect(scorecard.previousFrameIsSpare()).toBe(true);
+});
+
+
+
+
+
+
+
+
+  it('scoreboard starts with an empty array', function(){
+    expect(scorecard.getScoreBoard()).toEqual({})
+  });
+
+
 
   it('adds a score to the first roll', function(){
       scorecard.roll_1(5);
@@ -25,6 +107,9 @@ describe('ScoreCard', function(){
       scorecard.roll_1(10);
     expect(scorecard.checkForStrike()).toEqual("strike")
   });
+
+
+
 
 // --------
 
@@ -51,12 +136,7 @@ describe('ScoreCard', function(){
 
 
 
-  it('adds the current frame to the board', function(){
-    scorecard.roll_1(5);
-    scorecard.roll_2(3);
-    scorecard.addFrameToBoard();
-    expect(scorecard.getBoard()).toEqual([[5,3],]);
-  });
+
 
 
   it('should know the total points of a frame', function(){
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,25 +1,87 @@
 'use strict';
 
 function ScoreCard (){
-  this.board = [];
-  this.currenFrame = 1;
-
+  this.board = {};
   this.frame = [];
   this.total = 0;
 
+  this.frameNumber = 1;
+  this.previousFrame = 0;
+  this.rollNumber = 1 ;
+
 };
+
+
+ScoreCard.prototype.getFrameNumber = function() {
+  return this.frameNumber
+};
+
+
+ScoreCard.prototype.nextFrame = function(){
+  this.frameNumber += 1;
+  this.previousFrame += 1;
+};
+
+ScoreCard.prototype.getPreviousFrame = function() {
+  return this.previousFrame
+};
+
+
+ScoreCard.prototype.getRollNumber = function(){
+  return this.rollNumber;
+
+};
+
+
+ScoreCard.prototype.changeRollNumber = function(){
+  this.rollNumber === 1 ? this.rollNumber = 2 : this.rollNumber = 1;
+}
+
+
+ScoreCard.prototype.getBoard = function(){
+  return this.board;
+}
+
+
+
+ScoreCard.prototype.addRoll = function(roll){
+  var frame = this.getFrameNumber();
+  if(this.getRollNumber() === 1) {
+    if(roll === 10){
+      this.nextFrame();
+    }
+      this.board[frame] = [roll, undefined];
+    }
+    else {
+      this.board[frame][1] = roll;
+      this.nextFrame();
+    }
+    this.changeRollNumber();
+};
+
+ScoreCard.prototype.previousFrameIsSpare = function(){
+  var previousFrame = this.getPreviousFrame();
+  return this.frameScore(previousFrame) === 10;
+
+};
+
+ScoreCard.prototype.frameScore = function(frame){
+  return this.board[frame].reduce(function(sum, num){ return sum + num; })
+
+};
+
+
+
+
+
 
 
 ScoreCard.prototype.getScoreBoard = function() {
     return this.board
 };
 
-ScoreCard.prototype.getFrameNumber = function() {
-  return this.currenFrame
-};
-
 ScoreCard.prototype.roll_1 = function(value) {
-  this.frame.push(value);
+  this.frame[0] = value;
 };
 
 ScoreCard.prototype.getRoll_1 = function() {
@@ -35,7 +97,11 @@ ScoreCard.prototype.checkForStrike = function(){
 // --------
 
 ScoreCard.prototype.getNextFrame = function() {
-  return this.currenFrame += 1
+  if(this.frame[0] == 10 || this.frame[1] != undefined ) {
+      return this.frameNumber += 1
+
+  }
+
 };
 
 
@@ -44,7 +110,7 @@ ScoreCard.prototype.getNextFrame = function() {
 
 
 ScoreCard.prototype.roll_2 = function(value) {
-  this.frame.push(value);
+  this.frame[1] = value;
 };
 
 ScoreCard.prototype.getRoll_2 = function() {
@@ -59,9 +125,6 @@ ScoreCard.prototype.checkForSpare = function(){
 };
 
 
-ScoreCard.prototype.addFrameToBoard = function() {
-    this.board.push(this.frame);
-};
 
 ScoreCard.prototype.getBoard = function() {
     return this.board;

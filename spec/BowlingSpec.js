5cd908f631e66630634ffd40ff57f5209b443fe6
danieljonesdmj <danieljonesdmj@gmail.com>
2018-07-23 08:42:00 +0100

    WIP impletementing spare bonuses

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,18 +7,43 @@ describe('Bowling', function() {
     bowling = new Bowling();
   });
 
-  it('should have an object to hold scores', function(){
-    expect(bowling.score).toEqual([]);
+  it('should have an array to hold rolls', function(){
+    expect(bowling.scores).toEqual([]);
   });
 
-  it('can add a frame to the scores', function(){
-    bowling.roll(2, 4);
-    expect(bowling.score).toEqual([[2, 4]]);
+  it('can add rolls to the scores', function(){
+    bowling.rolls(6);
+    expect(bowling.scores).toEqual([6]);
   });
 
   it('can add multiples frame to the scores', function(){
-    bowling.roll(2, 4);
-    bowling.roll(3, 6);
-    expect(bowling.score).toEqual([[2, 4], [3 ,6]]);
+    bowling.rolls(2);
+    bowling.rolls(6);
+    bowling.rolls(2);
+    expect(bowling.scores).toEqual([2, 6, 2]);
   });
-})
+
+  it('can sum the total score', function(){
+    for (var i = 0; i < 20; i++) {
+      bowling.rolls(2);
+    }
+    expect(bowling.score()).toBe(40);
+  });
+
+  it('can roll a zero game', function(){
+    for (var i = 0; i < 20; i++) {
+      bowling.rolls(0);
+    }
+    expect(bowling.score()).toBe(0);
+  })
+
+  it('can calculate spare bonus', function(){
+    for (var i = 0; i < 17; i++) {
+      bowling.rolls(0);
+    }
+    bowling.rolls(4);
+    bowling.rolls(6);
+    bowling.rolls(2);
+    expect(bowling.score()).toBe(14)
+  });
+});
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,10 +1,24 @@
 'use strict';
 
-function Bowling() {
-  this.score = [];
-}
+var Bowling = function () {
+  this.scores = [];
+};
 
-Bowling.prototype.roll = function(one,two) {
-  var arr = [one, two];
-  this.score.push(arr);
+Bowling.prototype.rolls = function(roll) {
+  this.scores.push(roll);
+};
+
+Bowling.prototype.score = function() {
+  var sum = 0;
+  var rollCount = 0;
+
+  for (var i = 0; i < 10; i++) {
+    if (this.scores[rollCount] + this.scores[rollCount + 1] === 10) {
+      sum += this.scores[rollCount] + this.rolls[rollCount + 1] + this.rolls[rollCount + 2];
+    } else {
+    sum += this.scores[rollCount] + this.scores[rollCount + 1];
+  }
+  rollCount += 2;
+}
+  return sum;
 };

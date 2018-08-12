a37df71efe1e3c3b04143e7a279b1fd4d02adc0c
Angela Wolff <warriorwomenblog@gmail.com>
2018-08-12 21:51:09 +0100

    Third test - Initial rolls = spare followed by single score - RED

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -10,17 +10,36 @@ describe('Game', function(){
 
 
   it("A gutter game has a zero score", function(){
-    for (var i = 0; i < 20; i += 1 ) {
-      game.roll(0);
-    }
+    rollMultiple(20,0);
     expect(game.score()).toEqual(0);
   });
 
   it("All ones scores 20", function(){
-    for (var i = 0; i < 20; i += 1 ) {
-      game.roll(1);
-    }
+    rollMultiple(20,1);
     expect(game.score()).toEqual(20);
   });
+
+  it ("A single spare test, followed by 3, results in score 16", function() {
+    rollSpare;
+    game.roll(3);
+    rollMultiple(17,0);
+    expect(game.score()).toEqual(16);
+  });
+
+
+// Helper functions
+
+  function rollMultiple(rolls, pins) {
+    for(var i = 0; i < rolls; i += 1) {
+      game.roll(pins);
+    }
+  };
+
+  function rollSpare() {
+    game.roll(5);
+    game.roll(5);
+  };
+
+
 });
 

5980d2979b57206e780b50c32c46bec20783958b
Marek <MarekSchmidt@outlook.com>
2018-08-12 22:26:28 +0100

    Finished most of the logic. Ideally would want to change spares and 10th frame to be computed after the first roll, not the whole next frame. If that happens then I can say game_over is true when length of frame_scores === 10. TODO: 10th frame logic, DRY up code massively with web helpers, refactor methods into additional helper functions, and write interface.

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -7,15 +7,81 @@ beforeEach(function () {
 });
 
 describe("#rolls", function () {
+
   it("returns an array of all rolls so far for the current game", function () {
     expect(scorecard.rolls).toEqual([]);
   });
+
 });
 
 describe("#frame_scores", function () {
+
   it("returns an array of all frame scores so far for the current game", function () {
     expect(scorecard.rolls).toEqual([]);
   });
+
+  describe("special conditions", function () {
+
+    describe("single strike", function () {
+      it("does not update until the next non-strike", function () {
+        scorecard.enter_roll(10);
+        expect(scorecard.frame_scores).toEqual([]);
+      });
+      it("adds only one score of 20 if spare next", function () {
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(6);
+        scorecard.enter_roll(4);
+        expect(scorecard.frame_scores).toEqual([20]);
+      });
+      it("adds both scores if not spare or strike next", function () {
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(6);
+        scorecard.enter_roll(3);
+        expect(scorecard.frame_scores).toEqual([19, 9]);
+      });
+    });
+
+    describe("double strike", function () {
+      it("only adds one score of 30 if strike next", function () {
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(10);
+        expect(scorecard.frame_scores).toEqual([30]);
+      });
+      it("adds two scores if spare next", function () {
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(7);
+        scorecard.enter_roll(3);
+        expect(scorecard.frame_scores).toEqual([27, 20]);
+      });
+      it("adds two scores if neither spare nor strike next", function () {
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(10);
+        scorecard.enter_roll(7);
+        scorecard.enter_roll(2);
+        expect(scorecard.frame_scores).toEqual([27, 19, 9]);
+      });
+    });
+
+    describe("spare", function () {
+      it("adds a score of 20 if a strike next", function () {
+        scorecard.enter_roll(8);
+        scorecard.enter_roll(2);
+        scorecard.enter_roll(10);
+        expect(scorecard.frame_scores).toEqual([20]);
+      });
+      it("adds a score of 10 + first roll if not a strike next", function () {
+        scorecard.enter_roll(8);
+        scorecard.enter_roll(2);
+        scorecard.enter_roll(7);
+        scorecard.enter_roll(3);
+        expect(scorecard.frame_scores).toEqual([17]);
+      });
+    });
+
+  });
+
 });
 
 describe("#current_roll", function() {
@@ -24,40 +90,32 @@ describe("#current_roll", function() {
     expect(scorecard.current_roll).toEqual(1);
   });
 
-  describe("on the first roll of a frame", function() {
-
+  describe("on the first roll of a frame", function () {
     it("increases by 1 if not a strike", function () {
       scorecard.enter_roll(7);
       expect(scorecard.current_roll).toEqual(2);
     });
-
     it("increases by 2 if strike", function () {
       scorecard.enter_roll(10);
       expect(scorecard.current_roll).toEqual(3);
     });
-
   });
 
   describe("on the second roll of a frame", function () {
-
     it("increases by 1 if less than 10 pins knocked down", function () {
       scorecard.enter_roll(0);
       scorecard.enter_roll(7);
       expect(scorecard.current_roll).toEqual(3);
     });
-
-
     it("also increases by 1 if ten pins knocked down", function () {
       scorecard.enter_roll(0);
       scorecard.enter_roll(10);
       expect(scorecard.current_roll).toEqual(3);
     });
-
     it("throws an error if last roll + current roll > 10 pins", function () {
       scorecard.enter_roll(7);
       expect(function() { scorecard.enter_roll(8) }).toThrowError("Can not knock more than 10 pins in a single frame");
     });
-
   });
 
 });
@@ -88,37 +146,59 @@ describe("#enter_roll", function () {
 });
 
 describe("#last_frame_was_strike", function () {
-  it("returns true if last frame was a strike", function () {
-
+  it("is true if last frame was a strike", function () {
+    scorecard.enter_roll(10);
+    expect(scorecard.last_frame_was_strike).toBe(true);
   });
-  it("returns false if last frame was not a strike", function () {
-
+  it("is false if last frame was not a strike", function () {
+    scorecard.enter_roll(3);
+    scorecard.enter_roll(5);
+    expect(scorecard.last_frame_was_strike).toBe(false);
   });
-  it("returns false if last frame was a spare", function () {
-
+  it("is false if last frame was a spare", function () {
+    scorecard.enter_roll(3);
+    scorecard.enter_roll(7);
+    expect(scorecard.last_frame_was_strike).toBe(false);
   });
 });
 
 describe("#two_frames_ago_was_strike", function () {
-  it("returns true if two frames ago was a strike", function () {
+  it("is true if two frames ago was a strike", function () {
+    scorecard.enter_roll(10);
+    scorecard.enter_roll(3);
+    scorecard.enter_roll(5);
+    expect(scorecard.two_frames_ago_was_strike).toBe(true);
+  });
+  it("is false if two frames ago was not a strike", function () {
+    scorecard.enter_roll(4);
+    scorecard.enter_roll(4);
+    scorecard.enter_roll(3);
+    scorecard.enter_roll(5);
+    expect(scorecard.two_frames_ago_was_strike).toBe(false);
 
   });
-  it("returns false if two frames ago was not a strike", function () {
-
-  });
-  it("returns false if two frames ago was a spare", function () {
-
+  it("is false if two frames ago was a spare", function () {
+    scorecard.enter_roll(4);
+    scorecard.enter_roll(6);
+    scorecard.enter_roll(3);
+    scorecard.enter_roll(5);
+    expect(scorecard.two_frames_ago_was_strike).toBe(false);
   });
 });
 
 describe("#last_frame_was_spare", function () {
-  it("returns true if last frame was a spare", function () {
-
+  it("is true if last frame was a spare", function () {
+    scorecard.enter_roll(4);
+    scorecard.enter_roll(6);
+    expect(scorecard.last_frame_was_spare).toBe(true);
   });
-  it("returns false if last frame was not a spare", function () {
-
+  it("is false if last frame was not a spare", function () {
+    scorecard.enter_roll(4);
+    scorecard.enter_roll(5);
+    expect(scorecard.last_frame_was_spare).toBe(false);
   });
-  it("returns false if last frame was a strike", function () {
-
+  it("is false if last frame was a strike", function () {
+    scorecard.enter_roll(10);
+    expect(scorecard.last_frame_was_spare).toBe(false);
   });
 });
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -3,6 +3,11 @@ function ScoreCard() {
   this.frame_scores = [];
   this.current_roll = 1;
   this.current_frame = 1;
+  this.last_frame_was_strike = false;
+  this.two_frames_ago_was_strike = false;
+  this.last_frame_was_spare = false;
+  this.last_frame_score = 0;
+  this.two_frames_ago_score = 0;
 };
 
 ScoreCard.prototype.enter_roll = function (roll) {
@@ -13,6 +18,8 @@ ScoreCard.prototype.enter_roll = function (roll) {
     }
     else { //if strike on the first roll of a frame
       this.rolls.push(0);
+      this.update_previous_scores("strike");
+      this.update_strikes_and_spares("strike");
       this.current_roll += 2;
       this.current_frame ++;
     }
@@ -22,6 +29,14 @@ ScoreCard.prototype.enter_roll = function (roll) {
       throw new Error("Can not knock more than 10 pins in a single frame");
     }
     else {
+      if (this.spare(roll)) {
+        this.update_previous_scores("spare");
+        this.update_strikes_and_spares("spare");
+      }
+      else {
+        this.update_previous_scores(this.last_roll() + roll);
+        this.update_strikes_and_spares(roll);
+      }
       this.rolls.push(roll);
       this.current_roll ++;
       this.current_frame ++;
@@ -43,10 +58,62 @@ ScoreCard.prototype.over_ten_pins_in_one_frame = function (roll) {
   return ((this.last_roll() + roll) > 10);
 };
 
-ScoreCard.prototype.last_frame_was_strike = function () {
-
+ScoreCard.prototype.spare = function (roll) {
+  return ((this.last_roll() + roll) === 10);
 };
 
-ScoreCard.prototype.two_frames_ago_was_strike = function () {
+ScoreCard.prototype.update_strikes_and_spares = function (current_frame_score) {
+  if (this.last_frame_was_strike === true) {
+    this.two_frames_ago_was_strike = true;
+  }
+  else {
+    this.two_frames_ago_was_strike = false;
+  };
+  switch (current_frame_score) {
+    case "strike":
+    this.last_frame_was_spare = false;
+    this.last_frame_was_strike = true;
+    break;
+    case "spare":
+    this.last_frame_was_spare = true;
+    this.last_frame_was_strike = false;
+    break;
+    default:
+    this.last_frame_was_spare = false;
+    this.last_frame_was_strike = false;
+  };
+};
 
+ScoreCard.prototype.update_previous_scores = function (current_frame_score) {
+  switch (current_frame_score) {
+    case "strike":
+      if (this.last_frame_was_strike === true) {
+        if (this.two_frames_ago_was_strike  === true) {
+          this.frame_scores.push(30);
+        };
+      }
+      else if (this.last_frame_was_spare === true) {
+        this.frame_scores.push(20);
+      }
+    break;
+    case "spare":
+      if (this.last_frame_was_strike === true) {
+        if (this.two_frames_ago_was_strike === true) {
+          this.frame_scores.push(20 + this.last_roll());
+        };
+        this.frame_scores.push(20);
+      }
+      else if (this.last_frame_was_spare === true) {
+        this.frame_scores.push(10 + this.last_roll());
+      }
+    break;
+    default:
+      if (this.last_frame_was_strike === true) {
+        if (this.two_frames_ago_was_strike === true) {
+          this.frame_scores.push(20 + this.last_roll());
+        };
+        this.frame_scores.push(10 + current_frame_score);
+      };
+      this.frame_scores.push(current_frame_score);
+  }
 };

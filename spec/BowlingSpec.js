08eed906263c2d8d9814f8cc69b3cdce51110273
Kirtiman Singh <kirt93@hotmail.com>
2018-08-19 20:56:12 +0100

    can roll all 1's and show score

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,13 +1,29 @@
+'use strict'
 
 describe('BowlingGame', function() {
   var game;
+
   beforeEach(function() {
     game = new BowlingGame();
   });
+
+  function rollMany (rolls, pins) {
+		for (var i = 0; i < rolls; i++) {
+			game.roll(pins);
+		}
+	}
+
   it("Rolls all 0's", function() {
-    for (var i = 0; i < 20; i++) {
-      game.roll(0)
-    }
-  expect(game.score()).toBe(0);  
+    // for (var i = 0; i < 20; i++) {
+    //   game.roll(0)
+    // }
+    rollMany(20, 0);
+    expect(game.score()).toEqual(0);
   });
+
+  it("Rolls all 1's", function() {
+    rollMany(20, 1)
+    expect(game.score()).toEqual(20);
+  });
+
 });

f753578e5eaa4c7e0122ebe850098c3ea6f0fdc9
edthomas93 <et1270@my.bristol.ac.uk>
2018-08-19 16:30:18 +0100

    when a strike is rolled, frame is finished

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -9,12 +9,31 @@ describe("Scorecard", function() {
   });
 
   describe('#roll', function() {
-    it('records overall score when a score is input', function() {
+    it('records overall score when a full game has been played', function() {
       var times = 20;
       for(var i=0; i < times; i++){
         scorecard.roll(3);
       };
       expect(scorecard.score).toEqual(60);
+    });
+
+    it('rewards a strike by giving a bonus of the next 2 rolls', function() {
+      scorecard.roll(10);
+    });
+
+    it('rewards a spare by giving a bonus of the next roll', function() {
+      scorecard.roll(9);
+      scorecard.roll(1);
+    });
+
+    it('raises an error if more than 10 pins inputted for one frame', function() {
+      expect(function() { scorecard.roll(11) }).toThrowError("There is a maximum of 10 pins per frame");
+    });
+
+    it('ends the frame early if strike rolled', function() {
+      scorecard.roll(10);
+      scorecard.roll(10);
+      expect(scorecard.allFrames.length).toEqual(2);
     });
   });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -5,11 +5,29 @@ function Scorecard() {
 };
 
 Scorecard.prototype.roll = function(roll) {
-  this.frame.push(roll);
-  this.score += roll;
-  if (this.frame.length === 2){
-    console.log(this.frame);
-    this.allFrames.push(this.frame);
-    this.frame = [];
+  if(roll > 10){
+    throw new Error("There is a maximum of 10 pins per frame");
+  }
+  else{
+    this.frame.push(roll);
+    this.score += roll;
+    if(roll === 10){
+      this.allFrames.push(this.frame);
+      this.frame = [];
+    }
+    else if(this.frame.length === 2){
+      this.allFrames.push(this.frame);
+      this.frame = [];
+    }
   };
 };
+
+Scorecard.prototype.endFrame = function(){
+
+};
+
+// Scorecard.prototype.framePins = function(){
+//   var total=0;
+//   for(var i in this.frame) { total += this.frame[i]; }
+//   return total;
+// };

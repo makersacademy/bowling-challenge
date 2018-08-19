08a9f288797601aeb891863b031f2bec4557f4ae
edthomas93 <et1270@my.bristol.ac.uk>
2018-08-19 17:18:27 +0100

    Added strike and spare tests

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -9,6 +9,15 @@ describe("Scorecard", function() {
   });
 
   describe('#roll', function() {
+    it('raises an error if one roll is greater than 10', function(){
+      expect(function() { scorecard.roll(11) }).toThrowError("There is a maximum of 10 pins per frame");
+    });
+
+    it('raises an error if more than 10 pins inputted for one frame', function(){
+      scorecard.roll(4);
+      expect(function() { scorecard.roll(7) }).toThrowError("There is a maximum of 10 pins per frame");
+    });
+
     it('records overall score when a full game has been played', function() {
       var times = 20;
       for(var i=0; i < times; i++){
@@ -17,23 +26,26 @@ describe("Scorecard", function() {
       expect(scorecard.score).toEqual(60);
     });
 
+    it('ends the frame early if strike rolled', function() {
+      var times = 4;
+      for(var i=0; i < times; i++){
+        scorecard.roll(10);
+      };
+      expect(scorecard.allFrames.length).toEqual(4);
+    });
+
     it('rewards a strike by giving a bonus of the next 2 rolls', function() {
       scorecard.roll(10);
+      scorecard.roll(8);
+      scorecard.roll(1);
+      expect(scorecard.allFrames[0]).toEqual(19);
     });
 
     it('rewards a spare by giving a bonus of the next roll', function() {
       scorecard.roll(9);
       scorecard.roll(1);
-    });
-
-    it('raises an error if more than 10 pins inputted for one frame', function() {
-      expect(function() { scorecard.roll(11) }).toThrowError("There is a maximum of 10 pins per frame");
-    });
-
-    it('ends the frame early if strike rolled', function() {
-      scorecard.roll(10);
-      scorecard.roll(10);
-      expect(scorecard.allFrames.length).toEqual(2);
+      scorecard.roll(8);
+      expect(scorecard.allFrames[0]).toEqual(18);
     });
   });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,33 +1,32 @@
 function Scorecard() {
   this.frame = [];
+  this.frameScore = 0
+  this.allRolls = [];
   this.allFrames = [];
+  this.allFramesScore = [];
   this.score = 0
 };
 
 Scorecard.prototype.roll = function(roll) {
-  if(roll > 10){
+  if(roll > 10 || roll + this.frameScore > 10){
     throw new Error("There is a maximum of 10 pins per frame");
   }
   else{
     this.frame.push(roll);
+    this.frameScore += roll;
+    this.allRolls.push(roll);
     this.score += roll;
     if(roll === 10){
-      this.allFrames.push(this.frame);
-      this.frame = [];
+      this.endFrame();
     }
     else if(this.frame.length === 2){
-      this.allFrames.push(this.frame);
-      this.frame = [];
+      this.endFrame();
     }
   };
 };
 
 Scorecard.prototype.endFrame = function(){
-
+  this.allFrames.push(this.frame);
+  this.frame = [];
+  this.frameScore = 0;
 };
-
-// Scorecard.prototype.framePins = function(){
-//   var total=0;
-//   for(var i in this.frame) { total += this.frame[i]; }
-//   return total;
-// };

f9be0288efcc1d1c55f9fa378899695c855ad284
josh nickson <jnickson1506@gmail.com>
2018-09-08 18:07:44 +0100

    change addPoints fn to add bonus after spare

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,12 +1,22 @@
 function BowlingGame() {
-  this.scores = [];
+  this.scores = [0,0];
+  this.first
+  this.multiplier = 1;
+  this.firstRoll = true
 };
+var scores;
+var firstRoll = true;
+var multiplier
 
 BowlingGame.prototype.addPoints = function(points) {
-  this.scores.push(points)
+  if (this.firstRoll == true && this.scores.slice(-2).reduce((x, y) => x + y) === 10) {
+    this.multiplier = 2
+  }
+  this.scores.push(points * this.multiplier)
+  this.firstRoll = !this.firstRoll
+  this.multiplier = 1
 };
 
 BowlingGame.prototype.total = function() {
-  console.log(this.scores)
   return this.scores.reduce((x, y) => x + y); 
 }

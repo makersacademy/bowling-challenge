b692495df74f2a8e7fea54dace31d5962ab72d92
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-09-08 19:21:10 +0100

    add sum scores and improve first and second roll with edge cases

--- spec/scoreboardSpec.js
+++ spec/scoreboardSpec.js
@@ -5,14 +5,42 @@ describe ('Scoreboard', function() {
     scoreboard = new Scoreboard();
   });
 
-  it('adds the score of the first roll', function() {
-    scoreboard.firstRoll(5);
-    expect(scoreboard.scores).toContain(5);
+  describe('firstRoll', function() {
+    it('adds the score of the first roll', function() {
+      scoreboard.firstRoll(5);
+      expect(scoreboard.scores).toContain(5);
+    });
+
+    it('refuses to add a number above ten', function() {
+      expect(function() {
+        scoreboard.firstRoll(11);
+      }).toThrow("You can't knock down more than 10 pins!");
+    });
   });
 
-  it('adds the score of the second roll', function() {
-    scoreboard.secondRoll(3);
-    expect(scoreboard.scores).toContain(3);
+  describe('secondRoll', function() {
+    it('adds the score of the second roll', function() {
+      scoreboard.firstRoll(8);
+      scoreboard.secondRoll(1);
+      expect(scoreboard.scores).toContain(1);
+    });
+
+    it('refuses to add a number above the first roll', function() {
+      expect(function() {
+        scoreboard.firstRoll(9);
+        scoreboard.secondRoll(3);
+      }).toThrow("You can't knock down more than 10 pins!");
+    });
   });
+
+  describe('rollScore', function() {
+    it('sums the numbers inside the array', function() {
+      scoreboard.firstRoll(8);
+      scoreboard.secondRoll(2);
+      expect(scoreboard.rollScore()).toEqual(10);
+    });
+
+  });
+
 
 });
--- src/scoreboard.js
+++ src/scoreboard.js
@@ -3,11 +3,28 @@ function Scoreboard() {
 };
 
 Scoreboard.prototype.firstRoll = function(number) {
-  this.scores[0] = number;
-  return console.log(this.scores);
+  if (number <= 10) {
+    this.scores[0] = number;
+    return console.log(this.scores);
+  } else {
+    throw "You can't knock down more than 10 pins!";
+  }
 };
 
 Scoreboard.prototype.secondRoll = function(number) {
-  this.scores[1] = number;
-  return console.log(this.scores);
+  if (this.scores[0] <= 5 && number <= 5) {
+     this.scores[1] = number;
+     return console.log(this.scores);
+  }
+  else if (this.scores[0] > 5 && number <= (10-this.scores[0])) {
+    this.scores[1] = number;
+    return console.log(this.scores);
+  }
+  else {
+    throw "You can't knock down more than 10 pins!";
+  }
+};
+
+Scoreboard.prototype.rollScore = function() {
+  return this.scores.reduce((a,b) => a + b);
 };

d65f67293903bd26252f0ed465d07950eb033647
Lucien Najev <luciennajev@hotmail.com>
2018-09-08 22:32:48 +0100

    added a basic score reading feature test

---
+++ spec/featureSpec.js
@@ -0,0 +1,24 @@
+'use strict'
+
+describe('features', function() {
+
+  var scorecard;
+  var frame;
+
+  beforeEach(function() {
+    scorecard = new Scorecard();
+    frame = new Frame();
+  });
+
+
+
+  describe('roll a frame and retrieve scores', function() {
+    it('can return the result of a roll', function() {
+      frame.rollOne(0);
+      frame.rollTwo(0);
+      scorecard.addFrame(frame);
+      expect(scorecard.getFrames()[0].results[0]).toEqual(0);
+      expect(scorecard.getFrames()[0].results[0]).toEqual(0);
+    });
+  });
+});

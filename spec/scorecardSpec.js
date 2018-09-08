64be7e971a825533044b90e5cac1abf3f3ff021d
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-08 13:04:37 +0100

    put in place checks for a spare

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -6,9 +6,56 @@ describe("Scorecard", function () {
     scorecard = new Scorecard();
   });
 
-  it("adds a roll to the currentFrame", function () {
-    scorecard.addRoll(6);
-    expect(scorecard.currentFrame).toContain(6);
+  describe("addRoll", function () {
+
+    it("adds a roll to the currentFrame", function () {
+      scorecard.addRoll(6);
+      expect(scorecard.currentFrame).toContain(6);
+    });
+
+    describe("when 2 rolls have been added", function () {
+
+      beforeEach(function () {
+        scorecard.addRoll(6);
+        scorecard.addRoll(1);
+      });
+
+      it("adds the sum of the current frame to currentScore", function () {
+        expect(scorecard.currentScore).toEqual(7);
+      });
+
+      it("currentFrame is pushed into currentGame", function () {
+        expect(scorecard.currentGame).toContain([6, 1]);
+      });
+
+      it("clears the currentFrame", function () {
+        expect(scorecard.currentFrame).toEqual([]);
+      });
+
+      it("increases the frame count by 1", function () {
+        expect(scorecard.frame).toEqual(2);
+      });
+
+      describe("if they equal 10", function () {
+
+        beforeEach(function() {
+          scorecard.addRoll(5);
+          scorecard.addRoll(5);
+        });
+
+        it("sets wasLastFrameSpare to true", function () {
+          expect(scorecard.wasLastFrameSpare()).toEqual(true);
+        });
+      });
+
+    });
+
   });
+
+  describe("when user gets a spare", function () {
+
+  });
+
+
 
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,7 +1,44 @@
 function Scorecard() {
+  this.currentScore = 0;
   this.currentFrame = [];
+  this.currentGame = ["Start"];
+  this.frame = 1;
+  this.spare = false;
 }
 
 Scorecard.prototype.addRoll = function (number) {
+
   this.currentFrame.push(number);
+
+  if (this.currentFrame.length === 2) {
+    if (this.wasLastFrameSpare()) {
+      this.currentScore += this.currentFrame[0]
+      this.spare = false;
+    }
+    this.calcCurrentScore();
+    this.pushFrameToGame();
+    if (this.currentFrame[0] + this.currentFrame[1] === 10) {
+      this.spare = true;
+    }
+    this.currentFrame = [];
+    this.frame++
+  }
+}
+
+Scorecard.prototype.calcCurrentScore = function () {
+  var frameSum = 0;
+
+  this.currentFrame.map(function (i) {
+    frameSum += i;
+  });
+
+  this.currentScore += frameSum;
+}
+
+Scorecard.prototype.pushFrameToGame = function () {
+  this.currentGame.push(new Array(this.currentFrame[0], this.currentFrame[1]));
+}
+
+Scorecard.prototype.wasLastFrameSpare = function () {
+  return this.spare;
 }

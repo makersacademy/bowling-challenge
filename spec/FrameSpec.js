63162e0c6552fdb7c381384ce2d641bad7bc6066
Rashika Patel <rashika_patel@hotmail.com>
2018-09-08 21:43:12 +0100

    add roll score method

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -9,4 +9,10 @@ describe("Frame", function() {
     expect(frame.getScore()).toEqual(0);
   });
 
+  it("should be able to accept roll and return score", function() {
+    expect(frame.getScore()).toEqual(0);
+    frame.roll(3)
+    expect(frame.getScore()).toEqual(3);
+  })
+
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,6 +1,13 @@
 function Frame() {
   this.score = 0
 }
-Frame.prototype.getScore = function(song) {
-  return this.score
+
+Frame.prototype = {
+  getScore: function() {
+    return this.score
+  }, 
+  roll: function(pins) {
+    this.score += pins
+  }
+
 };

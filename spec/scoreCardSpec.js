740e6285e6042493def32168e74715963b6eaab5
James Sutherland <jrsutherland78@googlemail.com>
2018-09-09 12:17:30 +0100

    add score function and not defined checker

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -15,8 +15,19 @@ describe('ScoreCard', function() {
   describe('entering the knocked down pins', function () {
 
     it ('recorded in the bowls array', function () {
-      scoreCard.bowl(5)
-      expect(scoreCard._bowls).toEqual([5])
+      scoreCard.bowl(5);
+      expect(scoreCard._bowls).toEqual([5]);
     });
   });
+
+  describe('scoring system', function () {
+    it ('adds up the bowls in a frame', function () {
+      scoreCard.bowl(5);
+      scoreCard.bowl(5);
+      scoreCard.score()
+      expect(scoreCard._score).toEqual(10);
+    });
+
+  });
+
 });
--- src/scoreCard.js
+++ src/scoreCard.js
@@ -1,8 +1,22 @@
 function ScoreCard () {
   this._bowls = [];
   this._bowlIndex = 0;
+  this._frame = 1;
+  this._score = 0;
 }
 
 ScoreCard.prototype.bowl = function (knockedPins) {
   this._bowls.push(knockedPins);
 };
+
+ScoreCard.prototype.score = function () {
+  this._score += (this.notDefinedCheck(this._bowls[this._bowlIndex]) + this.notDefinedCheck(this._bowls[this._bowlIndex+1]));
+};
+
+ScoreCard.prototype.notDefinedCheck = function (indexToCheck) {
+  if (isNaN(indexToCheck)) {
+    return 0
+  } else {
+    return indexToCheck
+  }
+}

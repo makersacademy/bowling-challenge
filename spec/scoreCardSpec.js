6485149b56ef08fbce44991ef31b1431f725a6bc
James Sutherland <jrsutherland78@googlemail.com>
2018-09-09 13:07:09 +0100

    fix score not resetting before new iteration, add more init tests

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -10,6 +10,18 @@ describe('ScoreCard', function() {
       expect(scoreCard._bowls).toEqual([]);
     });
 
+    it ('bowl index number is 0', function() {
+      expect(scoreCard._bowlIndex).toEqual(0);
+    });
+
+    it ('frame number starts with 1', function() {
+      expect(scoreCard._frame).toEqual(1);
+    });
+
+    it ('score starts at 0', function() {
+      expect(scoreCard._score).toEqual(0);
+    });
+
   });
 
   describe('entering the knocked down pins', function () {
@@ -18,24 +30,26 @@ describe('ScoreCard', function() {
       scoreCard.bowl(5);
       expect(scoreCard._bowls).toEqual([5]);
     });
+
+    it ('recorded more than one bowl in bowls array', function () {
+      scoreCard.bowl(5);
+      scoreCard.bowl(3);
+      expect(scoreCard._bowls).toEqual([5,3]);
+    });
   });
 
   describe('scoring system', function () {
     it ('adds up the bowls in a frame', function () {
       scoreCard.bowl(5);
       scoreCard.bowl(5);
-      scoreCard.score();
       expect(scoreCard._score).toEqual(10);
     });
 
     it ('loops through the frames of a game adding the scores', function () {
-      scoreCard.bowl(1);
-      scoreCard.bowl(2);
-      scoreCard.bowl(3);
-      scoreCard.bowl(4);
+      scoreCard._bowls = [1, 2, 3, 4];
       scoreCard.score();
       expect(scoreCard._score).toEqual(10);
-      expect(scoreCard._frame).toEqual(11); // breaks loop if frame is higher than 10
+      expect(scoreCard._frame).toEqual(11); // breaks loop if frame is higher than 10 (end of game)
     });
 
   });
--- src/scoreCard.js
+++ src/scoreCard.js
@@ -7,20 +7,30 @@ function ScoreCard () {
 
 ScoreCard.prototype.bowl = function (knockedPins) {
   this._bowls.push(knockedPins);
+  this.score();
 };
 
 ScoreCard.prototype.score = function () {
+  this.resetForScore ();
   while (this._frame <= 10) {
     this._score += (this.notDefinedCheck(this._bowls[this._bowlIndex]) + this.notDefinedCheck(this._bowls[this._bowlIndex+1]));
     this._bowlIndex += 2;
+    console.log(this._frame);
+    console.log(this._score);
     this._frame += 1;
   };
 };
 
+ScoreCard.prototype.resetForScore = function () {
+  this._frame = 1;
+  this._bowlIndex = 0;
+  this._score = 0;
+}
+
 ScoreCard.prototype.notDefinedCheck = function (indexToCheck) {
   if (isNaN(indexToCheck)) {
-    return 0
+    return 0;
   } else {
-    return indexToCheck
+    return indexToCheck;
   }
 }

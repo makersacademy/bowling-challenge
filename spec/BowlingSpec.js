c8cf2cd930f32294ab1c3a281e229692a5248b00
josh nickson <jnickson1506@gmail.com>
2018-09-09 10:54:08 +0100

    add working end-game scoring

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,20 +7,47 @@ describe("Bowling", function() {
     game = new BowlingGame;
   });
 
-  it("should add bonus to score after spare", function() {
-    game.addPoints(9);
-    game.addPoints(1);
-    game.addPoints(5);
-    game.addPoints(2);
-    expect(game.total()).toEqual(22);
+  it("should calculate perfect game", function() {
+    for (var i = 0; i < 12; i++) { 
+      game.addPoints(10);
+    }
+    expect(game.total()).toEqual(300);
   });
 
-  it("should add bonus to score after strike", function() {
-    game.addPoints(10);
-    game.addPoints(1);
-    game.addPoints(5);
-    game.addPoints(2);
-    expect(game.total()).toEqual(24);
+  it("should correctly calculate sample game 1", function() {
+    var sample1 = [9, 1, 10, 9, 1, 9, 1, 9, 1, 10, 9, 1, 9, 1, 10, 10, 9, 1]
+    for (var i = 0; i < sample1.length; i++) {
+      game.addPoints(sample1[i]);
+    } 
+    expect(game.total()).toEqual(206);
+  });
+
+  it("should correctly calculate sample game 2", function() {
+    var sample2 = [10, 5, 5, 3, 4, 10, 7, 2, 1, 0, 10, 2, 8, 5, 2, 6, 4, 10]
+    for (var i = 0; i < sample2.length; i++) {
+      game.addPoints(sample2[i]);
+    } 
+    expect(game.total()).toEqual(131);
+  });
+
+  it("should correctly calculate sample game 3", function() {
+    var sample3 = [0, 1, 10, 1, 2, 3, 4, 5, 5, 10, 9, 1, 10, 0, 9, 0, 10, 5]
+    for (var i = 0; i < sample3.length; i++) {
+      game.addPoints(sample3[i]);
+    } 
+    expect(game.total()).toEqual(127);
+    console.log(game.scores)
+    console.log(game.pinLog)
+  });
+
+  it("should correctly calculate sample game 3", function() {
+    var sample3 = [0, 1, 10, 1, 2, 3, 4, 5, 5, 10, 9, 1, 10, 0, 9, 0, 10, 5]
+    for (var i = 0; i < sample3.length; i++) {
+      game.addPoints(sample3[i]);
+    } 
+    expect(game.total()).toEqual(127);
+    console.log(game.scores)
+    console.log(game.pinLog)
   });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,43 +1,41 @@
 function BowlingGame() {
-  this.scores = [0,0];
+  this.scores = [];
   this.multiplier = 1;
   this.firstRoll = true;
   this.strike = 0;
+  this.frame = 0;
   this.pinLog = [0,0]
 };
 
-var scores;
-var firstRoll = true;
-var multiplier;
-var strike;
-var pinLog;
-
 BowlingGame.prototype.addPoints = function(points) {
-  if (this.strike != 0) {
-    this.multiplier = 2
-    this.strike--
-  } else {
-    this.multiplier = 1;
-    if (this.firstRoll === true && this.pinLog.slice(-2).reduce((x, y) => x + y) === 10) {
-      this.multiplier = 2
-    }
+  if (this.firstRoll === true) {
+    this.frame++ 
   }
+  this.multiplier = this.getMultiplier();
+  this.strike = Math.max(0, this.strike - 1)
   this.scores.push(points * this.multiplier);
   this.pinLog.push(points)
-
-  if (points === 10) {
+  if (points === 10 && this.firstRoll === true) {
     this.firstRoll = !this.firstRoll;
     this.strike = 2;
-  }
+  } 
   this.firstRoll = !this.firstRoll;
-  console.log(this.strike);
-  console.log(this.scores);
-  console.log(this.pinLog);
-
 };
-
-
 
 BowlingGame.prototype.total = function() {
   return this.scores.reduce((x, y) => x + y); 
+}
+
+BowlingGame.prototype.getMultiplier = function() {
+  if (this.frame > 10 && this.firstRoll === true  && this.strike !== 0 ){
+    return (13 - this.frame);
+  } else if (this.frame === 11 & this.firstRoll === false) {
+    return 1;
+  } else if (this.pinLog.slice(-2).reduce((x, y) => x + y) === 20) {
+    return 3;
+  } else if (this.strike !== 0) {
+    return 2;
+  } else if (this.firstRoll === true && this.pinLog.slice(-2).reduce((x, y) => x + y) === 10 && this.frame <=10) {
+    return 2;
+  } else return  1;
 }

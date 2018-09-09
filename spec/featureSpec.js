26ae51d9e4eeb2b9cde474e97de734bc8e74d9e7
Lucien Najev <luciennajev@hotmail.com>
2018-09-09 18:25:58 +0100

    refactor

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -1,9 +1,6 @@
 'use strict'
-
 // Question to ask in feature testing are you supposed to be using doubles / spies.....i dont think so but not sure
-
 describe('features', function() {
-
   var scorecard;
   var frame1;
   var frame2;
@@ -15,7 +12,6 @@ describe('features', function() {
   var frame8;
   var frame9;
   var frame10;
-
   beforeEach(function() {
     scorecard = new Scorecard();
     frame1 = new Frame();
@@ -30,8 +26,6 @@ describe('features', function() {
     frame10 = new Frame();
   });
 
-
-
   describe('roll a frame and retrieve scores', function() {
     it('can return the result of a roll', function() {
       frame1.rollOne(0);
@@ -40,8 +34,6 @@ describe('features', function() {
       expect(scorecard.getFrames()[0].results[0]).toEqual(0);
       expect(scorecard.getFrames()[0].results[0]).toEqual(0);
     });
-
-
 
     describe('Gutter game', function() {
       it('can calculate the score of the first frame (2 gutterballs - 1 frame)', function() {
@@ -85,7 +77,6 @@ describe('features', function() {
       });
     });
 
-
     describe('No strikes or spares', function() {
       it('will display the result of the current frame if there are no strikes or spares', function() {
         frame1.rollOne(5);
@@ -120,11 +111,40 @@ describe('features', function() {
         scorecard.addFrame(frame5);
         expect(scorecard.frameTotal(5)).toEqual(31);
       });
+      it('will display the cumulative result of the 10th frame, no strikes or spares', function() {
+        frame1.rollOne(5);
+        frame1.rollTwo(3);
+        scorecard.addFrame(frame1);
+        frame2.rollOne(1);
+        frame2.rollTwo(3);
+        scorecard.addFrame(frame2);
+        frame3.rollOne(2);
+        frame3.rollTwo(7);
+        scorecard.addFrame(frame3);
+        frame4.rollOne(3);
+        frame4.rollTwo(2);
+        scorecard.addFrame(frame4);
+        frame5.rollOne(4);
+        frame5.rollTwo(1);
+        scorecard.addFrame(frame5);
+        frame6.rollOne(2);
+        frame6.rollTwo(2);
+        scorecard.addFrame(frame6);
+        frame7.rollOne(2);
+        frame7.rollTwo(2);
+        scorecard.addFrame(frame7);
+        frame8.rollOne(2);
+        frame8.rollTwo(2);
+        scorecard.addFrame(frame8);
+        frame9.rollOne(2);
+        frame9.rollTwo(2);
+        scorecard.addFrame(frame9);
+        frame10.rollOne(2);
+        frame10.rollTwo(2);
+        scorecard.addFrame(frame10);
+        expect(scorecard.frameTotal(10)).toEqual(51);
+      });
     });
-
-
-
-
 
     describe('Perfect game', function() {
       it('will correctly display the result given that every shot is a strike', function() {

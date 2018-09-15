bb0bd9da94b0960070d35dec4f14c645a49a4e52
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-15 11:51:07 +0100

    card stores frame bowls and tracks bonuses to apply

--- spec/CardSpec.js
+++ spec/CardSpec.js
@@ -4,10 +4,29 @@ describe('Card', function() {
 
   beforeEach(function(){
     card = new Card();
-    round = new roundDouble();
+    frame = new Frame();
   });
 
-  it('', function() {
-
+  it('stores a frames score', function() {
+    frame.bowl(6)
+    frame.bowl(0)
+    card.store(frame)
+    expect(card.frames).toContain(frame.bowls)
   });
+
+  describe('when a spare or strike frame is stored', function() {
+    it('knows to add the next two rolls for a strike', function() {
+      frame.bowl(10)
+      card.store(frame)
+      expect(card.bonuses).toContain(2)
+    });
+
+    it('knows to add the next one roll for a spare', function() {
+      frame.bowl(5)
+      frame.bowl(5)
+      card.store(frame)
+      expect(card.bonuses).toContain(1)
+    });
+  });
+  
 });
--- src/Card.js
+++ src/Card.js
@@ -1,3 +1,19 @@
 function Card() {
+  this.frames = []
+  this.bonuses = []
+};
 
+Card.prototype.store = function(frame) {
+  this.frames.push(frame.bowls)
+  this.trackBonus(frame)
+};
+
+Card.prototype.trackBonus = function(frame) {
+  if (frame.spare) {
+    this.bonuses.push(1)
+  } else if (frame.strike) {
+    this.bonuses.push(2)
+  } else {
+    this.bonuses.push(0)
+  };
 };

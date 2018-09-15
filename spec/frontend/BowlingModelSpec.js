3d20fc728fef2da7850235eb4820ae6ccc8194cb
davidlawes <davidlawes@hotmail.co.uk>
2018-09-15 21:37:59 +0100

    Refactored specs for model

--- spec/frontend/BowlingModelSpec.js
+++ spec/frontend/BowlingModelSpec.js
@@ -1,22 +1,13 @@
 "use strict";
 
 describe('BowlingModel', function() {
-  //var BowlingModel = require('../../public/js/BowlingModel');
   var bowlingModel;
 
   beforeEach(function(){
     bowlingModel = new BowlingModel();
-    // spyOn(bowlingModel.turnIncrementer, 'incrementTurn').and.returnValue({frame: 1, roll: 2});
-    // spyOn(bowlingModel.turnIncrementer, 'incrementFrame').and.returnValue({frame: 2, roll: 1});
-    // spyOn(bowlingModel.scoreCalculator, 'calculate').and.returnValue({total: 5, scoresArray: [5,0,0,0,0,0,0,0,0,0]});
   });
 
   describe('.play', function() {
-    it('If new frame, creates new Frame object', function() {
-      bowlingModel.play(5);
-      expect(bowlingModel._frameArray.length).toEqual(1);
-    });
-
     it('Adds new frame to _frameArray', function() {
       bowlingModel.play(5);
       expect(bowlingModel._frameArray.length).toEqual(1);
@@ -26,6 +17,20 @@ describe('BowlingModel', function() {
       bowlingModel.play(5);
       bowlingModel.play(5);
       expect(bowlingModel._frameArray.length).toEqual(1);
+    });
+  });
+
+  describe('.score', function() {
+    it('Add pins to current frame if not new Frame', function() {
+      bowlingModel.play(5);
+      expect(bowlingModel.score()).toEqual({ total: 5, frameScores: [ 5, 0, 0, 0, 0, 0, 0, 0, 0, 0 ] });
+    });
+  });
+
+  describe('.currentTurn', function() {
+    it('Returns current turn', function() {
+      bowlingModel.play(5);
+      expect(bowlingModel.currentTurn()).toEqual({frame: 1, roll:1})
     });
   });
 });

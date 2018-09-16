48021a0c740289d3d2025ff120d80beeeb5c1a06
Lucien Najev <luciennajev@hotmail.com>
2018-09-16 19:15:07 +0100

    score calculation function needs to be re worked stopping here

--- src/scorecard.js
+++ src/scorecard.js
@@ -4,6 +4,7 @@
 function Scorecard() {
   this.frames = [];
   this.buildScores = [];
+  this.totalScore = null;
 };
 
 
@@ -27,36 +28,63 @@ Scorecard.prototype.frameTotal = function(frameNumber) {
 
 // this has to come first in the order
   if(this.frames[x].results[0] === 10 && typeof this.frames[x + 1] === 'undefined' && this.frames.length < 10) {
+    console.log('One!');
     return displayTotal;
   };
 
   if(this.frames[x].results[0] === 10 && this.frames[x].results[1] === 10 && this.frames[x].results[2] === 10) {
     displayTotal = this.frames[x].frameScore();
+    console.log('TWO!');
     return displayTotal * frameNumber;
   };
 
 
   if(this.frames[x].results[0] === 10 && this.frames[x + 1].results[0] === 10 && this.frames[x + 1].results[1] === 10) {
     displayTotal = this.frames[x].frameScore() + this.frames[x + 1].frameScore();
+    console.log('Three!');
     return displayTotal * frameNumber;
   };
 
 
   if(this.frames[x].results[0] === 10 && this.frames[x + 1].results[0] === 10 && this.frames[x + 2].results[0] === 10) {
     displayTotal = this.frames[x].frameScore() + this.frames[x + 1].frameScore() + this.frames[x + 2].frameScore();
+    console.log('Four!');
     return displayTotal * frameNumber;
   };
 
   if(this.frames[x].results[0] < 10 && this.frames[x].remainingPins() === 0 && typeof this.frames[x + 1] === 'undefined') {
+    console.log('FIVE!!');
     return displayTotal;
   };
 
+
+//ok need to rebuild the calculation have completely misunderstood how this will all work!!!!
   if(this.frames[x].results[0] < 10 && this.frames[x].remainingPins() === 0 && typeof this.frames[x + 1] !== 'undefined') {
-    displayTotal = this.frames[x].frameScore() + this.frames[x + 1].frameScore();
+    for(let calcIndex = 0; calcIndex < this.frames.length - 1; calcIndex++) {
+      displayTotal += this.frames[calcIndex].frameScore() + this.frames[calcIndex + 1].results[0];
+    };
+    console.log('SIX!!');
+    return displayTotal;
+  };
+
+  if(this.frames.length === 10 && this.frames[9].results.length === 3) {
+    displayTotal = this.frames[x].frameScore();
+    console.log('SEVEN!');
     return displayTotal * frameNumber;
   };
 
 
+  if(this.frames[x].results[0] < 10 && this.frames[x].remainingPins() > 0 && typeof this.frames[x + 1] === 'undefined') {
+    for(let calcIndex = 0; calcIndex < this.frames.length - 1; calcIndex++) {
+
+      // Think i will need to put all my if conditions inside this for loop
+      // so ill build one big for loop and contain my if logic inside it?
+      // ?????
+      displayTotal += this.frames[calcIndex].frameScore() + this.frames[calcIndex + 1].results[0];
+    };
+    console.log('EIGHT!!!');
+    return displayTotal;
+  };
 
 
 
@@ -64,6 +92,7 @@ Scorecard.prototype.frameTotal = function(frameNumber) {
     var array = this.frames[i].results;
     displayTotal += this.arraySum(array);
   };
+  console.log('Hi im here!')
   return displayTotal;
 };
 

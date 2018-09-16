26c8767c6ad76288e1f39a20ef790e4efcfebf7a
Harry <harryjp1995@gmail.com>
2018-09-16 19:31:46 +0100

    Works, but without the final frame...

--- public/scripts/LiveGame.js
+++ public/scripts/LiveGame.js
@@ -4,13 +4,13 @@ function liveGame() {
   this.score = 0;
 }
 
-$("#pinButtons").on("click", function() {
-  console.log("hello");
-});
+function previousFrame() {
+  return $frames[$frames.length - 2];
+}
 
 function addRoll(pin) {
   if ($frames.length < 10) {
-    $currentFrame.firstRoll === null ? _addRollFirst(pin) : _addRollSecond(pin);
+    $currentFrame.firstRoll == null ? _addRollFirst(pin) : _addRollSecond(pin);
   }
 }
 
@@ -22,17 +22,43 @@ function _addRollFirst(pin) {
 function _addRollSecond(pin) {
   if ($currentFrame.firstRoll + pin > 10) return;
   $currentFrame.secondRoll = pin;
-  $currentGame.score += $currentFrame.frameScore();
   $frames.push($currentFrame);
-  $("#previousFrames").text($frames);
   $(`#frame${$frames.length}`).text(
     `${$currentFrame.firstRoll}, ${$currentFrame.secondRoll}`
   );
-  $(`#marker${$frames.length}`).text($currentGame.score);
+  if ($frames.length == 1) _firstFrameScore();
+  if ($frames.length > 1) bonusCheck();
   _clearFrame();
 }
 
+function bonusCheck() {
+  if (previousFrame().isStrike()) return _strikeScore();
+  if (previousFrame().isSpare()) return _spareScore();
+  $currentGame.score += $currentFrame.frameScore();
+  $(`#runningTotal${$frames.length}`).text($currentGame.score);
+}
+
+function _firstFrameScore() {
+  $currentGame.score = $currentFrame.frameScore();
+  $(`#runningTotal${$frames.length}`).text($currentGame.score);
+}
+
+function _spareScore() {
+  $(`#runningTotal${$frames.length - 1}`).text(
+    $currentGame.score + $currentFrame.firstRoll
+  );
+  $currentGame.score += $currentFrame.frameScore() + $currentFrame.firstRoll;
+  $(`#runningTotal${$frames.length}`).text($currentGame.score);
+}
+
+function _strikeScore() {
+  $(`#runningTotal${$frames.length - 1}`).text(
+    $currentGame.score + $currentFrame.frameScore()
+  );
+  $currentGame.score += 2 * $currentFrame.frameScore();
+  $(`#runningTotal${$frames.length}`).text($currentGame.score);
+}
+
 function _clearFrame() {
-  $currentFrame.firstRoll = null;
-  $currentFrame.secondRoll = null;
+  $currentFrame = new Frame();
 }

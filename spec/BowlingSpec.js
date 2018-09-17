585552b92d03374547bde672e48bcbeb4ab5cba4
josh nickson <jnickson1506@gmail.com>
2018-09-17 12:58:56 +0100

    commit for pull request

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -2,18 +2,15 @@
 
 describe("Bowling", function() {
   var game;
-
   beforeEach(function(){
     game = new BowlingGame;
   });
-
   it("should calculate perfect game", function() {
     for (var i = 0; i < 12; i++) { 
       game.addPoints(10);
     }
     expect(game.total()).toEqual(300);
   });
-
   it("should correctly calculate sample game 1", function() {
     var sample1 = [9, 1, 10, 9, 1, 9, 1, 9, 1, 10, 9, 1, 9, 1, 10, 10, 9, 1]
     for (var i = 0; i < sample1.length; i++) {
@@ -21,7 +18,6 @@ describe("Bowling", function() {
     } 
     expect(game.total()).toEqual(206);
   });
-
   it("should correctly calculate sample game 2", function() {
     var sample2 = [10, 5, 5, 3, 4, 10, 7, 2, 1, 0, 10, 2, 8, 5, 2, 6, 4, 10]
     for (var i = 0; i < sample2.length; i++) {
@@ -29,25 +25,25 @@ describe("Bowling", function() {
     } 
     expect(game.total()).toEqual(131);
   });
-
   it("should correctly calculate sample game 3", function() {
     var sample3 = [0, 1, 10, 1, 2, 3, 4, 5, 5, 10, 9, 1, 10, 0, 9, 0, 10, 5]
+    // [0, 1 | 10 | 1, 2 | 3, 4 | 5, 5 | 10 | 9,1 | 10 | 0, 9 | 0, 10 | 5]
+    // [0, 1, 10, 2, 4, 3, 4, 5, 5, 20, 18, 2, 20, 0, 18, 0, 10, 5]
+    // [1, 14, 17, 24, 44, 64, 84, 103 , 112, 127]
     for (var i = 0; i < sample3.length; i++) {
       game.addPoints(sample3[i]);
     } 
     expect(game.total()).toEqual(127);
     console.log(game.scores)
     console.log(game.pinLog)
+    console.log(game.display)
   });
 
-  it("should correctly calculate sample game 3", function() {
-    var sample3 = [0, 1, 10, 1, 2, 3, 4, 5, 5, 10, 9, 1, 10, 0, 9, 0, 10, 5]
-    for (var i = 0; i < sample3.length; i++) {
-      game.addPoints(sample3[i]);
+  it("should correctly calculate sample game 4", function() {
+    var sample4 = [10, 0, 10, 3, 4, 10, 7, 2, 1, 0, 10, 2, 8, 5, 2, 6, 4, 10]
+    for (var i = 0; i < sample4.length; i++) {
+      game.addPoints(sample4[i]);
     } 
-    expect(game.total()).toEqual(127);
-    console.log(game.scores)
-    console.log(game.pinLog)
+    expect(game.total()).toEqual(131);
   });
-
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,41 +1,66 @@
+'use strict';
+
 function BowlingGame() {
   this.scores = [];
-  this.multiplier = 1;
-  this.firstRoll = true;
-  this.strike = 0;
-  this.frame = 0;
   this.pinLog = [0,0]
+  this.display = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: []};
+  this.firstRoll = true;
+  this.multiplier = this.frame = this.strike = 0;
+  this.frameScores = {1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [], 10: [], 11: [], 12: []};
 };
+var display;
 
 BowlingGame.prototype.addPoints = function(points) {
-  if (this.firstRoll === true) {
-    this.frame++ 
-  }
+  if (this.firstRoll) this.frame++;
   this.multiplier = this.getMultiplier();
-  this.strike = Math.max(0, this.strike - 1)
+  this.display[this.frame].push(points);
   this.scores.push(points * this.multiplier);
-  this.pinLog.push(points)
-  if (points === 10 && this.firstRoll === true) {
-    this.firstRoll = !this.firstRoll;
-    this.strike = 2;
-  } 
-  this.firstRoll = !this.firstRoll;
+  this.pinLog.push(points);
+  this.switchRoll(points);
 };
-
 BowlingGame.prototype.total = function() {
   return this.scores.reduce((x, y) => x + y); 
 }
 
 BowlingGame.prototype.getMultiplier = function() {
-  if (this.frame > 10 && this.firstRoll === true  && this.strike !== 0 ){
-    return (13 - this.frame);
-  } else if (this.frame === 11 & this.firstRoll === false) {
-    return 1;
-  } else if (this.pinLog.slice(-2).reduce((x, y) => x + y) === 20) {
+  if (this.isLastFrameBonus() && this.isStrike()){
+    return (this.firstRoll) ? 13 - this.frame : 1;
+  } else if (this.isDoubleStrike()) {
     return 3;
-  } else if (this.strike !== 0) {
-    return 2;
-  } else if (this.firstRoll === true && this.pinLog.slice(-2).reduce((x, y) => x + y) === 10 && this.frame <=10) {
+  } else if (this.isStrike() || this.firstRoll && this.isSpare() && this.frame <=10) {
     return 2;
   } else return  1;
 }
+
+BowlingGame.prototype.switchRoll = function(points) {
+  this.strike = Math.max(0, this.strike - 1);
+  if (points === 10 && this.firstRoll) {
+    this.strike = 2;
+    this.firstRoll = !this.firstRoll;
+  }
+  this.firstRoll = !this.firstRoll;
+}
+BowlingGame.prototype.isStrike = function() {
+  return this.strike !== 0
+}
+BowlingGame.prototype.isDoubleStrike = function() {
+  return this.pinLog.slice(-2).reduce((x, y) => x + y) === 20
+}
+BowlingGame.prototype.isSpare = function() {
+  return this.pinLog.slice(-2).reduce((x, y) => x + y) === 10
+}
+BowlingGame.prototype.isLastFrameBonus = function() {
+  return this.frame > 10
+}
+
+// BowlingGame.prototype.updateDisplay() {
+//   for (var i = 1; i < this.display.length; i++) {
+//     if (this.display[i].length === 2) {
+//       this.frameScores[i] = this.display[i].reduce((x, y) => x + y)
+//     } else if (this.display[i].length === 2 && this.length.reduce((x, y) => x + y) === 10) {
+//       this.frameScores[i] = this.display[i].reduce((x, y) => x + y) + this.display[i][0]
+//     } else {
+//       this.frameScores[i] = this.display[i]
+//     }
+//   }
+// }

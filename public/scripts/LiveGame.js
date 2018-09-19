019b3e6968d924a129f6cfbc780801a1d1f2e34f
Harry <harryjp1995@gmail.com>
2018-09-19 09:37:18 +0100

    Still working to get bonus scoring working correctly

--- public/scripts/LiveGame.js
+++ public/scripts/LiveGame.js
@@ -2,6 +2,7 @@ import("./Frame.js");
 
 function liveGame() {
   this.score = 0;
+  this.isGameOver = false;
 }
 
 function _previousFrame() {
@@ -9,6 +10,7 @@ function _previousFrame() {
 }
 
 function addRoll(pin) {
+  if (this.isGameOver) return;
   if ($frames.length < 9) {
     $currentFrame.firstRoll == null ? _addRollFirst(pin) : _addRollSecond(pin);
   } else if ($frames.length == 9) {
@@ -30,11 +32,6 @@ function _addRollSecond(pin) {
   );
   if ($frames.length == 1) _firstFrameScore();
   if ($frames.length == 2) _secondFrameScore();
-  if ($frames.length == 10 && $currentFrame.noBonus() == false)
-    _finalFrameBonus();
-  if ($frames.length == 10 && $currentFrame.noBonus() == true) {
-    _previousFrame().isStrike() ? _doubleCheck() : _spareCheck();
-  }
   if ($frames.length > 2 && $frames.length < 10) {
     _previousFrame().isStrike() ? _doubleCheck() : _spareCheck();
   }
@@ -42,40 +39,68 @@ function _addRollSecond(pin) {
 }
 
 function addFinalFrame(pin) {
-  $currentFrame.firstRoll == 10 ? _finalStrike() : null;
-  $currentFrame.firstRoll == null
-    ? _addRollFirstFinal(pin)
-    : _addRollSecondFinal(pin);
+  if ($currentFrame.firstRoll == null) return _addRollFirstFinal(pin);
+  if ($currentFrame.firstRoll != null && $currentFrame.secondRoll == null)
+    return _addRollSecondFinal(pin);
+  _addRollThird(pin);
 }
 
 function _addRollFirstFinal(pin) {
-  if (pin == 10) return _finalStrike();
   $currentFrame.firstRoll = pin;
   $("#frame10").text(pin);
 }
 
 function _addRollSecondFinal(pin) {
-  if ($currentFrame.firstRoll + pin == 10) return _finalSpare();
   $currentFrame.secondRoll = pin;
   $("#frame10").text(`${$currentFrame.firstRoll}, ${pin}`);
   $frames.push($currentFrame);
   $currentGame.score += $currentFrame.frameScore();
   $("#runningTotal10").text(`${$currentGame.score}`);
+  this.isGameOver = true;
+  _checkNinth();
+  if ($currentFrame.isSpare()) finalBonus();
+  if ($currentFrame.isStrike()) finalBonus();
+}
+
+function finalBonus() {
+  this.isGameOver = false;
+  $frames.pop();
+}
+
+function _checkNinth() {
+  debugger;
+  if ($frames[7].isStrike() && $frames[8].isStrike()) {
+    $("#runningTotal8").text(
+      parseInt($("#runningTotal8").text(), 10) + $currentFrame.firstRoll
+    );
+    $currentGame.score += $currentFrame.firstRoll;
+    $("runningTotal9").text(
+      parseInt($("#runningTotal9").text(), 10) + $currentFrame.frameScore()
+    );
+  } else if ($frames[8].isStrike() && !$frames[7].isStrike()) {
+    $("runningTotal9").text(
+      parseInt($("#runningTotal9").text(), 10) + $currentFrame.frameScore()
+    );
+    $currentGame.score += $currentFrame.frameScore();
+  } else if ($frames[8].isSpare()) {
+    $("runningTotal9").text(
+      parseInt($("#runningTotal9").text(), 10) + $currentFrame.firstRoll
+    );
+    $currentGame.score += $currentFrame.firstRoll;
+  }
 }
 
 function _addRollThird(pin) {
-  firstRoll = $frames[9].firstRoll;
-  secondRoll = $frames[9].secondRoll;
-  $frames.splice(-1);
-  $currentFrame.firstRoll = firstRoll;
-  $currentFrame.secondRoll = secondRoll;
   $currentFrame.thirdRoll = pin;
   $frames.push($currentFrame);
-  $(`#frame${$frames.length + 1}`).text(
-    `${$currentFrame.firstRoll}, ${$currentFrame.secondRoll}, ${
-      $currentFrame.thirdRoll
-    }`
+  $("#frame10").text(
+    `${$currentFrame.firstRoll}, 
+     ${$currentFrame.secondRoll}, 
+     ${$currentFrame.thirdRoll}`
   );
+  $currentGame.score += pin;
+  $("#runningTotal10").text($currentGame.score);
+  this.isGameOver = true;
 }
 
 function _doubleCheck() {

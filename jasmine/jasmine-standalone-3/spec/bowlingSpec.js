7f307c69a4ea0bfd261e051ed40a8201d8d50296
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-10-05 15:10:33 +0100

    first commit with file and testing structure set up

---
+++ jasmine/jasmine-standalone-3/spec/bowlingSpec.js
@@ -0,0 +1,13 @@
+// describe ('Bowling', function () {
+//   var bowling;
+//
+//   beforeEach(function() {
+//     bowling = new Bowling() ;
+//   });
+//
+//   it ('bowls a ball', function () {
+//     bowling.play(5)
+//     expect(bowling.play).toEqual(5);
+//   });
+//
+// });

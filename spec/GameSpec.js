817bbaf1b3f1b7febe7e89cff0b2e07d293d7389
Florence Hapgood <flohapgood@icloud.com>
2018-10-07 00:32:44 +0100

    Progress with the rollBall function

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,9 +7,10 @@ describe ("Game", function(){
 
   describe ('rollBall', function(){
     it("creates a new frame class if current Frame has no rolls left", function(){
-      // used_frame = new Frame();
-      // used_frame.descreaseRoll();
-      // used_frame.descreaseRoll();
+      
+      used_frame = new Frame();
+      used_frame.decreaseRoll();
+      used_frame.decreaseRoll();
       //need attribute reader?
       // is the return a problem here?
       expect(game.rollBall()).toEqual(new Frame());
--- src/Game.js
+++ src/Game.js
@@ -3,13 +3,14 @@ function Game (){
   this._framesLeft = 10;
   this._doubleFirstRollNextRound = false;
   this._doubleBothRollsNextRound = false;
+  this.currentFrame = new Frame();
 };
 
-Game.prototype.rollBall = function(userInput){
-  // if (currentFrame._rollsLeft === 0){
-  //   var currentFrame = new Frame(); //remember to pass in user_input
-  // } else {
-  //   currentFrame.calculate(5);
-
-  return new Frame()
+Game.prototype.rollBall = function(userInput) {
+  if (this.currentFrame._rollsLeft === 0){
+  var  currentFrame = new Frame(); //remember to pass in user_input   //might not need var here
+  } else {
+    this.currentFrame.calculate(5);
+    return new Frame();
+  }
   };

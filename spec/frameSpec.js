d21c289c2bcd88eca2b3e6548a1250695225247a
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-06 21:55:40 +0100

    added #_spareOrBetter method to close frame early

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -4,6 +4,10 @@ var Frame = require('../src/frame')
 describe('#addScore', function () {
   var frame = new Frame()
 
+  beforeEach(function() {
+    frame.scores = []
+  })
+
   it('inserts score into scores array', function () {
     frame.addScore(3)
     expect(frame.scores[0]).toEqual(3)
@@ -13,6 +17,12 @@ describe('#addScore', function () {
     frame.addScore(7)
     frame.addScore(3)
     frame.addScore(4)
+    frame.addScore(1)
+    expect(frame.scores.length).toEqual(3)
+  })
+
+  it('closes the frame if the scores dont make a spare', function () {
+    frame.addScore(3)
     frame.addScore(3)
     expect(frame.scores.length).toEqual(3)
   })
--- src/frame.js
+++ src/frame.js
@@ -8,5 +8,15 @@ Frame.prototype.addScore = function (score) {
   if (this.scores.length < 3) {
     this.scores.push(score)
   }
+  if (this.scores.length === 2) {
+    this._spareOrBetter()
+  }
+}
+
+Frame.prototype._spareOrBetter = function () {
+  const REDUCER = (accumulator, currentValue) => accumulator + currentValue;
+  if (this.scores.reduce(REDUCER) < 10) {
+    this.scores.push(0)
+  }
 }
 module.exports = Frame

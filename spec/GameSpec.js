46d841e3d75732bfd924113e7503bdf7babaaa31
Florence Hapgood <flohapgood@icloud.com>
2018-10-06 20:23:17 +0100

    Methods taken out, only attributes currently, added framesleft

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,8 +6,15 @@ describe ("Game", function() {
 
   describe ('totalScore', function(){
     it("has a starting score of 0", function(){
-      expect(game.totalScore()).toEqual(0);
+      expect(game._totalScore).toEqual(0);
     })
   })
+
+  describe ('framesLeft', function(){
+    it("has a starting number of 10", function(){
+      expect(game._framesLeft).toEqual(10)
+    })
+  })
+
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,12 @@
 function Game (){
-  this._totalscore = 0;
+  this._totalScore = 0;
+  this._framesLeft = 10;
 };
 
-Game.prototype.totalScore = function(){
-  return this._totalscore;
-};
+// Game.prototype.totalScore = function(){
+//   return this._totalScore;
+// };
+//
+// Game.prototype.framesLeft = function(){
+//   return this._framesLeft;
+// }

80bd04e90a75fd83ae60975bbc67df998027326b
Damion Hardy <damionhardy@Damos-MBP-8.home>
2018-10-06 17:40:42 +0100

    committing rolling a totoal score test - passing

--- jasmine/jasmine-standalone-3/spec/bowlingSpec.js
+++ jasmine/jasmine-standalone-3/spec/bowlingSpec.js
@@ -12,4 +12,13 @@ describe ('Bowling game', function () {
     });
   });
 
+  describe('#score', function() {
+    it('can roll a total score for 20 bowls', function () {
+      for (var i = 0; i < 20; i++) {
+        game.roll(2);
+      }
+      expect(game.score()).toEqual(40);
+    });
+  })
+
 });
--- jasmine/jasmine-standalone-3/src/bowling.js
+++ jasmine/jasmine-standalone-3/src/bowling.js
@@ -2,10 +2,22 @@
 
 function Bowling() {
   this.rolls = [];
-}
+};
 
 Bowling.prototype.roll = function(pins) {
   this.rolls.push(pins);
   return this.rolls;
   console.log(this.rolls);
+};
+
+Bowling.prototype.score = function () {
+  var matchResult = 0;
+  var rollsIndex = 0;
+
+  for (var rollsIndex = 0; rollsIndex < 20; rollsIndex++) {
+      matchResult += this.rolls[rollsIndex];
+  };
+
+  return matchResult;
+
 };

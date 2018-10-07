5361a33e97655f412afb4a4eeb2077860c1eb2cd
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-10-07 20:56:36 +0100

    Can add frame to a game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,4 +10,11 @@ describe('Game',function(){
   it('starts with no frames by default', function(){
     expect(game.frames).toEqual([]);
   });
+
+  describe('adding frames', function(){
+    it('can add a frame to a game', function(){
+      game.addFrame('frame')
+      expect(game.frames).toEqual(['frame']);
+    });
+  });
 });
--- src/game.js
+++ src/game.js
@@ -3,3 +3,7 @@
 function Game(){
   this.frames = [];
 }
+
+Game.prototype.addFrame = function (frame) {
+  this.frames.push(frame);
+};

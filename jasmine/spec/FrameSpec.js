a7760db91653cd99e3a0afee69fa8763bf60f412
fbl11 <freyab_london@yahoo.co.uk>
2018-10-07 16:48:33 +0100

    extracts bonus rolls for spare and strike frames

--- jasmine/spec/FrameSpec.js
+++ jasmine/spec/FrameSpec.js
@@ -24,43 +24,60 @@ describe('frame', function () {
     //   expect(function () { frame.enterRoll(6) }).toThrowError('You cannot drop more than 10 pins in one frame');
     // })
   })
+  describe('enterBonus', function () {
+    it('allows the game to record the number of dropped pins for each bonus roll', function () {
+      frame.enterBonus(5)
+      expect(frame.getBonus()).toEqual([5])
+    })
+    it('does not allow the game to enter numbers over 10', function () {
+      expect(function () { frame.enterBonus(11) }).toThrowError('Enter a number between 1 and 10');
+    })
+  })
 
-  describe('has states', function () {
-    it('sets the state of a frame to active if it is current', function () {
-      frame.enterRoll(5)
-      frame.setState()
-      expect(frame.getState()).toEqual('active')
+  describe('setState', function () {
+    describe('state active', function () {
+      it('sets the state of a new frame to "active"', function () {
+        expect(frame.getState()).toEqual('active')
+      })
+      it('a frame that is not a strike or spare is "active" after its first roll', function () {
+        frame.enterRoll(5)
+        expect(frame.getState()).toEqual('active')
+      })
     })
-    it('sets the state of a regular frame to "closed" after two rolls', function () {
-      frame.enterRoll(5)
-      frame.enterRoll(3)
-      frame.setState()
-      expect(frame.getState()).toEqual('closed')
+    describe('state waiting', function () {
+      it('sets the state a frame to "waiting" after a "strike"', function () {
+        frame.enterRoll(10)
+        frame.setState()
+        expect(frame.getState()).toEqual('waiting')
+      })
+      it('sets the state a frame to "waiting" after a "spare"', function () {
+        frame.enterRoll(5)
+        frame.enterRoll(5)
+        frame.setState()
+        expect(frame.getState()).toEqual('waiting')
+      })
     })
-    it('sets the state of a "spare" frame to "closed" after three rolls', function () {
-      frame.enterRoll(5)
-      frame.enterRoll(5)
-      frame.enterRoll(4)
-      frame.setState()
-      expect(frame.getState()).toEqual('closed')
-    })
-    it('sets the state of a "strike" frame to "closed" after three rolls', function () {
-      frame.enterRoll(10)
-      frame.enterRoll(5)
-      frame.enterRoll(4)
-      frame.setState()
-      expect(frame.getState()).toEqual('closed')
-    })
-    it('sets the state a frame to open for bonus after a strike', function () {
-      frame.enterRoll(10)
-      frame.setState()
-      expect(frame.getState()).toEqual('bonus')
-    })
-    it('sets the state a frame to open for bonus after a spare', function () {
-      frame.enterRoll(5)
-      frame.enterRoll(5)
-      frame.setState()
-      expect(frame.getState()).toEqual('bonus')
+    describe('state closed', function () {
+      it('sets the state of a regular frame to "closed" after two rolls', function () {
+        frame.enterRoll(5)
+        frame.enterRoll(3)
+        frame.setState()
+        expect(frame.getState()).toEqual('closed')
+      })
+      it('sets the state of a "spare" frame to "closed" after three rolls', function () {
+        frame.enterRoll(5)
+        frame.enterRoll(5)
+        frame.enterBonus(4)
+        frame.setState()
+        expect(frame.getState()).toEqual('closed')
+      })
+      it('sets the state of a "strike" frame to "closed" after three rolls', function () {
+        frame.enterRoll(10)
+        frame.enterBonus(5)
+        frame.enterBonus(4)
+        frame.setState()
+        expect(frame.getState()).toEqual('closed')
+      })
     })
   })
 
--- jasmine/src/Frame.js
+++ jasmine/src/Frame.js
@@ -2,33 +2,40 @@
 
 function Frame() {
   this._rolls = []
+  this._bonusRolls = []
   // this._isStrike
   // this._isSpare
   // this._isLast
-  this._state = ''
+  this._state = 'active'
 
 }
 
 Frame.prototype = {
-  getRolls: function () {
-    return this._rolls
-  },
-
+  // regular frame
   enterRoll: function (pins) {
     this._validateRoll(pins)
     this._rolls.push(pins)
 
   },
-    // if frame is spare or strike
-    // enterBonus: function () {},
+
+  getRolls: function () {
+    return this._rolls
+  },
+  // if frame is spare or strike
+  enterBonus: function (pins) {
+    this._validateRoll(pins)
+    this._bonusRolls.push(pins)
+  },
+
+  getBonus: function () {
+    return this._bonusRolls
+  },
 
   setState: function () {
     if (this._isClosed()) {
       this._state = 'closed'
-    } else if (this._isActive()) {
-      this._state = 'active'
-    } else if (this._isBonus()) {
-      this._state = 'bonus'
+    } else if (this._isWaiting()) {
+      this._state = 'waiting'
     }
   },
 
@@ -36,39 +43,31 @@ Frame.prototype = {
     return this._state
   },
 
-  _isClosed: function () {
-    // strike
-    if (this._rolls.length === 3 && this._rolls[0] === 10) {
-      return true
-    // spare
-    } else if (this._rolls.length === 3 && this._rolls[0] + this._rolls[1] === 10) {
-      return true
-    // regular frame
-    } else {
-    return (this._rolls.length === 2 && this._rolls[0] + this._rolls[1] < 10)
-    }
-  },
-
-  _isBonus: function () {
+  _isWaiting: function () {
     // if strike
-    if (this._rolls[0] === 10) {
+    if (this._bonusRolls.length < 2 && this._rolls[0] === 10) {
       return true
-    }
     // if spare
-    return this._rolls[0] + this._rolls[1] === 10
-  },
-
-  _isActive: function () {
-    // if first roll
-    if (this._rolls.length === 0) {
-      return true
-    // if first roll was not a strike
-    } else if (this._rolls.length === 1 && this._rolls[0] < 10) {
+    } else if (this._bonusRolls.length < 1 && this._rolls[0] + this._rolls[1] === 10) {
       return true
     } else {
       return false
     }
   },
+
+  _isClosed: function () {
+    // strike
+    if (this._bonusRolls.length === 2) {
+      return true
+    // spare
+    } else if (this._rolls.length === 2 && this._bonusRolls.length === 1) {
+      return true
+    // regular frame
+    } else {
+      return (this._rolls.length === 2 && this._rolls[0] + this._rolls[1] < 10)
+    }
+  },
+
   // refactor
   // isStrike: function () {
     

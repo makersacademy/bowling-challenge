42e4dae6abe22fce842262044a108b90665db9e8
shweta patil <shwetapatil@shwetas-mbp.home>
2018-10-07 02:12:41 +0100

    adds spare logic

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -20,19 +20,9 @@ describe("Game", function(){
   describe('roll', function() {
     it('records score of roll', function(){
       game.roll(2);
-      expect(game.displayScore()).toEqual(2);
+      expect(game.totalScore()).toEqual(2);
     });
   });
-
-  // // As a player
-  // // So that I can keep track of a full game
-  // // I can record my score for 10 frames
-
-  // describe('10-frames', function () {
-  //   it('starts with ten frames', function () {
-  //     expect(game.frames().length).toBe(10);
-  //   });
-  // });  
   
   // As a player
   // So that I can view game progress
@@ -40,10 +30,59 @@ describe("Game", function(){
 
   describe('adds score into into frame', function () {
     it('add roll score into frame get total score', function () {
-      game.roll(3)
-      game.roll(6)
-      expect(game.frames()[1]).toContain(3, 6);
+      game.roll(2);
+      game.roll(4);
+      expect(game.frames()[1]).toContain(2, 4);
     });
-  });  
+  }); 
+
+  // As a player
+  // So that I know where I am in the game
+  // I can check which number frame I am currently playing
+  
+  describe('add new frame', function() {
+    it('adds 2nd frame', function () {
+      game.roll(2);
+      game.roll(4);
+      game.roll(5);
+      expect(game.frames()[2]).toContain(5);
+    });
+    
+    it('shows current frame', function () {
+      game.roll(2);
+      game.roll(4);
+      game.roll(5);
+      expect(game._currentFrameNumber).toEqual(2);
+    });
+  });
+
+  // As a Player
+  // So that I know how well I am playing
+  // I can see my current score
+
+  describe('Displays current score', function() {
+    it('shows currents total score', function () {
+      game.roll(2);
+      game.roll(4);
+      game.roll(5);
+      expect(game.totalScore()).toEqual(11);
+    });
+  });
+  
+  // As a Player
+  // So that I can see when I have scored a spare (scored 10 on the second roll of a frame)
+  // The second roll is marked with a '/' in the scorecard
+  
+  describe('Spare', function() {
+    it('shows currents total score', function () {
+      game.roll(2);
+      game.roll(4);
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);      
+      expect(game._frameScore[2]).toEqual(13);
+     
+    });
+  });
   
 })
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,9 @@
 function Game (){
-  // this._frames = [[], [], [], [], [], [], [], [], [], []];
-  this._frames = []
-  this._score = 0
+  this._frames = [[], [], [], [], [], [], [], [], [], [],[]]; //length 11 
+  // this._frames = []
+  this._currentScore = 0
   this._currentFrameNumber = 1
-  this._frameScore = 0
-  
+  this._frameScore = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
 
 Game.prototype = {
@@ -14,18 +13,33 @@ Game.prototype = {
   },
 
   roll : function(number){
-    if(!this._frames[this._currentFrameNumber]){
-      this._frames[this._currentFrameNumber] = [];
+    if(this._frames[this._currentFrameNumber].length === 2){
+      this._currentFrameNumber += 1;
     }
+
     this._frames[this._currentFrameNumber].push(number);
-    this._score += number;
+
+    let frameSum = this._frames[this._currentFrameNumber].reduce(function(a, b){return a+b;}, 0);
+    this._frameScore[this._currentFrameNumber] = frameSum;
+    
+    let previousFrame = this._frameScore[this._currentFrameNumber - 1]
+    // if ( previousFrame === 10) {
+    //   previousFrame += number;
+    // }
+    if ( this._frameScore[this._currentFrameNumber - 1] === 10) {
+      this._frameScore[this._currentFrameNumber - 1] += number;
+    }
+
+    // if(this._frames[this._currentFrameNumber].reduce(function(a, b){return a+b;}, 0) === 10 ){
+    //   this._currentScore = this._currentScore + number + this._frames[this._currentFrameNumber + 1][0];
+      
+    // } else {
+    //   this._currentScore += number;
+    // }
   },
 
-  score: function() {
-    return this._score;
-  },
-
-  displayScore : function() {
-    return this.score();
+  totalScore : function() {
+    this._currentScore = this._frameScore.reduce(function(a, b){return a+b;}, 0);
+    return this._currentScore;
   }
 }

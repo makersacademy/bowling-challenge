8f549b04f6b053b45d7e63286f7b65a01e2f22fb
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-07 11:20:34 +0100

    each roll sent to all frames

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,21 +1,49 @@
 var Game = require('../src/game')
+var Frame = require('../src/frame')
 
-describe('Gutter game', function () {
-  var game = new Game()
+describe('Game class', function() {
+  describe('Gutter game', function () {
+    var game = new Game()
 
-  it('Scores Zero on a Gutter game', function () {
-    for (var i = 0; i < 20; i++) {
-      game.roll(0)
-    }
-    expect(game.score).toEqual(0)
+    it('Scores Zero on a Gutter game', function () {
+      for (var i = 0; i < 20; i++) {
+        game.roll(0)
+      }
+      expect(game.score).toEqual(0)
+    })
   })
-})
 
-describe('#makeNewFrame', function () {
-  var game = new Game
+  describe('#roll', function () {
+    var frame = {
+      inputScore: jasmine.createSpy('inputScore')
+    }
+    var frameClass = {
+      prototype: jasmine.createSpy('Prototype')
+      // .and.returnValue("frame")
+    }
+    var game;
 
-  it('Makes a 1st frame for new Game', function () {
-    game.roll(4)
-    expect(game.frames.length).toEqual(1)
+    beforeEach(function() {
+      game = new Game ()
+    })
+
+
+    it('Makes a 1st frame for new Game', function () {
+      game.roll(4)
+      expect(game.frames.length).toEqual(1)
+    })
+
+    it('Makes a new frame for 3rd roll', function () {
+      game.roll(3)
+      game.roll(3)
+      game.roll(3)
+      expect(game.frames.length).toEqual(2)
+    })
+
+    it('sends score to all frames', function () {
+      game.roll(3)
+      console.log(game.frames)
+      expect(game.frames[0].scoreTotal()).toEqual(3)
+    })
   })
 })
--- src/game.js
+++ src/game.js
@@ -1,19 +1,25 @@
 'use strict'
 
-function Game () {
+var Frame = require('./frame')
+
+function Game (frameClass = Frame) {
+  this.frameClass = frameClass
   this.score = 0
   this.frames = []
-  this.newFrameNeeded = true
+  this.rolls = 0
 }
 
-Game.prototype.roll = function () {
-  this.makeNewFrame()
+Game.prototype.roll = function (roleScore) {
+  this._makeNewFrame()
+  this.rolls += 1
+  for (let eachFrame of this.frames) {
+    eachFrame.addScore(roleScore)
+  }
 }
 
-Game.prototype.makeNewFrame = function () {
-  if (this.newFrameNeeded === true) {
-    this.frames.push('Frame')
-    this.newFrameNeeded = false
+Game.prototype._makeNewFrame = function () {
+  if (this.rolls % 2 === 0) {
+    this.frames.push(new this.frameClass())
   }
 }
 

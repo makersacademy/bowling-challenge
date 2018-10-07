15aa2f98b58a17c7255d3f1783cef1aee2715ce7
NadiaAiraf <aidanfaria@gmail.com>
2018-10-07 17:44:49 +0100

    not sure this is exactly the right way to do it but it's progress, added a throw function

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -9,4 +9,18 @@ describe('bowlingGame',function() {
     expect(bowling.playerName).toEqual('aidan')
   })
 
+  it('should have an initial score of 0',function(){
+    expect(bowling.currentScore).toEqual(0)
+  })
+
+  describe('throw',function(){
+    it('should add things into an array of all throws', function(){
+      bowling.throw(2)
+      bowling.throw(10)
+      bowling.throw(4)
+      bowling.throw(6)
+      expect(bowling.allThrows).toEqual([2,10,4,6])
+    })
+  })
+
 })
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,3 +1,10 @@
 function BowlingGame(name) {
   this.playerName = name
+  this.currentScore = 0
+  this.currentTurn = 0
+  this.allThrows = []
 }
+
+BowlingGame.prototype.throw = function (pins) {
+  this.allThrows.push(pins)
+};

63e97ddf818274265618f97c636c6287b1bd8377
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-07 12:29:25 +0100

    added #totalScore method

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,29 +1,41 @@
 var Frame = require('../src/frame')
 
+describe('Frame Class', function () {
+  describe('#addScore', function () {
+    var frame = new Frame()
 
-describe('#addScore', function () {
-  var frame = new Frame()
+    beforeEach(function() {
+      frame.scores = []
+    })
 
-  beforeEach(function() {
-    frame.scores = []
+    it('inserts score into scores array', function () {
+      frame.addScore(3)
+      expect(frame.scores[0]).toEqual(3)
+    })
+
+    it('only accepts three scores', function () {
+      frame.addScore(7)
+      frame.addScore(3)
+      frame.addScore(4)
+      frame.addScore(1)
+      expect(frame.scores.length).toEqual(3)
+    })
+
+    it('closes the frame if the scores dont make a spare', function () {
+      frame.addScore(3)
+      frame.addScore(3)
+      expect(frame.scores.length).toEqual(3)
+    })
   })
 
-  it('inserts score into scores array', function () {
-    frame.addScore(3)
-    expect(frame.scores[0]).toEqual(3)
-  })
+  describe('#totalScore', function () {
+    var frame = new Frame
 
-  it('only accepts three scores', function () {
-    frame.addScore(7)
-    frame.addScore(3)
-    frame.addScore(4)
-    frame.addScore(1)
-    expect(frame.scores.length).toEqual(3)
-  })
-
-  it('closes the frame if the scores dont make a spare', function () {
-    frame.addScore(3)
-    frame.addScore(3)
-    expect(frame.scores.length).toEqual(3)
+    it('sums the score array', function () {
+      frame.addScore(7)
+      frame.addScore(3)
+      frame.addScore(10)
+      expect(frame.totalScore()).toEqual(20)
+    })
   })
 })
--- src/frame.js
+++ src/frame.js
@@ -8,15 +8,23 @@ Frame.prototype.addScore = function (score) {
   if (this.scores.length < 3) {
     this.scores.push(score)
   }
+  this._closeFrameEarly()
+}
+
+Frame.prototype._closeFrameEarly = function () {
   if (this.scores.length === 2) {
     this._spareOrBetter()
   }
 }
 
 Frame.prototype._spareOrBetter = function () {
-  const REDUCER = (accumulator, currentValue) => accumulator + currentValue;
-  if (this.scores.reduce(REDUCER) < 10) {
+  if (this.totalScore() < 10) {
     this.scores.push(0)
   }
+}
+
+Frame.prototype.totalScore = function () {
+  const REDUCER = (accumulator, eachRollScore) => accumulator + eachRollScore
+  return this.scores.reduce(REDUCER, 0)
 }
 module.exports = Frame

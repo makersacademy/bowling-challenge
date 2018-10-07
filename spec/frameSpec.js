d15b1b20d59cc0dcd7fc8dd37bd52fa71fcbe6e2
Jon Urry <jon@urry.me>
2018-10-07 10:29:48 +0100

    initialize frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -8,5 +8,21 @@ describe('Frame', function() {
     it('should have an empty roll array', function() {
       expect(this.frame.rolls).toEqual([]);
     });
+
+    it('should have a score of zero', function() {
+      expect(this.frame.score).toEqual(0);
+    });
+
+    it('should have a scoreSoFar of zero', function() {
+      expect(this.frame.scoreSoFar).toEqual(0);
+    });
+
+    it('should have an empty note', function() {
+      expect(this.frame.note).toEqual('');
+    });
+
+    it('should have an empty outcome', function() {
+      expect(this.frame.outcome).toEqual('');
+    });
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,9 @@
 export class Frame {
   constructor() {
+    this.note = '';
+    this.outcome = '';
     this.rolls = [];
+    this.score = 0;
+    this.scoreSoFar = 0;
   }
 }

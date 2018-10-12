ad8251adebde35cd2f3a5560773c25d98223b823
AlexDresco <alexandra.d1510@gmail.com>
2018-10-12 14:21:40 +0100

    refactored jquery

--- jasmine-standalone-3.2.1/js/interface.js
+++ jasmine-standalone-3.2.1/js/interface.js
@@ -1,117 +1,70 @@
 $(document).ready(function(){
-  var bowling = new Bowling();
 
+  function displayAll() {
+    $('#total_score').text(bowling.total_score());
+    $('#frame_number').text(bowling.frame_number());
+    $('#roll_number').text(bowling.roll_number());
+    $('#frame_score').text(bowling.frame_score());
+  }
+
+  var bowling = new Bowling();
   $('#frame_number').text(bowling.frame_number());
   $('#roll_number').text(bowling.roll_number());
   $('#frame_score').text(bowling.frame_score());
 
+
   $('#zero').click(function(){
     bowling.roll(0)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#one').click(function(){
     bowling.roll(1)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
+    displayAll();
 
   });
   $('#two').click(function(){
     bowling.roll(2)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#three').click(function(){
     bowling.roll(3)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#four').click(function(){
     bowling.roll(4)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#five').click(function(){
     bowling.roll(5)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#six').click(function(){
     bowling.roll(6)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#seven').click(function(){
     bowling.roll(7)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#eight').click(function(){
     bowling.roll(8)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#nine').click(function(){
     bowling.roll(9)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+    displayAll();
   });
 
   $('#ten').click(function(){
     bowling.roll(10)
-    console.log(bowling.total_score());
-    $('#total_score').text(bowling.total_score());
-    $('#frame_number').text(bowling.frame_number());
-    $('#roll_number').text(bowling.roll_number());
-    $('#frame_score').text(bowling.frame_score());
-
+  displayAll();
   });
-
 });

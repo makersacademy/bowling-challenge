997b16206343d0de09da04b36abbf3c65a5df016
Florence Hapgood <flohapgood@icloud.com>
2018-10-13 21:51:07 +0100

    Jquery refactored with updateScores method and frame-number display added

--- interface.js
+++ interface.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
 var game = new Game();
 
-$('#gamescore').text(game.totalScore);
-
-$('#framescore').text(game._currentFrame.frameScore);
+updateScores()
 
 $('#rollBall').submit(function(event){
   event.preventDefault();
   game.rollBall($('#number').val());
-  $('#gamescore').text(game.totalScore); //could refactor into a method
-  $('#framescore').text(game._currentFrame.frameScore);
+  updateScores()
 })
 
+
+
+function updateScores() {
+  $('#gamescore').text(game.totalScore);
+  $('#framescore').text(game._currentFrame.frameScore);
+  $('#Frame-number').text(game._frameNumber);
+}
 
 
 })

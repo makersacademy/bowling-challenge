62643a1b217929b41c2d79bc91f110c8fecdd43d
Florence Hapgood <flohapgood@icloud.com>
2018-10-13 21:18:34 +0100

    Working rollBall form added

--- interface.js
+++ interface.js
@@ -3,6 +3,12 @@ var game = new Game();
 
 $('#gamescore').text(game.totalScore);
 
-
+$('#rollBall').submit(function(event){
+  event.preventDefault();
+  var number = $('#number').val();
+  n = Number(number); //put this in logic
+  game.rollBall(n);
+  $('#gamescore').text(game.totalScore);
+})
 
 })

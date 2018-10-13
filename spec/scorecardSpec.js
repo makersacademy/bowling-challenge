a421483613b84304bc3e994f50ae98e915c5b5e6
Darcie <darciewalsh@hotmail.com>
2018-10-13 18:12:55 +0100

    add isSpare and isStrike functions, needed to refactor to use frame index

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -11,13 +11,38 @@ describe ('Scorecard', function() {
   describe ('#roll', function() {
     it('rolling the ball adds knockedPins score to an array', function() {
       scorecard.roll(4);
-      expect(scorecard.getFrameScore()).toEqual([4]);
+      expect(scorecard.getPointsTracker()).toEqual([4]);
     });
 
     it('rolling the ball adds one count to currentRoll tracker', function() {
       scorecard.roll(4);
       scorecard.roll(5);
       expect(scorecard.getCurrentRoll()).toEqual(2);
+    });
+
+  });
+
+  describe ('#isStrike', function() {
+    it('returns false when the game starts', function() {
+      expect(scorecard.isStrike()).toEqual(false);
+    });
+
+    it('returns true when the first roll scores 10', function () {
+      scorecard.roll(10);
+      expect(scorecard.isStrike()).toEqual(true);
+    });
+
+  });
+
+  describe ('#isSpare', function() {
+    it('returns false when the game starts', function() {
+      expect(scorecard.isSpare()).toEqual(false);
+    });
+
+    it('returns true if total of the frame score is 10', function () {
+      scorecard.roll(4);
+      scorecard.roll(6);
+      expect(scorecard.isSpare()).toEqual(true);
     });
 
   });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -2,18 +2,27 @@
 
 function Scorecard() {
   this.currentRoll = 0;
-  this.frameScore = [];
+  this.pointsTracker = [];
 };
 
 Scorecard.prototype.roll = function(knockedPins) {
-  this.currentRoll++;
-  this.frameScore.push(knockedPins);
+  this.pointsTracker[this.currentRoll++] = knockedPins;
 };
 
-Scorecard.prototype.getFrameScore = function() {
-  return this.frameScore;
+Scorecard.prototype.getPointsTracker = function() {
+  return this.pointsTracker;
 };
 
 Scorecard.prototype.getCurrentRoll = function() {
   return this.currentRoll;
+};
+
+Scorecard.prototype.isStrike = function() {
+  var frameIndex = 0;
+  return this.pointsTracker[frameIndex] === 10;
+};
+
+Scorecard.prototype.isSpare = function() {
+  var frameIndex = 0;
+  return this.pointsTracker[frameIndex] + this.pointsTracker[frameIndex + 1] === 10;
 };

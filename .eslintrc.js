73201646d91f77a649a06ce214064da9b10f0bda
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-13 17:42:25 +0100

    linted

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "extends": "standard",
+    "env": {
+        "jasmine": true
+      },
+      "extends": "eslint:recommended"
+};
--- src/Game.js
+++ src/Game.js
@@ -1,76 +1,73 @@
-"use strict";
+'use strict'
 
-function Game() {
-  this.rolls = [];
-  this.frameCount = 0;
+function Game () {
+  this.rolls = []
+  this.frameCount = 0
 };
 
-Game.prototype.roll = function(pins) {
-  if (pins > 10) {
-    throw "Invalid input!";
-  };
-  this.rolls.push(pins);
-  this.countFrame(pins);
-};
+Game.prototype.roll = function (pins) {
+  this.rolls.push(pins)
+  this.countFrame(pins)
+}
 
-Game.prototype.countFrame = function(pins) {
+Game.prototype.countFrame = function (pins) {
   if (this.frameCount !== 10) {
     if (pins === 10 || this.rolls.length % 2 === 0) {
-      this.frameCount += 1;
+      this.frameCount += 1
     };
   };
-};
+}
 
-Game.prototype.isSpare = function(index) {
+Game.prototype.isSpare = function (index) {
   if (index + 2 < this.rolls.length) {
     if (this.rolls[index] + this.rolls[index + 1] === 10) {
-      return true;
+      return true
     };
   };
-  return false;
-};
+  return false
+}
 
-Game.prototype.isStrike = function(index) {
+Game.prototype.isStrike = function (index) {
   if (index + 2 < this.rolls.length) {
-    if(this.rolls[index] === 10) {
-      return true;
+    if (this.rolls[index] === 10) {
+      return true
     };
   };
-    return false;
-};
+  return false
+}
 
-Game.prototype.hasBonus = function() {
-  var rollIndex = this.rolls.length - 3;
+Game.prototype.hasBonus = function () {
+  var rollIndex = this.rolls.length - 3
 
   if (this.frameCount === 10) {
     if (this.isStrike(rollIndex) || this.isSpare(rollIndex)) {
-      return true;
+      return true
     };
   };
-  return false;
-};
+  return false
+}
 
-Game.prototype.score = function() {
-  var total = 0;
-  var maxRolls = this.rolls.length;
+Game.prototype.score = function () {
+  var total = 0
+  var maxRolls = this.rolls.length
 
   if (this.hasBonus()) {
-    maxRolls -= 2;
+    maxRolls -= 2
   };
   for (var i = 0; i < maxRolls; i++) {
     if (this.isStrike(i)) {
-      total += this.rolls[i] + this.rolls[i + 1] + this.rolls[i + 2];
+      total += this.rolls[i] + this.rolls[i + 1] + this.rolls[i + 2]
     } else if (this.isSpare(i)) {
-      total += this.rolls[i] + this.rolls[i + 1] + this.rolls[i + 2];
-      i++;
+      total += this.rolls[i] + this.rolls[i + 1] + this.rolls[i + 2]
+      i++
     } else {
-      total += this.rolls[i];
+      total += this.rolls[i]
     };
   };
-   return total;
-};
+  return total
+}
 
-Game.prototype.resetGame = function() {
-  this.rolls = [];
-  this.frameCount = 0;
-};
+Game.prototype.resetGame = function () {
+  this.rolls = []
+  this.frameCount = 0
+}

183a0d61e29b615e8baade8aac79640a07a4a66f
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-10-14 22:49:48 +0100

    Added function to add number of pins to table

--- src/interface.js
+++ src/interface.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   var thermostat = new Game();
+
+  $('#submit-btn').click(function() {
+    let frameValue = $('#frame').val();
+    let rollValue = $('#roll').val();
+    let pinsValue = $('#pins').val();
+    $('#' + frameValue + '-' + rollValue +  '-' + 'pins').text(pinsValue);
+  });
 });

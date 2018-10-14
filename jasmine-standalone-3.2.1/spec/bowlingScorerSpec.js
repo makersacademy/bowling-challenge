0249358d68b88e1795c055d75baee23fdf51cc43
Amy Denny-Guerin <amydenny-guerin@amys-mbp.home>
2018-10-14 15:19:39 +0100

    Code working. All tests passing. No extra logic needed for bonus roll

--- jasmine-standalone-3.2.1/spec/bowlingScorerSpec.js
+++ jasmine-standalone-3.2.1/spec/bowlingScorerSpec.js
@@ -27,4 +27,28 @@ describe("BowlingScorer", function() {
     }
     expect(newGame.score()).toEqual(29);
   });
+
+  it("should be able to handle one strike", function() {
+    newGame.roll(10);
+    for(var n = 0; n < 18; n++) {
+      newGame.roll(1);
+    }
+    expect(newGame.score()).toEqual(30);
+  })
+
+  it("can handle extra frame logic for perfect game", function() {
+    for(var n = 0; n < 22; n++) {
+    newGame.roll(10);
+    }
+    expect(newGame.score()).toEqual(300);
+  })
+
+  it("can handle extra frame logic for spare on 10th frame", function() {
+    for(var n = 0; n < 19; n++) {
+      newGame.roll(1);
+    }
+    newGame.roll(9);
+    newGame.roll(1);
+    expect(newGame.score()).toEqual(29);
+  })
 });
--- jasmine-standalone-3.2.1/src/bowlingScorer.js
+++ jasmine-standalone-3.2.1/src/bowlingScorer.js
@@ -1,22 +1,48 @@
 function BowlingScorer() {
-  this._scoreCard = [];
-}
+    this._scoreCard = [];
+};
 
-  BowlingScorer.prototype.roll = function(pinsDown) {
+BowlingScorer.prototype.roll = function(pinsDown) {
     this._scoreCard.push(pinsDown);
-  }
+};
 
-  BowlingScorer.prototype.score = function() {
-      var scoreSF = 0;
-      var roll = 0;
+BowlingScorer.prototype.score = function() {
+    var scoreSF = 0;
+    var roll = 0;
+    var here = this;
 
     for(var frame = 0; frame < 10; frame++) {
-      if (this._scoreCard[roll] + this._scoreCard[roll + 1] == 10) {
-          scoreSF += this._scoreCard[roll] + this._scoreCard[roll + 1] + this._scoreCard[roll + 2];
-      } else {
-          scoreSF += this._scoreCard[roll] + this._scoreCard[roll + 1];
-      }
-      roll += 2;
+        if (isStrike()) {
+            scoreSF += strikeScore();
+            roll++;
+        } else if (isSpare()) {
+            scoreSF += spareScore();
+            roll += 2;
+        } else {
+            scoreSF += noStrikeSpare();
+            roll += 2;
+        }
     }
+
     return scoreSF;
-};
+
+    function isSpare() {
+        return here._scoreCard[roll] + here._scoreCard[roll + 1] == 10;
+    }
+
+    function spareScore() {
+        return here._scoreCard[roll] + here._scoreCard[roll + 1] + here._scoreCard[roll + 2];
+    }
+
+    function isStrike() {
+        return here._scoreCard[roll] == 10;
+    }
+
+    function strikeScore() {
+      return here._scoreCard[roll] + here._scoreCard[roll + 1] + here._scoreCard[roll + 2];
+    }
+
+    function noStrikeSpare() {
+      return here._scoreCard[roll] + here._scoreCard[roll + 1];
+    }
+  };

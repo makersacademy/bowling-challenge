974b874a4b08f4597fb252bbf1c82f2605038931
Amy Denny-Guerin <amydenny-guerin@amys-mbp.home>
2018-10-14 19:39:03 +0100

    Started on app. Would need to refactor logic scope to get app to do what I want.

--- jasmine-standalone-3.2.1/src/bowlingScorerjQuery.js
+++ jasmine-standalone-3.2.1/src/bowlingScorerjQuery.js
@@ -1,87 +1,153 @@
-$(document).ready(function() {
-  var game = new BowlingScorer();
+var game = new BowlingScorer();
+
+  $('#score0').on('click', function() {
+      game.roll(0);
+      showAll();
+  })
 
   $('#score1').on('click', function() {
     game.roll(1);
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score2').on('click', function() {
     game.roll(2);
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score3').on('click', function() {
     game.roll(3);
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score4').on('click', function() {
     game.roll(4);
-    $('#score7').hide();
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score7').hide();
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score5').on('click', function() {
     game.roll(5);
-    $('#score6').hide();
-    $('#score7').hide();
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score6').hide();
+        $('#score7').hide();
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score6').on('click', function() {
     game.roll(6);
-    $('#score5').hide();
-    $('#score6').hide();
-    $('#score7').hide();
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score5').hide();
+        $('#score6').hide();
+        $('#score7').hide();
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score7').on('click', function() {
     game.roll(7);
-    $('#score4').hide();
-    $('#score5').hide();
-    $('#score6').hide();
-    $('#score7').hide();
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score4').hide();
+        $('#score5').hide();
+        $('#score6').hide();
+        $('#score7').hide();
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score8').on('click', function() {
     game.roll(8);
-    $('#score3').hide();
-    $('#score4').hide();
-    $('#score5').hide();
-    $('#score6').hide();
-    $('#score7').hide();
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score3').hide();
+        $('#score4').hide();
+        $('#score5').hide();
+        $('#score6').hide();
+        $('#score7').hide();
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
   $('#score9').on('click', function() {
     game.roll(9);
-    $('#score2').hide();
-    $('#score3').hide();
-    $('#score4').hide();
-    $('#score5').hide();
-    $('#score6').hide();
-    $('#score7').hide();
-    $('#score8').hide();
-    $('#score9').hide();
-    $('#score10').hide();
+    if (game._scoreCard.length % 2 != 0) {
+        $('#score2').hide();
+        $('#score3').hide();
+        $('#score4').hide();
+        $('#score5').hide();
+        $('#score6').hide();
+        $('#score7').hide();
+        $('#score8').hide();
+        $('#score9').hide();
+        $('#score10').hide();
+    } else {
+        showAll();
+    }
   })
 
-  // $('#Score').text("hello world");
-  // $('#score6').hide();
-})
+
+
+function disableAll() {
+  $("#score0").attr("disabled", true);
+  $("#score1").attr("disabled", true);
+  $("#score2").attr("disabled", true);
+  $("#score3").attr("disabled", true);
+  $("#score4").attr("disabled", true);
+  $("#score5").attr("disabled", true);
+  $("#score6").attr("disabled", true);
+  $("#score7").attr("disabled", true);
+  $("#score8").attr("disabled", true);
+  $("#score9").attr("disabled", true);
+  $("#score10").attr("disabled", true);
+}
+
+function showAll() {
+    $('#score0').show();
+    $('#score1').show();
+    $('#score2').show();
+    $('#score3').show();
+    $('#score4').show();
+    $('#score5').show();
+    $('#score6').show();
+    $('#score7').show();
+    $('#score8').show();
+    $('#score9').show();
+    $('#score10').show();
+}

51fa0fb1229e36d75d045855af409dda67c8e52a
Florence Hapgood <flohapgood@icloud.com>
2018-10-14 13:24:50 +0100

    Frame class replaced with mock in more Game tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,13 +6,22 @@ describe ("Game", function(){
 
   beforeEach(function(){
     game = new Game();
-    frame = new Frame();
+    frame = new Frame(); ///necessary?
 
   })
 
   describe ('rollBall', function(){
 
     it("calls _sendPointsToFrame on currentFrame attribute if there are still rolls left on that frame", function(){
+      //Didn't mock frame class here as couldn't work out how to create spy for function which
+      //has arguments. Calculate takes an argument.
+
+      // mockFrame = jasmine.createSpy('mockFrame', {
+      //    "_rollsLeft": 1,
+      //    "calculate(userInput)": 5,
+      //  })
+      //
+      // game = new Game(mockFrame)
       expect(game.rollBall(5)).toEqual(game._currentFrame.calculate(5));
     })
 
@@ -21,30 +30,32 @@ describe ("Game", function(){
        mockFrame = jasmine.createSpy('mockFrame', { "_rollsLeft": 0})
        game = new Game(mockFrame)
        game.rollBall(3);
-       expect(game._currentFrame.frameScore).toEqual(3) //this tests that it's not mockFrame because mockFrame doesn't know this method  
+       expect(game._currentFrame.frameScore).toEqual(3) //this tests that it's not mockFrame because mockFrame doesn't know this method
     })
-    it("still calls _sendPointsToFrame on fresh currentFrame if no rolls left on previous frame", function(){
-      game.rollBall(2);
-      game.rollBall(5);
-      expect(game.rollBall(3)).toEqual(game._currentFrame.calculate(3))
-    }) //vacuous test?
 
     it("calls to descreaseFrames function to decreases the frames left", function(){
       //is this the right way to test this? As it's a private method, I guessed that it is.
       //Otherwise, I would have created a new test for increaseFrames and tested it directly
+
+
       game.rollBall(2);
       game.rollBall(3);
       game.rollBall(2);
       expect(game._frameNumber).toEqual(2)
     })
+
     it("calls to total function to add frameScore to total after a frame is done", function(){
-      game.rollBall(2);
-      game.rollBall(3);
-      game.rollBall(5);
+
+      mockFrame = jasmine.createSpy('mockFrame', {"frameScore": 5, "_rollsLeft": 0})
+      game = new Game(mockFrame)
+      game.rollBall(7);
       expect(game.totalScore).toEqual(5) //same as before, I didn't test private total method directly
     })
+
     it("checks if last round was strike, and changes doubleBothScoresNextRound to true if so", function(){
-      game.rollBall(10);
+
+      mockFrame = jasmine.createSpy('mockFrame', {"isStrike": true, "_rollsLeft": 0})
+      game = new Game(mockFrame)
       game.rollBall(2);
       expect(game.doubleBothScoresNextRound).toEqual(true)
     })

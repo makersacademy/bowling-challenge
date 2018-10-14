8c5914b3b4ef7b301069c84e25c2d78bb1e11d7c
Amy Denny-Guerin <amydenny-guerin@Amys-MacBook-Pro.local>
2018-10-14 14:23:39 +0100

    Can handle a spare)

--- jasmine-standalone-3.2.1/spec/bowlingScorerSpec.js
+++ jasmine-standalone-3.2.1/spec/bowlingScorerSpec.js
@@ -1,9 +1,30 @@
 describe("BowlingScorer", function() {
+
+  beforeEach(function() {
+    newGame = new BowlingScorer();
+  });
+
   it("should return a score of 0 for all gutter balls", function() {
-    var newGame = new BowlingScorer();
     for(var n = 0; n < 20; n++) {
       newGame.roll(0);
     }
-    expect(newGame.finalScore()).toEqual(0)
-  })
+    expect(newGame.score()).toEqual(0);
+  });
+
+
+  it("should return a score of 20 for all 1s", function() {
+    for(var n = 0; n < 20; n++) {
+      newGame.roll(1);
+    }
+    expect(newGame.score()).toEqual(20);
+  });
+
+  it("should be able to handle one spare", function() {
+    newGame.roll(9);
+    newGame.roll(1);
+    for(var n = 0; n < 18; n++) {
+      newGame.roll(1);
+    }
+    expect(newGame.score()).toEqual(29);
+  });
 });
--- jasmine-standalone-3.2.1/src/bowlingScorer.js
+++ jasmine-standalone-3.2.1/src/bowlingScorer.js
@@ -2,14 +2,21 @@ function BowlingScorer() {
   this._scoreCard = [];
 }
 
-BowlingScorer.prototype.roll = function(pinsDown) {
-  this._scoreCard.push(pinsDown);
-}
+  BowlingScorer.prototype.roll = function(pinsDown) {
+    this._scoreCard.push(pinsDown);
+  }
 
-BowlingScorer.prototype.finalScore = function() {
-  return this._scoreCard.reduce(sumTotal);
-}
+  BowlingScorer.prototype.score = function() {
+      var scoreSF = 0;
+      var roll = 0;
 
-function sumTotal(total, num) {
-  return total + num;
-}
+    for(var frame = 0; frame < 10; frame++) {
+      if (this._scoreCard[roll] + this._scoreCard[roll + 1] == 10) {
+          scoreSF += this._scoreCard[roll] + this._scoreCard[roll + 1] + this._scoreCard[roll + 2];
+      } else {
+          scoreSF += this._scoreCard[roll] + this._scoreCard[roll + 1];
+      }
+      roll += 2;
+    }
+    return scoreSF;
+};

1374d2124c7375ba78544d1f33d2f58906f99663
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-10-14 18:09:54 +0100

    linted

--- spec/features/indexPage.test.js
+++ spec/features/indexPage.test.js
@@ -11,42 +11,42 @@ describe('Homepage', function () {
     })
 
     it('opening roll appears on screen immediately', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
         await expect(page).toMatch('6')
     })
 
     it('add scores to total score immediately', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
         await expect(page).toMatch('10')
     })
 
     it('returns correct score after spare', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
         await expect(page).toMatch('18')
     })
 
     it('returns correct score after strike', async () => {
-        await page.type('#input_score', "10")
+        await page.type('#input_score', '10')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
         await expect(page).toMatch('26')
     })
 
     it('returns 0 for Gutter game', async () => {
         for (var i = 0; i < 20; i++) {
-            await page.type('#input_score', "0")
+            await page.type('#input_score', '0')
             await page.click('#submit_score')
         }
         await expect(page).toMatch('0')
@@ -54,11 +54,9 @@ describe('Homepage', function () {
 
     it('returns 300 for Perfect game', async () => {
         for (var i = 0; i < 12; i++) {
-            await page.type('#input_score', "10")
+            await page.type('#input_score', '10')
             await page.click('#submit_score')
         }
         await expect(page).toMatch('300')
     })
 })
-
-
--- spec/features/indexPageFrames.test.js
+++ spec/features/indexPageFrames.test.js
@@ -7,35 +7,35 @@ describe('Homepage', function () {
     })
 
     it('opening roll appears in first frame space immediately', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
         await expect(page).toMatchElement('#frame-1', { text: '6' })
     })
 
     it('add scores to total score immediately', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
         await expect(page).toMatchElement('#frame-1', { text: '10' })
     })
 
     it('3rd roll fills 2nd frame', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
-        await page.type('#input_score', "2")
+        await page.type('#input_score', '2')
         await page.click('#submit_score')
-        await page.type('#input_score', "1")
+        await page.type('#input_score', '1')
         await page.click('#submit_score')
         await expect(page).toMatchElement('#frame-2', { text: '1' })
     })
 
     it('returns correct score after spare', async () => {
-        await page.type('#input_score', "6")
+        await page.type('#input_score', '6')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
         await expect(page).toMatchElement('#frame-1', { text: '14' })
         await expect(page).toMatchElement('#frame-2', { text: '4' })
@@ -45,11 +45,11 @@ describe('Homepage', function () {
     })
 
     it('returns correct score after strike', async () => {
-        await page.type('#input_score', "10")
+        await page.type('#input_score', '10')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
-        await page.type('#input_score', "4")
+        await page.type('#input_score', '4')
         await page.click('#submit_score')
         await expect(page).toMatchElement('#frame-1', { text: '18' })
         await expect(page).toMatchElement('#frame-2', { text: '8' })
@@ -58,7 +58,7 @@ describe('Homepage', function () {
 
     it('returns 0 for Gutter game', async () => {
         for (var i = 0; i < 20; i++) {
-            await page.type('#input_score', "0")
+            await page.type('#input_score', '0')
             await page.click('#submit_score')
         }
         await expect(page).toMatchElement('#frame-1', { text: '0' })
@@ -68,7 +68,7 @@ describe('Homepage', function () {
 
     it('returns 300 for Perfect game', async () => {
         for (var i = 0; i < 12; i++) {
-            await page.type('#input_score', "10")
+            await page.type('#input_score', '10')
             await page.click('#submit_score')
         }
         await expect(page).toMatchElement('#frame-1', { text: '30' })

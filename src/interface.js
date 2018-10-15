dfe1968b64c6de18ab936bd7217726a46e24bb18
Marianne Rachid <marianne.rachid@gmail.com>
2018-10-15 09:06:12 +0100

    add interface.js file

---
+++ src/interface.js
@@ -0,0 +1,82 @@
+$(document).ready(function runDocument() {
+  var game = new Game()
+
+  $('#namesub').on('click', function(event) {
+    event.preventDefault()
+    let name = $('#nameInput').val()
+    $('#nameTable').text(name)
+    $('#playername').hide()
+  })
+
+  $('.scoreButton').on('click', function(data) {
+    var input = Number(data.currentTarget.innerHTML)
+    for (var i = 0; i < 11; i++) {
+      $('#' + i).prop('disabled', false)
+    }
+
+    hidebuttons(input)
+
+    if ( input === 10 ) {
+      $('#F' + game.currentFrame + 'r' + game.currentRoll).text('X')
+    } else {
+      $('#F' + game.currentFrame + 'r' + game.currentRoll).text(input)
+    }
+
+    game.roll(input)
+
+    $('#totframe' + game.currentFrame).text(game.score())
+
+
+     if (game.currentFrame === 10 && game.currentRoll > 3) {
+       $('#total').text(game.score())
+      }
+    $('#roll').text(game.currentRoll)
+    $('#frame-num').text(game.currentFrame)
+    gameOver()
+  })
+
+  hidebuttons = function(input) {
+    if (game.currentRoll === 1 && input !== 10) {
+      var num = (10 - input) + 1
+
+      for (var i = num; i < 11; i++) {
+        $('#' + i).prop('disabled', true)
+      }
+    }
+  }
+
+   $('#reset').click(function() {
+    for (var i = 0; i < 11; i++) {
+      for (var j = 1; j <3; j++) {
+        $('#F' + i + 'r' + j).text(' ')
+      }
+      $('#totframe' + i).text(' ')
+    }
+
+    for (var i = 0; i < 4; i++) {
+      $('#F10r' + i ).text(' ')
+    }
+
+    $('#total').text(' ')
+    $('#frame-num').text('1')
+    $('#roll').text('1')
+    $('#nameTable').text(' ')
+    $('#playername').show()
+    $('#gamescore').text(' ')
+    for (var i = 0; i < 11; i++) {
+      $('#' + i).prop('disabled', false)
+    }
+     game.resetGame()
+   })
+
+   gameOver = function() {
+     if (game.currentFrame === 10 && game.currentRoll === 4) {
+       $('#frame-num').hide()
+       $('#roll').hide()
+       for (var i = 0; i < 11; i++) {
+         $('#' + i).prop('disabled', true);
+       }
+       $('#gamescore').text($('#nameInput').val() + " scored " + game.score() + '!')
+     }
+   }
+})

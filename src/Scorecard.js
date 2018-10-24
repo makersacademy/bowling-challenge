9afcd6c3fa03009095dd1e5b5aa46a930d1ba9d4
edthomas93 <et1270@my.bristol.ac.uk>
2018-10-24 17:36:55 +0100

    converted to ES

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,88 +1,91 @@
-function Scorecard() {
-  this.frame = [];
-  this.frameScore = 0
-  this.allRolls = [];
-  this.rollNumber = 0
-  this.allFrames = [];
-  this.cumulativeScore = 0;
-};
+class Scorecard {
 
-Scorecard.prototype.roll = function(roll) {
-  this.rollnumber += 1
-  if(roll > 10 || roll + this.frameScore > 10){
-    throw new Error("There is a maximum of 10 pins per frame");
+  constructor() {
+    this.frame = [];
+    this.frameScore = 0
+    this.frameNumber = 0
+    this.allRolls = [];
+    this.rollNumber = 0
+    this.allFrames = [];
+    this.cumulativeScore = 0;
   }
-  else{
-    this.cumulativeScore += roll;
-    this.frame.push(roll);
-    this.frameScore += roll;
-    if(this.frame.length === 1 && this.allFrames === 9 || this.allFrames === 10){
-      frameTen();
+
+  roll(roll) {
+    this.rollnumber += 1
+    if(roll > 10 || roll + this.frameScore > 10){
+      throw new Error("There is a maximum of 10 pins per frame");
     }
     else{
-      if(this.strikebonus > 0){
-        this.cumulativeScore += roll;
-        this.strikebonus -=1;
-      };
-      if(this.strikebonus2 > 0){
-        this.cumulativeScore += roll;
-        this.strikebonus2 -=1;
-      };
-      if(this.sparebonus > 0){
-        this.cumulativeScore += roll;
-        this.sparebonus -= 1;
-      };
-      this.allRolls.push(roll);
-      if(roll === 10 && this.frame.length === 1){
-        this.strike();
-        this.endFrame();
+      this.cumulativeScore += roll;
+      this.frame.push(roll);
+      this.frameScore += roll;
+      if(this.frame.length === 1 && this.allFrames === 9 || this.allFrames === 10){
+        frameTen();
       }
-      else if(this.frame.length === 2){
-        if(this.frameScore === 10){
-          this.spare();
+      else{
+        if(this.strikebonus > 0){
+          this.cumulativeScore += roll;
+          this.strikebonus -=1;
+        };
+        if(this.strikebonus2 > 0){
+          this.cumulativeScore += roll;
+          this.strikebonus2 -=1;
+        };
+        if(this.sparebonus > 0){
+          this.cumulativeScore += roll;
+          this.sparebonus -= 1;
+        };
+        this.allRolls.push(roll);
+        if(roll === 10 && this.frame.length === 1){
+          this.strike();
           this.endFrame();
         }
-        else{
-          this.endFrame();
+        else if(this.frame.length === 2){
+          if(this.frameScore === 10){
+            this.spare();
+            this.endFrame();
+          }
+          else{
+            this.endFrame();
+          };
         };
       };
+    };
+  };
+  endFrame() {
+    this.allFrames.push(this.frame);
+    this.frame = [];
+    this.frameScore = 0;
+  };
+
+  spare() {
+    this.sparebonus = 1;
+  };
+
+  strike() {
+    if(this.strikebonus > 0){
+      this.strikebonus2 = 2
     }
+    else{
+      this.strikebonus = 2;
+    };
   };
-};
 
-Scorecard.prototype.endFrame = function(){
-  this.allFrames.push(this.frame);
-  this.frame = [];
-  this.frameScore = 0;
-};
-
-Scorecard.prototype.spare = function(){
-  this.sparebonus = 1;
-};
-
-Scorecard.prototype.strike = function(){
-  if(this.strikebonus > 0){
-    this.strikebonus2 = 2
-  }
-  else{
-    this.strikebonus = 2;
+  frameTen() {
+    if(roll === 10 && this.frame.length === 1){
+      this.strike();
+    }
+    else if(this.frame.length === 2 && this.frameScore === 10){
+      this.spare();
+    };
   };
-};
 
-Scorecard.prototype.frameTen = function(){
-  if(roll === 10 && this.frame.length === 1){
-    this.strike();
-  }
-  else if(this.frame.length === 2 && this.frameScore === 10){
-    this.spare();
-  };
-};
+  // Spechelper
 
-// Spechelper
-
-Scorecard.prototype.repeat = function(pins, number){
-  var times = number;
-  for(var i=0; i < times; i++){
-    this.roll(pins);
+  repeat(pins, number){
+    var times = number;
+    for(var i=0; i < times; i++){
+      this.roll(pins);
+    };
   };
 };

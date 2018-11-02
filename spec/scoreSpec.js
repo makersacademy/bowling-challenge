47cccb749d77b8ea169c0efe00bee18405e7c010
Caitlin Cooling <ccooling97@gmail.com>
2018-11-02 12:08:38 +0000

    First test: score has a getScore method. Written and passed

---
+++ spec/scoreSpec.js
@@ -0,0 +1,18 @@
+describe("Score", function() {
+  var score;
+
+  beforeEach(function() {
+    score = new Score();
+  });
+
+  describe("getScore", function() {
+    it("returns the current score", function () {
+      // arrange
+
+      // act
+
+      // assert
+      expect(score.getScore()).toBe(0)
+    });
+  });
+});
---
+++ src/score.js
@@ -0,0 +1,7 @@
+var Score = function(){
+  this.score = 0;
+};
+
+Score.prototype.getScore = function () {
+  return this.score;
+};

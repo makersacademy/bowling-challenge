1d71e27fe4dd8103bde40ee6c677783cd0f85b04
Aimée Craig <aimeecraig1@gmail.com>
2018-11-02 11:28:47 +0000

    game has 10 frames

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -5,6 +5,12 @@ describe('Game', function() {
     game = new Game();
   });
 
+  describe('initialize', function() {
+    it('has 10 frames', function() {
+      expect(game.frames).toEqual(10);
+    });
+  });
+
   describe('addScore', function() {
     it('adds a score to the array', function() {
       game.addScore(7)
--- src/game.js
+++ src/game.js
@@ -1,5 +1,6 @@
 function Game() {
   this.scores = [];
+  this.frames = 10;
 }
 
 Game.prototype.addScore = function (score) {

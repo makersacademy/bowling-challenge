6d7e6cffc2359689770b4d2a5b7846bef040cde7
hurc87 <zoe1987@btinternet.com>
2018-11-02 10:25:04 +0000

    started score and built methods to add to score

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -12,7 +12,55 @@ describe('Scorecard',function(){
     expect(scorecard.total).toEqual(0);
   });
 
+  it('adds 1 to the total',function(){
+    scorecard.addOne();
+    expect(scorecard.total).toEqual(1);
+  });
 
+  it('adds 2 to the total',function(){
+    scorecard.addTwo();
+    expect(scorecard.total).toEqual(2);
+  });
+
+  it('adds 3 to the total',function(){
+    scorecard.addThree();
+    expect(scorecard.total).toEqual(3);
+  });
+
+  it('adds 4 to the total',function(){
+    scorecard.addFour();
+    expect(scorecard.total).toEqual(4);
+  });
+
+  it('adds 5 to the total',function(){
+    scorecard.addFive();
+    expect(scorecard.total).toEqual(5);
+  });
+
+  it('adds 6 to the total',function(){
+    scorecard.addSix();
+    expect(scorecard.total).toEqual(6);
+  });
+
+  it('adds 7 to the total',function(){
+    scorecard.addSeven();
+    expect(scorecard.total).toEqual(7);
+  });
+
+  it('adds 8 to the total',function(){
+    scorecard.addEight();
+    expect(scorecard.total).toEqual(8);
+  });
+
+  it('adds 9 to the total',function(){
+    scorecard.addNine();
+    expect(scorecard.total).toEqual(9);
+  });
+
+  it('adds 10 to the total',function(){
+    scorecard.addTen();
+    expect(scorecard.total).toEqual(10);
+  });
 
 
 
--- src/bowling.js
+++ src/bowling.js
@@ -3,3 +3,43 @@
 function Scorecard(){
   this.total = 0;
 }
+
+Scorecard.prototype.addOne = function () {
+  this.total +=1;
+};
+
+Scorecard.prototype.addTwo = function () {
+  this.total +=2;
+};
+
+Scorecard.prototype.addThree = function () {
+  this.total +=3;
+};
+
+Scorecard.prototype.addFour = function () {
+  this.total +=4;
+};
+
+Scorecard.prototype.addFive = function () {
+  this.total +=5;
+};
+
+Scorecard.prototype.addSix = function () {
+  this.total +=6;
+};
+
+Scorecard.prototype.addSeven = function () {
+  this.total +=7;
+};
+
+Scorecard.prototype.addEight = function () {
+  this.total +=8;
+};
+
+Scorecard.prototype.addNine = function () {
+  this.total +=9;
+};
+
+Scorecard.prototype.addTen = function () {
+  this.total +=10;
+};

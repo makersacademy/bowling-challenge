48448573a8f9f2197f75f454cb4cac92494533da
Tolvic <terence.h.mace@gmail.com>
2018-11-02 15:36:47 +0000

    Test for bonus calculation on strikes written and passed. All previous tests and methods needed to be rewritten for this.

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,14 +2,46 @@ describe("Frame", function() {
   var frameOne;
 
   beforeEach(function() {
-    frameOne = new Frame();
+    standardFrame = new Frame([4,5]);
+    spareFrame = new Frame([5,5]);
+    strikeFrame = new Frame([10]);
+    gutterFrame = new Frame([]);
   });
 
-  it("should total the score from two bowls", function() {
-    expect(frameOne.score(5,4)).toEqual(9);
-    expect(frameOne.score(10,0)).toEqual(10);
-    expect(frameOne.score(5,5)).toEqual(10);
-    expect(frameOne.score(0,0)).toEqual(0);
+  it("totals the score from the two bowls in a single frame.", function() {
+
+    expect(standardFrame.frameScore).toEqual(9);
+    expect(spareFrame.frameScore).toEqual(10);
+    expect(strikeFrame.frameScore).toEqual(10);
+    expect(gutterFrame.frameScore).toEqual(0);
+  });
+
+  it("throws an error if more than 10 pins are knocked down in a standard frame.", function() {
+
+    expect( function(){ new Frame([9,9]); } ).toThrowError('Cannot knock down more than 10 pins in a single frame');
+  });
+
+  it("throws an error if there is more than two bowls in a standard frame", function() {
+
+    expect( function(){ new Frame([1,1,3]); } ).toThrowError('Cannot have more than two bowls in a standard frame');
+  });
+
+  it("should calculate the the bonus for a spare", function() {
+    spareFrame.calculateBonus(standardFrame, standardFrame)
+    expect(spareFrame.bonus).toEqual(4)
+    spareFrame.calculateBonus(strikeFrame, strikeFrame)
+    expect(spareFrame.bonus).toEqual(10)
+    spareFrame.calculateBonus(gutterFrame, gutterFrame)
+    expect(spareFrame.bonus).toEqual(0)
+  });
+
+  it("should calculate the the bonus for a strike", function() {
+    strikeFrame.calculateBonus(standardFrame, standardFrame)
+    expect(strikeFrame.bonus).toEqual(9)
+    strikeFrame.calculateBonus(strikeFrame, strikeFrame)
+    expect(strikeFrame.bonus).toEqual(20)
+    strikeFrame.calculateBonus(gutterFrame, gutterFrame)
+    expect(strikeFrame.bonus).toEqual(0)
   });
 
 
--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,63 @@
-function Frame() {
+var Frame = function(bowls) {
+  this.bowls = bowls;
+  this.frameScore;
+  this.bonus;
+  this.TotalScore;
+  this.score();
 }
-Frame.prototype.score = function(bowlOne, bowlTwo) {
-  return bowlOne + bowlTwo
+
+// **************** Class Functinos ******************
+
+Frame.prototype.score = function() {
+
+  if(this.bowls.length > 2) {
+   throw new Error('Cannot have more than two bowls in a standard frame');
+  };
+
+  var sum = sumArr(this.bowls)
+
+  if(sum > 10) {
+   throw new Error('Cannot knock down more than 10 pins in a single frame');
+  };
+
+  this.frameScore = sum;
+};
+
+Frame.prototype.calculateBonus = function(framePlusOne, framePlusTwo) {
+
+  if (framePlusOne.bowls.length === 0) {
+    this.bonus = 0
+  } else if(this._isSpare()){
+    this.bonus = framePlusOne.bowls[0];
+  } else if(this._isStrike()){
+    var nextFrames = framePlusOne.bowls.concat(framePlusTwo.bowls);
+    this.bonus = nextFrames[0] + nextFrames[1];
+  }
+
+
+
+
+
+
+};
+
+// ************* Private Functions ***********
+
+Frame.prototype._isStrike = function(){
+  return this.bowls[0] === 10;
+}
+
+Frame.prototype._isSpare = function(){
+  return this.bowls[0] < 10 && sumArr(this.bowls) === 10;
+}
+
+// ********* Utility Functions *********
+
+sumArr = function(arr){
+  var sum = arr.reduce(add, 0);
+
+  function add(a, b) {
+    return a + b;
+  }
+  return sum;
 };

61dfd3f1ba8ce1a94405117aff4fcbcf957c4756
Henry Cooksley <henry.cooksley@gmail.com>
2018-11-03 13:03:02 +0000

    small refactor to have a Game and Frame class

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,24 +7,32 @@ describe("Game", function() {
 
   it("scores a gutter game as zero", function() {
     for (var i = 0; i < 20; i++) {
-      game.addNewRoll(0);
+      game.roll(0);
     }
     expect(game.getCurrentScore()).toEqual(0);
   });
 
   it("scores a one point game as one", function() {
-    game.addNewRoll(1);
+    game.roll(1);
     for (var i = 0; i < 19; i++) {
-      game.addNewRoll(0);
+      game.roll(0);
     }
     expect(game.getCurrentScore()).toEqual(1);
   });
 
   it("scores a game where there is one point per frame as ten", function() {
     for (var i = 0; i < 10; i++) {
-      game.addNewRoll(1);
-      game.addNewRoll(0);
+      game.roll(1);
+      game.roll(0);
     }
     expect(game.getCurrentScore()).toEqual(10);
+  });
+
+  it("scores a game where there are two points per frame as twenty", function() {
+    for (var i = 0; i < 10; i++) {
+      game.roll(1);
+      game.roll(1);
+    }
+    expect(game.getCurrentScore()).toEqual(20);
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,48 @@
 function Game() {
+  this.frames = [];
+  this.currentFrame = null;
+}
+
+// this is called by the user
+Game.prototype.roll = function(rollToBeAdded) {
+  if (this.isEmptyGame() || this.isLastFrameFull()) {
+    this.createNewFrame();
+  }
+  this.updateCurrentFrame();
+  this.currentFrame.addNewRoll(rollToBeAdded);
+};
+
+// can be called throughout the game to update the current score in the UI
+Game.prototype.getCurrentScore = function() {
+  var total = 0;
+  this.frames.forEach(function(frame) {
+    total = frame.rollsArray.reduce(function(accumulator, currentValue) {
+      return accumulator + currentValue;
+    });
+  });
+  return total;
+}
+
+Game.prototype.createNewFrame = function() {
+  this.frames.push(new Frame());
+}
+
+Game.prototype.isEmptyGame = function() {
+  return this.frames.length == 0;
+}
+
+Game.prototype.isLastFrameFull = function() {
+  return this.frames[this.frames.length - 1].length == 2;
+}
+
+Game.prototype.updateCurrentFrame = function() {
+  this.currentFrame = this.frames[this.frames.length - 1];
+}
+
+function Frame() {
   this.rollsArray = [];
 }
 
-Game.prototype.addNewRoll = function(rollToBeAdded) {
+Frame.prototype.addNewRoll = function(rollToBeAdded) {
   this.rollsArray.push(rollToBeAdded);
-};
-
-Game.prototype.getCurrentScore = function() {
-  var total = this.rollsArray.reduce(function(accumulator, currentValue) {
-    return accumulator + currentValue;
-  });
-  return total;
 }

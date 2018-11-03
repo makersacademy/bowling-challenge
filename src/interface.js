9af28fef816665f168a547a52517293000eb7df3
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-03 15:34:09 +0000

    changed html layout

--- src/interface.js
+++ src/interface.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
   var game = new Game();
 
   $('#frame_submit').click(function() {
-    var bowl1 = $('#frame1_bowl1').val();
-    var bowl2 = $('#frame1_bowl2').val();
-    game.frame(Number(bowl1), Number(bowl2));
+    var bowl1 = Number($('#bowl1').val());
+    var bowl2 = Number($('#bowl2').val());
+    game.frame(bowl1 ,bowl2);
+    updateScoreCard(bowl1 ,bowl2);
+  });
+
+  function updateScoreCard(bowl1 ,bowl2) {
+    $('#frame1_bowl1').text(bowl1);
+    $('#frame1_bowl2').text(bowl2);
     $('#frame1_score').text(game.scoreCard[0].frameScore);
     $('#frame1_running_score').text(game.scoreCard[0].runningScore);
     $('#frame1_notes').text('');
-  })
+  };
 
 })

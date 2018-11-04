8f7c1a6c14c2f8f45377532d1d380df2c1294892
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-04 17:03:50 +0000

    Added frame class to increase accuracy for strike and spare scores

--- src/frame.js
+++ src/frame.js
@@ -2,11 +2,9 @@ function Frame() {
   this.score = []
 }
 
-Frame.prototype.add = function (roll_score) {
-  var roll = new Roll()
-  roll.pins(roll_score)
-  this.score.push(roll.score)
-  if (roll_score == 10) {
+Frame.prototype.add = function (number) {
+  this.score.push(number)
+  if (number == 10) {
     return this.score.push(0);
   }
 }
--- src/scorecard.js
+++ src/scorecard.js
@@ -2,42 +2,49 @@ function Scorecard() {
   this.frames = []
   this.score = 0
   this.bonus = []
+  this.frame = new Frame()
+  this.secondLastArrayinArray = 0
+  this.lastArrayinArray = 0
 }
 
 Scorecard.prototype.add = function (number) {
-  this.frames.push(number)
-  if (number === 10) {
-    return this.frames.push(0);
+  this.frame.add(number)
+  if (this.frame.score.length === 2) {
+    this.frames.push(this.frame.score)
+    this.frame.score = []
+    if (this.frames.length > 1) {
+      this.secondLastArrayinArray = this.frames[this.frames.length - 2]
+    }
+    this.lastArrayinArray = this.frames[this.frames.length - 1]
+    this._addBonus()
   }
-  this._addBonus()
 }
 
 Scorecard.prototype.sum = function () {
-  if ((this.frames[this.frames.length - 1] === 0 || this.frames[this.frames.length - 2] === 0) && this.bonus.length === 0) {
-    return this.score = this.frames.reduce(add) - 10
+  if (this.lastArrayinArray[this.lastArrayinArray.length - 2] === 10 && this.bonus.length === 0) {
+    return this.score = this.frames.flat().reduce(add) - 10
   }
-  if (this.frames[this.frames.length - 1] === 0 || this.frames[this.frames.length - 2] === 0) {
-    return this.score = this.frames.reduce(add) - 10 + this.bonus.reduce(add)
+  if (this.lastArrayinArray[this.lastArrayinArray.length - 2] === 10) {
+    return this.score = this.frames.flat().reduce(add) - 10 + this.bonus.flat().reduce(add)
   }
-  if (((this.frames[this.frames.length - 1] + this.frames[this.frames.length - 2]) === 10) && this.bonus.length === 0) {
-    return this.score = this.frames.reduce(add) - 10
+  if (((this.lastArrayinArray[this.lastArrayinArray - 1] + this.lastArrayinArray[this.lastArrayinArray.length - 2]) === 10) && this.bonus.length === 0) {
+    return this.score = this.frames.flat().reduce(add) - 10
   }
-  if ((this.frames[this.frames.length - 1] + this.frames[this.frames.length - 2]) === 10) {
-    return this.score = this.frames.reduce(add) - 10 + this.bonus.reduce(add)
+  if ((this.lastArrayinArray[this.lastArrayinArray.length - 1] +  this.lastArrayinArray[this.lastArrayinArray.length - 2]) === 10) {
+    return this.score = this.frames.flat().reduce(add) - 10 + this.bonus.flat().reduce(add)
   }
   if (this.bonus.length === 0) {
-    return this.score = this.frames.reduce(add)
+    return this.score = this.frames.flat().reduce(add)
   }
-  this.score = this.frames.reduce(add) + this.bonus.reduce(add)
+  this.score = this.frames.flat().reduce(add) + this.bonus.flat().reduce(add)
 }
 
 Scorecard.prototype._addBonus = function () {
-  if (this.frames[this.frames.length - 3] === 0) {
-    this.bonus.push(this.frames[this.frames.length - 1])
-    return this.bonus.push(this.frames[this.frames.length - 2])
+  if (this.secondLastArrayinArray[this.secondLastArrayinArray.length - 2] === 10) {
+    return this.bonus.push(this.lastArrayinArray)
   }
-  if ((this.frames[this.frames.length - 3] + this.frames[this.frames.length - 4]) === 10) {
-    this.bonus.push(this.frames[this.frames.length - 1])
+  if ((this.secondLastArrayinArray[this.secondLastArrayinArray.length - 1] + this.secondLastArrayinArray[this.secondLastArrayinArray.length - 2]) === 10) {
+    this.bonus.push(this.lastArrayinArray[this.lastArrayinArray.length - 2])
   }
 }
 

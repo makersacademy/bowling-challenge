841c1526717af8cc63d2455b054a21f1749fce75
Caitlin Cooling <ccooling97@gmail.com>
2018-11-04 09:15:40 +0000

    User can now see what frame they are on

--- src/interface.js
+++ src/interface.js
@@ -7,22 +7,26 @@ $(document).ready(function () {
 
   updateScoreText();
   frameScoreText();
+  updateFrameText();
 
   $('button').click(function() {
     if(frame.checkRolls() < 2){
       value = $(this).attr('value');
       newRoll1 = roll.getRoll(value);
-      console.log("roll 1: ", newRoll1);
       frame.addRoll();
     } else {
       value = $(this).attr('value');
       newRoll2 = roll.getRoll(value);
-      console.log("roll 2: ", newRoll2);
       frameScore();
       updateScore();
+      updateFrameText();
       frame.addRoll();
     };
   });
+
+  function updateFrameText() {
+    $('#frame_number').text(game.frameNumber());
+  }
 
   function frameScore() {
     frame.calculateScore(parseInt(newRoll1), parseInt(newRoll2));
@@ -30,8 +34,8 @@ $(document).ready(function () {
   };
 
   function updateScore() {
-    currentScore = frame.getCurrentScore()
-    game.updateTotalScore(currentScore)
+    currentScore = frame.getCurrentScore();
+    game.updateTotalScore(currentScore);
     updateScoreText();
   };
 

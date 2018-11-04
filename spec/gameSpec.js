2dc10099b04d4b5203166e6db9bcd3e7b804712e
sacullezzar <lucas.razzell@gmail.com>
2018-11-04 19:54:08 +0000

    bonus round implemented

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,21 +1,45 @@
 'use strict';
 
 describe('Game', function() {
-  // 
-  // var game;
-  //
-  // beforeEach(function() {
-  //   game = new Game();
-  // });
-  //
-  // it('starts at 0 scored', function() {
-  //   expect(game.getFinalScore()).toEqual(0);
-  // });
-  //
-  // it('provides a total score', function(){
-  //   for (var i = 0; i < 10; i++) {
-  //     game.frameScore(8);
-  //   }
-  //   expect(game.getFinalScore()).toEqual(80);
-  // })
+
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it('can score an entire game as 0', function() {
+    for(var i = 0; i < 20; i++) {
+      game.roll(0);
+    };
+    expect(game.score()).toEqual(0);
+  });
+
+  it('can score one pin per frame', function() {
+    for(var i = 0; i < 20; i++) {
+      game.roll(1);
+    };
+    expect(game.score()).toEqual(20);
+  })
+
+  it('grants a bonus bowl in frame 10', function() {
+    for(var i = 0; i < 18; i++) {
+      game.roll(0);
+    };
+    game.roll(5);
+    game.roll(5);
+    console.log(game.score());
+    game.roll(10);
+    expect(game.score()).toEqual(20);
+    expect(game.bonus_frame).toBe(true);
+  })
+
+  it('denies the bonus round', function() {
+    for(var i = 0; i < 10; i++) {
+      game.roll(9);
+      game.roll(0);
+    };
+    expect(game.score()).toEqual(90);
+    expect(game.bonus_frame).toBe(false);
+  })
 });
--- src/game.js
+++ src/game.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function Game() {
+  this.NORMAL_FRAMES = 10
+  this.rolls = []
+  this.bonus_frame = false
+};
 
-}
+Game.prototype.roll = function (pins) {
+
+  if (this.rolls[19] + this.rolls[18] >= 10) {
+    this.bonus_frame = true
+  }
+    else this.bonus_frame = false;
+  this.rolls.push(pins);
+
+};
+
+Game.prototype.score = function () {
+
+  for (var frameId = 0; frameId < 10; frameId++) {
+    result += this.rolls[rollId] + this.rolls[rollId + 1];
+    rollId += 2;
+  }
+  if (this.bonus_frame == true) {
+    result += this.rolls[20]
+  }
+  return result;
+};

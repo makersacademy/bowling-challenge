2363cc6c9ff4893ac9a9a1f359547e1fc8fe2390
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-11-07 22:45:45 +0000

    06-11-2018 @11pm: Tests for frame completed and passing. Next to write tests for totalling the score in Bowling Scorecard.

--- public/js/bowlingScore.js
+++ public/js/bowlingScore.js
@@ -11,15 +11,32 @@ function BowlingScorecard() {
 
 
 
-function Frame(userInput1, userInput2) {
+function Frame(userInput1 = 1, userInput2 = 1) {
   this.bowl1 = userInput1;
   this.bowl2 = userInput2;
 };
 
 Frame.prototype.getBowl1 = function() {
-  return this.bowl1
+  return this.bowl1;
 };
 
 Frame.prototype.getBowl2 = function() {
-  return this.bowl2
+  return this.bowl2;
+};
+
+Frame.prototype.setBowl1FinalScore = function() {
+  if (this.getBowl1() === 10) {
+    return 'X';
+  } else {
+  return this.getBowl1();
+  }
+};
+
+Frame.prototype.setBowl2FinalScore = function() {
+  if (this.getBowl1() < 10 && this.getBowl1() + this.getBowl2() === 10) {
+    return '/';
+  } 
+  if (this.getBowl1() < 10) {
+    return this.bowl2;
+  }
 };
--- spec/bowlingScoreSpec.js
+++ spec/bowlingScoreSpec.js
@@ -20,14 +20,25 @@ describe('BowlingScorecard', function() {
 
 describe('Frame', function() {
 
+  var strike;
+  var spare;
   var frame;
 
   beforeEach(function() {
     frame = new Frame(3, 5);
+    strike = new Frame(10)
+    spare = new Frame(6, 4)
   });
 
   it('records the outcomes of bowls', function() {
     expect(frame.getBowl1()).toEqual(3);
     expect(frame.getBowl2()).toEqual(5);
+  });
+
+  it('gets a bowling score', function() {
+    expect(strike.setBowl1FinalScore()).toEqual('X')
+    expect(spare.setBowl1FinalScore()).toEqual(6)
+    expect(spare.setBowl2FinalScore()).toEqual('/')
+    expect(frame.setBowl2FinalScore()).toEqual(5)
   });
 });

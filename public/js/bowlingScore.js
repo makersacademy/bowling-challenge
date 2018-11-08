8a943f75754378f3f6f72c8969c64298464634b5
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-11-08 09:03:11 +0000

    07-11-2018 @9am: Final Frame created, but some scoring and syntax issues require resolving.

--- public/js/bowlingScore.js
+++ public/js/bowlingScore.js
@@ -9,10 +9,10 @@ function BowlingScorecard() {
     return this.score;
   };
 
-  BowlingScorecard.prototype.setNewFrame = function(userInput1, userInput2) {
+  BowlingScorecard.prototype.setNewFrame = function(userInput1, userInput2, userInput3) {
     if(this.frameCounter < 9) {
     this.frame = new Frame(userInput1, userInput2);
-    } else { this.frame = new FinalFrame(userInput1, userInput2)
+    } else { this.frame = new FinalFrame(userInput1, userInput2, userInput3)
     }
     this.frameCounter += 1;
   };
@@ -38,5 +38,51 @@ Frame.prototype.setBowl2Score = function() {
   } 
   if (this.bowl1 < 10) {
     return this.bowl2;
+  }
+};
+
+function FinalFrame(userInput1 = 1, userInput2 = 1, userInput3 = 1) {
+  this.bowl1 = userInput1;
+  if(this.bowl1 < 10) {
+  this.bowl2 = userInput2;
+  } else {
+    this.bowl3 = userInput2;
+    return
+  }
+  if(this.bowl1 + this.bowl2 === 10) {
+    this.bowl3 = userInput3;
+    return
+  }  
+};
+
+FinalFrame.prototype.setBowl1Score = function() {
+  if (this.bowl1 === 10) {
+    return 'X';
+  } else {
+  return this.bowl1;
+  }
+};
+
+FinalFrame.prototype.setBowl2Score = function() {
+  if (this.bowl1 < 10 && this.bowl1 + this.bowl2 === 10) {
+    return '/';
+  } 
+  if (this.bowl1 < 10) {
+    return this.bowl2;
+  }
+};
+
+FinalFrame.prototype.setBowl3Score = function() {
+  if (this.bowl1 === 'X' && this.bowl3 === 10) {
+    return 'X';
+  }
+  if (this.bowl1 === 'X' && this.bowl3 < 10) {
+    return this.bowl3;
+  }
+  if (this.bowl2 === '/' && this.bowl3 === 10) {
+    return 'X';
+  }
+  if (this.bowl2 === '/' && this.bowl3 < 10) {
+    return this.bowl3;
   }
 };
--- spec/bowlingScoreSpec.js
+++ spec/bowlingScoreSpec.js
@@ -13,30 +13,27 @@ describe('BowlingScorecard', function() {
   });
 
   it('has a frame counter', function() {
-    expect(bowlingScorecard.frameCounter).toEqual(0)
+    expect(bowlingScorecard.frameCounter).toEqual(0);
   })
 
   it('holds a frame', function() {
     bowlingScorecard.setNewFrame(2, 3)
     expect(bowlingScorecard.frame instanceof Frame).toBeTruthy();
-    expect(bowlingScorecard.frameCounter).toEqual(1)
-    expect(bowlingScorecard.frame.setBowl1Score()).toEqual(2)
-    expect(bowlingScorecard.frame.setBowl2Score()).toEqual(3)
+    expect(bowlingScorecard.frameCounter).toEqual(1);
+    expect(bowlingScorecard.frame.setBowl1Score()).toEqual(2);
+    expect(bowlingScorecard.frame.setBowl2Score()).toEqual(3);
     });
   
   it('holds a final frame', function() {
     for (var i = 0; i < 10; i++) {
-      bowlingScorecard.setNewFrame(4, 5)
-    }
-    expect()
-    expect(bowlingScorecard.frame instanceof LastFrame).toBeTruthy();
-    expect(bowlingScorecard.frameCounter).toEqual(10)
-    expect(bowlingScorecard.frame.setBowl1Score()).toEqual(4)
-    expect(bowlingScorecard.frame.setBowl2Score()).toEqual(5)
+      bowlingScorecard.setNewFrame(4, 6, 8);
+    };
+    expect(bowlingScorecard.frame instanceof FinalFrame).toBeTruthy();
+    expect(bowlingScorecard.frameCounter).toEqual(10);
+    expect(bowlingScorecard.frame.setBowl1Score()).toEqual(4);
+    expect(bowlingScorecard.frame.setBowl2Score()).toEqual('/');
+    expect(bowlingScorecard.frame.setBowl2Score()).toEqual(8);
     });
-  
-  
-
 });
 
 describe('Frame', function() {
@@ -47,14 +44,37 @@ describe('Frame', function() {
 
   beforeEach(function() {
     frame = new Frame(3, 5);
-    strike = new Frame(10)
-    spare = new Frame(6, 4)
+    strike = new Frame(10);
+    spare = new Frame(6, 4);
   });
 
   it('gets a bowling score', function() {
-    expect(strike.setBowl1Score()).toEqual('X')
-    expect(spare.setBowl1Score()).toEqual(6)
-    expect(spare.setBowl2Score()).toEqual('/')
-    expect(frame.setBowl2Score()).toEqual(5)
+    expect(strike.setBowl1Score()).toEqual('X');
+    expect(spare.setBowl1Score()).toEqual(6);
+    expect(spare.setBowl2Score()).toEqual('/');
+    expect(frame.setBowl2Score()).toEqual(5);
+  });
+});
+
+describe('FinalFrame', function() {
+
+  var strike;
+  var spare;
+  var frame;
+
+  beforeEach(function() {
+    frame = new Frame(3, 5);
+    strike = new Frame(10,10);
+    spare = new Frame(6, 4, 2);
+  });
+
+  it('gets a bowling score', function() {
+    expect(strike.setBowl1Score()).toEqual('X');
+    expect(strike.setBowl3Score()).toEqual('X');
+    expect(spare.setBowl1Score()).toEqual(6);
+    expect(spare.setBowl2Score()).toEqual('/');
+    expect(spare.setBowl3Score()).toEqual(2);
+    expect(frame.setBowl1Score()).toEqual(3);
+    expect(frame.setBowl2Score()).toEqual(5);
   });
 });

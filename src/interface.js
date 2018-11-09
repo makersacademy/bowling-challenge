26ac5fa36afc799de678afd7cb0700d4b553d249
hurc87 <zoe1987@btinternet.com>
2018-11-09 09:44:04 +0000

    removed loop for now and added if statement to check if score is under 11, messy , will need clearing once added bonus scores

--- src/interface.js
+++ src/interface.js
@@ -76,11 +76,117 @@ $(document).ready(function(){
     alert('Well done you scored '+scorecard.total);
   });
 
+  // function updateTotal(){
+  //   $('#runningTotal').text(scorecard.total);
+  //   for (var i = 1; i <= 20; i ++){
+  //     $('#move' + i).text(scorecard._scoreArray[i - 1]);
+  //   };
+  //   $('#frame1').text(scorecard.frameOne());
+  //   $('#frame2').text(scorecard.frameTwo());
+  //   $('#frame3').text(scorecard.frameThree());
+  //   $('#frame4').text(scorecard.frameFour());
+  //   $('#frame5').text(scorecard.frameFive());
+  //   $('#frame6').text(scorecard.frameSix());
+  //   $('#frame7').text(scorecard.frameSeven());
+  //   $('#frame8').text(scorecard.frameEight());
+  //   $('#frame9').text(scorecard.frameNine());
+  //   $('#frame10').text(scorecard.frameTen());
+  // };
+
   function updateTotal(){
     $('#runningTotal').text(scorecard.total);
-    for (var i = 1; i <= 20; i ++){
-      $('#move' + i).text(scorecard._scoreArray[i - 1]);
-    };
+
+    $('#move1').text(scorecard._scoreArray[0]);
+    $('#move2').text(scorecard._scoreArray[1]);
+    if(scorecard._scoreArray[0]+scorecard._scoreArray[1]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move2').text("");
+      $('#move2').text(scorecard._scoreArray[1]);
+    }
+
+    $('#move3').text(scorecard._scoreArray[2]);
+    $('#move4').text(scorecard._scoreArray[3]);
+    if(scorecard._scoreArray[2]+scorecard._scoreArray[3]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move4').text("");
+      $('#move4').text(scorecard._scoreArray[3]);
+    }
+
+    $('#move5').text(scorecard._scoreArray[4]);
+    $('#move6').text(scorecard._scoreArray[5]);
+    if(scorecard._scoreArray[4]+scorecard._scoreArray[5]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move6').text("");
+      $('#move6').text(scorecard._scoreArray[5]);
+    }
+
+    $('#move7').text(scorecard._scoreArray[6]);
+    $('#move8').text(scorecard._scoreArray[7]);
+    if(scorecard._scoreArray[6]+scorecard._scoreArray[7]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move8').text("");
+      $('#move8').text(scorecard._scoreArray[7]);
+    }
+
+    $('#move9').text(scorecard._scoreArray[8]);
+    $('#move10').text(scorecard._scoreArray[9]);
+    if(scorecard._scoreArray[8]+scorecard._scoreArray[9]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move10').text("");
+      $('#move10').text(scorecard._scoreArray[9]);
+    }
+
+    $('#move11').text(scorecard._scoreArray[10]);
+    $('#move12').text(scorecard._scoreArray[11]);
+    if(scorecard._scoreArray[10]+scorecard._scoreArray[11]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move12').text("");
+      $('#move12').text(scorecard._scoreArray[11]);
+    }
+
+    $('#move13').text(scorecard._scoreArray[12]);
+    $('#move14').text(scorecard._scoreArray[13]);
+    if(scorecard._scoreArray[12]+scorecard._scoreArray[13]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move14').text("");
+      $('#move14').text(scorecard._scoreArray[13]);
+    }
+
+    $('#move15').text(scorecard._scoreArray[14]);
+    $('#move16').text(scorecard._scoreArray[15]);
+    if(scorecard._scoreArray[14]+scorecard._scoreArray[15]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move16').text("");
+      $('#move16').text(scorecard._scoreArray[15]);
+    }
+
+    $('#move17').text(scorecard._scoreArray[16]);
+    $('#move18').text(scorecard._scoreArray[17]);
+    if(scorecard._scoreArray[16]+scorecard._scoreArray[17]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move18').text("");
+      $('#move18').text(scorecard._scoreArray[17]);
+    }
+
+    $('#move19').text(scorecard._scoreArray[18]);
+    $('#move20').text(scorecard._scoreArray[19]);
+    if(scorecard._scoreArray[18]+scorecard._scoreArray[19]>=11){
+      alert('Sorry try again, max pins in one frame is 10');
+      scorecard._scoreArray.pop();
+      $('#move20').text("");
+      $('#move20').text(scorecard._scoreArray[19]);
+    }
+
+
     $('#frame1').text(scorecard.frameOne());
     $('#frame2').text(scorecard.frameTwo());
     $('#frame3').text(scorecard.frameThree());

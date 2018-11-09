6940923f7b9bcf940824b0139328697ae58586bf
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-09 09:13:36 +0000

    Added #isAStrike

--- spec/scoreLogicSpec.js
+++ spec/scoreLogicSpec.js
@@ -3,16 +3,25 @@ describe("ScoreLogic", function() {
   var scoreLogic = new ScoreLogic();
 
   describe("#isNoMoreThanTen", function () {
-    it('It returns false if the sum of the two numbers is greater than 10', function() {
+    it('returns false if the sum of the two numbers is greater than 10', function() {
       expect(scoreLogic.isNoMoreThanTen(4,7)).toEqual(false);
     });
-    it('It returns true if the sum of the two numbers is less than 10', function() {
+    it('returns true if the sum of the two numbers is less than 10', function() {
       expect(scoreLogic.isNoMoreThanTen(1,7)).toEqual(true);
     });
-    it('It returns true if the sum of the two numbers is equal to 10', function() {
+    it('returns true if the sum of the two numbers is equal to 10', function() {
       expect(scoreLogic.isNoMoreThanTen(10,0)).toEqual(true);
     });
   });
 
+  describe("#isAStrike", function() {
+    it('returns true if the user scores a strike', function() {
+      expect(scoreLogic.isAStrike(10,0)).toEqual(true);
+    });
+    it('returns false if the user does not scores a strike', function() {
+      expect(scoreLogic.isAStrike(5,2)).toEqual(false);
+    });
+  });
+
 })
-  // expect(function() {scoreLogic.isNoMoreThanTen(4, 7);}).toThrow("Incorrect score entered.")
+// expect(function() {scoreLogic.isNoMoreThanTen(4, 7);}).toThrow("Incorrect score entered.")
--- src/scoreLogic.js
+++ src/scoreLogic.js
@@ -4,3 +4,7 @@ function ScoreLogic() {
 ScoreLogic.prototype.isNoMoreThanTen = function(a, b) {
   return ( a + b <= 10);
 }
+
+ScoreLogic.prototype.isAStrike = function(a) {
+  return (a === 10);
+}

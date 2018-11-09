67755eb62f2c9447b40c6f20bb51b7a1b5ddad1d
hurc87 <zoe1987@btinternet.com>
2018-11-09 16:12:20 +0000

    added loop back in until frame 10

--- src/bowling.js
+++ src/bowling.js
@@ -1,80 +1,65 @@
 'use strict';
 
 function Scorecard(){
-  this.total = 0;
   this._scoreArray = [];
-  this.resultOne = 0;
-  this.resultTwo = 0;
-  this.resultThree = 0;
-  this.resultFour = 0;
-  this.resultFive = 0;
-  this.resultSix = 0;
-  this.resultSeven = 0;
-  this.resultEight = 0;
-  this.resultNine = 0;
-  this.resultTine = 0;
   this._strikeArray = [];
 }
+
+Scorecard.prototype.endGame = function () {
+  if(this.resultTen === 0){
+    alert('Gutter Game!');
+  } else if(this._strikeArray.length === 12) {
+    alert('Well done you rolled the perfect game! Your total is 300!!!');
+  } else {
+    alert('Well done you scored '+this.resultTen);
+  };
+};
 
 Scorecard.prototype.seeArray = function (){
   return this._scoreArray;
 };
 
 Scorecard.prototype.addZero = function () {
-  this.total +=0;
   this._scoreArray.push(0);
 };
 
 Scorecard.prototype.addOne = function () {
-  this.total +=1;
   this._scoreArray.push(1);
 };
 
 Scorecard.prototype.addTwo = function () {
-  this.total +=2;
   this._scoreArray.push(2);
 };
 
 Scorecard.prototype.addThree = function () {
-  this.total +=3;
   this._scoreArray.push(3);
 };
 
 Scorecard.prototype.addFour = function () {
-  this.total +=4;
   this._scoreArray.push(4);
 };
 
 Scorecard.prototype.addFive = function () {
-  this.total +=5;
   this._scoreArray.push(5);
 };
 
 Scorecard.prototype.addSix = function () {
-  this.total +=6;
   this._scoreArray.push(6);
 };
 
 Scorecard.prototype.addSeven = function () {
-  this.total +=7;
   this._scoreArray.push(7);
 };
 
 Scorecard.prototype.addEight = function () {
-  this.total +=8;
   this._scoreArray.push(8);
 };
 
 Scorecard.prototype.addNine = function () {
-  this.total +=9;
   this._scoreArray.push(9);
 };
 
-
-
-
 Scorecard.prototype.addTen = function () {
-  this.total +=10;
   this._scoreArray.push(10);
   this._strikeArray.push('strike');
 };
--- src/interface.js
+++ src/interface.js
@@ -59,159 +59,52 @@ $(document).ready(function(){
   });
 
   $('#finish').click(function(){
-    if(scorecard.total === 0){
-      alert('Gutter Game!');
-    } else if(scorecard._strikeArray.length === 12) {
-      alert('Well done you rolled the perfect game! Your total is 300!!!');
-    } else {
-      alert('Well done you scored '+scorecard.resultTen);
-    };
+    scorecard.endGame();
   });
 
-  // function updateTotal(){
-  //   $('#runningTotal').text(scorecard.total);
-  //   for (var i = 1; i <= 20; i ++){
-  //     $('#move' + i).text(scorecard._scoreArray[i - 1]);
-  //   };
-  //   $('#frame1').text(scorecard.frameOne());
-  //   $('#frame2').text(scorecard.frameTwo());
-  //   $('#frame3').text(scorecard.frameThree());
-  //   $('#frame4').text(scorecard.frameFour());
-  //   $('#frame5').text(scorecard.frameFive());
-  //   $('#frame6').text(scorecard.frameSix());
-  //   $('#frame7').text(scorecard.frameSeven());
-  //   $('#frame8').text(scorecard.frameEight());
-  //   $('#frame9').text(scorecard.frameNine());
-  //   $('#frame10').text(scorecard.frameTen());
-  // };
 
   function updateTotal(){
-    $('#runningTotal').text(scorecard.total);
-    console.log('total without bonus '+ scorecard.total);
-
-    $('#move1').text(scorecard._scoreArray[0]);
-    $('#move2').text(scorecard._scoreArray[1]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[0]+scorecard._scoreArray[1]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move2').text("");
-      $('#move2').text(scorecard._scoreArray[1]);
-    }
-
-    $('#move3').text(scorecard._scoreArray[2]);
-    $('#move4').text(scorecard._scoreArray[3]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[2]+scorecard._scoreArray[3]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move4').text("");
-      $('#move4').text(scorecard._scoreArray[3]);
-    }
-
-    $('#move5').text(scorecard._scoreArray[4]);
-    $('#move6').text(scorecard._scoreArray[5]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[4]+scorecard._scoreArray[5]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move6').text("");
-      $('#move6').text(scorecard._scoreArray[5]);
-    }
-
-    $('#move7').text(scorecard._scoreArray[6]);
-    $('#move8').text(scorecard._scoreArray[7]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[6]+scorecard._scoreArray[7]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move8').text("");
-      $('#move8').text(scorecard._scoreArray[7]);
-    }
-
-    $('#move9').text(scorecard._scoreArray[8]);
-    $('#move10').text(scorecard._scoreArray[9]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[8]+scorecard._scoreArray[9]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move10').text("");
-      $('#move10').text(scorecard._scoreArray[9]);
-    }
-
-    $('#move11').text(scorecard._scoreArray[10]);
-    $('#move12').text(scorecard._scoreArray[11]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[10]+scorecard._scoreArray[11]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move12').text("");
-      $('#move12').text(scorecard._scoreArray[11]);
-    }
-
-    $('#move13').text(scorecard._scoreArray[12]);
-    $('#move14').text(scorecard._scoreArray[13]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[12]+scorecard._scoreArray[13]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move14').text("");
-      $('#move14').text(scorecard._scoreArray[13]);
-    }
-
-    $('#move15').text(scorecard._scoreArray[14]);
-    $('#move16').text(scorecard._scoreArray[15]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[14]+scorecard._scoreArray[15]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move16').text("");
-      $('#move16').text(scorecard._scoreArray[15]);
-    }
-
-    $('#move17').text(scorecard._scoreArray[16]);
-    $('#move18').text(scorecard._scoreArray[17]);
-    // Validates the roll cannot be more than 10
-    if(scorecard._scoreArray[16]+scorecard._scoreArray[17]>=11){
-      alert('Sorry try again, max pins in one frame is 10');
-      scorecard._scoreArray.pop();
-      $('#move18').text("");
-      $('#move18').text(scorecard._scoreArray[17]);
-    }
-
-    $('#move19').text(scorecard._scoreArray[18]);
-    if (scorecard._scoreArray[18]===10){
-      $('#move20').text(scorecard._scoreArray[19]);
-      $('#move21').text(scorecard._scoreArray[20]);
-      $('#totalAmount').text(scorecard.resultTen);
-      if(scorecard._scoreArray[19]===10){
-        $('#move21').text(scorecard._scoreArray[20]);
-      } else if (scorecard._scoreArray[19]+scorecard._scoreArray[20]>=11){
+    for (var i = 1; i <= 18; i+=2){
+      $('#move' + i).text(scorecard._scoreArray[i - 1]);
+      $('#move' + (i+1)).text(scorecard._scoreArray[i]);
+      if(scorecard._scoreArray[i-1]+scorecard._scoreArray[i]>=11){
         alert('Sorry try again, max pins in one frame is 10');
         scorecard._scoreArray.pop();
-        $('#move21').text("");
-        $('#move21').text(scorecard._scoreArray[20]);
-        $('#totalAmount').text(scorecard.resultTen);
+        $('#move' + (i+1)).text("");
+        $('#move' + (i+1)).text(scorecard._scoreArray[i]);
       }
-    } else {
-      $('#move20').text(scorecard._scoreArray[19]);
-      // Validates the roll cannot be more than 10
-      if(scorecard._scoreArray[19]===10){
-        $('#move21').text(scorecard._scoreArray[20]);
-      } else if (scorecard._scoreArray[18]+scorecard._scoreArray[19]===10) {
-        $('#move21').text(scorecard._scoreArray[20]);
-        $('#totalAmount').text(scorecard.resultTen);
-      }
-      if (scorecard._scoreArray[18]+scorecard._scoreArray[19]>=11){
-        alert('Sorry try again, max pins in one frame is 10');
-        scorecard._scoreArray.pop();
-        $('#move20').text("");
-        $('#move20').text(scorecard._scoreArray[19]);
-      }
-      $('#totalAmount').text(scorecard.resultTen);
-    }
-
-
+        $('#move19').text(scorecard._scoreArray[18]);
+        if (scorecard._scoreArray[18]===10){
+          $('#move20').text(scorecard._scoreArray[19]);
+          $('#move21').text(scorecard._scoreArray[20]);
+          $('#totalAmount').text(scorecard.resultTen);
+          if(scorecard._scoreArray[19]===10){
+            $('#move21').text(scorecard._scoreArray[20]);
+          } else if (scorecard._scoreArray[19]+scorecard._scoreArray[20]>=11){
+            alert('Sorry try again, max pins in one frame is 10');
+            scorecard._scoreArray.pop();
+            $('#move21').text("");
+            $('#move21').text(scorecard._scoreArray[20]);
+            $('#totalAmount').text(scorecard.resultTen);
+          }
+        } else {
+          $('#move20').text(scorecard._scoreArray[19]);
+          // Validates the roll cannot be more than 10
+          if(scorecard._scoreArray[19]===10){
+            $('#move21').text(scorecard._scoreArray[20]);
+          } else if (scorecard._scoreArray[18]+scorecard._scoreArray[19]===10) {
+            $('#move21').text(scorecard._scoreArray[20]);
+            $('#totalAmount').text(scorecard.resultTen);
+          }
+          if (scorecard._scoreArray[18]+scorecard._scoreArray[19]>=11){
+            alert('Sorry try again, max pins in one frame is 10');
+            scorecard._scoreArray.pop();
+            $('#move20').text("");
+            $('#move20').text(scorecard._scoreArray[19]);
+          }
+          $('#totalAmount').text(scorecard.resultTen);
+        }
+    };
     $('#frame1').text(scorecard.frameOne());
     $('#frame2').text(scorecard.frameTwo());
     $('#frame3').text(scorecard.frameThree());
@@ -223,5 +116,142 @@ $(document).ready(function(){
     $('#frame9').text(scorecard.frameNine());
     $('#frame10').text(scorecard.frameTen());
   };
+
+  // function updateTotal(){
+  //
+  //   $('#move1').text(scorecard._scoreArray[0]);
+  //   $('#move2').text(scorecard._scoreArray[1]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[0]+scorecard._scoreArray[1]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move2').text("");
+  //     $('#move2').text(scorecard._scoreArray[1]);
+  //   }
+  //
+  //   $('#move3').text(scorecard._scoreArray[2]);
+  //   $('#move4').text(scorecard._scoreArray[3]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[2]+scorecard._scoreArray[3]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move4').text("");
+  //     $('#move4').text(scorecard._scoreArray[3]);
+  //   }
+  //
+  //   $('#move5').text(scorecard._scoreArray[4]);
+  //   $('#move6').text(scorecard._scoreArray[5]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[4]+scorecard._scoreArray[5]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move6').text("");
+  //     $('#move6').text(scorecard._scoreArray[5]);
+  //   }
+  //
+  //   $('#move7').text(scorecard._scoreArray[6]);
+  //   $('#move8').text(scorecard._scoreArray[7]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[6]+scorecard._scoreArray[7]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move8').text("");
+  //     $('#move8').text(scorecard._scoreArray[7]);
+  //   }
+  //
+  //   $('#move9').text(scorecard._scoreArray[8]);
+  //   $('#move10').text(scorecard._scoreArray[9]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[8]+scorecard._scoreArray[9]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move10').text("");
+  //     $('#move10').text(scorecard._scoreArray[9]);
+  //   }
+  //
+  //   $('#move11').text(scorecard._scoreArray[10]);
+  //   $('#move12').text(scorecard._scoreArray[11]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[10]+scorecard._scoreArray[11]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move12').text("");
+  //     $('#move12').text(scorecard._scoreArray[11]);
+  //   }
+  //
+  //   $('#move13').text(scorecard._scoreArray[12]);
+  //   $('#move14').text(scorecard._scoreArray[13]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[12]+scorecard._scoreArray[13]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move14').text("");
+  //     $('#move14').text(scorecard._scoreArray[13]);
+  //   }
+  //
+  //   $('#move15').text(scorecard._scoreArray[14]);
+  //   $('#move16').text(scorecard._scoreArray[15]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[14]+scorecard._scoreArray[15]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move16').text("");
+  //     $('#move16').text(scorecard._scoreArray[15]);
+  //   }
+  //
+  //   $('#move17').text(scorecard._scoreArray[16]);
+  //   $('#move18').text(scorecard._scoreArray[17]);
+  //   // Validates the roll cannot be more than 10
+  //   if(scorecard._scoreArray[16]+scorecard._scoreArray[17]>=11){
+  //     alert('Sorry try again, max pins in one frame is 10');
+  //     scorecard._scoreArray.pop();
+  //     $('#move18').text("");
+  //     $('#move18').text(scorecard._scoreArray[17]);
+  //   }
+  //
+  //   $('#move19').text(scorecard._scoreArray[18]);
+  //   if (scorecard._scoreArray[18]===10){
+  //     $('#move20').text(scorecard._scoreArray[19]);
+  //     $('#move21').text(scorecard._scoreArray[20]);
+  //     $('#totalAmount').text(scorecard.resultTen);
+  //     if(scorecard._scoreArray[19]===10){
+  //       $('#move21').text(scorecard._scoreArray[20]);
+  //     } else if (scorecard._scoreArray[19]+scorecard._scoreArray[20]>=11){
+  //       alert('Sorry try again, max pins in one frame is 10');
+  //       scorecard._scoreArray.pop();
+  //       $('#move21').text("");
+  //       $('#move21').text(scorecard._scoreArray[20]);
+  //       $('#totalAmount').text(scorecard.resultTen);
+  //     }
+  //   } else {
+  //     $('#move20').text(scorecard._scoreArray[19]);
+  //     // Validates the roll cannot be more than 10
+  //     if(scorecard._scoreArray[19]===10){
+  //       $('#move21').text(scorecard._scoreArray[20]);
+  //     } else if (scorecard._scoreArray[18]+scorecard._scoreArray[19]===10) {
+  //       $('#move21').text(scorecard._scoreArray[20]);
+  //       $('#totalAmount').text(scorecard.resultTen);
+  //     }
+  //     if (scorecard._scoreArray[18]+scorecard._scoreArray[19]>=11){
+  //       alert('Sorry try again, max pins in one frame is 10');
+  //       scorecard._scoreArray.pop();
+  //       $('#move20').text("");
+  //       $('#move20').text(scorecard._scoreArray[19]);
+  //     }
+  //     $('#totalAmount').text(scorecard.resultTen);
+  //   }
+  //
+  //
+  //   $('#frame1').text(scorecard.frameOne());
+  //   $('#frame2').text(scorecard.frameTwo());
+  //   $('#frame3').text(scorecard.frameThree());
+  //   $('#frame4').text(scorecard.frameFour());
+  //   $('#frame5').text(scorecard.frameFive());
+  //   $('#frame6').text(scorecard.frameSix());
+  //   $('#frame7').text(scorecard.frameSeven());
+  //   $('#frame8').text(scorecard.frameEight());
+  //   $('#frame9').text(scorecard.frameNine());
+  //   $('#frame10').text(scorecard.frameTen());
+  // };
 
 });

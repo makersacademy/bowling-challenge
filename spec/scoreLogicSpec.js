80e844ad615217bf16a965c3b20e6c1f9414b692
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-09 09:44:45 +0000

    started #frame

--- spec/scoreLogicSpec.js
+++ spec/scoreLogicSpec.js
@@ -4,33 +4,38 @@ describe("ScoreLogic", function() {
 
   describe("#isNoMoreThanTen", function () {
     it('returns false if the sum of the two numbers is greater than 10', function() {
-      expect(scoreLogic.isNoMoreThanTen(4,7)).toEqual(false);
+      expect(scoreLogic._isNoMoreThanTen(4,7)).toEqual(false);
     });
     it('returns true if the sum of the two numbers is less than 10', function() {
-      expect(scoreLogic.isNoMoreThanTen(1,7)).toEqual(true);
+      expect(scoreLogic._isNoMoreThanTen(1,7)).toEqual(true);
     });
     it('returns true if the sum of the two numbers is equal to 10', function() {
-      expect(scoreLogic.isNoMoreThanTen(10,0)).toEqual(true);
+      expect(scoreLogic._isNoMoreThanTen(10,0)).toEqual(true);
     });
   });
 
   describe("#isAStrike", function() {
     it('returns true if the player scores a strike', function() {
-      expect(scoreLogic.isAStrike(10,0)).toEqual(true);
+      expect(scoreLogic._isAStrike(10,0)).toEqual(true);
     });
     it('returns false if the player does not scores a strike', function() {
-      expect(scoreLogic.isAStrike(5,2)).toEqual(false);
+      expect(scoreLogic._isAStrike(5,2)).toEqual(false);
     });
   });
 
   describe("#isASpare", function() {
     it('returns true if the player scores a spare', function() {
-      expect(scoreLogic.isASpare(5,5)).toEqual(true);
+      expect(scoreLogic._isASpare(5,5)).toEqual(true);
     })
     it('returns false if the player does not scores a spare', function() {
-      expect(scoreLogic.isASpare(5,1)).toEqual(false);
+      expect(scoreLogic._isASpare(5,1)).toEqual(false);
     })
   });
 
+  describe("#frame", function() {
+    it('throws an error if the player enters an incorrect score', function() {
+      expect(function() {scoreLogic.frame(5,7);}).toThrow("Incorrect score entered.")
+    });
+  });
+
 })
-// expect(function() {scoreLogic.isNoMoreThanTen(4, 7);}).toThrow("Incorrect score entered.")
--- src/scoreLogic.js
+++ src/scoreLogic.js
@@ -1,14 +1,37 @@
 function ScoreLogic() {
 };
 
-ScoreLogic.prototype.isNoMoreThanTen = function(a, b) {
+ScoreLogic.prototype._isNoMoreThanTen = function(a, b) {
   return ( a + b <= 10);
 }
 
-ScoreLogic.prototype.isAStrike = function(a, b) {
+ScoreLogic.prototype._isNoLessThanZero = function(a, b) {
+  return ( a + b >= 0);
+}
+
+ScoreLogic.prototype._isAStrike = function(a, b) {
   return (a === 10);
 }
 
-ScoreLogic.prototype.isASpare = function(a, b) {
+ScoreLogic.prototype._isASpare = function(a, b) {
   return ( a + b === 10);
 }
+
+ScoreLogic.prototype.frame = function(a, b) {
+  if (!this._isNoMoreThanTen(a,b)) {
+    throw "Incorrect score entered.";
+  }
+}
+
+
+// FizzBuzz.prototype.fizzBuzz = function(number) {
+//     if (this.isDivisibleByFifteen(number)) {
+//       return 'fizzbuzz';
+//     } else if (this.isDivisibleByFive(number)) {
+//       return 'buzz';
+//     } else if (this.isDivisibleByThree(number)) {
+//       return 'fizz';
+//     } else {
+//       return number;
+//     }
+// };

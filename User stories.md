eb9f296e5a6069a48b49c6a46f11abc53273c345
David Best <david_best@ntlworld.com>
2018-11-09 10:09:30 +0000

    created passing tests and methods, roll and score

---
+++ User stories.md
@@ -0,0 +1,13 @@
+User stories
+
+As a player
+So I can keep my score at bowling
+I would like to count and sum the scores entered.
+
+Each frame will consist of 2 rolls, except frame 10 which could be up to 3 rolls.
+
+If rolls 1 and 2 equal 10, this is a spare, and the score from the next roll must be added to the current frame.
+
+If I get a strike the scores from the next 2 rolls must be added to the current frame.
+
+No more scores can be entered after frame 10 has completed.
--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Tests frame', function() {
+describe('Tests scoreCard', function() {
 
   var scorecard;
 
@@ -8,68 +8,20 @@ describe('Tests frame', function() {
     scorecard = new Scorecard();
   });
 
-  it('expects the score at the start to be zero', function() {
-    expect(scorecard.gameScore).toEqual(0);
+  it('expects scorecard to exist', function() {
+    expect(scorecard instanceof Scorecard).toBeTruthy;
   });
 
-  it('the user can enter a score', function() {
-    scorecard.enterScore(5);
-    expect(scorecard.frameScore).toEqual(5);
+  it('expects score to return 0', function() {
+    var n = 20;
+    var pins = 0;
+    for (var i = 0; i < n; i++) scorecard.roll(pins);
+    expect(scorecard.score()).toEqual(0);
   });
 
-  it('roll count changes', function() {
-    scorecard.changeRollCount();
-    expect(scorecard.rollCount).toEqual(2);
-    scorecard.changeRollCount();
-    expect(scorecard.rollCount).toEqual(1);
-  });
-
-  it('increments frame if rollScore equals 10', function() {
-    scorecard.enterScore(10);
-    scorecard.incrementFrame();
-    expect(scorecard.frame).toEqual(2);
-  });
-
-  it('does not increment frame if rollScore less than 10', function() {
-    scorecard.enterScore(8);
-    scorecard.incrementFrame();
-    expect(scorecard.frame).toEqual(1);
-  });
-
-  it('increments frame if rollCount equals 2', function() {
-    scorecard.changeRollCount();
-    scorecard.incrementFrame();
-    expect(scorecard.frame).toEqual(2);
-  });
-
-  it('cannot increment frame past 10', function() {
-    scorecard.changeRollCount();
-    for (var i = 0; i < 11; i++) scorecard.incrementFrame();
-    expect(scorecard.frame).toEqual(10);
-  });
-
-  it('does not increment frame if rollCount equals 1', function() {
-    scorecard.incrementFrame();
-    expect(scorecard.frame).toEqual(1);
-  });
-
-  it('adds frameScore to gameScore if rollCount equals 2', function() {
-    scorecard.enterScore(4);
-    scorecard.addToGameScore(); // rollCount 1, if failed would add 4 to gameScore
-    scorecard.changeRollCount();
-    scorecard.enterScore(4);
-    scorecard.addToGameScore(); // adds 8 to gameScore
-    expect(scorecard.gameScore).toEqual(8);
-  });
-
-  it('resets frameScore to zero after rollCount set to 1', function() {
-    scorecard.enterScore(4);
-    scorecard.changeRollCount();
-    expect(scorecard.frameScore).toEqual(4);
-    scorecard.enterScore(4);
-    scorecard.changeRollCount();
-    scorecard.resetFrameScore();
-    expect(scorecard.frameScore).toEqual(0);
+  it('expects score to return 20', function() {
+    for (var i = 0; i < 20; i++) scorecard.roll(1);
+    expect(scorecard.score()).toEqual(20);
   });
 
 });

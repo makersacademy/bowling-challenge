0d9751fbcfa3bbd49e737a1b43efb2424b46122b
David Best <david_best@ntlworld.com>
2018-11-09 15:19:51 +0000

    adds spare to score, passes tests

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,20 +8,33 @@ describe('Tests scoreCard', function() {
     scorecard = new Scorecard();
   });
 
+  function rollMany(n, pins) {
+    for (var i = 0; i < n; i++) scorecard.roll(pins);
+  }
+
   it('expects scorecard to exist', function() {
     expect(scorecard instanceof Scorecard).toBeTruthy;
   });
 
-  it('expects score to return 0', function() {
-    var n = 20;
-    var pins = 0;
-    for (var i = 0; i < n; i++) scorecard.roll(pins);
+  it('expects score to be 0 after 20 rolls of 0', function() {
+    rollMany(20, 0);
     expect(scorecard.score()).toEqual(0);
   });
 
-  it('expects score to return 20', function() {
-    for (var i = 0; i < 20; i++) scorecard.roll(1);
+  it('expects score to be 20 after 20 rolls of 1', function() {
+    rollMany(20, 1);
     expect(scorecard.score()).toEqual(20);
   });
+
+  it('expects spare to be added to score', function() {
+    scorecard.roll(4);
+    scorecard.roll(6);
+    scorecard.roll(4);
+    scorecard.roll(6);
+    scorecard.roll(3);
+    rollMany(15, 0);
+    expect(scorecard.score()).toEqual(30);
+  });
+
 
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,13 +1,34 @@
 'use strict';
 
 function Scorecard(){
-  this.gameScore = 0
+  this.currentRoll = 0
+  this.rolls = new Array(21)
 };
 
 Scorecard.prototype.roll = function (pins) {
-  this.gameScore += pins;
+  this.rolls[this.currentRoll++] = pins;
 };
 
+// Scorecard.prototype.score = function () {
+//   var gameScore = 0;
+//   for (i = 0, i < 3; i++) {
+//     console.log("Hello");
+//   //   this.gameScore += this.rolls[i];
+//   }
+//   return gameScore;
+// };
+
 Scorecard.prototype.score = function () {
-  return this.gameScore;
+  var gameScore = 0;
+  var rollIndex = 0;
+  for (var frame = 0; frame < 10; frame++) {
+    if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10) {
+      gameScore += 10 + this.rolls[rollIndex + 2];
+      rollIndex += 2;
+    } else {
+      gameScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+      rollIndex += 2;
+    }
+  }
+  return gameScore;
 };

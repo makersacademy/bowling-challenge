246772033738c423465efd13d7e6554503e68e87
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-11-09 15:56:28 +0000

    Displays rolls in table

--- src/interface.js
+++ src/interface.js
@@ -6,15 +6,23 @@ $(document).ready(function() {
      roll = ($(this).text());
      game.recordRoll(parseInt(roll));
      updateScore();
-     showPreviousRolls();
+     displayInFrame(game._currentFrame);
   });
 
   function updateScore() {
     $('#score').text(game._totalScore);
   };
 
-  function showPreviousRolls() {
-    $('#previous-rolls').text(game._allRolls.join(' '));
+  function displayInFrame(frame) {
+    if (game._rollsThisFrame.length == 0) {frame-- };
+    if ($('#f' + frame + 'r1').text() == '') {
+      roll = 1;
+    } else if (frame == 10 && $('#f' + frame + 'r2').text() != '') {
+      roll = 3;
+    }else {
+      roll = 2;
+    };
+    $('#f' + frame + 'r' + roll).text(game._allRolls[game._allRolls.length - 1]);
   };
 
 });

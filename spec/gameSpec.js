70602715ccc67c30da68b1990969bb6878f7528c
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-11-10 10:01:01 +0000

    first test written

---
+++ spec/gameSpec.js
@@ -0,0 +1,12 @@
+describe("Bowling", function() {
+  var game;
+
+  beforeEach(function () {
+    game = new Game()
+  })
+
+  it('can track rolls', function () {
+    game.roll(1)
+    expect(game.rolls).toEqual(1);
+  })
+})

2592e1aec137a1b3d59d20cbff949e9759e44b6e
jebax <joel.eliot.baxter@gmail.com>
2018-11-10 12:28:37 +0000

    added different isFinished for final frame

--- spec/final-frame-spec.js
+++ spec/final-frame-spec.js
@@ -9,8 +9,18 @@ describe ('Final Frame', function () {
     it('should inherit from a regular frame', function () {
       spyOn(Frame.prototype, 'getBowls')
       finalFrame.getBowls()
-      console.log(finalFrame.__proto__)
       expect(Frame.prototype.getBowls).toHaveBeenCalled()
+    })
+
+    it('is finished when two normal bowls are played', function () {
+      finalFrame.addBowl(3)
+      finalFrame.addBowl(4)
+      expect(finalFrame.isFinished()).toBe(true)
+    })
+
+    it('is not finished when a strike is played', function () {
+      finalFrame.addBowl(10)
+      expect(finalFrame.isFinished()).toBe(false)
     })
   })
 })
--- src/final-frame.js
+++ src/final-frame.js
@@ -2,4 +2,17 @@ function FinalFrame() {
   Frame.call(this)
 }
 
-FinalFrame.prototype = Frame.prototype
+FinalFrame.prototype = Object.create(Frame.prototype)
+FinalFrame.prototype.constructor = FinalFrame
+
+FinalFrame.prototype.isFinished = function () {
+  if (this.isStrike() && this._bowls.length > 2) {
+    return true
+  } else if (this.isSpare() && this._bowls.length > 2) {
+    return true
+  } else if (this._bowls.length > 1) {
+    return true
+  } else {
+    return false
+  }
+}

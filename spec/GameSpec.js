50e7639a7340705bc99547303ac82e9fd0058df2
mariusgg <marius.g.guei@gmail.com>
2018-11-11 21:03:39 +0000

    refactored and added bowl function in game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,29 +10,25 @@ describe('Game', function() {
     expect(game.frame).toEqual(1);
      expect(game.currentFrame).toEqual(1);
   });
-
-  it('starts the game at bowl 1', function() {
-    expect(game.currentBowl).toEqual(1);
-  });
-
-  it('has 10 pins for each frame', function() {
-    expect(game.bowlingPins).toEqual(10);
-  });
-
-  it('decreses the pins from number of bowlingPins', function() {
-    game.bowl(3);
-    expect(game.bowlingPins).toEqual(7);
+     it('starts a game at bowl 1', function() {
+      expect(game.currentBowl).toEqual(1);
     });
-
-  it('increase the currentBowl by 1', function() {
-    game.bowl(3);
-    expect(game.currentBowl).toEqual(2);
+     it('has 10 pins for each frame', function() {
+      expect(game.bowlingPins).toEqual(10);
     });
-
-  it('increases currentFrame by 1 after second bowl', function() {
-    game.bowl(3);
-    game.bowl(4);
-    expect(game.currentFrame).toEqual(2);
-   });
-
-});
+     describe('Bowl', function() {
+      it('decrese the bowlingPins by number of pins', function() {
+        game.bowl(3);
+        expect(game.bowlingPins).toEqual(7);
+      });
+       it('currentBowl increases by 1', function() {
+        game.bowl(3);
+        expect(game.currentBowl).toEqual(2);
+      });
+       it('increases currentFrame by 1 after second bowl', function() {
+        game.bowl(3);
+        game.bowl(4);
+        expect(game.currentFrame).toEqual(2);
+      });
+    });
+  });
--- src/game.js
+++ src/game.js
@@ -3,4 +3,15 @@ function Game() {
   this.currentFrame = 1
   this.currentBowl = 1
   this.bowlingPins = 10
+  // var score = new Score();
+};
+ Game.prototype.bowl = function(pins) {
+  this.currentBowl ++
+  this.bowlingPins -= pins
+  this.calculateFrame();
+};
+ Game.prototype.calculateFrame = function () {
+  if (this.currentBowl == 2) {
+    this.currentFrame ++
+  };
 };

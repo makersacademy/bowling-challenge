e800bc9dbcf2bc8592c33b653b81e7abcce54f96
chloeverity <chloeverity@hotmail.co.uk>
2018-11-11 02:16:55 +0000

    basic interface up, roll updating, score not

--- src/Game.js
+++ src/Game.js
@@ -1,47 +1,52 @@
 var Game = function() {
   this.rolls = [];
+  this.currentRoll = 0;
+  this.currentScore = 0
 };
 
 Game.prototype.roll = function (pins) {
-  this.rolls.push(pins)
+  this.rolls.push(pins);
+  this.currentRoll++
 };
 
 Game.prototype.score = function () {
   var result = 0;
-  var rollIndex = 0;
-  var game = this;
+  var frameIndex = 0;
+  var self = this;
 
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
     if (isStrike()) {
       result += getStrikeScore();
-      rollIndex++;
+      frameIndex++;
     } else if (isSpare()) {
       result += getSpareScore();
-      rollIndex += 2;
+      frameIndex += 2;
     } else {
       result += getNormalScore();
-      rollIndex += 2;
+      frameIndex += 2;
     }
   }
   return result;
 
   function isStrike() {
-    return game.rolls[rollIndex] == 10
+    return self.rolls[frameIndex] == 10
   }
 
   function isSpare() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] == 10;
+    return self.rolls[frameIndex] + self.rolls[frameIndex + 1] == 10;
   }
 
   function getStrikeScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+    return self.rolls[frameIndex] + self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2];
   }
 
   function getSpareScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+    return self.rolls[frameIndex] + self.rolls[frameIndex + 1] + self.rolls[frameIndex + 2];
   }
 
   function getNormalScore() {
-    return game.rolls[rollIndex] + game.rolls[rollIndex + 1];
+    return self.rolls[frameIndex] + self.rolls[frameIndex + 1];
   }
+  self.currentScore = score;
+  return score;
 };
--- src/GameInterface.js
+++ src/GameInterface.js
@@ -1,70 +1,77 @@
-// $(document).ready(function () {
-//
-//   // $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=72e80e23fc9e40e2ece92a1e77019ac7&units=metric', function(data) {
-//   // $('#current-temperature').text(data.main.temp);
-//   // })
-//   $('#city-input-form').submit(function (event) {
-//     event.preventDefault();
-//     var city = $('#city-input').val();
-//     var unit = $('#unit').val();
-//   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=72e80e23fc9e40e2ece92a1e77019ac7&units=' + unit, function (data) {
-//     $('#current-temperature').text(data.main.temp)
-//   })
-// })
-// $('#unit').change(function () {
-//   var city = $('#city').val();
-//   var unit = $('#unit').val();
-// $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=72e80e23fc9e40e2ece92a1e77019ac7&units=' + unit, function (data) {
-//   $('#current-temperature').text(data.main.temp)
-// })
-// })
-//
-//   var thermostat = new Thermostat();
-//   updateTemperature();
-//   updatePowerSave();
-//   updatePowerUsage();
-//   updateColour();
-//
-//   $('#temperature-up').click(function() {
-//     thermostat.increaseTemp();
-//     updateTemperature();
-//     updatePowerUsage();
-//     updateColour();
-//   })
-//   $('#temperature-down').click(function() {
-//     thermostat.decreaseTemp();
-//     updateTemperature();
-//     updatePowerUsage();
-//     updateColour();
-//   })
-//   $('#temperature-reset').click(function() {
-//     thermostat.reset();
-//     updateTemperature();
-//     updatePowerUsage();
-//     updateColour();
-//   })
-//   $('#power-saving-on').click(function() {
-//     thermostat.powerSaveOn();
-//     updatePowerSave();
-//   })
-//   $('#power-saving-off').click(function() {
-//     thermostat.powerSaveOff();
-//     updatePowerSave();
-//   })
-//
-//   function updateTemperature() {
-//     $('#temperature').text(thermostat.temp())
-//   }
-//
-//   function updatePowerSave() {
-//     $('#power-saving-mode').text(thermostat.powerSave())
-//   }
-//
-//   function updatePowerUsage() {
-//     $('#power-usage').text(thermostat.energyUsage())
-//   }
-//
-//   function updateColour() {
-//     $('#temperature').attr('class', thermostat.energyUsage())
-//   }
-// });
+$(document).ready(function(){
+
+  var game = new Game();
+
+  function updateScore() {
+  $('#current-score').text(game.currentScore);
+  $('#current-roll').text(game.currentRoll);
+  }
+
+  updateScore();
+
+  $('#zero-pins').on('click', function(){
+    game.roll(0);
+    game.score();
+    updateScore();
+  })
+
+  $('#one-pin').on('click', function(){
+    game.roll(1);
+    game.score();
+    updateScore();
+  })
+
+  $('#two-pins').on('click', function(){
+    game.roll(2);
+    game.score();
+    updateScore();
+  })
+
+  $('#three-pins').on('click', function(){
+    game.roll(3);
+    game.score();
+    updateScore();
+  })
+
+  $('#four-pins').on('click', function(){
+    game.roll(4);
+    game.score();
+    updateScore();
+  })
+
+  $('#five-pins').on('click', function(){
+    game.roll(5);
+    game.score();
+    updateScore();
+  })
+
+  $('#six-pins').on('click', function(){
+    game.roll(6);
+    game.score();
+    updateScore();
+  })
+
+  $('#seven-pins').on('click', function(){
+    game.roll(7);
+    game.score();
+    updateScore();
+  })
+
+  $('#eight-pins').on('click', function(){
+    game.roll(8);
+    game.score();
+    updateScore();
+  })
+
+  $('#nine-pins').on('click', function(){
+    game.roll(9);
+    game.score();
+    updateScore();
+  })
+
+  $('#ten-pins').on('click', function(){
+    game.roll(10);
+    game.score();
+    updateScore();
+  })
+})

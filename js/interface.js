17b87368391963aa5e9be2e75191a45868ffe822
Clare Jolly <clarejollyuk@gmail.com>
2018-11-11 23:35:56 +0000

    small updates

--- js/interface.js
+++ js/interface.js
@@ -1,53 +1,52 @@
 $(document).ready(function() {
   var game = new Game();
 
-scorebuttons_div
-var i;
-$('#scorebuttons_div').append("<button class='scorebuttons' id='score_btn_0' value='0'>0</button>")
-for (i = 1; i < 11; i++) {
+  scorebuttons_div
+  var i;
+  $('#scorebuttons_div').append("<button class='scorebuttons' id='score_btn_0' value='0'>0</button>")
+  for (i = 1; i < 11; i++) {
 
-  $('#scorebuttons_div').append("<button class='scorebuttons' id='score_btn_"+i+"' value='"+i+"'>"+i+"</button>")
-  $('#heading-row').append('<th colspan="6" width="10%" id="score_head_'+i+'">'+i+'</th>');
-  $('#score-row').append('<td colspan="3"></td><td colspan="3" width="10%" id="score_'+i+'"></td>');
-  $('#marker-row').append('<td colspan="6" width="10%" id="marker_'+i+'"></td>');
+    $('#scorebuttons_div').append("<button class='scorebuttons' id='score_btn_" + i + "' value='" + i + "'>" + i + "</button>")
+    $('#heading-row').append('<th colspan="6" width="10%" id="score_head_' + i + '">' + i + '</th>');
+    $('#score-row').append('<td colspan="3"></td><td colspan="3" width="10%" id="score_' + i + '"></td>');
+    $('#marker-row').append('<td colspan="6" width="10%" id="marker_' + i + '"></td>');
 
-}
-$('#score_head_1').attr('class', 'frame_selected');
+  }
+  $('#score_head_1').attr('class', 'frame_selected');
 
   function showScore(pin) {
-    $('#score_'+game.currentframenumber+'').text(game.currentframe.formatRolls());
-    $('#marker_'+game.currentframenumber+'').text(game.currentframe.getPinsScore());
+    $('#score_' + game.currentframenumber + '').text(game.currentframe.formatRolls());
+    $('#marker_' + game.currentframenumber + '').text(game.currentframe.getPinsScore());
     $('#current-score').text(game.calculateGameTotalScore());
-    if(game.currentframe.isFrameOpen()){
-    disableButtons(game.currentframe.remainingPins())
+    if (game.currentframe.isFrameOpen()) {
+      disableButtons(game.currentframe.remainingPins())
     } else {
       enableButtons()
       resetFormatting()
     }
-    if(game.calculateGameTotalScore() === 300) {
+    if (game.calculateGameTotalScore() === 300) {
       disableButtons(-1)
-      // $('#score_btn_0').prop('disabled', true);
     }
   }
 
   function disableButtons(remainder) {
     var i;
-    for (i = remainder+1; i < 11; i++) {
-      $('#score_btn_'+i+'').prop('disabled', true);
+    for (i = remainder + 1; i < 11; i++) {
+      $('#score_btn_' + i + '').prop('disabled', true);
     }
   }
 
   function enableButtons() {
     var i;
     for (i = 0; i < 11; i++) {
-      $('#score_btn_'+i+'').prop('disabled', false);
+      $('#score_btn_' + i + '').prop('disabled', false);
     }
   }
 
   function resetFormatting() {
     var i;
     for (i = 1; i < 11; i++) {
-      $('#score_head_'+i+'').attr('class', '');
+      $('#score_head_' + i + '').attr('class', '');
     }
   }
 
@@ -56,12 +55,21 @@ $('#score_head_1').attr('class', 'frame_selected');
     game.enterRoll(val);
     $('#frame-number').text(game.currentframenumber);
     showScore()
-    if(game.currentframe.isFrameOpen() || game.currentframe.isFinalFrame()){ frametext = game.currentframenumber } else {frametext = game.currentframenumber+1}
+    if (game.currentframe.isFrameOpen() || game.currentframe.isFinalFrame()) {
+      frametext = game.currentframenumber
+    } else {
+      frametext = game.currentframenumber + 1
+    }
     $('#frame-number').text(frametext);
-    $('#score_head_'+frametext+'').attr('class', 'frame_selected');
-    if (game.currentframe.isFinalFrame() && !game.currentframe.isFrameOpen()
-  && game.currentframe.hasStrike()){
-      $('#enter_pins_message').text("How many did you get for your bonus?")
+    $('#score_head_' + frametext + '').attr('class', 'frame_selected');
+    if (game.currentframe.isFinalFrame() && !game.currentframe.isFrameOpen() &&
+      game.currentframe.hasStrike()) {
+      $('#enter_pins_message').text("How many did you get for your strike bonus?")
+    }
+
+    if (game.currentframe.isFinalFrame() && !game.currentframe.isFrameOpen() &&
+      game.currentframe.hasSpare()) {
+      $('#enter_pins_message').text("How many did you get for your spare bonus?")
     }
 
 

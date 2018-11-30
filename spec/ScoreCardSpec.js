f64c8f80b9edc83fe4a44a9f06f16fa1bccb1111
Will Wayman <will.jdw@gmail.com>
2018-11-30 17:21:35 +0000

    second test written - can keep score

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -3,7 +3,7 @@ describe("ScoreCard", function() {
 
   beforeEach(function() {
     scoreCard = new ScoreCard();
-  })
+  });
 
   it("rolls should update the rollNumber and frameNumber", function() {
     expect(scoreCard.rollNumber).toEqual(1);
@@ -14,5 +14,13 @@ describe("ScoreCard", function() {
     scoreCard.addScore(2);
     expect(scoreCard.rollNumber).toEqual(1);
     expect(scoreCard.frameNumber).toEqual(2);
-  })
-})
+  });
+
+  it("can record the final score", function() {
+    var i;
+    for (i = 0; i < 20; i++) {
+      scoreCard.addScore(2);
+    };
+    expect(scoreCard.sumOfPins()).toEqual(40);
+  });
+});

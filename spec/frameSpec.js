33d8411b291434bf225fcaf9a04ae5d323dd3e27
willmcb <williamjamesmcbrien@gmail.com>
2018-11-30 15:37:13 +0000

    initial code and tests for frame class complete

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -8,7 +8,72 @@ describe("Frame", function() {
 
   it("is an instance of Frame", function() {
     expect(frame).toEqual(jasmine.any(Frame));
+  });
 
+  it("has methods to set and get the first score", function() {
+    frame.setFirstScore(3);
+    expect(frame.getFirstScore()).toBe(3);
+  });
+
+  it("has a methods to set and get the second score", function() {
+    frame.setSecondScore(4);
+    expect(frame.getSecondScore()).toBe(4);
+  });
+
+  it("will return null from getSecondScore() if first score was a strike", function(){
+    frame.setFirstScore(10);
+    expect(frame.getSecondScore()).toEqual(null);
+  });
+
+  it("will return true from isStrike() if the first score was a strike", function() {
+    frame.setFirstScore(10);
+    expect(frame.isStrike()).toEqual(true);
+  });
+
+  it("will not allow the second score to be entered if the first score was a stike", function() {
+    frame.setFirstScore(10);
+    var errorMessage =  frame.setSecondScore(3);
+    expect(errorMessage).toEqual("No second score as first score was a strike!");
+    expect(frame.getSecondScore()).toEqual(null);
+  });
+
+  it("will return true from isSpare() if the second + first score adds up to ten", function() {
+    frame.setFirstScore(3);
+    frame.setSecondScore(7);
+    expect(frame.isSpare()).toEqual(true);
+  });
+
+
+  it("will return 'pending' from getFinalFrameScore() if finalised() returns false", function() {
+    expect(frame.getFinalFrameScore()).toEqual("pending");
+  });
+
+  it("it will return a number between 0-9 from getFinalFrameScore() if first and second scores add up to less than ten", function(){
+    frame.setFirstScore(3);
+    frame.setSecondScore(6);
+    expect(frame.getFinalFrameScore()).toEqual(9);
+  });
+
+  it("will return true from isFinalised() if scores one and two add up to less than 10", function(){
+    frame.setFirstScore(3);
+    frame.setSecondScore(6);
+    expect(frame.isFinalised()).toEqual(true);
+  });
+
+  it("it will return true from isFinialised() if setFinalFrameScore() has been called", function(){
+    frame.setFinalFrameScore(22);
+    expect(frame.isFinalised()).toEqual(true);
+  });
+
+  it("will return false from isFinialised() if isSpare() or isStrike() returns true and setFinalFrameScore() has not been called ", function() {
+    frame.spare = true;
+    expect(frame.isFinalised()).toEqual(false);
+    frame.spare = false;
+    frame.strike = true;
+    expect(frame.isFinalised()).toEqual(false);
+    frame.setFinalFrameScore(24);
+    expect(frame.isFinalised()).toEqual(true);
   });
 
 });
+
--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,70 @@
 function Frame() {
+    this.firstScore = null;
+    this.secondScore = null;
+    this.finalFrameScore = null;
+    this.STRIKE_SCORE = 10;
+    this.strike = false;
+    this.spare = false;
+    this.finalised = false;
+}
 
+Frame.prototype.setFirstScore = function(score){
+  if (score == this.STRIKE_SCORE) {
+     this.strike = true;
+  }
+  this.firstScore = score;
+}
+
+Frame.prototype.setSecondScore = function(score){
+  if (this.isStrike()) {
+    return "No second score as first score was a strike!";
+  } else {
+    if(score + this.getFirstScore() === this.STRIKE_SCORE) {
+      this.spare = true;
+    } else {
+      this.finalised = true;
+    }
+    this.secondScore = score;
+  }
+}
+
+Frame.prototype.getFirstScore = function(score){
+    return this.firstScore;
+}
+
+Frame.prototype.getSecondScore = function(score){
+  if (this.getFirstScore() === this.STRIKE_SCORE) {
+    return null;
+  } else {
+    return this.secondScore;
+  }
+}
+
+Frame.prototype.getFinalFrameScore = function() {
+   if (!this.finalised) {
+      return "pending";
+   } else {
+     this.setFinalFrameScore(this.getFirstScore() +
+                             this.getSecondScore());
+     return this.finalFrameScore;
+   }
+}
+
+Frame.prototype.setFinalFrameScore = function(score) {
+   this.finalised = true;
+   this.finalFrameScore = score;
+}
+
+Frame.prototype.isStrike = function() {
+   return this.strike;
+}
+
+Frame.prototype.isSpare = function() {
+   return this.spare;
+}
+
+Frame.prototype.isFinalised = function() {
+  return this.finalised;
 }
 
 if ( typeof module !== 'undefined' && module.hasOwnProperty('exports') )

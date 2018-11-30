7990810ac457d3bec05cec85d55294ee4689a65f
Dana Gajewska <mdgajewska@outlook.com>
2018-11-30 11:53:00 +0000

    Adds (single)strike and spare score functionalities

--- spec/BowlingScorecardSpec.js
+++ spec/BowlingScorecardSpec.js
@@ -4,8 +4,40 @@ describe('BowlingScorecard', function(){
     scorecard = new BowlingScorecard();
     scorecard.enterPlayer('Matt');
   });
-  it('can store the scores of the bowls', function(){
+  it('tallies up framescore after second bowl', function(){
     scorecard.enterBowl(5);
-    expect(scorecard.scores.frame1['bowl1']).toEqual(5);
+    scorecard.enterBowl(4);
+    expect(scorecard.scores.frame1['frameScore']).toEqual(9);
+  });
+  describe('strike is bowled', function(){
+    beforeEach(function(){
+      scorecard.enterBowl(10);
+    });
+    it('only records one bowl for frame1 is a strike is bowled',function(){
+      scorecard.enterBowl(5);
+      expect(scorecard.scores.frame1['bowl2']).not.toBeDefined();
+      expect(scorecard.scores.frame2['bowl1']).toEqual(5);
+    });
+    it('adds the next 2 bowls to the frame score', function(){
+      scorecard.enterBowl(4);
+      scorecard.enterBowl(5);
+      expect(scorecard.scores.frame1['frameScore']).toEqual(19);
+    });
+    it('can add 2 bowls if the second bowl is a strike', function(){
+      scorecard.enterBowl(10);
+      scorecard.enterBowl(5);
+      expect(scorecard.scores.frame1['frameScore']).toEqual(25);
+    })
+  });
+  describe('spare is bowled', function(){
+    beforeEach(function(){
+      scorecard.enterBowl(9);
+      scorecard.enterBowl(1);
+    });
+    it('adds the next bowl to the frame score', function(){
+      scorecard.enterBowl(4);
+      console.log(scorecard.scores);
+      expect(scorecard.scores.frame1['frameScore']).toEqual(14);
+    });
   });
 });
--- src/BowlingScorecard.js
+++ src/BowlingScorecard.js
@@ -1,17 +1,6 @@
 function BowlingScorecard() {
   this.playerName;
   this.scores = {};
-    // frame1: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame2: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame3: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame4: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame5: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame6: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame7: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame8: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame9: {bowl1:'', bowl2:'', bonus:'', totalScore:0},
-    // frame10: {bowl1:'', bowl2:'', bowl3:'', bonus:'', totalScore:0}
-
   this.frameTracker = [1,1];
 }
 BowlingScorecard.prototype.enterPlayer = function (name) {
@@ -19,20 +8,59 @@ BowlingScorecard.prototype.enterPlayer = function (name) {
 };
 BowlingScorecard.prototype.enterBowl = function (score) {
   frame = 'frame' + this.frameTracker[0];
-  bowl = 'bowl' + this.frameTracker[1];
   if (this.isFirstBowl()) {
     if (this.isStrike(score)){ //strike
-      this.scores[frame] = {[bowl]:score, bonus:'strike'};
+      this.scores[frame] = {bowl1:score, bonus:'strike'};
+      this.updateFrameScore();
       this.frameTracker[0]++;
-    } else {
-      this.scores[frame] = {[bowl]:score};
+    } else { //first bowl, no strike
+      this.scores[frame] = {bowl1:score};
+      this.updateFrameScore();
       this.frameTracker[1]++;
     }
+  } else if (this.isSpare(frame, score)) { //spare
+    this.scores[frame]['bowl2'] = score;
+    this.scores[frame]['bonus'] = 'spare';
+    this.updateFrameScore();
+    this.frameTracker=[this.frameTracker[0]+1,1];
+  } else { //second bowl, no spare
+    this.scores[frame]['bowl2'] = score;
+    this.updateFrameScore();
+    this.frameTracker=[this.frameTracker[0]+1,1];
   }
+};
+BowlingScorecard.prototype.updateFrameScore = function () {
+  current_frame = 'frame' + this.frameTracker[0];
+  last_frame = 'frame' + (this.frameTracker[0]-1);
+  try {
+    bonus = this.scores[last_frame]['bonus'];
+  }
+  catch(err) {
+    bonus = 'none';
+  }
+    switch (bonus) {
+      case 'strike':
+        if (this.scores[current_frame]['bonus']){
+
+        }
+        this.scores[last_frame]['frameScore'] = this.scores[last_frame]['bowl1']
+             + this.scores[current_frame]['bowl1'] + this.scores[current_frame]['bowl2'];
+        break;
+      case 'spare':
+        this.scores[last_frame]['frameScore'] = this.scores[last_frame]['bowl1']
+             + this.scores[last_frame]['bowl2'] + this.scores[current_frame]['bowl1'];
+        break;
+      default:
+        this.scores[current_frame]['frameScore'] = this.scores[current_frame]['bowl1']
+          + this.scores[current_frame]['bowl2'];
+    }
 };
 BowlingScorecard.prototype.isFirstBowl = function () {
   return this.frameTracker[1] === 1;
 };
 BowlingScorecard.prototype.isStrike = function (score) {
   return score == 10;
+};
+BowlingScorecard.prototype.isSpare = function (frame, score) {
+  return this.scores[frame]['bowl1'] + score == 10;
 };

04a73b5ee7402410113bc5a811e5d8085303a308
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-30 11:32:21 +0000

    roll method test passing, will reduce pin number with 1 roll

--- jasmine/jasmine-standalone-3.3.0/spec/bowlingSpec.js
+++ jasmine/jasmine-standalone-3.3.0/spec/bowlingSpec.js
@@ -12,5 +12,7 @@ describe('Game', function() {
     expect(game.start()).toEqual(10);
   });
 
-
+  it('reduces pin number with a roll', function() {
+    expect(game.roll()).toBeLessThan(10);
+  });
 });
--- jasmine/jasmine-standalone-3.3.0/src/game.js
+++ jasmine/jasmine-standalone-3.3.0/src/game.js
@@ -3,8 +3,16 @@
   function Game() {
     this.DEFAULT_FRAMES = 10;
     this.game = this.DEFAULT_FRAMES;
+    this.DEFAULT_PINS = 10;
+    this.pin_amount = this.DEFAULT_PINS
   };
 
   Game.prototype.start = function () {
     return this.game;
   };
+
+  Game.prototype.roll = function() {
+    var num = Math.floor(Math.random() * 10 + 1);
+    this.pin_amount -= num;
+    return this.pin_amount;
+  }

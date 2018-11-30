975f0ac4f7ba0423ebc451147f710981d83e59f0
Will Wayman <will.jdw@gmail.com>
2018-11-30 16:39:10 +0000

    will use an array for pins knocked down

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,17 +1,15 @@
 function ScoreCard () {
-  this.rollNumber = 1;
   this.frameNumber = 1;
-  this.rollScore = 0;
-  this.runningScore = 0;
-  this.strikeOrSpare = false;
+  this.rollNumber = 1;
+  this.pinsKnockedDown = [ [],[],[],[],[],[],[],[],[],[] ];
+  this.strikeOrSpare = null;
 }
 
 ScoreCard.prototype.addScore = function() {
+  this.pinsKnockedDown[this.frameNumber-1].push(arguments[0]);
   if (this.rollNumber == 1) {
-    this.rollScore += arguments;
     this.rollNumber ++;
   } else {
-    this.runningScore += this.rollScore + arguments;
     this.frameNumber ++;
     this.rollNumber = 1;
   }

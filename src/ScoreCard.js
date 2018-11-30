07abfe7730f8423019f53cb17947615e22233c29
Will Wayman <will.jdw@gmail.com>
2018-11-30 17:35:43 +0000

    second test passes

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -3,7 +3,7 @@ function ScoreCard () {
   this.rollNumber = 1;
   this.pinsKnockedDown = [ [],[],[],[],[],[],[],[],[],[] ];
   this.strikeOrSpare = null;
-}
+};
 
 ScoreCard.prototype.addScore = function() {
   this.pinsKnockedDown[this.frameNumber-1].push(arguments[0]);
@@ -12,5 +12,15 @@ ScoreCard.prototype.addScore = function() {
   } else {
     this.frameNumber ++;
     this.rollNumber = 1;
-  }
-}
+  };
+};
+
+ScoreCard.prototype.sumOfPins = function () {
+  var score = 0;
+  for (var i = 0; i < 10; i++) {
+    score += this.pinsKnockedDown[i].reduce(function(a,b) {
+      return a + b
+    });
+  };
+  return score;
+};

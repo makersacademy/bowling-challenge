d9daa7569647e220d0ef89711be850357c375ca6
Al <>
2018-11-30 13:18:25 +0000

    Implement simple frame-by-frame scoring
    Test-drove the creation of read & record methods on the Scorecard, then
    expanded the record method stepwise to account for 2 rolls per frame with
    an exception for strikes.

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,9 +1,36 @@
 "use strict"
 
 describe("Scorecard", function() {
+  var scorecard;
+  beforeEach(function() {
+    scorecard = new Scorecard;
+  });
+
   it("starts at Frame 1, Roll 1", function() {
-    var scorecard = new Scorecard;
     expect(scorecard.currentFrame).toEqual(1);
     expect(scorecard.currentRoll).toEqual(1);
+  });
+
+  describe("recording scores", function() {
+    it("returns the score of the first roll", function() {
+      scorecard.recordScore(5);
+      expect(scorecard.readScores(1, 1)).toEqual(5);
+    });
+    it("returns the score of the second roll", function() {
+      scorecard.recordScore(5);
+      scorecard.recordScore(3);
+      expect(scorecard.readScores(1, 2)).toEqual(3);
+    });
+    it("goes to the next frame after 2 rolls", function() {
+      scorecard.recordScore(5);
+      scorecard.recordScore(3);
+      scorecard.recordScore(7);
+      expect(scorecard.readScores(2, 1)).toEqual(7);
+    });
+    it("goes straight to the next frame after a strike", function() {
+      scorecard.recordScore(10);
+      scorecard.recordScore(6);
+      expect(scorecard.readScores(2, 1)).toEqual(6);
+    });
   });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -5,3 +5,18 @@ function Scorecard() {
   this.currentFrame = 1;
   this.currentRoll = 1;
 }
+Scorecard.prototype.recordScore = function (score) {
+  if(this._scoresList[this.currentFrame] === undefined) {
+    this._scoresList[this.currentFrame] = [];
+  }
+  this._scoresList[this.currentFrame][this.currentRoll] = score;
+  if(this.currentRoll === 2 || score === 10) {
+    this.currentRoll = 1;
+    this.currentFrame ++;
+  } else {
+    this.currentRoll ++;
+  }
+};
+Scorecard.prototype.readScores = function (frame, roll) {
+  return this._scoresList[frame][roll];
+};

41b9679efe4aff97e2c2ca2a4a1a5ab448377dda
willmcb <williamjamesmcbrien@gmail.com>
2018-11-30 16:40:50 +0000

    Fix whitespace issues

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,13 +2,25 @@ var Game = require('../src/game.js');
 
 describe("Game", function() {
 
-  beforeEach(function() {
-    game = new Game();
-  });
+    beforeEach(function() {
+      game = new Game();
+    });
 
-  it("is an instance of Game", function() {
-    expect(game).toEqual(jasmine.any(Game));
-  });
+    it("is an instance of Game", function() {
+      expect(game).toEqual(jasmine.any(Game));
+    });
+
+    it("is can add a new Frame to its internal array of frames", function() {
+    });
+
+    xit("can return a represetative data structure with the current state of the game", function() {
+    });
+
+    xit("it can update frames with a frameUpdaterService", function() {
+    });
+
+    xit("it can return the current frame", function() {
+    });
 
 });
 
--- src/frame.js
+++ src/frame.js
@@ -1,31 +1,31 @@
 function Frame() {
+    this.STRIKE_SCORE = 10;
     this.firstScore = null;
     this.secondScore = null;
     this.finalFrameScore = null;
-    this.STRIKE_SCORE = 10;
     this.strike = false;
     this.spare = false;
     this.finalised = false;
 }
 
 Frame.prototype.setFirstScore = function(score){
-  if (score == this.STRIKE_SCORE) {
-     this.strike = true;
-  }
-  this.firstScore = score;
+    if (score == this.STRIKE_SCORE) {
+        this.strike = true;
+    }
+    this.firstScore = score;
 }
 
 Frame.prototype.setSecondScore = function(score){
-  if (this.isStrike()) {
-    return "No second score as first score was a strike!";
-  } else {
-    if(score + this.getFirstScore() === this.STRIKE_SCORE) {
-      this.spare = true;
+    if (this.isStrike()) {
+        return "No second score as first score was a strike!";
     } else {
-      this.finalised = true;
+        if(score + this.getFirstScore() === this.STRIKE_SCORE) {
+            this.spare = true;
+        } else {
+            this.finalised = true;
+        }
+        this.secondScore = score;
     }
-    this.secondScore = score;
-  }
 }
 
 Frame.prototype.getFirstScore = function(score){
@@ -33,38 +33,38 @@ Frame.prototype.getFirstScore = function(score){
 }
 
 Frame.prototype.getSecondScore = function(score){
-  if (this.getFirstScore() === this.STRIKE_SCORE) {
-    return null;
-  } else {
-    return this.secondScore;
-  }
+    if (this.getFirstScore() === this.STRIKE_SCORE) {
+        return null;
+    } else {
+        return this.secondScore;
+    }
 }
 
 Frame.prototype.getFinalFrameScore = function() {
-   if (!this.finalised) {
-      return "pending";
-   } else {
-     this.setFinalFrameScore(this.getFirstScore() +
-                             this.getSecondScore());
-     return this.finalFrameScore;
-   }
+    if (!this.finalised) {
+        return "pending";
+    } else {
+        this.setFinalFrameScore(this.getFirstScore() +
+                                this.getSecondScore());
+        return this.finalFrameScore;
+    }
 }
 
 Frame.prototype.setFinalFrameScore = function(score) {
-   this.finalised = true;
-   this.finalFrameScore = score;
+    this.finalised = true;
+    this.finalFrameScore = score;
 }
 
 Frame.prototype.isStrike = function() {
-   return this.strike;
+    return this.strike;
 }
 
 Frame.prototype.isSpare = function() {
-   return this.spare;
+    return this.spare;
 }
 
 Frame.prototype.isFinalised = function() {
-  return this.finalised;
+    return this.finalised;
 }
 
 if ( typeof module !== 'undefined' && module.hasOwnProperty('exports') )

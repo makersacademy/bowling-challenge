9c8ac2e4bf8f81c1790cfc046a8e777d4e97d18b
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-12-01 17:36:00 +0000

    Frame class calculates scores and bonuses per frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -2,13 +2,69 @@
 
 describe('Frame', function() {
   var frame;
+  var frame2;
   var game;
+
   beforeEach(function() {
     frame = new Frame();
     game = jasmine.createSpyObj('game',['addFrame','calcFrameScore']);
    });
 
+   it('can update the rolls in a frame', function () {
+     frame.update(1, 3);
+     expect(frame.getAllRolls()).toEqual([1, 3]);
+   });
+
   it('can get the number of knocked down pins in a roll', function () {
     expect(frame.getRoll(1)).toEqual(0);
+  });
+
+  it('can return the number of all rolls in a frame', function () {
+    expect(frame.getAllRolls()).toEqual([0, 0]);
+  });
+
+  it('can return the sum of all rolls in a frame', function () {
+    frame.update(4, 3);
+    expect(frame._rollSum()).toEqual(7);
+  });
+
+  it('can tell if a frame is a strike', function () {
+    frame.update(10, 0);
+    expect(frame._isStrike()).toEqual(true);
+    expect(frame._isSpare()).toEqual(false);
+  });
+
+  it('can tell if a frame is a spare', function () {
+    frame.update(4, 6);
+    expect(frame._isSpare()).toEqual(true);
+    expect(frame._isStrike()).toEqual(false);
+  });
+
+  it('can calculate the bonus for a strike', function () {
+    frame.update(10, 0);
+    frame2 = new Frame();
+    frame2.update(2, 5);
+    expect(frame.calcStrikeBonus(frame2)).toEqual(7);
+  });
+
+  it('can calculate the bonus for a spare', function () {
+    frame.update(4, 6);
+    frame2 = new Frame();
+    frame2.update(2, 5);
+    expect(frame.calcSpareBonus(frame2)).toEqual(2);
+  });
+
+  it('can calculate the bonus as 0 if the frame is neither spare nor strike', function () {
+    frame.update(3, 4);
+    frame2 = new Frame();
+    frame2.update(2, 5);
+    expect(frame.calcBonus(frame2)).toEqual(0);
+  });
+
+  it('can calculate the total score of a frame', function () {
+    frame.update(4, 6);
+    frame2 = new Frame();
+    frame2.update(2, 5);
+    expect(frame.calcScore(frame2)).toEqual(12);
   });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,9 +1,55 @@
 'use strict';
 
 function Frame(){
-  this.rolls = [0, 0]
+  this.rolls = [0, 0];
+  this.MAX_ROLL = 10;
+  this.totalScore = 0
 }
 
 Frame.prototype.getRoll = function(number) {
   return this.rolls[number - 1];
-}
+};
+
+Frame.prototype.update = function(firstroll, secondroll) {
+  this.rolls[0] = firstroll;
+  this.rolls[1] = secondroll;
+};
+
+Frame.prototype.getAllRolls = function () {
+  return this.rolls;
+};
+
+Frame.prototype._rollSum = function () {
+  return this.rolls.reduce(function (total, roll) {
+    return total + roll;
+  });
+};
+
+Frame.prototype._isStrike = function () {
+  return this.rolls[0] == this.MAX_ROLL;
+};
+
+Frame.prototype.calcBonus = function (nextframe) {
+  if (this._isStrike()) {
+    return this.calcStrikeBonus(nextframe);
+  } else if (this._isSpare()) {
+    return this.calcSpareBonus(nextframe);
+  } else { return 0; }
+};
+
+Frame.prototype._isSpare = function () {
+  return this.rolls[0] < this.MAX_ROLL && this._rollSum() == this.MAX_ROLL;
+};
+
+Frame.prototype.calcStrikeBonus = function (nextframe) {
+  return nextframe._rollSum();
+};
+
+Frame.prototype.calcSpareBonus = function (nextframe) {
+    return nextframe.getRoll(1);
+};
+
+Frame.prototype.calcScore = function (nextframe) {
+    this.totalScore =  this._rollSum() + this.calcBonus(nextframe);
+    return this.totalScore
+};

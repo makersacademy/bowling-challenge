81247e3373a3732055922403a9a3dfb1d301922c
leoncross <leon@lscross.co.uk>
2018-12-01 11:41:50 +0000

    created storage process for bowls/frames, and created searchFrames function to isolate scores

--- spec/bowlingScore_Spec.js
+++ spec/bowlingScore_Spec.js
@@ -4,50 +4,27 @@ describe('Bowling', function() {
 
   beforeEach(function() {
     score = new Score();
-    frame = new Frame();
+
 
   });
-
-  describe('#runner', function() {
-    it('correctly identifies the bowl score', function() {
-      score.runner(1, 1, 3);
-      expect(score.currentScore).toEqual(3);
+  describe('#scoresIntoFrames', function() {
+    it('correctly inputs 3 into frame 1, bowl 1', function() {
+      score.scoresIntoFrames(1, 1, 3);
+      expect(score.frames[0]['bowl1']).toBe(3);
     });
-
-    it('correctly identifies the current turn', function() {
-      score.runner(1, 1, 5);
-      expect(score.currentTurn).toEqual(1);
+    it('correctly inputs 8 into frame 5, bowl 2', function() {
+      score.scoresIntoFrames(5, 2, 8);
+      expect(score.frames[4]['bowl2']).toBe(8);
     });
-
-    it('correctly identifies the current frame', function() {
-      score.runner(9, 2, 3);
-      expect(score.currentFrame).toEqual(9);
-    });
-
-
-    it('adds score to scoreboard', function() {
-      score.runner(1, 1, 5)
-      expect(score.scorecard[0]).toEqual(5);
-    });
-
   });
-
-  describe('#isAStrike', function() {
-    it ('identifies a strike', function() {
-      score.runner(1, 1, 10);
-      expect(score.isAStrike()).toEqual(true);
-
+  describe('#searchFrames', function() {
+    it('correctly identifies individual frame/bowl score', function() {
+      score.scoresIntoFrames(1, 1, 3);
+      expect(score.searchFrames(1, 1)).toEqual(3);
     });
-
-  })
-
-
+    it('correctly identifies individual frame/bowl score', function() {
+      score.scoresIntoFrames(1, 2, 9);
+      expect(score.searchFrames(1, 2)).toEqual(9);
+    });
+  });
 });
-
-
-// describe('to increase temperature', function() {
-//   it('increases temperature by 1 degree', function() {
-//     thermostat.up();
-//     expect(thermostat.temperature).toEqual(21);
-//   });
-// });
--- src/bowlingScore.js
+++ src/bowlingScore.js
@@ -1,39 +1,36 @@
 'use strict'
 
 var Score = function () {
-  this.scorecard = []
-  this.currentScore = 0
-  this.currentTurn = 0
-  // far frames = new Frames
-  // var frame1 = { 1: 0 2: 0 strike: false spare: false }
-  // var frame1 = new Object
-  // var test = '1'
+  this.frames =[
+  {frame: 1, bowl1: 0, bowl2: 0}, {frame: 2, bowl1: 0, bowl2: 0},
+  {frame: 3, bowl1: 0, bowl2: 0}, {frame: 4, bowl1: 0, bowl2: 0},
+  {frame: 5, bowl1: 0, bowl2: 0}, {frame: 6, bowl1: 0, bowl2: 0},
+  {frame: 7, bowl1: 0, bowl2: 0}, {frame: 8, bowl1: 0, bowl2: 0},
+  {frame: 9, bowl1: 0, bowl2: 0}, {frame: 10, bowl1: 0, bowl2: 0, bowl3: 0}
+  ];
 };
 
+Score.prototype.scoresIntoFrames = function(frame, bowl, score) {
+  this.frames.map(f => {
+    if (f.frame === frame) {
+      if (bowl === 1) {
+        f.bowl1 = score;
+      } else if (bowl === 2) {
+        f.bowl2 = score;
+      }
+    } return
+  });
+}
 
-Score.prototype.runner = function(frame, turn, score) {
-  this.currentFrame = frame
-  this.currentTurn = turn
-  this.currentScore = score
-  this.addToScorecard()
-  this.isAStrike()
-  const frame = new Frame()
-
-};
-
-Score.prototype.addToScorecard = function() {
-  this.scorecard.push(this.currentScore)
-};
-
-
-Score.prototype.isAStrike = function() {
-  if (this.currentScore === 10) {
-    return true
-  } else {
-    return false
+Score.prototype.searchFrames = function (frame, bowl) {
+  for (var i = 0; i < this.frames.length; i++) {
+      if (this.frames[i]['frame'] === frame) {
+        if (bowl === 1) {
+          return this.frames[i]['bowl1'];
+        } else if (bowl === 2) {
+          return this.frames[i]['bowl2'];
+        }
+      }
   }
-};
-
-Score.prototype.isAStrike = function() {
-
-};
+  return null;
+}

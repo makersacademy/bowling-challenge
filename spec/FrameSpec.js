f34c44fbd0252896509a061f61e8c9cf1c867cf5
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-02 10:28:36 +0000

    Fixed some syntax

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -3,39 +3,39 @@
 describe("A frame of bowling", () => {
 
   let frame
-  let next
-  let next_but_next
+  let next_frame
+  let next_next_frame
 
   it("calculates a total for two rolls", () => {
     frame = makeFrame([1,3])
-    next = makeFrame([0,0])
+    next_frame = makeFrame([0,0])
     expect(frame.total()).toEqual(4)
   })
 
   it("calculates a total for a spare", () => {
     frame = makeFrame([5,5])
-    next = makeFrame([5,2])
-    expect(frame.total(next)).toEqual(15)
+    next_frame = makeFrame([5,2])
+    expect(frame.total(next_frame)).toEqual(15)
   })
 
   it("calculates a total for a strike", () => {
     frame = makeFrame([10])
-    next = makeFrame([5,2])
-    expect(frame.total(next)).toEqual(17)
+    next_frame = makeFrame([5,2])
+    expect(frame.total(next_frame)).toEqual(17)
   })
 
   it("calculates a total for two strikes in a row", () => {
     frame = makeFrame([10])
-    next = makeFrame([10])
-    next_but_next = makeFrame([5, 2])
-    expect(frame.total(next, next_but_next)).toEqual(25)
+    next_frame = makeFrame([10])
+    next_next_frame = makeFrame([5, 2])
+    expect(frame.total(next_frame, next_next_frame)).toEqual(25)
   })
 
   it("calculates three strikes in a row", () => {
     frame = makeFrame([10])
-    next = makeFrame([10])
-    next_but_next = makeFrame([10])
-    expect(frame.total(next, next_but_next)).toEqual(30)
+    next_frame = makeFrame([10])
+    next_next_frame = makeFrame([10])
+    expect(frame.total(next_frame, next_next_frame)).toEqual(30)
   })
 
   it ("calculates a strike in the final frame", () => {
@@ -45,8 +45,8 @@ describe("A frame of bowling", () => {
 
   it ("calculates a strike in the final frame but one", () => {
     frame = makeFrame([10])
-    next = makeFrame([10,10,10])
-    expect(frame.total(next)).toEqual(30)
+    next_frame = makeFrame([10,10,10])
+    expect(frame.total(next_frame)).toEqual(30)
   })
 
   function makeFrame(array) {
--- src/Frame.js
+++ src/Frame.js
@@ -5,8 +5,8 @@ var Frame = function(rolls) {
   this.rolls = rolls
 }
 
-Frame.prototype.total = function(next_frame, next_but_next_frame) {
-  return this._frameScore() + this._bonusScore(next_frame, next_but_next_frame)
+Frame.prototype.total = function(next_frame, next_next_frame) {
+  return this._frameScore() + this._bonusScore(next_frame, next_next_frame)
 }
 
 Frame.prototype._frameScore = function() {
@@ -15,9 +15,9 @@ Frame.prototype._frameScore = function() {
   })
 }
 
-Frame.prototype._bonusScore = function(next_frame, next_but_next_frame) {
+Frame.prototype._bonusScore = function(next_frame, next_next_frame) {
   if (undefined === next_frame) return 0
-  if (this._isStrike()) return next_frame._strikeBonus(next_but_next_frame)
+  if (this._isStrike()) return next_frame._strikeBonus(next_next_frame)
   if (this._isSpare()) return next_frame._spareBonus()
   return 0
 }
@@ -35,6 +35,7 @@ Frame.prototype._spareBonus = function() {
 }
 
 Frame.prototype._strikeBonus = function(next_frame) {
+  // this refers to the "next" frame here as its called on the object of the next frame
   if (this._isStrike() && next_frame !== undefined) {
     return this._frameScore() + next_frame._firstRoll()
   }

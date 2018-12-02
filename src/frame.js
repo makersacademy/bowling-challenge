046eb542efbf9328b1c757e4b58f00e167aeafbe
AObsiye <mrabdirizak@gamil.com>
2018-12-02 22:57:37 +0000

    Can store first value in frame

---
+++ src/frame.js
@@ -0,0 +1,11 @@
+function Frame() {
+    this.first;
+}
+
+Frame.prototype.setFirst = function(value){
+    this.first = value;
+}
+
+Frame.prototype.getFirst = function(){
+    return this.first;
+}

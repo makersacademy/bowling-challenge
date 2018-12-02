080f3b5a02120c847775e02894b35ff6a2b1069c
Asad Khan <asad_khan47@hotmail.co.uk>
2018-12-02 18:45:21 +0000

    Added functionality for spare score

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -2,34 +2,34 @@
 
 describe('Bowling', function() {
 
-  var game;
+  var bowling;
 
   beforeEach(function () {
-    game = new Bowling();
+    bowling = new Bowling();
   });
 
   var rollMany = function (pins, rolls) {
     for (var i = 0; i < rolls; i++) {
-      game.roll(pins)
+      bowling.roll(pins)
     }
   };
 
   it('can roll a gutter game', function () {
     rollMany(0, 20)
-    expect(game.score()).toEqual(0)
+    expect(bowling.score()).toEqual(0)
   });
 
   it('can roll a game with all ones', function () {
     rollMany(1, 20)
-    expect(game.score()).toEqual(20)
+    expect(bowling.score()).toEqual(20)
   });
 
-  // it('can roll a spare', function () {
-  //   game.roll(5);
-  //   game.roll(5);
-  //   game.roll(3);
-  //   rollMany(0, 17);
-  //   expect(game.score()).toEqual(16);
-  // });
+  it('can roll a spare', function () {
+    bowling.roll(5);
+    bowling.roll(5);
+    bowling.roll(3);
+    rollMany(0, 17);
+    expect(bowling.score()).toEqual(16);
+  });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -11,8 +11,13 @@ Bowling.prototype.roll = function (pins) {
 Bowling.prototype.score = function () {
   var result = 0;
   var rollIndex = 0;
+
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    result += this.frame[rollIndex] + this.frame[rollIndex + 1];
+    if (this.frame[rollIndex] + this.frame[rollIndex + 1] === 10) {
+      result += this.frame[rollIndex] + this.frame[rollIndex + 1] + this.frame[rollIndex + 2];
+    } else {
+      result += this.frame[rollIndex] + this.frame[rollIndex + 1];
+    }
     rollIndex += 2;
   }
   return result;

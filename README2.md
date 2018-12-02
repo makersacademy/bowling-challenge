0b4b8611a748a27b56ac005649fe6b7e89039657
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-12-02 14:32:08 +0000

    changed test and code for record first roll test - as this was not able to add for for further rolls

--- README2.md
+++ README2.md
@@ -4,4 +4,8 @@ user stories
 As a player
 So I can keep the score of a game of bowling
 I would like to record the first roll within a frame
+
+As a player
+So I can keep the score of a game of bowling
+I would like to record the score of a whole frame
 ///
--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -7,19 +7,26 @@ describe('ScoreCard', function() {
 
   describe('default roll value', function() {
     it('initializes each roll to default to zero before each throw', function() {
-      expect(scoreCard.roll).toEqual(0);
+      expect(scoreCard.defaultRollValue).toEqual(0);
     });
   });
 
   describe('record first roll', function() {
     it('adds first roll to first frame', function() {
-      expect(scoreCard.roll).toEqual(0);
+      expect(scoreCard.rollOne).toEqual(0);
       scoreCard.frameOne();
-      expect(scoreCard.roll).toEqual(1);
-      console.log(scoreCard.roll)
+      expect(scoreCard.rollOne).toEqual(1);
     });
   });
 
+  // describe('record the score of one frame', function() {
+  //   it('adds roll one and roll two to the frame score array', function() {
+  //     expect(scoreCard.roll).toEqual(0);
+  //     scoreCard.frameOne();
+  //     expect(scoreCard.roll).toEqual(1);
+  //
+  //   });
+  // });
 
 
 });

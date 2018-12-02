45a5f94eee3eb6883c15e36ec3b80c08334a5879
alittlecross <alittlecross@live.co.uk>
2018-12-02 06:49:14 +0000

    changed tack, recording scores, and running totals, in arrays

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,27 +1,90 @@
 'use strict';
 
 describe('Scorecard:', function() {
+
   var game;
 
   beforeEach(function() {
     game = new Scorecard();
   });
 
-  it('starts with a score of 0', function() {
-    expect(game.score()).toEqual(0);
+  describe('At the start of a game:', function() {
+    
+    it('_scores is empty', function() {
+      expect(game._scores).toEqual([]);
+    });
+
+    it('_frames is empty', function() {
+      expect(game._frames).toEqual([[],[],[],[],[],[],[],[],[],[]]);
+    });
   });
 
-  it('starts with a 10 pins standing', function() {
-    expect(game.pins()).toEqual(10);
+  describe('Even check:', function() {
+
+    it('returns true for 2, false for 3', function() {
+      expect(game.even(2)).toEqual(true);
+      expect(game.even(3)).toEqual(false);
+    });
   });
 
-  it('throw_1 hitting 5 to increase score by 5', function() {
-    game.throw_1(5);
-    expect(game.score()).toEqual(5);
+  describe('Throws:', function() {
+
+    beforeEach(function(){
+      game.throw(4);
+    });
+
+    it('after one throw, _scores records result, _frames is empty', function() {
+      expect(game._scores).toEqual([4]);
+      expect(game._frames).toEqual([[],[],[],[],[],[],[],[],[],[]]);
+    });
+
+    it('after multiple throws, _frames records the running total', function() {
+      game.throw(4);
+      game.throw(4);
+      game.throw(4);
+      expect(game._frames[1]).toEqual(16);
+    });
+  });
+  
+  describe('Spares:', function() {
+
+    beforeEach(function(){
+      game.throw(0);
+      game.throw(10);
+    });
+
+    it('_scores records the scores seperately', function() {
+      expect(game._scores).toEqual([0,10]);
+    });
+
+    it('_frames does not total the scores', function() {
+      expect(game._frames[0]).toEqual(NaN);
+    });
+
+    it('after one more throw, _frames shows the running total', function() {
+      game.throw(10);
+      expect(game._frames[0]).toEqual(20);
+    })
   });
 
-  it('throw_1 hitting 5 to decrease pins by 5', function() {
-    game.throw_1(5);
-    expect(game.score()).toEqual(5);
+  describe('Strikes:', function() {
+
+    beforeEach(function(){
+      game.throw(10);
+    });
+    
+    it('_scores records two scores (10, and 0)', function() {
+      expect(game._scores).toEqual([10,0]);
+    });
+
+    it('_frames does not total the scores', function() {
+      expect(game._frames[0]).toEqual(NaN);
+    });
+
+    it('after two more throws, _frames shows the running total', function() {
+      game.throw(10);
+      game.throw(10);
+      expect(game._frames[0]).toEqual(30);
+    });
   });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,17 +1,39 @@
 function Scorecard() {
-  this._score = 0;
-  this._pins = 10;
+  this._scores = [];
+  this._frames = [[],[],[],[],[],[],[],[],[],[]]
 };
 
-Scorecard.prototype.score = function() {
-  return this._score;
+Scorecard.prototype.even = function(something) {
+  if (something % 2 == 0) { return true; }
+  else { return false; }
 };
 
-Scorecard.prototype.pins = function() {
-  return this._pins;
+Scorecard.prototype.throw = function(pinsDown) {
+  if (this.even(this._scores.length) && pinsDown == 10) {
+    this._scores.push(pinsDown); 
+    this._scores.push(0); 
+  }
+  else { this._scores.push(pinsDown); }
+  this.eachFrame();
 };
 
-Scorecard.prototype.throw_1 = function(num) {
-  this._score += num;
-  this._pins -= num;
+Scorecard.prototype.eachFrame = function() {
+  var hold = 0;
+  var i;
+  for (i = 0; i < this._scores.length; i++) {
+    if (this.even(i)) { hold += this._scores[i]; }
+    else { 
+      if (this._scores[i - 1] == 10) {
+        hold += this._scores[i + 1];
+        if (this._scores[i + 1] == 10) { hold += this._scores[i + 3]; }
+        else { hold += this._scores[i + 2]; }
+      }
+      else if ((this._scores[i - 1] + this._scores[i]) == 10) { 
+        hold += this._scores[i + 1]; 
+      }
+      hold += this._scores[i];
+      index = (i / 2) - 0.5;
+      this._frames[index] = hold;
+    }
+  }
 };

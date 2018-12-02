909ce89c1df0dbdecd23bb47d95e2f5970575719
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-02 10:06:05 +0000

    refactored code

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,47 +1,55 @@
-'use strict';
+'use strict'
 
-describe("A frame of bowling", function(){
+describe("A frame of bowling", () => {
 
-  it("calculates a total for two rolls", function(){
-    var frame = new Frame([1,3]);
-    var next = new Frame([0,0]);
-    expect(frame.total()).toEqual(4);
-  });
+  let frame
+  let next
+  let next_but_next
 
-  it("calculates a total for a spare", function(){
-    var frame = new Frame([5,5]);
-    var next = new Frame([5,2]);
-    expect(frame.total(next)).toEqual(15);
-  });
+  it("calculates a total for two rolls", () => {
+    frame = makeFrame([1,3])
+    next = makeFrame([0,0])
+    expect(frame.total()).toEqual(4)
+  })
 
-  it("calculates a total for a strike", function(){
-    var frame = new Frame([10]);
-    var next = new Frame([5,2]);
-    expect(frame.total(next)).toEqual(17);
-  });
+  it("calculates a total for a spare", () => {
+    frame = makeFrame([5,5])
+    next = makeFrame([5,2])
+    expect(frame.total(next)).toEqual(15)
+  })
 
-  it("calculates a total for two strikes in a row", function(){
-    var frame = new Frame([10]);
-    var next = new Frame([10]);
-    var next_but_next = new Frame([5, 2]);
-    expect(frame.total(next, next_but_next)).toEqual(25);
-  });
+  it("calculates a total for a strike", () => {
+    frame = makeFrame([10])
+    next = makeFrame([5,2])
+    expect(frame.total(next)).toEqual(17)
+  })
 
-  it("calculates three strikes in a row", function(){
-    var frame = new Frame([10]);
-    var next = new Frame([10]);
-    var next_but_next = new Frame([10]);
-    expect(frame.total(next, next_but_next)).toEqual(30);
-  });
+  it("calculates a total for two strikes in a row", () => {
+    frame = makeFrame([10])
+    next = makeFrame([10])
+    next_but_next = makeFrame([5, 2])
+    expect(frame.total(next, next_but_next)).toEqual(25)
+  })
 
-  it ("calculates a strike in the final frame", function(){
-    var frame = new Frame([10,10,10]);
-    expect(frame.total()).toEqual(30);
-  });
+  it("calculates three strikes in a row", () => {
+    frame = makeFrame([10])
+    next = makeFrame([10])
+    next_but_next = makeFrame([10])
+    expect(frame.total(next, next_but_next)).toEqual(30)
+  })
 
-  it ("calculates a strike in the final frame but one", function(){
-    var frame = new Frame([10]);
-    var next = new Frame([10,10,10]);
-    expect(frame.total(next)).toEqual(30);
-  });
-});
+  it ("calculates a strike in the final frame", () => {
+    frame = makeFrame([10,10,10])
+    expect(frame.total()).toEqual(30)
+  })
+
+  it ("calculates a strike in the final frame but one", () => {
+    frame = makeFrame([10])
+    next = makeFrame([10,10,10])
+    expect(frame.total(next)).toEqual(30)
+  })
+
+  function makeFrame(array) {
+    return new Frame(array)
+  }
+})
--- src/Frame.js
+++ src/Frame.js
@@ -1,57 +1,50 @@
-'use strict';
+'use strict'
 
 var Frame = function(rolls) {
-  this.MAXIMUM_SCORE = 10;
-  this.rolls = rolls;
-};
+  this.MAXIMUM_SCORE = 10
+  this.rolls = rolls
+}
 
 Frame.prototype.total = function(next_frame, next_but_next_frame) {
-  return this._rollScore() + this._bonusScore(next_frame, next_but_next_frame);
-};
+  return this._frameScore() + this._bonusScore(next_frame, next_but_next_frame)
+}
 
-Frame.prototype._rollScore = function() {
+Frame.prototype._frameScore = function() {
   return this.rolls.reduce(function(score, roll) {
-    return score + roll;
-  });
-};
+    return score + roll
+  })
+}
 
 Frame.prototype._bonusScore = function(next_frame, next_but_next_frame) {
-  if (undefined === next_frame) {
-    return 0;
-  }
-  if (this._isStrike()) {
-    return next_frame._strikeBonus(next_but_next_frame);
-  }
-  if (this._isSpare()) {
-    return next_frame._spareBonus();
-  }
-  return 0;
-};
+  if (undefined === next_frame) return 0
+  if (this._isStrike()) return next_frame._strikeBonus(next_but_next_frame)
+  if (this._isSpare()) return next_frame._spareBonus()
+  return 0
+}
 
 Frame.prototype._isSpare = function() {
-  return this._rollScore() == this.MAXIMUM_SCORE;
-};
+  return this._frameScore() == this.MAXIMUM_SCORE
+}
 
 Frame.prototype._isStrike = function() {
-  return this._firstRoll() == this.MAXIMUM_SCORE;
-};
+  return this._firstRoll() == this.MAXIMUM_SCORE
+}
 
 Frame.prototype._spareBonus = function() {
-  return this._firstRoll();
-};
+  return this._firstRoll()
+}
 
 Frame.prototype._strikeBonus = function(next_frame) {
   if (this._isStrike() && next_frame !== undefined) {
-    return this._rollScore() + next_frame._firstRoll();
+    return this._frameScore() + next_frame._firstRoll()
   }
-
-  return this._firstRoll() + this._secondRoll();
-};
+  return this._firstRoll() + this._secondRoll()
+}
 
 Frame.prototype._firstRoll = function() {
-  return this.rolls[0];
-};
+  return this.rolls[0]
+}
 
 Frame.prototype._secondRoll = function () {
-  return this.rolls[1];
-};
+  return this.rolls[1]
+}

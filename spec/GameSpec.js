47114a0fba2e60621fe7832fc38eb92f8cff52dd
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-02 19:02:57 +0000

    awaiting large refactor

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -41,11 +41,22 @@ describe("Game", function() {
     expect(game.score()).toEqual(12);
   });
 
+  it('calculates a game with 2 strikes', function(){
+    game.roll(10);
+    game.roll("X");
+    game.roll(5);
+    game.roll(1);
+    game.roll(10);
+    game.roll("X");
+    game.roll(1);
+    multiRoll(13, 0);
+    expect(game.score()).toEqual(34);
+  });
+
   function multiRoll(amount, pins) {
     var i;
     for (i = 0; i < amount; i++) {
       game.roll(pins);
     };
   };
-
 });

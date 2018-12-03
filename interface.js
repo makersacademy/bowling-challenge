31c45990038c2b1487a52af4c64143e6eec46bdd
Ajay <ajaymungur@hotmail.com>
2018-12-03 08:54:15 +0000

    added some interface functionality

--- interface.js
+++ interface.js
@@ -1,23 +1,32 @@
 var game = new Game();
 var frame = new Frame(game.frame);
 
-
 $("#save-score").on('click', function(){
-  if (game.turn % 2 != 0) {
-    frame.addFirstScore($("#enter-score").val());
-    if (!is_strike(frame.firstTurn)) {
-      frame.firstTurn = Number(frame.firstTurn);
+
+    if (game.turn % 2 != 0) {
+      frame.addFirstScore($("#enter-score").val());
+      displayScore(frame.firstTurn);
+      if (frame.firstTurn === "x") {
+        game.turn ++
+        displayScore(frame.secondTurn);
+        game.addFrame(frame);
+        game.calculateScores();
+      }
     } else {
-      $('#score_' + game.turn.toString()).text(frame.firstTurn);
+      frame.addSecondScore($("#enter-score").val());
+      displayScore(frame.secondTurn);
+      game.addFrame(frame);
+      game.calculateScores();
     }
-    } else {
-    frame.secondTurn = $("#enter-score").val();
-    frame.secondTurn = Number(frame.secondTurn);
-    $('#score_' + game.turn.toString()).text(frame.secondTurn);
-    frame.addTotal();
-    game.addFrame(frame);
-    game.applyBonuses();
-    $("#total_" + (game.frame - 1).toString()).text(game.total);
-  }
-  game.turn ++
-})
+  displayTotal();
+  frame = new Frame();
+  game.turn ++;
+});
+
+displayScore = function(turn){
+  $('#score-' + game.turn.toString()).text(turn);
+};
+
+displayTotal = function(){
+  $('#total-' + game.frame.toString()).text(game.scoreTable[game.frame -1].total);
+};

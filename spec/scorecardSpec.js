0917a0d6ef0c30a7664c2f5b7831a382622c472c
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-12-03 12:20:25 +0000

    Scorecard can now record Strikes and Spares

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -2,7 +2,7 @@
 
 describe("Scorecard", function() {
     var score;
-    var testScorecard = Array(10).fill(0).map( x => Array(3).fill(0));
+    var testScorecard = Array(10).fill(0).map( x => Array(5).fill(0));
 
     beforeEach(function() {
         score = new Scorecard();
@@ -26,40 +26,74 @@ describe("Scorecard", function() {
         score.updateScore(10,1,10);
         score.updateScore(10,2,10);
         score.updateScore(10,3,5);
-        expect(score.scores[0][0]).toEqual(10);
-        expect(score.scores[0][1]).toEqual(0);
-        expect(score.scores[1][0]).toEqual(3);
-        expect(score.scores[1][1]).toEqual(6);
-        expect(score.scores[2][0]).toEqual(5);
+        expect(score.scores[0][1]).toEqual(10);
+        expect(score.scores[0][2]).toEqual(0);
+        expect(score.scores[1][1]).toEqual(3);
+        expect(score.scores[1][2]).toEqual(6);
         expect(score.scores[2][1]).toEqual(5);
-        expect(score.scores[8][0]).toEqual(7);
-        expect(score.scores[8][1]).toEqual(1);
-        expect(score.scores[9][0]).toEqual(10);
+        expect(score.scores[2][2]).toEqual(5);
+        expect(score.scores[8][1]).toEqual(7);
+        expect(score.scores[8][2]).toEqual(1);
         expect(score.scores[9][1]).toEqual(10);
-        expect(score.scores[9][2]).toEqual(5);
+        expect(score.scores[9][2]).toEqual(10);
+        expect(score.scores[9][3]).toEqual(5);
 
         console.log(JSON.stringify(score.scores));
 
     });
 
     it("can record frames between 1 and 10", function () {
-      var i;
-      for (i = 1; i < 11; i++) {
-          score.frame = i;
-          console.log(score.frame);
-          expect(score.frame >= 1 && score.frame <= 10).toBeTruthy();
-          }
+        var i;
+        for (i = 1; i < 11; i++) {
+            score.frame = i;
+            console.log(score.frame);
+            expect(score.frame >= 1 && score.frame <= 10).toBeTruthy();
+            }
     });
 
     it("can record rolls of a frame", function() {
-      var i;
-      for (i = 1; i < 3; i++) {
-          score.roll = i;
-          console.log(score.roll);
-          expect(score.roll == 1 || score.roll == 2).toBeTruthy();
-          }
-
+        var i;
+        for (i = 1; i < 3; i++) {
+            score.roll = i;
+            console.log(score.roll);
+            expect(score.roll == 1 || score.roll == 2).toBeTruthy();
+            }
     });
+
+    it("knows when a Strike has been bowled", function () {
+        score.updateScore(1,1,10);
+        score.updateScore(1,2,0);
+        score.updateScore(3,1,5);
+        score.updateScore(3,2,5);
+        score.updateScore(5,1,0);
+        score.updateScore(5,2,10);
+        score.updateScore(10,1,10);
+        score.updateScore(10,2,10);
+        score.updateScore(10,3,10);
+        console.log(JSON.stringify(score.scores));
+        expect(score.scores[0][0]).toEqual(1);
+        expect(score.scores[2][0]).toEqual(0);
+        expect(score.scores[4][0]).toEqual(0);
+        expect(score.scores[9][0]).toEqual(1);
+    });
+
+    it("knows when a Spare has been bowled", function () {
+      score.updateScore(1,1,10);
+      score.updateScore(3,1,0);
+      score.updateScore(3,2,10);
+      score.updateScore(5,1,5);
+      score.updateScore(5,2,5);
+      score.updateScore(10,1,3);
+      score.updateScore(10,2,7);
+      score.updateScore(10,3,10);
+      console.log(JSON.stringify(score.scores));
+      expect(score.scores[0][4]).toEqual(0);
+      expect(score.scores[2][4]).toEqual(1);
+      expect(score.scores[4][4]).toEqual(1);
+      expect(score.scores[9][4]).toEqual(1);
+    })
+
+
 
 
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,11 +1,27 @@
 'use strict';
 
 function Scorecard() {
-this.scores = Array(10).fill(0).map( x => Array(3).fill(0));
+this.scores = Array(10).fill(0).map( x => Array(5).fill(0));
 this.frame = 1
 this.roll = 1
 }
 
 Scorecard.prototype.updateScore = function(frame, roll, pins) {
-  this.scores[frame-1][roll-1] = pins;
+    this.scores[frame-1][roll] = pins;
+    this.isStrike (frame, roll, pins);
+    this.isSpare (frame, roll, pins);
 }
+
+Scorecard.prototype.isStrike = function (frame, roll, pins) {
+    if (roll === 1 && pins === 10) {
+        this.scores[frame-1][0] = 1};
+    if (frame === 10 && roll === 2 && pins === 10) {
+        this.scores[frame-1][0] = 1};
+  }
+
+Scorecard.prototype.isSpare = function (frame, roll, pins) {
+    if (roll === 2 && this.scores[frame-1][0] === 0) {
+        if (this.scores[frame-1][1] + this.scores[frame-1][2] == 10) {
+            this.scores[frame-1][4] = 1};
+        }
+    }

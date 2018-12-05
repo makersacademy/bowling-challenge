b5083fbaf4ae09f74e673182f51f793837342ec6
Will Wayman <will.jdw@gmail.com>
2018-12-05 22:07:19 +0000

    all tests pass

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -7,22 +7,20 @@ function ScoreCard () {
 };
 
 ScoreCard.prototype.addScore = function(pins) {
-  this.addPins(pins);
-  this.addBonus(pins);
-  this.updateProperties(pins);
-};
-
-ScoreCard.prototype.addPins = function(pins) {
-  var tenthFrameTotal = 0;
-  tenthFrameTotal += this.pinsKnockedDown[9].reduce(function(a,b) {
-    return a + b
-  }, 0);
-  if (tenthFrameTotal < 10) {
-    this.pinsKnockedDown[this.frameNumber-1].push(pins);
+  if (this.frameNumber != 10) {
+    this.addPins(pins);
+    this.addPinsToBonusScore(pins);
+    this.updateProperties(pins);
+  } else {
+    this.tenthFrame(pins);
   };
 };
 
-ScoreCard.prototype.addBonus = function(pins) {
+ScoreCard.prototype.addPins = function(pins) {
+  this.pinsKnockedDown[this.frameNumber-1].push(pins);
+};
+
+ScoreCard.prototype.addPinsToBonusScore = function(pins) {
   for (var i = 0; i < this.bonusTrackers.length; i++) {
     if (this.bonusTrackers[i].counter > 0) {
       this.bonusPins[this.bonusTrackers[i].frameRolledOn-1] += pins
@@ -32,30 +30,41 @@ ScoreCard.prototype.addBonus = function(pins) {
 };
 
 ScoreCard.prototype.updateProperties = function(pins) {
-  var tenthFrameTotal = 0;
-  tenthFrameTotal += this.pinsKnockedDown[9].reduce(function(a,b) {
-    return a + b
-  }, 0);
   if (this.rollNumber == 1) {
     if (pins == 10) {
-      if (tenthFrameTotal < 10) {
-        this.bonusTrackers.push(new BonusTracker(this.frameNumber, 2))
-        if (this.frameNumber != 10) {
-          this.frameNumber ++;
-        };
-      };
+      this.bonusTrackers.push(new BonusTracker(this.frameNumber, 2))
+      this.frameNumber ++;
     } else {
       this.rollNumber ++;
     };
   } else {
-    if (this.pinsKnockedDown[this.frameNumber-1][0] + pins == 10 && tenthFrameTotal < 10 ) {
+    if (this.pinsKnockedDown[this.frameNumber-1][0] + pins == 10) {
       this.bonusTrackers.push(new BonusTracker(this.frameNumber, 1))
     };
-    if (this.frameNumber != 10) {
-      this.frameNumber ++;
-      this.rollNumber = 1;
+    this.frameNumber ++;
+    this.rollNumber = 1;
+  };
+};
+
+ScoreCard.prototype.tenthFrame = function(pins) {
+  var tenthFrameTotal = 0;
+  tenthFrameTotal += this.pinsKnockedDown[9].reduce(function(a,b) {
+    return a + b
+  }, 0);
+  if (tenthFrameTotal < 10) {
+    this.addPins(pins);
+  };
+  this.addPinsToBonusScore(pins);
+  if (this.rollNumber == 1) {
+    if (pins == 10) {
+      this.bonusTrackers.push(new BonusTracker(this.frameNumber, 2))
+    };
+  } else if (this.rollNumber == 2) {
+    if (this.pinsKnockedDown[this.frameNumber-1][0] + pins == 10) {
+      this.bonusTrackers.push(new BonusTracker(this.frameNumber, 1))
     };
   };
+  this.rollNumber ++;
 };
 
 ScoreCard.prototype.calculateScore = function(toFrame = 10) {  // calculates all frames by default

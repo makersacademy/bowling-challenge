f6c75f784dcf15316222e9aa60fac21685b7445f
Luca <eto.lucafrancesco@gmail.com>
2018-12-08 11:55:29 +0000

    Can score a strike/perfect game

--- js/Bowling.js
+++ js/Bowling.js
@@ -12,12 +12,19 @@ Bowling.prototype.gameScore = function () {
   var roll = 0;
 
   for(var frames = 0; frames < 10; frames++) {
-    if (this.rolls[roll] + this.rolls[roll+1] == 10) {
+
+    if (this.rolls[roll] == 10) {
       score += this.rolls[roll] + this.rolls[roll+1] + this.rolls[roll+2]
+      roll += 1;
+    } else if (this.rolls[roll] + this.rolls[roll+1] == 10) {
+      score += this.rolls[roll] + this.rolls[roll+1] + this.rolls[roll+2]
+      roll += 2;
     } else {
       score += this.rolls[roll] + this.rolls[roll+1]
+      roll += 2;
     }
-    roll += 2;
+
   }
+
   return score;
 };
--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -23,12 +23,27 @@ describe ('bowling', function() {
     expect(game.gameScore()).toEqual(8)
   });
 
-  it('can roll a ball and hit pins', function() {
+  it('can score a spare', function() {
     game.roll(5);
     game.roll(5);
     game.roll(5);
     for(var i = 0; i < 18; i++) { game.roll(0) }
     expect(game.gameScore()).toEqual(20)
+  });
+
+  it('can score a stike', function() {
+    game.roll(10);
+    game.roll(5);
+    game.roll(5);
+    for(var i = 0; i < 18; i++) { game.roll(0) }
+    expect(game.gameScore()).toEqual(30)
+  });
+
+  it('can score 300 points (perfect game)', function() {
+    for(var i = 0; i < 10; i++) { game.roll(10) }
+    game.roll(10);
+    game.roll(10);
+    expect(game.gameScore()).toEqual(300)
   });
 
 });

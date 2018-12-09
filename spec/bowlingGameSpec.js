a5556b9b2fad4b70800d49440057c56331dbf1dd
Alpha Mackie <alphamackie@alphas-mbp.lan>
2018-12-09 18:54:29 +0000

    Create an interface file and run a passing gutter game test

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -1,7 +1,23 @@
 'use strict';
 
-describe("Bowling Game", function() {
-  it("can create a bowling game", function() {
-    var bowlingGame = new BowlingGame();
+describe('Bowling Game', function() {
+  it('can create a bowling game', function() {
+    var game = new BowlingGame();
   });
+
+  //player never hits a pin (20 zero scores)
+  it('can roll a gutter game', function() {
+    var game = new BowlingGame();
+    for(var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toBe(0);
+  });
+
+  // it('can roll ones', function() {
+  //   var bowlingGame = new BowlingGame();
+  //   for(var i = 0; i < 20; i ++) {
+  //     game.roll(1);
+  //   }
+  // });
 });

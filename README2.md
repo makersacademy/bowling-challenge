b7daf7dc60cf4d83e9f5575fd85530c98fde719e
Lorna Harwood <lornapeepwreckers@gmail.com>
2018-12-09 11:29:52 +0000

    create test for calculating running totals, refactored test for recording rolls for one frame and ensured rolls returned to 0 after each frame

--- README2.md
+++ README2.md
@@ -18,9 +18,9 @@ So I can keep the score of a game of bowling
 I would like to record the score for a whole game of ten frames
 ///
 
-Frame        1     2  
-Pins       [3,1] [2,8]
-Frame total  4     10
+Frame        1     2    3
+Pins        3,1   2,8  -,-
+Frame total  4     10   -
 Total: 14
 
 
--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -5,15 +5,8 @@ describe('ScoreCard', function() {
     scoreCard = new ScoreCard();
   });
 
-  describe('default roll value', function() {
-    it('initializes each roll to default to zero before each throw', function() {
-      expect(scoreCard.defaultRollValue).toEqual(0);
-    });
-  });
-
   describe('record first roll', function() {
     it('adds first roll to first frame', function() {
-      expect(scoreCard.rollOne).toEqual(0);
       scoreCard.addRoll(1);
       expect(scoreCard.rollOne).toEqual(1);
     });
@@ -21,10 +14,9 @@ describe('ScoreCard', function() {
 
   describe('record second roll', function() {
     it('adds second roll to first frame', function() {
-      expect(scoreCard.rollTwo).toEqual(0);
       scoreCard.addRoll(5);
       scoreCard.addRoll(3);
-      expect(scoreCard.rollTwo).toEqual(3);
+      expect(scoreCard.frames[0][1]).toEqual(3);
     });
   });
 
@@ -41,20 +33,23 @@ describe('ScoreCard', function() {
       scoreCard.addRoll(3)
       scoreCard.addRoll(6)
       expect(scoreCard.frameScore(1)).toEqual(9)
-
-
     });
 
-    // it('can calculate the running total', function() {
-    //
-    //
-    // })
+    it('can calculate the running total', function() {
+      scoreCard.addRoll(6)
+      scoreCard.addRoll(3)
+      expect(scoreCard.calculateTotal()).toEqual(9)
+      scoreCard.addRoll(1)
+      scoreCard.addRoll(4)
+      expect(scoreCard.calculateTotal()).toEqual(14)
+    });
   });
 
   // describe('prints the score card', function() {
   //   it('prints score card for the first 2 frames', function() {
-  //
-  //
+  //     scoreCard.addRoll(3)
+  //     scoreCard.addRoll(6)
+  //     expect(scoreCard.printScoreCard()).toEqual("Frame        1     2    3\nPins        3,1   2,8  -,-\nFrame total  4     10   -\nTotal: 14")
   //   })
   // })
 

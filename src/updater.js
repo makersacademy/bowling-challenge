13a0dcf393c2d409fe6133df931b98a736a179c4
willmcb <williamjamesmcbrien@gmail.com>
2018-12-09 00:52:08 +0000

    Refactor updater

--- src/updater.js
+++ src/updater.js
@@ -7,42 +7,27 @@ function Updater() {
 Updater.prototype.update = function(frames) {
     var that = this;
     frames.forEach(function(frame, index){
-       if (index == 8){
-            that.updateNinthFrame(that, frame, index, frames);
-        } else if (!(frame instanceof TenthFrame)) {
-            that.updateRegularFrame(that, frame, index, frames);
+        if (index !== 9) {
+            that.updateFrame(that, frame, index, frames);
         }
     });
 }
 
-Updater.prototype.updateRegularFrame = function(that, frame, index, frames){
+Updater.prototype.updateFrame = function(that, frame, index, frames){
     var next = frames[index + 1];
     var nextNext = frames[index + 2];
     if (frame.isStrike()) {
         if (next.isStrike()){
-          that.updateStrikeScoreInNextFrameIsStrike(frame, next, nextNext);
+            if ((next.getSecondScore() !== null)) {
+               that.updateStrike(frame, next);
+            } else {
+               that.updateStrikeScoreInNextFrameIsStrike(frame, next, nextNext);
+            }
         } else {
           that.updateStrike(frame, next);
         }
     } else if (frame.isSpare()) {
        that.updateSpare(frame, next);
-    }
-}
-
-Updater.prototype.updateNinthFrame = function(that, frame, index, frames){
-    var next = frames[index + 1];
-    if (frame.isStrike()) {
-        if (next.isStrike()){
-            if (next.getSecondScore() !== null) {
-                that.updateNinthFrameScore(frame, next);
-            }
-        } else {
-            if (next.getFirstScore() !== null) {
-                that.updateNinthFrameScore(frame, next);
-            }
-        }
-    } else if (frame.isSpare()) {
-        that.updateSpare(frame, next);
     }
 }
 
@@ -72,14 +57,6 @@ Updater.prototype.updateStrikeScoreInNextFrameIsStrike = function(frame, next, n
         frame.setFinalFrameScore(score);
     }
 }
-
-Updater.prototype.updateNinthFrameScore = function(frame, next){
-    var score = frame.getFirstScore() +
-                next.getFirstScore() +
-                next.getSecondScore();
-    frame.setFinalFrameScore(score);
-}
-
 
 if ( typeof module !== 'undefined' && module.hasOwnProperty('exports') )
 {

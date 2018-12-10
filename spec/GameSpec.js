2a8a0eb088d25b2f6fc76285412f10cfacace40c
Aga <a.dufrat@gmail.com>
2018-12-10 12:18:24 +0000

    Fix tests: include jQuery and update function name

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,14 +11,14 @@ describe("Game", function() {
 
   it("calculates points for the current frame after the 1st roll", function() {
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     expect(game.getFramePoints()).toEqual(5);
     expect(game.total()).toEqual(5);
   });
 
   it("calculates points for the current frame after the 2nd roll", function() {
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(3);
     // console.log("points (8)" + game.currentFramePoints);
     expect(game.getFramePoints()).toEqual(8);
@@ -26,46 +26,46 @@ describe("Game", function() {
 
   it("calculates points for a new frame", function() {
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(3);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(4);
     expect(game.getFramePoints()).toEqual(4);
   });
 
   it("adds bonus for a strike", function() {
     game.calculateFramePoints(10);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(2);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(2);
-    game.updateFrame();
+    game.updateFrames();
     expect(game.frameScores[1]).toEqual(14);
     expect(game.frameScores[2]).toEqual(4);
   });
 
   it("adds bonus for a spare", function() {
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(2);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(2);
-    game.updateFrame();
+    game.updateFrames();
     expect(game.frameScores[1]).toEqual(12);
     expect(game.frameScores[2]).toEqual(4);
   });
 
   it("calculates total score", function() {
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(5);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(2);
-    game.updateFrame();
+    game.updateFrames();
     game.calculateFramePoints(2);
-    game.updateFrame();
+    game.updateFrames();
     expect(game.total()).toEqual(16);
   });
 

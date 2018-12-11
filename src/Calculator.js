b95025177215fb3f861805ba41b3b7b532a2c9e0
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-12-11 12:51:17 +0000

    stats separating out aspects to scoring

--- src/Calculator.js
+++ src/Calculator.js
@@ -8,41 +8,15 @@ function Calculator() {
 
 Calculator.prototype.scoreGame = function() {
   this.eachScore = []
-  for (var i = 0; i < 8; i++) {
-    var frame = this.currentGame.frames[i]
-    var nextFrame = this.currentGame.frames[i+1]
-    var nexterFrame = this.currentGame.frames[i+2]
-    if(frame.firstRoll === undefined) {
-      return
-    } else if(frame.special !== 'strike' && frame.secondRoll === undefined) {
-      return
-    }
-    if(frame.special === 'strike') {
-      if(nextFrame.special === 'strike') {
-        if(nexterFrame.firstRoll === undefined) {
-          return
-        } else {
-          this.eachScore.push(20 + nexterFrame.firstRoll)
-        }
-      } else if(nextFrame.secondRoll === undefined) {
-        return
-      } else {
-        this.eachScore.push(10 + nextFrame.firstRoll + nextFrame.secondRoll)
-      }
-    } else if(frame.special === 'spare') {
-      if(nextFrame.firstRoll === undefined) {
-        return
-      } else {
-        this.eachScore.push(10 + nextFrame.firstRoll)
-      }
-    } else {
-      this.eachScore.push(frame.firstRoll + frame.secondRoll)
-    }
+  for(var i = 0; i < 10; i++) {
+    this.eachScore.push(this.frameScore(this.currentGame.frames[i], i))
   }
+}
 
-  if(this.currentGame.frames[8].special === 'strike') {
-    if(this.currentGame.frames[9].firstRoll === 10) {
-      if(this.currentGame.frames[9].secondRoll !== undefined)
-    }
-  }
+Calculator.prototype.frameScore = function(frame, index) {
+  if(index === 8) { return penultimateFrame() }
+  if(index === 9) { return ultimateFrame() }
+  if(frame.special === 'strike') { return strikeScore(index) }
+  if(frame.special === 'spare') { return spareScore(index) }
+  return (frame.firstRoll + frame.secondRoll)
 }

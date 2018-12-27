6c9393fcf6dfba5174a327ff4d4b90e71f324a2e
elly <elly.chan.mda@gmail.com>
2018-12-27 12:26:25 +0000

    start over challenge with tests

--- src/Game.js
+++ src/Game.js
@@ -1,78 +1,89 @@
 function Game() {
-  this.cur_frame = 0;
-  this.cur_roll = 0
+  this.cur_frame = 1;
+  this.cur_roll = 1
   this.frameRecord = [];
   this.gameRecord = [];
-  this.totalScore = 0;
+  this.totalScore = [];
   this.score;
-  this.pinsDown;
 }
 
 Game.prototype.knockDown = function(pins) {
-  this.pinsDown = 0;
-  this.updateFrame();
   this.frameRecord.push(pins);
-  if (this.isFrameEnd()) {
-    for (var i=0; i<this.frameRecord.length; i++) {
-      this.pinsDown += this.frameRecord[i];
-    }
-    this.gameRecord.push(this.frameRecord);
-    this.frameRecord = [];
+  if (this.isFrameEnd(this.cur_frame)) {
+    this.resetFrame()
   }
   this.cur_roll += 1
-  return this.pinsDown = pins; 
+  return pins; 
 };
 
-Game.prototype.updateFrame = function() {
-  if (!this.frameRecord[0] && this.frameRecord[0]!==0){
-    this.cur_frame++; 
-    this.cur_roll = 0;
-  };
+Game.prototype.resetFrame = function() {
+  this.gameRecord.push(this.frameRecord);
+  this.cur_frame++; 
+  this.cur_roll = 0;
+  this.frameRecord = [];
 };
 
 Game.prototype.frameScore = function(frameNo) {
   this.score = 0;
-  var index = frameNo - 1; // the index no of this frame in the gameRecord ary
+  var index = frameNo - 1;
   if (this.isStrike(this.gameRecord[index])) {this.strikeBonus(index);}
+  else if (this.isSpare(this.gameRecord[index])) {this.spareBonus(index);}
+  else { this.noBonus(index) }
+  if (this.score == 0) {return}
   else {
-    this.score += (this.gameRecord[(index)][0] + this.gameRecord[(index)][1]);
-    if (this.score < 10) {this.score}
-    else if (this.score === 10) {
-      this.score += this.gameRecord[index+1][0]
-    } 
+    this.totalScore.push(this.score)
+    return this.score
   }
-  return this.score;
 };
 
 Game.prototype.finalFrScore = function() {
   this.score = 0;
-  for (var i=0; i<this.gameRecord[9].length; i++) {
+  for (var i=0; this.gameRecord[9][i]; i++) {
     this.score += this.gameRecord[9][i]
   }
+  this.totalScore.push(this.score)
   return this.score;
 };
 
-Game.prototype.strikeBonus = function(index) {
-  var nextFrame = this.gameRecord[index+1];
-  var neNextFrame = this.gameRecord[index+2]
-  if (nextFrame == undefined){return}
-  (this.isStrike(nextFrame) && !nextFrame[1])? 
-  this.score += 20 + neNextFrame[0] : this.score += 10 + nextFrame[0] + nextFrame[1]
-};
-
 Game.prototype.accumScore = function(frameNo) {
-  if (frameNo===10) {return this.totalScore += this.finalFrScore()}
-  else {return this.totalScore += this.frameScore(frameNo)}
+  var frameAccum = 0;
+  if (!this.totalScore[frameNo - 1]) {return}
+  for (var i = 0; i < frameNo; i++) {
+    frameAccum += this.totalScore[i]
+  }
+  return frameAccum;
 };
 
-Game.prototype.isFrameEnd = function() {
-  if (this.cur_frame < 10) {
+Game.prototype.noBonus = function(index) {
+  if (!this.gameRecord[index]) {return}
+  this.score += (this.gameRecord[(index)][0] + this.gameRecord[(index)][1]);
+};
+
+Game.prototype.spareBonus = function(index) {
+  if (!this.gameRecord[index+1]) {return}
+  this.score += 10 + this.gameRecord[index+1][0]
+};
+
+Game.prototype.strikeBonus = function(index) {
+  if (!this.gameRecord[index+1]) {return}
+  var nextFrame = this.gameRecord[index+1];
+  this.isStrike(nextFrame)? this.doubleStrikeBonue(index) : this.score += 10 + nextFrame[0] + nextFrame[1]
+};
+
+Game.prototype.doubleStrikeBonue = function(index) {
+  if ( index == 8 ) {this.score += 20 + this.gameRecord[9][1]}
+  if (!this.gameRecord[index+2]) {return}
+  else { this.score += 20 + this.gameRecord[index+2][0] }
+};
+
+Game.prototype.isFrameEnd = function(frameNo) {
+  if (frameNo < 10) {
     return (this.frameRecord[0] === 10 || this.frameRecord.length >= 2)
   }
-  else if (this.cur_frame === 10 && (this.frameRecord[0] + this.frameRecord[1] < 10)) {
+  else if (frameNo === 10 && (this.frameRecord[0] + this.frameRecord[1] < 10)) {
     return (this.frameRecord.length >= 2)
   }
-  else if (this.cur_frame === 10){
+  else if (frameNo === 10){
     return (this.frameRecord.length >= 3)
   }
   else {throw new Error ("game ended")}
@@ -81,4 +92,9 @@ Game.prototype.isFrameEnd = function() {
 Game.prototype.isStrike = function(ary) {
   if (ary === undefined || ary[0] === undefined) {return false}
   else {return (ary[0] === 10)}
+}
+
+Game.prototype.isSpare = function(ary) {
+  if (ary === undefined || ary.length < 2) {return false}
+  return (ary[0] + ary[1] == 10)
 }
--- src/interface.js
+++ src/interface.js
@@ -4,49 +4,52 @@ $(document).ready(function () {
 
   for (let i = 0; i < 11; i += 1) {
     $('#score' + i).click(function () {
-      game.knockDown(i);
-      markInput();
-      console.log("haha");
+      markInput(i);
       frScore();
       finalFrScore();
-      console.log("helo");
       accumScore();
+      // disableButtons(i);
     })
   };
 
-  function markInput(){
+  function markInput(i){
     var cur_frame = game.cur_frame;
     var cur_roll = game.cur_roll;
-    $('#frame' + cur_frame + "_r" + cur_roll).text(game.pinsDown);
+    $('#frame' + cur_frame + "_r" + cur_roll).text(game.knockDown(i));
   }
 
   function frScore(){
     for (let i = 1; i < 10; i++) {
-      $('#frameScore' + i).text(game.frameScore(i));
-    }
+      if($.trim($('#frameScore' + i).html())=='') {
+        $('#frameScore' + i).text(game.frameScore(i));
+      }
+    } 
   }
 
   function finalFrScore() {
+    if (!game.gameRecord[9]) { return }
     $('#frameScore10').text(game.finalFrScore())
   }
 
   function accumScore(){
-    for (let i = 1; i < 11; i++) {
+    for (let i = 1; i <= 10; i++) {
       $('#totalScore' + i).text(game.accumScore(i));
     }
   }
+
+
+  // function disableButtons (num) {
+  //   var to_disable = 11 - num;
+  //   console.log(to_disable)
+  //   for (let i = to_disable; i <= 11; i++) {
+  //     $('#score' + i).hide();
+  //   }
+  // }
 
   // $("#hide").click(function(){
   //   var cur_roll = game.cur_roll;
   //   if (cur_roll == 1) {$("#score0").hide();}
   //   else {$("#score0").show();}
   // });
-
-  // function(){
-  //   var cur_roll = game.cur_roll;
-  //   if (cur_roll ==) {}
-  // }
-
-
 
 })

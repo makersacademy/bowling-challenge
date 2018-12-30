8a109675b42eac63eb37e2864f7b2882d85f207d
elly <elly.chan.mda@gmail.com>
2018-12-30 00:30:22 +0000

    add eslint

--- src/Game.js
+++ src/Game.js
@@ -1,100 +1,92 @@
-function Game() {
-  this.cur_frame = 1;
+function Game () {
+  this.cur_frame = 1
   this.cur_roll = 1
-  this.frameRecord = [];
-  this.gameRecord = [];
-  this.totalScore = [];
-  this.score;
+  this.frameRecord = []
+  this.gameRecord = []
+  this.totalScore = []
+  this.score
 }
 
-Game.prototype.knockDown = function(pins) {
-  this.frameRecord.push(pins);
+Game.prototype.knockDown = function (pins) {
+  this.frameRecord.push(pins)
   if (this.isFrameEnd(this.cur_frame)) {
     this.resetFrame()
   }
   this.cur_roll += 1
-  return pins; 
-};
+  return pins
+}
 
-Game.prototype.resetFrame = function() {
-  this.gameRecord.push(this.frameRecord);
-  this.cur_frame++; 
-  this.cur_roll = 0;
-  this.frameRecord = [];
-};
+Game.prototype.resetFrame = function () {
+  this.gameRecord.push(this.frameRecord)
+  this.cur_frame++
+  this.cur_roll = 0
+  this.frameRecord = []
+}
 
-Game.prototype.frameScore = function(frameNo) {
-  this.score = 0;
-  var index = frameNo - 1;
-  if (this.isStrike(this.gameRecord[index])) {this.strikeBonus(index);}
-  else if (this.isSpare(this.gameRecord[index])) {this.spareBonus(index);}
-  else { this.noBonus(index) }
-  if (this.score == 0) {return}
-  else {
+Game.prototype.frameScore = function (frameNo) {
+  this.score = 0
+  var index = frameNo - 1
+  if (this.isStrike(this.gameRecord[index])) { this.strikeBonus(index) } else if (this.isSpare(this.gameRecord[index])) { this.spareBonus(index) } else { this.noBonus(index) }
+  if (this.score === 0) {} else {
     this.totalScore.push(this.score)
     return this.score
   }
-};
+}
 
-Game.prototype.finalFrScore = function() {
-  this.score = 0;
-  for (var i=0; this.gameRecord[9][i]; i++) {
+Game.prototype.finalFrScore = function () {
+  this.score = 0
+  for (var i = 0; this.gameRecord[9][i]; i++) {
     this.score += this.gameRecord[9][i]
   }
   this.totalScore.push(this.score)
-  return this.score;
-};
+  return this.score
+}
 
-Game.prototype.accumScore = function(frameNo) {
-  var frameAccum = 0;
-  if (!this.totalScore[frameNo - 1]) {return}
+Game.prototype.accumScore = function (frameNo) {
+  var frameAccum = 0
+  if (!this.totalScore[frameNo - 1]) { return }
   for (var i = 0; i < frameNo; i++) {
     frameAccum += this.totalScore[i]
   }
-  return frameAccum;
-};
+  return frameAccum
+}
 
-Game.prototype.noBonus = function(index) {
-  if (!this.gameRecord[index]) {return}
-  this.score += (this.gameRecord[(index)][0] + this.gameRecord[(index)][1]);
-};
+Game.prototype.noBonus = function (index) {
+  if (!this.gameRecord[index]) { return }
+  this.score += (this.gameRecord[(index)][0] + this.gameRecord[(index)][1])
+}
 
-Game.prototype.spareBonus = function(index) {
-  if (!this.gameRecord[index+1]) {return}
-  this.score += 10 + this.gameRecord[index+1][0]
-};
+Game.prototype.spareBonus = function (index) {
+  if (!this.gameRecord[index + 1]) { return }
+  this.score += 10 + this.gameRecord[index + 1][0]
+}
 
-Game.prototype.strikeBonus = function(index) {
-  if (!this.gameRecord[index+1]) {return}
-  var nextFr = this.gameRecord[index+1];
-  this.isStrike(nextFr)? this.doubleStrikeBonue(index) : this.score += 10 + nextFr[0] + nextFr[1]
-};
+Game.prototype.strikeBonus = function (index) {
+  if (!this.gameRecord[index + 1]) { return }
+  var nextFr = this.gameRecord[index + 1]
+  this.isStrike(nextFr) ? this.doubleStrikeBonue(index) : this.score += 10 + nextFr[0] + nextFr[1]
+}
 
-Game.prototype.doubleStrikeBonue = function(index) {
-  if ( index == 8 ) {this.score += 20 + this.gameRecord[9][1]}
-  if (!this.gameRecord[index+2]) {return}
-  else { this.score += 20 + this.gameRecord[index+2][0] }
-};
+Game.prototype.doubleStrikeBonue = function (index) {
+  if (index === 8) { this.score += 20 + this.gameRecord[9][1] }
+  if (!this.gameRecord[index + 2]) {} else { this.score += 20 + this.gameRecord[index + 2][0] }
+}
 
-Game.prototype.isFrameEnd = function(frameNo) {
+Game.prototype.isFrameEnd = function (frameNo) {
   if (frameNo < 10) {
     return (this.frameRecord[0] === 10 || this.frameRecord.length >= 2)
-  }
-  else if (frameNo === 10 && (this.frameRecord[0] + this.frameRecord[1] < 10)) {
+  } else if (frameNo === 10 && (this.frameRecord[0] + this.frameRecord[1] < 10)) {
     return (this.frameRecord.length >= 2)
-  }
-  else if (frameNo === 10){
+  } else if (frameNo === 10) {
     return (this.frameRecord.length >= 3)
-  }
-  else {throw new Error ("game ended")}
+  } else { throw new Error('game ended') }
 }
 
-Game.prototype.isStrike = function(ary) {
-  if (ary === undefined || ary[0] === undefined) {return false}
-  else {return (ary[0] === 10)}
+Game.prototype.isStrike = function (ary) {
+  if (ary === undefined || ary[0] === undefined) { return false } else { return (ary[0] === 10) }
 }
 
-Game.prototype.isSpare = function(ary) {
-  if (ary === undefined || ary.length < 2) {return false}
-  return (ary[0] + ary[1] == 10)
+Game.prototype.isSpare = function (ary) {
+  if (ary === undefined || ary.length < 2) { return false }
+  return (ary[0] + ary[1] === 10)
 }
--- src/interface.js
+++ src/interface.js
@@ -1,75 +1,74 @@
 $(document).ready(function () {
-
-  var game = new Game();
+  var game = new Game()
 
   for (let i = 0; i < 11; i += 1) {
     $('#score' + i).click(function () {
-      console.log(game);
-      markInput(i);
-      frScore();
-      finalFrScore();
-      accumScore();
-      disableButtons(i);
-      gameOver();
+      console.log(game)
+      markInput(i)
+      frScore()
+      finalFrScore()
+      accumScore()
+      disableButtons(i)
+      gameOver()
     })
   };
 
-  function markInput(i){
-    var cur_frame = game.cur_frame;
-    var cur_roll = game.cur_roll;
-    $('#frame' + cur_frame + "_r" + cur_roll).text(game.knockDown(i));
+  function markInput (i) {
+    var cur_frame = game.cur_frame
+    var cur_roll = game.cur_roll
+    $('#frame' + cur_frame + '_r' + cur_roll).text(game.knockDown(i))
   }
 
-  function frScore(){
+  function frScore () {
     for (let i = 1; i < 10; i++) {
-      if($.trim($('#frameScore' + i).html())=='') {
-        $('#frameScore' + i).text(game.frameScore(i));
+      if ($.trim($('#frameScore' + i).html()) == '') {
+        $('#frameScore' + i).text(game.frameScore(i))
       }
-    } 
+    }
   }
 
-  function finalFrScore() {
+  function finalFrScore () {
     if (!game.gameRecord[9]) { return }
     $('#frameScore10').text(game.finalFrScore())
   }
 
-  function accumScore(){
+  function accumScore () {
     for (let i = 1; i <= 10; i++) {
-      $('#totalScore' + i).text(game.accumScore(i));
+      $('#totalScore' + i).text(game.accumScore(i))
     }
   }
 
   function disableButtons (num) {
     if (game.cur_roll == 2 && num !== 10) {
-      var to_disable = 11 - num;
+      var to_disable = 11 - num
       for (let i = to_disable; i <= 11; i++) {
-        $('#score' + i).hide();
+        $('#score' + i).hide()
       }
     } else {
       for (var i = 0; i < 11; i++) {
-        $('#score' + i).show();
+        $('#score' + i).show()
       }
     }
   }
 
-  function gameOver() {
+  function gameOver () {
     if (game.gameRecord[9]) {
       $('#gameOver').text('Game Over!')
-    } 
+    }
   }
 
-  $('#resetGame').click(function(){
-    resetGame();
+  $('#resetGame').click(function () {
+    resetGame()
     $('#frame_table td').empty()
-    console.log("game restart");
+    console.log('game restart')
   })
 
-  function resetGame() {
-    game.cur_frame = 1;
+  function resetGame () {
+    game.cur_frame = 1
     game.cur_roll = 1
-    game.frameRecord = [];
-    game.gameRecord = [];
-    game.totalScore = [];
-    $('#gameOver').hide();
+    game.frameRecord = []
+    game.gameRecord = []
+    game.totalScore = []
+    $('#gameOver').hide()
   }
 })

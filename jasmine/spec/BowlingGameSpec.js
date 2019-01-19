49c471b47f7e050c305126f744b260701b919e84
Simon Cass <scass1991@gmail.com>
2019-01-19 11:16:11 +0000

    Test couldnt recognise BowlingGame, basic test added

--- jasmine/spec/BowlingGameSpec.js
+++ jasmine/spec/BowlingGameSpec.js
@@ -1,12 +1,9 @@
 "use strict"
 
-describe ("BowlingGame", function () {
-  describe("Guttergame", function() {
-    it("Player hits the gutter twice", function() {
-      var bowlingGame = new BowlingGame();
-      bowlingGame.rollOne(0)
-      bowlingGame.rollTwo(0)
-      expect(bowlingGame.score).toEqual(0)
-    })
+describe ("BowlingGame", function() {
+  describe ("Initialisation", function () {
+  it ("creates a bowling game", function() {
+    var game = new BowlingGame();
   })
+})
 })
--- jasmine/src/BowlingGame.js
+++ jasmine/src/BowlingGame.js
@@ -1,4 +1,5 @@
 "use strict"
 
-function BowlingGame() {
+class BowlingGame {
+
 }

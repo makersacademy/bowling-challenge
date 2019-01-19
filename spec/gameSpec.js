10508c2be6e99a0c1f491ac404541065d1cd1008
Adam Lodowski <adamlodowski@gmail.com>
2019-01-19 18:51:32 +0000

    Game object can create and store Frame objects in array

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,13 +1,22 @@
 describe("Game", function() {
   var game;
-  var gutterFrame;
+  var newFrame;
 
   beforeEach(function() {
     game = new Game();
-    gutterFrame = new Frame(0, 0);
   });
 
   it("Can create a game class", function() {
     expect(game instanceof Game).toBe(true);
   });
+
+  it("Can create a new frame object", function() {
+    newFrame = game.addFrame(0,0)
+    expect(newFrame instanceof Frame).toBe(true)
+  })
+
+  it ("Can return the array of frames played", function() {
+    game.frames = ["cat"]
+    expect(game.getFrames()).toEqual(["cat"])
+  })
 })
--- src/game.js
+++ src/game.js
@@ -1,5 +1,15 @@
 class Game {
   constructor() {
-    var frames = []
+    this.frames = []
+  }
+
+  getFrames() {
+    return this.frames
+  }
+
+  addFrame(roll1, roll2) {
+    let frame = new Frame(roll1, roll2)
+    this.frames.push(frame)
+    return frame
   }
 }

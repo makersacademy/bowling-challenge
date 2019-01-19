588b6d5ad34181500480abb15f2d335d4487045b
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-01-19 14:20:25 +0000

    Gutter game feature tests written

---
+++ spec_source_lib/spec/ScoreCardSpec.js
@@ -0,0 +1,60 @@
+describe("ScoreCard", function() {
+  var score_card;
+  
+
+  beforeEach(function() {
+    score_card = new ScoreCard();
+  });
+
+  it("should be complete after the 20 throws of a gutter game", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);  
+  });
+
+  it("should return a score of 0 for a gutter game", function() {
+    player.play(song);
+    expect(player.currentlyPlayingSong).toEqual(song);    
+  });
+
+
+//   describe("when song has been paused", function() {
+//     beforeEach(function() {
+//       player.play(song);
+//       player.pause();
+//     });
+
+//     it("should indicate that the song is currently paused", function() {
+//       expect(player.isPlaying).toBeFalsy();
+
+//       // demonstrates use of 'not' with a custom matcher
+//       expect(player).not.toBePlaying(song);
+//     });
+
+//     it("should be possible to resume", function() {
+//       player.resume();
+//       expect(player.isPlaying).toBeTruthy();
+//       expect(player.currentlyPlayingSong).toEqual(song);
+//     });
+//   });
+
+//   // demonstrates use of spies to intercept and test method calls
+//   it("tells the current song if the user has made it a favorite", function() {
+//     spyOn(song, 'persistFavoriteStatus');
+
+//     player.play(song);
+//     player.makeFavorite();
+
+//     expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
+//   });
+
+//   //demonstrates use of expected exceptions
+//   describe("#resume", function() {
+//     it("should throw an exception if song is already playing", function() {
+//       player.play(song);
+
+//       expect(function() {
+//         player.resume();
+//       }).toThrowError("song is already playing");
+//     });
+//   });
+// });

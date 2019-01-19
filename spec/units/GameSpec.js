6589bdbf1fcefcd2f389668a03839ccf595cb852
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-19 18:03:45 +0000

    current roll added to after game.roll

--- spec/units/GameSpec.js
+++ spec/units/GameSpec.js
@@ -1,11 +1,17 @@
 describe('Game', function() {
   let game;
+  let currentRoll;
 
   beforeEach(function() {
     game = new Game;
   });
 
-  it('is unfinished at the bgeinning', function() {
+  it('is unfinished at the beginning', function() {
     expect(game.isOver).toBe(false);
+  });
+
+  it('game.roll cahnges the current roll', function() {
+    game.roll(0);
+    expect(game.currentRoll).toBe(2)
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,10 @@
 class Game {
   constructor() {
     this.isOver = false;
+    this.currentRoll = 1
+  }
+
+  roll(pinsDown) {
+    this.currentRoll++
   }
 }

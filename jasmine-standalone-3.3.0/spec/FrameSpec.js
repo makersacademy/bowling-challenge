c1a1ea937b6e34731b0770697e29eb544dfd5381
beatricecourage <bea_courage@hotmail.co.uk>
2019-01-19 16:27:31 +0000

    Test and method for oneframeScore

--- jasmine-standalone-3.3.0/spec/FrameSpec.js
+++ jasmine-standalone-3.3.0/spec/FrameSpec.js
@@ -1,7 +1,18 @@
 describe("Frame", function() {
-  var frame;
-})
 
-beforeEach(function(){
-  frame = new Frame();
+  var frame1;
+  var frame2;
+  var frame3;
+
+
+  function makeFrame(array) {
+     return new Frame(array)
+   }
+
+  it("Get the score from one frame", function() {
+    frame1 = makeFrame([4,2]);
+    expect(frame1.oneframescore().toEqual(6);
+  });
+
+
 });
--- jasmine-standalone-3.3.0/src/Frame.js
+++ jasmine-standalone-3.3.0/src/Frame.js
@@ -1,2 +1,14 @@
 function Frame () {
 }
+
+var Frame = function(rolls) {
+  this.rolls = rolls
+  this.maxpins = 10
+}
+
+
+Frame.prototype.oneframeScore = function() {
+  return this.rolls.reduce(function(score, score) {
+    return score + score;
+  })
+}

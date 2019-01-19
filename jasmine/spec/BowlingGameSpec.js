85cc08721d3ff83880d1297b0e5f7e298159c3cf
Simon Cass <scass1991@gmail.com>
2019-01-19 11:25:09 +0000

    Failing feature test added for gutter game

--- jasmine/spec/BowlingGameSpec.js
+++ jasmine/spec/BowlingGameSpec.js
@@ -1,9 +1,21 @@
 "use strict"
 
 describe ("BowlingGame", function() {
+
   describe ("Initialisation", function () {
   it ("creates a bowling game", function() {
     var game = new BowlingGame();
   })
 })
+
+  describe("Gutter game", function() {
+    it("can roll a gutter game", function() {
+      var game = new BowlingGame();
+      for (var i = 0; i < 20; i++) {
+        game.roll(0);
+      }
+      expect(game.score()).toBe(0);
+    })
+  })
+
 })

9c7a61211e2a4acbd786daf592c7b8a659ad9abd
CKKH <chriskkhassan@gmail.com>
2019-01-19 16:55:34 +0000

    Feature Test 2 failing: gutter frame score recorded in scoreCard - resolved linting offenses

---
+++ spec/features/ScorecardFeatureSpec.js
@@ -0,0 +1,14 @@
+/* global describe, it, expect, beforeEach */
+'use strict'
+
+describe('Scorecard', () => {
+  describe('#gutter frame score recorded in scoreCard', () => {
+    it('player misses the pins with 2 roles', () => {
+      var scorecard = new Scorecard();
+      let frame = new Frame();
+      frame.roll(0)
+      frame.roll(0)
+      let frameScore = frame.calculateFrameScore()
+    })
+  })
+})

b4d8e321db1778e8082e6fd8a01115f9a1ad2a6b
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-19 14:53:29 +0000

    Scoresheet is not complete by default

--- spec/unit/ScoresheetSpec.js
+++ spec/unit/ScoresheetSpec.js
@@ -1,1 +1,13 @@
 'use strict'
+
+describe ('Scoresheet', function () {
+  let scoresheet
+
+  beforeEach (function () {
+    scoresheet = new Scoresheet
+  })
+
+  it('is not complete by default', function () {
+    expect(scoresheet.isComplete).toBeFalsy()
+  })
+})
--- src/Scoresheet.js
+++ src/Scoresheet.js
@@ -1,1 +1,7 @@
 'use strict'
+
+class Scoresheet {
+  constructor () {
+    this.isComplete = false
+  }
+}

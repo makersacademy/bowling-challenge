adb3fc3ef7dbd75f414b4204a0639c7791df40be
CKKH <chriskkhassan@gmail.com>
2019-01-19 09:57:22 +0000

    Test refactor: removed redundant test & moved .roll test to describe block

--- spec/units/FrameSpec.js
+++ spec/units/FrameSpec.js
@@ -3,13 +3,10 @@
 'use strict'
 
 describe('Frame', () => {
-  it('instance of Frame can be stored as a variable', () => {
-    var frame = new Frame()
-    expect(frame instanceof Frame).toBe(true)
-  })
-
-  it('responds to .roll()', () => {
-    var frame = new Frame()
-    expect(frame.roll).toBeDefined()
+  describe('#roll()', () => {
+    it('responds to .roll()', () => {
+      var frame = new Frame()
+      expect(frame.roll).toBeDefined()
+    })
   })
 })

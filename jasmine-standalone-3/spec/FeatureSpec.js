304283dd2136815eda0632705d880f9a7256a4bf
Alex <alikhanialex@gmail.com>
2019-01-19 17:52:42 +0000

    third test passing for array to contain scores of each frame

--- jasmine-standalone-3/spec/FeatureSpec.js
+++ jasmine-standalone-3/spec/FeatureSpec.js
@@ -11,9 +11,15 @@ describe('BowlingGame', function () {
   expect(play.score(0)).toEqual(0)
 });
 
-  it('should display the total score after 1 frame', function () {
+  it('should display the total score of 1 frame', function () {
     play.score(9);
     expect(play.total).toContain(9);
+});
+
+it('should contain the scores of multiple frames', function () {
+  play.score(9);
+  play.score(5)
+  expect(play.total).toContain(9, 5);
 });
 
 });

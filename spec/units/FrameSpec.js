777f46418403cfc6cc9b1e6a554e9c7795beba32
CKKH <chriskkhassan@gmail.com>
2019-01-19 09:16:17 +0000

    Unit test 1 passing: Instances of Frame can be stored as a variable - renamed src/feature .js files from Scorecard to Frame

---
+++ spec/units/FrameSpec.js
@@ -0,0 +1,10 @@
+/* global describe, it, expect, beforeEach */
+
+'use strict'
+
+describe('Frame', () => {
+  it('instance of Frame can be stored as a variable', () => {
+    var frame = new Frame()
+    expect(frame instanceof Frame).toBe(true)
+  })
+})

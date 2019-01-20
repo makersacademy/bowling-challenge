1a835781cc5820904cdc97202cabd1d26c0c2e99
OSSahota <osstechunlimited@gmail.com>
2019-01-20 19:12:43 +0000

    Player rolls all ones. Currently a failing (red) test.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,4 +8,12 @@ describe('Game', ()=> {
     expect(game.score()).toEqual(0);
   });
 
+  it('player rolls all ones', ()=> {
+    var game = new Game();
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20);
+  });
+
 }); // end of describe 'Game'

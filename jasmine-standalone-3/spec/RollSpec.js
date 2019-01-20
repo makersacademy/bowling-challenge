47500a13fc4d01dc367ce8d3007730bbaaf3b0e4
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-01-20 21:02:23 +0000

    Added first roll test

---
+++ jasmine-standalone-3/spec/RollSpec.js
@@ -0,0 +1,12 @@
+describe('Roll', function() {
+
+  var roll;
+
+  beforeEach(function() {
+    roll = new Roll(6);
+  });
+
+  it('returns a roll', function() {
+      expect(roll.rollvalue).toBe(6);
+    });
+});

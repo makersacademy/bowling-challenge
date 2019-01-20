f2cfcd311a39c055ae0faae76ce7728c081698e2
Jacques <jacques.laffy@gmail.com>
2019-01-20 15:23:47 +0000

    New feature for strikes

---
+++ spec/features/StrikeSpec.js
@@ -0,0 +1,18 @@
+describe ('Strikes', function () {
+  var scorecard;
+
+  beforeEach (function () {
+    scorecard = new ScoreCard();
+  });
+
+  it('If a user rolls 10 in first roll of frame, end of frame, next frame num added to score', function () {
+    scorecard.roll(10);
+    scorecard.roll(8);
+    scorecard.roll(1);
+
+    expect(scorecard.isComplete()).toBe(false)
+    expect(scorecard.score).toEqual(28)
+    expect(scorecard.frame).toEqual(3)
+    expect(scorecard.rollCount).toEqual(1)
+  });
+});

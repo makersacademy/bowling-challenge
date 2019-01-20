9b78d6d1447fd70e6cc40a9f17790843b337bff9
Emanuele <emanuelegorga@gmail.com>
2019-01-20 16:41:00 +0000

    First failing test

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,3 +1,12 @@
 describe  ('Game', function() {
   var bowling;
+
+  beforeEach(function() {
+    bowling = new Game();
+  })
+
+  it('can score a gutter game', function() {
+    for(let i = 0; i < 20; i++) { bowling.roll(0) }
+    expect(bowling.getScore()).toEqual(0)
+  })
 })

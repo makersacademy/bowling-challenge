fca3c9ff92679a487033907a74bb02b726e23c66
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-01-20 22:11:00 +0000

    First frame test

---
+++ jasmine-standalone-3/spec/FrameSpec.js
@@ -0,0 +1,13 @@
+describe('Frame', function() {
+
+  var frame1;
+
+  beforeEach(function() {
+    frame1 = new Frame(1);
+  });
+
+  it('returns frame number', function() {
+    expect(frame1.framenumber).toBe(1);
+  });
+
+});

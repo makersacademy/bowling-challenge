1222a7d777012f8b816c9c9a011fa8515c5d97fc
Jacques <jacques.laffy@gmail.com>
2019-01-20 18:03:08 +0000

    Added feature and unit for spares

---
+++ spec/features/SpareSpec.js
@@ -0,0 +1,18 @@
+describe ('Spares', function () {
+  var scorecard;
+
+  beforeEach (function () {
+    scorecard = new ScoreCard();
+  });
+
+  it('If a user rolls 10 over a frame, next frame roll 1 is added to score', function () {
+    scorecard.roll(7);
+    scorecard.roll(3);
+    scorecard.roll(2);
+    scorecard.roll(2);
+
+    expect(scorecard.isComplete()).toBe(false)
+    expect(scorecard.total).toEqual(16)
+    expect(scorecard.frameCount).toEqual(3)
+  });
+});

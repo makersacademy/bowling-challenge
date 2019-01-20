60991b3aaebb915bd8fc32dc04ccb1df0f4b01a0
OSSahota <osstechunlimited@gmail.com>
2019-01-20 22:33:59 +0000

    Refactored tests and code to accommodate frames. Few more tests for frames added. Need further tests and refactoring around frames.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -17,11 +17,25 @@ describe('Game', ()=> {
     for (var i = 0; i < 20; i++) {
       game.roll(1);
     }
-    expect(game.score(20)).toEqual(20);
+    expect(game.score(0)).toEqual(20);
   });
 
   it('player starts the game from frame 1', ()=>{
     expect(game.frame()).toEqual(1);
+  });
+
+  it('player is on frame 2', ()=>{
+    for (var i = 0; i < 2; i++) {
+      game.roll(1);
+    }
+    expect(game.frame()).toEqual(2);
+  });
+
+  it('player is on frame 10', ()=>{
+    for (var i = 0; i < 18; i++) {
+      game.roll(1);
+    }
+    expect(game.frame()).toEqual(10);
   });
 
 }); // end of describe 'Game'
--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,43 @@
-class Game{
+class Game {
 
-  roll(pinsDown){
+  constructor() {
+    this.rolls = [];
+    // this.rollNumber = 0;
+    this.frameNumber = 0;
+    this.totalScore = 0;
+    this.frame_Score = [];
+    this.rollNumber = 0;
+  };
+
+  roll(pinsDown) {
+    this.rollNumber++;
+    // console.log("rollNumber at the start");
+    // console.log(this.rollNumber);
+    if (this.rollNumber === 1) {
+      this.frame();
+      // this.rollNumber++;
+    }
+    else if (this.rollNumber > 1) {
+      this.rollNumber = 0;
+    };
+    // console.log("rollNumber at the end");
+    // console.log(this.rollNumber);
     this.score(pinsDown);
   };
 
-  score(pinsDown){
-    var totalScore = 0;
-    totalScore += pinsDown;
-    return totalScore;
+  score(pinsDown) {
+    // var totalScore = 0;
+    // totalScore += pinsDown;
+    // return totalScore;
+    this.totalScore += pinsDown;
+    return this.totalScore;
   };
 
-  frame(){
-    return 1;
+  frame() {
+    this.frameNumber++;
+    console.log("frameNumber");
+    console.log(this.frameNumber);
+    return this.frameNumber;
   };
 
 }; // end of class

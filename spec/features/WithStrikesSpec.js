20345b43529b3c76efc6e6448a53839172703a63
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-20 15:48:45 +0000

    Pass feature test for game with non consecutive strikes

--- spec/features/WithStrikesSpec.js
+++ spec/features/WithStrikesSpec.js
@@ -17,12 +17,24 @@ describe('With strikes', function () {
     expect(scoresheet.finalScore).toEqual(28)
   })
 
-  it('A user rolls a strike then a (2, 7) and the current score is 28', function () {
+  it('A game with non consecutive strikes, no spares, the final score is 118', function () {
     scoresheet.roll(10)
-
+    scoresheet.roll(2)
+    scoresheet.roll(7)
+    scoresheet.roll(10)
     scoresheet.roll(3)
+    scoresheet.roll(5)
+    scoresheet.roll(10)
+    scoresheet.roll(2)
     scoresheet.roll(4)
+    scoresheet.roll(10)
+    scoresheet.roll(4)
+    scoresheet.roll(1)
+    scoresheet.roll(10)
+    scoresheet.roll(4)
+    scoresheet.roll(2)
 
-    expect(scoresheet.finalScore).toEqual(24)
+    expect(scoresheet.isComplete).toBeTruthy()
+    expect(scoresheet.finalScore).toEqual(118)
   })
 })

4880acad0e8014dff1c63a5cfeebfde9f7a84e62
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-20 18:02:54 +0000

    Failing test for game of 20

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -16,4 +16,11 @@ describe('Game Scorecard', function() {
     }
     expect(game.score()).toBe(0)
   })
+
+  it('can roll a game of ones', function() {
+    for(var i = 0; i < 20; i++) {
+      game.roll(1)
+    }
+    expect(game.score()).toBe(20)
+  })
 })

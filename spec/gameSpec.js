c8c496b2ffbb6176c9278c7365c8b92702305cee
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-20 17:49:29 +0000

    Failing test for gutter game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,4 +9,11 @@ describe('Game Scorecard', function() {
   it('checks that a game is created', function() {
     expect(game instanceof Game).toBe(true)
   })
+
+  it('rolls a gutter game', function() {
+    for(var i = 0; i < 20; i++) {
+      game.roll(0)
+    }
+    expect(game.score()).toBe(0)
+  })
 })

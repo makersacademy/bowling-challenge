0f9ed29d70d2e37f4162c2794c129798957c93e5
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-01-20 23:53:20 +0000

    Test passing for spare roll"

--- src/game.js
+++ src/game.js
@@ -9,8 +9,15 @@ class Game {
 
   score() {
     var total = 0
-    for(var i = 0; i < 20; i++) {
-      total += this.rolls[i]
+    var rollNumber = 0
+
+    for(var frameNumber = 0; frameNumber < 10; frameNumber++) {
+      if (this.rolls[rollNumber] + this.rolls[rollNumber + 1] == 10) {
+        total += this.rolls[rollNumber] + this.rolls[rollNumber + 1] + this.rolls[rollNumber + 2]
+      } else {
+      total += this.rolls[rollNumber] + this.rolls[rollNumber + 1]
+    }
+      rollNumber += 2
     }
     return total
   }

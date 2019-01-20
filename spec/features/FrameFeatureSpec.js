99d5f63bbdfd69f81d10f56272572e468cc3d4f7
CKKH <chriskkhassan@gmail.com>
2019-01-20 11:55:19 +0000

    Feature test 3 failing: maximum rolls per frame is 3

--- spec/features/FrameFeatureSpec.js
+++ spec/features/FrameFeatureSpec.js
@@ -9,5 +9,13 @@ describe('Frame', () => {
       frame.roll(0)
       expect(frame._score).toEqual([0, 0])
     })
+
+    it('maximum rolls per frame is 3', () => {
+      var frame = new Frame();
+      frame.roll(0)
+      frame.roll(0)
+      var error = 'Cannot record 2 rolls: maximum rolls per frame is 2'
+      expect(function() { frame.roll(0) }).toThrow(new Error(error))
+    })
   })
 })

40292a92333a10494f054142c041b46901903a62
Tom Russell <TR115251@hotmail.co.uk>
2019-01-20 17:49:33 +0000

    help

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,13 +1,42 @@
-describe('Setup Tests', () => {
+describe("Setup Tests", () => {
 
-// let anything;
+  // let anything;
 
-//beforeEach() => {
-// anything = "something";
-// }
+  //beforeEach() => {
+  // anything = "something";
+  // }
 
-  it('runs first spec test', () => {
-    expect(true).toBe(true)
-  })
+  it("runs first spec test", () => {
+    expect(true).toBe(true);
+  });
 
-})
+});
+
+describe("Feature Tests", () => {
+
+  var scorecard;
+
+  beforeEach(() => {
+    scorecard = new Scorecard();
+  });
+
+  it("gutter game", () => {
+    scorecard.roll(0).repeat(10);
+    expect(scorecard.gameComplete).toBe(true);
+    expect(scorecard.score).toBe(0);
+  });
+
+});
+describe("Unit Tests", () => {
+
+  // let anything;
+
+  //beforeEach() => {
+  // anything = "something";
+  // }
+
+  it("runs first spec test", () => {
+    expect(true).toBe(true);
+  });
+
+});
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,9 +1,17 @@
-function showMessage() {
-  const message = "testing esLint and jasmine";
-  for ( let i = 0; i < 5; i++ ) {
-    // eslint-disable-next-line no-console
-    console.log(message);
+// function showMessage() {
+//   const message = "testing esLint and jasmine";
+//   for ( let i = 0; i < 5; i++ ) {
+//     // eslint-disable-next-line no-console
+//     console.log(message);
+//   }
+// }
+//
+// showMessage();
+class Scorecard {
+  constructor() {
+    this._rolls = [];
+  }
+  function roll(pins) {
+    this._rolls.push(pins)
   }
 }
-
-showMessage();

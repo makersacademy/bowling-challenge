51da21d095172c05cb71e113b849d0d50313f4cb
Emanuele <emanuelegorga@gmail.com>
2019-01-20 17:16:22 +0000

    Passing test: player can get bonus points for strike

--- src/game.js
+++ src/game.js
@@ -11,12 +11,16 @@ class Game {
     let score = 0;
     let roll = 0;
     for(let frames = 0; frames < 10; frames++) {
-      if (this.rolls[roll] + this.rolls[roll+1] === 10) {
+      if (this.rolls[roll] === 10) {
         score += this.rolls[roll] + this.rolls[roll+1] + this.rolls[roll+2]
+        roll += 1;
+      } else if (this.rolls[roll] + this.rolls[roll+1] === 10) {
+        score += this.rolls[roll] + this.rolls[roll+1] + this.rolls[roll+2]
+        roll += 2;
       } else {
         score += this.rolls[roll] + this.rolls[roll+1]
+        roll += 2
       }
-      roll += 2
     }
     return score;
   }

51c2a4f48d0f5edb0994753814cca82e950f5c44
celine kaslin <celine.kaslin@gmail.com>
2019-01-20 23:19:22 +0000

    previous test re-done, test failing for gutter game

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -6,25 +6,10 @@ describe ('ScoreCard', function(){
   });
 
   describe ("Gutter Game", function(){
-    it('the player should have a complete score card because he played his 10 frames', function(){
-      scoreCard.frame(10);
-      expect(scoreCard.iscomplete()).toBeTruthy ();
-    });
-    it ('the player should have enter the number of pins he have knocked of his 1st roll of his last frame. In our case it will be roll 1 (frame10), pins 0', function(){
-      expect(scoreCard.roll_1(0)).toHaveBeenCalled();
-    });
-    it ('the player should have enter the number of pins he have knocked of his 2nd roll of his last frame. In our case it will be roll 2 (frame10), pins 0', function(){
-      expect(scoreCard.roll_2(0)).toHaveBeenCalled();
-    });
-
-    it('the player should have a score of 0', function(){
-      scoreCard.frame(10);
-      scoreCard.roll_1(0);
-      scoreCard.roll_2(0);
-      expect(scoreCard.score()).toEqual (0);
-    });
-
+    it('the player should have a score of 0 at the end of a game of 20 frame where they did not knocked any pins', function(){
+      for (let s = 0; s < 20; s++){
+        scoreCard.roll(0)
+      };
+      expect(scoreCard.score()).toEqual(0);
   });
-
-
 });

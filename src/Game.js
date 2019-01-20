e6bcc026962ad153381cd90a93a68f04cb86692d
OSSahota <osstechunlimited@gmail.com>
2019-01-20 23:47:32 +0000

    Scores for each roll/frame are being captured. All current tets are passing. Still need to implement strikes and spares.

--- src/Game.js
+++ src/Game.js
@@ -1,42 +1,46 @@
 class Game {
 
   constructor() {
-    this.rolls = [];
-    // this.rollNumber = 0;
+    this.allFramesScore = [];
     this.frameNumber = 0;
-    this.totalScore = 0;
-    this.frame_Score = [];
+    this.frameScore = [];
     this.rollNumber = 0;
+    this.totalScore = 0;
   };
 
   roll(pinsDown) {
+    // set the rollNumber to 1 - first roll of the frame.
+    this.score(pinsDown);
     this.rollNumber++;
-    // console.log("rollNumber at the start");
-    // console.log(this.rollNumber);
+    
     if (this.rollNumber === 1) {
+      // only increment frame number on first roll.
       this.frame();
       // this.rollNumber++;
     }
     else if (this.rollNumber > 1) {
+      // reset rollnumber, ready for next frame.
       this.rollNumber = 0;
     };
-    // console.log("rollNumber at the end");
-    // console.log(this.rollNumber);
-    this.score(pinsDown);
+
   };
 
   score(pinsDown) {
-    // var totalScore = 0;
-    // totalScore += pinsDown;
-    // return totalScore;
+
+    this.frameScore.push(pinsDown);
+
+    if (this.frameScore.length === 2) {
+      this.allFramesScore.push(this.frameScore);
+      // console.log(this.allFramesScore);
+      this.frameScore = [];
+    };
+
     this.totalScore += pinsDown;
     return this.totalScore;
   };
 
   frame() {
     this.frameNumber++;
-    console.log("frameNumber");
-    console.log(this.frameNumber);
     return this.frameNumber;
   };
 

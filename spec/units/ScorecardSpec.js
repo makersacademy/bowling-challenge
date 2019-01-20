9efd7f2f8f5b0a39428b54c09ee7b59e53de15b0
CKKH <chriskkhassan@gmail.com>
2019-01-20 08:07:54 +0000

    Test refactor: removed redundant tests

--- spec/units/ScorecardSpec.js
+++ spec/units/ScorecardSpec.js
@@ -3,25 +3,13 @@
 'use strict'
 
 describe('Scorecard', () => {
-  it('instance of Scorecard can be stored as a variable', () => {
-    var scorecard = new Scorecard();
-    expect(scorecard instanceof Scorecard).toBe(true)
-  })
-
   describe('#recordFrameScore()', () => {
-    it('responds to .recordFrameScore()', () => {
-      var scorecard = new Scorecard();
-      expect(scorecard.recordFrameScore).toBeDefined()
-    })
-
     it('records the score of a frame', () => {
       var scorecard = new Scorecard();
       let frame = new Frame();
       frame.roll(3)
       frame.roll(2)
       scorecard.recordFrameScore(frame)
-      console.log(frame._score)
-      console.log(scorecard)
       expect(scorecard._frameScores).toContain(frame._score)
     })
   })

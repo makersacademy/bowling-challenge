95372bc3c6ca3cbbd106c5d6ba779b8085273c20
celine kaslin <celine.kaslin@gmail.com>
2019-01-21 00:21:36 +0000

    test for strike passing

--- src/scorecard.js
+++ src/scorecard.js
@@ -11,12 +11,16 @@ class ScoreCard {
     let s = 0;
     let r = 0;
     for (let frames = 0; frames < 10; frames++){
-      if(this.rolls[r] + this.rolls[r+1] === 10){
+      if(this.rolls[r] === 10){
         s += this.rolls[r] + this.rolls[r+1] + this.rolls[r+2]
+        r += 1;
+      } else if (this.rolls[r] + this.rolls[r+1] === 10){
+        s += this.rolls[r] + this.rolls[r+1] + this.rolls[r+2]
+        r += 2
       } else {
         s += this.rolls[r] + this.rolls[r+1]
+        r += 2
       };
-      r += 2;
     };
     return s;
   };

e4c47038fc759548eb59726455db38c0a6e63c67
CKKH <chriskkhassan@gmail.com>
2019-01-23 09:52:52 +0000

    Tests passing: added frame functionality specifying restrictions on roll

--- spec/units/FinalFrameSpec.js
+++ spec/units/FinalFrameSpec.js
@@ -3,15 +3,26 @@
 'use strict'
 
 describe('Final Frame', () => {
-  it('a Final Frame can be assigned to a variable', () => {
-    var finalFrame = new FinalFrame()
-    expect(finalFrame instanceof FinalFrame).toBe(true)
+  let finalFrame
+
+    beforeEach(() => {
+    finalFrame = new FinalFrame()
   })
 
   describe('#roll()', () => {
-    it('.roll can be called on final frame', () => {
-      var finalFrame = new FinalFrame()
-      expect(finalFrame.roll).toBeDefined()
+    it('throws error if NaN is input instead of number', () => {
+      var error = 'Cannot record roll: roll must be an number'
+      expect(function () { finalFrame.roll('hello') }).toThrow(new Error(error))
+    })
+
+    it('throws error if number rolled is less than 0', () => {
+      var error = 'Cannot record roll: roll must be between 0-10'
+      expect(function () { finalFrame.roll(-1) }).toThrow(new Error(error))
+    })
+
+    it('throws error if number rolled is more than 10', () => {
+      var error = 'Cannot record roll: roll must be between 0-10'
+      expect(function () { finalFrame.roll(11) }).toThrow(new Error(error))
     })
   })
 })
--- src/FinalFrame.js
+++ src/FinalFrame.js
@@ -1,5 +1,9 @@
 function FinalFrame () {
 }
 
-FinalFrame.prototype.roll = function () {
+FinalFrame.prototype.roll = function (Number) {
+  let errorOne = 'Cannot record roll: roll must be an number'
+  let errorTwo = 'Cannot record roll: roll must be between 0-10'
+  if (isNaN(Number)) throw new Error(errorOne)
+  if (Number < 0 || Number > 10) throw new Error(errorTwo)
 }

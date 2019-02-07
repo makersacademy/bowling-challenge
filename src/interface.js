3c03d582b4c60e3bfe56be4c1735b9502ea44e67
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-07 11:53:36 +0000

    Able to add any score to the scorecards

--- src/interface.js
+++ src/interface.js
@@ -2,9 +2,76 @@ var bowling = new Bowling();
 
 $(document).ready(function(){
 
+	var x = 1;
+
 	$('#button1').on('click', function(){
-		$('#score1').text(bowling.roll(1));
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(1));
+		}
+		x++;
 	});
 
+	$('#button2').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(2));
+		}
+		x++;
+	});
+
+	$('#button3').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(3));
+		}
+		x++;
+	});
+
+	$('#button4').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(4));
+		}
+		x++;
+	});
+
+	$('#button5').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(5));
+		}
+		x++;
+	});
+
+	$('#button6').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(6));
+		}
+		x++;
+	});
+
+	$('#button7').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(7));
+		}
+		x++;
+	});
+
+	$('#button8').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(8));
+		}
+		x++;
+	});
+
+	$('#button9').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(9));
+		}
+		x++;
+	});
+
+	$('#button10').on('click', function(){
+		for (var i = 0; i < 10; i++) {
+			$('#score' + x).text(bowling.roll(10));
+		}
+		x++;
+	});
 
 });

ad954a8f22056635478f2c03077239b6cbe8b424
Asad Khan <asad_khan47@hotmail.co.uk>
2019-02-07 15:59:52 +0000

    Updated redundant code

--- src/interface.js
+++ src/interface.js
@@ -3,74 +3,55 @@ var bowling = new Bowling();
 $(document).ready(function(){
 
 	var x = 1;
+	var y = 1;
 
 	$('#button1').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(1));
-		}
+		$('#score' + x).text(bowling.roll(1));
 		x++;
 	});
 
 	$('#button2').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(2));
-		}
+		$('#score' + x).text(bowling.roll(2));
 		x++;
 	});
 
 	$('#button3').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(3));
-		}
+		$('#score' + x).text(bowling.roll(3));
 		x++;
 	});
 
 	$('#button4').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(4));
-		}
+		$('#score' + x).text(bowling.roll(4));
 		x++;
 	});
 
 	$('#button5').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(5));
-		}
+		$('#score' + x).text(bowling.roll(5));
 		x++;
 	});
 
 	$('#button6').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(6));
-		}
+		$('#score' + x).text(bowling.roll(6));
 		x++;
 	});
 
 	$('#button7').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(7));
-		}
+		$('#score' + x).text(bowling.roll(7));
 		x++;
 	});
 
 	$('#button8').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(8));
-		}
+		$('#score' + x).text(bowling.roll(8));
 		x++;
 	});
 
 	$('#button9').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(9));
-		}
+		$('#score' + x).text(bowling.roll(9));
 		x++;
 	});
 
 	$('#button10').on('click', function(){
-		for (var i = 0; i < 10; i++) {
-			$('#score' + x).text(bowling.roll(10));
-		}
+		$('#score' + x).text(bowling.roll(10));
 		x++;
 	});
 

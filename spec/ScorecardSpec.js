70ae09ec535fea8d74791d985ef18a5f17f3fb45
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-08 14:53:47 +0000

    RG can record a single frame. needs refactoring

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -5,4 +5,13 @@ describe("Scorecard", function() {
     scorecard = new Scorecard();
   });
 
+  it("can record a single turn", function(){
+    scorecard.record("5");
+    console.log(scorecard._scorecard);
+    scorecard.record("4");
+    console.log(scorecard._scorecard);
+    expect(scorecard.see()[0]).toEqual(["5","4"]);
+    expect(scorecard.score()[0]).toEqual([9]);
+  });
+
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,3 +1,38 @@
 function Scorecard(){
-  
+  this._scorecard = new Array();
+}
+
+Scorecard.prototype.record = function(score){
+  if(this._scorecard.length === 0){
+    console.log("undef was called")
+    this._scorecard.push([score])
+    console.log(this._scorecard[0]);
+    console.log(this._scorecard[this._scorecard.length-1])
+    return this._scorecard
+  };
+  if(this._scorecard[this._scorecard.length-1].length === 2){
+    console.log("length 2 was called")
+    this._scorecard.push([score])
+    return this._scorecard
+  };
+  if(this._scorecard[this._scorecard.length-1].length === 1){
+    console.log("i was called")
+    console.log(this._scorecard.last);
+    this._scorecard[this._scorecard.length-1].push(score);
+    return this._scorecard;
+  };
+}
+
+Scorecard.prototype.see = function(){
+  return this._scorecard
+}
+
+Scorecard.prototype.score = function(){
+  score_array = []
+  this._scorecard.forEach(function(frame,i){
+    var firstRoll = parseInt(frame[0]);
+    var secondRoll = parseInt(frame[1]);
+    score_array.push([firstRoll+secondRoll]);
+  })
+  return score_array;
 }

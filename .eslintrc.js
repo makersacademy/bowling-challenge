1e548bd40c4e2b05f38f2984c7ec8921d7446220
david-mears <kieukmail@gmail.com>
2019-02-08 10:12:03 +0000

    Initial commit with Jasmine and ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 5
+    },
+    "rules": {
+        'no-console': 'off',
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ src/test.js
@@ -0,0 +1,2 @@
+console.log('hi world');
+console.log('hi world' );

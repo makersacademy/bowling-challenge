1aad20007e20fe5c2a3f535491e2d9c9faf43112
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-09 17:15:38 +0000

    Added two new functions frame_score_display and total_Score_display - passing the tests

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,14 +1,31 @@
 function Scorecard(){
   this.pins_knocked_one = 0;
   this.pins_knocked_two = 0;
+  this.frame_score = 0;
+
+  this.total_score_array = [];
 };
 
-var number;
+var number_one;
+var number_two;
 
-Scorecard.prototype.record_first = function(number){
-  this.pins_knocked_one = number;
+Scorecard.prototype.record_first = function(number_one){
+  this.pins_knocked_one = number_one;
+  return this.pins_knocked_one;
 };
 
-Scorecard.prototype.record_second = function(number){
-  this.pins_knocked_two = number;
+Scorecard.prototype.record_second = function(number_two){
+  this.pins_knocked_two = number_two;
+  return this.pins_knocked_two;
+};
+
+Scorecard.prototype.frame_score_display = function(){
+this.frame_score = (this.pins_knocked_one + this.pins_knocked_two);
+this.total_score_array.push(this.frame_score);
+return this.frame_score;
+};
+
+Scorecard.prototype.total_score_display = function(){
+  var sum = this.total_score_array.reduce((a,b) => a + b, 0);
+  return sum;
 };

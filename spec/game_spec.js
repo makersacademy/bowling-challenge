974ee158e10c31dcbdf17b7934f4fa3ecbdd2ed2
Debora <debora.piu92@gmail.com>
2019-02-09 13:55:36 +0000

    Game object with array of turns

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,3 +1,12 @@
 describe("Game", function() {
+  var game;
+
+  beforeEach(function(){
+    game = new Game;
+  });
+
+  it("returns an empty array of turns", function(){
+    expect(game.turns).toEqual([]);
+  });
 
 });

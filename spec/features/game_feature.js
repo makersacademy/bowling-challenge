f74d1814534c57337f877c4d10909eaecb590023
Debora <debora.piu92@gmail.com>
2019-02-09 16:04:53 +0000

    feature test for a game without strikes or spares

---
+++ spec/features/game_feature.js
@@ -0,0 +1,51 @@
+describe("BowlingGame", function() {
+  var game;
+
+  beforeEach(function() {
+    game = new BowlingGame();
+  });
+
+  it("can play a normal game without bonuses", function() {
+    game.play(3, 4);
+    expect(game.turns[0].firstBowl).toEqual(3)
+    expect(game.turns[0].secondBowl).toEqual(4)
+    expect(game.score).toEqual(7)
+    game.play(1, 1);
+    expect(game.turns[1].firstBowl).toEqual(1)
+    expect(game.turns[1].secondBowl).toEqual(1)
+    expect(game.score).toEqual(9)
+    game.play(0, 9);
+    expect(game.turns[2].firstBowl).toEqual(0)
+    expect(game.turns[2].secondBowl).toEqual(9)
+    expect(game.score).toEqual(18)
+    game.play(8, 1);
+    expect(game.turns[3].firstBowl).toEqual(8)
+    expect(game.turns[3].secondBowl).toEqual(1)
+    expect(game.score).toEqual(27)
+    game.play(3, 0);
+    expect(game.turns[4].firstBowl).toEqual(3)
+    expect(game.turns[4].secondBowl).toEqual(0)
+    expect(game.score).toEqual(30)
+    game.play(3, 6);
+    expect(game.turns[5].firstBowl).toEqual(3)
+    expect(game.turns[5].secondBowl).toEqual(6)
+    expect(game.score).toEqual(39)
+    game.play(0, 4);
+    expect(game.turns[6].firstBowl).toEqual(0)
+    expect(game.turns[6].secondBowl).toEqual(4)
+    expect(game.score).toEqual(43)
+    game.play(1, 7);
+    expect(game.turns[7].firstBowl).toEqual(1)
+    expect(game.turns[7].secondBowl).toEqual(7)
+    expect(game.score).toEqual(51)
+    game.play(5, 2);
+    expect(game.turns[8].firstBowl).toEqual(5)
+    expect(game.turns[8].secondBowl).toEqual(2)
+    expect(game.score).toEqual(58)
+    game.play(5, 0);
+    expect(game.turns[9].firstBowl).toEqual(5)
+    expect(game.turns[9].secondBowl).toEqual(0)
+    expect(game.score).toEqual(63)
+  });
+
+});

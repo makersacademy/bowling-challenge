c1aa7e81a3f1ca0312d7c5cfa9aa4cd89f3df14c
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-09 17:14:05 +0000

    Test: displays a score from a frame & a total score at the end of the game

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -1,23 +1,39 @@
 describe('scorecard', function(){
 
   var card;
-  var number;
+  var number_one;
+  var number_two;
 
   beforeEach(function(){
     card = new Scorecard();
-    number = 5;
+    number_one = 5;
+    number_two = 4;
   });
 
   it('records a number of pins knocked in the first roll', function(){
-    card.record_first(number);
-    expect(card.pins_knocked_one).toEqual(number);
+    card.record_first(number_one);
+    expect(card.pins_knocked_one).toEqual(number_one);
   });
 
   it('records a number of pins knocked in the second roll', function(){
-    card.record_second(number);
-    expect(card.pins_knocked_two).toEqual(number);
+    card.record_second(number_two);
+    expect(card.pins_knocked_two).toEqual(number_two);
   });
 
+  it('displays a score from a frame', function(){
+    card.record_first(number_one);
+    card.record_second(number_two);
+    expect(card.frame_score_display()).toEqual(number_one + number_two);
+  });
+
+  it('displays a total score at the end of the game', function(){
+    for (var i of Array(10).keys()){
+      card.record_first(1);
+      card.record_second(2);
+      card.frame_score_display();
+    };
+    expect(card.total_score_display()).toEqual(30);
+  });
 
 
 

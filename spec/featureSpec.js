7ac627bfc2c6671db6f4485f900d54f3a8e069b6
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-09 16:23:20 +0000

    modify featureSpec.js test

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -8,11 +8,13 @@ describe('Features', () => {
   });
 
   it('completes a game after 20 rolls of 0', () => {
-    for(let i = 1 ; i < 21 ; i++){
-      scoreCard.recordScore(i, 0, 0);
+    for(let i = 1 ; i < 11 ; i++){
+      return scoreCard.recordScore(i, 0, 0);
     };
+    expect(scoreCard.isGameComplete).toEqual(true);
     expect(scoreCard.totalScore).toEqual(0);
-    expect(scoreCard.isComplete).toEqual(true);
   });
+
+  // it('')
 
 });

c4d78073599bd32815ed77c1c230bd29709aee64
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-09 18:27:20 +0000

    modify featureSpec.js to add a test for a strike in the middle of the game

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -23,13 +23,24 @@ describe('Features', () => {
     expect(scoreCard.finalScore()).toEqual(50);
   });
 
-  it('can score a game with that starts with a strike', () => {
-    scoreCard.recordScore(1, 10, 0);
-    for(let i = 1 ; i < 10 ; i++){
-      return scoreCard.recordScore(2, 1, 1);
+  // it('can score a game with that starts with a strike', () => {
+  //   scoreCard.recordScore(1, 10, 0);
+  //   for(let i = 1 ; i < 10 ; i++){
+  //     return scoreCard.recordScore(2, 1, 1);
+  //   }
+  //   expect(scoreCard.scoreArray).toEqual([ [1, 10, 0], [2, 1, 1] ]);
+  //   expect(scoreCard.finalScore()).toEqual(14);
+  // });
+
+  it('can score a game with 1 strike', () =>{
+    for(let i = 1 ; i < 6 ; i++){
+      return scoreCard.recordScore(i, 1, 1);
     }
-    expect(scoreCard.scoreArray).toEqual([ [1, 10, 0], [2, 1, 1] ]);
-    expect(scoreCard.finalScore()).toEqual(14);
+    scoreCard.recordScore(6, 10, 0);
+    for(let i = 7; i < 11; i++){
+      return scoreCard.recordScore(i, 1, 1);
+    }
+    expect(scoreCard.finalScore()).toEqual(30);
   });
 
 });

c40fbb3942e8cc817cc4fe5aed456a53ad3aa79d
Erin Stevenson <estevenson_94@live.com>
2019-02-09 17:48:08 +0000

    16. Wrote tests for single strike game for before strike

---
+++ spec/units/gameWithStikeSpec.js
@@ -0,0 +1,27 @@
+var BowlingGame = require('../../src/BowlingGame.js')
+
+describe('SingleStrikeBowlingGame', function() {
+
+  var bowlingGame;
+
+  var rolls = function(score, numOfRolls) {
+    for (i = 0; i < numOfRolls; i++) {
+      bowlingGame.roll(score)
+    };
+  };
+  
+  beforeEach(function() {
+    bowlingGame = new BowlingGame()
+  });
+
+  it('game has an initial score of 0 and frame 0', function() {
+    expect(bowlingGame.score).toEqual(0)
+    expect(bowlingGame.frame).toEqual(0)
+  });
+
+  it('game has score of 30 after first 10 rolls of 3', function() {
+    rolls(3, 10)
+    expect(bowlingGame.score).toEqual(30)
+    expect(bowlingGame.frame).toEqual(5)
+  });
+}); 

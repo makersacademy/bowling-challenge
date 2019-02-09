ca7bd70e722980f0f9b7a05d46075854a3f46011
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-09 12:53:06 +0000

    Frame individual rolls can be added to the frame score

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -21,4 +21,16 @@ describe('Unit tests', function () {
   it('frames hold an array of two rolls', function () {
       expect(frame1.rolls).toEqual([0,0]);
   })
+  it('can accept new values to the rolls', function () {
+      frame1.rolls[0] = 2;
+      frame1.rolls[1] = 3;
+      expect(frame1.rolls).toEqual([2,3]);
+  })
+  it('the two rolls, when modified get added to the frame score', function () {
+      frame1.rolls[0] = 2;
+      frame1.rolls[1] = 3;
+      frame1.addScore()
+      expect(frame1.score).toEqual(5);
+  })
+
 })
--- src/bowling.js
+++ src/bowling.js
@@ -6,3 +6,7 @@ function Frame () {
     this.score = 0
     this.rolls = [0, 0]
 }
+
+Frame.prototype.addScore = function () {
+    this.score = this.rolls[0] + this.rolls[1];
+}

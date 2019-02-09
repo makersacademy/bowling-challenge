707646d26b30d9b0a4049550dc69ad096822a6a8
jesslns <jesslns@yahoo.com.hk>
2019-02-09 15:18:13 +0000

    A frame can record pins knocked down and add score accordingly

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -12,7 +12,18 @@ describe('Frame', function(){
     });
 
     it ('each frame starts with no rolls', function(){
-      expect(frame._rolls).toEqual([]);
+      expect(frame.rolls).toEqual([]);
+    });
+
+    it('counts pins knocked down in roll(s) within a frame', function(){
+      frame.knockedDown(5);
+      frame.knockedDown(5);
+      expect(frame.rolls).toEqual([5,5]);
+    });
+
+    it('adds score pins are knocked down within a frame', function() {
+      frame.knockedDown(5);
+      expect(frame.getCurrentScore()).toEqual(5);
     });
   });
 
--- src/Frame.js
+++ src/Frame.js
@@ -1,10 +1,19 @@
 'use strict';
 
 function Frame () {
-  this._rolls = []
+  this.rolls = []
   this._score = 0;
 };
 
 Frame.prototype.getCurrentScore = function () {
+//   var frameScore
+//   frameScore = this.rolls.reduce((acc, val) => {
+//   return acc + val;
+// });
   return this._score
+};
+
+Frame.prototype.knockedDown = function (pins) {
+  this.rolls.push(pins)
+  this._score += pins
 };

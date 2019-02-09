bb513dbfbde4d1bd041b42e35e9376c4b22bd209
Jake Atkin <jake.a.atkin@gmail.com>
2019-02-09 13:06:31 +0000

    Added tests for 0 score across all 10 frames calling on currentScore

--- test/spec/FrameSpec.js
+++ test/spec/FrameSpec.js
@@ -5,3 +5,16 @@ describe("Frame", function() {
   beforeEach(function() {
     frame = new Frame();
   });
+
+function bowlingFrames(frame, frame10) {
+  for(var i = 0 ; i < 9; i++) {
+    fullgame.bowling(frame)
+    }
+  fullgame.bowling(frame 10 || frame)
+  }
+
+  it("Shows score of zero if you dont hit a single pin all game", function(){
+    bowlingFrames([0,0])
+    expect(fullgame.currentScore()).toEqual(0);
+  });
+});

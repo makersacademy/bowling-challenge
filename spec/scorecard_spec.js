481f69d1eaaf5e89efa9acb37446c61dead78e02
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-09 15:46:21 +0000

    Test: refactoring. Records a number of pins knocked in the first and second

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -1,10 +1,21 @@
 describe('scorecard', function(){
 
+  var card;
+  var number;
 
-  var card = new Scorecard();
+  beforeEach(function(){
+    card = new Scorecard();
+    number = 5;
+  });
 
-  it('records a number of pins knocked', function(){
-    expect(card.record()).toEqual(card.pins);
+  it('records a number of pins knocked in the first roll', function(){
+    card.record_first(number);
+    expect(card.pins_knocked_one).toEqual(number);
+  });
+
+  it('records a number of pins knocked in the second roll', function(){
+    card.record_second(number);
+    expect(card.pins_knocked_two).toEqual(number);
   });
 
 

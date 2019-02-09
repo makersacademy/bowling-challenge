8e32b62202555b15188152a8ad7a5d674a982ffc
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-09 16:25:11 +0000

    add passing code for unit and feature tests

---
+++ src/scoreCard.js
@@ -0,0 +1,27 @@
+
+class ScoreCard {
+  constructor(){
+    this.score = 0;
+    this.scoreArray = [];
+    this.MAXIMUM_FRAMES = 10;
+  }
+
+  recordScore(frameNumber, rollOne = 0, rollTwo = 0){
+    const frameArray = [frameNumber, rollOne, rollTwo];
+    this.scoreArray.push(frameArray);    
+  }
+
+  totalScore(){
+    const scores = this.scoreArray;
+    const total = scores.map(score => score[1] + score[2] )
+      .reduce( (sum, num) => sum + num );
+    return total;
+  }
+
+  isGameComplete(){
+    return this.scoreArray.length >= this.MAXIMUM_FRAMES;
+  }
+
+}
+
+module.exports = ScoreCard;

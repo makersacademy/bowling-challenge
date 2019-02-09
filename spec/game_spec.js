fa792e67947b40e8913c697871cd60bba3ce9424
Debora <debora.piu92@gmail.com>
2019-02-09 15:31:00 +0000

    Game.play function with push object into array

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -1,12 +1,18 @@
 describe("Game", function() {
   var game;
 
-  beforeEach(function(){
-    game = new Game;
+  beforeEach(function() {
+    game = new Game();
   });
 
-  it("returns an empty array of turns", function(){
+  it("returns an empty array of turns", function() {
     expect(game.turns).toEqual([]);
+  });
+
+  it("returns an array of objects", function() {
+    game.play(3, 4);
+    expect(game.turns[0].firstBowl).toEqual(3)
+    expect(game.turns[0].secondBowl).toEqual(4)
   });
 
 });
--- src/game.js
+++ src/game.js
@@ -1,3 +1,11 @@
-function Game() {
-  this.turns = []
+class Game {
+
+  constructor(turns) {
+    this.turns = [];
+  }
+
+  play(firstBowl, secondBowl) {
+    var turn = new Turn(firstBowl, secondBowl);
+    this.turns.push(turn);
+  }
 }

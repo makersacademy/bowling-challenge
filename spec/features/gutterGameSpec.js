6f44335c23ff9c5836a6dc122e693ec98f0a95f8
Erin Stevenson <estevenson_94@live.com>
2019-02-09 14:02:58 +0000

    3. Added feature test for gutter game

---
+++ spec/features/gutterGameSpec.js
@@ -0,0 +1,13 @@
+describe('BowlingGame', function() {
+  
+  var game = new Game;
+
+  it('Gutter Game has score 0 and finishes in the 10th frame', function() {
+    for (i = 0; i <= 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score).toEqual(0)
+    expect(game.frame).toEqual(10)
+  });
+});
+

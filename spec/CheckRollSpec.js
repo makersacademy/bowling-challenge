fa6cfa99f4c392d2282afb2e419ac0407431344f
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-09 16:56:22 +0000

    checkroll detects illegal inputs

--- spec/CheckRollSpec.js
+++ spec/CheckRollSpec.js
@@ -8,4 +8,15 @@ describe("CheckRoll",function(){
   it("Returns 0 for a valid roll",function(){
     expect(checkRoll.run("5")).toEqual(0)
   });
+  it("Raises an error if given invalid input",function(){
+    expect(function(){checkRoll.run("d")}).toThrow(
+      new Error("invalid character")
+    );
+    expect(function(){checkRoll.run("15")}).toThrow(
+      new Error("invalid character")
+    );
+    expect(function(){checkRoll.run("hello")}).toThrow(
+      new Error("invalid character")
+    );
+  });
 });
--- src/CheckRoll.js
+++ src/CheckRoll.js
@@ -2,6 +2,14 @@ function CheckRoll(scorecard) {
   this._scorecard = scorecard;
 }
 
-CheckRoll.prototype.run = function(){
+CheckRoll.prototype.run = function(roll){
+  if(!this._isValidRoll(roll)){throw new Error("invalid character")}
   return 0
+}
+
+CheckRoll.prototype._isValidRoll = function(roll){
+  var isNum = /^\d+$/.test(roll);
+  var isOneChar = roll.length === 1;
+  var isStrikeOrSpare = (roll === "/") || (roll === "X");
+  return (isNum || isStrikeOrSpare) && isOneChar;
 }

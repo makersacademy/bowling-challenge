641f2f00220411ae80c67ffb96742d9f2730268f
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-09 11:50:19 +0000

    Integrated ScoreRecorder object

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -6,97 +6,97 @@ describe("Scorecard", function() {
   });
 
   it("can record a single turn", function(){
-    scorecard.record("5");
-    scorecard.record("4");
+    scorecard.record.roll("5");
+    scorecard.record.roll("4");
     expect(scorecard.see()[0]).toEqual(["5","4"]);
     expect(scorecard.score()).toEqual([9]);
   });
 
   it("can record more than one turn", function(){
-    scorecard.record("5");
-    scorecard.record("4");
+    scorecard.record.roll("5");
+    scorecard.record.roll("4");
     console.log(scorecard.lastFrame());
     console.log(scorecard._scorecard.length)
-    scorecard.record("6");
-    scorecard.record("2");
+    scorecard.record.roll("6");
+    scorecard.record.roll("2");
     expect(scorecard.see()).toEqual([["5","4"],["6","2"]]);
     expect(scorecard.score()).toEqual([9,17]);
   });
 
   it("can record a spare",function(){
-    scorecard.record("5");
-    scorecard.record("/");
+    scorecard.record.roll("5");
+    scorecard.record.roll("/");
     expect(scorecard.see()).toEqual([["5","/"]]);
   });
 
   it("can record a strike",function(){
-    scorecard.record("X");
+    scorecard.record.roll("X");
     expect(scorecard.see()).toEqual([["X",null]]);
   });
 
   it("throws an error if given invalid roll",function(){
-    expect(function(){scorecard.record("a")}).toThrow(
+    expect(function(){scorecard.record.roll("a")}).toThrow(
       new Error("invalid character")
     );
-    expect(function(){scorecard.record("24")}).toThrow(
+    expect(function(){scorecard.record.roll("24")}).toThrow(
       new Error("invalid character")
     );
-    expect(function(){scorecard.record("10")}).toThrow(
+    expect(function(){scorecard.record.roll("10")}).toThrow(
       new Error("invalid character")
     );
   });
 
   it("prevents entering new scores after 10 frames",function(){
     for(var i = 1; i <= 10; i++){
-      scorecard.record("4");
-      scorecard.record("4");
+      scorecard.record.roll("4");
+      scorecard.record.roll("4");
     }
-    expect(function(){scorecard.record("5")}).toThrow(new Error ("Game is over"))
+    expect(function(){scorecard.record.roll("5")}).toThrow(new Error ("Game is over"))
   });
 
   it("allows one more roll on frame 10 if frame 10 had a spare", function(){
     for(var i = 1; i <= 10; i++){
-      scorecard.record("4");
-      scorecard.record("/");
+      scorecard.record.roll("4");
+      scorecard.record.roll("/");
     }
-    expect(function(){scorecard.record("5")}).not.toThrow(new Error ("Game is over"));
-    expect(function(){scorecard.record("5")}).toThrow(new Error ("Game is over"));
+    expect(function(){scorecard.record.roll("5")}).not.toThrow(new Error ("Game is over"));
+    expect(function(){scorecard.record.roll("5")}).toThrow(new Error ("Game is over"));
     expect(scorecard.lastFrame()).toEqual(["4","/","5"]);
   });
 
   it("allows one more roll on frame 10 if frame 10 had a strike", function(){
     for(var i = 1; i <= 10; i++){
-      scorecard.record("4");
-      scorecard.record("X");
+      scorecard.record.roll("4");
+      scorecard.record.roll("X");
     }
-    expect(function(){scorecard.record("5")}).not.toThrow(new Error ("Game is over"));
-    expect(function(){scorecard.record("5")}).toThrow(new Error ("Game is over"));
+    expect(function(){scorecard.record.roll("5")}).not.toThrow(new Error ("Game is over"));
+    expect(function(){scorecard.record.roll("5")}).toThrow(new Error ("Game is over"));
     expect(scorecard.lastFrame()).toEqual(["4","X","5"]);
   });
 
   it("allows one more roll on frame 10 if frame 10 had two strikes", function(){
     for(var i = 1; i <= 9; i++){
-      scorecard.record("4");
-      scorecard.record("X");
+      scorecard.record.roll("4");
+      scorecard.record.roll("X");
     }
-    scorecard.record("X");
-    scorecard.record("X");
+    scorecard.record.roll("X");
+    scorecard.record.roll("X");
     console.log(scorecard.score());
-    expect(function(){scorecard.record("5")}).not.toThrow(new Error ("Game is over"));
-    expect(function(){scorecard.record("5")}).toThrow(new Error ("Game is over"));
+    expect(function(){scorecard.record.roll("5")}).not.toThrow(new Error ("Game is over"));
+    expect(function(){scorecard.record.roll("5")}).toThrow(new Error ("Game is over"));
     expect(scorecard.lastFrame()).toEqual(["X","X","5"]);
   });
 
   it("throws an error if a player scores a / on first roll of frame",function(){
-    expect(function(){scorecard.record("/")}).toThrow(new Error ("invalid spare"))
+    expect(function(){scorecard.record.roll("/")}).toThrow(new Error ("invalid spare"))
   });
 
 
   it("can evaluate a spare",function(){
-    scorecard.record("5");
-    scorecard.record("/");
-    scorecard.record("5");
-    scorecard.record("4");
+    scorecard.record.roll("5");
+    scorecard.record.roll("/");
+    scorecard.record.roll("5");
+    scorecard.record.roll("4");
     expect(scorecard.score()).toEqual([15,24]);
   });
   //Handle most recent frame (strikeetc) having nothing to evaluate to
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,63 +1,11 @@
 function Scorecard(){
   this._scorecard = new Array();
+  this.record = new ScoreRecorder(this._scorecard);
   this.evaluateScore = new EvaluateScore(this._scorecard)
-  console.log(this.evaluteScore)
 }
-
-Scorecard.prototype._isValidRoll = function(roll){
-  var isNum = /^\d+$/.test(roll);
-  var isOneChar = roll.length === 1;
-  var isStrikeOrSpare = (roll === "/") || (roll === "X");
-  return (isNum || isStrikeOrSpare) && isOneChar;
-}
-
-Scorecard.prototype._isValidSpare = function(){
-  return this._scorecard.length > 0 ? (this.lastFrame().length > 0) : false
-}
-
-
 
 Scorecard.prototype.lastFrame = function(){
   return this._scorecard[this._scorecard.length-1]
-}
-
-
-Scorecard.prototype._isGameOver = function(){
-  if((this._scorecard.length === 10) && (this.lastFrame().length >= 2)){
-    return this._isLastFrameFinished()
-  }else{
-    return false
-  };
-}
-
-Scorecard.prototype._isLastFrameFinished = function(){
-  if(this.lastFrame().length >= 3){
-    return true
-  }else if(this.lastFrame().includes("/")||this.lastFrame().includes("X")){
-    return false
-  } else {
-  return true
-  };
-}
-
-Scorecard.prototype.record = function(score){
-  if(this._isGameOver()){throw new Error("Game is over")}
-  if(!this._isValidRoll(score)){throw new Error("invalid character")};
-  if(score === "/" && !this._isValidSpare()){throw new Error("invalid spare")};
-  if(this._scorecard.length === 0){
-    this._scorecard.push([score]);
-    return this._scorecard
-  }else if(this.lastFrame().length === 2){
-    if (this._scorecard.length == 10){
-      this.lastFrame().push(score);
-    } else {
-    this._scorecard.push([score])
-  }
-    return this._scorecard
-  }else if(this.lastFrame().length === 1){
-    this._scorecard[this._scorecard.length-1].push(score);
-    return this._scorecard;
-  };
 }
 
 Scorecard.prototype.see = function(){

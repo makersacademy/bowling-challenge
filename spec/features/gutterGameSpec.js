62942072e6de125ced3a36e43e092ddcd71996de
Erin Stevenson <estevenson_94@live.com>
2019-02-09 15:47:13 +0000

    5. Test for score and fram after first roll

--- spec/features/gutterGameSpec.js
+++ spec/features/gutterGameSpec.js
@@ -2,14 +2,14 @@ let BowlingGame = require('../../src/BowlingGame.js')
 
 describe('BowlingGutterGame', function() {
   
-  let bowlinggame = new BowlingGame();
+  let bowlingGame = new BowlingGame();
 
   it('Gutter Game has score 0 and finishes in the 10th frame', function() {
     for (i = 0; i <= 20; i++) {
-      bowlinggame.roll(0);
+      bowlingGame.roll(0);
     }
-    expect(bowlinggame.score).toEqual(0)
-    expect(bowlinggame.frame).toEqual(10)
+    expect(bowlingGame.score).toEqual(0)
+    expect(bowlingGame.frame).toEqual(10)
   });
 });
 
--- spec/units/gutterGameSpec.js
+++ spec/units/gutterGameSpec.js
@@ -2,15 +2,21 @@ let BowlingGame = require('../../src/BowlingGame.js')
 console.log(BowlingGame)
 describe('BowlingGame', function() {
 
-  let bowlinggame;
+  let bowlingGame;
 
   beforeEach(function() {
-    bowlinggame = new BowlingGame()
+    bowlingGame = new BowlingGame()
   });
 
   it('game has an initial score of 0 in frame 1', function() {
-    expect(bowlinggame.score).toEqual(0)
-    expect(bowlinggame.frame).toEqual(1)
+    expect(bowlingGame.score).toEqual(0)
+    expect(bowlingGame.frame).toEqual(1)
   });
 
+
+  it('game has a score of 0 after first roll of 0', function() {
+    bowlingGame.roll(0);
+    expect(bowlingGame.frame).toEqual(1)
+    expect(bowlingGame.score).toEqual(0)
+  });
 });

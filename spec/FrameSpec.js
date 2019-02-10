866e6398620cc7cc3071081a8d6d4ecd72fdac21
William Dunk <williamdunk@gmail.com>
2019-02-10 11:01:28 +0000

    added frame object unit test for strike

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -3,13 +3,19 @@
 describe("Frame", function() {
 
   var frame;
+  // var ball1;
 
   beforeEach(function() {
     frame = new Frame();
+    // ball1 = new Ball();
   });
 
   it("can score zero", function() {
     expect(frame.score()).toEqual(0);
+  });
+
+  it("can score a strike", function() {
+    expect(frame.score()).toEqual(10);
   });
 
 });

4125fa6d60d6cce04d5c72ee9ecc43d92d4a0a54
Erin Stevenson <estevenson_94@live.com>
2019-02-10 15:22:40 +0000

    26. Wrote new test to check for game with two stikes

--- spec/features/gameWithStrikesSpec.js
+++ spec/features/gameWithStrikesSpec.js
@@ -10,10 +10,17 @@ describe('BowlingGameWithStrikes', function() {
     };
   };
 
-  it('Game with one strike has score ', function() {
+  it('Game with one strike has score 70', function() {
     rolls(3, 10)
     bowlingGame.roll(10)
     rolls(3, 8)
     expect(bowlingGame.score).toEqual(70)
+  });
+  
+  it('Game with two strikes has score 42', function() {
+    rolls(10, 2)
+    rolls(2, 2)
+    rolls(0, 14)
+    expect(bowlingGame.score).toEqual(42)
   });
 });

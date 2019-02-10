afd7b13fddf8a4e4d42c5e2628a5604977f2f13f
William Dunk <williamdunk@gmail.com>
2019-02-10 22:05:04 +0000

    wrote 3rd feature test 1 point per roll scoress 20

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,6 +3,7 @@
 describe("Game", function() {
 
   var game;
+  var currentFrame;
 
   beforeEach(function() {
     game = new Game();
@@ -11,9 +12,6 @@ describe("Game", function() {
   // this is a feature spec
 
   it("gutter game score is zero", function() {
-    for (var i = 1; i <= 10; i++) {
-    game.recordFrame(0);
-    }
     expect(game.finalScore()).toEqual(0);
   });
 
@@ -22,6 +20,16 @@ describe("Game", function() {
     game.recordFrame(1);
     }
     expect(game.finalScore()).toEqual(10);
+  });
+
+  it("1 point per roll equals 20", function() {
+    for (var i = 1; i <= 10; i++) {
+        currentFrame = new Frame();
+        currentFrame.recordRoll(1)
+        currentFrame.recordRoll(1)
+        game.recordFrame(frame.Score());
+    }
+    expect(game.finalScore()).toEqual(20);
   });
 
 

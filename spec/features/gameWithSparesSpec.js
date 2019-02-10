56b1242cd90fbf2612830e4f6a9f5772da117e25
Erin Stevenson <estevenson_94@live.com>
2019-02-10 16:17:13 +0000

    34. Added test for one spare

---
+++ spec/features/gameWithSparesSpec.js
@@ -0,0 +1,25 @@
+var BowlingGame = require('../../src/BowlingGame.js')
+
+describe('BowlingGameWithStrikes', function() {
+  
+  var bowlingGame; 
+  
+  var rolls = function(score, numOfRolls) {
+    for (i = 0; i < numOfRolls; i++) {
+      bowlingGame.roll(score)
+    };
+  };
+
+  beforeEach(function() {
+    bowlingGame = new BowlingGame()
+  });
+  
+  it('Game with one spare has score of 16', function() {
+    bowlingGame.roll(4)
+    bowlingGame.roll(6)
+    rolls(2,2)
+    rolls(0,16)
+    bowlingGame.calculateScore()
+    expect(bowlingGame.score).toEqual(16)
+  });
+});

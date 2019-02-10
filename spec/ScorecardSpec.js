0bb35ebc98327379bb0e51e3ce1a60062bf64e6e
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-10 15:44:52 +0000

    RGR can roll a complete game

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -7,4 +7,12 @@ describe("Scorecard",function(){
     scorecard.record.roll(5);
     expect(scorecard.display()).toEqual([[5]])
   })
+  it("can record an entire game", function(){
+    for( var i = 1; i < 21; i++){
+      scorecard.record.roll(4)
+    }
+    expect(scorecard.display()).toEqual(
+      [[4,4],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4],[4,4]]
+    )
+  })
 })
--- src/ScoreRecorder.js
+++ src/ScoreRecorder.js
@@ -2,6 +2,19 @@ function ScoreRecorder(scorecard){
   this._scorecard = scorecard;
 }
 
-ScoreRecorder.prototype.roll = function(roll){
-  this._scorecard.push([roll]);
+
+ScoreRecorder.prototype.lastFrame = function(){
+  if (this._scorecard.length > 0){
+    return this._scorecard[this._scorecard.length - 1]
+  }else{
+    return []
+  }
 }
+
+ScoreRecorder.prototype.roll = function(roll){
+  if(this.lastFrame().length === 1){
+    this.lastFrame().push(roll);
+  }else{
+    this._scorecard.push([roll]);
+  }
+};

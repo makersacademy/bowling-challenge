737bfa8b2afdb13a3fa0cd34dbc0615420106d2b
Debora <debora.piu92@gmail.com>
2019-02-10 13:47:31 +0000

    tests for strike or spare to turn spec

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -13,7 +13,6 @@ describe("BowlingGame", function() {
       expect(game.turns[0].firstBowl).toEqual(3)
       expect(game.turns[0].secondBowl).toEqual(4)
     });
-
   });
 
   describe('#countScore', () => {
@@ -28,39 +27,9 @@ describe("BowlingGame", function() {
   });
 
   describe('#Bonus', () => {
-    it('should confirm if it\'s a strike', function(){
-      expect(game.strike(10)).toEqual(true)
-      expect(game.strike(4, 6)).toEqual(false)
-      expect(game.strike(4, 0)).toEqual(false)
-    });
-
-    it('should confirm if it\'s a spare', function(){
-      expect(game.spare(7, 3)).toEqual(true)
-      expect(game.spare(10)).toEqual(false)
-      expect(game.spare(4, 1)).toEqual(false)
-    });
-
-    it('should confirm true for strikeOrSpare', function(){
-      expect(game.strikeOrSpare(7, 3)).toEqual(true)
-      expect(game.strikeOrSpare(10, 0)).toEqual(true)
-      expect(game.strikeOrSpare(1, 3)).toEqual(false)
-    });
-
     it('should add bonus for a strike', function(){
       game.play(10)
       expect(game.play(5, 3)).toEqual(26)
-    });
-
-    it('should return hidden bonus', function(){
-      game.play(4, 6)
-      game.play(5, 5)
-      expect(game.bonus).toEqual(25)
-    });
-
-    it('should return hidden score', function(){
-      game.play(4, 6)
-      game.play(5, 5)
-      expect(game.score).toEqual(0)
     });
   });
 });

d15faad182073ce989b3ac4c71e054b95bd1982f
jesslns <jesslns@yahoo.com.hk>
2019-02-10 10:39:20 +0000

    BIG CHANGES: combine frame galss with game class, remove frame class

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -2,22 +2,41 @@
 
 describe('Game', function() {
   var game;
-  var frame;
+  //var frame;
 
   beforeEach(function() {
     game = new Game();
-    frame = jasmine.createSpy('frame',['score']);
+    //frame = jasmine.createSpyObj('frame',['roll','getCurrentScore']);
   });
 
-  describe('there are frames', function() {
-    it('there is no frame by default', function () {
-      expect(game.frames).toEqual([]);
-    });
+  it('there is no frame by default', function () {
+    expect(game.frameNumber).toEqual(0);
+  });
 
-    it('Store score after each frame', function () {
-      game.frameScore(frame._score)
-      expect(game.frames).toEqual([frame._score]);
-    });
+  it('a game starts from zero score in a new game', function(){
+    expect(game.getTotalScore()).toEqual(0);
+  });
+
+  it('counts pins knocked down in a frame', function(){
+    game.roll(5);
+    game.roll(4);
+    expect(game.getCurrentFrame()).toEqual([5,4]);
+  });
+
+  it('counts score after each roll', function (){
+    game.roll(5);
+    expect(game.getTotalScore()).toEqual(5);
+  });
+
+  it('counts frame number', function () {
+    game.newFrame()
+    expect(game.frameNumber).toEqual(1)
+  });
+
+  it('resets frame score in a new frame', function () {
+    game.roll(10)
+    game.newFrame()
+    expect(game.getCurrentFrame()).toEqual([]);
   });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 'use strict'
 
 function Game () {
-  this.frames = []
+  this.frameNumber = 0
+  this._score = 0
+  this.frame = []
 };
 
-Game.prototype.frameScore = function (score) {
-  this.frames.push(score);
+
+Game.prototype.getTotalScore = function () {
+  return this._score
+};
+
+Game.prototype.getCurrentFrame = function () {
+  return this.frame
+}
+
+Game.prototype.newFrame = function () {
+  this.frame = []
+  this.frameNumber += 1
+};
+
+Game.prototype.roll = function (pins) {
+  this.frame.push(pins)
+  this._score += pins
 };

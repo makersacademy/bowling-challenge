87df3e7fbc987f2774b4bedcdf64ad30207c1834
Debora <debora.piu92@gmail.com>
2019-02-10 15:08:02 +0000

    refactor

--- spec/scorecard_spec.js
+++ spec/scorecard_spec.js
@@ -3,8 +3,21 @@
 describe("Scorecard", function() {
   var scorecard;
 
-  beforeEach(function(){
+  beforeEach(function() {
     scorecard = new Scorecard();
   });
+
+//it should print the points for each frame calculating bonuses
+
+//if strikeOrSpare hides points of frames until bonus calculated:
+// if strike, hides for 2 bowls (if strike strike, until second turn first bowl)
+// if spare, hides until next bowl
+
+//if strike shows 'X'
+//if strike, hides score until no strikeOrSpare and adds following 2 bowls
+//if strike and strike after, adds second following turn first bowl)
+
+//if spare shows firstBowl and '/'
+//if spare, hides score until no strikeOrSpare and adds 1 following bowl
 
 });

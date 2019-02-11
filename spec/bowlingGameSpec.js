0fd47fb84cab08b17bb0b13c0dcec9aca2ebdebe
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-02-11 00:38:05 +0000

    refactored passing test : rollMultiple function added

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -2,22 +2,24 @@
 // spec/bowlingGame.js
 
 describe('Bowling Game', function () {
+  var rollMultiple = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      game.roll(pins);
+    }
+  };
+
   var game;
   beforeEach(function () {
     game = new BowlingGame();
   });
 
   it('can roll a gutter game', function () {
-    for (var i = 0; i < 20; i++) {
-      game.roll(0);
-    }
+    rollMultiple(0, 20);
     expect(game.score()).toBe(0);
   });
 
-  it('can roll a full game', function () {
-    for (var i = 0; i < 20; i++) {
-      game.roll(1);
-    }
+  it('can roll a full game of 1', function () {
+    rollMultiple(1, 20);
     expect(game.score()).toBe(20);
   });
 });

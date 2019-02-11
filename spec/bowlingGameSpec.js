b8de89ca71e40b5c3c56f0bb3273c8d4cf641270
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-02-11 00:28:53 +0000

    PASSED TEST for roll full game

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -2,16 +2,22 @@
 // spec/bowlingGame.js
 
 describe('Bowling Game', function () {
+  var game;
   beforeEach(function () {
-    var game = new BowlingGame();
+    game = new BowlingGame();
   });
 
   it('can roll a gutter game', function () {
-    var game = new BowlingGame();
-
     for (var i = 0; i < 20; i++) {
       game.roll(0);
     }
     expect(game.score()).toBe(0);
+  });
+
+  it('can roll a full game', function () {
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
   });
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -2,12 +2,18 @@
 'use strict';
 
 var BowlingGame = function () {
+  this.rolls = [];
 };
 
 BowlingGame.prototype = {
   roll: function (pins) {
+    this.rolls.push(pins);
   },
   score: function () {
-    return 0;
+    var results = 0;
+    for (var i = 0; i < 20; i++) {
+      results += this.rolls[i];
+    }
+    return results;
   }
 };

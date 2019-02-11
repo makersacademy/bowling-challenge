a412429a089af496ecfb5861c499d6fd75a2d887
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-02-11 02:00:31 +0000

    PASSED TEST FOR STRIKE

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -17,6 +17,12 @@ BowlingGame.prototype = {
     function isSpare () {
       return (game.rolls[rollIndex] + game.rolls[rollIndex + 1] === 10);
     }
+    function isStrike () {
+      return (game.rolls[rollIndex] === 10);
+    }
+    function getStrikeScore () {
+      return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
+    }
     function getSpareScore () {
       return game.rolls[rollIndex] + game.rolls[rollIndex + 1] + game.rolls[rollIndex + 2];
     }
@@ -27,10 +33,14 @@ BowlingGame.prototype = {
     for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
       if (isSpare()) {
         results += getSpareScore();
+        rollIndex += 2;
+      } else if (isStrike()) {
+        results += getStrikeScore();
+        rollIndex++;
       } else {
         results += getNormalScore();
+        rollIndex += 2;
       }
-      rollIndex += 2;
     }
     return results;
   }

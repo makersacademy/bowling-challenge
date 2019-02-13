872942358326d71d23fba1966dc38fabc13bbe5c
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-02-13 14:43:02 +0000

    added per frame scoring

--- src/game.js
+++ src/game.js
@@ -1,23 +1,21 @@
 class Game {
-  constructor(pins, score, history, index){
-		this.pins = 10;
-		this.score = 0;
-		this.history = [[],[],[],[],[],[],[],[],[],[]]
-		this.index = 0
-  }
-  
-
-  record(val){
-	  this.pins -= val;
-		this.history[this.index].push(val);
-		if (this.history[this.index].length > 1 || val === 10){
-			this.index += 1
-			this.pins = 10
-		}
-		console.log(this.pins)
+  constructor(pins, score, history, index) {
+    this.pins = 10;
+    this.score = [];
+    this.history = [[], [], [], [], [], [], [], [], [], []];
+    this.index = 0;
   }
 
-
+  record(val) {
+    this.pins -= val;
+    this.history[this.index].push(val);
+    if (this.history[this.index].length > 1 || val === 10) {
+			let framescore = this.history[this.index].reduce((acc, cur) => acc + cur);
+			this.score.push(framescore);
+			this.index += 1;
+      this.pins = 10;
+    }
+	}
 
 }
 
--- tests/game.test.js
+++ tests/game.test.js
@@ -1,59 +1,74 @@
-const Game = require('../src/game');
+const Game = require("../src/game");
 
-describe('Initializer', () => {
+describe("Initializer", () => {
   let game;
 
   beforeEach(() => {
-    game = new Game
+    game = new Game();
   });
 
-  it('start at 10 pins', () => {
+  it("start at 10 pins", () => {
     expect(game.pins).toBe(10);
   });
 
-  it('start with score of 0', ()=>{
-  	expect(game.score).toBe(0);
-  })
-})
 
-describe('Pins', () =>{
-	let game;
+});
+
+describe("Pins", () => {
+  let game;
 
   beforeEach(() => {
-    game = new Game
+    game = new Game();
   });
 
-  it('recorded score deducts number of pins', ()=>{
-		game.record(3);
-		expect(game.pins).toBe(7);
-	});
-  
-  it('new frame resets pins', () =>{
+  it("recorded score deducts number of pins", () => {
+    game.record(3);
+    expect(game.pins).toBe(7);
+  });
+
+  it("new frame resets pins", () => {
     game.record(4);
     game.record(6);
-    expect(game.pins).toBe(10)
-  })
-})
+    expect(game.pins).toBe(10);
+  });
+});
 
-describe('Frame', ()=>{
-	let game;
+describe("Frame", () => {
+  let game;
 
   beforeEach(() => {
-    game = new Game
+    game = new Game();
   });
 
-  it('strike advances frame', ()=>{
-		game.index = 4;
-		game.record(10);
-		expect(game.index).toBe(5)
-	});
-
-  it('frame advances after 2 balls', ()=>{
-  	game.index = 4;
-  	game.record(3);
-  	game.record(5);
-  	expect(game.index).toBe(5);
+  it("strike advances frame", () => {
+    game.index = 4;
+    game.record(10);
+    expect(game.index).toBe(5);
   });
 
-})
+  it("frame advances after 2 balls", () => {
+    game.index = 4;
+    game.record(3);
+    game.record(5);
+    expect(game.index).toBe(5);
+  });
+});
 
+describe("FrameScore", () => {
+  let game;
+
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it("frames are scored", () => {
+    game.index = 3;
+    game.record(4);
+    game.record(3);
+
+    game.index = 4;
+    game.record(3);
+    game.record(3);
+    expect(game.score).toEqual([7,6]);
+  });
+});

fb303237c07cc55fa15d83f6c5293bc003354cb6
Alex Hutchings <alexhutchings4@gmail.com>
2019-02-14 14:38:02 +0000

    Add gameOver function

--- src/game.js
+++ src/game.js
@@ -1,19 +1,30 @@
 function Game() {
   this.frames = []
-  this.currentFrame = 0
+}
+
+Game.prototype.currentFrameIndex = function () {
+  return this.frames.length - 1
 }
 
 Game.prototype.addFrame = function () {
-  this.currentFrame += 1
-  this.frames.push([this.currentFrame,[]])
+  this.frames.push([[]])
 };
 
 Game.prototype.addBowl = function (score) {
-  var frame = this.currentFrame - 1
-  if (this.frames[frame][1].length == 2) {
+  var frame = this.currentFrameIndex()
+  if (this.frames[frame][0].length == 2) {
     this.addFrame();
     this.addBowl(score);
   } else {
-    this.frames[frame][1].push(score)
+    this.frames[frame][0].push(score)
+  }
+};
+
+Game.prototype.gameOver = function () {
+  var frame = this.currentFrameIndex()
+  if (frame == 9 && this.frames[frame][0].length == 2) {
+    return true;
+  } else {
+    return false;
   }
 };
--- test/spec/game-spec.js
+++ test/spec/game-spec.js
@@ -14,13 +14,13 @@
 
     it('can add a frame', function () {
       game.addFrame();
-      expect(game.frames).toEqual([[1,[]]])
+      expect(game.frames).toEqual([[[]]])
     });
 
     it('can add more frames', function () {
       game.addFrame();
       game.addFrame();
-      expect(game.frames).toContain([2,[]])
+      expect(game.frames).toContain([[]])
     });
   });
 
@@ -32,14 +32,14 @@
     it('adds first bowl to the frame', function () {
       game.addFrame();
       game.addBowl(6);
-      expect(game.frames[0]).toEqual([1,[6]])
+      expect(game.frames[0]).toEqual([[6]])
     })
 
     it('adds second bowl to the frame', function () {
       game.addFrame();
       game.addBowl(6);
       game.addBowl(3);
-      expect(game.frames[0]).toEqual([1,[6, 3]])
+      expect(game.frames[0]).toEqual([[6, 3]])
     })
 
     it('creates a new frame on the third bowl, and adds bowl to the new frame', function () {
@@ -47,7 +47,22 @@
       game.addBowl(6);
       game.addBowl(3);
       game.addBowl(4);
-      expect(game.frames[1]).toEqual([2,[4]])
+      expect(game.frames[1]).toEqual([[4]])
+    })
+  })
+
+  describe('When game is over', function () {
+    beforeEach(function () {
+      game = new Game()
+    });
+    
+    it('can check if the game is over', function () {
+      for (var i = 0; i < 10; i++) {
+        game.addFrame();
+        game.addBowl(6);
+        game.addBowl(3);
+      }
+      expect(game.gameOver()).toEqual(true)
     })
   })
 })();

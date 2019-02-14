baf570ccf16ca9eddfee0b764862ccab41b06a73
Paul <paul.ayling@bglgroup.co.uk>
2019-02-14 12:39:27 +0000

    Interface connected, first pinHit method working, need to find way to reference cells

---
+++ interface/interface.js
@@ -0,0 +1,10 @@
+var scorecard = new Scorecard()
+var counter = 0;
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("Hello World")
+});
+
+function pinHit(number) {
+  document.getElementById('f1t1').innerHTML = 0;
+}

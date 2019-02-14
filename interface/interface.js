cacd69cddfdda11cf7a11779c923f83a629122ad
Paul <paul.ayling@bglgroup.co.uk>
2019-02-14 13:12:45 +0000

    pinHit writing correctly, still needs checks for Strikes, Spares and two numbers not greater than 10

--- interface/interface.js
+++ interface/interface.js
@@ -1,10 +1,28 @@
 var scorecard = new Scorecard()
-var counter = 0;
+var turnCounter = 1;
+var frameCounter = 1;
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log("Hello World")
 });
 
 function pinHit(number) {
-  document.getElementById('f1t1').innerHTML = 0;
+  logThrow(number)
+  displayThrow(number)
+  incrementCounters()
+}
+
+function logThrow(number) {
+  if (turnCounter === 1) { scorecard.firstThrow(number) }
+  else { scorecard.secondThrow(number) }
+}
+
+function displayThrow(number) {
+  let idToChange = `f${frameCounter}t${turnCounter}`
+  document.getElementById(idToChange).innerHTML = number;
+}
+
+function incrementCounters() {
+  turnCounter ++;
+  if (turnCounter === 3) { turnCounter = 1; frameCounter++; }
 }

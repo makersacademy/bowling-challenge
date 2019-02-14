b586dc77c8772985bede8136768b440be3c0bf46
habakkukyong <habakkukyong@gmail.com>
2019-02-14 14:45:47 +0000

    Allow Bowlers only to number 8

--- jasmine-standalone-3.3.0/spec/BowlersSpec.js
+++ jasmine-standalone-3.3.0/spec/BowlersSpec.js
@@ -1,11 +1,16 @@
 describe('Bowlers', function() {
     var bowlers;
     beforeEach(function() {
-        bowlers = new Bowlers();
-    });
+        bowlers = new Bowlers()
+    })
 
     it('should be able to receive the name of a player', function() {
-        bowlers.add('Foo');
-        expect(bowlers.all).toEqual(['Foo']);
-    });
-});
+        bowlers.add('Foo')
+        expect(bowlers.all).toEqual(['Foo'])
+    })
+
+    it('should accept no more than 8 players', function() {
+        for(i = 0; i < 9; i++) { bowlers.add('Foo') }
+        expect(bowlers.all.length).toEqual(8)
+    })
+})
--- jasmine-standalone-3.3.0/src/Bowlers.js
+++ jasmine-standalone-3.3.0/src/Bowlers.js
@@ -1,7 +1,16 @@
+// Stores no more than 8 bowlers and returns their names in an array
 function Bowlers() {
     this.all = [];
 };
 
 Bowlers.prototype.add = function(name) {
-    this.all.push(name);
-};
+    if (this.all.length === 8) {
+        try { 
+            throw new Error ("No more than 8 bowlers allowed")
+        }
+        catch(error) {
+            console.log(error)
+        }
+    }
+    else { this.all.push(name) }
+}

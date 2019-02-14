231458d8bd8c5a248530894e92a6b9dbf2b0193e
Alex Hutchings <alexhutchings4@gmail.com>
2019-02-14 12:07:07 +0000

    Add currentFrame property to Game, and change addFrame to increment currentFrame

--- src/game.js
+++ src/game.js
@@ -1,3 +1,9 @@
 function Game() {
   this.frames = []
+  this.currentFrame = 0
 }
+
+Game.prototype.addFrame = function () {
+  this.currentFrame += 1
+  this.frames.push([this.currentFrame,[null,null]])
+};
--- test/spec/game-spec.js
+++ test/spec/game-spec.js
@@ -1,11 +1,26 @@
 (function () {
   'use strict';
 
-  var game = new Game();
+  var game;
 
   describe('When the game is created', function () {
+    beforeEach(function () {
+      game = new Game()
+    });
+
     it('has an empty frames array', function () {
       expect(game.frames).toEqual([]);
+    });
+
+    it('can add a frame', function () {
+      game.addFrame();
+      expect(game.frames).toEqual([[1,[null,null]]])
+    });
+
+    it('can add more frames', function () {
+      game.addFrame();
+      game.addFrame();
+      expect(game.frames).toContain([2,[null,null]])
     });
   });
 })();

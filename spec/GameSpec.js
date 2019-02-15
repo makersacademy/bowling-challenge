f7eff89128d3a23b8b74c346111293688e868a40
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-15 10:02:06 +0000

    gutter game

---
+++ spec/GameSpec.js
@@ -0,0 +1,13 @@
+describe("Game", function(){
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it("gutter game if score is zero at the end of the game", function() {
+    for (i = 0; i < 20; i++) {
+      game.roll(0)};
+      expect(game.score()).toEqual(0);
+  });
+});
---
+++ src/Game.js
@@ -0,0 +1,11 @@
+function Game() {
+
+};
+
+Game.prototype.roll = function (pinsDown) {
+
+};
+
+Game.prototype.score = function () {
+  return 0;
+};

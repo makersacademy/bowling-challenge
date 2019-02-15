cf116d88211de468538f741fcc5a00ba1ed2df53
habakkukyong <habakkukyong@gmail.com>
2019-02-15 19:36:42 +0000

    Begin work on frame function

--- jasmine-standalone-3.3.0/src/Game.js
+++ jasmine-standalone-3.3.0/src/Game.js
@@ -14,6 +14,19 @@ Game.prototype.begin = function(players) {
 }
 
 /*
+    Display frame function
+        Have this here instead of in Interface as window.onload = function() {} wrapping Interface breaks Jasmine
+
+    Present [1] through [9] as "1/" through "9/"
+    Present [10] as "X"
+    Present [0,9] through [9, 0] as "0/9" through "9/0"
+    Present [0, 10] through [9,1] as "/"
+*/
+Game.prototype.presentFrame = function(frameThrows) {
+    if (frameThrows == [10]) { return 'X' }
+}
+
+/*
     Add scores to each frame
     Know when to give the frame to the next player
     Pass these scores to the gameResults data object

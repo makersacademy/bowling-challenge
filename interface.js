864080db598dbcb5ee280e16348daa531d0e95c4
isabelcooper <isabel@triptease.com>
2019-02-15 12:29:31 +0000

    refactor notes to only run at end of frame

--- interface.js
+++ interface.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
   let player = new Player("Jane");
-  let frame = 1;
+  let frame = player.frames.length + 1;
   let roll = 0;
+
+  $(".button").click(function(event) {
+    console.log(player.frames);
+    roll +=1
+
+    // if(roll === 1) {roll = 0} else {roll = 1}
+    player.enterRoll(parseInt(this.id));
+    refreshData(frame, roll);
+  });
 
   function refreshData(frame, roll) {
     rollData();
     fillFrameScore();
     fillRunningTotal();
-    addNotes();
 
     function rollData() {
       $(`#F${frame}R${roll}`).text(returnRollScore());
@@ -24,28 +32,21 @@ $(document).ready(function() {
 
       function returnFrameScore() {
         return player.frames[frame - 1]
-          ? player.frames[frame - 1].score
+          ? (player.frames[frame - 1].score, addNotes())
           : player.currentFrame.score;
+
+          function addNotes() {
+            $(`#Notes${frame}`).text(player.frames[frame - 1].notes);
+          }
       }
      }
 
     function fillRunningTotal() {
       $(`#total`).text(player.totalScore);
      }
-
-    function addNotes() {
-      $(`#Notes${frame}`).text(player.frames[frame - 1].notes);
-    }
   }
 
   function frameComplete() {
     return player.currentFrame.rolls[0] ? true : false;
   }
-
-  $(".button").click(function(event) {
-    // if player.currentFram
-    roll += 1;
-    player.enterRoll(parseInt(this.id));
-    refreshData(frame, roll);
-  });
 });

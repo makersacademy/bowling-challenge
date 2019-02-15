f181570f7717b618c60acdb8a746b89fb6d25a12
habakkukyong <habakkukyong@gmail.com>
2019-02-15 19:36:25 +0000

    Add tests for Game's presentFrame function

--- jasmine-standalone-3.3.0/spec/GameSpec.js
+++ jasmine-standalone-3.3.0/spec/GameSpec.js
@@ -13,4 +13,28 @@ describe('Game', function() {
     //         })
     //     })
     // })
+
+    // Test display frame function - upon pass update Interface to use game.presentFrame(<given frame>)
+    describe('presentFrame', function() {
+        it('should take an array containing a single digit integer and it as a string suffixed with a /', function() {
+            expect(game.presentFrame([1])).toBe("1/")
+            expect(game.presentFrame([9])).toBe("9/")
+        })
+        
+        it('should take an array containing only the number 10 and return a string, the character X', function() {
+            expect(game.presentFrame([10])).toBe("X")
+        })
+
+        it('should take an array with two numbers whose sum is less than 10 and return a string separating them with /', function() {
+            expect(game.presentFrame([1,8])).toBe("1/8")
+        })
+
+        it('should take an array with two numbers whose sum is 10 and return string /', function() {
+            expect(game.presentFrame([0,10])).toBe("/")
+            expect(game.presentFrame([3,7])).toBe("/")
+        })
+        // check Game comments for other tests
+    })
+
+    // Test add stuff to frame function
 })

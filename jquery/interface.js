f080187860e2950551bf22ae3f10c5c1ef67c7b3
sg1110 <saule.guzyte@tesco.com>
2019-02-15 11:58:23 +0000

    Buttons hidden to avoid exceeding 10 pins

--- jquery/interface.js
+++ jquery/interface.js
@@ -4,8 +4,15 @@ $(document).ready(function() {
   var frame = 1
 
     $('#0').click(function() {
-      game.roll(0)
-      $(`#f${game.frame}r${turn}`).text(game.frame_input[game.frame])
+      if (turn === 1) {
+        game.roll(0)
+        $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+        turn++}
+      else {
+        game.roll(0)
+        $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+        turn = 1
+        frame++}
     })
 
     $('#1').click(function(){
@@ -13,43 +20,192 @@ $(document).ready(function() {
           game.roll(1)
           $(`#f${frame}r${turn}`).text(game.frame_input[frame])
           turn++
+          $('#10').hide();
         }
         else {
           game.roll(1)
           $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
           turn = 1
           frame++
+          $("#buttons").show().children().show();
         }
-      })
-
     })
 
+    $('#2').click(function(){
+        if (turn === 1) {
+          game.roll(2)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+        }
+        else {
+          game.roll(2)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
 
-    // $('#1').click(function(){
-    //   game.roll(1)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
-    // $('#2').click(function(){
-    //   game.roll(2)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
-    // $('#3').click(function(){
-    //   game.roll(3)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
-    // $('#4').click(function(){
-    //   game.roll(4)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
-    // $('#5').click(function(){
-    //   game.roll(5)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
-    // $('#6').click(function(){
-    //   game.roll(6)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
-    // $('#7').click(function(){
-    //   game.roll(7)
-    //   $('#f1r1').text(game.frame_input[game.frame])
-    // })
+    $('#3').click(function(){
+        if (turn === 1) {
+          game.roll(3)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+        }
+        else {
+          game.roll(3)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#4').click(function(){
+        if (turn === 1) {
+          game.roll(4)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+          $('#7').hide();
+        }
+        else {
+          game.roll(4)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#5').click(function(){
+        if (turn === 1) {
+          game.roll(5)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+          $('#7').hide();
+          $('#6').hide();
+        }
+        else {
+          game.roll(5)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#6').click(function(){
+        if (turn === 1) {
+          game.roll(6)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+          $('#7').hide();
+          $('#6').hide();
+          $('#5').hide();
+        }
+        else {
+          game.roll(6)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#7').click(function(){
+        if (turn === 1) {
+          game.roll(7)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+          $('#7').hide();
+          $('#6').hide();
+          $('#5').hide();
+          $('#4').hide();
+        }
+        else {
+          game.roll(7)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#8').click(function(){
+        if (turn === 1) {
+          game.roll(8)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+          $('#7').hide();
+          $('#6').hide();
+          $('#5').hide();
+          $('#4').hide();
+          $('#3').hide();
+        }
+        else {
+          game.roll(8)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#9').click(function(){
+        if (turn === 1) {
+          game.roll(9)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++
+          $('#10').hide();
+          $('#9').hide();
+          $('#8').hide();
+          $('#7').hide();
+          $('#6').hide();
+          $('#5').hide();
+          $('#4').hide();
+          $('#3').hide();
+          $('#2').hide();
+        }
+        else {
+          game.roll(9)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+
+    $('#10').click(function(){
+        if (turn === 1) {
+          game.roll(10)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame])
+          turn++}
+        else {
+          game.roll(10)
+          $(`#f${frame}r${turn}`).text(game.frame_input[frame][1])
+          turn = 1
+          frame++
+          $("#buttons").show().children().show();
+        }
+    })
+})

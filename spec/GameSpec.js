6626f6201afb76d37eae123d19e9bff3ba8bc269
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-15 11:29:03 +0000

    strike

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -25,5 +25,13 @@ describe("Game", function(){
       game.roll(0)};
     expect(game.score()).toEqual(14);
   });
-  
+
+  it("can handle a Strike", function() {
+    game.roll(10);
+    game.roll(2);
+    game.roll(5);
+    for (i = 0; i < 16; i++) {
+      game.roll(0)};
+    expect(game.score()).toEqual(24);
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -10,12 +10,16 @@ Game.prototype.score = function () {
   var total = 0;
   var rollIndex = 0;
   for (i = 0; i < 10; i++) {
-    if (this.isSpare(rollIndex)) {
+    if (this.isStrike(rollIndex)) {
+      total += this.strikeScore(rollIndex);
+      rollIndex += 1;
+    } else if (this.isSpare(rollIndex)) {
       total += this.spareScore(rollIndex);
+      rollIndex += 2;
     } else {
       total += this.frameScore(rollIndex);
+      rollIndex += 2;
     };
-    rollIndex += 2;
   };
   return total;
 };
@@ -24,9 +28,17 @@ Game.prototype.isSpare = function(rollIndex) {
   return this._rolls[rollIndex] + this._rolls[rollIndex + 1] === 10;
 };
 
+Game.prototype.isStrike = function(rollIndex) {
+  return this._rolls[rollIndex] === 10;
+};
+
 Game.prototype.spareScore = function(rollIndex) {
   return 10 + this._rolls[rollIndex + 2];
 };
+
+Game.prototype.strikeScore = function(rollIndex) {
+  return 10 + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+}
 
 Game.prototype.frameScore = function(rollIndex) {
   return this._rolls[rollIndex] + this._rolls[rollIndex + 1];

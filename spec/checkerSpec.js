7c630963f66823c0e11bb71e47879a5dfebfbe0d
Paul <paul.ayling@bglgroup.co.uk>
2019-02-15 14:59:39 +0000

    Checker class written and tested

--- spec/checkerSpec.js
+++ spec/checkerSpec.js
@@ -1,8 +1,46 @@
-describe('Checker:', function () {
-  var checker
+describe("Checker:", function() {
+  var checker;
 
-  beforeEach(function () {
-    checker = new Checker()
-  })
+  beforeEach(function() {
+    checker = new Checker();
+  });
 
-})
+  describe("#standard checks:", function() {
+    it("returns true on a Strike", function() {
+      expect(checker.strikeCheck(10)).toEqual(true);
+      expect(checker.strikeCheck(0, 5)).toEqual(false);
+    });
+    it("returns true on a Spare", function() {
+      expect(checker.spareCheck(7, 3)).toEqual(true);
+      expect(checker.spareCheck(0, 10)).toEqual(true);
+      expect(checker.spareCheck(5, 5)).toEqual(true);
+    });
+  });
+
+  describe("#firstRolls:", function() {
+    it("returns true on a Strike/Spare", function() {
+      expect(checker.firstRollStrikeOrSpare(10, 0, 1)).toEqual(true);
+      expect(checker.firstRollStrikeOrSpare(7, 3, 1)).toEqual(true);
+      expect(checker.firstRollStrikeOrSpare(5, 5, 2)).toEqual(false);
+      expect(checker.firstRollStrikeOrSpare(5, 2, 1)).toEqual(false);
+    });
+  });
+
+  describe("#lastRolls:", function() {
+    it("returns true if previous score was a Strike and current isn't", function() {
+      expect(checker.lastRollStrikeThisRollNot(10, 5)).toEqual(true);
+      expect(checker.lastRollStrikeThisRollNot(5, 5)).toEqual(false);
+      expect(checker.lastRollStrikeThisRollNot(0, 10)).toEqual(false);
+    });
+    it("returns true if previous two rolls were Strikes and current isn't", function() {
+      expect(checker.lastTwoStrikesThisRollNot(10, 10, 5)).toEqual(true);
+      expect(checker.lastTwoStrikesThisRollNot(10, 10, 10)).toEqual(false);
+      expect(checker.lastTwoStrikesThisRollNot(10, 0, 10)).toEqual(false);
+    });
+    it("returns true on last three rolls were Strikes", function() {
+      expect(checker.tripleStrike(10, 10, 10)).toEqual(true);
+      expect(checker.tripleStrike(10, 10, 5)).toEqual(false);
+      expect(checker.tripleStrike(10, 0, 10)).toEqual(false);
+    });
+  });
+});
--- src/checker.js
+++ src/checker.js
@@ -1,28 +1,63 @@
-function Checker () {
-}
+function Checker() {}
 
-Checker.prototype.strikeCheck = function (firstScore) {
+Checker.prototype.strikeCheck = function(firstScore) {
   if (firstScore === 10) {
-    return true
+    return true;
+  } else {
+    return false;
   }
-}
+};
 
-Checker.prototype.spareCheck = function (firstScore, secondScore) {
+Checker.prototype.spareCheck = function(firstScore, secondScore) {
   if (firstScore != 10 && firstScore + secondScore === 10) {
-    return true
+    return true;
+  } else {
+    return false;
   }
-}
+};
 
-Checker.prototype.firstRollStrikeOrSpare = function(firstScore, secondScore, turn) {
-  if (turn === 1) {
-    if (this.strikeCheck(firstScore) || this.spareCheck(firstScore, secondScore)) {
-      return true
+Checker.prototype.firstRollStrikeOrSpare = function(
+  firstScore,
+  secondScore,
+  turn
+) {
+  if (
+    turn === 1 &&
+    (this.strikeCheck(firstScore) || this.spareCheck(firstScore, secondScore))
+  ) {
+    return true;
+  } else {
+    return false;
   }
-}
-}
+};
 
-Checker.prototype.lastRollStrikeThisRollNot = function(lastFirstScore, firstScore) {
+Checker.prototype.lastRollStrikeThisRollNot = function(
+  lastFirstScore,
+  firstScore
+) {
   if (this.strikeCheck(lastFirstScore) && !this.strikeCheck(firstScore)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Checker.prototype.lastTwoStrikesThisRollNot = function(lastLastFirstScore, lastFirstScore, firstScore) {
+  if (this.strikeCheck(lastLastFirstScore) &&
+    this.strikeCheck(lastFirstScore) &&
+    !this.strikeCheck(firstScore)) {
       return true
+    } else {
+      return false
+    }
+}
+
+Checker.prototype.tripleStrike = function(lastLastFirstScore, lastFirstScore, firstScore) {
+  if (this.strikeCheck(lastLastFirstScore) &&
+    this.strikeCheck(lastFirstScore) &&
+    this.strikeCheck(firstScore)) {
+      return true
+    } else {
+      return false
     }
 }

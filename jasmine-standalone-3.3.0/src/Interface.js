d8f506f67409855f2c2dddf92bb51c9a22cc0ece
habakkukyong <habakkukyong@gmail.com>
2019-02-15 12:04:40 +0000

    Add begin game button and have it style sections

--- jasmine-standalone-3.3.0/src/Interface.js
+++ jasmine-standalone-3.3.0/src/Interface.js
@@ -6,10 +6,10 @@ window.onload = function() {
     game = new Game()
 
     /*
-    When user enters name and clicks submit
-        Add bowler to list of bowlers
-        Clear input field
-        Update the list-of-bowlers to show each bowler
+        When user enters name and clicks submit
+            Add bowler to list of bowlers
+            Clear input field
+            Update the list-of-bowlers to show each bowler
     */
     document.getElementById('bowler-input-submit').addEventListener('click', function(event) {
         event.preventDefault();
@@ -17,5 +17,15 @@ window.onload = function() {
         bowlers.add(bowler)
         document.getElementById('bowler-input-field').value = ''
         document.getElementById('list-of-bowlers').innerText = bowlers.all.join(" ")
+    })
+
+    /*
+        When user clicks Begin Game
+            Hide get-bowlers section
+            Show play-game section
+    */
+    document.getElementById('begin-game').addEventListener('click', function(event) {
+        document.getElementById('get-bowlers').style.display = 'none';
+        document.getElementById('play-game').style.display = 'block';
     })
 }

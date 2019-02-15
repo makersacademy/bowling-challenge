2038851cc15da0e7b508a6b6368593ed458e3fac
habakkukyong <habakkukyong@gmail.com>
2019-02-15 11:48:26 +0000

    Update displayed list of bowlers when list updated

--- jasmine-standalone-3.3.0/src/Interface.js
+++ jasmine-standalone-3.3.0/src/Interface.js
@@ -5,11 +5,17 @@ window.onload = function() {
     bowlers = new Bowlers()
     game = new Game()
 
-    // Add bowler when user enters name and clicks submit (clear field also)
+    /*
+    When user enters name and clicks submit
+        Add bowler to list of bowlers
+        Clear input field
+        Update the list-of-bowlers to show each bowler
+    */
     document.getElementById('bowler-input-submit').addEventListener('click', function(event) {
         event.preventDefault();
         let bowler = document.getElementById('bowler-input-field').value
         bowlers.add(bowler)
         document.getElementById('bowler-input-field').value = ''
-    });
+        document.getElementById('list-of-bowlers').innerText = bowlers.all.join(" ")
+    })
 }

cfa6fc978b7f4637903ef5f3cdeed745c2db1c8d
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-15 10:28:34 +0000

    all ones

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,4 +10,10 @@ describe("Game", function(){
       game.roll(0)};
       expect(game.score()).toEqual(0);
   });
+
+  it("can handle all ones", function() {
+    for (i = 0; i < 20; i++) {
+      game.roll(1)};
+      expect(game.score()).toEqual(20);
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,17 @@
 function Game() {
-
+  this._rolls = [];
 };
 
 Game.prototype.roll = function (pinsDown) {
-
+  this._rolls.push(pinsDown);
 };
 
 Game.prototype.score = function () {
-  return 0;
+  var total = 0;
+  var rollIndex = 0;
+  for (i = 0; i < 20; i++) {
+    total += this._rolls[rollIndex];
+    rollIndex ++;
+  };
+  return total;
 };

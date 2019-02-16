60d52eb0062ab80bf41e139d5955d2fb31e2885e
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-16 12:28:07 +0000

    Changes to buttons - now if 5 pins are knocked in the first roll user cannot input value > 5 in second roll.Applied to all the buttons

--- ui.js
+++ ui.js
@@ -3,62 +3,111 @@ $(document).ready(function(){
   var card = new Scorecard();
   var clicks = 0;
 
+  function buttons(){
+    $("#ten, #nine, #eight, #seven, #six, #five, #four, #three, #two, #one").attr('class','page-link');
+    $('#ten').text('10');
+    $('#nine').text('9');
+    $('#eight').text('8');
+    $('#seven').text('7');
+    $('#six').text('6');
+    $('#five').text('5');
+    $('#four').text('4');
+    $('#three').text('3');
+    $('#two').text('2');
+    $('#one').text('1');
+  };
+
   $('#zero').click(function(){
     helper(0);
   });
+
   $('#one').click(function(){
+    $('#ten').attr('class','disabled');
+    $('#ten').text('');
     helper(1);
   });
+
   $('#two').click(function(){
+    $('#ten, #nine').attr('class','disabled');
+    $('#ten, #nine').text('');
     helper(2);
   });
+
   $('#three').click(function(){
+    $('#ten, #nine, #eight').attr('class','disabled');
+    $('#ten, #nine, #eight').text('');
     helper(3);
   });
+
   $('#four').click(function(){
+    $('#ten, #nine, #eight, #seven').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven').text('');
     helper(4);
   });
+
   $('#five').click(function(){
+    $('#ten, #nine, #eight, #seven, #six').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven, #six').text('');
     helper(5);
   });
+
   $('#six').click(function(){
+    $('#ten, #nine, #eight, #seven, #six, #five').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven, #six, #five').text('');
     helper(6);
   });
+
   $('#seven').click(function(){
+    $('#ten, #nine, #eight, #seven, #six, #five, #four').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven, #six, #five, #four').text('');
     helper(7);
   });
+
   $('#eight').click(function(){
+    $('#ten, #nine, #eight, #seven, #six, #five, #four, #three').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven, #six, #five, #four, #three').text('');
     helper(8);
   });
+
   $('#nine').click(function(){
+    $('#ten, #nine, #eight, #seven, #six, #five, #four, #three, #two').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven, #six, #five, #four, #three, #two').text('');
     helper(9);
   });
+
   $('#ten').click(function(){
+    $('#ten, #nine, #eight, #seven, #six, #five, #four, #three, #two, #one').attr('class','disabled');
+    $('#ten, #nine, #eight, #seven, #six, #five, #four, #three, #two, #one').text('');
     helper(10);
   });
+
   $('#play_again'). click(function(){
-    location. reload(true);
+    location.reload(true);
    });
 
 
   function helper(number){
 
     if (clicks == 0){
+      buttons();
       card.record_first(number);
       $('#s_box1_f1').text(`${number}`)
       clicks += 1;
 
     } else if (clicks == 1) {
+      buttons();
       card.record_second(number);
       $('#s_box2_f1').text(`${number}`)
       clicks += 1;
       $('#b_box_f1').text(card.frame_score_display());
+
 
     } else if (clicks == 2) {
       card.record_first(number);
       $('#s_box1_f2').text(`${number}`)
       clicks += 1;
     } else if (clicks == 3) {
+      buttons();
       card.record_second(number);
       $('#s_box2_f2').text(`${number}`)
       clicks += 1;
@@ -71,6 +120,7 @@ $(document).ready(function(){
       $('#s_box1_f3').text(`${number}`)
       clicks += 1;
     } else if (clicks == 5) {
+      buttons();
       card.record_second(number);
       $('#s_box2_f3').text(`${number}`)
       clicks += 1;

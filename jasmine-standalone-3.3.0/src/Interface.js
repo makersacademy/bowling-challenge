952719b337825dcd0f4643772bb45aa136170003
habakkukyong <habakkukyong@gmail.com>
2019-02-16 16:21:20 +0000

    Correctly display frame content

--- jasmine-standalone-3.3.0/src/Interface.js
+++ jasmine-standalone-3.3.0/src/Interface.js
@@ -31,25 +31,24 @@ window.onload = function() {
         document.getElementById('play-game').style.display = 'block';
         // Begin game with all bowlers
         game.begin(bowlers.all)
+        // frameRows becomes array of html strings each of which can display a row in frames table
         let frameRows = []
         for(i = 0; i < Object.keys(game.frames).length; i++) {
-            //Add a long string to frameRows which will be used to draw and label the table
-
-            // NEXT TASK!
-            // Replace frame name with actual frame content
+            let bowler = Object.keys(game.frames)[i]
+            let frames = game.frames[bowler]
             frameRows.push(
                 '<tr>' +
-                    '<td id="name-' + Object.keys(game.frames)[i] + '">' + Object.keys(game.frames)[i] + '</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f01">' + Object.keys(game.frames)[i].toLowerCase() + '-f01</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f02">' + Object.keys(game.frames)[i].toLowerCase()+ '-f02</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f03">' + Object.keys(game.frames)[i].toLowerCase()+ '-f03</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f04">' + Object.keys(game.frames)[i].toLowerCase()+ '-f04</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f05">' + Object.keys(game.frames)[i].toLowerCase()+ '-f05</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f06">' + Object.keys(game.frames)[i].toLowerCase()+ '-f06</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f07">' + Object.keys(game.frames)[i].toLowerCase()+ '-f07</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f08">' + Object.keys(game.frames)[i].toLowerCase()+ '-f08</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f09">' + Object.keys(game.frames)[i].toLowerCase()+ '-f09</td>' +
-                    '<td id="' + Object.keys(game.frames)[i] + '-f10">' + Object.keys(game.frames)[i].toLowerCase()+ '-f10</td>' +
+                    '<td id="name-' + bowler + '">' + bowler + '</td>' +
+                    '<td id="' + bowler + '-f01">' + game.presentFrame(frames[0].throws) + '</td>' +
+                    '<td id="' + bowler + '-f02">' + game.presentFrame(frames[1].throws) + '</td>' +
+                    '<td id="' + bowler + '-f03">' + game.presentFrame(frames[2].throws) + '</td>' +
+                    '<td id="' + bowler + '-f04">' + game.presentFrame(frames[3].throws) + '</td>' +
+                    '<td id="' + bowler + '-f05">' + game.presentFrame(frames[4].throws) + '</td>' +
+                    '<td id="' + bowler + '-f06">' + game.presentFrame(frames[5].throws) + '</td>' +
+                    '<td id="' + bowler + '-f07">' + game.presentFrame(frames[6].throws) + '</td>' +
+                    '<td id="' + bowler + '-f08">' + game.presentFrame(frames[7].throws) + '</td>' +
+                    '<td id="' + bowler + '-f09">' + game.presentFrame(frames[8].throws) + '</td>' +
+                    '<td id="' + bowler + '-f10">' + game.presentFrame(frames[9].throws) + '</td>' +
                 '</tr>'
             )
         }

c256077e3b1374e3bb40d30988e39c5b2a7cb1a3
Dan Groze <dangroze@gmail.com>
2019-02-16 23:06:03 +0000

    Test- Cannot add more than 10 frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,15 +1,23 @@
 describe('Game', function() {
   var game;
+  var frame;
   beforeEach(function(){
     game = new Game();
+    frame = jasmine.createSpy('frame');
   });
   it('has no frames by default', function() {
     expect(game._frames).toEqual([]);
   });
   describe('addFrame', function() {
     it('adds a frame', function() {
-      game.addFrame('frame');
-      expect(game._frames).toEqual(['frame']);
+      game.addFrame(frame);
+      expect(game._frames).toEqual([frame]);
+    })
+    it('cannot add more than 10 frames', function() {
+      for(var i = 0; i < 10; i++) {
+        game.addFrame(frame)
+      }
+      expect(function(){game.addFrame(frame)}).toThrowError('Cannot add more frames!');
     })
   })
 })

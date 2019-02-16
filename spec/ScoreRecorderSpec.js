a2a90caf9cb98b34345cca7ef4ea9a0098d6b965
Luca Fruzza <lfruzza@hotmail.co.uk>
2019-02-16 20:16:47 +0000

    added perfect game test. scoreRecorder should be fully funcitonal

--- spec/ScoreRecorderSpec.js
+++ spec/ScoreRecorderSpec.js
@@ -17,6 +17,21 @@ describe("ScoreRecorder",function(){
     expect(scorecard).toEqual([[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5]])
     scoreRecorder.roll(5);
     expect(scorecard).toEqual([[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5,5]])
-
   });
+  it ("records a 20th roll into frame 10 roll if frame 10 contained a strike",function(){
+    for(var i = 1; i <= 18; i++){
+      scoreRecorder.roll(5);
+    };
+    scoreRecorder.roll(10);
+    expect(scorecard).toEqual([[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[10]])
+    scoreRecorder.roll(5)
+    expect(scorecard).toEqual([[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[5,5],[10,5]])
+  });
+
+  it ("can record a perfect game",function(){
+    for(var i = 1; i <= 12; i++){
+      scoreRecorder.roll(10);
+    };
+    expect(scorecard).toEqual([[10],[10],[10],[10],[10],[10],[10],[10],[10],[10,10,10]])
+  })
 });

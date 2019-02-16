fe85f3e4392f7734cffb73fde85c780d74d2a619
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-16 13:47:27 +0000

    Frame should start with total points equalling 0

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,3 +1,13 @@
 describe("Frame", function() {
 
+  var frame;
+
+  beforeEach(function() {
+    frame = new Frame();
+  });
+
+  it("should start with total points 0", function() {
+    expect(frame.showTotalPoints()).toEqual(0);
+  });
+
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,3 +1,8 @@
 function Frame() {
-  
+  this._totalPoints = 0;
+  this._ballOne = 0;
 }
+
+Frame.prototype.showTotalPoints = function () {
+  return this._totalPoints;
+};

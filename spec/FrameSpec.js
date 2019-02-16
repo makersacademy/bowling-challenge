671b31ae7489d2c624d99915205ae74cc38fe163
Dan Groze <dangroze@gmail.com>
2019-02-16 22:29:28 +0000

    Added passing test for being able to roll a second time

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -2,14 +2,20 @@ describe('Frame', function() {
   var frame;
   beforeEach(function() {
     frame = new Frame();
-  });
-
-  it ('has no rolls by default', function() {
+  })  
+  it('has no rolls by default', function() {
     expect(frame._rolls).toEqual([]);
   });
-  it ('can add a roll', function() {
-    frame.addRoll(9);
-    expect(frame._rolls).toEqual([9]);
-  });
 
+  describe('addRoll', function() {
+    it('can add a roll', function() {
+      frame.addRoll(9);
+      expect(frame._rolls).toEqual([9]);
+    })
+    it('can add a second roll', function() {
+      frame.addRoll(5);
+      frame.addRoll(4);
+      expect(frame._rolls).toEqual([5,4]);
+    })
+  })
 })

9b488983ac7071e02e8a3ebe46a9a0a72ee8bb35
Dan Groze <dangroze@gmail.com>
2019-02-16 19:51:59 +0000

    Test -Frame can add a roll

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -7,5 +7,9 @@ describe('Frame', function() {
   it ('has no rolls by default', function() {
     expect(frame._rolls).toEqual([]);
   });
+  it ('can add a roll', function() {
+    frame.addRoll(9);
+    expect(frame._rolls).toEqual([9]);
+  });
 
 })

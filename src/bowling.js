cb84571057258df1ee7920dd2682675a2201ff9e
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-02-17 13:51:08 +0000

    Periodic ESLint-mandated update.

--- src/bowling.js
+++ src/bowling.js
@@ -21,11 +21,11 @@ Frame.prototype.isComplete = function () {
 }
 
 Frame.prototype.setFrameComplete = function () {
-	this.addScore();
-    if(this.score === 10) {
-        this.frameCompletionStatus = true;}
-    else if(this.rolls[0] !== 10 && this.rolls[1] !== null) {
-        this.frameCompletionStatus = true;
-    }
+	this.addScore()
+	if(this.score === 10) {
+		this.frameCompletionStatus = true}
+	else if(this.rolls[0] !== 10 && this.rolls[1] !== null) {
+		this.frameCompletionStatus = true
+	}
 
 }

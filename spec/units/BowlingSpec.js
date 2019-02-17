69189d219b964fd7c99499b8c114172c5ef10738
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-17 15:37:49 +0000

    Improve unit test grouping and install cypress

--- spec/units/BowlingSpec.js
+++ spec/units/BowlingSpec.js
@@ -14,33 +14,51 @@ describe('Bowling', function () {
     bowling = new Bowling()   // eslint-disable-line
   })
 
-  it('should begin the game on frame 0', function () {
-    expect(bowling._currentFrame().number()).toEqual(0)
+  describe('frame counting', function () {
+    it('should begin the game on frame 0', function () {
+      expect(bowling._currentFrame().number()).toEqual(0)
+    })
+
+    it('should count frames as scores are input', function () {
+      enterScores(0, 4)
+      expect(bowling._currentFrame().number()).toEqual(2)
+    })
+
+    it('should reset the frameScore to zero after each frame', function () {
+      enterScores(1, 2)
+      expect(bowling._currentFrame().frameScore()).toEqual(0)
+    })
+
+    it('.getCompleteFrames should return only complete frames', function () {
+      enterScores(5, 1)
+      let incompleteFrames = false
+      let frames = bowling.getCompleteFrames()
+      frames.forEach(function (frame) {
+        if (!frame.isComplete()) {
+          incompleteFrames = true
+        }
+      })
+      expect(incompleteFrames).toEqual(false)
+    })
   })
 
-  it('should count frames as scores are input', function () {
-    enterScores(0, 4)
-    expect(bowling._currentFrame().number()).toEqual(2)
+  describe('game completion', function () {
+    it('should not mark the game isComplete until after 20 rolls', function () {
+      enterScores(0, 4)
+      expect(bowling.scorecardComplete).toEqual(false)
+    })
+
+    it('should mark the game isComplete after 20 rolls', function () {
+      enterScores(0, 20)
+      expect(bowling.scorecardComplete).toEqual(true)
+    })
   })
 
-  it('should not mark the game isComplete until after 20 rolls', function () {
-    enterScores(0, 4)
-    expect(bowling.scorecardComplete).toEqual(false)
-  })
-
-  it('should mark the game isComplete after 20 rolls', function () {
-    enterScores(0, 20)
-    expect(bowling.scorecardComplete).toEqual(true)
-  })
-
-  it('should reset the frameScore to zero after each frame', function () {
-    enterScores(1, 2)
-    expect(bowling._currentFrame().frameScore()).toEqual(0)
-  })
-
-  it('should add gameScore from each frame', function () {
-    enterScores(2, 4)
-    expect(bowling.gameScore()).toEqual(8)
+  describe('routine scoring', function () {
+    it('should add gameScore from each frame', function () {
+      enterScores(2, 4)
+      expect(bowling.gameScore()).toEqual(8)
+    })
   })
 
   describe('spares are calculated correctly', function () {
@@ -74,7 +92,9 @@ describe('Bowling', function () {
       enterScores(2, 4)
       expect(bowling.gameScore()).toEqual(74)
     })
+  })
 
+  describe('strike and spare edge cases', function () {
     it('calculates strikes followed by a spare correctly', function () {
       enterScores(10, 1)
       enterScores(6, 1)
@@ -93,22 +113,12 @@ describe('Bowling', function () {
     })
   })
 
-  it('should stop the user going more than two times in round 10 if no spare or strike', function () {
-    enterScores(3, 20)
-    expect(bowling.gameScore()).toEqual(60)
-    enterScores(3, 1)
-    expect(bowling.gameScore()).toEqual(60)
-  })
-
-  it('returns only completed frames', function () {
-    enterScores(5, 1)
-    let incompleteFrames = false
-    let frames = bowling.getCompleteFrames()
-    frames.forEach(function (frame) {
-      if (!frame.isComplete()) {
-        incompleteFrames = true
-      }
+  describe('round 10 edge cases', function () {
+    it('should stop the user going more than two times in round 10 if no spare or strike', function () {
+      enterScores(3, 20)
+      expect(bowling.gameScore()).toEqual(60)
+      enterScores(3, 1)
+      expect(bowling.gameScore()).toEqual(60)
     })
-    expect(incompleteFrames).toEqual(false)
   })
 })

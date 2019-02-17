b5ac11063fef304f83d743e5d08628bf74261b20
jesslns <jesslns@yahoo.com.hk>
2019-02-17 23:12:38 +0000

    add pins buttons

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
   var game = new Game();
 
-  $('#total-score').text(game.getTotalScore());
+  pinHit = function(pins){
+    game.roll(pins);
+    game.endFrame();
+    frameScore();
+    // totalScore();
+    $('#total-score').text(game._score);
+  };
+
+  frameScore = function() {
+    frame1.innerHTML = game.frameScore['1'];
+    frame2.innerHTML = game.frameScore['2'];
+    frame3.innerHTML = game.frameScore['3'];
+    frame4.innerHTML = game.frameScore['4']
+    frame5.innerHTML = game.frameScore['5']
+    frame6.innerHTML = game.frameScore['6']
+    frame7.innerHTML = game.frameScore['7']
+    frame8.innerHTML = game.frameScore['8']
+    frame9.innerHTML = game.frameScore['9']
+    frame10.innerHTML = game.frameScore['10']
+  };
+
   $('#frame-number').text(game.frameNumber);
   $('#new-frame').click(function(){
     game.newFrame();

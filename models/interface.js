5e85bf0199bd037f3a6628c945632c30ccba53e3
Katie Cornish <katielcornish@gmail.com>
2019-02-17 16:56:08 +0000

    Refactor interface

--- models/interface.js
+++ models/interface.js
@@ -6,68 +6,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function loadTable() {
   document.getElementById("total").innerHTML = scorecard.totalScore
-  if (scorecard.frames[0]) {
-    frame = scorecard.frames[0]
-    document.getElementById("marker0").innerHTML = frame.frameScore()
-    document.getElementById("f1b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f1b2").innerHTML = frame.rollTwo }
+  for (var i = 0; i < scorecard.frames.length; i++) {
+      frame = scorecard.frames[i]
+      document.getElementById("marker" + i).innerHTML = scorecard.frames[i].frameScore()
+      if (frame.rollOne === 10) { document.getElementById("f" + (i+1) + "b2").innerHTML = 'X' }
+      else { document.getElementById("f" + (i+1) + "b1").innerHTML = frame.rollOne }
+      if (frame.rollTwo != null) {
+        if (frame.rollOne + frame.rollTwo === 10) { document.getElementById("f" + (i+1) + "b2").innerHTML = '/' }
+        else { document.getElementById("f" + (i+1) + "b2").innerHTML = frame.rollTwo }
+    }
   }
-  if (scorecard.frames[1]) {
-    frame = scorecard.frames[1]
-    document.getElementById("marker1").innerHTML = scorecard.frames[1].frameScore()
-    document.getElementById("f2b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f2b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[2]) {
-    frame = scorecard.frames[2]
-    document.getElementById("marker2").innerHTML = scorecard.frames[2].frameScore()
-    document.getElementById("f3b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f3b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[3]) {
-    frame = scorecard.frames[3]
-    document.getElementById("marker3").innerHTML = scorecard.frames[3].frameScore()
-    document.getElementById("f4b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f4b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[4]) {
-    frame = scorecard.frames[4]
-    document.getElementById("marker4").innerHTML = scorecard.frames[4].frameScore()
-    document.getElementById("f5b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f5b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[5]) {
-    frame = scorecard.frames[5]
-    document.getElementById("marker5").innerHTML = scorecard.frames[5].frameScore()
-    document.getElementById("f6b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f6b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[6]) {
-    frame = scorecard.frames[6]
-    document.getElementById("marker6").innerHTML = scorecard.frames[6].frameScore()
-    document.getElementById("f7b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f7b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[7]) {
-    frame = scorecard.frames[7]
-    document.getElementById("marker7").innerHTML = scorecard.frames[7].frameScore()
-    document.getElementById("f8b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f8b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[8]) {
-    frame = scorecard.frames[8]
-    document.getElementById("marker8").innerHTML = scorecard.frames[8].frameScore()
-    document.getElementById("f9b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f9b2").innerHTML = frame.rollTwo }
-  }
-  if (scorecard.frames[9]) {
-    frame = scorecard.frames[9]
-    document.getElementById("marker9").innerHTML = scorecard.frames[9].frameScore()
-    document.getElementById("f10b1").innerHTML = frame.rollOne
-    if (frame.rollTwo != null) { document.getElementById("f10b2").innerHTML = frame.rollTwo }
-  }
-
-
 }
 
   document.getElementById("0").addEventListener('click', function() {

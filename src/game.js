4c61b3491d1981ef4c54a735e6e573e33df8717e
Alex Hutchings <alexhutchings4@gmail.com>
2019-02-17 20:39:59 +0000

    Refactor code and test to simplest form.

--- src/game.js
+++ src/game.js
@@ -1,34 +1,15 @@
 function Game() {
-  this.DEFAULT_FRAMES = [[], [], [], [], [], [], [], [], [], []];
-  this.frames = this.DEFAULT_FRAMES;
-  this.currentFrame = 1;
-  this.currentBowl = 1;
   this.allBowls = [];
 }
 
-Game.prototype._currentFrameIndex = function() {
-  return this.currentFrame - 1;
-};
-
-Game.prototype.addBowl = function(score) {
-  this.frames[this._currentFrameIndex()].push(score);
-  this.allBowls.push(score);
-  this._updateTurn();
-};
-
-Game.prototype._updateTurn = function() {
-  if (this.currentBowl == 1) {
-    this.currentBowl++;
-  } else {
-    this.currentFrame++;
-    this.currentBowl = 1;
-  }
+Game.prototype.addBowl = function(pinsHit) {
+  this.allBowls.push(pinsHit);
 };
 
 Game.prototype.calculateTotal = function() {
   var total = 0;
   for (var i = 0; i < 20; i++) {
-    total += this.allBowls[i];
+    total += this.allBowls[i] || 0;
   }
   return total;
 };
--- test/spec/game-spec.js
+++ test/spec/game-spec.js
@@ -1,71 +1,41 @@
-(function () {
-  'use strict';
-
+describe('Bowling Scoreboard', function () {
   var game;
 
   beforeEach(function () {
     game = new Game();
   });
 
-  describe('When the game is created', function () {
-    it('has default frames const', function () {
-      expect(game.DEFAULT_FRAMES.length).toEqual(10);
-    });
+  function addManyBowls(bowl, times) {
+    for (var i = 0; i < times; i++) {
+      game.addBowl(bowl);
+    }
+  }
 
-    it('has frames equal to the default frames', function () {
-      expect(game.frames).toEqual(game.DEFAULT_FRAMES)
-    })
-
-    it('has current frame of 1', function () {
-      expect(game.currentFrame).toEqual(1)
-    })
-
-    it('has current bowl of 1', function () {
-      expect(game.currentBowl).toEqual(1)
-    })
-
+  describe('basic functions', function () {
     it('stores all of the bowls', function () {
       expect(game.allBowls).toEqual([])
     })
-  })
 
-  describe('When playing the game', function () {
-    beforeEach(function () {
-      game.addBowl(6)
-    })
-    it('adds first bowl to first frame', function () {
-      expect(game.allBowls).toContain(6)
-      expect(game.frames[0]).toEqual([6])
-      expect(game.currentBowl).toEqual(2)
+    it('adds a bowl to allBowls', function () {
+      game.addBowl(1)
+      expect(game.allBowls).toContain(1)
     })
 
-    it('adds second bowl to first frame, and moves the frame on', function () {
-      game.addBowl(3);
-      expect(game.frames[0]).toEqual([6, 3])
-      expect(game.currentBowl).toEqual(1)
-      expect(game.currentFrame).toEqual(2)
-    })
-
-    it('adds the third bowl to the second frame', function () {
-      game.addBowl(3);
-      game.addBowl(4);
-      expect(game.frames[1]).toEqual([4])
+    it('calculates the total', function () {
+      game.addBowl(5)
+      expect(game.calculateTotal()).toEqual(5)
     })
   })
 
   describe('Different games', function () {
     it('can bowl a gutter game', function () {
-      for (var i = 0; i < 20; i++) {
-        game.addBowl(0)
-      }
+      addManyBowls(0, 20)
       expect(game.calculateTotal()).toEqual(0);
     })
 
     it('can bowl all 1s', function () {
-      for (var i = 0; i < 20; i++) {
-        game.addBowl(1)
-      }
+      addManyBowls(1, 20)
       expect(game.calculateTotal()).toEqual(20)
     })
   })
-})();
+})

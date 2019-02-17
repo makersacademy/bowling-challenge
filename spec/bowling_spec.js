480d84e3c2c4470dd82759eb0b0bfbb6f741c872
Ross <ross.hunter@tesco.com>
2019-02-17 14:58:34 +0000

    scoring complete in console

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -26,4 +26,25 @@ describe('Bowling Game', function() {
       expect(game.score()).toEqual(0);
     })
 
+    it('can roll a spare', function(){
+      game.roll(5);
+      game.roll(5);
+      game.roll(3);
+      multiRoll(0,17);
+      expect(game.score()).toEqual(16);
+    })
+
+    it('can roll a strike', function(){
+      game.roll(10);
+      game.roll(4);
+      game.roll(3);
+      multiRoll(0,16);
+      expect(game.score()).toEqual(24);
+    })
+
+    it('can roll all strikes', function(){
+      multiRoll(10,12);
+      expect(game.score()).toEqual(300);
+    })
+
 });
--- src/bowling.js
+++ src/bowling.js
@@ -7,11 +7,44 @@ function Game() {
 
   function score() {
     let score = 0;
-    for (var i = 0; i < 20; i++) {
-      score += rolls[i];
+    var rollindex =0;
+
+    function isStrike() {
+      return rolls[rollindex] == 10;
+    }
+    function getStrikeScore() {
+      return rolls[rollindex] + rolls[rollindex + 1] + rolls[rollindex + 2];
+    }
+
+    function isSpare() {
+      return rolls[rollindex] + rolls[rollindex + 1] == 10;
+    }
+
+    function getSpareScore() {
+      return rolls[rollindex] + rolls[rollindex + 1] + rolls[rollindex + 2];
+    }
+
+    function getNormalScore() {
+      return rolls[rollindex] + rolls[rollindex + 1];
+    }
+
+    for (var frames = 0; frames < 10; frames++) {
+      if (isStrike()) {
+        score += getStrikeScore();
+        rollindex++;
+      } else if (isSpare()) {
+        score += getSpareScore();
+        rollindex += 2;
+      } else {
+      score += getNormalScore();
+      rollindex += 2;
+      }
     }
     return score;
+
+
   }
+
 
   return {rolls: rolls, roll: roll,  score: score}
 

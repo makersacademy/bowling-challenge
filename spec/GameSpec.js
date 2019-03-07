f173fb7a06fc33ff4b5ac8b46b7a8767ad7eec24
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-07 07:39:04 +0000

    pass test for creating with 10 empty frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,9 +6,8 @@ describe('Game', function() {
     game = new Game;
   });
 
-  it('is created with 10 empty frames, with a bonus roll for frame 10', function() {
-    expect(this.fames.length).toEqual(10);
-    expect(this.frames[9].length).toEqual(3);
+  it('is created with 10 empty frames', function() {
+    expect(game.frames.length).toEqual(10);
   });
 
 });

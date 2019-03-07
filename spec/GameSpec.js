b4367445d656133258ab50f71a46d39dfb7fac6d
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-07 22:03:27 +0000

    pass test for scoring a gutter game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,4 +6,12 @@ describe('Game', function() {
     game = new Game;
   });
 
+  it('scores a gutter game (no points scored at all) as zero', function() {
+    for(var i = 0; i <= 9; i++) {
+      game.addFrame(0,0);
+    }
+    expect(game.frames.length).toEqual(10);
+    expect(game.score).toEqual(0);
+  });
+
 });

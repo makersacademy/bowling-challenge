b748b5976cc6d3002565114b45d7a0d438c76e8e
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-07 22:04:40 +0000

    pass test for adding a frame to a game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,6 +6,11 @@ describe('Game', function() {
     game = new Game;
   });
 
+  it('can add a frame to a game', function() {
+    game.addFrame(3,3);
+    expect(game.frames.length).toEqual(1);
+  });
+
   it('scores a gutter game (no points scored at all) as zero', function() {
     for(var i = 0; i <= 9; i++) {
       game.addFrame(0,0);

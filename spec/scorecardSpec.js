e762cb8d1186655c37df98f263c3a38a5e4090cf
Simon Norman <01secn@gmail.com>
2019-03-08 23:18:52 +0000

    Scorecard returns new frame if frame for previous roll had finished

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,13 +1,26 @@
 
 describe('Scorecard', function () {
   let scorecard
+  let mockFrameClass
 
   beforeEach(function () {
     mockFrameClass = function() {}
     scorecard = new Scorecard(mockFrameClass)
   })
 
-  it('should return current frame', function () {
-    expect(scorecard.roll(7).currentFrame).toEqual(scorecard.frames[scorecard.frames.length - 1])
+  describe('When ball roll is added', function () {
+    describe('Given frame for previous roll had NOT finished', function() {
+      it('should return same frame as previous roll', function () {
+        mockFrameClass.prototype.isFinished = function() { return false }
+        expect(scorecard.roll(7).currentFrame).toEqual(scorecard.frames[0])
+      })
+    })
+
+    describe('Given frame for previous roll HAD finished', function() {
+      it('should return new frame', function () {
+        mockFrameClass.prototype.isFinished = function() { return true }
+        expect(scorecard.roll(7).currentFrame).toEqual(scorecard.frames[1])
+      })
+    })
   })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,10 +1,19 @@
 function Scorecard(Frame) {
+  this.Frame = Frame
   const frame = new Frame()
   this.frames = [frame]
 }
 
+Scorecard.prototype.currentFrame = function () {
+  return this.frames[this.frames.length - 1]
+}
+
 Scorecard.prototype.roll = function (noOfPins) {
+  if (this.currentFrame().isFinished()) {
+    this.frames.push(new this.Frame())
+  }
+
   return {
-    currentFrame: this.frames[this.frames.length - 1]
+    currentFrame: this.currentFrame()
   }
 }

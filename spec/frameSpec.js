851da3ca8eadfbad035298795daddb1147efea48
Clare Pinder <Clare.pinder@live.co.uk>
2019-03-08 15:17:31 +0000

    saves whether strike or spare on scorecard

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -10,14 +10,24 @@ describe('Frame', function () {
 
   it('saves the score of roll1 in a hash in an array', function() {
     frame.roll1(1, 5)
-    expect(frame.basicScore[0]).toEqual({'roll1': 5, 'roll2': null, 'special': null, 'total': null})
+    expect(frame.scoreCard[0]).toEqual({'roll1': 5, 'roll2': null, 'special': null, 'total': null})
   });
 
   it('saves the score of roll2 in a hash in an array', function() {
     frame.roll2(1, 5)
-    expect(frame.basicScore[0]).toEqual({'roll1': null, 'roll2': 5, 'special': null, 'total': null})
+    expect(frame.scoreCard[0]).toEqual({'roll1': null, 'roll2': 5, 'special': null, 'total': null})
   });
 
+  it('if strike, saves "strike" and does not allow input for second roll', function() {
+    frame.roll1(1, 10)
+    expect(frame.scoreCard[0]).toEqual({'roll1': 10, 'roll2': 0, 'special': 'strike', 'total': null})
+  });
+
+  it('if spare, saves "spare"', function() {
+    frame.roll1(2, 3)
+    frame.roll2(2, 7)
+    expect(frame.scoreCard[1]).toEqual({'roll1': 3, 'roll2': 7, 'special': 'spare', 'total': null})
+  });
 
 
 });
--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,5 @@
 function Frame() {
-  this.basicScore = [
+  this.scoreCard = [
     {'roll1': null, 'roll2': null, 'special': null, 'total': null},
     {'roll1': null, 'roll2': null, 'special': null, 'total': null},
     {'roll1': null, 'roll2': null, 'special': null, 'total': null},
@@ -14,9 +14,17 @@ function Frame() {
   ]}
 
 Frame.prototype.roll1 = function(frame, score1) {
-  this.basicScore[frame - 1]['roll1'] = score1
+  this.score1 = score1
+  this.scoreCard[frame - 1]['roll1'] = this.score1
+  if(score1 === 10) {
+    this.scoreCard[frame - 1]['roll2'] = 0
+    this.scoreCard[frame - 1]['special'] = 'strike'
+  }
 };
 
 Frame.prototype.roll2 = function(frame, score2) {
-  this.basicScore[frame - 1]['roll2'] = score2
+  this.scoreCard[frame - 1]['roll2'] = score2
+  if(this.score1 + score2 === 10) {
+    this.scoreCard[frame - 1]['special'] = 'spare'
+  }
 };

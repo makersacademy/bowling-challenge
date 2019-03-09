c69c887bb403a7f673b9b30dc15ec4a544f05d46
matt <mwhitaker1006@gmail.com>
2019-03-09 13:24:15 +0000

    refactored how score data is stored and collected, to make it more readable, simpler to handle and easier to calculate total scores from. Also added and passed tests to check for presence of each data structure on initialising new scorecard

--- spec/ScorerSpec.js
+++ spec/ScorerSpec.js
@@ -6,30 +6,40 @@ describe("Scorer", function() {
   });
 
   it("creates empty json to hold roll values", function() {
-    expect(scorer.rolls[1][1]).toEqual(null);
+    expect(scorer.rolls[1]).toEqual(null);
+  });
+
+  it("creates empty json to hold bonus values", function() {
+    expect(scorer.bonus[1]).toEqual(null);
+  });
+
+  it("creates empty json to hold notes values", function() {
+    expect(scorer.notes[1]).toEqual(null);
   });
 
   it("stores roll values", function() {
-    scorer.storeScore(5,"1-1")
-    expect(scorer.rolls[1][1]).toEqual(5)
+    scorer.storeScore(5,1)
+    expect(scorer.rolls[1]).toEqual(5)
   });
 
   it("calculates score from a spare", function() {
-    scorer.storeScore(5,"1-1")
-    scorer.storeScore(5,"1-2")
-    scorer.storeScore(5,"2-1")
+    scorer.storeScore(5,1)
+    scorer.storeScore(5,2)
+    scorer.storeScore(5,3)
     scorer.spareCalculator()
-    expect(scorer.rolls[1].bonus).toEqual(5)
+    expect(scorer.bonus[1]).toEqual(5)
   });
 
   it("calcualtes score from a strike within sequential frames", function() {
-    scorer.storeScore(10,"1-1")
-    scorer.storeScore(0,"1-2")
-    scorer.storeScore(5,"2-1")
-    scorer.storeScore(2,"2-2")
+    scorer.storeScore(10,1)
+    scorer.storeScore(0,2)
+    scorer.storeScore(5,3)
+    scorer.storeScore(2,4)
     scorer.spareCalculator()
-    expect(scorer.rolls[1].bonus).toEqual(7)
+    expect(scorer.bonus[1]).toEqual(7)
   });
+
+
 
 
 });

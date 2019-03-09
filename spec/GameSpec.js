373f0d5958d68c324f1dd8c9fd85420e844f37b5
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-09 15:05:44 +0000

    adds total for many frames

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -23,4 +23,17 @@ it('adds the scores of two bowl in a frame', function(){
   game.pushTotalForFrame();
   expect(game.totalScore[0]).toEqual(5);
 });
+
+it('adds the scores of many frames', function(){
+  game.start();
+  game.frames[0].bowl(1);
+  game.frames[0].bowl(4);
+  game.frames[3].bowl(1);
+  game.frames[3].bowl(4);
+  game.frames[6].bowl(2);
+  game.frames[6].bowl(2);
+  game.pushTotalForFrame();
+  expect(game.totalScore.sum()).toEqual(14);
+});
+
 });
---
+++ spec/PinSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('Pin', function(){
+  var pins_standing;
+
+  beforeEach(function(){
+    pins_standing = new Pin();
+  });
+
+});

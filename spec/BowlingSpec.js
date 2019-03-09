e7b652bc7deff04b800349ad542a5359578c5985
Soroush Khosravi <sorush.kh68@gmail.com>
2019-03-09 14:44:11 +0000

    Every frame now knows if it is a spare or not.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -8,6 +8,24 @@ describe("Frame",function(){
       expect(frame.second_role).toEqual(second);
     });
   });
+  describe("Every frame can understand if it is sapre or strike.",function(){
+    it("understads if it is strike.",function(){
+      var frame1 = new Frame(10,0);
+      var frame2 = new Frame(5,5);
+      var frame3 = new Frame(0,10);
+      expect(frame1._isStrike()).toEqual(true);
+      expect(frame2._isStrike()).toEqual(false);
+      expect(frame3._isStrike()).toEqual(false);
+    });
+    it("understads if it is spare.",function(){
+      var frame1 = new Frame(10,0);
+      var frame2 = new Frame(5,5);
+      var frame3 = new Frame(0,10);
+      expect(frame1._isSpare()).toEqual(false);
+      expect(frame2._isSpare()).toEqual(true);
+      expect(frame3._isSpare()).toEqual(true);
+    });
+  });
 
 
 
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,3 +2,13 @@ function Frame(first_role , second_role){
   this.first_role = first_role ;
   this.second_role = second_role;
 };
+
+Frame.prototype._isStrike = function(){
+  if(this.first_role === 10){return(true)}else{return(false)};
+}
+
+Frame.prototype._isSpare = function(){
+  if(this.first_role !== 10 && this.first_role + this.second_role === 10 ){
+    return(true)
+  }else{return(false)};
+}

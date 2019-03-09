6d0331285319c6f34e586e5a84796814f7946998
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-09 12:50:02 +0000

    pass test for frame saves the score of the first bowl

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -3,4 +3,9 @@ describe('frame', function() {
         frame = new Frame();
         expect(frame.total).toEqual(0);
     });
+    it('saves the score of the first bowl', function() {
+        frame = new Frame();
+        frame.firstBowl(3);
+        expect(frame.bowl1).toEqual(3);
+    })
 });
--- src/frame.js
+++ src/frame.js
@@ -1,7 +1,12 @@
 function Frame() {
     this.total = 0;
+    this.bowl1 = 0;
 };
 
 Frame.prototype.getTotal = function() {
     return this.total;
 };
+
+Frame.prototype.firstBowl = function(pins) {
+    this.bowl1 = pins;
+}

32d3883a2035104ad9d122e8267684f50fb4c1ca
“Brenda” <odonnell.brenda@gmail.com>
2019-03-10 10:13:46 +0000

    a basic frame: two rolls are stored in a frame

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -8,8 +8,24 @@ describe('Scorecard', function(){
   })
 
   it ('should be an empty scorecard', function() {
-    expect(scorecard.frames).toEqual([]);
+    expect(scorecard.allFrames).toEqual([[], [], [], [], [], [], [], [], [], []]);
   })
 
+  it ('should store the value of the first roll', function () {
+    scorecard.firstRoll(1)
+    expect(scorecard.roll1).toEqual(1)
+  })
 
+  it ('should store the value of the second roll', function() {
+    scorecard.secondRoll(9)
+    expect(scorecard.roll2).toEqual(9)
+  })
+
+  it ("should store the values of the two rolls in one frame", function () {
+    scorecard.firstRoll(1)
+    scorecard.secondRoll(9)
+    scorecard.eachFrame()
+    expect(scorecard.frame).toEqual([1,9])
+
+  })
 })
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,4 +1,21 @@
 function Scorecard() {
   this.totalScore = 0
-  this.frames = []
+  this.allFrames = [[], [], [], [], [], [], [], [], [], []]
+  this.frame = []
+  this.roll1 = 0
+  this.roll2 = 0
+}
+
+
+Scorecard.prototype.firstRoll = function(pinsDown) {
+  this.roll1 = pinsDown
+}
+
+Scorecard.prototype.secondRoll = function(pinsDown) {
+  this.roll2 = pinsDown
+}
+
+Scorecard.prototype.eachFrame = function() {
+  this.frame.push(this.roll1)
+  this.frame.push(this.roll2)
 }

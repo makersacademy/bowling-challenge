7b6b51d808a1fc1c3fcb345a53255a9ed961af1b
“Brenda” <odonnell.brenda@gmail.com>
2019-03-10 13:40:35 +0000

    starting again, keeping it simple

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -8,57 +8,23 @@ describe('Scorecard', function() {
 
   describe("At the start of the game:", function() {
 
-        it ('has a score of zero', function() {
-          expect(scorecard.totalScore).toEqual(0)
-        })
-
         it ('starts as an empty scorecard', function() {
-          expect(scorecard.allFrames).toEqual( [[], [], [], [], [], [], [], [], [], []] );
+          expect(scorecard.score).toEqual([])
         })
 
+        it ('the array of frames scores is empty', function() {
+          expect(scorecard.frames).toEqual( [[], [], [], [], [], [], [], [], [], []] );
+        })
   })
 
-  describe("After each round:", function() {
-
-        it ('stores the value of the first roll', function () {
-          scorecard.firstRoll(1)
-          expect(scorecard.roll1).toEqual(1)
-        })
-
-        it ('stores the value of the second roll', function() {
-          scorecard.secondRoll(9)
-          expect(scorecard.roll2).toEqual(9)
-        })
-
-        it ("stores the values of the two rolls in one frame", function () {
-          scorecard.firstRoll(1)
-          scorecard.secondRoll(9)
-          scorecard.eachFrame()
-          expect(scorecard.frame).toEqual([1,9])
-        })
-
-        it ("if the first roll is 10, we have 'stirke' and the next roll is 0", function () {
-          scorecard.firstRoll(10)
-          scorecard.roll2 = 0
-          scorecard.eachFrame()
-          expect(scorecard.frame).toEqual([10, 0])
-       })
-
-       it ("for each frame the score for the round is the sum of the two rolls", function() {
-         scorecard.firstRoll(1)
-         scorecard.secondRoll(9)
-         scorecard.eachFrame()
-         scorecard.runningTotal()
-         expect(scorecard.score).toEqual(10)
-
-       })
-
-       // it ("once there have been two rolls, the program stores the frame", function () {
-       //   scorecard.firstRoll(1)
-       //   scorecard.secondRoll(9)
-       //   scorecard.eachFrame()
-       //   expect(scorecard.allFrames[0]).toEqual([[1,9]])
-       // })
-    })
-
 })
+  //
+  // describe("After each round:", function() {
+  //       it ('stores the value of the roll', function () {
+  //         scorecard.roll(1)
+  //         expect(scorecard.score).toEqual(1)
+  //       })
+  //
+  //   })
+
+// })
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,38 +1,10 @@
 function Scorecard() {
-  this.totalScore = 0
-  this.score = 0
-  this.allFrames = [[], [], [], [], [], [], [], [], [], []]
-  this.frame = []
-  this.roll1 = 0
-  this.roll2 = 0
+  this.score = []
+  this.frames = [[], [], [], [], [], [], [], [], [], []]
+  this.roll = 0
+  // this.roll2 = 0
 }
 
-
-Scorecard.prototype.firstRoll = function(pinsDown) {
-  this.roll1 = pinsDown
+Scorecard.prototype.roll = function(pinsDown) {
+  this.roll = pinsDown
 };
-
-Scorecard.prototype.secondRoll = function(pinsDown) {
-  this.roll2 = pinsDown
-};
-
-Scorecard.prototype.eachFrame = function() {
-
-      if (this.roll1 == 10) {
-        this.roll12 = 0
-        this.frame.push(10)
-        this.frame.push(0)
-      } else {
-        this.frame.push(this.roll1)
-        this.frame.push(this.roll2)
-      }
-
-      if (this.frame.length == 2) {
-        this.allFrames[0].push(this.frame)
-      }
-  };
-
-
-Scorecard.prototype.runningTotal = function() {
-  this.score = this.frame[0] + this.frame[1]
-}

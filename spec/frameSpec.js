57f7084ec493c667250a2de04a8cfdeaf63134ff
“Brenda” <odonnell.brenda@gmail.com>
2019-03-10 17:05:55 +0000

    changed name of the rollScore method

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,12 +1,13 @@
 describe('Frame', function() {
 
   var frame;
+  // var next;
 
   // describe("Frame", function {
 
     it("holds the total of two rolls", function() {
       frame = new Frame([3, 4])
-      frame.total(3, 4)
+      frame.rollScore(3, 4)
       expect(frame._total).toEqual(7)
 
     })
@@ -15,6 +16,13 @@ describe('Frame', function() {
       frame = new Frame([10])
       expect(frame._roll2).toEqual(0)
     })
+
+    // it("if the total for the rolls is 10, the frame is a 'spare'", function() {
+    //   frame = new Frame([7, 3])
+    //   frame.total(7, 3)
+    //   expect(frame.)
+    //
+    // })
 
 // }) // describe block
 
--- src/frame.js
+++ src/frame.js
@@ -7,7 +7,7 @@ function Frame(roll1, roll2) {
 }
 
 
-Frame.prototype.total = function(roll1, roll2) {
+Frame.prototype.rollScore = function(roll1, roll2) {
   this._roll1 = roll1
   this._roll2 = roll2
   this._total = roll1 + roll2

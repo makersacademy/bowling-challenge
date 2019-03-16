71912a7f0554101a8329a62afbf5a02004da376c
Luke Phyall <lukeprospect@yahoo.com>
2019-03-16 13:47:00 +0000

    Adds ScoreCard constructor function.

---
+++ spec/scoreCardSpec.js
@@ -0,0 +1,13 @@
+describe('scoreCard', function(){
+
+  var scoreCard
+
+  describe('Initialization check', function(){
+
+    it('Is actually a class', function(){
+      scoreCard = new ScoreCard();
+    });
+
+  });
+
+});

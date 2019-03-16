8997c4f824a6ac824f2011de926ba13e85e1a687
“Brenda” <odonnell.brenda@gmail.com>
2019-03-16 15:21:42 +0000

    it can roll three ones

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -9,4 +9,13 @@ describe('Bowling', function() {
       expect(bowling.rolls).toEqual([])
       })
 
+      it('can roll three ones', function() {
+        bowling.roll(1)
+        bowling.roll(1)
+        bowling.roll(1)
+        expect(bowling.rolls[0]).toEqual(1)
+        expect(bowling.rolls[1]).toEqual(1)
+        expect(bowling.rolls[2]).toEqual(1)
+      })
+
 }) // last brackets
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,5 +1,18 @@
 function Bowling() {
   this.rolls = []
-  // this.pinsDown = 0
-  // this.result = 0
+  this.result = 0
+  this.rollIndex = 0
 }
+
+Bowling.prototype.roll = function(pinsDown) {
+  this.rolls.push(pinsDown);
+  console.log(this.rolls)
+}
+
+  Bowling.prototype.score = function() {
+    for (this.rollIndex = 0; this.rollIndex < this.rolls.length; this.rollIndex++) {
+    this.result += this.rolls[this.rollIndex];
+    }
+    return this.result
+    console.log(this.result)
+  }

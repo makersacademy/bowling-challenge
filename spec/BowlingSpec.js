1916d200308cd3fd2a1e196687644b97d69f5ecb
“Brenda” <odonnell.brenda@gmail.com>
2019-03-18 09:11:45 +0000

    it can score a strike

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,54 +1,55 @@
-describe('Bowling', function() {
-  var bowling
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line prefer-arrow-callback
+describe('Bowling', function () {
+  let bowling;
 
-  beforeEach(function() {
-    bowling = new Bowling()
-  })
+  beforeEach(function () {
+    bowling = new Bowling();
+  });
 
-      it('starts as an empty scorecard', function() {
-      expect(bowling._rolls).toEqual([])
-      })
+      it('starts as an empty scorecard', function () {
+      expect(bowling._rolls).toEqual([]);
+      });
 
-      it('can score a gutter game', function() {
-        var i
-        for (i = 0; i < 20; i ++ ) {
-        bowling.roll(0)
+      it('can score a gutter game', function () {
+        for (let i = 0; i < 20; i++) {
+        bowling.roll(0);
         }
-        // bowling.score()
-        expect(bowling.score()).toEqual(0)
-      })
+        expect(bowling.score()).toEqual(0);
+      });
 
-      it('can score a whole game of ones', function() {
-        rollMany(1, 20)
-        // bowling.score()
-        expect(bowling.score()).toEqual(20)
-      })
+      it('can score a whole game of ones', function () {
+        rollMany(1, 20);
+        expect(bowling.score()).toEqual(20);
+      });
 
-      var rollMany = function(pinsDown, rolls) {
-          var i
-          for (i = 0; i < rolls; i ++) {
-            bowling.roll(pinsDown)
+      // eslint-disable-next-line no-var
+      // eslint-disable-next-line vars-on-top
+      var rollMany = function (pinsDown, rolls) {
+          for (let i = 0; i < rolls; i++) {
+            bowling.roll(pinsDown);
       }
-    }
+    };
 
-    it('can score a spare', function() {
-      bowling.roll(5)
-      bowling.roll(5)
-      bowling.roll(3)
-      var i
-      for (i = 0; i < 17; i ++) {
-      bowling.roll(0)
+    it('can score a spare', function () {
+      bowling.roll(5);
+      bowling.roll(5);
+      bowling.roll(3);
+      for (let i = 0; i < 17; i++) {
+      bowling.roll(0);
       }
-      // bowling.score()
-      expect(bowling.score()).toEqual(16)
-    })
+      expect(bowling.score()).toEqual(16);
+    });
 
-    // it('can score a perfect game', function() {
-    // rollMany(10, 12)
-    // bowling.score()
-    // expect(bowling._result).toEqual(300)
-    //
-    // })
-    //
-
-}) // last brackets
+    it('can score a strike', function () {
+      bowling.roll(10);
+      bowling.roll(3);
+      bowling.roll(1);
+      rollMany(0, 16);
+      expect(bowling.score()).toEqual(18);
+    });
+  
+}); // last brackets

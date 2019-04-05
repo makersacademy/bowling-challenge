a0186f0370cc0e788f7146f21d852a9b5ec553f7
Lauren <laurenqurashi@me.com>
2019-04-05 13:52:05 +0100

    Fifth Commit. Refactored out the currentFrame method.

--- spec/ScoreCardSpec.js
+++ spec/ScoreCardSpec.js
@@ -11,20 +11,20 @@ describe('Score', function() {
   });
 
   it('has a currentScore which starts at 0', function() {
-    expect(bowlingcard.total()).toEqual(0);
+    expect(bowlingcard.currentScore).toEqual(0);
   });
 
-  it('adds a number to the currentScore', function() {
-    bowlingcard.add(6)
-    expect(bowlingcard.total()).toEqual(6)
+  it('adds 2 numbers to the currentScore', function() {
+    bowlingcard.add(6, 4)
+    expect(bowlingcard.currentScore).toEqual(10);
   });
 
-  it('has a currentFrame score that starts at 0', function() {
-    expect(bowlingcard.currentFrame).toEqual(0)
-  });
-
-  it('adds together two scores for the currentFrame', function() {
-    bowlingcard.currentBowl(1,2)
-    expect(bowlingcard.currentFrame).toEqual(3)
-  });
+  // it('has a currentFrame score that starts at 0', function() {
+  //   expect(bowlingcard.currentFrame).toEqual(0)
+  // });
+  //
+  // it('adds together two scores for the currentFrame', function() {
+  //   bowlingcard.currentBowl(1,2)
+  //   expect(bowlingcard.currentFrame).toEqual(3)
+  // });
 });
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,15 +1,14 @@
 
 function Score() {
   this.currentScore = 0
-  this.currentFrame = 0
-}
-
-Score.prototype.total = function(){ return this.currentScore; };
-
-Score.prototype.add = function(number) {
-  this.currentScore += number;
+  // this.currentFrame = 0
 };
 
-Score.prototype.currentBowl = function(go1, go2) {
-  this.currentFrame += (go1 + go2);
+// Score.prototype.total = function(){ return this.currentScore; };
+
+Score.prototype.add = function(go1, go2) {
+  this.currentScore += (go1 + go2);
 };
+
+// Score.prototype.currentBowl = function(go1, go2) {
+//   this.currentFrame += (go1 + go2);

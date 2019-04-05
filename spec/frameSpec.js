b146b2d841563931a197515f16db74ffd3edb01c
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-04-05 22:27:36 +0100

    restricted frame to one ball if a strike is scored

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -3,23 +3,34 @@ describe('Frame', function(){
     frame = new Frame();
   });
 
-  it("allows a score to be recorded for a frame", function(){
-    frame.recordScore(3);
-    expect(frame.balls).toContain(3);
+  describe("adding scores below 10", function(){
+    it("allows a score to be recorded for a frame", function(){
+      frame.recordScore(3);
+      expect(frame.balls).toContain(3);
+    });
+
+    it("sets the frame to not complete when scores have not been entered", function(){
+      expect(frame.isComplete()).toBe(false);
+    });
+
+    it("sets the frame to be complete after two scores equalling less than 10", function(){
+      frame.recordScore(3);
+      frame.recordScore(4);
+      expect(frame.isComplete()).toBe(true);
+    });
+
+    it("doesn't add further scores to the frame after completion", function(){
+      for(i=0; i<3; i++) { frame.recordScore(3); }
+      expect(frame.balls.length).not.toBe(3);
+    });
   });
 
-  it("sets the frame to not complete when scores have not been entered", function(){
-    expect(frame.isComplete()).toBe(false);
+  describe("adding a strike", function(){
+    it("sets the frame to complete after first roll of a strike", function(){
+      frame.recordScore(10);
+      expect(frame.isComplete()).toBe(true);
+    });
   });
 
-  it("sets the frame to be complete after two scores equalling less than 10", function(){
-    frame.recordScore(3);
-    frame.recordScore(4);
-    expect(frame.isComplete()).toBe(true);
-  });
 
-  it("doesn't add further scores to the frame after completion", function(){
-    for(i=0; i<3; i++) { frame.recordScore(3); }
-    expect(frame.balls.length).not.toBe(3);
-  });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,12 +1,14 @@
 function Frame(){
   this.balls = [];
   this.completed = false;
+  this.STRIKE = 10;
 };
 
 
 Frame.prototype.recordScore = function(score){
-  if (this.completed != true){
+  if (this.completed != true) {
     this.balls.push(score);
+    if (score === this.STRIKE) { this.completed = true; }
     if (this.balls.length === 2) { this.completed = true; }
   }
 };

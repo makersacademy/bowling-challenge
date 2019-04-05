eaec4d7043d9b88062a4cdefdebc1de1b172c46d
Sam Ixer <samantha.ixer@johnlewis.co.uk>
2019-04-05 22:17:18 +0100

    changed recordScore to move to set the frame to complete after 2 rolls

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -7,4 +7,14 @@ describe('Frame', function(){
     frame.recordScore(3);
     expect(frame.balls).toContain(3);
   });
+
+  it("sets the frame to not complete when scores have not been entered", function(){
+    expect(frame.isComplete()).toBe(false);
+  });
+
+  it("sets the frame to be complete after two scores equalling less than 10", function(){
+    frame.recordScore(3);
+    frame.recordScore(4);
+    expect(frame.isComplete()).toBe(true);
+  });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,8 +1,14 @@
 function Frame(){
   this.balls = [];
+  this.completed = false;
 };
 
 
 Frame.prototype.recordScore = function(score){
   this.balls.push(score);
+  if (this.balls.length === 2) { this.completed = true; }
+};
+
+Frame.prototype.isComplete = function(){
+  return this.completed;
 };

11cbe315d8b9035fb7a659d9bef61c927912a780
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-06 18:20:20 +0100

    Only takes 10 frames

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -20,6 +20,14 @@ describe('Scorecard', function() {
       };
       expect(scorecard.frames.length).toEqual(10);
     });
-  })
+
+    it('takes no more than 10 frames', function() {
+      for(var i=0; i < 10; i++){
+        scorecard.takeFrames(frame)
+      };
+      scorecard.takeFrames(frame)
+      expect(scorecard.frames.length).toEqual(10);
+    });
+  });
 
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -3,6 +3,9 @@ function Scorecard() {
 };
 
 Scorecard.prototype.takeFrames = function (frame) {
+  if (this.frames.length === 10) {
+    return;
+  }
   this.frames.push(frame);
 };
 

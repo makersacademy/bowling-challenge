177f198a409ee3683883a493d2c788059d47992e
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-06 17:51:47 +0100

    Takes in frames

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -10,4 +10,14 @@ describe('Scorecard', function() {
   it('frames is empty as default', function() {
     expect(scorecard.frames.length).toEqual(0);
   });
+
+  describe('#takeFrames', function() {
+    it('accepts 10 frames', function() {
+      for(var i=0; i < 10; i++){
+        scorecard.takeFrames(frame)
+      };
+      expect(scorecard.frames.length).toEqual(10);
+    });
+  })
+
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -2,6 +2,10 @@ function Scorecard() {
   this.frames = [];
 };
 
+Scorecard.prototype.takeFrames = function (frame) {
+  this.frames.push(frame);
+};
+
 function Frame() {
 
 };

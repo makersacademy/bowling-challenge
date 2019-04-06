ac5df7e7e1419885d49058dc344a72aaae4d8de8
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-06 17:55:26 +0100

    Test refactor for consistent pass

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -4,8 +4,10 @@ describe('Scorecard', function() {
   var scorecard;
   var frame;
 
-  scorecard = new Scorecard();
-  frame = new Frame();
+  beforeEach(function(){
+    scorecard = new Scorecard();
+    frame = new Frame();
+  })
 
   it('frames is empty as default', function() {
     expect(scorecard.frames.length).toEqual(0);

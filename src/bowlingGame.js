5fabe66349faaeceb0a508d5a008a1015100cd13
fo0s <fo0sies@gmail.com>
2019-04-07 19:34:18 +0100

    Cleaned out console.log tests dotted arround

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -13,19 +13,15 @@ bowlingGame.prototype.addFrames = function(frame){
     if (_isNotValidFrame(frame)){   
         return 'Error: Frame has incorrect values';
     } else {
-    this.frame.push(frame);
+        this.frame.push(frame);
     }
-    console.log("Checkpoint 1 " + frame);
     this.setState(frame);
 };
 
 bowlingGame.prototype.setState = function(values){
-    console.log("Checkpoint 2 " + values);
     if (_isEqualToTen(values)){
-        console.log("Checkpoint 3 " + values);
         this.state = _checkSpareOrStrike(values);
     } else {
-        console.log("Checkpoint 4 " + values);
         this.state = 'normal';
     }
 };
@@ -35,16 +31,13 @@ function getSum(total, num) {
 }
 
 function _isNotValidFrame(frame){
-    console.log("Checkpoint 6 " + frame);
     if (frame.reduce(getSum) > 10) { return true; }
 }
 
 function _isEqualToTen(frame){
-    console.log("Checkpoint 7 " + frame);
     return (frame.reduce(getSum) === 10) ? true : false;
 }
 
 function _checkSpareOrStrike(values) {
-    console.log("Checkpoint 5 " + values);
     return values.length === 2 ? 'spare' : 'strike';
 }

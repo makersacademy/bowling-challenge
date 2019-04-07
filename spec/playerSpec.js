054cb0fe2a2040f6829164b49805de22cc55d734
kimdiep <kim.diep.techno@gmail.com>
2019-04-07 10:07:13 +0100

    Added rollType function to player object

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -11,5 +11,12 @@ describe("Player", function () {
     it("returns the number of pins player rolled over", function () {
       expect(player.roll(5)).toEqual(5);
     });
-  });  
+  });
+
+  describe('rollType', function () {
+    it("returns the rollType - gutter - if the user knocked down no pins", function () {
+      expect(player.rollType(0)).toEqual("gutter");
+    });
+  });
+
 });

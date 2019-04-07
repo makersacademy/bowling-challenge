28a95f519f5a2956494ed7c01d203fdfb8a96bc4
fo0s <fo0sies@gmail.com>
2019-04-07 13:38:58 +0100

    Basics; 'stores name of 1 player' test fulfilled

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -4,12 +4,17 @@ describe("Javascript bowling score keeper: ", function(){
         it("starts a new game", function(){
             game = new bowlingGame();
             expect(game.score).toBe(0);
-        })
+        });
 
         it("keeps track of frames", function(){
-            game = new bowlingGame
+            game = new bowlingGame();
             expect(game.frame).toBe(0);
-        })
+        });
 
-    })
-})
+        it("stores name of 1 player", function(){
+            game = new bowlingGame();
+            game.addPlayers('theDude');
+            expect(game.players[0]).toBe('theDude');
+        });
+    });
+});
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,4 +1,9 @@
 function bowlingGame() { 
     this.score = 0;
     this.frame = 0;
+    this.players = [];
+}
+
+bowlingGame.prototype.addPlayers = function(playerName){
+    this.players.push(playerName);
 };

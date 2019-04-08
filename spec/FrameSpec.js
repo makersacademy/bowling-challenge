10f7a26182bf6f25e739115470c07ab95d5be3d1
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-08 09:57:17 +0100

    Added round score method

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -4,7 +4,13 @@ describe('Frame', function() {
   it('returns scores in array', function() {
     frame = new Frame(2,3);
     expect(frame.turn).toEqual([2,3]);
-    // expect(frame.firstTry).toEqual(2);
-    // expect(frame.secondTry).toEqual(3);
-  })
+  });
+
+  describe('#roundScore', function() {
+    it('adds up frame result to score', function() {
+      frame = new Frame(2,3);
+      frame.roundScore();
+      expect(frame.score).toEqual(5)
+    });
+  });
 });

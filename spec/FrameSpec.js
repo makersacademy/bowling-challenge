e42a3c28225e906afc6bec3053e2fae9adde5bb5
Amy Jordan <amy_jordan@live.co.uk>
2019-04-08 20:27:23 +0100

    Stores pins from first roll

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,9 +1,19 @@
 describe("Frame", function() {
 
-  var frame = new Frame();
+  var frame;
+
+  beforeEach(function(){
+    frame = new Frame();
+  });
 
   it('has two rolls', function() {
     expect(frame.firstRoll).toEqual(null);
     expect(frame.secondRoll).toEqual(null);
-  })
+  });
+
+  it('can add one roll', function() {
+    frame.roll(3);
+    expect(frame.firstRoll).toEqual(3);
+    expect(frame.secondRoll).toEqual(null);
+  });
 })
--- src/Frame.js
+++ src/Frame.js
@@ -2,3 +2,12 @@ function Frame() {
   this.firstRoll = null;
   this.secondRoll = null;
 }
+
+Frame.prototype = {
+
+  constructor: Frame,
+
+  roll: function(pins) {
+    this.firstRoll = pins;
+  }
+}

0893a04a26931a9188f1f48e8aa7f3d9f7494b58
Amy Jordan <amy_jordan@live.co.uk>
2019-04-09 22:41:09 +0100

    Adds feature test for complete game with two strikes in a row

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -163,4 +163,16 @@ describe('Feature Test', function() {
     })
   })
 
+  describe('complete game with two strikes in a row', function() {
+  it('hits strikes on first and second frame', function() {
+    scorecard.roll(10)
+    scorecard.roll(10)
+    for (i = 1; i <= 16; i++) {
+      scorecard.roll(4);
+    }
+    expect(scorecard.total()).toEqual(106)
+    expect(scorecard.isComplete()).toEqual(true)
+  })
+})
+
 });

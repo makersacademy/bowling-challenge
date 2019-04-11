36a0429663a254badbe390958cda4dd2028a666b
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-11 20:39:13 +0100

    Takes first two rolls

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,9 +1,27 @@
 describe("Frame", function() {
 
-  var frame = new Frame();
+  var frame;
 
-  it('has two rolls', function() {
+  beforeEach(function(){
+    frame = new Frame();
+  });
+
+  it('takes two rolls', function() {
     expect(frame.firstRoll).toEqual(null);
     expect(frame.secondRoll).toEqual(null);
-  })
-})
+  });
+
+  it('can add first roll', function() {
+    frame.roll(2);
+    expect(frame.firstRoll).toEqual(2);
+    expect(frame.secondRoll).toEqual(null);
+  });
+
+  it('can add second roll', function() {
+   frame.roll(2);
+   frame.roll(3);
+   expect(frame.firstRoll).toEqual(2);
+   expect(frame.secondRoll).toEqual(3);
+ });
+
+});
--- src/Frame.js
+++ src/Frame.js
@@ -2,3 +2,16 @@ function Frame() {
   this.firstRoll = null;
   this.secondRoll = null;
 }
+
+Frame.prototype = {
+
+   constructor: Frame, // tider syntax
+
+   roll: function(pins) {
+     if (this.firstRoll === null) {
+     this.firstRoll = pins;
+   } else {
+     this.secondRoll = pins;
+   }
+  }
+}

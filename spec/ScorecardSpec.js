25e60d15effb4b650b017175550bc77f95547308
James Palmer <jamesdcpalmer1@gmail.com>
2019-04-11 21:38:56 +0100

    Basic properties set up

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -6,4 +6,9 @@ describe('Scorecard', function() {
   it('frames begins empty', function(){
     expect(scorecard.frames.length).toEqual(0)
   });
+  it('basic bowling set up', function(){
+    expect(scorecard.score).toEqual(0);
+    expect(scorecard.framesMax).toEqual(10);
+    expect(scorecard.frameNumber).toEqual(1);
+  });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,3 +1,7 @@
-function Scorecard() {
+function Scorecard(frame = Frame) {
+  this.frame = frame
   this.frames = []
+  this.framesMax = 10;
+  this.score = 0;
+  this.frameNumber = 1;
 }

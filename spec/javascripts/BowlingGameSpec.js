66b3b19420f85cdc6c811b777a16b5702fda5323
masha-b <mashabudryte@gmail.com>
2019-04-14 19:56:35 +0100

    the maxx number of frames is 10

--- spec/javascripts/BowlingGameSpec.js
+++ spec/javascripts/BowlingGameSpec.js
@@ -9,4 +9,34 @@ describe('Bowling', function() {
 
     expect(bowling.result()).toEqual(0);
   });
+
+  it('adds a frame result into array after two rolls', function(){
+    const bowling = new Bowling();
+    const times = 2;
+    for (let i=0; i < times; i++) {
+      bowling.roll(0);
+    };
+
+    expect(bowling._frames.length).toEqual(1);
+  });
+
+  it('adds a frame result into array after four rolls', function(){
+    const bowling = new Bowling();
+    const times = 4;
+    for (let i=0; i < times; i++) {
+      bowling.roll(0);
+    };
+
+    expect(bowling._frames.length).toEqual(2);
+  });
+
+  it('no result stored after 10 frames', function(){
+    const bowling = new Bowling();
+    const times = 30;
+    for (let i=0; i < times; i++) {
+      bowling.roll(0);
+    };
+
+    expect(bowling._frames.length).toEqual(10);
+  });
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,11 +1,15 @@
-function Bowling() {
+function Bowling(frame = new Frame()) {
+  this._currentFrame = frame;
   this._result = 0;
-  this._frames = []
+  this._frames = [];
 };
 
 Bowling.prototype.roll = function(pins) {
- var frame = new Frame();
- frame.roll(pins);
+  this._currentFrame.roll(pins);
+  if (this._currentFrame.isOver() && this._frames.length < 10){
+    this._frames.push(this._currentFrame);
+    this._currentFrame = new Frame();
+  };
 };
 
 

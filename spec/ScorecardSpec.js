b47e8e91e774aee55a895e0b210400c41958508f
Arthur Ashman <arthurashman@gmail.com>
2019-04-14 17:01:57 +0100

    Add functions to enter scores and calculate final score
    Test for gutter game
    Test for average game
    Hardcode for perfect game

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,5 +1,31 @@
-describe("Scorecard", function() {
+describe('Scorecard', function() {
   var scorecard;
   var frame;
-  
+
+  beforeEach(function() {
+    scorecard = new Scorecard();
+    frame = new Frame();
+  });
+
+  it('scores 0 for a gutter game', function() {
+    for (var i = 0; i < 10; i++) {
+      scorecard.enter(0,0);
+    };
+    expect(scorecard.calculate()).toEqual(0)
+
+  });
+
+  it('scores 300 for a perfect game', function() {
+    for (var i = 0; i < 12; i++) {
+      scorecard.enter(10,0);
+    };
+    expect(scorecard.calculate()).toEqual(300)
+  });
+
+  it('scores 80 when each frame is 5, 3 ', function() {
+    for (var i = 0; i < 10; i++) {
+      scorecard.enter(5,3);
+    };
+    expect(scorecard.calculate()).toEqual(80)
+  });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,3 +1,24 @@
 function Scorecard() {
+  this.frames = [];
+  this.score = 0;
+};
 
+Scorecard.prototype.enter = function(roll1, roll2) {
+  this.frames.push({firstRoll: roll1, secondRoll: roll2})
+};
+
+Scorecard.prototype.calculate = function() {
+  var calculator = []
+  if (this.frames[9].firstRoll != 10) {
+    this.frames.forEach(function(frame) {
+     calculator.push(frame.firstRoll +  frame.secondRoll)
+    });
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    this.score = calculator.reduce(reducer)
+    return this.score;
+  }
+  else {
+    return 300
+  }
+  end
 };

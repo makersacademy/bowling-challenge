5f9e30306874c43ef12fafa245b18eddb7579f88
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-14 16:45:05 +0100

    ADD: Frame scores display on page
    Added a displayScore function to interface and implemented it

--- interface.js
+++ interface.js
@@ -7,22 +7,29 @@ $(document).ready(function() {
     var pins = parseInt($('#pins-knocked-down').val(), 10);
     scorecard.roll(pins);
     displayRolls();
+    displayScore();
 
   });
 
   function displayRolls() {
     scorecard.frames.forEach(function(frame) {
-      console.log(frame)
-      if(frame.rolls()[0] === 10) {
-        $(`#pins-${frame.index + 1}-1`).text("\\/")
-        $(`#pins-${frame.index + 1}-2`).text("/\\")
-      } else if(typeof(frame.rolls()[1]) === 'number') {
+      if(frame.index === 9) {
+        $(`#pins-10-1`).text(frame.rolls()[0])
+        $(`#pins-10-2`).text(frame.rolls()[1])
+        $(`#pins-10-3`).text(frame.rolls()[2])
+      } else if(frame.rolls()[0] === 10) {
+        $(`#pins-${frame.index + 1}-2`).text(10)
+      } else {
         $(`#pins-${frame.index + 1}-1`).text(frame.rolls()[0])
-        var secondRoll = frame.rolls()[1] + frame.rolls()[0] === 10 ? '/' : frame.rolls()[1]
-        $(`#pins-${frame.index + 1}-2`).text(secondRoll)
-      } else if(typeof(frame.rolls()[0]) === 'number') {
-        $(`#pins-${frame.index + 1}-1`).text(frame.rolls()[0])
+        $(`#pins-${frame.index + 1}-2`).text(frame.rolls()[1])
       };
     });
+  };
+
+  function displayScore() {
+    for(i=0;i<9;i++) {
+      $(`#score-${i + 1}-2`).text(scorecard.frameScores[i]);
+    };
+    $(`#score-10-3`).text(scorecard.frameScores[9]);
   };
 });

99fc182e105bd9687020295c3c5af3640a935cc4
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-04-14 17:21:04 +0100

    ADD: Updated bonus messages for final frame

--- interface.js
+++ interface.js
@@ -28,10 +28,16 @@ $(document).ready(function() {
   };
   function displayBonus() {
     scorecard.frames.forEach(function(frame) {
+      var spareMessage = "Spare! The next roll will be added to this frame's score"
+      var strikeMessage = "Strike! The next two rolls will be added to this frame's score"
+      if(frame.index === 9) {
+        spareMessage = 'Spare! You get a bonus roll!'
+        strikeMessage = 'Strike! You get 2 bonus rolls!'
+      };
       if(frame.rolls()[0] === 10) {
-        $(`#bonus-${frame.index + 1}-2`).text("Strike! The next two rolls will be added to this frame's score")
+        $(`#bonus-${frame.index + 1}-2`).text(strikeMessage)
       } else if(typeof(frame.rolls()[1]) === 'number' && frame.rolls()[0] + frame.rolls()[1] > 9) {
-        $(`#bonus-${frame.index + 1}-2`).text("Spare! The next roll will be added to this frame's score")
+        $(`#bonus-${frame.index + 1}-2`).text(spareMessage)
       } else {
       };
     });

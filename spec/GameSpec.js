3ee5bf671b41e5cb4de0876d1234ba1b0ffc3a37
Paul Martin <communty.hivemind@gmail.com>
2019-04-14 15:53:28 +0100

    Implement basic frame scoring
    Strikes and spares ignored so far.

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,11 +1,45 @@
 describe('Game', function () {
+  var game
+
   if (typeof (require) !== 'undefined') {
     var Game = require('../src/Game')
   }
 
-  describe('something', function () {
-    it("does something", function () {
-      expect(true).toBe(true)
+  beforeEach(function () {
+    game = new Game ()
+  })
+
+  describe('Add one roll', function () {
+    it('No frames are scored', function () {
+      game.roll(0)
+      expect(game.score()).toEqual([])
+    })
+  })
+
+  describe('Add a basic frame', function () {
+    it('One frame is scored', function () {
+      game.roll(0)
+      game.roll(1)
+      expect(game.score()).toEqual([1])
+    })
+  })
+
+  describe('Add a basic frame plus one roll', function () {
+    it('One frame is scored', function () {
+      game.roll(0)
+      game.roll(1)
+      game.roll(2)
+      expect(game.score()).toEqual([1])
+    })
+  })
+
+  describe('Add two basic frames', function () {
+    it('Two frames are scored', function () {
+      game.roll(0)
+      game.roll(1)
+      game.roll(2)
+      game.roll(3)
+      expect(game.score()).toEqual([1, 6])
     })
   })
 })
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,48 @@
 function Game () {
+  this._rolls = []
+  this.runningTotals = []
+  this._frameBall = 0
 }
 
-console.log('hello')
-
 Game.prototype = {
-  constructor: Game
+  constructor: Game,
+
+  roll: function (roll) {
+    this._rolls.push(roll)
+    this._updateFrameBall()
+    this._checkScoring()
+  },
+
+  score: function () {
+    return this.runningTotals
+  },
+
+  _checkScoring: function () {
+    if (this._frameBall === 2) {
+      this.runningTotals.push(this._lastRunningTotal() +
+                              this._lastRoll() +
+                              this._secondLastRoll())
+    }
+  },
+
+  _updateFrameBall: function () {
+    this._frameBall = (this._frameBall % 2) + 1
+  },
+
+  _lastRoll: function () {
+    return this._rolls[this._rolls.length - 1]
+  },
+
+  _secondLastRoll: function () {
+    return this._rolls[this._rolls.length - 2]
+  },
+
+  _lastRunningTotal: function () {
+    if (this.runningTotals.length === 0) {
+      return 0
+    }
+    return this.runningTotals[this.runningTotals.length - 1]
+  }
 }
 
 if (typeof (module) !== 'undefined') {

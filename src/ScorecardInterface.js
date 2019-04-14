45293d3b259ce4a574e7d2f9a17dcf788aa148aa
Amy Jordan <amy_jordan@live.co.uk>
2019-04-14 12:17:12 +0100

    Updates score with bonus when previous frame is strike

--- src/ScorecardInterface.js
+++ src/ScorecardInterface.js
@@ -16,8 +16,13 @@ $(document).ready(function() {
     }
 
     if (scorecard._currentFrame().isComplete()) {
+      $(`#frame-${scorecard.frameNumber - 1}-roll-1`).text(scorecard._currentFrame().firstRoll);
       $(`#frame-${scorecard.frameNumber - 1}-roll-2`).text(scorecard._currentFrame().secondRoll);
       $(`#frame-score-${scorecard.frameNumber - 1}`).text(scorecard._currentFrame().score());
+    }
+
+    if (scorecard._previousFrame().isStrike()) {
+      $(`#frame-score-${scorecard.frameNumber - 2}`).text(scorecard._previousFrame().score());
     }
   };
 

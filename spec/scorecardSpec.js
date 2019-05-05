e5bb3a86996576d877c671e00ea1afcf62b7aba9
Hannah Frost <h.7frost@gmail.com>
2019-05-05 21:48:33 +0100

    Increases frame number after strike

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,4 +8,9 @@ describe('Scorecard', function() {
   it("should start with a total score of 0", function() {
     expect(scorecard.showTotal()).toEqual(0)
   });
+
+  it("increases frame number after strike", function() {
+    scorecard.roll(10);
+    expect(scorecard.showFrame()).toEqual(1)
+  });
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,7 +1,21 @@
 function Scorecard() {
   this.totalScore = 0;
+  this.frameNumber = 0;
+  this.rolls = [];
 };
 
 Scorecard.prototype.showTotal = function() {
   return this.totalScore;
+};
+
+Scorecard.prototype.showFrame = function() {
+  return this.frameNumber;
+};
+
+Scorecard.prototype.roll = function(score) {
+  if (score === 10) {
+    this.frameNumber += 1;
+    this.rolls.push("X");
+  } else {
+  }
 };

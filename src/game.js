d4bd75ae32b06031a825d2b658fb70b11d7da716
Jordan Roberts <jordanroberts@talk21.com>
2019-05-05 23:46:32 +0100

    Game object amended so initialized with array of 10 frames

--- src/game.js
+++ src/game.js
@@ -1,4 +1,6 @@
 function Game(){
   this.frames = []
-  for(i = 0; i < 10; i ++) { this.frames.push(new Frame)}
+  for(i = 0; i < 10; i ++) { this.frames.push(new Frame()}
 };
+
+frame = new Frame() 

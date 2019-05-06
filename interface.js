c8366e431035b52807062e037fa0a1c5032326c0
Chenbo Yi <yichenbo@outlook.com>
2019-05-06 01:09:55 +0100

    refactor interface.js

--- interface.js
+++ interface.js
@@ -3,77 +3,15 @@ $(document).ready(function () {
 
   var bowling = new Bowling();
 
-  $('#submit1').on('click', function () {
-    bowling.knock(Number($('#fram1roll1').val()));
-    if (Number($('#fram1roll1').val()) < 10) {
-      bowling.knock(Number($('#fram1roll2').val()));
-    };
-    updateprevioustotal(1);
-  });
-
-  $('#submit2').on('click', function () {
-    bowling.knock(Number($('#fram2roll1').val()));
-    if (Number($('#fram2roll1').val()) < 10) {
-      bowling.knock(Number($('#fram2roll2').val()));
-    };
-    updateprevioustotal(2);
-  });
-
-  $('#submit3').on('click', function () {
-    bowling.knock(Number($('#fram3roll1').val()));
-    if (Number($('#fram3roll1').val()) < 10) {
-      bowling.knock(Number($('#fram3roll2').val()));
-    };
-    updateprevioustotal(3);
-  });
-
-  $('#submit4').on('click', function () {
-    bowling.knock(Number($('#fram4roll1').val()));
-    if (Number($('#fram4roll1').val()) < 10) {
-      bowling.knock(Number($('#fram4roll2').val()));
-    };
-    updateprevioustotal(4);
-  });
-
-  $('#submit5').on('click', function () {
-    bowling.knock(Number($('#fram5roll1').val()));
-    if (Number($('#fram5roll1').val()) < 10) {
-      bowling.knock(Number($('#fram5roll2').val()));
-    };
-    updateprevioustotal(5);
-  });
-
-  $('#submit6').on('click', function () {
-    bowling.knock(Number($('#fram6roll1').val()));
-    if (Number($('#fram6roll1').val()) < 10) {
-      bowling.knock(Number($('#fram6roll2').val()));
-    };
-    updateprevioustotal(6);
-  });
-
-  $('#submit7').on('click', function () {
-    bowling.knock(Number($('#fram7roll1').val()));
-    if (Number($('#fram7roll1').val()) < 10) {
-      bowling.knock(Number($('#fram7roll2').val()));
-    };
-    updateprevioustotal(7);
-  });
-
-  $('#submit8').on('click', function () {
-    bowling.knock(Number($('#fram8roll1').val()));
-    if (Number($('#fram8roll1').val()) < 10) {
-      bowling.knock(Number($('#fram8roll2').val()));
-    };
-    updateprevioustotal(8);
-  });
-
-  $('#submit9').on('click', function () {
-    bowling.knock(Number($('#fram9roll1').val()));
-    if (Number($('#fram9roll1').val()) < 10) {
-      bowling.knock(Number($('#fram9roll2').val()));
-    };
-    updateprevioustotal(9);
-  });
+  responseClick(1);
+  responseClick(2);
+  responseClick(3);
+  responseClick(4);
+  responseClick(5);
+  responseClick(6);
+  responseClick(7);
+  responseClick(8);
+  responseClick(9);
 
   $('#submit10').on('click', function () {
     var num_f10r1 = Number($('#fram10roll1').val());
@@ -86,6 +24,16 @@ $(document).ready(function () {
 
     updateprevioustotal(10);
   });
+
+  function responseClick(number) {
+    $('#submit' + number).on('click', function () {
+      bowling.knock(Number($('#fram' + number + 'roll1').val()));
+      if (Number($('#fram' + number + 'roll1').val()) < 10) {
+        bowling.knock(Number($('#fram' + number + 'roll2').val()));
+      };
+      updateprevioustotal(number);
+    });
+  };
 
   function updateprevioustotal(number) {
     if (number > 2) {
@@ -100,6 +48,4 @@ $(document).ready(function () {
     };
 
   }
-
-
 });

7a3d901cf49269a875e1bff0b27abf386cd6cc77
Callum Marshall <callum_marshall@hotmail.co.uk>
2019-05-06 11:08:19 +0100

    added ball1 and ball2 methods

--- bowling-challenge/spec/scorecardSpec.js
+++ bowling-challenge/spec/scorecardSpec.js
@@ -1,3 +1,4 @@
+'use strict'
 
 describe('scorecard', function() {
 
@@ -9,10 +10,18 @@ describe('scorecard', function() {
 
   describe('gutter game', function(){
     it('ends game', function() {
-      expect(scorecard.isOver).toBe(true)
+      expect(scorecard.over).toBe(true)
     })
     it('with a score of 0', function() {
       expect(scorecard.score).toEqual(0)
+    })
+  })
+
+  describe('entering 2 balls per frame', function(){
+    it('can enter 2 scores', function(){
+      scorecard.ball1(1);
+      scorecard.ball2(2);
+      expect(scorecard.score).toEqual(3)
     })
   })
 
--- bowling-challenge/src/scorecard.js
+++ bowling-challenge/src/scorecard.js
@@ -1,4 +1,22 @@
+'use strict'
+
 function Scorecard() {
   this.score = 0;
-  this.isOver = true;
-}
+  this.over = true;
+};
+
+Scorecard.prototype.isOver = function () {
+  return this.frames === 10;
+};
+
+Scorecard.prototype.frames = function () {
+
+};
+
+Scorecard.prototype.ball1 = function (pins) {
+  return this.score = this.score + pins
+};
+
+Scorecard.prototype.ball2 = function (pins) {
+  return this.score = this.score + pins
+};

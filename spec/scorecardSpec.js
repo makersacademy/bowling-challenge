ac1bcb10088c6b5dbfe2049624e845013ff38542
Oliver Cripps <ollycripps@gmail.com>
2019-05-06 17:31:58 +0100

    Add starting/default state tests and update varible objects for this purprose

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -6,9 +6,17 @@ describe('Bowling ScoreCard', function() {
     scorecard = new ScoreCard();
   });
 
+  it('frames are empty at game start', function() {
+    expect(scorecard.frames()).toEqual([]);
+  });
+
+  it('starts with a score of 0', function() {
+    expect(scorecard._totalScore).toEqual(0);
+  });
+
   describe('Scoring a game', function() {
     it('user can roll a gutter ball', function() {
-      rollHelper(0, 20); 
+      rollHelper(0, 20);
       expect(scorecard.score()).toEqual(0);
     });
 
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,15 +1,19 @@
 function ScoreCard() {
-  this.rolls = [];
+  this._frames = [];
+  this._totalScore = 0;
 } ;
 
+ScoreCard.prototype.frames = function () {
+  return this._frames;
+};
+
 ScoreCard.prototype.roll = function (pins) {
-  this.rolls.push(pins);
+  this._frames.push(pins);
 };
 
 ScoreCard.prototype.score = function () {
-  var totalScore = 0;
   for (var i = 0; i < 20; i++) {
-    totalScore += this.rolls[i];
+    this._totalScore += this._frames[i];
   }
-  return totalScore;
+  return this._totalScore;
 };

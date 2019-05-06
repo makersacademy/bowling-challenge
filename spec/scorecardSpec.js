451b3a370b335e9f10add1791cf11d8f7373f49c
Oliver Cripps <ollycripps@gmail.com>
2019-05-06 15:20:31 +0100

    Update score and roll functions to deal with a game of 1s

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -6,12 +6,19 @@ describe('Bowling ScoreCard', function() {
     scorecard = new ScoreCard();
   });
 
-  describe('Playing a game', function() {
+  describe('Scoring a game', function() {
     it('user can roll a gutter ball', function() {
       for (var i = 0; i < 20; i++) {
         scorecard.roll(0);
       }
       expect(scorecard.score()).toEqual(0);
+    });
+
+    it('user can roll a game of 1s', function() {
+      for (var i = 0; i < 20; i++) {
+        scorecard.roll(1);
+      }
+      expect(scorecard.score()).toEqual(20);
     });
   });
 
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,9 +1,15 @@
-function ScoreCard() {} ;
+function ScoreCard() {
+  this.rolls = [];
+} ;
 
 ScoreCard.prototype.roll = function (pins) {
-
+  this.rolls.push(pins);
 };
 
 ScoreCard.prototype.score = function () {
-  return 0; 
+  var totalScore = 0;
+  for (var i = 0; i < 20; i++) {
+    totalScore += this.rolls[i];
+  }
+  return totalScore;
 };

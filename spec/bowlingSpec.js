9585adf32650a7511ef3963b193f2b0d1eb760aa
Calum Darroch <calum_darroch@hotmail.com>
2019-05-06 23:49:17 +0100

    Gutter game added

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -10,7 +10,7 @@ describe('Game', function() {
 
     it('input the number of pins they have knocked down for one roll', function() {
       game.roll(8);
-      expect(game.frames).toEqual([8])
+      expect(game.points).toEqual([8])
     });
 
     it('NOT input more than 10 for a roll', function() {
@@ -18,6 +18,15 @@ describe('Game', function() {
       expect(function() { game.roll(11) }).toThrow(error);
     });
 
+  });
+
+  describe('A final score of zero', function() {
+    it('will return a "Gutter game"', function() {
+      for(var i = 0; i < 20; i++) {
+        game.roll(0);
+      };
+      expect(game.gutterGame()).toEqual(true);
+    });
   });
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,12 +1,24 @@
 function Game() {
   this.frameIndex = 0;
-  this.frames = [];
+  this.points = [];
 };
 
 Game.prototype.roll = function(pins) {
   if (pins > 10) {
     throw 'You cannot bowl more than 10!'
   } else {
-    this.frames.push(pins)
+    this.points.push(pins)
+  };
+};
+
+Game.prototype.finalScore = function(points) {
+  return points.reduce(function(a,b) {
+    return a + b
+  }, 0);
+};
+
+Game.prototype.gutterGame = function() {
+  if ((this.points.length === 20) && this.finalScore(this.points) === 0) {
+    return true;
   };
 };

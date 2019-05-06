e288641a5ae70534c5c36902ccaa5074937df4f4
Oliver Cripps <ollycripps@gmail.com>
2019-05-06 15:27:49 +0100

    Create rollHelper function in spec

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,18 +8,20 @@ describe('Bowling ScoreCard', function() {
 
   describe('Scoring a game', function() {
     it('user can roll a gutter ball', function() {
-      for (var i = 0; i < 20; i++) {
-        scorecard.roll(0);
-      }
+      rollHelper(0, 20); 
       expect(scorecard.score()).toEqual(0);
     });
 
     it('user can roll a game of 1s', function() {
-      for (var i = 0; i < 20; i++) {
-        scorecard.roll(1);
-      }
+      rollHelper(1, 20);
       expect(scorecard.score()).toEqual(20);
     });
   });
+
+  var rollHelper = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      scorecard.roll(pins);
+    }
+  };
 
 });

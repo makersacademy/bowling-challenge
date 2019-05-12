546ca030b8d83c2cc80cd30ff182ed11a1c76b94
Jordan Roberts <jordanroberts@talk21.com>
2019-05-12 15:30:47 +0100

    Added strike counter to try and solve the strike scoring functionality

--- src/game.js
+++ src/game.js
@@ -5,32 +5,45 @@ function Game(){
   this.frameCounter = 0
   this.spare = false
   this.strike = false
+  this.SC = 0 // SC is for Strike Counter
 };
 
 Game.prototype.inputScore = function(pins) {
-  if (this.currentFrame.length == 0) {
-    this.currentFrame.push(pins);
     this.strike = this.isStrike()
-    if(this.spare) {
-      this.total += pins
-    }
+
     if(this.strike) {
       this._addFrame()
       this._newFrame()
     }
+    if(this.SC > 0 ) {
+      this.total += pins
+      this.SC -= 1
+    }
+    if(pins === 10){
+      if(this.SC === 2){
+        this.total += pins
+      }
+      this.SC = 2
+    }
+    // if(this.SC > 0 && pins != 10) {
+    //   this.total += pins
+    //   this.SC -= 1
+    // }
+
+  if (this.currentFrame.length == 0) {
+    this.currentFrame.push(pins);
+    if(this.spare) {
+      this.total += pins
+    }
   }
   else if(this.currentFrame.length == 1) {
     this.currentFrame.push(pins);
-    frameTotal = this.currentFrame[0] + this.currentFrame[1]
     this._addFrame()
     this.spare = this.isSpare()
     this.currentFrame = []
-    if(this.frameCounter > 0 && this.scores[this.frameCounter-1][0] == 10) {
-      this.total += frameTotal
-    }
     this.frameCounter += 1
   }
-  this.total += pins;
+  return this.total += pins;
 };
 
 Game.prototype.isStrike = function() {
@@ -55,5 +68,5 @@ Game.prototype.getTotal = function() {
 }
 
 Game.prototype.isGameOver = function() {
-  return this.frameCounter == 10 
+  return this.frameCounter == 10
 }

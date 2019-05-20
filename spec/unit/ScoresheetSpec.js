6a5b86e4ed055ed247209fff6f8f0d81791a5363
Amina Jusic <amina_jusic@yahoo.com>
2019-05-20 16:40:14 +0100

    Unit test for Rolling increments the rollCount by 1

--- spec/unit/ScoresheetSpec.js
+++ spec/unit/ScoresheetSpec.js
@@ -15,4 +15,23 @@
       expect(scoresheet.finalScore).toEqual(0)
     })
 
+    it('has 0 rolls by default', function () {
+    expect(scoresheet.rollCount).toEqual(0)
+  })
+
+  describe('#roll', function () {
+    it('increments the rollCount by one', function () {
+      scoresheet.roll()
+      expect(scoresheet.rollCount).toEqual(1)
+    })
+
+    it('rolling 20 times completes the scoresheet', function () {
+      for (let i = 1; i <= 20; i++) {
+        scoresheet.roll(0)
+      }
+
+      expect(scoresheet.isComplete).toBeTruthy()
+    })
+  })
+
 })
--- src/Scoresheet.js
+++ src/Scoresheet.js
@@ -8,5 +8,6 @@
   }
 
    roll () {
+     this.rollCount += 1
   }
 }

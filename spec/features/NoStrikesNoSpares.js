d30590074aca1e7e1d8dd0b960cdf364a497cb17
Amina Jusic <amina_jusic@yahoo.com>
2019-05-20 16:43:51 +0100

    Added feature test for game without strikes and spares

---
+++ spec/features/NoStrikesNoSpares.js
@@ -0,0 +1,18 @@
+'use strict'
+
+ describe ('No strikes or spares', function () {
+  let scoresheet
+
+   beforeEach (function () {
+    scoresheet = new Scoresheet
+  })
+
+   it('A user rolls 1 20 times and the final score is 20', function () {
+    for (let i = 1; i <= 20; i++) {
+      scoresheet.roll(1)
+    }
+
+     expect(scoresheet.isComplete).toBeTruthy()
+    expect(scoresheet.finalScore).toEqual(20)
+  })
+})

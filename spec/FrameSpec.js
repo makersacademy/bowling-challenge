d09508ccf76adf93495826bfaa4c476707d68399
Michael Hughes <mhughes36@live.co.uk>
2019-06-01 23:10:17 +0100

    Spare bonus

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,7 +1,13 @@
 describe('A frame of bowling', function() {
 
   it('adds two rolls together', function() {
-    frame = new Frame([1,3]);
+    var frame = new Frame([1,3]);
     expect(frame.total()).toEqual(4);
+  });
+
+  it('adds a spare bonus', function() {
+    var frame = new Frame([9,1]);
+    var next_frame = new Frame([4,1]);
+    expect(frame.total(next_frame)).toEqual(14);
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,12 +1,31 @@
 function Frame(rolls) {
+  this.MAXIMUM_SCORE = 10;
   this.rolls = rolls;
 }
 
-Frame.prototype.total = function () {
-  score = this._rollScore();
+Frame.prototype.total = function (next_frame) {
+  score = this._rollScore() + this._bonus(next_frame);
   return score;
 };
 
-Frame.prototype._rollScore = function() {
+Frame.prototype._rollScore = function () {
   return (this.rolls[0] + this.rolls[1]);
+};
+
+Frame.prototype._bonus = function (next_frame) {
+  if (next_frame === undefined) {
+    return 0;
+  };
+
+  if (this._isSpare()) {
+    return next_frame._spareBonus();
+  };
+};
+
+Frame.prototype._isSpare = function () {
+  return this._rollScore() === this.MAXIMUM_SCORE;
+};
+
+Frame.prototype._spareBonus = function () {
+  return this.rolls[0];
 };

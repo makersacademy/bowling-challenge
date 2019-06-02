af6b5366cd603d2dc693fbdb8b08d30bfc1dba8e
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-06-02 18:09:13 +0100

    refactored frame reset to an extracted method

--- src/scorecard.js
+++ src/scorecard.js
@@ -8,15 +8,18 @@ Scorecard.prototype.showScore = function() {
 }
 
 Scorecard.prototype.enterScore = function(pins) {
-  if (this._score.reduce((a,b) => a + b, 0) + pins > 10) {
-    throw "maximum input per frame is 10";
-  } else this._score.push(pins);
-  if ((this._score.length === 2) || (this._score.pop() === 10)) {
-    this._frame += 1
-    this._score = []
+  if (this._score.reduce((a,b) => a + b, 0) + pins > 10) throw "maximum input per frame is 10";
+  this._score.push(pins);
+  if ((this._score.length === 2) || (pins === 10)) {
+    this.frameReset();
   }
 }
 
 Scorecard.prototype.isFrame = function() {
-  return this._frame + 1
+  return this._frame + 1;
+}
+
+Scorecard.prototype.frameReset = function() {
+  this._frame += 1;
+  this._score = [];
 }

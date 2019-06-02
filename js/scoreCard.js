9994c9d86f03cf38d7d3495f7401cf6231bfe39d
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-06-02 20:31:59 +0100

    Manage frames, rolls and end of the game

--- js/scoreCard.js
+++ js/scoreCard.js
@@ -1,3 +1,49 @@
 function ScoreCard() {
-    this.frames = []
+   this.frames = []
+};
+
+ScoreCard.prototype = {
+   recordScore: function(pinsHit) {
+      if (this.isGameEnded()) {
+         throw new Error('Game already ended');
+      }
+
+      var currentFrame = this._currentFrame();
+
+      if (currentFrame === undefined || currentFrame.isEnded()) {
+         currentFrame = new Frame();
+         this.frames.push(currentFrame);
+      } 
+      
+      currentFrame.recordScore(pinsHit);
+
+      var previousFrame = this._previousFrame();
+
+      if (previousFrame !== undefined 
+         && previousFrame.isSpare() 
+         && currentFrame._rollCount === 1) {
+         previousFrame.addBonus(pinsHit);
+      }
+
+      if (previousFrame !== undefined 
+         && previousFrame.isStrike()) {
+         previousFrame.addBonus(pinsHit);
+      }
+   },
+
+   isGameEnded: function() {
+      return this.frames.length === 10 && this._currentFrame().isEnded();
+   },
+
+   _currentFrame: function() {
+      return this.frames.length > 0 
+         ? this.frames[this.frames.length - 1] 
+         : undefined;
+   },
+
+   _previousFrame: function() {
+      return this.frames.length > 1 
+         ? this.frames[this.frames.length - 2] 
+         : undefined;
+   }
 };
--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -1,13 +1,109 @@
 describe('ScoreCard', function() {
-    var scoreCard;
+   var scoreCard;
 
-    beforeEach(function() {
-        scoreCard = new ScoreCard();
-    });
+   beforeEach(function() {
+      scoreCard = new ScoreCard();
+   });
 
-    describe('Defaults', function() {
-        it('does not have frames by default', function() {
-            expect(scoreCard.frames.length).toEqual(0)
-        });
-    });
+   describe('Defaults', function() {
+      it('does not have frames by default', function() {
+         expect(scoreCard.frames.length).toEqual(0)
+      });
+   });
+
+   describe('Creating new frame', function() {
+      it('records score on a new frame when game starts', function() {
+         scoreCard.recordScore(1);
+
+         expect(scoreCard.frames.length).toEqual(1);
+         expect(scoreCard.frames[0].getTotalScore()).toEqual(1);
+      });
+
+      it('records score on a new frame when previous frame ended with a strike', 
+      function() {
+         scoreCard.recordScore(10);
+
+         expect(scoreCard.frames.length).toEqual(1);
+
+         scoreCard.recordScore(1);
+
+         expect(scoreCard.frames.length).toEqual(2);
+         expect(scoreCard.frames[1].getTotalScore()).toEqual(1);
+      });
+
+      it('records score on a new frame when previous frame ended after two rolls', 
+      function() {
+         scoreCard.recordScore(1);
+         scoreCard.recordScore(2);
+
+         expect(scoreCard.frames.length).toEqual(1);
+
+         scoreCard.recordScore(4);
+
+         expect(scoreCard.frames.length).toEqual(2);
+         expect(scoreCard.frames[1].getTotalScore()).toEqual(4);
+      });
+   });
+
+   describe('Applying bonus', function() {
+      it('adds first roll as bonus to the previous frame if it ended with a spare',
+      function() {
+         scoreCard.recordScore(5);
+         scoreCard.recordScore(5);
+         scoreCard.recordScore(8);
+
+         expect(scoreCard.frames[0].getTotalScore()).toEqual(18);
+      });
+
+      it('does not add second roll as bonus to the previous frame if it ended with a spare',
+      function() {
+         scoreCard.recordScore(5);
+         scoreCard.recordScore(5);
+         scoreCard.recordScore(8);
+
+         expect(scoreCard.frames[0].getTotalScore()).toEqual(18);
+
+         scoreCard.recordScore(1);
+
+         expect(scoreCard.frames[0].getTotalScore()).toEqual(18);
+      });
+
+      it('adds first and second rolls as bonus to the previous frame if it ended with a strike',
+      function() {
+         scoreCard.recordScore(10);
+         scoreCard.recordScore(5);
+
+         expect(scoreCard.frames[0].getTotalScore()).toEqual(15);
+
+         scoreCard.recordScore(2);
+
+         expect(scoreCard.frames[0].getTotalScore()).toEqual(17);
+      });
+   });
+
+   describe('End of game', function() {
+      it('ends when all 10 frames ended', function() {
+         for(var i = 0; i < 20; i++) {
+            scoreCard.recordScore(1);
+         }
+
+         expect(scoreCard.isGameEnded()).toEqual(true);
+      });
+
+      it('is not ended when last frame is not ended', function() {
+         for(var i = 0; i < 19; i++) {
+            scoreCard.recordScore(1);
+         }
+
+         expect(scoreCard.isGameEnded()).toEqual(false);
+      });
+
+      it('is not allowed to roll after game ended', function() {
+         for(var i = 0; i < 20; i++) {
+            scoreCard.recordScore(1);
+         }
+
+         expect(function() { scoreCard.recordScore(1) }).toThrowError('Game already ended');
+      });
+   });
 });

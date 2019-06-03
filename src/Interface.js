d2108bc12b00d8e1390cf1e0cc5f0ebf7b89347d
VladC <vlad.chiriac24@gmail.com>
2019-06-03 08:18:38 +0100

    Update html file

--- src/Interface.js
+++ src/Interface.js
@@ -2,27 +2,27 @@ $(document).ready(function() {
 	var game = new Game();
 	updateScore();
 
-  $('#roll0').on('click', function() {
+  $('#roll_0').on('click', function() {
   	game.oneRoll(0);
   	updateScore();
   });
 
-  $('#roll1').on('click', function() {
+  $('#roll_1').on('click', function() {
   	game.oneRoll(1);
   	updateScore();
   });
 
-  $('#roll2').on('click', function() {
+  $('#roll_2').on('click', function() {
   	game.oneRoll(2);
   	updateScore();
   });
 
-  $('#roll3').on('click', function() {
+  $('#roll_3').on('click', function() {
   	game.oneRoll(3);
   	updateScore();
   });
 
-  $('#roll4').on('click', function() {
+  $('#roll_4').on('click', function() {
   	game.oneRoll(4);
   	updateScore();
   });
@@ -39,11 +39,12 @@ $(document).ready(function() {
   });
 
   function updateScore() {
-  	$('#totalscore').text(game.getScore());
+  	$('#totalscore').text('game.getScore()');
   	thisRoll = game.roll;    
-    $('#row1' + thisRoll).text(game.rollsScores[thisRoll - 1]);
+    $('#roll' + thisRoll).text(game.rollsScores[thisRoll - 1]);
 
   	thisFrame = game.frame - 1;
  	$('#marker' + thisFrame).text(game.oneFrameScores[thisFrame - ]);
  }
+ 
 });

7dd5625432c678188ac31093073943f43e95c4a0
Laurence <ltaylorapp@gmail.com>
2019-06-03 10:20:09 +0100

    Refactored game.js

--- src/game.js
+++ src/game.js
@@ -5,6 +5,8 @@ function Game() {
   this.frame = 1;
 
   this.allRolls = [];
+
+  // key = frame, value = score
   this.scores = {};
 
   // array of arrays with format [[frame, allRolls index], ...]
@@ -16,29 +18,32 @@ function Game() {
 
 // Methods
 Game.prototype.rollOne = function(pins) {
-  this.scoreOne = pins;
-  this.allRolls.push(pins);
-
-  if (this.activeStrikes.length > 0) { this._resolveStrikes(); }
-
-  this.scores[this.frame] = pins;
+  this._addScores(pins);
+  this._resolveStrikes();
   this._checkStrike(pins);
 }
 
 Game.prototype.rollTwo = function(pins) {
-  this.scoreTwo = pins;
-  this.allRolls.push(pins);
-
-  if (this.activeStrikes.length > 0) { this._resolveStrikes(); }
-
-  this.scores[this.frame] += pins;
+  this._addScores(pins);
+  this._resolveStrikes();
   this._newFrame();
 }
 
 // Private methods
+Game.prototype._addScores = function(pins) {
+  if (this.scoreOne === null) {
+    this.scoreOne = pins;
+    this.scores[this.frame] = pins;
+  } else {
+    this.scoreTwo = pins;
+    this.scores[this.frame] += pins;
+  }
+  this.allRolls.push(pins);
+}
+
 Game.prototype._checkStrike = function(pins) {
   if (pins === 10) {
-    this.activeStrikes.push([this.frame, this.allRolls.length - 1])
+    this.activeStrikes.push([this.frame, this.allRolls.length - 1]);
     this._newFrame();
   }
 }
@@ -46,15 +51,18 @@ Game.prototype._checkStrike = function(pins) {
 Game.prototype._newFrame = function() {
   this.scoreOne = null;
   this.scoreTwo = null;
-  this.frame++
+  this.frame++;
 }
 
 Game.prototype._resolveStrikes = function() {
-  var frame = this.activeStrikes[0][0];
-  var index = this.activeStrikes[0][1];
+  if (this.activeStrikes.length > 0) {
+    var frame = this.activeStrikes[0][0]; // frame of strike
+    var index = this.activeStrikes[0][1]; // index of strike in allRolls
 
-  if (index === this.allRolls.length - 3) {
-    this.scores[frame] += (this.allRolls[index + 1] + this.allRolls[index + 2]);
-    this.activeStrikes.shift();
+    // adds next two rolls from allRolls to scores dictionary
+    if (index === this.allRolls.length - 3) {
+      this.scores[frame] += (this.allRolls[index+1] + this.allRolls[index+2]);
+      this.activeStrikes.shift();
+    }
   }
 }

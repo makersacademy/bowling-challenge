a0552a647f2193c484a2915b1ed73551edbc7d3f
Student <comsin.c.croitoriu@gmail.com>
2019-06-09 09:59:48 +0100

    implemented 'spare' calculation

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,27 +1,35 @@
+/* eslint-disable no-undef */
 'use strict'
-
-
 describe('Bowling', function() {
-  
-  it('starts a game of bowling', function() {
-    var bowling = new Bowling();
-  })
+  var bowling;
+
+  beforeEach(function () {
+    bowling = new Bowling();
+  });
 
   it('can roll gutter game', function() {
-    var bowling = new Bowling();
-    for (var i = 0; i < 20; i++) {
-      bowling.roll(0);
-    };
+    rollMany(0, 20);
     expect(bowling.score()).toBe(0)
   })
 
   it('can roll ones', function () {
-    var bowling = new Bowling();
-    for (var i = 0; i < 20; i++) {
-      bowling.roll(1);
-    }
+    rollMany(1, 20);
     expect(bowling.score()).toBe(20);
   })
 
+  it('can roll spare', function () {
+    bowling.roll(5);
+    bowling.roll(5);
+    bowling.roll(3);
+    rollMany(0, 17);
+    expect(bowling.score()).toBe(16);
+
+  })
+  var rollMany = function (pins, rolls) {
+    for (var i = 0; i < rolls; i++) {
+      bowling.roll(pins);
+
+    }
+  }
 
 })

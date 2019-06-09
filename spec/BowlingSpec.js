b23e86a80909732aa351a226202d46a668dce6e3
Student <comsin.c.croitoriu@gmail.com>
2019-06-09 11:54:18 +0100

    implemented the function and the test for 'strike' situation

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,35 +1,41 @@
 /* eslint-disable no-undef */
 'use strict'
-describe('Bowling', function() {
-  var bowling;
+describe('Bowling', function () {
+  var bowling
 
   beforeEach(function () {
-    bowling = new Bowling();
-  });
+    bowling = new Bowling()
+  })
 
-  it('can roll gutter game', function() {
-    rollMany(0, 20);
+  it('can roll gutter game', function () {
+    rollMany(0, 20)
     expect(bowling.score()).toBe(0)
   })
 
   it('can roll ones', function () {
-    rollMany(1, 20);
-    expect(bowling.score()).toBe(20);
+    rollMany(1, 20)
+    expect(bowling.score()).toBe(20)
   })
 
   it('can roll spare', function () {
-    bowling.roll(5);
-    bowling.roll(5);
-    bowling.roll(3);
-    rollMany(0, 17);
-    expect(bowling.score()).toBe(16);
-
+    bowling.roll(5)
+    bowling.roll(5)
+    bowling.roll(3)
+    rollMany(0, 17)
+    expect(bowling.score()).toBe(16)
   })
+
+  it('can roll a strike', function () {
+    bowling.roll(10)
+    bowling.roll(4)
+    bowling.roll(3)
+    rollMany(0, 16)
+    expect(bowling.score()).toBe(24)
+  })
+
   var rollMany = function (pins, rolls) {
     for (var i = 0; i < rolls; i++) {
-      bowling.roll(pins);
-
+      bowling.roll(pins)
     }
   }
-
 })
--- src/Bowling.js
+++ src/Bowling.js
@@ -9,17 +9,28 @@ Bowling.prototype.score = function () {
   var rollIndex = 0
   var bowling = this
   for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
-    if (isSpare()) {
+    if (isStrike()) {
+      result += getStrikeScore()
+      rollIndex++
+    } else if (isSpare()) {
       result += getSpareScore()
+      rollIndex += 2
     } else {
       result += getNormalScore()
+      rollIndex += 2
     }
-    rollIndex += 2
   }
   return result
 
+  function isStrike () {
+    return bowling.rolls[rollIndex] === 10
+  }
   function isSpare () {
     return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] === 10
+  }
+
+  function getStrikeScore () {
+    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] + bowling.rolls[rollIndex + 2]
   }
 
   function getSpareScore () {

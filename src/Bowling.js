ee1e00922a7776aebfc078c931c960d88cb8b523
Student <comsin.c.croitoriu@gmail.com>
2019-06-09 11:14:27 +0100

    addapted the code according to EsLint

--- src/Bowling.js
+++ src/Bowling.js
@@ -1,36 +1,32 @@
 function Bowling () {
-  this.rolls= [];
-
+  this.rolls = []
 }
 Bowling.prototype.roll = function (pins) {
-  this.rolls.push(pins);
-
-
+  this.rolls.push(pins)
 }
-Bowling.prototype.score = function() {
-  var result = 0;
-  var rollIndex = 0;
-  var bowling = this;
-  
-  for (var frameIndex = 0; frameIndex < 10; frameIndex++ ) {
+Bowling.prototype.score = function () {
+  var result = 0
+  var rollIndex = 0
+  var bowling = this
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
     if (isSpare()) {
-      result += getSpareScore();
+      result += getSpareScore()
     } else {
-      result += getNormalScore();
+      result += getNormalScore()
     }
-    rollIndex += 2;
+    rollIndex += 2
   }
-  return result;
+  return result
 
-  function isSpare(){
-    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] == 10
-  }
-
-  function getSpareScore() {
-    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] + bowling.rolls[rollIndex + 2];
+  function isSpare () {
+    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] === 10
   }
 
-  function getNormalScore() {
-    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1];
+  function getSpareScore () {
+    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] + bowling.rolls[rollIndex + 2]
+  }
+
+  function getNormalScore () {
+    return bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1]
   }
 }

fdc9e922e821e1f2d6e698f7f0ba51d6e2ad6b08
Alexis Josephides <ajosephides@gmail.com>
2019-06-29 11:48:55 +0100

    test that game keeps track of frames correctly

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,4 +1,18 @@
-describe('Game'. function(){
+describe('Game', function(){
+
+  let game;
+
+  beforeEach(function(){
+    game = new Game();
+  });
+
+  it('keeps track of correct number of frames', function(){
+    game.roll(2);
+    game.roll(4);
+    game.roll(10);
+    game.roll(3);
+    expect(game.frameNumber()).toEqual(3);
+  })
 
 
 

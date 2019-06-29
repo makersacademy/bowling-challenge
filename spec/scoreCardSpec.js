1e27f5c061e8dc9e69d64ef4f7861f81ac058e1a
Ben Scott <bengscott2@gmail.com>
2019-06-29 09:21:07 +0100

    create roll1 and 2 defaulting 0

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -10,4 +10,9 @@ describe('ScoreCard', function() {
   it('starts with an empty frames array', function() {
     expect(scoreCard.frames).toEqual([])
   })
+
+  it('starts with roll 1 and roll 2 vars', function() {
+    expect(scoreCard.roll1).toEqual(0)
+    expect(scoreCard.roll2).toEqual(0)
+  })
 })
--- src/scoreCard.js
+++ src/scoreCard.js
@@ -1,3 +1,5 @@
 function ScoreCard() {
   this.frames = [];
+  this.roll1 = 0;
+  this.roll2 = 0;
 };

262a535b3b7960ab05a1721809078f0960f33e20
Alexis Josephides <ajosephides@gmail.com>
2019-06-29 13:04:45 +0100

    refactore some game methods with some private helpers

--- src/Game.js
+++ src/Game.js
@@ -5,17 +5,17 @@ function Game(){
 };
 
 
-Game.prototype.roll = function(pins, frame = new Frame()){
+Game.prototype.roll = function(pins){
   if(this._frames.length === 0){
-    this._frames.push(frame)
-    this._frames[this._frames.length - 1].roll(pins);
+    this._addFrame();
+    this._currentFrame().roll(pins);
   } 
-  else if (this._frames[this._frames.length - 1].isInPlay()) {
-    this._frames[this._frames.length - 1].roll(pins);
+  else if (this._currentFrame().isInPlay()) {
+    this._currentFrame().roll(pins);
   } 
   else {
-    this._frames.push(frame)
-    this._frames[this._frames.length - 1].roll(pins);
+    this._addFrame();
+    this._currentFrame().roll(pins);
   };
 };
 
@@ -23,3 +23,18 @@ Game.prototype.frameNumber= function(){
   return this._frames.length;
 };
 
+Game.prototype.score =  function(){
+  let score = 0;
+  this._frames.forEach(function(frame){
+    score = score + frame.totalScore()
+  });
+  return score;
+};
+
+Game.prototype._currentFrame = function(){
+  return this._frames[this._frames.length - 1];
+};
+
+Game.prototype._addFrame = function(frame = new Frame()){
+  this._frames.push(frame)
+};

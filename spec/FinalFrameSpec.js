af2440eba89c2668ebe80c3fd1152712bcd93b0e
Alexis Josephides <ajosephides@gmail.com>
2019-06-30 13:14:51 +0100

    added tests for scoring on final frame

--- spec/FinalFrameSpec.js
+++ spec/FinalFrameSpec.js
@@ -14,4 +14,26 @@ describe('FinalFrame', function(){
     expect(finalFrame.isExtraRoll()).toBe(true);
   });
 
+  it('calculates a final score correctly for 2 throw frame', function(){
+    finalFrame.roll(3);
+    finalFrame.roll(6);
+    expect(finalFrame.totalScore()).toEqual(9);
+  });
+
+  it('calculates a final score correctly for 3 throw frame', function(){
+    finalFrame.roll(4);
+    finalFrame.roll(6);
+    finalFrame.roll(5);
+    expect(finalFrame.totalScore()).toEqual(15);
+  });
+
+  it('calculates a final score correctly for 3 strike frame', function(){
+    finalFrame.roll(10);
+    finalFrame.roll(10);
+    finalFrame.roll(10);
+    expect(finalFrame.totalScore()).toEqual(30);
+  });
+
+
+
 });

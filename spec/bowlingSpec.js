f14098357c0e45fd596ab5a073c556fbe09176c1
petraartep <petra.pogacsas@gmail.com>
2019-06-30 16:01:57 +0100

    calculating spares refactored

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -18,13 +18,13 @@ describe('Bowling', function() {
     expect(game.score()).toBe(20);
   });
 
-  // it('can roll a spare', function() {
-  //   game.roll(5);
-  //   game.roll(5);
-  //   game.roll(3);
-  //   rollMany(0, 17);
-  //   expect(game.score()).toBe(17);
-  // });
+  it('can roll a spare', function() {
+    game.roll(5);
+    game.roll(5);
+    game.roll(5);
+    rollMany(0, 17);
+    expect(game.score()).toBe(20);
+  });
 
   var rollMany = function (pins, rolls) {
     for (var i = 0; i < rolls; i++) {

40de21cd4016cb8d8211b9eb94b7b50c4505f9b4
LucyMHall <lucymhall1995@gmail.com>
2019-07-06 10:32:51 +0100

    add function which_roll to work out whether player or roll 1 or 2

--- jasmine/spec/gameSpec.js
+++ jasmine/spec/gameSpec.js
@@ -8,10 +8,44 @@ describe ('Game', function() {
   });
 
   describe('input roll', function() {
-    it('takes a roll and stores it', function(){
-    game = new Game;
-    expect(game.input_roll(3)).toContain(3);
+
+    it('takes a roll and stores it', function() {
+      game = new Game;
+      expect(game.input_roll(3)).toContain(3);
     });
+
+    it('only accepts numbers between 0 and 10', function() {
+      game = new Game;
+      expect(game.input_roll(-1)).toEqual("Invalid number");
+      expect(game.input_roll(11)).toEqual("Invalid number");
+    });
+
+  });
+
+  // As a user I want it to be clear if I am on my first or second roll
+  describe('which_roll', function(){
+
+    it('accesses the roll property and returns 1 if on first roll', function() {
+      game = new Game;
+      game.input_roll(1)
+      expect(game.which_roll()).toEqual(1)
+    });
+
+    it('accesses the roll property and returns 2 if on second roll', function() {
+      game = new Game;
+      game.input_roll(1)
+      game.input_roll(3)
+      expect(game.which_roll()).toEqual(2);
+    });
+
+    it('resets after roll 2 back to roll 1 rather than increase to 3', function() {
+      game = new Game;
+      game.input_roll(1)
+      game.input_roll(3)
+      game.input_roll(4)
+      expect(game.which_roll()).toEqual(1)
+    });
+
   });
 
 
--- jasmine/src/game.js
+++ jasmine/src/game.js
@@ -7,6 +7,18 @@ function Game() {
 // add a function via prototype for all objects created by the constructor function
 // function = input(roll) takes a roll and adds to this.roll
 Game.prototype.input_roll = function (roll) {
-  this.roll.push(roll)
-  return this.roll
+  if(roll <= 10 && roll >= 0){
+    this.roll.push(roll)
+    return this.roll
+  } else {
+    return "Invalid number"
+  }
+};
+
+Game.prototype.which_roll = function() {
+  if(this.roll.length % 2 == 0) {
+    return 2
+  } else {
+    return 1
+  }
 };

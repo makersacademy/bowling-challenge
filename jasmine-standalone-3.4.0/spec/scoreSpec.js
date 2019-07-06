a168cebbf0664d57b6564577fc99ee960e0dd186
Celia <celiafrom12@gmail.com>
2019-07-06 13:51:45 +0100

    can roll all ones

--- jasmine-standalone-3.4.0/spec/scoreSpec.js
+++ jasmine-standalone-3.4.0/spec/scoreSpec.js
@@ -11,4 +11,14 @@ describe('Bowling game', function(){
     expect(game.score()).toBe(0);
   });
 
+  it("can roll all ones", function() {
+    var game = new BowlingGame();
+    for (var i = 0; i < 20; i++) {
+      game.roll(1);
+
+    }
+    expect(game.score()).toBe(20);
+  });
+
+
 });
--- jasmine-standalone-3.4.0/src/game.js
+++ jasmine-standalone-3.4.0/src/game.js
@@ -1,13 +1,16 @@
 
 var BowlingGame = function (){
-
+  this.rolls = [];
 }; //define a bowling game
 
 BowlingGame.prototype.roll = function(pins) {
-
-
+  this.rolls.push(pins);
 };
 
 BowlingGame.prototype.score = function () {
-  return 0;
+  var result = 0;
+  for (var i = 0; i < 20; i++){
+    result += this.rolls[i];
+  }
+  return result;
 };

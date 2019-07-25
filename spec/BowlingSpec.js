c67b8c8cd6f5aa48f0996a9a8d5b010ff0e559b1
Kaja <catcalledkaja@gmail.com>
2019-07-25 11:37:59 +0100

    adding points 1 to 20

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,15 +1,24 @@
 describe('bowling game', function() {
 
-  it('can create game', function () {
-    var game = new Bowling();
+  var game;
+
+  beforeEach(function () {
+    game = new Bowling();
   });
 
-  it('Gutter Game is when the player never hits a pin', function () {
-    var game = new Bowling();
+  var rollMany = function (pins,rolls) {
     for (var n=0; n<20; n++) {
-      game.roll(0);
+      game.roll(pins);
     }
+  };
+  it('Gutter Game is when the player never hits a pin', function () {
+    rollMany(0, 20);
     expect(game.score()).toEqual(0);
   });
+    it('can roll all pins', function() {
+    rollMany(1, 20);
+    expect(game.score()).toEqual(20);
+    })
+
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,11 +1,15 @@
 
 var Bowling = function () {
-
+  this.rolls = [];
 }
 Bowling.prototype.roll = function(pins) {
-
+  this.rolls.push(pins);
 };
 
 Bowling.prototype.score = function() {
-  return 0;
+  var result = 0;
+  for (var n=0; n<20; n++) {
+    result += this.rolls[n];
+  }
+  return result;
 };

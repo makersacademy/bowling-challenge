06acd4df9831b255d475d69517690f1a811d6005
Freddie <f.varnals@gmail.com>
2019-07-28 15:50:12 +0100

    New functions added/reformatted

--- src/frame.js
+++ src/frame.js
@@ -1,36 +1,64 @@
-function Frame(frameNumber, previousFrame) {
-  this._score = 0
-  this._bonus = 0
-  this._roll = 1
-  this._frameNumber = frameNumber
-  this._frameOver = false
+function Frame() {
+  this._rolls = [];
+  this._pinsRemaining = 10;
+  this._pinScore = 0;
+  this._bonus = 0;
+  this._frameScore = 0;
+  this._strike = false;
+  this._spare = false;
+  this._finalFrame = false;
+  this._frameOver = false;
 };
 
-Frame.prototype.score = function() {
-  return this._score;
+Frame.prototype.add = function(roll) {
+  this._rolls.push(roll);
+  this._pinScore += roll.score();
+  this._pinsRemaining -= roll.score();
+  this.updateStatus();
 };
+
+Frame.prototype.pinsRemaining = function() {
+  return this._pinsRemaining;
+};
+
+Frame.prototype.pinScore = function() {
+  return this._pinScore;
+}
 
 Frame.prototype.addPoints = function(points) {
   this._score += points;
   if ((points === 10) && (this._frameNumber !== 10)) {
     this._frameOver = true;
-  }
-  else if ((this._roll === 2) && (this._frameNumber !== 10)) {
-    this._frameOver = true}
-  else {
+  } else if ((this._roll === 2) && (this._frameNumber !== 10)) {
+    this._frameOver = true
+  } else {
     this._roll += 1;
   }
 };
 
-Frame.prototype.calculateBonus = function(previousFrame) {
-  if (previousFrame === null) {
-    null
-  }
-  else {
-    previousFrame._bonus += this._score;
-  }
+// Frame.prototype.calculateBonus = function(previousFrame) {
+//   if (previousFrame === null) {
+//     null
+//   }
+//   else {
+//     previousFrame._bonus += this._score;
+//   }
+// };
+
+Frame.prototype.updateStatus = function() {
+  if (roll.score() === 10) {
+    this._stike = true;
+    this.endFrame();
+  } else if (this.pinScore() === 10) {
+    this._spare = true;
+    this.endFrame();
+  } else if ((this._rolls.length === 2) && (this._finalFrame === false)) {
+    this.endFrame();
+  } else {
+    null;
+  };
 };
 
-Frame.prototype.status = function() {
-  return this._frameOver;
+Frame.prototype.endFrame = function() {
+  this._frameOver = true;
 };

2fde4b93eeab3030ef762ede5b72ea32ba474698
Kareem <kareem335@yahoo.co.uk>
2019-07-28 15:03:13 +0100

    git add .

--- jasmine-standalone-3/spec/bowlingScoreSpec.js
+++ jasmine-standalone-3/spec/bowlingScoreSpec.js
@@ -8,12 +8,43 @@ describe('Bowling Score', function(){
     scorer = new Scorer();
   });
 
-  it('shows points', function() {
-    expect(scorer.showPoint(7)).toEqual(7)
+  it('shows pins', function() {
+    expect(scorer.showPins(7)).toEqual(7);
   });
 
-  it('stores points in an array', function() {
-    scorer.insert(7)
-    expect(scorer.points).toContain(7)
+  it('stores pins in an array', function() {
+    scorer.insert(7);
+    expect(scorer.pins).toContain([7]);
+  });
+
+  it("stores each frame in it's own subarray", function() {
+    for (var i = 0; i < 4; i++) {
+      scorer.insert(2);
+    }
+    expect(scorer.pins).toEqual([[2,2],[2,2]]);
+  });
+
+  it("sums up the total score of the current frame", function() {
+    scorer.insert(5);
+    scorer.insert(4);
+    expect(scorer.currentFrameScore()).toEqual(9);
+  });
+
+  describe("When spare", function() {
+    it("accumulates score from next roll", function() {
+      scorer.insert(5);
+      scorer.insert(5);
+      scorer.insert(5);
+      expect(scorer.currentFrameScore()).toEqual(15);
+    });
+
+    it("scores next frame including accumulated extra roll from previous", function() {
+      scorer.insert(5);
+      scorer.insert(5);
+      scorer.insert(5)
+      expect(scorer.currentFrameScore()).toEqual(15);
+      scorer.insert(5);
+      expect(scorer.currentFrameScore()).toEqual(10);
+    });
   });
 });
--- jasmine-standalone-3/src/bowlingScore.js
+++ jasmine-standalone-3/src/bowlingScore.js
@@ -1,14 +1,37 @@
 'use strict';
 
 function Scorer() {
-  this.points = []
-}
-Scorer.prototype.showPoint = function(number) {
+  this.pins = [[]]
+  this._scoreFrame = 0
+};
+
+Scorer.prototype.showPins = function(number) {
   return number;
 };
+
 Scorer.prototype.insert = function(number) {
-  this.points.push(number);
+  if (this.pins[this._scoreFrame].length < 2) {
+    this.pins[this._scoreFrame].push(number);
+  }
+  else {
+    this.pins.push([number]);
+    this._scoreFrame += 1;
+  };
 };
-Scorer.prototype.points = function() {
-  return this.points;
+
+Scorer.prototype.currentFrameScore = function() {
+  return this._thisScoreFrameCalculator();
+};
+
+Scorer.prototype._thisScoreFrameCalculator = function() {
+  var sum = this.pins.reduce(function(accumulator, order){
+    return order.reduce(function(sum, order){
+      return sum + order;
+    }, 0);
+  }, 0);
+  return sum;
+};
+
+Scorer.prototype._isSpare = function() {
+  return this._thisScoreFrameCalculator() === 10;
 };

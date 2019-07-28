5d6fa93c94de456df88a6047772a8c15f98c4741
Freddie <f.varnals@gmail.com>
2019-07-28 16:24:07 +0100

    Updated tests for Game

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -7,24 +7,31 @@ describe('Start Game', function(){
     expect(game._totalScore).toEqual(0);
   });
 
-  it('Starts with the first frame (1)', function () {
-    expect(game._frame).toEqual(1);
+  it('Can record frames', function() {
+    expect(function() {game.add(frame)}).not.toThrow();
   });
 
-  it('Game is not finished when started', function() {
-    expect(game._gameOver).toEqual(false);
-  });
-});
-
-describe('Play Game', function(){
-  beforeEach(function(){
-    game = new Game();
-  });
-
-  it('Adds the score of a completed frame', function () {
-    spyOn(frame, "score").and.returnValue(5);
-    game.updateScore(frame)
-    expect(game._totalScore).toEqual(5);
-  });
+  it('Can return total score for Game', function() {
+    spyOn(frame, "frameScore").and.returnValue(10);
+    game.add(frame);
+    game.add(frame);
+    expect(game.totalScore()).toEqual(20);
+  })
+//
+//   it('Game is not finished when started', function() {
+//     expect(game._gameOver).toEqual(false);
+//   });
+// });
+//
+// describe('Play Game', function(){
+//   beforeEach(function(){
+//     game = new Game();
+//   });
+//
+//   it('Adds the score of a completed frame', function () {
+//     spyOn(frame, "score").and.returnValue(5);
+//     game.updateScore(frame)
+//     expect(game._totalScore).toEqual(5);
+//   });
 
 });

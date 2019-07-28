7759ee5fac48e91451b83b651543ead8f65a8b0c
Freddie <f.varnals@gmail.com>
2019-07-28 16:24:51 +0100

    Add frames, update and return total score

--- src/game.js
+++ src/game.js
@@ -1,10 +1,19 @@
 function Game() {
-  this._totalScore = 0
-  this._frame = 1
-  this._gameOver = false
+  this._totalScore = 0;
+  this._frames = [];
+  this._gameOver = false;
 };
 
 Game.prototype.updateScore = function(frame) {
   var frameScore = frame.score();
   this._totalScore += frameScore;
-}
+};
+
+Game.prototype.add = function(frame) {
+  this._frames.push(frame);
+  this._totalScore += frame.frameScore();
+};
+
+Game.prototype.totalScore = function() {
+  return this._totalScore;
+};

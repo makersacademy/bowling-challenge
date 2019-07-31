0c86f4b4820ee36ee1fb40507b49de2aeaf752c6
Will Howes <williameldenhowes@gmail.com>
2019-07-31 09:42:21 +0100

    passes test for multiple strikes and spares

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -16,8 +16,6 @@ describe ('Game', function(){
       expect(game.throws).toEqual([4,4])
       expect(game.throwsRemaining).toEqual(18)
       expect(game.frameRunningTotals).toEqual([8])
-      expect(game.totalScore).toEqual(8)
-      expect(game.showTotal).toEqual(8)
     })
     it('records two throws equaling a spare', function() {
       game.throw(5)
@@ -25,10 +23,7 @@ describe ('Game', function(){
       expect(game.totalScore).toEqual(10)
       expect(game.throws).toEqual([5,5])
       expect(game.throwsRemaining).toEqual(18)
-      // expect(game.index).toEqual(2)
       expect(game.frameRunningTotals).toEqual([10])
-      expect(game.totalScore).toEqual(10)
-      expect(game.showTotal).toEqual(0)
     })
 
     it('records two throws equaling a spare and then another frame not equaling a spare', function() {
@@ -41,7 +36,6 @@ describe ('Game', function(){
       expect(game.throwsRemaining).toEqual(16)
       expect(game.frameRunningTotals).toEqual([15, 5])
       expect(game.totalScore).toEqual(20)
-      expect(game.showTotal).toEqual(20)
     })
     it('records a strike correctly with bonus rolls', function() {
       game.throw(10)
@@ -52,7 +46,6 @@ describe ('Game', function(){
       expect(game.throwsRemaining).toEqual(16)
       expect(game.frameRunningTotals).toEqual([12, 2])
       expect(game.totalScore).toEqual(14)
-      expect(game.showTotal).toEqual(14)
     })
     it('records a multiple strikes correctly with bonus rolls', function() {
       game.throw(10)
@@ -62,8 +55,33 @@ describe ('Game', function(){
       expect(game.throws).toEqual([10,10,10])
       expect(game.throwsRemaining).toEqual(14)
       expect(game.frameRunningTotals).toEqual([30, 20,10])
-      expect(game.totalScore).toEqual(60)
       // expect(game.showTotal).toEqual(30)
     })
+    it('records a multiple strikes and spares correctly with bonus rolls', function() {
+      game.throw(10)
+      game.throw(5)
+      game.throw(5)
+      game.throw(10)
+      game.throw(5)
+      game.throw(5)
+      game.throw(5)
+      many_throws(11, 0)
+      expect(game.totalScore).toEqual(80)
+      expect(game.throwsRemaining).toEqual(0)
+      expect(game.frameRunningTotals).toEqual([20, 20, 20, 15, 5, 0, 0, 0, 0, 0])
+    })
   })
+
+  var many_throws = function(times, pins) {
+    for (var i = 0; i < times; i++) {
+      game.throw(pins)
+    }
+  }
+
 })
+
+var many_throws = function(times, pins) {
+  for (var i = 0; i < pins; i++) {
+    game.throw(pins)
+  }
+}

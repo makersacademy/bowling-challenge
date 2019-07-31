63ab117d7d859c5a6495d8bb7732be228bcad23f
Will Howes <williameldenhowes@gmail.com>
2019-07-31 08:11:26 +0100

    Test for strike works

--- src/game.js
+++ src/game.js
@@ -3,7 +3,6 @@
 function Game() {
   this.throws = []
   this.throwsRemaining = 20
-  // index = 0
   this.frameRunningTotals = []
   this.totalScore = 0
   this.showTotal = 0
@@ -20,7 +19,6 @@ Game.prototype.throw = function (score) {
   if (this.throwsRemaining === 0) {
     this.throws = []
     this.throwsRemaining = 20
-    // index = 0
     this.frameRunningTotals = []
     this.totalScore = 0
     this.showTotal = 0
@@ -29,38 +27,29 @@ Game.prototype.throw = function (score) {
 
   this.totalScore += score
   this.throws.push(score)
-  // var index = this.frameRunningTotals.length -1
 
   if (this.throwsRemaining % 2 === 0 ) {
     this.frameRunningTotals.push(score)
   } else {
-    // console.log("WE ARE HERE")
-    // console.log("frame frameRunningTotals:")
-    // console.log(Game)
     this.frameRunningTotals[this.frameRunningTotals.length -1] += score
-    // console.log(Game)
   }
 
   // CHECKING FOR STRIKE ON LAST THROW...
   if (index > 0 && this.throws[index -1] === 10) {
     this.totalScore += score;
-    this.frameRunningTotals[this.frameRunningTotals.length -1] += score;
-
-    // CHECKING FOR STRIKE ON LAST, LAST THROW
-    if (index > 1 && this.throws[index - 2 === 10]) {
-      this.totalScore += score;
-      this.frameRunningTotals[this.frameRunningTotals.length -2] += score;
-    }
-
-  // CHECKING FOR SPARE ON LAST FRAME
+    this.frameRunningTotals[this.frameRunningTotals.length -2] += score;
   }
+  // CHECKING FOR STRIKE ON LAST, LAST THROW...
+  if (index > 1 && this.throws[index - 2] === 10) {
+    console.log("WE ARE HERE")
+    this.totalScore += score;
+    this.frameRunningTotals[this.frameRunningTotals.length -2] += score;
+  }
+    // CHECKING FOR SPARE ON LAST FRAME
   if (index > 1 && this.throwsRemaining % 2 === 0 &&
       this.throws[index -2] + this.throws[index -1] === 10) {
         this.totalScore += score;
-        console.log("WE ARE HERE")
-        console.log(this)
         this.frameRunningTotals[this.frameRunningTotals.length -2] += score;
-        console.log(this)
         this.showTotal = this.totalScore
   }
 

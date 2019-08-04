28344a115a802deb82633ff3db3e6426b4fa30a4
Richie <richieganney@gmail.com>
2019-08-04 16:55:53 +0100

    refactor

--- public/src/interface.js
+++ public/src/interface.js
@@ -4,57 +4,33 @@ $( document ).ready(function() {
     var scorecard = new ScoreCard();
 
     showAllFrames();
-    combinedScore();
-
-    function getUrlParams( prop ) {
-        var params = {};
-        var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
-        var definitions = search.split( '&' );
-    
-        definitions.forEach( function( val, key ) {
-            var parts = val.split( '=', 2 );
-            params[ parts[ 0 ] ] = parts[ 1 ];
-        } );
-        return ( prop && prop in params ) ? params[ prop ] : params;
-    }
+    // combinedScore();
   
     $('#roll').click(function() { 
-        frame.rollOneToNine(getUrlParams('roll_one'), getUrlParams('roll_two'));
+        frame.rollOneToNine($('#roll_one').val(), $('#roll_two').val());
+        showAllFrames();
     });
 
     $('#last_roll').click(function() { 
-        frame.rollTen(getUrlParams('last_roll_one'), getUrlParams('last_roll_two'), getUrlParams('last_roll_three'));
+        frame.rollTen($('#last_roll_one').val(), $('#last_roll_two').val(), $('#last_roll_three').val());
+        showAllFrames();
     });
     
-    $('#spares').click(function() { 
-        var spares = scorecard.spares(frame._allFrames);
-    });
-
-    $('#strikes').click(function() { 
-        var strikes = scorecard.strikes(frame._allFrames);
-    });
-
-    $('#other').click(function() { 
-        var other = scorecard.normalScores(frame._allFrames);
-    });
-
-    $('#show_all_frames').click(function() { 
-        var total = scorecard.total(frame._allFrames, other, spares, strikes)
-    });
-
-    $('#total_score').click(function() { 
-        var combinedScore = scorecard.calculateTotal(total)
+    $('#calculate_total').click(function() { 
+        let n =  scorecard.normalScores(frame._allFrames);
+        let sp = scorecard.spares(frame._allFrames);
+        let st = scorecard.strikes(frame._allFrames);
+        let total = scorecard.total(frame._allFrames, n, sp, st)
+        return scorecard.calculateTotal(total)
     });
 
       function showAllFrames(){
         $('#all_frames').text(frame._allFrames);
       }
 
-      function combinedScore(){
-        $('#combined_score').text(combinedScore);
-      }
-    
-
+    //   function combinedScore(){
+    //     $('#combined_score').text(combinedScore);
+    //   }
 
     // $('#decrease-temp').click(function() {
     //   thermostat.decreaseTemperature();

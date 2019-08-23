aa2b12300f9bce823b31e3d4f2d8543be09e1094
RMaughan93 <rhys.maughan93@gmail.com>
2019-08-23 15:00:20 +0100

    added scorecard functionality and tests

--- public/javascripts/jasmine_examples/Game.js
+++ public/javascripts/jasmine_examples/Game.js
@@ -3,10 +3,44 @@
 function Game() {
   this.current_score = 0;
   this.current_frame = 1;
+  this.frame_score = 0;
+  this.score_card = {
+    frame_1 : 0,
+    frame_2 : 0,
+    frame_3 : 0,
+    frame_4 : 0,
+    frame_5 : 0,
+    frame_6 : 0,
+    frame_7 : 0,
+    frame_8 : 0,
+    frame_9 : 0,
+    frame_10 : 0
+  }
 
   Game.prototype = {
 
-  };
-};
+    firstBowl: function(score) {
+      this.frame_score += score;
+    },
+
+    secondBowl: function(score) {
+      if (this.frame_score + score <= 10){
+        this.frame_score += score;
+      } else {
+        throw "Something doesn't add up, check your scores and try resubmitting."
+      }
+    },
+
+    saveScore: function() {
+      this.score_card[`frame_${this.current_frame}`] = this.frame_score;
+      this.current_frame++;
+      this.frame_score = 0;
+    }
+
+  }
+
+}
+
+
 
 var game = new Game();
--- spec/javascripts/jasmine_examples/PlayerSpec.js
+++ spec/javascripts/jasmine_examples/PlayerSpec.js
@@ -5,13 +5,59 @@ describe("Game", function() {
     game = new Game();
   });
 
-  describe("scoring during a game", function() {
+  describe("a game", function() {
     it("can have a initial score of 0", function() {
       expect(game.current_score).toEqual(0);
     });
 
+    it("can have a score for a frame", function() {
+      expect(game.frame_score).toEqual(0);
+    });
+
+  });
+
+  describe("frames", function() {
     it("starts on the first frame", function() {
       expect(game.current_frame).toEqual(1);
+    });
+
+    it("can further the frame number", function() {
+      game.saveScore();
+      expect(game.current_frame).toEqual(2);
     })
   });
+
+  describe("scoring", function() {
+    it("can save a score for the first bowl", function() {
+      game.firstBowl(7);
+      expect(game.frame_score).toEqual(7);
+    });
+
+    it("can save a score for the second bowl", function() {
+      game.secondBowl(2);
+      expect(game.frame_score).toEqual(2);
+    });
+
+    it("can have a total score for the frame", function() {
+      game.firstBowl(7);
+      game.secondBowl(2);
+      expect(game.frame_score).toEqual(9);
+    });
+
+    it("can store a frame score in the scorecard", function() {
+      game.firstBowl(7);
+      game.secondBowl(2);
+      game.saveScore();
+      expect(game.score_card["frame_1"]).toEqual(9);
+    });
+
+    it("can store a second frame score after the first frame", function() {
+      game.saveScore();
+      game.firstBowl(1);
+      game.secondBowl(0);
+      game.saveScore();
+      expect(game.score_card["frame_2"]).toEqual(1);
+    });
+  });
+
 });

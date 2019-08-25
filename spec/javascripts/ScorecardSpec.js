b8b16ca2bfea1acbc7d9de10fd7dfcd35c661d84
dtrts <djt210@gmail.com>
2019-08-25 16:39:54 +0100

    RED test for frames calc including a spare bonus calc

--- spec/javascripts/ScorecardSpec.js
+++ spec/javascripts/ScorecardSpec.js
@@ -16,4 +16,21 @@ describe('Scorecard', function () {
     expect(scorecard.rolls).toEqual([5, 5, 10, null])
   });
 
+  it('reports the frames so far', function () {
+    scorecard.add_roll(5);
+    expect(scorecard.frames()).toEqual([5])
+    scorecard.add_roll(5);
+    expect(scorecard.frames()).toEqual([10])
+  });
+
+  it('calculates spare bonuses', function () {
+    scorecard.add_roll(5);
+    scorecard.add_roll(5);
+    scorecard.add_roll(5);
+    expect(scorecard.frames()).toEqual([15, 5])
+  });
+
+
+
+
 });

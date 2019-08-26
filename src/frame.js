92ddf8acdfc88c277a7cedd5871a3775dd0168e2
monmon8 <student@makerss-mbp.home>
2019-08-26 22:06:51 +0100

    basic setup & first part of model

---
+++ src/frame.js
@@ -0,0 +1,6 @@
+module.exports = function Frame(number, rollOneScore, rollTwoScore, bonusScore) {
+  this.number = number 
+  this.rollOneScore = rollOneScore
+  this.rollTwoScore = rollTwoScore
+  this.bonusScore = bonusScore
+}

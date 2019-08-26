49bb62ef2084c90b0c4dd6887930ad5b988528fe
kierepkae <kierepkaedyta@gmail.com>
2019-08-26 15:08:46 +0100

    user can have spare and receive correct score

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -7,20 +7,32 @@ describe("Bowling", function () {
     bowling = new Bowling();
   });
 
+  const rollHelper = (pins, rolls) => {
+    for (let i = 0; i < rolls; i++) {
+      bowling.roll(pins)
+    }
+  }
+
 
   it('when pin equals 0 returns correct score', () => {
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(0)
-    }
+    rollHelper(0, 20)
     expect(bowling.result()).toEqual(0)
   })
 
   it('when pin equals 1 returns correct score', () => {
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(1)
-    }
+    rollHelper(1, 20)
     expect(bowling.result()).toEqual(20)
   })
+
+
+  it('when spare can calculate correct score', () => {
+    bowling.roll(2);
+    bowling.roll(8);
+    bowling.roll(3);
+    rollHelper(0, 17);
+    expect(bowling.result()).toBe(13);
+  });
+
 
 });
 
--- src/bowling.js
+++ src/bowling.js
@@ -1,16 +1,28 @@
 "use strict";
 
 function Bowling() {
-  this.score = []
+  this.rolls = []
 
-}
 
-Bowling.prototype.roll = function (pins) {
-  this.score.push(pins)
-}
 
-Bowling.prototype.result = function () {
-  return this.score.reduce((a, b) => a + b)
+  Bowling.prototype.roll = function (pins) {
+    this.rolls.push(pins)
+  }
+
+  Bowling.prototype.result = function () {
+    let score = 0;
+    let rollPosition = 0
+    for (let framePosition = 0; framePosition < 10; framePosition++) {
+      if (this.rolls[rollPosition] + this.rolls[rollPosition] == 10) {
+        score += this.rolls[rollPosition] + this.rolls[rollPosition + 1] + this.rolls[rollPosition + 2];
+      }
+      else {
+        score += this.rolls[rollPosition] + this.rolls[rollPosition + 1];
+      }
+      rollPosition += 2;
+    }
+    return score
+  }
 
 };
 

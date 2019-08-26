7a03e1965dc48dcbc6431e0dd87ca62b27d307b7
kierepkae <kierepkaedyta@gmail.com>
2019-08-26 15:41:28 +0100

    user receives correct score if strike or spare

--- src/bowling.js
+++ src/bowling.js
@@ -11,18 +11,45 @@ function Bowling() {
 
   Bowling.prototype.result = function () {
     let score = 0;
-    let rollPosition = 0
+    let rollPosition = 0;
+    let currentGame = this;
+
     for (let framePosition = 0; framePosition < 10; framePosition++) {
-      if (this.rolls[rollPosition] + this.rolls[rollPosition] == 10) {
-        score += this.rolls[rollPosition] + this.rolls[rollPosition + 1] + this.rolls[rollPosition + 2];
+      if (isStrike()) {
+        score += strikeResult()
+        rollPosition++;
       }
-      else {
-        score += this.rolls[rollPosition] + this.rolls[rollPosition + 1];
+      else if (isSpare()) {
+        score += spareResult()
+        rollPosition += 2;
+
+      } else {
+        score += regularResult()
+        rollPosition += 2;
       }
-      rollPosition += 2;
     }
     return score
+
+
+    function isStrike() {
+      return currentGame.rolls[rollPosition] == 10
+    }
+    function isSpare() {
+      return currentGame.rolls[rollPosition] + currentGame.rolls[rollPosition + 1] == 10
+    }
+    function spareResult() {
+      return currentGame.rolls[rollPosition] + currentGame.rolls[rollPosition + 1] + currentGame.rolls[rollPosition + 2];
+    }
+    function strikeResult() {
+      return currentGame.rolls[rollPosition] + currentGame.rolls[rollPosition + 1] + currentGame.rolls[rollPosition + 2];
+    }
+
+    function regularResult() {
+      return currentGame.rolls[rollPosition] + currentGame.rolls[rollPosition + 1];
+    }
   }
+
+
 
 };
 

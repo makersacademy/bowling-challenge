3610214284b78d5361cc315757986b8f22aaee87
RMaughan93 <rhys.maughan93@gmail.com>
2019-08-28 10:36:04 +0100

    pull request for week 5

--- public/javascripts/jasmine_examples/Game.js
+++ public/javascripts/jasmine_examples/Game.js
@@ -1,9 +1,12 @@
 'use strict';
 
 function Game() {
-  this.current_score = 0;
+  this.total_score = 0;
   this.current_frame = 1;
   this.frame_score = 0;
+  this.strike_last = false;
+  this.double = false;
+  this.spare_last = false;
   this.score_card = {
     frame_1 : 0,
     frame_2 : 0,
@@ -20,25 +23,92 @@ function Game() {
   Game.prototype = {
 
     firstBowl: function(score) {
-      this.frame_score += score;
+      if (this.spare_last == true) {
+        if (score == 10) {
+          this.spare_last = false;
+          this.total_score += score;
+          this.score_card[`frame_${this.current_frame - 1}`] += score;
+          this.strike();
+        } else {
+          this.score_card[`frame_${this.current_frame - 1}`] += score;
+          this.total_score += score;
+          this.frame_score += score;
+        }
+      } else {
+        if (score == 10) {
+          this.spare_last = false;
+          this.strike();
+        } else {
+          this.frame_score += score;
+        }
+      }
     },
 
     secondBowl: function(score) {
-      if (this.frame_score + score <= 10){
-        this.frame_score += score;
+      if (this.strike_last == true) {
+        if (this.frame_score + score < 10){
+          this.frame_score += score;
+          this.score_card[`frame_${this.current_frame - 1}`] += this.frame_score;
+          this.total_score += this.frame_score;
+          this.strike_last = false;
+          this.saveScore();
+        } else if (this.frame_score + score == 10){
+          this.frame_score += score;
+          this.total_score += this.frame_score;
+          this.score_card[`frame_${this.current_frame - 1}`] += this.frame_score;
+          this.strike_last = false;
+          this.spare();
+        } else {
+          throw "Something doesn't add up, check your scores and try resubmitting."
+        }
       } else {
-        throw "Something doesn't add up, check your scores and try resubmitting."
-      }
+          if (this.frame_score + score < 10){
+            this.spare_last = false;
+            this.frame_score += score;
+            this.saveScore();
+          } else if (this.frame_score + score == 10){
+            this.spare();
+          } else {
+            throw "Something doesn't add up, check your scores and try resubmitting."
+          }
+        }
     },
 
     saveScore: function() {
       this.score_card[`frame_${this.current_frame}`] = this.frame_score;
+      this.total_score += this.frame_score;
       this.current_frame++;
       this.frame_score = 0;
-    }
+    },
+
+    strike: function() {
+      if (this.double == true) {
+        this.score_card[`frame_${this.current_frame - 2}`] += 10;
+        this.score_card[`frame_${this.current_frame - 1}`] += 10;
+        this.frame_score = 10;
+        this.strike_last = true;
+        this.saveScore();
+      } else if (this.strike_last == true){
+        this.score_card[`frame_${this.current_frame - 1}`] += 10;
+        this.frame_score = 10;
+        this.strike_last = true;
+        this.double = true;
+        this.saveScore();
+      } else {
+        this.frame_score = 10;
+        this.strike_last = true;
+        this.saveScore();
+      }
+    },
+
+    spare: function() {
+      this.spare_last = true;
+      this.frame_score = 10;
+      this.saveScore();
+    },
+
 
   }
-
 }
 
 
--- spec/javascripts/jasmine_examples/PlayerSpec.js
+++ spec/javascripts/jasmine_examples/PlayerSpec.js
@@ -7,7 +7,7 @@ describe("Game", function() {
 
   describe("a game", function() {
     it("can have a initial score of 0", function() {
-      expect(game.current_score).toEqual(0);
+      expect(game.total_score).toEqual(0);
     });
 
     it("can have a score for a frame", function() {
@@ -35,19 +35,18 @@ describe("Game", function() {
 
     it("can save a score for the second bowl", function() {
       game.secondBowl(2);
-      expect(game.frame_score).toEqual(2);
+      expect(game.score_card["frame_1"]).toEqual(2);
     });
 
     it("can have a total score for the frame", function() {
       game.firstBowl(7);
       game.secondBowl(2);
-      expect(game.frame_score).toEqual(9);
+      expect(game.total_score).toEqual(9);
     });
 
     it("can store a frame score in the scorecard", function() {
       game.firstBowl(7);
       game.secondBowl(2);
-      game.saveScore();
       expect(game.score_card["frame_1"]).toEqual(9);
     });
 
@@ -55,8 +54,68 @@ describe("Game", function() {
       game.saveScore();
       game.firstBowl(1);
       game.secondBowl(0);
-      game.saveScore();
       expect(game.score_card["frame_2"]).toEqual(1);
+    });
+
+    it("can store a total score for the whole game", function() {
+      game.firstBowl(7);
+      game.secondBowl(2);
+      game.firstBowl(3);
+      game.secondBowl(4);
+      expect(game.total_score).toEqual(16);
+    });
+
+    it("can have a first frame score of 20 when there is a strike in each of the first two frames", function() {
+      game.firstBowl(10);
+      game.firstBowl(10);
+      expect(game.score_card["frame_1"]).toEqual(20);
+    });
+
+    it("can have a first frame score of 30 when there is a triple in the first three frames", function() {
+      game.firstBowl(10);
+      game.firstBowl(10);
+      game.firstBowl(10);
+      expect(game.score_card["frame_1"]).toEqual(30);
+    });
+
+    it("can have a score of 24 when a strike in the first frame and 7 in the second frame", function() {
+      game.firstBowl(8);
+      game.secondBowl(2);
+      game.firstBowl(7);
+      game.secondBowl(0);
+      expect(game.total_score).toEqual(24);
+    });
+
+    it("can have a score of 38 when spare, 8/10 spare then 5/0", function() {
+      game.firstBowl(8);
+      game.secondBowl(2);
+      game.firstBowl(8);
+      game.secondBowl(2);
+      game.firstBowl(5);
+      game.secondBowl(0);
+      expect(game.total_score).toEqual(38);
+    });
+
+    it("can have a score of 60 when strike, spare, strike, 5/0", function() {
+      game.firstBowl(10);
+      game.firstBowl(8);
+      game.secondBowl(2);
+      game.firstBowl(10);
+      game.firstBowl(5);
+      game.secondBowl(0);
+      expect(game.total_score).toEqual(60);
+    });
+
+    it("can have a score of 43 when; spare, 7/2, 9/-, 7/1", function() {
+      game.firstBowl(7);
+      game.secondBowl(3);
+      game.firstBowl(7);
+      game.secondBowl(2);
+      game.firstBowl(9);
+      game.secondBowl(0);
+      game.firstBowl(7);
+      game.secondBowl(1);
+      expect(game.total_score).toEqual(43);
     });
   });
 

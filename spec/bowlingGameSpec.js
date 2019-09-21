ea54fc208258de07aa8d93e2e78e6c8a4b27e7f2
Daniel Segall <segalld@gmail.com>
2019-09-21 12:39:36 +0100

    General cleanup

--- spec/bowlingGameSpec.js
+++ spec/bowlingGameSpec.js
@@ -1,4 +1,6 @@
 describe('BowlingGame', function () {
+    "use strict";
+
     let game;
 
     beforeEach(function () {
@@ -7,7 +9,7 @@ describe('BowlingGame', function () {
 
     function rollMany(times, pins) {
         for (let i = 0; i < times; i++) {
-            game.roll(pins)
+            game.roll(pins);
         }
     }
 
@@ -17,21 +19,21 @@ describe('BowlingGame', function () {
     }
 
     function rollStrike() {
-        game.roll(10)
+        game.roll(10);
     }
 
     describe('gutter game', function () {
         it('scores 0', function () {
             rollMany(20, 0);
             expect(game.score()).toEqual(0);
-        })
+        });
     });
 
     describe('game of all ones', function () {
         it('scores 20', function () {
             rollMany(20, 1);
             expect(game.score()).toEqual(20);
-        })
+        });
     });
 
     describe('one spare followed by a three', function () {
@@ -39,24 +41,24 @@ describe('BowlingGame', function () {
             rollSpare();
             game.roll(3);
             rollMany(17, 0);
-            expect(game.score()).toEqual(16)
+            expect(game.score()).toEqual(16);
         });
-    })
+    });
 
     describe('one strike followed by a three and a four', function () {
         it('scores 24', function () {
             rollStrike();
-            game.roll(3)
-            game.roll(4)
-            rollMany(16, 0)
-            expect(game.score()).toEqual(24)
+            game.roll(3);
+            game.roll(4);
+            rollMany(16, 0);
+            expect(game.score()).toEqual(24);
         });
-    })
+    });
 
     describe('perfect game', function () {
         it ('scores 300', function () {
-            rollMany(12, 10)
-            expect(game.score()).toEqual(300)
-        })
-    })
+            rollMany(12, 10);
+            expect(game.score()).toEqual(300);
+        });
+    });
 });
--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -1,34 +1,36 @@
-function BowlingGame() {
-    this._rolls = [];
-}
-
-BowlingGame.prototype.roll = function(pins) {
-    this._rolls.push(pins);
-};
-
-BowlingGame.prototype.score = function() {
-    let score = 0;
-    let rollIndex = 0;
-
-    function isSpare() {
-        return this._rolls[rollIndex] + this._rolls[rollIndex + 1] === 10;
+class BowlingGame {
+    constructor() {
+        this._rolls = [];
     }
 
-    function isStrike() {
-        return this._rolls[rollIndex] === 10;
+    roll(pins) {
+        this._rolls.push(pins);
     }
 
-    for (let frame = 0; frame < 10; frame++) {
-        if (isSpare.call(this)) {
-            score += 10 + this._rolls[rollIndex + 2];
-            rollIndex += 2;
-        } else if (isStrike.call(this)) {
-            score += 10 + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
-            rollIndex += 1;
-        } else {
-            score += this._rolls[rollIndex] + this._rolls[rollIndex + 1];
-            rollIndex += 2;
+    score() {
+        let score = 0;
+        let rollIndex = 0;
+
+        function isSpare() {
+            return this._rolls[rollIndex] + this._rolls[rollIndex + 1] === 10;
         }
+
+        function isStrike() {
+            return this._rolls[rollIndex] === 10;
+        }
+
+        for (let frame = 0; frame < 10; frame++) {
+            if (isSpare.call(this)) {
+                score += 10 + this._rolls[rollIndex + 2];
+                rollIndex += 2;
+            } else if (isStrike.call(this)) {
+                score += 10 + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+                rollIndex += 1;
+            } else {
+                score += this._rolls[rollIndex] + this._rolls[rollIndex + 1];
+                rollIndex += 2;
+            }
+        }
+        return score;
     }
-    return score
-};
+}

2fbd89ab437f2aed3f3aca15fc9b31a70c834da4
karlo <deguzmankarlo@gmail.com>
2019-09-22 14:20:02 +0100

    Keep track of rolls - strikes and spares. Calculation of bonus score yet to be implemented

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,6 +1,44 @@
 describe('Bowling', function() {
-  it('initial score is 0', function() {
+  var bowling;
+
+  beforeEach(function() {
     bowling = new Bowling();
-    expect(bowling.getScore()).toEqual(0);
+  })
+
+  it('initial score is 0', function() {
+    expect(bowling.score).toEqual(0);
+  })
+
+  it('tallies the score on multiple rolls', function() {
+    bowling.roll(1);
+    bowling.roll(2);
+    expect(bowling.score).toEqual(3);
+  })
+
+  describe('records rolls', function() {
+    it('two non strike/spare rolls', function() {
+      bowling.roll(3);
+      bowling.roll(4);
+      expect(bowling.rollsArray).toEqual([3,4]);
+    })
+
+    it('one strike', function() {
+      bowling.roll(10);
+      expect(bowling.rollsArray).toEqual([10, 'X']);
+    })
+
+    it('one spare', function() {
+      bowling.roll(3);
+      bowling.roll(7);
+      expect(bowling.rollsArray).toEqual([3, '/']);
+    })
+
+    it('one strike and one spare', function() {
+      bowling.roll(10);
+      bowling.roll(4);
+      bowling.roll(6);
+      console.log(bowling.score);
+      expect(bowling.rollsArray).toEqual([10, 'X', 4, '/']);
+    })
   })
 })
--- src/bowling.js
+++ src/bowling.js
@@ -1,7 +1,27 @@
 function Bowling() {
   this.score = 0;
+  this.rollsArray = [];
 }
 
-Bowling.prototype.getScore = function() {
-  return this.score;
+Bowling.prototype.roll = function(num) {
+  if (this.isStrike(num)) {
+    this.rollsArray.push(10, 'X');
+  } else if (this.isSpare(num)) {
+    this.rollsArray.push('/');
+  } else {
+    this.rollsArray.push(num);
+  }
+  this.addToScore(num);
+}
+
+Bowling.prototype.isStrike = function(num) {
+  return num === 10 && this.rollsArray.length % 2 === 0;
+}
+
+Bowling.prototype.isSpare = function(num) {
+  return num + this.rollsArray.slice(-1)[0] === 10 && this.rollsArray.length % 2 !== 0;
+}
+
+Bowling.prototype.addToScore = function(num) {
+  this.score += num;
 }

8eaa971f36c46dedc2e5ba3cbb6872aea359581c
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-22 11:47:57 +0100

    score function adds up all rolls no limit towards spares or strikes

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -6,12 +6,19 @@ describe('Game', function(){
   beforeEach(function(){
     game = new BowlingGame();
   });
-  
+
   it('total score is 0 if no pins are knocked in any frame', function(){
     for(var i = 0; i < 20; i++) {
       game.roll(0);
     }
     expect(game.score()).toEqual(0);
+  });
+
+  it('can knock two pins per roll', function(){
+    for(var i = 0; i < 20; i++) {
+      game.roll(2)
+    }
+    expect(game.score()).toEqual(40);
   });
 
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,3 +1,15 @@
 var BowlingGame = function(){
+  this.frameRolls = [];
+};
 
+BowlingGame.prototype.roll = function (pins) {
+  this.frameRolls.push(pins);
+};
+
+BowlingGame.prototype.score = function () {
+  var result = 0;
+  for(var i = 0; i < 20; i++) {
+    result += this.frameRolls[i];
+  }
+  return result;
 };

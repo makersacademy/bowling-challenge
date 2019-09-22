dbcea9a79b29f85c33fd3d495490db39c13b3e0d
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-09-22 01:39:30 +0100

    Complete logics for scoring

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Bowling', function() {
   var bowling;
   beforeEach(function() {
@@ -5,9 +7,36 @@ describe('Bowling', function() {
   });
 
   it('starts from zero points', function() {
-    expect(bowling.score).toEqual(0);
+    expect(bowling.calculateScore()).toEqual(0);
   });
 
+  it ('example single frame', function() {
+    bowling.bowl(5);
+    bowling.bowl(3);
+    expect(bowling.calculateScore()).toEqual(8);
+  });
 
+  it('example ongoing game with multi frames', function() {
+    bowling.bowl(10);
+    bowling.bowl(5);
+    bowling.bowl(4);
+    bowling.bowl(2);
+    bowling.bowl(8);  
+    bowling.bowl(10);
+    expect(bowling.calculateScore()).toEqual(58);
+  });
 
+  it('scores 0 for gutter game', function() {
+    for (var i = 0; i < 20; i++) {
+      bowling.bowl(0);
+    }
+    expect(bowling.calculateScore()).toEqual(0);
+  });
+
+  it('scores 300 for perfect game', function() {
+    for (var i = 0; i < 12; i++) {
+      bowling.bowl(10);
+    }
+    expect(bowling.calculateScore()).toEqual(300);
+  });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,92 @@
-function Bowling() {
-  this.score = 0;
-};
+const strikeBonusRoll = 2;
+const spareBonusRoll = 1;
+
+class Bowling {
+  constructor() {
+    this.frame = 1;
+    this.roll = 1;
+    this.pinsInLane = 10;
+    this.score = [];
+  }
+
+  bowl(pinsHit) {
+    if (this.frame < 10) {
+      this.normalFrame(pinsHit);
+    } else {
+      this.finalFrame(pinsHit);
+    }
+  }
+
+  normalFrame(pinsHit) {
+    if (this.isStrike(pinsHit)) {
+      this.score.push([pinsHit, strikeBonusRoll]);
+      this.nextFrame();
+    } else if (this.roll === 1) {
+      this.score.push([pinsHit, 0]);
+      this.secondRoll(pinsHit);
+    } else if (this.isSpare(pinsHit)) {
+      this.score.push([pinsHit, spareBonusRoll]);   
+      this.nextFrame();
+    } else {
+      this.score.push([pinsHit, 0]);
+      this.nextFrame();
+    }
+    // return this.calculateScore();
+  }
+
+  nextFrame() {
+    this.frame += 1;
+    this.roll = 1;
+    this.pinInLane = 10;
+  }
+
+  secondRoll(pinsHit) {
+    this.roll = 2;
+    this.pinsInLane = 10 - pinsHit;
+  }
+
+  finalFrame(pinsHit) {
+    if  (this.isStrike(pinsHit)) {
+      this.score.push([pinsHit, 0]);
+      this.bonusRoll();
+    } else if (this.roll === 1) {
+      this.score.push([pinsHit, 0]);
+      this.secondRoll();
+    } else if (this.roll === 2 && pinsHit === 10) {
+      this.score.push([pinsHit, 0]);
+      this.bonusRoll();
+    } else if (this.isSpare(pinsHit)) {
+      this.score.push([pinsHit, 0]);
+      this.bonusRoll();
+    } else {
+      this.score.push([pinsHit, 0]);
+    }
+    // return this.calculateScore();
+  }
+
+  bonusRoll() {
+    this.roll += 1;
+    this.pinsInLane = 10;
+  }
+
+  calculateScore() {
+    var totalScore = 0;
+    for (var i = 0; i < this.score.length; i++) {
+      var [pins, bonus] = this.score[i];
+      totalScore += pins;
+      for (var j = 1; j <= bonus && i + j < this.score.length; j++) {
+        var [bonusPins, _] = this.score[i + j];
+        totalScore += bonusPins;
+      }
+    }
+    return totalScore;
+  }
+
+  isStrike(pinsHit) {
+    return (this.roll === 1 && pinsHit === 10);
+  }
+
+  isSpare(pinsHit) {
+    return (this.roll === 2 && pinsHit === this.pinsInLane);
+  }
+}

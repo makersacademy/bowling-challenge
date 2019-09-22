487d68208380e567ce4e401ce09d98815799389c
Student <student@Makerss-MacBook-Pro-2.local>
2019-09-22 12:37:00 +0100

    refactor

--- src/bowling.js
+++ src/bowling.js
@@ -5,39 +5,52 @@ function BowlingCard() {
   this.strike = false;
   this.spare = false;
   this.totalScore = 0;
-  // this.bonus = 0;
 };
 
 BowlingCard.prototype.enterRoll = function(number) {
   this._storeRoll(number);
-  this.totalScore += number;
-  if (this.rollNumber == 1) {
-    this._createNewFrame(); } else { this._addToFrame(); };
+  if (this.rollNumber == 1) { this._addToNewFrame(); }
+  else { this._addToCurrentFrame();
+  };
 };
 
-BowlingCard.prototype.showRoll = function(number) {
-  return this.currentFrame.rolls[number - 1];
+BowlingCard.prototype._storeRoll = function(pinsDown) {
+    this.currentRoll = new Roll();
+    this.currentRoll.pinsDown = pinsDown
+    this.totalScore += pinsDown;
+};
+
+BowlingCard.prototype._addToNewFrame = function() {
+  this._createNewFrame();
+  if (this.spare == true) { this._addBonusToPrevFrame() }
+  this._checkIfStrike();
+};
+
+BowlingCard.prototype._addToCurrentFrame = function() {
+  this.currentFrame.addRoll(this.currentRoll);
+  this.card.push(this.currentFrame);
+  this._endOfFrameChecks();
 };
 
 BowlingCard.prototype._createNewFrame = function() {
   this.currentFrame = new Frame();
   this.currentFrame.addRoll(this.currentRoll);
   this.rollNumber = 2;
-  if (this.spare == true) { this._addBonusToPrevFrame() }
-  this._checkIfStrike();
 };
 
-BowlingCard.prototype._addToFrame = function() {
-  this.currentFrame.addRoll(this.currentRoll);
-  this.card.push(this.currentFrame);
-  this._endOfFrameChecks();
+BowlingCard.prototype.showRoll = function(number) {
+  return this.currentFrame.rolls[number - 1];
 };
 
 BowlingCard.prototype._addBonusToPrevFrame = function() {
-  var prevFrame = this.card[this.frameNumber - 2];
-  prevFrame.score += this.currentFrame.score;
+  this._updatePrevFrameScore();
   this.totalScore += this.currentFrame.score;
   this.strike = false;
+};
+
+BowlingCard.prototype._updatePrevFrameScore = function() {
+  var prevFrame = this.card[this.frameNumber - 2];
+  prevFrame.score += this.currentFrame.score;
 };
 
 BowlingCard.prototype._createRoll = function() {
@@ -46,20 +59,23 @@ BowlingCard.prototype._createRoll = function() {
 
 BowlingCard.prototype._checkIfStrike = function() {
   if (this.currentFrame.strike == true) {
-    this.enterRoll(0);
-    this.strike = true;
-    this.spare = false;
+    this._completeStrikeFrame();
   };
 };
 
-BowlingCard.prototype._storeRoll = function(pinsDown) {
-    this.currentRoll = new Roll();
-    this.currentRoll.pinsDown = pinsDown
+BowlingCard.prototype._completeStrikeFrame = function() {
+  this.enterRoll(0);
+  this.strike = true;
+  this.spare = false;
 };
 
 BowlingCard.prototype._endOfFrameChecks = function() {
   if (this.strike == true) { this._addBonusToPrevFrame();};
   if (this.currentFrame.score == 10) { this.spare = true};
+  this._updateCounts();
+};
+
+BowlingCard.prototype._updateCounts = function() {
   this.frameNumber += 1;
   this.rollNumber = 1;
 };
--- src/frame.js
+++ src/frame.js
@@ -8,18 +8,14 @@ function Frame() {
 
 Frame.prototype.addRoll = function(roll) {
   this._validateThrow();
-  this.rolls.push(roll.pinsDown)
-  this.score += roll.pinsDown
+  this._addRollToFrame(roll);
   this._setBonusFlags();
 };
 
 Frame.prototype._validateThrow = function() {
-  if (this.rolls.length == 2 && this.lastFrame == false){
+if (this._frameComplete() == true || this._lastFrameComplete() == true){
     throw 'Frame already complete';
-  }else if (this.rolls.length == 2 && this.lastFrame == true && this.strike == false && this.spare == false) {
-    throw 'Frame already complete';
-  };
-
+  }
 };
 
 Frame.prototype._setBonusFlags = function() {
@@ -27,7 +23,19 @@ Frame.prototype._setBonusFlags = function() {
   if (this.rolls.length == 2 && this.score == 10) { this.spare = true};
 };
 
-Frame.prototype._addRollToFrame = function(role) {
+Frame.prototype._addRollToFrame = function(roll) {
   this.rolls.push(roll.pinsDown)
   this.score += roll.pinsDown
+};
+
+Frame.prototype._frameComplete = function() {
+  if (this.rolls.length == 2 && this.lastFrame == false){
+    return true;
+  };
+};
+
+Frame.prototype._lastFrameComplete = function() {
+  if (this.rolls.length == 2 && this.lastFrame == true && this.strike == false && this.spare == false){
+    return true;
+  };
 };

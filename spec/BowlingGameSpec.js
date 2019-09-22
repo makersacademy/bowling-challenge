58f4f796848ec907fbd890c1dc60da4d7280a123
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-22 11:25:13 +0100

    first failing test with an empty BowlingGame function

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -1,1 +1,17 @@
 'use strict';
+
+describe('Game', function(){
+  var game
+
+  beforeEach(function(){
+    game = new BowlingGame();
+  });
+  
+  it('total score is 0 if no pins are knocked in any frame', function(){
+    for(var i = 0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(0);
+  });
+
+});

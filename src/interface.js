81cd17cda10f5e8d419e33721a55a2f126171dbf
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-09-23 01:23:23 +0100

    Refactor warning message

--- src/interface.js
+++ src/interface.js
@@ -6,12 +6,28 @@ $(document).ready(function() {
   
   $("#submit").click(function() {
     var pinsHit = parseInt($('input#pins-hit').val());
+
+    if (pinsHit > bowling.pinsInLane) {
+      alert('Invalid input. Number greater than pins in lane.');
+    }
+    if (bowling._gameOver) {
+      alert('Game over!');
+    }
+
+    $('input#pins-hit').val('');
     bowling.bowl(pinsHit);
     updateGame();
   })
 
   function updateGame() {
-    $('#current-score').text(bowling.totalScore);
+    var displayScore;
+    if (bowling._gameOver) {
+      displayScore = 'Game over! Final score: ' + bowling.totalScore;
+    } else {
+      displayScore = 'Current score: ' + bowling.totalScore;
+    }
+
+    $('#display-score').text(displayScore);
     $('#frame').text(bowling.frame);
     $('#roll').text(bowling.roll);
   };

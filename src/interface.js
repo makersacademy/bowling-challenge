c8510ff6a1b61666cf3d896eb3e19db0730da752
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-09-23 17:06:25 +0100

    Add table to html page

--- src/interface.js
+++ src/interface.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 
   $(document).bind('keypress', function(event) {
     if(event.keyCode==13){
-         $('#submit').trigger('click');
-     }
+      $('#submit').trigger('click');
+    }
   });
   
   $("#submit").click(function() {
@@ -20,21 +20,40 @@ $(document).ready(function() {
       alert('Game over!');
     }
 
+    updateTable(pinsHit);
+
     $('input#pins-hit').val('');
     bowling.bowl(pinsHit);
     updateGame();
   })
 
+  function updateTable(pinsHit) {
+    $(`#frame-${bowling.frame}-roll-${bowling.roll}`).text(pinsHit);
+    if (bowling.isStrike(pinsHit)) {
+      $(`#note-${bowling.frame}-strike`).text("Strike");
+    }
+    if (bowling.isSpare(pinsHit)) {
+      $(`#note-${bowling.frame}-spare`).text("Spare");
+    }
+  }
+
   function updateGame() {
+    displayScore();
+    updateStatus();
+  };
+
+  function displayScore() {
     var displayScore;
     if (bowling._gameOver) {
       displayScore = 'Game over! Final score: ' + bowling.totalScore;
     } else {
       displayScore = 'Current score: ' + bowling.totalScore;
     }
-
     $('#display-score').text(displayScore);
+  }
+
+  function updateStatus() {
     $('#frame').text(bowling.frame);
     $('#roll').text(bowling.roll);
-  };
+  }
 })

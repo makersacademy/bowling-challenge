d783705cdc9a2ac0d871eb7f077733d637260fce
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-09-24 01:07:50 +0100

    Refactor html table

--- src/interface.js
+++ src/interface.js
@@ -13,11 +13,11 @@ $(document).ready(function() {
   $("#submit").click(function() {
     var pinsHit = parseInt($('input#pins-hit').val());
 
-    if (pinsHit > bowling.pinsInLane) {
-      alert('Invalid input. Number greater than pins in lane.');
-    }
     if (bowling._gameOver) {
       alert('Game over!');
+    }
+    if (pinsHit > bowling.pinsInLane) {
+      alert('Invalid input. Number greater than pins in lane.');
     }
 
     updateTable(pinsHit);
@@ -29,10 +29,20 @@ $(document).ready(function() {
 
   function updateTable(pinsHit) {
     $(`#frame-${bowling.frame}-roll-${bowling.roll}`).text(pinsHit);
+
     if (bowling.isStrike(pinsHit)) {
       $(`#note-${bowling.frame}-strike`).text("Strike");
     }
     if (bowling.isSpare(pinsHit)) {
+      $(`#note-${bowling.frame}-spare`).text("Spare");
+    }
+    if (bowling.frame ===10 && bowling.isStrike(pinsHit)) {
+      $('#bonus-1').text("Bonus")
+      $('#bonus-2').text("Bonus")
+      $(`#note-${bowling.frame}-strike`).text("Strike");
+    }
+    if (bowling.frame === 10 && bowling.isSpare(pinsHit)) {
+      $('#bonus-2').text("Bonus")
       $(`#note-${bowling.frame}-spare`).text("Spare");
     }
   }
@@ -56,31 +66,23 @@ $(document).ready(function() {
     $('#frame').text(bowling.frame);
     $('#roll').text(bowling.roll);
   }
-
 })
 
 function constructTable() {
   var tableData = '';
   var finalRow = '';
-  for (i = 1; i <= 10; i++) {
+  for (i = 1; i <= 9; i++) {
     tableData += '<tr>'
-                + '<td>' + i + '</td>'
-                + '<td>' + 1 + '</td>'
-                + `<td id=frame-${i}-roll-1>` + '</td>'
-                + `<td id=note-${i}-strike>` + '</td>'
+                + `<td rowspan='2'>${i}</td>`
+                + '<td>1</td>'
+                + `<td id=frame-${i}-roll-1></td>`
+                + `<td id=note-${i}-strike></td>`
                 + '</tr>'
                 + '<tr>'
-                + '<td>' + i + '</td>'
-                + '<td>' + 2 + '</td>'
-                + `<td id=frame-${i}-roll-2>` + '</td>'
-                + `<td id=note-${i}-spare>` + '</td>'
-                + '</tr>'
-    finalRow = '<tr>'
-                + '<td>' + 10 + '</td>'
-                + '<td>' + 3 + '</td>'
-                + `<td id=frame-10-roll-3>` + '</td>'
-                + '<td></td>'
+                + '<td>2</td>'
+                + `<td id=frame-${i}-roll-2></td>`
+                + `<td id=note-${i}-spare></td>`
                 + '</tr>'
   }
-  $('#constructTable').html(tableData + finalRow);
+  $('#constructTable').html(tableData);
 }

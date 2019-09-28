318c662d3aa2b070ac9681acf013b2db1a213e34
Julian George <jules104@gmail.com>
2019-09-28 14:07:48 +0100

    Add addOne function with passing test" git push

--- spec/bowlingChallengeSpec.js
+++ spec/bowlingChallengeSpec.js
@@ -7,4 +7,12 @@ describe("Scorecard", function () {
   it('Expect score to be 0 when game starts', function () {
     expect(scorecard.playerScore).toEqual(0)
   });
+
+  describe('#addOne', function () {
+    it('Expect score to be 1 when player enters 1', function () {
+      scorecard.addOne()
+      expect(scorecard.playerScore).toEqual(1)
+    })
+  })
+
 });
--- src/bowlingChallenge.js
+++ src/bowlingChallenge.js
@@ -1,3 +1,7 @@
 function Scorecard (score = 0) {
   this.playerScore = score
 };
+
+Scorecard.prototype.addOne = function () {
+  this.playerScore += 1 
+}

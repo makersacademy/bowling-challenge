a6fb19129554c6654c12f06fa3d9e96598349997
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-09-29 11:57:48 +0100

    changed the game object to suit me better

--- src/bowling.js
+++ src/bowling.js
@@ -1,34 +1,35 @@
 'use strict';
 
-function Game() {
-  this.frame1 = {roll1: 1, roll2: 4};
-  this.frame2 = {roll1: 4, roll2: 5};
-  this.frame3 = {roll1: 6, roll2: 4};
-  this.frame4 = {roll1: 5, roll2: 5};
-  this.frame5 = {roll1: 10, roll2: 0};
-  this.frame6 = {roll1: 0, roll2: 1};
-  this.frame7 = {roll1: 7, roll2: 3};
-  this.frame8 = {roll1: 6, roll2: 4};
-  this.frame9 = {roll1: 10, roll2: 0};
-  this.frame10 = {roll1: 2, roll2: 8, roll3: 6};
+function Game() {this.frames = [
+  {roll1: 1, roll2: 4},
+  {roll1: 4, roll2: 5},
+  {roll1: 6, roll2: 4},
+  {roll1: 5, roll2: 5},
+  {roll1: 10, roll2: 0},
+  {roll1: 0, roll2: 1},
+  {roll1: 7, roll2: 3},
+  {roll1: 6, roll2: 4},
+  {roll1: 10, roll2: 0},
+  {roll1: 2, roll2: 8, roll3: 6}]
+  console.log(this.frames[0]["roll1"]+this.frames[0]["roll2"])
 }
 
 Game.prototype.score = function(){
   if (this.isStrike() === true) {
-    return this.frame2["roll1"]+this.frame2["roll2"]+10
+    return this.frames[1]["roll1"]+this.frames[1]["roll2"]+10
   }
   else if (this.isSpare() === true) {
-    return this.frame2["roll1"]+10
+    return this.frames[1]["roll1"]+10
   }
   else {
-    return this.frame1["roll1"]+this.frame1["roll2"]
+    return this.frames[0]["roll1"]+this.frames[0]["roll2"]
   }
 }
 
 Game.prototype.isStrike = function(){
-  return this.frame1["roll1"] === 10
+  return this.frames[0]["roll1"]+this.frames["roll2"] === 10
 }
 
 Game.prototype.isSpare = function(){
-  return (this.frame1["roll1"]+this.frame1["roll2"]) === 10
+  return (this.frames[0]["roll1"]+this.frames[0]["roll2"]) === 10
 }

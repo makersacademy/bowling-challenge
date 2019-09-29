c1e4e44739ced82a8abb0f865404dbb9169cfdb9
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-09-29 18:36:36 +0100

    the loop is not working as expected

---
+++ README_SOLUTION.md
@@ -0,0 +1,5 @@
+Bowling challenge
+
+To use the tests copy the full path of the SpecRunner.html file and past it into the browser.  
+
+As the interface was an optional extra, and there were not instructions regarding how exactly a user would enter in the scores I made assumptions which suited me and created a Game object with the frames as instance variables.
--- src/bowling.js
+++ src/bowling.js
@@ -18,22 +18,26 @@ Game.prototype.score = function(){
   for( let i = 0; i < 10; i++) {
   if (this.isStrike(i) === true) {
     this.totalScore.push(this.frames[i+1]["roll1"]+this.frames[i+1]["roll2"]+10)
+    // console.log(1)
   }
   else if (this.isSpare(i) === true) {
-    this.totalScore.push(this.frames[i+1]["roll1"]+10)
+    this.totalScore.push(this.frames[i]["roll1"]+10)
+    // console.log(2)
   }
   else {
     this.totalScore.push(this.frames[i]["roll1"]+this.frames[i]["roll2"])
-  }}
+    // console.log(3)
+  }
+}
   // return console.log(this.totalScore.reduce(function(a,b) {
   //     return a + b}))
   console.log(this.totalScore)
 }
 
-Game.prototype.isStrike = function(n){
+Game.prototype.isStrike = function(n=0){
   return this.frames[n]["roll1"] === 10
 }
 
-Game.prototype.isSpare = function(n){
-  return (this.frames[n]["roll1"]+this.frames[0]["roll2"]) === 10
+Game.prototype.isSpare = function(n=0){
+  return (this.frames[n]["roll1"]+this.frames[n]["roll2"]) === 10
 }

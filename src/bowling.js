eacfb658df6e81f34173b12e4374ab1332b9ce35
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-09-29 16:23:42 +0100

    adds up frames with isStrike function

--- src/bowling.js
+++ src/bowling.js
@@ -16,23 +16,24 @@ function Game() {this.frames = [
 Game.prototype.score = function(){
   this.totalScore = []
   for( let i = 0; i < 10; i++) {
-  if (this.isStrike() === true) {
-    return this.frames[1]["roll1"]+this.frames[1]["roll2"]+10
+  if (this.isStrike(i) === true) {
+    this.totalScore.push(this.frames[i+1]["roll1"]+this.frames[i+1]["roll2"]+10)
   }
-  else if (this.isSpare() === true) {
-    return this.frames[1]["roll1"]+10
+  else if (this.isSpare(i) === true) {
+    this.totalScore.push(this.frames[i+1]["roll1"]+10)
   }
   else {
     this.totalScore.push(this.frames[i]["roll1"]+this.frames[i]["roll2"])
   }}
-  return console.log(this.totalScore.reduce(function(a,b) {
-      return a + b}))
+  // return console.log(this.totalScore.reduce(function(a,b) {
+  //     return a + b}))
+  console.log(this.totalScore)
 }
 
-Game.prototype.isStrike = function(){
-  return this.frames[0]["roll1"]+this.frames["roll2"] === 10
+Game.prototype.isStrike = function(n){
+  return this.frames[n]["roll1"] === 10
 }
 
-Game.prototype.isSpare = function(){
-  return (this.frames[0]["roll1"]+this.frames[0]["roll2"]) === 10
+Game.prototype.isSpare = function(n){
+  return (this.frames[n]["roll1"]+this.frames[0]["roll2"]) === 10
 }

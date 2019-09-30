d8a0eb839e8ba4e0a7c3bb67dd3f5f45405fc3ef
Ben <ben@benzeng.co.uk>
2019-09-30 10:29:35 +0100

    refactored if statements directly into strike and spare properties

--- src/Frame.js
+++ src/Frame.js
@@ -4,12 +4,6 @@ function Frame(bowl1, bowl2, bowl3) {
     this.bowl1 = bowl1;
     this.bowl2 = bowl2;
     this.bowl3 = bowl3;
-    this.strike = false;
-    this.spare = false;
-
-    if (bowl1 === 10)
-        this.strike = true;
-
-    if (bowl1 !== 10 && bowl1 + bowl2 === 10)
-        this.spare = true;
+    this.strike = bowl1 === 10;
+    this.spare = bowl1 !== 10 && bowl1 + bowl2 === 10;
 }

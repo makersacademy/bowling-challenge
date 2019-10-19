737bd3c3c161e2bfe26663ebd84771a290bd0652
Melvin Lau <lau.melvin@icloud.com>
2019-10-19 10:48:13 +0100

    Added setter functions for new frame and new roll

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,21 +1,45 @@
 describe('Game', () => {
 
   let game;
-  
-  beforeEach(() => {
-    game = new Game;
-  });
 
-  it('.score() tracks the total score', () => {
+  beforeEach(() => { game = new Game; });
+
+  // Getters
+
+  it('.getScore() returns the total score', () => {
     expect(game.getScore()).toEqual(0);
   });
 
-  it('.currentFrame() tracks the current frame', () => {
+  it('.getCurrentFrame() returns the current frame', () => {
     expect(game.getCurrentFrame()).toEqual(1);
   });
 
-  it('.getCurrentRoll() tracks the current roll', () => {
+  it('.getCurrentRoll() returns the current roll', () => {
     expect(game.getCurrentRoll()).toEqual(1);
   });
+
+  it('.getBonus() tracks any bonus gained from the previous frame', () => {
+    expect(game.getBonus()).toEqual(0);
+  });
+
+  // Setters
+
+  it('.newRoll() starts the next roll in the current frame', () => {
+    game.newRoll();
+    expect(game.getCurrentRoll()).toEqual(2);
+  });
+
+  it('.newFrame() starts the next frame and resets currentRoll to 1', () => {
+    game.newRoll();
+    game.newFrame();
+    expect(game.getCurrentFrame()).toEqual(2);
+    expect(game.getCurrentRoll()).toEqual(1);
+  });
+
+  // user form submission should trigger a mechanism to add knocks
+  // it('.addKnocks() records the number of knocked pins', () => {
+  //   game.addKnocks(5);
+  //   expect(game.getScore()).toEqual(5);
+  // });
 
 });
--- src/Game.js
+++ src/Game.js
@@ -5,19 +5,25 @@ class Game {
     this.totalScore = 0;
     this.currentFrame = 1;
     this.currentRoll = 1;
+    this.bonus = 0;
+    this.game = new Object;
   }
 
-  getScore() {
-    return this.totalScore;
+  // Getters (Trackers)
+  getScore() { return this.totalScore; }
+  getCurrentFrame() { return this.currentFrame; }
+  getCurrentRoll() { return this.currentRoll; }
+  getBonus() { return this.bonus; }
+
+  // Setters
+
+  newRoll() { this.currentRoll += 1; }
+
+  newFrame() {
+    this.currentFrame += 1;
+    this.currentRoll = 1;
   }
 
-  getCurrentFrame() {
-    return this.currentFrame;
-  }
-
-  getCurrentRoll() {
-    return this.currentRoll;
-  }
 
 
 }

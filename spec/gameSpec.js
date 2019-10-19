adc11906698572f610fc09d7f37f9605ad7b38e5
Nabille Helal <nabilleh@outlook.com>
2019-10-19 10:20:09 +0100

    Add strikes and spares to score card

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,19 +7,55 @@ describe('Game', function() {
     game = new Game();
   });
 
+  describe('addBowl', function() {
+
+    it("adds a bowl to an empty scorecard", function() {
+      game.addBowl(5);
+      expect(game.frames[0][0]).toEqual(5);
+    });
+
+    it("adds another bowl to a score card containing one bowl", function() {
+      game.addBowl(5);
+      game.addBowl(2);
+      expect(game.frames[0][1]).toEqual(2);
+    });
+
+    it("adds a third bowl to a new frame", function() {
+      game.addBowl(5);
+      game.addBowl(2);
+      game.addBowl(8);
+      expect(game.frames[1][0]).toEqual(8);
+    });
+
+    it("adds a strike on the first bowl and ends the turn", function() {
+      game.addBowl("X");
+      expect(game.frames[0][0]).toEqual("X");
+      expect(game.frames[0][1]).toEqual("-");
+    });
+
+    it("adds a spare to the scorecard", function() {
+      game.addBowl(4);
+      game.addBowl(6);
+      expect(game.frames[0][0]).toEqual(4);
+      expect(game.frames[0][1]).toEqual("/");
+    });
+
+  });
+
   describe('calculateScore', function() {
 
     it("calculates the total score for 1 frame", function() {
       game.frames = [[5,2]];
       game.calculateScore();
-      expect(game.totalScore).toEqual(7);
+      expect(game.total_score).toEqual(7);
     });
 
     it("calculates the total score for 2 frames", function() {
       game.frames = [[5,2],[2,7]];
       game.calculateScore();
-      expect(game.totalScore).toEqual(16);
+      expect(game.total_score).toEqual(16);
     });
+
   });
 
 });
--- src/game.js
+++ src/game.js
@@ -1,14 +1,46 @@
 'use strict';
 
 function Game() {
-  this.totalScore = 0;
-  this.frames = [];
+  this.total_score = 0;
+  this.frames = [[],[],[],[],[],[],[],[],[],[]];
+  this.current_frame = 1;
+  this.turn = 1;
 }
+
+Game.prototype.addBowl = function(pins) {
+  this.frames[this.current_frame - 1].push(pins);
+  if ( this.wasStrike() === true ) {
+    this.frames[this.current_frame - 1].push("-");
+  } else if ( this.wasSpare() === true ) {
+    this.frames[this.current_frame - 1][1] = "/";
+  }
+  this.nextFrame();
+};
+
+Game.prototype.wasStrike = function() {
+  if (this.frames[this.current_frame - 1].includes("X")) {
+    return true;
+  }
+};
+
+Game.prototype.wasSpare = function() {
+  if (this.frames[this.current_frame - 1][0] +
+      this.frames[this.current_frame - 1][1] === 10 ) {
+    return true;
+  }
+  
+};
+
+Game.prototype.nextFrame = function() { //Look into making this private
+  if ( this.frames[this.current_frame - 1].length === 2 ) {
+    this.current_frame += 1;
+  }
+};
 
 Game.prototype.calculateScore = function() {
   var total = 0;
   this.frames.forEach(function(frame) {
     total += frame.reduce((partial_sum, a) => partial_sum + a,0);
   });
-  this.totalScore = total
+  return this.total_score = total;
 };

a3936e8808c2be327f86095bc65a72e3e53ea12f
Student <student@Makerss-MBP.home>
2019-10-20 19:46:00 +0100

    set up first failing test

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -5,8 +5,11 @@ describe("Game", function() {
     game = new Game();
   });
 
-  it("", function() {
-    
+  it("allows a person to record a gutter game", function() {
+    for (i=0; i < 20; i++) {
+      game.roll(0);
+    }
+    expect(game.score).toEqual(0);
   });
 
 });

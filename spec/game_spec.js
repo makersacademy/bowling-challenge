f4dd8169c808685685c3dfa0c19e1d31a277f90a
Student <student@Makerss-MBP.home>
2019-10-20 20:04:41 +0100

    tests for adding up final score

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -5,11 +5,29 @@ describe("Game", function() {
     game = new Game();
   });
 
+  it('displays the players score.', function() {
+  expect(game.score).toEqual(0)
+  });
+
+  describe('roll', function() {
+    it('increments the temp', function() {
+      game.roll(1)
+      expect(game.score).toEqual(1)
+    })
+  })
+
   it("allows a person to record a gutter game", function() {
     for (i=0; i < 20; i++) {
       game.roll(0);
     }
     expect(game.score).toEqual(0);
+  });
+
+  it("allows a person to record a normal score, no bonuses", function() {
+    for (i=0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score).toEqual(20);
   });
 
 });

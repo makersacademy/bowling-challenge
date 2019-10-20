11bfad4a92f9d7019da287623697cc0f881b0eaa
AlecDWalker <walkeralec@hotmail.com>
2019-10-20 18:49:54 +0100

    non-strike/split scoring for frame implemented

--- jasmine-standalone-3.5.0/spec/frameSpec.js
+++ jasmine-standalone-3.5.0/spec/frameSpec.js
@@ -10,7 +10,7 @@ describe('Frame', function(){
   it('stores a number to identify itself', function(){
     expect(frame.NUMBER).toEqual(1)
   });
-  
+
   describe('point storage', function(){
     it('starts with a total point score of zero', function(){
       expect(frame.returnpoints()).toEqual(0)
@@ -18,6 +18,19 @@ describe('Frame', function(){
     it('starts with empty scores for the first and second rolls', function(){
       expect(frame.roll1).toEqual(0)
       expect(frame.roll2).toEqual(0)
+    });
+    it('can store a non-strike result as a first roll', function(){
+      frame.firstroll(5)
+      expect(frame.roll1).toEqual(5)
+    });
+    it('can store a non-split result as a second roll', function(){
+      frame.secondroll(3)
+      expect(frame.roll2).toEqual(3)
+    });
+    it('can add the result of a non-bonus pair of rolls', function(){
+      frame.firstroll(3)
+      frame.secondroll(6)
+      expect(frame.POINTS).toEqual(9)
     });
 
   });
--- jasmine-standalone-3.5.0/src/frame.js
+++ jasmine-standalone-3.5.0/src/frame.js
@@ -5,8 +5,20 @@ function Frame(number){
   this.POINTS = 0
   this.roll1 = 0
   this.roll2 = 0
+  this.strike = false
+  this.split = false
 }
 
 Frame.prototype.returnpoints = function(){
   return this.POINTS
+}
+
+Frame.prototype.firstroll = function(pins){
+  this.roll1 = pins
+  this.POINTS = pins
+}
+
+Frame.prototype.secondroll = function(pins){
+  this.roll2 = pins
+  this.POINTS = this.roll1 + pins
 }

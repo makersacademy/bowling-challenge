fed7984f416f1b5cfd7a9ff11ba19db40a36f8d3
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-20 14:48:02 +0100

    Strikes and Spares add to bonus roll

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -12,7 +12,43 @@ describe ('BowlingGame', function() {
     expect(game.score).toEqual(0);
   });
 
+  it('starts on frame 1', function() {
+    expect(game.frame).toEqual(1);
+  });
 
+  it('pushes 2 rolls to a frame', function() {
+    game.inputRoll(1);
+    game.inputRoll(2);
+    expect(game.allRolls).toEqual([[1, 2]])
+  });
+
+  it('pushes 4 rolls to a frame', function() {
+    game.inputRoll(1);
+    game.inputRoll(2);
+    game.inputRoll(4);
+    game.inputRoll(5);
+    expect(game.allRolls).toEqual([[1, 2], [4, 5]])
+  });
+
+  it('pushes a single roll if its a strike to a frame', function() {
+    game.inputRoll(1);
+    game.inputRoll(2);
+    game.inputRoll(10);
+    game.inputRoll(5);
+    game.inputRoll(2);
+    expect(game.allRolls).toEqual([[1, 2], [10], [5, 2]])
+  });
+
+  it('adds 1 to bonus roll when there is a spare', function() {
+    game.inputRoll(5);
+    game.inputRoll(5);
+    expect(game.bonusRoll).toEqual(1);
+  });
+
+  it('adds 2 to bonus roll when there is a strike', function() {
+    game.inputRoll(10);
+    expect(game.bonusRoll).toEqual(2);
+  });
 
 
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -2,4 +2,36 @@
 
 function BowlingGame() {
   this.score = 0;
+  this.frame = 1;
+  this.bonusRoll = 0;
+
+  this.frameRoll = [];
+  this.allRolls = [];
+};
+
+BowlingGame.prototype.inputRoll = function(number) {
+  this.frameRoll.push(number);
+
+  if(number == 10) {
+    this.allRolls.push(this.frameRoll);
+    this.frameRoll = [];
+    this.frame =+ 1;
+    this.bonusRoll += 2;
+  };
+
+  if(this.frameRoll.length == 2) {
+    this.allRolls.push(this.frameRoll);
+
+    var sum = 0;
+    for(var i = 0; i < this.frameRoll.length; i++) {
+      sum += this.frameRoll[i];
+      if(sum == 10) {
+        this.bonusRoll += 1;
+      };
+    };
+
+    this.frameRoll = [];
+    this.frame =+ 1;
+  };
+
 };

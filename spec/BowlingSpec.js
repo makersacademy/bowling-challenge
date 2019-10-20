2c72a1d5f78f939c3eb3d286541400c3f5efa68f
Olly Holovchenko <oleh.holo@gmail.com>
2019-10-20 18:10:57 +0100

    Update models to use only Bowling glass. Add tests for rolls, strike, spare.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -15,9 +15,43 @@ describe('Bowling', () => {
     expect(game.frames).toEqual([]);
   });
 
-  it('Player rolls first time and score changes', () => {
+  it('Roll is stored in the current frame', () => {
     game.roll(5);
 
-    expect(game.getCurrentScore()).toEqual(5);
+    expect(game.currentFrame).toContain(5);
+  });
+
+  it('Frame score is a sum of the rolls', () => {
+    game.roll(4);
+    game.roll(3);
+
+    expect(game.getFrameScore()).toEqual(7);
+  });
+
+  it('Frame is complete when frame score is 10', () => {
+    game.roll(4);
+    game.roll(6);
+
+    expect(game.isFrameComplete()).toEqual(true);
+  });
+
+  it('Player rolls strike', () => {
+    game.roll(10);
+
+    expect(game.isStrike()).toEqual(true);
+  });
+
+  it('Player rolls spare', () => {
+    game.roll(4);
+    game.roll(6);
+
+    expect(game.isSpare()).toEqual(true);
+  });
+
+  it('Frame is complete and stored', () => {
+    game.roll(4);
+    game.roll(6);
+
+    expect(game.frames).toContain([4, 6]);
   });
 });

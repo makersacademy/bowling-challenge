76ad55ee6c484fd2f586e670d59fd671a9373e6b
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-20 15:44:25 +0100

    Refactored strike and spare methods

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -4,7 +4,6 @@ function BowlingGame() {
   this.score = 0;
   this.frame = 1;
   this.bonusRoll = 0;
-
   this.frameRoll = [];
   this.allRolls = [];
 };
@@ -13,25 +12,31 @@ BowlingGame.prototype.inputRoll = function(number) {
   this.frameRoll.push(number);
 
   if(number == 10) {
-    this.allRolls.push(this.frameRoll);
-    this.frameRoll = [];
-    this.frame =+ 1;
-    this.bonusRoll += 2;
+    this.strike();
   };
 
   if(this.frameRoll.length == 2) {
     this.allRolls.push(this.frameRoll);
-
-    var sum = 0;
-    for(var i = 0; i < this.frameRoll.length; i++) {
-      sum += this.frameRoll[i];
-      if(sum == 10) {
-        this.bonusRoll += 1;
-      };
-    };
-
+    this.spare();
     this.frameRoll = [];
-    this.frame =+ 1;
+    this.frame += 1;
   };
 
+};
+
+BowlingGame.prototype.spare = function() {
+  var sum = 0;
+  for(var i = 0; i < this.frameRoll.length; i++) {
+    sum += this.frameRoll[i];
+    if(sum == 10) {
+      this.bonusRoll += 1;
+    };
+  };
+};
+
+BowlingGame.prototype.strike = function() {
+  this.allRolls.push(this.frameRoll);
+  this.frameRoll = [];
+  this.frame += 1;
+  this.bonusRoll += 2;
 };

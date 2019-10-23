8e2cbd77bd317d7474c1fb9ba6f16f521e059b24
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-23 11:16:21 +0100

    Calculates total frame points for 2 frames ago

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -23,33 +23,35 @@ describe ('BowlingGame', function() {
     expect(game.currentFrame).toEqual(3);
   });
 
-  it('adds bonus points to previous frame if there is a spare', function() {
-    game.bowl(5);
-    game.bowl(5);
-    game.bowl(3);
-    game.bowl(1);
-    expect(game.allFrames[1]).toEqual([5, 5, 3]);
-    expect(game.allFrames[2]).toEqual([3, 1]);
-  });
+  describe ('Calculating scores and bonuses', function() {
 
-  it('adds bonus points to previous frame if there are 2 spares', function() {
-    game.bowl(5);
-    game.bowl(5);
-    game.bowl(7);
-    game.bowl(3);
-    game.bowl(2);
-    game.bowl(2);
-    expect(game.allFrames[1]).toEqual([5, 5, 7]);
-    expect(game.allFrames[2]).toEqual([7, 3, 2]);
-  });
+    it('adds bonus points to previous frame if there is a spare', function() {
+      game.bowl(5);
+      game.bowl(5);
+      game.bowl(3);
+      game.bowl(1);
+      expect(game.allFrames[1]).toEqual([5, 5, 3]);
+      expect(game.allFrames[2]).toEqual([3, 1]);
+    });
 
-  it('adds bonus points to previous frame if there is a strike', function() {
-    game.bowl(10);
-    game.bowl(5);
-    game.bowl(1);
-    expect(game.allFrames[1]).toEqual([10, 5, 1]);
-    expect(game.allFrames[2]).toEqual([5, 1]);
-  });
+    it('adds bonus points to previous frame if there are 2 spares', function() {
+      game.bowl(5);
+      game.bowl(5);
+      game.bowl(7);
+      game.bowl(3);
+      game.bowl(2);
+      game.bowl(2);
+      expect(game.allFrames[1]).toEqual([5, 5, 7]);
+      expect(game.allFrames[2]).toEqual([7, 3, 2]);
+    });
+
+    it('adds bonus points to previous frame if there is a strike', function() {
+      game.bowl(10);
+      game.bowl(5);
+      game.bowl(1);
+      expect(game.allFrames[1]).toEqual([10, 5, 1]);
+      expect(game.allFrames[2]).toEqual([5, 1]);
+    });
 
     it('calculates the correct score for a double strike', function() {
       game.bowl(10);
@@ -62,4 +64,29 @@ describe ('BowlingGame', function() {
       expect(game.allFrames[2]).toEqual([10, 5, 1]);
     });
 
+    it('calculates the total points for the frame', function() {
+      game.bowl(10);
+      game.bowl(10);
+      game.bowl(9);
+      game.bowl(1);
+      game.bowl(5);
+      game.bowl(5);
+      game.bowl(10);
+      game.bowl(2);
+      game.bowl(4);
+      game.bowl(3);
+      game.bowl(4);
+      game.bowl(5);
+      game.bowl(2);
+      expect(game.allFramesScore[1]).toEqual(29);
+      expect(game.allFramesScore[2]).toEqual(20);
+      expect(game.allFramesScore[3]).toEqual(15);
+      expect(game.allFramesScore[4]).toEqual(20);
+      expect(game.allFramesScore[5]).toEqual(16);
+      expect(game.allFramesScore[6]).toEqual(6);
+    });
+
+
+
+  });
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -3,7 +3,10 @@
 function BowlingGame() {
   this.allFrames = {"-1": [], 0: [], 1: [], 2: [], 3: [], 4: [], 5: [],
     6: [], 7: [], 8: [], 9: [], 10:[]
-    }
+  };
+  this.allFramesScore = {1: [], 2: [], 3: [], 4: [], 5: [],
+    6: [], 7: [], 8: [], 9: [], 10:[]
+    };
   this.currentFrame = 1;
   this.previousFrame = 0;
   this.previous2Frame = -1;
@@ -13,6 +16,7 @@ BowlingGame.prototype.bowl = function(number) {
   this.frameNumber();
   this.allFrames[this.currentFrame].push(number)
   this.addBonus();
+  this.calculateFrame();
 };
 
 BowlingGame.prototype.frameNumber = function() {
@@ -35,4 +39,15 @@ BowlingGame.prototype.addBonus = function() {
   if(this.allFrames[this.previousFrame][0] + this.allFrames[this.previousFrame][1] == 10 && this.allFrames[this.previousFrame].length < 3) {
     this.allFrames[this.previousFrame].push(this.allFrames[this.currentFrame][0])
   };
+};
+
+BowlingGame.prototype.calculateFrame = function() {
+  var frame = this.allFrames[this.previous2Frame];
+  var sum = 0;
+  var i = 0;
+  for (i = 0; i < frame.length; i++)
+  {
+    sum += frame[i];
+  };
+  this.allFramesScore[this.previous2Frame] = sum;
 };

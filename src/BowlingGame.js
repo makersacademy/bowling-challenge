1167c73ea23e0b59b1f16065a757484ca18769f1
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-25 10:28:47 +0100

    Refacored calculate method

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -30,46 +30,28 @@ BowlingGame.prototype.calculateFrame = function() {
   if(this.allFrames[this.previousFrame][0] == 10 && this.previousFrame > 0 && this.allFrames[this.currentFrame].length == 2) {
     this.allFrames[this.previousFrame].push(this.allFrames[this.currentFrame][0]);
     this.allFrames[this.previousFrame].push(this.allFrames[this.currentFrame][1]);
-    var frame = this.allFrames[this.previousFrame];
-    var sum = 0;
-    var i = 0;
-    for (i = 0; i < frame.length; i++)
-    {
-      sum += frame[i];
-    };
-    this.allFramesScore[this.previousFrame] = sum;
+    this.calculate(this.previousFrame);
   };
   if(this.allFrames[this.previous2Frame][0] == 10 && this.allFrames[this.previousFrame][0] == 10 && this.allFrames[this.previousFrame].length < 3) {
     this.allFrames[this.previous2Frame].push(this.allFrames[this.previousFrame][0]);
     this.allFrames[this.previous2Frame].push(this.allFrames[this.currentFrame][0]);
-    var frame = this.allFrames[this.previous2Frame];
-    var sum = 0;
-    var i = 0;
-    for (i = 0; i < frame.length; i++)
-    {
-      sum += frame[i];
-    };
-    this.allFramesScore[this.previous2Frame] = sum;
+    this.calculate(this.previous2Frame);
   };
   if(this.allFrames[this.previousFrame][0] + this.allFrames[this.previousFrame][1] == 10 && this.allFrames[this.previousFrame].length < 3) {
     this.allFrames[this.previousFrame].push(this.allFrames[this.currentFrame][0]);
-    var frame = this.allFrames[this.previousFrame];
-    var sum = 0;
-    var i = 0;
-    for (i = 0; i < frame.length; i++)
-    {
-      sum += frame[i];
-    };
-    this.allFramesScore[this.previousFrame] = sum;
+    this.calculate(this.previousFrame);
   };
   if(this.allFrames[this.previousFrame][0] + this.allFrames[this.previousFrame][1] < 10) {
-    var frame = this.allFrames[this.previousFrame];
-    var sum = 0;
-    var i = 0;
-    for (i = 0; i < frame.length; i++)
-    {
-      sum += frame[i];
-    };
-    this.allFramesScore[this.previousFrame] = sum;
+    this.calculate(this.previousFrame);
   };
+};
+
+BowlingGame.prototype.calculate = function(frameNumber) {
+  var frame = this.allFrames[frameNumber];
+  var sum = 0;
+  var i = 0;
+  for (i = 0; i < frame.length; i++) {
+    sum += frame[i];
+  };
+  this.allFramesScore[frameNumber] = sum;
 };

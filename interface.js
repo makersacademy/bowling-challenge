8aa75cbeac54b80525b22204025d53e00a2665a8
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-26 18:15:47 +0100

    Scores for 2 frames ago

--- interface.js
+++ interface.js
@@ -6,59 +6,74 @@ $(document).ready(function() {
     $('#score').text(game.cumulativeScore);
   };
 
+  function updateFrame(frameNumber, frameScore) {
+    $('#frame'+frameNumber).text(frameScore);
+  };
+
   $('#bowl-0').click(function() {
     game.bowl(0);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-1').click(function() {
     game.bowl(1);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-2').click(function() {
     game.bowl(2);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-3').click(function() {
     game.bowl(3);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-4').click(function() {
     game.bowl(4);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-5').click(function() {
     game.bowl(5);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-6').click(function() {
     game.bowl(6);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-7').click(function() {
     game.bowl(7);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-8').click(function() {
     game.bowl(8);
     updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-9').click(function() {
     game.bowl(9);
-    updateScore()
+    updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
   $('#bowl-10').click(function() {
     game.bowl(10);
-    updateScore()
+    updateScore();
+    updateFrame(game.previous2Frame, game.allFramesScore[game.previous2Frame]);
   });
 
 });

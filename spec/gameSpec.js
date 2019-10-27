01f57f96636c2216c7784b173c1c6dbe52f1ce7b
Nabille Helal <nabilleh@outlook.com>
2019-10-27 14:25:43 +0000

    Update unit tests to include frame score instead of total score

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -87,7 +87,7 @@ describe('Game', function() {
       game.addBowl(6);
       game.addBowl(1);
       game.calculateScore();
-      expect(game.total_score).toEqual(7);
+      expect(game.frame_scores[0]).toEqual(7);
     });
 
     it("calculates the total score of less than 10 for 2 frames", function() {
@@ -96,7 +96,7 @@ describe('Game', function() {
       game.addBowl(2);
       game.addBowl(7);
       game.calculateScore();
-      expect(game.total_score).toEqual(16);
+      expect(game.frame_scores[1]).toEqual(16);
     });
 
     it("Adds a bonus score to the first frame strike, after the second frame", function() {
@@ -104,25 +104,17 @@ describe('Game', function() {
       game.addBowl(3);
       game.addBowl(1);
       game.calculateScore();
-      expect(game.total_score).toEqual(18);
+      expect(game.frame_scores[0]).toEqual(14);
     });
 
-    it("calculates the correct score for 2 strikes and then a score below 10", function() {
+    it("calculates the correct score frames 1 and 2, for 2 strikes and then a score below 10", function() {
       game.addBowl(10);
       game.addBowl(10);
       game.addBowl(3);
       game.addBowl(0);
       game.calculateScore();
-      expect(game.total_score).toEqual(39);
-    });
-
-    it("calculates the correct score for 2 strikes and then a score below 10", function() {
-      game.addBowl(10);
-      game.addBowl(10);
-      game.addBowl(3);
-      game.addBowl(2);
-      game.calculateScore();
-      expect(game.total_score).toEqual(43);
+      expect(game.frame_scores[0]).toEqual(23);
+      expect(game.frame_scores[1]).toEqual(36);
     });
 
     it("calculates the correct score for 3 strikes and then a score below 10", function() {
@@ -132,7 +124,9 @@ describe('Game', function() {
       game.addBowl(4);
       game.addBowl(0);
       game.calculateScore();
-      expect(game.total_score).toEqual(72);
+      expect(game.frame_scores[0]).toEqual(30);
+      expect(game.frame_scores[1]).toEqual(54);
+      expect(game.frame_scores[2]).toEqual(68);
     });
 
     it("calculates 9 strikes in a row", function() {
@@ -140,7 +134,7 @@ describe('Game', function() {
         game.addBowl(10);
       }
       game.calculateScore();
-      expect(game.total_score).toEqual(240);
+      expect(game.frame_scores[8]).toEqual(240);
     });
 
     it("Adds a bonus score for a spare followed by a score of 2 and 6", function() {
@@ -149,7 +143,7 @@ describe('Game', function() {
       game.addBowl(2);
       game.addBowl(6);
       game.calculateScore();
-      expect(game.total_score).toEqual(20);
+      expect(game.frame_scores[0]).toEqual(12);
     });
 
     it("Adds a bonus score for 2 spares followed by a score of 4 and 3", function() {
@@ -160,7 +154,7 @@ describe('Game', function() {
       game.addBowl(4);
       game.addBowl(3);
       game.calculateScore();
-      expect(game.total_score).toEqual(36);
+      expect(game.frame_scores[1]).toEqual(29);
     });
 
     it("correctly scores 9 strikes and a sub 10 in last frame", function() {
@@ -170,7 +164,7 @@ describe('Game', function() {
       game.addBowl(1);
       game.addBowl(1);
       game.calculateScore();
-      expect(game.total_score).toEqual(245);
+      expect(game.frame_scores[9]).toEqual(245);
     });
 
     it("correctly scores 11 spares and a 5", function() {
@@ -178,28 +172,16 @@ describe('Game', function() {
         game.addBowl(5);
       }
       game.calculateScore();
-      expect(game.total_score).toEqual(150);
-    });
-
-    it("correctly scores a combination of spares and strikes", function() {
-      game.addBowl(5);
-      game.addBowl(5);
-      game.addBowl(10);
-      game.addBowl(10);
-      game.addBowl(5);
-      game.addBowl(5);
-      game.addBowl(1);
-      game.addBowl(9);
-      game.calculateScore();
-      expect(game.total_score).toEqual(86);
+      expect(game.frame_scores[9]).toEqual(150);
     });
 
     it("scores the perfect game as 300", function() {
       for ( var i = 0; i < 12; i++ ) {
         game.addBowl(10);
+        game.calculateScore();
       }
       game.calculateScore();
-      expect(game.total_score).toEqual(300);
+      expect(game.frame_scores[9]).toEqual(300);
     });
   });
 });

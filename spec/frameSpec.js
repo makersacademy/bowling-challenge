d319648b9c68b1db355cc695c7d9c0f0cdf1c0ca
karlo <deguzmankarlo@gmail.com>
2019-11-12 11:17:11 +0000

    Frame function records rolls and detects invalid rolls

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,10 +1,31 @@
-describe('frame', function() {
-  it('is initially empty', function() {
+describe('Frame', function() {
+  let frame;
+
+  beforeEach(function() {
     frame = new Frame();
+  })
+
+  it('is initially empty', function() {
     expect(frame.scores).toEqual([]);
   })
 
-  it('records scores', function() {
-    frame = new Frame();
+  it('records a single roll', function() {
+    frame.roll(1);
+    expect(frame.scores).toEqual([1]);
+  })
+
+  it('records two rolls', function() {
+    frame.roll(1);
+    frame.roll(2);
+    expect(frame.scores).toEqual([1, 2]);
+  })
+
+  it('detects invalid first roll', function() {
+    expect(function() {frame.roll(11)}).toThrow(new Error("Invalid roll"))
+  })
+
+  it('detects invalid second roll', function() {
+    frame.roll(5);
+    expect(function() {frame.roll(6)}).toThrow(new Error("Invalid roll"))
   })
 })

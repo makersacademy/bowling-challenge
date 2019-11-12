95cb7543d46fae3b3197bce450b13b050e3eaadf
karlo <deguzmankarlo@gmail.com>
2019-11-12 09:28:38 +0000

    Introduce frame object

---
+++ spec/frameSpec.js
@@ -0,0 +1,10 @@
+describe('frame', function() {
+  it('is initially empty', function() {
+    frame = new Frame();
+    expect(frame.scores).toEqual([]);
+  })
+
+  it('records scores', function() {
+    frame = new Frame();
+  })
+})

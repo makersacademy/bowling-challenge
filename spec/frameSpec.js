26d8f905395e314dd930f9f7ce6f5e5d92d93396
bradley <bradley.plater1@gmail.com>
2019-11-15 10:18:55 +0000

    Added test for the roll mechanic within frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -23,4 +23,34 @@ describe('Frame', function(){
       expect(frames.rollTwo).toEqual(0);
     });
   });
+
+  describe('Roll', function(){
+    it('Can accept a first roll and add it to the score for the first roll', function(){
+      frames.roll(4);
+      expect(frames.rollOne).toEqual(4);
+    });
+
+    it('Can accept a second roll and add it to the score for the second roll', function(){
+      frames.roll(4);
+      frames.roll(4);
+      expect(frames.rollTwo).toEqual(4);
+    });
+
+    it('Takes both rolls and will set the total score', function(){
+      frames.roll(4);
+      frames.roll(4);
+      expect(frames.score).toEqual(8);
+    });
+
+    it('Will only allow 2 rolls to be set', function(){
+      frames.roll(4);
+      frames.roll(4);
+
+      expect(function() { frames.roll(2); }).toThrowError('Can only accept 2 rolls');
+    });
+
+    it('Will only allow the user to input a score of 10', function(){
+      expect(function() { frames.roll(11); }).toThrowError('Score of over 10 is not possible');
+    });
+  });
 });

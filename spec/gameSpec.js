adddb271bdfe213489294fd714a7c411510f796b
Rob Baile <robbaile12@outlook.com>
2019-11-15 08:35:48 +0000

    added tests for set up of the score and get method to retrieve score

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -6,6 +6,16 @@ describe("Bowling", () => {
     
     beforeEach(() => bowling = new Bowling());
 
+    describe("Bowling Setup", () => {
+        it("should have empty rolls array", () => {
+            expect(bowling.rolls).toEqual([]);
+        });
+
+        it("should start with score of 0", () => {
+            expect(bowling.getScore()).toEqual(0);
+        })
+    });
+
     describe("Each go", () => {
         it('should have a score added to the rolls array when a roll is made', () => {
             bowling.roll(5)

6743b141e4672b608841d2d02c4cc41f9c568010
bradley <bradley.plater1@gmail.com>
2019-11-15 09:27:00 +0000

    Written test for defaults within the scorecard class, could be more added later

---
+++ spec/scorecardSpec.js
@@ -0,0 +1,25 @@
+describe('Scorecard', function(){
+  var scorecard;
+
+  beforeEach(function(){
+    scorecard = new Scorecard;
+  });
+
+  describe('Defaults', function(){
+    it('Can be an instance of scorecard', function(){
+      expect(scorecard).toBeInstanceOf(Scorecard);
+    });
+
+    it('Has default amount of rolls set to 2', function(){
+      expect(scorecard.rolls).toEqual(2);
+    });
+
+    it('Has a total score set to 0', function(){
+      expect(scorecard.totalScore).toEqual(0);
+    });
+
+    it('Has an array of played frames which are empty', function(){
+      expect(scorecard.frames).toBeEmpty();
+    });
+  });
+});

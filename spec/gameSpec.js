42970b7e561b04e5014a1aa99744c4874854d5eb
Rob Baile <robbaile12@outlook.com>
2019-11-15 10:26:55 +0000

    initial test and code for working bowling

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -21,6 +21,14 @@ describe("Bowling", () => {
             bowling.roll(5)
             expect(bowling.rolls[0]).toEqual(5)
         });
+
+        it("should not be able to roll a negative number", () => {
+            expect(() => bowling.roll(-2)).toThrow("roll must be between 0 and 10");
+        });
+
+        it("should not be able to roll a number bigger than 10", () => {
+            expect(() => bowling.roll(11)).toThrow("roll must be between 0 and 10");
+        });
     });
     
     describe("Calculating scores", () => {
@@ -50,7 +58,76 @@ describe("Bowling", () => {
             rolls.map(roll => bowling.roll(roll));
 
             expect(bowling.getScore()).toEqual(16); 
+        });
+
+        it("test consecutive spares each get a one roll bonus", () => {
+            let rolls = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(31); 
+        })
+
+        it("test a spare in the last frame gets a one roll bonus that is counted once", () => {
+            let rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(17); 
+        })
+
+        it("test a strike earns ten points in a frame with a single roll", () => {
+            let rolls = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(10); 
+        })
+
+        it("test points scored in the two rolls after a strike are counted twice as a bonus", () => {
+            let rolls = [10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(26); 
+        })
+
+        it("test consecutive strikes each get the two roll bonus", () => {
+            let rolls = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(81); 
         })
         
-    })
+        it("test a strike in the last frame gets a two roll bonus that is counted once", () => {
+            let rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(18); 
+        });
+
+        it("test rolling a spare with the two roll bonus does not get a bonus roll", () => {
+            let  rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(20); 
+        });
+
+        it("test strikes with the two roll bonus do not get bonus rolls", () => {
+            let  rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(30); 
+        });
+
+        it("test a strike with the one roll bonus after a spare in the last frame does not get a bonus", () => {
+            let  rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(20); 
+        });
+
+        it("all strikes in a perfect game", () => {
+            let rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+            rolls.map(roll => bowling.roll(roll));
+
+            expect(bowling.getScore()).toEqual(300); 
+        })
+    });
 });

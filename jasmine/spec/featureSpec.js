4624d811dc71e6ec758b7a54d2c4d9591eec6cd0
Student <student@Makerss-MacBook-Pro.local>
2019-11-16 21:55:27 +0000

    creating feature tests

---
+++ jasmine/spec/featureSpec.js
@@ -0,0 +1,10 @@
+describe("Features", function() {
+  var game;
+  var frame;
+  var strikeFrame;
+  var spareFrame;
+  beforeEach(function() {
+    game = new Game;
+    frame = new Frame;
+  });
+});

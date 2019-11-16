c23f13819ef213dbfa8d65e579a656efc686e6d9
Emma Van Oss <e.vanoss@gmail.com>
2019-11-16 17:09:22 +0000

    remove bonusMode - cases of strikes/spares to be added to #calculate instead

--- src/scoreTracker.js
+++ src/scoreTracker.js
@@ -3,28 +3,20 @@
 function ScoreTracker (calculate) {
   this._scoreSheet = []
   this._calculate = calculate
-  this._bonusMode = false
 }
 
 ScoreTracker.prototype.add = function (score) {
-  if (this._bonusMode) {
-    console.log('bonus time')
-    return score
-  } else {
-    console.log('calling normalAdd')
-    this._normalAdd(score)
-    return score
-  }
-}
-
-ScoreTracker.prototype._normalAdd = function (score) {
-  var lastIndex = this._scoreSheet.length - 1
+  // first roll of the game
   if (this._scoreSheet.length < 1) {
     this._scoreSheet.push([score])
-  } else if (this._scoreSheet[lastIndex].length === 1) {
-    this._scoreSheet[lastIndex].push(score)
-  } else if (this._scoreSheet[lastIndex].length === 2) {
+    return score
+  }
+  // all other rolls
+  var lastFrame = this._scoreSheet[this._scoreSheet.length - 1]
+  if (lastFrame.length > 1) {
     this._scoreSheet.push([score])
+  } else {
+    lastFrame.push(score)
   }
   return score
 }

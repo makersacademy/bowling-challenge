9daedca44adb400123896f15fe0fedb0a1f3c0ea
Donald Newholm <newholm1@hotmail.com>
2019-11-17 00:03:57 +0000

    Game caps at 10 frames - requires 11th input to trigger ending though

--- jasmine-standalone-3.5.0/spec/bowling-challengeSpec.js
+++ jasmine-standalone-3.5.0/spec/bowling-challengeSpec.js
@@ -2,38 +2,65 @@
 
 describe('Scorer', function(){
 
-    var scorer;
+  var scorer;
 
-    beforeEach(function() { scorer = new Scorer(); } );
+  beforeEach(function() { scorer = new Scorer(); } );
 
-    it('starts with a score of 0', function(){
-       expect(scorer.tally).toEqual(0);
-    });
+  it('starts with a score of 0', function(){
+    expect(scorer.tally).toEqual(0);
+  });
 
-    it('adds a normal frame to the tally', function(){
-       scorer.addFrame(3, 2);
-       expect(scorer.tally).toEqual(5);
-    });
+  it('adds a normal frame to the tally', function(){
+     scorer.addFrame(3, 2);
+     expect(scorer.tally).toEqual(5);
+  });
 
-    it('adds a first roll to the tally', function(){
-       scorer.addFrame(2, 0);
-       expect(scorer.tally).toEqual(2); 
-    });
+  it('adds a first roll to the tally', function(){
+    scorer.addFrame(2, 0);
+    expect(scorer.tally).toEqual(2); 
+  });
 
-    it('adds a second roll to the tally', function(){
-       scorer.addFrame(0, 6);
-       expect(scorer.tally).toEqual(6); 
-    });
+  it('adds a second roll to the tally', function(){
+    scorer.addFrame(0, 6);
+    expect(scorer.tally).toEqual(6); 
+  });
 
-    it('adds a strike bonus to the tally', function(){
-       scorer.addFrame(10, 0);
-       scorer.addFrame(3, 4);
-       expect(scorer.tally).toEqual(24);
-    });
+  it('adds a strike bonus to the tally', function(){
+    scorer.addFrame(10, 0);
+    scorer.addFrame(3, 4);
+    expect(scorer.tally).toEqual(24);
+  });
 
-    it('adds a spare bonus to the tally', function(){
-        scorer.addFrame(7, 3);
-        scorer.addFrame(4, 5);
-        expect(scorer.tally).toEqual(23);
-    });
+  it('adds a spare bonus to the tally', function(){
+    scorer.addFrame(7, 3);
+    scorer.addFrame(4, 5);
+    expect(scorer.tally).toEqual(23);
+  });
+
+  it('runs for a maximum of ten frames', function(){
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    scorer.addFrame(2,3);
+    console.log(scorer.frame);
+    expect(scorer.tally).toEqual(50);
+  });
 });
+
--- jasmine-standalone-3.5.0/src/bowling-challenge.js
+++ jasmine-standalone-3.5.0/src/bowling-challenge.js
@@ -1,16 +1,19 @@
 function Scorer(){
   this.tally = (0);  
-  this.strike = false;
-  // this.spare = false
+  this.frame = (1);
 };
 
 Scorer.prototype.addFrame = function(roll1, roll2) {
-  if (this.strike) { this.tally += (roll1 + roll2) };
-  if (this.spare) { this.tally += (roll1) };
-  this.tally += (roll1 + roll2);
-  if (roll1 === (10)) { (this.strike = true) }
-  else { (this.strike = false) };
-  if ((this.strike === false) && (roll1 + roll2 === 10)) {
-    this.spare = true
+  if (this.frame <= (10)) {
+    if (this.strike) { this.tally += (roll1 + roll2) };
+    if (this.spare) { this.tally += (roll1) };
+    this.tally += (roll1 + roll2);
+    if (roll1 === (10)) { (this.strike = true) }
+    else { (this.strike = false) };
+    if ((this.strike === false) && (roll1 + roll2 === 10)) {
+      this.spare = true };
+    this.frame += (1);
+   } else {
+     return (this.tally);
+   };
   };
-};

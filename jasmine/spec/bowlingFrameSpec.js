2693986188c51f23bdef24052c29febee70064d8
Ingrid Bjarman <ingridbjarman@Makerss-MacBook-Pro-6.local>
2019-11-17 00:40:51 +0000

    Frame structure set up, all tests passing. Has no rolls on initialization, is set to complete after a strike or when both rolls are entered.

--- jasmine/spec/bowlingFrameSpec.js
+++ jasmine/spec/bowlingFrameSpec.js
@@ -1,2 +1,38 @@
 'use strict';
 
+describe ('frames', function() {
+  
+  var frame;
+
+  beforeEach(function() {
+    frame = new Frame()
+  });
+
+  it ('is not completed when created', function() {
+    expect(frame.isComplete()).toEqual(false);
+  });
+
+  it ('is not set to Strike when created', function() {
+    expect(frame.isStrike()).toEqual(false);
+  });
+
+  it ('is not set to Spare when created', function() {
+    expect(frame.isSpare()).toEqual(false);
+  });
+
+  it ('all rolls are set to 0 when created', function() {
+    expect(frame.roll1).toEqual(null);
+    expect(frame.roll2).toEqual(null);
+  });
+
+  it ('is complete when the first roll is a strike', function() {
+    frame.roll1 = 10;
+    expect(frame.isComplete()).toEqual(true);
+  });
+
+  it ('is complete when both rolls have been entered', function() {
+    frame.roll1 = 6;
+    frame.roll2 = 3;
+    expect(frame.isComplete()).toEqual(true);
+  });
+});
--- jasmine/src/bowlingFrame.js
+++ jasmine/src/bowlingFrame.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function Frame() {
+  this.complete = false;
+  this.roll1 = null
+  this.roll2 = null
+};
+
+Frame.prototype.isComplete = function() {
+  if(this.roll2 !== null || this.isStrike()) {
+    return this.complete = true
+  }
+  return this.complete;
+};
+
+Frame.prototype.isStrike = function() {
+  return this.roll1 === 10;
+};
+
+Frame.prototype.isSpare = function() {
+  return this.roll1 + this.roll2 === 10;
+};
+

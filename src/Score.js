0fdf887a3e1d5feceb2c089e6e62017b2c908654
Sara <srancati0@gmail.com>
2019-11-17 23:59:08 +0000

    calculating score at end of game

--- src/Score.js
+++ src/Score.js
@@ -15,23 +15,43 @@ Score.prototype.addRoll = function(one, two) {
 }
 
 Score.prototype.totalScore = function(array){
+
+    for (var j=1; j<=array.length; j++) {
+        if (this.isStrike(array[j-1])) {
+            if(array[j] === undefined){
+                break
+            }
+            this.subtotal += (array[j][0] + array[j][1]);
+        }
+        else if (this.isSpare(array[j-1])) {
+            if(array[j] === undefined){
+                break
+            }
+            this.subtotal += array[j][0];
+        }
+    }
+
     for(var i=0; i<array.length; i++){
         this.subtotal += this.singleScoreSum(array[i]);
-        console.log(this.subtotal)
+    }
+
+    return this.subtotal;
+}
+
+
+Score.prototype.showSum = function(){
+    if (this.subtotal === 0) {
+        return 'Gutter game'
     }
     return this.subtotal;
 }
 
-Score.prototype.showSum = function(){
-    return this.subtotal;
-}
-
 Score.prototype.isSpare = function([a,b]) {
-    (a + b) === 10 ? true : false;
+    return ((a + b) === 10 ? true : false);
 }
 
 Score.prototype.isStrike = function([a,b]) {
-    a === 10 ? true : false;
+    return (a === 10 ? true : false);
 }
 
 // [[1,4],[4,5],[6,4],[5,5],[10,0],[0,1],[7,3],[6,4],[10,0],[2,8,6]]
--- src/interface.js
+++ src/interface.js
@@ -7,12 +7,34 @@ $(document).ready(function(){
       score.totalScore(score.scoreArray);
     }
 
+  //   $("#enter-value").change(function()
+  //   {
+  //   if (score.scoreArray.length == 9) {
+  //     $('roll-three').show(); 
+  //   } else {
+  //   $('roll-three').hide(); 
+  //   }
+  //  });
+  //  $('roll-three').hide(); 
+
     $('#enter-value').submit(function(event) {
         event.preventDefault();
         score1 = parseInt($('#roll-one').val());
         score2 = parseInt($('#roll-two').val());
         score3 =  parseInt($('#roll-three').val());
+
+        $(".error").remove();
+
+        if (score1 >10) {
+          $('#roll-one').after('<span class="error">Maximum value for a roll is 10</span>');
+        }
+
+        if ((score1 + score2) >10) {
+        $('#roll-two').after('<span class="error">Maximum value for a frame is 10</span>');
+        }
+
         score.addRoll(score1,score2);
+
         $('#display-array').text(score.scoreArray.join(" - "));
         $('#display-frame').text(score.scoreArray.length);
       });

683bf6c0cc2ef4b904e8d53ed8a5d53d279455c4
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-17 22:15:43 +0000

    Added error handling on data entry

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -13,22 +13,45 @@ describe ('frame', function() {
     expect(frame_score.getScore()).toEqual(0);
   });
 
+  it('throws an error if we pass a negative number when creating a new \
+  object', function() {
+    expect(function(){frame_score = new Frame(-1,5);}).toThrow("No negative scores");
+  });
+
+  it('throws an error if we pass numbers greater than 10 when creating \
+  a new object', function() {
+    expect(function(){frame_score = new Frame(20,0);}).toThrow("No scores\
+ over 10");
+  });
+
+  it('throws an error if our total scores are more than ten on each \
+  roll', function() {
+    expect(function(){frame_score = new Frame(8,8);}).toThrow("Frame\
+ scores should not exceed 10");
+  });
+
   it('returns 9 on a 4 and 5 roll', function() {
     frame_score = new Frame(4,5)
     expect(frame_score.getScore()).toEqual(9);
   });
 
-  it('returns no bonus if it is a regular roll', function() {
+  it('returns nil if the frame has nothing special', function() {
     frame_score = new Frame(3,6)
-    expect(frame_score.getBonus()).toMatch("nil");
+    expect(frame_score.getFrameType()).toMatch("nil");
   });
 
   it('returns strike if the frame has a strike', function() {
     frame_score = new Frame(10,0)
-    expect(frame_score.getBonus()).toMatch("strike");
+    expect(frame_score.getFrameType()).toMatch("strike");
   });
+
   it('returns spare if the frame has a spare', function() {
     frame_score = new Frame (0,10)
-    expect(frame_score.getBonus()).toMatch("spare");
+    expect(frame_score.getFrameType()).toMatch("spare");
   });
+
+  it('returns 3 when that is the first roll', function() {
+    frame_score = new Frame (3,4)
+    expect(frame_score.getFirstRoll()).toEqual(3);
+  })
 });
--- src/frame.js
+++ src/frame.js
@@ -1,22 +1,49 @@
 'use strict';
 
 function Frame(firstroll, secondroll) {
+  this.firstroll = firstroll
+  this.secondroll = secondroll
+  this.validateRolls()
   this.rolls =[firstroll, secondroll];
-};
+}
 
 var output
 
+Frame.prototype.validateRolls = function() {
+  if (this.firstroll < 0 || this.secondroll < 0) {
+    throw "No negative scores";
+  }
+  if (this.firstroll > 10 || this.secondroll >10) {
+    throw "No scores over 10";
+  }
+  if ((this.firstroll + this.secondroll) >10) {
+    throw "Frame scores should not exceed 10";
+  }
+}
+
 Frame.prototype.getScore = function() {
-  output = this.rolls[0] + this.rolls[1]
+  output = this.rolls[0] + this.rolls[1];
   return (output);
 };
 
-Frame.prototype.getBonus = function() {
+Frame.prototype.getFrameType = function() {
   if (this.getScore() != 10) {
     return('nil');
-  } else if (this.rolls[0] != 10) {
-    return('spare');
   } else {
-    return('strike');
+   output = this.isSpareorStrike();
+    return(output);
   }
+};
+
+Frame.prototype.isSpareorStrike = function() {
+  if (this.rolls[0] === 10) {
+    return('strike');
+  } else {
+    return('spare');
+  }
+};
+
+Frame.prototype.getFirstRoll = function() {
+  output = this.rolls[0];
+  return(output);
 };

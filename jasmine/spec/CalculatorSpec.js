2d5bfb3a649b607ceaec1b60fa81aab27b995431
ellieSMASH <eliseaston@gmail.com>
2019-11-17 15:29:29 +0000

    isolated scoring function for strikes

--- jasmine/spec/CalculatorSpec.js
+++ jasmine/spec/CalculatorSpec.js
@@ -1,7 +1,7 @@
 describe("Calculator", function() {
 
   beforeEach(function() {
-    calculator = new Calculator([[1, 4], [9, 1], [10, 0], [3, 6], [10, 0], [10, 0], [10, 0]]);
+    calculator = new Calculator([[1, 4], [9, 1], [10, 0], [3, 6], [10, 0], [10, 0], [7, 2]]);
   })
 
   it("if a player doesn't bowl a strike or a spare, the score for the frame is the sum of the two rolls", function() {
@@ -11,5 +11,17 @@ describe("Calculator", function() {
   it("if a player bowls a spare, the score for that frame includes the next roll as a bonus", function() {
     expect(calculator.spareScore(calculator._frames[1], calculator._frames[2])).toEqual(20);
   })
+
+  it("if a player bowls a strike, they get the next two rolls as a bonus score if the next roll is NOT a strike", function() {
+    expect(calculator.strikeScore(calculator._frames[2], calculator._frames[3])).toEqual(19);
+  })
+
+  it("if a player bowls a strike, they get the next two rolls as a bonus, even if the next roll is a strike", function() {
+    expect(calculator.strikeScore(calculator._frames[4], calculator._frames[5], calculator._frames[6])).toEqual(27);
+  })
+
+  it("a user can enter the results from a whole game and see the final score", function() {
+    expect(calculator.classify()).toEqual(108);
+  });
 
 });
--- jasmine/src/Calculator.js
+++ jasmine/src/Calculator.js
@@ -2,25 +2,34 @@ Calculator = function(frames) {
   this._frames = frames
 };
 
-Calculator.prototype.classify = function(frame) {
-    if (frame[0] === 10) {
+Calculator.prototype.classify = function() {
+  for(i = 0; i < this._frames.length; i++) {
+    if (this._frames[i][0] === 10) {
       // STRIKE
       console.log("STRIKE")
-    } else if (frame[0] + frame[1] === 10) {
+    } else if (this._frames[i][0] + this._frames[i][1] === 10) {
       // SPARE
       console.log("SPARE")
     } else {
       // NORMAL ROLL
       console.log("NORMAL ROLL")
-      score = this.normalScore(this._frames[i]);
+      // score = this.normalScore(this._frames[i]);
     };
+  };
 };
 
 Calculator.prototype.normalScore = function(frame) {
   return frame[0] + frame[1];
 };
 
-
 Calculator.prototype.spareScore = function(frame1, frame2) {
   return frame1[0] + frame1[1] + frame2[0];
+};
+
+Calculator.prototype.strikeScore = function(frame1, frame2, frame3) {
+  if (frame2[0] === 10) {
+    return frame1[0] + frame2[0] + frame3[0];
+  } else {
+    return frame1[0] + frame2[0] + frame2[1];
+  };
 };

7b160c1e5a50e56c9221cac7bfecd512ef60f713
Natalie <49326857+natyeo@users.noreply.github.com>
2019-11-17 12:58:44 +0000

    set up project

---
+++ spec/gameSpec.js
@@ -0,0 +1,15 @@
+describe ('Game', function() {
+
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+  });
+
+  it ('calculates a gutter game', function() {
+    game.roll(0)
+    expect(game.score()).toEqual(0);
+  })
+
+
+    });
---
+++ src/game.js
@@ -0,0 +1,9 @@
+function Game() {
+}
+
+Game.prototype.roll = function(pins) {
+};
+
+Game.prototype.score = function() {
+  return 0;
+}

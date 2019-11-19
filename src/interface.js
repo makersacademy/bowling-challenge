1593ceafd1adebe5dab5d1005d51e34478c8738b
Sara <srancati0@gmail.com>
2019-11-19 20:10:49 +0000

    refactored and changed html

--- src/interface.js
+++ src/interface.js
@@ -7,45 +7,52 @@ $(document).ready(function(){
       score.totalScore(score.scoreArray);
     }
 
-  //   $("#enter-value").change(function()
-  //   {
-  //   if (score.scoreArray.length == 9) {
-  //     $('roll-three').show(); 
-  //   } else {
-  //   $('roll-three').hide(); 
-  //   }
-  //  });
-  //  $('roll-three').hide(); 
+    $('#roll-three').hide();
+    $(".no_game").hide(); 
 
     $('#enter-value').submit(function(event) {
         event.preventDefault();
         score1 = parseInt($('#roll-one').val());
         score2 = parseInt($('#roll-two').val());
-        score3 =  parseInt($('#roll-three').val());
+        score3 = parseInt($('#roll-three').val());
 
         $(".error").remove();
+      
+        if (score.scoreArray.length === 8) {
+          $('#roll-three').show(); 
+        } 
+
+        if (score.scoreArray.length > 8) {
+          $('.game').hide();
+          $('.no_game').show();
+        } 
 
         if (score1 >10) {
           $('#roll-one').after('<span class="error">Maximum value for a roll is 10</span>');
+          return false;
         }
 
         if ((score1 + score2) >10) {
         $('#roll-two').after('<span class="error">Maximum value for a frame is 10</span>');
+        return false;
         }
 
         score.addRoll(score1,score2);
 
         $('#display-array').text(score.scoreArray.join(" - "));
-        $('#display-frame').text(score.scoreArray.length);
+        $('#display-frame').text((score.scoreArray.length) + 1);
+        calculateScore();
+        $('#display-score').text(score.showSum());
+        
       });
     
       
 
 
-      $('#calculate-score').submit(function(event) {
-        event.preventDefault();
-        calculateScore();
-        $('#display-score').text(score.showSum());
-      });
+      // $('#calculate-score').submit(function(event) {
+      //   event.preventDefault();
+      //   calculateScore();
+      //   $('#display-score').text(score.showSum());
+      // });
       
 });

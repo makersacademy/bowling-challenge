9495c6b50f600040d832a2af5091a8a480aec597
fahus <faduma.h@yahoo.com>
2019-11-23 15:45:19 +0000

    can roll all ones

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,15 +6,26 @@ describe ('Game', function(){
     game = new Game();
   });
 
-  it ('can calculate a gutter game', function(){
+  it ('calculates a gutter game', function(){
     rollMany(20, 0);
     expect(game.score()).toBe(0)
   });
+
+  it ('can roll all ones', function(){
+    rollMany(20, 1);
+    expect(game.score()).toBe(20);
+  });
+
 
 
   function rollMany(rolls, pins){
      for(var i = 0; i < rolls; i ++) {
        game.roll(pins)
      };
+   };
+
+   function rollSpare(){
+     game.roll(5);
+     game.roll(5);
    };
  });
--- src/game.js
+++ src/game.js
@@ -8,5 +8,11 @@ Game.prototype.roll = function (pins) {
 
 Game.prototype.score = function () {
   var score = 0
-  return score;
+  var frameIndex = 0;
+  for (var frame = 0; frame < 10; frame ++)
+  };
+
+  Game.prototype.isSpare = function (frameIndex) {
+      return this.rolls[frameIndex] + this.roll[frameIndex + 1 ] == 10
+  };
 };

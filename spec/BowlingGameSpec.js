e2725eda670249f4cfa0fe38d0138d872f5f1f95
fahus <faduma.h@yahoo.com>
2019-11-24 19:09:29 +0000

    rolls perfect game - score 300

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -33,6 +33,11 @@ it('can roll all ones', function(){
     expect(game.score()).toBe(24);
   });
 
+  it('can roll a perfect game', function(){
+    rollMany(10, 12)
+    expect(game.score()).toBe(300)
+  });
+
 
 
   var rollMany = function (pins, rolls){

da459a9b14093be22cf7096c3bcde20617e54af7
Donald Newholm <newholm1@hotmail.com>
2019-11-24 22:20:45 +0000

    Two semi functional versions - too late to continue

--- jasmine-standalone-3.5.0/spec/bowling-challengeSpec.js
+++ jasmine-standalone-3.5.0/spec/bowling-challengeSpec.js
@@ -1,70 +1,74 @@
-// 'use strict';
+// ATTEMPT 1
 
-// describe('Scorer', function(){
+'use strict';
 
-//   var scorer;
+describe('Scorer', function(){
 
-//   beforeEach(function() { scorer = new Scorer(); } );
+  var scorer;
 
-//   it('starts with a score of 0', function(){
-//     expect(scorer.tally).toEqual(0);
-//   });
+  beforeEach(function() { scorer = new Scorer(); } );
 
-//   it('adds a normal frame to the tally', function(){
-//      scorer.addFrame(3, 2);
-//      expect(scorer.tally).toEqual(5);
-//   });
+  it('starts with a score of 0', function(){
+    expect(scorer.tally).toEqual(0);
+  });
 
-//   it('adds a first roll to the tally', function(){
-//     scorer.addFrame(2, 0);
-//     expect(scorer.tally).toEqual(2); 
-//   });
+  it('adds a normal frame to the tally', function(){
+     scorer.addFrame(3, 2);
+     expect(scorer.tally).toEqual(5);
+  });
 
-//   it('adds a second roll to the tally', function(){
-//     scorer.addFrame(0, 6);
-//     expect(scorer.tally).toEqual(6); 
-//   });
+  it('adds a first roll to the tally', function(){
+    scorer.addFrame(2, 0);
+    expect(scorer.tally).toEqual(2); 
+  });
 
-//   it('adds a strike bonus to the tally', function(){
-//     scorer.addFrame(10, 0);
-//     scorer.addFrame(3, 4);
-//     expect(scorer.tally).toEqual(24);
-//   });
+  it('adds a second roll to the tally', function(){
+    scorer.addFrame(0, 6);
+    expect(scorer.tally).toEqual(6); 
+  });
 
-//   it('adds a spare bonus to the tally', function(){
-//     scorer.addFrame(7, 3);
-//     scorer.addFrame(4, 5);
-//     expect(scorer.tally).toEqual(23);
-//   });
+  it('adds a strike bonus to the tally', function(){
+    scorer.addFrame(10, 0);
+    scorer.addFrame(3, 4);
+    expect(scorer.tally).toEqual(24);
+  });
 
-//   it('scores 0 for a gutter game', function(){
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(0,0);
-//     scorer.addFrame(2,3);
-//     expect(scorer.tally).toEqual(0);
-//   });
+  it('adds a spare bonus to the tally', function(){
+    scorer.addFrame(7, 3);
+    scorer.addFrame(4, 5);
+    expect(scorer.tally).toEqual(23);
+  });
 
-//   it('scores a maximum of ten frames', function(){
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     scorer.addFrame(2,3);
-//     expect(scorer.addFrame(2,3)).toEqual("Final score: 50");
-//   });
-// });
+  it('scores 0 for a gutter game', function(){
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(0,0);
+    scorer.addFrame(2,3);
+    expect(scorer.tally).toEqual(0);
+  });
+
+  it('scores a maximum of ten frames', function(){
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    scorer.addFrame(2,3);
+    expect(scorer.addFrame(2,3)).toEqual("Final score: 50");
+  });
+});
+
+// ATTEMPT 2
 
 'use strict';
 
@@ -79,26 +83,67 @@ describe('Scorecard', function(){
   });
 
   it('starts with an array', function(){
-    expect(scorecard.array).toEqual([]);
+    expect(scorecard.rolls).toEqual([]);
   });
 
   it('array is empty before first roll', function(){
-    expect(scorecard.array).not.toEqual([0]);
+    expect(scorecard.rolls).not.toEqual([0]);
   });
 
   it('records a roll1 of 0 as 0', function(){
     scorecard.roll1(0);
-    expect(scorecard.array[1]).toEqual(0);
+    expect(scorecard.rolls[0]).toEqual(0);
   });
 
   it('records a roll1 of 9 as 9', function(){
     scorecard.roll1(9);
-    expect(scorecard.array[1]).toEqual(9);
+    expect(scorecard.rolls[0]).toEqual(9);
   });
 
   it('records a roll1 of 10 as a strike', function(){
     scorecard.roll1("X");
-    expect(scorecard.array[1]).toEqual(10);
+    expect(scorecard.rolls[0]).toEqual(10);
+  });
+
+  it('records a roll2 of 0 as 0', function(){
+    scorecard.roll1(8);
+    scorecard.roll2(0);
+    expect(scorecard.rolls[1]).toEqual(0);
+  });
+
+  it('records a roll2 of 9 as 9', function(){
+    scorecard.roll1(0);
+    scorecard.roll2(9);
+    expect(scorecard.rolls[1]).toEqual(9);
+  });
+
+  it('recognises a strike', function(){
+    scorecard.roll1(10);
+    expect(scorecard.strike).toEqual(true);
+  });
+
+  it('recognises a spare', function(){
+    scorecard.roll1(3);
+    scorecard.roll2(7);
+    expect(scorecard.spare).toEqual(true);
+  });
+
+  it('awards a bonus for a spare', function(){
+    scorecard.roll1(4);
+    scorecard.roll2(6);
+    scorecard.roll1(5);
+    expect(scorecard.frames[scorecard.frames.length-1]).toEqual(15);
+  });
+
+  it('awards a bonus for a strike', function(){
+    console.log(scorecard.frames);
+    scorecard.roll1(10);
+    console.log(scorecard.frames);
+    scorecard.roll1(4);
+    console.log(scorecard.frames);
+    scorecard.roll2(4);
+    console.log(scorecard.frames);
+    expect(scorecard.frames[scorecard.frames.length-2]).toEqual(18);
   });
 
 });
--- jasmine-standalone-3.5.0/src/bowling-challenge.js
+++ jasmine-standalone-3.5.0/src/bowling-challenge.js
@@ -1,30 +1,61 @@
-// function Scorer(){
-//   this.tally = (0);  
-//   this.frame = (1);
-// };
+// ATTEMPT 1
 
-// Scorer.prototype.addFrame = function(roll1, roll2) {
-//   if (this.frame <= (10)) {
-//     if (this.strike) { this.tally += (roll1 + roll2) };
-//     if (this.spare) { this.tally += (roll1) };
-//     this.tally += (roll1 + roll2);
-//     if (roll1 === (10)) { (this.strike = true) }
-//     else { (this.strike = false) };
-//     if ((this.strike === false) && (roll1 + roll2 === 10)) {
-//       this.spare = true };
-//     this.frame += (1);
-//    } else {
-//      return "Final Score: " + (this.tally);
-//    };
-//   };
+function Scorer(){
+  this.tally = (0);  
+  this.frame = (1);
+};
+
+Scorer.prototype.addFrame = function(roll1, roll2) {
+  if (this.frame <= (10)) {
+    if (this.strike) { this.tally += (roll1 + roll2) };
+    if (this.spare) { this.tally += (roll1) };
+    this.tally += (roll1 + roll2);
+    if (roll1 === (10)) { (this.strike = true) }
+    else { (this.strike = false) };
+    if ((this.strike === false) && (roll1 + roll2 === 10)) {
+      this.spare = true };
+    this.frame += (1);
+   } else {
+     return "Final Score: " + (this.tally);
+   };
+  };
+
+// ATTEMPT 2
 
 function Scorecard(){
   this.tally = (0);  
   this.frame = (1);
-  this.array = [];
+  this.rolls = [];
+  this.spare = (false);
+  this.strike = (false);
+  this.frameTally = (0);
+  this.frames = [];
 };
 
 Scorecard.prototype.roll1 = function(pins) {
-  pins === "X" ? this.array[this.frame] = 10 :
-  this.array[this.frame] = pins;
+
+  if (this.spare === (true) || this.strike === (true)) { 
+    this.frames[this.frames.length-1] += pins;
+    this.spare = false;
+  };
+
+  if (pins === "X" || pins === (10)) {
+    this.rolls.push(10);
+    this.rolls.push(0);
+    this.strike = true;
+    this.frame ++;
+    this.frames.push(this.frameTally);
+  } else {
+    this.rolls.push(pins);
+  } this.frameTally += pins;
+
+Scorecard.prototype.roll2 = function(pins) {
+  this.frameTally += pins;
+  if (this.frameTally > (10)) { return "Too many pins"; };
+  if (this.frameTally === (10)) { this.spare = (true); };
+  if (this.strike === (true)) { this.frames[this.frames.length-1] += pins; };
+  this.rolls.push(pins);
+  this.frames.push(this.frameTally);
+  this.frameTally = (0);
+  };
 };

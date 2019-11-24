8e13a80ee8edb6a2f4c4e893051d05f5298d88fc
Robert Woolley <rob@robertwoolley.co.uk>
2019-11-24 10:34:27 +0000

    Final frame supports / and X

--- src/finalFrame.js
+++ src/finalFrame.js
@@ -3,9 +3,6 @@
 function FinalFrame(firstroll, secondroll, thirdroll){
   this.firstroll = firstroll
   this.secondroll = secondroll
-  if (isNaN(thirdroll)) {
-    thirdroll = 0;
-  }
   this.thirdroll = thirdroll
   this.validateRolls()
 }
@@ -24,17 +21,46 @@ FinalFrame.prototype.getRawScore = function(){
   return (output);
 };
 FinalFrame.prototype.validateRolls = function(){
+
+  if (this.firstroll === 'X' || this.firstroll === 'x') {
+    this.firstroll = 10
+  }
+  if (this.secondroll === 'X' || this.secondroll === 'x') {
+    this.secondroll = 10
+  }
+  if (this.thirdroll === 'X' || this.thirdroll === 'x') {
+    this.thirdroll = 10
+  }
+  if (this.secondroll === '/') {
+    this.secondroll = (10 - this.firstroll)
+  }
+  if (this.thirdroll === '/' && this.firstroll === 10) {
+    this.thirdroll = (10 - this.secondroll);
+  } else  if (this.thirdroll === '/' && this.secondroll != 10){
+    throw "Spare not valid"
+  }
+
+  if (isNaN(this.firstroll)) {
+    this.firstroll = 0
+  }
+  if (isNaN(this.secondroll)) {
+    this.secondroll = 0
+  }
+  if (isNaN(this.thirdroll)) {
+    this.thirdroll = 0
+  }
+
   if (this.firstroll < 0 || this.secondroll < 0
-   || this.thirdroll < 0) {
-    throw "No negative scores";
-  }
+    || this.thirdroll < 0) {
+      throw "No negative scores";
+    }
   if (this.firstroll > 10 || this.secondroll >10
-  || this.thirdroll > 10) {
-    throw "No scores over 10";
-  }
+    || this.thirdroll > 10) {
+      throw "No scores over 10";
+    }
   if ((this.firstroll + this.secondroll < 10) &&
-  this.thirdroll > 0) {
+    this.thirdroll > 0) {
     throw "No third roll unless there is a \
 strike or spare earlier in the frame";
-  }
-};
+      }
+    };

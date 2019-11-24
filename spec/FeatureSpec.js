5e3e9bd0ed208fa11a3f204f9dfdad36cdd44464
Student <student@Makerss-MacBook-Pro.local>
2019-11-24 23:31:57 +0000

    Basic frame rules added

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -1,1 +1,29 @@
 
+
+// describe('Feature Test', function(){
+//   var firstroll
+//   var secondroll
+//   var frame
+//   var pin1
+//   var pin2
+//
+//   beforeEach(function(){
+//     frame = new Frame();
+//   });
+//
+//   // As a player
+//   // So that I can keep score
+//   // I want to be able to record score for a regular frame
+//   it('gives 1 point per pin knocked down', function(){
+//     frame.firstroll(pin1);
+//     expect(frame.score()).toEqual((pin1 + pin2));
+//   });
+//
+//   // As a player
+//   // So that I can keep score
+//   // I want to be able to record score for a strike in frame.firstroll
+//
+//   // As a player
+//   // So that I can keep score
+//   // I want to be able to record score for a strike in frame.secondroll
+// });

96e58ac57aa8a051b95444e3d57abde4c3843900
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2019-11-25 00:55:06 +0000

    completed first draft of frame scoring.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -6,6 +6,26 @@ describe("Bowling Game", function(){
   });
 
   it("should have a starting score of '0' zero", function(){
-    expect(bowl.score).toEqual(0);
+    expect(bowl._score).toEqual(0);
+  });
+
+  it("can increase the score by '1'",function(){
+    bowl.increaseScore(1);
+    expect(bowl._score).toEqual(1);
+  });
+
+  it("score cannot go over the value '10'", function(){
+    bowl._score = 10;
+    expect(function(){ bowl.increaseScore()}).toThrow("Strike: Maximum frame score attained");
+  });
+
+  it("can decrease the score by '-1'", function(){
+    bowl.decreaseScore(1);
+    expect(bowl._score).toEqual(-1);
+  });
+
+  it("can calculate the subtotal score of a frame", function(){
+    bowl.frameSubTotal(5,4);
+    expect(bowl._score).toEqual(9);
   });
 });

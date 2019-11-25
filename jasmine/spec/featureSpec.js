5a8702559a2ab5733c4482ec1f4aac04cb156e78
Student <student@Makerss-MacBook-Pro.local>
2019-11-25 09:43:10 +0000

    cleaning

--- jasmine/spec/featureSpec.js
+++ jasmine/spec/featureSpec.js
@@ -7,4 +7,11 @@ describe("Features", function() {
     game = new Game;
     frame = new Frame;
   });
+
+  it("can display a single frame's score" function() {
+    frame.roll(8)
+    frame.roll(2)
+    game.addFrame(frame)
+    expect(game.printGame();).toEqual([frame]);
+  });
 });

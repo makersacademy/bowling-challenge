3288b021a72de478f05e8663cc895cda06fb810d
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2019-12-14 17:11:48 +0000

    Add test and code to check that the game can calculate bonus points

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -8,33 +8,23 @@ describe("Game", function() {
   });
 
   describe("when setting up the game", function() {
-    it("has a default total score of 0", function() {
+    it("has a total score of 0", function() {
       expect(game.getTotalScore()).toEqual(0);
     });
-  
-    it("has an empty defaul frame sheet", function() {
-      expect(game.getFrameSheet()).toEqual([]);
-    });
-  
-    it("starts with a current roll number of 1", function() {
-      expect(game.getCurrentRollNumber()).toEqual(1);
-    });
-  })
 
-  describe("when calculating the score for frames 0 to 9", function() {
-    it("can calculate the score of a frame without spare or strike", function() {
-      game.calculateFrameScore(2, 5);
-      expect(game.getTotalScore()).toEqual(7);
+    it("has an empty score sheet", function() {
+      expect(game.getScoreSheet()).toEqual([]);
     });
+  });
 
-    it("can calculate the score of a frame with spare", function() {
-      game.calculateFrameScore(2, "/");
-      expect(game.getTotalScore()).toEqual(10);
-    });
+  it("can calculate the score of a frame", function() {
+    game.play(2, 5);
+    expect(game.getTotalScore()).toEqual(7);
+  });
 
-    it("can calculate the score of a frame with strike", function() {
-      game.calculateFrameScore("X");
-      expect(game.getTotalScore()).toEqual(10);
-    });
+  it("can add bonus points to the previous frame, if there was a spare", function() {
+    game.play(2, 8);
+    game.play(2, 5);
+    expect(game.getTotalScore()).toEqual(19);
   });
 });
--- src/game.js
+++ src/game.js
@@ -2,29 +2,50 @@
 
 function Game() {
   this.totalScore = 0;
-  this.frameSheet = [];
-  this.NUMBER_OF_FRAMES = 10;
-  this.currentRollNumber = 1;
+  this.scoreSheet = [];
+  this.isPreviousFrameSpare = false;
+  this.isPreviusFrameStrike = false;
 };
 
 Game.prototype.getTotalScore = function() {
   return this.totalScore;
 };
 
-Game.prototype.getFrameSheet = function() {
-  return this.frameSheet;
+Game.prototype.getScoreSheet = function() {
+  return this.scoreSheet;
 };
 
-Game.prototype.getCurrentRollNumber = function() {
-  return this.currentRollNumber;
+Game.prototype.play = function(roll1, roll2) {
+  this.calculateBonusPoints(roll1, roll2);
+  this.resetPreviousFrame();
+  this.setSpareOrStrike(roll1, roll2);
+  this.scoreSheet.push({ pins: [roll1, roll2], score: roll1 + roll2 });
+  this.totalScore = this.calculateTotalScore();
 };
 
-Game.prototype.calculateFrameScore = function(roll1, roll2) {
-  if (roll1 === "X" ) {
-    this.totalScore += 10;
-  } else if (roll2 === "/") {
-      this.totalScore += 10;
-    } else {
-      this.totalScore += roll1 + roll2;
-  }
+Game.prototype.calculateBonusPoints = function(roll1, roll2) {
+  if (this.isPreviousFrameSpare) {
+    this.scoreSheet[this.scoreSheet.length - 1].score += roll1;
+  } else if (this.isPreviusFrameStrike) {
+    this.scoreSheet[this.scoreSheet.length - 1].score += roll1 + roll2;
+  };
+};
+
+Game.prototype.resetPreviousFrame = function() {
+  this.isPreviousFrameSpare = false;
+  this.isPreviousFrameStrike = false;
+}
+
+Game.prototype.setSpareOrStrike = function(roll1, roll2) {
+  if (roll1 === 10) {
+    this.isPreviusFrameStrike = true;
+  } else if (roll1 + roll2 === 10) {
+    this.isPreviousFrameSpare = true;
+  };
+}
+
+Game.prototype.calculateTotalScore = function() {
+  return this.scoreSheet.reduce((total, frame) => {
+    return total + frame.score;
+  }, 0);
 };

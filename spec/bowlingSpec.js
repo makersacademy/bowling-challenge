46545cded137a1ef728e2c5af2d4039524970acd
Hyan18 <Hyan18@github.com>
2019-12-14 16:15:14 +0000

    add score and rolling

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,1 +1,26 @@
 'use strict';
+
+
+describe("Bowling", function() {
+
+  var bowling
+
+  beforeEach(function(){
+    bowling = new Bowling();
+  });
+
+  it("has a total score", function() {
+    bowling.score = [1, 5]
+    console.log(bowling.total())
+    expect(bowling.total()).toEqual(6)
+  });
+
+  describe("#roll", function() {
+    it("adds the roll to the score", function() {
+      bowling.roll(5)
+      expect(bowling.score).toContain(5)
+    });
+
+  });
+
+});
--- src/bowling.js
+++ src/bowling.js
@@ -1,1 +1,14 @@
 'use strict';
+
+function Bowling() {
+  this.score = [];
+};
+
+Bowling.prototype.total = function() {
+  return this.score.reduce((a,b) => a + b, 0);
+};
+
+
+Bowling.prototype.roll = function(pins) {
+  this.score.push(pins)
+};

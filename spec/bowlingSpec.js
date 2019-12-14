cc65537c9b9c74c4ee7f4c3d2125e43d02d20268
Hyan18 <Hyan18@github.com>
2019-12-14 16:26:51 +0000

    ESLint refactor

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,26 +1,22 @@
-'use strict';
+'use strict'
 
-
-describe("Bowling", function() {
-
+describe('Bowling', function () {
   var bowling
 
-  beforeEach(function(){
-    bowling = new Bowling();
-  });
+  beforeEach(function () {
+    bowling = new Bowling()
+  })
 
-  it("has a total score", function() {
+  it('has a total score', function () {
     bowling.score = [1, 5]
     console.log(bowling.total())
     expect(bowling.total()).toEqual(6)
-  });
+  })
 
-  describe("#roll", function() {
-    it("adds the roll to the score", function() {
+  describe('#roll', function () {
+    it('adds the roll to the score', function () {
       bowling.roll(5)
       expect(bowling.score).toContain(5)
-    });
-
-  });
-
-});
+    })
+  })
+})
--- src/bowling.js
+++ src/bowling.js
@@ -1,14 +1,13 @@
-'use strict';
+'use strict'
 
-function Bowling() {
-  this.score = [];
+function Bowling () {
+  this.score = []
 };
 
-Bowling.prototype.total = function() {
-  return this.score.reduce((a,b) => a + b, 0);
-};
+Bowling.prototype.total = function () {
+  return this.score.reduce((a, b) => a + b, 0)
+}
 
-
-Bowling.prototype.roll = function(pins) {
+Bowling.prototype.roll = function (pins) {
   this.score.push(pins)
-};
+}

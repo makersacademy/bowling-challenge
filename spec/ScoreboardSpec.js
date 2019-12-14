d1e1db4fa7ea5a5304ae9506973229de39bb346e
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-14 22:34:52 +0000

    Add test and conditions for second roll

--- spec/ScoreboardSpec.js
+++ spec/ScoreboardSpec.js
@@ -9,21 +9,36 @@ describe('Scoreboard', function () {
     });
 
     it('has no rolls by default', function () {
-        expect(scoreboard.rolls()).toEqual([]);
+        expect(scoreboard.rolls).toEqual([]);
     });
 
     it('can add rolls', function () {
         scoreboard.addRoll(8);
-        expect(scoreboard.rolls()).toContain(8);
+        expect(scoreboard.rolls).toContain(8);
     });
 
     it('does not add a roll with a score of < 0', function(){
         scoreboard.addRoll(-1);
-        expect(scoreboard.rolls()).not.toContain(-1);
+        expect(scoreboard.rolls).not.toContain(-1);
     });
 
     it('does not add a roll with a score of > 10', function(){
         scoreboard.addRoll(11);
-        expect(scoreboard.rolls()).not.toContain(11);
+        expect(scoreboard.rolls).not.toContain(11);
     });
+
+    describe('second roll', function(){
+
+        it('when added to first roll, the sum does not exceed 10', function(){
+            scoreboard.rolls = [4];
+            scoreboard.addRoll(7);
+            expect(scoreboard.rolls).not.toContain(7);
+        });
+
+        it('when to added to a strike, it returns null', function(){
+            scoreboard.addRoll(10);
+            expect(scoreboard.rolls).toEqual([10, null]);
+        });
+    });
+
 });
--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,18 +1,29 @@
 'use strict';
 
 function Scoreboard() {
-    this._rolls = [];
-};
-
-Scoreboard.prototype.rolls = function () {
-    return this._rolls;
+    this.rolls = [];
 };
 
 Scoreboard.prototype.addRoll = function (roll) {
-    if ((roll >= 0) && (roll <= 10)) {
-        this._rolls.push(roll);
+    var maxRoll = 10;
+
+    if (this.rolls.length % 2 === 0) {
+        maxRoll = 10;
+    } else {
+        maxRoll = 10 - this.rolls[-1];
     };
+
+    if ((roll >= 0) && (roll <= maxRoll)) {
+        this.rolls.push(roll);
+    };
+
+    if ((this.rolls.length % 2 === 1) && (roll === 10)) {
+        this.rolls.push(null);
+    };
+
+
 };
+
 
 
 

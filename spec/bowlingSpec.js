a2f1823d5f52ccdfec828c9498ad8cea27367b0a
Daniel <demartinezgatell@gmail.com>
2019-12-15 14:16:00 +0000

    gutter game

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -20,3 +20,17 @@ describe('Game', function() {
     });
 
 });
+
+describe('Scorings', function() {
+
+  var game;
+
+  it('tests whether it was gutter game!', function(){
+    game = new Game
+    for (var i = 0; i < 11; i++) {
+      game.addFrame(0,0);
+    }
+    expect(game.gutter()).toEqual("It's a gutter game!")
+  });
+
+});
--- src/bowling.js
+++ src/bowling.js
@@ -15,7 +15,7 @@ Game.prototype.pinNumber = function (number) {
   return "Invalid role please, number of pins needs to be between 0 and 10"
 }
 
-Game.prototype.rollNumber = function (number) {
+Game.prototype.rollNumber = function () {
   var i = 0; i ++;
   if(i % 2 == 0) {
     return 2;
@@ -26,4 +26,16 @@ Game.prototype.rollNumber = function (number) {
  Game.prototype.addFrame = function (roll1, roll2) {
    this._frame.push(roll1, roll2);
    return this._frame;
+ }
+
+ Game.prototype.gutter = function () {
+   var sum = 0;
+   var arr = this._frame;
+   for (var i = 0; i < arr.length; i++) {
+  sum += arr[i];
+  }
+  if(sum == 0) {
+    return "It's a gutter game!";
+  }
+  return ;
  }

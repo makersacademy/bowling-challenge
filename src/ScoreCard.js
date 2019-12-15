bcb433c22c1785d6d03ba1b920a044df467b9155
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-12-15 23:38:25 +0000

    Added rudimentary check for spare bonus, with applied score multiplier

--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -3,6 +3,8 @@ ScoreCard = function(){
   this.throw = 1;
   this.score = 0;
   this.pins = 0;
+  this.spare = false;
+  this.strike = false;
 };
 ScoreCard.prototype.frameTrack = function(){
   if(this.throw === 2 || this.pins === 10){
@@ -14,9 +16,14 @@ ScoreCard.prototype.frameReset = function(){
   this.frame += 1;
   this.throw = 0;
   this.score += this.pins;
+  if(this.pins === 10) this.spare = true;
   this.pins = 0;
 };
 ScoreCard.prototype.bowl = function(hit){
+  if(this.spare === true){
+    hit *= 2;
+    this.spare = false
+  };
   this.pins += hit;
   this.frameTrack();
 };

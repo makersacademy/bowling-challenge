0111de97936980a20ee3f751204d77fcacab5ebe
Jamie95187 <jamiewong63@googlemail.com>
2019-12-15 21:44:53 +0000

    add test for checking if the previous frames were strikes or spares.

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -8,12 +8,46 @@ describe('Bowling', function(){
   });
 
   it('should add the number of pins knocked to the scorecard', function(){
-    bowling.addScore([5,'/'])
-    expect(bowling.scorecard).toContain([5,'/']);
+    bowling.addScore([5,5]);
+    expect(bowling.scorecard).toContain([5,5]);
+  });
+
+  it('should return return true if the frame before last was a spare', function(){
+    bowling.addScore([5,5]);
+    bowling.addScore([4,]);
+    expect(bowling.isPrevSpare()).toBeTruthy();
+  });
+
+  it('should return false if the frame before the last was not a spare', function(){
+    bowling.addScore([4,5]);
+    bowling.addScore([2,]);
+    expect(bowling.isPrevSpare()).toBeFalsy();
+  });
+
+  it('should return false if there has only been one frame played', function(){
+    bowling.addScore([4,5]);
+    expect(bowling.isPrevSpare()).toBeFalsy();
+  });
+
+  it('should return true if the frame before last was a strike', function(){
+    bowling.addScore([10,'-']);
+    bowling.addScore([2,8]);
+    expect(bowling.isPrevStrike()).toBeTruthy();
+  });
+
+  it('should return false if the frame before last was not a strike', function(){
+    bowling.addScore([7, 0]);
+    bowling.addScore([10,'-']);
+    expect(bowling.isPrevStrike()).toBeFalsy();
+  });
+
+  it('should return false if only one frame has been played', function(){
+    bowling.addScore([1,8]);
+    expect(bowling.isPrevStrike()).toBeFalsy();
   });
 
   it('should return true if the last frame was a spare', function(){
-    bowling.addScore([5,'/']);
+    bowling.addScore([5,5]);
     expect(bowling.isSpare()).toBeTruthy();
   });
 
@@ -23,12 +57,26 @@ describe('Bowling', function(){
   })
 
   it('should return true if the last frame was a strike', function(){
-    bowling.addScore(['X', '-']);
+    bowling.addScore([10,'-']);
     expect(bowling.isStrike()).toBeTruthy();
   });
 
   it('should return false if the last frame was not a strike', function(){
-    bowling.addScore([9, '/']);
+    bowling.addScore([9,1]);
     expect(bowling.isStrike()).toBeFalsy();
-  })
+  });
+
+  it('should return the accumulated total score of 9', function(){
+    bowling.addScore([4,5]);
+    bowling.updateTotal();
+    expect(bowling.total).toEqual(9);
+  });
+
+  // it('should return the accumulated total score of 15', function(){
+  //   bowling.addScore([9,1]);
+  //   bowling.addScore([5,]);
+  //   bowling.updateTotal();
+  //   expect(bowling.total).toEqual(15);
+  // });
+
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,14 +2,35 @@
 
 function Bowling(){
   this.scorecard = [];
+  this.total = 0;
+  this.firstThrow = true;
 };
 
 Bowling.prototype.addScore = function(score){
   this.scorecard.push(score);
+  this.firstThrow = !this.firstThrow;
+};
+
+Bowling.prototype.isPrevSpare = function(){
+  if(this.scorecard.length > 1){
+    if(this.scorecard[this.scorecard.length-2][0] + this.scorecard[this.scorecard.length-2][1] == 10){
+      return true;
+    };
+  };
+  return false;
+};
+
+Bowling.prototype.isPrevStrike = function(){
+  if(this.scorecard.length > 1){
+    if(this.scorecard[this.scorecard.length-2][0] == 10){
+      return true;
+    };
+  };
+  return false;
 };
 
 Bowling.prototype.isSpare = function(){
-  if (this.scorecard[this.scorecard.length-1][1] === '/'){
+  if (this.scorecard[this.scorecard.length-1][0] + this.scorecard[this.scorecard.length-1][1] == 10){
     return true;
   } else {
     return false;
@@ -17,9 +38,21 @@ Bowling.prototype.isSpare = function(){
 };
 
 Bowling.prototype.isStrike = function(){
-  if (this.scorecard[this.scorecard.length-1][0] === 'X'){
+  if (this.scorecard[this.scorecard.length-1][0] == 10){
     return true;
   } else {
     return false;
+  };
+};
+
+Bowling.prototype.updateTotal = function(){
+  if(!this.isPrevSpare() && !this.isPrevStrike()){
+    if(!this.isSpare() && !this.isStrike()){
+      if(!this.firstThrow){
+        this.total += this.scorecard[this.scorecard.length-1][0] + this.scorecard[this.scorecard.length-1][1];
+      };
+    } else if(this.isSpare())
+  } else if(this.isSpare()){
+    this.total += 10 + this.scorecard[this.scorecard.length-1][0,'']
   };
 };

fd08aadbaebce0fe38d59d3e05dbd91606e14d3d
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-12-15 19:35:36 +0000

    tidy up semi-colons

--- src/bowling.js
+++ src/bowling.js
@@ -1,56 +1,55 @@
 var Frame = require('../src/frame.js');
 
 function Bowling(frames = 10) {
-  this._maxFrames = frames
-  this._frames = [new Frame()]
-  this._frame = 0
-  this._end = false
-  this._lastFrame
+  this._maxFrames = frames;
+  this._frames = [new Frame()];
+  this._frame = 0;
+  this._end = false;
+  this._lastFrame = false;
 }
 
 // public functions
 Bowling.prototype.roll = function(...args) {
   args.forEach( pins => {
-    this._currentFrame().inputRoll(pins)
-    if(this._frames.length > 1) this._extraPoints(pins)
-    if(!this._currentFrame().canRoll()) this._newFrame()
+    this._currentFrame().inputRoll(pins);
+    if(this._frames.length > 1) this._extraPoints(pins);
+    if(!this._currentFrame().canRoll()) this._newFrame();
     // this.checkEnd()  
   });
 }
 
 Bowling.prototype.totalScore = function() {
-  console.log(this._frames[0].score())
-  return this._frames.reduce((a, b) => a + b.score(), 0) 
+  return this._frames.reduce((a, b) => a + b.score(), 0);
 }
 
 Bowling.prototype.frameNum = function() {
-  return (this._frames.length)
+  return (this._frames.length);
 }
 
 // private functions
 
 Bowling.prototype._currentFrame = function() {
-  return this._frames[this._frames.length - 1]
+  return this._frames[this._frames.length - 1];
 }
 
 Bowling.prototype._newFrame = function() {
-  this._frames.push(new Frame())
+  this._frames.push(new Frame());
 }
 
 Bowling.prototype._oneFrameBack = function() {
-  return this._frames[this.frameNum() - 2]
+  return this._frames[this.frameNum() - 2];
 }
 
 Bowling.prototype._twoFramesBack = function() {
-  return this._frames[this.frameNum() - 3]
+  return this._frames[this.frameNum() - 3];
 }
 
 Bowling.prototype._extraPoints = function (pins) {
   if(this.frameNum() > 2 && this._twoFramesBack().numExtras() > 0) {
-    this._twoFramesBack().inputExtra(pins)
-    this._oneFrameBack().inputExtra(pins)
+    this._twoFramesBack().inputExtra(pins);
+    this._oneFrameBack().inputExtra(pins);
   } else if(this._oneFrameBack().numExtras() > 0) {
-    this._oneFrameBack().inputExtra(pins)
+    this._oneFrameBack().inputExtra(pins);
   }
 }
 
--- src/frame.js
+++ src/frame.js
@@ -1,38 +1,23 @@
 function Frame() {
   this._rolls = []
+  this._extras = []
   this._numRolls = 2
   this._extrasRequired = 0
-  this._extras = []
 }
 
 Frame.prototype.inputRoll = function(pins) {
   if(!this.canRoll()) throw new Error ("Cannot Roll, frame has ended");
   else {
     this._rolls.push(pins);
-    this._numRolls--
-    if(!this.isStrike()) this.isSpare()
+    this._numRolls--;
+    if(!this._isStrike()) this._isSpare();
   }
 }
 
 Frame.prototype.score = function() {
-  var rollScore = this._rolls.reduce((a,b) => a + b, 0)
-  var extraScore = this._extras.reduce((a,b) => a + b, 0)
+  var rollScore = this._rolls.reduce((a,b) => a + b, 0);
+  var extraScore = this._extras.reduce((a,b) => a + b, 0);
   return (rollScore + extraScore);
-}
-
-Frame.prototype.isStrike = function () {
-  if(this._rolls[this._rolls.length - 1] === 10) {
-    this._numRolls = 0
-    this._extrasRequired = 2
-    return true
-  }
-}
-
-Frame.prototype.isSpare = function () {
-  if(this._rolls.reduce((a,b) => a + b, 0) === 10) {
-    this._extrasRequired = 1
-    return true
-  }
 }
 
 Frame.prototype.canRoll = function() {
@@ -40,7 +25,7 @@ Frame.prototype.canRoll = function() {
 }
 
 Frame.prototype.numExtras = function() {
-  return this._extrasRequired
+  return this._extrasRequired;
 }
 
 Frame.prototype.inputExtra = function(pins) {
@@ -48,7 +33,24 @@ Frame.prototype.inputExtra = function(pins) {
 }
 
 Frame.prototype.closed = function() {
-  return !this._numRolls && !this._extrasRequired
+  return !this._numRolls && !this._extrasRequired;
+}
+
+// private functions
+
+Frame.prototype._isSpare = function () {
+  if(this._rolls.reduce((a,b) => a + b, 0) === 10) {
+    this._extrasRequired = 1;
+    return true;
+  }
+}
+
+Frame.prototype._isStrike = function () {
+  if(this._rolls[this._rolls.length - 1] === 10) {
+    this._numRolls = 0;
+    this._extrasRequired = 2;
+    return true;
+  }
 }
 
 module.exports = Frame

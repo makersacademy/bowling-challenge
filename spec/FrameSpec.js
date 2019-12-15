4ebb41f621d3494aa055878eb84b6ab0c3a90a46
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-12-15 15:22:31 +0000

    implemented spare and strike logic to Frame class. tests to pass

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -16,22 +16,69 @@ describe("Frame", function() {
     expect(frame.score()).toEqual(7)
   });
 
-  it ("should have a score of 9 when 7 and 2 are rolled and the frame should end", function() {
+  it ("should have a score of 9 when 7 and 2 are rolled and no more rolls should be allowed and the frame should be closed", function() {
     frame.inputRoll(7)
     frame.inputRoll(2)
     expect(frame.score()).toEqual(9)
-    expect(frame.isEnded()).toEqual(true)
+    expect(frame.canRoll()).toEqual(false)
+    expect(frame.closed()).toEqual(true)
   });
 
   it ("should end the frame if a strike is rolled", function() {
     frame.inputRoll(10)
     expect(frame.score()).toEqual(10)
-    expect(frame.isEnded()).toEqual(true)
+    expect(frame.canRoll()).toEqual(false)
   });
 
   it ("allow for 2 extras if a strike is rolled", function() {
     frame.inputRoll(10)
-    expect(frame.extras()).toEqual(2)
+    expect(frame.numExtras()).toEqual(2)
   });
+
+  it ("should show extras required is 1 and update the score 1 roll after a strike", function() {
+    frame.inputRoll(10)
+    frame.inputExtra(7)
+    expect(frame.score()).toEqual(17)
+    expect(frame.numExtras()).toEqual(1)
+  });
+
+  it ("should show extras required is 0 and update the score 2 rolls after a strike", function() {
+    frame.inputRoll(10)
+    frame.inputExtra(7)
+    frame.inputExtra(2)
+    expect(frame.score()).toEqual(19)
+    expect(frame.numExtras()).toEqual(0)
+  });
+
+  it ("should close the frame once no rolls or no extras are required after a strike", function() {
+    frame.inputRoll(10)
+    frame.inputExtra(7)
+    frame.inputExtra(2)
+    expect(frame.closed()).toEqual(true)
+  });
+
+
+  it ("allow for 1 extras if a spare is rolled", function() {
+    frame.inputRoll(7)
+    frame.inputRoll(3)
+    expect(frame.numExtras()).toEqual(1)
+  });
+
+  it ("should show extras required is 0 and update the score 1 rolls after a spare", function() {
+    frame.inputRoll(7)
+    frame.inputRoll(3)
+    frame.inputExtra(7)
+    expect(frame.score()).toEqual(17)
+  });
+
+  it ("should close the frame once no rolls or no extras are required after a spare", function() {
+    frame.inputRoll(7)
+    frame.inputRoll(3)
+    frame.inputExtra(7)
+    expect(frame.closed()).toEqual(true)
+  });
+
+  
+
   
 });
--- src/frame.js
+++ src/frame.js
@@ -1,39 +1,57 @@
 function Frame() {
   this._rolls = []
-  this._frameEnded = false
-  this._extras = 0
+  this._numRolls = 2
+  this._extrasRequired = 0
+  this._extras = []
 }
 
 Frame.prototype.inputRoll = function(pins) {
-  if(this._frameEnded) throw new Error ("Cannot Roll, frame has ended");
-  else this._rolls.push(pins);
-  this.isStrike()
-  this.frameEndCheck()
+  if(!this.canRoll()) throw new Error ("Cannot Roll, frame has ended");
+  else {
+    this._rolls.push(pins);
+    this._numRolls--
+    if(!this.isStrike()) this.isSpare()
+  }
 }
 
 Frame.prototype.score = function() {
-  return this._rolls.reduce((a,b) => a + b, 0);
-}
-
-Frame.prototype.frameEndCheck = function() {
-  if(this._rolls[this._rolls.length -1] === 10 || this._rolls.length === 2) {
-    this._frameEnded = true
-  }
+  var rollScore = this._rolls.reduce((a,b) => a + b, 0)
+  var extraScore = this._extras.reduce((a,b) => a + b, 0)
+  return (rollScore + extraScore);
 }
 
 Frame.prototype.isStrike = function () {
-  if(this._rolls[this._rolls.length -1] === 10) {
-    this._frameEnded = true
-    this._extras = 2
+  if(this._rolls[this._rolls.length - 1] === 10) {
+    this._numRolls = 0
+    this._extrasRequired = 2
+    console.log("strike")
+    return true
   }
 }
 
-Frame.prototype.isEnded = function() {
-  return this._frameEnded;
+Frame.prototype.isSpare = function () {
+  if(this._rolls.reduce((a,b) => a + b, 0) === 10) {
+    this._extrasRequired = 1
+    console.log("spare")
+    return true
+  }
 }
 
-Frame.prototype.extras = function() {
-  return this._extras
+Frame.prototype.canRoll = function() {
+  return !!this._numRolls;
+}
+
+Frame.prototype.numExtras = function() {
+  return this._extrasRequired
+}
+
+Frame.prototype.inputExtra = function(pins) {
+  this._extras.push(pins)
+  this._extrasRequired--
+}
+
+Frame.prototype.closed = function() {
+  return !this._numRolls && !this._extrasRequired
 }
 
 module.exports = Frame

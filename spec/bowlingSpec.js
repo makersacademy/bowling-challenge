d4025bdeeafe48ba0488f13adba057dd333b238d
kealanheena <kealanheena@gmail.com>
2019-12-15 15:27:47 +0000

    add frames to add method

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,9 +1,22 @@
-describe("Bowling", function(){
+describe("Bowling", () => {
+  var game;
 
-  it("should add the points to total score", function(){
-    var game = new Game();
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it("should add the points to total score", () => {
     game.add(5)
 
     expect(game.points).toEqual(5);
   });
+
+  it("should switch turn after calculating the points twice", () => {
+    game.add(2);
+    game.add(5);
+
+    expect(game.frame).toEqual(2);
+  });
+
+  // it("should default all points to zero after being reset")
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,7 +1,8 @@
 function Game() {
   this.points = 0
+  this.frame = 1
 }
 Game.prototype.add = function(points) {
+  if(this.points != 0) this.frame ++
   this.points += points;
-  return this.points;
 };

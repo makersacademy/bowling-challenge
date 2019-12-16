91ecd1b36c32c3b4e59a7647ad2c51db0a237824
Jamie95187 <jamiewong63@googlemail.com>
2019-12-16 06:53:54 +0000

    refactor code logic

--- src/Bowling.js
+++ src/Bowling.js
@@ -7,13 +7,34 @@ function Bowling(){
 };
 
 Bowling.prototype.addScore = function(score){
-  this.scorecard.push(score);
+  if(this.firstThrow){
+    this.scorecard.push([score]);
+  } else {
+    this.scorecard[this.scorecard.length-1].push(score);
+  };
   this.firstThrow = !this.firstThrow;
 };
 
 Bowling.prototype.isPrevSpare = function(){
   if(this.scorecard.length > 1){
     if(this.scorecard[this.scorecard.length-2][0] + this.scorecard[this.scorecard.length-2][1] == 10){
+      return true;
+    };
+  };
+  return false;
+};
+
+Bowling.prototype.isSpare = function(){
+  if (this.sumCurrent() == 10 && this.scorecard[this.scorecard.length-1][0] != 10){
+    return true;
+  } else {
+    return false;
+  };
+};
+
+Bowling.prototype.isDoubleStrike = function(){
+  if(this.scorecard.length > 2){
+    if(this.scorecard[this.scorecard.length-3][0] == 10 && this.isPrevStrike){
       return true;
     };
   };
@@ -29,14 +50,6 @@ Bowling.prototype.isPrevStrike = function(){
   return false;
 };
 
-Bowling.prototype.isSpare = function(){
-  if (this.scorecard[this.scorecard.length-1][0] + this.scorecard[this.scorecard.length-1][1] == 10){
-    return true;
-  } else {
-    return false;
-  };
-};
-
 Bowling.prototype.isStrike = function(){
   if (this.scorecard[this.scorecard.length-1][0] == 10){
     return true;
@@ -46,13 +59,9 @@ Bowling.prototype.isStrike = function(){
 };
 
 Bowling.prototype.updateTotal = function(){
-  if(!this.isPrevSpare() && !this.isPrevStrike()){
-    if(!this.isSpare() && !this.isStrike()){
-      if(!this.firstThrow){
-        this.total += this.scorecard[this.scorecard.length-1][0] + this.scorecard[this.scorecard.length-1][1];
-      };
-    } else if(this.isSpare())
-  } else if(this.isSpare()){
-    this.total += 10 + this.scorecard[this.scorecard.length-1][0,'']
-  };
+
+};
+
+Bowling.prototype.sumCurrent = function(){
+  return this.scorecard[this.scorecard.length-1][0] + this.scorecard[this.scorecard.length-1][1];
 };

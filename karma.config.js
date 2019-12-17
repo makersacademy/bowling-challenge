d8ffa2c8f93c9e80e361c12a89e4ab307efa4649
Mezela <p_mezue@hotmail.com>
2019-12-17 13:49:31 +0000

    Tests travis

--- karma.config.js
+++ karma.config.js
@@ -1,6 +1,6 @@
-module.exports = function(config) {
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 
-  
+module.exports = function(config) {
   config.set({
     basePath: '../..',
     frameworks: ['jasmine'],

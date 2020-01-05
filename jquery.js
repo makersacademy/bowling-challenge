be49b23ef29f5b13a23c5520cc6cf097a629340c
Ellie <elliemmanifold@gmail.com>
2020-01-05 13:06:34 +0000

    Input two rolls per frame on front-end and link that to updated score (but score incorrect)

--- jquery.js
+++ jquery.js
@@ -1,4 +1,156 @@
 $(document).ready(function() {
   let bowling = new Bowling
+
   $('#score').text(`${bowling.score()}`)
-})
+
+  $('#roll1').change(function() {
+    var value1 = $(this).val();
+    console.log(value1)
+    bowling.roll(value1);
+    console.log(bowling.score())
+  });
+
+  $('#roll2').change(function() {
+    var value2 = $(this).val();
+    console.log(value2)
+    bowling.roll(value2);
+    console.log(bowling.score())
+  });
+
+  $('#roll3').change(function() {
+    var value3 = $(this).val();
+    console.log(value3)
+    bowling.roll(value3);
+    console.log(bowling.score())
+  });
+
+  $('#roll4').change(function() {
+    var value4 = $(this).val();
+    console.log(value4)
+    bowling.roll(value4);
+    console.log(bowling.score())
+  });
+
+  $('#roll5').change(function() {
+    var value5 = $(this).val();
+    console.log(value5)
+    bowling.roll(value5);
+    console.log(bowling.score())
+  });
+
+  $('#roll6').change(function() {
+    var value6 = $(this).val();
+    console.log(value6)
+    bowling.roll(value6);
+    console.log(bowling.score())
+  });
+
+  $('#roll7').change(function() {
+    var value7 = $(this).val();
+    console.log(value7)
+    bowling.roll(value7);
+    console.log(bowling.score())
+  });
+
+  $('#roll8').change(function() {
+    var value8 = $(this).val();
+    console.log(value8)
+    bowling.roll(value8);
+    console.log(bowling.score())
+  });
+
+  $('#roll9').change(function() {
+    var value9 = $(this).val();
+    console.log(value9)
+    bowling.roll(value9);
+    console.log(bowling.score())
+  });
+
+  $('#roll10').change(function() {
+    var value10 = $(this).val();
+    console.log(value10)
+    bowling.roll(value10);
+    console.log(bowling.score())
+  });
+
+  $('#roll11').change(function() {
+    var value11 = $(this).val();
+    console.log(value11)
+    bowling.roll(value11);
+    console.log(bowling.score())
+  });
+
+  $('#roll12').change(function() {
+    var value12 = $(this).val();
+    console.log(value12)
+    bowling.roll(value12);
+    console.log(bowling.score())
+  });
+
+  $('#roll13').change(function() {
+    var value13 = $(this).val();
+    console.log(value13)
+    bowling.roll(value13);
+    console.log(bowling.score())
+  });
+
+  $('#roll14').change(function() {
+    var value14 = $(this).val();
+    console.log(value14)
+    bowling.roll(value14);
+    console.log(bowling.score())
+  });
+
+  $('#roll2').change(function() {
+    var value2 = $(this).val();
+    console.log(value2)
+    bowling.roll(value2);
+    console.log(bowling.score())
+  });
+
+  $('#roll15').change(function() {
+    var value15 = $(this).val();
+    console.log(value15)
+    bowling.roll(value15);
+    console.log(bowling.score())
+  });
+
+  $('#roll16').change(function() {
+    var value16 = $(this).val();
+    console.log(value16)
+    bowling.roll(value16);
+    console.log(bowling.score())
+  });
+
+  $('#roll17').change(function() {
+    var value17 = $(this).val();
+    console.log(value17)
+    bowling.roll(value17);
+    console.log(bowling.score())
+  });
+
+  $('#roll18').change(function() {
+    var value18 = $(this).val();
+    console.log(value18)
+    bowling.roll(value18);
+    console.log(bowling.score())
+  });
+
+  $('#roll19').change(function() {
+    var value19 = $(this).val();
+    console.log(value19)
+    bowling.roll(value19);
+    console.log(bowling.score())
+  });
+
+  $('#roll20').change(function() {
+    var value20 = $(this).val();
+    console.log(value20)
+    bowling.roll(value20);
+    console.log(bowling.score())
+
+    $('#score').text(`${bowling.score()}`)
+    
+  });
+
+});

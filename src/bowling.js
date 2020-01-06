e20b359d4e067d0bc803bd3accb52218298f5ceb
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-01-06 21:49:34 +0000

    add eslint and update to appease the standard linter

--- src/bowling.js
+++ src/bowling.js
@@ -1,79 +1,79 @@
 // var Frame = require('./frame.js');
 
-function Bowling(frames = 10) {
-  this._maxFrames = frames;
-  this.frames = [new Frame()];
-  this.isEnd = false;
-  this._isLastFrame = false;
+function Bowling (frames = 10) {
+  this._maxFrames = frames
+  this.frames = [new Frame()]
+  this.isEnd = false
+  this._isLastFrame = false
 }
 
 // public functions
-Bowling.prototype.roll = function(...args) {
-  args.forEach( pins => {
-    if(this.isEnd) throw new Error("Cannot roll, the game has ended, total Points: " + this.totalScore())
+Bowling.prototype.roll = function (...args) {
+  args.forEach(pins => {
+    if (this.isEnd) throw new Error('Cannot roll, the game has ended, total Points: ' + this.totalScore())
 
-    if(this._currentFrame().canRoll()) {
+    if (this._currentFrame().canRoll()) {
       this._addRoll(pins)
     } else if (this._isLastFrame && !this._currentFrame().closed()) {
       this._lastFrame(pins)
     }
-  });
+  })
 }
 
-Bowling.prototype.totalScore = function(max = this.frames.length) {
-  return this.frames.slice(0, max).reduce((a, b) => a + b.score(), 0);
+Bowling.prototype.totalScore = function (max = this.frames.length) {
+  return this.frames.slice(0, max).reduce((a, b) => a + b.score(), 0)
 }
 
-Bowling.prototype.frameNum = function() {
-  return (this.frames.length);
+Bowling.prototype.frameNum = function () {
+  return (this.frames.length)
 }
 
 // private functions
 
-Bowling.prototype._addRoll = function(pins) {
+Bowling.prototype._addRoll = function (pins) {
   // console.log("here")
-  this._currentFrame().inputRoll(pins);
-  if(this.frames.length > 1) this._extraPoints(pins);
-  this._checkEnd();
-  if(!this._currentFrame().canRoll() && !this._isLastFrame) this._newFrame();
+  this._currentFrame().inputRoll(pins)
+  if (this.frames.length > 1) this._extraPoints(pins)
+  this._checkEnd()
+  if (!this._currentFrame().canRoll() && !this._isLastFrame) this._newFrame()
 }
 
-Bowling.prototype._newFrame = function() {
-  this.frames.push(new Frame());
+Bowling.prototype._newFrame = function () {
+  this.frames.push(new Frame())
 }
 
-Bowling.prototype._currentFrame = function() {
-  return this.frames[this.frames.length - 1];
+Bowling.prototype._currentFrame = function () {
+  return this.frames[this.frames.length - 1]
 }
 
-Bowling.prototype._oneFrameBack = function() {
-  return this.frames[this.frameNum() - 2];
+Bowling.prototype._oneFrameBack = function () {
+  return this.frames[this.frameNum() - 2]
 }
 
-Bowling.prototype._twoFramesBack = function() {
-  return this.frames[this.frameNum() - 3];
+Bowling.prototype._twoFramesBack = function () {
+  return this.frames[this.frameNum() - 3]
 }
 
 Bowling.prototype._extraPoints = function (pins) {
-  if(this.frameNum() > 2) {
-    this._twoFramesBack().inputExtra(pins);
-    this._oneFrameBack().inputExtra(pins);
-  } else if(this._oneFrameBack().numExtras() > 0) {
-    this._oneFrameBack().inputExtra(pins);
+  if (this.frameNum() > 2) {
+    this._twoFramesBack().inputExtra(pins)
+    this._oneFrameBack().inputExtra(pins)
+  } else if (this._oneFrameBack().numExtras() > 0) {
+    this._oneFrameBack().inputExtra(pins)
   }
 }
 
-Bowling.prototype._lastFrame = function(pins) {
-  this._oneFrameBack().inputExtra(pins);
-  this._currentFrame().inputExtra(pins);
+Bowling.prototype._lastFrame = function (pins) {
+  this._oneFrameBack().inputExtra(pins)
+  this._currentFrame().inputExtra(pins)
   this._checkEnd()
 }
 
-Bowling.prototype._checkEnd = function() {
-  if(this.frameNum() === this._maxFrames) this._isLastFrame = true
-  if(this._isLastFrame && this._currentFrame().closed()) {
-    this.isEnd = true;
-  } 
+Bowling.prototype._checkEnd = function () {
+  if (this.frameNum() === this._maxFrames) this._isLastFrame = true
+  if (this._isLastFrame && this._currentFrame().closed()) {
+    this.isEnd = true
+  }
 }
 
 // module.exports = Bowling

f8b2e6101275f35df2398d0a1119d989b4b65e58
Josh Davies <jdaviescreative@gmail.com>
2020-01-23 13:21:16 +0000

    added chart annimation and ability to update values - still currently using a static example

--- src/chart.js
+++ src/chart.js
@@ -1,12 +1,14 @@
-var myChart = document.getElementById('myChart').getContext('2d');
+var updatedData = [1, 2, 3, 4, 5];
+var frameScoreChart = document.getElementById('frameScoreChart').getContext('2d');
 Chart.defaults.global.defaultFontSize=20;
-var barChart = new Chart(myChart, {
+
+var barChart = new Chart(frameScoreChart, {
   type:'bar',
   data:{
     labels:['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10'],
     datasets:[{
       label:'Frame Score',
-      data:[4, 3, 6, 1, 2, 3, 4, 5, 6],
+      data:[],
       backgroundColor: [
             'rgba(54, 162, 235, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -19,27 +21,31 @@ var barChart = new Chart(myChart, {
             'rgba(54, 162, 235, 0.2)',
             'rgba(54, 162, 235, 0.2)'
       ],
-      borderWidth:1,
-      borderColor:'#777',
-      hoverBorderWidth:3,
-      hoverBorderColor:'#000'
+      borderWidth: 1,
+      borderColor: '#777',
+      hoverBorderWidth: 3,
+      hoverBorderColor: '#000'
     }]
   },
   options: {
     legend:{
       display: false
     },
-    title: {
-        display: true,
-        text: 'Frame Scores',
-        fontSize:28
-    },
     scales: {
-        yAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    },
+    animation: {
+      duration: 2000,
+      easing: 'easeInOutElastic'
     }
   }
 });
+
+function updateChart() {
+    barChart.data.datasets[0].data = updatedData
+    barChart.update();
+};

b17b5b5e5dd9f4ec38a4da46cfa61dc97f271908
nicolasraffray <nraffray@gmail.com>
2020-01-25 17:58:24 +0000

    players can put in how many pins they knocked over

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -3,12 +3,19 @@
 describe('Player', function(){
   var player 
 
+  beforeEach(function(){
+    player = new Player 
+  })
+
   describe('Score',function(){
-    beforeEach(function(){
-      player = new Player 
-    })
     it('Has a starting score of zero', function(){
       expect(player.currentScore()).toEqual(0)
+    })
+  })
+
+  describe('bowledOver', function(){
+    it('Lets the player put in how many pins they knocked over',function(){
+      expect(player.bowledOver(5)).toEqual(5)
     })
   })
 })
--- src/player.js
+++ src/player.js
@@ -5,3 +5,7 @@ function Player(){}
 Player.prototype.currentScore = function(){
   return 0
 }
+
+Player.prototype.bowledOver = function(downedPins){
+  return downedPins 
+}

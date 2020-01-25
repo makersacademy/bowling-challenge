561a15b465e9591263d2f1aedc94e287730f3973
nicolasraffray <nraffray@gmail.com>
2020-01-25 17:44:12 +0000

    fist user story: adding a player to the game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -2,13 +2,23 @@
 
 describe('The Game', function(){
   var game 
+  var player
+
+  beforeEach(function(){
+    game = new Game
+    player = new Player
+  })
 
   describe('A game contains players',function(){
-    beforeEach(function(){
-      game = new Game
+    it('expects a player to not be in the game at start',function(){
+      expect(game.players()).toEqual([])
     })
-    it('expects a player to be in the game',function(){
-      expect(game.players).toEqual([])
+  })
+
+  describe('addPlayer', function(){
+    it('adds a player to the array', function(){
+      game.addPlayer(player)
+      expect(game.players()).toContain(player)
     })
   })
 })

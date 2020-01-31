0b2d0305fe853c5f88abf7ceeabd169e10f22de7
nboyse <natasha.boyse@trade.gov.uk>
2020-01-31 16:26:23 +0000

    Create bowlingBallSpec.js

---
+++ spec/bowlingBallSpec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe("Bowling Ball", function(){
+  var bowlingBall;
+  var lanes;
+  beforeEach(function() {
+    bowlingBall = new BowlingBall();
+    lanes = jasmine.createSpyObj('lanes')
+  });
+
+});

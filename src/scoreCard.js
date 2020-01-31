1dda6fe297ded9d597ec94dfddb0b1bb58437e58
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-01-31 15:20:39 +0000

    testing calculation to return total game score

--- src/scoreCard.js
+++ src/scoreCard.js
@@ -2,5 +2,24 @@ class scoreCard {
   constructor() {
     this.score = [];
     this.frame = [];
+    this.MAX_FRAMES = 10;
+  }
+
+  points(roll1, roll2) {
+    if (this.isMaxFrames()) {
+      return;
+    }
+    this.frame.push(roll1, roll2);
+    this.score.push(this.frame);
+    this.empty();
+  }
+
+
+  empty() {
+    this.frame = [];
+  }
+
+  isMaxFrames() {
+    return this.score.length === this.MAX_FRAMES;
   }
 }

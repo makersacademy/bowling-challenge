9f707a67bbad5e6d337be83e65e1c09b7c73e4f4
Imraan Ali <imraan91@hotmail.co.uk>
2020-02-01 18:43:45 +0000

    can roll a srtike, perffect game and spare

--- jasmine-standalone-3.5.0/spec/bowlingChallengeSpec.js
+++ jasmine-standalone-3.5.0/spec/bowlingChallengeSpec.js
@@ -1,8 +1,13 @@
 describe('Bowling game logic', function() {
 
+  var game;
+
+  beforeEach(function (){
+    game = new BowlingGame();
+  });
+
 
   it('can roll a gutter game', function(){
-    var game = new BowlingGame();
     for (var i = 0; i < 20; i++) {
       game.roll(0);
     }
@@ -10,10 +15,39 @@ describe('Bowling game logic', function() {
   });
 
   it('can roll all ones', function(){
-    var game = new BowlingGame();
     for (var i = 0; i < 20; i++) {
       game.roll(1);
     }
     expect(game.score()).toEqual(20);
-  })
+  });
+
+  it('can roll a spare', function (){
+    game.roll(5);
+    game.roll(5);
+    game.roll(2);
+    for (var i = 0; i < 17; i++) {
+      game.roll(0);
+    }
+    expect(game.score()).toEqual(14);
+  });
+
+  it('can roll a strike', function (){
+    game.roll(10);
+    game.roll(2);
+    game.roll(5);
+    for (var i = 0; i < 16; i++) {
+      game.roll(0)
+    }
+    expect(game.score()).toEqual(24)
+  });
+
+  it('can roll a perfect game', function (){
+
+    for (var i = 0; i < 12; i++) {
+      game.roll(10)
+    }
+    expect(game.score()).toEqual(300)
+  });
+
+
 });
--- jasmine-standalone-3.5.0/src/bowlingChallenge.js
+++ jasmine-standalone-3.5.0/src/bowlingChallenge.js
@@ -1,19 +1,50 @@
 var BowlingGame = function () {
   this.rolls = [];
 
+
 BowlingGame.prototype.roll = function(pins) {
   this.rolls.push(pins);
 };
 
-BowlingGame.prototype.score = function() {
-  result = 0
-  rollIndex = 0
-  for (var rollIndex = 0; rollIndex < 20; rollIndex++) {
-    result += this.rolls[rollIndex];
+BowlingGame.prototype.score = function () {
+   result = 0;
+   rollIndex = 0;
+   var game = this;
 
-  }
-  return result
+  for (var frameIndex = 0; frameIndex < 10; frameIndex++) {
+
+    if (isStrike()){
+     result += strikeAndSpareScore();
+     rollIndex += 1;
+   } else if (isSpare()) {
+      result += strikeAndSpareScore();
+      rollIndex += 2;
+    } else {
+      result += normalScore();
+       rollIndex += 2;
+    };
+
+  };
+
+
+
+return result;
+
+function isStrike(){
+  return (game.rolls[rollIndex] == 10);
+}
+
+function isSpare(){
+  return (game.rolls[rollIndex] + game.rolls[rollIndex +1] == 10);
+}
+
+function strikeAndSpareScore(){
+  return (game.rolls[rollIndex] + game.rolls[rollIndex +1] + game.rolls[rollIndex +2]);
+}
+
+function normalScore(){
+  return (game.rolls[rollIndex] + game.rolls[rollIndex +1]);
+}
+
 };
-
-
 };

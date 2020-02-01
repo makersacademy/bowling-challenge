d5080c4646b383ea0bb69f10d3076201b903a971
nicolasraffray <nraffray@gmail.com>
2020-02-01 18:02:14 +0000

    knows when there is a strike or spare and can calculate score of frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -20,6 +20,26 @@ describe('Frame', function(){
     })
   })
 
-  
+  describe('check_strike', function(){
+    it('returns true if a player has rolled a strike', function(){
+      frame.addroll(10)
+      expect(frame.check_strike()).toEqual(true)
+    })
+  })
 
+  describe('check_spare',function(){
+    it('returns true if a spare is rolled', function(){
+      frame.addroll(8)
+      frame.addroll(2)
+      expect(frame.check_spare()).toEqual(true)
+    })
+  })
+
+  describe('score_frame', function(){
+    it('sums the elements of the frame', function(){
+      frame.addroll(4)
+      frame.addroll(3)
+      expect(frame.score_frame()).toEqual(7)
+    })
+  })
 })
--- src/frame.js
+++ src/frame.js
@@ -7,3 +7,18 @@ function Frame(){
 Frame.prototype.addroll = function(roll){
   this._throws.push(roll)
 }
+
+Frame.prototype.check_strike = function(){
+  return this._throws[0] === 10
+}
+
+Frame.prototype.check_spare = function(){
+  return (this._throws[0] + this._throws[1]) === 10
+}
+
+Frame.prototype.score_frame = function(){
+  return this._throws.reduce(function(x,y){
+    return x + y
+  })
+}
+

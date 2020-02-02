90ae36640147950fd739795d1a723975cfd10533
Student <katierodgers95@gmail.com>
2020-02-02 17:47:10 +0000

    returns correct score for a spare and refactored

--- jasmine-standalone-3.5.0/spec/bowlingChallengeSpec.js
+++ jasmine-standalone-3.5.0/spec/bowlingChallengeSpec.js
@@ -15,6 +15,15 @@ describe('bowlingRules',function(){
     expect(game.score).toEqual(20)
   });
 
+  it('should return the correct score when a spare is rolled',function () {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    rollMany(0, 17);
+
+    expect(game.score).toEqual(16);
+  });
+
   function rollMany(pins, rolls) {
     for (let loopcounter = 0; loopcounter < rolls; loopcounter++) {
       game.roll(pins);
--- jasmine-standalone-3.5.0/src/bowlingChallenge.js
+++ jasmine-standalone-3.5.0/src/bowlingChallenge.js
@@ -1,14 +1,37 @@
 class BowlingGame {
   constructor() {
-    this.runningScore = 0;
+    this.rolls = [];
   }
 
   roll(pins) {
-    this.runningScore += pins;
+    this.rolls.push(pins);
   }
 
   get score() {
-    return this.runningScore;
+    let score = 0;
+    let rollIndex = 0;
+
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+      const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+
+      if (this.isSpare(frameScore)) {
+          score += this.spareBonus(rollIndex);
+      } else {
+          score += frameScore;
+      }
+
+        rollIndex += 2;
+    }
+
+        return score;
+  }
+
+  isSpare(frameScore) {
+    return frameScore === 10;
+  }
+
+  spareBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex + 2];
   }
 }
-
+  

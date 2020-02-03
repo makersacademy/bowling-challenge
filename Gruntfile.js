7427b1aac46e3dbefb06c6813c267feee6f627c9
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-03 00:13:23 +0000

    update travis.yml

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-    grunt.initConfig({ jasmine: { src: 'src/*.js', options: { specs: 'spec/*.js' } } });
+    grunt.initConfig({ jasmine: { src: 'src/**/*.js', options: { specs: 'spec/**/*.js' } } });
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.registerTask('default', ['jasmine']);
 };

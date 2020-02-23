c01d5157b1181b9d159b35157d0823e953ee539c
Peter <pc.dean@icloud.com>
2020-02-23 22:06:49 +0000

    refactor findScore to abstract logic

--- src/score.js
+++ src/score.js
@@ -2,14 +2,27 @@ function Score() {
   this.total = 0;
 };
 
-Score.prototype.add = function(results) {
-  numFrames = results.length;
-  ans = 0;
-  for(i = 0; i < numFrames; i++) {
-    frameLength = results[i].length;
-    for(n = 0; n < frameLength; n++) {
-      ans += results[i][n]
-    }
-  }
-  this.total = ans;
+Score.prototype.findScore = function(results) {
+  this.total = results.reduce(addArray, 0);
 }
+
+function add(total, num) {
+  return total + num;
+}
+
+function addArray(total, nextElement) {
+  return total + nextElement.reduce(add);
+}
+
+// Score.prototype.findScore = function(results) {
+//   numFrames = results.length;
+//   ans = 0;
+//   for(i = 0; i < numFrames; i++) {
+//     if (results[i] === 10 && results[i].length) {
+//       if (results[i+1].length === 2) {
+//         ans +=
+//       }
+//     }
+//   }
+//
+// }

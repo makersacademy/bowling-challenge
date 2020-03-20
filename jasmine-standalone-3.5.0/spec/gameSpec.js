8439ed1357d6d91f78bc0885f3087cc8c1ce6243
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-03-20 12:00:28 +0000

    wrote tests for game of only ones

--- jasmine-standalone-3.5.0/spec/gameSpec.js
+++ jasmine-standalone-3.5.0/spec/gameSpec.js
@@ -4,11 +4,16 @@ describe('bowling', function() {
   
   beforeEach(function() {
     game = new bowlingGame()
-  })
+  });
 
   it('can roll a game of zeros', function() {
-    for(let i = 0; i < 20; i++) {game.roll(0)}
-    expect(game.score()).toBe(0)
-  })
+    for(let i = 0; i < 20; i++) {game.roll(0)};
+    expect(game.score()).toBe(0);
+  });
+
+  it('can roll a game of ones', function() {
+    for(let i = 0; i < 20; i++) {game.roll(1)};
+    expect(game.score()).toBe(20);
+  });
 
 })

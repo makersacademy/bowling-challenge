910c80803a9d907178cb6df7839db29990fe1cfd
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-03-20 12:22:18 +0000

    Added ability to roll a spare

--- jasmine-standalone-3.5.0/spec/gameSpec.js
+++ jasmine-standalone-3.5.0/spec/gameSpec.js
@@ -16,4 +16,11 @@ describe('bowling', function() {
     expect(game.score()).toBe(20);
   });
 
+  it('can roll a spare', function() {
+    game.roll(7)
+    game.roll(3)
+    game.roll(5)
+    for(let i = 0; i < 17; i++) {game.roll(0)};
+    expect(game.score()).toBe(20);
+  })
 })
--- jasmine-standalone-3.5.0/src/game.js
+++ jasmine-standalone-3.5.0/src/game.js
@@ -8,7 +8,17 @@ bowlingGame.prototype.roll = function(num) {
 }
 
 bowlingGame.prototype.score = function() {
-  let score = 0
-  for(let roll = 0; roll < 20; roll++) { score += this.rolls[roll]}
+  let score = 0;
+  let rollIndex = 0;
+
+  for(let frameIndex = 0; frameIndex < 10; frameIndex++) {
+    if (this.rolls[rollIndex] + this.rolls[rollIndex + 1] == 10) {
+      score += this.rolls[rollIndex] + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    } else {
+      score += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+    rollIndex += 2
+  }
+  // for(let roll = 0; roll < 20; roll++) { score += this.rolls[roll]}
   return score
 }

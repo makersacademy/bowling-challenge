a3a57835a7c39fee7607753fec038940858d9401
Sophia Bell <Kittaru87@gmail.com>
2020-03-22 13:09:20 +0000

    adding in test and code for spare

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,5 +1,3 @@
-// var Game = require('../src/Game');
-
 describe ('Game', function(){
 
   var game;
@@ -8,13 +6,12 @@ describe ('Game', function(){
     game = new Game();
   });
 
-  it ('has a zero score by default', function() {
-    rollMany(0, 0)
-    expect(game.currentScore()).toEqual(0);
-  });
+  // it ('has a zero score by default', function() {
+  //   rollMany(0, 0)
+  //   expect(game.currentScore()).toEqual(0);
+  // });
 
-
-  it ('tests for guttergame (0 score from 20 games)', function(){
+  it ('tests for guttergame (0 score from 20 rolls)', function(){
     rollMany(20, 0)
     expect(game.currentScore()).toEqual(0);
   });
@@ -24,6 +21,13 @@ describe ('Game', function(){
     expect(game.currentScore()).toEqual(80);
   });
 
+  it ('tests for a spare', function (){
+    game.roll(5);
+    game.roll(5);
+    game.roll(2);
+    rollMany(17, 0);
+    expect(game.currentScore()).toEqual(14)
+  })
 
 
   function rollMany(x, pins) {
--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,27 @@
 
 
 function Game(){
-  this._rolls = [];
-  this.score = 0;
-  this.ball = 0;
+  this.rolls = [];
+  this.currentBall = 0;
 }
 
 Game.prototype.currentScore = function (){
-  return this.score;
-}
+  var score = 0;
+  var i = 0
+  for (var frame = 0; frame < 10; frame++) {
+    if (this.rolls[i] + this.rolls[i+1] === 10) {
+      score += 10 + this.rolls[i+2];
+      i += 2;
+    } else {
+      score += this.rolls[i] + this.rolls[i+1];
+      i += 2;
+    }
+  }
+  return score;
+};
 
 Game.prototype.roll = function(pins){
-  this.score += pins;
+  this.rolls[this.currentBall++] = pins;
 };
 
 

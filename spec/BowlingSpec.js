1bad7f0151a94bb0e23eff99fb5f10371cac0cf8
plkujaw <jawor.jakub@gmail.com>
2020-03-22 14:26:27 +0000

    test to start the game with 0 points

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,1 +1,20 @@
 'use strict'
+
+describe('Bowling Scoreboard', function () {
+  var scoreboard;
+  var round;
+  var roll1;
+  var roll2;
+
+  beforeEach(function () {
+    scoreboard = new Scoreboard ();
+  })
+
+  it('Starts with 0 points', function () {
+    expect(scoreboard.totalScore()).toEqual(0);
+  })
+
+
+
+
+})
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,1 +1,18 @@
 'use strict'
+
+class Scoreboard {
+
+  constructor(round, roll1, roll2) {
+    this.INITIAL_SCORE = 0;
+    this.score = this.INITIAL_SCORE;
+    this.round = round;
+    this.roll1 = roll1; // pins from 1st roll
+    this.roll2 = roll2; // pins from 2nd roll
+
+  }
+
+  totalScore() {
+    return this.score;
+  }
+
+}

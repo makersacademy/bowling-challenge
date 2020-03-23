894a95fa63ea7e5771cd49bf59df8016fc99739d
James Oddy <jamesoddy8@gmail.com>
2020-03-23 08:28:35 +0000

    added test for scores

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -6,17 +6,37 @@ describe("Game", function() {
     game = new Game();
   });
 
-    it("Gutter game", function() {
-      rollMany(20, 0)
-    })
+  it("Gutter game", function() {
+    rollMany(20, 0)
+    expect(game.score()).toBe(0);
+  });
 
-    it("Strike")
-  // it("Perfect game", function() {
-  //   rollMany(12, 10);
-  //   expect(game.score()).toBe(300);
-  // });
+  it("All ones", function() {
+    rollMany(20, 1);
+    expect(game.score()).toBe(20);
+  });
 
-  function rollMany() {
+  it("One spare", function() {
+    rollSpare();
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score()).toBe(16);
+  });
+
+  it("One strike", function() {
+    rollStrike();
+    game.roll(3);
+    game.roll(4);
+    rollMany(16, 0);
+    expect(game.score()).toBe(24);
+  });
+
+  it("Perfect game", function() {
+    rollMany(12, 10);
+    expect(game.score()).toBe(300);
+  });
+
+  function rollMany(rolls, pins) {
     for(var i = 0; i < rolls; i++){
       game.roll(pins);
     }
--- src/game.js
+++ src/game.js
@@ -1,11 +1,37 @@
 'use strict';
 
 function Game() {
-  this.rolls[]
+  this.rolls = []
   this.currentRoll = 0;
 
-}
+};
 
 Game.prototype.roll = function (pins) {
   this.rolls[this.currentRoll++] = pins;
+};
+
+Game.prototype.score = function () {
+  var score = 0;
+  var frameIndex = 0;
+  for(var frame = 0; frame < 10; frame++){
+    if(this.isStrike(frameIndex)){
+      score += 10 + this.strikeBonus(frameIndex);
+      frameIndex ++;
+    } else if(this.isSpare(frameIndex)){
+      score += 10 + this.spareBonus(frameIndex);
+      frameIndex += 2
+    } else {
+      score += this.sumOfBallsInFrame(frameIndex);
+      frameIndex += 2;
+    }
+  }
+  return score;
+};
+
+Game.prototype.isStrike = function (frameIndex) {
+  return this.rolls[frameIndex] === 10;
+};
+
+Game.prototype.isSpare = function () {
+  return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
 };

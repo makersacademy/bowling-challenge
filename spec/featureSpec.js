5381d9ef7e8a46d839f2f6794e331c45d7e961c4
LXie-Saintex <admin@saintex.co.uk>
2020-03-28 11:38:21 +0000

    added end-of-game bonus to model

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -12,11 +12,27 @@ describe('Scorecard', function(){
 	it('can add frame', function(){
 		scorecard.add(frame);
 		expect(scorecard.frames()).toEqual([frame]);
-	})
+	});
 	it('can calculate total score',function(){
 		frame.add(4);
 		frame.bonus(0);
 		scorecard.add(frame);
 		expect(scorecard.total()).toEqual(9);
-	})
-})
+	});
+	it("can end game", function(){
+		scorecard.add(frame);
+		scorecard.endGame();
+		expect(scorecard._gameOver).toBe(true);
+		expect(function(){ scorecard.add(new Frame()); }).toThrowError('Game Over');
+	});
+	describe('Bonus', function(){
+		it("can add bonus after the end of the game", function(){
+			scorecard.endGame();
+			scorecard.bonus(10);
+			expect(scorecard._sum).toEqual(10);
+		});
+		it('prevents adding bonus during the game', function(){
+			expect(function(){ scorecard.bonus(10); }).toThrowError('Game ongoing, bonus error');
+		})
+	});
+});
--- src/scorecard.js
+++ src/scorecard.js
@@ -2,11 +2,13 @@ class Scorecard {
 	constructor(){
 		this._frames = [];
 		this._sum = 0;
+		this._gameOver = false;
 	}
 	frames(){
 		return this._frames;
 	}
 	add(frame) {
+		if(this._gameOver){ throw new Error("Game Over"); }
 		this._frames.push(frame);
 		return this._frames;
 	}
@@ -19,5 +21,14 @@ class Scorecard {
 			this._frames.forEach(frame => this._sum += frame.total()); 
 		}
 		return this._sum; 
+	}
+	endGame() {
+		this._gameOver = true;
+	}
+
+	bonus(bonus){
+		if(!this._gameOver){ throw new Error('Game ongoing, bonus error'); }
+		this._sum += bonus;
+		return this._sum;
 	}
 }

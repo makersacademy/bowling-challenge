cc39833e2c8ff2b95621b19e8f95b9da3b4fda8a
Karla <karla.gardiner@gmail.com>
2020-03-28 13:18:18 +0000

    basic algorithm for scoreboard written in javascript

---
+++ public/js/ScoreBoard.js
@@ -0,0 +1,23 @@
+function ScoreBoard() {
+
+    this.frames = [];
+    this.score = [];
+    this.totalScore = 0
+
+  ScoreBoard.prototype.newBoard = function() {
+    for (i=0; i<12; i++) {
+      this.frames.push(new Frame);
+      if (i > 0) { this.frames[i - 1].next = this.frames[i]; }
+    };
+  };
+
+  ScoreBoard.prototype.update = function() {
+    runningTotal = 0;
+    this.score = [];
+    for (i=0; i<10; i++) {
+      runningTotal += this.frames[i].score();
+      this.score.push(runningTotal);
+    }
+  };
+
+};

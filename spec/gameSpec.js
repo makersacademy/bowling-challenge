d5ba17ca1db9db9d172cb9e474a5ddf60c8a9c5f
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-29 12:03:55 +0100

    Added test for rolling only 1s - passed)

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -14,4 +14,11 @@ describe('Game', () => {
     expect(game.score()).toBe(0);
   })
 
+  it ('can roll all ones', () => {
+    for (let i = 0; i < 20; i ++) {
+      game.roll(1);
+    }
+    expect(game.score()).toBe(20);
+  })
+
 })
--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,19 @@
 class Game {
 
   constructor() {
-
+    this._rolls = [];
   }
 
   roll(pins) {
-
+    this._rolls.push(pins);
   }
-  
+
   score() {
-    return 0;
+    let result = 0;
+    for (let i = 0; i < 20; i ++) {
+      result += this._rolls[i];
+    }
+    return result;
   }
 
 }

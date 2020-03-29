90e4666f08b63b173827096c555cc5b6543fd7a2
Karla <karla.gardiner@gmail.com>
2020-03-29 17:25:12 +0100

    ends game by removing pin buttons and displays a game over message

--- public/js/interface.js
+++ public/js/interface.js
@@ -4,8 +4,6 @@ $( document ).ready(function() {
   let game;
   let scoreboard;
   let score;
-  // let frame = 1;
-  // let roll = 1;
 
   $( '.scoreboard').hide();
   $( '.btn-startGame' ).hide();
@@ -31,19 +29,24 @@ $( document ).ready(function() {
     $( '.msg-playersTurn' ).html("It's " + game.currentPlayer + "'s turn!")
     $( '.msg-rollNum' ).html("Roll " + game.roll);
     updatePinButtons();
-    // $( '.pinButtons' ).append("button")
   });
 
   $( '.pinButtons' ).on('click', '.Pins', function() {
     updateScoreboard(parseInt($(this).val()));
-    // updateRoll();
+    // debugger;
+    if (!game.inPlay) {
+      $( '.msg-playersTurn' ).hide();
+      $( '.msg-rollNum' ).hide();
+      $( '.pinButtons' ).hide();
+      $( '.msg-gameOver' ).html("Game Over!")
+    }
     game.turn();
     updatePinButtons();
-    $( '.msg-rollNum' ).html("Roll " + game.roll);
+    $( '.msg-rollNum' ).html(messageRollNumber());
   });
 
   function updatePinButtons() {
-    pinsLeft = 10 - scoreboard.frames[game.frame].roll_1;
+    pinsLeft = 10 - scoreboard.frames[game.frame -1].roll_1;
     $( '.pinButtons' ).empty();
     for (i = 0; i <= pinsLeft; i++) {
       $( '.pinButtons' ).append("<button class='Pins' value=" + i + " >" + ((i == 0) ? 'Gutter' : i) + "</button>")
@@ -51,59 +54,66 @@ $( document ).ready(function() {
   }
 
   function updateScoreboard(pins) {
-    scoreboard.frames[game.frame].roll(game.roll, pins);
+    scoreboard.frames[game.frame-1].roll(game.roll, pins);
     scoreboard.update();
     if (game.frame <= 10) {
       updateFrames1to10Scoreboard();
     } else {
       updateBonusRollscoreboard();
     };
-    for (i = 1; i <= game.frame ; i++) {
-      $( '.frame' + i ).find('.Total').html(scoreboard.score[i]);
+    for (i = 1; i <= Math.min(game.frame,10) ; i++) {
+      $( '.frame' + i ).find('.Total').html(scoreboard.score[i-1]);
     };
   };
 
   function updateFrames1to10Scoreboard(pins) {
     if (game.roll == 1){
-      if (scoreboard.frames[game.frame].strike) {
+      if (scoreboard.frames[game.frame-1].strike) {
         score = 'X'
         if (game.frame < 10) { $( '.frame' + game.frame ).find('.Roll2').html('-'); }
         game.turn();
-      } else { score = scoreboard.frames[game.frame].roll_1 };
+      } else { score = scoreboard.frames[game.frame-1].roll_1 };
       $( '.frame' + game.frame ).find('.Roll1').html(score);
     } else {
-      (scoreboard.frames[game.frame].spare) ? score = '/' : score = scoreboard.frames[game.frame].roll_2
+      (scoreboard.frames[game.frame-1].spare) ? score = '/' : score = scoreboard.frames[game.frame-1].roll_2
       $( '.frame' + game.frame ).find('.Roll2').html(score);
+      // debugger;
+      if (game.frame == 10 && !scoreboard.frames[game.frame-1].spare) { game.inPlay = false };
     };
   };
 
   function updateBonusRollscoreboard() {
-    // debugger;
-    (scoreboard.frames[game.frame].roll_1 == 10) ? score = 'X' : score = scoreboard.frames[game.frame].roll_1;
+    (scoreboard.frames[game.frame-1].roll_1 == 10) ? score = 'X' : score = scoreboard.frames[game.frame-1].roll_1;
     if (game.frame == 11) {
-      if (scoreboard.frames[10].spare) {
+      if (scoreboard.frames[9].spare) {
+        // debugger;
         $( '.frame10' ).find('.Roll3').html(score);
+        game.inPlay = false;
       };
-      if (scoreboard.frames[10].strike) {
+      if (scoreboard.frames[9].strike) {
         $( '.frame10' ).find('.Roll2').html(score);
         game.turn();
       };
     };
     if (game.frame == 12) {
+      // debugger;
       $( '.frame10' ).find('.Roll3').html(score);
+      game.inPlay = false;
     };
-      // if frame 10 is spare: roll_1 added to Roll3 in scoreboard and game over
-      // if frame 10 is strike:
-      // roll_1 not strike : update roll_1 in Roll2 in scoreboard and roll_2 to Roll3 in scoreboard and game over
-      // roll_1 == strike : update roll_1 in Roll2 in scoreboard and move to frame 12
   };
 
-
-    // game.frame == 12
-      // if frame 10 == strike && frame 11 == strike: update roll_1 in Roll3 in scoreboard and game over
-
+  function messageRollNumber() {
+    if (game.frame <= 10) {
+     return "Roll " + game.roll
+   };
+    if (game.frame == 11 && scoreboard.frames[9].spare) {
+      return "Roll 3"
+    };
+    if (game.frame == 11 && scoreboard.frames[9].strike) {
+      return "Roll 2"
+    };
+    if (game.frame == 12) {
+      return "Roll 3"
+    };
+  }
 });
-
-// roll_1 not strike && roll_2 not spare : update normally and game over
-// roll_1 not strike && roll_2 == spare : update normally and move to frame 11
-// roll_1 == strike : update and move to frame 11

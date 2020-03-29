7f520e8e4cb70d450769847e2d210c12b1d3af58
naeglinghaff <kate.o.haffenden@gmail.com>
2020-03-29 12:20:46 +0100

    Refactoring score function to accuratley reflect spare bonus scoring and frames

--- src/Game.js
+++ src/Game.js
@@ -10,12 +10,15 @@ class Game {
 
   score() {
     let result = 0;
-    for (let i = 0; i < 20; i ++) {
-      result += this._rolls[i];
+    let rollindex = 0;
+    for (let frame = 0; frame < 10; frame ++) {
+      result += this._rolls[rollindex] + this._rolls[rollindex + 1];
+      //index increases by 2 as there are 2 rolls per frame
+      rollindex += 2;
     }
     return result;
   }
 
-  
+
 
 }

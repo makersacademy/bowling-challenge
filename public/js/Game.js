4d7cf3e3ca4a8295472b1cbf019db85638a1b8fa
Karla <karla.gardiner@gmail.com>
2020-03-29 13:30:41 +0100

    created Game.js to store and update the game frame, currentPlayer and roll with a turn() to update this

---
+++ public/js/Game.js
@@ -0,0 +1,40 @@
+function Game() {
+
+  this.players = [];
+  this.frame = 1;
+  this.playerIdx = 0;
+  this.currentPlayer = this.players[this.playerIdx];
+  this.roll = 1;
+};
+
+Game.prototype.updatePlayersList = function (player) {
+  this.players.push(player);
+};
+
+Game.prototype.turn = function () {
+  this._updateRoll();
+  this._updatePlayer();
+  this._updateFrame();
+  this.updateCurrentPlayer();
+};
+
+Game.prototype.updateCurrentPlayer = function () {
+  this.currentPlayer = this.players[this.playerIdx];
+};
+
+Game.prototype._updateRoll = function () {
+  this.roll = this.roll % 2 + 1;
+};
+
+Game.prototype._updatePlayer = function () {
+  if (this.roll == 1) {
+    this.playerIdx = (this.playerIdx + 1) % this.players.length;
+    // this.currentPlayer = this.players[(this.playerIdx + 1) % this.players.length];
+  };
+};
+
+Game.prototype._updateFrame = function () {
+  if (this.roll == 1 && this.playerIdx == 0) {
+    this.frame += 1;
+  };
+};
--- public/js/interface.js
+++ public/js/interface.js
@@ -1,59 +1,58 @@
 $( document ).ready(function() {
 
   // will probably make a game object that stores these values
+  let game;
   let scoreboard;
   let score;
-  let frame = 1;
-  let roll = 1;
+  // let frame = 1;
+  // let roll = 1;
 
   $( '.scoreboard').hide();
   $( '.btn-startGame' ).hide();
   $( '.playing').hide();
 
   $( '.btn-newGame' ).on('click', function() {
+    game = new Game;
+    scoreboard = new ScoreBoard;
+    scoreboard.newBoard();
     $( '.scoreboard').show();
     $( '.btn-startGame' ).show();
     $( '.playing').hide();
-
-    scoreboard = new ScoreBoard;
-    scoreboard.newBoard();
-
     $( '.playerName' ).html("<input type='text' class='playerNameForm' placeholder='Enter name'>")
   });
 
   $( '.btn-startGame' ).on('click', function() {
     $( '.btn-startGame' ).hide();
     $( '.playing').show();
-
-    scoreboard.playerName = $( '.playerNameForm' ).val();
-    $( '.playerName' ).html(scoreboard.playerName);
-    $( '.msg-playersTurn' ).html("It's " + scoreboard.playerName + "'s turn!")
-    $( '.msg-rollNum' ).html("Roll " + roll);
+    // this needs to loop through html form
+    game.updatePlayersList($( '.playerNameForm' ).val());
+    $( '.playerName' ).html(game.players[0]);
+    game.updateCurrentPlayer();
+    $( '.msg-playersTurn' ).html("It's " + game.currentPlayer + "'s turn!")
+    $( '.msg-rollNum' ).html("Roll " + game.roll);
   });
 
   $( '.Pins' ).on('click', function() {
     updateScoreboard(parseInt($(this).val()));
-    updateRoll();
-    $( '.msg-rollNum' ).html("Roll " + roll);
+    // updateRoll();
+    game.turn();
+    $( '.msg-rollNum' ).html("Roll " + game.roll);
   });
 
   function updateScoreboard(pins) {
-    scoreboard.frames[0].roll(roll, pins);
+    scoreboard.frames[game.frame].roll(game.roll, pins);
     scoreboard.update();
-    // if roll_1, check for strike, update with 'X' if strike
-    // if roll_2 check for spare, update with '/' if spare
-    if (roll == 1){
-      (scoreboard.frames[0].strike) ? score = 'X' : score = scoreboard.frames[0].roll_1
+    if (game.roll == 1){
+      (scoreboard.frames[game.frame].strike) ? score = 'X' : score = scoreboard.frames[game.frame].roll_1
       $( '.frame1' ).find('.Roll1').html(score);
     } else {
-      (scoreboard.frames[0].spare) ? score = '/' : score = scoreboard.frames[0].roll_2
+      (scoreboard.frames[game.frame].spare) ? score = '/' : score = scoreboard.frames[game.frame].roll_2
       $( '.frame1' ).find('.Roll2').html(score);
     };
-
-    $( '.frame1' ).find('.Total').html(scoreboard.frames[0].score());
+    $( '.frame1' ).find('.Total').html(scoreboard.frames[game.frame].score());
   };
 
-  function updateRoll() {
-    roll = roll % 2 + 1;
-  };
+  // function updateRoll() {
+  //   roll = roll % 2 + 1;
+  // };
 });

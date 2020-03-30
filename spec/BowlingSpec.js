f8608d92d84b8c30fdab1362900949c7660c9222
plkujaw <jawor.jakub@gmail.com>
2020-03-30 09:34:27 +0100

    linter change

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -34,32 +34,32 @@ describe('Bowling Scoreboard', function() {
   });
 
 
-it('Returns the correct score for strike', function() {
-  game.roll(10);
-  game.roll(1);
-  game.roll(1);
-  rollMany(0, 17);
+  it('Returns the correct score for strike', function() {
+    game.roll(10);
+    game.roll(1);
+    game.roll(1);
+    rollMany(0, 17);
 
-  expect(game.score()).toEqual(14);
-});
+    expect(game.score()).toEqual(14);
+  });
 
-it('Returns the correct score for a perfect game (300)', function() {
-  rollMany(10, 12);
+  it('Returns the correct score for a perfect game (300)', function() {
+    rollMany(10, 12);
 
-  expect(game.score()).toEqual(300);
-});
+    expect(game.score()).toEqual(300);
+  });
 
-it('Returns the correct score for a random game', function(){
-  game.roll(3);
-  game.roll(6);
-  game.roll(10);
-  game.roll(10)
-  game.roll(4);
-  game.roll(6);
-  game.roll(2);
-  rollMany(4,13);
-  expect(game.score()).toEqual(111);
-});
+  it('Returns the correct score for a random game', function() {
+    game.roll(3);
+    game.roll(6);
+    game.roll(10);
+    game.roll(10)
+    game.roll(4);
+    game.roll(6);
+    game.roll(2);
+    rollMany(4, 13);
+    expect(game.score()).toEqual(111);
+  });
 
   function rollMany(pins, rolls) {
     for (let i = 0; i < rolls; i++) {

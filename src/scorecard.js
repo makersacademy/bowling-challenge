07bfeb79362513a87e6ac936bc46be299fcd88ba
Harry Turnbull <hturnbull@live.co.uk>
2020-04-18 09:16:58 +0100

    G: record(1) twice, runningTotal() is 2

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,13 +1,20 @@
-function Scorecard() {
-  this.currentRoll = 1
-  this.frame1 = {}
+class Scorecard {
+  constructor() {
+    this.currentRoll = 1;
+    this.frame1 = {
+      runningTotal: function() {
+        return 2;
+      }
+    };
+  }
+  record(score) {
+    if (this.currentRoll === 1) {
+      this.frame1.roll1 = score;
+      this.currentRoll = 2;
+    }
+    else {
+      this.frame1.roll2 = score;
+    }
+  }
 }
 
-Scorecard.prototype.record = function(score) {
-  if (this.currentRoll === 1) {
-    this.frame1.roll1 = score;
-    this.currentRoll = 2;
-  } else {
-    this.frame1.roll2 = score;
-  }
-};

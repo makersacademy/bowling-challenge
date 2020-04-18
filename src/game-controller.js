a315cac486c684f5d1d626ecced1b05683df4d8c
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-18 09:41:44 +0100

    Create a game controller script
    The script will connect the interface with the Game model. At
    present it just links the score input number field and button
    with the Game addScore function.

---
+++ src/game-controller.js
@@ -0,0 +1,8 @@
+$( document ).ready( () => {
+  const game = new Game();
+
+  $( "#input-score-button" ).click( () => {
+    const newScore = $( "#input-score" ).val();
+    game.addScore( newScore );
+  } );
+} );

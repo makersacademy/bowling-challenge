10a453d6738c999c1193331d52ba78cd9acd2aaa
Faye Carter <fayecarter333@gmail.com>
2020-04-18 11:12:57 +0100

    Feature test - Game bowl updates frame score (test corrected)- failing

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -2,9 +2,11 @@
 
 describe('Feature Test:', function() {
   var game;
+  var frame;
 
   beforeEach(function(){
     game = new Game;
+    frame = new Frame;
   })
 
   it('A game starts with a new frame', function() {

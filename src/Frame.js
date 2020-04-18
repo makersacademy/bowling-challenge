9facd3ec1426a77976ad19b5e79abf0d8d0e4d9b
Harry Turnbull <hturnbull@live.co.uk>
2020-04-18 12:53:51 +0100

    G: hardcoded strike as true

--- src/Frame.js
+++ src/Frame.js
@@ -4,10 +4,13 @@ class Frame {
     this.roll1 = 0
     this.roll2 = 0
   }
+  isStrike() {
+    return true
+  }
   isSpare() {
     return this.total() === 10
   }
   total() {
-    return this.roll1 + this.roll2 + this.spareBonus;
+    return this.roll1 + this.roll2 + this.spareBonus
   }
 }

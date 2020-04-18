7994477582d454fe9cba7e53a5d4685449d614ee
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-18 10:59:33 +0100

    Implement adding the first frame score

--- spec/frame_spec.js
+++ spec/frame_spec.js
@@ -8,4 +8,10 @@ describe( "Frame", () => {
   it( "should initialize the frame scores correctly", () => {
     expect( frame.score() ).toEqual( { score1: undefined, score2: undefined, total: undefined } );
   } );
+
+  it( "should add the first score correctly", () => {
+    frame.addScore( 4 );
+
+    expect( frame.score() ).toEqual( { score1: 4, score2: undefined, total: undefined } );
+  } );
 } );
--- src/frame.js
+++ src/frame.js
@@ -1,5 +1,12 @@
-function Frame() {}
+function Frame() {
+}
 
 Frame.prototype.score = function score() {
-  return { score1: undefined, score2: undefined, total: undefined };
+  return { score1: this._score1, score2: undefined, total: undefined };
+};
+
+Frame.prototype.addScore = function addScore( score ) {
+  if ( this.score1 === undefined ) {
+    this._score1 = score;
+  }
 };

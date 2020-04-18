e7ecc93aec72ab2345982acd1cdc23e6a0bc8dec
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-18 10:38:58 +0100

    Add failing test for frame score after second score added

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -35,4 +35,11 @@ describe( "Game", () => {
 
     expect( game.frame( 1 ).score ).toEqual( { score1: 5, score2: undefined, total: undefined } );
   } );
+
+  it( "should give you the current frame scores after the second score", () => {
+    game.addScore( 5 );
+    game.addScore( 2 );
+
+    expect( game.frame( 1 ).score ).toEqual( { score1: 5, score2: 2, total: 7 } );
+  } );
 } );

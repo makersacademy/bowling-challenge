46782b60c157df912d28b7122d24d61d48735ab1
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-18 11:38:06 +0100

    Add frame divs to index.html and link with controller

--- src/game-controller.js
+++ src/game-controller.js
@@ -6,5 +6,6 @@ $( document ).ready( () => {
   $( "#input-score-button" ).click( () => {
     const newScore = $( "#input-score" ).val();
     game.addScore( newScore );
+    $( "#frame-1" ).text( game.frame( 1 ).score().total );
   } );
 } );

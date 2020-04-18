5e3eec0d2c4d8568c4ae641c05ff9accdf15fc19
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-18 10:16:35 +0100

    Add a currentScore method to Game
    The method currently returns 0, as this passes the test to confirm
    the score initializes to 0.

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -8,4 +8,12 @@ describe( "Game", () => {
   it( "should accept a score from the player", () => {
     expect( game.addScore ).toBeDefined();
   } );
+
+  it( "should allow you to access the current score", () => {
+    expect( game.currentScore ).toBeDefined();
+  } );
+
+  it( "should set the current score to zero on creation", () => {
+    expect( game.currentScore() ).toEqual( 0 );
+  } );
 } );
--- src/game.js
+++ src/game.js
@@ -3,3 +3,7 @@ function Game() {}
 Game.prototype.addScore = function addScore( score ) {
   console.log( score );
 };
+
+Game.prototype.currentScore = function currentScore() {
+  return 0;
+};

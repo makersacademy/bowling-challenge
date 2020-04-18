d03c3e4eae31897e504c0119497ba802591016c7
Alex Lewis <alexlewis374@gmail.com>
2020-04-18 12:46:34 +0100

    can add a new frame score to their total score

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,4 +8,9 @@ describe('Scorecard', function() {
     it('Has a starting score of 0', function() {
         expect(scorecard.totalScore).toEqual(0)
     })
+
+    it('can add a new frame score to their total score', function(){
+        scorecard.addNewScore(2, 3)
+        expect(scorecard.totalScore).toEqual(5)
+    })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,3 +1,7 @@
 function Scorecard() {
     this.totalScore = 0
 }
+
+Scorecard.prototype.addNewScore = function(roll1, roll2) {
+    this.totalScore += (roll1 + roll2)
+}

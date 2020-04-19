c5bef2d4c387f4a3e2453d8a66e5679f2da71906
tommyrharper <tommyrharper@gmail.com>
2020-04-19 14:00:21 +0100

    strike adds value of two following rolls

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -9,15 +9,15 @@ describe('Game', function() {
   describe('open frame games', function() {
 
     it('Gutter game returns 0', function() {
-      expect(game.score([00, 00, 00, 00, 00, 00, 00, 00, 00, 00])).toEqual(0);
+      expect(game.setScore([00, 00, 00, 00, 00, 00, 00, 00, 00, 00])).toEqual(0);
     });
   
     it('game with 5 pins hit returns 5', function() {
-      expect(game.score([05, 00, 00, 00, 00, 00, 00, 00, 00, 00])).toEqual(5);
+      expect(game.setScore([05, 00, 00, 00, 00, 00, 00, 00, 00, 00])).toEqual(5);
     });
 
     it('game with 10 scores of 5 scores 50', function() {
-      expect(game.score([05, 05, 05, 05, 05, 05, 05, 05, 05, 05])).toEqual(50);
+      expect(game.setScore([05, 05, 05, 05, 05, 05, 05, 05, 05, 05])).toEqual(50);
     });
 
   });
@@ -25,7 +25,11 @@ describe('Game', function() {
   describe('strikes', function() {
 
     it('Game with 1 strike and then 5 returns 20', function() {
-      expect(game.score([10, 05, 00, 00, 00, 00, 00, 00, 00, 00])).toEqual(20);
+      expect(game.setScore([10, 05, 00, 00, 00, 00, 00, 00, 00, 00])).toEqual(20);
+    });
+
+    it('Game with 1 strike and then two 5s returns 25', function() {
+      expect(game.setScore([10, 05, 05, 00, 00, 00, 00, 00, 00, 00])).toEqual(25);
     });
   
   });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,10 +1,25 @@
 function Game() {
-
+  this.score = 0;
 }
 
-Game.prototype.score = function(frames) {
+Game.prototype.setScore = function(frames) {
   if (frames.includes(10)) {
-    return 20
+    this.strikeCalculator(frames);
+    return this.score
   }
-  return frames.reduce((a, b) => a + b, 0)
+  this.score = frames.reduce((a, b) => a + b, 0);
+  return this.score;
+}
+
+
+Game.prototype.strikeCalculator = function(array) {
+  array.forEach((number, index, arr) => {
+    console.log(number);
+    if (number == 10) {
+      this.score += 10;
+      this.score += arr[index+1];
+    } else {
+      this.score += number;
+    }
+  })
 }

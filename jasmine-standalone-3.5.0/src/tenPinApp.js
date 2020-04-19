7ada3cbfdf7ff071dd2079dc920106c7002d33b0
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-19 04:56:42 +0100

    added score update and testing

--- jasmine-standalone-3.5.0/src/tenPinApp.js
+++ jasmine-standalone-3.5.0/src/tenPinApp.js
@@ -1,4 +1,15 @@
 function Bowling () {
 this.player1_score = 0;
+this.player2_score = 0;
 
+}
+
+Bowling.prototype.addPoints = function(points, player){
+
+if(player == 1){
+    this.player1_score += points;
+}
+else
+    this.player2_score += points;
+    
 }

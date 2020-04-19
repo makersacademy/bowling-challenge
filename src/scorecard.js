88941ca90c5638d634fda5850f1a09fb5cb57a46
Graham <grahamalconer@icloud.com>
2020-04-19 15:48:06 +0100

    TEST 1 PASSED: scorecard returns 5

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,3 +1,10 @@
+Scorecard = function() {
+  this.score = 5;
+};
+
+Scorecard.prototype.scorecheck = function() {
+  return this.score;
+}
 
 //make a variable for thermostat called power saving mode
 //set that to false

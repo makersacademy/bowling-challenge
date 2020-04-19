9e304608409470d4ff983ddd3277bd58078b41dd
Harry Turnbull <hturnbull@live.co.uk>
2020-04-19 21:54:32 +0100

    reverted back to working but bloated code

--- src/Frame10.js
+++ src/Frame10.js
@@ -7,34 +7,26 @@ class Frame10 {
     this.spareBonus = null;
   }
 
-  // isComplete() {
-  //   if (this.roll3 != null) {
-  //     return true;
-  //   }
-  //   if (this.roll2 == null) {
-  //     return false
-  //   }
-  //   if (this.roll1 === 10 && this.roll2 === 10) {
-  //     return false;
-  //   }
-  //   if (this.roll1 == 10 && this.roll2 != null) {
-  //     return true;
-  //   }
-  //   if (this.roll1 != null && this.roll1 + this.roll2 < 10) {
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
   isComplete() {
+    if (this.roll3 != null) {
+      return true;
+    }
     if (this.roll2 == null) {
+      return false
+    }
+    if (this.roll1 === 10 && this.roll2 === 10) {
       return false;
     }
-    if (this.bonusRoll() && this.roll3 == null) {
-      return false;
+    if (this.roll1 == 10 && this.roll2 != null) {
+      return true;
     }
-    return true;
+    if (this.roll1 != null && this.roll1 + this.roll2 < 10) {
+      return true;
+    }
+    return false;
   }
+
+
 
   bonusRoll() {
     return this.roll1 + this.roll2 >= 10;

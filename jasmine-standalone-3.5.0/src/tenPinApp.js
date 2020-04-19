1e10661ab686b5671d4880574660872470883072
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-19 13:01:03 +0100

    added spare function, tested

--- jasmine-standalone-3.5.0/src/tenPinApp.js
+++ jasmine-standalone-3.5.0/src/tenPinApp.js
@@ -1,21 +1,25 @@
 function Bowling () {
 this.player_score = 0;
-this.strike = false
-}
-
-Bowling.prototype.addPoints = function(points){
-this.player_score += points;     
+this.strike = false;
+this.spare = false;
 }
 
 Bowling.prototype.calcPoints = function (r1,r2){
     if (r1 == 10){
         this.strike = true;
-        this.addPoints(10)
-       
+        this.player_score +=10;   
     }
      else if (this.strike == true){
         this.player_score += (r1 + r2) * 2;
         this.strike = false;
+    }
+    else if (this.spare == true){
+        this.spare = false;
+        this.player_score += (r1*2)+r2;
+    }
+    else if ((r1+r2) == 10){
+        this.spare = true;
+        this.player_score += 10;
     }
     else {
         this.player_score += (r1+r2);

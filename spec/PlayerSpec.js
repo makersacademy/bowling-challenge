71cefa87231cb62deb8689892800c4ec74d6e9d0
Edward Phillips <edd0ddi0@gmail.com>
2020-04-19 12:39:46 +0100

    unit test - Player: should be able to add the first and second bowls for a frame to the player's bowls - passing

--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -1,6 +1,20 @@
 describe("Player", function() {
+  var playerOne;
+
+  beforeEach(function() {
+     playerOne = new Player("steve");
+  })
   it(" should be able to store the player name", function() {
-    var playerOne = new Player("steve");
     expect(playerOne.name).toEqual("steve");
   });
+
+  it(" should be able to store the player's bowls", function() {
+    expect(playerOne.bowls).toEqual([]);
+  });
+  it(" should be able to add the first and second bowls for a frame to the player's bowls", () => {
+    playerOne.addFrame(2,7)
+    expect(playerOne.bowls).toEqual([2,7]);
+  });
+
+
 });
--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,9 @@
 function Player(name) {
   this.name = name
+  this.bowls = []
 }
+
+Player.prototype.addFrame = function(one, two) {
+  this.bowls.push(one)
+  this.bowls.push(two)
+};

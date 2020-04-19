e80e5a829c94f057ba62ce947376c5b029d2a2b9
Graham <grahamalconer@icloud.com>
2020-04-19 17:56:53 +0100

    Scorecard Frame: Holds bowlOne, bowlTwo, bowlThree

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -5,14 +5,25 @@ describe('Scorecard', function() {
   });
   // The first test - Does my frame contain 3 objects [Score1, Score2, Score3[sum]]
 
-  it('It allows value to be added to array', function() {
+  it('It allows firstbowl value to be added to be added to frame', function() {
     expect(scorecard.firstBowl(5)).toEqual(scorecard.bowlOne)
   });
+
   it('Stores users firstBowl in bowlOne ', function() {
     scorecard.firstBowl(5)
     expect(scorecard.bowlOne).toEqual([5]);
   });
 
+  it('Stores users secondBowl in bowlTwo', function () {
+    scorecard.secondBowl(3)
+    expect(scorecard.bowlTwo).toEqual([3]);
+  });
 
+  it('Calculates the users frame total', function () {
+    scorecard.firstBowl(5)
+    scorecard.secondBowl(3)
+    scorecard.roundSummary()
+    expect(scorecard.frameTotal).toEqual([8])
+  });
 });
 //frame(arrays),
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,16 +1,25 @@
+// the following code is for one frame in a scorecard...
 Scorecard = function() {
   this.bowlOne = [];
   this.bowlTwo = [];
-  this.bowlThree = [];
-
+  this.frameTotal = [];
 };
-
-var userFirstbowl = []
 
 Scorecard.prototype.firstBowl = function(num) {
   this.bowlOne.push(num);
   return this.bowlOne
 }
+
+Scorecard.prototype.secondBowl = function(num) {
+  this.bowlTwo.push(num);
+  return this.bowlTwo
+}
+
+Scorecard.prototype.roundSummary = function() {
+  this.frameTotal.push(this.bowlOne[0] + this.bowlTwo[0]);
+  return this.frameTotal
+}
+
 
 //make a variable for thermostat called power saving mode
 //set that to false

bb6116694778ba3f900d0c7c9ee5a485d2e79043
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-20 05:12:28 +0100

    Add feature test for game calculating correct scores

---
+++ spec/calc-correct-scores_spec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+describe( "game should automatically calcuate the correct scores", () => {
+  let game;
+
+  beforeEach( () => {
+    game = new Game();
+  } );
+  
+  describe( "should calcuate the correct scores for normal frames", () => {
+    it( "after the first ball", () => {
+      game.addScore( 1 );
+
+      expect( game.frame( 0 ).score1 ).toEqual( 2 );
+      expect( game.frame( 0 ).score2 ).toEqual( null );
+      expect( game.frame( 0 ).total ).toEqual( null );
+    } );
+  } );
+} );

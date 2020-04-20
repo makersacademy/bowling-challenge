226362dd05c789e30a044dea8948f282dd883c34
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-20 17:30:15 +0100

    Implement addScore on Frame10

--- spec/frame-10_spec.js
+++ spec/frame-10_spec.js
@@ -12,4 +12,28 @@ describe( "Frame10", () => {
     expect( frame10.score2 ).toEqual( null );
     expect( frame10.score3 ).toEqual( null );
   } );
+
+  describe( ".addScore", () => {
+    it( "sets the first score", () => {
+      frame10.addScore( 1 );
+
+      expect( frame10.score1 ).toEqual( 1 );
+    } );
+
+    it( "sets the second score", () => {
+      frame10.addScore( 1 );
+      frame10.addScore( 2 );
+
+      expect( frame10.score2 ).toEqual( 2 );
+    } );
+
+    it( "sets the third score", () => {
+      frame10.addScore( 1 );
+      frame10.addScore( 2 );
+      frame10.addScore( 3 );
+
+
+      expect( frame10.score3 ).toEqual( 3 );
+    } );
+  } );
 } );
--- src/frame-10.js
+++ src/frame-10.js
@@ -1,8 +1,21 @@
+"use strict";
+
+/* eslint-disable no-underscore-dangle */
 function Frame10() {
   this._score1 = null;
   this._score2 = null;
   this._score3 = null;
 }
+
+Frame10.prototype.addScore = function addScore( score ) {
+  if ( this._score1 === null ) {
+    this._score1 = score;
+  } else if ( this._score2 === null ) {
+    this._score2 = score;
+  } else {
+    this._score3 = score;
+  }
+};
 
 Object.defineProperty( Frame10.prototype, "score1", {
   get: function score1() {

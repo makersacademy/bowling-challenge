340684da7dc275390a683feeb58ec79343315e87
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-20 06:00:43 +0100

    Add correct scores later frames feature test

--- spec/calc-correct-scores_spec.js
+++ spec/calc-correct-scores_spec.js
@@ -8,22 +8,35 @@ describe( "game should automatically calcuate the correct scores", () => {
   } );
 
   describe( "should calcuate the correct scores for normal frames", () => {
-    it( "after the first ball", () => {
-      game.addScore( 2 );
+    describe( "first frame", () => {
+      it( "after the first ball", () => {
+        game.addScore( 2 );
 
-      expect( game.frame( 0 ).score1 ).toEqual( 2 );
-      expect( game.frame( 0 ).score2 ).toEqual( null );
-      expect( game.frame( 0 ).total ).toEqual( null );
+        expect( game.frame( 0 ).score1 ).toEqual( 2 );
+        expect( game.frame( 0 ).score2 ).toEqual( null );
+        expect( game.frame( 0 ).total ).toEqual( null );
+      } );
+
+      it( "after the second ball", () => {
+        game.addScore( 2 );
+        game.addScore( 4 );
+
+        expect( game.frame( 0 ).score1 ).toEqual( 2 );
+        expect( game.frame( 0 ).score2 ).toEqual( 4 );
+        expect( game.frame( 0 ).total ).toEqual( 6 );
+      } );
     } );
 
-    it( "after the second ball", () => {
-      game.addScore( 2 );
-      game.addScore( 4 );
+    describe( "later frame", () => {
+      it( "after 2 frames complete", () => {
+        game.addScore( 2 );
+        game.addScore( 4 );
+        game.addScore( 5 );
+        game.addScore( 3 );
 
-
-      expect( game.frame( 0 ).score1 ).toEqual( 2 );
-      expect( game.frame( 0 ).score2 ).toEqual( 4 );
-      expect( game.frame( 0 ).total ).toEqual( 6 );
+        expect( game.frame( 0 ).total ).toEqual( 6 );
+        expect( game.frame( 1 ).total ).toEqual( 8 );
+      } );
     } );
   } );
 } );

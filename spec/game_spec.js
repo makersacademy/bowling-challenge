6c53765544bf8af63978d46735ef08dc0a9a040f
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-20 04:46:35 +0100

    Refactor game specs into describes

--- spec/game_spec.js
+++ spec/game_spec.js
@@ -7,41 +7,47 @@ describe( "Game", () => {
     game = new Game();
   } );
 
-  it( "should accept a score from the player", () => {
-    expect( game.addScore ).toBeDefined();
+  describe(".addScore", () => {
+    it( "should accept a score from the player", () => {
+      expect( game.addScore ).toBeDefined();
+    } );
   } );
 
-  it( "should allow you to access the current score", () => {
-    expect( game.currentScore ).toBeDefined();
+  describe(".currentScore", () => {
+    it( "should allow you to access the current score", () => {
+      expect( game.currentScore ).toBeDefined();
+    } );
+
+    it( "should set the current score to zero on creation", () => {
+      expect( game.currentScore() ).toEqual( 0 );
+    } );
+
+    it( "should give the correct score when you add the first score", () => {
+      game.addScore( 5 );
+
+      expect( game.currentScore() ).toEqual( 5 );
+    } );
+
+    it( "should give the correct score when you add the second score", () => {
+      game.addScore( 5 );
+      game.addScore( 2 );
+
+      expect( game.currentScore() ).toEqual( 7 );
+    } );
   } );
 
-  it( "should set the current score to zero on creation", () => {
-    expect( game.currentScore() ).toEqual( 0 );
-  } );
+  describe(".frame", () => {
+    it( "should give you the current frame scores after the first score", () => {
+      game.addScore( 5 );
 
-  it( "should give the correct score when you add the first score", () => {
-    game.addScore( 5 );
+      expect( game.frame( 1 ).score() ).toEqual( { score1: 5, score2: undefined, total: 5 } );
+    } );
 
-    expect( game.currentScore() ).toEqual( 5 );
-  } );
+    it( "should give you the current frame scores after the second score", () => {
+      game.addScore( 5 );
+      game.addScore( 2 );
 
-  it( "should give the correct score when you add the second score", () => {
-    game.addScore( 5 );
-    game.addScore( 2 );
-
-    expect( game.currentScore() ).toEqual( 7 );
-  } );
-
-  it( "should give you the current frame scores after the first score", () => {
-    game.addScore( 5 );
-
-    expect( game.frame( 1 ).score() ).toEqual( { score1: 5, score2: undefined, total: 5 } );
-  } );
-
-  it( "should give you the current frame scores after the second score", () => {
-    game.addScore( 5 );
-    game.addScore( 2 );
-
-    expect( game.frame( 1 ).score() ).toEqual( { score1: 5, score2: 2, total: 7 } );
+      expect( game.frame( 1 ).score() ).toEqual( { score1: 5, score2: 2, total: 7 } );
+    } );
   } );
 } );

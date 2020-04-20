8fea01f4846e08ec07bc211851afd117562e3fc6
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-20 01:31:13 +0100

    frames now tracked

--- jasmine-standalone-3.5.0/src/tenPinApp.js
+++ jasmine-standalone-3.5.0/src/tenPinApp.js
@@ -2,9 +2,12 @@ function Bowling () {
 this.player_score = 0;
 this.strike = false;
 this.spare = false;
+this.frame = 0;
+this.bonus = false;
 }
 
 Bowling.prototype.calcPoints = function (r1,r2){
+
     if (r1 == 10){
         this.strike = true;
         this.player_score +=10;   
@@ -15,7 +18,7 @@ Bowling.prototype.calcPoints = function (r1,r2){
     }
     else if (this.spare == true){
         this.spare = false;
-        this.player_score += (r1*2)+r2;
+        this.player_score += (r1*2)+r2
     }
     else if ((r1+r2) == 10){
         this.spare = true;
@@ -24,5 +27,17 @@ Bowling.prototype.calcPoints = function (r1,r2){
     else {
         this.player_score += (r1+r2);
     }
+    this.addBonus(r1,r2); 
+    this.updateFrame();
+}
+Bowling.prototype.updateFrame = function (){
+    this.frame += 1;
+}
+
+Bowling.prototype.addBonus = function (r1,r2){
+    if (r1 + r2 == 10 && this.frame == 10){
+        this.bonus = true;
+    }
+
 }
 

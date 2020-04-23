1a4afaabbbabc27f1e425bb55059f143c0c722c1
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-23 07:05:14 +0100

    Refactor game controller

--- src/game-controller.js
+++ src/game-controller.js
@@ -28,23 +28,38 @@ $( document ).ready( () => {
     }
   }
 
-  for ( let i = 0; i < 11; i += 1 ) {
-    $( `#input-score-${i}` ).click( () => {
-      game.addScore( i );
-      updateScoreCard();
+  function attachScoreButtonListeners() {
+    for ( let i = 0; i < 11; i += 1 ) {
+      $( `#input-score-${i}` ).click( () => {
+        game.addScore( i );
+        updateScoreCard();
+      } );
+    }
+  }
+
+  function resetFrameScore( frameNumber ) {
+    $( `#frame-${frameNumber}-score-1` ).text( "" );
+    $( `#frame-${frameNumber}-score-2` ).text( "" );
+    $( `#frame-${frameNumber}-total` ).text( "" );
+  }
+
+  function attachResetButtonListener() {
+    $( "#reset-game" ).click( () => {
+      game = new Game();
+
+      for ( let i = 0; i < 10; i += 1 ) {
+        resetFrameScore( i );
+      }
+
+      $( `#frame-${9}-score-3` ).text( "" );
+      $( "#game-total" ).text( "" );
     } );
   }
 
-  $( "#reset-game" ).click( () => {
-    game = new Game();
+  function attachEventListeners() {
+    attachScoreButtonListeners();
+    attachResetButtonListener();
+  }
 
-    for ( let i = 0; i < 10; i += 1 ) {
-      $( `#frame-${i}-score-1` ).text( "" );
-      $( `#frame-${i}-score-2` ).text( "" );
-      $( `#frame-${i}-total` ).text( "" );
-    }
-
-    $( `#frame-${9}-score-3` ).text( "" );
-    $( "#game-total" ).text( "" );
-  } );
+  attachEventListeners();
 } );

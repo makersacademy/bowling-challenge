c1fc4713206dd677011bdd2f14411753cd354f86
‘Gina-Frankel <g.frankel22@gmail.com>
2020-04-23 19:56:31 +0100

    fills in the number of PINS  after TWO rolls

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,19 +1,3 @@
-/* describe('Thermostat', function() { 
-  var thermostat; 
-  
-    beforeEach(function() {  
-      thermostat  = new Thermostat();
-    });
-  
-    describe( 'temperature to start at 20', function(){
-      it('returns 20', function(){
-        expect(thermostat.startTemperature).toEqual(20)
-      });
-    }); 
- });
-
- */
-
 describe("#Bowling", function () {
   var bowling;
 
@@ -34,96 +18,85 @@ describe("#Bowling", function () {
   describe("#makeCardTemplate", function () {
     it("stores -> roll1 number of pins down; roll1 score; roll2 number of pins down; roll2 score --- in key  labelled by the frame number ", function () {
       bowling.calculateScore(1);
-
       bowling.calculateScore(4);
-
       expect(bowling.makeCardTemplate()["frame1"]).toEqual({
         r1PinsDown: 0,
         r1score: 0,
         r2PinsDown: 0,
         r2score: 0,
       });
-      expect(bowling.card["frame10"]).toEqual(
-        { r1PinsDown: 0, r1score: 0, r2PinsDown: 0, r2score: 0 }
-        //);
-      );
+      expect(bowling.card["frame10"]).toEqual({
+        r1PinsDown: 0,
+        r1score: 0,
+        r2PinsDown: 0,
+        r2score: 0,
+      });
     });
   });
-
-  xdescribe("#loop", function () {
-    it("loops 'frame + number of loop' 10 times", function () {
-      bowling.loop();
-      expect(bowling.frameKey).toEqual("frame10");
-    });
-
-    it("creates a nested loop and loops 'Roll' twice for every time it loops 'frame' ", function () {
-      bowling.loop();
-      expect(bowling.rollNum).toEqual(2);
-    });
-  });
-
-
-
 
   describe("#fillCard", function () {
-    describe("fills in FRAME after  ONE roll", function () {
+    describe("fills in FRAME correctly", function () {
       it("fills in one for number of PINS  after ONE roll ", function () {
         bowling.makeCardTemplate();
-        expect(bowling.fillCard(1,1,1)["frame1"]["r1PinsDown"]).toEqual(1);
+        expect(bowling.fillCard(1, 1, 1)["frame1"]["r1PinsDown"]).toEqual(1);
       });
 
       it("fills in the SCORE after ONE roll", function () {
         bowling.makeCardTemplate();
-        expect(bowling.fillCard(1,1,1)["frame1"]["r1Score"]).toEqual(1);
+        expect(bowling.fillCard(1, 1, 1)["frame1"]["r1Score"]).toEqual(1);
       });
 
-      // 
+      //
       xit("fills in the SCORE  after THREE rolls ", function () {
-        //will not work without a loop 
+        //will not work is refractored loop out of here   - is it neccesary to test or can I just delete? 
         bowling.makeCardTemplate();
-        bowling.fillCard(1,1,1)
-        bowling.fillCard(2,3,2)
-        expect(bowling.fillCard(1,5,3)["frame3"]["r1Score"]).toEqual(9);
+        bowling.fillCard(1, 1, 1);
+        bowling.fillCard(2, 3, 2);
+        expect(bowling.fillCard(1, 100, 3)["frame3"]["r1Score"]).toEqual(9);
       });
 
-
-
-
-      xdescribe("fills in ROLL correctly", function () {
+      describe("fills in ROLL correctly", function () {
         it("fills in the number of PINS  after TWO rolls ", function () {
           bowling.makeCardTemplate();
-          bowling.calculateScore(1);
-          bowling.calculateScore(4);
-          expect(bowling.fillCard(1,4)["frame1"]["r2PinsDown"]).toEqual(1);
+          expect(bowling.fillCard(1, 4, 1)["frame1"]["r2PinsDown"]).toEqual(1);
+          expect(bowling.fillCard(1, 4, 1)["frame1"]["r1PinsDown"]).toEqual(undefined);
         });
       });
     });
   });
 
-
-
-
-  xdescribe("#makeCard", function () {
-    it("returns 1", function () {
-      bowling.numPinsDown = 1;
-      bowling.makeCardTemplate();
-      //bowling.makeCard()
-      expect(bowling.makeCard()["frame1"]["r1PinsDown"]).toEqual(1);
-    });
-  });
-
   describe("run_card_making", function () {
-    it("fills in the number of PINS ", function () {
-      var bowlingMock = new Bowling();
-      spyOn(bowlingMock, "getInput").and.returnValue(3);
-      expect(bowlingMock.run_card_making()["frame1"]["r1PinsDown"]).toEqual(3);
+
+    describe("returns the correct amount of PINS", function () {
+
+      it("fills in the roll 1 number of PINS for Frame 1  as 3", function () {
+        var bowlingMock = new Bowling();
+        spyOn(bowlingMock, "getInput").and.returnValue(3);
+        expect(bowlingMock.run_card_making()["frame1"]["r1PinsDown"]).toEqual(3)
+      });
+
+      it("fills in the roll 1 number of PINS for Frame 3 as 5 ", function () {
+        var bowlingMock = new Bowling();
+        spyOn(bowlingMock, "getInput").and.returnValue(5);
+        expect(bowlingMock.run_card_making()["frame3"]["r1PinsDown"]).toEqual(5)
+      });
     });
-    it("fills in the SCORE after ONE roll", function () {
-      var bowlingMock = new Bowling();
-      bowlingMock.makeCardTemplate();
-      spyOn(bowlingMock, "getInput").and.returnValue(2);
-      expect(bowlingMock.run_card_making()["frame1"]["r1Score"]).toEqual(2);
-      expect(bowlingMock.card["frame3"]["r1Score"]).toEqual(6)
+
+      describe("returns the correct score", function () {
+
+      it("fills in the roll 1 SCORE for Frame 1 as 3", function () {
+        var bowlingMock = new Bowling();
+        spyOn(bowlingMock, "getInput").and.returnValue(3);
+        expect(bowlingMock.run_card_making()["frame1"]["r1Score"]).toEqual(3);
+      });
+
+      it("fills in the roll 1 SCORE for Frame 3 as 6 ", function () {
+        var bowlingMock = new Bowling();
+        bowlingMock.makeCardTemplate();
+        spyOn(bowlingMock, "getInput").and.returnValue(2);
+        expect(bowlingMock.run_card_making()["frame1"]["r1Score"]).toEqual(2);
+        expect(bowlingMock.card["frame3"]["r1Score"]).toEqual(6);
+      });
     });
   });
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 const MAX_TEMPERATURE_PSM_ON = 25
@@ -21,117 +21,72 @@ Thermostat.prototype.reset = function () {
 }
 */
 
-function Bowling(){
-  this.score = 0
-  this.numPinsDown = 0
-  this.card 
-  this.frameKey 
-  this.rollNum
-  this.roll
- 
-}
-
-Bowling.prototype.makeCard =function() {
-  this.calculateScore(this.numPinsDown)
-  return this.fillCard()
-  
+function Bowling() {
+  this.score = 0;
+  this.numPinsDown = 0;
+  this.card;
+  this.frameKey;
+  this.rollNum;
+  this.roll;
 }
 
 
-Bowling.prototype.setUpCard =function() {
-  this.loop()
-  this.makeCardTemplate()
-  
-}
+Bowling.prototype.setUpCard = function () {
+  this.loop();
+  this.makeCardTemplate();
+};
 
-Bowling.prototype.calculateScore = function(numPinsDown){
-  //this.numPinsDown = numPinsDown
-  this.score = numPinsDown + this.score
-  return this.score
+Bowling.prototype.calculateScore = function (numPinsDown) {
+  this.score = numPinsDown + this.score;
+  return this.score;
+};
 
-}
-
-Bowling.prototype.makeCardTemplate =  function(){
-  // change name  to makeCardTemplateTemplate? 
+Bowling.prototype.makeCardTemplate = function () {
   var counter1;
   var counter2;
-  var  obj = {};
-  
-  for (counter1= 1; counter1 < 11; counter1++) {
-    var key = `frame${counter1}`
-    //for (counter2 = 1; counter2 < 3; counter2 ++) 
-    obj[key] = { 'r1PinsDown': 0, 'r1score': 0, 'r2PinsDown' :0, 'r2score' : 0 }
-    this.card = obj
+  var obj = {};
+
+  for (counter1 = 1; counter1 < 11; counter1++) {
+    var key = `frame${counter1}`;
+    obj[key] = { r1PinsDown: 0, r1score: 0, r2PinsDown: 0, r2score: 0 };
+    this.card = obj;
   }
-   //console.dir(this.card)
-   return this.card
+  
+  return this.card;
+};
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getRndInteger(min, max) { 
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-
-
-
-Bowling.prototype.run_card_making = function(){
-  this.makeCardTemplate()
+Bowling.prototype.run_card_making = function () {
+  this.makeCardTemplate();
   var numFrame;
-  //var counter2;
-  for (numFrame= 1; numFrame < 11; numFrame++) { 
-    var  rolls = this.getInput()
-    var score = this.calculateScore(rolls)
-    this.fillCard(rolls,score, numFrame)
+  for (numFrame = 1; numFrame < 11; numFrame++) {
+    var pins = this.getInput();
+    var score = this.calculateScore(pins);
+    this.fillCard(pins, score, numFrame);
   }
-  console.log(this.card)
-  return (this.card)
-}
+  //console.log(this.card);
+  return this.card;
+};
 
+Bowling.prototype.getInput = function () {
+  this.numPinsDown = getRndInteger(1, 4);
+  this.fillCard;
+  return getRndInteger(1, 4);
+};
 
-Bowling.prototype.getInput = function(){
-  this.numPinsDown = getRndInteger(1, 4)
-  this.fillCard
-  return getRndInteger(1, 4)
+Bowling.prototype.fillCard = function (pins, score, numFrame) {
+  this.frameKey = "frame" + numFrame;
+  this.card[this.frameKey]["r1PinsDown"] = pins;
+  this.card[this.frameKey]["r1Score"] = score;
 
+  this.card[this.frameKey]["r2PinsDown"] = pins;
   
-}
-
-Bowling.prototype.fillCard = function(rolls,score, numFrame){
-
   
-    
-    // need to make argument = to something that changes each roll
-    this.frameKey =  ("frame" + numFrame)
-    this.card[this.frameKey]['r1PinsDown'] = rolls
-    // this.PinsDown updates to the latest 
-    this.card[this.frameKey]['r1Score'] = score 
-    //for (counter2 = 1; counter2 < 3; counter2 ++) 
-    // this.rollNum = (  counter2);
- 
-  //console.dir(this.card)
-  return this.card
 
-}
+  return this.card;
+};
 
 
-
-
-
-Bowling.prototype.loop = function(objectToLoop){
-  //var counter1;
-  //var counter2;
-  //for (counter1= 1; counter1 < 11; counter1++) {
-    this.frameKey =  ("frame" + counter1)
-    for (counter2 = 1; counter2 < 3; counter2 ++) 
-     this.rollNum = (  counter2);
-  //}
-}
-
-
-/*
-for (counter = 1; counter < 4; counter++) { // count from 1 to 3 three times
-  console.log("Count from 1 to 3");
-  for (counterTwo = 1; counterTwo < 4; counterTwo++){
-      console.log(counterTwo);
-  }
-}
-*/

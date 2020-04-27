c332071764322082da431e9ce5e1f66c1c1d4f91
Graham <grahamalconer@icloud.com>
2020-04-27 10:13:30 +0100

    5/10 eddd

--- interface.js
+++ interface.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
     var scorecard = new Scorecard();
+    var scorecard2 = new Scorecard();
     $('#round_one').submit(function(event) {
       event.preventDefault();
       var ball1 = parseInt($('#usersFirstBowl').val()),
@@ -10,10 +11,26 @@ $(document).ready(function() {
         scorecard.firstBowl(ball1);
         scorecard.secondBowl(ball2);
         scorecard.roundSummary();
-        roundTotal();
+        round1Total();
     })
 
-  function roundTotal() {
-    $('#result_round_one').text(scorecard.printFrameScore());
+    $('#round_two').submit(function(event) {
+      event.preventDefault();
+      var ball1 = parseInt($('#usersFirstBowl2').val()),
+          ball2 = parseInt($('#usersSecondBowl2').val());
+        $('#usersFirstBowl2').val('');
+        $('#usersSecondBowl2').val('');
+        scorecard2.firstBowl(ball1);
+        scorecard2.secondBowl(ball2);
+        scorecard2.roundSummary();
+        round2Total();
+    })
+
+  function round1Total() {
+    $('#allScores').text(scorecard.printFrameScore());
+  };
+
+  function round2Total() {
+    $('#result_round_two').text(scorecard2.printFrameScore());
   };
 })

ab46ee3e553731a19439f5cb9bf2d9cef6897f1b
tommyrharper <tommyrharper@gmail.com>
2020-04-28 12:27:24 +0100

    refactored for readability

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -2,6 +2,7 @@ class Scorecard {
   constructor() {
     this.score = 0;
     this.frames= [];
+    this.TEN_FRAMES = 10;
   }
   addFrame(frame) {
     this.isGameComplete();
@@ -14,63 +15,65 @@ class Scorecard {
     this.calculateScore();
   }
   calculateScore() {
-    this.frames.forEach((frame, index, frames) => {
-      if (index < 10) {
-        this.scoreLogic(frame, index, frames);
+    this.frames.forEach((frame, frameNumber, frames) => {
+      // this ensures rolls after the 10th frame are only counted for bonus points.
+      if (frameNumber < this.TEN_FRAMES) {
+        this.scoreLogic(frame, frameNumber, frames);
       }
     });
   }
-  scoreLogic(frame, index, frames) {
+  scoreLogic(frame, frameNumber, frames) {
     if (frame.isSpare()) {
-      this.addSpare(frame, index, frames);
+      this.addSpare(frame, frameNumber, frames);
     } else if (frame.isStrike()) {
-      this.addStrike(frame, index, frames);
+      this.addStrike(frame, frameNumber, frames);
     } else {
       this.addOpenFrame(frame);
     }
   }
-  addSpare(frame, index, frames) {
+  addSpare(frame, frameNumber, frames) {
     this.addOpenFrame(frame);
-    if (this.nextRollIsUndefined(index, frames)) {
+    if (this.nextRollIsUndefined(frameNumber, frames)) {
     } else {
-      this.score += frames[index+1].roll1;
+      this.score += frames[frameNumber+1].roll1;
     }
   }
-  addStrike(frame, index, frames) {
+  addStrike(frame, frameNumber, frames) {
     this.score += 10;
-    if (this.nextRollIsUndefined(index, frames)) {
-    } else if (this.nextRollIsSpare(index, frames)) {
-    } else if (this.rollAfterNextIsUndefined(index, frames)) {
-    } else if (this.nextRollIsStrike(index, frames)) {
+    if (this.nextRollIsUndefined(frameNumber, frames)) {
+      this.score = 'Add another frame.';
+    } else if (this.nextRollIsSpare(frameNumber, frames)) {
+      this.score += 10;
+    } else if (this.rollAfterNextIsUndefined(frameNumber, frames)) {
+      this.score = 'Add another frame.';
+    } else if (this.nextRollIsStrike(frameNumber, frames)) {
+      this.score += 10;
+      this.score += frames[frameNumber+2].roll1;
     } else {
-      this.addOpenFrame(frames[index+1]);
+      this.addOpenFrame(frames[frameNumber+1]);
     }
   }
   addOpenFrame(frame) {
     this.score += frame.roll1 + frame.roll2;
   }
-  nextRollIsUndefined(index, frames) {
-    if (frames[index+1] == undefined) {
-      this.score = 'Add another frame.';
+  nextRollIsUndefined(frameNumber, frames) {
+    if (this.frames.length == frameNumber+1) {
       return true;
     }
   }
-  nextRollIsSpare(index, frames) {
-    if (frames[index+1].isSpare()) {
-      this.score += 10;
+  nextRollIsSpare(frameNumber, frames) {
+    if (frames[frameNumber+1].isSpare()) {
       return true;
     }
   }
-  rollAfterNextIsUndefined(index, frames) {
-    if (frames[index+1].isStrike() && frames[index+2] == undefined) {
-      this.score = 'Add another frame.';
+  rollAfterNextIsUndefined(frameNumber, frames) {
+    if (frames[frameNumber+1].isStrike() &&
+    this.frames.length == frameNumber+2) {
       return true;
     }
   }
-  nextRollIsStrike(index, frames) {
-    if (frames[index+1].isStrike()) {
-      this.score += 10;
-      this.score += frames[index+2].roll1;
+  nextRollIsStrike(frameNumber, frames) {
+    if (frames[frameNumber+1].isStrike()) {
       return true;
     }
   }
@@ -80,11 +83,15 @@ class Scorecard {
     const frameBeforeLast = this.frames[numberOfFrames-2];
 
     if (numberOfFrames == 10 &&
-    previousFrame.isOpenFrame() ||
-    numberOfFrames == 11 &&
-    (previousFrame.isSpare() ||
-    frameBeforeLast.isSpare()) ||
-    numberOfFrames == 12) {
+    previousFrame.isOpenFrame()) {
+      throw new Error('Game complete!');
+    }
+    if (numberOfFrames == 11 &&
+      (previousFrame.isSpare() ||
+      frameBeforeLast.isSpare())) {
+      throw new Error('Game complete!');
+    }
+    if (numberOfFrames == 12) {
       throw new Error('Game complete!');
     }
   }

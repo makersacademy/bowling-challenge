4ec734e2539b585fa242b08d1158ac479feef984
tommyrharper <tommyrharper@gmail.com>
2020-04-28 12:42:22 +0100

    separated out multiple clauses for readability in checkRolls method

--- src/Frame.js
+++ src/Frame.js
@@ -3,30 +3,37 @@ class Frame {
     this.checkRolls(roll1, roll2);
     this.roll1 = roll1;
     this.roll2 = roll2;
+    this.ALL_PINS = 10;
   }
   isStrike() {
-    if (this.roll1 != 10) {
+    if (this.roll1 != this.ALL_PINS) {
       return false;
     }
     return true;
   }
   isOpenFrame() {
-    if (this.roll1 + this.roll2 == 10) {
+    if (this.roll1 + this.roll2 == this.ALL_PINS) {
       return false;
     }
     return true;
   }
   isSpare() {
-    if (this.roll1 != 10 && this.roll1 + this.roll2 == 10) {
+    if (this.roll1 != this.ALL_PINS && this.roll1 + this.roll2 == this.ALL_PINS) {
       return true;
     }
     return false;
   }
   checkRolls(roll1, roll2) {
-    if ((roll1 || roll2) > 10 || (roll1 || roll2) < 0 ||
-    isNaN(roll1 || roll2)) {
+    if ((roll1 || roll2) > 10) {
       throw new Error('Must roll a number between 0 and 10');
-    } if (roll1 + roll2 > 10) {
+    }
+    if ((roll1 || roll2) < 0) {
+      throw new Error('Must roll a number between 0 and 10');
+    }
+    if (isNaN(roll1 || roll2)) {
+      throw new Error('Must roll a number between 0 and 10');
+    }
+    if (roll1 + roll2 > 10) {
       throw new Error('Cannot knock down more than 10 pins.');
     }
   }

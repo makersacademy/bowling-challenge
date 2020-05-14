89dde5d62feb731ce61ce4705d2f65279dde59e8
Ed <lookatemail@gmail.com>
2020-05-14 12:51:10 +0100

    Jasmine test framework setup

---
+++ spec/bowlingSpec.js
@@ -0,0 +1,16 @@
+describe('JavaFizzBuzz', function(){
+    // method statement
+    var bowling;
+    beforeEach(function() {
+      bowling = new Bowling();
+    });
+  
+    describe('knows when the number is', function() {
+        // it('devisable by 3', function() {
+        //     expect(javabuzz.isDivisibleByThree(3)).toBe(true);
+        // });
+
+    });
+
+
+});

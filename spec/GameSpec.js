85ec71a507629bda865f8b3ebb97458c4f9e920a
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-15 18:02:37 +0100

    play method tracks frame and roll values

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,4 +11,17 @@ describe('Game', function () {
         expect(game.frame).toEqual(1);
         expect(game.roll).toEqual(1);
     });
+
+    describe('play', function () {
+        it("increments roll by 1 if roll was 1", function () {
+            game.play(4);
+            expect(game.roll).toEqual(2);
+        });
+        it("increments frame by 1 if roll was 2", function () {
+            game.roll = 2
+            game.play(4)
+            expect(game.frame).toEqual(2);
+            expect(game.roll).toEqual(1);
+        });
+    });
 });
--- src/Game.js
+++ src/Game.js
@@ -4,4 +4,13 @@ class Game {
       this.frame = 1;
       this.roll = 1;
     }
+
+    play(){
+      if(this.roll === 2){
+        this.frame ++;
+        this.roll = 1;
+      } else {
+        this.roll ++;
+      }
+    }
 }

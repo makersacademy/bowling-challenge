77c5850dc82dca08518f3daf8dee3744e1b88489
Joanne Young <joanneyoung_@hotmail.com>
2020-05-15 12:21:12 +0100

    scorecard can total scores and knockdown pins

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,17 +1,32 @@
-// it starts with zero knocked down pins
 
-// it start with zero total score
+'use strict'
 
-// it can take a knocked down pins input
+describe('Scorecard',function(){
+
+var scorecard;
+
+beforeEach(function(){
+  scorecard = new Scorecard;
+});
+
+it('has a total default to zero', function(){
+  expect(scorecard.totalScore()).toEqual(0);
+});
+
+it('can add to the score', function(){
+  expect(scorecard.knockDownPins(5,3)).toEqual(8)
+});
 
 // it can add knocked pins to total score
-
-// it can total the second round
-
-// is spare?
+it('can add two frames to the score', function(){
+  expect(scorecard.knockDownPins(5,3)).toEqual(8)
+});
 
 // spare - it adds the next score on
 
 // is strike?
 
 // strike - it adds the next two scores on
+
+});
+
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,1 +1,25 @@
 // strike bonus = 10
+'use strict'
+
+class Scorecard {
+
+  constructor(){
+    this.totalArr = []
+    this.frameScore = 0; 
+  };
+
+  totalScore(){
+    var totalScore = 0;
+      for (var i = 0, len = this.totalArr.length; i < len; i++) {
+        totalScore += totalArr[i];  // Iterate over your first array and then grab the second element add the values up
+      }
+    return totalScore
+  }
+
+  knockDownPins(roll1, roll2){
+    var score = roll1 + roll2
+    this.totalArr.push(score)
+    return score
+  }
+
+}

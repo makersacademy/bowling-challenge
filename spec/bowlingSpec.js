f17b118edf43369c2fbd3976cced916a31571300
Ed <lookatemail@gmail.com>
2020-05-15 19:23:04 +0100

    refactor code add some helper methods to JS file

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -11,20 +11,32 @@ describe('Bowling', function() {
       expect(bowling.firstBowl).toBe(true);
     });
 
-    describe('Frame score', function() {
+    describe('Frame score without strikes', function() {
       it('able to count score when in position 1', () => {
         bowling.countScore(4);
-        expect(bowling.getFrameScore()).toEqual(4);
+        expect(bowling.getTotalScore()).toEqual(4);
         expect(bowling.frame).toEqual(1);
         expect(bowling.firstBowl).toBe(false);
-        expect(bowling.totalScore).toEqual(4);
+        expect(bowling.getTotalScore()).toEqual(4);
       });
       it('able to count score when in position 2', () => {
         bowling.countScore(5);
         bowling.countScore(3);
-        expect(bowling.getFrameScore()).toEqual(8);
+        expect(bowling.getTotalScore()).toEqual(8);
         expect(bowling.firstBowl).toBe(true);
         expect(bowling.frame).toEqual(2);
+      });
+
+      describe('Frame score with strikes', () => {
+        it('able to count score when in position 1',  () => {
+          bowling.countScore(10);
+          expect(bowling.getTotalScore()).toBe(10);
+          expect(bowling.firstBowl).toBe(true);
+          expect(bowling.frame).toEqual(2);
+        });
+        it('able to count score when in position 2', () => {
+          
+        });
       });
 
     });
--- src/bowling.js
+++ src/bowling.js
@@ -4,27 +4,32 @@ class Bowling {
     this.MAX_FRAME_SCORE = 10;
     this.firstBowl = true;
     this.frame = 1;
-    this.frameScore = 0;
-    this.totalScore = 0;
-  }
-
-  getTotalScore() {
-    return this.totalScore
-  }
-
-  getFrameScore() {
-    return this.frameScore
+    this.frameScore = [];
   }
 
   countScore(score) {
+    if(score === this.MAX_FRAME_SCORE) {
+      this.frame += 1;
+      this.firstBowl = true;
+      this.frameScore.push(score, 0)
+    } else {
+      this._countHelper();
+      this.frameScore.push(score);
+      console.log(this.frameScore)
+    }
+  }
+  getTotalScore() {
+    return this.frameScore.reduce((a, b) => a + b, 0)
+  }
+  
+  _countHelper() {
     if(this.firstBowl === false) {
       this.firstBowl = true;
       this.frame += 1;
     } else {
       this.firstBowl = false;
     }
-    this.frameScore += score;
-    this.totalScore += this.frameScore;
+    
   }
 
 }

4318cf7e48f352e67e11a084fb952b779f9af563
Joanne Young <joanneyoung_@hotmail.com>
2020-05-15 20:53:18 +0100

    add logic for spare and strike

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -3,40 +3,44 @@
 
 describe('Scorecard',function(){
 
-var scorecard;
+  var scorecard;
 
-beforeEach(function(){
-  scorecard = new Scorecard;
-});
+  beforeEach(function(){
+    scorecard = new Scorecard;
+  });
 
-it('has a total default to zero', function(){
-  expect(scorecard.totalScore()).toEqual(0);
-});
+  it('has a total default to zero', function(){
+    expect(scorecard.totalScore()).toEqual(0);
+  });
 
-it('can add to the score', function(){
-  expect(scorecard.knockDownPins(5,3)).toEqual(8)
-});
+  // it can add knocked pins together
+  it('can add two rolls', function(){
+    expect(scorecard.frameScore(5,3)).toEqual(8)
+  });
 
-// it can add knocked pins to total score
-it('can add two frames to the score', function(){
-  expect(scorecard.knockDownPins(5,3)).toEqual(8)
-});
+  // is spare?
+  it('can result a spare', function(){
+    expect(scorecard.isSpare(6,4)).toBe(true)
+  });
 
-// is spare?
-it('can result a spare', function(){
-  scorecard.knockDownPins(5,5)
-  expect(scorecard.isSpare()).toBe(true)
-});
+  // is strike?
+  it('can result a strike', function(){
+    expect(scorecard.isStrike(5,5)).toBe(true)
+  });
 
-// is strike?
-it('can result a strike', function(){
-  scorecard.knockDownPins(1,10)
-  expect(scorecard.isStrike()).toBe(true)
-});
 
-// spare - it adds the next score on
+  describe('score is a spare', function(){
+    beforeEach(function(){
+      scorecard.isStrike.and.returnValue(true);
+    });
 
-// strike - it adds the next two scores on
-
+    // spare = add 10 point and add next array item to this total
+    // it('calculates spare scores', function(){
+    //   scorecard.input(5,5)
+    //   expect(scorecard.framesScore).toContain(10)
+    //   scorecard.input(4,1)
+    //   expect(scorecard.framesScore).toContain(14)
+    // });
+  });
 });
 
--- src/scorecard.js
+++ src/scorecard.js
@@ -5,43 +5,55 @@ class Scorecard {
 
   constructor(){
     this.totalArr = []
-    this.frameScore = 0; 
+    this.Scores = []
+    this.roll1 = 0;
+    this.roll2 = 0;
   };
 
+ 
+
+  input(roll1, roll2){
+    if (this.isSpare(roll1,roll2) === true) {
+      console.log("spare!");
+    } else if (this.isStrike(roll1,roll2) === true) {
+      console.log("strike!");
+    } else {
+      console.log("normal score!")
+      this.totalArr.push(roll1);
+      this.totalArr.push(roll2);
+      this.totalArr.push(scorecard.frameScore(roll1,roll2));
+    }
+  }
+
+  frameScore(roll1 ,roll2){
+    return roll1 + roll2
+  }
+ 
+  isSpare(roll1, roll2){
+    if (roll1 + roll2 === 10){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  isStrike(roll1, roll2){
+    if (roll1 || roll2 === 10){
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
+  // overall score
   totalScore(){
     var totalScore = 0;
-      for (var i = 0, len = this.totalArr.length; i < len; i++) {
+      for (var i = 0, len = this.frameScoreslength; i < len; i++) {
         totalScore += totalArr[i];  // Iterate over your first array and then grab the second element add the values up
       }
     return totalScore
   }
-
-  knockDownPins(roll1, roll2){
-    this.roll1 = roll1
-    this.roll2 = roll2
-    var score = roll1 + roll2
-    this.totalArr.push(score)
-    return score
-  }
-
-  isSpare(){
-    if (this.roll1 + this.roll2 === 10){
-      return true
-    }
-    else {
-      return false
-    }
-  }
-
-  isStrike(){
-    if (this.roll1 || this.roll2 === 10){
-      return true
-    }
-    else {
-      return false
-    }
-  }
-
-
 
 }

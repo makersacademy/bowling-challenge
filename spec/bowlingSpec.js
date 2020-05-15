8300ce018de4855ed3ee8e0526a6fad59a8a9534
Ed <lookatemail@gmail.com>
2020-05-15 14:25:36 +0100

    feature to add count when user hit 10 pins

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -3,25 +3,28 @@ describe('Bowling', function() {
     var bowling;
     beforeEach(function() {
       bowling = new Bowling();
+      bowling.totalScore === 0;
     });
   
     it('score at the start of the game is 0', () => {
       expect(bowling.getTotalScore()).toEqual(0);
+      expect(bowling.firstBowl).toBe(true);
     });
 
     describe('Frame score', function() {
-      it('able to count score for the frame', () => {
-        bowling.countScore(5);
-        expect(bowling.getFrameScore()).toEqual(5);
-        expect(bowling.FRAME).toEqual(1)
-        expect(bowling.POSITION).not.toEqual(1);
+      it('able to count score when in position 1', () => {
+        bowling.countScore(4);
+        expect(bowling.getFrameScore()).toEqual(4);
+        expect(bowling.frame).toEqual(1);
+        expect(bowling.firstBowl).toBe(false);
+        expect(bowling.totalScore).toEqual(4);
       });
       it('able to count score when in position 2', () => {
         bowling.countScore(5);
         bowling.countScore(3);
         expect(bowling.getFrameScore()).toEqual(8);
-        expect(bowling.FRAME).toEqual(2);
-        expect(bowling.POSITION).toEqual(2);
+        expect(bowling.firstBowl).toBe(true);
+        expect(bowling.frame).toEqual(2);
       });
 
     });
--- src/bowling.js
+++ src/bowling.js
@@ -2,14 +2,14 @@ class Bowling {
 
   constructor() {
     this.MAX_FRAME_SCORE = 10;
-    this.POSITION = 1;
-    this.FRAME = 1;
+    this.firstBowl = true;
+    this.frame = 1;
     this.frameScore = 0;
     this.totalScore = 0;
   }
 
   getTotalScore() {
-    return this.totalScore 
+    return this.totalScore
   }
 
   getFrameScore() {
@@ -17,14 +17,14 @@ class Bowling {
   }
 
   countScore(score) {
-    if(this.POSITION === 2) {
-      this.POSITION = 1;
-      this.FRAME += 1;
+    if(this.firstBowl === false) {
+      this.firstBowl = true;
+      this.frame += 1;
+    } else {
+      this.firstBowl = false;
     }
-    if(this.frameScore < this.MAX_FRAME_SCORE && this.POSITION <= 2) {
-      this.POSITION = 2;
-      this.frameScore += score;
-    }
+    this.frameScore += score;
+    this.totalScore += this.frameScore;
   }
 
 }

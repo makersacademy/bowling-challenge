0db35fff84633c6f772ec7a9b297b547fa0e9ccd
Ed <lookatemail@gmail.com>
2020-05-15 08:29:01 +0100

    able to count frame score in position 1

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,4 +1,4 @@
-describe('Bowling', function(){
+describe('Bowling', function() {
     // method statement
     var bowling;
     beforeEach(function() {
@@ -7,8 +7,18 @@ describe('Bowling', function(){
   
     it('score at the start of the game is 0', function() {
       expect(bowling.getTotalScore()).toEqual(0);
+    });
+
+    describe('Frame score', function() {
+      it('able to count score for the frame', function() {
+        bowling.countScore(5);
+        expect(bowling.getFrameScore()).toEqual(5);
+        expect(bowling.FRAME).toEqual(1)
+        expect(bowling.POSITION).not.toEqual(1);
+      });
 
     });
+
 
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,12 +1,30 @@
 class Bowling {
 
-  constructor(){
-
-    this.totalScore = 0
+  constructor() {
+    this.MAX_FRAME_SCORE = 10;
+    this.POSITION = 1;
+    this.FRAME = 1;
+    this.frameScore = 0;
+    this.totalScore = 0;
   }
 
   getTotalScore() {
     return this.totalScore 
+  }
+
+  getFrameScore() {
+    return this.frameScore
+  }
+
+  countScore(score) {
+    if(this.POSITION === 2) {
+      this.POSITION = 1;
+      this.FRAME += 1;
+    }
+    if(this.frameScore < this.MAX_FRAME_SCORE && this.POSITION <= 2) {
+      this.POSITION += 1;
+      this.frameScore += score;
+    }
   }
 
 }

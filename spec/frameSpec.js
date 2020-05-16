bd35a8af9599cfb15861b64fb52491d538fc09df
mbrad26 <mbrad26@gmail.com>
2020-05-16 16:13:54 +0100

    added test for when a frame ends as strike

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -15,9 +15,17 @@ describe('Frame', () => {
       expect(frame.total()).toEqual(8);
     });
 
-    describe('when a frame ends as spare', () => {
+    describe('when a frame ends with spare', () => {
       it('calculates a frame score', () => {
         frame.rolls = [3, 7];
+
+        expect(frame.total()).toEqual(10);
+      });
+    });
+
+    describe('when a frame ends with strike', () => {
+      it('calculates a frame score', () => {
+        frame.rolls = [10];
 
         expect(frame.total()).toEqual(10);
       });

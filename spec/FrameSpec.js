79f0ca2e16debeabf16126e28c4036ffd1c92fcd
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-16 15:23:13 +0100

    #displayScore - returns score if there are no turns pending

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -9,38 +9,57 @@ describe("Frame", function() {
     expect(frame.index).toEqual(1);
   });
 
-  describe("first roll", function() {
+  describe("#displayScore", function() {
+    beforeEach(function() {
+      frame.firstRoll   = 5;
+      frame.secondRoll  = 4;
+    });
+
+    it("should display blank if there are turns pending", function() {
+      frame.turns = 1;
+      expect(frame.displayScore()).toEqual("")
+    });
+
+    it("should display score if there are no turns pending", function() {
+      frame.turns = 0;
+      expect(frame.displayScore()).toEqual(9)
+    });
+  });
+
+  describe("#firstRoll", function() {
     it("should return score", function() {
-      frame.first = 5;
+      frame.firstRoll = 5;
       expect(frame.first).toEqual(5);
       expect(frame.hasStrike()).toEqual(false);
     });
 
     it("#hasStrike should return true if score is 10", function() {
-      frame.first = 10;
+      frame.firstRoll = 10;
       expect(frame.hasStrike()).toEqual(true);
+      expect(frame.turns).toEqual(2);
     });
   });
 
-  describe("second roll", function() {
+  describe("#secondRoll", function() {
     beforeEach(function() {
-      frame.first = 3;
+      frame.firstRoll = 3;
     });
 
     it("should return score", function() {
-      frame.second = 5;
+      frame.secondRoll = 5;
       expect(frame.second).toEqual(5);
     });
 
     it("#score should return total score", function() {
-      frame.second = 5;
+      frame.secondRoll = 5;
       expect(frame.score()).toEqual(8);
       expect(frame.hasSpare()).toEqual(false);
     });
 
     it("#hasSpare should return true if total score is 10", function() {
-      frame.second = 7;
+      frame.secondRoll = 7;
       expect(frame.hasSpare()).toEqual(true);
+      expect(frame.turns).toEqual(1);
     });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,22 +1,36 @@
 class Frame {
   constructor(index) {
-    this.index  = index
-    this.first  = 0;
-    this.second = 0;
+    this.index = index;
+    this.turns = 0;
+  }
+
+  set firstRoll(value) {
+    this.first = value;
+    if (this.hasStrike()) this.turns = 2;
+  }
+
+  set secondRoll(value) {
+    this.second = value;
+    if (this.hasSpare()) this.turns = 1;
   }
 
   hasSpare() {
     if (this.hasStrike() == false && this.score() == 10) return true;
     return false;
-  }
+  };
 
   hasStrike() {
     if (this.first == 10) return true;
     return false;
-  }
+  };
 
   score() {
     return this.first + this.second;
-  }
+  };
+
+  displayScore() {
+    if (this.turns == 0) return this.score();
+    return "";
+  };
 
 };

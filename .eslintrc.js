b40c46fce0b37511d55c722315911ef0b7171f04
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-16 08:47:46 +0100

    Sets up ESLint and adds .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 11
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};

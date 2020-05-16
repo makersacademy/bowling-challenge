32a37779d34da7ce062ae8ce7f9aa891d5403b2f
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-16 13:42:41 +0100

    Added extra round functionality

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -34,6 +34,27 @@ describe('Game', function () {
             game.play(4);
             expect(game.addScore).toHaveBeenCalled();
         });
+        it('strike - so is an extra round', function () {
+          for (let i = 0; i < 20; i += 1) {
+            game.play(10)
+          };
+          game.play(2);
+          expect(game.score[game.score.length - 1].roll).toEqual(3);
+        });
+        it('spare - so is an extra round', function () {
+          for (let i = 0; i < 19; i += 1) {
+            game.play(4)
+          };
+          game.play(6);
+          game.play(2);
+          expect(game.score[game.score.length - 1].roll).toEqual(3);
+        });
+        it('neither - so game ends', function () {
+          for (let i = 0; i < 20; i += 1) {
+            game.play(4)
+          };
+          expect(game.play(4)).toEqual("The game has ended.");
+        });
     });
 
     describe('end', function () {
@@ -46,6 +67,30 @@ describe('Game', function () {
         it('adds score from round to array as hash', function () {
             game.play(4);
             expect(game.score).toEqual([{frame: 1, roll: 1, knocked: 4}]);
+        });
+    });
+
+    describe('isExtraRound', function () {
+        it('returns true for a strike', function(){
+          for (let i = 0; i < 20; i += 1) {
+            game.play(10)
+          };
+          expect(game.isExtraRound()).toEqual(true)
+        });
+
+        it('returns true for a spare', function(){
+          for (let i = 0; i < 19; i += 1) {
+            game.play(4)
+          };
+          game.play(6);
+          expect(game.isExtraRound()).toEqual(true)
+        });
+
+        it('returns false for neither', function(){
+          for (let i = 0; i < 20; i += 1) {
+            game.play(4)
+          };
+          expect(game.isExtraRound()).toEqual(false)
         });
     });
 });
--- src/Game.js
+++ src/Game.js
@@ -5,11 +5,25 @@ class Game {
       this.roll = 1;
       this.knocked = 0;
       this.score = [];
+      this.isEnded = false;
     }
 
     play(pins){
-      if(this.frame >= 11){
-        return this.end()
+      if(this.isEnded){
+        return this.end();
+      }
+      if(this.frame === 10 && this.roll === 3){
+        this.addScore(pins)
+        return this.end();
+      }
+      if(this.frame === 10 && this.roll === 2){
+        this.addScore(pins)
+        if(this.isExtraRound()){
+          this.roll ++;
+          return;
+        } else {
+          return this.end();
+        }
       }
       this.addScore(pins)
       if(this.roll === 2){
@@ -19,12 +33,28 @@ class Game {
         this.roll ++;
       }
     }
+
     end(){
+      this.isEnded = true;
       return "The game has ended.";
     }
 
     addScore(pins){
       this.knocked = pins
       this.score.push({frame: this.frame, roll: this.roll, knocked: this.knocked})
+    }
+
+    isExtraRound(){
+      var finalPins = this.score[this.score.length - 1].knocked;
+      var preceedingPins = this.score[this.score.length - 2].knocked;
+      /// checks for spare
+      if(finalPins + preceedingPins === 10){
+        return true;
+      }
+      /// checks for strike in preceeding roll (frame: 10, roll: 1)
+      if(preceedingPins === 10){
+        return true;
+      }
+      return false;
     }
 }

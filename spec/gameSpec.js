770969413d71c1f058ad273f95de2b478cabd7eb
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-16 20:05:55 +0100

    Adds tests for 0, 2 and 4 rolls

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -12,18 +12,40 @@ describe('Game', function() {
     expect(game.totalScore).toEqual(0)
   });
 
-  it ('can add 5 to the total score', function() {
-    game.addScore(5);
-    expect(game.totalScore).toEqual(5)
-  });
-
 // only calculate score at end
   it ('gives a total score of 20 when just rolling 1s', function() {
     for (let i = 0; i < 9; i ++) {game.addFrame([1, 1])};
     game.addLastFrame([1, 1]);
-    expect(game.frames).toEqual([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
-    game.calculateTotalScore()
-    expect(game.totalScore).toEqual(20)
+    expect(game.frames).toEqual([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]);
+
+    game.calculateTotalScore();
+    expect(game.totalScore).toEqual(20);
+  });
+
+
+  it ('gives a total score of 0 when had a gutter game', function() {
+    for (let i = 0; i < 9; i ++) {game.addFrame([0, 0])};
+    game.addLastFrame([0, 0]);
+
+    game.calculateTotalScore();
+    expect(game.totalScore).toEqual(0);
+  });
+
+
+  it ('gives a total score of 40 when just rolling 2s', function() {
+    for (let i = 0; i < 9; i ++) {game.addFrame([2, 2])};
+    game.addLastFrame([2, 2]);
+
+    game.calculateTotalScore();
+    expect(game.totalScore).toEqual(40);
+  });
+
+  it ('gives a total score of 80 when just rolling 4s', function() {
+    for (let i = 0; i < 9; i ++) {game.addFrame([4, 4])};
+    game.addLastFrame([4, 4]);
+
+    game.calculateTotalScore();
+    expect(game.totalScore).toEqual(80);
   });
 
 });
--- src/game.js
+++ src/game.js
@@ -7,27 +7,16 @@ class Game {
     this.frames = [];
   }
 
-  addScore(number) {
-    this.totalScore += number;
-  }
-
-
   addFrame(frame){
     this.frames.push(frame)
-    // this.calculateTotalScore()
   }
 
   addLastFrame(frame){
     this.frames.push(frame)
-    // this.calculateTotalScore()
   }
 
   calculateTotalScore() {
     var total = 0
-    // console.log("This frames")
-    // console.log(this.frames)
-    // console.log("This totalscore")
-    // console.log(this.totalScore)
 
     this.frames.forEach(function (value, index, array) {
       total += value.reduce(function(a, b){

79ee874f884806042959b199b7d227b5057ae7f9
mbrad26 <mbrad26@gmail.com>
2020-05-16 16:20:32 +0100

    added test for a gutter frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -9,10 +9,18 @@ describe('Frame', () => {
   });
 
   describe('total', () => {
-    it('calculates a frame score', () => {
-      frame.rolls = [3, 5];
+    describe('when a frame is not a spare or strike', () => {
+      it('calculates a frame score', () => {
+        frame.rolls = [3, 5];
 
-      expect(frame.total()).toEqual(8);
+        expect(frame.total()).toEqual(8);
+      });
+
+      it('calculates a gutter frame score', () => {
+        frame.rolls = [0, 0];
+
+        expect(frame.total()).toEqual(0);
+      });
     });
 
     describe('when a frame ends with spare', () => {

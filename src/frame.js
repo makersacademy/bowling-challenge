69aa6831869327bb14896ae8ea52d01156dfbaa7
mbrad26 <mbrad26@gmail.com>
2020-05-16 15:48:53 +0100

    implemented the logic for Frame.total()

--- src/frame.js
+++ src/frame.js
@@ -1,11 +1,15 @@
 'use strict'
 
 class Frame {
-  // constructor() {
-  //   this.rolls = [];
-  // }
+  constructor() {
+    this.rolls = [];
+  }
 
   total() {
-    return 8;
+    let roll1 = this.rolls[0];
+    let roll2 = this.rolls[1];
+
+    let sum = this.rolls.reduce((roll1, roll2) => roll1 + roll2, 0);
+    return sum;
   }
 }

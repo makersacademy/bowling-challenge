38d0a13b916ad4d5edf80cca0042df1c270c84bd
Ed <lookatemail@gmail.com>
2020-05-16 09:49:13 +0100

    get score when in position 1 frame 6

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,9 +1,7 @@
 describe('Bowling', function() {
-    // method statement
     var bowling;
     beforeEach(function() {
       bowling = new Bowling();
-      bowling.totalScore === 0;
     });
   
     it('score at the start of the game is 0', () => {
@@ -14,10 +12,9 @@ describe('Bowling', function() {
     describe('Frame score without strikes', function() {
       it('when in position 1 and frame 1', () => {
         bowling.countScore(4);
-        expect(bowling.getTotalScore()).toEqual(4);
+        expect(bowling.frameScore).toEqual(4);
         expect(bowling.frame).toEqual(1);
         expect(bowling.firstBowl).toBe(false);
-        expect(bowling.getTotalScore()).toEqual(4);
       });
       it('when in position 2 and frame 1', () => {
         bowling.countScore(5);
@@ -31,7 +28,7 @@ describe('Bowling', function() {
         bowling.countScore(4);
         bowling.countScore(4);
         bowling.countScore(4);
-        expect(bowling.getTotalScore()).toEqual(12);
+        expect(bowling.getTotalScore() + bowling.frameScore).toEqual(12);
         expect(bowling.firstBowl).toBe(false);
         expect(bowling.frame).toEqual(2);
       });
@@ -45,8 +42,19 @@ describe('Bowling', function() {
         expect(bowling.firstBowl).toBe(true);
         expect(bowling.frame).toEqual(2);
       });
-      it('able to count score when in position 2', () => {
-
+      it('when in position 1 frame 2', () => {
+        bowling.countScore(10);
+        bowling.countScore(4);
+        bowling.countScore(0);
+        expect(bowling.getTotalScore()).toEqual(18);
+        expect(bowling.firstBowl).toBe(true);
+        expect(bowling.frame).toEqual(3);
+      });
+      it('when in position 1 frame 6', () => {
+        for(let i = 0; i < 6; i++) {
+          bowling.countScore(10);
+        }
+        expect(bowling.getTotalScore()).toEqual(150);
       });
     });
 
--- src/bowling.js
+++ src/bowling.js
@@ -4,32 +4,121 @@ class Bowling {
     this.MAX_FRAME_SCORE = 10;
     this.firstBowl = true;
     this.frame = 1;
-    this.frameScore = [];
+    this.frameScore;
+    this.gameScore = [];
   }
 
   countScore(score) {
-    if(score === this.MAX_FRAME_SCORE) {
-      this.frame += 1;
-      this.firstBowl = true;
-      this.frameScore.push(score, 0)
+    if(score === this.MAX_FRAME_SCORE && this.firstBowl === true) {
+      this._countHelperWhenStrike(score);
+    } else if(this.frameScore + score === 10) {
+      this._countHelperWhenSpare(score);
     } else {
-      this._countHelper();
-      this.frameScore.push(score);
-      // console.log(this.frameScore)
+      this._countHelper(score);
     }
   }
   getTotalScore() {
-    return this.frameScore.reduce((a, b) => a + b, 0)
+    let gameScore = this.gameScore;
+    let score_modified = [];
+    gameScore.forEach(function(item, index) {
+      if(item %2 === 0 && item === 10) {
+        let indexPlusOne = gameScore[index + 1] === undefined ? 0 : gameScore[index + 1]
+        let indexPlusTwo = gameScore[index + 2] === undefined ? 0 : gameScore[index + 2]
+        let score = gameScore[index + 1] === 10 ? indexPlusOne + indexPlusTwo : indexPlusOne
+        score_modified.push(gameScore[index] + score)
+      } else {
+        score_modified.push(item)
+      }
+    })
+    return score_modified.reduce((a, b) => a + b, 0)
+  }
+
+  getFrameScore() {
+    console.log(this.frameScore)
+    name = this.frameScore;
+    return this.frameScore.shift;
   }
   
-  _countHelper() {
+  _countHelper(score) {
     if(this.firstBowl === false) {
       this.firstBowl = true;
       this.frame += 1;
+      this.gameScore.push(this.frameScore + score);
+      this.frameScore = 0;
     } else {
       this.firstBowl = false;
+      this.frameScore = score;
     }
-    
+  }
+
+  _countHelperWhenSpare() {
+    let score = gameScore;
+    let score_modified = [];
+    score.forEach(function(item, index) {
+      if((score[index] + score[index + 1]) === 10) {
+        score_modified.push(score[index + 1] + score[index + 2])
+      } else {
+        score_modified.push(item)
+      }
+    })
+  }
+  
+  _countHelperWhenStrike(score) {
+    this.frame += 1;
+    this.firstBowl = true;
+    this.gameScore.push(score)
   }
 
 }
+
+
+
+// let score = [2, 3, 10, 0, 10, 0, 2];
+// let score_modified = [];
+// score.forEach(function(item, index) {
+//   if(item %2 === 0 && item === 10) {
+//     console.log(score[index])
+//     let number = score[index + 1] === 10 ? score[index] + score[index + 1] + score[index + 2] : score[index + 1]
+//     score_modified.push(score[index] + number)
+//   } else {
+//     score_modified.push(item)
+//   }
+// })
+
+// console.log(score_modified)
+
+// let score = [2, 8, 10, 2, 2];
+// let score_modified = [];
+// score.forEach(function(item, index) {
+//   if((score[index] + score[index + 1]) === 10) {
+//     console.log(score[index + 1] + score[index + 2])
+//     // console.log(item[index] + item[index + 1])
+//     score_modified.push(score[index] + score[index + 1])
+//   } else {
+//     score_modified.push(item)
+//   }
+// })
+
+// console.log(score_modified)
+
+let firstBowl = true;
+let frame = 0;
+let gameScore = [10, 10, 10, 10, 10, 10];
+let frameScore = 0;
+
+let score_modified = [];
+gameScore.forEach(function(item, index) {
+  if(item %2 === 0 && item === 10) {
+    let indexPlusOne = gameScore[index + 1] === undefined ? 0 : gameScore[index + 1]
+    let indexPlusTwo = gameScore[index + 2] === undefined ? 0 : gameScore[index + 2]
+    let score = gameScore[index + 1] === 10 ? indexPlusOne + indexPlusTwo : indexPlusOne
+    score_modified.push(gameScore[index] + score)
+  } else {
+    score_modified.push(item)
+  }
+})
+let total = score_modified.reduce((a, b) => a + b, 0)
+
+
+console.log(score_modified)
+console.log(total)

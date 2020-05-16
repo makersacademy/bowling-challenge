0755029df33fe557ba02c2fd95c4ece76303aa64
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-16 19:57:17 +0100

    Adds score when only input 1s

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,13 +9,21 @@ describe('Game', function() {
   });
 
   it ('score starts at 0', function() {
-    expect(game.totalscore).toEqual(0)
+    expect(game.totalScore).toEqual(0)
   });
 
   it ('can add 5 to the total score', function() {
-    game.addscore(5);
-    expect(game.totalscore).toEqual(5)
+    game.addScore(5);
+    expect(game.totalScore).toEqual(5)
   });
 
+// only calculate score at end
+  it ('gives a total score of 20 when just rolling 1s', function() {
+    for (let i = 0; i < 9; i ++) {game.addFrame([1, 1])};
+    game.addLastFrame([1, 1]);
+    expect(game.frames).toEqual([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
+    game.calculateTotalScore()
+    expect(game.totalScore).toEqual(20)
+  });
 
 });
--- src/game.js
+++ src/game.js
@@ -3,11 +3,39 @@
 class Game {
 
   constructor() {
-    this.totalscore = 0;
+    this.totalScore = 0;
+    this.frames = [];
   }
 
-  addscore(number) {
-    this.totalscore += number;
+  addScore(number) {
+    this.totalScore += number;
+  }
+
+
+  addFrame(frame){
+    this.frames.push(frame)
+    // this.calculateTotalScore()
+  }
+
+  addLastFrame(frame){
+    this.frames.push(frame)
+    // this.calculateTotalScore()
+  }
+
+  calculateTotalScore() {
+    var total = 0
+    // console.log("This frames")
+    // console.log(this.frames)
+    // console.log("This totalscore")
+    // console.log(this.totalScore)
+
+    this.frames.forEach(function (value, index, array) {
+      total += value.reduce(function(a, b){
+        return a + b;
+      }, 0);
+    });
+
+    this.totalScore = total
   }
 
 }

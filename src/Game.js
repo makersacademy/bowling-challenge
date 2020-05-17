4a6023ac115987d33a996efdb2187ab76bcd0b65
joannacollier <collj035@gmail.com>
2020-05-17 12:10:09 +0100

    Refactor for max pins to have this.

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,12 @@ class Game {
   constructor(){
     this.rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.currentRoll = 0;
+    this.maxPins = 10;
     //this.pinErr = new Error('Please choose number between 1 to 10.')
   }
 
   roll(pins) {
-    if(pins > 10){
+    if(pins > this.maxPins){
       return 'Please choose number between 1 to 10.'
     }
     this.rolls[this.currentRoll] = pins

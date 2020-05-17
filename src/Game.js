cf5f78e1dcfdbae6fc0959d29ea234e5fac3e6b4
joannacollier <collj035@gmail.com>
2020-05-17 12:48:09 +0100

    max array length using this.

--- src/Game.js
+++ src/Game.js
@@ -4,14 +4,13 @@ class Game {
     this.currentRoll = 0;
     this.maxPins = 10;
     this.maxLength = 21;
-    //this.pinErr = new Error('Please choose number between 1 to 10.')
   }
 
   roll(pins) {
     if(pins > this.maxPins){
       return 'Please choose number between 1 to 10.';
     }
-    else if(this.rolls.length === 21 && !this.rolls.includes(0)){
+    else if(this.rolls.length === this.maxLength && !this.rolls.includes(0)){
       return;
     }
     this.rolls[this.currentRoll] = pins

512d332cb8786cc552574312267a82e4abf9d7c2
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-05-17 14:51:28 +0100

    Added calculate function

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -1,15 +1,22 @@
 describe('ScoreCard', function () {
     'use strict';
     var scorecard;
-    var game;
     beforeEach(function () {
         scorecard = new ScoreCard();
-        game = new Game();
     })
     describe('addScore', function(){
         it('adds score from round to array as hash', function () {
             scorecard.addScore(1, 1, 4);
             expect(scorecard.scoreboard).toEqual([{frame: 1, roll: 1, knocked: 4}]);
+        });
+    });
+    describe('calculate', function(){
+        it('calculates the total', function(){
+            for (let i = 0; i < 10; i++) {
+                scorecard.addScore(1, 1, 4);
+              }
+            scorecard.calculate()
+            expect(scorecard.total).toEqual(40)
         });
     });
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -6,11 +6,42 @@ class ScoreCard{
     }
 
     addScore(frame, roll, knocked){
-      if(knocked === 10 && frame !== 10){
+      if(knocked === 10 && roll !== 3){
         this.scoreboard.push({frame: frame, roll: roll, knocked: knocked})
         this.scoreboard.push({frame: frame, roll: 2, knocked: 0})
       } else {
         this.scoreboard.push({frame: frame, roll: roll, knocked: knocked})
       }
+    }
+
+    calculate(){
+      for(let i = 0; i < this.scoreboard.length; i++) {
+        this.total += this.scoreboard[i].knocked;
+      }
+
+      // add bonus for strike
+      for(let i = 0; i < this.scoreboard.length; i++) {
+        if(this.scoreboard[i].roll === 3){
+          return;
+        }
+        if(this.scoreboard[i].knocked === 10){
+          if(this.scoreboard[i].frame !== 10){
+            this.total += (this.scoreboard[i + 2].knocked + this.scoreboard[i + 3].knocked);
+          }else{
+            this.total += (this.scoreboard[i + 2].knocked);
+          }
+        }
+      }
+
+      // add bonus for spare
+      for(let i = 0; i < (this.scoreboard.length - 1); i += 2) {
+        if((this.scoreboard[i].knocked + this.scoreboard[i+1].knocked) === 10
+        && this.scoreboard[i].knocked !== 10 && this.scoreboard[i+1].knocked !== 10){
+          if(this.scoreboard[i + 2].roll !== 3){
+            this.total += this.scoreboard[i + 2].knocked;
+          }
+        }
+      }
+      console.log(this.total)
     }
 }

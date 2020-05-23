79205fb9a59f1bbeb9e72acb07b88d888079a5e1
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-23 16:08:37 +0100

    Restarted bowling challenge

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -8,83 +8,14 @@ describe('Game', function() {
     game = new Game();
   });
 
-  it ('score starts at 0', function() {
-    expect(game.calculateCurrentScore()).toEqual(0)
-  });
-
-// only calculate score at end
-  it ('gives a total score of 20 when just rolling 1s', function() {
-    for (let i = 0; i < 9; i ++) {game.addFrame([1, 1])};
-    game.addLastFrame([1, 1]);
-    expect(game.frames).toEqual([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]]);
-
-    expect(game.calculateCurrentScore()).toEqual(20);
-  });
-
-
-  it ('gives a total score of 0 when had a gutter game', function() {
-    for (let i = 0; i < 9; i ++) {game.addFrame([0, 0])};
-    game.addLastFrame([0, 0]);
-
+  it('score starts at 0 points', function () {
     expect(game.calculateCurrentScore()).toEqual(0);
   });
 
-
-  it ('gives a total score of 40 when just rolling 2s', function() {
-    for (let i = 0; i < 9; i ++) {game.addFrame([2, 2])};
-    game.addLastFrame([2, 2]);
-
-    expect(game.calculateCurrentScore()).toEqual(40);
-  });
-
-  it ('gives a total score of 80 when just rolling 4s', function() {
-    for (let i = 0; i < 9; i ++) {game.addFrame([4, 4])};
-    game.addLastFrame([4, 4]);
-
-    expect(game.calculateCurrentScore()).toEqual(80);
-  });
-
-
-  it ('gives a total score of 150 when just rolling 5s', function() {
-    for (let i = 0; i < 9; i ++) {game.addFrame([5, 5])};
-    game.addLastFrame([5, 5, 5]);
-
-    expect(game.calculateCurrentScore()).toEqual(150);
-  });
-
-
-  it ('gives a total score of 300 when just rolling 10s', function() {
-    for (let i = 0; i < 9; i ++) {game.addFrame([10, 0])};
-    game.addLastFrame([10, 10, 10]);
-
-    expect(game.calculateCurrentScore()).toEqual(300);
-  });
-
-
-  it ('gives a total score of 114 when rolling 5s then 4s', function() {
-    for (let i = 0; i < 5; i ++) {game.addFrame([5, 5])};
-    for (let i = 5; i < 9; i ++) {game.addFrame([4, 4])};
-    game.addLastFrame([4, 4]);
-
-    expect(game.calculateCurrentScore()).toEqual(114);
-  });
-
-  it ('gives a total score of 159 when rolling 10s then 3s', function() {
-    for (let i = 0; i < 5; i ++) {game.addFrame([10, 0])};
-    for (let i = 5; i < 9; i ++) {game.addFrame([3, 3])};
-    game.addLastFrame([3, 3]);
-
-    expect(game.calculateCurrentScore()).toEqual(159);
-  });
-
-
-  it ('gives a total score of 123 when rolling 5s, 10s, then 3s', function() {
-    for (let i = 0; i < 3; i ++) {game.addFrame([5, 5])};
-    for (let i = 3; i < 6; i ++) {game.addFrame([10, 0])};
-    for (let i = 6; i < 9; i ++) {game.addFrame([3, 3])};
-    game.addLastFrame([3, 3]);
-
-    expect(game.calculateCurrentScore()).toEqual(143);
-  });
+  // it('returns a final score of 0 when rolling a gutter game', function() {
+  //
+  //   game.addFrame(0, 0)
+  //   game.addLastFrame(0, 0)
+  // });
 
 });
--- src/game.js
+++ src/game.js
@@ -2,67 +2,8 @@
 
 class Game {
 
-  constructor() {
-    this.totalScore = 0;
-    this.frames = [];
+  calculateCurrentScore(){
+    return 0
   }
-
-  addFrame(frame){
-    // frame = new Frame(frame);
-    this.frames.push(frame)
-  }
-
-  addLastFrame(frame){
-    this.frames.push(frame)
-  }
-
-
-  calculateCurrentScore() {
-    var total = 0
-
-    this.frames.forEach(function (value, index, array) {
-
-      if (isStrike(value)) {
-
-        // 8 and 9 to 0 and 1
-        if (index !== 8 && index !== 9) {
-          if (array[index + 1][0] === 10) {
-            total += value[0] + array[index + 1][0] + array[index + 2][0];
-          } else {
-            total += value[0] + array[index + 1][0] + array[index + 1][1];
-          }
-
-        } else if (index === 8){
-          total += value[0] + array[index + 1][0] + array[index + 1][1];
-
-        } else {
-          total += value[0] + value[1] + value[2];
-        }
-
-
-      } else if (isSpare(value)) {
-
-        if (index !== 9){
-          total += value[0] + value[1] + array[index + 1][0]
-        } else {
-          total += value[0] + value[1] + value[2]
-        }
-
-
-      } else {
-        total += value[0] + value[1];
-      }
-    });
-
-    this.totalScore = total
-    return total
-  }
+  
 }
-
-function isStrike(value){
-  return value[0] === 10;
-};
-
-function isSpare(value){
-  return value[0] + value[1] === 10 && value[0] !== 10;
-};

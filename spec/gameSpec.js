47c64c074e120e063bd00a15a161a21f07f153f4
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-23 22:10:04 +0100

    Refactored calculateCurrentScore function

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -12,69 +12,72 @@ describe('Game', function() {
     expect(game.calculateCurrentScore()).toEqual(0);
   });
 
-  it('returns a final score of 0 when rolling a gutter game', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([0, 0]);
-    }
-    game.addLastFrame([0, 0]);
+  describe('Game has finished', function(){
+    it('returns a final score of 0 when rolling a gutter game', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([0, 0]);
+      }
+      game.addLastFrame([0, 0]);
 
-    expect(game.frames.length).toEqual(10);
-    expect(game.calculateCurrentScore()).toEqual(0);
+      expect(game.frames.length).toEqual(10);
+      expect(game.calculateCurrentScore()).toEqual(0);
+    });
+
+    it('returns a final score of 20 when rolling all 1s', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([1, 1]);
+      }
+      game.addLastFrame([1, 1]);
+
+      expect(game.calculateCurrentScore()).toEqual(20);
+    });
+
+    it('returns a final score of 40 when rolling all 2s', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([2, 2]);
+      }
+      game.addLastFrame([2, 2]);
+
+      expect(game.calculateCurrentScore()).toEqual(40);
+    });
+
+    it('returns a final score of 60 when rolling all 3s', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([3, 3]);
+      }
+      game.addLastFrame([3, 3]);
+
+      expect(game.calculateCurrentScore()).toEqual(60);
+    });
+
+    it('returns a final score of 80 when rolling all 4s', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([4, 4]);
+      }
+      game.addLastFrame([4, 4]);
+
+      expect(game.calculateCurrentScore()).toEqual(80);
+    });
+
+    it('returns a final score of 150 when rolling all 5s', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([5, 5]);
+      }
+      game.addLastFrame([5, 5, 5]);
+
+      expect(game.calculateCurrentScore()).toEqual(150);
+    });
+
+    it('returns a final score of 300 when rolling a perfect game', function() {
+      for (let i = 1; i <= 9; i += 1) {
+        game.addFrame([10, 0]);
+      }
+      game.addLastFrame([10, 10, 10]);
+
+      expect(game.calculateCurrentScore()).toEqual(300);
+    });
   });
 
-  it('returns a final score of 20 when rolling all 1s', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([1, 1]);
-    }
-    game.addLastFrame([1, 1]);
-
-    expect(game.calculateCurrentScore()).toEqual(20);
-  });
-
-  it('returns a final score of 40 when rolling all 2s', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([2, 2]);
-    }
-    game.addLastFrame([2, 2]);
-
-    expect(game.calculateCurrentScore()).toEqual(40);
-  });
-
-  it('returns a final score of 60 when rolling all 3s', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([3, 3]);
-    }
-    game.addLastFrame([3, 3]);
-
-    expect(game.calculateCurrentScore()).toEqual(60);
-  });
-
-  it('returns a final score of 80 when rolling all 4s', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([4, 4]);
-    }
-    game.addLastFrame([4, 4]);
-
-    expect(game.calculateCurrentScore()).toEqual(80);
-  });
-
-  it('returns a final score of 150 when rolling all 5s', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([5, 5]);
-    }
-    game.addLastFrame([5, 5, 5]);
-
-    expect(game.calculateCurrentScore()).toEqual(150);
-  });
-
-  it('returns a final score of 300 when rolling a perfect game', function() {
-    for (let i = 1; i <= 9; i += 1) {
-      game.addFrame([10, 0]);
-    }
-    game.addLastFrame([10, 10, 10]);
-
-    expect(game.calculateCurrentScore()).toEqual(300);
-  });
 
 
   describe('Game has not finished', function(){
--- src/game.js
+++ src/game.js
@@ -11,48 +11,28 @@ class Game {
 
     this.frames.forEach(function(frame, index, array){
 
-      // if game has ended
-      if (array.length === 10) {
-
         // last frame
         if (frame.length === 3){
           total += frame[0] + frame[1] + frame[2]
 
-        } else if (isStrike(frame) && isStrike(array[index + 1]) && index < 8) {
-          total += frame[0] + array[index + 1][0] + array[index + 2][0]
-        } else if (isStrike(frame)) {
-          total += frame[0] + array[index + 1][0] + array[index + 1][1]
-
-        } else if (isSpare(frame)){
-          total += frame[0] + frame[1] + array[index + 1][0]
-        } else {
-          total += frame[0] + frame[1]
-        }
-
         // if game hasn't finished yet
-      } else {
-
-        if (index === array.length - 1 && isSpare(frame)){
+        } else if (array.length !== 10 && index === array.length - 1 && isSpare(frame)){
+          total = total
+        } else if (array.length !== 10 && index === array.length - 1 && isStrike(frame)) {
+          total = total
+        } else if (array.length !== 10 && index === array.length - 2 && isStrike(frame) && isStrike(array[index + 1])) {
           total = total
 
-
-        } else if (index === array.length - 1 && isStrike(frame)) {
-          total = total
-        } else if (index === array.length - 2 && isStrike(frame) && isStrike(array[index + 1])) {
-          total = total
         } else if (isStrike(frame) && isStrike(array[index + 1]) && index < 8) {
           total += frame[0] + array[index + 1][0] + array[index + 2][0]
         } else if (isStrike(frame)) {
           total += frame[0] + array[index + 1][0] + array[index + 1][1]
-
 
         } else if (isSpare(frame)){
           total += frame[0] + frame[1] + array[index + 1][0]
         } else {
           total += frame[0] + frame[1]
         }
-
-      }
 
     });
 

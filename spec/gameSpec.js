77bf0a8b1b7109442103bc587129f8749fa6e19c
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-23 16:18:07 +0100

    Calculates total score for a gutter game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -12,10 +12,14 @@ describe('Game', function() {
     expect(game.calculateCurrentScore()).toEqual(0);
   });
 
-  // it('returns a final score of 0 when rolling a gutter game', function() {
-  //
-  //   game.addFrame(0, 0)
-  //   game.addLastFrame(0, 0)
-  // });
+  it('returns a final score of 0 when rolling a gutter game', function() {
+    for (let i = 1; i <= 9; i += 1) {
+      game.addFrame([0, 0]);
+    }
+    game.addLastFrame([0, 0]);
+
+    expect(game.frames.length).toEqual(10);
+    expect(game.calculateCurrentScore()).toEqual(0);
+  });
 
 });
--- src/game.js
+++ src/game.js
@@ -2,8 +2,20 @@
 
 class Game {
 
+  constructor(){
+    this.frames = []
+  }
+
   calculateCurrentScore(){
     return 0
   }
-  
+
+  addFrame(frame){
+    this.frames.push(frame)
+  }
+
+  addLastFrame(frame){
+    this.frames.push(frame)
+  }
+
 }

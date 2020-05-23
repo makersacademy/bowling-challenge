4f7103517baf2804941bf29135ae62615e165dd3
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-23 12:52:11 +0100

    Test and implements player.score, player.add and player.subtract

--- src/player.js
+++ src/player.js
@@ -4,6 +4,7 @@ class Player {
 
   constructor(name) {
     this._name = name;
+    this._score = 0;
   }
 
   get name() {
@@ -12,5 +13,21 @@ class Player {
 
   set name(value) {
     this._name = value;
+  }
+
+  get score() {
+    return this._score;
+  }
+
+  set score(value) {
+    this._score = value;
+  }
+
+  add(value) {
+    this._score += value;
+  }
+
+  subtract(value) {
+    this._score -= value;
   }
 }

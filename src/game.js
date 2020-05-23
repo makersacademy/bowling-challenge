603d2a8e839726e3cf0421a6a66aed080583183c
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-23 22:25:37 +0100

    Extracts isGameUnfinished function

--- src/game.js
+++ src/game.js
@@ -15,16 +15,14 @@ class Game {
 
     this.frames.forEach(function(frame, index, array){
 
-        // last frame
         if (frame.length === 3){
           total += frame[0] + frame[1] + frame[2]
 
-        // if game hasn't finished yet
-        } else if (array.length !== 10 && index === array.length - 1 && isSpare(frame)){
+        } else if (isGameUnfinished(array) && index === array.length - 1 && isSpare(frame)){
           total = total
-        } else if (array.length !== 10 && index === array.length - 1 && isStrike(frame)) {
+        } else if (isGameUnfinished(array) && index === array.length - 1 && isStrike(frame)) {
           total = total
-        } else if (array.length !== 10 && index === array.length - 2 && isStrike(frame) && isStrike(array[index + 1])) {
+        } else if (isGameUnfinished(array) && index === array.length - 2 && isStrike(frame) && isStrike(array[index + 1])) {
           total = total
 
         } else if (isStrike(frame) && isStrike(array[index + 1]) && index < 8) {
@@ -49,4 +47,8 @@ function isSpare(frame){
 
 function isStrike(frame){
   return frame[0] === 10
+}
+
+function isGameUnfinished(array){
+  return array.length !== 10
 }

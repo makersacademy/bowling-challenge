b171c51f6c4ca777b6f661be21002d70e9923b4a
joannacollier <collj035@gmail.com>
2020-05-24 09:18:06 +0100

    10 frame bonus scoring

--- spec/scoreSpec.js
+++ spec/scoreSpec.js
@@ -41,5 +41,43 @@ describe('Score', () => {
 
       expect(score.scoring(game)).toEqual(15)
     });
+
+    it("Has 10th frame bonus for strike",() => {
+      for (var i = 0; i < 22; i++) {
+        game.roll(10);
+      }
+
+      expect(score.scoring(game)).toEqual(300);
+    });
+
+    it("Has 10th frame bonus for spare",() => {
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(9);
+      game.roll(1);
+      game.roll(1);
+
+      expect(score.scoring(game)).toEqual(270);
+    });
+
+    it("Only scores 20 frames if no 10 frame bonus",() => {
+      for (var i = 0; i < 22; i++) {
+        game.roll(1);
+      }
+      console.log("VVV")
+      console.log(game.rolls)
+      console.log("^^^")
+
+      expect(score.scoring(game)).toEqual(20);
+    });
+
+
   });
 });

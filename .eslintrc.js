fa85b584b44a0e98e2523e9bc9f1e6f913325079
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-24 12:15:58 +0100

    Changes ESLint to use babel-eslint parser to fix static = error

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,9 @@ module.exports = {
     'Atomics': 'readonly',
     'SharedArrayBuffer': 'readonly'
   },
+  'parser': 'babel-eslint',
   'parserOptions': {
-    'parser': 'babel-eslint',
-    'ecmaVersion': 2018,
+    'ecmaVersion': 11,
     'sourceType': 'module'
   },
   'rules': {

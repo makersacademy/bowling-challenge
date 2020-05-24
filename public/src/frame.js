3c393112c4f9b63844c7f8a15f997f79dc836efa
tanilserbes <serbestanil@gmail.com>
2020-05-24 18:49:33 +0100

    some updates

--- public/src/frame.js
+++ public/src/frame.js
@@ -4,17 +4,27 @@ class Frame {
     this.pins = 10;
     this.firstroll = 0;
     this.secondroll = 0;
-  }
+    this.rollcount = 0;
+    this.score = 0;
+  };
 
-   receiveShot(knocked_down_pin_number) {
+  receiveShot(knocked_down_pin_number) {
     this.pins = this.pins - knocked_down_pin_number;
-    if (this.pins < 0) {throw new Error('Not allowed more than ten balls');}
+    this.rollcount ++;
+    if ( this.rollcount > 2 ) {throw new Error('Only two rolls'); }
+    if ( this.pins < 0 ) {throw new Error('Not allowed more than ten balls'); }
     this.firstroll === 0 ? this.firstroll = knocked_down_pin_number : this.secondroll = knocked_down_pin_number; 
-  }
+    this.score += knocked_down_pin_number;
+  };
 
-  _strike(){
-    true
-  }
-  
+  isStrike(){
+    if( this.firstroll === 10 )  { return true }
+    else {return false}
+  };
+
+  isSpare(){
+    if ( this.firstroll + this.secondroll === 10 && this.secondroll !==0 ) { return true }
+    else { return false }
+  };
 
 };
---
+++ public/src/game.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class Game {
+
+  constructor() {
+    this.frames = [];
+  };
+ 
+
+create = function(element) {
+  if (this.frameCount() === 10) {throw new Error ("No more frames available, create a new game to play again."); }
+  for (var i = 0; i < 10; i++) { this.frames.push(new element); }
+  //  this.frames.push(new FrameTen());
+};
+
+frameCount = function() { 
+  return this.frames.length;
+};
+
+score = function () {
+  var total = 0;
+  for (var i = 0; i < this.frames.length; i++) { total += this.frames[i].score; }
+  return total;
+};
+
+
+evaluateScores = function() {
+  for (var i = 0; i < 9; i++) { 
+    this._evaluateSpare(i);
+    this._evaluateStrike(i);  
+  }
+};
+
+_evaluateSpare = function(i) {
+   if (this.frames[i].isSpare()) { this.frames[i].score += this._firstExtraRoll(i); }
+};
+
+_evaluateStrike = function(i) {
+   if (this.frames[i].isStrike()) { this.frames[i].score += (this._firstExtraRoll(i) + this._secondExtraRoll(i)); }
+};
+
+_firstExtraRoll = function(i) {
+  return this.frames[i+1].firstroll;
+};
+
+_secondExtraRoll = function(i) {
+  return this.frames[i+1].secondroll|| this.frames[i+2].firstroll;
+};
+
+};

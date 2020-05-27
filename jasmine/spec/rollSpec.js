1d637d49e0c37323522c0e7f448e740dd0e43f14
LTurns <lizzieturney@gmail.com>
2020-05-27 10:59:45 +0100

    more on jasmine

--- jasmine/spec/rollSpec.js
+++ jasmine/spec/rollSpec.js
@@ -5,6 +5,7 @@ describe('Roll', function() {
 
   beforeEach(function(){
     roll = new Roll();
+    var score = [1, 2];
   });
 
   it('pins knocked down starts at 0', function() {
@@ -16,22 +17,39 @@ describe('Roll', function() {
     expect(roll.getCurrentPins()).toEqual(1);
   });
 
+  it('increases the score with up', function() {
+    roll.up();
+    roll.up();
+    roll.up();
+    roll.up();
+    expect(roll.getCurrentPins()).toEqual(4);
+  });
+
   it('decreases the score with down', function() {
     roll.up();
     roll.down();
     expect(roll.getCurrentPins()).toEqual(0);
   });
 
+  it('decreases the score with down', function() {
+    roll.up();
+    roll.up();
+    roll.up();
+    roll.down();
+    expect(roll.getCurrentPins()).toEqual(2);
+  });
+
+
   it('shows when there is a strike', function() {
     expect(roll.showStrike()).toEqual(10);
   });
 
-  it('can be reset to zero for a new game', function() {
+  it('resets the score to zero', function(){
     for (var i = 0; i < 6; i++) {
-      roll.up();
-    }
-    roll.reset();
-    expect(roll.getCurrentPins()).toEqual(0);
+    roll.up()
+  }
+   score = [1, 2]
+    expect(roll.reset(roll, score)).toEqual(0);
   });
 
 });

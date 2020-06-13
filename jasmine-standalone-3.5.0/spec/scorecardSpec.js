c4a78920ec18540845ba674ef95d7047d4a19cc7
Rich Ewin <richardewinlambert@gmail.com>
2020-06-13 16:53:18 +0100

    raises error if user enters +10 points per frame

--- jasmine-standalone-3.5.0/spec/scorecardSpec.js
+++ jasmine-standalone-3.5.0/spec/scorecardSpec.js
@@ -17,14 +17,19 @@ describe('Scorecard', function () {
   });
 
   it('starts with 10 pins', function () {
-    expect(scorecard.pins).toEqual(10)
+    expect(scorecard.pins).toEqual(10);
   });
 });
 
 describe('records roll', function () {
 
-  it('records results of roll', function() {
+  it('records results of roll', function () {
     scorecard.rollResult(1);
-    expect(scorecard.pins).toEqual(9)
+    expect(scorecard.pins).toEqual(9);
+  });
+
+  it('raises error if user enters +10 points per frame', function () {
+    expect(scorecard.rollResult(11)).toBe("Error: Please enter a result from 1-10");
+    expect(scorecard.pins).toEqual(scorecard.pins)
   });
 });
--- jasmine-standalone-3.5.0/src/scorecard.js
+++ jasmine-standalone-3.5.0/src/scorecard.js
@@ -7,4 +7,9 @@ function Scorecard() {
 
 Scorecard.prototype.rollResult = function(result) {
   this.pins -= result;
+
+  if(result > 10){
+    this.pins = this.pins - result
+    return "Error: Please enter a result from 1-10";
+  };
 };

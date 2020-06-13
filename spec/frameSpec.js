e441177bb0ddd72e95da49474d59429e169fa3a6
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-13 09:50:03 +0100

    Add first tests for max score, total and is a strike

---
+++ spec/frameSpec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Frame', function () {
+
+  it('has a max score of 10', function () {
+    let frame = new Frame();
+    expect(frame.getMaxScore()).toEqual(10);
+  });
+
+  it('calculates the total of two bowls', function () {
+    let frame = new Frame(5,3);
+    expect(frame.total()).toEqual(8)
+  });
+
+  it('knows if bowl one was a strike', function () {
+    let frame = new Frame(10);
+    expect(frame.isAStrike()).toEqual(true);
+  });
+
+});
---
+++ src/frame.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class Frame {
+  constructor(bowlOne, bowlTwo) {
+    this.MAX_SCORE = 10;
+    this.bowlOne = bowlOne;
+    this.bowlTwo = bowlTwo;
+  }
+
+  getMaxScore() {
+    return this.MAX_SCORE;
+  };
+
+  total() {
+    return this.bowlOne + this.bowlTwo;
+  };
+
+  isAStrike() {
+    if(this.bowlOne === 10); {
+      return true;
+    }
+  }
+
+};

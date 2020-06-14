2b8a3208b3172b58f82e2f4ca59482ede71a0190
Bene Arinci <benedetta.arinci@gmail.com>
2020-06-14 16:02:13 +0100

    added methods firstRoll and secondRoll to assign points to roll1 and roll2

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -3,11 +3,18 @@
 describe('Frame class', function(){
   var frame;
   beforeEach(function(){
-    frame = new Frame ()
+    frame = new Frame ();
   });
   it('when created has 2 properties - roll1,roll2 - equal to null and 2 methods for visualizing them', function(){
     expect(frame.pointsFirstRoll()).toEqual(null);
     expect(frame.pointsSecondRoll()).toEqual(null);
   });
-
+  it('has a method to assign points to the first roll', function(){
+  frame.firstRoll(5);
+  expect(frame.pointsFirstRoll()).toEqual(5);
+  });
+  it('has a method to assign points to the second roll', function(){
+    frame.secondRoll(2);
+    expect(frame.pointsSecondRoll()).toEqual(2);
+  });
 });
--- src/frame.js
+++ src/frame.js
@@ -13,6 +13,12 @@ class Frame {
   pointsSecondRoll(){
     return this._roll2
   }
+  firstRoll(points){
+    this._roll1 = points
+  }
 
-  
+  secondRoll(points){
+    this._roll2 = points
+  }
+
 }

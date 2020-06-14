e646469dba61f9ce44e93f2e5e4938b5ef283a05
J Perrin <perrinjack96@gmail.com>
2020-06-14 15:48:21 +0100

    scorecard calculates current score of frames finished

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,23 +1,31 @@
 'use strict';
 
 describe('Scorecard', function () {
+  var scorecard;
+
+  beforeEach(function () {
+    scorecard = new Scorecard();
+  });
+
   it('stores frame instances within an array', function () {
-    var scorecard = new Scorecard();
     expect(scorecard.frameArray).toEqual([]);
   });
 
-
   it('adds instances of frames to array', function () {
-    var scorecard = new Scorecard();
     var frame = new Frame();
-    scorecard.addFrame(frame)
+    scorecard.addFrame(frame);
     expect(scorecard.frameArray).toContain(frame);
   });
-
-
-
-
-
-
-
+  it('has the current score of completed frames', function () {
+    var frame1 = new Frame();
+    var frame2 = new Frame();
+    frame1.reportBowlOne(4);
+    frame1.reportBowlTwo(5);
+    frame2.reportBowlOne(4);
+    frame2.reportBowlTwo(5);
+    scorecard.addFrame(frame1);
+    scorecard.addFrame(frame2);
+    scorecard.computeScore();
+    expect(scorecard.currentScore).toEqual(18);
+  });
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -3,8 +3,13 @@ class Scorecard {
     this.frame1 = new Frame();
     this.frame2 = new Frame();
     this.frameArray = [];
+    this.currentScore = 0;
   }
   addFrame(frame) {
     this.frameArray.push(frame);
+  }
+
+  computeScore() {
+    this.frameArray.forEach((frame) => (this.currentScore += frame.score()));
   }
 }

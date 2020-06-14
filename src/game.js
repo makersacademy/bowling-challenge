2e0f271a802a5e6283ae447f8355178ba7302939
Bene Arinci <benedetta.arinci@gmail.com>
2020-06-14 23:27:54 +0100

    Game.frames contains multiple frames, up to 10

--- src/game.js
+++ src/game.js
@@ -1,9 +1,21 @@
 class Game {
   constructor(){
-    this.frames = []
+    this.frames = [];
+    this.MAX_FRAMES = 10;
   }
 
   gameFrames(){
-    return this.frames
+    return this.frames;
+  }
+
+  addFrame(frame){
+    if (this._gameOver()){
+      return;
+    }
+    this.frames.push(frame);
+  }
+
+  _gameOver(){
+    return this.gameFrames().length === this.MAX_FRAMES;
   }
 }

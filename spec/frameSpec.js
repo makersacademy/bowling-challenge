49f049e5d2d8f752b3e54c7f3ace3bdc05e4b18d
Rachael <rachaelewins@hotmail.com>
2020-06-14 13:31:30 +0100

    feature, spare, gives correct score

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,8 +1,14 @@
 describe('frame', function() {
-  it( 'should be able to take two numbers and return the total', function() {
+  it('should be able to take two numbers and return the total', function() {
     var frame = new Frame
     frame.roll1(2)
     frame.roll2(5)
     expect(frame.baseScore()).toEqual( 2 + 5 )
+  })
+
+  it('should have its first roll visible', function() {
+    var frame = new Frame
+    frame.roll1(7)
+    expect(frame.roll1result()).toEqual(7)
   })
 })
--- src/frame.js
+++ src/frame.js
@@ -1,14 +1,18 @@
 function Frame() {
 }
 
-Frame.prototype.roll1 = function (num) {
+Frame.prototype.roll1 = function(num) {
   this.roll1 = num
 }
 
-Frame.prototype.roll2 = function (num) {
+Frame.prototype.roll2 = function(num) {
   this.roll2 = num
 }
 
-Frame.prototype.baseScore = function () {
+Frame.prototype.baseScore = function() {
   return (this.roll1 + this.roll2);
 };
+
+Frame.prototype.roll1result = function() {
+  return (this.roll1)
+}

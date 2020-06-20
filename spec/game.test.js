ac2b8b6a510278a7b0c1018d09b5520a28e816d9
victorvallet <victorvallet8@gmail.com>
2020-06-20 19:07:10 +0200

    Passed test to check if roll() decrease pins number

--- spec/game.test.js
+++ spec/game.test.js
@@ -7,14 +7,17 @@ describe('Game class', () => {
     test('it starts at frame 1 with 10 pins', () => {
         expect(game.pins).toEqual(10);
         expect(game.frame).toEqual(1);
-    })
-})
+    });
+});
 
 describe('roll()', () => {
     var game = new Game();
+    const roll = game.roll();
     test('it returns a number between 0 and 10 pins', () => {
-        expect(game.roll()).toBeGreaterThanOrEqual(0);
-        expect(game.roll()).toBeLessThanOrEqual(10);
-    })
-    test('it can be invoque again if num')
-})
+        expect(roll).toBeGreaterThanOrEqual(0);
+        expect(roll).toBeLessThanOrEqual(10);
+    });
+    test('it descreased the number of pins by the returned number', () => {
+        expect(game.pins).toEqual(10 - roll);
+    });
+});

397be0b93bfafffd1a40e521def84957b8e5ea0b
Nick Butler <nwmbutler@gmail.com>
2020-06-21 17:51:14 +0100

    little refactor

--- src/bowlingGame.js
+++ src/bowlingGame.js
@@ -5,8 +5,8 @@ class Game{
     this.rolls = []
     this.gameScore = 0
     this.frame = 0
-    this.spare = false
     this.strike = false
+    this.spare = false
   }
 
   score(a, b){
@@ -14,26 +14,47 @@ class Game{
       throw 'End of Game'
     }
     else if ((this.canPlay() && a === 10)) {
-      this.isStrike
-      this.frame ++
-      return "strike";
+      this.rolls.push(a, b)
+      this.isStrike();
     } else if (a + b === 10) {
-      this.isSpare
-      this.frame ++
-      return "spare";
+      this.rolls.push(a, b)
+      this.isSpare();
+    } else if (this.spare === true){
+      this.spareBonus();
+    } else if (this.strike === true){
+      this.strikeBonus();
     } else {
       this.gameScore += a + b
       this.rolls.push(a, b)
-      this.frame ++
+      this.strike = false
+      this.spare = false
       return this.gameScore
     }
   }
 
   canPlay(){
+    this.frame ++
     return (this.frame < 10)
   }
 
+  isStrike(){
+    this.gameScore += 10
+    this.strike = true
+  }
 
+  isSpare(){
+    this.gameScore += 10
+    this.spare = true
+  }
+
+  strikeBonus(){
+
+  }
+
+  spareBonus(){
+
+
+  }
 
 
 };

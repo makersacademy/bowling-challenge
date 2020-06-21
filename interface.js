555d8f975852c3622107b676290887c76dd611b9
Tim <TimCPB@github.com>
2020-06-21 22:56:54 +0100

    JQuery is now working and user can enter roll scores online

--- interface.js
+++ interface.js
@@ -1,4 +1,12 @@
-// $(document).ready(function() {
-//   var game = new Game();
-//   $(#)
-// })
+$(document).ready(function() {
+  var game = new Game();
+  // console.log(game)
+  $('#submit').on('click', function() {
+    var rollScore = $('#RollScore').val();
+    // var numScore = parseInt(rollScore);
+    game.addRoll(parseInt(rollScore));
+    console.dir(game);
+  })
+  // console.log(game.totalScore)
+})
+

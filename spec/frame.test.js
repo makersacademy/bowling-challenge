57a33baa9a4f272f25750d06e3c7b440b7eb8599
victorvallet <victorvallet8@gmail.com>
2020-06-21 13:54:12 +0200

    Passed test for isSpare()

--- spec/frame.test.js
+++ spec/frame.test.js
@@ -9,8 +9,12 @@ describe('Frame Class', () => {
         var frame = new Frame([10])
         expect(frame.isStrike()).toBeTrue
     });
-    test('it returns a gutter if the player knocks down 0 pins', () => {
+    test('it returns a Gutter if the player knocks down 0 pins', () => {
         var frame = new Frame([0])
         expect(frame.isGutter()).toBeTrue
+    });
+    test('it returns a Spare if the player knocks down 10 pins within the 2 rolls', () => {
+        var frame = new Frame([2, 8])
+        expect(frame.isSpare()).toBeTrue
     });
 });

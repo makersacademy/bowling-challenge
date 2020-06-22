d02aeee07d548bbf8b4081c487a28b7854210fae
Rachael <rachaelewins@hotmail.com>
2020-06-22 06:40:42 +0100

    tidying Spec file

--- spec/scoreSpec.js
+++ spec/scoreSpec.js
@@ -6,28 +6,29 @@ describe('score', function() {
 
   beforeEach( function (){
     frame1 = jasmine.createSpyObj('frame1', ['roll1result', 'roll2result', 'baseScore']);
-    frame1.roll1result.and.callFake(function() {
-      return (2);
-    });
-    frame1.roll2result.and.callFake(function() {
-      return (5);
-    });
+    frame2 = jasmine.createSpyObj('frame2', ['roll1result', 'roll2result', 'baseScore']);
+    frame3 = jasmine.createSpyObj('frame2', ['roll1result', 'roll2result', 'baseScore']);
     score1 = new Score(frame1)
   })
 
   describe('no spare/strike game', function() {
     it('should return the base score for the round', function(){
+      frame1.roll1result.and.callFake(function() {
+        return (2);
+      });
+      frame1.roll2result.and.callFake(function() {
+        return (5);
+      });
+      frame1.baseScore.and.callFake(function() {
+        return (7)
+      })
       expect(score1.score()).toEqual(7);
     });
   })
 
   describe('spare', function() {
     it('should return the score for the round', function(){
-      frame1 = jasmine.createSpyObj('frame1', ['roll1result', 'baseScore']);
       frame2 = jasmine.createSpyObj('frame2', ['roll1result']);
-      frame1.roll1result.and.callFake(function() {
-        return (2);
-      });
       frame1.baseScore.and.callFake(function() {
         return (2 + 8);
       });
@@ -41,9 +42,6 @@ describe('score', function() {
 
   describe('strike, followed by not strike', function() {
     it('should return the score for the round', function(){
-      frame1 = jasmine.createSpyObj('frame1', ['roll1result']);
-      frame2 = jasmine.createSpyObj('frame2', ['roll1result', 'roll2result', 'baseScore']);
-      frame3 = jasmine.createSpyObj('frame1', ['roll1result']);
       frame1.roll1result.and.callFake(function() {
         return (10);
       });
@@ -59,8 +57,6 @@ describe('score', function() {
   })
   describe('strike, followed by not strike', function() {
     it('should return the score for the round', function(){
-      frame1 = jasmine.createSpyObj('frame1', ['roll1result']);
-      frame2 = jasmine.createSpyObj('frame2', ['roll1result', 'roll2result', 'baseScore']);
       frame1.roll1result.and.callFake(function() {
         return (10);
       });

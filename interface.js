ab759dc734f0719fce7ebe57c432cd059f36b1ad
tommyrharper <tommyrharper@gmail.com>
2020-07-02 19:26:19 +0100

    Add first and second rolls to page

--- interface.js
+++ interface.js
@@ -1,24 +1,57 @@
 $(document).ready(function() {
   const scorecard = new Scorecard();
+  let numberOfRolls = 0;
 
-  $('#addFrame').click(function(e) {
+  // $('#addFrame').click(function(e) {
+  //   e.preventDefault();
+  //   let firstRoll = parseInt($('#firstRoll').val());
+  //   let secondRoll = parseInt($('#secondRoll').val());
+  //   if (isNaN(firstRoll)) {
+  //     firstRoll = 0;
+  //   } if (isNaN(secondRoll)) {
+  //     secondRoll = 0;
+  //   }
+
+  //   scorecard.addFrame(new Frame(firstRoll, secondRoll));
+
+  //   $('#score').text(scorecard.score);
+  //   $('#frames').text(scorecard.frames.length);
+  //   if (scorecard.frames.length === 1) {
+  //     console.log('hello');
+  //     $('#roll1').text(firstRoll);
+  //     $('#roll2').text(secondRoll);
+  //     $('#frame1').text(firstRoll+secondRoll);
+  //   }
+  // });
+
+  $('#addRoll').click(function(e) {
     e.preventDefault();
-    let roll1 = parseInt($('#roll1').val());
-    let roll2 = parseInt($('#roll2').val());
-    if (isNaN(roll1)) {
-      roll1 = 0;
-    } if (isNaN(roll2)) {
-      roll2 = 0;
+    console.log(numberOfRolls);
+    numberOfRolls += 1;
+
+    let roll = parseInt($('#roll').val());
+
+    if (isNaN(roll)) {
+      roll = 0;
+    }
+    console.log(numberOfRolls);
+
+    if (numberOfRolls === 1) {
+      $('#roll1').text(roll);
+    }
+    if (numberOfRolls === 2) {
+      $('#roll2').text(roll);
     }
 
-    scorecard.addFrame(new Frame(roll1, roll2));
+    // scorecard.addFrame(new Frame(roll1, roll2));
 
-    $('#score').text(scorecard.score);
-    $('#frames').text(scorecard.frames.length);
-    if (scorecard.frames.length === 1) {
-      console.log('hello');
-      $('#r1').text(roll1);
-      $('#r2').text(roll2);
-    }
+    // $('#score').text(scorecard.score);
+    // $('#frames').text(scorecard.frames.length);
+    // if (scorecard.frames.length === 1) {
+    //   console.log('hello');
+    //   $('#r1').text(roll1);
+    //   $('#r2').text(roll2);
+    //   $('#frame1').text(roll1+roll2);
+    // }
   });
 });

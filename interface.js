bf05b8bac86fcf5b86ec5663b6308c23a8197fc3
tommyrharper <tommyrharper@gmail.com>
2020-07-02 21:42:30 +0100

    Refactor out updateButtons method

--- interface.js
+++ interface.js
@@ -2,53 +2,15 @@ $(document).ready(function() {
   const scorecard = new Scorecard2();
   let numberOfRolls = 0;
 
-  // $('#addFrame').click(function(e) {
-  //   e.preventDefault();
-  //   let firstRoll = parseInt($('#firstRoll').val());
-  //   let secondRoll = parseInt($('#secondRoll').val());
-  //   if (isNaN(firstRoll)) {
-  //     firstRoll = 0;
-  //   } if (isNaN(secondRoll)) {
-  //     secondRoll = 0;
-  //   }
-
-  //   scorecard.addFrame(new Frame(firstRoll, secondRoll));
-
-  //   $('#score').text(scorecard.score);
-  //   $('#frames').text(scorecard.frames.length);
-  //   if (scorecard.frames.length === 1) {
-  //     console.log('hello');
-  //     $('#roll1').text(firstRoll);
-  //     $('#roll2').text(secondRoll);
-  //     $('#frame1').text(firstRoll+secondRoll);
-  //   }
-  // });
-
-  // $('#addRoll').click(function(e) {
-  //   e.preventDefault();
-
-  //   numberOfRolls += 1;
-
-  //   let roll = parseInt($('#roll').val());
-
-  //   if (isNaN(roll)) {
-  //     roll = 0;
-  //   }
-
-  //   $('#roll' + numberOfRolls).text(roll);
-  // });
-
   $('.record').click(function(e) {
     e.preventDefault(e);
 
     numberOfRolls += 1;
 
-    let rollScore = parseInt(e.currentTarget.value);
+    const rollScore = parseInt(e.currentTarget.value);
 
     scorecard.addRoll(rollScore);
-    if (scorecard.gameComplete) {
-      $('.record').prop('disabled', true);
-    }
+
     $('#score').text(scorecard.score);
     $('#frames').text(scorecard.frames.length);
 
@@ -63,19 +25,18 @@ $(document).ready(function() {
       $('#frame' + scorecard.frames.length).text;
     }
 
-    if (scorecard.gameComplete) {
-      $('.record').prop('disabled', true);
-    }
+    updateButtons(scorecard.rolls.length, rollScore);
 
-    if (scorecard.rolls.length === 1) {
+    $('#frame' + scorecard.frames.length).text(scorecard.score);
+  });
+
+  function updateButtons(numberOfRolls, rollScore) {
+    if (numberOfRolls === 1) {
       for (i = 11-rollScore; i < 11; i++) {
         $(`#${i}.record`).prop('disabled', true);
       }
     } else {
       $('.record').prop('disabled', false);
     }
-
-
-    $('#frame' + scorecard.frames.length).text(scorecard.score);
-  });
+  }
 });

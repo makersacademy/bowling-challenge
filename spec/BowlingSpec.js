617a4c55e378e44afe467479d7cad825780effe3
Katie Jones <katieljones29@gmail.com>
2020-07-11 11:17:17 +0100

    sets max points for one roll strike, a spare, max points of 10 per roll, and frames array

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -1,10 +1,26 @@
 'use strict';
 describe('Game', function() {
-  var game;
+  var newGame;
 
   beforeEach(function() {
-    game = new Game();
+    newGame = new Game();
+  })
+
+  it('has a maximum of 10 points per roll', function(){
+    expect(newGame.MAXIMUM_POINTS).toEqual(10);
   });
 
+  it('has 10 frames', function() {
+    expect(newGame._frames).toEqual([]);
+  });
+
+
+  it('calculates a spare', function() {
+    expect(newGame.isSpare()).toEqual(10)
+  })
+
+  it('calculates a strike', function() {
+    expect(newGame.isStrike()).toEqual(10)
+  })
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,4 +1,17 @@
 'use strict';
 class Game{
+  constructor() {
+    this.MAXIMUM_POINTS = 10;
+    this._frames = [];
+  }
 
+  isSpare() {
+    return this.MAXIMUM_POINTS
+
+  }
+
+  isStrike() {
+    return this.MAXIMUM_POINTS
+
+  }
 };

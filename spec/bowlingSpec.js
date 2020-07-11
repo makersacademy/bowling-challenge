0259cc69ae1c495735820dad596a327d385d5f2b
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-11 14:00:42 +0100

    Second test for first roll written and passed

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,10 +1,19 @@
-describe('Bowling', function () {
+describe('Bowling', function() {
   var bowling;
+
+  beforeEach(function() {
+    bowling = new Bowling();
+  });
 
   describe('creates a game of bowling', function() {
     it('starts the game', function() {
-      bowling = new Bowling();
       expect(bowling).toBeInstanceOf(Bowling);
+    });
+  });
+  describe('roll', function() {
+    it('starts first roll and records to screen', function() {
+      bowling.roll(5);
+      expect(bowling.display()).toContain(5);
     });
   });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,1 +1,14 @@
-class Bowling {}
+class Bowling {
+
+constructor() {
+  this.frame = []
+};
+
+roll(n) {
+  this.frame.push(n)
+};
+
+display() {
+  return this.frame;
+}
+}

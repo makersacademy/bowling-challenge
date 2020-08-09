b0e0b0950e02090ae9732b09e21a9a280242fac8
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-09 12:51:26 +0100

    added finalframe and splitted two different kind of frames in 2 functions: enterFinalFrame and enterNormalFrame

--- src/scoreCard.js
+++ src/scoreCard.js
@@ -4,14 +4,9 @@ $(document).ready(function() {
 
   
   $('#submit').click(function(){
-    const frame = new Frame();
-    const firstBall = parseInt($('#ball-1').val())
-    const secondBall = parseInt($('#ball-2').val())
-    frame.firstRoll(firstBall);
-    frame.secondRoll(secondBall);
-    $(`#${game.currentFrame}.first-bowl`).text(firstBall)
-    $(`#${game.currentFrame}.second-bowl`).text(secondBall)
-    game.addFrame(frame)
+    if (!isLastFrame()) {
+      enterNormalFrame()
+    } else {enterFinalFrame()}
     updateScore()
     console.log(game)
   });
@@ -20,5 +15,41 @@ $(document).ready(function() {
     game.sharingInfoWithFrames();
     game.getFramesScores()
     $('.total').text(game.finalScore())
+  }
+
+  function isLastFrame() {
+    return game.currentFrame === 9
+  }
+
+  function enterNormalFrame() {
+    const frame = new Frame();
+    const firstBall = parseInt($('#ball-1').val())
+    const secondBall = parseInt($('#ball-2').val())
+    frame.firstRoll(firstBall);
+    frame.secondRoll(secondBall);
+    $(`#${game.currentFrame}.first-bowl`).text(firstBall)
+    $(`#${game.currentFrame}.second-bowl`).text(secondBall)
+    game.addFrame(frame)
+    _addBonusbox()
+  }
+
+  function enterFinalFrame() {
+    const finalframe = new FinalFrame()
+    const firstBall = parseInt($('#ball-1').val())
+    const secondBall = parseInt($('#ball-2').val())
+    const bonus = parseInt($('#ball-3').val())
+    finalframe.firstRoll(firstBall);
+    finalframe.secondRoll(secondBall);
+    finalframe.bonusRoll(bonus);
+    $(`#${game.currentFrame}.first-bowl`).text(firstBall)
+    $(`#${game.currentFrame}.second-bowl`).text(secondBall)
+    $(`#${game.currentFrame}.bonus-bowl`).text(bonus)
+    game.addFrame(finalframe)
+  }
+
+  function _addBonusbox () {
+    if (isLastFrame()){
+      $('#ball-3').removeClass('hidden')
+    }
   }
 });

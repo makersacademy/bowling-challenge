ae59cd2bcc63c664c83aca7dfb2731f86c73b1c5
Hilda Amponsah <hilda.amp@hotmail.com>
2020-08-09 18:47:20 +0100

    planning|user story|

---
+++ jasmine-standalone-3.6.0 (6)/spec/gameSpec.js
@@ -0,0 +1,16 @@
+describe(" ", function() {
+  // var player;
+  // var song;
+
+  // beforeEach(function() {
+  //   player = new something();
+  //   song = new something();
+  // });
+
+  describe('', function(){
+    it('', function(){
+      expect().toEqual([])
+    });
+  });
+
+});

e04befa3292c5bf4c64e52f30f394199d0916237
Hilda Amponsah <hilda.amp@hotmail.com>
2020-08-10 00:14:21 +0100

    cal score- not passong|second roll

--- jasmine-standalone-3.6.0 (6)/spec/gameSpec.js
+++ jasmine-standalone-3.6.0 (6)/spec/gameSpec.js
@@ -1,3 +1,5 @@
+'use strict'
+
 describe("Frame ", function() {
   var frame;
   var scorecard;
@@ -12,13 +14,31 @@ describe("Frame ", function() {
   describe('a single roll', function(){
     beforeEach(function() {
       frame = new Frame();
-
       frame.enterFirstRollScore(3, scorecard);
+      frame.enterSecondRollScore(2, scorecard)
     });
 
     it('displays score for a single roll', function(){
       expect(frame.firstRollScore).toEqual(3);
     });
   });
+
+//   Calculate frame score.
+// ----------------
+// As a bowler, 
+// So I can calculate my score for each frame,
+// I want to see the total score for each frame for pins I have knocked out (not strike/10 down
+
+  it('adds score of first roll to frame total', function(){
+    frame.calculateTotalScore(frame)
+    expect(frame.calculateTotalScore()).toEqual(3);
+  })
+
+
+
+it('displays score from second roll', function(){
+  // frame.enterSecondRollScore(2, scorecard);
+  expect(frame.secondRollScore).toEqual(2);
+});
 
 });
--- jasmine-standalone-3.6.0 (6)/src/game.js
+++ jasmine-standalone-3.6.0 (6)/src/game.js
@@ -1,9 +1,22 @@
 class Frame{
   constructor(){
   this.firstRollScore = 0
+  this.calculatorTotalScore = 0
+  this.secondRollScore = 0
   }
 
+
   enterFirstRollScore(pinsDown, scorecard){
-    this.firstRollScore = pinsDown;
-  }
+   this.firstRollScore = pinsDown
+  // this.calculatorTotalScore;
+  };
+
+ calculatorTotalScore(){
+   this.firstRollScore = pinsDown;
+ }
+
+  enterSecondRollScore(pinsDown, scorecard){
+    this.secondRollScore = pinsDown;
+  };
 }
+

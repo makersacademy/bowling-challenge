88dbc1d04dff90c1280b848673c6f71fab4ab65b
Bene Arinci <benedetta.arinci@gmail.com>
2020-08-09 01:03:42 +0100

    creating normal frames and filling out the scorecard

--- src/scoreCard.js
+++ src/scoreCard.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
   const game = new Game()
   console.log(game)
+
+  $('#submit').click(function(){
+    const frame = new Frame();
+    const firstBall = parseInt($('#ball-1').val())
+    const secondBall = parseInt($('#ball-2').val())
+    frame.firstRoll(firstBall);
+    frame.secondRoll(secondBall);
+    $(`#${game.currentFrame}.first-bowl`).text(firstBall)
+    $(`#${game.currentFrame}.second-bowl`).text(secondBall)
+    game.addFrame(frame)
+    console.log(game)
+  });
 });

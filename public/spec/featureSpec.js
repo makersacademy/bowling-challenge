9fa0c130334efa173a97f64c77456e3f2a73d99b
Ryan Viegas <rjkviegas@gmail.com>
2020-08-10 19:26:32 +0100

    refactored tests resulting in test: add() records only 2 rolls

--- public/spec/featureSpec.js
+++ public/spec/featureSpec.js
@@ -6,27 +6,21 @@ describe("Feature Test", function() {
   beforeEach(function() {
     frame = new Frame();
   });
-  describe("adding rolls to frames", function() {
-    it("records the player's rolls", function() {
-      // frame = new Frame();
-      var firstRoll = 1;
-      var secondRoll = 2;
-      frame.add(firstRoll);
-      expect(frame.showRolls()).toContain(firstRoll);
-      frame.add(secondRoll);
-      expect(frame.showRolls()).toContain(secondRoll);
-    });
-    it("only accepts 2 rolls per frame", function() {
-      // frame = new Frame();
+  describe("add()", function() {
+
+    it("records only 2 rolls", function() {
       var firstRoll = 1;
       var secondRoll = 2;
       var thirdRoll = 3;
       frame.add(firstRoll);
+      expect(frame.showRolls()).toContain(firstRoll);
       frame.add(secondRoll);
+      expect(frame.showRolls()).toContain(secondRoll);
       frame.add(thirdRoll);
       expect(frame.showRolls().length).toBe(2);
       expect(frame.showRolls()).not.toContain(thirdRoll);
-    })
+    });
+    
   });
   
 });

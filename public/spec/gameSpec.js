41135036fb637201382ff1c3028515e1007b838f
Ryan Viegas <rjkviegas@gmail.com>
2020-08-13 21:16:35 +0100

    test getTotalPoints() returns total points for game when no bonus points accrued

--- public/spec/gameSpec.js
+++ public/spec/gameSpec.js
@@ -2,11 +2,18 @@
 
 describe("Game", function() {
   var game;
+  var frame;
   beforeEach(function() {
     game = new Game();
     });
   describe("getTotalPoints() returns total points for frame", function() {
     it("when no bonus points accrued", function() {
+      frame = new Frame([4,4]);
+      for (var i = 0; i <= 9; i += 1) {
+        game.add(frame);
+      }
+      expect(game.frames.length).toBe(10);
+      expect(game.getTotalPoints()).toBe(80);
     });
     it("when there is a spare", function() {
     });

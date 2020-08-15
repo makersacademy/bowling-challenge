0650eb8958b0deebf3560db45a1d0e4757904c6c
Ryan Viegas <rjkviegas@gmail.com>
2020-08-15 13:12:29 +0100

    test Points total half spares and then half strikes

--- public/spec/featureSpec.js
+++ public/spec/featureSpec.js
@@ -22,20 +22,20 @@ describe("Feature Test", function() {
   describe("Points total", function () {
     it("Gutter Game", function() {
       var gutterFrame = new Frame([0,0]);
-      for (var i = 0; i <= 9; i += 1) {
+      for (var i = 0; i < 10; i += 1) {
         game.add(gutterFrame);
       }
       expect(game.getTotalPoints()).toBe(0);
     });
     it("No spare/strike game", function() {
-      for (var i = 0; i <= 9; i += 1) {
+      for (var i = 0; i < 10; i += 1) {
         game.add(frame);
       }
       expect(game.getTotalPoints()).toBe(80);
     });
     it("Game of spares", function() {
       var spareFrame = new Frame([5, 5]);
-      for (var i = 0; i <= 8; i += 1) {
+      for (var i = 0; i < 9; i += 1) {
         game.add(spareFrame);
       }
       var finalFrame = new Frame([5, 5, 5]);
@@ -44,12 +44,25 @@ describe("Feature Test", function() {
     });
     it("Game of strikes", function() {
       var strikeFrame = new Frame([10]);
-      for (var i = 0; i <= 8; i += 1) {
+      for (var i = 0; i < 9; i += 1) {
         game.add(strikeFrame);
       }
       var finalFrame = new Frame([10, 10, 10]);
       game.add(finalFrame);
       expect(game.getTotalPoints()).toBe(300);
     });
+    it("half spares and then half strikes", function() {
+      var spareFrame = new Frame([5, 5]);
+      for (var i = 0; i < 5; i += 1) {
+        game.add(spareFrame);
+      }
+      var strikeFrame = new Frame([10]);
+      for (var i = 0; i < 4; i += 1) {
+        game.add(strikeFrame);
+      }
+      var finalFrame = new Frame([10, 10, 10]);
+      game.add(finalFrame);
+      expect(game.getTotalPoints()).toBe(230);
+    })
   });
 });

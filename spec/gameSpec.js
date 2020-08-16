a7f9bbd6eb9824fb6d7121bbc98f0d8881135c11
DillonBarker <barker.dill98@gmail.com>
2020-08-16 14:02:35 +0100

    some work on bowling;

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -9,14 +9,34 @@ describe('Bowling', function() {
       expect(game.showScore()).toEqual(0);
     });
     it('Score can increase after a frame', function() {
-      game.frame(3,4);
+      game.bowlFrame(3,4);
       expect(game.showScore()).toEqual(7);
     });
+    it('Score adds up more than 1 frame', function() {
+      game.bowlFrame(1,1)
+      game.bowlFrame(3,4)
+      expect(game.showScore()).toEqual(9);
+    })
   });
 
   describe('frame', function() {
     it('A user can bowl a whole frame', function() {
-      expect(game.frame(3, 4)).toEqual([3,4]);
+      expect(game.bowlFrame(3, 4)).toEqual([3,4]);
     });
+    it('A user can bowl more than one frame', function() {
+      game.bowlFrame(1,1)
+      game.bowlFrame(3,4)
+      expect(game.bowlFrame(4,5)).toEqual([4,5])
+    })
+    it('A user can bowl 10 frames after this its the end', function() {
+      manyFrames();
+      expect(function() { game.bowlFrame(2,2); }).toThrowError('game over');
+      expect(game.showScore()).toEqual(40);
+    })
   });
+  function manyFrames() {
+    for (var i = 0; i < 10; i++) {
+      game.bowlFrame(2,2);
+    };
+  };
 });
--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,30 @@
 class Game {
   constructor() {
     this.score = 0;
+    this.frameCounter = 0;
   }
 
-  frame(roll1, roll2) {
-    this.score += (roll1+roll2)
+  bowlFrame(roll1, roll2) {
+    if (this.isGameOver()) {
+      throw new Error('game over')
+    }
+    this.addScore(roll1,roll2)
+    ++this.frameCounter
     return [roll1, roll2];
+  }
+
+  addScore(roll1, roll2) {
+    this.score += (roll1+roll2)
   }
 
   showScore() {
     return this.score;
+  }
+
+  isGameOver() {
+    if (this.frameCounter === 10) {
+      return true
+    }
+    return false
   }
 }

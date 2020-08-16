c884462f76a859e5611d5c2440858a026c4dd8c5
Ryan Viegas <rjkviegas@gmail.com>
2020-08-16 12:35:50 +0100

    refactored gameSpec tests

--- public/spec/gameSpec.js
+++ public/spec/gameSpec.js
@@ -1,44 +1,37 @@
 "use strict";
 
 describe("Game", function() {
-  var game;
-  var frame;
+  var game, frame, spareFrame, strikeFrame;
   beforeEach(function() {
     game = new Game();
+    frame = new Frame([4, 4]);
+    spareFrame = new Frame([5, 5]);
+    strikeFrame = new Frame([10]);
     });
   describe("totalPoints()", function() {
     it("when no bonus points accrued", function() {
-      frame = new Frame([4, 4]);
       for (var i = 0; i <= 9; i += 1) {
         game.add(frame);
       }
       expect(game.totalPoints()).toBe(80);
     });
     it("when there is a spare", function() {
-      frame = new Frame([5, 5]);
-      var anotherFrame = new Frame([4, 4]);
+      game.add(spareFrame);
       game.add(frame);
-      game.add(anotherFrame);
       expect(game.totalPoints()).toBe(22);
     });
     it("when there is a strike", function() {
-      frame = new Frame([10]);
-      var anotherFrame = new Frame([4, 4]);
+      game.add(strikeFrame);
       game.add(frame);
-      game.add(anotherFrame);
       expect(game.totalPoints()).toBe(26);
     });
     it("when there is a strike and then a spare", function() {
-      frame = new Frame([10]);
-      var spareFrame = new Frame([5, 5]);
-      game.add(frame);
+      game.add(strikeFrame);
       game.add(spareFrame);
       expect(game.totalPoints()).toBe(30);
     });
     it("when there is a spare and then a strike", function() {
-      frame = new Frame([5, 5]);
-      var strikeFrame = new Frame([10]);
-      game.add(frame);
+      game.add(spareFrame);
       game.add(strikeFrame);
       expect(game.totalPoints()).toBe(30);
     });

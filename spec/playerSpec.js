f39fae98183db9b1e18c78378874cc069eb420b7
Iliyan-Y <maximizebg@gmail.com>
2020-09-04 13:32:23 +0300

    frames added to the player

--- spec/playerSpec.js
+++ spec/playerSpec.js
@@ -8,4 +8,15 @@ describe("Player", () => {
   it("Displays player name", () => {
     expect(player.name).toEqual("Player 1");
   });
+
+  it("Has numbers of frames", () => {
+    expect(player.frames).toEqual([]);
+  });
+
+  describe("newFrame", () => {
+    it("add new frame to the frames list", () => {
+      player.newFrame("this is a frame");
+      expect(player.frames.length).toEqual(1);
+    });
+  });
 });
--- src/player.js
+++ src/player.js
@@ -1,5 +1,10 @@
 class Player {
   constructor(name) {
     this.name = name;
+    this.frames = [];
+  }
+
+  newFrame(frame) {
+    this.frames.push(frame);
   }
 }

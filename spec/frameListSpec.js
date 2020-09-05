98b2054c614f49387e24aed550fe36f269994cf0
Iliyan-Y <maximizebg@gmail.com>
2020-09-06 00:22:43 +0300

    Big refactoring test for  strikes and spares in a roll

--- spec/frameListSpec.js
+++ spec/frameListSpec.js
@@ -7,17 +7,17 @@ describe("FrameList", () => {
   describe("add", () => {
     it("adds last frame to the frame list", () => {
       frameList.add(5, 3);
-      expect(frameList.frames).toEqual([8]);
+      expect(frameList.frames).toEqual({ 1: 8 });
     });
 
     it("adds Spare to the frame list when player spare", () => {
       frameList.add(5, 5);
-      expect(frameList.frames).toEqual(["Spare"]);
+      expect(frameList.frames).toEqual({ 1: "Spare" });
     });
 
     it("adds Strike to the frame list when player Strikes", () => {
       frameList.add("Strike");
-      expect(frameList.frames).toEqual(["Strike"]);
+      expect(frameList.frames).toEqual({ 1: "Strike" });
     });
   });
 
@@ -25,20 +25,34 @@ describe("FrameList", () => {
     it("add Strike + bonus from the next round", () => {
       frameList.add("Strike");
       frameList.add(4, 1);
-      expect(frameList.frames).toEqual(["Strike", 20]);
+      expect(frameList.displayFrames).toEqual(["Strike", 20]);
     });
 
     it("add Strike + bonus from the next round", () => {
       frameList.add(5, 5);
       frameList.add(4, 1);
-      expect(frameList.frames).toEqual(["Spare", 19]);
+      expect(frameList.displayFrames).toEqual(["Spare", 19]);
     });
 
     it("add Strike + Spare + bonus from the next round", () => {
       frameList.add("Strike");
       frameList.add(5, 5);
       frameList.add(4, 1);
-      expect(frameList.frames).toEqual(["Strike", "Spare", 39]);
+      expect(frameList.displayFrames).toEqual(["Strike", "Spare", 39]);
+    });
+
+    it("add Spare + Strike + bonus from the next round", () => {
+      frameList.add("Spare");
+      frameList.add("Strike");
+      frameList.add(4, 1);
+      expect(frameList.displayFrames).toEqual(["Spare", "Strike", 40]);
+    });
+
+    it("add Strike + Strike + bonus from the next round", () => {
+      frameList.add("Strike");
+      frameList.add("Strike");
+      frameList.add(4, 2);
+      expect(frameList.displayFrames).toEqual(["Strike", "Strike", 42]);
     });
   });
 });
--- src/frameList.js
+++ src/frameList.js
@@ -1,42 +1,50 @@
 class FrameList {
   constructor() {
-    this.frames = [];
+    this.frames = {};
+    this.rounds = 1;
+    this.displayFrames = [];
   }
 
   add(roll_1, roll_2) {
     let total = roll_1 + roll_2;
     if (total == 10 || roll_1 == "Spare") {
-      this.frames.push("Spare");
+      this.frames[[this.rounds]] = "Spare";
+      this.rounds++;
     } else if (roll_1 == "Strike" || roll_1 == 10) {
-      this.frames.push("Strike");
+      this.frames[[this.rounds]] = "Strike";
+      this.rounds++;
     } else {
-      this.frames.push(total);
+      this.frames[[this.rounds]] = total;
+      this.rounds++;
     }
 
     this.calculate(roll_1, roll_2);
   }
 
   calculate(roll_1, roll_2) {
-    let result = [];
-    let strikes = 0;
-    let spares = 0;
-    this.frames.map((frame) => {
-      if (frame == "Strike") {
-        strikes += 10;
-        result.push(frame);
-      } else if (frame == "Spare") {
-        spares += 10;
-        result.push(frame);
-      } else if (spares > 0) {
-        result.push(roll_1 * 2 + roll_2 + spares);
-        spares = 0;
-      } else if (strikes > 0) {
-        result.push((roll_1 + roll_2) * 2 + strikes);
-        strikes = 0;
-      } else {
-        result.push(frame);
+    let bonus = 0;
+    let displayFrames = [];
+
+    for (const [key, value] of Object.entries(this.frames)) {
+      if (value === "Strike" || value === "Spare") {
+        displayFrames.push(value);
+        bonus += 10;
+        if (
+          this.frames[key - 1] == "Strike" ||
+          this.frames[key - 1] == "Spare"
+        ) {
+          console.log("BONUS");
+          bonus += 10;
+        }
+      } else if (this.frames[key - 1] == "Strike") {
+        displayFrames.push((roll_1 + roll_2) * 2 + bonus);
+        this.bonus = 0;
+      } else if (this.frames[key - 1] == "Spare") {
+        displayFrames.push(roll_1 * 2 + roll_2 + bonus);
+        this.bonus = 0;
       }
-    });
-    this.frames = result;
+    }
+
+    this.displayFrames = displayFrames;
   }
 }

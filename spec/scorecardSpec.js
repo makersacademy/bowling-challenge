24a0e54876a72253edf455b79fb6cfd865fbdac7
Yasmin Underdown <yunderdown1@gmail.com>
2020-09-06 15:03:24 +0100

    scorecard class setup test failing

---
+++ spec/scorecardSpec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Scorecard', function(){
+
+  var scorecard;
+  var frame;
+  var finalframe;
+
+  beforeEach(function(){
+    scorecard = new Scorecard();
+  });
+
+  it('starts with 1 frame object', function() {
+    expect(scorecard.frames.length).toBe(1);
+    expect(scorecard.frames[0]).toBeInstanceOf(Frame);
+  });
+  
+});

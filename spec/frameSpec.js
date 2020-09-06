0e9c1894b8650063192a44e86f97e5b8aa0410ef
Yasmin Underdown <yunderdown1@gmail.com>
2020-09-06 12:44:11 +0100

    second bowl test failing

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -14,6 +14,12 @@ describe('Frame', function() {
       expect(frame.firstBowl).toEqual(5);
     });
 
+    it('adds score to second bowl on second go', function(){
+      frame.bowl(5);
+      frame.bowl(2);
+      expect(frame.secondBowl).toEqual(2);
+    });
+
 
   });
 
--- src/frame.js
+++ src/frame.js
@@ -1,1 +1,16 @@
+'use strict'
 
+class Frame {
+
+  constructor() {
+    this.firstBowl = '-'
+  }
+
+  bowl(score) {
+    this.firstBowl = score;
+  }
+
+  firstBowl() {
+    return this._firstBowl;
+  }
+}

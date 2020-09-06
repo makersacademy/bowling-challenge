683d76c20e688c4886d86ce9ccd7c0f4f162a0c8
JRG <47917431+JR-G@users.noreply.github.com>
2020-09-06 12:52:11 +0100

    Test: checks the number of rolls left

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,12 +6,17 @@ describe('Game', function(){
     game = new Game();
   });
 
-  it('starts on the first frame', function() {
-    expect(game.frameCount).toEqual(1);
-  });
-
-  it('starts with 10 pins', function() {
-    expect(game.pins).toEqual(10);
-  });
+  describe('a new game', function() {
+    it('starts on the first frame', function() {
+      expect(game.frameCount).toEqual(1);
+    });
   
+    it('starts with 10 pins', function() {
+      expect(game.pins).toEqual(10);
+    });
+  
+    it('starts with 2 rolls', function() {
+      expect(game.rolls).toEqual(2);
+    });
+  });
 });

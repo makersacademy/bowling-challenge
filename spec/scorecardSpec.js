a41a741308d0ed85390436998227283a8435ee65
Yasmin Underdown <yunderdown1@gmail.com>
2020-09-06 15:35:02 +0100

    scorecard play method tests failing

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -8,6 +8,7 @@ describe('Scorecard', function(){
 
   beforeEach(function(){
     scorecard = new Scorecard();
+    frame = jasmine.createSpyObj('airport',['clearForLanding', 'clearForTakeOff']);
   });
 
   it('starts with 1 frame object', function() {
@@ -15,4 +16,16 @@ describe('Scorecard', function(){
     expect(scorecard.frames[0]).toBeInstanceOf(Frame);
   });
 
+  describe('Play', function() {
+    it('calls bowl method on frame object with same score', function(){
+      scorecard.play(5);
+      expect(scoredcard.frames[0].bowl()).toHaveBeenCalledWith(5);
+    });
+
+    it('adds new frame object if previous frame complete', function() {
+      scorecard.play(10);
+      scorecard.play(5);
+      expect(scorecard.frames.length).toBe(2);
+    });
+  });
 });

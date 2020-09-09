1af5d01430406a1b62389417c1a7e8d7fcdc4bfe
Iliyan-Y <maximizebg@gmail.com>
2020-09-09 23:21:16 +0100

    the simplest way

--- src/version4.js
+++ src/version4.js
@@ -1,63 +1,74 @@
 class Version4 {
   constructor() {
-    this.frames = [];
+    //this.frames = [];
     this.scores = [];
     this.counter = 0;
+    this.frame = undefined;
   }
 
   play(one, two) {
-    if (one == 10) {
-      this.frames.push("Strike");
-      this.scores.push(10);
-    } else if (one + two == 10) {
-      this.frames.push("Spare");
-      this.scores.push(one, two);
+    if (this.frame == "strike") {
+      this.bonus = 10;
+    } else if (this.frame == "spare") {
+      this.bonus = one;
     } else {
-      this.frames.push(one, two);
-      this.scores.push(one, two);
+      this.bonus = 0;
     }
 
-    this.counter = 0;
-    this.calc();
+    let rez = one + two + this.bonus;
+    this.scores.push(rez);
+
+    if (one == 10) {
+      this.frame = "strike";
+    } else if (one + two == 10) {
+      this.frame = "spare";
+    } else {
+      this.frame = undefined;
+    }
+    return rez;
   }
 
-  calc() {
-    let nextFrameRoll_1 = () => {
-      if (this.scores[this.counter + 1] == undefined) {
-        return 0;
-      } else if (this.scores[this.counter + 1] == "Strike") {
-        return 10;
-      } else {
-        return this.scores[this.counter + 1];
-      }
-    };
-    let nextFrameRoll_2 = () => {
-      if (this.scores[this.counter + 2] == undefined) {
-        return 0;
-      } else if (this.scores[this.counter + 2] == "Strike") {
-        return 10;
-      } else {
-        return this.scores[this.counter + 2];
-      }
-    };
-
-    let result = [];
-
-    this.frames.forEach((frame) => {
-      if (frame == "Strike") {
-        result.push(10 + nextFrameRoll_1() + nextFrameRoll_2());
-        this.counter++;
-      } else if (frame == "Spare") {
-        result.push(10 + nextFrameRoll_1());
-        this.counter++;
-      } else {
-        result.push(frame);
-        this.counter++;
-      }
-    });
-
-    this.scores = result;
+  total() {
+    return this.scores.reduce((a, b) => a + b, 0);
   }
+
+  // calc() {
+  //   let nextFrameRoll_1 = () => {
+  //     if (this.scores[this.counter + 1] == undefined) {
+  //       return 0;
+  //     } else if (this.scores[this.counter + 1] == "Strike") {
+  //       return 10;
+  //     } else {
+  //       return this.scores[this.counter + 1];
+  //     }
+  //   };
+  //   let nextFrameRoll_2 = () => {
+  //     if (this.scores[this.counter + 2] == undefined) {
+  //       return 0;
+  //     } else if (this.scores[this.counter + 2] == "Strike") {
+  //       return 10;
+  //     } else {
+  //       return this.scores[this.counter + 2];
+  //     }
+  //   };
+
+  //   let result = [];
+
+  //   this.frames.forEach((frame) => {
+  //     if (frame == "Strike") {
+  //       result.push(10 + nextFrameRoll_1() + nextFrameRoll_2());
+  //       this.counter++;
+  //     } else if (frame == "Spare") {
+  //       result.push(10 + nextFrameRoll_1());
+  //       this.counter++;
+  //     } else {
+  //       result.push(frame);
+  //       this.counter++;
+  //     }
+  //   });
+
+  //   this.scores = result;
+  // }
 
   // calc(one, two) {
   //   if (this.frames[this.counter - 1] == "Spare") {

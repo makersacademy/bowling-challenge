9e5ddb01cb03ff755fd87034868768b72f684cf7
imogenkraak <imogen.kraak@gmail.com>
2020-09-13 18:05:21 +0100

    game and score classes created with all tests passing

---
+++ src/Bowling.js
@@ -0,0 +1,37 @@
+'use strict'
+
+class Bowling {
+
+  constructor() {
+    this.numberOfTurns = 0;
+    this.currentRoll = 0;
+    this.allRolls = [];
+  }
+
+  roll(numberOfPins) {
+    this.currentRoll = numberOfPins
+    this.allRolls.push(numberOfPins)
+    return numberOfPins;
+  }
+
+  isStrike(indexOfRoll) {
+    return this.allRolls[indexOfRoll] === 10; 
+  }
+
+  isSpare(indexOfRoll) {
+    return this.allRolls[indexOfRoll] + this.allRolls[indexOfRoll + 1] === 10;
+  }
+  
+  nextRollsAfterStrike(indexOfRoll) {
+    return this.allRolls[indexOfRoll + 1] + this.allRolls[indexOfRoll + 2];
+  }
+
+  nextRollsAfterSpare(indexOfRoll) {
+    return this.allRolls[indexOfRoll + 2];
+  }
+
+  allRollsSum(indexOfRoll) {
+    return this.allRolls[indexOfRoll] + this.allRolls[indexOfRoll + 1];
+  }
+}
+

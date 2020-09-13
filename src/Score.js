403b5d0b480e78f42580299208a7c5aca7a6b17b
imogenkraak <imogen.kraak@gmail.com>
2020-09-13 18:45:18 +0100

    code cleanup

--- src/Score.js
+++ src/Score.js
@@ -1,19 +1,18 @@
-'use strict'
+'use strict';
 
 class Score {
 
-constructor() {
-    this._game = typeof game !== 'undefined' ? game : new Bowling();
-}
+    constructor() {
+        this._game = typeof game !== 'undefined' ? game : new Bowling();
+    }
 
-  score() {
+    score() {
     var indexOfRoll = 0
     var totalScore = 0
     for(var turn = 0; turn < 10; turn++){
         if(this._game.isStrike(indexOfRoll)) {
         totalScore += (10 + this._game.nextRollsAfterStrike(indexOfRoll))
         indexOfRoll += 1
-        this._game.numberOfTurns++
         } else if(this._game.isSpare(indexOfRoll)) {
         totalScore += (10 + this._game.nextRollsAfterSpare(indexOfRoll))
         indexOfRoll += 2
@@ -22,6 +21,6 @@ constructor() {
         indexOfRoll += 2
         }
     }
-      return totalScore;
-  };
+        return totalScore;
+    };
 }

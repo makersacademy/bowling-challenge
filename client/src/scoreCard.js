4e699589466d622d7996d9e17046ed150bd058d2
Iliyan-Y <maximizebg@gmail.com>
2020-09-13 11:09:27 +0100

    Css

--- client/src/scoreCard.js
+++ client/src/scoreCard.js
@@ -7,16 +7,24 @@ $(document).ready(() => {
 
   $("#rollForm").submit((e) => {
     e.preventDefault();
-
     let pins = $("#pins").val();
-
     frameList.roll(parseInt(pins));
-
     frameList.calc();
 
-    $("#game").text(frameList.score.join(" - "));
+    refreshPage();
+    displayScore();
+  });
+
+  let refreshPage = () => {
     $("#score").text("Score: " + frameList.total());
     $("#pins").val("");
     $("#counter").text("Rolls played: " + frameList.rollCounter);
-  });
+  };
+
+  let displayScore = () => {
+    $("#game").text(frameList.score.join(" - "));
+    $("#game")
+      .css({ top: "-8em", opacity: "20%", "font-size": "0.1em" })
+      .animate({ top: "0", opacity: "100%", "font-size": "1.3em" }, 700);
+  };
 });

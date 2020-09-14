be78b46100196ace792c3338f2756c37cf1dde3a
Yasmin Underdown <yunderdown1@gmail.com>
2020-09-14 09:43:06 +0100

    styling

--- src/interface.js
+++ src/interface.js
@@ -56,39 +56,6 @@ $(document).ready(function() {
     updateScores();
   });
 
-
-  $("#powersaving-on").click(function() {
-    thermostat.switchPowerSavingOn();
-    $('#power-saving-status').text('ON')
-    thermostat.reset();
-    updateTemperature();
-  });
-
-  $("#powersaving-off").click(function() {
-    thermostat.switchPowerSavingOff();
-    $('#power-saving-status').text('OFF')
-    thermostat.reset();
-    updateTemperature();
-  });
-
-
-  $('#select-city').submit(function(event) {
-    event.preventDefault();
-    var city = $('#typed-city').val();
-    displayWeather(city);
-  })
-
-  function displayWeather(city) {
-    var url  = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
-    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
-    var units = '&units=metric';
-    $.get(url + token + units, function(data) {
-      $('#select-temperature').text(data.main.temp);
-      $('#city-name').text(data.name);
-    });
-  }
-
-
   function updateScores() {
     $(`#${(scorecard.currentFrameIndex)+1}-1`).text(scorecard.currentFrame().firstBowl());
     if(scorecard.currentFrame().frame.length===2) {
@@ -101,28 +68,5 @@ $(document).ready(function() {
       $(`#${i+1}-score`).text(scorecard.getRunningTotalUpTo(i));
     }
   }
-
-  /*  $("1:1").text(scorecard.frames[0].firstBowl)
-    $("1:2").text(scorecard.frames[0].secondBowl)
-    $("2:1").text(scorecard.frames[1].firstBowl)
-    $("2:2").text(scorecard.frames[1].secondBowl)
-    $("3:1").text(scorecard.frames[2].firstBowl)
-    $("3:2").text(scorecard.frames[2].secondBowl)
-    $("4:1").text(scorecard.frames[3].firstBowl)
-    $("4:2").text(scorecard.frames[3].secondBowl)
-    $("5:1").text(scorecard.frames[4].firstBowl)
-    $("5:2").text(scorecard.frames[4].secondBowl)
-    $("6:1").text(scorecard.frames[5].firstBowl)
-    $("6:2").text(scorecard.frames[5].secondBowl)
-    $("7:1").text(scorecard.frames[6].firstBowl)
-    $("7:2").text(scorecard.frames[6].secondBowl)
-    $("8:1").text(scorecard.frames[7].firstBowl)
-    $("8:2").text(scorecard.frames[7].secondBowl)
-    $("9:1").text(scorecard.frames[8].firstBowl)
-    $("9:2").text(scorecard.frames[8].secondBowl)
-    $("10:1").text(scorecard.frames[9].firstBowl)
-    $("10:2").text(scorecard.frames[9].secondBowl)
-    $("10:3").text(scorecard.frames[9].thirdBowl)
-  }*/
 
 });

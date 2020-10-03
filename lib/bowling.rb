d3701ac719fbc7c9af69fa84f6316d2ed4c0e4b8
Richard <rich_pryce@yahoo.co.uk>
2020-10-03 15:12:50 +0100

    calculates spare score from 2 rounds

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,14 +1,32 @@
 class Bowling
 
   def initialize
-  @total_score = 0
-  @round = 1
+    @total_score = 0
+    @round = 1
+    @frame_score = 0
+    @strike = false
+    @spare = false
   end
   
   def score(score)
-
+    p score
+    if @spare == true
+      @total_score += score
+      @spare = false
+    end
+    
+    @frame_score += score
+    
+    spare if @round == 2 && @frame_score == 10
+    
+   
     strike if score == 10 
     @total_score += score
+    @round += 1
+    if @round == 3
+    @round = 1
+    @frame_score = 0
+    end
   end
 
 
@@ -19,4 +37,12 @@ class Bowling
   def strike
     true
   end
+
+  def spare
+    p @spare = true
+    
+    
+  end
+
+ 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,24 +1,35 @@
 require 'bowling'
 describe Bowling do
-  game = Bowling.new
-  20.times do
-    game.score(0)
+  before (:each) do
+    @game = Bowling.new
   end
   it "Gutter game scores 0" do
-    expect(game.total_score).to eq 0
+    
+  20.times do
+    @game.score(0)
+  end
+    expect(@game.total_score).to eq 0
   end
   it "adds up 1 round" do
     scores = [2,3,4,5]
     scores.each do |score|
-      game.score(score)
+      @game.score(score)
     end
-    expect(game.total_score).to eq 14
+    expect(@game.total_score).to eq 14
   end
 
 
   it "a strike scores 10" do
-    game = Bowling.new
-    game.score(10)
-    expect(game.total_score).to eq 10
+    @game.score(10)
+    expect(@game.total_score).to eq 10
   end
+
+  it "calculates a spare score from 2 rounds" do
+    scores = [5,5,4,5]
+    scores.each do |score|
+      @game.score(score)
+    end
+    expect(@game.total_score).to eq 23
+  end
+
 end

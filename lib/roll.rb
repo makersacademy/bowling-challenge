6bc001100a0675514b008638bd47c6f277d87ce4
Veronica Lee <veronicavlee@icloud.com>
2020-10-04 00:30:16 +0100

    Refactor Roll initialize to use between? check

--- lib/roll.rb
+++ lib/roll.rb
@@ -2,8 +2,7 @@ class Roll
   attr_reader :pins
 
   def initialize(pins)
-    raise "Cannot roll less than 0 pins" if pins < 0
-    raise "Cannot roll more than 10 pins" if pins > 10
+    raise "Roll pins must be between 0 and 10" unless pins.between?(0, 10)
     @pins = pins
   end
 end

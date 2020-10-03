81e12d738df23c5ca09fb1f499b03ea6b39538de
Richard <rich_pryce@yahoo.co.uk>
2020-10-03 12:29:33 +0100

    1 round with no strikes or spares

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,9 +1,22 @@
 class Bowling
+
+  def initialize
+  @total_score = 0
+  @round = 1
+  end
+  
   def score(score)
+
+    strike if score == 10 
+    @total_score += score
   end
 
 
   def total_score
-  0
+  @total_score
+  end
+  
+  def strike
+    true
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -7,4 +7,18 @@ describe Bowling do
   it "Gutter game scores 0" do
     expect(game.total_score).to eq 0
   end
+  it "adds up 1 round" do
+    scores = [2,3,4,5]
+    scores.each do |score|
+      game.score(score)
+    end
+    expect(game.total_score).to eq 14
+  end
+
+
+  it "a strike scores 10" do
+    game = Bowling.new
+    game.score(10)
+    expect(game.total_score).to eq 10
+  end
 end

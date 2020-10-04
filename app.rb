63c75948c483e769e2544cda945aad42fd4bdbf6
Victoria Jusko <vikjusko@gmail.com>
2020-10-04 19:19:56 +0100

    some css

--- app.rb
+++ app.rb
@@ -2,10 +2,20 @@
 
 require 'sinatra/base'
 require './lib/bowling'
+require './lib/player'
 class BowlingGame < Sinatra::Base
+	enable :sessions
   get '/' do
     erb :main_page
   end
 
+	post '/game' do
+		@player = Player.new(params[:name])
+	end 
+
+	get '/game' do
+		erb :game
+	end 
+	
   run! if app_file == $PROGRAM_NAME
 end

c2095a712c4e6d8b96e1f40d4fafa2eef0cb5bf2
Johnny Yip <jyippy2012@gmail.com>
2020-10-04 12:24:09 +0100

    Spare refactor

--- lib/game.rb
+++ lib/game.rb
@@ -12,14 +12,27 @@ class Game
         result = 0
         rollIndex = 0
         10.times do
-            if @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
-                result += @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+            if spare?(rollIndex)
+                result += spare_score(rollIndex)
             else
-                result += @rolls[rollIndex] + @rolls[rollIndex + 1]
+                result += frame_score(rollIndex)
             end
             rollIndex += 2
         end
-
         result
     end
+
+    def spare?(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1] == 10
+    end
+
+    def spare_score(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1] + @rolls[rollIndex + 2]
+    end
+
+    def frame_score(rollIndex)
+        @rolls[rollIndex] + @rolls[rollIndex + 1]
+    end
+
+
 end

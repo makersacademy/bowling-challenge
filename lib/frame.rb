529b0488b01d308e0538b42b7295cc6ccdfa08a6
Veronica Lee <veronicavlee@icloud.com>
2020-10-04 10:35:27 +0100

    Refactor add_roll to make it drier

--- lib/frame.rb
+++ lib/frame.rb
@@ -8,10 +8,18 @@ class Frame
   end
 
   def add_roll(roll)
-    raise "No more rolls for this frame. Already had 2 rolls." if @rolls.count >= 2
-    raise "No more rolls for frame. Already bowled 10 pins." if @rolls.reduce(0) { |sum, num| sum + num.pins } >= 10
-    raise "Cannot roll more than 10 pins in this frame." if @rolls.reduce(0) { |sum, num| sum + num.pins } + roll.pins > 10
+    raise "No more rolls, already had 2." if num_rolls >= 2
+    raise "No more rolls, already bowled 10 pins." if running_score >= 10
+    raise "Invalid roll, cannot bowl more than 10 pins in this frame." if running_score + roll.pins > 10
     @rolls << roll
+  end
+
+  def num_rolls
+    @rolls.count
+  end
+
+  def running_score
+    @rolls.reduce(0) { |sum, num| sum + num.pins }
   end
 
 end

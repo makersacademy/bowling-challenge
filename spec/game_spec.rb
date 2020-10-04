2f3f5481ebcd868789c718d6a544107ddd38bcff
Johnny Yip <jyippy2012@gmail.com>
2020-10-04 13:48:46 +0100

    Extra test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -39,7 +39,30 @@ describe Game do
             12.times{game.roll(10)}
             expect(game.score).to eq 300
         end
-
+        
+        it 'Roll a full game from example' do
+            game = Game.new
+            game.roll(1)
+            game.roll(4)
+            game.roll(4)
+            game.roll(5)
+            game.roll(6)
+            game.roll(4)
+            game.roll(5)
+            game.roll(5)
+            game.roll(10)
+            game.roll(0)
+            game.roll(1)
+            game.roll(7)
+            game.roll(3)
+            game.roll(6)
+            game.roll(4)
+            game.roll(10)
+            game.roll(2)
+            game.roll(8)
+            game.roll(6)
+            expect(game.score).to eq 133
+        end
     end
 
 end

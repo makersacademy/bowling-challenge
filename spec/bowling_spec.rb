f91c7cfb74c1c4d3c8ef5367dbae241f4ef4888a
Victoria Jusko <vikjusko@gmail.com>
2020-10-04 18:37:06 +0100

    Added all the game logic and tests

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,7 +2,7 @@ require "./lib/bowling"
  describe Bowling do
 	subject(:game) { described_class.new }
 
-	it "can calculate correct score" do 
+	it "can calculate correct score when never scored" do 
 		20.times{game.roll(0)}
 		expect(game.score).to eq 0
 	end
@@ -11,4 +11,20 @@ require "./lib/bowling"
 		12.times{game.roll(10)}
 		expect(game.score).to eq 300
 	end
+
+	it "scores a spare" do 
+		game.roll(4)
+		game.roll(6)
+		game.roll(2)
+		game.roll(4)
+		16.times{game.roll(0)}
+		expect(game.score).to eq 18
+	end
+
+	it "scores correcly" do
+		game.roll(2)
+		game.roll(5)
+		18.times{game.roll(0)}
+		expect(game.score).to eq 7
+	end 
 end 

daa927104e07bb77faf63fcb4e5b81597e44936f
Veronica Lee <veronicavlee@icloud.com>
2020-10-10 21:11:30 +0100

    Add addRoll to Scorecard

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -1,25 +1,57 @@
 'use strict';
 
 describe('Scorecard', function() {
-  let scorecard;
+  let scorecard, frame, rolls = null;
 
   beforeEach(function() {
-    scorecard = new Scorecard();
+    frame = {
+      number: 1,
+      addRoll: function(roll) { rolls = [roll] },
+      isComplete: function() { return false; }
+    }
+    spyOn(frame, 'addRoll');
+
+    scorecard = new Scorecard([frame]);
   });
 
-  describe('calculateScore', function() {
-    it('returns 0 for a game of 20 gutter balls (0 pins)', function() {
-      for(let i = 0; i < 20; i++){
-        scorecard.addRoll(0);
-      }
-      expect(scorecard.calculateScore()).toEqual(0)
-    });
+  describe('#addRoll(pins)', function() {
+    it('sends a request to the current frame to add the roll', function() {
+      scorecard.addRoll(3);
+      expect(frame.addRoll).toHaveBeenCalled();
+    })
 
-    it('returns 20 for a game of 20 rolls that knocked down 1 pin each', function() {
-      for(let i = 0; i < 20; i++){
-        scorecard.addRoll(1);
+    it('does not change frame if the current one is incomplete', function() {
+      scorecard.addRoll(3);
+      expect(scorecard._currentFrame().number).toEqual(1);
+    })
+
+    it('moves to the next frame if the current one is complete', function() {
+      frame = {
+        number: 1,
+        addRoll: function() { return true; },
+        isComplete: function() { return true; }
       }
-      expect(scorecard.calculateScore()).toEqual(20)
-    });
-  });
+      scorecard = new Scorecard([frame]);
+      scorecard.addRoll(5);
+      expect(scorecard._currentFrame().number).toEqual(2);
+    })
+  })
+
+  
+
+  // describe('calculateScore', function() {
+  //   it('returns 0 for a game of 20 gutter balls (0 pins)', function() {
+  //     for(let i = 0; i < 20; i++){
+  //       scorecard.addRoll(0);
+  //     }
+  //     expect(scorecard.calculateScore()).toEqual(0)
+  //   });
+  //
+  //   it('returns 20 for a game of 20 rolls that knocked down 1 pin each', function() {
+  //     for(let i = 0; i < 20; i++){
+  //       scorecard.addRoll(1);
+  //     }
+  //     expect(scorecard.calculateScore()).toEqual(20)
+  //   });
+  // });
 });
--- src/scorecard.js
+++ src/scorecard.js
@@ -1,18 +1,32 @@
 class Scorecard {
-  constructor() {
-    this.rolls = []
+  constructor(frames = [new Frame(1)]) {
+    this.frames = frames;
   }
 
   addRoll(pins) {
-    let roll = new Roll(pins)
-    this.rolls.push(roll)
+    let roll = new Roll(pins);
+    this._currentFrame().addRoll(roll);
+    this._updateFrames();
   }
 
   calculateScore() {
-    let score = 0
-    this.rolls.forEach(function(roll) {
-      score += roll.pins
-    })
-    return score
+    // let score = 0
+    // this.rolls.forEach(function(roll) {
+    //   score += roll.pins
+    // })
+    // return score
+  }
+  //
+
+  _updateFrames() {
+    console.log(this._currentFrame().number)
+    if (this._currentFrame().isComplete() && this._currentFrame().number !== 10) {
+      let nextFrameNum = this._currentFrame().number + 1;
+      this.frames.push(new Frame(nextFrameNum));
+    }
+  }
+
+  _currentFrame() {
+    return this.frames[this.frames.length - 1];
   }
 }

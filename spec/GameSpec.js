36921d06bf7bad026b6c6087715c044572ae8560
farhaan-ali <fa8g12@gmail.com>
2020-10-10 16:09:41 +0100

    added functionality roll and addFrame
    roll adds the score to the frame and has tests associated with it
    addFrame adds the frame to the score to be used later

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,1 +1,32 @@
-describe("Game", function () {});
+describe("Game", function () {
+  var game;
+
+  beforeEach(function () {
+    game = new Game();
+  });
+
+  describe("Should store the players score for each frame", function () {
+    it("stores the first roll of the player in a frame", function () {
+      game.roll(5);
+      expect(game.frame).toEqual([5]);
+    });
+    it("stores the second roll of the player in a frame", function () {
+      game.roll(5);
+      game.roll(6);
+      expect(game.frame).toEqual([5, 6]);
+    });
+    it("rolling a number greater than 10 throws an alert", function () {
+      expect(game.roll(11)).toBe(alert("Score must be between 0-10"));
+    });
+  });
+
+  describe("Store frames into the game instance variable", function () {
+    it("stores the first frame into the game instance variable", function () {
+      game.roll(5);
+      game.roll(6);
+      game.roll(7);
+      expect(game.gameFrames).toEqual([[5, 6]]);
+      expect(game.frame).toEqual([7]);
+    });
+  });
+});
---
+++ src/Game.js
@@ -0,0 +1,22 @@
+class Game {
+  constructor() {
+    this.frame = [];
+    this.gameFrames = [];
+  }
+
+  roll(score) {
+    if (this.frame.length == 2) {
+      this.addFrame();
+      this.frame.push(score);
+    } else if (score <= 10 && score >= 0) {
+      this.frame.push(score);
+    } else {
+      alert("Score must be between 0-10");
+    }
+  }
+
+  addFrame() {
+    this.gameFrames.push(this.frame);
+    this.frame = [];
+  }
+}

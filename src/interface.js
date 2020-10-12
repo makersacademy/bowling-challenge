667cff81b5e99f42b4ade5bc4e8ac2cce536aa90
Victoria Jusko <vikjusko@gmail.com>
2020-10-12 08:48:56 +0100

    filling out the frame

--- src/interface.js
+++ src/interface.js
@@ -9,52 +9,72 @@ $(function() {
 	$('#roll-0').on('click', function () {
 		bowling.roll(0);
 		getScore('/');
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-1').on('click', function () {
 		bowling.roll(1);
 		getScore(1);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-2').on('click', function () {
 		bowling.roll(2);
 		getScore(2);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-3').on('click', function () {
 		bowling.roll(3);
 		getScore(3);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-4').on('click', function () {
 		bowling.roll(4);
 		getScore(4);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-5').on('click', function () {
 		bowling.roll(5);
 		getScore(5);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-6').on('click', function () {
 		bowling.roll(6);
 		getScore(6);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-7').on('click', function () {
 		bowling.roll(7);
 		var num = 7;
 		getScore(7);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-8').on('click', function () {
 		bowling.roll(8);
 		getScore(8);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
  
 	$('#roll-9').on('click', function () {
 		bowling.roll(9);
 		getScore(9);
+		var n = bowling.currentTotal();
+		fillFrame(n);
 	});
 
 	$('#roll-10').on('click', function () {

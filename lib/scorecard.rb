ae827e5dc88afe53cbc4c4e5a56f856cac88b293
Cameron Logie <cameronlogie24@gmail.com>
2020-11-01 18:03:38 +0000

    scorecard sums the total after each round using calculator

--- lib/scorecard.rb
+++ lib/scorecard.rb
@@ -1,10 +1,11 @@
 class Scorecard
 
-  attr_reader :round_score, :rounds
+  attr_reader :round_score, :rounds, :total
 
   def initialize
     @round_score = 0
     @rounds = []
+    @total = 0
   end
 
   def score(pins)
@@ -13,6 +14,16 @@ class Scorecard
 
   def score_round(round)
     @rounds.push(round)
+    sum_total
+  end
+
+private
+
+  def sum_total
+    @total = 0
+    @rounds.each do |round|
+      @total += Calculator.new.add_round(round.round[:first_throw], round.round[:second_throw])
+    end
   end
 
 end
--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,12 +1,19 @@
 describe Scorecard do
+  let(:first_round) { Round.new(5, 4) }
+
   it ' #score can store one bowl as a round score' do
     expect{ subject.score(5) }.to change { subject.round_score }.by 5
   end
 
   it 'stores each round within an array' do
-    first_round = Round.new(5,4)
     expect(subject.rounds).to eq []
     subject.score_round(first_round)
     expect(subject.rounds).to eq [first_round]
   end
+
+  it 'adds all of the scores from the round into a total' do
+    subject.score_round(first_round)
+    expect(subject.total).to eq 9
+  end
+
 end

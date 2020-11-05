3b4b1fca47272e91288cc491cd1cc598b30dfbc3
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-05 12:36:58 +0000

    Added functions for adding up and tracking frames and totals, tested for total score of game

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,9 +7,23 @@ describe('Bowling', function () {
   });
 
   it('score returns 0', function () {
-    expect(bowl.score(0)).toEqual(0);
+    bowl.roll(0)
+    expect(bowl.getCurrentRollScore()).toEqual([0]);
   });
 
+  it('scores 20 when only 1s are rolled', function () {
+    for (let roll = 0; roll < 20; roll++) {
+      bowl.roll(1);
+    };
+    expect(bowl.totalScore()).toEqual(20);
+  });
+  // THIS METHOD NEEDS THE TOTAL METHOD (AT THE MOMENT IT RETURNS AN ARRAY OF 20 "0'S")
+  // it('scores a gutter game', function () {
 
+  //   for(let roll = 0; roll <= 20; roll++) {
+  //     bowl.roll(0);
+  //   };
+  //   expect(bowl.getCurrentRollScore()).toEqual([0]);
+  // });
 
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -1,9 +1,54 @@
 class Bowling {
 
-  score(pins) {
-    return 0;
+  constructor() {
+    this.rollScore = [];
+    this.index = 0;
+    this.result = 0;
   };
 
+  getCurrentRollScore() {
+    return this.rollScore;
+  };
+
+  getCurrentResult() {
+    return this.result;
+  };
+
+  roll(pins) {
+    this.rollScore.push(pins);
+  };
+
+  totalScore() {
+    return this.rollScore.reduce((a, b) => a + b, 0)
+  };
+
+  currentScore() {
+    this.result = 0;
+    this.index = 0;
+
+    this.rollScore.forEach(roll => {
+      this.result += this.frameScore()
+      this.index += 2
+    });
+  };
+
+  frameScore() {
+    return this.rollScore[this.index] + this.rollScore[this.index + 1];
+  };
+
+  strikeScore() {
+    return 10 + this.rollScore[this.index +1] + this.rollScore[this.index +2]
+  };
+
+  // SPARE
+  isSpare() {
+    this.rollScore[this.index] + this.rollScore[this.index + 1] === 10;
+  };
+
+  spareScore() {
+    return 10 + this.rollScore[this.index +1];
+  };
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 };

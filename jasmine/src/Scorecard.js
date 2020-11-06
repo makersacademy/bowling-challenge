81e5d59ddbc90d83e713f2d9dd1e3dcfb3a8e671
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-06 20:46:51 +0000

    Refactored with variables.  all tests pass

--- jasmine/src/Scorecard.js
+++ jasmine/src/Scorecard.js
@@ -2,9 +2,9 @@ class Scorecard{
 
   constructor(){
     this.score = {'1.1':0, '1.2':0, '2.1':0, '2.2':0, '3.1':0, '3.2':0, '4.1':0, '4.2':0, '5.1':0, '5.2':0, '6.1':0, '6.2':0, '7.1':0, '7.2':0, '8.1':0, '8.2':0, '9.1':0, '9.2':0, '10.1':0, '10.2':0, '10.3':0}
-    this.frame = 0
+    this.currentframe = 0
     this.sum = 0
-    this.totalframe = 0
+    this.currentframe = 0
   };
 
   resetScorecard() {
@@ -13,7 +13,7 @@ class Scorecard{
 
   addRoll(turn, pins) {
      this.turn = turn
-     this.frame = parseInt(this.turn)
+     this.currentframe = parseInt(this.turn)
      this.score[turn] = pins
   };
 
@@ -25,17 +25,28 @@ class Scorecard{
     return (input1 + input2) === 10 && input1 > 0 || input2 > 0
   };
 
-  total(totalframe) {
-    this.totalframe = totalframe
-    while (this.totalframe > 0) {
-    this.sum += this.score[this.totalframe+0.1] + this.score[this.totalframe+0.2]
-    if (this.score[this.totalframe+0.1] === 10 || this.score[this.totalframe+0.2] === 10) {
-      this.sum += (this.score[this.totalframe+1.1] + this.score[this.totalframe+1.2])
-    } else if (this.score[this.totalframe+0.1] + this.score[this.totalframe+0.2] === 10) {
-      this.sum += this.score[this.totalframe+1.1];
+  total(frame) {
+    this.frame = frame
+    while (this.frame > 0) {
+    let [currRoll1, currRoll2, nextRoll1, nextRoll2] = [this.score[this.frame+0.1], this.score[this.frame+0.2], this.score[this.frame+1.1], this.score[this.frame+1.2]];
+    this.sum += currRoll1 + currRoll2
+    if (currRoll1 === 10 || currRoll2 === 10) {
+      if (nextRoll1 != null || nextRoll2 != null) {
+        this.sum += nextRoll1 + nextRoll2
+      };
+    } else if (currRoll1 + currRoll2 === 10) {
+      if (nextRoll1 != null) {
+        this.sum += nextRoll1
+      }
     }
-    this.totalframe -= 1;
+    this.frame -= 1;
     };
+    let [tenthRoll1, tenthRoll2, tenthRoll3] = [this.score[this.frame+10.1], this.score[this.frame+10.2], this.score[this.frame+10.3]]
+    if (tenthRoll1+tenthRoll2 === 10){
+      this.sum += tenthRoll3;
+    } else if (tenthRoll1 === 10 && tenthRoll2 == 10) {
+      this.sum += tenthRoll3;
+      }
     return this.sum
   };
 

862d52b9d2769ad7b4361a7b07977e353d77116b
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-11-06 17:20:00 +0000

    Added bonus calculation.  Tests pass

--- jasmine/spec/scorecardSpec.js
+++ jasmine/spec/scorecardSpec.js
@@ -21,5 +21,30 @@ describe('Scorecard', function() {
     expect(scorecard.score).toEqual({'1.1':0, '1.2':0, '2.1':0, '2.2':0, '3.1':0, '3.2':0, '4.1':0, '4.2':0, '5.1':0, '5.2':0, '6.1':0, '6.2':0, '7.1':0, '7.2':0, '8.1':0, '8.2':0, '9.1':0, '9.2':0, '10.1':0, '10.2':0, '10.3':0})
   });
 
+  it('checks that total adds one frame together', function(){
+    scorecard.addRoll(1.1, 5)
+    scorecard.addRoll(1.2, 3)
+    expect(scorecard.total(1)).toEqual(8)
+  });
+
+  it('checks that total adds 4 frames together', function(){
+    scorecard.addRoll(1.1, 5)
+    scorecard.addRoll(1.2, 3)
+    scorecard.addRoll(2.1, 2)
+    scorecard.addRoll(2.2, 4)
+    scorecard.addRoll(3.1, 6)
+    scorecard.addRoll(3.2, 1)
+    scorecard.addRoll(4.1, 2)
+    scorecard.addRoll(4.2, 1)
+    expect(scorecard.total(4)).toEqual(24)
+  });
+
+  it('checks that a strike on roll 1 on frame 1 adds bonus points of next frame', function(){
+    scorecard.addRoll(1.1, 10)
+    scorecard.addRoll(1.2, 0)
+    scorecard.addRoll(2.1, 5)
+    scorecard.addRoll(2.2, 3)
+    expect(scorecard.total(1)).toEqual(18)
+  });
 
 });
--- jasmine/src/Scorecard.js
+++ jasmine/src/Scorecard.js
@@ -2,6 +2,9 @@ class Scorecard{
 
   constructor(){
     this.score = {'1.1':0, '1.2':0, '2.1':0, '2.2':0, '3.1':0, '3.2':0, '4.1':0, '4.2':0, '5.1':0, '5.2':0, '6.1':0, '6.2':0, '7.1':0, '7.2':0, '8.1':0, '8.2':0, '9.1':0, '9.2':0, '10.1':0, '10.2':0, '10.3':0}
+    this.frame = 0
+    this.sum = 0
+    this.totalframe = 0
   };
 
   resetScorecard() {
@@ -12,6 +15,28 @@ class Scorecard{
      this.turn = turn
      this.frame = parseInt(this.turn)
      this.score[turn] = pins
+  };
+
+  isStrike(input1, input2) {
+    return input1 === 10 || input2 === 10
+  };
+
+  isSpare(input1, input2) {
+    return (input1 + input2) === 10 && input1 > 0 || input2 > 0
+  };
+
+  total(totalframe) {
+    this.totalframe = totalframe
+    while (this.totalframe > 0) {
+    this.sum += this.score[this.totalframe+0.1] + this.score[this.totalframe+0.2]
+    if (this.score[this.totalframe+0.1] === 10 || this.score[this.totalframe+0.2] === 10) {
+      this.sum += (this.score[this.totalframe+1.1] + this.score[this.totalframe+1.2])
+    } else if (this.score[this.totalframe+0.1] + this.score[this.totalframe+0.2] === 10) {
+      this.sum += this.score[this.totalframe+1.1];
+    }
+    this.totalframe -= 1;
+    };
+    return this.sum
   };
 
 };

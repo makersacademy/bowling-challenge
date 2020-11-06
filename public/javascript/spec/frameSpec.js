ab7b74a124db68cbb471d56273506504b04fe2dd
charlie-galb <galbraith.charlie@gmail.com>
2020-11-06 16:01:30 +0000

    Feature: Frame object knows if it is a strike or a spare

--- public/javascript/spec/frameSpec.js
+++ public/javascript/spec/frameSpec.js
@@ -17,4 +17,27 @@ describe('frame', function(){
     frame.recordRoll(2)
     expect(frame.points()).toEqual(9)
   })
+
+  it('knows if it is a strike', function(){
+    frame.recordRoll(10)
+    expect(frame.isStrike()).toEqual(true)
+  })
+
+  it('knows if it is not a strike', function(){
+    frame.recordRoll(7)
+    frame.recordRoll(2)
+    expect(frame.isStrike()).toEqual(false)
+  })
+
+  it('knows if it is a spare', function(){
+    frame.recordRoll(9)
+    frame.recordRoll(1)
+    expect(frame.isSpare()).toEqual(true)
+  })
+
+  it('knows if it is not a spare', function(){
+    frame.recordRoll(7)
+    frame.recordRoll(2)
+    expect(frame.isSpare()).toEqual(false)
+  })
 })
--- public/javascript/src/frame.js
+++ public/javascript/src/frame.js
@@ -4,17 +4,31 @@ class Frame{
 
   constructor(){
     this.contents = [];
-    this.points;
   };
 
   recordRoll(pins){
     this.contents.push(pins)
   };
 
-  points(_reducer){
+  points(){
     this.points = this.contents.reduce((a, b) => a + b, 0);
     return this.points;
   };
 
+  isStrike(){
+    if (this.contents.length === 1 && this.points() === 10){
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  isSpare(){
+    if (this.contents.length === 2 && this.points() === 10){
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 }

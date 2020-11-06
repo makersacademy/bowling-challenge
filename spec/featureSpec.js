01f8ee508dd1179c0fc0d876f7a0221afda74c45
Emily Sesto <emilyalicesesto@gmail.com>
2020-11-06 17:04:34 +0000

    can score a gutter game and game of all 2s

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -20,4 +20,18 @@ describe('Feature test', function(){
     }
     expect(game.score()).toEqual(4);
   })
+
+  it('can score a gutter game', function(){
+    for (var i = 0; i < 20; i ++){
+      game.bowl(0);
+    }
+    expect(game.score()).toEqual(0);
+  })
+
+  it('can score a game of all 2s', function(){
+    for (var i = 0; i < 20; i ++){
+      game.bowl(2);
+    }
+    expect(game.score()).toEqual(40);
+  })
 });

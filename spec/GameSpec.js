90a0a3f8c0751bceaeb738351a859961f7090180
jotkafomat <k-j@xl.wp.pl>
2020-11-07 16:30:09 +0000

    add second roll to existing frame

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,9 +6,15 @@ describe('Game', function() {
   });
 
   describe('#roll',function() {
-    it('creates a new frame if a frame is not open and add it to an array',function() {
+    it('creates a new frame if a frame is not open and adds frame to an array',function() {
       game.roll(1);
 
+      expect(game.frames.length).toEqual(1)
+    });
+
+    it('with first roll it creates a new frame and adds to an array, with second roll it adds roll to curently open frame',function() {
+      game.roll(1);
+      game.roll(1);
       expect(game.frames.length).toEqual(1)
     });
 
--- src/Game.js
+++ src/Game.js
@@ -5,7 +5,11 @@ class Game {
   }
 
   roll(pinsKnockedDown) {
-    this.addFrame(pinsKnockedDown);
+    if (this.isNewFrameNeeded()) {
+      this.addFrame(pinsKnockedDown);
+    } else {
+    this.addRollToExistingFrame(pinsKnockedDown)
+    }
   }
 
 
@@ -13,6 +17,14 @@ class Game {
     var frame = new Frame();
     frame.roll(pinsKnockedDown)
     this.frames.push(frame);
+  }
+  isNewFrameNeeded() {
+    return this.frames.length === 0 || this.frames[this.frames.length -1].rolls.length === 2
+  }
+
+  addRollToExistingFrame(pinsKnockedDown) {
+    var frame = this.frames[this.frames.length -1]
+    frame.roll(pinsKnockedDown)
   }
 
 };

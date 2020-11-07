89f0c7d07d367058bd471f69f532e318587f534c
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-11-07 15:27:41 +0000

    Tests for strikes and spares passing

--- src/Bowling.js
+++ src/Bowling.js
@@ -2,13 +2,51 @@
 
 class Bowling{
   constructor(){
-    this.rolls = []
+    this.rolls = [];
   }
-  roll(score) {
-    this.rolls.push(score);
+
+  roll(pins) {
+    this.rolls.push(pins);
   };
-  totalScore() {
-    return this.rolls.reduce((a, b) => a + b, 0)
-  };
+
+   totalScore() {
+      let total = 0
+      let rollNumber = 0
+     
+    for (let frames = 0 ; frames < 10; frames++) {
+
+      if(this.isStrike(rollNumber)) {
+        total += this.strikeScore(rollNumber);
+        rollNumber++;
+        continue;
+      }
+      const frameScore = this.rolls[rollNumber] + this.rolls[rollNumber + 1];
+  
+      if(this.isSpare(frameScore)) {
+        total += this.spareScore(rollNumber);
+      }
+      else  {
+        total += frameScore;
+      }
+        rollNumber += 2; 
+      }
+      return total; 
+      }
+
+
+  isStrike(rollNumber) {
+    return this.rolls[rollNumber] === 10;
+  }
+
+  strikeScore(rollNumber) {
+    return 10 + this.rolls[rollNumber + 1] + this.rolls[rollNumber + 2];
+  }
+
+  isSpare(frameScore) {
+    return frameScore === 10;
+  }
+  spareScore(rollNumber) {
+    return 10 + this.rolls[rollNumber + 2];
+  }
 };
 

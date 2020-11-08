2a4ff7152e7d75117d13210af6f9678a0e2d5fe0
Tim Castillo-Gill <timgillviola@gmail.com>
2020-11-08 09:16:56 +0000

    Refactored and added totalScore function

--- src/Bowling.js
+++ src/Bowling.js
@@ -6,49 +6,70 @@ class Bowling {
     this.result = 0;
   };
 
-  getCurrentRollScore() {
-    return this.rollScore;
-  };
+    getCurrentRollScore() {
+      return this.rollScore;
+    };
 
-  getCurrentResult() {
-    return this.result;
-  };
+    getCurrentResult() {
+      return this.result;
+    };
 
-  roll(pins) {
-    this.rollScore.push(pins);
-  };
+    roll(pins) {
+      this.rollScore.push(pins);
+    };
 
-  totalScore() {
-    return this.rollScore.reduce((a, b) => a + b, 0)
-  };
+    totalScore() {
+      for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+        if(isStrike()) {
+          this.strikeScore;
+          this.index ++;
+        } else if(isSpare()) {
+          this.result += this.spareScore;
+          this.index ++;
+        } else {
+          this.result += this.gameScore;
+        }
+      }
+    };
 
-  currentScore() {
-    this.result = 0;
-    this.index = 0;
+    gameScore() {
+        return this.rollScore.reduce((a, b) => a + b, 0);
+    };
 
-    this.rollScore.forEach(roll => {
-      this.result += this.frameScore()
-      this.index += 2
-    });
-  };
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // STRIKE CHECK AND SCORE
+    isStrike() {
+      this.rollScore[this.index] === 10;
+    };
 
-  frameScore() {
-    return this.rollScore[this.index] + this.rollScore[this.index + 1];
-  };
+    strikeScore() {
+      return 10 + this.rollScore[this.index + 1] + this.rollScore[this.index + 2]
+    };
 
-  strikeScore() {
-    return 10 + this.rollScore[this.index +1] + this.rollScore[this.index +2]
-  };
+    // SPARE CHECK AND SCORE
+    isSpare() {
+      this.rollScore[this.index] + this.rollScore[this.index + 1] === 10;
+    };
 
-  // SPARE
-  isSpare() {
-    this.rollScore[this.index] + this.rollScore[this.index + 1] === 10;
-  };
-
-  spareScore() {
-    return 10 + this.rollScore[this.index +1];
-  };
-////////////////////////////////////////////////////////////////////////////////////////////////
-
+    spareScore() {
+      return 10 + this.rollScore[this.index + 1];
+    };
+  //   ////////////////////////////////////////////////////////////////////////////////////////////////
 
 };
+
+// bowlingScore(rolls) {
+//   for (let frame = 0; frame < 10; frame ++) {
+//     if(rolls[this.index] == 10) {
+//       this.result += 10 + rolls[this.index+1] + rolls[this.index +2];
+//     } else if (rolls[this.index] + rolls[this.index+1] === 10) {
+//       this.result += 10 + rolls[this.index+2];
+//       this.index++;
+//     } else {
+//       this.result += rolls[this.index] + rolls[this.index+1];
+//       this.index++;
+//     }
+//     this.index++;
+//   }
+//   return this.result;
+// };

aa6543d8eaa62ea4993ff1fe457674a33c9596ec
charlie-galb <galbraith.charlie@gmail.com>
2020-11-08 09:39:12 +0000

    JQuery fully connected

--- public/javascript/src/interface.js
+++ public/javascript/src/interface.js
@@ -1,13 +1,80 @@
 $(document).ready(function() {
   var scorecard = new Scorecard;
   updateScore();
+  updateFrame();
 
   $('#one-pin').click(function(){
     scorecard.roll(1);
     updateScore();
+    updateFrame();
   })
+
+  $('#two-pins').click(function(){
+    scorecard.roll(2);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#three-pins').click(function(){
+    scorecard.roll(3);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#four-pins').click(function(){
+    scorecard.roll(4);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#five-pins').click(function(){
+    scorecard.roll(5);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#six-pins').click(function(){
+    scorecard.roll(6);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#seven-pins').click(function(){
+    scorecard.roll(7);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#eight-pins').click(function(){
+    scorecard.roll(8);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#nine-pins').click(function(){
+    scorecard.roll(9);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#ten-pins').click(function(){
+    scorecard.roll(10);
+    updateScore();
+    updateFrame();
+  })
+
+  $('#reset').click(function(){
+    scorecard = new Scorecard;
+    updateScore();
+    updateFrame();
+  })
+
+
 
   function updateScore(){
     $('#score').text(scorecard.calculateScore())
+  }
+  function updateFrame(){
+    $('#current-frame').text(scorecard.getCurrentFrame().number)
   }
 })

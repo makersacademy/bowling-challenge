40bfcbed3f95e2750b906fbc47a8d6d5ae2d4090
Maggie Hibberd <maggiehibberd@gmail.com>
2020-11-09 08:18:11 +0000

    Initial firs part draft, logic not linked yet

--- src/bowling.js
+++ src/bowling.js
@@ -1,10 +1,37 @@
+function sampleFunction() {
+    var user = prompt("SUBTOTAL CAN NOT TOTAL ABOVE 100! Strike and spare points will be totaled up in the 'BONUS POINTS' section, please enter name to confirm understanding");
+    if (user != null) {
+      document.getElementById("greeting").innerHTML =
+        "Thank you, please proceed " + user + "!";
+    }
+  }
+      
+function rollOne() {
+    var scoreInputTwo = document.getElementById("scoreInputTwo")
+    var scoreInputOne = document.getElementById("scoreInputOne")
+    if (scoreInputOne == 10){
+        scoreInputTwo.disabled = true;
+    }
+      else {
+        scoreInputTwo.disabled = false;
+      }
+  }
+
+
 class Scorecard{
     constructor() {
-        this.score = 10
+        this.score = 0
     }
     getScores(givemestuff) {
 
         this.score += givemestuff;
+    //  }
+    //  getScoresTwo(givemestuff) {
+
+    //     this.score += givemestuff;
      }
+
+     
+     
 }
 
--- src/interface.js
+++ src/interface.js
@@ -1,14 +1,23 @@
 
-$ (document).ready(function() {
-    scorecard = new Scorecard();
-    //$("#show_score").text(scorecard.score);
+   var scoreInput = document.getElementById("score")
+   scoreInput.onclick = function(){
+       alert("We have made it here!");
 
-$("#score").click(function(){
-    scorecard.getScores($('#score_input').val());   //so this is getting the value from the user from the textboxt
-    $("#show_score").text(scorecard.score);
-
-});
+   }
 
 
 
-});
+//     scorecard = new Scorecard();
+//     $("#show_score").text(scorecard.score);
+
+// $('#score').click(function(){
+//     scorecard.getScores($('#score_input').val());   //so this is getting the value from the user from the textboxt
+//     $("#show_score").text(scorecard.score);
+// });
+
+// // $('#score').click(function(){
+// //     scorecard.getScoresTwo($('#score_input_two').val());   //so this is getting the value from the user from the textboxt
+// //     $('#show_score').text(scorecard.score);
+
+// // });
+// });

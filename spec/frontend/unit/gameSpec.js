84b05e6391d22f2245c820022c24ca693cc2faf4
elsstrange <ellenabstrange@gmail.com>
2020-11-13 22:24:29 +0000

    Added unit test for #score on Game. It passes.

--- spec/frontend/unit/gameSpec.js
+++ spec/frontend/unit/gameSpec.js
@@ -5,10 +5,19 @@ describe('Game', () => {
     game = new Game()
   })
 
-  describe('#roll', () => {
+  describe('#roll', ()=> {
     it('records the score from a roll', () => {
       game.roll(2)
       expect(game.score()).toEqual(2)
+    })
+  })
+
+  describe('#total', ()=> {
+    it('returns to sum of all recorded scores', ()=> {
+      for(let i = 0; i < 5; i++) {
+        game.roll(i)
+      }
+      expect(game.score()).toEqual(10)
     })
   })
 })

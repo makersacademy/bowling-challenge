b19be89c1b020337d7c53b83e040886fa4001d69
elsstrange <ellenabstrange@gmail.com>
2020-11-13 23:09:26 +0000

    Added feature test for scoring spares

--- spec/frontend/feature/bowlingSpec.js
+++ spec/frontend/feature/bowlingSpec.js
@@ -6,10 +6,18 @@ describe('bowling', () => {
     game = new Game()
   })
 
-  it('records rolls and provides a total score', () => {
+  it('records rolls and provides a total score', ()=> {
     for(i = 0; i < 20; i++) {
       game.roll(2)
     }
     expect(game.score()).toEqual(40)
+  })
+
+  it('adds scoring bonuses for spares', ()=> {
+    for(i = 0; i < 20; i++) {
+      game.roll(5)
+    }
+    game.roll(5)
+    expect(game.score()).toEqual(150)
   })
 })

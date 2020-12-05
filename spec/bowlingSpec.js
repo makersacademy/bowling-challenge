a2e85eb2ae338baace158ccb65ab724076e2712c
Melissa <mystic101@gmail.com>
2020-12-05 11:48:47 +0000

    First test passing with hardcoded 0

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,1 +1,18 @@
 'use strict';
+
+describe('Bowling', function() {
+    var game;
+
+    beforeEach(function() {
+        game = new Bowling();
+    });
+
+
+    it('should return a score of 0 if all rolls were 0', () => {
+        for (var i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+        expect(game.score()).toEqual(0);
+    });
+
+});
--- src/bowling.js
+++ src/bowling.js
@@ -1,1 +1,16 @@
 'use strict';
+
+class Bowling {
+    constructor() {
+        this.scorecard = []
+    }
+
+    roll(number) {
+        this.scorecard.push(number);
+    }
+
+    score() {
+        return 0
+    }
+
+}

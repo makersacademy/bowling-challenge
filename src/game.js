2f0491feae444c62d56508ee47b3ac49b55bdbea
Adam G <adamwizzy@hotmail.co.uk>
2020-12-05 14:39:05 +0000

    planned game class

---
+++ src/game.js
@@ -0,0 +1,24 @@
+class Game {
+  constructor() {
+    this.frames = [];
+    this.runningScore = 0;
+  };
+
+  getRunningScore() {};
+
+  currentFrameNumber() {};
+
+  currentFrameObject() {};
+
+  newFrame(frame) {};
+
+  knocked(pins) {};
+
+  updateScore() {};
+
+  _normalUpdate() {};
+
+  _doubleUpdate() {};
+
+  _tripleUpdate() {};
+}

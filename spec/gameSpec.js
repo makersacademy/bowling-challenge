70d39b20609bbe1c6615a16d384e034377e6a486
francescoFH <francescofacca@gmail.com>
2020-12-05 17:47:14 +0000

    Passed test that checks pins and roll number are decresing

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,19 +1,36 @@
-describe('Game', function(){
+describe('Game', function() {
+
   var game;
 
   beforeEach(function() {
     game = new Game();
   });
 
-  it('begins with frame number one', function() {
-    expect(game.frameNumber).toEqual(1);
+  describe('new game', function() {
+    it('begins with frame number one', function() {
+      expect(game.frameNumber).toEqual(1);
+    });
+
+    it('begins with ten pins', function() {
+      expect(game.pins).toEqual(10);
+    });
+
+    it('begins with two rolls', function() {
+      expect(game.rollNumber).toEqual(2);
+    });
   });
 
-  it('begins with ten pins', function() {
-    expect(game.pins).toEqual(10);
-  });
+  describe('new roll', function() {
+    beforeEach(function() {
+      game.roll(6);
+    });
 
-  it('begins with two rolls', function() {
-    expect(game.rollNumber).toEqual(2);
+    it('decreses pins when user rolls', function() {
+      expect(game.pins).toEqual(4);
+    });
+
+    it('decreses the roll number', function() {
+      expect(game.rollNumber).toEqual(1);
+    });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,8 +1,15 @@
+'use strict';
+
 class Game {
 
   constructor() {
     this.frameNumber = 1
     this.pins = 10
     this.rollNumber = 2
+  };
+
+  roll(score) {
+    this.pins -= score;
+    this.rollNumber --;
   };
 };

2403741010edd216eacc7c7ee57541764515fd12
Georgie Horth <georgiehorth@gmail.com>
2020-12-06 15:13:57 +0000

    refactored tests up to spare

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -27,7 +27,17 @@ describe('bowling', () => {
     for (var i = 0; i < 17; i++){
       game.roll(0);
     }
-    expect(game.score()).toEqual(18)
+    expect(game.score()).toEqual(18);
   });
+
+  // it('can roll a strike', () => {
+  //   game.roll(10);
+  //   game.roll(10);
+  //   game.roll(5);
+  //   for (var i = 0; i < 13; i++){
+  //     game.roll(0);
+  //   }
+  //   expect(game.score()).toEqual(45);
+  // });
 
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -10,14 +10,26 @@ class BowlingGame{
   score() {
     let result = 0;
     let rollIndex = 0;
+    let game = this;
       for (var i = 0; i < 10; i++) {
-        if (this.rolls[rollIndex] + this.rolls[rollIndex+1] === 10) {
-          result += this.rolls[rollIndex] + this.rolls[rollIndex+1] + this.rolls[rollIndex+2]
+        if (isSpare()) {
+          result += getSpareScore()
         } else {
-          result += this.rolls[rollIndex] + this.rolls[rollIndex+1]
+          result += getFrameScore()
         }
         rollIndex += 2
       }
-    return result
+    return result;
+    function isSpare() {
+      return game.rolls[rollIndex] + game.rolls[rollIndex+1] === 10;
+    }
+    function getSpareScore () {
+      return 10 + game.rolls[rollIndex+2];
+    }
+
+    function getFrameScore() {
+      return game.rolls[rollIndex] + game.rolls[rollIndex+1]
+    }
   }
+
 }

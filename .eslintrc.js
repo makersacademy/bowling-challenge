e82291f9ed5ac3850baf11f091499408cfe4c3e6
Samantha <samanthagottlieb31@gmail.com>
2020-12-06 21:27:04 +0000

    Update ESLint config file to using CommonJS
    Using require/exports rather than JavaScript modules (import/export)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
     "env": {
         "browser": true,
+        "commonjs": true,
         "es2021": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+        "ecmaVersion": 12
     },
     "rules": {
     }

3be1602cbc6b86da8b567b61f90b7310760e5be5
Alex Withington-Smith <alex@awithsmith.com>
2020-12-06 18:27:39 +0000

    added skeleton front end

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 9292;
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
     res.render('../views/index');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
 app.listen(port);

00d4f993b26448eebe41a2e211e1feecf1bb1251
stringiest <lucyjstringer@gmail.com>
2020-12-06 20:09:21 +0000

    updated view to show current frame number

--- interface.js
+++ interface.js
@@ -7,11 +7,17 @@ $(document).ready(function() {
     var pins = parseInt(pinsString);
     console.log(pins);
     game.roll(pins);
+    updateFrameNumberInterface();
   });
 
   $('#final-score').on('click', function() { // event listener
     $('#score').text(game.score());
   });
 
-  
+  function updateFrameNumberInterface() {
+    $('#frame').text(Math.floor(game.frameNumber));
+  }
+
+  updateFrameNumberInterface(1); // sets default frame number as 1
+
 });
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -3,10 +3,13 @@ class BowlingGame {
 
   constructor() {
     this.rolls = [];
+    this.frameNumber = 1;
   }
 
   roll(pins) {
     this.rolls.push(pins);
+    console.log('in the roll method');
+    this.updateFrameNumber();
   }
 
   score() {
@@ -26,6 +29,26 @@ class BowlingGame {
     }
     console.log(result);
     return result;
+  }
+
+  updateFrameNumber() {
+    var rollIndex = 0;
+    this.frameNumber = 1;
+    console.log(this.rolls);
+    for (var i = 0; i < this.rolls.length; i++) {
+      console.log('in for loop');
+      console.log('printing rollIndex next');
+      console.log(rollIndex);
+      if (this._isStrike(rollIndex)) {
+        rollIndex += 1;
+        this.frameNumber += 1;
+      } else {
+        rollIndex += 1;
+        this.frameNumber += 0.5;
+      }
+    }
+    console.log(this.frameNumber);
+    return this.frameNumber;
   }
 
   _isSpare(rollIndex) {

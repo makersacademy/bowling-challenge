ebe2dbd0e1bc4638e035cfcb08e91b760e1ec77e
Samantha <samanthagottlieb31@gmail.com>
2020-12-07 10:07:56 +0000

    Create constructor properties and addScore method
    Adding totalScore, scorecard, frame and rollNumber properties, and adding method to add score to frame

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,5 +10,41 @@ describe('Game', () => {
 
   it('creates an instance of Game', () => {
     expect(game).toBeInstanceOf(Game);
-  }); 
+  });
+
+  describe('totalScore', () => {
+    it('has a total score of 0 by default', () => {
+      expect(game.totalScore).toEqual(0);
+    })
+  });
+
+  describe('scorecard', () => {
+    it('has an empty scorecard by default', () => {
+      expect(game.scorecard).toEqual([]);
+    })
+  });
+
+  describe('rollNumber', () => {
+    it('has a roll number of 1 by default', () => {
+      expect(game.rollNumber).toEqual(1);
+    })
+
+    it('increases the roll number as each score is added', () => {
+      game.addScore(4);
+      game.addScore(3);
+      expect(game.rollNumber).toEqual(3);
+    })
+  });
+
+  describe('#addScore', () => {
+    it('instructs a game to add a score', () => {
+      expect(game.addScore).toBeDefined();
+    });
+
+    it('adds scores to the frame', () => {
+      game.addScore(4);
+      game.addScore(6);
+      expect(game.frame).toEqual([4, 6])
+    });
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,7 +1,16 @@
 'use strict';
 
 class Game {
-
-};
+  constructor() {
+    this.totalScore = 0
+    this.scorecard = []
+    this.frame = []
+    this.rollNumber = 1
+  }
+  addScore(score) {
+    this.frame.push(score)
+    this.rollNumber += 1
+  }
+}
 
 module.exports = Game;

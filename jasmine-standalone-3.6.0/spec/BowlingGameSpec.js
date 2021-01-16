21e4e7576b9f12cff071e22693843d00ee5ef26e
calavell <calavell@tcd.ie>
2021-01-16 14:38:53 +0000

    added methods to check if previous rolls are strikes or spares

--- jasmine-standalone-3.6.0/spec/BowlingGameSpec.js
+++ jasmine-standalone-3.6.0/spec/BowlingGameSpec.js
@@ -3,7 +3,7 @@ describe("BowlingGame", function() {
 
   beforeEach(function(){
     bowlingGame = new BowlingGame;
-  })
+  });
 
   describe("addRolls", function() {
     it("adds inputted rolls to the frame", function(){
@@ -33,5 +33,46 @@ describe("BowlingGame", function() {
       expect(bowlingGame.getGameScore()).toEqual(0);
     });
   });
+
+  describe("isPreviousFrameSpare", function() {
+    it("will return true if previous roll was a spare", function() {
+      bowlingGame.addRolls(4, 6);
+      bowlingGame.addRolls(3, 4);
+      expect(bowlingGame.isPreviousFrameSpare()).toEqual(true);
+      // maybe use spies with this method later on when testing adding a bonus, same with below tests in describe block
+    });
+
+    it("will return false if previous roll was neither spare nor strike", function() {
+      bowlingGame.addRolls(4, 3);
+      bowlingGame.addRolls(3, 4);
+      expect(bowlingGame.isPreviousFrameSpare()).toEqual(false);
+    });
+
+    it("will return false if previous roll was a strike", function() {
+      bowlingGame.addRolls(10);
+      bowlingGame.addRolls(3, 4);
+      expect(bowlingGame.isPreviousFrameSpare()).toEqual(false);
+    });
+  });
+
+  describe("isPreviousFrameStrike", function() {
+    it("will return true if previous roll was a strike", function() {
+      bowlingGame.addRolls(10);
+      bowlingGame.addRolls(3, 4);
+      expect(bowlingGame.isPreviousFrameStrike()).toEqual(true);
+    });
+
+    it("will return false if previous roll was a spare", function() {
+      bowlingGame.addRolls(5, 5);
+      bowlingGame.addRolls(3, 4);
+      expect(bowlingGame.isPreviousFrameStrike()).toEqual(false);
+    });
+
+    it("will return false if previous roll neither spare nor strike", function() {
+      bowlingGame.addRolls(5, 2);
+      bowlingGame.addRolls(3, 4);
+      expect(bowlingGame.isPreviousFrameStrike()).toEqual(false);
+    });
+  })
 
 });
--- jasmine-standalone-3.6.0/src/BowlingGame.js
+++ jasmine-standalone-3.6.0/src/BowlingGame.js
@@ -5,15 +5,34 @@ class BowlingGame {
     this.frameScores = [];
   }
 
-  addRolls(roll1, roll2= 0) {
-    this.frameRolls.push([roll1, roll2]);
-    this.frameScores.push(roll1 + roll2);
+  addRolls(roll1, roll2= 'no throw') {
+    if (roll2 === 'no throw') {
+      this.frameRolls.push([roll1]);
+      this.frameScores.push(roll1);
+    } else {
+      this.frameRolls.push([roll1, roll2]);
+      this.frameScores.push(roll1 + roll2);
+    }
   }
 
   getGameScore() {
     return this.frameScores.reduce(function(a,b) {
-      return a + b
+      return a + b;
     }, 0);
+  }
+
+  getFrameTotal(array) {
+    return array.reduce(function(a,b) {
+      return a + b;
+    }, 0);
+  }
+
+  isPreviousFrameSpare() {
+    return this.getFrameTotal(this.frameRolls[this.frameRolls.length - 2]) === 10 && this.frameRolls[this.frameRolls.length - 2].length === 2;
+  }
+
+  isPreviousFrameStrike() {
+    return this.getFrameTotal(this.frameRolls[this.frameRolls.length - 2]) === 10 && this.frameRolls[this.frameRolls.length - 2].length === 1;
   }
 
 }

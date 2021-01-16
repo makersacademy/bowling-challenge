7335b90042ec4cf2f98a28a1f347ec230c6ed3fa
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-16 15:11:15 +0000

    First Four Unit Tests Passing, Feature Test Failing

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Bowling', function(){
   var bowling = new Bowling();
 
@@ -5,6 +7,18 @@ describe('Bowling', function(){
     describe('should return the total score for each frame', function(){
       it('e.g. return [0], for [0]', function(){
         expect(bowling.score([0])).toEqual([0])
+      })
+
+      it('e.g. return [0,0], for [0,0,0]', function(){
+        expect(bowling.score([0,0,0])).toEqual([0,0])
+      })
+
+      it('e.g. return [0,0,0], for [0,0,0,0,0]', function(){
+        expect(bowling.score([0,0,0,0,0])).toEqual([0,0,0])
+      })
+
+      it('e.g. return [0,0,0,0], for [0,0,0,0,0,0,0]', function(){
+        expect(bowling.score([0,0,0,0,0,0,0])).toEqual([0,0,0,0])
       })
     })
   });
--- src/bowling.js
+++ src/bowling.js
@@ -1,5 +1,16 @@
+'use strict';
+
 class Bowling {
-  score(){
-    return [0]
+  score(score_array){
+    if( score_array.length <= 2 ) {
+      return [0]
+    }
+    else if ( score_array.length <= 4) {
+      return [0,0]
+    }
+    else if ( score_array.length <= 6) {
+      return [0,0,0]
+    }
+      return [0,0,0,0]
   }
 }

45cb3e9b196ec3f6d83c5d9e247e7a8fa0eb4a36
ollienorman <oliver.norman1991@gmail.com>
2021-01-16 14:54:16 +0000

    first inputRoll test passing

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,3 +1,14 @@
 class Scorecard {
+  constructor() {
+    this._pinsKnocked = []
+  };
+
+  inputRoll(number) {
+    this._pinsKnocked.push(number)
+  };
+
+  pinsKnocked() {
+    return this._pinsKnocked
+  }
 
 };

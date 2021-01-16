30416b7e666f646c03194426848f879ef8ae0f04
xavierloos <xavier.loos04@gmail.com>
2021-01-16 14:24:59 +0000

    gets 20 on roll all ones

--- spec/BowlingGameSpec.js
+++ spec/BowlingGameSpec.js
@@ -3,14 +3,25 @@ describe("Bowling Game", () => {
     beforeEach(() => {
         game = new BowlingGame
     })
-    describe("roll", () => {
+    describe("roll()", () => {
         it("is been defined", () => {
-            expect(game.roll).toBeDefined();
+            expect(game.roll).toBeDefined()
         })
         it("can add a roll to the rollStorage", () => {
-            for (var roll = 1; roll <= 20; roll++)
+            for (var i = 1; i <= 20; i++)
                 game.roll(0)
             expect(game.rollStorage.length).toEqual(20)
+        })
+    })
+    describe("score()", () => {
+        it("is been defined", () => {
+            expect(game.score).toBeDefined()
+        })
+        it("gets 20 on roll all ones", () => {
+            for (var i = 1; i <= 20; i++)
+                game.roll(1)
+            expect(game.rollStorage.length).toEqual(20)
+            expect(game.score()).toEqual(20)
         })
     })
 
--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,8 +1,17 @@
 class BowlingGame {
   constructor() {
     this.rollStorage = []
+    this.result = 0
+    this.rollIndex = 0
   }
   roll(pins) {
     return this.rollStorage.push(pins)
+  }
+  score() {
+    for (var i = 1; i <= 20; i++) {
+      this.result += this.rollStorage[this.rollIndex]
+      this.rollIndex += 1
+    }
+    return this.result
   }
 }

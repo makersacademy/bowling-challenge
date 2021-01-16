4edbc8f938791d64898e7cffa51f3d6952f70f28
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-16 16:23:56 +0000

    Matching spares feature and unit failing tests:

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -12,6 +12,10 @@ describe('Bowling', function(){
       it('e.g. return [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], for [1,1,1,1,1,1,1,1,1,1]', function(){
         expect(bowling.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).toEqual([2,4,6,8,10,12,14,16,18,20])
       })
+
+      it('e.g. return [15,5], for [5, 5, 5]', function(){
+        expect(bowling.score([5, 5, 5])).toEqual([15,5])
+      })
     })
   });
 });

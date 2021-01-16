741dc8202181c25fa957899e102887d97108ae47
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-16 21:20:15 +0000

    Fully refactored with Game class

--- src/bowling.js
+++ src/bowling.js
@@ -2,69 +2,43 @@
 
 class Bowling {
 
-  constructor(frameClass = Frame) {
-    this.frameClass = frameClass;
+  constructor( game = new Game) {
+    this.game = game;
     this.scoreArray = [];
-    this.frameArray = [];
-    this.outputArray = [];
   };
 
   score(scoreArray) {
     this.scoreArray = scoreArray
     this.scoresToFrames()
-    console.log(this.frameArray)
     this.addSpareBonuses()
     this.addStrikeBonuses()
-    this.accumulator()
-
-    return this.outputArray;
+    return this.accumulator()
   };
 
   scoresToFrames() {
     this.scoreArray.forEach((score, index) => {
-      if( this.frameArray.length === 0 ){
-        this.frameArray.push(new this.frameClass(score));
+      if( this.game.isNew() ){
+        this.game.addFrame(score);
       }
-      else if( this.frameArray[this.frameArray.length - 1].isComplete() && this.frameArray.length < 10) {
-        this.frameArray.push(new this.frameClass(score));
+      else if( this.game.currentFrame().isComplete() && this.game.notFrameTen()) {
+        this.game.addFrame(score);
       }
       else {
-        this.frameArray[this.frameArray.length - 1].addRoll(score);
+        this.game.addRoll(score);
       };
     });
   };
 
   addSpareBonuses() {
-    this.frameArray.forEach((frame, index) => {
-      if(this.frameArray[index].isSpare()){
-        frame.addBonus(this.frameArray[index + 1].firstRoll())
-      }
-    });
+    this.game.addSpareBonuses()
   };
 
   addStrikeBonuses() {
-    this.frameArray.forEach((frame, index) => {
-      if(this.frameArray[index].isStrike()) {
-        frame.addBonus(this.frameArray[index + 1].firstRoll())
-        if(this.frameArray[index + 1].isStrike()) {
-          frame.addBonus(this.frameArray[index + 2].firstRoll())
-        }
-        else {
-          frame.addBonus(this.frameArray[index + 1].secondRoll())
-        }
-      }
-    });
+    this.game.addStrikeBonuses()
   };
 
   accumulator() {
-    this.frameArray.forEach((frame, index) => {
-      if(index === 0){
-        this.outputArray.push(this.frameArray[index].totalScore())
-      }
-      else {
-        this.outputArray.push(this.outputArray[index - 1 ] + this.frameArray[index].totalScore())
-      }
-    });
+    return this.game.cumulative()
   }
 
 };

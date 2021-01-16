b4e57bbd898646782c3aefe27a10ff709caa87ad
Chris Whitehouse <chris_whitehouse@hotmail.com>
2021-01-16 15:53:26 +0000

    Unit Tests Refactored

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -9,24 +9,8 @@ describe('Bowling', function(){
 
   describe('#score', function(){
     describe('should return the total score for each frame', function(){
-      it('e.g. return [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], for [0,0,0,0,0,0,0,0,0,0]', function(){
-        expect(bowling.score([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])).toEqual([0,0,0,0,0,0,0,0,0,0])
-      })
-
-      it('e.g. return [1], for [1]', function(){
-        expect(bowling.score([1])).toEqual([1])
-      })
-
-      it('e.g. return [2], for [1,1]', function(){
-        expect(bowling.score([1,1])).toEqual([2])
-      })
-
-      it('e.g. return [2,1], for [1,1,1]', function(){
-        expect(bowling.score([1,1,1])).toEqual([2,1])
-      })
-
-      it('e.g. return [2,2], for [1,1,1,1]', function(){
-        expect(bowling.score([1,1,1,1])).toEqual([2,2])
+      it('e.g. return [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], for [1,1,1,1,1,1,1,1,1,1]', function(){
+        expect(bowling.score([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])).toEqual([2,2,2,2,2,2,2,2,2,2])
       })
     })
   });

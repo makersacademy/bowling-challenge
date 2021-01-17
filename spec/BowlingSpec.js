34c553497ea4a39231d3075f73dae2a155611a77
Sujeevan Rajagopalan <rsujee@MA307.local>
2021-01-17 22:30:23 +0000

    created gutter game

---
+++ spec/BowlingSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe ('The Bowling Game', function() {
+
+  it('can roll a gutter game', function () {
+      var game = new BowlingGame();
+      for(var i = 0; i < 20; i++) {
+        game.roll(0);
+      }
+      expect(game.score()).toBe(0);
+  })
+
+});

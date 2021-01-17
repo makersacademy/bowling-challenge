3c0571519daf21f8d1f826322f51b53c7ee07983
calavell <calavell@tcd.ie>
2021-01-17 13:43:36 +0000

    tidying up tests, getting rid of magic numbers

--- jasmine-standalone-3.6.0/spec/BowlingGameSpec.js
+++ jasmine-standalone-3.6.0/spec/BowlingGameSpec.js
@@ -5,122 +5,70 @@ describe("BowlingGame", function() {
     bowlingGame = new BowlingGame;
   });
 
-  describe("addRolls", function() {
+  describe("playFrame", function() {
     it("adds inputted rolls to the frame", function(){
-      bowlingGame.addRolls(3, 4);
+      bowlingGame.playFrame(3, 4);
       expect(bowlingGame.getGameScore()).toEqual(7);
     });
 
     it("works with a strike", function() {
-      bowlingGame.addRolls(10);
+      bowlingGame.playFrame(10);
       expect(bowlingGame.getGameScore()).toEqual(10);
     });
 
     it("works with a spare", function() {
-      bowlingGame.addRolls(4, 6);
+      bowlingGame.playFrame(4, 6);
       expect(bowlingGame.getGameScore()).toEqual(10);
     });
 
     it("works with a gutter roll", function() {
-      bowlingGame.addRolls(0, 0);
+      bowlingGame.playFrame(0, 0);
       expect(bowlingGame.getGameScore()).toEqual(0);
     });
 
     it("works with a gutter game", function() {
       for (var i = 0; i < 10; i++) {
-        bowlingGame.addRolls(0, 0);
+        bowlingGame.playFrame(0, 0);
       }
       expect(bowlingGame.getGameScore()).toEqual(0);
     });
-  });
 
-  describe("isPreviousFrameSpare", function() {
-    it("will return true if previous roll was a spare", function() {
-      bowlingGame.addRolls(4, 6);
-      bowlingGame.addRolls(3, 4);
-      expect(bowlingGame.isPreviousFrameSpare()).toEqual(true);
-      // maybe use spies with this method later on when testing adding a bonus, same with below tests in describe block
-    });
-
-    it("will return false if previous roll was neither spare nor strike", function() {
-      bowlingGame.addRolls(4, 3);
-      bowlingGame.addRolls(3, 4);
-      expect(bowlingGame.isPreviousFrameSpare()).toEqual(false);
-    });
-
-    it("will return false if previous roll was a strike", function() {
-      bowlingGame.addRolls(10);
-      bowlingGame.addRolls(3, 4);
-      expect(bowlingGame.isPreviousFrameSpare()).toEqual(false);
-    });
-  });
-
-  describe("isPreviousFrameStrike", function() {
-    it("will return true if previous roll was a strike", function() {
-      bowlingGame.addRolls(10);
-      bowlingGame.addRolls(3, 4);
-      expect(bowlingGame.isPreviousFrameStrike()).toEqual(true);
-    });
-
-    it("will return false if previous roll was a spare", function() {
-      bowlingGame.addRolls(5, 5);
-      bowlingGame.addRolls(3, 4);
-      expect(bowlingGame.isPreviousFrameStrike()).toEqual(false);
-    });
-
-    it("will return false if previous roll neither spare nor strike", function() {
-      bowlingGame.addRolls(5, 2);
-      bowlingGame.addRolls(3, 4);
-      expect(bowlingGame.isPreviousFrameStrike()).toEqual(false);
-    });
-  });
-// for the below describe block, need to try to use spies to mock for previous frames being strikes and spares
-  describe("addBonusScores", function() {
     it("adds first throw of latest frame to total score of the previous frame if the previous frame was a spare", function() {
-      bowlingGame.addRolls(4, 6);
-      bowlingGame.addRolls(5, 2);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(4, 6);
+      bowlingGame.playFrame(5, 2);
       expect(bowlingGame.getGameScore()).toEqual(22);
     });
 
     it("does not add first throw of latest frame to total score of the previous frame unless the previous frame was a spare", function() {
-      bowlingGame.addRolls(4, 5);
-      bowlingGame.addRolls(5, 3);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(4, 5);
+      bowlingGame.playFrame(5, 3);
       expect(bowlingGame.getGameScore()).not.toEqual(22);
     });
 
     it("adds total score of latest frame to total score of the previous frame if the previous frame was a strike", function() {
-      bowlingGame.addRolls(10);
-      bowlingGame.addRolls(5, 2);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(10);
+      bowlingGame.playFrame(5, 2);
       expect(bowlingGame.getGameScore()).toEqual(24);
     });
 
     it("does not add total score of latest frame to total score of the previous frame unless the previous frame was a strike", function() {
-      bowlingGame.addRolls(7, 3);
-      bowlingGame.addRolls(5, 2);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(7, 3);
+      bowlingGame.playFrame(5, 2);
       expect(bowlingGame.getGameScore()).not.toEqual(24);
     });
 
     it("adds first throw of latest frame to total score of 2 frames previous if the 2 previous frames were both strikes", function() {
-      bowlingGame.addRolls(10);
-      bowlingGame.addBonusScores();
-      bowlingGame.addRolls(10);
-      bowlingGame.addBonusScores();
-      bowlingGame.addRolls(5, 2);
-      bowlingGame.addBonusScores();
+      for (var i = 0; i < 2; i++) {
+        bowlingGame.playFrame(10);
+      }
+      bowlingGame.playFrame(5, 2);
       expect(bowlingGame.getGameScore()).toEqual(49);
     });
 
     it("does not add first throw of latest frame to total score of 2 frames previous unless the 2 previous frames were both strikes", function() {
-      bowlingGame.addRolls(10);
-      bowlingGame.addBonusScores();
-      bowlingGame.addRolls(5, 5);
-      bowlingGame.addBonusScores();
-      bowlingGame.addRolls(5, 2);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(10);
+      bowlingGame.playFrame(5, 5);
+      bowlingGame.playFrame(5, 2);
       expect(bowlingGame.getGameScore()).not.toEqual(49);
     });
   });
@@ -128,43 +76,35 @@ describe("BowlingGame", function() {
   describe("areBonusRollsNeeded", function() {
     it("returns true if the 10th frame is a spare", function() {
       for (var i = 0; i < 9; i++) {
-        bowlingGame.addRolls(6, 3);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(6, 3);
       }
-      bowlingGame.addRolls(6, 4);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(6, 4);
       expect(bowlingGame.areBonusRollsNeeded()).toEqual(true)
     });
 
     it("returns true if the 10th frame is a strike", function() {
       for (var i = 0; i < 9; i++) {
-        bowlingGame.addRolls(6, 3);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(6, 3);
       }
-      bowlingGame.addRolls(10);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(10);
       expect(bowlingGame.areBonusRollsNeeded()).toEqual(true)
     });
 
     it("returns true if the 9th & 10th frames are strikes", function() {
       for (var i = 0; i < 8; i++) {
-        bowlingGame.addRolls(6, 3);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(6, 3);
       }
       for (var i = 0; i < 2; i++) {
-        bowlingGame.addRolls(10);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(10);
       }
       expect(bowlingGame.areBonusRollsNeeded()).toEqual(true)
     });
 
     it("returns false if the 10th frame neither a strike nor a spare", function() {
       for (var i = 0; i < 9; i++) {
-        bowlingGame.addRolls(10);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(10);
       }
-      bowlingGame.addRolls(6, 3);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(6, 3);
       expect(bowlingGame.areBonusRollsNeeded()).toEqual(false)
     });
   });
@@ -172,38 +112,73 @@ describe("BowlingGame", function() {
   describe("addFinalBonusScores", function() {
     it("if the final frame is a spare, it gives you 1 extra bonus roll and it gets added to the total game score", function() {
       for (var i = 0; i < 9; i++) {
-        bowlingGame.addRolls(6, 3);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(6, 3);
       }
-      bowlingGame.addRolls(6, 4);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(6, 4);
       bowlingGame.addFinalBonusScores(5);
       expect(bowlingGame.getGameScore()).toEqual(96);
     });
 
     it("if the final frame is a strike, it gives you 2 extra bonus rolls and they get added to the total game score", function() {
       for (var i = 0; i < 9; i++) {
-        bowlingGame.addRolls(6, 3);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(6, 3);
       }
-      bowlingGame.addRolls(10);
-      bowlingGame.addBonusScores();
+      bowlingGame.playFrame(10);
       bowlingGame.addFinalBonusScores(5, 1);
       expect(bowlingGame.getGameScore()).toEqual(97);
     });
 
     it("if the final 2 frames are strikes, it gives you 2 extra bonus rolls and the first roll is added twice to total game score and second roll added once", function() {
       for (var i = 0; i < 8; i++) {
-        bowlingGame.addRolls(6, 3);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(6, 3);
       }
       for (var i = 0; i < 2; i++) {
-        bowlingGame.addRolls(10);
-        bowlingGame.addBonusScores();
+        bowlingGame.playFrame(10);
       }
       bowlingGame.addFinalBonusScores(5, 1);
       expect(bowlingGame.getGameScore()).toEqual(113);
     });
   });
+
+  // describe("isPreviousFrameSpare", function() {
+  //   it("will return true if previous roll was a spare", function() {
+  //     bowlingGame.playFrame(4, 6);
+  //     bowlingGame.playFrame(3, 4);
+  //     expect(bowlingGame._isPreviousFrameSpare()).toEqual(true);
+  //     // maybe use spies with this method later on when testing adding a bonus, same with below tests in describe block
+  //   });
+  //
+  //   it("will return false if previous roll was neither spare nor strike", function() {
+  //     bowlingGame.playFrame(4, 3);
+  //     bowlingGame.playFrame(3, 4);
+  //     expect(bowlingGame._isPreviousFrameSpare()).toEqual(false);
+  //   });
+  //
+  //   it("will return false if previous roll was a strike", function() {
+  //     bowlingGame.playFrame(10);
+  //     bowlingGame.playFrame(3, 4);
+  //     expect(bowlingGame._isPreviousFrameSpare()).toEqual(false);
+  //   });
+  // });
+  //
+  // describe("isPreviousFrameStrike", function() {
+  //   it("will return true if previous roll was a strike", function() {
+  //     bowlingGame.playFrame(10);
+  //     bowlingGame.playFrame(3, 4);
+  //     expect(bowlingGame._isPreviousFrameStrike()).toEqual(true);
+  //   });
+  //
+  //   it("will return false if previous roll was a spare", function() {
+  //     bowlingGame.playFrame(5, 5);
+  //     bowlingGame.playFrame(3, 4);
+  //     expect(bowlingGame._isPreviousFrameStrike()).toEqual(false);
+  //   });
+  //
+  //   it("will return false if previous roll neither spare nor strike", function() {
+  //     bowlingGame.playFrame(5, 2);
+  //     bowlingGame.playFrame(3, 4);
+  //     expect(bowlingGame._isPreviousFrameStrike()).toEqual(false);
+  //   });
+  // });
 
 });
--- jasmine-standalone-3.6.0/src/BowlingGame.js
+++ jasmine-standalone-3.6.0/src/BowlingGame.js
@@ -4,9 +4,56 @@ class BowlingGame {
     this.frameRolls = [];
     this.frameScores = [];
     this.STRIKE_VALUE = 10;
+    this.INDEX_OF_FINAL_FRAME = 9;
   }
 
-  addRolls(roll1, roll2= 'no throw') {
+  playFrame(roll1, roll2 = 'no throw') {
+    this._addRolls(roll1, roll2);
+    this._addBonusScores();
+  }
+
+  getGameScore() {
+    return this.frameScores.reduce(function(a,b) {
+      return a + b;
+    }, 0);
+  }
+
+  areBonusRollsNeeded() {
+    if (this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME].length === 2) {
+      // if the final frame is a spare
+      return true;
+    } else if (this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME - 1]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME - 1].length === 1 && this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME].length === 1) {
+      // if the final 2 frames are strike
+      return true;
+    } else if (this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME].length === 1) {
+      // if the final frame is a strikes
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  addFinalBonusScores(roll1, roll2 = 'no throw') {
+    if (this.areBonusRollsNeeded() === false) {
+      return;
+    }
+    if (this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME].length === 2) {
+      // if your 10th frame is a spare
+      this.frameRolls[this.INDEX_OF_FINAL_FRAME].push(roll1);
+      this.frameScores[this.INDEX_OF_FINAL_FRAME] += roll1;
+    } else if (this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME].length === 1 && this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME - 1]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME - 1].length === 1) {
+      // if your 9th and 10th frames were both strikes
+      this.frameRolls[this.INDEX_OF_FINAL_FRAME].push(roll1, roll2);
+      this.frameScores[this.INDEX_OF_FINAL_FRAME] += roll1 + roll2;
+      this.frameScores[this.INDEX_OF_FINAL_FRAME - 1] += roll1;
+    } else if (this._getFrameTotal(this.frameRolls[this.INDEX_OF_FINAL_FRAME]) === this.STRIKE_VALUE && this.frameRolls[this.INDEX_OF_FINAL_FRAME].length === 1) {
+      // if your 10th frame is a strike
+      this.frameRolls[this.INDEX_OF_FINAL_FRAME].push(roll1, roll2);
+      this.frameScores[this.INDEX_OF_FINAL_FRAME] += roll1 + roll2;
+    }
+  }
+
+  _addRolls(roll1, roll2= 'no throw') {
     if (roll2 === 'no throw') {
       this.frameRolls.push([roll1]);
       this.frameScores.push(roll1);
@@ -16,88 +63,40 @@ class BowlingGame {
     }
   }
 
-  getGameScore() {
-    return this.frameScores.reduce(function(a,b) {
-      return a + b;
-    }, 0);
-  }
-
-  getFrameTotal(array) {
+  _getFrameTotal(array) {
     return array.reduce(function(a,b) {
       return a + b;
     }, 0);
   }
 
-  isPreviousFrameSpare() {
-    return this.getFrameTotal(this.frameRolls[this.frameRolls.length - 2]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 2].length === 2;
+  _isPreviousFrameSpare() {
+    return this._getFrameTotal(this.frameRolls[this.frameRolls.length - 2]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 2].length === 2;
     // if the total of the previous frame is 10 & it took 2 throws to get 10
   }
 
-  isPreviousFrameStrike() {
-    return this.getFrameTotal(this.frameRolls[this.frameRolls.length - 2]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 2].length === 1;
+  _isPreviousFrameStrike() {
+    return this._getFrameTotal(this.frameRolls[this.frameRolls.length - 2]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 2].length === 1;
     // if the total of the previous frame is 10 and it only took 1 throw
   }
 
-  isFrameBeforePreviousFrameStrike() {
-    return this.getFrameTotal(this.frameRolls[this.frameRolls.length - 3]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 3].length === 1;
+  _isFrameBeforePreviousFrameStrike() {
+    return this._getFrameTotal(this.frameRolls[this.frameRolls.length - 3]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 3].length === 1;
     // if the total of the frame before the previous frame is 10 and it only took 1 throw
   }
 
-  addBonusScores() {
-    if (this.frameRolls.length >= 2 && this.isPreviousFrameSpare() === true) {
+  _addBonusScores() {
+    if (this.frameRolls.length >= 2 && this._isPreviousFrameSpare() === true) {
       this.frameScores[this.frameScores.length - 2] += this.frameRolls[this.frameRolls.length -1][0];
       // if at least 2 frames have taken place and the previous frame was a spare, add the first roll of latest frame to score of previous frame
     }
-    if (this.frameRolls.length >= 2 && this.isPreviousFrameStrike() === true) {
+    if (this.frameRolls.length >= 2 && this._isPreviousFrameStrike() === true) {
       this.frameScores[this.frameScores.length - 2] += this.frameScores[this.frameScores.length - 1]
       // if at least 2 frames have taken place and the previous frame was a strike, add total score of latest frame to total score of the frame previous
     }
-    if (this.frameRolls.length >= 3 && this.isPreviousFrameStrike() === true && this.isFrameBeforePreviousFrameStrike() === true) {
+    if (this.frameRolls.length >= 3 && this._isPreviousFrameStrike() === true && this._isFrameBeforePreviousFrameStrike() === true) {
       this.frameScores[this.frameScores.length - 3] += this.frameRolls[this.frameRolls.length -1][0];
       // if at least 3 frames have taken place and the previous 2 frames were both strikes, then add the first throw of latest frame to the total of the frame 2 frames previous
     }
   }
-
-  // playFrame(roll1, roll2 = 'no throw') {
-  //   this.addRolls(roll1, roll2);
-  //   this.addBonusScores();
-  // }
-
-  addFinalBonusScores(roll1, roll2 = 'no throw') {
-    if (this.getFrameTotal(this.frameRolls[9]) === this.STRIKE_VALUE && this.frameRolls[9].length === 2) {
-      // if your 10th frame is a spare
-      this.frameRolls[9].push(roll1);
-      this.frameScores[9] += roll1;
-    } else if (this.getFrameTotal(this.frameRolls[9]) === this.STRIKE_VALUE && this.frameRolls[9].length === 1 && this.getFrameTotal(this.frameRolls[8]) === this.STRIKE_VALUE && this.frameRolls[8].length === 1) {
-      // if your 9th and 10th frames were both strikes
-      this.frameRolls[9].push(roll1, roll2);
-      this.frameScores[9] += roll1 + roll2;
-      this.frameScores[8] += roll1;
-    } else if (this.getFrameTotal(this.frameRolls[9]) === this.STRIKE_VALUE && this.frameRolls[9].length === 1) {
-      // if your 10th frame is a strike
-      this.frameRolls[9].push(roll1, roll2);
-      this.frameScores[9] += roll1 + roll2;
-    }
-  }
-
-  areBonusRollsNeeded() {
-    if (this.getFrameTotal(this.frameRolls[this.frameRolls.length - 1]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 1].length === 2) {
-      // if the final frame is a spare
-      return true;
-    } else if (this.getFrameTotal(this.frameRolls[this.frameRolls.length - 1]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 1].length === 1) {
-      // if the final frame is a strike
-      return true;
-    } else if (this.isPreviousFrameStrike() === true && this.getFrameTotal(this.frameRolls[this.frameRolls.length - 1]) === this.STRIKE_VALUE && this.frameRolls[this.frameRolls.length - 1].length === 1) {
-      // if the final 2 frames are strikes
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  // notes for when I start again
-  // uncomment the playFrame() and refactor tests to use it
-  // mark methods as private
-  // use spies in tests to check stub in values for strike and spare getters
 
 }

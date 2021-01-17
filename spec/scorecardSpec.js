8a74b139b5dc18ccbbc3b9b1f06bd1a02b9cf628
ollienorman <oliver.norman1991@gmail.com>
2021-01-17 13:38:32 +0000

    tests for addStrike and addSpare created and passing

--- spec/scorecardSpec.js
+++ spec/scorecardSpec.js
@@ -22,4 +22,27 @@ describe('Scorecard', function(){
       expect(scorecard.pinsKnocked()[1]).toEqual([1])
     })
   })
+
+  describe('calculateFrame', function(){
+    it('adds the total score from knocked pins to the frameScores array', function(){
+      scorecard.inputRoll(1)
+      scorecard.inputRoll(1)
+      expect(scorecard.frameScores()[0]).toEqual(2)
+    })
+  })
+
+  describe('addStrike', function(){
+    it('adds a strike flag to the relevant frame in the strikesSpares hash', function(){
+      scorecard.inputRoll(10)
+      expect(scorecard.strikesSpares()[0]).toEqual('strike')
+    })
+  })
+
+  describe('addSpare', function(){
+    it('adds a spare flag to the relevant frame in the strikesSpares hash', function(){
+      scorecard.inputRoll(9)
+      scorecard.inputRoll(1)
+      expect(scorecard.strikesSpares()[0]).toEqual('spare')
+    })
+  })
 })
--- src/scorecard.js
+++ src/scorecard.js
@@ -3,6 +3,8 @@ class Scorecard {
     this._pinsKnocked = []
     this._frame = 0
     this._roll = 0
+    this._frameScores = []
+    this._strikesSpares = {}
   };
 
   inputRoll(number) {
@@ -12,12 +14,43 @@ class Scorecard {
     this._pinsKnocked[this._frame].push(number)
     this._roll++
     if(this._isFrameComplete()) {
+      this.calculateFrame()
       this._frame++
     }
   };
 
+  calculateFrame() {
+    this._frameScores.push(this.sum(this._pinsKnocked[this._frame]))
+    this.strikeOrSpare()
+  }
+
+  strikeOrSpare() {
+    if(this._pinsKnocked[this._frame][0] === 10) {
+      this.addStrike();
+    }
+    else if(this.sum(this._pinsKnocked[this._frame]) === 10) {
+      this.addSpare();
+    }
+  }
+
+  addSpare() {
+    this._strikesSpares[this._frame] = "spare"
+  }
+
+  addStrike() {
+    this._strikesSpares[this._frame] = "strike"
+  }
+
   pinsKnocked() {
     return this._pinsKnocked
+  }
+
+  frameScores() {
+    return this._frameScores
+  }
+
+  strikesSpares() {
+    return this._strikesSpares
   }
 
   _frameNotExist() {

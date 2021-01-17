36e7375174315a09970892f80335992865f90678
PiperS52 <simon.a.piper87@gmail.com>
2021-01-17 10:24:44 +0000

    setup check

---
+++ spec/ScorecardSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe("Scorecard", function(){
+  var scorecard = new Scorecard();
+
+  describe("constructor", function(){
+    it("creates a new instance", function(){
+      expect(scorecard instanceof Scorecard).toBe(true)
+    })
+  })
+
+
+})
---
+++ src/Scorecard.js
@@ -0,0 +1,11 @@
+'use strict';
+
+class Scorecard {
+
+  constructor(){
+    this.scores = []
+    this.running_score = []
+  }
+
+
+}

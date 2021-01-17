bafbc0e4c61ed693b755ec0eff144d0b3a424efa
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-01-17 14:35:43 +0000

    removed instance variables from print method that aren't required

--- App/src/Print.js
+++ App/src/Print.js
@@ -1,10 +1,5 @@
 class Print{
 
-  constructor(){
-    this.scorecard
-    this.roll = 0
-  };
-
   output(scorecard, totalscore, turn) {
     let outputmessage = []
     let total = `Your score = ${totalscore}`

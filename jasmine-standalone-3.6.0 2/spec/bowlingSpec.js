a8cad38482d259c098580c70ee1e8b0680100cf8
Aman Tank <amantank187@gmail.com>
2021-01-17 16:09:56 +0000

    Finished the bowling.js file

--- jasmine-standalone-3.6.0 2/spec/bowlingSpec.js
+++ jasmine-standalone-3.6.0 2/spec/bowlingSpec.js
@@ -12,11 +12,47 @@ describe('bowling', function(){
 
   describe('#roll', function(){
     it('Should allow a player to enter a roll', function(){
-      for (var i = 0; i < 20; i++) {
-        bowling.roll(1);
-      }
+      rollMany(1, 20);
       expect(bowling.score).toEqual(20);
     });
-
+    it('should be able to roll a gutter game', function(){
+      rollMany(0,20)
+      expect(bowling.score).toEqual(0);
+    });
+    it('should return the correct score when spare is rolled', function(){
+      bowling.roll(5)
+      bowling.roll(5)
+      bowling.roll(3)
+      rollMany(0,17)
+      expect(bowling.score).toEqual(16);
+    });
+    it('should return the correct score when a strike is rolled', function(){
+      bowling.roll(10)
+      bowling.roll(1)
+      bowling.roll(1)
+      rollMany(0,17)
+      expect(bowling.score).toEqual(14);
+    });
+    it('should return the correct score when perfect game is rolled', function(){
+      rollMany(10,12)
+      expect(bowling.score).toEqual(300);
+    });
   });
+
+  describe('#reset', function(){
+    it('Should allow a reset of score', function(){
+      bowling.roll(10)
+      bowling.roll(1)
+      bowling.roll(1)
+      rollMany(0,17)
+      bowling.reset()
+      expect(bowling._rolls).toEqual([]);
+    });
+  });
+
+  function rollMany(pins, rolls){
+    for (let i = 0; i < rolls; i++){
+      bowling.roll(pins);
+    }
+  }
 });
--- jasmine-standalone-3.6.0 2/src/bowling.js
+++ jasmine-standalone-3.6.0 2/src/bowling.js
@@ -9,16 +9,50 @@ class Bowling {
     this._rolls.push(pins)
   }
 
-  get score(){
-    var result = 0;
-    var roll_index = 0;
-
-    for(var i = 0; i < 10; i++) {
-      result += this._rolls[roll_index] + this._rolls[roll_index + 1];
-      roll_index += 2;
-    }
-    return result;
+  reset(){
+    this._rolls = [];
   }
+
+    get score(){
+    let score = 0;
+    let rollIndex = 0;
+
+    for(let i = 0; i < 10; i++) {
+
+      if (this.isStrike(rollIndex)){
+        score += this.strikeBonus(rollIndex);
+        rollIndex++;
+        continue;
+      }
+      const frameScore = this._rolls[rollIndex] + this._rolls[rollIndex + 1];
+
+      if (this.isSpare(frameScore)){
+        score += this.spareBonus(rollIndex);
+      } else {
+        score += frameScore;
+      }
+
+
+      rollIndex += 2;
+    }
+    return score;
+  }
+
+isSpare(frameScore){
+  return frameScore === 10;
+}
+
+spareBonus(rollIndex){
+  return 10 + this._rolls[rollIndex + 2];
+}
+
+isStrike(rollIndex){
+  return this._rolls[rollIndex] === 10;
+}
+
+strikeBonus(rollIndex){
+  return 10 + this._rolls[rollIndex + 1] + this._rolls[rollIndex + 2];
+}
 
 
 }

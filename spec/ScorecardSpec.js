dad999dbf966356c5f78f442bdc7636edca16dd8
Kiki Dawson <kikiadawson@gmail.com>
2021-01-19 18:31:21 +0000

    refactored tests and added describe blocks

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -7,152 +7,96 @@ describe('Scorecard', function() {
     scorecard = new Scorecard();
   });
 
-  it('starts with empty scorecard', function() {
-    expect(scorecard.getFrames()).toEqual([]);
+  describe('Storing Frames', function() {
+    it('starts with empty scorecard', function() {
+      expect(scorecard.getFrames()).toEqual([]);
+    });
+
+    it('hold rolls in current frame until frame complete', function() {
+      scorecard.roll(3)
+      expect(scorecard.getCurrentFrame()).toEqual([3])
+    });
+
+    it('saves rolls as frames and stores together', function() {
+      for (let i = 0; i < 5; i++) {
+        scorecard.roll(3)
+      }
+      expect(scorecard.getFrames()).toEqual([[3, 3], [3, 3]])
+    });
   });
 
-  it('hold rolls in current frame until frame complete', function() {
-    scorecard.roll(3)
+  describe('Gets Total', function() {
+    it('adds points to total once frame is complete', function() {
+      for (let i = 0; i < 5; i++) {
+        scorecard.roll(3)
+      }
+      expect(scorecard.getTotal()).toEqual(12)
+    });
 
-    expect(scorecard.getCurrentFrame()).toEqual([3])
+    it('works with ten frames', function() {
+      for (let i = 0; i < 20; i++) {
+        scorecard.roll(3)
+      }
+      expect(scorecard.getTotal()).toEqual(60)
+    });
   });
 
-  it('saves rolls as frames and stores together', function() {
-    scorecard.roll(3)
-    scorecard.roll(4)
-    scorecard.roll(2)
-    scorecard.roll(5)
-
-    expect(scorecard.getFrames()).toEqual([[3, 4], [2, 5]])
+  describe('Gutterball', function() {
+    it('gives 0', function() {
+      for (let i = 0; i < 20; i++) {
+        scorecard.roll(0)
+      }
+      expect(scorecard.getTotal()).toEqual(0)
+    });
   });
 
-  it('adds points to total once frame is complete', function() {
-    scorecard.roll(3)
-    scorecard.roll(4)
-    scorecard.roll(2)
-    scorecard.roll(5)
-
-    expect(scorecard.getTotal()).toEqual(14)
+  describe('Spares', function() {
+    it('gives bonus for spare - next roll doubled', function() {
+      for (let i = 0; i < 4; i++) {
+        scorecard.roll(5)
+      }
+      expect(scorecard.getTotal()).toEqual(25)
+    });
   });
 
-  it('saves strike in frame alone', function() {
-    scorecard.roll(10)
+  describe('Strikes', function() {
+    it('saves strike in frame alone', function() {
+      scorecard.roll(10)
+      expect(scorecard.getFrames()).toEqual([[10]])
+    });
 
-    expect(scorecard.getFrames()).toEqual([[10]])
-  })
+    it('tests a perfect game', function() {
+      for (let i = 0; i < 12; i++) {
+        scorecard.roll(10)
+      }
+      expect(scorecard.getTotal()).toEqual(300)
+    });
 
-  it('gives bonus for one strike - next two rolls doubled', function() {
-    scorecard.roll(10)
-    scorecard.roll(2)
-    scorecard.roll(6)
+    it('gives bonus for one strike - next two rolls doubled', function() {
+      scorecard.roll(10)
+      scorecard.roll(3)
+      scorecard.roll(3)
+      expect(scorecard.getTotal()).toEqual(22)
+    });
 
-    expect(scorecard.getTotal()).toEqual(26)
+    it('gives bonus for double strike - two rolls after strikes doubles', function() {
+      scorecard.roll(10)
+      scorecard.roll(10)
+      scorecard.roll(3)
+      scorecard.roll(3)
+      expect(scorecard.getTotal()).toEqual(45)
+    });
   });
 
-  it('gives bonus for spare - next roll doubled', function() {
-    scorecard.roll(5)
-    scorecard.roll(5)
-    scorecard.roll(2)
-    scorecard.roll(6)
-
-    expect(scorecard.getTotal()).toEqual(20)
-  })
-
-  it('gives bonus for double strike - two rolls after strikes doubles', function() {
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(2)
-    scorecard.roll(6)
-
-    expect(scorecard.getTotal()).toEqual(48)
-  })
-
-  it('works with ten frames', function() {
-    scorecard.roll(1)
-    scorecard.roll(4)
-    scorecard.roll(4)
-    scorecard.roll(5)
-    scorecard.roll(6)
-    scorecard.roll(4)
-    scorecard.roll(5)
-    scorecard.roll(5)
-    scorecard.roll(10)
-    scorecard.roll(0)
-    scorecard.roll(1)
-    scorecard.roll(7)
-    scorecard.roll(3)
-    scorecard.roll(6)
-    scorecard.roll(4)
-    scorecard.roll(10)
-    scorecard.roll(2)
-    scorecard.roll(4)
-
-    expect(scorecard.getTotal()).toEqual(119)
-  })
-
-  it('gives 0 for gutterball', function() {
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-    scorecard.roll(0)
-
-    expect(scorecard.getTotal()).toEqual(0)
-  })
-
-  it('tenth frame bonus - test with spare', function() {
-    scorecard.roll(1)
-    scorecard.roll(4)
-    scorecard.roll(4)
-    scorecard.roll(5)
-    scorecard.roll(6)
-    scorecard.roll(4)
-    scorecard.roll(5)
-    scorecard.roll(5)
-    scorecard.roll(10)
-    scorecard.roll(0)
-    scorecard.roll(1)
-    scorecard.roll(7)
-    scorecard.roll(3)
-    scorecard.roll(6)
-    scorecard.roll(4)
-    scorecard.roll(10)
-    scorecard.roll(2)
-    scorecard.roll(8)
-    scorecard.roll(6)
-
-    expect(scorecard.getTotal()).toEqual(133)
-  })
-
-  it('tests a perfect game', function() {
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-    scorecard.roll(10)
-
-    expect(scorecard.getTotal()).toEqual(300)
-  })
-})
+  describe('Tenth Frame', function() {
+    it('tenth frame bonus - test with spare', function() {
+      for (let i = 0; i < 18; i++) {
+        scorecard.roll(0)
+      }
+      for (let i = 0; i < 3; i++) {
+        scorecard.roll(5)
+      }
+      expect(scorecard.getTotal()).toEqual(15)
+    });
+  });
+});

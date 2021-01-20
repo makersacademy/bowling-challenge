a3852bbb103765c3d290082b2b1fcbcd8d572f8e
Will-Helliwell <wj.helliwell@gmail.com>
2021-01-20 18:22:27 +0000

    changed mocking in gameSpec.js not using spies, created own fake Frame class

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -3,29 +3,34 @@ describe('Game', () => {
   var frame;
 
   beforeEach( function() {
-    // frameDouble = jasmine.createSpyObj('fake frame',['score'])
-    exampleFrame = new(Frame);
-    game = new Game(frame);
+    class FrameClassDouble {
+      constructor() {
+        this.score = 0;
+        this.bonus_rolls = 0;
+      };
+    }
+    test = new FrameClassDouble();
+    game = new Game();
+    gameWithDoubles = new Game(FrameClassDouble);
   });
 
   it('is initialized with a score of zero', () => {
-    expect(game.score).toEqual(0);
+    expect(gameWithDoubles.score).toEqual(0);
   });
-
-  it('is initialized with an array of 10 frame instances', () => {
-    expect(game.frames.length).toEqual(10);
-    expect(game.frames[0]).toEqual(exampleFrame);
-    expect(game.frames[7]).toEqual(exampleFrame);
+  it('by default is initialized with an array of 10 frame instances', () => {
+    expect(gameWithDoubles.frames.length).toEqual(10);
+    expect(gameWithDoubles.frames[0]).toEqual(test);
+    expect(gameWithDoubles.frames[7]).toEqual(test);
   });
-  it('is initialized 10 distinct frame instances', () => {
+  it('by default is initialized 10 distinct frame instances', () => {
     expect(game.frames[0]).not.toBe(game.frames[1]);
+  });
+  it('random', () => {
   });
 
   describe('returnFrameScores', () => {
     it('prints out the score for all 10 frames', () => {
-      spyOn(exampleFrame, 'score').and.returnValue(5);
-      game.returnFrameScores()
-      expect(game.returnFrameScores()).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+      expect(gameWithDoubles.returnFrameScores()).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     });
   });
   xdescribe('getRoll', () => {

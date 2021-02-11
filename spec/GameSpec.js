9872ededb05285f0fee12c85f3c81216a9e56a41
Symion Edwards <symion.edwards@gmail.com>
2021-02-11 15:03:28 +0000

    Attempt to create fake frame object for Game unit tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,44 +1,49 @@
-'use strict';
+"use strict";
 
-describe('Game', () => {
-  var Frame;
+describe("Game", () => {
+  var frameDouble;
   var game;
 
-  beforeEach(() => {
-  Frame = jasmine.createSpyObj('Frame', ['knocked', 'isInPlay', 'score', 'isStrike', 'isSpare']);
-  game = new Game(Frame);
-  });
+  class FrameDouble {
+    kocked() {}
+    isInPlay() {}
+    score() {}
+    isStrike() {}
+    isSpare() {}
+  }
 
-  describe('constructor', () => {
+  // beforeEach(() => {
+  //   game = new Game(FrameDouble);
+  // });
 
-    it('has property frames, with array of frame objects', () => {
-      expect(game.frames()).toEqual([Frame]);
+  describe("constructor", () => {
+    it("has property frames, with array of frame objects", () => {
+      game = new Game(FrameDouble);
+      expect(game.frames()).toEqual([new FrameDouble()]);
     });
-
   });
 
-  describe('roll', () => {
-    it('calls the knocked function on the current frame', () => {
+  describe("roll", () => {
+    it("calls the knocked function on the current frame", () => {
+      game = new Game(FrameDouble);
+      game.currentFrame().returnValue(new FrameDouble());
       game.roll(3);
-      expect(Frame.knocked).toHaveBeenCalled();
+      expect(game.frames().knocked).toHaveBeenCalled();
     });
 
-    it('adds new frame objects to the frames property appropriately', () => {
+    it("adds new frame objects to the frames property appropriately", () => {
       Frame.isInPlay.and.returnValue(false);
       game.roll(3);
       expect(game.frames().length).toEqual(2);
     });
 
-    xit('awards bonus points appropriately', () => {
-
-    });
+    xit("awards bonus points appropriately", () => {});
   });
 
-  describe('total score', () => {
-    it('calculates the total score of the game', () => {
-      var frames = [{_score: 30}, {_score: 10}]
+  describe("total score", () => {
+    it("calculates the total score of the game", () => {
+      var frames = [{ _score: 30 }, { _score: 10 }];
       expect(game.totalScore(frames)).toEqual(40);
     });
   });
-
-  });
+});

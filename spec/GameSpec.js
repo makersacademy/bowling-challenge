da2687f72a1cc4538d04815be735ff8ce90234e7
Symion Edwards <symion.edwards@gmail.com>
2021-02-12 13:12:47 +0000

    Amend framedouble class. All tests passing

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -3,67 +3,68 @@
 describe("Game", () => {
   var game;
 
-  class FrameDoubleInPlay {
+  class FrameDouble {
     constructor() {
       this.knockedCallCount = 0;
+      this.strikeBonusCount = 0;
+      this._isInPlay = true;
+      this._rolls = [];
     }
     knocked(pins) {
       this.knockedCallCount++;
+      this._rolls.push(pins);
+      this.frameCheck();
     }
     isInPlay() {
-      return true;
+      return this._isInPlay;
+    }
+    frameCheck() {
+      if (this.knockedCallCount === 2) this._isInPlay = !this._isInPlay;
+    }
+    isStrike() {
+      return this._rolls[0] === 10;
+    }
+    pointBonus(pins) {
+      this.strikeBonusCount++;
     }
     score() {}
-    isStrike() {}
-    isSpare() {}
-  }
-
-  class FrameDoubleNotInPlay {
-    constructor() {
-      this.knockedCallCount = 0;
-      this.strikeCallCount = 0;
-      this.spareCallCount = 0;
-    }
-    knocked(pins) {
-      this.knockedCallCount++;
-    }
-    isInPlay() {
-      return false;
-    }
-    score() {}
-    isStrike() {}
     isSpare() {}
   }
 
   describe("constructor", () => {
     it("has property frames, with array of frame objects", () => {
-      game = new Game(FrameDoubleInPlay);
-      expect(game.frames()).toEqual([new FrameDoubleInPlay()]);
+      game = new Game(FrameDouble);
+      expect(game.frames()).toEqual([new FrameDouble()]);
     });
   });
 
   describe("roll", () => {
     it("calls the knocked function on the current frame", () => {
-      game = new Game(FrameDoubleInPlay);
+      game = new Game(FrameDouble);
       game.roll(3);
       expect(game.frames()[0].knockedCallCount).toEqual(1);
     });
 
     it("adds new frame objects to the frames property appropriately", () => {
-      game = new Game(FrameDoubleNotInPlay);
+      game = new Game(FrameDouble);
+      game.roll(3);
+      game.roll(3);
       game.roll(3);
       expect(game.frames().length).toEqual(2);
     });
 
-    xit("awards bonus points appropriately", () => {
-      game = new Game(FrameDoubleNotInPlay);
-      expect(game.frames()[0].strikeCallCount).toEqual(1);
+    it("awards bonus points appropriately", () => {
+      game = new Game(FrameDouble);
+      game.roll(10);
+      game.roll(5);
+      game.roll(5);
+      expect(game.frames()[0].strikeBonusCount).toEqual(1);
     });
   });
 
   describe("total score", () => {
     it("calculates the total score of the game", () => {
-      game = new Game(FrameDoubleInPlay);
+      game = new Game(FrameDouble);
       var frames = [{ _score: 30 }, { _score: 10 }];
       expect(game.totalScore(frames)).toEqual(40);
     });

7eb32989b8e425e5400b3d2e9fdbae10edb353c6
Sean Edwards <seantrevoredwards@gmail.com>
2021-02-12 12:57:06 +0000

    added second test and passed

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -7,10 +7,19 @@ describe('Bowling', () => {
 
   describe('No strikes or spares', () => {
     it('can work out a gutter ball', () => {
-      console.log(bowling);
-      bowling.roll(0, 20);
-      console.log(bowling);
+      rollMultiple(0, 20);
       expect(bowling.score()).toEqual(0);
+    });
+
+    it('can work out a game with different roll amounts', () => {
+      bowling.roll(1);
+      bowling.roll(6);
+      bowling.roll(2);
+      bowling.roll(3);
+      bowling.roll(4);
+      bowling.roll(5);
+      rollMultiple(0, 14);
+      expect(bowling.score()).toBe(21);
     });
   });
 

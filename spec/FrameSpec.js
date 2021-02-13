b7c61628d319e0ae11b0a8b1d774b19e1e966ddf
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-02-13 12:12:16 +0000

    is strike and is spare

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -5,9 +5,56 @@ describe ('Frame', function() {
   beforeEach(function() {
     frame = new Frame ();
   })
-  
+
   it('can score a rolls', function() {
     frame.roll(5);
     expect(frame.showScore()).toEqual([5])
+  })
+
+  it('can score two rolls', function() {
+    frame.roll(4);
+    frame.roll(4);
+    expect(frame.showScore()).toEqual([4, 4])
+  })
+
+  it('throws an error when you try to roll a third', function() {
+    frame.roll(4);
+    frame.roll(4);
+    expect(function() {
+      frame.roll(1);
+    }).toThrow(new Error('frame complete'));
+  })
+
+    it('throws an error when you roll a strike and try to roll again', function() {
+      frame.roll(10);
+      expect(function() {
+        frame.roll(4);
+      }).toThrow(new Error('frame complete'))
+    })
+
+  describe('isStrike', function() {
+    it('returns true when there is a strike', function() {
+      frame.roll(10);
+      expect(frame.isStrike()).toEqual(true);
+    })
+
+    it('returns false when there is not a strike', function() {
+      frame.roll(1);
+      expect(frame.isStrike()).toEqual(false);
+    })
+  })
+
+  describe('isSpare', function() {
+    it('returns true when there is a spare', function() {
+      frame.roll(4);
+      frame.roll(6);
+      expect(frame.isSpare()).toEqual(true);
+    })
+
+    it('returns false when there is not a spare', function() {
+      frame.roll(4);
+      frame.roll(3);
+      expect(frame.isSpare()).toEqual(false);
+    })
   })
 })
--- src/Frame.js
+++ src/Frame.js
@@ -4,10 +4,29 @@ class Frame {
   }
 
   roll = pins => {
+    if(this.score.length === 2 || this.isStrike()){
+      throw new Error('frame complete')
+    }
     this.score.push(pins);
   }
 
   showScore = () => {
     return this.score;
+  }
+
+  isStrike = () => {
+    if(this.score[0] === 10) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isSpare = () => {
+    if(!this.isStrike() && this.score[0] + this.score[1] === 10) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }

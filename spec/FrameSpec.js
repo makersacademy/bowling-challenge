ee18308f068c5a4e03428f27d3702084baebe6e8
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-13 16:42:17 +0000

    Complete the Frame class translation

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -14,4 +14,60 @@ describe("Frame", function() {
     expect(frame.rolls).toContain(3)
   })
 
+  it("knows if it's ended (except 10th frame)", function() {
+    frame.add_roll(3)
+    frame.add_roll(2)
+    expect(frame.isCompleted()).toBe(true)
+  })
+
+  it("knows if it ended early (except 10th frame)", function() {
+    frame.add_roll(10)
+    expect(frame.isCompleted()).toBe(true)
+  })
+
+  it("won't add rolls if completed", function() {
+    frame.add_roll(10)
+    expect(function() {
+      frame.add_roll(2);
+    }).toThrowError("This frame is completed")
+  })
+
+  it("knows it's owed 2 extra rolls for scoring on a strike", function() {
+    frame.add_roll(10)
+    expect(frame.owedRolls()).toEqual(2)
+  })
+
+  it("knows it's owed 1 extra roll for scoring on a spare", function() {
+    frame.add_roll(3)
+    frame.add_roll(7)
+    expect(frame.owedRolls()).toEqual(1)
+  })
+
+  describe("has special rules for 10th frame; it", function() {
+    beforeEach(function() {
+      frame = new Frame(10);
+    })
+
+    it("is completed with 2 rolls if no spare/strike", function() {
+      frame.add_roll(3)
+      frame.add_roll(2)
+      expect(frame.isCompleted()).toBe(true)
+    })
+
+    it("is not completed with 2 rolls if a spare", function() {
+      frame.add_roll(3)
+      frame.add_roll(7)
+      expect(frame.isCompleted()).toBe(false)
+    })
+
+    it("is completed with 3 rolls if a spare/strike", function() {
+      frame.add_roll(3)
+      frame.add_roll(7)
+      frame.add_roll(5)
+      expect(frame.isCompleted()).toBe(true)
+    })
+
+
+  })
+
 })
--- src/Frame.js
+++ src/Frame.js
@@ -6,7 +6,37 @@ class Frame {
   }
 
   add_roll(pins) {
+    if (this.isCompleted()) {
+      throw new Error("This frame is completed")
+    }
     this.rolls.push(pins)
+  }
+
+  isCompleted() {
+    if (this.number < 10) {
+      console.log(this.rolls.length)
+      return ( this.rolls.length === 2) || ( this.rolls[0] === 10 )
+    } else if ((this.rolls[0] + this.rolls[1]) < 10 ) {
+      return this.rolls.length === 2
+    } else {
+      return this.rolls.length === 3
+    }
+  }
+
+  isStrike() {
+    return this.rolls[0] === 10
+  }
+
+  isSpare() {
+    return (this.rolls[0] + this.rolls[1]) === 10
+  }
+
+  owedRolls() {
+    if (this.isStrike()) {
+      return 2
+    } else {
+      return this.isSpare() ? 1 : 0
+    }
   }
 
 }

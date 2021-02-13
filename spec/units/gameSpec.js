a82c7175be741183b2cedf6c1aaa104e757523cb
acavalla <anna.cavalla@gmail.com>
2021-02-13 16:56:30 +0000

    Setup complete one test failing

---
+++ spec/units/gameSpec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Game', function() {
+  let game = new Game();
+
+  describe('it initializes with an empty frames array', function() {
+    expect(game.frames).toBe([])
+  })
+})

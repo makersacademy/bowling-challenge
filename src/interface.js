675e851454201cebd58def8b0c0515d7d45c671d
bateso88 <bateso@hotmail.co.uk>
2021-02-13 12:39:48 +0000

    started interface

---
+++ src/interface.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  let game = new Game();
+
+  // $('#select-city').submit(function(event){
+  //   event.preventDefault();
+  //   let city = $('#current-city').val();
+  //   displayWeather(city);
+  // })
+
+  // function displayWeather(city) {
+  //   let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  //   let token = '&appid=39f4e0936bb0edbcca55a829a6e33bc3';
+  //   let units = '&units=metric';
+  //   $.get(url + token + units, function(data){
+  //     $('#current-temperature').text(data.main.temp);
+  //   })
+  // }
+
+
+})

190017fd4b84d70076f4e1cac20048aeaf27fd98
jshields123 <jackshields@live.co.uk>
2021-02-14 16:25:17 +0000

    added in strike functionality and test passed

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,16 +10,35 @@ describe('Game', function() {
     for (let i = 0; i < 20; i++){
       game.roll(0)
     }
-    expect(game.score()).toEqual(0);
+    expect(game.score).toEqual(0);
   })
 
   it('can roll all 1s', function(){
     for (let i = 0; i < 20; i++){
     game.roll(1)
   }
-  expect(game.score()).toEqual(20);
+  expect(game.score).toEqual(20);
   })
 
+  it('can roll all spares', function(){
+      game.roll(5);
+      game.roll(5);
+      game.roll(2);
+      for (let i = 0; i < 17; i++) {
+        game.roll(0)
+    }
+    expect(game.score).toEqual(14)
+  })
+
+  it('can roll a strike', function(){
+    game.roll(10);
+    game.roll(1);
+    game.roll(1);
+    for (let i = 0; i < 17; i++) {
+      game.roll(0)
+    }
+    expect(game.score).toEqual(14)
+  })
 
 })
 
--- src/game.js
+++ src/game.js
@@ -1,15 +1,53 @@
 'use strict';
 
 class Game {
-  constructor(){
-    this.currentScore = 0;
+  constructor() {
+    this.rolls = [];
+
   }
 
-  roll(pins){
-    this.currentScore += pins;
+  roll(pins) {
+    this.rolls.push(pins);
   }
 
-  score() {
-    return this.currentScore;
+  get score() {
+    // keep track of score and roll index
+    let score = 0;
+    let rollIndex = 0;
+    //for the 10 frames check if roll strike or spare or else calc score.
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+      if (this.isStrike(rollIndex)) {
+        score += this.strikeBonus(rollIndex);
+        rollIndex++;
+        continue;
+      }
+      const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+
+      if (this.isSpare(frameScore)) {
+        score += this.spareBonus(rollIndex);
+      } else {
+        score += frameScore;
+      }
+
+      rollIndex += 2;
+    }
+
+    return score;
+  }
+
+  isSpare(frameScore) {
+    return frameScore === 10;
+  }
+
+  spareBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex + 2];
+  }
+
+  isStrike(rollIndex) {
+    return this.rolls[rollIndex] === 10
+  }
+
+  strikeBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
   }
 }

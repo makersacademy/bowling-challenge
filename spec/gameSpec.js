75931a49989db34ad00f6cdf7b6ffce22f29ccde
Reeshul <reeshul@hotmail.com>
2021-02-14 12:02:58 +0000

    failing test for a game of hitting 0 pins

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -10,4 +10,28 @@ describe("score", () => {
   it("can create a scorecard", () => {
     expect(game).toBeInstanceOf(Game);
   });
+
+  it("outputs a score of 0 after 20 consecutive rolls of hitting 0 pins", () => {
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    game.rolls(0);
+    expect(game.score()).toBe(0);
+  });
 });

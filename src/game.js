be620ead4df94ff884294b1c607b0ee66f662598
Reeshul <reeshul@hotmail.com>
2021-02-14 12:19:03 +0000

    passed test

--- src/game.js
+++ src/game.js
@@ -1,10 +1,22 @@
 "use-strict";
 
 class Game {
-  rolls(pins) {
-
+  constructor() {
+    this.rolls = [];
+    this.rollIndex = 0;
+    this.points = 0;
   }
+
+  roll(pins) {
+    this.rolls.push(pins);
+  }
+
   score() {
-    return 0
+    for (let i = 1; i < 11; i++) {
+      // where i is the frame number
+      this.points += this.rolls[this.rollIndex] + this.rolls[this.rollIndex + 1];
+      this.rollIndex += 2;
+    }
+    return this.points;
   }
 }

53e8f31a0b8266b8e97609ab1a6d6f6a9b70bd28
Morne van Zyl <Morne@Mornes-MacBook-Air.local>
2021-02-14 13:14:07 +0000

    Added Game class current frame unit tests

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -6,5 +6,18 @@ describe('Game', () => {
   beforeEach(() => {
     game = new Game();
   });
+
+  describe('A Game ', () => {
+
+    it('knows what the current frame is', () => {
+      expect(game.currentFrame().id).toEqual(1);
+    });
+    
+    it('can log a roll', () => {
+      game.roll(5);
+      expect(game.currentFrame().score()).toEqual(5);
+    });
+  });
+
   
 });

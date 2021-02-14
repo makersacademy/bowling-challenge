91d2e5db45c9da35f15fa472b30712aaa720fa57
Miranda Wilson <mirandascwilson@gmail.com>
2021-02-14 15:12:39 +0000

    Check that inputs are number 0-10

--- src/BowlingJQuery.js
+++ src/BowlingJQuery.js
@@ -5,18 +5,27 @@ $(document).ready( function() {
 
   $("#rolls-form").submit( function() {
     event.preventDefault();
-    let roll = parseInt($("#input-roll").val());
+    let input = $("#input-roll").val()
+    if (isValidInput(input) === false) {
+      return
+    }
+    let roll = parseInt(input);
     game.inputRoll(roll)
 
     frameScores()
   })
 
-
   function frameScores() {
-    for ( i = 1; i < 11; i++) {
+    for (let i = 1; i < 11; i++) {
       $(`#score${i}`).text(game.cumulScores[`frame_${i}`]);
     }
   }
 
+  function isValidInput(input) {
+    let validNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+    if (!validNums.includes(input)) {
+      return false
+    }
+  }
 
 })

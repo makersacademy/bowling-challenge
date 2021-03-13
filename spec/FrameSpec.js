6caca79a38921a5bae1cb162bd509b20bca58afb
rhc07 <rorieclarke@gmail.com>
2021-03-13 18:37:55 +0000

    added a frame class

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -1,1 +1,63 @@
 "use strict";
+
+describe("Frame", function() {
+
+let frame;
+
+beforeEach(function() {
+  frame = new Frame;
+});
+
+it('has an empty current frame score by default', function(){
+   expect(frame.currentFrameScore()).toEqual([]);
+ })
+
+describe('adding to frames', function(){
+  it('adds pins to current frame', function(){
+    frame.addToFrame(10);
+    expect(frame.currentFrameScore()).toContain(10);
+  })
+ })
+
+ describe('summing the total frame', function(){
+   it('adds pins to current frame', function(){
+     frame.addToFrame(3);
+     frame.addToFrame(6);
+     expect(frame.total()).toEqual(9);
+   })
+  })
+
+  describe('First Bowl', function(){
+    it('return first bowl of current frame', function(){
+      frame.addToFrame(3);
+      frame.addToFrame(6);
+      expect(frame.firstBowl()).toEqual(3);
+    })
+   })
+
+   describe('strike function', function(){
+     it('returns true if a strike', function(){
+       frame.addToFrame(10);
+       frame.addToFrame(0);
+       expect(frame.isStrike()).toEqual(true);
+     })
+     it('returns false if not a strike', function(){
+       frame.addToFrame(2);
+       frame.addToFrame(3);
+       expect(frame.isStrike()).toEqual(false);
+    })
+  })
+
+  describe('spare function', function(){
+    it('returns true if a spare', function(){
+      frame.addToFrame(5);
+      frame.addToFrame(5);
+      expect(frame.isSpare()).toEqual(true);
+    })
+    it('returns false if not a strike', function(){
+      frame.addToFrame(2);
+      frame.addToFrame(3);
+      expect(frame.isSpare()).toEqual(false);
+   })
+ })
+})
--- src/Frame.js
+++ src/Frame.js
@@ -6,11 +6,35 @@ class Frame {
     this.current_frame = [];
   };
 
-add_to_frame = function(pins) {
+
+currentFrameScore = function(){
+  return this.current_frame;
+}
+
+addToFrame = function(pins) {
   this.current_frame.push(pins);
 }
 
 total = function() {
   return this.current_frame.reduce((x, y) => x + y);
 }
+
+firstBowl = function() {
+  return this.current_frame[0];
+}
+
+isStrike = function() {
+  if(this.current_frame[0] === 10){
+    return true;
+  }
+  else if(this.current_frame[1] === 10){
+    return true;
+  }
+  else return false;
+}
+
+isSpare = function() {
+  return this.total() === 10;
+}
+
 };

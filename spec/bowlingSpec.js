c29030441f317a1167b424ef8a8721efb2b3166a
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-14 10:43:58 +0100

    first failing test for score starting at 0

---
+++ spec/bowlingSpec.js
@@ -0,0 +1,26 @@
+describe('Bowling', function() {
+
+var bowling;
+
+beforeEach(function() {
+bowling = new bowling();
+});
+
+describe('before each game starts', function() {
+
+    it('starts with a score of 0', function() {
+       expect(bowling.getScore()).toEqual(0);
+    });
+
+
+})
+
+
+
+
+
+
+
+
+
+})

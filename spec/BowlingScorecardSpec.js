a4a5a032aae1715a350afee541e9c2d9dc752450
Eds <steadyede@live.co.uk>
2021-03-14 23:09:28 +0000

    frame scores recorded

--- spec/BowlingScorecardSpec.js
+++ spec/BowlingScorecardSpec.js
@@ -16,6 +16,20 @@ describe('BowlingScorecard', function() {
     it("only takes numbers under 11 as input", function() {
       expect( function() {scorecard.recordRoll("three")} ).toThrow ("Can only record numbers")
       expect( function() {scorecard.recordRoll("12")} ).toThrow ("Scores must be 0-10")
+      expect( function() {scorecard.recordRoll("-2")} ).toThrow ("Scores must be 0-10")
+    })
+  });
+
+  describe('Recording a frame', function(){
+    it("can record a frame and track its score", function() { 
+      scorecard.recordRoll(3)
+      scorecard.recordRoll(5)
+      scorecard.recordRoll(2)
+      scorecard.recordRoll(1)
+      scorecard.recordRoll(8)
+      expect(scorecard.frameScore(1)).toEqual(8);
+      expect(scorecard.frameScore(2)).toEqual(3);
+      expect(scorecard.frameScore(3)).toEqual(8);
     })
   });
 
--- src/BowlingScorecard.js
+++ src/BowlingScorecard.js
@@ -3,14 +3,43 @@
 class BowlingScorecard {
   constructor() {
     this.total_score = 0;
+    this.currentFrame = 1;
+    this.currentFrameRolls = 0;
+    this.scorecard = { 1: [] }
   }
 
-recordRoll(userInput){
-  if (isNaN(userInput)) {throw "Can only record numbers"}
-  else if (userInput > 10) {throw "Scores must be 0-10"}
-  else {this.total_score += userInput;}
+recordRoll(userInput) {
+  this._checkUserInput(userInput);
+
+  this.currentFrameRolls += 1;
+  this.total_score += userInput;
+
+  this.scorecard[this.currentFrame].push(userInput);
+
+  if (this.currentFrameRolls == 2) {
+    this.currentFrame += 1
+    this.scorecard[this.currentFrame] = []
+    this.currentFrameRolls = 0
+  }
+
 }
-totalScore(){
+
+frameScore(frame) {
+  let frame_score = 0
+  const frame_array = this.scorecard[frame]  
+  for(let i=0; i < frame_array.length; i++) {
+    frame_score += frame_array[i]
+  }
+  return frame_score
+}
+
+_checkUserInput(userInput) {
+  if (isNaN(userInput)) {throw "Can only record numbers"}
+  else if (userInput > 10 || userInput < 0) {throw "Scores must be 0-10"}
+}
+
+
+totalScore() {
   return this.total_score;
 }
 

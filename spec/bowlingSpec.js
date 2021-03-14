d6eb5c47bffe14d75e429c60570aa3a296725142
Joanna Brigham <joannabrigham@hotmail.co.uk>
2021-03-14 11:35:41 +0100

    failing test for frame incrementing by one for each frame played

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -14,11 +14,21 @@ describe('before each game starts', function() {
 
     it('starts with a frame count of 0', function() {
         expect(bowling.checkFrameCount()).toEqual(0);
-
     });
 
-
 });
+
+describe('for each new frame played', function() {
+
+    it('increments the frame count by one', function() {
+        bowling.newFrame(3,2);
+        expect(bowling.checkFrameCount()).toEqual(1);
+
+    });
+});
+
+
+
 
 
 

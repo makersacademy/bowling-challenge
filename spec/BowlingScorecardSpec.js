43b71f4acfa64752bd6360b6b2280d71be17596a
Eds <steadyede@live.co.uk>
2021-03-14 21:12:51 +0000

    roll input checks

--- spec/BowlingScorecardSpec.js
+++ spec/BowlingScorecardSpec.js
@@ -6,13 +6,18 @@ describe('BowlingScorecard', function() {
 
   beforeEach(function() {
     scorecard = new BowlingScorecard()
-  })
+  });
 
-  it("can record a user's roll", function() {
-    scorecard.recordRoll(6)
-    expect(scorecard.totalScore()).toEqual(6);
-  })
-
+  describe('Recording rolls', function() {
+    it("can record a user's roll", function() {
+      scorecard.recordRoll(6)
+      expect(scorecard.totalScore()).toEqual(6);
+    })
+    it("only takes numbers under 11 as input", function() {
+      expect( function() {scorecard.recordRoll("three")} ).toThrow ("Can only record numbers")
+      expect( function() {scorecard.recordRoll("12")} ).toThrow ("Scores must be 0-10")
+    })
+  });
 
 
 })
--- src/BowlingScorecard.js
+++ src/BowlingScorecard.js
@@ -1,12 +1,14 @@
 'use strict';
 
 class BowlingScorecard {
-  constructor(){
+  constructor() {
     this.total_score = 0;
   }
 
 recordRoll(userInput){
-  this.total_score += userInput;
+  if (isNaN(userInput)) {throw "Can only record numbers"}
+  else if (userInput > 10) {throw "Scores must be 0-10"}
+  else {this.total_score += userInput;}
 }
 totalScore(){
   return this.total_score;

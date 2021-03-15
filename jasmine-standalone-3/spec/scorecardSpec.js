225143b1b705807c13eb1989294814a236a9b19a
AdamP2000 <Adam2000Phillips@gmail.com>
2021-03-15 09:54:23 +0000

    test for spare implementation

--- jasmine-standalone-3/spec/scorecardSpec.js
+++ jasmine-standalone-3/spec/scorecardSpec.js
@@ -34,5 +34,31 @@ describe('Scorecard', function() {
       expect(scorecard.points()).toBe(0)
     });
   });
+  describe('isSpare', function() {
+    it('shows score with spare implementation', function() {
+      scorecard.player_roll(7)
+      scorecard.player_roll(3)
+      scorecard.player_roll(5)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      scorecard.player_roll(0)
+      expect(scorecard.points()).toBe(20)
+    });
+  });
+
 
 });

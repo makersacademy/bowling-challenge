047e17fc08971e456e6c4d3c65352db925aa15f6
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-04-09 16:08:44 +0100

    added travis and karma integration

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,3 +1,13 @@
 describe('Bowling', function () {
+  let bowling;
 
+  beforeEach(function () {
+    bowling = new Bowling();
+  });
+
+  describe('the constructor creates the required vars', function () {
+    it('has a variable total', function () {
+      expect(bowling.total).toEqual(0);
+    });
+  });
 });

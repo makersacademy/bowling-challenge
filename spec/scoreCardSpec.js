5dcec55a4c0a8b6fa964508cf2428b7f3afd404a
Emmapr123 <emmapriester@hotmail.com>
2021-04-10 10:49:57 +0100

    empty frames array initiated

--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -1,3 +1,20 @@
+describe('scoreCard', function() {
+  let scoreCard = new ScoreCard;
+  
+  describe('frames', function() {
+    it ('should be empty by default', function() {
+      expect(scoreCard.frames).toEqual([])
+    })
+  })
+})
 
+// describe ScoreCard do
+//   let(:bowl) { ScoreCard.new }
+//   let(:frame) { double :frame }
+//   describe '#frames' do
+//     it 'should be empty by default' do
+//       expect(bowl.frames).to eq []
+//     end
+//   end
 
 

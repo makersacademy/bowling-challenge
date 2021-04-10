9982c24de0fc48a3d36c59bd3b220b65cea762fd
jackmcc08 <jackmcc08@gmail.com>
2021-04-10 15:46:42 +0100

    set up enter roll function and built in edge case to disallow invalid scores (over 10 or under 0, and non-number entries)

--- spec/BowlingScorecardSpec.js
+++ spec/BowlingScorecardSpec.js
@@ -10,5 +10,21 @@ describe("BowlingScorecard", function() {
     it("receives a roll from the user and processes it, returning the score", function() {
       expect(testScorecard.enterRoll(5)).toEqual(5);
     });
+
+    it("receives a roll from the user which is over 10 and it rejects it as an invalid roll", function() {
+      expect(testScorecard.enterRoll(11)).toEqual("Invalid score entered, score must be between 0 and 10.");
+    });
+
+    it("receives a roll from the user which is under 0 and it rejects it as an invalid roll", function() {
+      expect(testScorecard.enterRoll(-1)).toEqual("Invalid score entered, score must be between 0 and 10.");
+    });
+
+    it("receives a roll from the user which is not a number and it rejects it as an invalid roll", function() {
+      expect(testScorecard.enterRoll('s')).toEqual("Invalid score entered, score must be between 0 and 10.");
+    });
+
+    // still need to check for invalid scores on the second roll
   });
+
+  // describe
 });
--- src/BowlingScorecard.js
+++ src/BowlingScorecard.js
@@ -1,7 +1,22 @@
 class BowlingScorecard {
   enterRoll(score) {
+    // if (score > 10) {
+    //   return "Invalid score entered, score must be between 0 and 10."
+    // }
+
+    if (this._isInvalidScore(score)) return "Invalid score entered, score must be between 0 and 10.";
+
     return score
+  }
+
+  _isInvalidScore(score) {
+    if (score > 10 || score < 0) return true
+    if (`${score}`.match(/\D/) !== null) return true
+
+    return false
   }
 }
 
 module.exports = BowlingScorecard;
+
+// console.log("0".match(/\D/g))

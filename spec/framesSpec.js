3b308fa7f11c0c4cbc31eec9a0f710023aeb03d2
Emmapr123 <emmapriester@hotmail.com>
2021-04-10 11:05:10 +0100

    rolls are now saved in the rolls array in the frame class

--- spec/framesSpec.js
+++ spec/framesSpec.js
@@ -4,6 +4,12 @@ describe('frames', function() {
   describe('roll', function() {
     it ('should be empty before the first roll', function() {
       expect(frame.rolls).toEqual([])
-    })
-  })
-})
+    });
+    it ('can receive user input and save it', function() {
+      frame.roll(3)
+      frame.roll(5)
+
+      expect(frame.rolls).toEqual([3, 5])
+    });
+  });
+});
--- src/frames.js
+++ src/frames.js
@@ -2,4 +2,8 @@ class Frame {
   constructor(rolls) {
     this.rolls = []
   }
+  roll(pins) {
+    this.rolls.push(pins);
+  }
 }
+

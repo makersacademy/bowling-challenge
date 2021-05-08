53a55c73ec7919964c023d4147ce967c718f8df8
Jessica Brand <jeshbrand@gmail.com>
2021-05-08 21:59:02 +0100

    got some no-strike games passing tests

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -4,4 +4,9 @@ describe("Scorecard", ()=> {
     let guttergame = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
     expect(Scorecard(guttergame)).toEqual(0)
   })
+
+  it('returns 10 when player only hits 1 pin per frame',()=>{
+    let game = [[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0]]
+    expect(Scorecard(game)).toEqual(10)
+  })
 })
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,3 +1,24 @@
-function Scorecard(game){
-  return 0 
+function strikeOrSpare(frame){
+  return (frame.sum == 10 ? true : false);
+}
+function strike(frame){
+  return (frame == [10,0] ? true : false)
+}
+// takes in a nested arr.
+function Scorecard(bowls){
+  let total = 0
+  for (let f = 0 ; f < 10 ; f++) {
+    if (f < 9) {
+      console.log(`frame ${f}: ${bowls[f]}`)
+      if (strikeOrSpare(bowls[f])) {
+        // check for strikes
+      } else {
+        total += bowls[f].reduce((a, b) => a + b, 0)
+      }
+    } else if (f == 9) {
+      total += bowls[f].reduce((a, b) => a + b, 0)
+      console.log(total)
+    }
+  }
+  return total
 }

fd04790d0c8cc35140f8cdb0785e7efb93c0d41c
Adam Jonas <adamjonas25@gmail.com>
2021-05-09 09:52:43 +0100

    Move constants outside of frame class

--- public/js/src/frame.js
+++ public/js/src/frame.js
@@ -1,56 +1,58 @@
+const TOTAL_PINS = 10
+const STRIKE_BONUS = 2
+const SPARE_BONUS = 1
+
 // eslint-disable-next-line no-unused-vars
 class Frame {
-  constructor() {
-    this.TOTAL_PINS = 10;
-    this.STRIKE_BONUS = 2;
-    this.SPARE_BONUS = 1;
-    this.bonusCount = 0;
-    this.bonus = 0;
-    this.rolls = [];
-    this.isFinal = false;
+  constructor () {
+    this.bonusCount = 0
+    this.bonus = 0
+    this.rolls = []
+    this.isFinal = false
   }
 
-  addRoll(roll) {
-    this.rolls.push(roll);
-    if (this.isFinal) { return; }
+  addRoll (roll) {
+    this.rolls.push(roll)
+    if (this.isFinal) { return }
     if (this._isStrike()) {
-      this.bonusCount += this.STRIKE_BONUS;
+      this.bonusCount += STRIKE_BONUS
     } else if (this._isSpare()) {
-      this.bonusCount += this.SPARE_BONUS;
+      this.bonusCount += SPARE_BONUS
     }
   }
 
-  score() {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    if (this.rolls.length === 0) { return this.bonus; }
-    return this.rolls.reduce(reducer) + this.bonus;
+  score () {
+    if (this.rolls.length === 0) { return this.bonus }
+
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    return this.rolls.reduce(reducer) + this.bonus
   }
 
-  makeFinal() {
-    this.isFinal = true;
+  makeFinal () {
+    this.isFinal = true
   }
 
-  addBonus(bonus) {
-    if (this.bonusCount === 0) { return; }
-    this.bonusCount -= this.SPARE_BONUS;
-    this.bonus += bonus;
+  addBonus (bonus) {
+    if (this.bonusCount === 0) { return }
+    this.bonusCount -= SPARE_BONUS
+    this.bonus += bonus
   }
 
-  isOver() {
-    if (!this.isFinal) { return this._isStrike() || this.rolls.length >= 2; }
-    return this.rolls.length >= 3 || this._isTwoRollFinalFrame();
+  isOver () {
+    if (!this.isFinal) { return this._isStrike() || this.rolls.length >= 2 }
+    return this.rolls.length >= 3 || this._isTwoRollFinalFrame()
   }
 
-  _isTwoRollFinalFrame() {
-    return this.rolls.length >= 2 && this.score() < this.TOTAL_PINS;
+  _isTwoRollFinalFrame () {
+    return this.rolls.length >= 2 && this.score() < TOTAL_PINS
   }
 
-  _isStrike() {
-    return this.rolls[0] === this.TOTAL_PINS;
+  _isStrike () {
+    return this.rolls[0] === TOTAL_PINS
   }
 
-  _isSpare() {
-    const pins = this.rolls[0] + this.rolls[1];
-    return pins === this.TOTAL_PINS && !this._isStrike();
+  _isSpare () {
+    const pins = this.rolls[0] + this.rolls[1]
+    return pins === TOTAL_PINS && !this._isStrike()
   }
 }
--- public/js/src/game.js
+++ public/js/src/game.js
@@ -1,65 +1,64 @@
+// eslint-disable-next-line no-unused-vars
 class Game {
-  constructor(frameClass = Frame) {
-    this.FRAME_CLASS = frameClass;
-    this.TOTAL_FRAMES = 10;
-    this.frames = [];
+  constructor (frameClass = Frame) {
+    this.FRAME_CLASS = frameClass
+    this.TOTAL_FRAMES = 10
+    this.frames = []
   }
 
-  addRoll(pins) {
-    if (this.isOver()) { throw new Error('Game Over'); }
-    if (this.frames.length === 0) { this._newFrame(); }
+  addRoll (pins) {
+    if (this.isOver()) { throw new Error('Game Over') }
+    if (this.frames.length === 0) { this._newFrame() }
 
-    const roll = parseInt(pins, 10);
-    this._validate(roll);
-    this.frames.forEach((frame) => { frame.addBonus(roll); });
-    this._currentFrame().addRoll(roll);
+    const roll = parseInt(pins, 10)
+    this._validate(roll)
+    this.frames.forEach((frame) => { frame.addBonus(roll) })
+    this._currentFrame().addRoll(roll)
 
-    if (this._isFinalFrame()) { return; }
-    if (this._currentFrame().isOver()) { this._newFrame(); }
+    if (this._isFinalFrame()) { return }
+    if (this._currentFrame().isOver()) { this._newFrame() }
   }
 
-  totalScore() {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    return this._scores().reduce(reducer);
+  totalScore () {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue
+    return this._scores().reduce(reducer)
   }
 
-  scoreBoard() {
-    const scores = ((sum) => (value) => sum += value)(0);
-    return this._scores().map(scores);
+  scoreBoard () {
+    const accumulator = ((sum) => (value) => sum += value)(0)
+    return this._scores().map(accumulator)
   }
 
-  isOver() {
-    return this._isFinalFrame() && this._currentFrame().isOver();
+  isOver () {
+    return this._isFinalFrame() && this._currentFrame().isOver()
   }
 
-  _scores() {
-    return this.frames.map((frame) => frame.score());
+  _scores () {
+    return this.frames.map((frame) => frame.score())
   }
 
-  _currentFrame() {
-    return this.frames[this.frames.length - 1];
+  _currentFrame () {
+    return this.frames[this.frames.length - 1]
   }
 
-  _newFrame() {
-    this.frames.push(new this.FRAME_CLASS());
-    if (this._isFinalFrame()) {
-      this._currentFrame().makeFinal();
-    }
+  _newFrame () {
+    this.frames.push(new this.FRAME_CLASS())
+    if (this._isFinalFrame()) { this._currentFrame().makeFinal() }
   }
 
-  _isFinalFrame() {
-    return this.frames.length === this.TOTAL_FRAMES;
+  _isFinalFrame () {
+    return this.frames.length === this.TOTAL_FRAMES
   }
 
-  _validate(roll) {
-    if (roll > 10 || roll < 0) { throw new Error('Invalid roll'); }
+  _validate (roll) {
+    if (roll > 10 || roll < 0) { throw new Error('Invalid roll') }
     if (roll + this._currentFrame().score() > this._currentFrame().TOTAL_PINS) {
-      if (!this._isBonusRoll()) { throw new Error('Invalid roll'); }
+      if (!this._isBonusRoll()) { throw new Error('Invalid roll') }
     }
   }
 
-  _isBonusRoll() {
-    const strikeRoll = this._isFinalFrame() && this._currentFrame()._isStrike();
-    return (strikeRoll || this._currentFrame()._isSpare());
+  _isBonusRoll () {
+    const strikeRoll = this._isFinalFrame() && this._currentFrame()._isStrike()
+    return (strikeRoll || this._currentFrame()._isSpare())
   }
 }

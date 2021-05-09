285bd18654877eaf70eb693e760f92f80ee85510
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-05-09 01:10:39 +0100

    spare functions refactored

--- src/bowling.js
+++ src/bowling.js
@@ -11,19 +11,27 @@ class Bowling {
   score() {
     let totalScore = 0;
     let rollNumber = 0;
+    let bowling = this;
+
     for (let frame = 0; frame < 10; frame++) {
-      if (this.rolls[rollNumber] + this.rolls[rollNumber + 1] == 10) {
-        totalScore += this.rolls[rollNumber] + this.rolls[rollNumber + 1] + this.rolls[rollNumber + 2]
+      if (spare(rollNumber) ){
+        spareBonus(rollNumber)
       } else {
-      totalScore += this.rolls[rollNumber] + this.rolls[rollNumber + 1] 
+        frameScore(rollNumber)
       }
       rollNumber += 2
     }
-    return totalScore
+    return totalScore;
+
+  function spare(rollNumber) {
+   return (bowling.rolls[rollNumber] + bowling.rolls[rollNumber + 1] == 10)
   }
+  function spareBonus(rollNumber) {
+    return totalScore += bowling.rolls[rollNumber] + bowling.rolls[rollNumber + 1] + bowling.rolls[rollNumber + 2]
+  }
+  function frameScore(rollNumber) {
+    return totalScore += bowling.rolls[rollNumber] + bowling.rolls[rollNumber + 1] 
+  }
+  }
+  
 }
-
-
-  // message() {
-  //   return `You are currently on Frame ${this.frame}, with a total score of ${totalScore}.`
-  // }

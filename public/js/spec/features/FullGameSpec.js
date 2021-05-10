efbc40ea84c9a63e35baccf6babd85a9c635e05b
Adam Jonas <adamjonas25@gmail.com>
2021-05-10 17:01:31 +0100

    Add CSS classes to all html elements

--- public/js/spec/features/FullGameSpec.js
+++ public/js/spec/features/FullGameSpec.js
@@ -9,7 +9,7 @@ describe('Feature testing a full game', () => {
 
   it('runs correctly when game is a gutter game', () => {
     const game = new Game()
-    new Array(20).fill(0).forEach((roll) => game.addRoll(roll))
+    new Array(20).fill(0).forEach(roll => game.addRoll(roll))
 
     expect(scoreBoard.totalScore(game.frames)).toBe(0)
     expect(scoreBoard.calculateRunningTotal(game.frames)).toEqual(
@@ -19,7 +19,7 @@ describe('Feature testing a full game', () => {
 
   it('runs correctly when every frame is a spare', () => {
     const game = new Game()
-    new Array(21).fill(5).forEach((roll) => game.addRoll(roll))
+    new Array(21).fill(5).forEach(roll => game.addRoll(roll))
 
     expect(scoreBoard.totalScore(game.frames)).toBe(150)
     expect(scoreBoard.calculateRunningTotal(game.frames)).toEqual(
@@ -29,7 +29,7 @@ describe('Feature testing a full game', () => {
 
   it('runs correctly when game is a perfect game', () => {
     const game = new Game()
-    new Array(12).fill(10).forEach((roll) => game.addRoll(roll))
+    new Array(12).fill(10).forEach(roll => game.addRoll(roll))
 
     expect(scoreBoard.totalScore(game.frames)).toBe(300)
     expect(scoreBoard.calculateRunningTotal(game.frames)).toEqual(
@@ -42,7 +42,7 @@ describe('Feature testing a full game', () => {
     const exampleGame = [
       1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6
     ]
-    exampleGame.forEach((roll) => game.addRoll(roll))
+    exampleGame.forEach(roll => game.addRoll(roll))
 
     expect(scoreBoard.totalScore(game.frames)).toBe(133)
     expect(scoreBoard.calculateRunningTotal(game.frames)).toEqual(
@@ -55,7 +55,7 @@ describe('Feature testing a full game', () => {
     const exampleGame2 = [
       9, 1, 0, 10, 10, 10, 6, 2, 7, 3, 8, 2, 10, 9, 0, 10, 10, 8
     ]
-    exampleGame2.forEach((roll) => game.addRoll(roll))
+    exampleGame2.forEach(roll => game.addRoll(roll))
 
     expect(scoreBoard.totalScore(game.frames)).toBe(176)
     expect(scoreBoard.calculateRunningTotal(game.frames)).toEqual(

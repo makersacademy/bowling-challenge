b8e8ffd62a4b6c7a23cea4691c9cadd88cbf81e6
Kane <kgin888@gmail.com>
2021-05-10 16:02:53 +0100

    clean up and readme

--- spec/gameSpec.js
+++
@@ -1,9 +0,0 @@
-describe('Game', function() {
-  let game
-  let scorecard
-
-  beforeEach(function() {
-    game = new Game
-    scorecard = createSpyObj('scorecard', ['add_score', 'calculate_score', 'game_over?'])
-  })
-})

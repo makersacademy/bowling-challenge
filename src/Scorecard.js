d715cc6d61d6412c9b4e879d9bba7208b09f71a1
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-10 01:03:48 +0100

    implemented final functionality

--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,7 +1,6 @@
 class Scorecard {
   constructor(){
     this.scoreData = []
-
     let counter = 1
     for (let i = 1; i <= 10; i++) {
       let frame = {
@@ -16,42 +15,34 @@ class Scorecard {
       counter ++
       this.scoreData.push(frame)
     }
-
     this.runningTotal = 0
     this.currentFrameNumber = 1
     this.currentRoll = 1
+    this.gameOver = false
   };
-
   addScore(score){
-    if (this.currentFrameNumber > 10){
-      if(this.scoreData[(this.currentFrameNumber - 2)].isSpare){
-        this._lastRound(score)
-        return
-      } else {
-        this._gameOver()
-        return
-      }  
-    } else if (this._isCurrentRollFirst()){
+    if (this.currentFrameNumber === 10){
+      
+      this._lastRound(score)
+      return
+    }
+    if (this.currentRoll === 1){
       this._updateFirstRollScore(score)
       if(this._isStrike(score)){
         this._strike(score)
         return
       }
       this._switchRolls()
-    } else if (this._isCurrentRollSecond()) {
+    } else if (this.currentRoll === 2) {
       this._updateSecondRollScore(score)
       this._switchRolls()
-      this._updateFrameNumber()  
+      this._updateFrameNumber() 
     }
     this._spare(score)
   };
-
   _updateFrameNumber(){
-    // if(this.currentFrameNumber < 10){
-      this.currentFrameNumber ++ 
-    // }
+    this.currentFrameNumber ++
   };
-
   _switchRolls(){
     if(this.currentRoll === 1) {
       this.currentRoll ++
@@ -59,33 +50,24 @@ class Scorecard {
       this.currentRoll = (this.currentRoll - 1)
     }
   }
-
   _updateFirstRollScore(score) {
     this._updateStrikeBonus(score)
     this._updateSpareBonus(score)
     this.scoreData[(this.currentFrameNumber - 1)].firstRollScore += score
     this._updateTotalScore(score)
   }
-
   _updateSecondRollScore(score){
     this._updateStrikeBonus(score)
     this.scoreData[(this.currentFrameNumber - 1)].secondRollScore += score
     this._updateTotalScore(score)
   }
-
   _strike(score){
-    if(this._isLastFrame()) {
-      return
-    } else {
-      this.scoreData[(this.currentFrameNumber - 1)].isStrike = true
-      this._updateFrameNumber()
-    }
+    this.scoreData[(this.currentFrameNumber - 1)].isStrike = true
+    this._updateFrameNumber()
   }
-
   _isStrike(score){
     return score === 10
   }
-
   _updateStrikeBonus(score){
     if(this.currentFrameNumber > 1){
       if(this.scoreData[(this.currentFrameNumber - 2)].isStrike === true){
@@ -95,13 +77,11 @@ class Scorecard {
       }
     }
   }
-
   _spare(score){
     if (this.currentFrameNumber > 1 && this.scoreData[this.currentFrameNumber - 2].totalFrameScore === 10) {
       this.scoreData[this.currentFrameNumber - 2].isSpare = true
     }  
   }
-
   _updateSpareBonus(score){
     if(this.currentFrameNumber > 1){
       if(this.scoreData[(this.currentFrameNumber - 2)].isSpare === true){
@@ -111,36 +91,34 @@ class Scorecard {
       }
     }
   }
-
   _updateTotalScore(score){
     this.scoreData[(this.currentFrameNumber - 1)].totalFrameScore += score
     this.runningTotal += score
     this.scoreData[(this.currentFrameNumber - 1)].totalScore = this.runningTotal
   }
-
   _lastRound(score){
-    this.scoreData[(this.currentFrameNumber - 2)].totalScore += score
-    this.scoreData[(this.currentFrameNumber - 2)].totalFrameScore += score
-    this.runningTotal += score
-    this.scoreData[9].thirdRoll = score
-    this._gameOver() 
+    if (this.gameOver){
+      return "Game Over!"
+    }
+    if (this.currentRoll === 1){
+      this._updateFirstRollScore(score)
+      this._switchRolls()
+    } else if (this.currentRoll === 2){
+      this._updateSecondRollScore(score)
+      if (score === 10) {
+        this.scoreData[9].isStrike = true
+        this.currentRoll = 3
+      } else if (this.scoreData[9].firstRollScore + score === 10){
+        this.scoreData[9].isSpare = true
+        this.currentRoll = 3
+      } else {
+        this.gameOver = true
+      }
+    } else if(this.currentRoll === 3) {
+      this.scoreData[9].thirdRollScore = score
+      this.scoreData[9].totalScore += score
+      this.scoreData[9].totalFrameScore += score
+      this.gameOver = true
+    }
   }
-
-  _isLastFrame() {
-    return this.currentFrameNumber >= 9
-  }
-
-  _gameOver() {
-    return 'Game Over'
-  }
-
-  _isCurrentRollFirst(){
-    return this.currentRoll === 1
-  }
-
-  _isCurrentRollSecond(){
-    return this.currentRoll === 2
-  }
-
 };
-

0539f48bbef0c7c3735fe04a9ef8d3558569e870
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-05-10 01:43:41 +0100

    final refactor

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 
 Bowling Challenge - Solution 
 =================
+
+Please run the following command to run the test:
+
+```
+open SpecRunner.html
+```
+
+After that you can open JS console to test the code manually.
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -20,34 +20,36 @@ class Scorecard {
     this.currentRoll = 1
     this.gameOver = false
   };
+
   addScore(score){
     if (this.currentFrameNumber === 10){
-      
       this._lastRound(score)
       return
     }
-    if (this.currentRoll === 1){
+    if (this._isCurrentRoleFirst()){
       this._updateFirstRollScore(score)
       if(this._isStrike(score)){
         this._strike(score)
         return
       }
       this._switchRolls()
-    } else if (this.currentRoll === 2) {
+    } else if (this._isCurrentRoleSecond()) {
       this._updateSecondRollScore(score)
       this._switchRolls()
       this._updateFrameNumber() 
     }
     this._spare(score)
   };
+
   _updateFrameNumber(){
     this.currentFrameNumber ++
   };
+
   _switchRolls(){
-    if(this.currentRoll === 1) {
-      this.currentRoll ++
+    if(this._isCurrentRoleFirst()) {
+      this.currentRoll = 2
     } else {
-      this.currentRoll = (this.currentRoll - 1)
+      this.currentRoll  = 1
     }
   }
   _updateFirstRollScore(score) {
@@ -65,9 +67,11 @@ class Scorecard {
     this.scoreData[(this.currentFrameNumber - 1)].isStrike = true
     this._updateFrameNumber()
   }
+
   _isStrike(score){
     return score === 10
   }
+
   _updateStrikeBonus(score){
     if(this.currentFrameNumber > 1){
       if(this.scoreData[(this.currentFrameNumber - 2)].isStrike === true){
@@ -77,11 +81,16 @@ class Scorecard {
       }
     }
   }
+
   _spare(score){
-    if (this.currentFrameNumber > 1 && this.scoreData[this.currentFrameNumber - 2].totalFrameScore === 10) {
+    if (
+      this.currentFrameNumber > 1 && 
+      this.scoreData[this.currentFrameNumber - 2].totalFrameScore === 10
+    ){
       this.scoreData[this.currentFrameNumber - 2].isSpare = true
     }  
   }
+
   _updateSpareBonus(score){
     if(this.currentFrameNumber > 1){
       if(this.scoreData[(this.currentFrameNumber - 2)].isSpare === true){
@@ -91,34 +100,56 @@ class Scorecard {
       }
     }
   }
+
   _updateTotalScore(score){
     this.scoreData[(this.currentFrameNumber - 1)].totalFrameScore += score
     this.runningTotal += score
     this.scoreData[(this.currentFrameNumber - 1)].totalScore = this.runningTotal
   }
+
   _lastRound(score){
     if (this.gameOver){
       return "Game Over!"
     }
-    if (this.currentRoll === 1){
+    if (this._isCurrentRoleFirst()){
       this._updateFirstRollScore(score)
       this._switchRolls()
-    } else if (this.currentRoll === 2){
+    } else if (this._isCurrentRoleSecond()){
       this._updateSecondRollScore(score)
-      if (score === 10) {
+      if (this._isStrike(score)) {
         this.scoreData[9].isStrike = true
         this.currentRoll = 3
       } else if (this.scoreData[9].firstRollScore + score === 10){
         this.scoreData[9].isSpare = true
         this.currentRoll = 3
       } else {
-        this.gameOver = true
+        this._gameOver()
       }
-    } else if(this.currentRoll === 3) {
-      this.scoreData[9].thirdRollScore = score
-      this.scoreData[9].totalScore += score
-      this.scoreData[9].totalFrameScore += score
-      this.gameOver = true
+    } else if(this._isCurrentRoleThird()) {
+      this._updateThirdRollScore(score)
+      this._gameOver()
     }
+  }
+
+  _isCurrentRoleFirst(){
+    return this.currentRoll === 1
+  }
+  
+  _isCurrentRoleSecond(){
+    return this.currentRoll === 2
+  }
+  
+  _isCurrentRoleThird(){
+    return this.currentRoll === 3
+  }
+
+  _updateThirdRollScore(score){
+    this.scoreData[9].thirdRollScore = score
+    this.scoreData[9].totalScore += score
+    this.scoreData[9].totalFrameScore += score
+  }
+
+  _gameOver(){
+    this.gameOver = true
   }
 };

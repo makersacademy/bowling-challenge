3e2bc195e3ebe723e24e24d2f11e3be49fae9e87
Adam Jonas <adamjonas25@gmail.com>
2021-05-11 10:16:08 +0100

    Fix preprocessor path in Karma and NYC config

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = (config) => {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'public/js/src/*.js': 'coverage',
+      'public/js/src/models/*.js': 'coverage',
     },
 
     // test results reporter to use

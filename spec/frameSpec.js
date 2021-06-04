ee1ab640e27d2269e6ec957eabe2d6a8cfb77f5a
BecaLParker <becaparker@gmail.com>
2021-06-04 13:53:36 +0100

    Initial commit

---
+++ spec/frameSpec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+describe('Frame', () => {
+  let frame
+  beforeEach(function () {
+    frame = new Frame()
+  })
+
+  
+})
---
+++ spec/scorecardSpec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+describe('Scorecard', () => {
+  let scorecard
+  beforeEach(function () {
+    scorecard = new Scorecard()
+  })
+
+  
+})

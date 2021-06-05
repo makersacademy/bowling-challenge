4865b6337806a2a2c6a6ad9e1cc9328a53a9a206
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-05 14:30:16 +0100

    Game.prototype.bowl() sends .addBonus to all frames

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -12,8 +12,8 @@ describe('Game', () => {
     game = new Game(FrameClassDouble)
   })
 
-  describe('.prototype.bowl()', () => {
-    it('sends .addRoll() to the current frame', () => {
+  describe('.prototype.bowl(pinfall)', () => {
+    it('sends .addRoll(pinfall) to the current frame', () => {
       const firstFrame = game._currentFrame
       spyOn(firstFrame, 'addRoll')
       game._addNewFrame()
@@ -23,6 +23,22 @@ describe('Game', () => {
 
       expect(firstFrame.addRoll).not.toHaveBeenCalled()
       expect(secondFrame.addRoll).toHaveBeenCalledWith(4)
+    })
+
+    it('sends .addBonus(pinfall) to all frames', () => {
+      const firstFrame = game._currentFrame
+      spyOn(firstFrame, 'addBonus')
+      game._addNewFrame()
+      const secondFrame = game._currentFrame
+      spyOn(secondFrame, 'addBonus')
+      game._addNewFrame()
+      const thirdFrame = game._currentFrame
+      spyOn(thirdFrame, 'addBonus')
+      game.bowl(4)
+
+      expect(firstFrame.addBonus).toHaveBeenCalledWith(4)
+      expect(secondFrame.addBonus).toHaveBeenCalledWith(4)
+      expect(thirdFrame.addBonus).toHaveBeenCalledWith(4)
     })
   })
 })

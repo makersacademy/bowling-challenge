b50b281788b2212be4a0ce0ff9ca2ac17cc78b6b
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-05 14:25:05 +0100

    Game.prototype.bowl() sends .addRoll() to current frame

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -3,21 +3,26 @@ const Game = require('../lib/game')
 describe('Game', () => {
   let game
 
+  class FrameClassDouble {
+    addRoll () {}
+    addBonus () {}
+  }
+
   beforeEach(() => {
-    game = new Game()
+    game = new Game(FrameClassDouble)
   })
 
   describe('.prototype.bowl()', () => {
-    describe('when no strike or spare', () => {
-      it('when given 4 it returns 4', () => {
-        expect(game.bowl(4)).toBe(4)
-      })
+    it('sends .addRoll() to the current frame', () => {
+      const firstFrame = game._currentFrame
+      spyOn(firstFrame, 'addRoll')
+      game._addNewFrame()
+      const secondFrame = game._currentFrame
+      spyOn(secondFrame, 'addRoll')
+      game.bowl(4)
 
-      it('when given 4 then 2 it returns 6', () => {
-        game.bowl(4)
-
-        expect(game.bowl(2)).toBe(6)
-      })
+      expect(firstFrame.addRoll).not.toHaveBeenCalled()
+      expect(secondFrame.addRoll).toHaveBeenCalledWith(4)
     })
   })
 })

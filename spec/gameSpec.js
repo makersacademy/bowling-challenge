355aa58839151d2f64337c9001133407cbd1d4f9
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-05 13:58:48 +0100

    Game.prototype.bowl() returns sum of previous pinfalls

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -12,6 +12,12 @@ describe('Game', () => {
       it('when given 4 it returns 4', () => {
         expect(game.bowl(4)).toBe(4)
       })
+
+      it('when given 4 then 2 it returns 6', () => {
+        game.bowl(4)
+
+        expect(game.bowl(2)).toBe(6)
+      })
     })
   })
 })

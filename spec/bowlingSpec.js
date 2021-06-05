e3ee9cf30cc3832710506b0e13d8a5f8f38d54c1
charlie-casserly <charliecasserly215@gmail.com>
2021-06-05 15:34:27 +0100

    Adding to scoreboard complete apart from round 10

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -4,10 +4,43 @@ describe('Bowling', () => {
   let bowling;
 
   beforeEach(() => {
-    bowling = new bowling();
+    bowling = new Bowling();
   }) 
 
   it('starts on round 1', () => {
-    expect(bowling.round()).toEqual(1)
+    expect(bowling.getCurrentRound()).toEqual(1);
+  })
+
+  it('starts on ball 1', () => {
+    expect(bowling.getCurrentBall()).toEqual(1);
+  })
+
+  it('increases the round after two balls, no strike', () => {
+    bowling.inputPins(1);
+    bowling.inputPins(1);
+    expect(bowling.getCurrentRound()).toEqual(2);
+  })
+
+  it('increases the ball after each throw', () => {
+    bowling.inputPins(1);
+    expect(bowling.getCurrentBall()).toEqual(2);
+  })
+
+  it('adds the pins to the scoreboard', () => {
+    bowling.inputPins(5);
+    expect(bowling.showScorecard()).toContain([5, null]);
+  })
+
+  it('adds a round to the scoreboard', () => {
+    bowling.inputPins(1);
+    bowling.inputPins(1);
+    expect(bowling.showScorecard()).toContain([1, 1]);
+  })
+
+  it('creates a new array for each round', () => {
+    for (let i = 0; i < 4; i++) {
+      bowling.inputPins(1);
+    }
+    expect(bowling.showScorecard()).toContain([1, 1], [1, 1]);
   })
 })
--- src/bowling.js
+++ src/bowling.js
@@ -1,2 +1,41 @@
 'use strict';
 
+class Bowling {
+  constructor() {
+    this.currentRound = 1
+    this.currentBall = 1
+    this.scorecard = [[null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null], [null, null]]
+  }
+
+  getCurrentRound() {
+    return this.currentRound
+  }
+
+  getCurrentBall() {
+    return this.currentBall
+  }
+
+  // inputPins(pins) {
+  //   if (this.currentBall === 1) {
+  //     this.firstPin = pins;
+  //     this.currentBall += 1;
+  //   }
+  //   else if (this.currentBall === 2) {
+  //     (this.scorecard).push([this.firstPin, pins]);
+  //     this.currentBall = 1;
+  //   }
+  // }
+
+  inputPins(pins) {
+    this.scorecard[this.currentRound - 1][this.currentBall - 1] = pins;
+    this.currentBall += 1;
+    if (this.currentBall === 3) {
+      this.currentBall = 1;
+      this.currentRound += 1;
+    }
+  }
+
+  showScorecard() {
+    return this.scorecard
+  }
+}

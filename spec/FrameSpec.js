7b714002a520f6ccf5fb9dd1a40c8627b60a9f9c
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-06 12:55:53 +0100

    frame class added

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -8,11 +8,26 @@ describe("Frame", () => {
   });
 
   describe("#roll", () => {
-    it("adds to knocked down pins", () => {
+    it("adds to the frame score", () => {
       frame.roll(3);
       frame.roll(4);
 
-      expect(frame.knockedDownPins).toEqual(7);
+      expect(frame.frameScore()).toEqual(7);
+    });
+  });
+
+  describe("#isStrike", () => {
+    it("returns true if the frame is a strike", () => {
+      frame.roll(10);
+
+      expect(frame.isStrike()).toBe(true);
+    });
+
+    it("returns false if the frame is not a strike", () => {
+      frame.roll(0);
+      frame.roll(10);
+
+      expect(frame.isStrike()).toBe(false);
     });
   });
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,13 +1,51 @@
 module.exports = class Frame {
   constructor() {
-    this._knockedDownPins = 0;
+    this.FRAME_PINS = 10;
+    this.FRAME_ROLLS = 2;
+    this.rolls = [];
   }
 
   roll(pins) {
-    this._knockedDownPins += pins;
+    this.rolls.push(pins);
   }
 
-  get knockedDownPins() {
-    return this._knockedDownPins;
+  firstRoll() {
+    return this.rolls[0];
+  }
+
+  secondRoll() {
+    return this.rolls[1];
+  }
+
+  firstTwoRolls() {
+    return this.firstRoll() + this.secondRoll();
+  }
+
+  frameScore() {
+    return this.rolls.reduce((previous, current) => previous + current, 0);
+  }
+
+  isStrike() {
+    return this.frameScore() == 10 && this.rolls.length === 1;
+  }
+
+  isSpare() {
+    return this.frameScore() == 10 && this.rolls.length === 2;
+  }
+
+  isClosed(isFinalFrameInput) {
+    if (
+      isFinalFrameInput &&
+      (this.isSpare() || this.isStrike() || this.frameScore() % 10 === 0) &&
+      this.rolls.length !== 3
+    ) {
+      return false;
+    } else if (isFinalFrameInput && this.rolls.length === 3) {
+      return true;
+    }
+
+    return (
+      this.rolls.length === 2 || (this.rolls.length === 1 && this.isStrike())
+    );
   }
 };

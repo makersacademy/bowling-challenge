caaf55d6b36e5e13327845dbacae17b1dd27916a
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-06-06 14:14:27 +0100

    10th frame: when a spare

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -76,5 +76,19 @@ describe("Game", () => {
         expect(game.score()).toEqual(119);
       });
     });
+
+    describe("when the player makes a spare", () => {
+      it("returns 133", () => {
+        let rolls = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10];
+        rolls.forEach((roll) => {
+          game.bowl(roll);
+        });
+
+        game.bowl(2);
+        game.bowl(8);
+        game.bowl(6);
+        expect(game.score()).toEqual(133);
+      });
+    });
   });
 });

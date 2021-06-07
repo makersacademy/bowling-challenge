7ccacad6248f0ac934941d9fe75eab0735ea53fb
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-06-07 01:25:13 +0100

    extra tests with example score

--- spec/bowlingChallengeSpec.js
+++ spec/bowlingChallengeSpec.js
@@ -32,5 +32,35 @@ describe("BowlingScore", function() {
       }
       expect(bowlingScore.totalScore()).toEqual(12);
     })
+
+    it("saves a perfect game", function() {
+      for(i = 1; i <= 12; i++) {
+        bowlingScore.roll(10);
+      }
+      expect(bowlingScore.totalScore()).toEqual(300)
+    })
+
+    it("returns score for example game correctly", function() {
+      bowlingScore.roll(1);
+      bowlingScore.roll(4);
+      bowlingScore.roll(4);
+      bowlingScore.roll(5);
+      bowlingScore.roll(6);
+      bowlingScore.roll(4);
+      bowlingScore.roll(5);
+      bowlingScore.roll(5);
+      bowlingScore.roll(10);
+      bowlingScore.roll(0);
+      bowlingScore.roll(1);
+      bowlingScore.roll(7);
+      bowlingScore.roll(3);
+      bowlingScore.roll(6);
+      bowlingScore.roll(4);
+      bowlingScore.roll(10);
+      bowlingScore.roll(2);
+      bowlingScore.roll(8);
+      bowlingScore.roll(6);
+      expect(bowlingScore.totalScore()).toEqual(133)
+    })
   });
 })

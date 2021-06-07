81f3e5c7cecb003e12f9e3c93b9eeeea4ff743a0
JP Ferreira <jpferreira@MA301.broadband>
2021-06-07 09:13:15 +0100

    Completing logic

---
+++ spec/bowlingScorecardSpec.js
@@ -0,0 +1,55 @@
+'use sctrict';
+
+describe('BowlingScorecard', () => {
+
+  let game;
+
+  beforeEach(() => {
+    game = new BowlingScorecard();
+  })
+
+  let bowlMulti = (pins, bowls) => {
+    for (let i = 0; i < bowls; i++) {
+      game.bowl(pins);
+    }
+  }
+
+  describe('game', () => {
+    it('can bowl a gutter game', () => {
+      bowlMulti(0, 20)
+      expect(game.score()).toEqual(0);
+    });
+
+    it('can bowl all ones', () => {
+      bowlMulti(1, 20)
+      expect(game.score()).toEqual(20);
+    });
+
+    it('can bowl a spare', () => {
+      game.bowl(7);
+      game.bowl(3);
+      game.bowl(3);
+      bowlMulti(0, 17);
+      expect(game.score()).toEqual(16);
+    });
+
+    it('can bowl a strike', () => {
+      game.bowl(10);
+      game.bowl(3);
+      game.bowl(4);
+      bowlMulti(0, 16);
+      expect(game.score()).toEqual(24);
+    });
+
+    it('can bowl a perfect game', () => {
+      bowlMulti(10, 12);
+      expect(game.score()).toEqual(300);
+    });
+
+    it('can bowl a game of all spares', () => {
+      bowlMulti(5, 21);
+      expect(game.score()).toEqual(150);
+    });
+  });
+
+});

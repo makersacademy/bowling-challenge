20a60dbcf827c0bf8efb4e19e50732c1fca6963d
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-07-10 19:07:01 +0100

    functionality for recording frame and tenth frame

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -11,4 +11,23 @@ describe('Bowling', () => {
     expect(bowling.rolls).toEqual([]);
   });
 
-})
+  it('pushes first and second roll into the rolls array', () => {
+      bowling.frame(2, 3)
+      expect(bowling.rolls[0]).toEqual({ first: 2, second: 3 })
+  });
+
+  it('records a strike', () => {
+    bowling.frame(10)
+    expect(bowling.rolls[0]).toEqual({ first: 10 })
+  });
+
+  it('pushes only two rolls if there is no strike or spare in tenth frame', () => {
+    bowling.tenthFrame(2, 3)
+    expect(bowling.rolls[0]).toEqual({ first: 2, second: 3 })
+  });
+
+  it('pushes three rolls with a strike or spare in the tenth', () => {
+    bowling.tenthFrame(6, 4, 10)
+    expect(bowling.rolls[0]).toEqual({ first: 6, second: 4, third: 10 })
+  });
+});
--- src/bowling.js
+++ src/bowling.js
@@ -3,5 +3,22 @@
 class Bowling {
   constructor() {
     this.rolls = []
-  }
+  };
+
+  frame(first, second = 'strike') {
+    if (second === 'strike') {
+      this.rolls.push({first: first});
+    } else {
+    this.rolls.push({first: first, second: second})
+    };
+  };
+
+  tenthFrame(first, second, third = 'no strike or spare') {
+    if (third === 'no strike or spare') {
+      this.rolls.push({first: first, second: second});
+    } else {
+      this.rolls.push({first: first, second: second, third: third});
+    };
+  };
+
 };

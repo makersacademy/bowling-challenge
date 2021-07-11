44a79cdc95cff9514a40188dd0eb1ae0daff6bb7
Craig Eaton <craigea92@gmail.com>
2021-07-11 19:59:03 +0100

    Implemented Spare Logic

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -8,17 +8,27 @@ describe ('Bowling', () => {
     bowling = new Bowling();
   });
 
-  it('can score a gutter game', () => {
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(0)
+  function rollMany(pins, rolls) {
+    for (let i = 0; i < rolls; i++) {
+      bowling.roll(pins)
     }
+  }
+
+  it('can score a gutter game', () => {
+    rollMany(0, 20)
     expect(bowling.score()).toEqual(0);
   });
 
   it('can score 20 for a game of all 1s', () => {
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(1)
-    }
+    rollMany(1, 20)
     expect(bowling.score()).toEqual(20)
+  });
+
+  it('can score a spare round', () => {
+    bowling.roll(5);
+    bowling.roll(5);
+    bowling.roll(3);
+    rollMany(0, 17);
+    expect(bowling.score()).toEqual(16);
   });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -2,14 +2,27 @@
 
 class Bowling {
   constructor() {
-    this.runningScore = 0;
-  }
-
-  score() {
-    return this.runningScore;
+    this.rolls = [];
   }
 
   roll(pins) {
-    this.runningScore += pins;
+    this.rolls.push(pins)
+  }
+
+  score() {
+    let score = 0;
+    let rollIndex = 0;
+
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+      const framescore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+
+      if (framescore === 10) {
+        score += 10 + this.rolls[rollIndex + 2];
+      } else {
+        score += framescore;
+      }
+      rollIndex +=2
+    }
+    return score;
   }
 }

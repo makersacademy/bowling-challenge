8def24a2d642202ed6601f12bbdd6a0ef2e5224e
Cynthia Fu <yinnycfu@gmail.com>
2021-07-11 19:33:12 +0100

    frame can add rolls

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -6,12 +6,27 @@ describe("Frame", () => {
   beforeEach( () => {
     frame = new Frame();
   });
-  
+
   describe("isEnded()", () => {
     it("is not true by default", () => {
       expect(frame.isEnded()).not.toBe(true);
     });
-
-    
   });
+
+  describe("add()", () => {
+    it("can store rolls", () => {
+      frame.add(2);
+
+      expect(frame.pins(1)).toEqual(2);
+    });
+
+    it("can store a second roll", () => {
+      frame.add(2);
+      frame.add(3);
+
+      expect(frame.pins(2)).toEqual(3);
+    });
+  });
+  
+  
 });
--- src/Frame.js
+++ src/Frame.js
@@ -1,8 +1,21 @@
 'use strict';
 
 class Frame {
+  constructor() {
+    this.rolls = [];
+  }
 
   isEnded() {
     return false
   }
+
+  add(number) {
+    this.rolls.push(number);
+  }
+
+  pins(turn) {
+    const i = turn - 1;
+    return this.rolls[i];
+  }
+
 }

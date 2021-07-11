3bd31aa488560e2c23948ccab3527078087e1d5a
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-11 17:41:09 +0200

    normal game

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -17,4 +17,13 @@ describe('Bowl', function() {
     })
   })
 
+  describe('can roll all ones', function() {
+    it('can roll all ones', function(){
+     for (let i = 0; i < 20; i++) {
+      game.bowl(1)
+     }
+     expect(game.score()).toBe(20)
+    })
+  })
+
 })
--- src/game.js
+++ src/game.js
@@ -5,10 +5,16 @@ class Game {
 
 
   score() {
-    return 0
+    let total = 0
+    for (let i = 0; i < 20; i++) {
+      total += this.scores[i]
+    }
+    return total
   }
 
   bowl(pins) {
-
+    this.scores.push(pins)
   }
 }
+
+

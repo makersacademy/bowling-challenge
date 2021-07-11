5acffbec6a393099a5d31cdc07c1fc3f5a955af8
Fabio <alessf13@gmail.com>
2021-07-11 18:35:11 +0200

    refactoring

--- jasmine/src/frame.js
+++ jasmine/src/frame.js
@@ -2,7 +2,6 @@ class Frame {
   constructor() {
     this.roll_1 = 0;
     this.roll_2 = 0;
-    this.roll_3 = 0;
   }
   stRoll(roll) {
     this.roll_1 += roll;
@@ -10,10 +9,7 @@ class Frame {
   ndRoll(roll) {
     this.roll_2 += roll;
   }
-  rdRoll(roll) {
-    this.roll_3 += roll;
-  }
   getTotal() {
-    return this.roll_1 + this.roll_2 + this.roll_3;
+    return this.roll_1 + this.roll_2;
   }
 }

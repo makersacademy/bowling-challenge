59233a51e4582bb2d2354adcde777e01d2d02a38
Cynthia Fu <yinnycfu@gmail.com>
2021-07-11 21:17:56 +0100

    finalFrame can store 3 rolls

--- spec/FinalFrameSpec.js
+++ spec/FinalFrameSpec.js
@@ -18,6 +18,58 @@ describe("FinalFrame", () => {
       }
       expect(finalFrame.isEnded()).toBe(true);
     });
+
+    xit("does not end the frame after a spare", () => {
+      for (let i = 0; i < 2; i++) {
+        finalFrame.add(5);
+      }
+      expect(finalFrame.isEnded()).toBe(false);
+    });
+
+    xit("does not end the frame after a strike", () => {
+      finalFrame.add(10);
+
+      expect(finalFrame.isEnded()).toBe(false);
+    });
+    
+    xit("does not end the frame after 2 strikes", () => {
+      for (let i = 0; i < 2; i++) {
+        finalFrame.add(10);
+      }
+
+      expect(finalFrame.isEnded()).toBe(false);
+    });
+
+    xit("it ends the frame after 3 rolls", () => {
+      for (let i = 0; i < 3; i++) {
+        finalFrame.add(5);
+      }
+
+      expect(finalFrame.isEnded()).toBe(true);
+    });
+  });
+
+  describe("add()", () => {
+    it("can store rolls", () => {
+      finalFrame.add(2);
+
+      expect(finalFrame.pins(1)).toEqual(2);
+    });
+
+    it("can store a second roll", () => {
+      finalFrame.add(2);
+      finalFrame.add(3);
+
+      expect(finalFrame.pins(2)).toEqual(3);
+    });
+
+    it("can store a third roll", () => {
+      for (let i = 0; i < 3; i++) {
+        finalFrame.add(5);
+      }
+
+      expect(finalFrame.pins(3)).toEqual(5);
+    });
   });
   
 

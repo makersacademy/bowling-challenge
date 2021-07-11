f5671b4272f96fcf7ebea6b75f310a8e451c7657
Cynthia Fu <yinnycfu@gmail.com>
2021-07-11 21:08:27 +0100

    finalFrame can be ended after 2 rolls if no strike or spare

--- spec/FinalFrameSpec.js
+++ spec/FinalFrameSpec.js
@@ -11,6 +11,14 @@ describe("FinalFrame", () => {
     it("is not true by default", () => {
       expect(finalFrame.isEnded()).not.toBe(true);
     });
+
+    it("is ended after 2 rolls if no strike or spare", () => {
+      for (let i = 0; i < 2; i++) {
+        finalFrame.add(1);
+      }
+      expect(finalFrame.isEnded()).toBe(true);
+    });
   });
+  
 
 });
--- src/FinalFrame.js
+++ src/FinalFrame.js
@@ -1,7 +1,24 @@
 'use strict';
 
 class FinalFrame {
+  constructor() {
+    this.rolls = [];
+  }
+  
   isEnded() {
-    return false;
+    if (this.rolls.length === 2 && (this.pins(1) + this.pins(2) < 10)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  add(number) {
+    this.rolls.push(number);
+  }
+
+  pins(turn) {
+    const i = turn - 1;
+    return this.rolls[i];
   }
 }

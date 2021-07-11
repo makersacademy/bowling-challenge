1efb15f1c32a4c1db6616fd076f29a6dd557296a
Fabio <alessf13@gmail.com>
2021-07-11 19:21:30 +0200

    it can calculate the points of a simple frame

--- jasmine/spec/gameSpec.js
+++ jasmine/spec/gameSpec.js
@@ -12,5 +12,11 @@ describe("Game", function () {
       game.frames[0].ndRoll(3);
       expect(game.frames[0].getTotal()).toEqual(7);
     });
+    it("can store the points a regular frame", function () {
+      game.add();
+      game.frames[0].stRoll(4);
+      game.frames[0].ndRoll(3);
+      expect(game.getPoints(0)).toEqual(7);
+    });
   });
 });
--- jasmine/src/game.js
+++ jasmine/src/game.js
@@ -1,8 +1,16 @@
 class Game {
   constructor() {
     this.frames = [];
+    this.points = [];
   }
   add() {
     this.frames.push(new Frame());
+  }
+  getPoints(n) {
+    this.calculatePoints(n);
+    return this.points[n];
+  }
+  calculatePoints(n) {
+    this.points[n] = this.frames[n].getTotal();
   }
 }

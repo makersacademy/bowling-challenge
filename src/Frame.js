ddc593e736cf1be6afef117010cb8f23e0c4e8ad
Ashley Slaney <ashleyslaney96@gmail.com>
2021-07-12 09:39:02 +0100

    removed console logs

--- src/Frame.js
+++ src/Frame.js
@@ -10,13 +10,8 @@ class Frame {
   totalScore() {
     let game = this.game.flat();
     let total = 0;
-    console.log(`game length: ${game.length}`);
-    console.log(game);
 
     for(let i = 0; i < game.length; i += 2) {
-      console.log(`frame: ${i + 1}`);
-      console.log(`shot one: ${game[i]}`)
-
       let frame = (game[i] + game[i + 1]);
       let bonus = (frame + game[i + 2]);
 
@@ -28,7 +23,6 @@ class Frame {
       } else {
         total += frame;
       }
-      console.log(total);
     }
     return total;
   }

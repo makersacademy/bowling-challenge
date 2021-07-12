f10785c2bee7ed04ead1de7c956d58b642088364
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-12 09:30:42 +0200

    strike bonus and final strike score

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -14,14 +14,14 @@ describe('game', function() {
   };
 
   describe('guttergame', function() {
-    it('can roll a gutter game' , function() {
+    it('can roll all zeros' , function() {
     multiRoll(0,20)
     expect(game.score()).toBe(0)
     })
   })
 
   describe('can roll a normal game', function() {
-    it('can roll produces resuls from a normal game', function(){
+    it('roll one 20 times and score 20', function(){
      multiRoll(1,20)
      expect(game.score()).toEqual(20)
     })
@@ -37,63 +37,28 @@ describe('game', function() {
     })
   })
 
+  describe('strike game', function(){
+    it('can roll a strike', function(){
+      game.roll(10)
+      game.roll(5)
+      game.roll(3)
+      multiRoll(0,16)
+      expect(game.score()).toEqual(26)
+    })
+    
+    it('can calculate final strike frame correctly', function(){
+      multiRoll(3,18)
+      game.roll(10)
+      game.roll(7)
+      game.roll(2)
+      expect(game.score()).toEqual(73)
+    })
+
+    it('can roll a perfect game', function(){
+      multiRoll(10,12)
+      expect(game.score()).toEqual(300)
+    })
+  })
+
 })
 
-
-
-
-
-
-// describe 'game' do
-//   before do
-//     @game = Game.new
-//   end
-
-//   it 'can bowl all zeros' do
-//     20.times { @game.bowl 0 }
-//     expect(@game.score).to eq 0
-//   end
-
-//   it 'can bowl all ones' do
-//     20.times { @game.bowl 1 }
-//     expect(@game.score).to eq 20
-//   end
-
-//   it 'can bowl a spare' do
-//     @game.bowl(5)
-//     @game.bowl(5)
-//     @game.bowl(2)
-//     17.times { @game.bowl 0 }
-//     expect(@game.score).to eq 14
-//   end
-
-//   it 'bowl a strike' do
-//     @game.bowl(10)
-//     @game.bowl(5)
-//     @game.bowl(2)
-//     16.times { @game.bowl 0 }
-//     expect(@game.score).to eq 24
-//   end
-
-//   it 'can bowl 12 strikes' do
-//     16.times { @game.bowl 10 }
-//     expect(@game.score).to eq 300
-//   end
-
-//   it 'can bowl all spares' do
-//     21.times { @game.bowl 5 }
-//     expect(@game.score).to eq 150
-//   end
-
-//   it 'raise an error if pin_down is more than 10' do
-//     expect { @game.bowl(11) }.to raise_error 'enter a score less than 10'
-//   end
-
-//   it 'last @game strike' do
-//     18.times { @game.bowl(3) }
-//     @game.bowl(10)
-//     @game.bowl(7)
-//     @game.bowl(2)
-//     expect(@game.score).to eq 73
-//   end
-// end
--- src/game.js
+++ src/game.js
@@ -1,5 +1,3 @@
-
-
 'use strict';
 
 class Game {
@@ -16,32 +14,36 @@ class Game {
     let bowling = this;
 
     for (let frame = 0; frame < 10; frame++) {
-      if (spare(rollIndex) ){
+      if (strike(rollIndex) ){
+        strikeScore(rollIndex)
+        rollIndex++
+      } else if (spare(rollIndex) ){
         spareScore(rollIndex)
+        rollIndex += 2
       } else {
         frameScore(rollIndex)
+        rollIndex += 2
       }
-      rollIndex += 2
     }
     return total;
 
-  function spare(rollIndex) {
-   return (bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] == 10)
+    function frameScore(rollIndex) {
+      return total += bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] 
+    }
+    function bonus(rollIndex) {
+      return bowling.rolls[rollIndex + 1] + bowling.rolls[rollIndex + 2]
+    }
+    function spare(rollIndex) {
+    return (bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] === 10)
+    }
+    function spareScore(rollIndex) {
+      return total += bowling.rolls[rollIndex] + bonus(rollIndex)
+    }
+    function strike(rollIndex) {
+      return bowling.rolls[rollIndex] === 10
+    }
+    function strikeScore(rollIndex) {
+      return total += bowling.rolls[rollIndex] + bonus(rollIndex)
+    }
   }
-  function spareScore(rollIndex) {
-    return total += bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] + bowling.rolls[rollIndex + 2]
-  }
-  function frameScore(rollIndex) {
-    return total += bowling.rolls[rollIndex] + bowling.rolls[rollIndex + 1] 
-  }
-  }
-  
 }
-
-
-
-
-
-
-
-

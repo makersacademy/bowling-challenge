f20df219192658c228cb77a6fbdaf7f0729d16d2
Frank Mckenna <fmckenna56330@gmail.com>
2021-07-21 12:30:29 +0100

    added new image and updated interface

--- public/src/interface.js
+++ public/src/interface.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const interfaceRolls = ['#roll1', '#roll2', '#roll3', '#roll4', '#roll5', '#roll6', '#roll7', 
   '#roll8', '#roll9', '#roll10', '#roll11', '#roll12', '#roll13', '#roll14', '#roll15', 
   '#roll16', '#roll17', '#roll18', '#roll19', '#roll20'];
-  
+
+  let index = 0
+
   const bowling = new Bowling();
   const frame = new Frame();
 
@@ -17,9 +19,13 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const updatingRolls = () => {
-    frame.interfaceRolls().map((roll, index) => {
-      document.querySelector(roll).innerText = frame.rolls[index] || "";
-    })
+    if (frame.rolls[index - 1] === 10) {
+        document.querySelector(interfaceRolls[index]).remove();
+        interfaceRolls.splice(index, 1);
+        document.querySelector(interfaceRolls[index]).innerText = frame.rolls[index] || "";
+      } else {
+      document.querySelector(interfaceRolls[index]).innerText = frame.rolls[index] || "";
+      }
   }
 
   const updateFrameAndAddScore = () => {
@@ -32,56 +38,67 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#zero').addEventListener('click', () => {
     frame.addRoll(0);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#first').addEventListener('click', () => {
     frame.addRoll(1);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#second').addEventListener('click', () => {
     frame.addRoll(2);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#third').addEventListener('click', () => {
     frame.addRoll(3);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#fourth').addEventListener('click', () => {
     frame.addRoll(4);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#fifth').addEventListener('click', () => {
     frame.addRoll(5);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#sixth').addEventListener('click', () => {
     frame.addRoll(6);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#seventh').addEventListener('click', () => {
     frame.addRoll(7);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#eigth').addEventListener('click', () => {
     frame.addRoll(8);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#ninth').addEventListener('click', () => {
     frame.addRoll(9);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.querySelector('#tenth').addEventListener('click', () => {
     frame.addRoll(10);
     updateFrameAndAddScore();
+    index += 1
   });
 
   document.getElementById('reset').addEventListener('click', () => {

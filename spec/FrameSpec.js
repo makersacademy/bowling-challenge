19f384ce97457c435130e7df669a79dd78b7c453
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-04 13:36:17 +0100

    Adding error messages to addBowls()

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -14,14 +14,25 @@ describe('Frame', () => {
     });
 
     it('is expected to have an Array attribute', () => {
-      expect(frame).toEqual(jasmine.objectContaining({_bowls: []}))
+      expect(frame).toEqual(jasmine.objectContaining({_bowls: []}));
     })
   });
 
   describe('add bowl', () => {
     it('is expected to add the bowl to the _bowls array', () => {
+      frame.addBowl(5);
+      expect(frame._bowls).toContain(5);
+    })
+
+    it('is expected to throw error if the sum of bowls is greater than 10', () => {
+      frame.addBowl(5);
+      expect( () => { frame.addBowl(6) } ).toThrow(new Error('Sum of bowls for this frame cannot exceed 10'));
+    })
+
+    it('is expected to throw error if adding more than two bowls', () => {
       frame.addBowl(5)
-      expect(frame._bowls).toContain(5)
+      frame.addBowl(2)
+      expect( () => { frame.addBowl(2) } ).toThrow(new Error('Limit of bowls for this frame has been reached'))
     })
   })
 
--- src/Frame.js
+++ src/Frame.js
@@ -2,12 +2,28 @@
 
 class Frame {
 
+  #maxPins = 10;
+
   constructor() {
     this._bowls = [];
   }
 
   addBowl(number) {
+    this.#maxBowls();
+    this.#sumTooHigh(number)
     this._bowls.push(number);
+  }
+
+  #sumTooHigh(number) {
+    if (this._bowls[0] + number > this.#maxPins || number > this.#maxPins) {
+      throw Error('Sum of bowls for this frame cannot exceed 10');
+    };
+  }
+
+  #maxBowls() {
+    if (this._bowls.length === 2) {
+      throw Error('Limit of bowls for this frame has been reached');
+    };
   }
 
 }

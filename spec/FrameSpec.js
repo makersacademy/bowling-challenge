fb43a35cad4a3b7f07dbc2b23e9c6c2a954263b4
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-09-04 12:48:08 +0100

    Adding _bowls array and addBowl function

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -12,6 +12,17 @@ describe('Frame', () => {
     it('is expected to be an instance of Frame', () => {
       expect(frame).toBeInstanceOf(Frame);
     });
+
+    it('is expected to have an Array attribute', () => {
+      expect(frame).toEqual(jasmine.objectContaining({_bowls: []}))
+    })
   });
+
+  describe('add bowl', () => {
+    it('is expected to add the bowl to the _bowls array', () => {
+      frame.addBowl(5)
+      expect(frame._bowls).toContain(5)
+    })
+  })
 
 })
--- src/Frame.js
+++ src/Frame.js
@@ -2,4 +2,12 @@
 
 class Frame {
 
+  constructor() {
+    this._bowls = [];
+  }
+
+  addBowl(number) {
+    this._bowls.push(number);
+  }
+
 }

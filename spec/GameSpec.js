0c92024a1539d9b920d77f685c35fc825547f76f
eballantine <lizzy.kayballantine@gmail.com>
2021-09-05 15:17:32 +0100

    the game checks if frames are over, score tracking is updated

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -10,6 +10,14 @@ describe('Game', () => {
     expect(game.frames.length).toEqual(1);
   })
 
+  it('starts with a score of 0', () => {
+    expect(game.score).toEqual(0);
+  })
+
+  it('starts with no frame scores', () => {
+    expect(game.frameScores).toHaveSize(0);
+  })
+
   describe('addFrame', () => {
     it('adds a new frame to the game', () => {
       game.addFrame();
--- src/Game.js
+++ src/Game.js
@@ -4,11 +4,29 @@ class Game {
     let frame = new Frame();
 
     this.frames = [frame];
+    this.score = 0;
+    this.frameScores = [];
   } 
+
+  roll(pins) {
+    for (let i = 0; i < this.frames.length; i++) {
+      this.frames[i].addRoll(pins);
+    }
+    checkFrameOver();
+  }
 
   addFrame() {
     if (this.frames.length < 10) {
       this.frames.push(new Frame());
+    }
+  }
+
+  checkFrameOver() {
+    for (let i = 0; i < this.frames.length; i++) {
+      if (this.frames[i].isFrameOver()) {
+        this.frameScores.push(this.frames[i].frameScore);
+        this.score += this.frames[i].frameScore;
+      }
     }
   }
 }

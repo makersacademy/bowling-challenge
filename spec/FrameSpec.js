effb2571878c2f0cbb7eafbdba118e4fc57fe2da
Corinne <corinne.boesch@gmail.com>
2021-09-05 16:04:00 +0100

    play function calls roll if scoe is empty & incements bonus by one if its a perfect score

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -13,12 +13,27 @@ describe('Frame', () => {
     });
   });
 
+  describe('play', () => {
+    it('calls roll function when score array is empty', () => {
+      frame.play(6);
+      expect(frame.score).toContain(6);
+    });
+
+    it('calls roll function and adds plus 1 to bonus when score is empty ', () => {
+      frame.play(10);
+      expect(frame.score).toContain(10);
+      expect(frame.bonus).toEqual(1);
+    });
+  });
+
   describe('roll', () => {
     it('takes knocked down pins as argument & pushes it into score array', () => {
       frame.roll(5);
       expect(frame.score).toContain(5);
     });
+  });
 
+  describe('isPerfectStrike', () => {
     it('is a perfect strike when kocking down all 10 pins in first round', () => {
       expect(frame.isPerfectScore(10)).toBe(true);
     });
--- src/Frame.js
+++ src/Frame.js
@@ -6,6 +6,20 @@ class Frame {
     this.bonus = Frame.#SPARE_BONUS;
   }
 
+  play(knocked_pins) {
+    if (this.score.length === 0) {
+      this.isPerfectScore(knocked_pins)
+        ? [this.roll(knocked_pins), this.bonus++]
+        : this.roll(knocked_pins);
+    }
+  }
+
+  whatever(knocked_pins) {
+    if (this.isPerfectScore(knocked_pins)) {
+      this.bonus++;
+    }
+  }
+
   roll(knocked_pins) {
     this.score.push(knocked_pins);
   }

c933975fcdc009d134fbdea93c48fc0cd5d1413e
Ahsan <rasul.ahsan@gmail.com>
2021-09-05 17:19:28 +0100

    some changes to interface as couldn't get the interface to score strikes properly with multiple inputs on a sinlge click, attempted code is currently commented out and will be deleted on next commit

--- Interface.js
+++ Interface.js
@@ -1,19 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const game = new Game();
 	document.querySelector("#game").innerText = game.frameNo;
+	document.querySelector("#ballno").innerText = game.currentFrame.currentroll;
+	// document.getElementById("ballThree").style.display = "none";
 
 	document.querySelector("#balls").addEventListener("click", () => {
 		let ballOne = document.getElementById("ballOne").value;
 		let rollOne = parseInt(ballOne, 10);
 		// let ballTwo = document.getElementById("ballTwo").value;
 		// let rollTwo = parseInt(ballTwo, 10);
+		// if (game.frameNo === 9) {
+		// 	document.getElementById("ballThree").style.display = "block";
+		// 	let ballThree = document.getElementById("ballThree").value;
+		// 	let rollThree = parseInt(ballThree, 10);
+		// 	game.rollBall(rollThree);
+		// }
 		game.rollBall(rollOne);
 		// game.rollBall(rollTwo);
-		// if (game.frameNo > 10) {
-		// 	document.querySelector("#game").innerText = "Game Over";
-		// } else {
+		if (game.frameNo > 10) {
+			document.querySelector("#game").innerText = "10";
+		} else {
 			document.querySelector("#game").innerText = game.frameNo;
-		// }
+		}
+		document.querySelector("#ballno").innerText = game.currentFrame.currentroll;
 	});
 
 	document.querySelector("#score").addEventListener("click", () => {

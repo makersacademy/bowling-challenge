d0abbbee976a7226a2a121015d67f4d276177727
edpackard <edpackard81@gmail.com>
2021-09-05 12:46:03 +0100

    updating and prettifying frame class and tests

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,132 +1,125 @@
-'use strict';
+"use strict";
 
-describe('Frame', () => {
-
+describe("Frame", () => {
   let frame;
 
-  describe('Frames 1 to 9', () => {
-
+  describe("Frames 1 to 9", () => {
     beforeEach(() => {
       frame = new Frame(1);
     });
 
-    it('initializes with a frame_id', () => {
-      expect(frame.getId()).toEqual(1)
+    it("initializes with a frame_id", () => {
+      expect(frame.getId()).toEqual(1);
     });
 
-    it('does not create 3rd roll', () => {
-      expect(frame.rollThree()).toBeUndefined()
+    it("does not create 3rd roll", () => {
+      expect(frame.rollThree()).toBeUndefined();
     });
 
-    it('can add a valid roll', () => {
-      frame.add(3)
-      expect(frame.rollOne()).toEqual(3)
+    it("can add a valid roll", () => {
+      frame.add(3);
+      expect(frame.rollOne()).toEqual(3);
     });
 
-    it('cannot add an invalid roll: out of range number', () => {
-      frame.add(11)
-      expect(frame.rollOne()).toBeNull()
+    it("cannot add an invalid roll: out of range number", () => {
+      frame.add(11);
+      expect(frame.rollOne()).toBeNull();
     });
 
-    it('cannot add an invalid roll: not an integer', () => {
-      frame.add("a")
-      expect(frame.rollOne()).toBeNull()
+    it("cannot add an invalid roll: not an integer", () => {
+      frame.add("a");
+      expect(frame.rollOne()).toBeNull();
     });
 
-    it('can add two valid rolls', () => {
-      frame.add(3)
-      frame.add(4)
-      expect(frame.rollTwo()).toEqual(4)
+    it("can add two valid rolls", () => {
+      frame.add(3);
+      frame.add(4);
+      expect(frame.rollTwo()).toEqual(4);
     });
 
-    it('cannot add two rolls that exceed maximum pin number', () => {
-      frame.add(3)
-      frame.add(8)
-      expect(frame.rollTwo()).toBeNull()
+    it("cannot add two rolls that exceed maximum pin number", () => {
+      frame.add(3);
+      frame.add(8);
+      expect(frame.rollTwo()).toBeNull();
     });
 
-    it('sets 2nd roll to zero if strike', () => {
-      frame.add(10)
-      expect(frame.rollTwo()).toEqual(0)
+    it("sets 2nd roll to zero if strike", () => {
+      frame.add(10);
+      expect(frame.rollTwo()).toEqual(0);
     });
 
-    it('is not full if only contains one roll under 10', () => {
-      frame.add(9)
-      expect(frame.isFull()).toBeFalsy()
+    it("is not full if only contains one roll under 10", () => {
+      frame.add(9);
+      expect(frame.isFull()).toBeFalsy();
     });
 
-    it('is full after a strike', () => {
-      frame.add(10)
-      expect(frame.isFull()).toBeTruthy()
+    it("is full after a strike", () => {
+      frame.add(10);
+      expect(frame.isFull()).toBeTruthy();
     });
 
-    it('cannot add two strikes to same frame', () => {
-      frame.add(10)
-      frame.add(10)
-      expect(frame.rollTwo()).toEqual(0)
+    it("cannot add two strikes to same frame", () => {
+      frame.add(10);
+      frame.add(10);
+      expect(frame.rollTwo()).toEqual(0);
     });
 
-    it('is full if two valid rolls added', () => {
-      frame.add(3)
-      frame.add(2)
-      expect(frame.isFull()).toBeTruthy()
+    it("is full if two valid rolls added", () => {
+      frame.add(3);
+      frame.add(2);
+      expect(frame.isFull()).toBeTruthy();
     });
-
   });
 
-  describe('Frame 10', () => {
-
+  describe("Frame 10", () => {
     beforeEach(() => {
       frame = new Frame(10);
     });
 
-    it('initializes with _rollThree', () => {
-      expect(frame.rollThree()).toBeNull()
+    it("initializes with _rollThree", () => {
+      expect(frame.rollThree()).toBeNull();
     });
 
-    it('allows a valid first roll', () => {
-      frame.add(5)
-      expect(frame.rollOne()).toEqual(5)
+    it("allows a valid first roll", () => {
+      frame.add(5);
+      expect(frame.rollOne()).toEqual(5);
     });
 
-    it('is full after two rolls totalling less than 10', () => {
-      frame.add(3)
-      frame.add(2)
-      expect(frame.rollTwo()).toEqual(2)
-      expect(frame.isFull()).toBeTruthy()
+    it("is full after two rolls totalling less than 10", () => {
+      frame.add(3);
+      frame.add(2);
+      expect(frame.rollTwo()).toEqual(2);
+      expect(frame.isFull()).toBeTruthy();
     });
 
-    it('can accept two strikes in the first two rolls', () => {
-      frame.add(10)
-      frame.add(10)
-      expect(frame.rollTwo()).toEqual(10)
+    it("can accept two strikes in the first two rolls", () => {
+      frame.add(10);
+      frame.add(10);
+      expect(frame.rollTwo()).toEqual(10);
     });
 
-    it('allows third roll if first two rolls total more than 10', () => {
-      frame.add(3)
-      frame.add(7)
-      frame.add(9)
-      expect(frame.rollThree()).toEqual(9)
-      expect(frame.isFull()).toBeTruthy()
+    it("allows third roll if first two rolls total more than 10", () => {
+      frame.add(3);
+      frame.add(7);
+      frame.add(9);
+      expect(frame.rollThree()).toEqual(9);
+      expect(frame.isFull()).toBeTruthy();
     });
 
-    it('will not allow third roll if first two rolls total less than 10', () => {
-      frame.add(3)
-      frame.add(2)
-      frame.add(9)
-      expect(frame.rollThree()).toBeNull()
-      expect(frame.isFull()).toBeTruthy()
+    it("will not allow third roll if first two rolls total less than 10", () => {
+      frame.add(3);
+      frame.add(2);
+      frame.add(9);
+      expect(frame.rollThree()).toBeNull();
+      expect(frame.isFull()).toBeTruthy();
     });
 
-    it('can accept three strikes in final frame', () => {
-      frame.add(10)
-      frame.add(10)
-      frame.add(10)
-      expect(frame.rollThree()).toEqual(10)
-      expect(frame.isFull()).toBeTruthy()
-      console.log(frame)
+    it("can accept three strikes in final frame", () => {
+      frame.add(10);
+      frame.add(10);
+      frame.add(10);
+      expect(frame.rollThree()).toEqual(10);
+      expect(frame.isFull()).toBeTruthy();
     });
-
   });
-
 });
--- src/frame.js
+++ src/frame.js
@@ -1,84 +1,90 @@
-'use strict';
+"use strict";
+
+// test
 
 class Frame {
-
   constructor(id) {
-    this.TOTAL_PINS = 10
-    this._remainingPins = this.TOTAL_PINS
-    this._frameId = id
-    this._isFull = false
-    this._rollOne = null
-    this._rollTwo = null
+    this.TOTAL_PINS = 10;
+    this._remainingPins = this.TOTAL_PINS;
+    this._frameId = id;
+    this._isFull = false;
+    this._rollOne = null;
+    this._rollTwo = null;
     if (id === 10) {
-      this._rollThree = null
+      this._rollThree = null;
     }
   }
 
   add(pins) {
     if (this._isValidRoll(pins) === false || this.isFull()) {
-      return
+      return;
     }
-    this._frameId === 10 ? this._lastFrame(pins) : this._normalFrame(pins)
+    this._frameId === 10 ? this._lastFrame(pins) : this._normalFrame(pins);
   }
 
   getId() {
-    return this._frameId
+    return this._frameId;
   }
 
   rollOne() {
-    return this._rollOne
+    return this._rollOne;
   }
 
   rollTwo() {
-    return this._rollTwo
+    return this._rollTwo;
   }
 
   rollThree() {
-    return this._rollThree
+    return this._rollThree;
   }
 
   isFull() {
-    return this._isFull
+    return this._isFull;
   }
 
   _normalFrame(pins) {
-    this._rollOne === null ? this._normalFirstRoll(pins) : this._normalSecondRoll(pins)
+    this._rollOne === null
+      ? this._normalFirstRoll(pins)
+      : this._normalSecondRoll(pins);
   }
 
   _normalFirstRoll(pins) {
-    this._rollOne = pins
-    this._remainingPins -= pins
+    this._rollOne = pins;
+    this._remainingPins -= pins;
     if (pins === this.TOTAL_PINS) {
-      this._normalSecondRoll(this._remainingPins)
+      this._normalSecondRoll(this._remainingPins);
     }
   }
 
   _normalSecondRoll(pins) {
-    this._rollTwo += pins
-    this._isFull = true
+    this._rollTwo += pins;
+    this._isFull = true;
   }
 
   _lastFrame(pins) {
-    this._rollOne === null ? this._rollOne = pins : this._isBonusRoll(pins)
+    this._rollOne === null ? (this._rollOne = pins) : this._isBonusRoll(pins);
   }
 
   _isBonusRoll(pins) {
-    this._rollTwo === null ? this._finalSecondRoll(pins) : this._finalThirdRoll(pins)
+    this._rollTwo === null
+      ? this._finalSecondRoll(pins)
+      : this._finalThirdRoll(pins);
   }
 
   _finalSecondRoll(pins) {
-    this._rollTwo += pins
-    let frameTotal = this._rollOne + this._rollTwo
-    frameTotal >= 10 ? this._remainingPins = this.TOTAL_PINS : this._isFull = true
+    this._rollTwo += pins;
+    let frameTotal = this._rollOne + this._rollTwo;
+    frameTotal >= 10
+      ? (this._remainingPins = this.TOTAL_PINS)
+      : (this._isFull = true);
   }
 
   _finalThirdRoll(pins) {
-    this._rollThree += pins
-    this._isFull = true
+    this._rollThree += pins;
+    this._isFull = true;
   }
 
   _isValidRoll(pins) {
-    return Number.isInteger(pins) && (pins >= 0 && pins <= this._remainingPins)
+    return Number.isInteger(pins) && pins >= 0 && pins <= this._remainingPins;
   }
-
-};
+}

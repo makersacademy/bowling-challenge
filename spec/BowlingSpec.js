2650b688cfe78c05007d60f26939a2b957207872
Sophie Bonneval <maldorana@gmail.com>
2021-10-01 21:45:03 +0100

    Spare test and changed datatype of previous rolls

--- spec/BowlingSpec.js
+++ spec/BowlingSpec.js
@@ -7,17 +7,29 @@ describe ('Bowling', () => {
     bowling = new Bowling();
   });
 
-  it('can score a gutter game', () => {
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(0);
+  const rollLoop = (pins, rolls) => {
+    for (let i = 0; i < rolls; i++) {
+      bowling.roll(pins);
     }
+  }
+
+  it('can score a gutter game', () => {
+    rollLoop(0, 20);
     expect(bowling.score()).toEqual(0);
   });
 
   it('can score 80 for a game of all 4s', () => {
-    for (let i = 0; i < 20; i++) {
-      bowling.roll(4);
-    }
+    rollLoop(4, 20);
     expect(bowling.score()).toEqual(80);
   });
+
+  it('can score a Spare', () => {
+    bowling.roll(5);
+    bowling.roll(5);
+    bowling.roll(3);
+    rollLoop(0, 17);
+    expect(bowling.score()).toEqual(16);
+  });
+
+  
 });
--- src/Bowling.js
+++ src/Bowling.js
@@ -2,14 +2,21 @@
 
 class Bowling {
   constructor() {
-    this.runningScore = 0;
-  }
-
-  score() {
-    return this.runningScore;
+    this.rolls = [];
   }
 
   roll(pins) {
-    this.runningScore += pins;
+    if (pins === 10) {
+      this.rolls.push([pins, 0])
+    } else if (this.rolls.length === 0 || this.rolls[this.rolls.length - 1].length === 2) {
+      this.rolls.push([pins])
+    } else if (this.rolls[this.rolls.length - 1].length == 1) {
+      this.rolls[this.rolls.length - 1].push(pins);
+    }
   }
+
+  score() {
+    return this.rolls.flat().reduce((partial_sum, a) => partial_sum + a, 0);
+  }
+
 }

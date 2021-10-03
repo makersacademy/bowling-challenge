e42ec911123c4886c11918603789b23a37007cc1
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-10-03 14:29:27 +0100

    Writing the second test, but not passing it

--- jasmine/spec/GameSpec.js
+++ jasmine/spec/GameSpec.js
@@ -3,11 +3,20 @@ describe("Scorecard", function () {
     game = new Game();
   });
 
-  it("can total points from one ordinary frame", function () {
+  it("can total points from one ordinary roll", function () {
     expect(
       game.score([
         [1, 0]
       ])
     ).toBe(1);
   });
+
+  it("can total points from one ordinary frame", function () {
+    expect(
+      game.score([
+        [1, 1]
+      ])
+    ).toBe(2);
+  });
+
 });

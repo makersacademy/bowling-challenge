91bc11d5f5b9fff58c3be8c8e31de59d7681e43c
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-10-03 16:51:34 +0100

    Adding scoring for spare and strike

--- jasmine/spec/GameSpec.js
+++ jasmine/spec/GameSpec.js
@@ -3,20 +3,38 @@ describe("Scorecard", function () {
     game = new Game();
   });
 
-  it("can total points from one ordinary roll", function () {
-    expect(
-      game.score([
-        [1, 0]
-      ])
-    ).toBe(1);
-  });
+  it('should return 0 for a game of all zeros', () => {
+    for(let i=0;i<20;i++){ //for 20 rolls
+      game.roll(0); //roll a 0
+    }
+    expect(game.score).toEqual(0); //check that the score = 0
+  })
 
-  it("can total points from one ordinary frame", function () {
-    expect(
-      game.score([
-        [1, 1]
-      ])
-    ).toBe(2);
-  });
+  it('should return 20 for game of all ones', () => {
+    rollMany(20, 1); //roll 20 1's
+    expect(game.score).toEqual(20); //check if the score = 20
+  })
+  
+  it('handles a spare with correct bonus', () => {
+    //roll a spare
+    game.roll(5); 
+    game.roll(5);
+    game.roll(1); //roll a 1
+    rollMany(17, 0); //roll 17 0's
+    expect(game.score).toEqual(12); //check that the score = 12
+  })
 
+  it('handles a strike with correct bonus', () => {
+    game.roll(10); //roll a strike
+    game.roll(1); //roll a 1
+    game.roll(1); //roll a 1
+    rollMany(17, 0); //roll 17 0's
+    expect(game.score).toEqual(14); //check that the score = 14
+})
+
+  function rollMany(rolls, pins){
+    for(let i=0;i<rolls;i++){ //for number of rolls specified
+        game.roll(pins); //roll the number of pins specified
+    }
+  }
 });
--- jasmine/src/Game.js
+++ jasmine/src/Game.js
@@ -1,6 +1,42 @@
 class Game {
-  score() {
-    return 1;
-  }
-}
 
+  constructor() {
+    this.rolls = []; //to store number of pins knocked over in each roll
+  }
+
+  roll(pins) {
+    this.rolls.push(pins); //save number of pins knocked every roll
+  }
+
+  get score() {
+    let score = 0; //to keep track of total score
+    let rollIndex = 0; //to keep track of which roll
+    
+    //for each frame (10 in total)
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+        
+      if(this.rolls[rollIndex] === 10){ //if strike
+        //apply strike bonus
+        score += 10 + this.rolls[rollIndex+1]+this.rolls[rollIndex+2]; 
+        rollIndex++; //move onto the next roll
+        continue; //move onto next iteration in the loop 
+    }
+
+      //sum total pins knocked in this frame (2 rolls)
+      let frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1]
+        
+      if (frameScore === 10) { //if spare
+        score += 10 + this.rolls[rollIndex + 2]; //apply spare bonus
+      } else {
+        score += frameScore; //add reg framescore to total score
+      }
+      
+      rollIndex += 2 //move onto the first roll of the next frame
+    }
+
+    return score;
+  }
+
+  
+  
+}

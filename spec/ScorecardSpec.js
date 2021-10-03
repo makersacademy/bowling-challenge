535019a5b959c3567c8e3335bf1fd9feadeb4d16
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-03 12:03:10 +0100

    updating tests for bonus frame - passed

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -2,53 +2,118 @@
 
 describe ("Scorecard", function() {
 
-let scorecard;
+  let scorecard;
 
-beforeEach(function(){
-  scorecard = new Scorecard();
-})
+  beforeEach(function(){
+    scorecard = new Scorecard();
+  })
 
-it ("should return the values of the first frames", function () {
-  scorecard.roll([0,0]);
-  expect(scorecard.getCurrentScore()).toEqual(0)
-})
+  it ("should return the values of the first frames", function () {
+    scorecard.roll([0,0]);
+    expect(scorecard.getCurrentScore()).toEqual(0)
+  })
 
-it ("should return the values of the first two frames", function () {
-  scorecard.roll([0,0]);
-  scorecard.roll([1,2]);
-  expect(scorecard.getCurrentScore()).toEqual(3)
-})
-it ("should test for spares", function () {
-  scorecard.roll([7,3]);
-  scorecard.roll([1,4]);
-  expect(scorecard.getCurrentScore()).toEqual(16)
-})
+  it ("should return the values of the first two frames", function () {
+    scorecard.roll([0,0]);
+    scorecard.roll([1,2]);
+    expect(scorecard.getCurrentScore()).toEqual(3)
+  })
+  it ("should test for spares", function () {
+    scorecard.roll([7,3]);
+    scorecard.roll([1,4]);
+    expect(scorecard.getCurrentScore()).toEqual(16)
+  })
 
-it ('should count a strike', function (){
-  scorecard.roll("X");
-  scorecard.roll([1,4]);
-  expect(scorecard.getCurrentScore()).toEqual(20)
-  // 10 + 1 + 4 + 1 + 4
-})
+  it ('should count a strike', function (){
+    scorecard.roll("X");
+    scorecard.roll([1,4]);
+    expect(scorecard.getCurrentScore()).toEqual(20);
+    // 10 + 1 + 4 + 1 + 4
+  })
 
-it ('should count the value of two strikes', function (){
-  scorecard.roll([2,3]);
-  scorecard.roll("X");
-  scorecard.roll("X");
-  scorecard.roll([1,0]);
-  expect(scorecard.getCurrentScore()).toEqual(38)
-  // (2 + 3) + ( 10 + 10 + 1) + (10 + 1 + 0) + (1 + 0)
-})
+  it ('should count the value of two strikes', function (){
+    scorecard.roll([2,3]);
+    scorecard.roll("X");
+    scorecard.roll("X");
+    scorecard.roll([1,0]);
+    expect(scorecard.getCurrentScore()).toEqual(38);
+    // (2 + 3) + ( 10 + 10 + 1) + (10 + 1 + 0) + (1 + 0)
+  })
 
-it ('should count the value of three strikes', function(){
-  scorecard.roll("X");
-  scorecard.roll("X");
-  scorecard.roll("X");
-  scorecard.roll([1,5]);
+  it ('should count the value of three strikes', function (){
+    scorecard.roll("X");
+    scorecard.roll("X");
+    scorecard.roll("X");
+    scorecard.roll([1,5]);
 
-  expect(scorecard.getCurrentScore()).toEqual(73)
-})
-// (10 + 10 + 10) + ( 10 + 10 + 1) + (10 + 1 + 5) + (1 + 5)
+    expect(scorecard.getCurrentScore()).toEqual(73);
+  })
+  // (10 + 10 + 10) + ( 10 + 10 + 1) + (10 + 1 + 5) + (1 + 5)
+
+  it ('should test for rolling a spare then a strike', () => {
+    scorecard.roll([5,5]);
+    scorecard.roll("X");
+    scorecard.roll([1,2]);
+    expect(scorecard.getCurrentScore()).toEqual(36);
+
+    //(5+5+10)+(10+1+2)+(1+2)
+
+  })
+
+  describe('return the values of the tenth frame', () => {
+  it('#no strike or spare', function (){
+    for (let i = 0; i < 9; i++) {
+      scorecard.roll([1,0])
+    }
+    scorecard.rollTenthFrame([1,0])
+    expect(scorecard.getCurrentScore()).toEqual(10)
+
+    
+  });
+
+  it('#spare', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.roll([1,0])
+    }
+    scorecard.rollTenthFrame([5,5,5])
+    expect(scorecard.getCurrentScore()).toEqual(19);
+    expect(scorecard.getBonusScore()).toEqual(5);
+    //need to refactor the test so we are calling one function not two
+  });
+
+  it('#one strike', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.roll([1,0])
+    }
+    scorecard.rollTenthFrame(["X",4,3]);
+    expect(scorecard.getCurrentScore()).toEqual(19);
+    expect(scorecard.getBonusScore()).toEqual(7);
+  });
+
+  it('#two strikes', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.roll([1,0])
+    }
+    scorecard.rollTenthFrame(["X","X",5]);
+    expect(scorecard.getCurrentScore()).toEqual(19);
+    expect(scorecard.getBonusScore()).toEqual(15);
+  });
+
+  it('#three strikes', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.roll([1,0])
+    }
+    scorecard.rollTenthFrame(["X","X","X"]);
+    expect(scorecard.getCurrentScore()).toEqual(19);
+    expect(scorecard.getBonusScore()).toEqual(20);
+  });
+
+  });
+
+  
 
 });
+
+
+// TIDY UP ES6 vs ES5 functions
 
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,8 +1,12 @@
+'use strict';
+
+
 class Scorecard {
 
   constructor() {
     this.STARTING_SCORE = 0;
     this.score = this.STARTING_SCORE;
+    this.bonusScore = this.STARTING_SCORE
     this.previousFrame = 0;
     this.strikesCount = [];
     this.strikeSum = 0;
@@ -13,41 +17,86 @@ class Scorecard {
   }
 
   calculateStrikeTally(){
-    for (let i = 0; (i < this.strikesCount.length - 2); i++) {
+    if (this.strikesCount.length == 1){
+      this.strikeSum += (this.strikesCount[1] + this.strikesCount[2] + 
+      this.strikesCount[3])
+    }
+    else {
+        for (let i = 0; (i < this.strikesCount.length - 2); i++) {
       this.strikeSum += (this.strikesCount[i] + this.strikesCount[i + 1] + 
-        this.strikesCount[i + 2]);
-    }}
+      this.strikesCount[i + 2]);
+      }
+      }
+  }
 
-    roll(value) {
-      if (value === "X" ) {
-        this.strikesCount.push(10)
+  roll(value){
+    if ((value === "X") && (this.isPreviousFrameSpare())) {
+      this.score += 10 ;
+      this.strikesCount.push(10);
+      this.previousFrame = 0;
+      }
+    else if (value === "X") {
+      this.strikesCount.push(10)
+    }
+    else {
+      if (this.isPreviousFrameSpare()){
+        this.score += (value[0] * 2)
+        this.score += value[1]
+      } 
+      else if (this.strikesCount.length > 0 ){
+        this.strikesCount.push(value[0]);
+        this.strikesCount.push(value[1]);
+        this.calculateStrikeTally();
+        this.score += (this.strikeSum + value[0] + value[1]);
         }
       else {
-        if (this.isPreviousFrameSpare()){
-          this.score += (value[0] * 2)
-          this.score += value[1]
-        } else if (this.strikesCount.length > 0 ){
-            this.strikesCount.push(value[0]);
-            this.strikesCount.push(value[1]);
-            this.calculateStrikeTally();
-            this.score += (this.strikeSum + value[0] + value[1]);
-        }
-        else {
-          this.score += value[0];
-          this.score += value[1];
-        }
-        this.strikesCount = [];
-        this.strikeSum = 0;
-        this.previousFrame = value[1] + value[0]
-        this.consecutiveStrikes  = 0 
-        }  
-    }
+        this.score += value[0];
+        this.score += value[1];
+      }
+    this.resetValues();
+    this.previousFrame = value[1] + value[0];
+    }  
+  }
 
+  rollTenthFrame(value) {
+
+    if (value[0] === "X" && value[1] === "X" && value[2] === "X") {
+      this.score += 10 
+      this.rollBonusFrame(20)
+    }
+     else if (value[0] === "X" && value[1] === "X" && value[2] !== "X") {
+      this.score += 10 
+      this.rollBonusFrame(10 + value[2]) }
+     else if (value[0] === "X" && value[1] !== "X") {
+        this.score += 10 
+        this.rollBonusFrame(value[1] + value[2])
+     } 
+    else if ((value[0]+ value[1]) > 9) {
+      this.score += (value[0] + value[1]);
+      this.rollBonusFrame(value[2]);
+      // will need to update this to capture a value store it and call 
+    }
+    else {
+    this.score += (value[0] + value[1]);
+    }
+  }
+
+  rollBonusFrame(value){
+    this.bonusScore += value;
+  }
 
   getCurrentScore() {
     return this.score
   }
 
+  getBonusScore(){
+    return this.bonusScore
+  }
+
+  resetValues(){
+    this.strikesCount = [];
+    this.strikeSum = 0;
+  }
 
 
 };

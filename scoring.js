fe81547b6958f8c547ba74a0721d4ed4c465a904
luke <luke44@gmail.com>
2021-10-27 23:11:31 +0100

    Blocker using .map with conditions, to do something different incase of strike

--- scoring.js
+++ scoring.js
@@ -1,7 +1,32 @@
 class Scoring {
-  totalScore() {
-    return 300
+  constructor(scorecard) {
+    this.scorecard = scorecard
   }
+  // regularScorePerFrame() { 
+  //   return this.scorecard.map( element => 
+  //     if ( element[0] === 10 ) { return element => 10 }
+  //     else { return element => element[0] + element[1] }
+  //   )
+  // }
 }
+let mockScorecard = { pins: [
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 0],
+  [10, 10, 10]
+]}
 
+scoring = new Scoring(mockScorecard.pins)
+console.log(scoring.scorecard.map( element =>  element[0] ))
 module.exports = Scoring;
+
+    // return this.scorecard.map(
+    //   if ( element[0] === 10 ) { element => 10 }
+    //   else { element => element[0] + element[1] }
+    // )
--- scoring.test.js
+++ scoring.test.js
@@ -1,7 +1,23 @@
 const Scoring = require('./scoring');
 
-describe('.totalScore', () => {
-  it('reports 300 points for a perfect game', () => {
+describe('.regularScorePerFrame', () => {
+  // it('reports 300 points for a perfect game', () => {
+  //   let mockScorecard = { pins: [
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 0],
+  //     [10, 10, 10]
+  //   ]}
+  //   scoring = new Scoring(mockScorecard)
+  //   expect(scoring.totalScore()).toEqual(300);
+  // });
+  it('calculates regular score per frame', () => {
     let mockScorecard = { pins: [
       [10, 0],
       [10, 0],
@@ -12,9 +28,22 @@ describe('.totalScore', () => {
       [10, 0],
       [10, 0],
       [10, 0],
-      [10, 0, 0]
+      [10, 10, 10]
     ]}
-    scoring = new Scoring(mockScorecard)
-    expect(scoring.totalScore()).toEqual(300);
+    scoring = new Scoring(mockScorecard.pins)
+    expect(scoring.regularScorePerFrame()).toEqual(
+      [
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10,
+        10
+      ]
+    );
   });
 });

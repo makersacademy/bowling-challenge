c4f0c3ed66dcb4fb4582d036ef6e5d071d187ea5
luke <luke44@gmail.com>
2021-10-29 11:12:43 +0100

    Tests added and code refined

--- scoring.js
+++ scoring.js
@@ -8,7 +8,7 @@ class Scoring {
     this.n = 0
   }
   regularScorePerFrame() { 
-    // var regularScore = []
+    // this is just the number of pins per frame, though frame 10 is an exception
     this.regularScore = this.scorecard.map( element => element.reduce(function(a,b){ return a + b }, 0) )
     if ( this.scorecard[9][0] === 10) { this.regularScore[9] = 10 }
     else { this.regularScore[9] = this.scorecard[9][0] + this.scorecard[9][1] }
@@ -17,11 +17,11 @@ class Scoring {
     var frameBonus = 0
     while ( this.n < 8 ) {
       // 3 or 2 strikes in a row
-      if ( this.scorecard[this.n][0] == 10 && this.scorecard[this.n + 1 ][0] == 10 && this.scorecard[this.n + 2][0] == 10) {
+      if ( this.scorecard[this.n][0] == 10 && this.scorecard[this.n + 1 ][0] == 10 ) {
         frameBonus = this.scorecard[this.n + 1 ][0] + this.scorecard[this.n + 2 ][0]
       } 
       //  1 strike in a row
-      else if ( this.scorecard[this.n][0] == 10 ) {
+      else if ( this.scorecard[this.n][0] == 10 && this.scorecard[this.n + 1 ][0] !== 10) {
         frameBonus = this.scorecard[this.n + 1 ][0] + this.scorecard[this.n + 1 ][1]
       }
       // split
@@ -29,13 +29,14 @@ class Scoring {
         frameBonus = this.scorecard[this.n + 1][0]
       };
       this.bonusScore.push(frameBonus);
+      frameBonus = 0;
       this.n += 1; 
     }
   }
   bonusScoreFrame9() {
     var frameBonus = 0
     // 3 or 2 or 1 strikes in a row
-    if ( this.scorecard[8][0] == 10 && this.scorecard[8 + 1 ][0] == 10 && this.scorecard[8 + 1][1] == 10) {
+    if ( this.scorecard[8][0] == 10 ) {
       frameBonus = this.scorecard[8 + 1][0] + this.scorecard[8 + 1][1]
     } 
     // split
@@ -60,16 +61,11 @@ class Scoring {
     this.bonusScore8Frames();
     this.bonusScoreFrame9();
     this.bonusScoreFrame10();
-
-    // DO the below
-    // return this.bonusScore = this.bonusScore.map( element => element.reduce(function(a,b){ return a + b }, 0) )
-    // this.regularScore = this.scorecard.map( element => element.reduce(function(a,b){ return a + b }, 0) )
-
   }
   totalScoreAllFrames() {
     let n = 0
     while ( n < 10 ) {
-      this.totalScorePerFrame.push( this.regularScore[n] + this.bonusScore[0] );
+      this.totalScorePerFrame.push( this.regularScore[n] + this.bonusScore[n] );
       n += 1
     }
   }
@@ -82,23 +78,18 @@ class Scoring {
 
 let mockScorecard = { pins: [
   [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 0],
-  [10, 10, 10]
+  [1, 0],
+  [1, 0],
+  [1, 0],
+  [1, 0],
+  [1, 0],
+  [1, 0],
+  [1, 0],
+  [1, 0],
+  [1, 9, 10]
 ]}
 
 scoring = new Scoring(mockScorecard.pins)
-// console.log(scoring.bonusScore8Frames())
-// console.log(scoring.bonusScoreFrame9())
-// console.log(scoring.bonusScoreFrame10())
-// console.log('bonus scores below')
-
 
 scoring.regularScorePerFrame()
 console.log('regular scores below')
--- scoring.test.js
+++ scoring.test.js
@@ -1,22 +1,26 @@
 const Scoring = require('./scoring');
 
-describe('.regularScorePerFrame', () => {
-  // it('reports 300 points for a perfect game', () => {
-  //   let mockScorecard = { pins: [
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 10, 10]
-  //   ]}
-  //   scoring = new Scoring(mockScorecard)
-  //   expect(scoring.totalScore()).toEqual(300);
-  // });
+describe('Scoring a perfect game', () => {
+  it('reports total score', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 10, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.regularScorePerFrame()
+    scoring.bonusScoreAllFrames()
+    scoring.totalScoreAllFrames()
+    scoring.findTotalScore()
+    expect(scoring.totalScore).toEqual(300);
+  });
   it('calculates regular score per frame', () => {
     let mockScorecard = { pins: [
       [10, 0],
@@ -31,7 +35,8 @@ describe('.regularScorePerFrame', () => {
       [10, 10, 10]
     ]}
     scoring = new Scoring(mockScorecard.pins)
-    expect(scoring.regularScorePerFrame()).toEqual(
+    scoring.regularScorePerFrame()
+    expect(scoring.regularScore).toEqual(
       [
         10,
         10,
@@ -46,38 +51,186 @@ describe('.regularScorePerFrame', () => {
       ]
     );
   });
-  // it('calculates bonus score for first frame', () => {
-  //   let mockScorecard = { pins: [
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 10, 10]
-  //   ]}
-  //   scoring = new Scoring(mockScorecard.pins)
-  //   scoring.bonusScore1stFrame()
-  //   expect(scoring.bonusScore[0]).toEqual(20);
-  // });
-  // it('calculates bonus score for first 8 frames', () => {
-  //   let mockScorecard = { pins: [
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 0],
-  //     [10, 10, 10]
-  //   ]}
-  //   scoring = new Scoring(mockScorecard.pins)
-  //   scoring.bonusScore1stFrame()
-  //   expect(scoring.bonusScore[0]).toEqual(160);
-  // });
+  it('calculates bonus score per frame', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 10, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.bonusScoreAllFrames()
+    expect(scoring.bonusScore).toEqual(
+      [
+        20,
+        20,
+        20,
+        20,
+        20,
+        20,
+        20,
+        20,
+        20,
+        20
+      ]
+    );
+  });
+  it('calculates total points per frame', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 0],
+      [10, 10, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.regularScorePerFrame()
+    scoring.bonusScoreAllFrames()
+    scoring.totalScoreAllFrames()
+    expect(scoring.totalScorePerFrame).toEqual(
+    [
+      30,
+      30,
+      30,
+      30,
+      30,
+      30,
+      30,
+      30,
+      30,
+      30
+    ]
+  );
+  });
+});
+
+
+
+
+
+
+describe('Scoring an imperfect game', () => {
+  it('reports total score', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 9, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.regularScorePerFrame()
+    scoring.bonusScoreAllFrames()
+    scoring.totalScoreAllFrames()
+    scoring.findTotalScore()
+    expect(scoring.totalScore).toEqual(39);
+  });
+  it('calculates regular score', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 9, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.regularScorePerFrame()
+    expect(scoring.regularScore).toEqual(
+      [
+        10,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        10
+      ]
+    );
+  });
+  it('calculates bonus score', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 9, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.bonusScoreAllFrames()
+    expect(scoring.bonusScore).toEqual(
+      [
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        10
+      ]
+    );
+  });
+  it('calculates total points per frame for perfect game', () => {
+    let mockScorecard = { pins: [
+      [10, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 0],
+      [1, 9, 10]
+    ]}
+    scoring = new Scoring(mockScorecard.pins)
+    scoring.regularScorePerFrame()
+    scoring.bonusScoreAllFrames()
+    scoring.totalScoreAllFrames()
+    expect(scoring.totalScorePerFrame).toEqual(
+    [
+      11,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      20
+    ]
+  );
+  });
 });

c9364041c7382d7a651a73bfdd7b8942b8b058bd
Douglas <douglaskerins@gmail.com>
2021-10-29 16:33:24 +0100

    adding addFirstBowl and addSecondBowl functions

--- bowling.test.js
+++ bowling.test.js
@@ -1,6 +1,27 @@
 const Bowling = require('./bowling')
 
-describe("", () => {
-  test("", () => {
+
+beforeEach(() => {
+  bowling = new Bowling;
+})
+describe("constructor function", () => {
+  test("initialize with default values", () => {
+    expect(bowling.gameArray).toEqual([])
+    expect(bowling.roundArray).toEqual([])
+    expect(bowling.roundCounter).toEqual(1)
+    expect(bowling.firstBowl).toBeNull()
+    expect(bowling.secondBowl).toBeNull()
   });
 });
+describe("addFirstBowl function", () => {
+  test("should give firstBowl a value of 5", () => {
+    bowling.addFirstBowl(5);
+    expect(bowling.firstBowl).toEqual(5);
+  })
+})
+describe("addSecondBowl function", () => {
+  test("should give secondBowl a value of 7", () => {
+    bowling.addSecondBowl(7);
+    expect(bowling.secondBowl).toEqual(7);
+  })
+})

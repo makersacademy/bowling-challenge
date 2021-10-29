e274a3e01c3e997dd410b339a936dfe29d17c010
AJOsmaston <dajoxx@gmail.com>
2021-10-29 17:55:11 +0100

    added roll method

--- test/bowling.test.js
+++ test/bowling.test.js
@@ -6,15 +6,15 @@ describe('Bowling', () => {
 
     beforeEach(() => {
       this.bowling = new Bowling
-    })
+    });
 
     it('can calculate two rolls', () => {
       this.bowling.roll(3)
       this.bowling.roll(4)
       
       expect(this.bowling.score).toBe(7)
-    })
+    });
 
-  })
+  });
 
 });

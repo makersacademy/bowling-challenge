ffa2c3d8ee1fa6258f1fd8f282353dbbfa877b06
ConorButler <cwdbutler@gmail.com>
2021-10-29 22:14:10 +0100

    Prettier

--- bowling.js
+++ bowling.js
@@ -1,9 +1,8 @@
 const Frame = require('./frame');
 
 class Bowling {
-
   constructor() {
-    this.frames = []
+    this.frames = [];
   }
 
   totalScore() {
@@ -11,82 +10,68 @@ class Bowling {
   }
 
   inputFullGame(arrayOfRolls) {
-    arrayOfRolls.forEach( pins =>
-      this.roll(pins)
-    )
+    arrayOfRolls.forEach((pins) => this.roll(pins));
   }
 
   roll(pins) {
-    if (this.gameOver) { throw 'Game Over!' }
-    else if (this.finalFrame()) { this.updateFinalFrame(pins) }
-    else {
-      this.updateFrames(pins)
-      if (this.currentFrame.isComplete()) { this.storeFrame() }
+    if (this.gameOver) {
+      throw 'Game Over!';
+    } else if (this.finalFrame()) {
+      this.updateFinalFrame(pins);
+    } else {
+      this.updateFrames(pins);
+      if (this.currentFrame.isComplete()) {
+        this.storeFrame();
+      }
     }
   }
 
   updateFrames(pins) {
-    this.currentFrame ||= new Frame.Frame()
-    if (this.bonusFrames) { this.updateBonusFrames(pins) }
-    this.updateCurrentFrame(pins)
+    this.currentFrame ||= new Frame.Frame();
+    if (this.bonusFrames) {
+      this.updateBonusFrames(pins);
+    }
+    this.updateCurrentFrame(pins);
   }
 
   bonusFrames() {
-    return this.frames.filter(frame => frame.hasActiveBonus())
+    return this.frames.filter((frame) => frame.hasActiveBonus());
   }
 
   updateBonusFrames(pins) {
-    this.bonusFrames().forEach(function(bonusframe) {
-      bonusframe.updateTotal(pins),
-      bonusframe.deductBonusRoll()
-    })
+    this.bonusFrames().forEach(function (bonusframe) {
+      bonusframe.updateTotal(pins), bonusframe.deductBonusRoll();
+    });
   }
 
   updateCurrentFrame(pins) {
-    this.currentFrame.addPins(pins)
-    this.currentFrame.calculateTotal()
+    this.currentFrame.addPins(pins);
+    this.currentFrame.calculateTotal();
   }
 
   updateFinalFrame(pins) {
-    this.currentFrame ||= new Frame.FinalFrame()
-    this.updateFrames(pins)
-    this.currentFrame.calculateBonus()
-    if (this.currentFrame.isComplete()) { this.endGame() }
+    this.currentFrame ||= new Frame.FinalFrame();
+    this.updateFrames(pins);
+    this.currentFrame.calculateBonus();
+    if (this.currentFrame.isComplete()) {
+      this.endGame();
+    }
   }
 
   storeFrame() {
-    this.currentFrame.calculateBonus()
-    this.frames.push(this.currentFrame)
-    this.currentFrame = null
+    this.currentFrame.calculateBonus();
+    this.frames.push(this.currentFrame);
+    this.currentFrame = null;
   }
 
   finalFrame() {
-    return this.frames.length === 9
+    return this.frames.length === 9;
   }
 
   endGame() {
-    this.storeFrame()
-    this.gameOver = true
-    console.log('Game Over!')
+    this.storeFrame();
+    this.gameOver = true;
   }
-
 }
 
 module.exports = Bowling;
-
-// class Bowling
-  
-//   attr_reader :frames
-
-//   def initialize(frame_class: Frame, final_frame_class: FinalFrame)
-//     @frames = []
-//     @frame_class = frame_class
-//     @final_frame_class = final_frame_class
-//   end
-
-//   def calculate_full_game(rolls)
-//     rolls.each { |pins| roll(pins) }
-//     return total_score
-//   end
-
-// end
--- bowling.test.js
+++ bowling.test.js
@@ -1,108 +1,102 @@
 const Bowling = require('./bowling');
 
 describe('Bowling', () => {
-
   beforeEach(() => {
-    game = new Bowling;
+    game = new Bowling();
   });
 
   describe('Basic rules', () => {
-
     it('calculates the total score', () => {
-      expect(game.totalScore()).toBe(0)
-      game.roll(1)
-      game.roll(4)
-      expect(game.totalScore()).toBe(5)
+      expect(game.totalScore()).toBe(0);
+      game.roll(1);
+      game.roll(4);
+      expect(game.totalScore()).toBe(5);
     });
 
     it('accounts for spares', () => {
-      game.roll(6)
-      game.roll(4) 
-      game.roll(5) // spare + bonus 5 = 15, total + 5 for this frame
-      game.roll(0) // ending the frame so the score updates
-      expect(game.totalScore()).toBe(20)
+      game.roll(6);
+      game.roll(4);
+      game.roll(5); // spare + bonus 5 = 15, total + 5 for this frame
+      game.roll(0); // ending the frame so the score updates
+      expect(game.totalScore()).toBe(20);
     });
 
     it('accounts for strikes', () => {
-      game.roll(10)
-      game.roll(1) 
-      game.roll(2) // strike + bonus 1 and 2 = 13, total + 3 for this frame
-      game.roll(0) // ending the frame so the score updates
-      expect(game.totalScore()).toBe(16)
+      game.roll(10);
+      game.roll(1);
+      game.roll(2); // strike + bonus 1 and 2 = 13, total + 3 for this frame
+      game.roll(0); // ending the frame so the score updates
+      expect(game.totalScore()).toBe(16);
     });
-
   });
 
   describe('Full games', () => {
-
     it('calculates a total score of 20 for 20 rolls of 1', () => {
       for (i = 1; i <= 20; i++) {
-        game.roll(1)
+        game.roll(1);
       }
-      expect(game.totalScore()).toBe(20)
+      expect(game.totalScore()).toBe(20);
     });
 
     it('calculates a total score of 0 for a gutter game', () => {
       for (i = 1; i <= 20; i++) {
-        game.roll(0)
+        game.roll(0);
       }
-      expect(game.totalScore()).toBe(0)
+      expect(game.totalScore()).toBe(0);
     });
 
     it('calculates a total score of 300 for a perfect game', () => {
       for (i = 1; i <= 12; i++) {
-        game.roll(10)
+        game.roll(10);
       }
-      expect(game.totalScore()).toBe(300)
+      expect(game.totalScore()).toBe(300);
     });
 
-    it('calculates a total score of 133', () => { // example from README
-      rolls = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]
-      game.inputFullGame(rolls)
-      expect(game.totalScore()).toBe(133)
+    it('calculates a total score of 133', () => {
+      // example from README
+      rolls = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6];
+      game.inputFullGame(rolls);
+      expect(game.totalScore()).toBe(133);
     });
 
-    it('calculate a score of 122', () => { // ending with a spare and strike
-      rolls = [8, 1, 0, 9, 2, 8, 10, 6, 3, 7, 0, 5, 2, 10, 0, 6, 2, 8, 10]
-      game.inputFullGame(rolls)
-      expect(game.totalScore()).toBe(122)
+    it('calculate a score of 122', () => {
+      // ending with a spare and strike
+      rolls = [8, 1, 0, 9, 2, 8, 10, 6, 3, 7, 0, 5, 2, 10, 0, 6, 2, 8, 10];
+      game.inputFullGame(rolls);
+      expect(game.totalScore()).toBe(122);
     });
-
   });
 
   describe('Ending games', () => {
-
     const extraRoll = () => {
       game.roll(1); // expect___.toThrow() needs an anonymous function
     };
 
     beforeEach(() => {
-      game = new Bowling
+      game = new Bowling();
       for (i = 1; i <= 18; i++) {
-        game.roll(0) // getting to the final frame
-      };
+        game.roll(0); // getting to the final frame
+      }
     });
 
     it("ends game after 2 rolls if they aren't a strike or spare", () => {
-      extraRoll()
-      extraRoll()
-      expect(extraRoll).toThrow('Game Over!')
+      extraRoll();
+      extraRoll();
+      expect(extraRoll).toThrow('Game Over!');
     });
 
     it('ends the game after 3 rolls if there is a strike', () => {
-      game.roll(10)
-      extraRoll()
-      extraRoll()
-      expect(extraRoll).toThrow('Game Over!')
+      game.roll(10);
+      extraRoll();
+      extraRoll();
+      expect(extraRoll).toThrow('Game Over!');
     });
 
     it('ends the game after 3 rolls if there is a spare', () => {
-      game.roll(8)
-      game.roll(2)
-      extraRoll()
-      expect(extraRoll).toThrow('Game Over!')
+      game.roll(8);
+      game.roll(2);
+      extraRoll();
+      expect(extraRoll).toThrow('Game Over!');
     });
-
   });
-
 });

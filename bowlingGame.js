18cea0eb9de2b3d46d8676a64946e44f3d6c6c90
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-31 10:12:56 +0000

    handles a spare now

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,27 +1,23 @@
 class BowlingGame {
   constructor(){
     this.rolls = [];
-    this.currentFrame = 1;
+    // this.currentFrame = 1;
   };
   roll(number){
     this.rolls.push(number);
-    console.log(`rolled a ${number}`)
   };
   score(){
-    console.log(`rolls length is ${this.rolls.length}`);
     let totalScore = 0;
-    let currentIndex = 0;
-    for(i=0; i < 10; i++){
-      if(this.rolls[currentIndex] + this.rolls[currentIndex + 1] == 10){
-        totalScore += (this.rolls[currentIndex] + this.rolls[currentIndex + 1] + this.rolls[currentIndex + 2]); 
-        console.log(totalScore)  
+    let currentRoll = 0;
+    while(currentRoll < this.rolls.length){
+      if((this.rolls[currentRoll] + this.rolls[currentRoll + 1]) == 10){
+        totalScore += (this.rolls[currentRoll] + this.rolls[currentRoll + 1] + this.rolls[currentRoll + 2]);
       }
-      else{
-        totalScore += this.rolls[currentIndex] + this.rolls[currentIndex + 1];   
-        console.log(totalScore)
+      else {
+        totalScore += (this.rolls[currentRoll] + this.rolls[currentRoll + 1]);
       };
-      currentIndex += 2;
-    };
+      currentRoll += 2;
+    }
     return totalScore;
   };
   

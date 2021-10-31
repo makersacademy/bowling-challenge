32a187d352c153703199978d2c26d4492ae07606
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-31 15:08:54 +0000

    game is functional but working on roll validation

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,25 +1,65 @@
 class BowlingGame {
   constructor(){
     this.rolls = [];
-    // this.currentFrame = 1;
+    this.currentFrame = 1;
   };
   roll(number){
-    this.rolls.push(number);
+    if(number > 10){
+      throw "Invalid Roll";
+    }
+    else {
+      this.rolls.push( { value: number, frame: this.currentFrame } );
+    };
+    
   };
+  // [10, 5, 5, 10, 5, 3]
   score(){
     let totalScore = 0;
     let currentRoll = 0;
     while(currentRoll < this.rolls.length){
-      if((this.rolls[currentRoll] + this.rolls[currentRoll + 1]) == 10){
-        totalScore += (this.rolls[currentRoll] + this.rolls[currentRoll + 1] + this.rolls[currentRoll + 2]);
+      if(this.currentFrame == 10){
+        totalScore += this.sumTheRest(currentRoll);
+        break;
+      }
+      else if(this.frameIsStrike(currentRoll)){       
+        totalScore += this.bonusScoring(currentRoll);
+        currentRoll += 1;
+        this.currentFrame += 1
+      }
+      else if(this.frameIsSpare(currentRoll)){
+        totalScore += this.bonusScoring(currentRoll);
+        currentRoll += 2;
+        this.currentFrame += 1
       }
       else {
-        totalScore += (this.rolls[currentRoll] + this.rolls[currentRoll + 1]);
+        totalScore += this.rolls[currentRoll].value + this.rolls[currentRoll + 1].value;
+        currentRoll += 2;
+        this.currentFrame += 1
       };
-      currentRoll += 2;
+      
     }
     return totalScore;
   };
+
+  sumTheRest(currentRoll){
+    let sum = 0;
+    this.rolls.slice(currentRoll).forEach(roll => {
+      sum += roll.value
+    });
+    return sum;
+  };
+  frameIsSpare(currentRoll){
+    return this.rolls[currentRoll].value + this.rolls[currentRoll + 1].value == 10;
+  };
+  frameIsStrike(currentRoll){
+    return this.rolls[currentRoll].value == 10;
+  };
+  finalFrame(currentRoll){
+    
+  };
+  bonusScoring(currentRoll){
+    return this.rolls[currentRoll].value + this.rolls[currentRoll + 1].value + this.rolls[currentRoll + 2].value;
+  }; 
   
 };
 
--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -21,11 +21,64 @@ describe(BowlingGame, () => {
   it('handles a spare then a six then all gutters', () => {
     this.game.roll(3);
     this.game.roll(7);
-    this.game.roll(6);
-    
+    this.game.roll(6); 
     this.game.roll(1);  
+    for(i=0; i<16; i++){
+      this.game.roll(0);
+    };
     
     expect(this.game.score()).toBe(23); // 3 + 7 + 6 + bonus 6
+  });
+  it('handles a strike followed by standard rolls', () => {
+    this.game.roll(10);
+    this.game.roll(3);
+    this.game.roll(2);
+    for(i=0; i<16; i++){
+      this.game.roll(0);
+    };
+
+    expect(this.game.score()).toBe(20);
+  });
+  it('handles two strikes in a row', () => {
+    this.game.roll(10);
+    this.game.roll(10);
+    this.game.roll(4);
+    this.game.roll(3);
+    for(i=0; i<14; i++){
+      this.game.roll(0);
+    };
+
+    expect(this.game.score()).toBe(10 + 14 + 10 + 7 + 4 + 3);
+  });
+  it('handles mix of strikes and spares', () => {
+    this.game.roll(10);
+    this.game.roll(5);
+    this.game.roll(5);
+    this.game.roll(10);
+    this.game.roll(5);
+    this.game.roll(3);
+    for(i=0; i<12; i++){
+      this.game.roll(0);
+    };
+    expect(this.game.score()).toBe(66);
+  }); 
+  it('handles perfect game', () => {
+    for(i = 0; i < 12; i++){
+      this.game.roll(10)
+    };
+    expect(this.game.score()).toBe(300);
+    
+  });
+  it("doesn't let you roll more than 10", () => {
+    expect(() => {
+      this.game.roll(12)
+    }).toThrowError('Invalid Roll')
+  });
+  it("doesn't let you knock down more than 10 in a frame", () => {
+    expect(() => {
+      this.game.roll(5);
+      this.game.roll(6);
+    }).toThrowError('Invalid Roll')
   });
 });
 

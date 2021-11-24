787cc47e2b2450ca9a3738e721d1ae1f40d0df21
Matt Zimmer <zimmermja@gmail.com>
2021-11-24 21:04:22 +0000

    first test passed: bonusType

--- frame.js
+++ frame.js
@@ -1,5 +1,36 @@
 class Frame {
+  constructor(frame, nextFrame = null, nextNextFrame = null) {
+    this.frame = frame;
+    this.nextFrame = nextFrame;
+    this.nextNextFrame = nextNextFrame;
+  }
 
-};
+  checkfunc = () => {
+    console.log(this.nextFrame == null);
+    console.log(this.sumFrame(this.frame) < 10);
+  };
+
+  bonusType = () => {
+    if (this.nextFrame == null || this.sumFrame(this.frame) < 10) {
+      return "no_bonus";
+    } else if (this.inFrame(this.frame, 10)) {
+      return "strike";
+    } else {
+      return "spare";
+    }
+  };
+
+  sumFrame = (frame) => {
+    let sum = 0;
+    for (let i = 0; i < frame.length; i++) {
+      sum += frame[i];
+    }
+    return sum;
+  };
+
+  inFrame = (frame, num) => {
+    return frame.includes(num);
+  };
+}
 
 module.exports = Frame;
--- frame.test.js
+++ frame.test.js
@@ -1,7 +1,28 @@
 const Frame = require("./frame");
 
-describe(".bonus_type", () => {
-  it('returns the type of bonus the frame should receive', () => {
+const frames = [
+  new Frame([0, 0], [3, 3], [3, 3]), // normal frames
+  new Frame([5, 3], [3, 3], [3, 3]),
+  new Frame([5, 5], [3, 3], [3, 3]), // spares
+  new Frame([5, 5]),
+  new Frame([10, 0], [3, 3], [3, 3]), // strikes
+  new Frame([10, 0], [10, 0], [3, 3]),
+  new Frame([10, 0]),
+];
 
+describe(".bonus_type", () => {
+  it("returns the type of bonus the frame should receive", () => {
+    let expectations = [
+      "no_bonus",
+      "no_bonus",
+      "spare",
+      "no_bonus",
+      "strike",
+      "strike",
+      "no_bonus",
+    ];
+    expectations.forEach((roll, index) => {
+      expect(frames[index].bonusType()).toEqual(roll);
+    });
   });
 });

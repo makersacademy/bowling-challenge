d856bd2ab3822328ec8640174e2e1060b811b690
Matt Zimmer <zimmermja@gmail.com>
2021-11-25 12:42:44 +0000

    refactored frame.js

--- frame.js
+++ frame.js
@@ -5,29 +5,17 @@ class Frame {
     this.nextNextFrame = nextNextFrame;
   }
 
-  checkfunc = () => {
-    console.log(this.nextFrame == null);
-    console.log(this.sumFrame(this.frame) < 10);
-  };
-
   bonusType = () => {
-    if (this.nextFrame == null || this.sumFrame(this.frame) < 10) {
+    if (this.nextFrame == null || this.sumFrame(this.frame) < 10)
       return "no_bonus";
-    } else if (this.inFrame(this.frame, 10)) {
-      return "strike";
-    } else {
-      return "spare";
-    }
+    if (this.inFrame(this.frame, 10)) return "strike";
+    return "spare";
   };
 
   score = () => {
-    if (this.bonusType() == "no_bonus") {
-      return this.sumFrame(this.frame);
-    } else if (this.bonusType() == "spare") {
-      return this.calcSpare(this.nextFrame);
-    } else if (this.bonusType() == "strike") {
-      return this.calcStrike(this.nextFrame, this.nextNextFrame);
-    }
+    if (this.bonusType() == "no_bonus") return this.sumFrame(this.frame);
+    if (this.bonusType() == "strike") return this.calcStrike();
+    return this.calcSpare();
   };
 
   sumFrame = (frame) => {
@@ -38,16 +26,13 @@ class Frame {
     return frame.includes(num);
   };
 
-  calcSpare = (frame) => {
+  calcSpare = (frame = this.nextFrame) => {
     return 10 + frame[0];
   };
 
-  calcStrike = (nframe, nnframe) => {
-    if (this.inFrame(nframe, 10) && nnframe != null) {
-      return 20 + nnframe[0];
-    } else {
-      return 10 + this.sumFrame(nframe);
-    }
+  calcStrike = (nframe = this.nextFrame, nnframe = this.nextNextFrame) => {
+    if (this.inFrame(nframe, 10) && nnframe != null) return 20 + nnframe[0];
+    return 10 + this.sumFrame(nframe);
   };
 }
 

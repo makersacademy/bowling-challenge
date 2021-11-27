cc7f1d214698bc078982237ca05e7e864c36b9e2
Mark McLeod <markmcleod45@gmail.com>
2021-11-27 17:22:54 +0000

    Second test written as passing for gutter game. Third test for perfect game written but not passing

--- scorecard.js
+++ scorecard.js
@@ -2,8 +2,12 @@ class Scorecard {
   constructor(game) {
     this.game = game
     this.score = null
+    this.gameGutter = [ [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0] ];
+    this.gamePerfect = [ [10], [10], [10], [10], [10], [10], [10], [10], [10], [10, 10, 10] ];
   }
-//calculateScore ()
+  calculateScore() {
+  return this.score = 0
+  }
 }
 
 module.exports = Scorecard;
--- scorecard.test.js
+++ scorecard.test.js
@@ -1,48 +1,22 @@
 const Scorecard = require('./scorecard');
 
-const game = [
-  [1, 4],
-  [4, 5],
-  [6, 4],
-  [5, 5],
-  [10],
-  [0, 1],
-  [7, 3],
-  [6, 4],
-  [10],
-  [2, 8, 6]
-];
-
-const gameGutter = [
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-  [0,0],
-];
-
-const gamePerfect = [
-  [10],
-  [10],
-  [10],
-  [10],
-  [10],
-  [10],
-  [10],
-  [10],
-  [10],
-  [10, 10, 10]
-];
-
 describe('Scorecard class', () => {
-  it('initializes an instance of a scorecard with a game attribute and score attribute of 0', () => {
-    const scorecard = new Scorecard(game);
-    expect(scorecard.game).toEqual(game);
-  });
   
+  it('initializes an instance of a scorecard with a game attribute and score attribute of 0', () => {
+    const scorecard = new Scorecard([ [1, 4], [4, 5], [6, 4], [5, 5], [10], [0, 1], [7, 3], [6, 4], [10], [2, 8, 6] ]);
+    expect(scorecard.game).toEqual([ [1, 4], [4, 5], [6, 4], [5, 5], [10], [0, 1], [7, 3], [6, 4], [10], [2, 8, 6] ]);
+  });
+
+  it('gets the score for a gutter game', () => {
+    const scorecard = new Scorecard([ [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0] ]);
+    expect(scorecard.game).toEqual([ [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0] ]);
+    expect(scorecard.calculateScore()).toEqual(0);
+  });
+
+  it('gets the score for a perfect game', () => {
+    const scorecard = new Scorecard([ [10], [10], [10], [10], [10], [10], [10], [10], [10], [10, 10, 10] ]);
+    expect(scorecard.game).toEqual([ [10], [10], [10], [10], [10], [10], [10], [10], [10], [10, 10, 10] ]);
+    expect(scorecard.calculateScore()).toEqual(300);
+  });
+
 });

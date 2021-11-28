3eeaa74d99debeb0a76f5ec944b37ee0a3ed9f5c
Matt Zimmer <zimmermja@gmail.com>
2021-11-28 18:27:38 +0000

    added cli.js file to play game in node

---
+++ cli.js
@@ -0,0 +1,26 @@
+let readlineSync = require("readline-sync");
+const Scorecard = require("./scorecard");
+const Frame = require("./frame");
+
+const newScorecard = new Scorecard();
+
+const optionsMessage = (pins) => {
+  return `Knocked down ${pins} pins. Scorebard:\n${newScorecard.board()}`;
+};
+
+let loop = true;
+
+console.log("------------------\nWELCOME TO BOWLING\n------------------");
+console.log(
+  "Enter the number of pins knocked down with each bowl below.\nLeave input blank and press enter to exit.\n"
+);
+while (loop == true) {
+  let command = readlineSync.question("Pins knocked down: ");
+  if (command == "") {
+    loop = false;
+    console.log("Goodbye!");
+  } else {
+    newScorecard.addBowl(Number(command));
+    console.log(`${optionsMessage(command)}\n`);
+  }
+}

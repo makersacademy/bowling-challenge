64659da95fce5758cb02e40c4e757aa86666a967
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-28 23:24:02 +0000

    calculates a strike - test passed

--- game.js
+++ game.js
@@ -11,25 +11,29 @@ class Game {
 
   get points() {
     let points = 0;
-    let rollIndex =0;
+    let rollIndex = 0;
 
     // calculates points per game frames(therefore 10 is the iteration value)
-    for (let frame = 0; frame < 10; frame ++) {
-      const framePoints = this.rolls[rollIndex] + this.rolls[rollIndex + 1]; 
-        
-        if (framePoints === 10) {
-          points += 10 + this.rolls[rollIndex + 2]
-        } else {
-            points += framePoints;
-        }
-        rollIndex += 2;
-    }
-      return points;   
+    for (let frame = 0; frame < 10; frame++) {
+      
+      // isStrike?
+      if(this.rolls[rollIndex] === 10) {
+        points += 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+        rollIndex ++;
+        continue;
+      } 
+
+      const framePoints = this.rolls[rollIndex] + this.rolls[rollIndex + 1];  
+      
+      if (framePoints === 10) {
+        points += 10 + this.rolls[rollIndex + 2];
+      } else {
+        points += framePoints;
+      }
+      rollIndex += 2;
+    } 
+      return points; 
   }
-
-
-
-
 
 }
 
--- game.test.js
+++ game.test.js
@@ -1,3 +1,4 @@
+// const { it } = require('jest-circus');
 const Game = require('./game');
 
 let game;
@@ -6,26 +7,45 @@ beforeEach(() => {
   game = new Game;
 });
 
-test('calculates a gutter game', () => {
-  for(let i = 0; i < 20; i++) {
-    game.roll(0); 
-  }
-  expect(game.points).toEqual(0);
+describe('gutter game', () => {
+  it('calculates a gutter game', () => {
+    for(let i = 0; i < 20; i++) {
+      game.roll(0); 
+    }
+    expect(game.points).toEqual(0);
+  });
 });
 
-test('calculates all ones', () => {
-  for(let i = 0; i < 20; i++) {
-    game.roll(1);
-  }
-  expect(game.points).toEqual(20);
+describe('all ones', () => {
+  it('calculates all ones', () => {
+    for(let i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.points).toEqual(20);
+  });
 });
 
-test ('calculates a spare', () => {
-  game.roll(5);
-  game.roll(5);
-  game.roll(3);
-  for(let i = 0; i < 17; i++) {
-    game.roll(0);
-  }
-  expect(game.points).toEqual(16);
+
+describe('spare', () => {
+  it('calculates a spare', () => {
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    for(let i = 0; i < 17; i++) {
+      game.roll(0);
+    }
+    expect(game.points).toEqual(16);
+  });
+});
+
+describe('strike', () => {
+  it('calculates a strike', () => {
+    game.roll(10);
+    game.roll(5);
+    game.roll(3);
+    for (let i = 0; i < 16; i++){
+      game.roll(0);
+    }
+    expect(game.points).toEqual(26);
+  });
 });

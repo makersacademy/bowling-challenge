b72c64429fe3e9eca60f9d931ac8cf32ebf50122
Adam Harbisher <aharbisher@gmail.com>
2021-11-29 07:41:34 +0000

    Doubles first roll of frame on spare

--- bowling.js
+++ bowling.js
@@ -3,6 +3,7 @@ class Bowling {
     this.frame = 0;
     this.totalScore = 0;
     this.frames = [];
+    this.scoreFrames = [];
     this.roll = 1;
   }
 
@@ -14,14 +15,36 @@ class Bowling {
     return total + roll;
   }
 
+  isSpare() {
+    if(this.frame != 0) {
+      if ((this.frames[this.frame - 1].reduce(this.addFrame)) === 10) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  addToScore(frameScore) {
+    this.totalScore += frameScore;
+  }
+
   addRoll(pins) {
     if (this.roll === 1) {
-      this.frames.push([pins])
+      this.frames.push([pins]);
+      if (this.isSpare()) {
+        this.scoreFrames.push([pins * 2]);
+      } else {
+        this.scoreFrames.push([pins]);
+      }
       this.roll = 2;
     } else if (this.roll === 2) {
-      this.frames[this.frame].push(pins)
+      this.frames[this.frame].push(pins);
+      this.scoreFrames[this.frame].push(pins);
       this.roll = 1;
-      this.totalScore += this.frames[this.frame].reduce(this.addFrame);
+      this.addToScore(this.scoreFrames[this.frame].reduce(this.addFrame));
       this.frame += 1;
     }
   }
--- bowling.test.js
+++ bowling.test.js
@@ -5,14 +5,23 @@ describe('bowling', () => {
 
   describe('addRoll', () => {
     it('adds the roll score to the total score', () => {
+      bowling.addRoll(1);
       bowling.addRoll(4);
-      bowling.addRoll(4);
-      expect(bowling.getTotalScore()).toBe(8);
+      expect(bowling.getTotalScore()).toBe(5);
     });
 
     it('only adds the score when both rolls have been done', () => {
-      bowling.addRoll(4);
-      expect(bowling.getTotalScore()).toBe(8);
-    })
+      bowling.addRoll(5);
+      expect(bowling.getTotalScore()).toBe(5);
+      bowling.addRoll(5);
+      expect(bowling.getTotalScore()).toBe(15);
+    });
+
+    it('doubles the score of the first roll when previous frame was a spare', () => {
+      bowling.addRoll(3);
+      bowling.addRoll(3);
+      expect(bowling.getTotalScore()).toBe(24);
+    });
+
   });
 });

233e78604fb2dd05eeb1c79a98bfceda9c863bdf
Adam Harbisher <aharbisher@gmail.com>
2021-11-29 08:06:07 +0000

    added logic and test for strikes

--- bowling.js
+++ bowling.js
@@ -5,6 +5,7 @@ class Bowling {
     this.frames = [];
     this.scoreFrames = [];
     this.roll = 1;
+    this.isStrike = false;
   }
 
   getTotalScore() {
@@ -15,15 +16,15 @@ class Bowling {
     return total + roll;
   }
 
-  isSpare() {
+  isSpare(pins) {
     if(this.frame != 0) {
       if ((this.frames[this.frame - 1].reduce(this.addFrame)) === 10) {
-        return true;
+        return pins * 2;
       } else {
-        return false;
+        return pins;
       }
     } else {
-      return false;
+      return pins;
     }
   }
 
@@ -31,15 +32,46 @@ class Bowling {
     this.totalScore += frameScore;
   }
 
+  setIsStrike(pins) {
+    if (pins === 10) {
+      this.isStrike = true;
+      this.addToScore(this.scoreFrames[this.frame].reduce(this.addFrame));
+      this.frame += 1;
+      this.roll = 1;
+    } else {
+      false;
+      this.roll = 2;
+    }
+  }
+
+  Roll(pins) {
+    if (this.isStrike) {
+      this.addStrikeRoll(pins);
+    } else {
+      this.addRoll(pins);
+    }
+  }
+
   addRoll(pins) {
     if (this.roll === 1) {
       this.frames.push([pins]);
-      if (this.isSpare()) {
-        this.scoreFrames.push([pins * 2]);
-      } else {
-        this.scoreFrames.push([pins]);
-      }
-      this.roll = 2;
+      this.scoreFrames.push([this.isSpare(pins)]);
+      this.setIsStrike(pins);
+    } else if (this.roll === 2) {
+      this.frames[this.frame].push(pins);
+      this.scoreFrames[this.frame].push(pins);
+      this.roll = 1;
+      this.addToScore(this.scoreFrames[this.frame].reduce(this.addFrame));
+      this.frame += 1;
+    }
+  }
+
+  addStrikeRoll(pins) {
+    if (this.roll === 1) {
+      this.totalScore += 10;
+      this.frames.push([pins]);
+      this.scoreFrames.push([pins + 2]);
+      this.setIsStrike(pins);
     } else if (this.roll === 2) {
       this.frames[this.frame].push(pins);
       this.scoreFrames[this.frame].push(pins);
--- bowling.test.js
+++ bowling.test.js
@@ -5,23 +5,35 @@ describe('bowling', () => {
 
   describe('addRoll', () => {
     it('adds the roll score to the total score', () => {
-      bowling.addRoll(1);
-      bowling.addRoll(4);
+      bowling.Roll(1);
+      bowling.Roll(4);
       expect(bowling.getTotalScore()).toBe(5);
     });
 
     it('only adds the score when both rolls have been done', () => {
-      bowling.addRoll(5);
+      bowling.Roll(5);
       expect(bowling.getTotalScore()).toBe(5);
-      bowling.addRoll(5);
+      bowling.Roll(5);
       expect(bowling.getTotalScore()).toBe(15);
     });
 
     it('doubles the score of the first roll when previous frame was a spare', () => {
-      bowling.addRoll(3);
-      bowling.addRoll(3);
+      bowling.Roll(3);
+      bowling.Roll(3);
       expect(bowling.getTotalScore()).toBe(24);
     });
+
+    it('adds score after one roll on strike', () => {
+      bowling.Roll(10);
+      expect(bowling.getTotalScore()).toBe(34);
+    });
+
+    it('adds strike bonus', () => {
+      bowling.Roll(5);
+      bowling.Roll(4);
+      expect(bowling.getTotalScore()).toBe(55);
+    })
+
 
   });
 });

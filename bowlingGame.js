0619a3d9e5d2dcb3515855e1276094f5068b4860
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-07 17:40:24 +0000

    Implement turn logic

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,12 +1,21 @@
 class Bowling {
   constructor() {
     this.totalScore = 0;
+    this.turn = 1;
   }
   getTotalScore() {
     return this.totalScore;
   }
   roll(pins) {
     this.totalScore += pins;
+    this.changeTurn()
+  }
+  getTurn() {
+    return this.turn;
+  }
+  changeTurn() {
+    if(this.turn === 1) { this.turn = 2}
+    else if(this.turn === 2) { this.turn = 1}
   }
 }
 
--- testing/bowlingGame.test.js
+++ testing/bowlingGame.test.js
@@ -15,9 +15,28 @@ describe('class Bowling', () => {
     it('has a total of 0 for a gutter game', () => {
       for(let i = 0; i < 20 ; i++) {
         bowling.roll(0);
-        i += 1;
       }
       expect(bowling.getTotalScore()).toEqual(0);
+    })
+    it('has a correct total for a game without spares and strikes', () => {
+      for(let i = 0; i < 20 ; i++) {
+        bowling.roll(2);
+      }
+      expect(bowling.getTotalScore()).toEqual(40);
+    })
+  })
+  describe('turn', () => {
+    it('starts at 1', () => {
+      expect(bowling.getTurn()).toEqual(1);
+    })
+    it('increases to 2 after a roll', () => {
+      bowling.roll(5);
+      expect(bowling.getTurn()).toEqual(2);
+    })
+    it('goes back to 1 after 2 rolls', () => {
+      bowling.roll(5);
+      bowling.roll(1);
+      expect(bowling.getTurn()).toEqual(1);
     })
   })
 })

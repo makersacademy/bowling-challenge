66122577d3a42a0fb1c37db98f0004ccc1c96a95
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-07 17:16:53 +0000

    getTotalScore function created to pass first test

---
+++ bowlingGame.js
@@ -0,0 +1,10 @@
+class Bowling {
+  constructor() {
+    this.totalScore = 0;
+  }
+  getTotalScore() {
+    return this.totalScore
+  }
+}
+
+module.exports = Bowling
--- testing/bowlingGame.test.js
+++ testing/bowlingGame.test.js
@@ -1,8 +1,10 @@
+const Bowling = require('../bowlingGame.js')
+
 describe('class Bowling', () => {
   beforeEach(()=> {
     bowling = new Bowling();
   })
   it('starts with a total score of 0', () => {
-    expect(bowling.totalScore()).toEqual(0);
+    expect(bowling.getTotalScore()).toEqual(0);
   })
 })

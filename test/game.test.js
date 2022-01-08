e84d398a55d0076b0449af5f58edca260c77c9e6
riky moroni <riky5.moroni@gmail.com>
2022-01-08 20:47:12 +0000

    add isSpare, isLastFrame to game class and modify existing addRolls method

--- test/game.test.js
+++ test/game.test.js
@@ -18,12 +18,27 @@ describe('Game class', () => {
       game.addRolls(1, 4)
       expect(game.frames).toEqual([[1, 4]])
     })
-    
-    it('does not add the rolls and logs Sum of the rolls cannot exceed 10', () => {
-      const consoleSpy = jest.spyOn(console, 'log');
-      game.addRolls(5,6)
-      expect(consoleSpy).toHaveBeenCalledWith('Sum of the rolls cannot exceed 10')
+
+    it('does not add the rolls and throws error Sum of the rolls cannot exceed 10', () => {
+      expect(() => game.addRolls(5,6)).toThrow('Sum of the rolls cannot exceed 10')
       expect(game.frames).toEqual([])
+    })
+
+    it('add all rolls including a bonus roll if it is the last frame and a spare is scored', () => {
+      for (let i=0; i < 9; i++) {
+        game.addRolls(4,2)
+      }
+      game.addRolls(2, 8, 6)
+      expect(game.frames.at(-1)).toEqual([2, 8, 6])
+    })
+  })
+
+  describe('isSpare', () => {
+    it('should return true', () => {
+      expect(game.isSpare([6,4])).toEqual(true)
+    })
+    it('should return false', () => {
+      expect(game.isSpare([3,1])).toEqual(false)
     })
   })
 })

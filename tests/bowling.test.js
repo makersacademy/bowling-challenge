ef7641b8e6751418003bb13ccbf381c0b0aef729
Francesco <francescocollotto@gmail.com>
2022-01-08 12:17:17 +0000

    initialise bowling class with this.rolls

--- tests/bowling.test.js
+++ tests/bowling.test.js
@@ -1,7 +1,9 @@
 const Bowling = require('../bowling');
 
 describe('Bowling', () => {
-  it('', () => {
-    
+  it('can be initialised with a complete rolls scores', () => {
+    const finishedBowling = new Bowling([1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]);
+    expect(finishedBowling.rolls).toEqual([1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]);
   })
+
 })

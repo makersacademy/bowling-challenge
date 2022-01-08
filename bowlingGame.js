87aa109f2d9e660e9fc1313b42dd5f4871ef66a4
Chris Clement <chrisclement@Chriss-Air.connect>
2022-01-08 10:24:31 +0000

    Added frame variable with methods which change the frame based on turn

--- bowlingGame.js
+++ bowlingGame.js
@@ -2,20 +2,30 @@ class Bowling {
   constructor() {
     this.totalScore = 0;
     this.turn = 1;
+    this.frame = 1;
   }
   getTotalScore() {
     return this.totalScore;
   }
   roll(pins) {
     this.totalScore += pins;
-    this.changeTurn()
+    this.changeTurn();
   }
   getTurn() {
     return this.turn;
   }
   changeTurn() {
     if(this.turn === 1) { this.turn = 2}
-    else if(this.turn === 2) { this.turn = 1}
+    else if(this.turn === 2) { 
+      this.turn = 1
+      this.changeFrame()
+    };
+  }
+  changeFrame() {
+    this.frame += 1
+  }
+  getFrame() {
+    return this.frame;
   }
 }
 
--- testing/bowlingGame.test.js
+++ testing/bowlingGame.test.js
@@ -39,4 +39,18 @@ describe('class Bowling', () => {
       expect(bowling.getTurn()).toEqual(1);
     })
   })
+  describe('frame', () => {
+    it('start at 1', () => {
+      expect(bowling.getFrame()).toEqual(1);
+    })
+    it('stays at 1 after 1 roll if no strike is thrown', () => {
+      bowling.roll(5);
+      expect(bowling.getFrame()).toEqual(1);
+    })
+    it('changes to 2 after 2 rolls if no strike is thrown', () => {
+      bowling.roll(5);
+      bowling.roll(2);
+      expect(bowling.getFrame()).toEqual(2);
+    })
+  })
 })

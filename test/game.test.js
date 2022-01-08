2e11a3a4d1e5dab4fb9ae4572f576a69e79890d3
riky moroni <riky5.moroni@gmail.com>
2022-01-08 17:29:31 +0000

    add a function maxTen

--- test/game.test.js
+++ test/game.test.js
@@ -1,23 +1,28 @@
-const Game = require('../lib/game');
+const Game = require('../lib/game')
 
 describe('Game class', () => {
-
   beforeEach(() => {
-    game = new Game;
-  });
+    game = new Game()
+  })
 
   it('create an instance of Game', () => {
-    expect(game).toBeInstanceOf(Game);
-  });
+    expect(game).toBeInstanceOf(Game)
+  })
 
   it('initialize with an empty array', () => {
-    expect(game.frames).toEqual([]);
+    expect(game.frames).toEqual([])
   })
 
   describe('addRolls', () => {
     it('add the rolls to the frames array', () => {
-      game.addRolls(1, 4);
+      game.addRolls(1, 4)
       expect(game.frames).toEqual([[1, 4]])
     })
   })
-});
+
+  describe('maxTen', () => {
+    it('log sum of rolls is over 10', () => {
+      expect(game.maxTen(6, 5)).toEqual('Sum of the rolls cannot exceed 10')
+    })
+  })
+})

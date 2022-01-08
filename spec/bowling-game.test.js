4b194d17be57502b648abec131fcd0d7d85db227
zidehen <zoeidehen@gmail.com>
2022-01-08 14:20:13 +0000

    first test

--- spec/bowling-game.test.js
+++ spec/bowling-game.test.js
@@ -1,1 +1,7 @@
+const BowlingGame = require('../lib/bowling-game')
 
+describe( 'Bowling Game', () => {
+  it('creates a new game', () => {
+    expect(new BowlingGame()).toBeInstanceOf(BowlingGame)
+  })
+})

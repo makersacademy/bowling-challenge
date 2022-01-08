ae5cbb53968590e58cf9b444719c6cc1365a06f8
zidehen <zoeidehen@gmail.com>
2022-01-08 15:26:39 +0000

    implemented roll(pins) function and refactored tests

--- spec/bowling-game.test.js
+++ spec/bowling-game.test.js
@@ -1,7 +1,16 @@
 const BowlingGame = require('../lib/bowling-game')
 
 describe( 'Bowling Game', () => {
-  it('creates a new game', () => {
-    expect(new BowlingGame()).toBeInstanceOf(BowlingGame)
+
+  let game;
+
+  beforeEach(() => { game = new BowlingGame() })
+
+  describe('.roll', () => {
+    it('it can roll and knock down 10 pins', () => {
+      game.roll(10)
+      expect(game.rolls).toContain(10);
+    })
   })
+
 })

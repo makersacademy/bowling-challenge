44c41418c3cf73d1649a261e00cde8bda61aa977
Francesco <francescocollotto@gmail.com>
2022-01-08 12:28:28 +0000

    add roll method to Bowling class

--- bowling.js
+++ bowling.js
@@ -1,6 +1,12 @@
 class Bowling {
   constructor(rolls = []) {
     this.rolls = rolls;
+    this.frame = 0;
+  }
+
+  roll(frameRolls) {
+    this.rolls.push(...frameRolls);
+    this.frame = 1;
   }
 }
 
--- tests/bowling.test.js
+++ tests/bowling.test.js
@@ -5,5 +5,10 @@ describe('Bowling', () => {
     const finishedBowling = new Bowling([1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]);
     expect(finishedBowling.rolls).toEqual([1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6]);
   })
-
+  
+  it('add rolls', () => {
+    const bowling = new Bowling();
+    bowling.roll([8, 1]);
+    expect(bowling.rolls[bowling.rolls.length - 1]).toBe(1);
+  })
 })

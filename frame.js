3e397a6da39d691ea29efce1f003e7868a3dcfdd
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-08 20:48:57 +0000

    basic frame class written

--- frame.js
+++ frame.js
@@ -1,5 +1,41 @@
 class Frame {
 
+  constructor() {
+    this.rolls = [];
+    this.maxRolls = 2;
+  }
+
+  roll(pins) {
+    this.rolls.push(pins);
+  }
+
+  getRolls() {
+    return [...this.rolls];
+  }
+
+  getTotal() {
+    return this.getRolls().reduce((previousValue, currentValue) => previousValue + currentValue);
+  }
+
+  isStrike() {
+    return this.getRolls()[0] === 10;
+  }
+
+  isSpare() {
+    return this.getTotal() === 10 && this.numberOfRolls() === this.maxRolls;
+  }
+
+  numberOfRolls() {
+    return this.getRolls().length;
+  }
+
+  isComplete() {
+    return this.numberOfRolls() === this.maxRolls || this.isStrike();
+  }
+
 }
+
+frame = new Frame();
+
 
 module.exports = Frame;
--- frame.test.js
+++ frame.test.js
@@ -1,7 +1,60 @@
+const { hasUncaughtExceptionCaptureCallback } = require('process');
 const Frame = require('./frame')
 
-describe("Frame", () => {
-  it("", () => {
+let frame = new Frame();
 
+describe("Frame", () => {
+
+  describe("#roll", () => {
+    it("should be able to record a roll", () => {
+      frame.roll(1);
+      expect(frame.getRolls()[0]).toBe(1);
+    })
+    it("should be able to record two rolls", () => {
+      frame.roll(2);
+      expect(frame.getRolls()[1]).toBe(2);
+    })
+  })
+
+  describe("#getTotal", () => {
+    it("should be able to calculate the sum of all the rolls", () => {
+      expect(frame.getTotal()).toBe(3);
+    })
+  })
+
+  describe("#isStrike", () => {
+    it("should be able to know if it is a strike", () => {
+      frame = new Frame();
+      frame.roll(10);
+      expect(frame.isStrike()).toBe(true);
+    })
+  })
+
+  describe("#isSpare", () => {
+    it("should be able to know if it is a spare", () => {
+      frame = new Frame();
+      frame.roll(1);
+      frame.roll(9);
+      expect(frame.isSpare()).toBe(true);
+    })
+    it("should not identify a strike as a spare", () => {
+      frame = new Frame();
+      frame.roll(10);
+      expect(frame.isSpare()).toBe(false);
+    })
+  })
+
+  describe("#isComplete", () => {
+    it("should know when it is complete (vanilla frame)", () => {
+      frame = new Frame();
+      frame.roll(2);
+      frame.roll(3);
+      expect(frame.isComplete()).toBe(true);
+    })
+    it("should know when it is complete (strike)", () => {
+      frame = new Frame();
+      frame.roll(10);
+      expect(frame.isComplete()).toBe(true);
+    })
   })
 })

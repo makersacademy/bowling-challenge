398e6a52471a633aaa3ea1321651c8cea034692a
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-01-08 12:28:40 +0100

    added basic implementation of adding frames to scorecard

--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -16,6 +16,38 @@ describe('Scorecard', () => {
       scorecard.addRoll(2)
       expect(scorecard._frame).toEqual([2]);
     })
+
+    test('if strike rolled frame added to framesArray', () => {
+      scorecard.addRoll(10)
+      expect(scorecard.framesArray.length).toEqual(1);
+      expect(scorecard.framesArray[0]).toEqual([10]);  
+      expect(scorecard._frame).toEqual([]);
+    })
+
+    test('if frame is 2 rolls long it is added to framesArray', () => {
+      scorecard.addRoll(2)
+      scorecard.addRoll(4)
+      expect(scorecard.framesArray.length).toEqual(1);
+      expect(scorecard.framesArray[0]).toEqual([2,4]); 
+      expect(scorecard._frame).toEqual([]);
+    });
+
+    test('if frame is 1 roll long and not strike it is not added', () => {
+      scorecard.addRoll(2)
+      expect(scorecard._frame).toEqual([2]);
+      expect(scorecard.framesArray.length).toEqual(0);
+    });
+
+    test('adding multiple frames', () => {
+      for (let i = 0; i < 5; i++) {
+        scorecard.addRoll(10);
+      }
+      for (let i = 0; i < 4; i++) {
+        scorecard.addRoll(5);
+        scorecard.addRoll(5);
+      }
+      expect(scorecard.framesArray.length).toEqual(9);
+    })
   });
 
 });

58d92b9a93903b730c49f8da931b3f47be47f5cf
Hereloss <Hereloss@googlemail.com>
2022-01-09 21:52:44 +0000

    Cli interface added, which shows rolls each turn and total score at the end

--- cli.js
+++ cli.js
@@ -8,15 +8,28 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+ function bowlingRoll(){
+  return new Promise(resolve => {
+    rl.question('Enter a roll: ', (answer) => {
+      if (parseInt(answer) - bowling.remainder() <= 0 ) {
+        bowling.myRoll(parseInt(answer))
+        console.log(`Current bowls:`)
+        bowling.rollsIveDone()
+        if (bowling.roll > 3 || bowling.turn >= 11)
+    {       console.log('The game is over! No more rolls')
+            console.log(`Your final score is: ${bowling.totalScore()}`)}
+      } 
+      else
+      console.log('Too high of a score or not a valid input!')
+      resolve(answer);
+    });
+  });
+}
 
-rl.question('Enter score ', (answer) => {
- if (typeof answer === 'number') {
-   if (answer - bowling.remainder > 0 ) {
-    bowling.roll(number)
-   } 
-   else
-   console.log('Too high of a score!')
- }
- else
- {console.log('Not a valid input!')}
-})
+async function run() {
+    while (true) {
+      await bowlingRoll();
+    }
+}
+
+run()

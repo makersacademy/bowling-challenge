963baa1ecdb8a4ae72101e4ad23f307f85687002
Hereloss <Hereloss@googlemail.com>
2022-01-09 20:10:56 +0000

    Adding up scores works correctly

--- test/bowling.test.js
+++ test/bowling.test.js
@@ -27,4 +27,25 @@ describe('Bowling class', () => {
     bowling.myRoll(3)
     expect(bowling.myRoll(5)).toEqual({'1' : [6, 3], '2' : [5]});
   })
+  it('Will add up the scores when asked to', () => {
+    bowling = new Bowling()
+    bowling.myRoll(6)
+    bowling.myRoll(3)
+    bowling.myRoll(3)
+    expect(bowling.calculateBasic()).toEqual(12);
+  })
+
+  it('Will add up strikes and spares correctly', () => {
+    bowling = new Bowling()
+    bowling.myRoll(10)
+    bowling.myRoll(3)
+    expect(bowling.calculateStrikes()).toEqual(3)
+    bowling2 = new Bowling()
+    bowling2.myRoll(7)
+    bowling2.myRoll(3)
+    bowling2.myRoll(4)
+    expect(bowling2.calculateStrikes()).toEqual(4)
+  })
+
+  // it('Can correctly add up a game')
 })

2f84250509f2d5a366c4b51019ebe901e503cd43
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-01-09 21:16:12 +0000

    create methods addStrikeBonus and addSpareBonus to refactor calcBonus method

--- bowlingScore.js
+++ bowlingScore.js
@@ -12,7 +12,7 @@ class BowlingScore {
     this.scorecard.push(frame_score);
   }
   getScorecard() {
-    return this.scorecard
+    return this.scorecard;
   }
   addToTotal() {
     this.totalScore = 0;
@@ -28,19 +28,26 @@ class BowlingScore {
     if (frames.length >= 2) {
       for(let i = 0; i < frames.length - 1; i++) {
         if(this.checkStrike(frames, i)) { 
-          if( frames[i + 1][0] === 10) { this.bonus += frames[i + 1][0] + frames[i + 2][0] }
-          else { this.bonus += frames[i + 1][0] + frames[i + 1][1] }
+          if(this.checkStrike(frames, i + 1)) { this.addStrikeBonus(frames, i, true) }
+          else { this.addStrikeBonus(frames, i, false) }
         }
-        else if(this.checkSpare(frames, i)) { this.bonus += frames[i + 1][0]}
+        else if(this.checkSpare(frames, i)) { this.addSpareBonus(frames, i + 1) }
       }
     }
     return(this.bonus)
   }
   checkStrike(frames, frameIndex) {
-    return frames[frameIndex][0] === 10
+    return frames[frameIndex][0] === 10;
+  }
+  addStrikeBonus(frames, frameIndex, doubleStrike) {
+    if(doubleStrike === true) { this.bonus += frames[frameIndex + 1][0] + frames[frameIndex + 2][0] }
+    else { this.bonus += frames[frameIndex + 1][0] + frames[frameIndex + 1][1] }
   }
   checkSpare(frames, frameIndex) {
-    return frames[frameIndex][0] + frames[frameIndex][1] === 10
+    return frames[frameIndex][0] + frames[frameIndex][1] === 10;
+  }
+  addSpareBonus (frames, frameIndex) {
+    this.bonus += frames[frameIndex][0];
   }
 }
 

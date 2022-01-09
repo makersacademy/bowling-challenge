841439d02433507ed10747436cbfdded586687f8
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-09 21:47:36 +0000

    added creating and logging frame functionality

--- game.js
+++ game.js
@@ -1,4 +1,36 @@
+const Frame = require('./frame')
+
 class Game {
+
+  constructor(frameClass = Frame) {
+    this.frameClass = frameClass;
+    this.currentFrame = new frameClass();
+    this.frames = [];
+  }
+
+  inputRoll(pins) {
+    this.currentFrame.roll(pins);
+    if (this.currentFrame.isComplete()) {
+      this.nextFrame();
+    }
+  }
+
+  newFrame() {
+    this.currentFrame = new this.frameClass();
+  }
+
+  logFrame() {
+    this.frames.push(this.currentFrame);
+  }
+
+  nextFrame() {
+    this.logFrame();
+    this.newFrame();
+  }
+
+  getFrames() {
+    return [...this.frames];
+  }
 
 }
 
--- game.test.js
+++ game.test.js
@@ -1,8 +1,42 @@
 const Game = require('./game')
 
 describe("Game", () => {
-  describe("#", () => {
-    it("", () => {
+
+  describe("#inputRoll", () => {
+    it("user can input one roll", () => {
+      game = new Game();
+      game.inputRoll(1);
+      expect(game.currentFrame.getRolls()[0]).toBe(1);
+    })
+    it("user can input another roll", () => {
+      game = new Game();
+      game.inputRoll(1);
+      game.inputRoll(2);
+      expect(game.getFrames()[0].getRolls()[1]).toBe(2);
+    })
+  })
+
+  describe("#newFrame", () => {
+    it("can start a new frame", () => {
+      game = new Game();
+      game.inputRoll(1);
+      game.newFrame();
+      expect(game.currentFrame.getRolls()).toEqual([]);
+    })
+  })
+
+  describe("#nextFrame", () => {
+    it("can log a frame and start the next", () => {
+      game = new Game();
+      game.inputRoll(1);
+      game.inputRoll(2);
+      expect(game.currentFrame.getRolls()).toEqual([]);
+      expect(game.getFrames()[0].getRolls()).toEqual([1,2]);
+    })
+  })
+
+  describe("#calculateScore", () => {
+    it("can play one vanilla frame and score correctly", () => {
 
     })
   })

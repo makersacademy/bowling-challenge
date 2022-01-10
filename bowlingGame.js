a4d5a461388df906b0f93cb244711e78344fa892
EviePalaiochorinou <evangeliapal@gmail.com>
2022-01-10 00:26:33 +0000

    Finish with basic game features. Getting user input properly is work in progress

--- bowlingGame.js
+++ bowlingGame.js
@@ -23,27 +23,49 @@ class BowlingGame {
   }
 
   play() {
-    for (let i = 0; i<10; i++) {
-      const currentFrame = this.playFrame();
-      if (this.frames.at(-1).isSpare) {
-        this.frames.at(-1).frameScores.push(currentFrame.frameScores[0]);
-      } else if (this.frames.at(-1).isStrike) {
-
-      }
-
+    for (let i = 1; i<=10; i++) {
+      const currentFrame = this.playFrame(i);
       this.frames.push(currentFrame);
+      if (this.frames.at(-1).isSpare) {
+        this.frames.at(-2).frameScores.push(this.frames.at(-1)[0]);
+      }
+      let frameIndex = this.frames.length-2;
+      while (frameIndex > 0 || this.frames.at(frameIndex).isStrike) {
+        this.frames.at(frameIndex).frameScores.push(this.frames.at(frameIndex+1).getFrameScoresSum);
+        frameIndex--;
+      }
     }
   }
 
+  totalScore() {
+    let sum = 0;
+    this.frames.forEach(frame => {
+      sum += frame.getFrameScoresSum
+    });
+    return sum;
+  }
   getRollScore() {
-    return prompt();
+    const prompt = require('prompt');
+    prompt.start()
+    prompt.get(['input'], function (err, result) {
+      if (err) {
+        return err;
+      }
+      return result.input;
+    });
   }
 
-  playFrame() {
+  playFrame(round) {
+    let rolls = 2;
+    if (round === 10) {
+      rolls = 3
+    }
     const currentFrame = new Frame();
-    for (let i = 0; i<2; i++) {
+    for (let i = 0; i<rolls; i++) {
       currentFrame.addRollScore(this.getRollScore());
-      if (currentFrame.isStrike()) {
+      if (currentFrame.isStrike() && round != 10) {
+        break;
+      } else if (round === 10 && i === 3 && (!currentFrame.isStrike || !currentFrame.isSpare) ) {
         break;
       }
     }
@@ -53,3 +75,7 @@ class BowlingGame {
 
 
 module.exports = BowlingGame;
+
+let bowlingGame = new BowlingGame;
+bowlingGame.play();
+bowlingGame.sum();

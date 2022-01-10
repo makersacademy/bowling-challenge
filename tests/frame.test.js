a863d31927f561d73c1afd4f4313ca5f14128bfd
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2022-01-10 10:58:41 +0000

    Add test for perfect game

--- tests/frame.test.js
+++ tests/frame.test.js
@@ -1,30 +1,30 @@
-const Frame = require('../frame')
+const Frame = require('../frame');
 
 describe('Frame', () => {
   const frame = new Frame();
 
   it('records a roll', () => {
-    frame.addRoll(1)
+    frame.addRoll(1);
     expect(frame.rolls[0]).toEqual(1);
-  })
+  });
 
   it('returns the frame score', () => {
     expect(frame.totalScore()).toEqual(1);
-  })
+  });
 
   it('adds bonuses to the frame score', () => {
     frame.bonus = 1;
     expect(frame.totalScore()).toEqual(2);
-  })
+  });
 
   it('sets frame as spare', () => {
     frame.addRoll(9);
     expect(frame.isSpare()).toEqual(true);
-  })
+  });
 
   it('sets frame as strike', () => {
     const strikeFrame = new Frame();
     strikeFrame.addRoll(10);
     expect(strikeFrame.isStrike()).toEqual(true);
-  })
-})
+  });
+});
--- tests/game.test.js
+++ tests/game.test.js
@@ -5,29 +5,37 @@ describe('Game', () => {
 
   it('plays 10 frames', () => {
     expect(game.frames.length).toEqual(10);
-  })
+  });
 
   it('can record a roll', () => {
     game.roll(1);
+    expect(game.score()).toEqual(1);
   });
 
   it('returns the total score', () => {
-    game.roll(1)
+    game.roll(1);
     expect(game.score()).toEqual(2);
-  })
+  });
 
   it('sets spare bonus', () => {
     game.roll(1);
     game.roll(9);
     game.roll(1);
     game.roll(1);
-    expect(game.score()).toEqual(15)
-  })
+    expect(game.score()).toEqual(15);
+  });
 
   it('sets strike bonus', () => {
     game.roll(10);
     game.roll(1);
     game.roll(1);
-    expect(game.score()).toEqual(29)
-  })
+    expect(game.score()).toEqual(29);
+  });
+
+  it('can record a perfect game', () => {
+    for (let i = 0; i < 12; i++) {
+      game.roll(10);
+    }
+    expect(game.score()).toEqual(300);
+  });
 });

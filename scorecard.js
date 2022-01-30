b009f4b4cea9626942422c92991e977cdcde5454
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-01-30 13:57:25 +0000

    additional scorecard tests added to make sure the final frame variations are calculated correctly

--- scorecard.js
+++ scorecard.js
@@ -2,7 +2,7 @@ const Frame = require('./frame');
 
 class Scorecard {
   constructor() {
-    this.frames = [] // each frame will be pushed to this array
+    this.frames = []
   }
 
   input_frame(rolls) { //rolls will be e.g. [2,3]
@@ -11,24 +11,11 @@ class Scorecard {
   }
 
   score() {
-    // this.frames.reduce((total, frame, index, frames) => {total += frame.score(frames[index+1], frames[index+2])
-    // });
     let total = 0; 
-  
     this.frames.map((frame, index, frames) => {
-      // console.log(total)
-      // console.log(index) 
-      // console.log(frame)
       total += frame.score(frames[index+1], frames[index+2])
     });
     return total; 
   }
 } 
 module.exports = Scorecard;
-
-// syntax of reduce((previousValue, currentValue, currentIndex, array) => { /* ... */ }, initialValue)
-    // reducer = (score, frame, index, frames) => {
-    //   return score + frame.score([frames[index+1], frames[index+2]])
-    // }
-    
-    // this.frames.reduce(reducer, 0);
--- scorecard.test.js
+++ scorecard.test.js
@@ -11,9 +11,43 @@ describe('Scorecard', () => {
       scorecard.input_frame([10]);
     }
     scorecard.input_frame([10,10,10]);
-    //console.log(scorecard.frames);
-    
     expect(scorecard.score()).toBe(300);
+  });
+
+  it('can calculate a gutter game', () => {
+    for(i = 1; i < 11 ; i++) {
+      scorecard.input_frame([0]);
+    }
+    expect(scorecard.score()).toBe(0);
+  });
+
+  it('can calculate a game with spare in final frame', () => {
+    for(i = 1; i < 10 ; i++) {
+      scorecard.input_frame([10]);
+    }
+    scorecard.input_frame([5,5,5]);
+
+    expect(scorecard.score()).toBe(270);
+  });
+
+  it('can calculate a game with spare in penultimate frame and strike in final frame', () => {
+    for(i = 1; i < 9 ; i++) {
+      scorecard.input_frame([10]);
+    }
+    scorecard.input_frame([5,5]);
+    scorecard.input_frame([10,10,2]);
+    //console.log(scorecard.frames);
+    expect(scorecard.score()).toBe(267);
+  });
+
+  it('can calculate a game with spare in penultimate frame and spare in final frame', () => {
+    for(i = 1; i < 9 ; i++) {
+      scorecard.input_frame([10]);
+    }
+    scorecard.input_frame([5,5]);
+    scorecard.input_frame([5,5,2]);
+    //console.log(scorecard.frames);
+    expect(scorecard.score()).toBe(252);
   });
 
   it('can calculate a no bonus game', () => {

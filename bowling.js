22722a21c8de98d6427d311e885c9c91f508892a
Cristian <cristian.christensen@gmail.com>
2022-01-30 19:16:56 +0100

    Basic implementation to pass the test for a gutter game

--- bowling.js
+++ bowling.js
@@ -1,5 +1,12 @@
 class Bowling {
 
+  roll() {
+
+  }
+
+  get score() {
+    return 0;
+  }
 }
 
 module.exports = Bowling;
--- bowling.test.js
+++ bowling.test.js
@@ -1,9 +1,22 @@
 const Bowling = require('./bowling.js');
 
+let bowling;
+
 describe("Bowling", () => {
+
+beforeEach(() => {
+    bowling = new Bowling;
+  })
+
   it("is an instance of Bowling class", () => {
-    const bowling = new Bowling;
     expect(bowling).toBeInstanceOf(Bowling);
+  });
+
+  it('score should be 0 for a gutter game', () => {
+    for (let i = 0; i < 20; i++) {
+      bowling.roll(0);
+    }
+    expect(bowling.score).toEqual(0);
   });
 
 });

810608517a906b7458f877ab524cb4f8ebf27812
izaowl <izasow@yahoo.com>
2022-01-30 21:09:03 +0000

    Applied eslint to tidy up my code

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,43 +1,29 @@
-const R = require('ramda');
-class BowlingGame{
-    constructor(){
-        this.rolls = [];
-        // this.score = score;
+class BowlingGame {
+  constructor () {
+    this.rolls = []
+  }
+
+  getRolls () {
+    return this.rolls
+  }
+
+  roll (roll) {
+    this.rolls += roll
+  }
+
+  getScore () {
+    let res = 0
+    let rollIndex = 0
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+      const frameScore = (parseInt(this.rolls[rollIndex]) + parseInt(this.rolls[rollIndex + 1]))
+      if (frameScore === 10) {
+        res += 10 + parseInt(this.rolls[rollIndex + 2])
+      } else {
+        res += frameScore
+      }
+      rollIndex += 2
     }
-
-    getRolls(){
-        return this.rolls;
-    }
-
-    roll(roll){
-        this.rolls += roll;
-    }
-
-    getScore(){
-        let res = 0;
-        let rollIndex = 0;
-        
-        for (let frameIndex=0; frameIndex<10; frameIndex++){
-            let frameScore = (parseInt(this.rolls[rollIndex]) + parseInt(this.rolls[rollIndex+1]));
-            
-
-            if(frameScore ===10){
-              res += 10 + parseInt(this.rolls[rollIndex+2]);
-            }
-            else if (frameScore ===20){
-                return res === 300;
-                abort = true;
-            }
-
-            else{
-              res += frameScore;
-            }
-            rollIndex+=2;
-        }
-        return res;
-    }
-    
-
+    return res
+  }
 }
-
-module.exports = BowlingGame;
+module.exports = BowlingGame

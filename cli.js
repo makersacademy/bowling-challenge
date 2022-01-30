2348b4cb6007a34eda8843f5a16e24f235b0623d
icvotria <icvotria@gmail.com>
2022-01-30 20:12:27 +0000

    refactoring cli

--- cli.js
+++ cli.js
@@ -11,21 +11,21 @@ function letsGoBowling() {
   rl.question(`Frame ${scorecard.frameNumber() + 1}. How many pins did you knock down on roll 1? `, (answer1) => {
     switch(answer1) {
       case '10':
-        console.log(`   _____ _        _ _        _ \r\n  \/ ____| |      (_) |      | |\r\n | (___ | |_ _ __ _| | _____| |\r\n  \\___ \\| __| \'__| | |\/ \/ _ \\ |\r\n  ____) | |_| |  | |   <  __\/_|\r\n |_____\/ \\__|_|  |_|_|\\_\\___(_)\r\n`);
-        scorecard.roll(parseInt(answer1));
+        strikeDisplay();
+        enterRoll(answer1);
         letsGoBowling();
         break;
       default:
         frameScore += parseInt(answer1);
-        scorecard.roll(parseInt(answer1));
+        enterRoll(answer1);
         rl.question('And how many did you knock down on roll 2? ', (answer2) => {
           frameScore += parseInt(answer2)
           if(frameScore === 10) {
             console.log('Woo, a spare!')
-            scorecard.roll(parseInt(answer2));
+            enterRoll(answer2);
             letsGoBowling();
           } else {
-            scorecard.roll(parseInt(answer2));
+            enterRoll(answer2);
             score = scorecard.calculateScore();
             console.log(`Your current score is ${score}`)
             letsGoBowling();
@@ -35,6 +35,16 @@ function letsGoBowling() {
       }
   })
 }
+
+function enterRoll(answer) {
+  scorecard.roll(parseInt(answer));
+}
+
+function strikeDisplay() {
+  console.log(`   _____ _        _ _        _ \r\n  \/ ____| |      (_) |      | |\r\n | (___ | |_ _ __ _| | _____| |\r\n  \\___ \\| __| \'__| | |\/ \/ _ \\ |\r\n  ____) | |_| |  | |   <  __\/_|\r\n |_____\/ \\__|_|  |_|_|\\_\\___(_)\r\n`);
+}
+
+
 
 console.log("  _          _   _        _____         ____                _ _             _ \r\n | |        | | ( )      \/ ____|       |  _ \\              | (_)           | |\r\n | |     ___| |_|\/ ___  | |  __  ___   | |_) | _____      _| |_ _ __   __ _| |\r\n | |    \/ _ \\ __| \/ __| | | |_ |\/ _ \\  |  _ < \/ _ \\ \\ \/\\ \/ \/ | | \'_ \\ \/ _` | |\r\n | |___|  __\/ |_  \\__ \\ | |__| | (_) | | |_) | (_) \\ V  V \/| | | | | | (_| |_|\r\n |______\\___|\\__| |___\/  \\_____|\\___\/  |____\/ \\___\/ \\_\/\\_\/ |_|_|_| |_|\\__, (_)\r\n                                                                       __\/ |  \r\n                                                                      |___\/   ");
 console.log(`         .-.\r\n      .-.\\ \/    .-.\r\n      \\ \/|=|    \\ \/\r\n      |=|   \\   |=|\r\n     \/   \\ ---.\/   \\\r\n     |   \/ ..  \\   |\r\n     |  |#  \'   |  |\r\n      \'._\\     \/_.\'\r\n          \'---\'\r\n`);

bce799d05d1612daccb02b1374d24bc000170ddf
icvotria <icvotria@gmail.com>
2022-01-30 12:24:43 +0000

    refactoring, can return score at any point in the game

--- scorecard.js
+++ scorecard.js
@@ -7,23 +7,80 @@ class Scorecard {
     this.scores.push(pins);
   }
 
+  #getScores() {
+    return this.scores;
+  }
+
   calculateScore() {
     let totalScore = 0;
-    let rollNumber = 0
-    for(var i = 0; i < 10; i++) {
-      if (this.scores[rollNumber] === 10) { 
-        totalScore += 10 + this.scores[rollNumber + 1] + this.scores[rollNumber + 2];
-        rollNumber++
-      } else if (this.scores[rollNumber] + this.scores[rollNumber + 1] === 10) {
-        totalScore += 10 + this.scores[rollNumber + 2];
+    let rollNumber = 0;
+
+    for(let i = 0; i < this.#frameNumber(); i++) {
+      if(this.#isStrike(rollNumber)) { 
+        totalScore += this.#strikeScore(rollNumber)
+        rollNumber++;
+      } else if (this.#isSpare(rollNumber)) {
+        totalScore += this.#spareScore(rollNumber);
         rollNumber += 2
       } else {
-        totalScore += this.scores[rollNumber] + this.scores[rollNumber + 1];
+        totalScore += this.#normalScore(rollNumber);
         rollNumber += 2
       }
     }
     return totalScore;
   }
+
+  #frameNumber() {
+    if(this.#getScores().length + this.#strikeCount() > 20) {
+      return 10;
+    } else {
+      return (this.#getScores().length + this.#strikeCount())/2;
+    }
+  }
+
+  #strikeCount() {
+    return this.#getScores().filter(roll => roll === 10).length;
+  }
+
+  #isStrike(index) {
+    return this.scores[index] === 10;
+  }
+
+  #isSpare(index) {
+    return this.scores[index] + this.scores[index + 1] === 10;
+  }
+
+  #strikeScore(index) {
+    return 10 + this.scores[index + 1] + this.scores[index + 2];
+  }
+
+  #spareScore(index) {
+    return 10 + this.scores[index + 2];
+  }
+
+  #normalScore(index) {
+    return this.scores[index] + this.scores[index + 1];
+  }
 }
 
 module.exports = Scorecard;
+
+game = new Scorecard;
+game.roll(10);
+game.roll(10);
+game.roll(10);
+game.roll(10);
+game.roll(3);
+game.roll(4);
+game.roll(3);
+game.roll(4);
+game.roll(3);
+game.roll(4);
+game.roll(3);
+game.roll(4);
+game.roll(3);
+game.roll(4);
+game.roll(10);
+game.roll(3);
+game.roll(3);
+console.log(game.calculateScore());

9050d9a86f26e1187f841cf4be9c466adb6ce97c
jmodiale <remen.omodiale@outlook.com>
2022-01-30 19:47:45 +0000

    calc score

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,10 +1,29 @@
 class BowlingGame{
-    roll(){
+    constructor(){
+        this.rolls = []
+    }
 
+    roll(pins){
+        this.rolls.push(pins)
     }
 
     score(){
-        return 0;
+        let curr_score = 0;
+        let each_roll = 0;
+
+        for(let i=0; i<10; i++){
+            const frameScore = this.rolls[each_roll] + this.rolls[each_roll + 1];
+
+            if(frameScore === 10){
+                curr_score += 10 + this.rolls[each_roll + 2]
+            } else {
+                curr_score += frameScore
+            }
+            
+            each_roll += 2;
+            
+        }
+        return curr_score;
     }
 }
 
--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -1,12 +1,32 @@
 const BowlingGame = require('./bowlingGame');
 
+let game;
+beforeEach(() => {
+    game = new BowlingGame();
+})
+
+function rolls(pins, roll){
+    for(let i=0; i<roll; i++){
+        game.roll(pins)
+    }
+}
+
 describe('bowlingGame', () => {
     it('gutter game of zeros returns zero', () => {
-      const game = new BowlingGame();
-      for(let i=0; i<10; i++){
-          game.roll(0)
-      }
-
+      rolls(0, 20)
       expect(game.score()).toEqual(0)
+    });
+
+    it('returns 20 for a game of ones', () => {
+        rolls(1, 20)
+        expect(game.score()).toEqual(20)
+    });
+
+    it('returns score when spare is rolled', () => {
+        game.roll(5)
+        game.roll(5)
+        game.roll(3)
+        rolls(0, 17)
+        expect(game.score()).toEqual(16)
     });
 }); 

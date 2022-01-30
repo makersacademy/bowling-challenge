cabb53176e33a215e6f585ce5e4e654ed19a67bc
Neto <fpottinger.marques@gmail.com>
2022-01-30 09:46:13 +0000

    added jest coverage folder

--- scoreCard.js
+++ scoreCard.js
@@ -1,9 +1,12 @@
 class ScoreCard {
   constructor() {
     this.totalPoints = 0;
+    this.rolls = [];
   }
 
   addKnockedPins(pins) {
+    if (this.rolls.reduce((partialSum, a) => partialSum + a, 0) + pins > 10) throw "this is a 10 pins bowling game!";
+    this.rolls.push(pins);
     this.totalPoints += pins;
   }
 
--- scoreCard.test.js
+++ scoreCard.test.js
@@ -1,10 +1,25 @@
 const ScoreCard = require("./scoreCard.js");
 
 describe(".addKnockedPins", () => {
-  const scorecard = new ScoreCard;
+  const scorecard = new ScoreCard();
 
-  it("adds knocked pins information", () => {
+  it("adds knocked pins number", () => {
     scorecard.addKnockedPins(4);
     expect(scorecard.getTotalPoints()).toEqual(4);
+  });
+
+  it("adds maximum of 10 pins", () => {
+    expect(() => scorecard.addKnockedPins(11)).toThrow(
+      "this is a 10 pins bowling game!"
+    );
+  });
+
+  it("adds maximum of 10 pins across two throws or a frame", () => {
+    const anotherCard = new ScoreCard;
+
+    anotherCard.addKnockedPins(5);
+    expect(() => anotherCard.addKnockedPins(6)).toThrow(
+      "this is a 10 pins bowling game!"
+    );
   });
 });

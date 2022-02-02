a9b433cf2205b4723fcb20a9c15746cf06a17d79
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-02 11:48:23 +0000

    minor change to cli

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface({
 
 scorecard = new Scorecard;
 
-const recursiveAsyncReadLine = () => {
+const askUserIput = () => {
   rl.question(`How many pins did you roll in frame ${scorecard.frames.length + 1}? Please enter two numbers for your first and second rolls, separated by a comma e.g. 2,3 If you bowled a strike, simply enter 10.\n`, (answer) => {
   const rolls = answer.split(',');
   frame = rolls.map(roll => {
@@ -19,10 +19,10 @@ const recursiveAsyncReadLine = () => {
     return rl.close();
   }
   console.log('Thank you. Your rolls have been added to your scorecard. Your final score will be calculated after all 10 frames have been entered.\n');
-  recursiveAsyncReadLine();
+  askUserIput();
 });
 }
-recursiveAsyncReadLine();
+askUserIput();
 
 const confirmOutcome = () => {
   if (scorecard.score() == 300) {

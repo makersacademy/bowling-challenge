e1659e3b089c1d4712a87c02b9bb11a23e3dee6d
izaowl <izasow@yahoo.com>
2022-02-06 01:25:27 +0000

    All tests passing

--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -1,19 +1,19 @@
 const BowlingGame = require('./BowlingGame')
-const R = require('ramda')
+
 describe(BowlingGame, () => {
-  it('Can roll 0 score game', () => {
+  it('Can roll gutter game', () => {
     const bowlingGame = new BowlingGame()
-    R.call(R.times(() => {
+    for (let roll = 0; roll <20; roll ++){
       bowlingGame.roll(0)
-    }), 20)
+    }
     expect(bowlingGame.getScore()).toBe(0)
   });
 
-  it('Can roll gutter game', () => {
+  it('Can roll 20*1 game', () => {
     const bowlingGame = new BowlingGame()
-    R.call(R.times(() => {
+    for (let roll = 0; roll <20; roll ++){
       bowlingGame.roll(1)
-    }), 20)
+    }
     expect(bowlingGame.getScore()).toBe(20)
   })
   it('Can roll a spare', () => {
@@ -21,9 +21,9 @@ describe(BowlingGame, () => {
     bowlingGame.roll(5)
     bowlingGame.roll(5)
     bowlingGame.roll(3)
-    R.call(R.times(() => {
+    for (let roll = 0; roll <17; roll ++){
       bowlingGame.roll(0)
-    }), 17)
+    }
     expect(bowlingGame.getScore()).toBe(16)
   })
 
@@ -33,9 +33,9 @@ describe(BowlingGame, () => {
     bowlingGame.roll(5)
     bowlingGame.roll(3)
     
-    R.call(R.times(() => {
+    for (let roll = 0; roll <15; roll ++){
       bowlingGame.roll(0)
-    }), 15)
+    }
     bowlingGame.roll(5)
     bowlingGame.roll(5)
     bowlingGame.roll(3)
@@ -44,16 +44,16 @@ describe(BowlingGame, () => {
 
   it('Can roll all 5/s game', () => {
     const bowlingGame = new BowlingGame()
-    R.call(R.times(() => {
+    for (let roll = 0; roll <21; roll ++){
       bowlingGame.roll(5)
-    }), 21)
+    }
     expect(bowlingGame.getScore()).toBe(150)
   })
   it ('Can roll the Perfect Game',()=>{
     const bowlingGame = new BowlingGame;
-    R.call(R.times(() => {
-        bowlingGame.roll(10);
-    }), 12);
+    for (let roll = 0; roll <12; roll ++){
+      bowlingGame.roll(10)
+    }
     expect(bowlingGame.getScore()).toBe(300);
   });
 })

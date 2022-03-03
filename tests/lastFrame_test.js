d6976167cb75dda2ca2f3061d4265e317db8bbb0
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-03 22:32:23 +0000

    Refactor of Frame class, removed lots of unncessary code

---
+++ tests/lastFrame_test.js
@@ -0,0 +1,16 @@
+const LastFrame = require("../lib/lastFrame");
+
+// describe ("Last Frame class", () => {
+//   let lastFrame
+
+//   beforeEach(() => {
+//     lastFrame = new LastFrame();
+//   });
+
+//   it("the frame is completed", () => {
+//     lastFrame.logRoll(6);
+//     lastFrame.logRoll(3);
+//     expect(lastFrame.frameComplete()).toBe(true);
+//   });
+
+// })

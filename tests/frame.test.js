f8c6da2af09ca01f377d322dd464716adc517c00
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-03 20:36:59 +0000

    coverted more tests for Frame class

--- tests/frame.test.js
+++ tests/frame.test.js
@@ -12,4 +12,21 @@ describe("Frame class", () => {
     frame.logRoll(3);
     expect(frame.frameComplete()).toBe(true);
   });
+
+  it("frame is not completed", () => {
+    frame.logRoll(6);
+    expect(frame.frameComplete()).toBe(false);
+  });
+
+  it("frame is not completed", () => {
+    frame.logRoll(10);
+    expect(frame.frameComplete()).toBe(true);
+  });
+
+  it("sums a frame", () => {
+    frame.logRoll(3);
+    frame.logRoll(6);
+    expect(frame.sumFrame()).toBe(9);
+  });
+
 });

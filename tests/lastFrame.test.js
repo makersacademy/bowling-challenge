d429f59561e5ac4553896b75e8e8c999145dc26e
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-03 22:42:16 +0000

    fixed frameComplete on LastFrame

---
+++ tests/lastFrame.test.js
@@ -0,0 +1,28 @@
+const LastFrame = require("../lib/lastFrame");
+
+describe ("Last Frame class", () => {
+  let lastFrame
+
+  beforeEach(() => {
+    lastFrame = new LastFrame();
+  });
+
+  it("the frame is completed", () => {
+    lastFrame.logRoll(6);
+    lastFrame.logRoll(3);
+    expect(lastFrame.frameComplete()).toBe(true);
+  });
+
+  it("the frame is completed", () => {
+    lastFrame.logRoll(6);
+    lastFrame.logRoll(4);
+    lastFrame.logRoll(3);
+    expect(lastFrame.frameComplete()).toBe(true);
+  });
+
+  it("the frame is completed", () => {
+    lastFrame.logRoll(10);
+    expect(lastFrame.frameComplete()).toBe(false);
+  });
+
+})

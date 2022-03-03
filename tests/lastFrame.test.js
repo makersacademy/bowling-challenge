d9e570526a1981a6dabd759d83961f85e0918e65
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-03-03 22:47:51 +0000

    added further tests for LastFrame

--- tests/lastFrame.test.js
+++ tests/lastFrame.test.js
@@ -20,9 +20,23 @@ describe ("Last Frame class", () => {
     expect(lastFrame.frameComplete()).toBe(true);
   });
 
-  it("the frame is completed", () => {
+  it("the frame is not completed on a strike", () => {
     lastFrame.logRoll(10);
     expect(lastFrame.frameComplete()).toBe(false);
+  });
+
+  
+  it("the frame is not completed on a strike and following roll", () => {
+    lastFrame.logRoll(10);
+    lastFrame.logRoll(10);
+    expect(lastFrame.frameComplete()).toBe(false);
+  });
+
+  it("the frame is completed on 3 rolls and a strike", () => {
+    lastFrame.logRoll(10);
+    lastFrame.logRoll(10);
+    lastFrame.logRoll(10);
+    expect(lastFrame.frameComplete()).toBe(true);
   });
 
 })

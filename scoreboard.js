763ad3229187e201754698861ab009dabe14d88f
Ledders26 <benlhall584@hotmail.co.uk>
2022-03-05 01:42:46 +0000

    added scoreboard and 2 test

--- scoreboard.js
+++ scoreboard.js
@@ -1,7 +1,17 @@
-class Scorboard {
+class Scoreboard {
+  
   constructor() {
-    this.game_score = 0
+    this.gameScore = 0
+    this.throwScores = []
+  }
+
+  singleRoll(score) {
+    if(score > 10) {
+    throw new Error('Score recorded for throw 1 is invalid')
+    }
+    else
+    this.throwScores.push(score)
   }
 }
 
-module.exports = Scorboard
+module.exports = Scoreboard;
--- scoreboard.test.js
+++ scoreboard.test.js
@@ -1,10 +1,22 @@
 const Scoreboard = require('./scoreboard')
 
+let scoreboard;
+
+beforeEach(() => {
+  scoreboard = new Scoreboard;
+});
+
 describe (Scoreboard, () => {
+
   describe ('constructor', () => {
     it ('starts a game with a score of 0', () => {
-      let scoreboard = new Scoreboard;
-      expect(scoreboard.game_score).toBe (0);
+      expect(scoreboard.gameScore).toBe(0);
+    });
+  });
+
+  describe ('singleRoll', () => {
+    it ('should return an error if a single roll is greater than 10', () => {
+      expect(() => scoreboard.singleRoll(14)).toThrow('Score recorded for throw 1 is invalid');
     });
   });
 });

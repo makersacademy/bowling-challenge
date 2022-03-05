6983550f6a44ea09187a25fbc998fad29749a93e
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-03-05 10:38:43 +0000

    Made a default score of zero

---
+++ bowling.js
@@ -0,0 +1,13 @@
+
+class BowlingCard{
+  constructor(){
+    this.score = 0
+  }
+
+  getScore(){
+    return this.score
+  }
+}
+
+
+module.exports = BowlingCard
---
+++ bowling.test.js
@@ -0,0 +1,26 @@
+
+const BowlingCard = require('./bowling');
+
+describe(BowlingCard, () => {
+  describe('getScore', () => {
+    let bowlingCard = new BowlingCard
+    it('should have the default score as 0', () => {
+      expect(bowlingCard.getScore()).toBe(0)
+    });
+  });
+  
+});
+
+
+
+// A bowling game consists of 10 frames in which 
+// the player tries to knock down the 10 pins. 
+
+// In every frame the player can roll one or two times. 
+
+// The actual number depends on strikes and spares. 
+
+// The score of a frame is the number of knocked down pins plus 
+// bonuses for strikes and spares. 
+
+// After every frame the 10 pins are reset.

73824fe9df3eaf481d511ba8a334dad0698056a3
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-05 17:23:01 +0000

    Spare bonus added

--- bowling.js
+++ bowling.js
@@ -4,30 +4,81 @@ class Bowling{
         this.score = 0;
         this.frame_no = 1;
         this.frame = [];
-
-    };
-
-
-    frameDecider() {
-        let frame_score = this.frame[0] + this.frame[1];
-            this.score += frame_score;
-            this.frame_no++;
-            this.frame = [];
-
+        this.max_frame = 11
+        this.spare = false
+        this.strike = false
+        this.results = [];
     };
 
     newRoll(pins) {
-        this.frame.push(pins);
-        if(this.frame.length === 2) {
-            let frame_score = this.frame[0] + this.frame[1];
-            this.frameDecider();
+        if(this.frame_no === this.max_frame) {
+            return 'The game is over';
+        } else {
+            this.results.push(pins);
+            this.frameType();
         };
     };
-    
-    
+
+    frameType() {
+        if(this.spare === true){
+            this.spareBonusCalc(); 
+        } else {
+            this.strikeChecker();
+        };
+    };
+
+    strikeChecker() {
+        let last = (this.results.length - 1);
+        if(this.results[last] === 10){
+            this.strikeSwitch();
+        } else {
+            this.frame.push(this.results[last]);
+            this.frameCompleteChecker();
+        };
+    };
+
+    frameCompleteChecker() {
+        let flength = this.frame.length
+        if(flength === 2) {
+            this.spareChecker();
+        };
+    };
+
+    spareChecker() {
+        let frame_score = this.frame[0] + this.frame[1];
+        if( frame_score === 10 ){
+            this.spareSwitch();
+        } else {
+            this.score += frame_score;
+            this.frameFinisher();
+        };
+    };
+   
+    strikeSwitch() {
+        this.strike = true;
+        this.frameFinisher();
+    };
+
+    spareSwitch() {
+        this.spare = true;
+        this.frameFinisher();
+    };
+
+    spareBonusCalc() {
+        let spare_bonus = 10 + (this.results[this.results.length - 1]);
+        this.score += spare_bonus;
+        this.spare = false;
+        this.strikeChecker();
+    };
+
+    frameFinisher() {
+        this.frame_no++;
+        this.frame = [];
+    };
+
     getScore() {
         return this.score;
-    }
+    };
 
 };
 
--- bowling.test.js
+++ bowling.test.js
@@ -27,6 +27,47 @@ describe("Bowling", () => {
         game.newRoll(2);
         game.newRoll(3);
         game.newRoll(0);
+        console.log(game.results);
         expect(game.getScore()).toBe(6);
     });
+    
+    it('returns a string informing that the game had ended, after the 10th frames has been scored', () =>{
+        const game = new Bowling;
+        for(i = 0; i < 21; i++){ 
+            game.newRoll(0);
+        };
+        expect(game.frame_no).toEqual(11);
+        expect(game.newRoll(0)).toEqual('The game is over');
+    });
+
+    it('recognises a spare', () =>{
+        const game = new Bowling;
+        game.newRoll(5);
+        game.newRoll(5);
+        expect(game.spare).toEqual(true);
+    });
+
+    it('recognises a strike', () =>{
+        const game = new Bowling;
+        game.newRoll(10);
+        expect(game.strike).toEqual(true)
+    });
+
+    it('a strike only has one roll in frame', () => {
+        const game = new Bowling;
+        game.newRoll(10);
+        game.newRoll(1);
+        expect(game.frame_no).toEqual(2);
+        expect(game.frame).toEqual([1]);
+    });
+
+    it('it calculates the spare bonus', () => {
+        const game = new Bowling;
+        game.newRoll(5);
+        game.newRoll(5);
+        game.newRoll(5);
+        game.newRoll(0);
+        expect(game.getScore()).toEqual(20);
+    });
+
 });

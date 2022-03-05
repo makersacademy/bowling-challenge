2cbabdd73c4e1d64675bdc94effcf68d3638c34a
Ledders26 <benlhall584@hotmail.co.uk>
2022-03-05 22:11:00 +0000

    renames spare and strike with a starting is

--- frame.js
+++ frame.js
@@ -4,14 +4,14 @@ class Frame{
   this.currentFrame = 1;
   };
 
-  strike(frame) {
+  isStrike(frame) {
     if (frame.length === 1 && frame.reduce((a,b) => a + b, 0) === 10) {
       return true
     }
     return false
   };
 
-  spare(frame) {
+  isSpare(frame) {
     if (frame.length === 2 && frame.reduce((a,b) => a + b, 0) === 10) {
       return true
     }
--- frame.test.js
+++ frame.test.js
@@ -14,27 +14,27 @@ describe (Frame, () => {
     });
   });
 
-  describe ('strike', () => {
+  describe ('isStrike', () => {
     it ('returns true when score of ten is recorded with 1st roll', () => {
-      expect(frame.strike([10])).toBe(true);
+      expect(frame.isStrike([10])).toBe(true);
     });
 
     it ('returns false when a spare is passed', () => {
-      expect(frame.strike([8,2])).toBe(false);
+      expect(frame.isStrike([8,2])).toBe(false);
     });
   });
 
-  describe ('spare', () => {
+  describe ('isSpare', () => {
     it ('returns true when a spare is passed', () => {
-      expect(frame.spare([8,2])).toBe(true);
+      expect(frame.isSpare([8,2])).toBe(true);
     });
 
     it ('returns false when a strike is passed', () => {
-      expect(frame.spare([10])).toBe(false);
+      expect(frame.isSpare([10])).toBe(false);
     });
 
     it ('returns false when 2 scores not totalling ten are passed', () => {
-      expect(frame.spare([6,1])).toBe(false);
+      expect(frame.isSpare([6,1])).toBe(false);
     });
   });
 });

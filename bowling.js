ec8f364c71bbd986e2f44fcc568a7cc3b05f540e
Trushna Rabadia <rabadiatrushna@gmail.com>
2022-03-06 22:34:24 +0000

    spare added

--- bowling.js
+++ bowling.js
@@ -1,14 +1,29 @@
 class Bowling {
   constructor() {
-    this.scoreSoFar = 0;
+    this.rolls = [];
   };
 
   roll(pins) {
-    this.scoreSoFar += pins;
+    this.rolls.push(pins);
   };
 
   get score() {
-    return this.scoreSoFar;
+    let score = 0;
+    let rollIndex = 0;
+
+    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
+      const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+
+      if (frameScore === 10) {
+        score += 10 + this.rolls[rollIndex + 2];
+      } else {
+        score += frameScore;
+      }
+      
+      rollIndex += 2;
+    }
+
+    return score;
   };
 }
 
--- bowling.test.js
+++ bowling.test.js
@@ -1,8 +1,13 @@
 const Bowling = require('./bowling.js');
 
+let bowling;
+
+beforeEach(() => { 
+  bowling = new Bowling();
+});
+
 describe('bowling', () => {
     test('it can return a gutter game', () => {
-      const bowling = new Bowling();
       for (let i = 0; i < 20; i++) {
         bowling.roll(0);
       }
@@ -10,10 +15,34 @@ describe('bowling', () => {
     });
 
     test('can roll all 1s', () => {
-      const bowling = new Bowling();
       for (let i = 0; i < 20; i++) {
         bowling.roll(1);
       }
       expect(bowling.score).toEqual(20);
     });
+
+    test('it can roll a spare', () => {
+        bowling.roll(5);
+        bowling.roll(5);
+        bowling.roll(3);
+        for (let i = 0; i < 17; i++) {
+            bowling.roll(0);
+        }
+        expect(bowling.score).toEqual(16);
+      });
+
 });
+
+
+//   it 'can roll a strike' do
+//     @game.roll(10)
+//     @game.roll(5)
+//     @game.roll(3)
+//     16.times{@game.roll(0)}
+//     expect(@game.score).to eq(26)
+//   end
+
+//   it 'can have a perfect game' do 
+//     12.times{@game.roll(10)}
+//     expect(@game.score).to eq(300)
+//   end

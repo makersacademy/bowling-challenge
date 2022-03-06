445c56c1aa013b0c2cdd5856d4deb443533fff05
Sarah Moffat <sarah.moffat5@gmail.com>
2022-03-06 15:46:00 +0000

    First two tests passing

--- scorecard.js
+++ scorecard.js
@@ -1,13 +1,26 @@
-const scorecard = require('./scorecard');
+class Scorecard {
 
-describe('scorecard', () => {
-  beforeEach(() => {
-    scorecard = new Scorecard;
-  });
+  constructor() {
+    this.rolls = [];
+  };
 
-  describe('initialize', () => {
-    it('initializes a new instance of scorecard', () => {
-      expect(scorecard).toBe(Scorecard);
-    });
-  });
-});
+  roll(pins) {
+    this.rolls.push(pins);
+  };
+
+  score() {
+    let score = 0; //to keep track of total score
+    let rollIndex = 0; //to keep track of which roll
+        
+    for(let frameIndex = 0; frameIndex < 10; frameIndex++) { 
+      let frameScore = this.rolls[rollIndex] + this.rolls[rollIndex+1]
+      score += frameScore;
+      rollIndex+=2
+    }
+
+    return score;
+  };
+}
+
+module.exports = Scorecard;
+let scorecard = new Scorecard
--- scorecard.test.js
+++ scorecard.test.js
@@ -1,3 +1,25 @@
-function scorecard () {
+const Scorecard = require('./scorecard');
 
-}
+describe('scorecard', () => {
+  let scorecard;
+
+  beforeEach(() => {
+    scorecard = new Scorecard();
+  });
+
+  describe('score', () => {
+    it('can roll a gutter game', () => {
+      for (let i = 0; i < 20; i++) {
+        scorecard.roll(0);
+      }
+      expect(scorecard.score()).toBe(0);
+    });
+
+    it('should return 0 for a game of all zeros', () => {
+      for (let i = 0; i < 20; i++) {
+        scorecard.roll(0);
+      }
+      expect(scorecard.score()).toEqual(0);
+    });
+  });
+});

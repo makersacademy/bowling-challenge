00255ca45c4c81468e06471284b6e0ddb52365ea
Sarah Moffat <sarah.moffat5@gmail.com>
2022-03-06 16:47:50 +0000

    Basic functionality passing

--- scorecard.js
+++ scorecard.js
@@ -9,16 +9,46 @@ class Scorecard {
   };
 
   score() {
-    let score = 0; //to keep track of total score
-    let rollIndex = 0; //to keep track of which roll
-        
-    for(let frameIndex = 0; frameIndex < 10; frameIndex++) { 
-      let frameScore = this.rolls[rollIndex] + this.rolls[rollIndex+1]
-      score += frameScore;
-      rollIndex+=2
-    }
+    let score = 0;
+    let rollIndex = 0;
 
+      for(let frameIndex = 0; frameIndex < 10; frameIndex++) {
+        if(this.isStrike(rollIndex)) {
+          score += this.strikeBonus(rollIndex);
+          rollIndex++;
+          continue;
+        }
+
+        let frameScore = this.sumFrame(rollIndex);
+        if(this.isSpare(frameScore)) {
+          score += this.spareBonus(rollIndex)
+        } else {
+            score += frameScore;
+        }
+            
+        rollIndex+=2
+    }
     return score;
+  };
+
+  sumFrame(rollIndex) {
+    return this.rolls[rollIndex] + this.rolls[rollIndex+1];
+  };
+    
+  spareBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex+2];
+  };
+
+  strikeBonus(rollIndex) {
+    return 10+this.rolls[rollIndex+1]+this.rolls[rollIndex+2];
+  };
+
+  isSpare(frameScore) {
+    return frameScore===10;
+  };
+
+  isStrike(rollIndex) {
+    return this.rolls[rollIndex]===10;
   };
 }
 
--- scorecard.test.js
+++ scorecard.test.js
@@ -9,17 +9,41 @@ describe('scorecard', () => {
 
   describe('score', () => {
     it('can roll a gutter game', () => {
-      for (let i = 0; i < 20; i++) {
-        scorecard.roll(0);
-      }
-      expect(scorecard.score()).toBe(0);
-    });
-
-    it('should return 0 for a game of all zeros', () => {
-      for (let i = 0; i < 20; i++) {
-        scorecard.roll(0);
-      }
+      rollMany(20, 0);
       expect(scorecard.score()).toEqual(0);
     });
+
+    it('should return 20 for game of all ones', () => {
+      rollMany(20, 1);
+      expect(scorecard.score()).toEqual(20);
+    });
+
+    it('handles a spare with correct bonus', () => {
+      scorecard.roll(5); 
+      scorecard.roll(5);
+      scorecard.roll(1);
+      rollMany(17, 0);
+      expect(scorecard.score()).toEqual(12);
+    });
+
+    it('handles a strike with correct bonus', () => {
+      scorecard.roll(10);
+      scorecard.roll(1);
+      scorecard.roll(1);
+      rollMany(17, 0);
+      expect(scorecard.score()).toEqual(14);
+    });
+
+    it('can return the max score of 300', () => {
+      rollMany(1000000, 10);
+      expect(scorecard.score()).toEqual(300);
+    });
   });
+
+
+  function rollMany(rolls, pins) {
+    for (let i = 0; i < rolls; i++) {
+      scorecard.roll(pins);
+    }
+  };
 });

a347874b2d08263a497687da9474dd8a5e4cfd06
Ed <ed321@hotmail.co.uk>
2022-03-06 15:11:43 +0000

    create multiple frames and total score

--- game.js
+++ game.js
@@ -1,14 +1,33 @@
 const Frame = require("./frame");
 
 class Game {
+  constructor(){
+    this.frames = [];
+    this.rolls = [];
+  }
   roll(pins_knocked_down) {
+    this.rolls.push(pins_knocked_down);
+    if(this.rolls.length == 2){
+      this.createFrame(this.rolls[0],this.rolls[1]);
+      this.rolls = [];
+    }
     return pins_knocked_down;
   }
 
-  createFrame(roll1, roll2){
-    return new Frame(roll1,roll2);
+  score(){
+    let total = 0
+    this.frames.forEach(frame => {
+      total += frame.score();
+    })
+    return total;
   }
-  
+
+  createFrame(roll1, roll2){
+    let newFrame = new Frame(roll1,roll2);
+    this.frames.push(newFrame);
+    return newFrame;
+  }
+
 }
 
 module.exports = Game;
--- game.test.js
+++ game.test.js
@@ -14,5 +14,19 @@ describe("Game", () => {
       const frameDouble = { ball1: 5, ball2: 2, spare: false, strike: false }
       expect(game.createFrame(game.roll(5), game.roll(2))).toEqual(frameDouble);
     })
+    it('has adds additional frames', () => {
+      game.roll(5); game.roll(2);
+      game.roll(5); game.roll(2);
+      expect(game.frames.length).toEqual(2);
+    })
   })
+
+  describe('score', () => {
+    it('totals the score after 2 games', () => {
+      game.roll(3); game.roll(2);
+      game.roll(3); game.roll(2);
+      expect(game.score()).toEqual((3 + 2) + (3 + 2));
+    })
+  })
+  
 });

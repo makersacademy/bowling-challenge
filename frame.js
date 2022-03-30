79f16d9fb7ebadfce3f34cdaff3735a3d50b91c3
ruiined <ruiined@gmail.com>
2022-03-30 14:01:18 +0100

    worked on frame basics

--- frame.js
+++ frame.js
@@ -1,11 +1,27 @@
 class Frame {
   constructor(score) {
-    this.score = 0;
+    this.score = [];
   }
 
   getScore() {
-    return this.score;
+    let score = 0
+    this.score.forEach( roll => score += roll );
+    return score
+  }
+
+  rollPins(pins) {
+    if (this.score.length < 2) {
+    this.score.push(pins)} else {
+    throw new Error('cannot roll more than twice')};
   }
 }
+
+const frame = new Frame;
+
+console.log(frame.getScore());
+console.log(frame.rollPins(4));
+console.log(frame.rollPins(4));
+
+console.log(frame.getScore());
 
 module.exports = Frame
--- frame.test.js
+++ frame.test.js
@@ -7,4 +7,18 @@ describe('bowling frame', () => {
     expect(frame.getScore()).toBe(0);
   });
 
+  it ('adds the first roll to the frame', () => {
+    frame.rollPins(4)
+    expect(frame.getScore()).toBe(4);
+  });
+
+  it ('adds the second roll to the frame and sums the total', () => {
+    frame.rollPins(4)
+    expect(frame.getScore()).toBe(8);
+  });
+
+  // it ('throws an error when trying to roll more than twice', () => {
+  //   expect(frame.rollPins(4)).toThrowError('cannot roll more than twice');
+  // });
+
 });

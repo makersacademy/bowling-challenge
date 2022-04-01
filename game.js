b647564129512345ef8e502d87fe85a69f57acac
ruiined <ruiined@gmail.com>
2022-04-01 17:43:35 +0100

    finished working on game class

--- game.js
+++ game.js
@@ -1,89 +1,120 @@
 const Frame = require('./frame');
 
 class Game {
-  constructor(score) {
+  constructor() {
     this.score = [];
     this.frame = new Frame;
     this.max_frames = 10;
   };
 
-  rollPins(pins) {
-    if (this.totalScore().length === this.max_frames) return this._gameOver();
-    this.frame.rollPins(pins);
-
-    if (this.frame.frameOver()) {
-      this.score.push(this._frameScore());
-      this.frame = new Frame;
-    };
-  };
-
-  totalScore() {
+  getScore() {
     return this.score
   };
 
+  rollPins(pins) {
+    if (this._tenthFrame()) return this._checkBonuses() ? this._bonusRound(pins) : this.totalScore()
+    this.frame.rollPins(pins);
+    if (this.frame.frameOver()) return this._newFrame();
+  };
+
+  totalScore() {
+    let total = 0
+    this.score.map ( frame => frame.map( score => total += score) )
+    return total
+  }
+
   _frameScore() {
     return this.frame.getScore();
+  };
+
+  _newFrame() {
+    if (this.score.length >=1) this._calculateBonuses()
+    this.score.push(this._frameScore());
+    this.frame = new Frame;
+  };
+
+  _prevFrame() {
+    return this.score[this.score.length - 1];
+  };
+
+  _prevFrameStrike() {
+    return this._prevFrame()[0] === 10;
+  };
+
+  _prevFrameSpare() {
+    return (this._prevFrame()[0] + this._prevFrame()[1] === 10) 
+    && !this._prevFrameStrike();
+  };
+
+  _calculateBonuses() {
+    if (this._prevFrameStrike()) this._prevFrame()[0] += this._frameScore()[0] + this._frameScore()[1];
+    if (this._prevFrameSpare()) this._prevFrame()[1] += this._frameScore()[0];
+  };
+
+  _tenthFrame() {
+    return this.score.length === this.max_frames
+  };
+
+  _checkBonuses () { 
+    return this._bonus("spare", 2) 
+        || this._bonus("strike", 1) 
+        || (this._bonus("strike", 2) && this._prevFrame()[1] === 10);
+  };
+
+  _bonus(bonus, rolled) {
+    bonus === "spare" ? bonus = this._prevFrameSpare() : bonus = this._prevFrameStrike()
+    return bonus && this._prevFrame().length === rolled
   }
 
-  _gameOver() {
-    return "game over"
-    // break
-  }
+  _bonusRound(pins) {
+    this._prevFrame().push(pins);
+  };
 };
 
-const game = new Game;
-// 1
-game.rollPins(4);
-console.log(game.totalScore());
-game.rollPins(3);
-console.log(game.totalScore());
-// 2
-game.rollPins(7);
-console.log(game.totalScore());
-game.rollPins(3);
-console.log(game.totalScore());
-// 3
-game.rollPins(10);
-console.log(game.totalScore());
-// 4
-game.rollPins(6);
-console.log(game.totalScore());
-game.rollPins(4);
-console.log(game.totalScore());
-// 5
-game.rollPins(4);
-console.log(game.totalScore());
-game.rollPins(4);
-console.log(game.totalScore());
-// 6
-game.rollPins(4);
-console.log(game.totalScore());
-game.rollPins(3);
-console.log(game.totalScore());
-// 7
-game.rollPins(0);
-console.log(game.totalScore());
-game.rollPins(0);
-console.log(game.totalScore());
-// 8
-game.rollPins(0);
-console.log(game.totalScore());
-game.rollPins(3);
-console.log(game.totalScore());
-// 9
-game.rollPins(4);
-console.log(game.totalScore());
-game.rollPins(3);
-console.log(game.totalScore());
-// 10
-game.rollPins(4);
-console.log(game.totalScore());
-game.rollPins(3);
-console.log(game.totalScore());
-// // 11
-game.rollPins(1);
-console.log(game.totalScore());
-game.rollPins(1);
-console.log(game.totalScore());
+// const game = new Game;
+// // 1
+// game.rollPins(4);
+// game.rollPins(3);
+// // 2
+// console.log("spare:");
+// game.rollPins(7);
+// game.rollPins(3);
+// // 3
+// // console.log("strike:");
+// game.rollPins(10);
+// console.log(game.getScore());
+// // // 4
+// // console.log("spare:");
+// game.rollPins(6);
+// game.rollPins(4);
+// console.log(game.getScore());
+// // 5
+// game.rollPins(4);
+// game.rollPins(4);
+// // 6
+// game.rollPins(4);
+// game.rollPins(3);
+// // 7
+// game.rollPins(0);
+// game.rollPins(0);
+// // 8
+// game.rollPins(0);
+// game.rollPins(3);
+// // 9
+// game.rollPins(4);
+// game.rollPins(3);
+// // 10
+// console.log("spare:");
+// game.rollPins(7);
+// game.rollPins(3);
+// console.log(game.getScore());
+// // // 11
+// console.log("11th frame:");
+// game.rollPins(10);
+// console.log(game.getScore());
+// game.rollPins(1);
+// console.log(game.getScore());
+// game.rollPins(1);
+// console.log(game.getScore());
 
 module.exports = Game
--- game.test.js
+++ game.test.js
@@ -1,28 +1,43 @@
-const Game = require('./game')
+const Game = require('./game');
+const Frame = require('./frame')
+// jest.mock('./frame');
 
-describe('bowling game basic features', () => {
+describe('bowling game', () => {
   const game = new Game();
-
+  // beforeAll(() => {
+  //   Frame.mockImplementation(() => {
+  //     return {
+  //       rollPins: () => { return true },
+  //       getScore: () => { return ('[4, 4]') },
+  //       frameOver: () => { return false } 
+  //     }
+  //   })
+  // })
+ 
   it ('starts the score with an empty array', () => {
-    expect(game.totalScore()).toBeEmpty;
+    expect(game.getScore()).toBeEmpty;
   });
+
+  // it ('game class depends on the (mocked) frame class', () => {
+  //   expect(Frame).toHaveBeenCalledTimes(1)
+  // })
 
   it ('stores the first frames scores', () => {
     game.rollPins(4)
     game.rollPins(4)
-    expect(game.totalScore()[0]).toEqual([4, 4]);
+    expect(game.getScore()[0]).toEqual([4, 4]);
   });
 
   it ('starts a new frame after 2 rolls', () => {
     game.rollPins(4)
     game.rollPins(4)
-    expect(game.totalScore().length).toBe(2);
+    expect(game.getScore().length).toBe(2);
   });
 
   it ('finishes the frame after a strike', () => {
     game.rollPins(10)
     game.rollPins(4)
-    expect(game.totalScore()[2]).toEqual([10]);
+    expect(game.getScore()[2]).toEqual([10]);
   });
 
 

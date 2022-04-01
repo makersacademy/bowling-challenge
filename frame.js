5acb3aa730b3f338f23974e975b28cb6bbd4a19f
ruiined <ruiined@gmail.com>
2022-04-01 21:23:32 +0100

    fixed formatting with eslint

--- frame.js
+++ frame.js
@@ -1,12 +1,12 @@
 class Frame {
-  constructor(score) {
+  constructor() {
     this.max_pins = 10
     this.score = [];
   }
 
   getScore() {
     return this.score;
-  };
+  }
 
   rollPins(pins) {
     if (this.frameOver()) throw new Error('cannot roll more than twice'); 
@@ -14,11 +14,11 @@ class Frame {
     if (pins > this.max_pins) throw new Error('cannot roll more than a maximum amount of pins');
     if (pins > this.max_pins - this.score[0]) throw new Error('the roll input exceeds the amount of pins left');
     this.score.push(pins);
-  };
+  }
 
   frameOver() {
     return this.score.length >= 2 || (this.score.length === 1 && this.score[0] === 10);
-  };
-};
+  }
+}
 
 module.exports = Frame
--- game.js
+++ game.js
@@ -5,17 +5,17 @@ class Game {
     this.score = [];
     this.frame = new Frame;
     this.max_frames = 10;
-  };
+  }
 
   getScore() {
     return this.score
-  };
+  }
 
   rollPins(pins) {
     if (this._tenthFrame()) return this._checkBonuses() ? this._bonusRound(pins) : this.totalScore()
     this.frame.rollPins(pins);
     if (this.frame.frameOver()) return this._newFrame();
-  };
+  }
 
   totalScore() {
     let total = 0
@@ -25,50 +25,50 @@ class Game {
 
   _frameScore() {
     return this.frame.getScore();
-  };
+  }
 
   _newFrame() {
     if (this.score.length >=1) this._calculateBonuses()
     this.score.push(this._frameScore());
     this.frame = new Frame;
-  };
+  }
 
   _prevFrame() {
     return this.score[this.score.length - 1];
-  };
+  }
 
-  _prevFrameStrike() {
+  _wasStrike() {
     return this._prevFrame()[0] === 10;
-  };
+  }
 
-  _prevFrameSpare() {
+  _wasSpare() {
     return (this._prevFrame()[0] + this._prevFrame()[1] === 10) 
-    && !this._prevFrameStrike();
-  };
+    && !this._wasStrike();
+  }
 
   _calculateBonuses() {
-    if (this._prevFrameStrike()) this._prevFrame()[0] += this._frameScore()[0] + this._frameScore()[1];
-    if (this._prevFrameSpare()) this._prevFrame()[1] += this._frameScore()[0];
-  };
+    if (this._wasStrike()) this._prevFrame()[0] += this._frameScore()[0] + this._frameScore()[1];
+    if (this._wasSpare()) this._prevFrame()[1] += this._frameScore()[0];
+  }
 
   _tenthFrame() {
     return this.score.length === this.max_frames
-  };
+  }
 
   _checkBonuses () { 
     return this._bonus("spare", 2) 
         || this._bonus("strike", 1) 
         || (this._bonus("strike", 2) && this._prevFrame()[1] === 10);
-  };
+  }
 
   _bonus(bonus, rolled) {
-    bonus === "spare" ? bonus = this._prevFrameSpare() : bonus = this._prevFrameStrike()
+    bonus === "spare" ? bonus = this._wasSpare() : bonus = this._wasStrike()
     return bonus && this._prevFrame().length === rolled
   }
 
   _bonusRound(pins) {
     this._prevFrame().push(pins);
-  };
-};
+  }
+}
 
 module.exports = Game

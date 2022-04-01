ee19d8a7909dea15a50416f4e4ea91119c3b441c
Rob Oman <rob.oman@icloud.com>
2022-04-01 22:55:23 +0100

    Completed Frame Class and tests

---
+++ spec/frame.test.js
@@ -0,0 +1,41 @@
+jest.mock('../lib/bonus.js');
+const Bonus = require('../lib/bonus');
+const Frame = require('../lib/frame');
+
+describe('Frame', () => {
+  let frame;
+  beforeEach(() => {
+    Bonus.mockClear();
+    frame = new Frame();
+  });
+  test('#getScore - score is 0 at the start of the frame', () => {
+    expect(frame.getScore()).toBe(0);
+  });
+  test('#roll - it adds score to the total frame score', () => {
+    frame.roll(4);
+    expect(frame.getScore()).toBe(4);
+  });
+  test('#roll - returns a Bonus instance if strike', () => {
+    expect(frame.roll(10)).toBe(Bonus.mock.instances[0]);
+  });
+  test('#roll - returns a Bonus instance if spare', () => {
+    frame.roll(2);
+    expect(frame.roll(8)).toBe(Bonus.mock.instances[0]);
+  });
+  test('#roll - does not return a Bonus instance if no strike/spare', () => {
+    expect(frame.roll(3)).toBeNull();
+  });
+  test('#completed - returns true when strike', () => {
+    frame.roll(10);
+    expect(frame.completed()).toBe(true);
+  });
+  test('#completed - returns true when 2 rolls have been made', () => {
+    frame.roll(2);
+    frame.roll(1);
+    expect(frame.completed()).toBe(true);
+  });
+  test('#completed - returns false when 1 rolls have been made', () => {
+    frame.roll(2);
+    expect(frame.completed()).toBe(false);
+  });
+});

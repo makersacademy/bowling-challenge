e2ca02642bafcb8bd541f12e7a6afb3fb01d74f7
Rob Oman <rob.oman@icloud.com>
2022-04-02 19:22:01 +0100

    Add Bonus class and tests

---
+++ spec/bonus.test.js
@@ -0,0 +1,26 @@
+const Bonus = require('../lib/bonus');
+const Frame = require('../lib/frame');
+jest.mock('../lib/frame.js');
+
+describe('Bonus', () => {
+  let frameDouble;
+  let bonus;
+
+  beforeEach(() => {
+    frameDouble = new Frame();
+    bonus = new Bonus(frameDouble, 2);
+  })
+  test('#apply', () => {
+    const rollSpy = jest.spyOn(frameDouble, 'roll').mockReturnValue();
+    bonus.apply(4);
+    expect(rollSpy).toHaveBeenCalledWith(4);
+  });
+  test('#isActive is false when 2 bonuses are added', () => {
+    bonus.apply(4);
+    bonus.apply(5);
+    expect(bonus.isActive()).toBe(false);
+  });
+  test('#isActive is true when initialised', () => {
+    expect(bonus.isActive()).toBe(true);
+  });
+})

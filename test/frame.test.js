f7f67fc0e9f447d5c5b84cce2f3655f64808a4d9
GDRMVRM <gdrmvrm@gmail.com>
2022-04-02 14:48:58 +0100

    added the addRoll function to frame class with tests

---
+++ test/frame.test.js
@@ -0,0 +1,40 @@
+const Frame = require("../lib/frame");
+
+describe("Frame Class", () => {
+  describe("add roll function", () => {
+    it("has no score when created", () => {
+      const frame = new Frame();
+      expect(frame.getRoll1()).toBe(null);
+    });
+
+    it("has a score against roll1", () => {
+      const frame = new Frame();
+      frame.addRoll(4);
+      expect(frame.getRoll1()).toBe(4);
+      expect(frame.getRoll2()).toBe(null);
+    });
+
+    it("has a score against roll2", () => {
+      const frame = new Frame();
+      frame.addRoll(4);
+      frame.addRoll(3);
+      expect(frame.getRoll1()).toBe(4);
+      expect(frame.getRoll2()).toBe(3);
+    });
+
+    it("does not allow a score greater than 10", () => {
+      const frame = new Frame();
+      frame.addRoll(7);
+      expect(() => {
+        frame.addRoll(6);
+      }).toThrow("Maximum score of 10 allowed");
+    });
+
+    it("does not allow a score greater than 10", () => {
+      const frame = new Frame();
+      expect(() => {
+        frame.addRoll(11);
+      }).toThrow("Maximum score of 10 allowed");
+    });
+  });
+});

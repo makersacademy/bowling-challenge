3ffe0e84d577be60319366da3320d8c5e17d9c45
Rob Oman <rob.oman@icloud.com>
2022-04-02 22:22:12 +0100

    refactor code

--- spec/frame.test.js
+++ spec/frame.test.js
@@ -11,35 +11,35 @@ describe('Frame', () => {
   test('#getScore - score is 0 at the start of the frame', () => {
     expect(frame.getScore()).toBe(0);
   });
-  test('#roll - it adds score to the total frame score', () => {
-    frame.roll(4);
+  test('#addScore - it adds score to the total frame score', () => {
+    frame.addScore(4);
     expect(frame.getScore()).toBe(4);
   });
-  test('#roll - returns a Bonus instance if strike', () => {
-    expect(frame.roll(10)).toBe(Bonus.mock.instances[0]);
+  test('#addScore - returns a Bonus instance if strike', () => {
+    expect(frame.addScore(10)).toBe(Bonus.mock.instances[0]);
   });
-  test('#roll - returns a Bonus instance if spare', () => {
-    frame.roll(2);
-    expect(frame.roll(8)).toBe(Bonus.mock.instances[0]);
+  test('#addScore - returns a Bonus instance if spare', () => {
+    frame.addScore(2);
+    expect(frame.addScore(8)).toBe(Bonus.mock.instances[0]);
   });
-  test('#roll - does not return a Bonus instance if no strike/spare', () => {
-    expect(frame.roll(3)).toBeNull();
+  test('#addScore - does not return a Bonus instance if no strike/spare', () => {
+    expect(frame.addScore(3)).toBeUndefined();
   });
-  test('#roll - does not return a Bonus instance on any bonus rolls', () => {
-    frame.roll(10)
-    expect(frame.roll(4)).toBeNull();
+  test('#addScore - does not return a Bonus instance on any bonus rolls', () => {
+    frame.addScore(10)
+    expect(frame.addScore(4)).toBeUndefined();
   });
-  test('#completed - returns true when strike', () => {
-    frame.roll(10);
-    expect(frame.completed()).toBeTruthy();
+  test('#completed - returns a truthy value when strike', () => {
+    frame.addScore(10);
+    expect(frame.isComplete()).toBeTruthy();
   });
-  test('#completed - returns a truthy value when 2 rolls have been made', () => {
-    frame.roll(2);
-    frame.roll(1);
-    expect(frame.completed()).toBeTruthy();
+  test('#isComplete - returns a truthy value when 2 rolls have been made', () => {
+    frame.addScore(2);
+    frame.addScore(1);
+    expect(frame.isComplete()).toBeTruthy();
   });
-  test('#completed - returns falsy value when 1 rolls have been made', () => {
-    frame.roll(2);
-    expect(frame.completed()).toBeNull();
+  test('#isComplete - returns falsy value when 1 rolls have been made', () => {
+    frame.addScore(2);
+    expect(frame.isComplete()).toBeFalsy();
   });
 });

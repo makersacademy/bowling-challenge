27bc3e5e351b2af91a143b4423f1a10d38bdfdf5
Patryk Okpara <patrykokpara@hotmail.com>
2022-04-03 20:10:14 +0100

    Added ability to create a game

---
+++ bowlingGame.js
@@ -0,0 +1,5 @@
+class BowlingGame{
+  
+}
+
+module.exports = BowlingGame
---
+++ bowlingGame.test.js
@@ -0,0 +1,7 @@
+const BowlingGame = require ('./bowlingGame')
+
+describe ('Bowling game', () =>{
+  test ('Creates a game', () => {
+    const game = new BowlingGame();
+  })
+})

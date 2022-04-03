9da51552ff6f949bc8df93bd02380e0b76b07c6c
James Mooney <jmooney9304@gmail.com>
2022-04-04 00:12:56 +0100

    frame class passing all tests

--- frame.js
+++ frame.js
@@ -1,12 +1,70 @@
 class Frame {
   constructor(round = 1) {
-    this.round = round
+    this.points = 0;
+    this.round = round;
     this.rollOne = null;
     this.rollTwo = null;
-    this.score = 0
+    this.rollThree = null;
+    this.complete = false;
   };
 
+  roll(pins) {
+    if (this.complete) throw "It's over! Stop lobbing the balls!";
+  
+    if (this.rollOne === null) {
+      this.firstGo(pins);
+    } else if (this.round < 10 && this.rollTwo === null) {
+      this.secondGo(pins);
+    } else if (this.rollTwo === null) {
+      this.secondGoRound10(pins);
+    } else {
+      this.thirdRoll(pins);
+    }
+  }
 
+  strike() {
+    return this.rollOne === 10;
+  }
+  
+  spare() {
+    return this.rollOne !== 10 && this.rollOne + this.rollTwo === 10;
+  }
+
+  firstGo(pins) {
+    this.rollOne = pins;
+    if (pins === 10 && this.round < 10) {
+      this.complete = true;
+    };
+    this.frameTotal();
+  };
+
+  secondGo(pins) {
+    this.rollTwo = pins;
+    this.complete = true;
+    this.frameTotal();
+  };
+
+  secondGoRound10(pins) {
+    this.rollTwo = pins;
+    if (this.rollOne + this.rollTwo < 10) {
+      this.complete = true;
+    }
+    this.frameTotal();
+  }
+
+  thirdRoll(pins) {
+    this.rollThree = pins;
+    this.complete = true;
+    this.frameTotal();
+  };
+
+  frameTotal() {
+    if (this.rollOne !== null) this.points = this.rollOne;
+    if (this.rollTwo !== null) this.points += this.rollTwo;
+    if (this.rollThree !== null) this.points += this.rollThree;
+    return this.points;
+  };
 };
+
 
 module.exports = Frame;
--- frame.test.js
+++ frame.test.js
@@ -1,8 +1,105 @@
 const Frame = require("./frame");
 
+beforeEach(() => {
+  frame = new Frame();
+});
+
+
 describe('Frame', () => {
  it('frame starts at score equalling 0', () => {
-    frame = new Frame();
-    expect(frame.score).toEqual(0);
+    expect(frame.points).toEqual(0);
   });
+
+  it('starts with rollOne and rollTwo equalling null', () => {
+    expect(frame.rollOne).toEqual(null);
+    expect(frame.rollTwo).toEqual(null);
+  });
+
+  it('starts with round being round 1', () => {
+    expect(frame.round).toEqual(1);
+  });
+
+  it('perform first roll and the amount of pins get added to rollOne', () => {
+    frame.roll(8);
+    expect(frame.rollOne).toEqual(8);
+  });
+
+  it('perform two rolls and you can shows rollTwo score and round score', () => {
+    frame.roll(1);
+    frame.roll(2);
+    expect(frame.rollTwo).toEqual(2);
+  });
+
+  it('performs two rolls and calculates round score', () => {
+    frame.roll(1);
+    frame.roll(2);
+    expect(frame.points).toEqual(3);
+  });
+
+  it('confirms neither a strike or spare has been bowled in a round', () => {
+    frame.roll(4);
+    frame.roll(3);
+    expect(frame.spare()).toEqual(false);
+    expect(frame.strike()).toEqual(false);
+  })
+
+  it('confirms whether spare has been bowled in a round', () => {
+    frame_1 = new Frame();
+    frame_2 = new Frame();
+    frame_1.roll(8);
+    frame_1.roll(1)
+    expect(frame_1.spare()).toEqual(false);
+    frame_2.roll(9);
+    frame_2.roll(1);
+    expect(frame_2.spare()).toEqual(true);
+  });
+
+  it('confirms whether strike has been bowled in a round', () => {
+    frame_1 = new Frame();
+    frame_2 = new Frame();
+    frame_1.roll(9);
+    expect(frame_1.strike()).toEqual(false);
+    frame_2.roll(10);
+    expect(frame_2.strike()).toEqual(true);
+  });
+
+  it('checks to see if round is complete or not', () => {
+    expect(frame.complete).toEqual(false);
+    frame.roll(1);
+    expect(frame.complete).toEqual(false);
+    frame.roll(2)
+    expect(frame.complete).toEqual(true);
+  });
+
+  it('checks to see if round is complete with a one roll strike', () => {
+    frame.roll(10);
+    expect(frame.complete).toEqual(true);
+  });
+
+  it('checks to see whether round 10 complete if spare happens or not and allows 3rd go', () => {
+    frame_3 = new Frame(10);
+    frame_4 = new Frame(10);
+    frame_3.roll(8);
+    frame_3.roll(1);
+    expect(frame_3.complete).toEqual(true);
+    frame_4.roll(8);
+    frame_4.roll(2);
+    expect(frame_4.complete).toEqual(false);
+    frame_4.roll(7);
+    expect(frame_4.complete).toEqual(true);
+    });
+  
+    it('will not allow more rolls if frame is complete', () => {
+      frame.roll(10);
+      expect(() => {
+        frame.roll(1).toThrow("It's over! Stop lobbing the balls!")
+      });
+      frame_5 = new Frame(10);
+      frame_5.roll(8);
+      expect(() => {
+        frame_5.roll(1).toThrow("It's over! Stop lobbing the balls!")
+      });
+    });
+
+
 });

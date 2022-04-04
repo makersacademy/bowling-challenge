73b5029ed0ee78e710aaa1bd56e2ea22be84d3d6
James Mooney <jmooney9304@gmail.com>
2022-04-04 02:10:50 +0100

    added roll method

--- scorecard.js
+++ scorecard.js
@@ -3,8 +3,13 @@ const Frame = require('./frame');
 class Scorecard {
   constructor(round = 1) {
     this.round = round;
-    this.xFrame = [new Frame()];
+    this.allFrames = [new Frame()];
   };
+
+  roll(pins) {
+    
+  }
+
 };
 
 module.exports = Scorecard;
--- scorecard.test.js
+++ scorecard.test.js
@@ -10,4 +10,22 @@ describe('Scorecard', () => {
   it('checks that scorecard is an instance of the Scorecard Class', () => {
     expect(scorecard).toBeInstanceOf(Scorecard);
   });
+
+  it('initiates with an empty frame array that has also been initialized', () => {
+    expect(scorecard.allFrames).toEqual([{"complete": false, 
+    "points": 0, 
+    "rollOne": null, 
+    "rollThree": null, 
+    "rollTwo": null, 
+    "round": 1}]);
+  });
+
+  it('checks what round we are currently on', () => {
+    expect(scorecard.round).toEqual(1);
+    // console.log(scorecard.allFrames[scorecard.allFrames.length - 1]);
+    // console.log('NEXT');
+    // console.log(scorecard.allFrames);
+    // console.log('NEXT');
+    // console.log([scorecard.allFrames.length - 1]);
+  });
 });

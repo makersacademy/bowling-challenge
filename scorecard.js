11a5820fc08417d62179c87f14696371108eef3d
kat <katharinebrooks@hotmail.co.uk>
2022-04-14 11:28:12 +0100

    Refactored score function within Scorecard, with an attempt to make it more functional style. But unsure how to make it work as solely reduce i.e. without the forEach

--- scorecard.js
+++ scorecard.js
@@ -14,10 +14,6 @@ class Scorecard {
     return this.frames.length + 1;
   }
   score() {
-    // let total = 0;
-    // reducerFunction(previous, frame, index, frames) {
-    //   previous.score(frames[index + 1], frames[index + 2]) + frame.score(frames[index + 2], frames[index + 3]) +
-    // }
     const scores_array = [];
     this.frames.forEach((frame, index, frames) => {
       console.log(frame.score(frames[index + 1], frames[index + 2]));
@@ -26,12 +22,6 @@ class Scorecard {
     return scores_array.reduce((previousFrameScore, currentFrameScore) => {
       return previousFrameScore + currentFrameScore;
     }, 0);
-
-    // this.frames.reduce(reducerFunction)
-    // this.frames.map((frame, index, frames) => {
-    //   total += frame.score(frames[index + 1], frames[index + 2]);
-    // });
-    // return total;
   }
 }
 module.exports = Scorecard;

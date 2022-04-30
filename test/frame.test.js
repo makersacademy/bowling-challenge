a20aedcc4a4ac53f69d8685c08378933d4f3be3e
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-30 15:04:12 +0100

    two test failing rest all done

--- test/frame.test.js
+++ test/frame.test.js
@@ -17,4 +17,27 @@ describe('frames', () => {
     var nextFrame = new Frame([2,0]);
     expect(frame.total(nextFrame)).toBe(12)
   });
+  it('return the score for three strikes', () => {
+    var frame = new Frame([10]);
+    var nextFrame = new Frame([10]);
+    var next_to_next = new Frame([2, 1]);
+    expect(frame.total(nextFrame, next_to_next)).toBe(22);
+  });
+
+  it("can deal with mutiple strikes in a row", function(){
+    var frame = new Frame([10]);
+    var next_to_next = new Frame([10]);
+    var next_to_next_1 = new Frame([10]);
+    expect(frame.total(next_to_next, next_to_next_1)).toEqual(30);
+  });
+  it ("all strikes in final frame", function(){
+    var frame = new Frame([10,10,10]);
+    expect(frame.total()).toEqual(30);
+  });
+
+  it('it can calaculate all strikes in final but score for 30 if there is strike before', ()=> {
+    var frame = new Frame([10]);
+    var next = new Frame([10,10,10]);
+    expect(frame.total(next)).toEqual(30);
+  });
 });

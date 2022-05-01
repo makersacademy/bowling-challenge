a83bd2586701621cbcff280db3545109706e2c5a
Joe McNally <joe.t.mcnally@hotmail.com>
2022-05-01 22:37:35 +0100

    Added the frameScore and gameScore functions for very simple games.

--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -19,4 +19,39 @@ describe("Scorecard", () => {
       expect(card.getFrames()).toEqual([[3,6]]);
     });
   });
+
+  describe("frameScore", () => {
+    it("calculates the score of a simple frame", () => {
+      const card = new Scorecard;
+      const mockFrame = {
+        addFirstRoll: () => undefined,
+        addSecondRoll: () => undefined,
+        getFirstRoll: () => 3,
+        getSecondRoll: () => 6
+      }
+      card.addFrame(3, 6, mockFrame);
+      expect(card.frameScore(card.frames[0])).toEqual(9);
+    });
+  });
+
+  describe("gameScore", () => {
+    it("calculates the score of a simple two frame game", () => {
+      const card = new Scorecard;
+      const mockFrameOne = {
+        addFirstRoll: () => undefined,
+        addSecondRoll: () => undefined,
+        getFirstRoll: () => 3,
+        getSecondRoll: () => 6
+      }
+      const mockFrameTwo = {
+        addFirstRoll: () => undefined,
+        addSecondRoll: () => undefined,
+        getFirstRoll: () => 4,
+        getSecondRoll: () => 2
+      }
+      card.addFrame(3, 6, mockFrameOne);
+      card.addFrame(4, 2, mockFrameTwo);
+      expect(card.gameScore()).toEqual(15);
+    });
+  });
 });

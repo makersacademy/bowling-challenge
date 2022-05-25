a06342f3fef70742983e8bee828b49759e9d8d59
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-05-25 11:21:02 +0100

    refactored the test

--- spec/Game-spec.js
+++ spec/Game-spec.js
@@ -8,10 +8,14 @@ describe("bowlingGame", () => {
 
     it("rolls a 2 four times, the total score should equals 8", () => {
         let Game = new bowlingGame();
-        Game.rollPin(2);
-        Game.rollPin(2);
-        Game.rollPin(2);
-        Game.rollPin(2);
+        rollPins(Game, 2, 4)
         expect(Game.calculateTotalScore()).toBe(8);
     })
+
+    function rollPins(Game, number, times)  {
+        for(let i = 0; i < times; i++){
+            Game.rollPin(number)
+        }
+
+    }
 })

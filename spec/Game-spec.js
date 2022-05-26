8189288af7c0569106805d084fae970972065f91
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-05-26 10:50:32 +0100

    test for a spare game

--- spec/Game-spec.js
+++ spec/Game-spec.js
@@ -12,12 +12,20 @@ describe("bowlingGame", () => {
         expect(Game.calculateTotalScore()).toBe(8);
     })
 
-    it ("can roll a gutter game, zero 20 times", () => {
+    it ("can roll a gutter game, zero 20 times should equal 0", () => {
         let Game = new bowlingGame();
         rollPins(Game, 0, 20);
         expect(Game.calculateTotalScore()).toEqual(0)
     })
 
+    it("rolls a spare game, when i roll 1 4 and 6, then a 2 and 3 should equal 17", () => {
+        let Game = new bowlingGame();
+        Game.rollPin(4);
+        Game.rollPin(6);
+        Game.rollPin(2);
+        Game.rollPin(3);
+        expect(Game.calculateTotalScore()).toEqual(17)
+    })
     function rollPins(Game, number, times)  {
         for(let i = 0; i < times; i++){
             Game.rollPin(number)
--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,35 @@
 class bowlingGame {
-    #totalScore;
+    #rolls;
 
     constructor() {
-        this.#totalScore = 0;
+        this.#rolls = [];
     }
 
     rollPin(pins) {
-        this.#totalScore += pins
+        this.#rolls.push(pins)
     }
 
+    #calculateSpareBonus(){
+        let numberOfFrames = Math.floor(this.#rolls.length / 2)
+        let spareBonus = 0
+
+        for (let i = 0; i < numberOfFrames; i++){
+            let frameStartingIndex = i * 2;
+            let frameRoll1 = this.#rolls[frameStartingIndex];
+            let frameRoll2 = this.#rolls[frameStartingIndex + 1];
+
+            if(frameRoll1 + frameRoll2 === 10){
+                spareBonus += this.#rolls[frameStartingIndex + 2];
+            }
+
+        }
+        
+        return spareBonus;
+
+    }
     calculateTotalScore(){
-       return this.#totalScore;
+        let totalScore = this.#rolls.reduce((acc, curr) => acc + curr)
+         totalScore += this.#calculateSpareBonus();
+        return totalScore 
     }
 }

a80fbf75e02dbbbf8b6d9f836f138430641568b8
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-05-29 18:54:10 +0100

    README and pseudo code

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ My initial thoughts with Ruby lead me to something resembling this:
 My inititial code managed to calculate the scores of a gutter-game, a perfect-game, and a "one-er" (all 1s) game. But when it came to stringing spares and strikes, the code I had written was fundamentally wrong, and otherwise highly convoluted. The original ruby challenge lead me to researching the proper methodology, post-submission. I was pleased to see that we were challenged with approaching it the week after.
 
 To explain the logic, I've retained the pseudo-code I initially placed within the files, as to clarify for anyone looking over (or myself in the future) what exactly is going on; from line to line.
+
+I learnt that the attributes I intitially set as instance variables within the initiation of the class object could just as easily be instance variables within the '#score' function itself. The initial tracking of the frame by instance/attribute, turned into a simple defined for loop that tracked the frame and would only iterate, say, when there was a strike, or when there was no strike (see scorecardIndex += 2, as to account for an entire frame).
+
+I like this solution and I'm pleased I could regurgitate and refactor it for JavaScript. The only issue with it currently is viewing a current score. The score is only viewable after the set 10 frames are over with.
+
+If you read all this congratulations to you. I've got to stop drinking wine and watching Frasier. I sound like a right highfalutin ponce :^)
--- bowlingscorer.js
+++ bowlingscorer.js
@@ -5,35 +5,45 @@ class BowlingScorer {
   }
 
   enterScore(pins) {
+    if (pins > 10 || pins < 0) {
+      return 'Please enter a score below 10, and above 0'
+    }
     this.scorecard.push(pins)
   }
 
   score() {
     let score = 0
     let scorecardIndex = 0
-
-    //frames are iterated through within the score function
+    // the score function depends on reading off an already established and appended upon array
+    //only when '#score' is called do we iterate through the entire scorecard since last entry
+    // the frames are defined and iterated through within the score function itself
     for (let frameIndex = 0 ; frameIndex < 10 ; frameIndex++) {
       //strike logic
-      //isStrike checks to see that the current score
+      //isStrike checks to see that the current score being checked during this iteration
+      // of the scorecard was 10
       if (this.isStrike(scorecardIndex)) {
         score += this.strikeBonus(scorecardIndex)
-        //
-        scorecardIndex++;
+        scorecardIndex += 1;
+        // if we know that there's been a strike, then we know we can iterate to the next element within the scorecard
         continue;
+        //continue is a great keyword (similar to break) that skips any conditionals beneath it and begins the next conditional
       }
       
+      //if a strike doesn't apply then we're going to have to start considering/define a frameScore
       const frameScore = this.scorecard[scorecardIndex] + this.scorecard[scorecardIndex + 1]
 
       // spare logic
+      // are the next two frames added equal to 10? if so, spare /
       if (this.isSpare(frameScore)) {
         score += this.spareBonus(scorecardIndex)
+        //and if so, add the spare bonus
       } else {
+        //otherwise nothing is going on here, frameScore is added to score. end of
         score += frameScore
       }
 
       scorecardIndex += 2
-    }
+    } //end of for loop
     return score
   }
 
@@ -42,14 +52,17 @@ class BowlingScorer {
   }
 
   spareBonus(scorecardIndex) {
+    //10 plus only the first roll after that in the next frame
     return 10 + this.scorecard[scorecardIndex + 2]
   }
 
   isStrike(scorecardIndex) {
+    // was the current score entry equal to a 10? if it was then it was a strike
     return this.scorecard[scorecardIndex] === 10
   }
 
   strikeBonus(scorecardIndex) {
+    // 10 plus the next roll and the second roll after that in the next frame
     return 10 + this.scorecard[scorecardIndex + 1] + this.scorecard[scorecardIndex + 2]
   }
 }

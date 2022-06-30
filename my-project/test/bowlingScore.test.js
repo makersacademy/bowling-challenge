b57a091e8e6f4ff92989fcdab33bec5aa229cf79
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-30 16:14:23 +0100

    Created 3 tests to check finalScore method

--- my-project/test/bowlingScore.test.js
+++ my-project/test/bowlingScore.test.js
@@ -1,9 +1,22 @@
 const BowlingScore = require("../lib/bowlingScore");
 
 describe("BowlingScore", () => {
-  it("returns the final score of the players game", () => {
+  it("returns the score for one frame", () => {
+    const bowlingScore = new BowlingScore([[4, 2]]);
+    expect(bowlingScore.finalScore()).toEqual("Final Score: 6");
+  });
+
+  it("returns the score for multiple frames", () => {
     const bowlingScore = new BowlingScore([
-      [0, 0],
+      [4, 2],
+      [3, 3],
+      [1, 1],
+    ]);
+    expect(bowlingScore.finalScore()).toEqual("Final Score: 14");
+  });
+
+  it("returns the final score of the players gutter game", () => {
+    const bowlingScore = new BowlingScore([
       [0, 0],
       [0, 0],
       [0, 0],
@@ -16,5 +29,21 @@ describe("BowlingScore", () => {
       [0, 0],
     ]);
     expect(bowlingScore.finalScore()).toEqual("Final Score: 0");
+  });
+
+  it("returns the final score of the players gutter game", () => {
+    const bowlingScore = new BowlingScore([
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+    ]);
+    expect(bowlingScore.finalScore()).toEqual("Final Score: 20");
   });
 });

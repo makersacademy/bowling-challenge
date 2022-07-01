40a75ba0c078c81d4173ba12e812f46c828592bd
Amir Khan <ak2022dev@gmail.com>
2022-07-01 15:46:24 +0100

    Fifth test passed

--- BowlingScoreManager.test.js
+++ BowlingScoreManager.test.js
@@ -18,13 +18,14 @@ describe( 'BowlingScoreManager', () => {
     const bsm = new BowlingScoreManager();    
     expect(bsm.score_game( [5,5, 3,1, 2,2, 3,3, 6,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).toBe(5+5 + 3+1 + 2+2 + 3+3 + 6+4 + 4+5 + 4+3 + 3+2 + 2+1 + 0+0 + 3 + 4);
   });
+  it( 'Successfully scores a game including multiple successive spares', () => {
+    const bsm = new BowlingScoreManager();
+    expect(bsm.score_game( [5,2, 3,7, 2,8, 5,5, 6,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).toBe(5+2 + 3+7 + 2+8 + 5+5 + 6+4 + 4+5 + 4+3 + 3+2 + 2+1 + 0+0 + 2 + 5 + 6 + 4);
+  });
 
 });
 
 /*
-    it "Successfully scores a game including two spares" do
-      expect(BowlingScoreManager.score_game( [5,5, 3,1, 2,2, 3,3, 6,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).to eq ([5,5, 3,1, 2,2, 3,3, 6,4, 4,5, 4,3, 3,2, 2,1, 0,0].sum + 3 + 4)
-    end
     it "Successfully scores a game including multiple successive spares" do
       expect(BowlingScoreManager.score_game( [5,2, 3,7, 2,8, 5,5, 6,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).to eq ([5,2, 3,7, 2,8, 5,5, 6,4, 4,5, 4,3, 3,2, 2,1, 0,0].sum + 2 + 5 + 6 + 4)
     end

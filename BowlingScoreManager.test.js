b664bc59b9e73e869a9f991b719ac5ddf1cc42a1
Amir Khan <ak2022dev@gmail.com>
2022-07-01 12:27:49 +0100

    Third test passed

--- BowlingScoreManager.test.js
+++ BowlingScoreManager.test.js
@@ -10,13 +10,14 @@ describe( 'BowlingScoreManager', () => {
     const bsm = new BowlingScoreManager();
     expect(bsm.score_game( [0,0, 1,1, 2,2, 3,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0])).toBe(0+0 + 1+1 + 2+2 + 3+3 + 4+4 + 4+5 + 4+3 + 3+2 + 2+1 + 0+0 );
   });
+  it('Successfully scores a game including a spare', () => {
+    const bsm = new BowlingScoreManager();    
+    expect(bsm.score_game( [5,5, 3,1, 2,2, 3,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).toBe(5+5 + 3+1 + 2+2 + 3+3 + 4+4 + 4+5 + 4+3 + 3+2 + 2+1 + 0+0 + 3);
+  });
+
 });
 
 /*
-    end
-    it "Successfully scores a minimal non-zero game" do
-      expect(BowlingScoreManager.score_game( [0,0, 1,1, 2,2, 3,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).to eq ([0,0, 1,1, 2,2, 3,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0].sum)
-    end
     it "Successfully scores a game including a spare" do
       expect(BowlingScoreManager.score_game( [5,5, 3,1, 2,2, 3,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0] )).to eq ([5,5, 3,1, 2,2, 3,3, 4,4, 4,5, 4,3, 3,2, 2,1, 0,0].sum + 3)
     end

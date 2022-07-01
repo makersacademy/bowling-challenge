d4f0007a535e4325113bb002a66acf22c0324eb2
Amir Khan <ak2022dev@gmail.com>
2022-07-01 11:38:30 +0100

    Translated all of Ruby code with no syntax errors reported by Node

---
+++ BowlingScoreManager.js
@@ -0,0 +1,186 @@
+require ('./Frame');    //The frame is a smaller unit of scoring the game
+                        // Made up of one two, or possibly in the tenth and final
+                        // frame, three rolls.
+
+
+class BowlingScoreManager {
+
+  constructor() {
+    this.frames = []; 
+  }
+  
+  score_game( rolls ) {
+    this.frames = setup_frames();
+    score_frames( rolls );
+    return get_grand_total( this.frames );
+  }
+    
+   
+  score_frames( rolls ) {
+    frame_num = 1;
+    on_roll = 1;
+    for ( let roll_from_start=0; roll_from_start<rolls.length; roll_from_start++ ) { 
+      if ( frame_num === 10 ) {
+        // In Ruby this was as below
+        // handle_frame_10( rolls.drop(roll_from_start), this.frames )
+        // Want to remove the elements 0..roll_from_start-1
+        rolls.splice(0, roll_from_start );
+        handle_frame_10( rolls, this.frames );
+        break;
+      }
+      if (on_roll === 1) {
+        manage_frame_roll1( rolls[roll_from_start], this.frames, frame_num );
+        if (this.frames[frame_num].getStatus() === Frame.strike ) {
+          // Move to roll one in next frame
+          frame_num += 1;
+          continue;
+        }
+        on_roll = 2;
+      }  
+      else { // on_roll == 2
+        manage_frame_roll2( rolls[roll_from_start], this.frames, frame_num );        
+        frame_num += 1;
+        on_roll = 1;
+      }      
+    }
+  }
+   
+
+
+  setup_frames() {
+    // Do not use index 0 as this models the domain more closely
+    frames_array = [null];
+    for( let i=1; i<=10; i++ ) {
+      frames_array.push( new Frame() );
+    }
+    return frames_array;
+  }
+
+  
+  manage_frame_roll1( rollValue, frames, frame_num ) {
+    (frames[frame_num]).setRoll1( rollValue );
+    manage_poss_prev_spare( rollValue, frames, frame_num );
+    manage_roll1_poss_prev_strike( frames, frame_num );
+    manage_poss_strike_this_frame( frames, frame_num );
+  }
+
+
+  manage_frame_roll2( rollValue, frames, frame_num ) {        
+    (frames[frame_num]).setRoll2( rollValue );
+    manage_roll2_poss_prev_strike( frames, frame_num );
+    manage_poss_spare_this_frame( frames, frame_num );
+    if ( frames[frame_num].getStatus() !== Frame.spare ) {
+      sum_normal_frame( frames, frame_num );
+    }
+  }
+
+  
+  manage_poss_prev_spare( roll, frames, frame_num ) {
+    if ((frame_num > 1) && (frames[frame_num-1].getStatus() === Frame.spare)) {
+      frames[frame_num-1].setTotal( frames[frame_num-1].getTotal() + roll );
+      frames[frame_num-1].setCompleted( true );
+    }    
+  }
+
+  
+  
+  manage_roll1_poss_prev_strike( frames, frame_num ) {
+    if ((frame_num > 1) && (frames[frame_num-1].getStatus() === Frame.strike)) {
+      // Add this roll to previous frame but don't yet mark completed
+      // because two rolls need to be added to a strike
+      frames[frame_num-1].setTotal( frames[frame_num-1].getTotal() + (frames[frame_num]).getRoll1() );
+      //  Deal with possible strike from two frames ago
+      if ((frame_num > 2) && (frames[frame_num-2].getStatus() === Frame.strike)) {
+        // Add this roll to strike from two frames ago
+        // and mark it complete as both of its following rolls
+        // have now been added
+        frames[frame_num-2].setTotal( frames[frame_num-2].getTotal() + (frames[frame_num]).getRoll1() );
+        frames[frame_num-2].setCompleted( true );
+      }
+    }
+  }
+
+
+  
+  manage_roll2_poss_prev_strike( frames, frame_num ) {
+    if ((frame_num > 1) && (frames[frame_num-1].getStatus() === Frame.strike)) {
+      // Add this roll to previous frame and then mark completed
+      // because this is second of two rolls need to be added to a strike
+      frames[frame_num-1].setTotal( frames[frame_num-1].getTotal() + (frames[frame_num]).getRoll2() );
+      frames[frame_num-1].setCompleted( true );
+    }          
+  }
+
+  
+  
+  manage_poss_spare_this_frame( frames, frame_num ) {
+    if ((frames[frame_num].getRoll1() + (frames[frame_num].getRoll2())) === 10) {
+      frames[frame_num].setStatus( Frame.spare );
+      frames[frame_num].setTotal( 10 );
+      frames[frame_num].setCompleted( false );
+    }    
+  }
+
+
+  manage_poss_strike_this_frame( frames, frame_num ) {
+    if (((frames[frame_num]).getRoll1()) === 10) {
+      frames[frame_num].setStatus( Frame.strike );
+      frames[frame_num].setRoll2( 0 );
+      frames[frame_num].setTotal( 10 );
+      frames[frame_num].setCompleted( false );
+    }
+  }
+
+  
+  handle_frame_10( last_rolls, frames ) {
+    frame_num = 10;
+    on_roll = 1;
+    // Similar loop to above just slightly modified
+    for( let rolls_at_end=0; rolls_at_end<last_rolls.length; rolls_at_end++ ) {
+      if (on_roll === 1) {
+        (frames[frame_num]).setRoll1( last_rolls[rolls_at_end] );
+        manage_poss_prev_spare( frames[frame_num].getRoll1(), frames, frame_num );
+        manage_roll1_poss_prev_strike( frames, frame_num );
+        manage_poss_strike_this_frame( frames, frame_num );
+        on_roll = 2;
+        continue;
+      }
+      else if (on_roll === 2) { 
+        (frames[frame_num]).setRoll2( last_rolls[rolls_at_end] );
+        manage_roll2_poss_prev_strike( frames, frame_num );
+        manage_poss_spare_this_frame( frames, frame_num );
+        if ( frames[frame_num].getStatus() === Frame.spare ) {
+          on_roll = 3;
+          continue;
+        }
+      }
+      else { // on_roll == 3
+        // Add last roll value to current total
+        frames[frame_num].setTotal( frames[frame_num].getTotal() + last_rolls[rolls_at_end]);
+        // No need to loop further
+        break;
+      }
+      sum_normal_frame( frames, frame_num );
+    }
+  }
+
+  
+  sum_normal_frame( frames, frame_num ) {
+    frames[frame_num].setTotal( frames[frame_num].getRoll1() + frames[frame_num].getRoll2() );
+    frames[frame_num].setCompleted( true );    
+  }
+
+  
+  
+  get_grand_total( frames ) {
+    grand_total = 0;
+    for( let i=1; i<=10; i++ ) {
+      grand_total += frames[i].getTotal();
+    }
+    return grand_total;
+  }
+  
+  
+}
+
+module.exports = BowlingScoreManager;
--- Frame.js
+++ Frame.js
@@ -8,10 +8,10 @@ class Frame {
     Frame.spare  = new Symbol('spare');
     Frame.strike = new Symbol('strike');
     
-    this.status = Frame.normal      // This is :normal or :spare or :strike
+    this.status = Frame.normal // This is 'normal' or 'spare' or 'strike'
     this.roll1 = 0             
     this.roll2 = 0             
-    this.total = 0             // This is pending if status is :spare or :strike
+    this.total = 0             // This is pending if status is 'spare' or 'strike'
                                // but will alter otherwise based on either one or two further rolls
     this.completed = false     // This is set to false until the total is the final total for the
                                // frame taking into account additional rolls for spare and strike

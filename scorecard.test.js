d3a9fadc1ebe066c9b57c2adc9ffa4aa884628a8
Kay <kard@hotmail.co.uk>
2022-07-01 12:07:50 +0100

    passes gutter game and perfect game tests using if statements

--- scorecard.test.js
+++ scorecard.test.js
@@ -1,8 +1,13 @@
 const Scorecard = require('./scorecard');
 
 describe('Scorecard',() => {
-  it('returns a score of 0 for a gutter game',() => {
+  it('returns a final score of 0 for a gutter game',() => {
     const scorecard = new Scorecard([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     expect(scorecard.giveFinalScore()).toEqual('Gutter Game: 0 points');    
+  })
+
+  it('returns a final score of 300 for a perfect game',() => {
+    const scorecard = new Scorecard([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]);
+    expect(scorecard.giveFinalScore()).toEqual('Perfect Game: 300 points');
   })
 })

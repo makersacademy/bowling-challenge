68a7afd7ed49c4273770731e8da3b5b87c992d35
Kay <kard@hotmail.co.uk>
2022-07-01 12:10:26 +0100

    removed comments

--- scorecard.js
+++ scorecard.js
@@ -1,14 +1,10 @@
 class Scorecard {
   constructor(scores) {
     this.scores = scores;
-    // console.log(this.scores);
   }
+  
   giveFinalScore() {
-    // console.log(this.scores);
     let totalScore = this.scores.reduce((total, amount) => total += amount);
-    // console.log([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] === [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
-    // console.log([1] === [1]);
-    // console.log(0 === 0);
     if (totalScore === 0) {
       return "Gutter Game: 0 points";
     }

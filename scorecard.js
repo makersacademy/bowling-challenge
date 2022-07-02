a4077e4046e86ac0219d832b6cde3620ee0405ce
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-02 17:24:40 +0100

    can calculate score when one strike is scored

--- scorecard.js
+++ scorecard.js
@@ -5,11 +5,21 @@ class Scorecard {
 
   total_score() {
     let total = 0;
-    this.scores.map((frame) => {
-      frame.forEach(roll => {
-        total += roll;
-      });
-    })
+
+    for (let i = 0; i < this.scores.length; i++) {
+      
+      if(this.scores[i][0] == 10) {
+        this.scores[i + 1].forEach(roll => {
+          total += roll;
+        });
+        total += 10;
+      } else {
+        this.scores[i].forEach(roll => {
+          total += roll;
+        });
+      }
+      
+    }
     return total;
   }
 
--- scorecard.test.js
+++ scorecard.test.js
@@ -13,5 +13,10 @@ describe(Scorecard, () => {
     expect(scorecard.total_score()).toBe(74);
   })
 
+  it("returns score for game where a strike was scored", () => {
+    arr = [[10], [2, 4], [2, 4], [3, 6], [5, 2], [3, 6], [2, 4], [2, 4], [3, 6], [5, 2]];
+    const scorecard = new Scorecard(arr);
+    expect(scorecard.total_score()).toBe(81)
+  })
  
 })

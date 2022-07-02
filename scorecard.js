3e580d09cef69566b24eea0ba902051a489b9c82
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-02 17:29:48 +0100

    moved strike logic to separate function

--- scorecard.js
+++ scorecard.js
@@ -3,16 +3,22 @@ class Scorecard {
     this.scores = scores;
   }
 
+  strike_score(next_frame) {
+    let total = 0;
+    next_frame.forEach(roll => {
+      total += roll;
+    });
+    total += 10;
+    return total;
+  }
+
   total_score() {
     let total = 0;
 
     for (let i = 0; i < this.scores.length; i++) {
       
       if(this.scores[i][0] == 10) {
-        this.scores[i + 1].forEach(roll => {
-          total += roll;
-        });
-        total += 10;
+        total += this.strike_score(this.scores[i + 1]);
       } else {
         this.scores[i].forEach(roll => {
           total += roll;

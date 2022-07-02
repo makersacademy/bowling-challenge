007b0ba3c23eb830473822891a8402f8cb21f8bc
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-02 20:05:12 +0100

    Added frames method, updated spare and finalScore method, passed all tests

--- my-project/lib/bowlingScore.js
+++ my-project/lib/bowlingScore.js
@@ -1,13 +1,40 @@
 class BowlingScore {
   constructor(scoresheet) {
     this.scoresheet = scoresheet;
+    this.spareCount = 0;
+    this.sparePoints = 0;
+    this.total = [];
+  }
+
+  frame() {
+    // check if a frame sum is less than 10
+    // if yes then add the total
+    // push the total into total array
+    this.scoresheet.forEach((frame) => {
+      const roll = frame.reduce((a, b) => a + b, 0);
+      if (roll < 10) {
+        this.total.push(frame);
+      }
+    });
+  }
+
+  spare() {
+    // check each array, if the sum of array is 10
+    // and if the first roll does not equal 10
+    this.scoresheet.forEach((frame) => {
+      const roll = frame.reduce((a, b) => a + b, 0);
+
+      if (roll === 10 && frame[0] != 10) {
+        this.sparePoints = 10 + frame[0];
+        this.total.push(this.sparePoints, 0);
+        this.spareCount += 1;
+      }
+    });
   }
 
   finalScore() {
-    const points = this.scoresheet.flat();
+    const points = this.total.flat();
     const totalPoints = points.reduce((a, b) => a + b, 0);
-    console.log(totalPoints);
-    console.log(points);
     return `Final Score: ${totalPoints}`;
   }
 }
--- my-project/test/bowlingScore.test.js
+++ my-project/test/bowlingScore.test.js
@@ -3,6 +3,7 @@ const BowlingScore = require("../lib/bowlingScore");
 describe("BowlingScore", () => {
   it("returns the score for one frame", () => {
     const bowlingScore = new BowlingScore([[4, 2]]);
+    bowlingScore.frame();
     expect(bowlingScore.finalScore()).toEqual("Final Score: 6");
   });
 
@@ -12,6 +13,7 @@ describe("BowlingScore", () => {
       [3, 3],
       [1, 1],
     ]);
+    bowlingScore.frame();
     expect(bowlingScore.finalScore()).toEqual("Final Score: 14");
   });
 
@@ -28,10 +30,11 @@ describe("BowlingScore", () => {
       [0, 0],
       [0, 0],
     ]);
+    bowlingScore.frame();
     expect(bowlingScore.finalScore()).toEqual("Final Score: 0");
   });
 
-  it("returns the final score of the players gutter game", () => {
+  it("returns the final score of players game without spares and strikes", () => {
     const bowlingScore = new BowlingScore([
       [1, 1],
       [1, 1],
@@ -44,6 +47,25 @@ describe("BowlingScore", () => {
       [1, 1],
       [1, 1],
     ]);
+    bowlingScore.frame();
     expect(bowlingScore.finalScore()).toEqual("Final Score: 20");
+  });
+
+  it("returns the final score including points from one spare scored in the game", () => {
+    const bowlingScore = new BowlingScore([
+      [0, 0],
+      [6, 4],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+      [0, 0],
+    ]);
+    bowlingScore.frame();
+    bowlingScore.spare();
+    expect(bowlingScore.finalScore()).toEqual("Final Score: 16");
   });
 });

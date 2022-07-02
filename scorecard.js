d4c8db91d29e77399a8a36b61ad218097be7c3dc
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-02 16:49:35 +0100

    returns zero for gutter game

--- scorecard.js
+++ scorecard.js
@@ -1,13 +1,10 @@
 class Scorecard {
 
-
-  add_score(frame) {
-    return this.frame_total(frame[0], frame[1])
+  total_score() {
+    return 0
   }
 
-  frame_total(roll1, roll2) {
-    return roll1 + roll2;
-  }
+
 }
 
 
--- scorecard.test.js
+++ scorecard.test.js
@@ -1,13 +1,13 @@
 const Scorecard = require('./scorecard');
 
 describe(Scorecard, () => {
-  it("records score for 1 frame when user scores 3 and 6", () => {
-    const scorecard = new Scorecard();
-    expect(scorecard.add_score([3,6])).toBe(9)
+  it("returns score for gutter game", () => {
+    arr = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
+    const scorecard = new Scorecard(arr);
+    expect(scorecard.total_score()).toBe(0);
   })
 
-  it("records score for 1 frame when user scores 4 and 4", () => {
-    const scorecard = new Scorecard();
-    expect(scorecard.add_score([4,4])).toBe(8)
-  })
+  
+
+ 
 })

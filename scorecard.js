abc0beb61df018a6091d50c736994a02f8910eec
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-03 21:22:08 +0100

    updated next frame variable

--- scorecard.js
+++ scorecard.js
@@ -27,26 +27,26 @@ class Scorecard {
   total_score() {
     let total = 0;
     for (let i = 0; i < this.scores.length; i++) {
-      let roll1 = this.scores[i][0] 
-      // let nextFrameRoll1 = this.scores
+      let roll1 = this.scores[i][0];
+      let nextFrame = this.scores[i + 1];
 
       if(i == 9 && roll1 == 10) {
         total += this.frame_total(this.scores[i]);
       } else if(i == 8 && roll1 == 10) {
-        total += this.scores[i + 1][0] + this.scores[i + 1][1] + 10;
-      } else if(roll1 == 10 && this.scores[i + 1][0] == 10) {
+        total += nextFrame[0] + nextFrame[1] + 10;
+      } else if(roll1 == 10 && nextFrame[0] == 10) {
         if(i < 7) {
           total += this.frame_total(this.scores[i + 2]) + 20;
         } else if(i == 7) {
           total += this.frame_total(this.scores[i + 2]) + 20;
         } else {
-          total += this.frame_total(this.scores[i + 1]) + 10;
+          total += this.frame_total(nextFrame) + 10;
         }
         
       } else if(this.is_strike(roll1)) {
-        total += this.frame_total(this.scores[i + 1]) + 10;
+        total += this.frame_total(nextFrame) + 10;
       } else if(this.is_spare(this.frame_total(this.scores[i]))) {
-        total += this.frame_total(this.scores[i]) + this.scores[i + 1][0];
+        total += this.frame_total(this.scores[i]) + nextFrame[0];
       } else {
         total += this.frame_total(this.scores[i]);
       }

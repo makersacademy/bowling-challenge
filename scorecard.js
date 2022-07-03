133e76af64eeb42511ba18faffec707c8e3f99fe
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-07-03 21:24:00 +0100

    updated current frame variable

--- scorecard.js
+++ scorecard.js
@@ -28,10 +28,11 @@ class Scorecard {
     let total = 0;
     for (let i = 0; i < this.scores.length; i++) {
       let roll1 = this.scores[i][0];
+      let currentFrame = this.scores[i];
       let nextFrame = this.scores[i + 1];
 
       if(i == 9 && roll1 == 10) {
-        total += this.frame_total(this.scores[i]);
+        total += this.frame_total(currentFrame);
       } else if(i == 8 && roll1 == 10) {
         total += nextFrame[0] + nextFrame[1] + 10;
       } else if(roll1 == 10 && nextFrame[0] == 10) {
@@ -45,10 +46,10 @@ class Scorecard {
         
       } else if(this.is_strike(roll1)) {
         total += this.frame_total(nextFrame) + 10;
-      } else if(this.is_spare(this.frame_total(this.scores[i]))) {
-        total += this.frame_total(this.scores[i]) + nextFrame[0];
+      } else if(this.is_spare(this.frame_total(currentFrame))) {
+        total += this.frame_total(currentFrame) + nextFrame[0];
       } else {
-        total += this.frame_total(this.scores[i]);
+        total += this.frame_total(currentFrame);
       }
       console.log(i, total)
     }

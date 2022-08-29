7e82e451cd30f607d44e71f8a47da097e6a8d63e
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-29 21:13:51 +0100

    some refactoring of ScoreCard class

--- scorecard.js
+++ scorecard.js
@@ -3,33 +3,30 @@ const Game = require('./game')
 class ScoreCard {
   constructor(game = (new Game)) {
     this.game = game
-    this.framescores = [];
-    this.totalisers = []
   }
 
   currentScore() {
-    this.frameScore()
-    this.addbonus()
-    this.totaliser()
+    const scores = this.frameScore()
     let result = ""
-    for (let i = 0; i < this.totalisers.length; i++) {
+    for (let i = 0; i < scores.length; i++) {
       result += (
         `--------------\n` +
         `Frame: ${i + 1}\n` +
         `Pins: ${this.pins(this.game.game[i])}\n` +
-        `Running Score: ${this.totalisers[i]}\n`
+        `Running Score: ${scores[i]}\n`
       );
     }
 
     if (this.game.framenum() == 10 && this.game.currentframe().done()) {
       result += (
         `--------------\n` +
-        `Final Total: ${this.totalisers[this.totalisers.length - 1]}`
+        `Final Total: ${scores[scores.length - 1]}`
       )
     }
     return result
   }
 
+  // display pins knocked down in a frame
   pins(frame) {
     let pins = `${frame.frame[0]}`
     if (frame.frame.length > 1) {
@@ -42,22 +39,22 @@ class ScoreCard {
     return pins
   }
 
+  // score each frame without strike or spare bonus
   frameScore() {
-    this.framescores = []
+    let list = []
     for (let i = 0; i < this.game.framenum(); i++) {
-      this.framescores.push(this.game.game[i].score())
+      list.push(this.game.game[i].score())
     }
+    return this.addbonus(list)
   }
 
-  totaliser() {
-    this.totalisers = []
+  // totalise scores per frame
+  totaliser(bonusadded) {
+    let list = [0]
     for (let i = 0; i < this.game.framenum(); i++) {
-      if (i == 0) {
-        this.totalisers.push(this.framescores[i])
-      } else {
-        this.totalisers.push(this.framescores[i] + this.totalisers[i - 1])
-      }
+      list.push(bonusadded[i] + list[i])
     }
+    return list.slice(1)
   }
 
   getframes() {
@@ -66,13 +63,14 @@ class ScoreCard {
     return frames
   }
 
-  addbonus() {
+  // add strike and spare bonus to relevant frames
+  addbonus(framescores) {
     let frames = this.getframes()
     // add the next roll if a spare
     for (let i = 0; i < this.game.framenum() - 1; i++) {
       if (frames[i].spare()) {
         // add next roll to all results
-        this.framescores[i] += (frames[i + 1].frame[0]);
+        framescores[i] += (frames[i + 1].frame[0]);
       }
     }
 
@@ -86,10 +84,10 @@ class ScoreCard {
         const canFlatten = forStrike.map((item) => { return item.frame; })
         const flattened = canFlatten.flat();
         // add next two rolls
-        this.framescores[i] += (flattened[0] + flattened[1]);
+        framescores[i] += (flattened[0] + flattened[1]);
       }
     }
-
+    return this.totaliser(framescores)
   }
 };
 

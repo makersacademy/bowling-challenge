752e23838973c1a4e17890fa68b09e9e3be743a1
naomischlosser <naomi_schlosser@hotmail.com>
2022-08-29 12:55:26 +0100

    TDD score per frame

--- score.js
+++ score.js
@@ -7,8 +7,26 @@ class Score {
   
   add(frame) {
     this.frames.push(frame);
-    return this.frames;
   };
+
+  forFrame(number) {
+    if (this.frames[number - 1].isStrike()) {
+      if (this.frames[number].isStrike()) {
+        return 20 + this.frames[number + 1].roll_1
+      }
+      return 10 + this.frames[number].getSum();
+
+    } else if (this.frames[number - 1].isSpare()) {
+      return 10 + this.frames[number].roll_1
+
+    } else {
+      return this.frames[number - 1].getSum();
+    };
+  };
+
+  // total() {
+
+  // };
 };
 
 module.exports = Score;
--- score.test.js
+++ score.test.js
@@ -1,10 +1,45 @@
 const Score = require('./score');
 
 describe('Score', () => {
-  it('returns an array of frames', () => {
+  it('returns the result when player plays two frames', () => {
     const score = new Score();
-    score.add([2,5]);
-    result = score.add([6,3]);
-    expect(result).toEqual([[2,5], [6,3]]);
+    const mockFrame_1 = { getSum: () => 5, isSpare: () => false, isStrike: () => false }
+    const mockFrame_2 = { getSum: () => 8, isSpare: () => false, isStrike: () => false }
+    score.add(mockFrame_1);
+    score.add(mockFrame_2);
+
+    expect(score.forFrame(1)).toBe(5);
+    expect(score.forFrame(2)).toBe(8);
+  });
+
+  it('returns the result when player plays two frames with the first one being a strike', () => {
+    const score = new Score();
+    const mockFrame_1 = { getSum: () => 10, isSpare: () => false, isStrike: () => true }
+    const mockFrame_2 = { getSum: () => 8, isSpare: () => false, isStrike: () => false }
+    score.add(mockFrame_1);
+    score.add(mockFrame_2);
+
+    expect(score.forFrame(1)).toBe(18);
+  });
+
+  it('returns the result when player plays three frames with the first two being a strike', () => {
+    const score = new Score();
+    const mockFrame_1 = { getSum: () => 10, isSpare: () => false, isStrike: () => true }
+    const mockFrame_2 = { roll_1: 1, roll_2: 2, getSum: () => 3, isSpare: () => false, isStrike: () => false }
+    score.add(mockFrame_1);
+    score.add(mockFrame_1);
+    score.add(mockFrame_2);
+
+    expect(score.forFrame(1)).toBe(21);
+  });
+
+  it('returns the result when player plays two frames with the first two being a spare', () => {
+    const score = new Score();
+    const mockFrame_1 = { roll_1: 5, roll_2: 5, getSum: () => 10, isSpare: () => true, isStrike: () => false }
+    const mockFrame_2 = { roll_1: 1, roll_2: 2, getSum: () => 3, isSpare: () => false, isStrike: () => false }
+    score.add(mockFrame_1);
+    score.add(mockFrame_2);
+
+    expect(score.forFrame(1)).toBe(11);
   });
 });

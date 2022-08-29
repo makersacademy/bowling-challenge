d728853726b2a34dc448aecc5587677072094ebf
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-29 14:39:39 +0100

    messy code but does add in spares

--- scorecard.js
+++ scorecard.js
@@ -4,25 +4,27 @@ class ScoreCard {
   constructor(game = (new Game)) {
     this.game = game
     this.framescores = [];
+    this.totalisers = []
   }
 
   currentScore() {
     this.frameScore()
     this.addbonus()
+    this.totaliser()
     let result = ""
-    for (let i = 0; i < this.framescores.length; i++) {
+    for (let i = 0; i < this.totalisers.length; i++) {
       result += (
         `--------------\n` +
         `Frame: ${i + 1}\n` +
         `Pins: ${this.pins(this.game.game[i])}\n` +
-        `Running Score: ${this.framescores[i]}\n`
+        `Running Score: ${this.totalisers[i]}\n`
       );
     }
 
     if (this.game.framenum() == 10 && this.game.currentframe().done()) {
       result += (
         `--------------\n` +
-        `Final Total: ${this.framescores[this.framescores.length - 1]}`
+        `Final Total: ${this.totalisers[this.totalisers.length - 1]}`
       )
     }
     return result
@@ -39,18 +41,37 @@ class ScoreCard {
   frameScore() {
     this.framescores = []
     for (let i = 0; i < this.game.framenum(); i++) {
-      let lastFrame = 0
-      if (i > 0) {
-        lastFrame = this.framescores[i - 1]
-      }
-      const thisFrame = this.game.game[i].score()
-      this.framescores.push(lastFrame + thisFrame)
+      this.framescores.push(this.game.game[i].score())
     }
   }
 
-  addbonus() {
+  totaliser() {
+    this.totalisers = []
+    for (let i = 0; i < this.game.framenum(); i++) {
+      if (i == 0) {
+        this.totalisers.push(this.framescores[i])
+      } else {
+        this.totalisers.push(this.framescores[i] + this.totalisers[i - 1])
+      }
+    }
+  }
+
+  getframes() {
     let frames = []
-    // f this!
+    this.game.game.forEach((frame) => { frames.push(frame) })
+    return frames
+  }
+
+  addbonus() {
+    let frames = this.getframes()
+    // add spares to this.framescores
+    // don't process the last available frame
+    for (let i = 0; i < this.game.framenum() - 1; i++) {
+      if (frames[i].spare()) {
+        // add next roll to all results
+        this.framescores[i] += (frames[i + 1].frame[0]);
+      }
+    }
   }
 };
 

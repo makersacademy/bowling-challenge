7ce35fb3c4ada8257bf2b6a763b9d9aced87405a
ritaaktay <ritaaktay@gmail.com>
2022-09-27 16:29:38 +0100

    elaborate draft with class design and functions, not tested

---
+++ run.js
@@ -0,0 +1,10 @@
+const UserInterface = require("./lib/userInterface");
+const Frame = require("./lib/frame");
+const Formatter = require("./lib/formatter");
+const ScoringTable = require("./lib/scoringTable");
+
+const scoringTable = new ScoringTable();
+const formatter = new Formatter(scoringTable);
+const userInterface = new UserInterface(Frame, scoringTable, formatter);
+
+userInterface.run();

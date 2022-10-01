a49fbc867feeb30443ec8ff0cd4b9159b6e5eab5
Jovi <kwong.jovi@gmail.com>
2022-10-01 19:08:39 +0100

    feat(Frame) - add #setBonus(), add 'spare' and 'strike' as instance variables

--- app/Frame.js
+++ app/Frame.js
@@ -1,10 +1,21 @@
 class Frame {
   constructor() {
+    // eventually separate frame score?
+    // i.e. base score vs bonus score
     this.score = 0;
     this.scoreThrow1 = 0;
     this.scoreThrow2 = 0;
-    this.scoreThrow3= 0;
-    this.bonus = 'TBC';
+    this.scoreThrow3 = 0;
+    this.spare = false;
+    this.strike = false;
+  }
+
+  setBonus() {
+    if (this.scoreThrow1 === 10) {
+      this.strike = true;
+    } else if (this.scoreThrow1 + this.scoreThrow2 === 10) {
+      this.spare = true;
+    }
   }
 };
 
--- test/Frame.test.js
+++ test/Frame.test.js
@@ -11,22 +11,34 @@ describe('Frame class', () => {
       expect(frame.scoreThrow1).toEqual(0);
       expect(frame.scoreThrow2).toEqual(0);
       expect(frame.scoreThrow3).toEqual(0);
-      expect(frame.bonus).toEqual('TBC');
+      expect(frame.spare).toEqual(false);
+      expect(frame.strike).toEqual(false);
     });
   });
   
-  describe('frame attributes during game', () => {
-    it('can be updated', () => {
-      frame.score = 1;
-      expect(frame.score).toEqual(1);
-      frame.scoreThrow1 = 2;
-      expect(frame.scoreThrow1).toEqual(2);
-      frame.scoreThrow2 = 3;
-      expect(frame.scoreThrow2).toEqual(3);
-      frame.scoreThrow3 = 4;
-      expect(frame.scoreThrow3).toEqual(4);
-      frame.bonus = 'strike';
-      expect(frame.bonus).toEqual('strike');
+  describe('#setBonus', () => {
+    it('assigns strike as true', () => {
+      frame.scoreThrow1 = 10;
+      frame.setBonus();
+      expect(frame.scoreThrow1).toEqual(10);
+      expect(frame.strike).toEqual(true);
+    });
+    it('assigns spare as true', () => {
+      frame.scoreThrow1 = 5;
+      frame.scoreThrow2 = 5;
+      frame.setBonus();
+      expect(frame.scoreThrow1).toEqual(5);
+      expect(frame.scoreThrow2).toEqual(5);
+      expect(frame.spare).toEqual(true);
+    });
+    it('does not assign strike or spare as true', () => {
+      frame.scoreThrow1 = 0;
+      frame.scoreThrow2 = 1;
+      frame.setBonus();
+      expect(frame.scoreThrow1).toEqual(0);
+      expect(frame.scoreThrow2).toEqual(1);
+      expect(frame.spare).toEqual(false);
+      expect(frame.strike).toEqual(false);
     });
   });
 });

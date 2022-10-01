96f9669a90e9052215cf348f73a0a552e22b9e17
Jovi <kwong.jovi@gmail.com>
2022-10-01 21:43:38 +0100

    feat(Frame and Scorecard) - Scorecard to handle assignment of attribute values to Frame instances. Pass tests for 1 frame (strike/spare/less than 10 points)

--- app/Scorecard.js
+++ app/Scorecard.js
@@ -12,7 +12,60 @@ class Scorecard {
     this.frame8 = new Frame();
     this.frame9 = new Frame();
     this.frame10 = new Frame();
+    this.currentFrame = 0;
   }
+  
+  setCurrentFrame(frame) {
+    
+    // set up so frame instance is only set once!
+    if (frame === 1) {
+      this.currentFrame = this.frame1;
+    } else if (frame === 2) {
+      this.currentFrame = this.frame2;
+    } else if (frame === 3) {
+      this.currentFrame = this.frame3;
+    } else if (frame === 4) {
+      this.currentFrame = this.frame4;
+    } else if (frame === 5) {
+      this.currentFrame = this.frame5;
+    } else if (frame === 6) {
+      this.currentFrame = this.frame6;
+    } else if (frame === 7) {
+      this.currentFrame = this.frame7;
+    } else if (frame === 8) {
+      this.currentFrame = this.frame8;
+    } else if (frame === 9) {
+      this.currentFrame = this.frame9;
+    } else if (frame === 10) {
+      this.currentFrame = this.frame10;
+    }
+  }
+  
+  throw1(pins) {
+    this.currentFrame.scoreThrow1 = pins
+  }
+
+  throw2(pins) {
+    this.currentFrame.scoreThrow2 = pins
+  }
+
+  updateFrameScore(points) {
+    this.currentFrame.score = points;
+  }
+
+  setFrameBonusType() {
+    this.currentFrame.setBonus();
+  }
+  
+
+
+
+  // // reserved for frame 10 logic
+  // throw3(pins) {
+
+  // }
+  
+
 };
 
 module.exports = Scorecard;
--- test/Scorecard.test.js
+++ test/Scorecard.test.js
@@ -5,13 +5,63 @@ beforeEach( () => {
 });
 
 describe('Scorecard class', () => {
-  describe('frame score at start of game', () => {
-    it('to equal zero', () => {
+  describe('frame attributes - before game', () => {
+    it('to be default values before throw1', () => {
       expect(scorecard.frame1.score).toEqual(0);
       expect(scorecard.frame1.scoreThrow1).toEqual(0);
       expect(scorecard.frame1.scoreThrow2).toEqual(0);
       expect(scorecard.frame1.scoreThrow3).toEqual(0);
-      expect(scorecard.frame1.bonus).toEqual('TBC');
+      expect(scorecard.frame1.spare).toEqual(false);
+      expect(scorecard.frame1.strike).toEqual(false);
+    });
+  });
+
+  describe('frame attributes - during game', () => {
+    it('1 frame, 2 throws, no spare', () => {
+      scorecard.setCurrentFrame(1);
+      scorecard.throw1(0);
+      scorecard.throw2(1);
+      scorecard.updateFrameScore(
+        scorecard.frame1.scoreThrow1 + scorecard.frame1.scoreThrow2
+      );
+      scorecard.setFrameBonusType();
+
+      expect(scorecard.currentFrame).toEqual(scorecard.frame1);
+      expect(scorecard.frame1.score).toEqual(1);
+      expect(scorecard.frame1.scoreThrow1).toEqual(0);
+      expect(scorecard.frame1.scoreThrow2).toEqual(1);
+      expect(scorecard.frame1.spare).toEqual(false);
+      expect(scorecard.frame1.strike).toEqual(false);
+    });
+
+    it('1 frame, 2 throws, spare', () => {
+      scorecard.setCurrentFrame(1);
+      scorecard.throw1(5);
+      scorecard.throw2(5);
+      scorecard.updateFrameScore(
+        scorecard.frame1.scoreThrow1 + scorecard.frame1.scoreThrow2
+      );
+      scorecard.setFrameBonusType();
+
+      expect(scorecard.currentFrame).toEqual(scorecard.frame1);
+      expect(scorecard.frame1.score).toEqual(10);
+      expect(scorecard.frame1.scoreThrow1).toEqual(5);
+      expect(scorecard.frame1.scoreThrow2).toEqual(5);
+      expect(scorecard.frame1.spare).toEqual(true);
+      expect(scorecard.frame1.strike).toEqual(false);
+    });
+
+    it('1 frame, 1 throw, strike', () => {
+      scorecard.setCurrentFrame(1);
+      scorecard.throw1(10);
+      scorecard.updateFrameScore(scorecard.frame1.scoreThrow1);
+      scorecard.setFrameBonusType();
+
+      expect(scorecard.currentFrame).toEqual(scorecard.frame1);
+      expect(scorecard.frame1.score).toEqual(10);
+      expect(scorecard.frame1.scoreThrow1).toEqual(10);
+      expect(scorecard.frame1.spare).toEqual(false);
+      expect(scorecard.frame1.strike).toEqual(true);
     });
   });
 });

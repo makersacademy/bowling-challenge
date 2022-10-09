6fb8d62ac2830da59b9cc91df6202eecd5f487ed
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-10-09 23:19:35 +0100

    Refactor BowlingScorecard class
    Refactor & TDD class BowlingScorecard so that it calculates the score for the game only."

--- bowlingScorecard.js
+++ bowlingScorecard.js
@@ -1,105 +1,39 @@
 class BowlingScorecard{
-  constructor(){
-    this.frame = 0;   // up to 10 frames
-    this.roll = 0;   // can be either 0 or 1 - 2 rolls per frame
-    this.score = 0;   // updated
-    this.pinsDown = 0;  // represent the number of pins knocked down
-    this.gameArray = [[],[],[],[],[],[],[],[],[],[]];
-  }
-  
-  userInput(number) {
-    this.pinsDown = number
+  constructor(scoreboard){
+    this._scoreboard = scoreboard;
+    this._score = 0;
   }
 
-  frameRollLogic() {
-    if (this.roll === 0 && this.pinsDown === 10) {
-      this.frame +=1
-    } else if (this.roll === 1) {
-      this.roll = 0;
-      this.frame += 1;
-    } else {
-      this.roll += 1;
-    }
+  getScore() {
+    return this._score;
   }
 
-  populateGameArray() {
-    if (this.frame !== 9) {
-      this.gameArray[this.frame].push(this.pinsDown)
-      this.frameRollLogic()
-    } else { //round 10 includes three rolls. to add validation
-       if (this.gameArray[this.frame].length < 4) {  
-          this.gameArray[this.frame].push(this.pinsDown);
-      }
-    }
+  calculateScore() {
+    this.calculateScoreFromPinsDown();
+    this.calculateBonuses();
+  }
+
+  // score from pins down
+  calculateScoreFromPinsDown() {
+    this._score += (this._scoreboard.flat().reduce((sum, pins) => sum + pins));
   }
   
-  getGameArray() {
-    return this.gameArray;
-  }
-  
-  scorecard() {
-    // score from pins down
-    this.score = (this.gameArray.flat().reduce((sum, pins) => sum + pins));
-    // score from bonuses
-    let bonus = 0
-    for (let i = 0; i < 9; i++ ) {
+  // score from bonuses
+  calculateBonuses(){
+    const arrayLength = this._scoreboard.length < 9 ? this._scoreboard.length : 9
+    for (let i = 0; i < arrayLength; i++ ) {
       //bonuses from strikes
-      if (this.gameArray[i].length === 1) {
-        bonus += (this.gameArray[i+1][0] + this.gameArray[i+1][1]);
-        //bonuses from spares
-      } else if  (this.gameArray[i].length === 2 && this.gameArray[i][0] + this.gameArray[i][1] === 10) {
-        bonus += this.gameArray[i+1][0];
+      if (this._scoreboard[i].length === 1) {
+        const nextFrameScore = this._scoreboard[i+1][0] + this._scoreboard[i+1][1];
+        this._score += nextFrameScore;
+      
+      //bonuses from spares
+      } else if (this._scoreboard[i].length === 2 && this._scoreboard[i][0] + this._scoreboard[i][1] === 10) {
+        const nextThrowScore = this._scoreboard[i+1][0];
+        this._score += nextThrowScore;
       }
     }
-    this.score += bonus
-  }
-
-  getScore(){
-    return this.score
   }
 }
 
 module.exports = BowlingScorecard;
-
-// game = new BowlingScorecard;
-// game.userInput(1) //f0
-// game.populateGameArray()
-// game.userInput(4)
-// game.populateGameArray()
-// game.userInput(4)  //f1
-// game.populateGameArray()
-// game.userInput(5)
-// game.populateGameArray()
-// game.userInput(6)  //f2
-// game.populateGameArray()
-// game.userInput(4)
-// game.populateGameArray()
-// game.userInput(5)  //f3
-// game.populateGameArray()
-// game.userInput(5)
-// game.populateGameArray()
-// game.userInput(10) //f4
-// game.populateGameArray()
-// game.userInput(0) //f5
-// game.populateGameArray()
-// game.userInput(1)
-// game.populateGameArray()
-// game.userInput(7)  //f6
-// game.populateGameArray()
-// game.userInput(3)
-// game.populateGameArray()
-// game.userInput(6) //f7
-// game.populateGameArray()
-// game.userInput(4)
-// game.populateGameArray()
-// game.userInput(10)   //f8
-// game.populateGameArray()
-// game.userInput(2)  //f9
-// game.populateGameArray()
-// game.userInput(8) 
-// game.populateGameArray()
-// game.userInput(6)
-// game.populateGameArray()
-// game.scorecard()
-// console.log(game.getGameArray())
-// console.log(game.getScore())
--- bowlingScorecard.test.js
+++ bowlingScorecard.test.js
@@ -1,53 +1,64 @@
 const BowlingScorecard = require('./bowlingScorecard')
 
 describe('BowlingScorecard', () => {
-  it('creates a game table and returns a tolal score', () => {
-    const game = new BowlingScorecard;
-    game.userInput(1); //f0
-    game.populateGameArray();
-    game.userInput(4);
-    game.populateGameArray();
-    game.userInput(4);  //f1
-    game.populateGameArray();
-    game.userInput(5);
-    game.populateGameArray();
-    game.userInput(6);  //f2
-    game.populateGameArray();
-    game.userInput(4);
-    game.populateGameArray();
-    game.userInput(5);  //f3
-    game.populateGameArray();
-    game.userInput(5);
-    game.populateGameArray();
-    game.userInput(10); //f4
-    game.populateGameArray();
-    game.userInput(0); //f5
-    game.populateGameArray();
-    game.userInput(1);
-    game.populateGameArray();
-    game.userInput(7);  //f6
-    game.populateGameArray();
-    game.userInput(3);
-    game.populateGameArray();
-    game.userInput(6); //f7
-    game.populateGameArray();
-    game.userInput(4);
-    game.populateGameArray();
-    game.userInput(10);   //f8
-    game.populateGameArray();
-    game.userInput(2);  //f9
-    game.populateGameArray();
-    game.userInput(8) ;
-    game.populateGameArray();
-    game.userInput(6);
-    game.populateGameArray();
-    game.scorecard();
-    expect(game.getGameArray()).toEqual([ [ 1, 4 ], [ 4, 5 ],
-        [ 6, 4 ], [ 5, 5 ],
-        [ 10 ],   [ 0, 1 ],
-        [ 7, 3 ], [ 6, 4 ],
-        [ 10 ],   [ 2, 8, 6 ]]);
-    expect(game.getScore()).toBe(133);
+  it ('calculates score for one pin down', () => {
+    const scoreboard = [ [ 1]];
+  
+    const scoring = new BowlingScorecard(scoreboard);
+    scoring.calculateScoreFromPinsDown();
+  
+    expect(scoring.getScore()).toBe(1);
   });
 
-});
+  it ('calculates score for two pins down', () => {
+    const scoreboard = [ [1, 2]];
+  
+    const scoring = new BowlingScorecard(scoreboard);
+    scoring.calculateScoreFromPinsDown();
+  
+    expect(scoring.getScore()).toBe(3);
+  });
+
+  it ('calculates score for the pins down for the whole game', () => {
+    const scoreboard = [ [ 0, 4 ], [ 4, 5 ],
+      [ 3, 4 ], [ 2, 5 ], 
+      [ 1, 0 ], [ 0, 1 ], 
+      [ 1, 3 ], [ 5, 4 ], 
+      [ 0, 1 ], [ 2, 4 ]];
+  
+    const scoring = new BowlingScorecard(scoreboard);
+    scoring.calculateScoreFromPinsDown();
+  
+    expect(scoring.getScore()).toBe(49);
+  });
+
+  it ('calculates bonus for strike', () => {
+    const scoreboard = [ [10], [1, 2]];
+  
+    const scoring = new BowlingScorecard(scoreboard);
+    scoring.calculateBonuses();
+    expect(scoring.getScore()).toBe(3);
+  });
+
+  it ('calculates bonus for spares', () => {
+    const scoreboard = [ [2, 8], [1, 2]];
+  
+    const scoring = new BowlingScorecard(scoreboard);
+    scoring.calculateBonuses();
+    expect(scoring.getScore()).toBe(1);
+  });
+
+
+  it ('calculates the score for pins and bonuses', () => {
+    const scoreboard = [ [ 1, 4 ], [ 4, 5 ],
+      [ 6, 4 ], [ 5, 5 ], 
+      [ 10 ], [ 0, 1 ], 
+      [ 7, 3 ], [ 6, 4 ], 
+      [ 10 ], [ 2, 8, 6 ]];
+
+    const scoring = new BowlingScorecard(scoreboard);
+    scoring.calculateScore();
+    
+    expect(scoring.getScore()).toBe(133);
+  });
+})

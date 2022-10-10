3649f0b1cf21c7e2c71ca4eac2f9105033945551
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-10-10 13:29:04 +0100

    Refactor inputValidation class

--- inputValidation.js
+++ inputValidation.js
@@ -10,28 +10,22 @@ class InputValidation {
   validateInput(userInput, scoreboard) {
     this._error = null;
     this.checkRange(userInput);
-    if (this._error === null) {
-      this.checkPinsLeft(scoreboard, userInput);
-    }
+    if (this._error !== null) return;
+    this.checkPinsLeft(scoreboard, userInput);
   }
 
   // players can input only the number ranging from 0 to 10
   checkRange(userInput) {
-    this._error = null;
     const acceptedRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    if (!acceptedRange.includes(userInput)) {
-      this._error = "Please enter a whole number from 0 to 10. ";
-    } 
+    if (acceptedRange.includes(userInput)) return;
+    this._error = "Please enter a whole number from 0 to 10. ";
   }
 
   // the total number of knocked down pins can't exceed 10 per frame.
   checkPinsLeft(scoreboard, userInput) {
     const pinsLeft = this.pinsAvailable(scoreboard);
-    if (userInput <= pinsLeft){
-      this._error = null;
-    } else {
-      this._error = `You have only ${pinsLeft} ${pinsLeft === 1 ? "pin" : "pins"} left. `;
-    }
+    if (userInput <= pinsLeft) return;
+    this._error = `You have only ${pinsLeft} ${pinsLeft === 1 ? "pin" : "pins"} left. `;
   }
 
   pinsAvailable(scoreboard) {

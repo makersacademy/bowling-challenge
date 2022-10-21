393a02a93ee6d1d2ec85152736222b04383065cf
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-21 17:07:56 +0100

    Add failing test to account for three strikes on a row

--- GameScore.test.js
+++ GameScore.test.js
@@ -32,7 +32,7 @@ describe('gamescore', () => {
       expect(gameScore.frameScores).toStrictEqual([7, 10]);
     });
 
-    it('adds the bonus points when strike', () => {
+    it('adds the bonus points when strike on the last round', () => {
       let gameScore = new GameScore();
       const fakeFrame1 = {
         framePins: () => [10],
@@ -50,6 +50,60 @@ describe('gamescore', () => {
       gameScore.addFrameScore(fakeFrame2);
 
       expect(gameScore.frameScores).toStrictEqual([17, 7]);
+    });
+
+    it('adds the bonus points when tree strikes on a row', () => {
+      let gameScore = new GameScore();
+      const fakeFrame1 = {
+        framePins: () => [10],
+        isStrike: () => true,
+        isSpare: () => false,
+      };
+
+      const fakeFrame2 = {
+        framePins: () => [10],
+        isStrike: () => true,
+        isSpare: () => false,
+      };
+
+      const fakeFrame3 = {
+        framePins: () => [10],
+        isStrike: () => true,
+        isSpare: () => false,
+      };
+
+      const fakeFrame4 = {
+        framePins: () => [2, 5],
+        isStrike: () => false,
+        isSpare: () => false,
+      };
+
+      gameScore.addFrameScore(fakeFrame1);
+      gameScore.addFrameScore(fakeFrame2);
+      gameScore.addFrameScore(fakeFrame3);
+      gameScore.addFrameScore(fakeFrame4);
+
+      expect(gameScore.frameScores).toStrictEqual([30, 22, 17, 7]);
+    });
+
+    it('adds the bonus points when spare on the last round', () => {
+      let gameScore = new GameScore();
+      const fakeFrame1 = {
+        framePins: () => [6, 4],
+        isStrike: () => false,
+        isSpare: () => true,
+      };
+
+      const fakeFrame2 = {
+        framePins: () => [5, 2],
+        isStrike: () => false,
+        isSpare: () => false,
+      };
+
+      gameScore.addFrameScore(fakeFrame1);
+      gameScore.addFrameScore(fakeFrame2);
+
+      expect(gameScore.frameScores).toStrictEqual([15, 7]);
     });
   });
 });

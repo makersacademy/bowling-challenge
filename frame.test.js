bbb0fdc497853789f31f01f962ae58c42bd6db02
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-10-21 14:32:31 +0100

    Add two more passing tests for isSpare()

--- frame.test.js
+++ frame.test.js
@@ -49,5 +49,26 @@ describe('frame', () => {
 
       expect(currentFrame.isSpare()).toEqual(true);
     });
+
+    it('returns false if frame is not a spare', () => {
+      let currentFrame = new Frame();
+
+      let pinsKnocked1 = 2;
+      currentFrame.addPins(pinsKnocked1);
+
+      let pinsKnocked2 = 7;
+      currentFrame.addPins(pinsKnocked2);
+
+      expect(currentFrame.isSpare()).toEqual(false);
+    });
+
+    it('returns false if frame is a strike', () => {
+      let currentFrame = new Frame();
+
+      let pinsKnocked1 = 10;
+      currentFrame.addPins(pinsKnocked1);
+
+      expect(currentFrame.isSpare()).toEqual(false);
+    });
   });
 });

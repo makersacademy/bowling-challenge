574da2268257746cb8ec12431b1fa6918e591bb4
eggs-benny <benedict.smith87@gmail.com>
2022-10-22 11:17:37 +0100

    tested addRoll & totalScore for open frames

--- test/frame.test.js
+++ test/frame.test.js
@@ -3,66 +3,85 @@ const Frame = require('../lib/frame');
 describe('Frame', () => {
   describe('#addRolls', () => {
     it('adds 2 rolls to rolls array', () => {
-      const frame = new Frame;
-      frame.addRolls(6)
-      frame.addRolls(1)
-      expect(frame.frameArr()).toEqual([6,1])
-    })
+      const frame = new Frame();
+      frame.addRolls(6);
+      frame.addRolls(1);
+      expect(frame.frameArr()).toEqual([6, 1]);
+    });
     it("doesn't add 3rd roll into frame", () => {
-      const frame = new Frame;
-      frame.addRolls(6)
-      frame.addRolls(1)
-      frame.addRolls(1)
-      expect(frame.frameArr()).toEqual([6,1])
-    })
-  })
+      const frame = new Frame();
+      frame.addRolls(6);
+      frame.addRolls(1);
+      frame.addRolls(1);
+      expect(frame.frameArr()).toEqual([6, 1]);
+    });
+  });
 
   describe('#sumRolls', () => {
     it('sums rolls in a frame to return a raw score', () => {
-      const frame = new Frame;
-      frame.addRolls(6)
-      frame.addRolls(1)
-      expect(frame.sumRolls()).toEqual(7)
-    })
-  })
+      const frame = new Frame();
+      frame.addRolls(6);
+      frame.addRolls(1);
+      expect(frame.sumRolls()).toEqual(7);
+    });
+  });
 
   describe('#isSpare', () => {
     it('returns true if sum = 10', () => {
-      const frame = new Frame;
-      frame.addRolls(9)
-      frame.addRolls(1)
-      expect(frame.isSpare()).toEqual(true)
-    })
+      const frame = new Frame();
+      frame.addRolls(9);
+      frame.addRolls(1);
+      expect(frame.isSpare()).toEqual(true);
+    });
     it('returns false if sum != 10', () => {
       const frame = new Frame();
-      frame.addRolls(6)
-      frame.addRolls(1)
-      expect(frame.isSpare()).toEqual(false)
-    })
+      frame.addRolls(6);
+      frame.addRolls(1);
+      expect(frame.isSpare()).toEqual(false);
+    });
     it('returns false if first roll = 10', () => {
       const frame = new Frame();
-      frame.addRolls(10)
-      expect(frame.isSpare()).toEqual(false)
-    })
-  })
+      frame.addRolls(10);
+      expect(frame.isSpare()).toEqual(false);
+    });
+  });
 
   describe('#isStrike', () => {
     it('returns first roll = 10', () => {
-      const frame = new Frame;
-      frame.addRolls(10)
-      expect(frame.isStrike()).toEqual(true)
-    })
+      const frame = new Frame();
+      frame.addRolls(10);
+      expect(frame.isStrike()).toEqual(true);
+    });
     it('returns false if first roll != 10', () => {
-      const frame = new Frame;
-      frame.addRolls(9)
-      frame.addRolls(1)
-      expect(frame.isStrike()).toEqual(false)
-    })
+      const frame = new Frame();
+      frame.addRolls(9);
+      frame.addRolls(1);
+      expect(frame.isStrike()).toEqual(false);
+    });
     it('returns false if second roll = 10', () => {
-      const frame = new Frame;
-      frame.addRolls(0)
-      frame.addRolls(10)
-      expect(frame.isStrike()).toEqual(false)
-    })
-  })
-})
+      const frame = new Frame();
+      frame.addRolls(0);
+      frame.addRolls(10);
+      expect(frame.isStrike()).toEqual(false);
+    });
+  });
+
+  describe('#isComplete', () => {
+    it('is true if frame has two rolls', () => {
+      const frame = new Frame();
+      frame.addRolls(6);
+      frame.addRolls(1);
+      expect(frame.isComplete()).toEqual(true);
+    });
+    it('is false if frame has one roll <10', () => {
+      const frame = new Frame();
+      frame.addRolls(6);
+      expect(frame.isComplete()).toEqual(false);
+    });
+    it("is true if frame's 1st roll = 10", () => {
+      const frame = new Frame();
+      frame.addRolls(10);
+      expect(frame.isComplete()).toEqual(true);
+    });
+  });
+});
--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -1,30 +1,17 @@
 const Scorecard = require('../lib/scorecard');
-// const Frame = require('../lib/frame')
 
 describe('Scorecard', () => {
-  describe('#scoreArr', () => {
-    xit('adds 2 open frame scores to score array', () => {
-      const scorecard = new Scorecard();
-      const dummyFrame1 = { sumRolls: () => 7 };
-      const dummyFrame2 = { sumRolls: () => 8 };
-      scorecard.sumScore(dummyFrame1);
-      scorecard.sumScore(dummyFrame2);
-      // scorecard.addRoll(7)
-      // scorecard.addRoll(1)
-      // scorecard.addRoll(1)
-      // scorecard.addRoll(1)
-      expect(scorecard.scoreArray()).toStrictEqual([7,8]);
+  describe('#totalScore', () => {
+    const scorecard = new Scorecard();
+    it('returns sum after first open frame', () => {
+      scorecard.addRoll(6);
+      scorecard.addRoll(1);
+      expect(scorecard.totalScore()).toEqual(7);
     });
-  });
-
-  describe('#sumScore', () => {
-    xit('sums two open frame scores', () => {
-      const scorecard = new Scorecard();
-      const dummyFrame1 = { sumRolls: () => 7 };
-      const dummyFrame2 = { sumRolls: () => 8 };
-      scorecard.sumScore(dummyFrame1);
-      scorecard.sumScore(dummyFrame2);
-      expect(scorecard.scoreArray()).toStrictEqual([7,8]);
+    it('then returns sum after 2nd open frame', () => {
+      scorecard.addRoll(5);
+      scorecard.addRoll(3);
+      expect(scorecard.totalScore()).toEqual(15);
     });
   });
 });

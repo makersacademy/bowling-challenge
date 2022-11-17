17dfbdd6d9b94e32c2d29024e61edde6a3e7731f
adonl <adnlwly@gmail.com>
2022-11-17 16:22:00 +0000

    add support for spare scores

--- bowlingScoring.js
+++ bowlingScoring.js
@@ -1,16 +1,34 @@
 class BowlingScoring {
   constructor(scorecard) {
     this.scorecard = scorecard;
+    this.score = [];
+    this.total = 0;
   }
 
   calculate() {
-    let total = 0
-    let score = this.scorecard.flat();
+    let rounds = this.scorecard.length;
+    let frame = 0;
 
-    for (let i = 0; i < score.length; i++) {
-      total += score[i];
+    while (rounds > 0) {
+      this.score.push(this.scorecard[frame][0]);
+      this.score.push(this.scorecard[frame][1]);
+
+      if (frame > 0 && this.scorecard[frame - 1][0] + this.scorecard[frame - 1][1] === 10) {
+        this.score.push(this.scorecard[frame][0]);
+      }
+
+      frame += 1;
+      rounds -= 1;
+
     }
-    return total;
+
+    console.log(this.score)
+
+    for (let i = 0; i < this.score.length; i++) {
+        this.total += this.score[i];
+    }
+
+    return this.total;
     
   }
 
--- bowlingScoring.test.js
+++ bowlingScoring.test.js
@@ -9,4 +9,16 @@ describe('BowlingScoring', () => {
     });
   });
 
+  describe('after the second frame', () => {
+    it('returns total score without a spare or strike', () => {
+      const scorecard = new BowlingScoring([[2, 3], [2, 5]]);
+      expect(scorecard.calculate()).toEqual(12);
+    });
+
+    it('returns total score with a spare in 1st frame', () => {
+      const scorecard = new BowlingScoring([[2, 8], [2, 5]]);
+      expect(scorecard.calculate()).toEqual(19);
+    });
+  });
+
 });

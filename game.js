a5ad78a7574f789691c800f56a5b87797144260a
Marina Papapetrou <contact@marinapapap.com>
2022-12-16 23:21:50 +0000

    calculates total score plus strike bonus (not in tenth frame)

--- game.js
+++ game.js
@@ -1,18 +1,27 @@
 class Game {
 
   constructor() {
-    this.frame = []
+    this.frames = []
   }
 
   addFrame(frame) {
-    this.frame.push(frame);
+    this.frames.push(frame);
+  }
+
+  strikeBonuses() {
+    this.strikeBonus = 0;
+    this.frames.forEach((x, i) => x.strike() ? this.strikeBonus += this.frames[i+1].score() : null);
+    return this.strikeBonus;
   }
 
   totalScore() {
     let total = 0;
-    this.frame.forEach(x => total += x.score());
-    return total;
+    this.frames.forEach(x => total += x.score());
+    return total + this.strikeBonuses();
   }
 }
 
+
 module.exports = Game;
+
+

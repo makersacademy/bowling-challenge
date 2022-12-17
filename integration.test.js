0def93e9751a71fd1d309b28af04f19a9ac8be2d
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-12-17 17:28:26 +0000

    Scoreboard tested, without error tests

--- integration.test.js
+++ integration.test.js
@@ -77,29 +77,160 @@ describe("bowler bowls a scoring game, but no strikes or spares", () => {
 
   it("adds frames with varying scores", () => {
     const scoreboard = new Scoreboard
-    let frame = new Frame(1, 8)
-    scoreboard.addFrame(frame)
-    frame = new Frame(2, 7)
-    scoreboard.addFrame(frame)
-    frame = new Frame(3, 6)
-    scoreboard.addFrame(frame)
-    frame = new Frame(4, 5)
-    scoreboard.addFrame(frame)
-    frame = new Frame(5, 4)
-    scoreboard.addFrame(frame)
-    frame = new Frame(6, 3)
-    scoreboard.addFrame(frame)
-    frame = new Frame(7, 2)
-    scoreboard.addFrame(frame)
-    frame = new Frame(8, 1)
-    scoreboard.addFrame(frame)
-    frame = new Frame(4, 4)
-    scoreboard.addFrame(frame)
-    frame = new Frame(3, 5)
-    scoreboard.addFrame(frame)
+    scoreboard.addFrame(new Frame(1, 8))
+    scoreboard.addFrame(new Frame(2, 7))
+    scoreboard.addFrame(new Frame(3, 6))
+    scoreboard.addFrame(new Frame(4, 5))
+    scoreboard.addFrame(new Frame(5, 4))
+    scoreboard.addFrame(new Frame(6, 3))
+    scoreboard.addFrame(new Frame(7, 2))
+    scoreboard.addFrame(new Frame(8, 1))
+    scoreboard.addFrame(new Frame(4, 4))
+    scoreboard.addFrame(new Frame(3, 5))
     expect(scoreboard.total()).toBe(88)
     expect(scoreboard.frameCount()).toBe(10)
   })
 
 
+})
+
+describe("a bonus is added if a spare is bowled", () => {
+
+  it("gives a bonus after the first frame is a spare and the second frame is bowled", () => {
+    const scoreboard = new Scoreboard
+    scoreboard.addFrame(new Frame(2, 8))
+    scoreboard.addFrame(new Frame(3, 3))
+    expect(scoreboard.total()).toBe(19)
+    expect(scoreboard.frameCount()).toBe(2)
+  })
+
+  it("gives several bonus' if multiple spares bowled", () => {
+    const scoreboard = new Scoreboard   
+    scoreboard.addFrame(new Frame(2, 8))
+    scoreboard.addFrame(new Frame(3, 3))
+    scoreboard.addFrame(new Frame(1, 7))
+    scoreboard.addFrame(new Frame(4, 4))
+    scoreboard.addFrame(new Frame(5, 5))
+    scoreboard.addFrame(new Frame(6, 2))
+    scoreboard.addFrame(new Frame(5, 3))
+    scoreboard.addFrame(new Frame(9, 1))
+    scoreboard.addFrame(new Frame(5, 3))
+    scoreboard.addFrame(new Frame(5, 3)  )
+    expect(scoreboard.total()).toBe(98)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
+})
+
+describe("a bonus is added if a strike is bowled", () => {
+  it("gives a bonus after the first frame is a strike and the second frame is bowled", () => {
+    const scoreboard = new Scoreboard
+    scoreboard.addFrame(new Frame(10, 0))
+    scoreboard.addFrame(new Frame(3, 3))
+    expect(scoreboard.total()).toBe(22)
+    expect(scoreboard.frameCount()).toBe(2)
+  })
+
+  it("gives several bonus' if multiple strikes bowled", () => {
+    const scoreboard = new Scoreboard   
+    scoreboard.addFrame(new Frame(10, 0))
+    scoreboard.addFrame(new Frame(3, 3))
+    scoreboard.addFrame(new Frame(1, 7))
+    scoreboard.addFrame(new Frame(4, 4))
+    scoreboard.addFrame(new Frame(10, 0))
+    scoreboard.addFrame(new Frame(6, 2))
+    scoreboard.addFrame(new Frame(5, 3))
+    scoreboard.addFrame(new Frame(10, 0))
+    scoreboard.addFrame(new Frame(5, 3))
+    scoreboard.addFrame(new Frame(5, 3))
+    expect(scoreboard.total()).toBe(106)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
+})
+
+describe("mix of strikes and spares bowled", () => {
+  it("gives bonus' and correct final score", () => {
+    const scoreboard = new Scoreboard   
+    scoreboard.addFrame(new Frame(1,4))
+    scoreboard.addFrame(new Frame(4,5))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(5,5))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(0,1))
+    scoreboard.addFrame(new Frame(7,3))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(5,3))
+    expect(scoreboard.total()).toBe(123)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
+})
+
+describe("final frame has three rolls", () => {
+  it("adds a single roll bonus if spare", () => {
+    const scoreboard = new Scoreboard
+    scoreboard.addFrame(new Frame(1,4))
+    scoreboard.addFrame(new Frame(4,5))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(5,5))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(0,1))
+    scoreboard.addFrame(new Frame(7,3))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(2,8,6))
+    expect(scoreboard.total()).toBe(133)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
+
+  it("adds a double roll bonus if strike", () => {
+    const scoreboard = new Scoreboard   
+    scoreboard.addFrame(new Frame(1,4))
+    scoreboard.addFrame(new Frame(4,5))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(5,5))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(0,1))
+    scoreboard.addFrame(new Frame(7,3))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,8,2))
+    expect(scoreboard.total()).toBe(145)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
+
+  it("adds final frame score of 30 if all strikes", () => {
+    const scoreboard = new Scoreboard   
+    scoreboard.addFrame(new Frame(1,4))
+    scoreboard.addFrame(new Frame(4,5))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(5,5))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(0,1))
+    scoreboard.addFrame(new Frame(7,3))
+    scoreboard.addFrame(new Frame(6,4))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,10,10))
+    expect(scoreboard.calculateLastFrame()).toBe(30)
+    expect(scoreboard.total()).toBe(157)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
+})
+
+describe("perfect game is scored", () => {
+  it("has a grand total of 300", () => {
+    const scoreboard = new Scoreboard
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,0))
+    scoreboard.addFrame(new Frame(10,10,10))
+    expect(scoreboard.total()).toBe(300)
+    expect(scoreboard.calculateLastFrame()).toBe(30)
+    expect(scoreboard.frameCount()).toBe(10)
+  })
 })
--- scoreboard.js
+++ scoreboard.js
@@ -3,6 +3,7 @@ class Scoreboard {
   constructor() {
     this.scoreboard = []
     this.frame_count = 0
+    this.sum = 0
   }
 
   addFrame(frame) {
@@ -15,12 +16,38 @@ class Scoreboard {
     return this.frame_count
   }
 
-  total() {
-    let sum = 0
-    this.scoreboard.forEach(frame => {
-      sum += frame.frameTotal()
+  calculateLastFrame() {
+    const last_frame = this.scoreboard[9]
+    return (this.frame_count === 10 ? last_frame.frameTotal() : 0);
+  }
+
+  calculateFramesTotal() {
+    let index = 0
+    let normal_frames = this.scoreboard.slice(0,9)
+    normal_frames.forEach(frame => {
+      let frame1 = this.scoreboard[index + 1]
+      let frame2 = this.scoreboard[index + 2]
+      if (frame.checkForSpare()) {
+        this.sum += frame.frameTotal() + frame1.accessFrame()[0]
+      } else if (frame.checkForStrike()) {
+        this.sum += frame.frameTotal()
+        if (frame1.checkForStrike()) {
+          this.sum += (index === 8 ? frame1.accessFrame()[0] + frame1.accessFrame()[1] : frame1.accessFrame()[0] + frame2.accessFrame()[0])
+        } else {
+          this.sum += frame1.accessFrame()[0] + frame1.accessFrame()[1]
+        }
+      } else {
+        this.sum += frame.frameTotal()
+      }
+      index++
     })
-    return sum
+    return this.sum
+  }
+
+  total() {
+    this.sum += this.calculateFramesTotal()
+    this.sum += this.calculateLastFrame()
+    return this.sum
   }
 }
 

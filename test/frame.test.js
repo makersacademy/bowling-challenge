fed74a81ac44262a4ac2afcbc71081616d9cc6ef
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-17 16:45:02 +0000

    Adds strike and spare status

--- test/frame.test.js
+++ test/frame.test.js
@@ -7,7 +7,7 @@ describe(Frame, () => {
 
       expect(frame.getScore()).toEqual(0);
       expect(frame.getRolls()).toEqual([]);
-      expect(frame.getStatus()).toEqual("active");
+      expect(frame.getStatus()).toEqual('active');
     });
   });
   
@@ -18,7 +18,16 @@ describe(Frame, () => {
       frame.addRoll(5);
       expect(frame.getScore()).toEqual(5);
       expect(frame.getRolls()).toEqual([5]);
-      expect(frame.getStatus()).toEqual("active");
+      expect(frame.getStatus()).toEqual('active');
+    });
+
+    it('detects a strike', () => {
+      const frame = new Frame();
+
+      frame.addRoll(10);
+      expect(frame.getScore()).toEqual(10);
+      expect(frame.getRolls()).toEqual([10]);
+      expect(frame.getStatus()).toEqual('strike');
     });
 
     it('throws error when adding a roll not between 0 and 10', () => {
@@ -55,7 +64,17 @@ describe(Frame, () => {
       expect(frame.getScore()).toEqual(9);
       expect(frame.getStatus()).toEqual('completed');
     });
+    
+    it('detects a spare', () => {
+      const frame = new Frame();
 
+      frame.addRoll(7);
+      frame.addRoll(3);
+      expect(frame.getRolls()).toEqual([7,3]);
+      expect(frame.getScore()).toEqual(10);
+      expect(frame.getStatus()).toEqual('spare');
+    });
+    
     it('throws error if rolls add up to more than 10', () => {
       const frame = new Frame();
 
@@ -73,6 +92,21 @@ describe(Frame, () => {
       frame.addRoll(4);
       expect(() => frame.addRoll(4))
         .toThrow('Cannot add more than two throws to a frame');
+    });
+  });
+
+  describe("Add roll when status isn't active", () => {
+    it('throws error', () => {
+      let frame = new Frame();
+      frame.addRoll(10);
+      expect(frame.getStatus()).toEqual('strike');
+      expect(() => frame.addRoll()).toThrow();
+
+      frame = new Frame();
+      frame.addRoll(1);
+      frame.addRoll(9);
+      expect(frame.getStatus()).toEqual('spare');
+      expect(() => frame.addRoll()).toThrow();
     });
   });
 });

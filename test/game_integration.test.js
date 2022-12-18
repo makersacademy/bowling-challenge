2934a84b00d0e0d97a206c1f25e65824b45ca444
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-18 15:11:14 +0000

    Adds full game logic and tests

--- test/game_integration.test.js
+++ test/game_integration.test.js
@@ -50,4 +50,53 @@ describe('Game integration', () => {
       expect(frames[1].getStatus()).toEqual('active');
     });
   });
+
+  describe('Two and a half frames', () => {
+    it('moves from frame to frame as more rolls are added', () => {
+      const game = new Game();
+      for (let i = 0; i < 5; i++) {
+        game.addRoll(4);
+      }
+
+      const frames = game.getFrames();
+      expect(frames[0].getStatus()).toEqual('completed');
+      expect(frames[1].getStatus()).toEqual('completed');
+      expect(frames[2].getRolls()).toEqual([4]);
+      expect(frames[2].getStatus()).toEqual('active');
+    });
+  });
+
+  describe('Full game', () => {
+    it('gutter game has a score of zero', () => {
+      const game = new Game();
+      for (let i = 0; i < 20; i++) {
+        game.addRoll(0);
+      }
+
+      const frames = game.getFrames();
+      expect(frames.every(frame => frame.getScore() === 0)).toBe(true);
+      expect(frames.every(frame => frame.getStatus() === 'completed')).toBe(true);
+    });
+
+    it('full game with rolls of 4', () => {
+      const game = new Game();
+      for (let i = 0; i < 20; i++) {
+        game.addRoll(4);
+      }
+
+      const frames = game.getFrames();
+      expect(frames.every(frame => frame.getScore() === 8)).toBe(true);
+      expect(frames.every(frame => frame.getStatus() === 'completed')).toBe(true);
+    });
+
+    it('throws error when trying to add a new roll', () => {
+      const game = new Game();
+      for (let i = 0; i < 20; i++) {
+        game.addRoll(4);
+      }
+
+      const errorMessage = 'The game is complete, cannot add any more rolls';
+      expect(() => game.addRoll(4)).toThrow(errorMessage);
+    });
+  });
 });

f834fd709d420f87db9abba2181e120a9e98ea85
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-12-20 22:55:08 +0000

    pass unit tests for frame class

---
+++ frame.js
@@ -0,0 +1,54 @@
+class Frame {
+  constructor(rolls) {
+    this.rolls = rolls;
+  }
+
+  scores() {
+    return this.rolls;
+  }
+
+  strike() {
+    if (this.rolls[0] === 10) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  spare() {
+    if (this.strike() === false && this.#sum() === 10) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  complete(frame_count) {
+    if (this.rolls.length >= this.#frameLength(frame_count)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  #sum() {
+    const sum = this.rolls.reduce((accumulator, value) => {
+      return accumulator + value;
+    }, 0);
+    return sum;
+  }
+
+  #frameLength(frame_count) {
+    let frameLength = 2;
+    if (this.#sum() >= 10) {
+      if (frame_count === 10) {
+        frameLength = 3;
+      } else {
+        frameLength = 1;
+      }
+    }
+    return frameLength;
+  }
+}
+
+module.exports = Frame;

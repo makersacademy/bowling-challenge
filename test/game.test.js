bcb457ceee5632113e75c7d89b9e4b2a45838174
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-20 18:40:01 +0000

    Adds addBonus calls to Game.addRoll

--- test/game.test.js
+++ test/game.test.js
@@ -3,19 +3,23 @@ const Frame = require('../lib/frame');
 
 jest.mock('../lib/frame');
 
+mockFrame = (status) => {
+  Frame.mockImplementation(() => {
+    return {
+      getScore: () => 0,
+      getRolls: () => [],
+      getStatus: () => status,
+      addRoll: jest.fn(x => null),
+      addBonus: jest.fn(x => null)
+    };
+  });
+}
+
 describe(Game, () => {
   let game;
 
   beforeAll(() => {
-    Frame.mockImplementation(() => {
-      return {
-        getScore: () => 0,
-        getRolls: () => [],
-        getStatus: () => 'active',
-        addRoll: jest.fn(x => null)
-      };
-    });
-
+    mockFrame('active');
     game = new Game();
   });
 
@@ -31,5 +35,22 @@ describe(Game, () => {
   it("addFrame calls the first frame's addFrame method", () => {
     game.addRoll(5);
     expect(game.getFrames()[0].addRoll).toHaveBeenCalledTimes(1);
+  });
+
+  it('addFrame calls addBonus once on the second frame and twice on the third frame', () => {
+    mockFrame('completed');
+    game = new Game();
+
+    game.addRoll(0);
+    expect(game.getFrames()[0].addBonus).not.toHaveBeenCalled();
+
+    game.addRoll(1);
+    expect(game.getFrames()[0].addBonus).toHaveBeenCalledWith(1);
+    expect(game.getFrames()[1].addBonus).not.toHaveBeenCalled();
+
+    game.addRoll(2);
+    expect(game.getFrames()[0].addBonus).toHaveBeenCalledWith(2);
+    expect(game.getFrames()[1].addBonus).toHaveBeenCalledWith(2);
+    expect(game.getFrames()[2].addBonus).not.toHaveBeenCalled();
   });
 });

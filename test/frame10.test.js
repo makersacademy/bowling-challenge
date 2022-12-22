5ca02231eb491668f2098b2a6421c332e1e88c15
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-22 16:41:57 +0000

    Adds third roll functionality

--- test/frame10.test.js
+++ test/frame10.test.js
@@ -28,29 +28,86 @@ describe(Frame10, () => {
   });
 
   describe('Roll a strike', () => {
-    xit("adds two bonus rolls (that don't add up to more than 10)", () => {
-      frame.addRoll(10);
-      expect(frame.score).toBe(10);
-      expect(frame.rolls).toEqual([10]);
-      expect(frame.status).toEqual('bonus');
+    describe("Don't roll another strike", () => {
+      it("adds two bonus rolls (that don't add up to more than 10)", () => {
+        frame.addRoll(10);
+        expect(frame.score).toBe(10);
+        expect(frame.rolls).toEqual([10]);
+        expect(frame.status).toEqual('bonus');
+  
+        frame.addRoll(2);
+        expect(frame.score).toBe(12);
+        expect(frame.rolls).toEqual([10, 2]);
+        expect(frame.status).toEqual('bonus');
+  
+        frame.addRoll(7);
+        expect(frame.score).toBe(19);
+        expect(frame.rolls).toEqual([10, 2, 7]);
+        expect(frame.status).toEqual('completed');
+  
+        expect(() => frame.addRoll(0))
+          .toThrow('Cannot add rolls to this frame');
+      });
+  
+      it('throws if the two bonus rolls add up to more than 10', () => {
+        frame.addRoll(10);
+        frame.addRoll(7);
+        expect(() => frame.addRoll(7))
+          .toThrow('Rolls cannot add up to more than 10');
+      });
+    });
 
-      frame.addRoll(2);
-      expect(frame.score).toBe(12);
-      expect(frame.rolls).toEqual([10, 2]);
-      expect(frame.status).toEqual('bonus');
+    describe('Roll another strike', () => {
+      it('any valid roll can be added', () => {
+        frame.addRoll(10);
+        frame.addRoll(10);
+        expect(() => frame.addRoll(0)).not.toThrow();
+        expect(frame.score).toBe(20);
+        expect(frame.rolls).toEqual([10, 10, 0]);
 
-      frame.addRoll(7);
-      expect(frame.score).toBe(19);
-      expect(frame.rolls).toEqual([10, 2, 7]);
-      expect(frame.status).toEqual('completed');
+        frame = new Frame10();
+        frame.addRoll(10);
+        frame.addRoll(10);
+        expect(() => frame.addRoll(5)).not.toThrow();
+        expect(frame.score).toBe(25);
+        expect(frame.rolls).toEqual([10, 10, 5]);
 
-      expect(() => frame.addRoll(0)).toThrow('Cannot add rolls to this frame');
+        frame = new Frame10();
+        frame.addRoll(10);
+        frame.addRoll(10);
+        expect(() => frame.addRoll(10)).not.toThrow();
+        expect(frame.score).toBe(30);
+        expect(frame.rolls).toEqual([10, 10, 10]);
+        expect(frame.status).toEqual('completed');
+
+        frame = new Frame10();
+        frame.addRoll(10);
+        frame.addRoll(10);
+        expect(() => frame.addRoll(11))
+          .toThrow('A roll must be between 0 and 10');
+      });
     });
   });
 
   describe('Roll a spare', () => {
-    xit('adds one bonus roll', () => {
+    it('adds one bonus roll', () => {
+      frame.addRoll(0);
+      frame.addRoll(10);
+      expect(frame.status).toBe('bonus');
 
+      frame.addRoll(10);
+      expect(frame.score).toBe(20);
+      expect(frame.rolls).toEqual([0, 10, 10]);
+      expect(frame.status).toEqual('completed');
+    });
+  });
+
+  describe('addBonus', () => {
+    it('does nothing', () => {
+      frame.addRoll(10);
+      frame.addBonus(5);
+      expect(frame.score).toBe(10);
+      expect(frame.status).toEqual('bonus');
     });
   });
 });

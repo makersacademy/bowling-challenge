15424646bfeb2f1f883845734e2fae4b6992f712
Andy <ajscottlewis@gmail.com>
2022-12-28 14:34:59 +0000

    Test to see if Scorecard successfully adds two frames to this.scorecard

--- spec/integration.test.js
+++ spec/integration.test.js
@@ -10,14 +10,20 @@ describe('integration', () => {
   });
 
   describe('seeScorecard() (with newFrame() and addFrameToScorecard()', () => {
-    it('returns [6, 3], the score of the current frame', () => {
+    it('returns [6, 3], the score of the first frame', () => {
       scorecard.newFrame(6, 3);
       expect(scorecard.seeScorecard()).toEqual([6, 3]);
     });
 
-    it('returns [10, 0], the score of the current frame', () => {
+    it('returns [10, 0], the score of the first frame', () => {
       scorecard.newFrame(10, 0);
       expect(scorecard.seeScorecard()).toEqual([10, 0]);
+    });
+
+    it('returns [7, 2, 5, 0], the score of first two frames', () => {
+      scorecard.newFrame(7, 2);
+      scorecard.newFrame(5, 0);
+      expect(scorecard.seeScorecard()).toEqual([7, 2, 5, 0]);
     });
   });
 });

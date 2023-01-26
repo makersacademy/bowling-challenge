db63b474be8acf490187b0d6c14c174d7594fbfa
abodian <ajnbodian@gmail.com>
2023-01-26 16:25:03 +0000

    bowling functions and tests written

--- bowlingScoreCard.js
+++ bowlingScoreCard.js
@@ -1,5 +1,59 @@
 class BowlingScoreCard {
-  constructor() {}
+  constructor() {
+    this.eachRollScoreArray = [];
+  }
+
+  roll = (pinsDown) => {
+    this.eachRollScoreArray.push(pinsDown);
+  };
+
+  overallScore = () => {
+    let score = 0;
+    let rollNumber = 0;
+    for (let i = 0; i < 10; i++) {
+      if (this.isASpareFrame(rollNumber)) {
+        score += 10 + this.spareBonusScore(rollNumber);
+        rollNumber += 2;
+      } else if (this.isAStrikeFrame(rollNumber)) {
+        score += 10 + this.strikeBonusScore(rollNumber);
+        rollNumber = +1;
+      } else {
+        score += this.sumPinsDownInFrame(rollNumber);
+        rollNumber += 2;
+      }
+    }
+    return score;
+  };
+
+  sumPinsDownInFrame = (rollNumber) => {
+    return (
+      this.eachRollScoreArray[rollNumber] +
+      this.eachRollScoreArray[rollNumber + 1]
+    );
+  };
+
+  isASpareFrame = (rollNumber) => {
+    return (
+      this.eachRollScoreArray[rollNumber] +
+        this.eachRollScoreArray[rollNumber + 1] ===
+      10
+    );
+  };
+
+  isAStrikeFrame = (rollNumber) => {
+    return this.eachRollScoreArray[rollNumber] === 10;
+  };
+
+  spareBonusScore = (rollNumber) => {
+    return this.eachRollScoreArray[rollNumber + 2];
+  };
+
+  strikeBonusScore = (rollNumber) => {
+    return (
+      this.eachRollScoreArray[rollNumber + 1] +
+      this.eachRollScoreArray[rollNumber + 2]
+    );
+  };
 }
 
 module.exports = BowlingScoreCard;
--- bowlingScoreCard.test.js
+++ bowlingScoreCard.test.js
@@ -1,17 +1,55 @@
 const BowlingScoreCard = require("./BowlingScoreCard");
 
 describe("bowlingScoreCard class", () => {
-  let scoreCard = new BowlingScoreCard();
+  beforeEach(() => {
+    scoreCard = new BowlingScoreCard();
+  });
 
-  it("tests for a zero score game", () => {});
+  it("tests for a zero score game", () => {
+    for (let i = 0; i < 20; i++) {
+      return scoreCard.roll(0);
+    }
+    expect(scoreCard.overallScore()).toEqual(0);
+  });
 
-  it("tests for user hitting 1 for every roll", () => {});
+  it("tests for user hitting 1 for every roll", () => {
+    for (let i = 0; i < 20; i++) {
+      return scoreCard.roll(1);
+    }
+    expect(scoreCard.overallScore()).toEqual(20);
+  });
 
-  it("tests for user rolling one spare and then a five", () => {});
+  it("tests for user rolling one spare and then a four", () => {
+    let spareGame = () => {
+      scoreCard.roll(5), scoreCard.roll(5), scoreCard.roll(4);
+      for (let i = 0; i < 17; i++) {
+        scoreCard.roll(0);
+      }
+    };
+    spareGame();
+    expect(scoreCard.overallScore()).toEqual(18);
+  });
 
-  it("tests for user rolling one strike then a four and a five", () => {});
+  it("tests for user rolling one strike then a four and a five", () => {
+    let strikeGame = () => {
+      scoreCard.roll(10), scoreCard.roll(4), scoreCard.roll(5);
+      for (let i = 0; i < 17; i++) {
+        scoreCard.roll(0);
+      }
+    };
+    strikeGame();
+    expect(scoreCard.overallScore()).toEqual(28);
+  });
 
-  it("tests for the perfect game", () => {});
+  it("tests for the perfect game", () => {
+    let perfectGame = () => {
+      for (let i = 0; i < 12; i++) {
+        scoreCard.roll(10);
+      }
+    };
+    perfectGame();
+    expect(scoreCard.overallScore()).toEqual(300);
+  });
 });
 
 // 1. test for a 0 score game (0)

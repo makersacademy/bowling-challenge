0f9e83f8fdbffa439f8722e17e937a0f39b46da7
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-27 19:29:42 +0000

    Adds frame and reads back scorecard

--- test/scoreCard.test.js
+++ test/scoreCard.test.js
@@ -1,4 +1,19 @@
-// const ScoreCard = require('../lib/scoreCard')
+const ScoreCard = require('../lib/scoreCard')
 
-// describe('ScoreCard', () => {
-// })
+describe('ScoreCard', () => {
+
+  it('adds the frame to the scorecard and reads it back', () => {
+    let scoreCard = new ScoreCard();
+    expect(scoreCard.addFrame([4, 5])).toEqual([[4,5]])
+  })
+
+  it('reads the whole score card back', () => {
+    let scoreCard = new ScoreCard();
+    scoreCard.addFrame([4, 5])
+    scoreCard.addFrame([2, 7])
+    scoreCard.addFrame([0, 9])
+    expect(scoreCard.readScore()).toEqual([[4,5], [2, 7], [0, 9]])
+  })
+
+
+})

a82bfb23ac9478b4b8b33f45bcb73cac7cf04247
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-28 11:12:14 +0000

    Adds multiple frame objects to array of arrays

--- test/frame.test.js
+++ test/frame.test.js
@@ -3,33 +3,31 @@ const Frame = require('../lib/frame')
 describe('Frame', () => {
   it('adds two rolls to the frame', () => {
     let frame1 = new Frame()
-    frame1.rollsInFrame(5, 6)
+    frame1.addRollsToFrame(5, 6)
     expect(frame1.frame).toEqual([5, 6])
   })
 
   it('returns true if a strike is bowled', () => {
     let frame1 = new Frame()
-    frame1.rollsInFrame(10, 0)
+    frame1.addRollsToFrame(10, 0)
     expect(frame1.checkStrike()).toEqual(true)
   })
 
   it('returns false if a strike is bowled', () => {
     let frame1 = new Frame()
-    frame1.rollsInFrame(5, 3)
+    frame1.addRollsToFrame(5, 3)
     expect(frame1.checkStrike()).toEqual(false)
   })
 
   it('returns true if a spare is bowled', () => {
     let frame1 = new Frame()
-    frame1.rollsInFrame(5, 5)
+    frame1.addRollsToFrame(5, 5)
     expect(frame1.checkSpare()).toEqual(true)
   })
 
   it('returns false if a spare is not bowled', () => {
     let frame1 = new Frame()
-    frame1.rollsInFrame(5, 4)
+    frame1.addRollsToFrame(5, 4)
     expect(frame1.checkSpare()).toEqual(false)
   })
-
-
 });
--- test/scoreCard.test.js
+++ test/scoreCard.test.js
@@ -1,19 +1,21 @@
-const ScoreCard = require('../lib/scoreCard')
+const Frame = require("../lib/frame");
+const ScoreCard = require("../lib/scoreCard");
 
-describe('ScoreCard', () => {
 
-  it('adds the frame to the scorecard and reads it back', () => {
-    let scoreCard = new ScoreCard();
-    expect(scoreCard.addFrame([4, 5])).toEqual([[4,5]])
+describe('Frame', () => {
+  it('adds a frame to the scorecard and reads it back', () => {
+    const scoreCard = new ScoreCard();
+    const frame = new Frame();
+    scoreCard.addFrame(frame.addRollsToFrame(1,2))
+    expect(scoreCard.readScore()).toEqual([[1,2]])
   })
 
-  it('reads the whole score card back', () => {
-    let scoreCard = new ScoreCard();
-    scoreCard.addFrame([4, 5])
-    scoreCard.addFrame([2, 7])
-    scoreCard.addFrame([0, 9])
-    expect(scoreCard.readScore()).toEqual([[4,5], [2, 7], [0, 9]])
+  it('adds multiple frames to the scorecard and reads it back', () => {
+    const scoreCard = new ScoreCard();
+    const frame1 = new Frame();
+    const frame2 = new Frame();
+    scoreCard.addFrame(frame1.addRollsToFrame(1,2))
+    scoreCard.addFrame(frame2.addRollsToFrame(5,3))
+    expect(scoreCard.readScore()).toEqual([[1,2], [5,3]])
   })
-
-
 })

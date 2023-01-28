162808eed8791b4391e0281a4afe9888569d56d8
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-28 12:03:29 +0000

    Tests passed for multiple frams. Changed name of readScore method to currentScore

--- test/scoreCard.test.js
+++ test/scoreCard.test.js
@@ -7,15 +7,26 @@ describe('Frame', () => {
     const scoreCard = new ScoreCard();
     const frame = new Frame();
     scoreCard.addFrame(frame.addRollsToFrame(1,2))
-    expect(scoreCard.readScore()).toEqual([[1,2]])
+    expect(scoreCard.currentScore()).toEqual([[1,2]])
   })
 
-  it('adds multiple frames to the scorecard and reads it back', () => {
+  it('adds two frames to the scorecard and reads it back', () => {
     const scoreCard = new ScoreCard();
     const frame1 = new Frame();
     const frame2 = new Frame();
     scoreCard.addFrame(frame1.addRollsToFrame(1,2))
     scoreCard.addFrame(frame2.addRollsToFrame(5,3))
-    expect(scoreCard.readScore()).toEqual([[1,2], [5,3]])
+    expect(scoreCard.currentScore()).toEqual([[1,2], [5,3]])
+  })
+
+  it('adds three frames to the scorecard and reads it back', () => {
+    const scoreCard = new ScoreCard();
+    const frame1 = new Frame();
+    const frame2 = new Frame();
+    const frame3 = new Frame();
+    scoreCard.addFrame(frame1.addRollsToFrame(1,2))
+    scoreCard.addFrame(frame2.addRollsToFrame(5,3))
+    scoreCard.addFrame(frame3.addRollsToFrame(6,1))
+    expect(scoreCard.currentScore()).toEqual([[1,2], [5,3], [6,1]])
   })
 })

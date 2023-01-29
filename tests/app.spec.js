770b4100a00757e1bd441d67db5d745a30e2e201
Joe Burgess <josephburgess@gmail.com>
2023-01-29 12:07:31 +0000

    amending app test to use consolespy

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -9,7 +9,7 @@ jest.mock('../lib/userInterface', () => {
       getRollTwo: jest.fn().mockReturnValue(5),
       getRollThree: jest.fn().mockReturnValue(0),
       game: {
-        frameCount: 0,
+        frameCount: 1,
         add: jest.fn(),
         calculateGrandTotal: jest.fn().mockReturnValue(90),
       },
@@ -21,6 +21,7 @@ describe('Application', () => {
   let app;
   let game;
   let ui;
+  let spy;
 
   beforeEach(() => {
     game = new Game();
@@ -28,19 +29,21 @@ describe('Application', () => {
     app = new Application();
     app.game = game;
     app.UI = ui;
+    spy = jest.spyOn(console, 'log');
   });
 
-  test('run method should call the expected methods and log the expected output', () => {
-    console.log = jest.fn();
+  afterEach(() => {
+    spy.mockRestore();
+  });
+
+  test('run method should call the expected methods the expected number of times and log the expected output', () => {
     app.run();
-    expect(console.log.mock.calls[8][0]).toContain(
-      '\nYou are on frame 0'.yellow.bold
-    );
+    expect(spy.mock.calls[8]).toContain('\nYou are on frame 1'.yellow.bold);
     expect(ui.getRollOne.mock.calls.length).toBe(10);
     expect(ui.getRollTwo.mock.calls.length).toBe(10);
     expect(ui.getRollThree.mock.calls.length).toBe(10);
     expect(ui.game.add.mock.calls.length).toBe(10);
     expect(ui.game.calculateGrandTotal.mock.calls.length).toBe(1);
-    expect(console.log.mock.calls[18][0]).toContain('Your total score is: 90');
+    expect(spy.mock.calls[18][0]).toContain('Your total score is: 90');
   });
 });

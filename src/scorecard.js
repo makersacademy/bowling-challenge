e4eff78f3bd7ec8c23edf0e9ded9cd58e6a6f9c9
Murat <murat@sertay.tech>
2023-01-29 06:51:38 +0000

    Added 2 tests and rewrote scorecard.js in order to use an array of objects rather than just an array for the scores for readability and ease of score calculations

--- src/scorecard.js
+++ src/scorecard.js
@@ -1,20 +1,44 @@
-import _ from "lodash"
 
 
 export class Scorecard {
     constructor() {
-        this.currentFrameScore = []
+        this.currentFrameScore = {
+            frame: 0,
+            bonus: 0,
+            rolls: []
+        }
+        this.frameCount = 0
+        this.scorecard = []
     }
 
     startGame = () => {
-
+//        this.frameCount++
     }
-    getScoreFromJustPlayedFrame = () => _.sum(this.currentFrameScore)
+    getScoreFromJustPlayedFrame = () => this.currentFrameScore.rolls
+        .reduce((pv,cv) => pv + cv,0 )
     startFrame = () => {
-
+        this.frameCount++
+        this.currentFrameScore = {
+            frame: this.frameCount,
+            bonus: 0,
+            rolls: []
+        }
     }
     endFrame = () => {
+        if (this.frameCount > 1) {
+            if (this?.previousFrameScore && this.previousFrameScore?.rolls[0] === 10) {
+                this.scorecard[this.getPreviousArrayLocation()].bonus = this.getScoreFromJustPlayedFrame()
+            }
+        }
+        this.previousFrameScore = this.currentFrameScore
+        this.scorecard.push(this.currentFrameScore)
 
     }
-    rollInput = (item) => this.currentFrameScore.push(item)
+    rollInput = (item) => this.currentFrameScore.rolls.push(item)
+
+    // below the magic number 2 is taking away 1 for the array location and another 1 for the previous frame
+    getPreviousArrayLocation = () => this.frameCount - 2
+    getScorecard = () => this.scorecard
+
+
 }
--- tests/scorecard.test.js
+++ tests/scorecard.test.js
@@ -1,17 +1,50 @@
 import {Scorecard} from "../src/scorecard.js";
 import {describe, expect, it} from "@jest/globals";
 //import {describe, expect, test} from "@jest/globals";
-
+let playFrameMock = (roll1,roll2,card) => {
+    card.startFrame()
+    card.rollInput(roll1)
+    card.rollInput(roll2)
+    expect(card.getScoreFromJustPlayedFrame()).toEqual(roll1 +roll2)
+    card.endFrame()
+    return card
+}
  describe("Scorecard",()=>{
-  it("First Frame",() => {
+  it("First Single Frame, return score from getScore",() => {
       let card = new Scorecard()
       card.startGame()
-      card.startFrame()
-      card.rollInput(1)
-      card.rollInput(5)
-      card.endFrame()
-        expect(card.getScoreFromJustPlayedFrame()).toEqual(6)
+      playFrameMock(1,4,card)
+        expect(card.getScoreFromJustPlayedFrame()).toEqual(5)
     })
+     it("First Single Frame, return object from scorecard",() => {
+         let card = new Scorecard()
+         card.startGame()
+         playFrameMock(1,4,card)
+         expect(card.getScorecard()[0]).toHaveProperty("rolls",[1,4])
+         expect(card.getScorecard()[0]).toHaveProperty("frame",1)
+     })
+     it(" return the second Frame, return object from scorecard",() => {
+         let card = new Scorecard()
+         card.startGame()
+         playFrameMock(1,4,card)
+         playFrameMock(5,4,card)
+         expect(card.getScorecard()[1]).toHaveProperty("rolls",[5,4])
+         expect(card.getScorecard()[1]).toHaveProperty("frame",2)
+     })
+     it("First Strike then 2nd score Frame",() => {
+         let card = new Scorecard()
+         card.startGame()
+        playFrameMock(10,0,card)
+        playFrameMock(5,3,card)
+         expect(card.getScorecard()[0]).toHaveProperty("bonus",8)
+     })
+     it("First Spare then 2nd score Frame",() => {
+         let card = new Scorecard()
+         card.startGame()
+         playFrameMock(10,0,card)
+         playFrameMock(5,3,card)
+         expect(card.getScorecard()[0]).toHaveProperty("bonus",8)
+     })
  })
 
 //card.rollInput(1)

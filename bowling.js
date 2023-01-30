056f993bd690ada7e8267835e047a68bfe2ddc0d
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-01-30 09:17:46 +0000

    Different approach needed - outputs 124 instead of 133

--- bowling.js
+++ bowling.js
@@ -1,41 +1,38 @@
 class Bowling {
   constructor() {
     this.score = 0;
-    this.current_frame = 1;
-    this.bonus_roll = 0;
+    this.frame = 1;
+    this.bonusRoll = 0;
   }
 
   process_frame (rolls) {
-    if (this.current_frame > 10) {
+    if (this.frame > 10) {
       throw `Game over`;
     }
 
-    //Calculates roll score
-    rolls.forEach((roll) => {
-    this.score += roll;
+    let pins = 10;
+
+    //Calculate roll score
+    rolls.forEach((roll, index) => {
+      pins -= roll;
+      this.score += roll;
+    
+      //Apply bonus from previous roll
+      if (this.bonusRoll > 0) {
+        const consecutive_strikes = (roll === 10 && this.bonusRoll === 2);
+        const bonusMultiplier = consecutive_strikes ? 2 : 1;
+        this.score += roll * bonusMultiplier;
+        this.bonusRoll -= 1;
+      }  
+    
+      //Set bonus in case of strike or spare
+      if ((pins == 0) && (this.frame < 10)) {
+        this.bonusRoll = 2 - index;
+        return;
+      }
     });
-    
-    //Calculates num of consecutive strikes
-    this.bonus_roll =
-    (rolls[0] === 10) ? 2 :
-    (rolls[0] + rolls[1] === 10) ? 1 : 0;
-    
-    //Apply bonus from previous roll
-    //   if (this.bonus_roll > 0) {
-    //     this.score += roll * multiplier;
-    //     this.bonus_roll -= 1;
-    //   }  
-    // }
-    //Update bonus for next roll
-    // this.bonus_roll = 
-    //   if (rolls[0] === 10) {
-    //     return 2;
-    //   } else if (rolls[0] + rolls[1] == 10) {
-    //     return 1;
-    //   } else {
-    //     return 0;
-    //   }
-    this.current_frame += 1;
+
+    this.frame += 1;
   }
 
   report_score () {
@@ -54,15 +51,17 @@ module.exports = Bowling;
 //#1 on terminal, enter node and require the file:
 //Bowling = require('./bowling');
 // output: [class Bowling]
-//bowling = new Bowling();
-// output: Bowling { score: 0, current_frame: 1, bonus_roll: 0 }
-// bowling.process_frame([1, 4]);
-// bowling.process_frame([4, 5]);
-// bowling.process_frame([6, 4]);
-// bowling.process_frame([5, 5]);
-// bowling.process_frame([10, 0]);
-// bowling.process_frame([0, 1]);
-// bowling.process_frame([7, 3]);
-// bowling.process_frame([6, 4]);
-// bowling.process_frame([10, 0]);
-// bowling.process_frame([2, 8, 6]);
+bowling = new Bowling();
+// output: Bowling { score: 0, frame: 1, bonusRoll: 0 }
+console.log(bowling.process_frame([1, 4]));
+console.log(bowling.process_frame([4, 5]));
+console.log(bowling.process_frame([6, 4]));
+console.log(bowling.process_frame([5, 5]));
+console.log(bowling.process_frame([10, 0]));
+console.log(bowling.process_frame([0, 1]));
+console.log(bowling.process_frame([7, 3]));
+console.log(bowling.process_frame([6, 4]));
+console.log(bowling.process_frame([10, 0]));
+console.log(bowling.process_frame([2, 8, 6]));
+console.log(bowling.report_score());
+

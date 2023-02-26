35060a8b9e8f8d762fce5eb38ee48b1cf923bfc1
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-26 21:30:50 +0000

    Game tests for assigning rolls to frames and strike conditions passed

--- spec/game.test.js
+++ spec/game.test.js
@@ -13,4 +13,25 @@ describe('Game', () => {
     const firstFrame = game.frames[0];
     expect(firstFrame.rolls).toEqual([5, 4]);
   });
+
+  it('can add multiple rolls and assign to the correct frame', () => {
+    const game = new Game;
+    game.roll(5);
+    game.roll(4);
+    game.roll(6);
+    game.roll(7);
+    const currFrame = game.frames[1];
+    expect(currFrame.rolls).toEqual([6, 7]);
+  });
+
+  it('can add rolls to the correct frame and account for strike', () => {
+    const game = new Game;
+    game.roll(5);
+    game.roll(4);
+    game.roll(10);
+    game.roll(6);
+    game.roll(7);
+    const currFrame = game.frames[1];
+    expect(currFrame.rolls).toEqual([10]);
+  });
 })

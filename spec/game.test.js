2330eac3570b8964e5865c9a6d7e9c7ad0d85e6a
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-26 21:27:25 +0000

    Game test for adding rolls to frames array passed

--- spec/game.test.js
+++ spec/game.test.js
@@ -5,4 +5,12 @@ describe('Game', () => {
     const game = new Game;
     expect(game.frames.length).toBe(10);
   });
+
+  it('can add two rolls to the same object in frames array', () => {
+    const game = new Game;
+    game.roll(5);
+    game.roll(4);
+    const firstFrame = game.frames[0];
+    expect(firstFrame.rolls).toEqual([5, 4]);
+  });
 })

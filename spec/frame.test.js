c781f5384f55ce2593db9d5cdcd4f758d0719268
Hayley Dobbs <iamdobbs@icloud.com>
2023-02-26 20:52:01 +0000

    Test for handling of multiple rolls passed

--- spec/frame.test.js
+++ spec/frame.test.js
@@ -6,4 +6,11 @@ describe('frame', () => {
     frame.addRoll(5);
     expect(frame.score()).toBe(5);
   });
+
+  it('can handle two rolls', () => {
+    const frame = new Frame();
+    frame.addRoll(5);
+    frame.addRoll(3);
+    expect(frame.score()).toBe(8);
+  });
 })

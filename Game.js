bcf109ca23e7a682bfcbf7faefbee62be8ec2ef5
jpates1 <jamesandrewpates@gmail.com>
2023-03-26 21:00:46 +0100

    test score.3 (spare) working

--- Game.js
+++ Game.js
@@ -11,13 +11,25 @@ class Game {
     let totalScore = 0;
     let rollIndex = 0;
 
-    for (let frameIndex = 0; frameIndex < 10; frameIndex++) {
-      const frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
-      totalScore += frameScore;
-      rollIndex += 2;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.spare(rollIndex)) {
+        if (this.rolls[rollIndex + 2] !== undefined) {
+          totalScore += 10 + this.rolls[rollIndex + 2];
+        } else {
+          break;
+        }
+        rollIndex += 2;
+      } else {
+        totalScore += this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+        rollIndex += 2;
+      }
     }
 
     return totalScore;
+  }
+
+  spare(rollIndex) {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
   }
 }
 

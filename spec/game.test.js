558ff215acc247afd9c889954df449f1d9a48e91
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-03-26 22:03:47 +0100

    Changed Game class based on previous challenge in Ruby

--- spec/game.test.js
+++ spec/game.test.js
@@ -15,32 +15,18 @@ describe ('Game.', () => {
     it ('Returns score board after two rolls.', () => {
         game1 = new Game();
         game1.roll(6);
-        game1.score();
         game1.roll(4);
         expect(game1.score()).toEqual (10);
     })
 
     it ('Fails when first roll is incorrect.', () => {
         game1 = new Game();
-        game1.roll(6);
-        game1.roll(4);
-        expect(game1.score()).toEqual (10);
+        expect(() => {game1.roll(12); }).toThrow ('Incorrect roll. Try again!');
     })
 
-    xit ('Fails when second roll is incorrect.', () => {
+    it ('Fails when first roll is incorrect.', () => {
         game1 = new Game();
-        game1.roll(6);
-        game1.roll(4);
-        expect(game1.score()).toEqual (10);
+        expect(() => {game1.roll(-2); }).toThrow ('Incorrect roll. Try again!');
     })
-
-    xit ('Fails when first + second rolls are incorrect.', () => {
-        game1 = new Game();
-        game1.roll(6);
-        game1.roll(4);
-        expect(game1.score()).toEqual (10);
-    })
-
-// To delete Frames and Frames.test!
 
 });

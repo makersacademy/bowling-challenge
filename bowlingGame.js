0c37dcf75d57ee16769ee927a85d96d3debee534
Dilan Patel <patel_dilan@hotmail.com>
2023-03-26 20:51:23 +0100

    Tests passing - refactored test suite to include 'beforeEach' function that initiates a new game before each test

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,55 +1,57 @@
 class BowlingGame {
 
   constructor() {
+    // constructs with an empty array to store all rolls
     this.rolls = [];
   }
 
   roll(pins) {
+    // pushes the number of pins for each roll into the rolls array
     this.rolls.push(pins);
   };
 
   score() {
+    // returns 0 if score is called before any rolls are added
     if (this.rolls.length === 0) {
       return 0;
     };
 
     let totalScore = 0;
-    let rollIndex = 0;
-
+    let currentRollIndex = 0;
+    // iterates through each frame and applies the appropriate logic
     for (let frame = 0 ; frame < 10 ; frame++) {
-      if (this.isSpare(rollIndex)) {
-        totalScore += this.spareBonus(rollIndex);
-        rollIndex += 2;
-      } else if (this.isStrike(rollIndex)) {
-        totalScore += this.strikeBonus(rollIndex);
-        rollIndex += 1;
+      if (this.isSpare(currentRollIndex)) {
+        totalScore += this.spareBonus(currentRollIndex);
+        currentRollIndex += 2;
+      } else if (this.isStrike(currentRollIndex)) {
+        totalScore += this.strikeBonus(currentRollIndex);
+        currentRollIndex += 1;
       } else {
-        totalScore += this.frameScore(rollIndex);
-        rollIndex += 2;
-      };
-    };
-
+        totalScore += this.frameScore(currentRollIndex);
+        currentRollIndex += 2;
+      }
+    }
     return totalScore;
   }
 
-  frameScore(rollIndex) {
-    return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+  frameScore(currentRollIndex) {
+    return this.rolls[currentRollIndex] + this.rolls[currentRollIndex + 1];
   }
 
-  isSpare(rollIndex) {
-    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
+  isSpare(currentRollIndex) {
+    return this.rolls[currentRollIndex] + this.rolls[currentRollIndex + 1] === 10;
   }
 
-  spareBonus(rollIndex) {
-    return 10 + this.rolls[rollIndex + 2];
+  spareBonus(currentRollIndex) {
+    return 10 + this.rolls[currentRollIndex + 2];
   }
 
-  isStrike(rollIndex) {
-    return this.rolls[rollIndex] === 10;
+  isStrike(currentRollIndex) {
+    return this.rolls[currentRollIndex] === 10;
   }
 
-  strikeBonus(rollIndex) {
-    return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  strikeBonus(currentRollIndex) {
+    return 10 + this.rolls[currentRollIndex + 1] + this.rolls[currentRollIndex + 2];
   }
 
 };
--- bowlingGame.test.js
+++ bowlingGame.test.js
@@ -2,56 +2,56 @@ const BowlingGame = require('./bowlingGame');
 
 describe('BowlingGame', () => {
 
+  let bowlingGame;
+
+  beforeEach(() => {
+    bowlingGame = new BowlingGame();
+  });
+
   it('constructs a new game with a score of 0', () => {
-    const bowlingGame = new BowlingGame();
     expect(bowlingGame).toBeInstanceOf(BowlingGame);
     expect(bowlingGame.score()).toBe(0);
   });
 
   it('scores a gutter game when the player rolls all zeros', () => {
-    const bowlingGame = new BowlingGame();
     for (let i = 0 ; i < 20 ; i++) {
       bowlingGame.roll(0);
-    };
+    }
     expect(bowlingGame.score()).toBe(0);
   });
 
   it('scores a game of 20 when the player rolls all ones', () => {
-    const bowlingGame = new BowlingGame();
     for (let i = 0 ; i < 20 ; i++) {
       bowlingGame.roll(1);
-    };
+    }
     expect(bowlingGame.score()).toBe(20);
   });
 
   it('adds the score of the next roll to 10 when the player rolls a spare', () => {
-    const bowlingGame = new BowlingGame();
     bowlingGame.roll(5);
     bowlingGame.roll(5);
     bowlingGame.roll(5);
     bowlingGame.roll(3);
     for (let i = 0 ; i < 16 ; i++) {
       bowlingGame.roll(0);
-    };
+    }
     expect(bowlingGame.score()).toBe(23);
   });
 
   it('adds the score of the next two rolls to 10 when the player rolls a strike', () => {
-    const bowlingGame = new BowlingGame();
     bowlingGame.roll(10);
     bowlingGame.roll(5);
     bowlingGame.roll(2);
     for (let i = 0 ; i < 16 ; i++) {
       bowlingGame.roll(0);
-    };
+    }
     expect(bowlingGame.score()).toBe(24);
   });
 
   it('scores a perfect game when the player rolls all strikes', () => {
-    const bowlingGame = new BowlingGame();
     for (let i = 0 ; i < 12 ; i++) {
       bowlingGame.roll(10);
-    };
+    }
     expect(bowlingGame.score()).toBe(300);
   });
 

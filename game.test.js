dcf3e6e428760e663840a2bbc7a47fa26c1a8d49
jpates1 <jamesandrewpates@gmail.com>
2023-03-26 20:32:08 +0100

    test score.1

--- game.test.js
+++ game.test.js
@@ -1,19 +1,22 @@
 const Game = require('./Game');
 
-describe('Roll', () => {
-  it ('adds the number of pins knocked down to the rolls array', () => {
-    const newGame = new Game;
-    newGame.roll(5);
-    expect(newGame.rolls).toEqual([5]);
+describe('Game', () => {  
+  describe('roll', () => {
+    it ('adds the number of pins knocked down to the rolls array', () => {
+      const newGame = new Game;
+      newGame.roll(5);
+      expect(newGame.rolls).toEqual([5]);
+    });
   });
 
-describe('score' () => {
-  it('scores a gutter game', () => {
-    const game = new BowlingGame();
-    for (let i = 0; i < 20; i++) {
-      game.roll(0);
-    }
-    expect(game.score()).toEqual(0);
+  describe('score', () => {
+    it('scores a gutter game', () => {
+      const newGame = new Game();
+      for (let i = 0; i < 20; i++) {
+        newGame.roll(0);
+      }
+      expect(newGame.score()).toEqual(0);
+    });
   });
 })
-});
+

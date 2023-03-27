a38663370873b731a3b99611f1344773a6e5d59a
Francesco Guglielmi <francescoguglielmi@Francescos-MacBook-Air.local>
2023-03-27 01:25:10 +0100

    test driven and implemented all methods except for calculateBonus() and calculateScore()

--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -6,7 +6,7 @@ describe ('Scorecard', () => {
     scorecard = new Scorecard();
   });
 
-  // Tests for addFrame()
+  // Tests for validateRolls() 
 
   it ('throws an error if the rolls are an invalid input', () => {
     expect(() => {
@@ -22,6 +22,8 @@ describe ('Scorecard', () => {
     }).toThrow('You can only hit 10 pins in one frame except in the tenth.');
   });
 
+  // Tests for tenthFrameError()
+
   it ('throws an error if you call the method addFrame on the tenth frame', () => {
     expect(() => {
       for (let i = 0; i < 11; i++) {
@@ -30,6 +32,8 @@ describe ('Scorecard', () => {
     }).toThrow('This is the tenth frame, use method addTenthFrame instead');
   });
  
+  // Tests for addFrame()
+
   it ('adds a frame to the scorecard', () => {
     scorecard.addFrame(1, 2);
 
@@ -43,6 +47,46 @@ describe ('Scorecard', () => {
     expect(scorecard.game).toEqual([1, 2, 4, 5]); 
   });
 
-  // Tests for addTenthFrame
+  // Tests for isTenthFrame()
+
+  it ("it throws an error if all the other 9 frames haven't been played yet", () => {
+    scorecard.addFrame(1, 2);
+    scorecard.addFrame(4, 5);
+    expect(() => {
+      scorecard.isTenthFrame(1, 2, 3);
+    }).toThrow('You did not reach the tenth frame yet, use method addFrame instead');
+  });
+
+  // Tests for addTenthFrame()
+
+  it ('turns the 3rd roll to 0 is in the first 2 there is no spare or strike', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.addFrame(8, 2);
+    }
+    scorecard.addTenthFrame(1, 2, 3);
+    expect(scorecard.game.slice(-1)[0]).toEqual(0);
+  });
+
+  it ('stores all three rolls as they are if there is a strike in the first two', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.addFrame(8, 2);
+    }
+    scorecard.addTenthFrame(10, 10, 10);
+    expect(scorecard.game.slice(-1)[0]).toEqual(10);
+    expect(scorecard.game.slice(-2)[0]).toEqual(10);
+    expect(scorecard.game.slice(-3)[0]).toEqual(10);
+  });
+
+  it ('stores all three rolls as they are if there is a spare in the first two', () => {
+    for (let i = 0; i < 9; i++) {
+      scorecard.addFrame(8, 2);
+    }
+    scorecard.addTenthFrame(8, 2, 10);
+    expect(scorecard.game.slice(-1)[0]).toEqual(10);
+    expect(scorecard.game.slice(-2)[0]).toEqual(2);
+    expect(scorecard.game.slice(-3)[0]).toEqual(8);
+  });
+
+  
 
 });

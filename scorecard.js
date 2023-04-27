e49076c271042d13ae6b13edfe8cd187f3ef8273
umut <umut_baykan@hotmail.com>
2023-04-27 16:08:42 +0100

    merging scorecard to bowl class, starting counting methods

--- scorecard.js
+++ scorecard.js
@@ -1,8 +1,12 @@
 class Scorecard{
-  constructor(frames){
-    this.frames = frames
+  constructor(){
+    this.frames = []
   }
 
+  addFrame(...args) {
+    this.frames.push(args)
+  }
+  
   checkSpecials(frame_index) {
     if (this.frames[frame_index][0] === 10) {return "strike"}
     else if (this.flattenArrayAndSumContents(this.frames[frame_index]) === 10) {return "spare"}
@@ -11,14 +15,20 @@ class Scorecard{
   }
   
   calculateScore() {
-    return this.current_score
+    // return this.current_score
   }
 
-  addFrameScore(frame) {
-    // let sum = flat(this.frames)
-    // const flattened_array = this.frames.flat()
-    // const sum = this.frames.flat().reduce((ps, a) => ps + a, 0);
-    // return sum
+  addFrameScore(index) {
+    const current_frame = this.frames[index]
+    const previous_frame = this.frames[index-1]
+    let frame_current_score = this.flattenArrayAndSumContents(current_frame)
+    if (this.checkSpecials(index-1) === "strike") {
+      frame_current_score += this.flattenArrayAndSumContents(current_frame)
+    }
+    else if (this.checkSpecials(index) === "spare") {
+      frame_current_score += previous_frame[0]
+    }
+    return frame_current_score
   }
 
   addBonusScore() {
@@ -27,9 +37,14 @@ class Scorecard{
     return
   }
 
+  // Helper methods //
   // This method is for refactoring purposes. It flattens and array and sums contents
   flattenArrayAndSumContents(frame) {
     return frame.reduce((ps,a) => ps + a, 0)
+  }
+
+  getFrames() {
+    return this.frames
   }
 }
 
--- scorecard.test.js
+++ scorecard.test.js
@@ -1,52 +1,86 @@
 const Scorecard = require('./scorecard')
 
 describe ('scorecard class', () =>{
+
+  beforeEach(() => {
+    scorecard = new Scorecard()
+  })
+  
+  describe ('initialization and adding frames', () => {  
+    it('initializes with an empty frame', () =>{
+      expect(scorecard.getFrames()).toEqual([])
+    })
+  
+    it('adds a frame into players frames with addFrame', () =>{
+      scorecard.addFrame(1,2)
+      expect(scorecard.getFrames()).toEqual([[1,2]])
+    })
+    
+    it('stacks multiple frames in an array', () =>{
+      scorecard.addFrame(1,2)
+      scorecard.addFrame(2,3)
+      expect(scorecard.getFrames()).toEqual([[1,2], [2,3]])
+    })
+  })
+
+  describe('bonus frame after 10th shot', () =>{
+    it ('creates a bonus frame of a length of 1 and pushes to array', () =>{
+      scorecard.addFrame(1)
+      expect(scorecard.getFrames()).toEqual([[1]])
+    })
+    it ('creates a bonus frame of a length of 3 and pushes to array', () =>{
+      scorecard.addFrame(1,2,3)
+      expect(scorecard.getFrames()).toEqual([[1,2,3]])
+    })
+  })
+ 
   describe ('checkSpecials method', () =>{
     it ('returns strike if given frame is [10,0]', () =>{
-      scorecard = new Scorecard([[10,0]])
+      scorecard.addFrame(10,0)
       expect(scorecard.checkSpecials(0)).toEqual("strike")
     })
-
+ 
     it ('returns a spare if given frame is [5,5]', () =>{
-      scorecard = new Scorecard([[5,5]])
+      scorecard.addFrame(5,5)
       expect(scorecard.checkSpecials(0)).toEqual("spare")
     })
 
     it ('returns a spare if given frame is [0,10]', () =>{
-      scorecard = new Scorecard([[0,10]])
+      scorecard.addFrame(0,10)
       expect(scorecard.checkSpecials(0)).toEqual("spare")
     })
 
     it ('returns normal if frame is normal [2,3]', () =>{
-      scorecard = new Scorecard([[2,3]])
+      scorecard.addFrame(2,3)
       expect(scorecard.checkSpecials(0)).toBe("normal")
     })
 
     it ('returns gutter if frame is [0,0]', () =>{
-      scorecard = new Scorecard([[0,0]])
+      scorecard.addFrame(0,0)
       expect(scorecard.checkSpecials(0)).toBe("gutter")
     })
   })
+    
+  describe ('addFrameScore method', () =>{
+    it ('counts the score of the given frame normally if no strike or spare', () => {
+      scorecard.addFrame(1,2)
+      scorecard.addFrame(4,5)
+      expect(scorecard.addFrameScore(1)).toEqual(9)
+    })
 
+    // it ('shows the current score as 0 upon initialisation', () => {
+    //   expect(scorecard.calculateScore()).toEqual(0)
+    // })
+
+    // it ('update players current score each frame', () => {
+    //   scorecard.addFrame(2,3)
+      // expect(scorecard.getCurrentScore()).toEqual(5)
+    // })
+  })
+  
   describe ('helper methods', () => {
     it ('flattens an array and returns the sum', () =>{
-      scorecard = new Scorecard([])
       expect(scorecard.flattenArrayAndSumContents([4,5])).toEqual(9)
     })
-
-    it ('shows the current score as 0 upon initialisation', () => {
-      scorecard = new Scorecard([])
-      expect(scorecard.getCurrentScore()).toEqual(0)
-    })
   })
-    
-  describe ('score counting methods', () =>{
-    it ('update players current score each frame', () => {
-      scorecard = new Scorecard([[2,3]])
-      // expect(scorecard.getCurrentScore()).toEqual(5)
-    })
-  })
-  // it ('initializes a scorecard', () =>{
-  //   expect(scorecard.getCurrentScore()).toBe(10)
-  // })
 })

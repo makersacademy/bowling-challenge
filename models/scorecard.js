b3e91ea9b32b4f98a92f46ccfc128cc1494072ba
Sidra Iqbal <114309741+siqbal181@users.noreply.github.com>
2023-04-29 19:26:54 +0100

    removed ruby files and added unit test for scorecard

--- models/scorecard.js
+++ models/scorecard.js
@@ -1,5 +1,3 @@
-const Frame = require('./frame');
-
 class Scorecard {
   constructor() {
     this.frames = [];
--- tests/scorecard.test.js
+++ tests/scorecard.test.js
@@ -1,7 +1,21 @@
 const Scorecard = require('../models/scorecard.js');
+const Frame = require('../models/frame.js');
+
+jest.mock('../models/frame.js');
 
 describe('Scorecard', () => {
-  xit("", () => {
-    
+  beforeEach(() => {
+    Frame.mockClear();
+  })
+
+  it("adds frame to the scorecard", () => {
+    const mockFrame = new Frame;
+
+    mockFrame.framePoints.mockImplementation(() => [5,3]);
+
+    const scorecard = new Scorecard;
+    scorecard.addFrame(mockFrame);
+    expect(scorecard.frames.length).toBe(1);
+    expect(scorecard.frames[0].framePoints()).toEqual([5, 3]);
   });
 });

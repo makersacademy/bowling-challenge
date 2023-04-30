af4ed0eda9a493dd33cda3116d078ae4345a882b
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-04-30 15:15:07 +0100

    Refactored tests

--- scorecard.test.js
+++ scorecard.test.js
@@ -1,7 +1,20 @@
 const Scorecard = require('./scorecard');
 
+let scorecard;
+
+const firstNineFrames = () => {
+  scorecard.addFrame(1, 4);
+  scorecard.addFrame(4, 5);
+  scorecard.addFrame(6, 4);
+  scorecard.addFrame(5, 5);
+  scorecard.addFrame(10);
+  scorecard.addFrame(0, 1);
+  scorecard.addFrame(7, 3);
+  scorecard.addFrame(6, 4);
+  scorecard.addFrame(10);
+}
+
 describe('Scorecard', () => {
-  let scorecard;
 
   beforeEach(() => {
     scorecard = new Scorecard();
@@ -47,71 +60,35 @@ describe('Scorecard', () => {
   });
 
   test('the score when there is no strike or spare in the tenth frame', () => {
-    scorecard.addFrame(1, 4);
-    scorecard.addFrame(4, 5);
-    scorecard.addFrame(6, 4);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(10);
-    scorecard.addFrame(0, 1);
-    scorecard.addFrame(7, 3);
-    scorecard.addFrame(6, 4);
-    scorecard.addFrame(10);
+    firstNineFrames();
     scorecard.addFrame(2, 2);
     expect(scorecard.calculateScore()).toEqual(115);
   });
 
   test('the score when there is a strike in the tenth frame plus the two extra rolls', () => {
-    scorecard.addFrame(1, 4);
-    scorecard.addFrame(4, 5);
-    scorecard.addFrame(6, 4);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(10);
-    scorecard.addFrame(0, 1);
-    scorecard.addFrame(7, 3);
-    scorecard.addFrame(6, 4);
-    scorecard.addFrame(10);
+    firstNineFrames();
     scorecard.addFrame(10, 10, 10);
     expect(scorecard.calculateScore()).toEqual(157);
   });
 
   test('the score when there is a spare in the tenth frame plus the one extra roll', () => {
-    scorecard.addFrame(1, 4);
-    scorecard.addFrame(4, 5);
-    scorecard.addFrame(6, 4);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(10);
-    scorecard.addFrame(0, 1);
-    scorecard.addFrame(7, 3);
-    scorecard.addFrame(6, 4);
-    scorecard.addFrame(10);
+    firstNineFrames();
     scorecard.addFrame(8, 2, 10);
     expect(scorecard.calculateScore()).toEqual(137);
   });
 
   test('the score after bowling all spares', () => {
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
-    scorecard.addFrame(5, 5);
+    for (i = 0; i < 9; i++) {
+      scorecard.addFrame(5, 5);
+    }
     scorecard.addFrame(5, 5, 5);
     expect(scorecard.calculateScore()).toEqual(150);
   });
 
   test('the score after bowling 12 strikes - a perfect game', () => {
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
-    scorecard.addFrame(10);
+    for (i = 0; i < 9; i++) {
+      scorecard.addFrame(10);
+    }
     scorecard.addFrame(10, 10, 10);
     expect(scorecard.calculateScore()).toEqual(300);
   });

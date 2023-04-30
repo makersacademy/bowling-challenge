053b4d9122d5c40157f00549e0889ba21f1c908b
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-04-30 16:57:30 +0100

    Refactored code

--- scorecard.js
+++ scorecard.js
@@ -1,51 +1,58 @@
 class Scorecard {
   constructor() {
     this.frames = [];
+    this.score = 0;
+    this.frameIndex = 0;
   }
 
   addFrame(roll1, roll2 = 0, roll3 = 0) { 
-    // create a new frame object and add it to the frames array
     this.frames.push({ roll1, roll2, roll3 });
   }
 
   calculateScore() {
-    let score = 0;
-    let frameIndex = 0;
     const numFrames = this.frames.length;
   
-    for (let i = 0; i < 10 && frameIndex < numFrames; i++) {
-      const frame = this.frames[frameIndex];
+    for (let i = 0; i < 10 && this.frameIndex < numFrames; i++) {
+      const frame = this.frames[this.frameIndex];
       const isStrike = frame.roll1 === 10;
       const isSpare = !isStrike && frame.roll1 + frame.roll2 === 10;
       const frameScore = frame.roll1 + frame.roll2;
   
-      // add bonuses for strikes and spares in the current frame
       if (i === 9) {
-        // Tenth frame logic
-        if (isStrike) {
-          score += 10 + frame.roll1 + frame.roll2 + frame.roll3;
-          frameIndex += 1;
-        } else if (isSpare) {
-          score += 10 + frame.roll3;
-        } else {
-          score += frameScore;
-        }
+        this.tenthFrameScoringLogic(frame, isStrike, isSpare, frameScore);
+        
       } else {
-          // Frames 1-9 logic
-          if (isStrike) {
-            score += 10 + this.getStrikeBonus(frameIndex + 1);
-            frameIndex++;
-          } else if (isSpare) {
-            score += 10 + this.getSpareBonus(frameIndex + 1);
-            frameIndex++;
-          } else {
-            score += frameScore;
-            frameIndex++;
-          }
+          this.firstNineFramesScoringLogic(isStrike, isSpare, frameScore);
       }
     }
-    return score;
+    return this.score;
   }  
+
+  firstNineFramesScoringLogic(isStrike, isSpare, frameScore) {
+    if (isStrike) {
+      this.score += 10 + this.getStrikeBonus(this.frameIndex + 1);
+      this.frameIndex++;
+    } else if (isSpare) {
+        this.score += 10 + this.getSpareBonus(this.frameIndex + 1);
+        this.frameIndex++;
+    } else {
+        this.score += frameScore;
+        this.frameIndex++;
+    }
+    return this.score;
+  }
+
+  tenthFrameScoringLogic(frame, isStrike, isSpare, frameScore) {
+    if (isStrike) {
+      this.score += 10 + frame.roll1 + frame.roll2 + frame.roll3;
+      this.frameIndex++;
+    } else if (isSpare) {
+        this.score += 10 + frame.roll3;
+    } else {
+        this.score += frameScore;
+    }
+    return this.score;
+  }
 
   getStrikeBonus(nextFrameIndex) {
     if (nextFrameIndex < this.frames.length) {

df9fe16ebee8f85ad4ff556de17e240013769d7a
Jon Pillay <jonpillay@MASTUDENT-Jon-Pillay-C02G5H47Q6L4.local>
2023-05-01 18:33:22 +0100

    backend working and scorecard rendering coming along, however needs to some more. MVP... just

--- express/server.js
+++ express/server.js
@@ -40,42 +40,71 @@ app.post("/start_game", (req, res) => {
 })
 
 app.get('/game', (req, res) => {
-  res.render('game', { session: req.session })
+  const cardData = req.session.scorecard
+  const scorecard = new ScoreCard(cardData.frames)
+  res.render('game', { session: req.session, scorecard: scorecard })
 })
 
 app.post('/add_frame', (req, res) => {
   score = req.body.roll
   if (req.session.roll_num == 1 ) {
-    req.session.cur_frame = new Frame(req.session.frame_num)
-    req.session.cur_frame.add_roll(score)
-    req.session.roll_num++
-    req.session.save()
+    const frame = new Frame(req.session.frame_num)
+    const cardData = req.session.scorecard
+    const scorecard = new ScoreCard(cardData.frames)
+    frame.add_roll(parseInt(score))
+    console.log(req.session.frame_num)
+    if (score == '10' && req.session.frame_num != 10 ) {
+      scorecard.add_frame(frame)
+      req.session.frame_num++
+      req.session.roll_num = 1
+    } else if (score == '10' && req.session.frame_num == 10 ) {
+      req.session.roll_num++
+    } else {
+      req.session.roll_num++
+    }
+    req.session.cur_frame = frame
+    req.session.scorecard = scorecard
     res.redirect('/game')
   } else if (req.session.roll_num == 2 && req.session.frame_num != 10) {
-    console.log(req.session.scorecard)
-    req.session.cur_frame.score.push(score)
-    req.session.scorecard.frames.push(req.session.cur_frame)
-    req.session.cur_frame = null
-    req.session.roll_num = 1
-    req.session.frame_num++
-    res.redirect('/game')
+      const frameData = req.session.cur_frame
+      const frame = new Frame(frameData.frame_num, frameData.score)
+      const cardData = req.session.scorecard
+      const scorecard = new ScoreCard(cardData.frames)
+      frame.add_roll(parseInt(score))
+      scorecard.add_frame(frame)
+      req.session.cur_frame = null
+      req.session.scorecard = scorecard
+      req.session.roll_num = 1
+      req.session.frame_num++
+      res.redirect('/game')
   } else if (req.session.roll_num == 2 && req.session.frame_num == 10) {
-    console.log(req.session.scorecard)
-    req.session.cur_frame.score.push(score);
-      if ( sum_arr(req.session.cur_frame.score) < 10 ) {
-        req.session.scorecard.add_frame(req.session.cur_frame)
-        req.session.cur_frame = null
-        req.session.game_in_progress = false
-        res.redirect('/game')
-      } else {
-        req.session.roll_num = 3
-        res.redirect('/game')
-      }
+      console.log(req.session.scorecard)
+      req.session.cur_frame.score.push(parseInt(score));
+        if ( sum_arr(req.session.cur_frame.score) < 10 && req.session.roll_num == 2 ) {
+          const frameData = req.session.cur_frame
+          const frame = new Frame(frameData.frame_num, frameData.score)
+          const cardData = req.session.scorecard
+          const scorecard = new ScoreCard(cardData.frames)
+          scorecard.add_frame(req.session.cur_frame)
+          req.session.scorecard = scorecard
+          req.session.cur_frame = null
+          req.session.game_in_progress = false
+          res.redirect('/game')
+        } else {
+          req.session.roll_num = 3
+          res.redirect('/game')
+        }
   } else if (req.session.roll_num == 3 ) {
-        req.session.cur_frame.score.push(score)
-        req.session.scorecard.frames.push(req.session.cur_frame)
-        req.session.game_in_progress = false
-        res.redirect('/game')
+      const frameData = req.session.cur_frame
+      const frame = new Frame(frameData.frame_num, frameData.score)
+      const cardData = req.session.scorecard
+      const scorecard = new ScoreCard(cardData.frames)
+      frame.add_roll(parseInt(score))
+      scorecard.add_frame(req.session.cur_frame)
+      req.session.scorecard = scorecard
+      req.session.game_in_progress = false
+      console.log(req.session.scorecard)
+      res.redirect('/game')
   }
 })
 

26844adf9fb50691d9f9c535a1cfcc228487b1b4
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-26 15:50:16 +0100

    one bug to be fix on last frame

--- scorecard.js
+++ scorecard.js
@@ -3,15 +3,16 @@ class Scorecard {
     this.frames = [];
     this.basicScore = 0;
     this.strikeBonusScore = 0;
+    this.spareBonusScore = 0;
   }
 
-  addFrame(roll1, roll2) {
-    return this.frames.push([roll1, roll2]);
+  addFrame(roll1, roll2, roll3) {
+    return this.frames.push([roll1, roll2, roll3]);
   }
 
   calculateBasicScore() {
     this.frames.map((frame) => {
-      const frameBasicScore = frame[0] + frame[1];
+      const frameBasicScore = frame[0] + frame[1] + frame[2];
       this.basicScore += frameBasicScore;
     });
   }
@@ -21,14 +22,24 @@ class Scorecard {
       const currentArray = this.frames[i];
       const nextArray = this.frames[i + 1];
       if (currentArray[0] === 10) {
-          const sum = nextArray.reduce((accumulator, element) => accumulator + element, 0);
+          const sum = nextArray.reduce((roll1, roll2, roll3) => roll1 + roll2, 0);
           this.strikeBonusScore += sum;
       }
     }
   }
 
+  calculateSpareBonuses() {
+    for (let i = 0; i < this.frames.length - 1; i++) {
+      const currentArray = this.frames[i];
+      const nextArray = this.frames[i + 1];
+      if ((currentArray[0] + currentArray[1] === 10) && (currentArray[0] !== 10)) {
+        this.spareBonusScore += nextArray[0];
+      }
+    }
+  }
+
   getTotalScore() {
-    return this.basicScore + this.strikeBonusScore;
+    return this.basicScore + this.strikeBonusScore + this.spareBonusScore;
   }
   
   getFrames() {
--- scorecard.test.js
+++ scorecard.test.js
@@ -9,34 +9,79 @@ describe('Scorecard', () => {
 
   it('should return the score of 7 first frame in the array', () => {
     const scorecard = new Scorecard();
-    scorecard.addFrame(2, 5);
+    scorecard.addFrame(2, 5, 0);
     scorecard.calculateBasicScore();
     scorecard.calculateStrikeBonuses();
 
-    expect(scorecard.getFrames()).toEqual([[2, 5]]); 
+    expect(scorecard.getFrames()).toEqual([[2, 5, 0]]); 
     expect(scorecard.getTotalScore()).toBe(7);
   });
 
   it('should return a score of 15 and two frames in the array', () => {
     const scorecard = new Scorecard();
-    scorecard.addFrame(2, 5);
-    scorecard.addFrame(3, 5);
+    scorecard.addFrame(2, 5, 0);
+    scorecard.addFrame(3, 5, 0);
     scorecard.calculateBasicScore();
     scorecard.calculateStrikeBonuses();
 
-    expect(scorecard.getFrames()).toEqual([[2, 5], [3, 5]]);
+    expect(scorecard.getFrames()).toEqual([[2, 5, 0], [3, 5, 0]]);
     expect(scorecard.getTotalScore()).toBe(15);
   });
 
   it('should return 29 instead of 27 because the strike doubles the next two rolls', () => {
     const scorecard = new Scorecard();
-    scorecard.addFrame(2, 5);
-    scorecard.addFrame(3, 5);
-    scorecard.addFrame(10, 0);
-    scorecard.addFrame(1, 1);
+    scorecard.addFrame(2, 5, 0);
+    scorecard.addFrame(3, 5, 0);
+    scorecard.addFrame(10, 0, 0);
+    scorecard.addFrame(1, 1, 0);
     scorecard.calculateBasicScore();
     scorecard.calculateStrikeBonuses();
 
     expect(scorecard.getTotalScore()).toBe(29);
+  })
+
+  it('should return 55 instead of 45 because two strikes doubles the next rolls', () => {
+    const scorecard = new Scorecard();
+    scorecard.addFrame(2, 5, 0);
+    scorecard.addFrame(3, 5, 0);
+    scorecard.addFrame(10, 0, 0);
+    scorecard.addFrame(1, 1, 0);
+    scorecard.addFrame(10, 0, 0);
+    scorecard.addFrame(3, 5, 0);
+    scorecard.calculateBasicScore();
+    scorecard.calculateStrikeBonuses();
+
+    expect(scorecard.getTotalScore()).toBe(55);
+  })
+
+  it('should return 17 instead of 16 because the spare doubles the first roll of the next frame', () => {
+    const scorecard = new Scorecard();
+    scorecard.addFrame(5, 5, 0);
+    scorecard.addFrame(1, 5, 0);
+    scorecard.calculateBasicScore();
+    scorecard.calculateStrikeBonuses();
+    scorecard.calculateSpareBonuses();
+
+    expect(scorecard.getTotalScore()).toBe(17);
+  })
+
+  it('should return 17 instead of 16 because the spare doubles the first roll of the next frame', () => {
+    const scorecard = new Scorecard();
+    scorecard.addFrame(1, 4, 0);
+    scorecard.addFrame(4, 5, 0);
+    scorecard.addFrame(6, 4, 0);
+    scorecard.addFrame(5, 5, 0);
+    scorecard.addFrame(10, 0, 0);
+    scorecard.addFrame(0, 1, 0);
+    scorecard.addFrame(7, 3, 0);
+    scorecard.addFrame(6, 4, 0);
+    scorecard.addFrame(10, 0, 0);
+    scorecard.addFrame(2, 8, 6);
+
+    scorecard.calculateBasicScore();
+    scorecard.calculateStrikeBonuses();
+    scorecard.calculateSpareBonuses();
+
+    expect(scorecard.getTotalScore()).toBe(133);
   })
 });

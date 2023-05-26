81236372e6a5a4b70467f9870fc3cd8ea69a3851
awdem <wdemontmollin@gmail.com>
2023-05-26 09:03:24 +0100

    created placeholder frame class to automock in bowlingGame tests

--- spec/BowlingGame.test.js
+++ spec/BowlingGame.test.js
@@ -1,14 +1,34 @@
-const { BowlingGame } = require('../lib/index')
+const {BowlingGame} = require('../lib/index');
+const Frame = require('../lib/Frame');
+// auto mocks the frame class
+jest.mock('../lib/Frame');
 
 let bowlingGame = new BowlingGame();
 
-// this creates new instances before each test
+// this creates a new game instance before each test
 beforeEach(() => {
-  bowlingGame = new BowlingGame();
+  Frame.mockClear();
 });
 
 describe('BowlingGame class', () => {
   it('constructs', () => {
+    const mockFrame = new Frame();
+    bowlingGame = new BowlingGame(mockFrame);
     expect(bowlingGame).toBeTruthy();
-  })
+    expect(bowlingGame).toHaveProperty('currentFrame', mockFrame);
+  });
+
+  describe('RollResult method', () => {
+    it('adds the result of a regular frame to the current frame', () => {
+      const mockFrame = new Frame();
+      bowlingGame = new BowlingGame(mockFrame);
+
+      bowlingGame.rollResult(2);
+      bowlingGame.rollResult(4);
+
+      expect(mockFrame.addRoll).toHaveBeenCalledTimes(2);
+      expect(mockFrame.addRoll).toHaveBeenNthCalledWith(1, 2);
+      expect(mockFrame.addRoll).toHaveBeenCalledTimes(2, 4);
+    });
+  });
 });

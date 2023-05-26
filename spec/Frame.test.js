352a455eef369a794fd6abf08d5e708846434a67
awdem <wdemontmollin@gmail.com>
2023-05-26 09:47:16 +0100

    implemented getRolls() with test

--- spec/Frame.test.js
+++ spec/Frame.test.js
@@ -6,10 +6,15 @@ beforeEach(() => {
   frame = new Frame();
 })
 
-
 describe('Frame class', () => {
   it('constructs', () => {
     expect(frame).toBeTruthy();
     expect(frame).toHaveProperty('rolls', []);
+  });
+
+  describe('getRolls', () => {
+    it('returns this.rolls', () => {
+      expect(frame.getRolls()).toEqual([]);
+    });
   });
 });

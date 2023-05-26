489f5179443d5a2bcaea095afe9d80c2062cc4fa
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-26 15:08:19 +0100

    divide #calculate method in two methods for basic score and bonus after strike

--- scorecard.js
+++ scorecard.js
@@ -1,26 +1,39 @@
 class Scorecard {
   constructor() {
     this.frames = [];
-    this.score = 0;
-  }
-  calculateScore() {
-    this.frames.forEach(frame => {
-      const frameScore = frame[0] + frame[1];
-      this.score += frameScore;
-    });
-    return this.score;
-  };
-
-  getFrames() {
-    return this.frames;
+    this.basicScore = 0;
+    this.strikeBonusScore = 0;
   }
 
   addFrame(roll1, roll2) {
     return this.frames.push([roll1, roll2]);
   }
 
+  calculateBasicScore() {
+    this.frames.map((frame) => {
+      const frameBasicScore = frame[0] + frame[1];
+      this.basicScore += frameBasicScore;
+    });
+  }
 
+  calculateStrikeBonuses() {
+    for (let i = 0; i < this.frames.length - 1; i++) {
+      const currentArray = this.frames[i];
+      const nextArray = this.frames[i + 1];
+      if (currentArray[0] === 10) {
+          const sum = nextArray.reduce((accumulator, element) => accumulator + element, 0);
+          this.strikeBonusScore += sum;
+      }
+    }
+  }
+
+  getTotalScore() {
+    return this.basicScore + this.strikeBonusScore;
+  }
+  
+  getFrames() {
+    return this.frames;
+  }
 };
-
 
 module.exports = Scorecard;
--- scorecard.test.js
+++ scorecard.test.js
@@ -3,22 +3,40 @@ const Scorecard = require('./scorecard');
 describe('Scorecard', () => {
   it('should initially return 0 and an empty array of frames', () => {
     const scorecard = new Scorecard();
-    expect(scorecard.calculateScore()).toBe(0);
     expect(scorecard.getFrames()).toEqual([]);
+    expect(scorecard.getTotalScore()).toBe(0);
   });
 
-  it('should return the first frame in the array', () => {
+  it('should return the score of 7 first frame in the array', () => {
     const scorecard = new Scorecard();
     scorecard.addFrame(2, 5);
-    expect(scorecard.getFrames()).toEqual([[2, 5]]);
-    expect(scorecard.calculateScore()).toBe(7);
+    scorecard.calculateBasicScore();
+    scorecard.calculateStrikeBonuses();
+
+    expect(scorecard.getFrames()).toEqual([[2, 5]]); 
+    expect(scorecard.getTotalScore()).toBe(7);
   });
 
   it('should return a score of 15 and two frames in the array', () => {
     const scorecard = new Scorecard();
     scorecard.addFrame(2, 5);
     scorecard.addFrame(3, 5);
+    scorecard.calculateBasicScore();
+    scorecard.calculateStrikeBonuses();
+
     expect(scorecard.getFrames()).toEqual([[2, 5], [3, 5]]);
-    expect(scorecard.calculateScore()).toBe(15);
+    expect(scorecard.getTotalScore()).toBe(15);
   });
+
+  it('should return 29 instead of 27 because the strike doubles the next two rolls', () => {
+    const scorecard = new Scorecard();
+    scorecard.addFrame(2, 5);
+    scorecard.addFrame(3, 5);
+    scorecard.addFrame(10, 0);
+    scorecard.addFrame(1, 1);
+    scorecard.calculateBasicScore();
+    scorecard.calculateStrikeBonuses();
+
+    expect(scorecard.getTotalScore()).toBe(29);
+  })
 });

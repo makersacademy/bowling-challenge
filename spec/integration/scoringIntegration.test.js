402ef7b9ecc062d120152a3f24fb8f942890aee6
awdem <wdemontmollin@gmail.com>
2023-05-26 11:31:39 +0100

    updated first integration test

--- spec/integration/scoringIntegration.test.js
+++ spec/integration/scoringIntegration.test.js
@@ -2,22 +2,20 @@ const {BowlingGame, Frame} = require('../../lib/index');
 
 
 let bowlingGame = new BowlingGame();
-let frame = new Frame();
+
 // resets bowling game to a new instance before each test
 beforeEach(() => {
-  frame = new Frame();
   bowlingGame = new BowlingGame();
 });
 
 describe('scoring integration', () => {
-  it.skip('scores a regular bowling game with no strikes or spares', () => {
+  it('scores a regular bowling game with no strikes or spares', () => {
     // 10 regular frames
     for (let i = 1; i <= 10; i++) {
       bowlingGame.addRollToFrame(2);
       bowlingGame.addRollToFrame(2);
       bowlingGame.checkFrameEnd();
     }
-
-    expect(bowlingGame.getScore).toBe(40);
+    expect(bowlingGame.getRunningScore()).toBe(40);
   });
 });

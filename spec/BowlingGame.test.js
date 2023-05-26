80373adfd4285d7aacb9bfdcc8b260446947210a
awdem <wdemontmollin@gmail.com>
2023-05-26 10:16:16 +0100

    refactored test suite to be easier to read

--- spec/BowlingGame.test.js
+++ spec/BowlingGame.test.js
@@ -3,31 +3,36 @@ const Frame = require('../lib/Frame');
 
 // auto mocks the frame class
 jest.mock('../lib/Frame');
+let mockFrame = new Frame();
+let bowlingGame = new BowlingGame(mockFrame);
 
 // this creates a new game instance before each test
 beforeEach(() => {
+  mockFrame = new Frame();
+  bowlingGame = new BowlingGame(mockFrame);
   Frame.mockClear();
 });
 
 describe('BowlingGame class', () => {
   it('constructs', () => {
-    const mockFrame = new Frame();
-    bowlingGame = new BowlingGame(mockFrame);
     expect(bowlingGame).toBeTruthy();
     expect(bowlingGame).toHaveProperty('currentFrame', mockFrame);
   });
 
   describe('RollResult method', () => {
     it('adds the result of a regular frame to the current frame', () => {
-      const mockFrame = new Frame();
-      bowlingGame = new BowlingGame(mockFrame);
-
       bowlingGame.rollResult(2);
       bowlingGame.rollResult(4);
 
       expect(mockFrame.addRoll).toHaveBeenCalledTimes(2);
       expect(mockFrame.addRoll).toHaveBeenNthCalledWith(1, 2);
       expect(mockFrame.addRoll).toHaveBeenNthCalledWith(2, 4);
+    });
+  });
+
+  describe('checkFrameEnd method', () => {
+    it('placeholder', () => {
+      // test here
     });
   });
 });

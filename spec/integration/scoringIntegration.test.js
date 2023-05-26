03fc0467531c8bccdc1de93a0432a2b5292cf90b
awdem <wdemontmollin@gmail.com>
2023-05-26 07:46:53 +0100

    wrote first integration test

---
+++ spec/integration/scoringIntegration.test.js
@@ -0,0 +1,24 @@
+const { BowlingGame } = require('../../lib/index')
+
+
+let bowlingGame = new BowlingGame();
+
+// this creates new instances before each test
+beforeEach(() => {
+  bowlingGame = new BowlingGame();
+});
+
+
+describe('scoring integration', () => {
+  it('scores a regular bowling game with no strikes or spares', () => {
+    // 10 regular frames
+    for (let i = 1 ; i <= 10 ; i++) {
+      bowlingGame.rollResult(2)
+      bowlingGame.rollResult(2)
+      bowlingGame.checkFrameEnd()
+    }
+
+    expect(bowlingGame.getScore).toBe(40)
+  });
+});
+

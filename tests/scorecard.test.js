3a2933aa02c636c50aed8c643b50be2ef82baa9d
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-26 15:15:17 +0100

    cleaned up test for frame rolls

--- tests/scorecard.test.js
+++ tests/scorecard.test.js
@@ -5,27 +5,24 @@ describe('Scorecard', () => {
     it('starts with an empty frames array', () => {
       scorecard = new Scorecard();
 
-      expect(scorecard.frames).toEqual([])
+      expect(scorecard.frames).toEqual([]);
     })
 
     it('adds one frame to frames array', () => {
       scorecard = new Scorecard();
-      scorecard.addFrame(2, 2)
+      scorecard.addFrame(2, 2);
 
-      expect(scorecard.frames[0].rolls[0]).toEqual(2)
-      expect(scorecard.frames[0].rolls[1]).toEqual(2)
+      expect(scorecard.frames[0].rolls).toEqual([2, 2]);
     })
 
 
     it('adds two frames to frames array', () => {
       scorecard = new Scorecard();
-      scorecard.addFrame(2, 2)
-      scorecard.addFrame(3, 4)
+      scorecard.addFrame(2, 2);
+      scorecard.addFrame(3, 4);
 
-      expect(scorecard.frames[0].rolls[0]).toEqual(2)
-      expect(scorecard.frames[0].rolls[1]).toEqual(2)
-      expect(scorecard.frames[1].rolls[0]).toEqual(3)
-      expect(scorecard.frames[1].rolls[1]).toEqual(4)
+      expect(scorecard.frames[0].rolls).toEqual([2, 2]);
+      expect(scorecard.frames[1].rolls).toEqual([3, 4]);
     })
   })
 })

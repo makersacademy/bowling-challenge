d2a16c6971f47381d9dee816b2e6bd40492e6620
Verity <veritywong90@gmail.com>
2023-05-28 17:37:31 +0100

    strike bonus points added

--- score.js
+++ score.js
@@ -1,65 +1,98 @@
 class Scorecard {
 
     constructor() {
-        this.scorecard = []
+        this.frames = []
 
     }
 
     calculateScore() {
-        let sum = 0;
-    
-        for (let i = 0; i < this.scorecard.length; i++) {
-            // frame is the frame array e.g. [10, 2]
-            let currentFrame = this.scorecard[i];
-            let nextFrame = this.scorecard[i + 1]
-                for (let j = 0 ; j < currentFrame.length; j++) {
-                    if (currentFrame.length < 2) {
-                        for (let j = 0 ; j < nextFrame.length; j++) {
-                        sum += nextFrame[j];
-                        sum += 10;
-                        }
-                    } 
+    let sum = 0;
+   
+        for (let i = 0; i < this.frames.length; i++) {
+            let currentFrame = this.frames[i]
+            let nextFrame = this.frames[i + 1]
+            let nextNextFrame = this.frames[i + 2]
+
+            sum += this.frameScore(currentFrame)
+              // when strike on final frame
+                if (currentFrame === this.frames[-1] && nextFrame === undefined && nextNextFrame === undefined ) {
+                    sum += this.frameScore(currentFrame);
                 }
-                
-            }
-            return sum;
-            // // frame is the frame array e.g. [10, 2]
-            // let frame = this.scorecard[i].frame;
-            // let nextFrame = this.scorecard[i += 1].frame;
-            // console.log(nextFrame);
-            // if (frame.length === 1) {
-            //     frame.push(nextFrame[0]);
-            //     frame.push(nextFrame[1]);
-            //     for (let j = 0 ; j < frame.length; j++) {
-            //         sum += frame[j];
-            //     }
-            // }
-            
+            // when strike   
+  
+                else if (currentFrame.length < 2 && nextFrame.length === 2) {
+                    sum += this.addStrikeBonus(currentFrame, nextFrame);
+                    
+                }
+            // when strikes in a row
+                else if (currentFrame.length < 2 && nextFrame.length < 2) {
+                    sum += this.addStrikesInARowBonus(currentFrame, nextFrame, nextNextFrame);
+                    
+                }
+
+            // when spare
+                if (this.frameScore(currentFrame) === 10 && currentFrame.length === 2 ) {
+                sum += nextFrame[0];
+                }  
+        }
+        return sum;
+
+
     }
-        
-    
 
     addFrame(roll1, roll2) {
         if (roll1 < 10) {
-            this.scorecard.push([roll1,  roll2]);
+            this.frames.push([roll1, roll2]);
         } else if (roll1 === 10) {
-            this.scorecard.push('strike');
+            this.frames.push([roll1]);   
         }
-        // this.scorecard.push({frame: [roll1,  roll2]});
-        return this.scorecard;
+        
+    }
 
+    frameScore(currentFrame) {
+        this.currentFrame = currentFrame
+        let sum = 0;
 
+        for (let j = 0 ; j < currentFrame.length; j++) {
+            sum += currentFrame[j]; 
+        }
+        return sum;
+    }
+
+    addStrikeBonus(currentFrame, nextFrame) {
+        this.currentFrame = currentFrame;
+        this.nextFrame = nextFrame;
+
+        let sum = 0;
+
+        sum += nextFrame[0];
+        sum += nextFrame[1];
+
+        return sum;
+    }
+    addStrikesInARowBonus(currentFrame, nextFrame, nextNextFrame) {
+        this.currentFrame = currentFrame;
+        this.nextFrame = nextFrame;
+        this.nextNextFrame = nextNextFrame;
+
+        let sum = 0;
+
+        sum += nextFrame[0];
+        sum += nextNextFrame[0];
+
+        return sum;
     }
 
 }
 
 let scorecard = new Scorecard;
 
-scorecard.addFrame(10, 0);
-scorecard.addFrame(2, 2);
+scorecard.addFrame(2,2);
+scorecard.addFrame(10,0);
+scorecard.addFrame(5,2);
 
-console.log(scorecard.scorecard.length)
-console.log(scorecard)
+console.log(scorecard.calculateScore());
+
 
 
 
--- tests/scorecard.test.js
+++ tests/scorecard.test.js
@@ -11,14 +11,74 @@ describe('Scorecard', () => {
         
     })
 
-    it('adds next two rolls to first frame when strike', () => {
+    it('adds bonus points when strike', () => {
         let scorecard = new Scorecard()
 
         expect(scorecard.calculateScore()).toEqual(0)
         scorecard.addFrame(10, 0)
         scorecard.addFrame(2, 2)
-        expect(scorecard.calculateScore()).toEqual(85)
+        expect(scorecard.calculateScore()).toEqual(18)
         
     })
+
+    it('adds bonus points when strike', () => {
+        let scorecard = new Scorecard()
+
+        expect(scorecard.calculateScore()).toEqual(0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(2, 2)
+        expect(scorecard.calculateScore()).toEqual(40)
+        
+    })
+
+    it('adds bonus points when strike', () => {
+        let scorecard = new Scorecard()
+
+        expect(scorecard.calculateScore()).toEqual(0)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        scorecard.addFrame(2, 2)
+        expect(scorecard.calculateScore()).toEqual(40)
+        
+    })
+    
+    it('adds bonus points for spare', () => {
+        let scorecard = new Scorecard()
+
+        expect(scorecard.calculateScore()).toEqual(0)
+        scorecard.addFrame(1, 9)
+        scorecard.addFrame(1, 3)
+        expect(scorecard.calculateScore()).toEqual(15)
+        
+    })
+
+    xit('allows bonus points for srike on final frame', () => {
+        let scorecard = new Scorecard()
+
+        expect(scorecard.calculateScore()).toEqual(0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        scorecard.addFrame(10, 0)
+        // scorecard.addFrame(10, 0)
+        scorecard.addFrame(5, 5)
+        
+        expect(scorecard.calculateScore()).toEqual(300)
+        
+    })
+
 })
 

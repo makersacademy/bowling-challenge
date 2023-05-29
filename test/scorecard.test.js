757cb689fdc400b61b9ea6b40beb8e747468c5f4
Cameron <cameronjamesbishop@googlemail.com>
2023-05-29 22:58:48 +0100

    Finishing touches

--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -21,4 +21,51 @@ describe('Scorecard', () => {
         score.addFrame(6,2);
         expect(score.calculateScore()).toBe(24);
     });
+
+    it('calculates score : one strike, two frames', () => {
+        const score = new Scorecard ();
+        score.addFrame(10,0);
+        score.addFrame(6,1);
+        expect(score.calculateScore()).toBe(24);
+    });
+
+    it('calculates score : two strike, three frames', () => {
+        const score = new Scorecard ();
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(6,1);
+        expect(score.calculateScore()).toBe(50);
+    });
+
+    it('calculates score : all frames', () => {
+        const score = new Scorecard ();
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(6,1);
+        score.addFrame(9,1);
+        score.addFrame(9,0);
+        score.addFrame(7,2);
+        score.addFrame(3,5);
+        score.addFrame(10,0);
+        score.addFrame(9,1);
+        score.addFrame10(9,1,10);
+
+        expect(score.calculateScore()).toBe(154);
+    });
+
+    it('calculates score : all frames, perfect score', () => {
+        const score = new Scorecard ();
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame(10,0);
+        score.addFrame10(10,10,10);
+        expect(score.calculateScore()).toBe(300);
+    });
+
 });

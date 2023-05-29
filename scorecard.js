30ff415ac258aa3b2d1ea04f8f93f82d6e00f9e0
sarahc-dev <sarahclements@hotmail.co.uk>
2023-05-29 16:43:14 +0100

    refactor and make methods private

--- scorecard.js
+++ scorecard.js
@@ -10,10 +10,10 @@ class Scorecard {
   addFrame(frame) {
     const newFrame = { frame: frame };
     this.scores.push(newFrame);
-    this.calculateScore();
+    this.#calculateScore();
   }
 
-  calculateScore() {
+  #calculateScore() {
     let currentTotal = 0;
     this.scores.forEach((score, index) => {
       if (score.score) {
@@ -21,25 +21,15 @@ class Scorecard {
         return;
       }
 
-      const frameTotal = this.frameTotal(score);
+      const frameTotal = this.#frameTotal(score);
 
       if (frameTotal < 10 || score.frame.length === 3) {
         currentTotal += frameTotal;
         score.score = currentTotal;
       } else if (score.frame.length === 2) {
-        // spare
-        const bonus = this.calculateSpareBonus(index);
-        if (bonus) {
-          currentTotal += frameTotal + bonus;
-          score.score = currentTotal;
-        }
+        currentTotal = this.#addBonus("spare", index, currentTotal, frameTotal, score);
       } else if (score.frame.length === 1) {
-        //strike
-        const bonus = this.calculateStrikeBonus(index);
-        if (bonus) {
-          currentTotal += frameTotal + bonus;
-          score.score = currentTotal;
-        }
+        currentTotal = this.#addBonus("strike", index, currentTotal, frameTotal, score);
       } else {
         console.log("Error");
       }
@@ -47,22 +37,32 @@ class Scorecard {
     return currentTotal;
   }
 
-  calculateSpareBonus(index) {
+  #frameTotal(score) {
+    return score.frame.reduce((a, b) => a + b, 0);
+  }
+
+  #addBonus(type, index, currentTotal, frameTotal, score) {
+    const bonus = type === "strike" ? this.#calculateStrikeBonus(index) : this.#calculateSpareBonus(index);
+
+    if (bonus) {
+      currentTotal += frameTotal + bonus;
+      score.score = currentTotal;
+      return currentTotal;
+    }
+  }
+
+  #calculateSpareBonus(index) {
     if (this.scores[index + 1]) {
       return this.scores[index + 1].frame[0];
     }
   }
 
-  calculateStrikeBonus(index) {
+  #calculateStrikeBonus(index) {
     if (this.scores[index + 2]) {
       return this.scores[index + 2].frame[0] + this.scores[index + 1].frame[0];
     } else if (this.scores[index + 1] && this.scores[index + 1].frame.length > 1) {
       return this.scores[index + 1].frame[0] + this.scores[index + 1].frame[1];
     }
-  }
-
-  frameTotal(score) {
-    return score.frame.reduce((a, b) => a + b, 0);
   }
 }
 

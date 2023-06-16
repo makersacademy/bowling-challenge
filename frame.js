1dfabd33f6f70eb0a23cc503b7722cbc2178546b
Sean Peters <sean@snptrs.dev>
2023-06-16 19:01:34 +0100

    Adds .frameScore, .totalFrameScore and .updateBonusScore

--- frame.js
+++ frame.js
@@ -1,6 +1,7 @@
 class Frame {
   constructor() {
     this.ballScores = [0, 0, 0];
+    this.bonusScore = 0;
   }
 
   addBallScore(ball, score) {
@@ -9,6 +10,25 @@ class Frame {
 
   getBallScore(ball) {
     return this.ballScores[ball - 1];
+  }
+
+  frameScore() {
+    return this.ballScores.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+  }
+
+  totalFrameScore() {
+    return (
+      this.bonusScore +
+      this.ballScores.reduce(
+        (accumulator, currentValue) => accumulator + currentValue
+      )
+    );
+  }
+
+  updateBonusScore(bonus) {
+    this.bonusScore += bonus;
   }
 }
 
--- frame.test.js
+++ frame.test.js
@@ -9,4 +9,17 @@ describe("Frame", () => {
     frame.addBallScore(1, 5);
     expect(frame.getBallScore(1)).toBe(5);
   });
+
+  test(".frameScore returns the score for the frame, excluding bonuses", () => {
+    frame.addBallScore(1, 5);
+    frame.addBallScore(2, 3);
+    expect(frame.frameScore()).toBe(8);
+  });
+
+  test(".totalFrameScore returns the score for the frame, including bonuses", () => {
+    frame.addBallScore(1, 5);
+    frame.addBallScore(2, 3);
+    frame.updateBonusScore(10);
+    expect(frame.totalFrameScore()).toBe(18);
+  });
 });

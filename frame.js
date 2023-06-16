9a43977ed9336f548654d4faabf0158e032ef9d9
Sean Peters <sean@snptrs.dev>
2023-06-16 18:37:20 +0100

    Adds Frame.addBallScore and .getBallScore

--- frame.js
+++ frame.js
@@ -1,1 +1,15 @@
-class Frame {}
+class Frame {
+  constructor() {
+    this.ballScores = [0, 0, 0];
+  }
+
+  addBallScore(ball, score) {
+    this.ballScores[ball - 1] = score;
+  }
+
+  getBallScore(ball) {
+    return this.ballScores[ball - 1];
+  }
+}
+
+module.exports = Frame;
--- frame.test.js
+++ frame.test.js
@@ -1,7 +1,12 @@
 const Frame = require("./frame");
 
 describe("Frame", () => {
-  it("", () => {
-    expect().toBe();
+  beforeEach(() => {
+    frame = new Frame();
+  });
+
+  test(".addBallScore adds a score to the frame", () => {
+    frame.addBallScore(1, 5);
+    expect(frame.getBallScore(1)).toBe(5);
   });
 });

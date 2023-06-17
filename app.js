f4e4f9f2eee940ca1d1ae6c70ba7d740fb888008
Sean Peters <sean@snptrs.dev>
2023-06-17 17:41:54 +0100

    Adds routes

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const Frame = require("./frame");
 const Gameplay = require("./gameplay");
 const ScoreCard = require("./scorecard");
 const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const multer = require("multer"); // v1.0.5
+const upload = multer(); // for parsing multipart/form-data
 
 const express = require("express");
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set("view engine", "hbs");
 app.engine(
   "hbs",
@@ -18,6 +23,7 @@ app.engine(
 
 const frames = {};
 const game = new Gameplay();
+const scoreCard = new ScoreCard();
 
 const createFrames = () => {
   for (i = 0; i < 10; i++) {
@@ -29,13 +35,26 @@ const createFrames = () => {
 createFrames();
 
 app.get("/", (req, res) => {
-  res.render("main", { layout: "index" });
+  console.log(frames[game.currentFrame].getRemainingPins());
+  res.render("scorecard", {
+    layout: "index",
+    scores: scoreCard.getBallScores(frames),
+    currentFrame: game.currentFrame,
+    currentBall: game.currentBall,
+    remainingPins: frames[game.currentFrame].getRemainingPins(),
+  });
 });
 
-app.get("/:frame/:ball/:score", (req, res) => {
-  res.send(
-    `Frame: ${req.params["frame"]}<br />Ball: ${req.params["ball"]}<br />Score: ${req.params["score"]}`
-  );
+app.post("/score", (req, res) => {
+  const score = req.body.score;
+  console.log(score);
+  frames[game.currentFrame].setBallScore(game.currentBall, req.body.score);
+
+  if (game.checkContinue(frames)) {
+    game.setNextBall(frames);
+  }
+
+  res.redirect("/");
 });
 
 console.log(`Server listening on localhost:${port}`);

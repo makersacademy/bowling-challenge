5894f299b292345a03cc8fc22dff448e1dee9de8
Sean Peters <sean@snptrs.dev>
2023-06-18 16:58:08 +0100

    Updates Scorecard to handle spares and strikes better

--- scorecard.js
+++ scorecard.js
@@ -2,16 +2,23 @@ class ScoreCard {
   constructor() {}
 
   formatScoreForDisplay(frame, frameNumber, ball) {
-    if (frame.getStrike()) {
-      if (ball === 1 || (ball > 1 && frameNumber === 10)) {
-        return "X";
-      } else if (ball > 1 && frameNumber === 10) {
-        return frame.getBallScore(ball);
-      }
+    if (frame.getBallScore(ball) === 10) {
+      return "X";
     } else if (
-      (frame.getSpare() && frameNumber < 10 && ball == 2) ||
-      (frameNumber === 10 && frame.getSpare() && (ball == 2 || ball == 3))
+      frameNumber === 10 &&
+      ball === 2 &&
+      frame.getSpare() &&
+      frame.getBallScore(1) + frame.getBallScore(2) === 10
     ) {
+      return "/";
+    } else if (
+      frameNumber === 10 &&
+      ball === 3 &&
+      frame.getSpare() &&
+      frame.getBallScore(2) + frame.getBallScore(3) === 10
+    ) {
+      return "/";
+    } else if (ball === 2 && frame.getSpare()) {
       return "/";
     } else if (frame.getBallScore(ball) === 0 && ball <= frame.lastBallPlayed) {
       return "-";
@@ -44,40 +51,32 @@ class ScoreCard {
     const previousFrame = currentFrame - 1;
     const frameBeforeLast = currentFrame - 2;
 
-    if (previousFrame === 0) {
-      return;
-    }
-
     if (
-      frames[currentFrame].checkTwoBallsPlayed() &&
-      frames[currentFrame].getStrike() == false &&
+      currentFrame === 10 &&
+      frames[currentFrame].getBallScore(1) === 10 &&
+      frames[currentFrame].getBallScore(2) === 10 &&
       frames[previousFrame].getStrike() &&
       frames[previousFrame].bonusScore === 0
     ) {
       frames[previousFrame].setBonusScore(frames[currentFrame].frameScore());
-    }
-
-    if (frameBeforeLast === 0) {
-      return;
-    }
-
-    if (
-      frames[currentFrame].getStrike() &&
+    } else if (
+      previousFrame > 0 &&
+      frames[currentFrame].checkTwoBallsPlayed() &&
       frames[previousFrame].getStrike() &&
-      frames[frameBeforeLast].getStrike()
+      frames[previousFrame].bonusScore === 0
     ) {
-      frames[frameBeforeLast].setBonusScore(20);
-    }
-
-    if (
-      frames[currentFrame].getBallScore(1) > 0 &&
-      frames[previousFrame].getStrike() == true &&
-      frames[frameBeforeLast].getStrike() == true &&
-      frames[frameBeforeLast].getBonusScore() === 0
+      frames[previousFrame].setBonusScore(frames[currentFrame].frameScore());
+    } else if (
+      frameBeforeLast > 0 &&
+      (frames[currentFrame].getStrike() ||
+        frames[currentFrame].getBallScore(1) > 0) &&
+      frames[previousFrame].getStrike() &&
+      frames[frameBeforeLast].getStrike() &&
+      frames[frameBeforeLast].bonusScore === 0
     ) {
-      frames[frameBeforeLast].setBonusScore(
-        10 + frames[currentFrame].frameScore()
-      );
+      let bonusScore =
+        frames[currentFrame].frameScore() + frames[previousFrame].frameScore();
+      frames[frameBeforeLast].setBonusScore(bonusScore);
     }
   }
 

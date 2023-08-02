82edd63adfab086879dc059791cf5afbc0595d9d
Graeme Stirling <graemejstirling@gmail.com>
2023-08-02 18:09:57 +0100

    refactor

--- src/scorecard.js
+++ src/scorecard.js
@@ -4,9 +4,6 @@ module.exports = class Scorecard {
     }
 
     addFrame(frame){
-        if (this.#sum(frame) > 10){
-            throw new Error("Frame Invalid")
-        }
         if (this.frames.length == 10) {
             throw new Error("You can only have ten frames on a scorecard")
         }
@@ -23,7 +20,6 @@ module.exports = class Scorecard {
 
 
     #bonus(frameIndex){
-        // if on end frame no bonus
         if (frameIndex >= this.frames.length - 1) {
             return 0;
         }
@@ -34,56 +30,40 @@ module.exports = class Scorecard {
             return this.#finalFrame(currentFrame, nextFrame);
         }
 
-        // Is current frame a double
-        if (currentFrame[0] === 10 && nextFrame[0] === 10){
-            const sumOfBonusFrame = this.frames[frameIndex+2][0]
-            // streak
-            if(frameIndex >= this.frames.length - 1){
-                return 10 + this.#bonus(frameIndex + 1)
-            }
-            else {
-                return 10 + sumOfBonusFrame + this.#bonus(frameIndex + 1)
-            }
+        if (this.#isStreak(currentFrame, nextFrame)){
+                const frameBonus = this.frames[frameIndex+2][0]
+                return 10 + frameBonus + this.#bonus(frameIndex + 1)
         }
-
-        // Is current frame a strike ?
-        if (currentFrame[0] === 10) {
+        if (this.#isStrike(currentFrame)) {
             const bonus = nextFrame[0] + nextFrame[1];
-
             return bonus + this.#bonus(frameIndex + 1);
         }
-        // if current frame is a spare
-        if (currentFrame[0] + currentFrame[1] === 10) {
+        if (this.#isSpare(currentFrame)) {
             const bonus = nextFrame[0];
             return bonus + this.#bonus(frameIndex + 1);
         }
         return this.#bonus(frameIndex + 1);
     }
 
-    #sum(frame){
-        return frame.reduce((a, b) => a + b, 0)
-    }
-
     #finalFrame(currentFrame, nextFrame) {
-        // turkey or streak
-        if (currentFrame[0] === 10 && nextFrame[0] === 10 && nextFrame[1] === 10){
-            console.log(`Turkey for frame ${currentFrame}`)
-            return 20;
-        }
-        // double
-        if (currentFrame[0] === 10 && nextFrame[0] === 10){
-            console.log(`Double for frame ${currentFrame}`)
+        if (this.#isStreak(currentFrame, nextFrame)){
             return 10 + nextFrame[1];
         }
-        // strike
-        if(currentFrame[0] === 10){
+        if(this.#isStrike(currentFrame, nextFrame)){
             return nextFrame[0] + nextFrame[1]
         }
-        // spare
-        if (currentFrame[0] + currentFrame[1] === 10) {
+        if (this.#isSpare(currentFrame)) {
             return nextFrame[0]
         }
-
         return 0;
     }
+
+    #sum(frame){return frame.reduce((a, b) => a + b, 0)}
+
+    #isStreak(currentFrame, nextFrame){return currentFrame[0] === 10 && nextFrame[0] === 10;}
+
+    #isStrike(currentFrame){return currentFrame[0] === 10;}
+
+    #isSpare(currentFrame){return currentFrame[0] + currentFrame[1] === 10;}
 }
+
--- test/scorecard.test.js
+++ test/scorecard.test.js
@@ -26,15 +26,6 @@ describe("Scorecard", () => {
                 expect(error.message).toBe('You can only have ten frames on a scorecard');
             }
         });
-
-        test('declines invalid frames', () => {
-            try {
-                TestData.EMPTY_SCORECARD.addFrame([10, 10]);
-            } catch (error) {
-                expect(error).toBeInstanceOf(Error);
-                expect(error.message).toBe('Frame Invalid');
-            }
-        });
     });
 
 
@@ -71,7 +62,6 @@ describe("Scorecard", () => {
 
         test('calculate the score with spares and strikes', () => {
             const scorecard = new Scorecard([[3,4],[5,5],[3,0],[10,0],[5,5],[1,2],[0,0],[5,0],[10,0],[5,0]])
-            // 7 + 13 + 3 + 20 + 11 + 3 + 0 + 5 + 15 + 5 = 82
 
             expect(scorecard.total()).toEqual(82);
         });
@@ -79,14 +69,12 @@ describe("Scorecard", () => {
         // double logic is incorrect
         test('calculates a game with doubles', () => {
             const scorecard = new Scorecard([[1,3],[10,0],[10,0],[3,6]])
-            // 4 + 23 + 19 + 9 = 61
 
             expect(scorecard.total()).toEqual(55)
         });
 
         test('calculates a game with a turkey', () => {
             const scorecard = new Scorecard([[1,3],[10,0],[10,0],[10,0],[3,5]])
-            // 4 + 30 + 28 + 18 + 8 = 88
 
             expect(scorecard.total()).toEqual(83)
         });
@@ -97,21 +85,17 @@ describe("Scorecard", () => {
             expect(scorecard.total()).toEqual(145)
         });
 
-        test('calculates a game with a double in second last frame', () => {
+        test('calculates a game with a double in 9th frame', () => {
             const scorecard = new Scorecard([[1,3],[10,0],[10,0],[4,3],[10,0],[10,0],[10,0],[3,2],[10,0],[10,4,3]])
-            // 4 + 24 + 17 + 7 + 30 + 23 + 15 + 5 + 24 + 17 = 166
 
             expect(scorecard.total()).toEqual(166)
         });
 
         test('calculates the perfect game', () => {
             const scorecard = new Scorecard([[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,0],[10,10,10]])
-            // 4 + 27 + 17 + 7 + 25 + 15 + 5
 
             expect(scorecard.total()).toEqual(300)
         });
-
-        // test comment
     });
 
 });

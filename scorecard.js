8e87fa7c5e5c2d1856cdb9987f1c5325a0ca4ecc
Perran Thomas <perran.thomas@icloud.com>
2023-11-02 15:23:07 +0000

    Implemented basic error handling behaviour of Scorecard.addRoll via TDD

--- scorecard.js
+++ scorecard.js
@@ -1,10 +1,20 @@
 class Scorecard {
   constructor() {
-    this.currentFrame = 1;
-    this.rollsMadeThisFrame = 0;
-    this.pinsRemaining = 10;
-    this.activeBonusLifetimes = [];
+    this._currentFrame = 1;
+    this._rollsMadeInCurrentFrame = 0;
+    this._pinsRemaining = 10;
+    this._activeBonusLifetimes = [];
     this.historyLog = [];
+    this.currentScore = 0;
+  }
+  addRoll(pinsHit) {
+    if (
+      !Number.isInteger(pinsHit) || // Use short-circuiting || operator
+      pinsHit < 0 ||
+      pinsHit > 10
+    ) {
+      throw new Error(`${pinsHit} is not a valid value for pinsHit`);
+    }
   }
 }
 
--- scorecard.test.js
+++ scorecard.test.js
@@ -7,10 +7,38 @@ describe("Scorecard", () => {
   });
   it("has values of properties set correctly on construction", () => {
     const scorecard = new Scorecard();
-    expect(scorecard.currentFrame).toBe(1);
-    expect(scorecard.rollsMadeThisFrame).toBe(0);
-    expect(scorecard.pinsRemaining).toBe(10);
-    expect(scorecard.activeBonusLifetimes).toEqual([]);
+    expect(scorecard._currentFrame).toBe(1);
+    expect(scorecard._rollsMadeInCurrentFrame).toBe(0);
+    expect(scorecard._pinsRemaining).toBe(10);
+    expect(scorecard._activeBonusLifetimes).toEqual([]);
     expect(scorecard.historyLog).toEqual([]);
-  })
+    expect(scorecard.currentScore).toBe(0);
+  });
+  it(
+    "on construction, accepts all and only values between 0 and 10" +
+      "inclusive as an argument for .addRoll, throwing an error otherwise",
+    () => {
+      for (let pinsHit = 0; pinsHit <= 10; pinsHit++) {
+        const scorecard = new Scorecard();
+        scorecard.addRoll(pinsHit);
+      }
+      const scorecard1 = new Scorecard();
+      expect(() => {
+        scorecard1.addRoll(-1);
+      }).toThrow(new Error("-1 is not a valid value for pinsHit"));
+      const scorecard2 = new Scorecard();
+      expect(() => {
+        scorecard2.addRoll(11);
+      }).toThrow(new Error("11 is not a valid value for pinsHit"));
+      const scorecard3 = new Scorecard();
+      expect(() => {
+        scorecard3.addRoll(3.14);
+      }).toThrow(new Error("3.14 is not a valid value for pinsHit"));
+    },
+  );
+  // it("sets .rollsMadeInCurrentFrame to 1 after 1 non-strike roll made", () => {
+  //   const scorecard1 = new Scorecard();
+  //   scorecard1.addRoll(0);
+  //   expect(scorecard1.rollsMadeInCurrentFrame).toBe()
+  // });
 });

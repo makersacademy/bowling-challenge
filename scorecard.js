fad1df0987512d9412bb034b89df80941a6085d8
Perran Thomas <perran.thomas@icloud.com>
2023-11-02 15:00:26 +0000

    Implemented Scorecard.constructor via TDD

--- scorecard.js
+++ scorecard.js
@@ -1,5 +1,11 @@
 class Scorecard {
-  constructor() {}
+  constructor() {
+    this.currentFrame = 1;
+    this.rollsMadeThisFrame = 0;
+    this.pinsRemaining = 10;
+    this.activeBonusLifetimes = [];
+    this.historyLog = [];
+  }
 }
 
 module.exports = Scorecard;
--- scorecard.test.js
+++ scorecard.test.js
@@ -5,4 +5,12 @@ describe("Scorecard", () => {
     const scorecard = new Scorecard();
     expect(scorecard).toBeInstanceOf(Scorecard);
   });
+  it("has values of properties set correctly on construction", () => {
+    const scorecard = new Scorecard();
+    expect(scorecard.currentFrame).toBe(1);
+    expect(scorecard.rollsMadeThisFrame).toBe(0);
+    expect(scorecard.pinsRemaining).toBe(10);
+    expect(scorecard.activeBonusLifetimes).toEqual([]);
+    expect(scorecard.historyLog).toEqual([]);
+  })
 });

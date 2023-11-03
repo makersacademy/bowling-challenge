2d6bc14a70308b231bbea4e3c9bddefc58dc7783
Perran Thomas <perran.thomas@icloud.com>
2023-11-03 14:14:03 +0000

    Commented out buggy version of UserInterface

--- userInterface.js
+++ userInterface.js
@@ -1,18 +1,34 @@
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
+// === Buggy version ===
+// class UserInterface {
+//   constructor(scorecard) {
+//     this.scorecard = scorecard;
+//     this.rl = readline.createInterface({ input, output });
+//   }
+//   tellMeTheScore() {
+//     this.rl.question("What is your name? ", (answer) => {
+//       const score = this.scorecard.getGameStateInfo().gameInfo.score;
+//       this.rl.write(
+//         `Thanks, ${answer}. The current score is ${score}.`
+//       );
+//       this.tellMeTheScore();
+//     });
+//   }
+// }
+
 class UserInterface {
   constructor(scorecard) {
     this.scorecard = scorecard;
     this.rl = readline.createInterface({ input, output });
   }
   tellMeTheScore() {
-    this.rl.question("What is your name? ", (answer) => {
-      const score = this.scorecard.getGameStateInfo().gameInfo.score;
+    this.rl.question("What is your name?", (answer) => {
+      const score = this.scorecard.getGameStateInfo.gameInfo.score;
       this.rl.write(
         `Thanks, ${answer}. The current score is ${score}.`
       );
-      this.tellMeTheScore();
     });
   }
 }

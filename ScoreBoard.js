9da91e35080f6dd1afa92f367e91b28055fa9b22
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-17 20:51:22 +0000

    working on the logic

--- ScoreBoard.js
+++ ScoreBoard.js
@@ -1,4 +1,4 @@
-class ScoreBoard {
+class scoreBoard {
 
     constructor() {
         this.frames = { 'frame01': 0, 'frame02': 0}
@@ -7,10 +7,23 @@ class ScoreBoard {
         this.notes = ""
     }
 
-
     calculateScore() {
        return this.totalScore
     }
+
+    game_01() {
+        for (let i = 1; i <= 2; i++) {
+            if (i == 1) {
+                this.frames['frame01'] = i
+                this.frames['frame02'] = i
+                this.rolls['roll01'] = i
+            } else {
+                this.rolls['roll02'] = i
+            }
+        }
+        return [this.frames, this.rolls]
+    }   
+
     noted () {
         note01 = [`Spare: 10 pins plus bonus of 5 from next roll (roll 1 of frame 4)`]
         note02 = ['Strike']

5a38cddac3b1eff756ef47b07c8b58fc203d4a3e
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-18 11:22:43 +0000

    changes in the logic to make the game more fair

--- Pin.js
+++ Pin.js
@@ -3,21 +3,20 @@ class bowlingPin {
         this.rolls = { 'rollOne' : 0,
                        'rollTwo' : 0 }
     }
-
+    // changing the logic here, I will do a random first, and then i will do a random on the remaining items, example, if in the first rowll I got 7 points, 
+    // the second random will be 10 - 7 == 3, so the second round I can only get the remaining pins
     roll() {
-        for (let i = 1; i <= 2; i++) {
-            let randomNum = Math.floor(Math.random() * 10) + 1;
-            if (i == 1) {
-                this.rolls['rollOne'] = randomNum
-            } else {
-                this.rolls['rollTwo'] = randomNum
-            }
-        }
+        let first_row = Math.floor(Math.random() * 10) + 1;
+        let second = 10 - first_row
+        let second_row = Math.floor(Math.random() * second) + 1;
+        this.rolls['rollOne'] = first_row
+        this.rolls['rollTwo'] = second_row
         return this.rolls
     }
 }
 
 
 module.exports.bowlingPin = bowlingPin;
+
 
 

34c64476412d9ac11bcabb3b8ef554034801c6eb
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-01-18 11:01:42 +0000

    working on the logic for the game

--- game.js
+++ game.js
@@ -10,6 +10,10 @@ class Game {
         this.scoreBoard = new scoreBoard();
         this.game_is_on = true;
         this.number_of_plays = 0;
+        this.rollResult = this.bowlingPinInstance.roll();
+        this.scoreBoard_game_01 = this.scoreBoard.game_01();
+        this.strike = false;
+        this.spare = false
 
         this.rl = readline.createInterface({
             input: process.stdin,
@@ -17,42 +21,59 @@ class Game {
         });
     }
 
-    rollAndAsk(playername, turn = 1) {
+    rollAndAsk(playerName, turn = 1) {
         if (turn > 10) {
             console.log("Game over");
             this.rl.close();
             return;
         }
 
-        let rollResult = this.bowlingPinInstance.roll();
-        let player = this.player.add(playername);
-        let scoreBoard = this.scoreBoard.game_01();
-        console.log(rollResult, player, scoreBoard);
-        console.log(`Turn ${turn}`);
-
-        // Additional game logic here
-
-        this.rl.question("Roll again? (press Enter for yes, type 'no' to stop) ", (answer) => {
-            if (answer.toLowerCase() !== 'no') {
-                this.rollAndAsk(playername, turn + 1);
+        if (turn == 1) {
+            this.rl.question(`Alright, this is the First Turn, please press enter to start bowling or 'no' to stop and end the game: `, (answer) => {
+                if (answer.toLowerCase() !== 'no') {
+                    console.log(this.rollResult);
+                    if (this.rollResult.rollOne == 10) {
+                        console.log("WOW, You got a Strike");
+                        this.rollResult.rollTwo = 0;
+                        this.strike = true;
+                        this.rollAndAsk(playerName, turn + 1);
+                    } else if (this.rollResult.rollOne + this.rollResult.rollTwo >= 10) {
+                        console.log("Nice, You got a Spare");
+                        
+                    }
+                    this.rollAndAsk(playerName, turn + 1);
+                } else {
+                    console.log("Game stopped");
+                    this.rl.close();
+                }
+            });
+        } else { 
+            this.rl.question(`Turn: ${turn} Roll again? (press Enter for yes, type 'no' to stop) `, (answer) => {
+                if (answer.toLowerCase() !== 'no') {
+                    console.log(this.rollResult, this.bowlingPinInstance.roll());
+                    this.scoreBoard.frames['frame01'] += 1;
+                    this.scoreBoard.frames['frame02'] += 1;
+                    this.rollAndAsk(playerName, turn + 1);
+                } else {
+                    console.log("Game stopped");
+                    this.rl.close();
+                }
+            });
+        }
+    }
+    
+    is_game_on(playerName) {
+        this.player.add(playerName);
+        this.rl.question(`Hi ${playerName} Are you ready to play? (press Enter to start) `, (answer) => {
+            if (answer.toLowerCase() !== 'no') { 
+                this.rollAndAsk(playerName);
             } else {
-                console.log("Game stopped");
+                console.log("Alright, we are not gonna start the game.");
                 this.rl.close();
             }
         });
     }
-
-    is_game_on() {
-        this.rl.question("Are you ready to play? (press Enter to start) ", (answer) => {
-            this.rollAndAsk('gustavo');
-        });
-    }
-
-    next_game() {
-        this.scoreBoard.frames['frame01'] += 1;
-        this.scoreBoard.frames['frame02'] += 1;
-    }
 }
 
 let game = new Game();
-game.is_game_on(); // Start the game with a prompt
+game.is_game_on("Gustavo"); // Start the game with a prompt
